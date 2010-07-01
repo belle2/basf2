90abec4bb5ba0e7056d972018e7f090d4217135f
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2010-07-01 16:27:11 +0000

    fix dependencies for dictionaries
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@567 b38c6c8f-bee8-465d-8448-9abe6915d496

--- site_scons/site_tools/root_dict.py
+++ site_scons/site_tools/root_dict.py
@@ -1,5 +1,6 @@
 import os, re
 from SCons.Builder import Builder
+from SCons.Scanner.C import CScanner
 
 # regular expression to find class names in linkdef files
 linkdef_class_re = re.compile(r'^#pragma\s+link\s+C\+\+\s+class\s+Belle2::(\S+);\s*$', re.M)
@@ -7,19 +8,29 @@ linkdef_class_re = re.compile(r'^#pragma\s+link\s+C\+\+\s+class\s+Belle2::(\S+);
 # define emitter for linkdef files:
 # add all header files for classes which are listed in the linkdef file
 def linkdef_emitter(target, source, env):
+
+    # determine the right include directory
+    include_dir = os.path.dirname(str(source[0]))
+    if include_dir.endswith('include'):
+        include_dir = os.path.dirname(include_dir)
+    include_dir = os.path.join(env['INCDIR'], include_dir)
+
+    # loop over class names and construct the corresponding header file names
     sources = []
     contents = source[0].get_text_contents()
-    source_dir = os.path.dirname(str(source[0]))
     for entry in linkdef_class_re.findall(contents):
-        header_file = os.path.join(source_dir, entry.split('<')[0] + '.h')
-        if not header_file in sources:
-            sources.append(header_file)
+        include_base = entry.split('<')[0] + '.h'
+        include_file = os.path.join(include_dir, include_base)
+        if not include_file in sources:
+            sources.append(include_file)
+
     sources.append(source[0])
     return target, sources
 
 # define builder for root dictionaries
 rootcint = Builder(action = "rootcint -f $TARGET -c $_CPPDEFFLAGS $_CPPINCFLAGS $SOURCES",
-                   emitter = linkdef_emitter)
+                   emitter = linkdef_emitter,
+                   source_scanner = CScanner())
 
 def generate(env):
     env['BUILDERS']['RootDict'] = rootcint

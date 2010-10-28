686f43d88ad62dfed95b51b6c1239934d6ad158e
Oksana Brovchenko <oksana.brovchenko@kit.edu>
2010-10-28 18:18:45 +0000

    cdcTracking modul added
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@839 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/modules/karlsruhe/include/CDCTrackingModule.h
@@ -0,0 +1,91 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                   *
+ * Copyright(C) 2010 - Belle II Collaboration                       *
+ *                                    *
+ * Author: The Belle II Collaboration                     *
+ * Contributors: Oksana Brovchenko                          *
+ *                                    *
+ * This software is provided "as is" without any warranty.          *
+**************************************************************************/
+
+#ifndef CDCTRACKINGMODULE_H
+#define CDCTRACKINGMODULE_H
+
+#include <framework/core/Module.h>
+#include <fstream>
+
+namespace Belle2 {
+
+//! Module CDCTrackingModule
+  /*!
+    The CDCTrackingModule performs the first pattern recognition step in the CDC through conformal transformation of hit coordinates. First Digitized CDCHits (CDCDigitizer module should be executed before this module) are combined to Segments. Then Segments from axial superlayers are combined to Tracks. In the following stereo Segments are assigned to these Tracks.
+  */
+  class CDCTrackingModule : public Module {
+
+  public:
+    //! Returns a new instance of the module.
+    // virtual ModulePtr newModule() { ModulePtr nm(new CDCTrackingModule(false)); return nm; };
+
+    //! Constructor
+    /*!
+      Create and allocate memory for variables here. Add the module parameters in this method.
+      \param selfRegisterType True if this module is self-registering, otherwise false.
+    */
+    CDCTrackingModule();
+
+    //! Destructor
+    /*!
+       Use the destructor to release the memory you allocated in the constructor.
+    */
+    virtual ~CDCTrackingModule();
+
+    //! Initialize the Module
+    /*! This method is called only once before the actual event processing starts.
+        Use this method to initialize variables, open files etc.
+    */
+    virtual void initialize();
+
+    //! Called when entering a new run
+    /*! Called at the beginning of each run, the method gives you the chance to change run dependent constants like alignment parameters, etc.
+    */
+    virtual void beginRun();
+
+    //! This method is the core of the module.
+    /*! This method is called for each event. All processing of the event has to take place in this method.
+    */
+    virtual void event();
+
+    //! This method is called if the current run ends.
+    /*! Use this method to store information, which should be aggregated over one run.
+    */
+    virtual void endRun();
+
+    //! This method is called at the end of the event processing.
+    /*! This method is called only once after the event processing finished.
+        Use this method for cleaning up, closing files, etc.
+    */
+    virtual void terminate();
+
+  protected:
+
+
+  private:
+
+    std::string m_inSimHitsColName; /*!< Input simulated hits collection name (should already be used by the CDCDigitizer module) */
+    std::string m_inHitsColName; /*!< Input digitized hits collection name (output of CDCDigitizer module) */
+    std::string m_outTrackHitsColName; /*!< Output hits collection name (digitized hits collection changed to be used in tracking) */
+    std::string m_outSegmentsColName; /*!< Output segments collection name (tracklets within one superlayer) */
+    std::string m_outTracksColName; /*!< Output tracks collection name*/
+
+    bool m_textFileOutput;
+
+    std::ofstream SimHitsfile;
+    std::ofstream Hitsfile;
+    std::ofstream ConfHitsfile;
+    std::ofstream Tracksfile;
+    std::ofstream ConfTracksfile;
+
+  };
+} // end namespace Belle2
+#endif
+
---
+++ tracking/modules/karlsruhe/src/CDCTrackingModule.cc
@@ -0,0 +1,264 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                   *
+ * Copyright(C) 2010 - Belle II Collaboration                       *
+ *                                    *
+ * Author: The Belle II Collaboration                     *
+ * Contributors: Oksana Brovchenko                          *
+ *                                    *
+ * This software is provided "as is" without any warranty.          *
+**************************************************************************/
+
+#include <tracking/modules/karlsruhe/CDCTrackingModule.h>
+#include <framework/core/ModuleManager.h>
+
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/StoreDefs.h>
+
+#include <framework/logging/Logger.h>
+#include <cdc/hitcdc/SimHitCDC.h>
+#include <cdc/simcdc/CDCHit.h>
+
+#include <tracking/karlsruhe/CDCTrackHit.h>
+#include <tracking/karlsruhe/CDCSegment.h>
+#include <tracking/karlsruhe/CDCTrack.h>
+
+#include <tracking/karlsruhe/AxialTrackFinder.h>
+#include <tracking/karlsruhe/SegmentFinder.h>
+#include <tracking/karlsruhe/StereoFinder.h>
+#include <tracking/karlsruhe/HitSalvager.h>
+
+
+#include <cstdlib>
+#include <iomanip>
+#include <string>
+
+#include <iostream>
+
+using namespace std;
+using namespace Belle2;
+
+REG_MODULE(CDCTrackingModule, "CDCTrackingModule")
+
+
+CDCTrackingModule::CDCTrackingModule() : Module()
+{
+  setDescription("The CDCTrackingModule performs the first rough pattern recognition step in the CDC. Digitized CDC Hits are combined to Tracks");
+
+  addParam("InputSimHitsColName", m_inSimHitsColName, string("SimHitCDCArray"), "Input simulated hits collection name");
+  addParam("InputHitsColName", m_inHitsColName, string("HitCDCArray"), "Input digitized hits collection name");
+  addParam("OutputTrackHitsColName", m_outTrackHitsColName, string("TrackHitCDCArray"), "Output hits (slightly changed digitized hits) collection name");
+  addParam("OutputSegmentsColName", m_outSegmentsColName, string("SegmentCDCArray"), "Output segments collection name");
+  addParam("OutputTracksColName", m_outTracksColName, string("TrackCDCArray"), "Output tracks collection name");
+  addParam("TextFileOutput", m_textFileOutput, false, "Boolean to choose if some text files with hit coordinates should be created");
+
+}
+
+CDCTrackingModule::~CDCTrackingModule()
+{
+}
+
+void CDCTrackingModule::initialize()
+{
+  if (m_textFileOutput) {
+    //open the output txt files
+    SimHitsfile.open("SimHits.txt");
+    Hitsfile.open("Hits.txt");
+    ConfHitsfile.open("ConfHits.txt");
+    Tracksfile.open("Tracks.txt");
+    ConfTracksfile.open("ConfTracks.txt");
+  }
+
+}
+
+void CDCTrackingModule::beginRun()
+{
+  INFO("CDCTrackingModule beginRun");
+}
+
+void CDCTrackingModule::event()
+{
+
+  //StoreArray with simulated CDCHits, should already be created by previous modules
+  StoreArray<SimHitCDC> cdcSimHitArray(m_inSimHitsColName);
+  INFO("Number of simulated Hits:  " << cdcSimHitArray.GetEntries());
+
+
+  //StoreArray with digitized CDCHits, should already be created by CDCDigitized module
+  StoreArray<HitCDC> cdcHitArray(m_inHitsColName);
+
+  //StoreArray with CDCTrackHits, a class derived from HitCDC but with additional member variables needed for tracking.
+  StoreArray<CDCTrackHit> cdcTrackHitArray(m_outTrackHitsColName);
+
+  //Fill the new StoreArray with CDCTrackHits
+  for (int i = 0; i < cdcHitArray.GetEntries(); i++) {
+
+    HitCDC *aHit = cdcHitArray[i];
+    CDCTrackHit  aTrackHit = CDCTrackHit::castTrackHit(*aHit);
+    new(cdcTrackHitArray->AddrAt(i)) CDCTrackHit(aTrackHit);
+  }
+
+  int NHits = cdcTrackHitArray.GetEntries();
+  INFO("Number of digitized Hits: " << NHits);
+
+  //Calculates the coordinates in the conformal plane for each TrackHit in the StoreArray
+  SegmentFinder::ConformalTransformation(m_outTrackHitsColName);
+
+
+  //create StoreArray for CDCSegments
+  StoreArray<CDCSegment> cdcSegmentsArray(m_outSegmentsColName);
+
+  //Combine CDCTrackHits to CDCSegment, fill cdcSegmentsArray with new created Segments
+  INFO("Searching for Segments... ");
+  SegmentFinder::FindSegments(m_outTrackHitsColName, m_outSegmentsColName);
+  //Count good and bad segments
+  int goodSeg = 0;
+  int badSeg = 0;
+
+  for (int i = 0; i < cdcSegmentsArray.GetEntries(); i++) {
+    if (cdcSegmentsArray[i]->getIsGood() == true) goodSeg++;
+    else badSeg++;
+  }
+
+  INFO("Number of found Segments: " << cdcSegmentsArray.GetEntries() << " (good: " << goodSeg << ", bad: " << badSeg << " )");
+
+
+  INFO("Superlayer Segment Id  Nr of Hits ");
+
+  for (int j = 0; j < cdcSegmentsArray.GetEntries(); j++) {
+
+    INFO("     " << cdcSegmentsArray[j]->getSuperlayerId() << "           " << cdcSegmentsArray[j]->getId() << "         " << cdcSegmentsArray[j]->getNHits());
+  }
+
+//Divide Segments into Axial and Stereo, create two new StoreArrays
+
+  StoreArray<CDCSegment> cdcAxialSegmentsArray("AxialSegmentsCDCArray");
+
+  StoreArray<CDCSegment> cdcStereoSegmentsArray("StereoSegmentsCDCArray");
+
+
+  int j_axial = 0;
+  int j_stereo = 0;
+
+  for (int i = 0; i < cdcSegmentsArray.GetEntries(); i++) {
+
+    if (cdcSegmentsArray[i]->getIsAxial()) {
+
+      new(cdcAxialSegmentsArray->AddrAt(j_axial)) CDCSegment(*cdcSegmentsArray[i]);
+      j_axial++;
+    }
+
+    else {
+
+      new(cdcStereoSegmentsArray->AddrAt(j_stereo)) CDCSegment(*cdcSegmentsArray[i]);
+      j_stereo++;
+
+    }
+  }
+
+//create StoreArray for Tracks
+  StoreArray<CDCTrack> cdcTracksArray(m_outTracksColName);
+//combinde Axial Segments to Tracks, fill cdcTracksArray with new Tracks
+  INFO("Connect axial Segments...");
+  AxialTrackFinder::ConnectSegments("AxialSegmentsCDCArray", m_outTracksColName); //assigns Segments to Tracks, returns a Tracks array
+
+  INFO("Number of Tracks: " << cdcTracksArray.GetEntries());
+  INFO("Track Id  Nr of Segments  Nr of Hits: ");
+
+
+  for (int j = 0; j < cdcTracksArray.GetEntries(); j++) { //loop over all Tracks
+
+    INFO("  " << cdcTracksArray[j]->getId()  << "             " << cdcTracksArray[j]->getNSegments() << "           " << cdcTracksArray[j]->getNHits());
+
+  }//end loop over all Tracks
+
+//Append Stereo Segment to existing Tracks
+  INFO("Append stereo Segments...");
+  StereoFinder::AppendStereoSegments("StereoSegmentsCDCArray", m_outTracksColName);
+  INFO("Salvage axial Hits...");
+  HitSalvager::SalvageHits("AxialSegmentsCDCArray", m_outTracksColName, 0.0005);
+  INFO("Salvage stereo Hits...");
+  HitSalvager::SalvageHits("StereoSegmentsCDCArray", m_outTracksColName, 0.0005);
+
+  INFO("Track Id  Nr of Segments  Nr of Hits: ");
+
+  for (int j = 0; j < cdcTracksArray.GetEntries(); j++) { //loop over all Tracks
+
+    INFO("  " << cdcTracksArray[j]->getId() << "             " << cdcTracksArray[j]->getNSegments() << "           " << cdcTracksArray[j]->getNHits());
+  }//end loop over all Tracks
+
+
+  //calculate the fraction of used hits
+  int usedHits = 0;
+  for (int j = 0; j < cdcTracksArray.GetEntries(); j++) {
+    usedHits = usedHits + cdcTracksArray[j]->getNHits();
+  }
+  double fraction = double(usedHits) / double(NHits);
+  INFO(std::setprecision(3) << fraction*100 << " %" << "  of all hits were used to reconstruct " << cdcTracksArray.GetEntries() << " tracks");
+
+
+
+  if (m_textFileOutput) {
+
+//Dump the position information of simulated Hits to a textfile
+    for (int i = 0; i < cdcSimHitArray.GetEntries(); i++) { //loop over all SimHits
+      SimHitCDC *aSimHit = cdcSimHitArray[i];
+      //prints the position of simulated axial Hits to a file
+      SimHitsfile << std::setprecision(5) << (aSimHit->getPosIn()[0] + aSimHit->getPosOut()[0]) / 2. / mm << " \t"
+      << (aSimHit->getPosIn()[1] + aSimHit->getPosOut()[1]) / 2. / mm << " \t"
+      << (aSimHit->getPosIn()[2] + aSimHit->getPosOut()[2]) / 2. / mm << " \t"
+      << aSimHit->getDriftLength() / mm << endl;
+    }
+
+
+//Dump the position information of digitized Hits to a textfile
+    for (int i = 0; i < NHits; i++) {     //loop over all Hits
+      CDCTrackHit *aTrackHit = cdcTrackHitArray[i];
+
+      //Prints the Hit position to output files
+      Hitsfile << std::setprecision(5) << aTrackHit->getWirePosX() / mm << " \t"
+      <<  aTrackHit->getWirePosY() / mm << " \t"
+      <<  aTrackHit->getWirePosZ() / mm << " \t"
+      << aTrackHit->getDriftLength() / mm << endl;
+
+      ConfHitsfile << std::setprecision(5) << aTrackHit->getConformalX() / mm << " \t"
+      <<  aTrackHit->getConformalY() / mm << endl;
+    }
+
+//Dump the position information of digitized Hits *trackwise* to a textfile. First number which indicates the number of Hits in this track is folowed by coordinates of Hits assigned to this track.
+    for (int j = 0; j < cdcTracksArray.GetEntries(); j++) { //loop over all Tracks
+      Tracksfile << cdcTracksArray[j]->getNHits() ;
+      ConfTracksfile << cdcTracksArray[j]->getNHits() ;
+
+      for (int hit = 0; hit < cdcTracksArray[j]->getNHits(); hit++) {//loop over all Hits
+        Tracksfile << "\t" << std::setprecision(5) << cdcTracksArray[j]->getTrackHits()[hit].getWirePosX() / mm << " \t"
+        <<  cdcTracksArray[j]->getTrackHits()[hit].getWirePosY() / mm << " \t"
+        <<  cdcTracksArray[j]->getTrackHits()[hit].getWirePosZ() / mm << endl;
+
+        ConfTracksfile << "\t" << std::setprecision(5) << cdcTracksArray[j]->getTrackHits()[hit].getConformalX() / mm << " \t"
+        <<  cdcTracksArray[j]->getTrackHits()[hit].getConformalY() / mm << " \t"
+        << "0" << endl;
+
+      }
+
+    }//end loop over all Tracks
+  }//endif m_textFileOutput
+}
+
+void CDCTrackingModule::endRun()
+{
+
+}
+
+void CDCTrackingModule::terminate()
+{
+  if (m_textFileOutput) {
+    SimHitsfile.close();
+    Hitsfile.close();
+    ConfHitsfile.close();
+    Tracksfile.close();
+    ConfTracksfile.close();
+  }
+}
+
+
+

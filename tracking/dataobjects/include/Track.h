45b35f7011c2a06b9ed4dc258f88687f75ede5e3
Martin Heck <martin.heck@kit.edu>
2010-10-29 01:33:52 +0000

    Added a relatively simple track object
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@841 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/dataobjects/include/Track.h
@@ -0,0 +1,205 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Martin Heck                                              *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#ifndef TRACK_H
+#define TRACK_H
+
+#include <TObject.h>
+
+namespace Belle2 {
+
+  /*! Track parameters. */
+  /*! This Track class is inspired by the lcio track implementation ( see http://lcio.desy.de/v01-51/doc/doxygen_api/html/classIMPL_1_1TrackImpl.html )
+      \author <a href="mailto:martin.heck@kit.edu?subject=Track">Martin Heck</a>
+  */
+  class Track : public TObject {
+  public:
+
+    /*! Constructor. */
+    /*! This is as well the parameter free I/O constructor.
+    */
+    Track() {
+    }
+
+    /*! Destructor. */
+    ~Track() {
+    }
+
+    // Getters for all the parameters.
+    /*! Getter for Impact Parameter.
+    */
+    float getD0() {
+      return m_d0;
+    }
+
+    /*! Getter for phi
+    */
+    float getPhi() {
+      return m_phi;
+    }
+
+    /*! Getter for omega.
+    */
+    float getOmega() {
+      return m_omega;
+    }
+
+    /*! Getter for z0.
+    */
+    float getZ0() {
+      return m_z0;
+    }
+
+    /*! Getter for cotTheta
+    */
+    float getCotTheta() {
+      return m_cotTheta;
+    }
+
+    //Some non-helix parameters.
+    /*! Getter for Chi2 of track.
+    */
+    float getChi2() {
+      return m_chi2;
+    }
+
+    /*!Getter for energy loss in the tracking detectors.
+     */
+    float getdEdx() {
+      return m_dEdx;
+    }
+
+    /*! Getter for uncertainty on energy loss in tracking detectors.
+    */
+    float getdEdxSigma() {
+      return m_dEdxSigma;
+    }
+
+    /*! Getter for radius of innermost hit contributing to the track.
+    */
+    float getRadiusOfInnermostHit() {
+      return m_radiusOfInnermostHit;
+    }
+
+    // Setters for all the parameters.
+    /*! Setter for Impact Parameter.
+    */
+    void setD0(const float& d0) {
+      m_d0 = d0;
+    }
+
+    /*! Setter for phi
+    */
+    void setPhi(const float& phi) {
+      m_phi = phi;
+    }
+
+    /*! Setter for omega.
+    */
+    void setOmega(const float& omega) {
+      m_omega = omega;
+    }
+
+    /*! Setter for z0.
+    */
+    void setZ0(const float&z0) {
+      m_z0 = z0;
+    }
+
+    /*! Setter for cotTheta
+    */
+    void setCotTheta(const float& cotTheta) {
+      m_cotTheta = cotTheta;
+    }
+
+    //Some non-helix parameters.
+    /*! Setter for Chi2 of track.
+    */
+    void setChi2(const float& chi2) {
+      m_chi2 = chi2;
+    }
+
+    /*! Setter for energy loss in the tracking detectors.
+     */
+    void setdEdx(const float& dEdx) {
+      m_dEdx = dEdx;
+    }
+
+    /*! Setter for uncertainty on energy loss in tracking detectors.
+    */
+    void setdEdxSigma(const float& dEdxSigma) {
+      m_dEdxSigma = dEdxSigma;
+    }
+
+    /*! Setter for radius of innermost hit contributing to the track.
+    */
+    void setRadiusOfInnermostHit(const float& radiusOfInnermostHit) {
+      m_radiusOfInnermostHit = radiusOfInnermostHit;
+    }
+
+
+  private:
+
+    /*! Impact Parameter.
+        The Impact Parameter is the two-dimensional point of closest approach of a track to the origin.
+        A sign is assigned such, that if the projection of the momentum on this distance points to the origin,
+        d0 is negative.
+    */
+    float m_d0;
+
+    /*! Phi at the perigee.
+        This is the angle of the track momentum at the perigee in the plane perpendicular to the detector axis.
+    */
+    float m_phi;
+
+    /*! Signed curvature of the track.
+        The curvature is 1/(2R) where R is the radius of the track measured in cm.
+        It is negative if we have a negatively charged track leaving the origin.
+    */
+    float m_omega;
+
+    /*! z Position of the perigee.
+        z is parallel to the detector axis.
+    */
+    float m_z0;
+
+    /*! Cotangens of polar angle theta.
+        This equals dz/ds with s the path length in R-Phi at the perigee.
+    */
+    float m_cotTheta;
+
+    /*! Chi2 of track fit.
+        This is a measure for the quality of the track.
+    */
+    float m_chi2;
+
+    /*! dE/dx in the tracking detectors.
+        For the moment, this is intended to be used with the CDC.
+        We have to see, how to handle the energy loss in the other detectors.
+    */
+    float m_dEdx;
+
+    /*! Uncertainty on energy loss in tracking detectors.
+    */
+    float m_dEdxSigma;
+
+    /*! Radius of innermost hit contributing to the track.
+        This gives a hint, if we may have to do it with a track from a V0 particle.
+    */
+    float m_radiusOfInnermostHit;
+
+
+    /*! ROOT Macro to make Track a ROOT class.*/
+    /*!
+    */
+    ClassDef(Track, 1);
+
+  }; //class
+} // namespace Belle2
+#endif // TRACK
---
+++ tracking/dataobjects/src/Track.cc
@@ -0,0 +1,17 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Martin Heck, Andreas Moll                                *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <tracking/storage/Track.h>
+
+using namespace std;
+using namespace Belle2;
+
+ClassImp(Track)
+

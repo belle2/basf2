997eb3b3b2756d262a3a16f344751f12446f0148
Martin Heck <martin.heck@kit.edu>
2010-11-23 12:04:21 +0000

    changed doxygen description + JAVA style instead of QT Style
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@961 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/modules/simpleoutput/include/simpleoutput.h
+++ framework/modules/simpleoutput/include/simpleoutput.h
@@ -31,48 +31,59 @@
 
 namespace Belle2 {
 
-  /*! Module Simple Output */
-  /*! This module writes the objects from the DataStore into a root file.
-      Please make sure, that you create the same objects in each call of event,
-      otherwise the module might not write out everything or has dangling pointers.
-      For more information consult the TWiki basf2 Software Portal.
-      \sa EDurability
-      \author <a href="mailto:martin.heck@kit.edu?subject=Output Module">Martin Heck</a>
-  */
+  /** Write objects from DataStore into a ROOT file.
+   *
+   *  Please make sure, that you create the same objects in each call of event,
+   *  otherwise the module might not write out everything or has dangling pointers.
+   *  For more information consult the TWiki basf2 Software Portal.
+   *
+   *  @sa EDurability
+   *  @author <a href="mailto:martin.heck@kit.edu?subject=Output Module">Martin Heck</a>
+   */
   class SimpleOutput : public Module {
 
   public:
 
-    /*! Constructor */
+    /** Constructor.
+     *
+     *  The following module properties are set: <br>
+     */
     SimpleOutput();
 
-    /*! Destructor */
+    /** Destructor.
+     *
+     *  Deletion of objects, that were created in the Constructor.
+     */
     virtual ~SimpleOutput();
 
-    /*! Initialize the Module */
-    /*! Opens a file, creates TTree(s), fetches Iterators over the maps of the DataStore,
-        sets m_done to false.
-    */
+    /** Setting up of various stuff.
+     *
+     *  Opens a file, creates TTree(s), fetches Iterators over the maps of the DataStore,
+     *  sets m_done to false.
+     */
     virtual void initialize();
 
-    /*! Does nothing.
-    */
+    /** Does nothing.
+     */
     virtual void beginRun();
 
-    /*! Running over all events */
-    /*! Loops over all objects in event maps and writes to event-TTree.
-    */
+    /** Write data in c_Event DataStore maps.
+     *
+     *  Loops over all objects in event maps (in the first call of the function) and writes them to event-TTree.
+     */
     virtual void event();
 
-    /*! Is called after processing the last event of a run */
-    /*! Loops over all objects in run maps and writes to run-TTree.
-    */
+    /** Write data in c_Run DataStore maps.
+     *
+     *  Loops over all objects in run maps (in the first call of the function) and writes them to run-TTree.
+     */
     virtual void endRun();
 
-    /*! Is called at the end of your Module */
-    /*! Loops over all objects in persistent maps and writes to persistent-TTree.
-        Writes TTree(s) out.
-    */
+    /** Write data in the c_Persistent DataStore maps.
+     *
+     *  Loops over all objects in persistent maps and writes them to persistent-TTree.
+     *  Finally the TTree(s) is/are written out.
+     */
     virtual void terminate();
 
 
@@ -80,97 +91,114 @@ namespace Belle2 {
 
 
   private:
-    /*! Find out, how many objects and arrays, actually are in the datastore. */
-    /*! Used to fill m_size and m_sizeObj.
-        \return Number of elements in the respective map.
-        \par mapID mapID is 2 times durability plus one for the array maps.
-    */
+    /** Find out, how many objects and arrays, actually are in the datastore.
+     *
+     *  Used to fill m_size and m_sizeObj.
+     *
+     *  @return    Number of elements in the respective map.
+     *  @par mapID mapID is 2 times durability plus one for the array maps.
+     */
     size_t getSize(const int& mapID);
 
-    /*! Configuring the TFile. */
+    /** Configuring the TFile.
+     *
+     *  Checks for duplicates in branchName vectors.
+     */
     void setupTFile();
 
-    /*! Fill TTree. */
-    /*! Read the objects from the DataStore and store them in m_objects.
-        m_objects is connected with the tree, so the ROOT TTree::Fill() function can be called.
-        \par durability Specifies map and tree to be used.
-    */
+    /** Fill TTree.
+     *
+     *  Read the objects from the DataStore and store them in m_objects.
+     *  m_objects is connected with the tree, so the ROOT TTree::Fill() function can be called.
+     *
+     *  @par durability Specifies map and tree to be used.
+     */
     void fillTree(const EDurability& durability);
 
-    /*! Switch branch name meaning function. */
-    /*! This function reads the elements from the respective durability map, eliminates the members
-        that are in the inital branchNames vector and returns the remaining ones.
-        \param durability Specifies branchNames to be taken.
-    */
+    /** Switch branch name meaning function.
+     *
+     *  This function reads the elements from the respective durability map, eliminates the members
+     *  that are in the inital branchNames vector and returns the remaining ones.
+     *  This way you can give a list of names, that should NOT be written out, instead
+     *  of a list of names to be written out.
+     *
+     *  @param durability Specifies branchNames to be taken.
+     */
     void switchBranchNameMeaning(const EDurability& durability);
 
 
     //first the steerable variables:
 
-    /*! Name for output file. */
-    /*! This string is steerable.
-        Best practice: Let it end on .root
-    */
+    /** Name for output file.
+     *
+     *  This string is steerable.
+     *  Best practice: Let it end on .root
+     */
     std::string m_outputFileName;
 
-    /*! Name array for output tree names. */
-    /*! Each element of the array is steerable as separate string.
-    */
+    /** Name array for output tree names.
+     *
+     *  Each element of the array is steerable as separate string.
+     */
     std::string m_treeNames[c_NDurabilityTypes];
 
-    /*! Array for names of branches, that shall be written out. */
-    /*! Empty vectors result in all branches of the specific durability being written.
-        These vectors can be configured in the steering file.
-    */
+    /** Array for names of branches, that shall be written out.
+     *
+     *  Empty vectors result in all branches of the specific durability being written.
+     *  These vectors can be configured in the steering file.
+     */
     std::vector<std::string> m_branchNames[c_NDurabilityTypes];
 
-    /*! Switch branchNames from exclusion to inclusion list. */
-    /*!
-    */
+    /** Switch branchNames from exclusion to inclusion list.
+     *
+     * Switching happens, if the variable is true.
+     */
     bool m_switchBranchNameMeaning;
 
-    /*! TFile compression level. */
-    /*! Set the TFile compression level.
-        Should be between 1 for low compression and 9 for much compression.
-        <a href="http://root.cern.ch/root/html/src/TFile.cxx.html#b.RlJE">See the ROOT page for more information</a>.
-    */
+    /** TFile compression level.
+     *
+     *  Set the TFile compression level.
+     *  Should be between 1 for low compression and 9 for much compression.
+     *  <a href="http://root.cern.ch/root/html/src/TFile.cxx.html#b.RlJE">See the ROOT page for more information</a>.
+     */
     int m_compressionLevel;
 
 
     //then those for purely internal use:
 
-    /*! TFile for output. */
+    /** TFile for output. */
     TFile* m_file;
 
-    /*! TTree for output. */
+    /** TTree for output. */
     TTree* m_tree[c_NDurabilityTypes];
 
-    /*! Has branch creation already happened? */
-    /*! People will create objects in the event loop.
-        Therefore the branch creation can not happen before the event function.
-        However, in the event function, the branches should be created just once.
-    */
+    /** Has branch creation already happened?
+     *
+     *  People will create objects in the event loop.
+     *  Therefore the branch creation can not happen before the event function.
+     *  However, in the event function, the branches should be created just once.
+     */
     bool m_done[c_NDurabilityTypes];
 
-    /*! Total number of branches. */
+    /** Total number of branches. */
     size_t m_size[c_NDurabilityTypes];
 
-    /*! Number of branches for non-array objects. */
+    /** Number of branches for non-array objects. */
     size_t m_sizeObj[c_NDurabilityTypes];
 
-    /*! Pointer to pointer, that can be utilised by the TTree. */
+    /** Pointer to pointer, that can be utilised by the TTree.*/
     TObject** m_objects[c_NDurabilityTypes];
 
-    /*! Iterators over the maps. */
+    /** Iterators over the maps.*/
     StoreIter* m_iter[2*c_NDurabilityTypes];
 
-    /*! ROOT Object ID Restore Counter */
+    /** ROOT Object ID Restore Counter.*/
     int m_nObjID;
 
-    /*! String vector with steering parameter Names for m_treeNames. */
+    /** String vector with steering parameter Names for m_treeNames. */
     std::vector<std::string>  m_steerTreeNames;
 
-    /*! String vector with steering parameter Names for m_branchNames. */
+    /** String vector with steering parameter Names for m_branchNames. */
     std::vector<std::string>  m_steerBranchNames;
   };
 

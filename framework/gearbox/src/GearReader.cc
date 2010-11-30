7c077ab0d5ce9c76b6177cc6620c597a9efaccac
Andreas Moll <molland@mpp.mpg.de>
2010-11-30 17:16:10 +0000

    Already existing mixtures which are used inside of other mixtures can now be specified by their name.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@980 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/gearbox/src/GearReader.cc
+++ framework/gearbox/src/GearReader.cc
@@ -126,71 +126,81 @@ TGeoMixture* GearReader::readMixtureSection(GearDir& mixtureContent, double& wei
     return NULL;
   }
 
-  //1) Get the number of Elements, Materials and Mixtures
-  int nElem = 0;
-  GearDir elements(mixtureContent);
-  if (mixtureContent.isPathValid("Elements/Element")) {
-    elements.append("Elements/Element");
-    nElem = elements.getNumberNodes();
-  }
+  TGeoMixture *geoMix = NULL;
 
-  int nMat = 0;
-  GearDir materials(mixtureContent);
-  if (mixtureContent.isPathValid("Elements/Material")) {
-    materials.append("Elements/Material");
-    nMat = materials.getNumberNodes();
-  }
+  //Check if the mixture consists of elements, if not try to get the mixture from gGeoManager
+  if (mixtureContent.isPathValid("Elements")) {
 
-  int nMix = 0;
-  GearDir mixtures(mixtureContent);
-  if (mixtureContent.isPathValid("Elements/Mixture")) {
-    mixtures.append("Elements/Mixture");
-    nMix = mixtures.getNumberNodes();
-  }
-
-  //2) Create Mixture
-  double localWeight = -1.0;
-  TGeoMixture *geoMix = new TGeoMixture(matName.c_str(), nElem + nMat + nMix);
-
-  //3) Loop over all elements
-  for (int iElem = 1; iElem <= nElem; ++iElem) {
-    GearDir elemContentIdx(elements, iElem);
-    TGeoElement* currElement = readElementSection(elemContentIdx, localWeight);
-
-    if (localWeight < 0.0) {
-      B2ERROR("The element " << currElement->GetName() << " has no weight defined. It was not added to the mixture " << geoMix->GetName() << " !")
-      continue;
+    //1) Get the number of Elements, Materials and Mixtures
+    int nElem = 0;
+    GearDir elements(mixtureContent);
+    if (mixtureContent.isPathValid("Elements/Element")) {
+      elements.append("Elements/Element");
+      nElem = elements.getNumberNodes();
     }
-    geoMix->AddElement(currElement, localWeight);
-  }
 
-  //4) Loop over all materials
-  for (int iMat = 1; iMat <= nMat; ++iMat) {
-    GearDir matContentIdx(materials, iMat);
-    TGeoMaterial* currMaterial = readMaterialSection(matContentIdx, localWeight);
-
-    if (localWeight < 0.0) {
-      B2ERROR("The material " << currMaterial->GetName() << " has no weight defined. It was not added to the mixture " << geoMix->GetName() << " !")
-      continue;
+    int nMat = 0;
+    GearDir materials(mixtureContent);
+    if (mixtureContent.isPathValid("Elements/Material")) {
+      materials.append("Elements/Material");
+      nMat = materials.getNumberNodes();
     }
-    geoMix->AddElement(currMaterial, localWeight);
-  }
 
-  //5) Loop over all mixtures
-  for (int iMix = 1; iMix <= nMix; ++iMix) {
-    GearDir mixContentIdx(mixtures, iMix);
-    TGeoMixture* currMixture = readMixtureSection(mixContentIdx, localWeight);
-
-    if (localWeight < 0.0) {
-      B2ERROR("The mixture " << currMixture->GetName() << " has no weight defined. It was not added to the mixture " << geoMix->GetName() << " !")
-      continue;
+    int nMix = 0;
+    GearDir mixtures(mixtureContent);
+    if (mixtureContent.isPathValid("Elements/Mixture")) {
+      mixtures.append("Elements/Mixture");
+      nMix = mixtures.getNumberNodes();
     }
-    geoMix->AddElement(currMixture, localWeight);
-  }
 
-  //3) Finalize
-  double density = mixtureContent.getParamDensity("Density");
-  geoMix->SetDensity(density);
+    //2) Create Mixture
+    double localWeight = -1.0;
+    geoMix = new TGeoMixture(matName.c_str(), nElem + nMat + nMix);
+
+    //3) Loop over all elements
+    for (int iElem = 1; iElem <= nElem; ++iElem) {
+      GearDir elemContentIdx(elements, iElem);
+      TGeoElement* currElement = readElementSection(elemContentIdx, localWeight);
+
+      if (localWeight < 0.0) {
+        B2ERROR("The element " << currElement->GetName() << " has no weight defined. It was not added to the mixture " << geoMix->GetName() << " !")
+        continue;
+      }
+      geoMix->AddElement(currElement, localWeight);
+    }
+
+    //4) Loop over all materials
+    for (int iMat = 1; iMat <= nMat; ++iMat) {
+      GearDir matContentIdx(materials, iMat);
+      TGeoMaterial* currMaterial = readMaterialSection(matContentIdx, localWeight);
+
+      if (localWeight < 0.0) {
+        B2ERROR("The material " << currMaterial->GetName() << " has no weight defined. It was not added to the mixture " << geoMix->GetName() << " !")
+        continue;
+      }
+      geoMix->AddElement(currMaterial, localWeight);
+    }
+
+    //5) Loop over all mixtures
+    for (int iMix = 1; iMix <= nMix; ++iMix) {
+      GearDir mixContentIdx(mixtures, iMix);
+      TGeoMixture* currMixture = readMixtureSection(mixContentIdx, localWeight);
+
+      if (localWeight < 0.0) {
+        B2ERROR("The mixture " << currMixture->GetName() << " has no weight defined. It was not added to the mixture " << geoMix->GetName() << " !")
+        continue;
+      }
+      geoMix->AddElement(currMixture, localWeight);
+    }
+
+    //3) Finalize
+    double density = mixtureContent.getParamDensity("Density");
+    geoMix->SetDensity(density);
+
+  } else {
+    geoMix = dynamic_cast<TGeoMixture*>(gGeoManager->GetMaterial(matName.c_str()));
+    if (geoMix == NULL) B2ERROR("Mixture " << matName << " could not be found in gGeoManager !")
+    }
 
   return geoMix;
 }

265fc84933b901aec71faafb0fad2c668e47091d
Soohyung Lee <shlee@hep.korea.ac.kr>
2010-12-31 09:39:38 +0000

    Add Doxygen comments Clean up the code
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@1062 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/hlt/src/SignalMan.cc
+++ daq/hlt/src/SignalMan.cc
@@ -12,16 +12,29 @@
 
 using namespace Belle2;
 
+/* @brief SignalMan constructor
+ * Is this really needed?
+*/
 SignalMan::SignalMan(void)
     : m_inPort(-1), m_outPort(-1)
 {
 }
 
+/* @brief SignalMan constructor with port numbers
+ * Is this really needed, again?
+ * @param inPort Port number for incoming data
+ * @param outPort Port number for outgoing data
+*/
 SignalMan::SignalMan(const int inPort, const int outPort)
     : m_inPort(inPort), m_outPort(outPort)
 {
 }
 
+/* @brief SignalMan constructor with port numbers and single destination
+ * @param inPort Port number for incoming data
+ * @param outPort Port number for outgoing data
+ * @param dest IP address of destination
+*/
 SignalMan::SignalMan(const int inPort, const int outPort, std::string dest)
     : m_inPort(inPort), m_outPort(outPort)
 {
@@ -29,6 +42,12 @@ SignalMan::SignalMan(const int inPort, const int outPort, std::string dest)
   m_dest.push_back(dest);
 }
 
+/* @brief SignalMan constructor with port numbers and multiple destination
+ * @param inPort Port number for incoming data
+ * @param outPort Port number for outgoing data
+ * @param dest Vector container of IP addresses of destinations
+ * @param mode I don't remember what this is...
+*/
 SignalMan::SignalMan(const int inPort, const int outPort, std::vector<std::string> dest, const std::string mode)
     : m_inPort(inPort), m_outPort(outPort)
 {
@@ -36,6 +55,9 @@ SignalMan::SignalMan(const int inPort, const int outPort, std::vector<std::strin
   m_dest = dest;
 }
 
+/* @brief SignalMan destructor
+ * Ring buffers for IPC should be only managed by the framework
+*/
 SignalMan::~SignalMan(void)
 {
   if (m_pidEvtSender != 0 && m_pidEvtReceiver != 0) {
@@ -44,6 +66,12 @@ SignalMan::~SignalMan(void)
   }
 }
 
+/* @brief Initialization of SignalMan
+ * Buffer size is hard-coded now and it should be flexible somehow
+ * @return c_Success Initialization success (only framework returns this)
+ * @return c_InitFailed Initialization failed
+ * @return c_TermCalled Termination of forked processes (EvtSender and EvtReceiver)
+*/
 EStatus SignalMan::init(void)
 {
   char inBufName[] = "inBuffer";
@@ -55,6 +83,11 @@ EStatus SignalMan::init(void)
   return doCommunication();
 }
 
+/* @brief Fork EvtSender and EvtReceiver and let them work
+ * @return c_Success Forking success (only framework returns this)
+ * @return c_InitFailed Initialization failed (only EvtSender and EvtReceiver return this)
+ * @return c_TermCalled Termination of forked processes (EvtSender and EvtReceiver)
+*/
 EStatus SignalMan::doCommunication(void)
 {
   // Forking off an event sender to send data
@@ -122,104 +155,120 @@ EStatus SignalMan::doCommunication(void)
   return c_Success;
 }
 
+/* @brief Set port numbers to be used
+ * @param inPort port number for incoming data
+ * @param outPort port number for outgoing data
+*/
 const void SignalMan::setPorts(const int inPort, const int outPort)
 {
   m_inPort = inPort;
   m_outPort = outPort;
 }
 
+/* @brief Set IP address of single destination
+ * @param dest IP address of the destination
+*/
 const void SignalMan::setDest(const std::string dest)
 {
   m_dest.clear();
   m_dest.push_back(dest);
 }
 
+/* @brief Set IP addresses of multiple destinations
+ * @param dest IP addresses of the destinations
+*/
 const void SignalMan::setDest(std::vector<std::string> dest)
 {
   m_dest.clear();
   m_dest = dest;
 }
 
+/* @brief Get incoming buffer
+ * @return Pointer to the incoming buffer
+*/
 RingBuffer* SignalMan::getInBuffer()
 {
   return m_inBuf;
 }
 
+/* @brief Get outgoing buffer
+ * @return Pointer to the outgoing buffer
+*/
 RingBuffer* SignalMan::getOutBuffer()
 {
   return m_outBuf;
 }
 
+/* @brief Get IP addresses of destinations
+ * @return Vector container which has IP addresses of destinations
+*/
 std::vector<std::string> SignalMan::dest()
 {
   return m_dest;
 }
 
+/* @brief Test if the process is the framework or not
+ * @return 1 if the process is the framework
+ * @return 0 if the process is not the framework
+*/
 int SignalMan::isMother()
 {
   return (m_pidEvtSender && m_pidEvtReceiver);
 }
 
+/* @brief test if the process is EvtSender of not
+ * @return 1 if the process is EvtSender
+ * @return 0 if the process is not EvtSender
+*/
 int SignalMan::isEvtSender()
 {
   return !(m_pidEvtSender);
 }
 
+/* @brief test if the process is EvtReceiver of not
+ * @return 1 if the process is EvtReceiver
+ * @return 0 if the process is not EvtReceiver
+*/
 int SignalMan::isEvtReceiver()
 {
   return !(m_pidEvtReceiver);
 }
 
+/* @brief Get data from incoming buffer
+ * @return Serialized data transferred
+*/
 std::string SignalMan::get(void)
 {
-  //if (m_pidEvtSender != 0 && m_pidEvtReceiver != 0) {
   while (1) {
     if (m_inBuf->numq() > 0) {
       char* tmp = new char[255];
       m_inBuf->remq((int*)tmp);
       std::string input(tmp);
 
-      /*
-         if (input == "EOF")
-         break;
-         */
-
       return input;
     }
   }
-  //}
-  /*
-  if (m_outBuffer->ninsq () > 0) {
-    return m_inBuf->get ();
-  }
-  else
-    return "NOTHING";
-    */
 }
 
+/* @brief Put data into outgoing buffer
+ * @param data Serialized data to be transferred
+*/
 void SignalMan::put(const std::string data)
 {
   m_outBuf->insq((int*)(data.c_str()), data.size());
-  //m_outBuffer.push_back (data);
-  //m_inBuffer->put (data);
-  /*
-  try {
-    m_sender.connect ();
-    m_sender << data;
-  }
-  catch (B2SocketException& e) {
-    std::cout << "[\033[22;31mError\033[0m] " << e.description () << std::endl;
-  }
-  */
 }
 
-// unused?
+/* @brief I don't even remember the intension of this function now...
+ * Maybe it should be removed
+*/
 void SignalMan::broadCasting(void)
 {
   m_sender.connect();
 }
 
-// unused?
+/* @brief I don't even remember the intension of this function now...
+ * Maybe it should be removed (even worse this is the same with get() function
+*/
 std::string SignalMan::listening(void)
 {
   B2INFO("m_pidEvtSender = " << m_pidEvtSender);
@@ -233,36 +282,15 @@ std::string SignalMan::listening(void)
       return input;
     }
   }
-  /*
-    std::string data;
-    bool flag = false;
-    while (!flag) {
-      EvtReceiver new_sock;
-      m_receiver.accept (new_sock);
-
-      try {
-        while (1) {
-          new_sock >> data;
-
-          if (data != "")
-            flag = true;
-
-          return data;
-        }
-      }
-      catch (B2SocketException&) {}
-    }
-
-    return data;
-    */
 }
 
+/* @brief Displaying information of SignalMan (only for debugging)
+*/
 const void SignalMan::Print(void)
 {
-  std::cout << "--SignalMan info" << std::endl;
-  std::cout << "  [Incoming]" << std::endl;
-  std::cout << "    port# = " << m_inPort << std::endl;
-  std::cout << "  [Outgoing]" << std::endl;
-  std::cout << "    port# = " << m_outPort << std::endl;
-  //std::cout << "    dest  = " << m_dest << std::endl;
+  B2INFO("=================================================");
+  B2INFO(" SignalMan Summary");
+  B2INFO("   Incoming port# = " << m_inPort);
+  B2INFO("   Outgoing port# = " << m_outPort);
+  B2INFO("=================================================");
 }

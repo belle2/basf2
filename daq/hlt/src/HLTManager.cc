81f69d44d0acaf47558976b84fafd70034921f69
Soohyung Lee <shlee@hep.korea.ac.kr>
2011-01-03 00:58:18 +0000

    Fixing doxygen style comments to be compatible with other codes
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@1070 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/hlt/src/HLTManager.cc
+++ daq/hlt/src/HLTManager.cc
@@ -1,38 +1,49 @@
-/// @file HLTManager.cc
-/// @brief HLTManager class implementation
-/// @author Soohyung Lee
-/// @date Jul 14 2010
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Soohyung Lee                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
 
 #include <daq/hlt/HLTManager.h>
 
 using namespace Belle2;
 
-/// @brief HLTManager class constructor
+/* @brief HLTManager constructor
+*/
 HLTManager::HLTManager(void)
     : m_expNo(-1), m_runStart(-1), m_runEnd(-1)
 {
 }
 
-/// @brief HLTManager class constructor
-/// @param input Input XML file name
+/* @brief HLTManager constructor
+*/
 HLTManager::HLTManager(char* input)
     : m_inputXML(input)
 {
   init();
 }
 
+/* @brief HLTManager constructor
+ * @param input File name of input XML file
+*/
 HLTManager::HLTManager(std::string& input)
 {
   m_inputXML = (char*)(input.c_str());
   init();
 }
 
-/// @brief HLTManager class destructor
+/* @brief HLTManager destructor
+*/
 HLTManager::~HLTManager(void)
 {
 }
 
-/// @brief Initialize HLTManager class
+/* @brief Initializing HLTManager
+*/
 void HLTManager::init(void)
 {
   m_units.clear();
@@ -44,17 +55,23 @@ void HLTManager::init(void)
   initUnit();
 }
 
+/* @brief Broadcasting node information to all nodes in HLT farm
+*/
 void HLTManager::broadCasting(void)
 {
   m_units[0].broadCasting();
 }
 
+/* @brief Broadcasting node information to all nodes in a specific node
+ * For extension of functionality and doing nothing at this moment
+*/
 void HLTManager::broadCasting(int unitNo)
 {
 
 }
 
-/// @brief Initialize HLT farm information
+/* @brief Initialize HLT farm information
+*/
 void HLTManager::initHLT(void)
 {
   m_expNo = m_XMLParser->expNo();
@@ -64,7 +81,8 @@ void HLTManager::initHLT(void)
   m_inputDescription = m_XMLParser->inputDescription();
 }
 
-/// @brief Initialize units
+/* @brief Initialize units
+*/
 void HLTManager::initUnit(void)
 {
   std::vector<UnitInfo>::iterator i = m_XMLParser->unitInfo();
@@ -75,15 +93,17 @@ void HLTManager::initUnit(void)
   }
 }
 
-/// @brief Printing internal information
+/* @brief Displaying HLT manager information (only for debugging)
+*/
 void HLTManager::Print(void)
 {
-  B2INFO("---------------- \033[22;31mHLT Manager Summary\033[0m ----------------");
-  B2INFO("  HLT Name        : " << m_inputName);
-  B2INFO("  HLT Description : " << m_inputDescription);
-  B2INFO("  Exp = " << m_expNo << " (Run = " << m_runStart << "-" << m_runEnd << ")");
-  B2INFO("  Units = " << m_units.size());
+  B2INFO("=================================================");
+  B2INFO(" HLTManager Summary");
+  B2INFO("   HLT Name: " << m_inputName);
+  B2INFO("   HLT Description: " << m_inputDescription);
+  B2INFO("   Exp = " << m_expNo << " (Run = " << m_runStart << " - " << m_runEnd << ")");
+  B2INFO("   Units = " << m_unit.size());
   for (std::vector<UnitManager>::iterator i = m_units.begin(); i != m_units.end(); i++)
     (*i).Print();
-  B2INFO("-----------------------------------------------------");
+  B2INFO("=================================================");
 }

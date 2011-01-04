9a71eb1e03665c33b278fc41fe724930874a97f0
Soohyung Lee <shlee@hep.korea.ac.kr>
2011-01-04 02:32:27 +0000

    Documenting enumerators for hbasf2
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@1077 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/hlt/include/HLTDefs.h
+++ daq/hlt/include/HLTDefs.h
@@ -12,37 +12,49 @@
 #define HLTDEFS_H
 
 namespace Belle2 {
+  /** Port numbers to be used in HLT
+   *  Info*Port and ControlPort, MonitorPort are redundant so need to be cleaned up
+   */
   enum EHLTPort {
-    c_DataInPort = 20000,
-    c_DataOutPort = 25000,
-    c_InfoInPort = 30000,
-    c_InfoOutPort = 35000,
-    c_ControlPort = 30000,
-    c_MonitorPort = 35000
+    c_DataInPort = 20000,     /**< Port for incoming data */
+    c_DataOutPort = 25000,    /**< Port for outgoing data */
+    c_InfoInPort = 30000,     /**< Port for incoming information */
+    c_InfoOutPort = 35000,    /**< Port for outgoing information */
+    c_ControlPort = 30000,    /**< Port for control */
+    c_MonitorPort = 35000     /**< Port for monitoring information */
   };
 
+  /** Types of nodes in HLT
+   *  At this moment (r1076), only c_ManagerNode and c_ProcessNode are used
+   */
   enum ENodeType {
-    c_ManagerNode,
-    c_ProcessNode,
-    c_EventSeparator,
-    c_WorkerNode,
-    c_EventMerger,
-    c_NotDefined
+    c_ManagerNode,      /**< Manager node */
+    c_ProcessNode,      /**< Process nodes including event separator, worker node, and event merger */
+    c_EventSeparator,   /**< Event separator node */
+    c_WorkerNode,       /**< Worker node */
+    c_EventMerger,      /**< Event merger node */
+    c_NotDefined        /**< Not specified node */
   };
 
+  /** Types of message passed in HLT
+   *  Not used right now
+   */
   enum EMessage {
-    c_Initialization,
-    c_StartRun,
-    c_EndRun,
-    c_Termination
+    c_Initialization,   /**< To initialize */
+    c_StartRun,         /**< To start runs */
+    c_EndRun,           /**< To end runs */
+    c_Termination       /**< To terminate all */
   };
 
+  /** Status of functions in hbasf2
+   *  ProcFailed is not used at this moment
+   */
   enum EStatus {
-    c_FuncError,
-    c_InitFailed,
-    c_ProcFailed,
-    c_TermCalled,
-    c_Success
+    c_FuncError,        /**< Function error somehow (for normal functions) */
+    c_InitFailed,       /**< Initialization failure (for initialization functions) */
+    c_ProcFailed,       /**< Process failure */
+    c_TermCalled,       /**< Termination requested */
+    c_Success           /**< Everything's ok :D */
   };
 }
 

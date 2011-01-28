2a1e25b4e8f411b7468c2ee308ce6187437fe6c2
Andreas Moll <molland@mpp.mpg.de>
2011-01-28 15:57:00 +0000

    Framework: Added examples folder and first steering file example
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@1269 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ framework/examples/EvtMetaDataGen.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+######################################################
+# This steering file shows how to run the framework
+# for different experiment, run and event numbers.
+#
+# In the example below, basf2 will run on and display
+# the following experiment, run and event numbers:
+#
+# Experiment 71, Run  3, Event  0 to  3
+# Experiment 71, Run  4, Event 10 to 15
+# Experiment 73, Run 10, Event  0 to  1
+# Experiment 73, Run 20, Event 30 to 34
+# Experiment 73, Run 30, Event  1 to  5
+#
+# Example steering file - 2011 Belle II Collaboration
+######################################################
+
+from basf2 import *
+
+# Register modules
+evtmetagen = register_module('EvtMetaGen')
+evtmetainfo = register_module('EvtMetaInfo')
+
+evtruninfo = {
+    'ExpList': [71, 71, 73, 73, 73],
+    'RunList': [3, 4, 10, 20, 30],
+    'EvtStartList': [0, 10, 0, 30, 1],
+    'EvtEndList': [3, 15, 1, 34, 5],
+    }
+
+evtmetagen.param(evtruninfo)
+
+# Create path
+main = create_path()
+
+# Add module to path
+main.add_module(evtmetagen)
+main.add_module(evtmetainfo)
+
+# Process all events
+process(main)

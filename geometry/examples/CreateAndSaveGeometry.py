5b408f3c249593b5510832806d81c27be0b807ff
Andreas Moll <molland@mpp.mpg.de>
2011-01-28 16:14:19 +0000

    Geometry: Updated steering file example.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@1272 b38c6c8f-bee8-465d-8448-9abe6915d496

--- geometry/examples/CreateAndSaveGeometry.py
+++ geometry/examples/CreateAndSaveGeometry.py
@@ -6,28 +6,35 @@
 # geometry, does an overlap check and saves it as
 # a Root file.
 #
+# The tolerance for overlaps, which are considered
+# to be problematic, is set to 1mm.
+#
 # Example steering file - 2011 Belle II Collaboration
 ######################################################
 
 import os
 from basf2 import *
 
-# Register modules
+# Geometry parameter loader
 paramloader = register_module('ParamLoaderXML')
-geobuilder = register_module('GeoBuilder')
-overlapchecker = register_module('OverlapChecker')
-geosaver = register_module('GeoSaver')
-
-# Set parameters
 paramloader.param('InputFileXML', os.path.join(basf2datadir,
                   'simulation/Belle2.xml'))
-geosaver.param('Filename', 'Belle2Geo.root')
+
+# Geometry builder
+geobuilder = register_module('GeoBuilder')
+
+# Overlap Checker
+overlapchecker = register_module('OverlapChecker')
 overlapchecker.param('Tolerance', 0.01)
 
-# Create paths
+# Saves the geometry as a Root file
+geosaver = register_module('GeoSaver')
+geosaver.param('Filename', 'Belle2Geo.root')
+
+# Create main path
 main = create_path()
 
-# Add modules to paths
+# Add modules to main path
 main.add_module(paramloader)
 main.add_module(geobuilder)
 main.add_module(overlapchecker)

645828761e3a5a2e61578367710fa0596fc3ed8a
Andreas Moll <molland@mpp.mpg.de>
2011-04-28 12:08:46 +0000

    Fixed missing length units for the magnetic field space point, added missing Doxygen documentation, added new magnetic field XML file.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@1629 b38c6c8f-bee8-465d-8448-9abe6915d496

--- simulation/kernel/include/MagneticField.h
+++ simulation/kernel/include/MagneticField.h
@@ -20,21 +20,31 @@ namespace Belle2 {
 
   namespace Simulation {
 
-    //! The Class for the Belle2 magnetic field implementation for Geant4.
-    /*!
-     * The field is currently a 1.5 Tesla constant magnetic field.
-    */
+    /**
+     * The Class for the Belle2 magnetic field implementation for Geant4.
+     *
+     * The Geant4 simulation uses the central magnetic field map
+     * implementation of the basf2 framework to return the magnetic field
+     * vector for a space point.
+     */
     class MagneticField : public G4MagneticField {
 
     public:
 
-      //! Constructor
+      /** Constructor. */
       MagneticField();
 
-      //! Destructor
+      /** Destructor. */
       ~MagneticField();
 
+      /**
+       * Sets the magnetic field vector for a specified space point.
+       *
+       * @param Point An array specifying the space point (x,y,z) in [mm].
+       * @param Bfield Returns the magnetic field vector in tesla.
+       */
       void GetFieldValue(const G4double Point[3], G4double *Bfield) const;
+
 
     private:
 
--- simulation/kernel/src/MagneticField.cc
+++ simulation/kernel/src/MagneticField.cc
@@ -34,11 +34,11 @@ MagneticField::~MagneticField()
 
 void MagneticField::GetFieldValue(const G4double Point[3], G4double *Bfield) const
 {
-  //Get the magnetic field vector from the central magnetic field map
-  TVector3 magField = m_bField.getBField(TVector3(Point[0], Point[1], Point[2]));
+  //Get the magnetic field vector from the central magnetic field map (Geant4 uses [mm] as a length unit)
+  TVector3 magField = m_bField.getBField(TVector3(Point[0] * Unit::mm, Point[1] * Unit::mm, Point[2] * Unit::mm));
 
-  //Set the magnetic field
-  Bfield[0] = magField[0];
-  Bfield[1] = magField[1];
-  Bfield[2] = magField[2] * tesla; //Use the Geant4 tesla unit here !
+  //Set the magnetic field (Use the Geant4 tesla unit here !)
+  Bfield[0] = magField[0] * tesla;
+  Bfield[1] = magField[1] * tesla;
+  Bfield[2] = magField[2] * tesla;
 }

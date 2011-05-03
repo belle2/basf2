41686520149f960671ba6925fc4e126926620902
Martin Heck <martin.heck@kit.edu>
2011-05-03 16:36:37 +0000

    Final simplification of the cms2LabBoost... Tested to give the same result as the original function from Martin Ritter and Susanne in the generator package.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@1655 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/utilities/include/Boosts.h
+++ framework/utilities/include/Boosts.h
@@ -12,6 +12,8 @@
 #define BOOSTS_H
 
 #include "TLorentzRotation.h"
+#include <framework/gearbox/Unit.h>
+
 
 
 namespace Belle2 {
@@ -22,27 +24,55 @@ namespace Belle2 {
    *  You don't have to construct an object of this class yourself, but you can directly
    *  call the functions of the class by writing e.g.
    *  Boosts::getCMS2LabB...
+   *  @TODO Can this stay at the generators package, or do we really want to have it in the the framework package?
+   *  @sa RunMetaData
    */
   class Boosts {
   public:
 
     /** Get Lorentz boost for transformation from CMS to lab system.
+     *  For definition of the parameters, see the following:<br>
+    <pre>
+
+                                 Inverse Lab z-Axis
+                                <_______________________________________________
+    _ _ _ _ _                            *                   .........&lt;
+      /   HER > _ _ _ _ _                *         .........&lt; LER
+     /                   &gt; _ _ _ _ _     .*.......&lt;
+     /                        .......&gt; _&lt;_ _ _ _
+     /              .........&lt;                  &gt; _ _ _ _ _
+      /   .........&lt;                                       &gt; _ _ _ _ _
+    .........&lt;                       Lab z-Axis                           &gt;_ _ _ _ _
+                                 defined by solenoidal field
+                                 _____________________________________________________&gt;
+
+    Symbol * represents angleLER.
+    Symbol / represents crossAngle.
+    </pre>
      *
-     *  @param EHER         Energy of high energy beam. The default value is for running on Y(4S).
-     *  @param ELER         Energy of low energy beam. The default value is for running on Y(4S).
-     *  @param crossAngle   Crossing Angle of the two beams. The default value is the design parameter.
-     *  @param angle        Angle between the inverse direction of the LER and the solenoid magnetic field axis.
+     *  @param energyHER        Energy of high energy beam. The default value is for running on Y(4S).
+     *  @param energyLER        Energy of low energy beam. The default value is for running on Y(4S).
+     *  @param crossAngle       Crossing Angle of the two beams. The default value is the design parameter.
+     *  @param angleLER         Angle between the inverse direction of the LER and the solenoid magnetic field axis.
+     *  @return                 <a href="http://root.cern.ch/root/html/TLorentzRotation.html">TLorentzRotation</a>
+     *                          from the ROOT::physics Vector package, that translates a TLorentzVector from
+     *                          the center of mass system of the beam interaction into the laboratory system.
      */
-    TLorentzRotation getCMS2LabBoost(double EnergyHER, double EnergyLER, double crossAngle, double angle);
+    TLorentzRotation getCMS2LabBoost(const float& energyHER  = 7. * Unit::GeV,  const float& energyLER = 4.  * Unit::GeV,
+                                     const float& crossAngle = 83 * Unit::mrad, const float& angleLER = 41.5 * Unit::mrad);
 
     /** Get Lorentz boost for transformation from lab to CMS system.
      *
-     *  @param EHER         Energy of high energy beam. The default value is for running on Y(4S).
-     *  @param ELER         Energy of low energy beam. The default value is for running on Y(4S).
-     *  @param crossAngle   Crossing Angle of the two beams. The default value is the design parameter.
-     *  @param angle        Angle between the inverse direction of the LER and the solenoid magnetic field axis.
+     *  @param energyHER        Energy of high energy beam. The default value is for running on Y(4S).
+     *  @param energyLER        Energy of low energy beam. The default value is for running on Y(4S).
+     *  @param crossAngle       Crossing Angle of the two beams. The default value is the design parameter.
+     *  @param angleLER         Angle between the inverse direction of the LER and the solenoid magnetic field axis.
+     *  @return                 <a href="http://root.cern.ch/root/html/TLorentzRotation.html">TLorentzRotation</a>
+     *                          from the ROOT::physics Vector package, that translates a TLorentzVector from
+     *                          the laboratory system into the center of mass system of the beam interaction.
      */
-    TLorentzRotation getLab2CMSBoost(double EnergyHER, double EnergyLER, double crossAngle, double angle);
+    inline TLorentzRotation getLab2CMSBoost(const float& energyHER  = 7. * Unit::GeV,  const float& energyLER = 4.  * Unit::GeV,
+                                            const float& crossAngle = 83 * Unit::mrad, const float& angleLER = 41.5 * Unit::mrad);
   };
 }
 
--- framework/utilities/src/Boosts.cc
+++ framework/utilities/src/Boosts.cc
@@ -8,76 +8,45 @@
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/
 
-#include "TLorentzVector.h"
-#include "TLorentzRotation.h"
-#include <framework/gearbox/Unit.h>
+#include <TLorentzVector.h>
+#include <TLorentzRotation.h>
 #include <framework/utilities/Boosts.h>
 
 using namespace Belle2;
 
-TLorentzRotation Boosts::getLab2CMSBoost(double EnergyHER, double EnergyLER, double crossAngle, double angle)
+inline TLorentzRotation Boosts::getLab2CMSBoost(const float& energyHER, const float& energyLER, const float& crossAngle, const float& angleLER)
 {
-
-// Beam parameter
-  double mEle = Unit::electron_mass;
-
-// Lab frame Z axis is defined by B files
-// its positive direction close to direction of
-// high energy beam, i.e. e- beam
-
-  double angleLerToB = TMath::Pi() - angle;
-  double pLerZ = EnergyLER * cos(angleLerToB);
-  double pLerX = EnergyLER * sin(angleLerToB);
-  double pLerY = 0.;
-
-
-  double angleHerToB = crossAngle - angle;
-
-  double pHerZ = EnergyHER * cos(angleHerToB);
-  double pHerX = EnergyHER * sin(angleHerToB);
-  double pHerY = 0.;
-
-  TLorentzVector vlLer;
-  vlLer.SetXYZM(pLerX, pLerY, pLerZ, mEle);
-
-  TLorentzVector vlHer;
-  vlHer.SetXYZM(pHerX, pHerY, pHerZ, mEle);
-
-  TLorentzVector vlY4s = vlHer + vlLer;
-
-
-// ---------------------------------------------
-
-// Transformation from Lab system to CMS system
-  TVector3 cmsBoostVec = vlY4s.BoostVector();
-  TLorentzRotation lab2cmsBoost(-cmsBoostVec);
-
-// boost e- to CMS system
-  TLorentzVector vlEleCms = lab2cmsBoost * vlHer;
-
-// now rotate CMS such that incomming e- is parallel to z-axis
-  TVector3 zaxis(0., 0., 1.);  TVector3 rotaxis = zaxis.Cross(vlEleCms.Vect()) * (1. / vlEleCms.Vect().Mag());
-  double rotangle = TMath::ASin(rotaxis.Mag());
-  TLorentzRotation lab2cmsBoostRotZ = lab2cmsBoost.Rotate(-rotangle, rotaxis);
-
-
-// boost lab e- and e+ to properly rotated CMS system
-  TLorentzVector vlEleCmsR = lab2cmsBoostRotZ * vlHer;
-  TLorentzVector vlPosCmsR = lab2cmsBoostRotZ * vlLer;
-
-// obtain the backward transformation
-  TLorentzRotation cms2labBoostRotZ = lab2cmsBoostRotZ.Inverse();
-  /*    for(int i=0; i<4; i++){
-         for(int j=0;j<4;j++){
-             std::cout <<  cms2labBoostRotZ(i,j) << " ";
-         }
-          std::cout << std::endl;
-     }*/
-
-  return cms2labBoostRotZ;
+  return getLab2CMSBoost(energyHER, energyLER, crossAngle, angleLER).Inverse();
 }
 
-TLorentzRotation Boosts::getCMS2LabBoost(double EnergyHER, double EnergyLER, double crossAngle, double angle)
+TLorentzRotation Boosts::getCMS2LabBoost(const float& energyHER, const float& energyLER, const float& crossAngle, const float& angleLER)
 {
-  return (Boosts::getLab2CMSBoost(EnergyHER, EnergyLER, crossAngle, angle).Inverse());
+  // Lab frame Z axis is defined by B field;
+  // its positive direction close to direction of
+  // high energy beam, i.e. e- beam.
+  double angleHerToB = crossAngle  - angleLER;
+  double angleLerToB = TMath::Pi() - angleLER;
+
+  // Getting the various LorentzVectors.
+  TLorentzVector lorentzVecLER;
+  lorentzVecLER.SetXYZM(energyLER * sin(angleLerToB), 0., energyLER * cos(angleLerToB), Unit::electronMass);
+
+  TLorentzVector lorentzVecHER;
+  lorentzVecHER.SetXYZM(energyHER * sin(angleHerToB), 0., energyHER * cos(angleHerToB), Unit::electronMass);
+
+  TLorentzVector vectorLorentzY4S = lorentzVecHER + lorentzVecLER;
+
+  // Transformation from Lab system to CMS system
+  TLorentzRotation lab2cmsBoost(vectorLorentzY4S.BoostVector());
+
+  // boost HER e- from Lab system to CMS system
+  const TLorentzVector lorentzVecElectronCMS = lab2cmsBoost * lorentzVecHER;
+
+  // now rotate CMS such that incomming e- is parallel to z-axis
+  TVector3 zaxis(0., 0., 1.);
+  TVector3 rotaxis = zaxis.Cross(lorentzVecElectronCMS.Vect()) * (1. / lorentzVecElectronCMS.Vect().Mag());
+  double rotangle = TMath::ASin(rotaxis.Mag());
+
+  // return rotated Vector.
+  return (lab2cmsBoost.Rotate(-rotangle, rotaxis));
 }

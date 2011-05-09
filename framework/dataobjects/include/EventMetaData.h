39c6f44d63d1784962d9ef29f966110b6eb0df47
Martin Heck <martin.heck@kit.edu>
2011-05-09 19:20:50 +0000

    changed location of EventMetaData
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@1682 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ framework/dataobjects/include/EventMetaData.h
@@ -0,0 +1,168 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Martin Heck, Andreas Moll                                *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#ifndef EVENTMETADATA_H
+#define EVENTMETADATA_H
+
+#include <TObject.h>
+
+namespace Belle2 {
+
+  /** Store event number, run number, and experiment number.
+   *
+   *  Master modules have to create an object of this type!
+   *
+   *  @author <a href="mailto:martin.heck@kit.edu?subject=EventMetaData">Martin Heck</a>
+   */
+  class EventMetaData : public TObject {
+  public:
+
+    /** Constructor.
+     *
+     *  Initilises meta data of event with Nulls.
+     */
+    EventMetaData(unsigned long event = -1, unsigned long run = -1, unsigned long experiment = -1)
+        : m_event(event), m_run(run), m_experiment(experiment), m_parent_index(-1), m_generated_weight(1) {}
+
+    /** Destructor. */
+    ~EventMetaData() {}
+
+    /** Event Setter.
+     *
+     *  @param event The current event number.
+     */
+    void setEvent(const unsigned long& event) {
+      m_event = event;
+    }
+
+    /** Run Setter.
+     *
+     *  @param run The number of the current run.
+     */
+    void setRun(const unsigned long& run) {
+      m_run = run;
+    }
+
+    /** Experiment Setter.
+     *
+     *  @param experiment The number of the current experiment.
+     */
+    void setExperiment(const unsigned long& experiment) {
+      m_experiment = experiment;
+    }
+
+    /** Marks the end of the data processing.
+     * Sets the values for the experiment, run and event to the
+     * default values.
+     */
+    void setEndOfData() {
+      m_event = -1;
+      m_run = -1;
+      m_experiment = -1;
+    }
+
+    /** Parent Index Setter.
+     *
+     *  @param index The index of the current parent file.
+     */
+    void setParentIndex(int index) {
+      m_parent_index = index;
+    }
+
+    /** Generated Weight Setter.
+     *
+     *  @param weight The weight produced by the event generator
+     */
+    void setGeneratedWeight(const double& weight) {
+      m_generated_weight = weight;
+    }
+
+    /** Event Getter.
+     *
+     *  @return The number of the current event.
+     */
+    unsigned long getEvent() const {
+      return m_event;
+    }
+
+    /** Run Getter.
+     *
+     *  @return The number of the current run.
+     */
+    unsigned long getRun() const {
+      return m_run;
+    }
+
+    /** Experiment Getter.
+     *
+     *  @return The number of the current experiment.
+     */
+    unsigned long getExperiment() const {
+      return m_experiment;
+    }
+
+    /** Parent Index Getter.
+     *
+     *  @return The index of the current parent file.
+     */
+    int getParentIndex() const {
+      return m_parent_index;
+    }
+
+    /** Generated Weight Getter.
+     *
+     *  @return The weight produced by the event generator
+     */
+    double getGeneratedWeight() const {
+      return m_generated_weight;
+    }
+
+    /** Comparison Operator.
+     *
+     *  @param eventMetaData The event meta data to compare with.
+     *  @return True if event, run, and experiment numbers are the same
+     */
+    bool operator== (const EventMetaData& eventMetaData) const;
+
+    /** Comparison Operator.
+     *
+     *  @param eventMetaData The event meta data to compare with.
+     *  @return True if event, run, or experiment numbers are different
+     */
+    bool operator!= (const EventMetaData& eventMetaData) const;
+
+  private:
+
+    /** Event number.
+     */
+    unsigned long m_event;
+
+    /** Run number.
+     */
+    unsigned long m_run;
+
+    /** Experiment number.
+     */
+    unsigned long m_experiment;
+
+    /** Experiment number.
+     */
+    int m_parent_index;
+
+    /** Generated weight.
+     */
+    double m_generated_weight;
+
+    /** ROOT Macro to make EventMetaData a ROOT class.
+     */
+    ClassDef(EventMetaData, 1);
+
+  }; //class
+} // namespace Belle2
+#endif // EVENTMETADATA
--- framework/dataobjects/src/EventMetaData.cc
+++ framework/dataobjects/src/EventMetaData.cc
@@ -8,7 +8,7 @@
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/
 
-#include <framework/datastore/EventMetaData.h>
+#include <framework/dataobjects/EventMetaData.h>
 
 using namespace std;
 using namespace Belle2;

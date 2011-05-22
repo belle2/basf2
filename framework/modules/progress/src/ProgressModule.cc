76824ec4bd831ea06e0a8caa3a7557726105f9f5
Martin Heck <martin.heck@kit.edu>
2011-05-22 23:38:49 +0000

    Adding the Progress module written by Martin Ritter and Susanne Koblitz. This module prints out how many events are already processed with a configurable interval.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@1709 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ framework/modules/progress/src/ProgressModule.cc
@@ -0,0 +1,48 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010-2011  Belle II Collaboration                         *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Susanne Koblitz                                          *
+ *                                                                        *
+ **************************************************************************/
+
+#include <framework/modules/ProgressModule.h>
+#include <framework/logging/Logger.h>
+#include <cmath>
+
+using namespace std;
+using namespace Belle2;
+
+REG_MODULE(Progress)
+
+ProgressModule::ProgressModule() : Module(), m_maxOrder(3), m_evtNr(0), m_runNr(0),
+    m_output("Processed: %3d runs, %6d events")
+{
+  setDescription("Output number of processed events. "
+                 "Output cannot be suppressed using set_log_level. "
+                 "If you don't want messages, you don't want this module");
+  addParam("maxN", m_maxOrder,
+           "maximum 10^N events between output", m_maxOrder);
+}
+
+void ProgressModule::initialize()
+{
+  //Force module logging level to be info
+  setLogLevel(LogConfig::c_Info);
+}
+
+void ProgressModule::beginRun()
+{
+  ++m_runNr;
+  B2INFO("Begin of new run")
+}
+
+void ProgressModule::event()
+{
+  ++m_evtNr;
+  //Calculate the order of magnitude
+  int order = (m_evtNr == 0) ? 1 : min(log10(m_evtNr), (double)m_maxOrder);
+  int interval = pow(10., order);
+  if (m_evtNr % interval == 0) B2INFO(m_output % m_runNr % m_evtNr);
+}

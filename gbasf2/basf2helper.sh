2c524e57c1de227d05e22594177a9350d166d9fc
Yanliang Han <hanyl@ihep.ac.cn>
2011-09-27 07:54:15 +0000

    Update the grid basf2 software initialization
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@1992 b38c6c8f-bee8-465d-8448-9abe6915d496

--- gbasf2/basf2helper.sh
+++ gbasf2/basf2helper.sh
@@ -4,20 +4,31 @@
 # $1 = steering file
 # $2 = release
 # Tom Fifield - 2010-01
+# Yanliang Han 2011-09 Update the grid basf2 software initialization.
 
 ##############################################################################
 # Initialisation
 ##############################################################################
+
+#VO_BELLE_SW_DIR=${VO_BELLE_SW_DIR:-$OSG_APP/belle}
+#VO_BELLE2_SW_DIR=$VO_BELLE_SW_DIR/belle2
+#BELLE2_TOOLS=$VO_BELLE2_SW_DIR/tools
+#WORKDIR=`pwd`
+#
+#. $BELLE2_TOOLS/setup_belle2.sh
+#
+#cd $VO_BELLE2_SW_DIR/releases/$2
+#setuprel
+#cd $WORKDIR
+
 VO_BELLE_SW_DIR=${VO_BELLE_SW_DIR:-$OSG_APP/belle}
-VO_BELLE2_SW_DIR=$VO_BELLE_SW_DIR/belle2
-BELLE2_TOOLS=$VO_BELLE2_SW_DIR/tools
+unset VO_BELLE2_SW_DIR
+. ${VO_BELLE_SW_DIR}/belle2/tools/setup_belle2.sh
 WORKDIR=`pwd`
-
-. $BELLE2_TOOLS/setup_belle2.sh
-
-cd $VO_BELLE2_SW_DIR/releases/$2
-setuprel
+cd ${VO_BELLE_SW_DIR}/belle2/releases/build-2011-07-01
+setuprel build-2011-07-01
 cd $WORKDIR
+
 
 basf2 -i > basf2.error 2>&1
 if [ $? -ne 0 ]; then

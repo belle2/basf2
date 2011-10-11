6568a9f1f4e0e13706dbf42e1a729ecae0fac5b4
Martin Ritter <martin.ritter@lmu.de>
2011-10-11 17:29:13 +0000

    Added MaterialScan Module
    Added a new module which allows to determine the Material budget of the
    geometry in different ways:
    
    - Spherical Scan: Shoot rays from the origin and scan theta and phi. The
      range for theta, phi and the number of rays can be defined using the
      module parameters
    - Planar Scan: Shoot rays perpendiular to a given plane. The plane,
      range and number of rays can be defined freely using the module
      parameters.
    
    For both types of scans the depth of the scan can be limited to provide
    cut views through the detector at any plane.
    
    The output will be a root file containing 2D histograms of the scanned
    X0 and lambda per defined G4Region.
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@2077 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ simulation/modules/include/MaterialScan.h
@@ -0,0 +1,210 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Martin Ritter                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef MATERIALSCANMODULE_H_
+#define MATERIALSCANMODULE_H_
+
+#include <framework/core/Module.h>
+#include <framework/gearbox/Unit.h>
+
+#include <string>
+#include <vector>
+
+#include <G4UserSteppingAction.hh>
+#include <G4ThreeVector.hh>
+
+#include <TFile.h>
+class TH2D;
+
+namespace Belle2 {
+
+  /** Base class to create a Material Scan of the detector geometry.
+   * This class is used to record the Material budget encountered by a particle
+   * and provide origin and directions of all particles used for the scan.
+   */
+  class MaterialScan: public G4UserSteppingAction {
+  public:
+    /**Helper struct to Store Parameters of a Scan. */
+    struct ScanParams {
+      /** Default Constructor */
+      ScanParams(): nU(0), nV(0), minU(0), maxU(0), minV(0), maxV(0), maxDepth(-1) {}
+      /** Number of rays along u coordinate */
+      int nU;
+      /** Number of rays along v coordinate */
+      int nV;
+      /** Minimum u value to scan */
+      double minU;
+      /** Maximum u value to scan */
+      double maxU;
+      /** Minimum v value to scan */
+      double minV;
+      /** Maximum v value to scan */
+      double maxV;
+      /** Maximum depth of the scan */
+      double maxDepth;
+    };
+
+    /** Constructor
+     * @param rootFile Pointer to the ROOTFile where the histograms will be stored.
+     * @param histPrefix Prefix to preprend to all Histograms
+     * @param axisLabel Labels for the histogram axes, separated by semicolon
+     * @param params Parameters for the scan
+     */
+    MaterialScan(TFile* rootFile, const std::string &histPrefix, const std::string& axisLabel, ScanParams params);
+
+    /** virtual destructor */
+    virtual ~MaterialScan() {};
+
+    /** Get the origin and direction for the next scan particle.
+     * @param[out] origin Origin of the next scan particle
+     * @param[out[ direction Direction of the next scan particle
+     * @return false if the scan is finished
+     */
+    bool createNext(G4ThreeVector &origin, G4ThreeVector &direction);
+
+    /** Record the material budget for each step of the particles */
+    void UserSteppingAction(const G4Step* step);
+  protected:
+    /** Get the origin and direction for the next scan particle.
+     * To be overridden by descendents
+     * @param[out] origin Origin of the next scan particle
+     * @param[out[ direction Direction of the next scan particle
+     */
+    virtual void getRay(G4ThreeVector &origin, G4ThreeVector &direction) = 0;
+
+    /** Fill the recorded material budget into the corresponding histogram
+     * @param name Name of the histogram
+     * @param value Value to store
+     */
+    void fillValue(const std::string &name, double value);
+
+    /** Pointer to the root file for the histograms */
+    TFile *m_rootFile;
+    /** Prefix to prepend for all histogram names */
+    std::string m_histPrefix;
+    /** Labels for the coordinate axes */
+    std::string m_axisLabel;
+    /** Parameters for the scan */
+    ScanParams m_params;
+    /** Current value of the parametetr u */
+    double m_curU;
+    /** Stepsize for the parameter u */
+    double m_stepU;
+    /** Current value of the parametetr v */
+    double m_curV;
+    /** Stepsize for the parameter v */
+    double m_stepV;
+    /** Tracklength of the current Ray */
+    double m_curDepth;
+    /** Map holding pointers to all created histograms */
+    std::map<std::string, TH2D*> m_regions;
+  };
+
+  /** Specific implementation of MaterialScan to do Spherical scanning.
+   * That is shooting rays from the origin with varying aximuth and polar angle.
+   */
+  class MaterialScanSpherical: public MaterialScan {
+  public:
+    /** Create a Spherical Scan object with the given parameters
+     * @param rootFile pointer to the ROOT File containing the histograms
+     * @param params Parameters of the scan
+     */
+    MaterialScanSpherical(TFile* rootFile, ScanParams params):
+        MaterialScan(rootFile, "Spherical", "#theta [deg];#phi [deg]", params) {}
+  protected:
+    /** Create a ray with the current parameter values according to a spherical distribution */
+    void getRay(G4ThreeVector &origin, G4ThreeVector &direction);
+  };
+
+  /** Specific implementaion of MaterialScan to scan parallel to a given plane.
+   *
+   * Particles will be created in a grid on the given plane and will be traced
+   * perpendicluar to that plane The plane is defined by one origin and the
+   * directions of the grid coordinates u and v. The flight direction is
+   * determined by the cross product between the u and v axis.
+   */
+  class MaterialScanPlanar: public MaterialScan {
+  public:
+    /** Create a Planar Scan object with the given parameters
+     * @param rootFile pointer to the ROOT File containing the histograms
+     * @param origin Origin of the plane to shoot rays from
+     * @param dirU Direction of the u coordinate
+     * @param dirV Direction of the v coordinate
+     * @param params Parameters of the scan
+     */
+    MaterialScanPlanar(TFile* rootFile, const G4ThreeVector& origin, const G4ThreeVector& dirU, const G4ThreeVector& dirV, ScanParams params):
+        MaterialScan(rootFile, "Plane", "u [cm];v [cm]", params), m_origin(origin), m_dirU(dirU.unit()), m_dirV(dirV.unit()), m_dirW(m_dirU.cross(m_dirV)) {
+    }
+  protected:
+    /** Create a ray with the current parameter values according to a planar distribution */
+    void getRay(G4ThreeVector &origin, G4ThreeVector &direction);
+  protected:
+    /** Origin of the scan plane */
+    G4ThreeVector m_origin;
+    /** u direction of the scan plane */
+    G4ThreeVector m_dirU;
+    /** v direction of the scan plane */
+    G4ThreeVector m_dirV;
+    /** direction perpendicluar to u and v */
+    G4ThreeVector m_dirW;
+  };
+
+  /**
+   * The MaterialScan module.
+   *
+   * This Module is intended to scan the material budget of the geometry.
+   * Currently, there are two different kinds of scans available: Spherical and Planar scan.
+   *
+   * - Spherical scan will shoot rays from the origin of the detector and scan
+   *   along the polar and azimuth angle.
+   * - Planar scan will shoot rays perpendicular to a given plane.
+   */
+  class MaterialScanModule : public Module {
+
+  public:
+
+    /* The constructor of the module.
+     * Sets the description and the parameters of the module.
+     */
+    MaterialScanModule();
+
+    /** Initialize the output file and check the parameters. */
+    void initialize();
+
+    /** Do the actual scanning */
+    void beginRun();
+
+    /** Save the output */
+    void terminate();
+  private:
+    /** Return a vector along the axis with the given name
+     * @param name Name of the axis */
+    G4ThreeVector getAxis(char name);
+
+    /** Pointer to the ROOT file for the histograms */
+    TFile* m_rootFile;
+    /** Filename for the ROOT output */
+    std::string m_filename;
+    /** Wether or not to do a spherical scan */
+    bool m_doSpherical;
+    /** Wether or not to do a planar scan */
+    bool m_doPlanar;
+    /** Name of the plane to use for scanning */
+    std::string m_planeName;
+    /** Scan parameters for the spherical scan */
+    MaterialScan::ScanParams m_spherical;
+    /** Scan parameters for the planar scan */
+    MaterialScan::ScanParams m_planar;
+    /** Custom plane definition if m_planName is "custom" */
+    std::vector<double> m_customPlane;
+  };
+}
+
+#endif /* MATERIALSCANMODULE_H_ */
---
+++ simulation/modules/src/MaterialScan.cc
@@ -0,0 +1,304 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Martin Ritter                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <simulation/modules/MaterialScan.h>
+#include <framework/gearbox/Unit.h>
+#include <boost/format.hpp>
+
+#include <TFile.h>
+#include <TH2D.h>
+
+#include <G4Event.hh>
+#include <G4EventManager.hh>
+#include <G4UserEventAction.hh>
+#include <G4UserStackingAction.hh>
+#include <G4UserTrackingAction.hh>
+#include <G4UserSteppingAction.hh>
+#include <G4RayShooter.hh>
+#include <G4Track.hh>
+
+#include <boost/foreach.hpp>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
+using namespace Belle2;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(MaterialScan);
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+MaterialScan::MaterialScan(TFile* rootFile, const std::string &histPrefix, const std::string& axisLabel, ScanParams params):
+    G4UserSteppingAction(), m_rootFile(rootFile), m_histPrefix(histPrefix), m_axisLabel(axisLabel), m_params(params)
+{
+  //Sort the parameters accordingly
+  if (m_params.minU > m_params.maxU) std::swap(m_params.minU, m_params.maxU);
+  if (m_params.minV > m_params.maxV) std::swap(m_params.minV, m_params.maxV);
+  //Calculate step size of the parameters
+  m_stepU = (m_params.maxU - m_params.minU) / m_params.nU;
+  m_stepV = (m_params.maxV - m_params.minV) / m_params.nV;
+  //Set Start values
+  m_curU  = m_params.minU - m_stepU / 2.;
+  m_curV  = m_params.minV + m_stepV / 2.;
+
+  //Convert max depth to G4 units
+  m_params.maxDepth /= Unit::mm;
+}
+
+
+bool MaterialScan::createNext(G4ThreeVector &origin, G4ThreeVector &direction)
+{
+  //Increase the internal coordinates
+  m_curU += m_stepU;
+  if (m_curU >= m_params.maxU) {
+    m_curU  = m_params.minU + m_stepU / 2.;
+    m_curV += m_stepV;
+  }
+  //Reset depth counter
+  m_curDepth = 0;
+
+  //Get the origin and direction of the ray
+  getRay(origin, direction);
+
+  //Check wether we are finished
+  return (m_curV <= m_params.maxV);
+}
+
+void MaterialScan::fillValue(const std::string &name, double value)
+{
+  TH2D* &hist = m_regions[name];
+  if (!hist) {
+    //Create new histogram
+    m_rootFile->cd();
+    hist = new TH2D((m_histPrefix + name).c_str(), (name + ";" + m_axisLabel).c_str(),
+                    m_params.nU, m_params.minU, m_params.maxU,
+                    m_params.nV, m_params.minV, m_params.maxV);
+  }
+  hist->Fill(m_curU, m_curV, value);
+}
+
+void MaterialScan::UserSteppingAction(const G4Step* step)
+{
+  //Get information about radiation and interaction length
+  G4StepPoint* preStepPoint = step->GetPreStepPoint();
+  G4Region* region = preStepPoint->GetPhysicalVolume()->GetLogicalVolume()->GetRegion();
+  G4Material* material = preStepPoint->GetMaterial();
+  double stlen = step->GetStepLength();
+  double x0 = stlen / (material->GetRadlen());
+  double lambda = stlen / (material->GetNuclearInterLength());
+
+  //check if the depth is limited
+  if (m_params.maxDepth > 0) {
+    m_curDepth += stlen;
+    if (m_curDepth > m_params.maxDepth) {
+      //Depth reached, kill track and add remaining part of the material budget
+      G4Track* track = step->GetTrack();
+      track->SetTrackStatus(fStopAndKill);
+      double remaining = stlen - m_curDepth + m_params.maxDepth;
+      x0 *= remaining / stlen;
+      lambda *= remaining / stlen;
+    }
+  }
+
+  //Fill x0 and lambda in a histogram for each region
+  string x0_name = region->GetName() + "_x0";
+  string lambda_name = region->GetName() + "_lambda";
+  fillValue(x0_name, x0);
+  fillValue(lambda_name, lambda);
+}
+
+void MaterialScanSpherical::getRay(G4ThreeVector &origin, G4ThreeVector &direction)
+{
+  //We always shoot from the origin
+  origin.set(0, 0, 0);
+  double theta = m_curU * Unit::deg;
+  double phi   = m_curV * Unit::deg;
+  direction.set(sin(theta) * cos(phi),
+                sin(theta) * sin(phi),
+                cos(theta));
+}
+
+void MaterialScanPlanar::getRay(G4ThreeVector &origin, G4ThreeVector &direction)
+{
+  //We shoot perpendicular to the plane, so direction is always the same but the position varies.
+  origin = m_origin + m_curU / Unit::mm * m_dirU + m_curV / Unit::mm * m_dirV;
+  direction = m_dirW;
+}
+
+MaterialScanModule::MaterialScanModule(): m_rootFile(0)
+{
+  //Set module properties
+  setDescription("This Module is intended to scan the material budget of the "
+                 "geometry.  Currently, there are two different kinds of scans "
+                 "available: Spherical and Planar scan. \n"
+                 "Spherical scan will shoot rays from the origin of the "
+                 "detector and scan along the polar and azimuth angle.\n"
+                 "Planar scan will shoot rays perpendicular to a given "
+                 "plane.");
+
+  addParam("Filename",            m_filename,
+           "The filename where the material scan will be stored",
+           string("MaterialScan.root"));
+  addParam("spherical",           m_doSpherical,
+           "Do a spherical scan, that is shooting rays from the origin with "
+           "varying angles", true);
+  addParam("spherical.nTheta",    m_spherical.nU,
+           "Number of rays in theta", 200);
+  addParam("spherical.minTheta",  m_spherical.minU,
+           "Theta start angle", 17.);
+  addParam("spherical.maxTheta",  m_spherical.maxU,
+           "Theta start angle", 150.);
+  addParam("spherical.nPhi",      m_spherical.nV,
+           "Number of rays in phi", 200);
+  addParam("spherical.minPhi",    m_spherical.minV,
+           "Phi start angle", 0.);
+  addParam("spherical.maxPhi",    m_spherical.maxV,
+           "Phi start angle", 360.);
+  addParam("spherical.maxDepth",  m_spherical.maxDepth,
+           "Maximum scan depth in cm. The ray will be killed after having "
+           "reached the maximum Depth. <=0 means no Limit.", -1.0);
+
+
+  addParam("planar",              m_doPlanar,
+           "Do a plane scan, that is shooting parallel rays from a defined "
+           "plane", true);
+  addParam("planar.plane",        m_planeName,
+           "Plane to use for scanning, available are all two letter "
+           "combinations of X,Y and Z like XY and XZ and custom", string("ZX"));
+  addParam("planar.nU",           m_planar.nU,
+           "Number of rays in U", 200);
+  addParam("planar.minU",         m_planar.minU,
+           "U start", -400.0);
+  addParam("planar.maxU",         m_planar.maxU,
+           "U stop", 400.0);
+  addParam("planar.nV",           m_planar.nV,
+           "Number of rays in V", 200);
+  addParam("planar.minV",         m_planar.minV,
+           "V start", -400.0);
+  addParam("planar.maxV",         m_planar.maxV,
+           "V stop", 400.0);
+  addParam("planar.maxDepth",     m_planar.maxDepth,
+           "Maximum scan depth in cm. The ray will be killed after having "
+           "reached the maximum Depth. <=0 means no Limit.", -1.0);
+  addParam("planar.custom",       m_customPlane,
+           "Parameters of the plane when choosing custom. This is supposed to "
+           "be a list of 9 values, the first three are the coordinates of the "
+           "plane origin, the second three is the direction of U and the last "
+           "three are the direction of V", m_customPlane);
+
+}
+
+void MaterialScanModule::initialize()
+{
+  m_rootFile = new TFile(m_filename.c_str(), "RECREATE");
+
+  boost::to_lower(m_planeName);
+  boost::trim(m_planeName);
+  //Check if plane definition makes sense
+  if (m_planeName == "custom") {
+    if (m_customPlane.size() < 9) {
+      B2ERROR("planar.custom: At least 9 values needed to define custom plane, only " << m_customPlane.size() << " provided");
+    }
+  } else if (m_planeName.size() != 2 || !boost::all(m_planeName, boost::is_any_of("xyz"))) {
+    B2ERROR("planar.plane: Only custom or any two letter combinations of X,Y and Z are recognized");
+  } else if (m_planeName[0] == m_planeName[1]) {
+    B2ERROR("planar.plane: " << m_planeName << " not valid, cannot use the same axis twice");
+  }
+
+  //Convert plane definition to mm since Geant4 is of course using other units
+  BOOST_FOREACH(double &value, m_customPlane) value /= Unit::mm;
+}
+
+void MaterialScanModule::terminate()
+{
+  if (m_rootFile) {
+    m_rootFile->Write();
+    m_rootFile->Close();
+  }
+}
+
+void MaterialScanModule::beginRun()
+{
+  G4EventManager *eventManager = G4EventManager::GetEventManager();
+
+  //First we save all user actions
+  G4UserEventAction    *vanillaEventAction    = eventManager->GetUserEventAction();
+  G4UserStackingAction *vanillaStackingAction = eventManager->GetUserStackingAction();
+  G4UserTrackingAction *vanillaTrackingAction = eventManager->GetUserTrackingAction();
+  G4UserSteppingAction *vanillaSteppingAction = eventManager->GetUserSteppingAction();
+
+  //Then we clear the user actions
+  eventManager->SetUserAction((G4UserEventAction*)0);
+  eventManager->SetUserAction((G4UserStackingAction*)0);
+  eventManager->SetUserAction((G4UserTrackingAction*)0);
+
+  //We create our own stepping actions which will
+  //- create the vectors for shooting rays
+  //- record the material budget for each ray
+  vector<MaterialScan*> scans;
+  if (m_doSpherical) {
+    scans.push_back(new MaterialScanSpherical(m_rootFile, m_spherical));
+  }
+  if (m_doPlanar) {
+    G4ThreeVector origin(0, 0, 0);
+    G4ThreeVector uDir;
+    G4ThreeVector vDir;
+    if (m_planeName == "custom") {
+      origin.set(m_customPlane[0], m_customPlane[1], m_customPlane[2]);
+      uDir.set(m_customPlane[3], m_customPlane[4], m_customPlane[5]);
+      vDir.set(m_customPlane[6], m_customPlane[7], m_customPlane[8]);
+    } else {
+      uDir = getAxis(m_planeName[0]);
+      vDir = getAxis(m_planeName[1]);
+    }
+    scans.push_back(new MaterialScanPlanar(m_rootFile, origin, uDir, vDir, m_planar));
+  }
+
+  //Do each configured scan
+  BOOST_FOREACH(MaterialScan* scan, scans) {
+    //Set the Scan as steppingaction to see material
+    eventManager->SetUserAction(scan);
+    //Now we can scan
+    G4RayShooter rayShooter;
+    int evtNr(1);
+    G4ThreeVector origin;
+    G4ThreeVector direction;
+    //Create one event per ray and process it
+    while (scan->createNext(origin, direction)) {
+      G4Event* event = new G4Event(evtNr++);
+      rayShooter.Shoot(event, origin, direction);
+      eventManager->ProcessOneEvent(event);
+    }
+    //Free the scan object
+    delete scan;
+  }
+
+  //And now we re set the user actions
+  eventManager->SetUserAction(vanillaEventAction);
+  eventManager->SetUserAction(vanillaStackingAction);
+  eventManager->SetUserAction(vanillaTrackingAction);
+  eventManager->SetUserAction(vanillaSteppingAction);
+}
+
+G4ThreeVector MaterialScanModule::getAxis(char name)
+{
+  switch (name) {
+    case 'x': return G4ThreeVector(1, 0, 0);
+    case 'y': return G4ThreeVector(0, 1, 0);
+    case 'z': return G4ThreeVector(0, 0, 1);
+    default:
+      B2FATAL("Unknown axis: " << name);
+  }
+  return G4ThreeVector(0, 0, 0);
+}

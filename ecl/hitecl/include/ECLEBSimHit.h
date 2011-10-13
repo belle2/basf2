d9680e5d0eb333bdfb7d9255325f41c8bfd164fb
Poyuan Chen <poyuan@hep1.phys.ntu.edu.tw>
2011-10-13 15:42:04 +0000

    add ECLEBhit class
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@2101 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ ecl/hitecl/include/ECLEBSimHit.h
@@ -0,0 +1,98 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Martin Heck, Guofu Cao                                   *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef ECLEBSIMHIT_H
+#define ECLEBSIMHIT_H
+
+#include <TObject.h>
+#include <TVector3.h>
+
+#define DEFAULT_ECLEBSIMHITS           "ECLSimHits"
+#define DEFAULT_MCPART_TO_ECLEBSIMHITS "MCPartToECLSimHits"
+
+namespace Belle2 {
+
+  //! Example Detector
+  class ECLEBSimHit : public TObject {
+  public:
+
+    //! The cell id of this hit.
+    int m_cellId;
+
+    //! The track id of this hit.
+    int m_trackId;
+
+    //! Particle PDG (can be one of secondaries).
+    int m_pdg;
+
+    //! Deposited energy of this hit.
+    double m_edep;
+
+    //! The momentum of pre-step.
+    TVector3 m_momentum;
+
+    //! The method to set cell id
+    void setCellId(int cellId) { m_cellId = cellId; }
+
+    //! The method to set track id
+    void setTrackId(int trackId) { m_trackId = trackId; }
+
+    //! The method to set PDG code
+    void setPDGCode(int pdg) { m_pdg = pdg; }
+
+    //! The method to set deposited energy
+    void setEnergyDep(double edep) { m_edep = edep; }
+
+    //! The method to set momentum
+    void setMomentum(TVector3 momentum) { m_momentum = momentum; }
+
+
+    //! The method to get cell id
+    int getCellId() const { return m_cellId; }
+
+    //! The method to get track id
+    int getTrackId() const { return m_trackId; }
+
+    //! The method to get PDG code
+    int getPDGCode() const { return m_pdg; }
+
+
+    //! The method to get deposited energy
+    double getEnergyDep() const { return m_edep; }
+
+    //! The method to get momentum
+    TVector3 getMomentum() const { return m_momentum; }
+
+    //! Empty constructor
+    /*! Recommended for ROOT IO
+    */
+    ECLEBSimHit() {;}
+
+    //! Useful Constructor
+    ECLEBSimHit(
+      int cellId,
+      int trackId,
+      int pdg,
+      double edep,
+      TVector3 momentum) {
+      m_cellId = cellId;
+      m_trackId = trackId;
+      m_pdg = pdg;
+      m_edep = edep;
+      m_momentum = momentum;
+    }
+
+    ClassDef(ECLEBSimHit, 1);/**< the class title */
+
+  };
+
+} // end namespace Belle2
+
+#endif

aed2459aef9fccd833b6e33488f5f6d33630b86c
Oksana Brovchenko <oksana.brovchenko@kit.edu>
2011-10-18 12:32:42 +0000

    Update tracking examples. Caution: currently no SVDTrueHits are produced and the geometry conversion does not work for glue in PXD, so the execution of the steering files may fail.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@2119 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/examples/CDCPatternRecoFitting.py
+++ tracking/examples/CDCPatternRecoFitting.py
@@ -7,19 +7,13 @@
 # perfoms the simulation and pattern recognition in der CDC. Subsequent one also can fit the resulting tracks.
 #
 # EvtMetaGen and EvtMetaInfo generates and shows event meta data (see example in the framework package).
-# ParamLoaderXML and GeoBuilder are used to create the Belle2 detector geometry.
-# The generator used in this example is geant4 particle gun (PGunInput) (see example in the simulation or generator package how to set a random seed).
-# FullSim performs the full simulation and PrintMCParticles shows MCParticles information.
+# Gearbox and Geometry are used to create the Belle2 detector geometry.
+# The generator used in this example is geant4 particle gun (see example in the simulation or generator package).
+# FullSim performs the full simulation.
 
 # CDCDigi creates the detecotor response in the CDC for the simulated Hits.
-# SVDDigi creates the detecotor response in the SVD for the simulated Hits.
-# PXDDigi creates the detecotor response in the PXD for the simulated Hits.
 
-# CDCRecoHitMaker creates RecoHits, which are needed and used during the fit.
-# SVDRecoHitMaker creates RecoHits, which are needed and used during the fit.
-# PXDRecoHitMaker creates RecoHits, which are needed and used during the fit.
-
-# CDCTracking performs pattern recognition in the CDC based on conformal algorithm. GFTrackCandidates and corresponding relations between TrackCandidates and CDCRecoHits are created.
+# CDCTracking performs pattern recognition in the CDC based on conformal algorithm. GFTrackCandidates with corresponding hit incides and start values are created.
 # GenFitter fits the found GFTrackCandidates and created two track collections: GFTracks (Genfit class) and Tracks (class with helix parametrization)
 # Caution: it is still under testing how well GenFitter can handle 'unperfect' pattern reco track candidates, so the fitting may fail here...
 #
@@ -32,90 +26,90 @@ from basf2 import *
 
 # Register necessary modules
 evtmetagen = register_module('EvtMetaGen')
+
+# one event
+evtmetagen.param('ExpList', [0])
+evtmetagen.param('RunList', [1])
+evtmetagen.param('EvtNumList', [1])
+
 evtmetainfo = register_module('EvtMetaInfo')
 
 # Create geometry
-paramloader = register_module('ParamLoaderXML')
-geobuilder = register_module('GeoBuilder')
+gearbox = register_module('Gearbox')
+geometry = register_module('Geometry')
+
+# if you want you can simulate only tracking detectors, if you want to simulate the whole detector, comment the next line out
+geometry.param('Components', ['MagneticField', 'BeamPipe', 'PXD', 'SVD', 'CDC'
+               ])
 
 # Simulation
-pGun = register_module('PGunInput')
+pGun = register_module('ParticleGun')
+
+# choose the particles you want to simulate
+param_pGun = {
+    'pdgCodes': [13, -13],
+    'randomSeed': 1028307,
+    'nTracks': 1,
+    'momentumGeneration': 'uniform',
+    'momentumParams': [0.8, 1.2],
+    'thetaGeneration': 'fixed',
+    'thetaParams': [100., 100.],
+    'phiGeneration': 'uniform',
+    'phiParams': [0, 360],
+    'vertexGeneration': 'uniform',
+    'xVertexParams': [0.0, 0.0],
+    'yVertexParams': [0.0, 0.0],
+    'zVertexParams': [0.0, 0.0],
+    }
+
+pGun.param(param_pGun)
+
 g4sim = register_module('FullSim')
-mcparticle = register_module('PrintMCParticles')
 
 # Digitizer
 cdcDigitizer = register_module('CDCDigi')
 
-# Create RecoHits
-cdcrecohitmaker = register_module('CDCRecoHitMaker')
+# use one gaussian with resolution of 0.01 in the digitizer
+param_cdcdigi = {'Fraction': 1, 'Resolution1': 0.01, 'Resolution2': 0.0}
+cdcDigitizer.param(param_cdcdigi)
 
 # Pattern Recognition
 cdctracking = register_module('CDCTracking')
 
-# Fitting
-cdcfitting = register_module('GenFitter')
-# cdcfitting.logging.log_level = LogLevel.DEBUG
-
-# Output
-output = register_module('SimpleOutput')
-
-import random
-intseed = random.randint(1, 10000000)
-
-# Set parameters
-
-# one event
-evtmetagen.param('EvtNumList', [1])
-
-paramloader.param('InputFileXML', os.path.join(basf2datadir,
-                  'simulation/Belle2.xml'))
-
-# 5 particles with uniform momentum distribution between 0.5 an 2 GeV
-param_pGun = {
-    'PIDcodes': [211, -211],
-    'Rseed': intseed,
-    'nTracks': 5,
-    'pPar1': 0.5,
-    'pPar2': 2.0,
-    'thetaPar1': 87,
-    'thetaPar2': 120,
-    }
-
-pGun.param(param_pGun)
-mcparticle.param('onlyPrimaries', 1)
-
-# Input collections: all types of CDCHits
-# Output collections: names of TrackCandidates
-# TextFileOutput: if set to 1, textfiles with hitcoordinates are produced, only usefull to plot and visualize some events
 param_cdctracking = {
     'CDCSimHitsColName': 'CDCSimHits',
     'CDCHitsColName': 'CDCHits',
-    'CDCRecoHitsColName': 'CDCRecoHits',
     'CDCTrackCandidatesColName': 'CDCTrackCandidates',
-    'GFTrackCandidatesColName': 'GFTrackCandidates_PatternReco',
-    'GFTrackCandToCDCRecoHitsColName': 'GFTrackCandidateToCDCRecoHits_PatternReco',
+    'GFTrackCandidatesColName': 'GFTrackCands_PatternReco',
     'TextFileOutput': 0,
     }
 cdctracking.param(param_cdctracking)
 
-# Input collections: names for RecoHits and GFTrackCandidates
-# Output collections: names for GFTracks and Tracks
-# Here only CDCHits hits are available
+# Matches the found track candidate with an MCParticle
+mcmatching = register_module('CDCMCMatching')
+
+param_mcmatching = {'GFTrackCandidatesColName': 'GFTrackCands_PatternReco'}
+mcmatching.param(param_mcmatching)
+
+# Fitting
+cdcfitting = register_module('GenFitter')
+
 param_cdcfitting = {
-    'GFTrackCandidatesColName': 'GFTrackCandidates_PatternReco',
-    'CDCRecoHitsColName': 'CDCRecoHits',
-    'SVDRecoHitsColName': 'SVDRecoHits',
-    'PXDRecoHitsColName': 'PXDRecoHits',
-    'TracksColName': 'Tracks',
-    'GFTracksColName': 'GFTracks',
-    'UseCDCHits': 1,
-    'UseSVDHits': 0,
-    'UsePXDHits': 0,
+    'GFTrackCandidatesColName': 'GFTrackCands_PatternReco',
+    'TracksColName': 'Tracks_PatternReco',
+    'GFTracksColName': 'GFTracks_PatternReco',
+    'StoreFailedTracks': 1,
+    'mcTracks': 0,
+    'FilterId': 0,
+    'NIterations': 1,
+    'ProbCut': 0.001,
     }
 
 cdcfitting.param(param_cdcfitting)
 
-output.param('outputFileName', 'PatternRecoTracks.root')
+# Output
+output = register_module('SimpleOutput')
+output.param('outputFileName', 'CDCPatternRecoOutput.root')
 
 # Create paths
 main = create_path()
@@ -123,21 +117,19 @@ main = create_path()
 # Add modules to paths
 main.add_module(evtmetagen)
 main.add_module(evtmetainfo)
-main.add_module(paramloader)
-main.add_module(geobuilder)
+
+main.add_module(gearbox)
+main.add_module(geometry)
 main.add_module(pGun)
 main.add_module(g4sim)
-# main.add_module(mcparticle)
 
 main.add_module(cdcDigitizer)
-main.add_module(cdcrecohitmaker)
 
 main.add_module(cdctracking)
-
-# uncomment this module if you only want to look on pattern reco (without fitting)
+main.add_module(mcmatching)
 main.add_module(cdcfitting)
-
 main.add_module(output)
 
 # Process events
 process(main)
+print statistics
--- tracking/examples/MCFitting.py
+++ tracking/examples/MCFitting.py
@@ -4,18 +4,17 @@
 ###########################################################################################################################
 #
 # This steering file creates the Belle II detector geometry,
-# perfoms the simulation and MC based track finding and fitting.
+# and perfoms the simulation and MC based track finding and fitting.
 #
 # EvtMetaGen and EvtMetaInfo generates and shows event meta data (see example in the framework package).
-# ParamLoaderXML and GeoBuilder are used to create the Belle2 detector geometry.
-# The generator used in this example is geant4 particle gun (PGunInput) (see example in the simulation or generator package how to set a random seed).
-# FullSim performs the full simulation and PrintMCParticles shows MCParticles information.
+# Gearbox and Geometry are used to create the Belle2 detector geometry.
+# The generator used in this example is geant4 particle gun (see example in the simulation or generator package).
+# FullSim performs the full simulation.
 
-# CDCDigi creates the detecotor response in the CDC for the simulated Hits.
-# SVDDigi creates the detecotor response in the SVD for the simulated Hits.
-# PXDDigi creates the detecotor response in the PXD for the simulated Hits.
+# CDCDigi creates the detector response in the CDC for the simulated Hits.
+# For the PXD and SVD currently the TrueHits are used (created directly by the sensitive detector), will be replaced by realistic clusters later on.
 
-# MCTrackFinder creates relations between MCParticles and CDCRecoHits produced by it.
+# MCTrackFinder creates relations between MCParticles and CDCHits/PXDTrueHits/SVDTrueHits produced by it.
 # GenFitter fits the found MCTracks and created two track collections: GFTracks (Genfit class) and Tracks (class with helix parametrization)
 #
 # For details about module parameters just type > basf2 -m .
@@ -27,95 +26,81 @@ from basf2 import *
 
 # Register necessary modules
 evtmetagen = register_module('EvtMetaGen')
+
+# one event
+evtmetagen.param('ExpList', [0])
+evtmetagen.param('RunList', [1])
+evtmetagen.param('EvtNumList', [1])
+
 evtmetainfo = register_module('EvtMetaInfo')
 
 # Create geometry
-paramloader = register_module('ParamLoaderXML')
-geobuilder = register_module('GeoBuilder')
+gearbox = register_module('Gearbox')
+geometry = register_module('Geometry')
+
+# if you want you can simulate only tracking detectors, if you want to simulate the whole detector, comment the next line out
+geometry.param('Components', ['MagneticField', 'BeamPipe', 'PXD', 'SVD', 'CDC'
+               ])
 
 # Simulation
-pGun = register_module('PGunInput')
-g4sim = register_module('FullSim')
-mcparticle = register_module('PrintMCParticles')
+pGun = register_module('ParticleGun')
 
-# Digitizer
-cdcDigitizer = register_module('CDCDigi')
-pxdDigitizer = register_module('PXDDigi')
-pxdClusterizer = register_module('PXDClusterizer')
-svdDigitizer = register_module('SVDDigi')
-
-# Find MCTracks
-mctrackfinder = register_module('MCTrackFinder')
-# mctrackfinder.logging.log_level = LogLevel.DEBUG
-
-# Fitting
-cdcfitting = register_module('GenFitter')
-# cdcfitting.logging.log_level = LogLevel.DEBUG
-
-# Output
-output = register_module('SimpleOutput')
-
-import random
-intseed = random.randint(1, 10000000)
-
-# Set parameters
-
-# one event
-evtmetagen.param('EvtNumList', [1])
-
-paramloader.param('InputFileXML', os.path.join(basf2datadir,
-                  'simulation/Belle2.xml'))
-
-# 5 particles with uniform momentum distribution between 0.5 an 2 GeV
+# choose the particles you want to simulate
 param_pGun = {
-    'PIDcodes': [211, -211],
-    'Rseed': intseed,
-    'nTracks': 5,
-    'pPar1': 0.5,
-    'pPar2': 2.0,
-    'thetaPar1': 87,
-    'thetaPar2': 120,
+    'pdgCodes': [13, -13],
+    'randomSeed': 1028307,
+    'nTracks': 1,
+    'momentumGeneration': 'uniform',
+    'momentumParams': [0.8, 1.2],
+    'thetaGeneration': 'fixed',
+    'thetaParams': [100., 100.],
+    'phiGeneration': 'uniform',
+    'phiParams': [0, 360],
+    'vertexGeneration': 'uniform',
+    'xVertexParams': [0.0, 0.0],
+    'yVertexParams': [0.0, 0.0],
+    'zVertexParams': [0.0, 0.0],
     }
 
 pGun.param(param_pGun)
-mcparticle.param('onlyPrimaries', 1)
 
-# Parameters of the MCTrackFinder.
-# Input collections: names for MCParticle collection as well as for the collections of Hits.
-# Options: select hits from which detectors should be used (all hits selected here are then used in the fit)
-# Smearing: smear the true MCMomentum bevor set it as start values for the GFTrackCand (it has to be investigated if it is better for the fit)
-# Output collections: names for GFTrackCandidates, relations between GFTrackCandidates and Hits and GFTrackCandidates and MCParticles.
+g4sim = register_module('FullSim')
+
+# Digitizer
+cdcDigitizer = register_module('CDCDigi')
+
+# use one gaussian with resolution of 0.01 in the digitizer
+param_cdcdigi = {'Fraction': 1, 'Resolution1': 0.01, 'Resolution2': 0.0}
+cdcDigitizer.param(param_cdcdigi)
+
+# Find MCTracks
+mctrackfinder = register_module('MCTrackFinder')
+
+# select which detectors you would like to use and if the values storen in the track candidates should be smeared
 param_mctrackfinder = {
-    'MCParticlesColName': 'MCParticles',
-    'CDCHitsColName': 'CDCHits',
-    'SVDHitsColName': 'SVDHits',
-    'PXDHitsColName': 'PXDHits',
     'UseCDCHits': 1,
     'UseSVDHits': 1,
     'UsePXDHits': 1,
-    'GFTrackCandidatesColName': 'GFTrackCandidates',
-    'GFTrackCandToMCParticleColName': 'GFTrackCandidateToMCParticle',
     'Smearing': 0,
     }
 mctrackfinder.param(param_mctrackfinder)
 
-# Input collections: names for Hits and GFTrackCandidates
-# Output collections: names for GFTracks and Tracks
-# MCTrack and Tracks from pattern recognition have to be handled slightly different (unknown pdg for pattern reco tracks), so set 'mcTracks' to 1 if you are fitting Tracks from MCTrackFinderModule.
-param_cdcfitting = {
-    'GFTrackCandidatesColName': 'GFTrackCandidates',
-    'CDCHitsColName': 'CDCHits',
-    'SVDHitsColName': 'SVDHits',
-    'PXDHitsColName': 'PXDHits',
-    'TracksColName': 'Tracks',
-    'GFTracksColName': 'GFTracks',
-    'mcTracks': 1,
-    }
+# Fitting
+cdcfitting = register_module('GenFitter')
 
+# fit the tracks with Kalman filter
+param_cdcfitting = {
+    'StoreFailedTracks': 0,
+    'mcTracks': 1,
+    'FilterId': 0,
+    'NIterations': 1,
+    'ProbCut': 0.001,
+    }
 cdcfitting.param(param_cdcfitting)
 
-# Name of the output root file
-output.param('outputFileName', 'FittedMCTracks.root')
+# Output
+output = register_module('SimpleOutput')
+output.param('outputFileName', 'MCFittingOutput.root')
 
 # Create paths
 main = create_path()
@@ -123,20 +108,20 @@ main = create_path()
 # Add modules to paths
 main.add_module(evtmetagen)
 main.add_module(evtmetainfo)
-main.add_module(paramloader)
-main.add_module(geobuilder)
+
+main.add_module(gearbox)
+main.add_module(geometry)
 main.add_module(pGun)
 main.add_module(g4sim)
-# main.add_module(mcparticle)
 
 main.add_module(cdcDigitizer)
-main.add_module(svdDigitizer)
-main.add_module(pxdDigitizer)
-main.add_module(pxdClusterizer)
 
 main.add_module(mctrackfinder)
 main.add_module(cdcfitting)
+
 main.add_module(output)
 
 # Process events
 process(main)
+
+print statistics

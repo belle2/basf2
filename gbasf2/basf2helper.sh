37f1f58099078bb64c5841b1770622aa4c419fdb
Yanliang Han <hanyl@ihep.ac.cn>
2011-10-24 08:34:04 +0000

    basf2help disorder
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@2160 b38c6c8f-bee8-465d-8448-9abe6915d496

--- gbasf2/basf2helper.sh
+++ gbasf2/basf2helper.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-# basf2helper - performs top and tail work to allow basf2 to function well
+# basf2helper - performs top and tail work to allow basf2 to function well 
 #               in a grid environment
 # $1 = steering file
 # $2 = release
 # Tom Fifield - 2010-01
-# Yanliang Han 2011-09 Update the grid basf2 software initialization.
 
 ##############################################################################
 # Initialisation
 ##############################################################################
-
 #VO_BELLE_SW_DIR=${VO_BELLE_SW_DIR:-$OSG_APP/belle}
 #VO_BELLE2_SW_DIR=$VO_BELLE_SW_DIR/belle2
 #BELLE2_TOOLS=$VO_BELLE2_SW_DIR/tools
@@ -21,20 +19,23 @@
 #setuprel
 #cd $WORKDIR
 
-VO_BELLE_SW_DIR = $ {VO_BELLE_SW_DIR: -$OSG_APP / belle}
-                  unset VO_BELLE2_SW_DIR
-                  . $ {VO_BELLE_SW_DIR} / belle2 / tools / setup_belle2.sh
-                  WORKDIR = `pwd`
-                            cd $ {VO_BELLE_SW_DIR} / belle2 / releases / build - 2011 - 07 - 01
-                            setuprel build - 2011 - 07 - 01
-                            cd $WORKDIR
+echo "HHH"
+echo $VO_BELLE_SW_DIR
+
+VO_BELLE_SW_DIR=${VO_BELLE_SW_DIR:-$OSG_APP/belle}
+unset VO_BELLE2_SW_DIR
+. ${VO_BELLE_SW_DIR}/belle2/tools/setup_belle2.sh
+WORKDIR=`pwd`
+cd ${VO_BELLE_SW_DIR}/belle2/releases/build-2011-07-01
+setuprel build-2011-07-01
+cd $WORKDIR
 
 
-                            basf2 - i > basf2.error 2 > &1
-                            if [ $? -ne 0 ]; then
-cat basf2.error
-echo "basf2 not installed correctly"
-exit 1
+basf2 -i > basf2.error 2>&1
+if [ $? -ne 0 ]; then
+  cat basf2.error
+  echo "basf2 not installed correctly"
+  exit 1
 fi
 
 ##############################################################################
@@ -42,9 +43,14 @@ fi
 ##############################################################################
 basf2 $1
 
+
+##########
+#touch hanyltest1.root
+#touch hanyltest2.root
+##########
+
+
 ##############################################################################
 # Work with output files, metadata
 ##############################################################################
-ls - l
-find ..
-. / gbasf2output.py - s $1
+./gbasf2output.py -s $1

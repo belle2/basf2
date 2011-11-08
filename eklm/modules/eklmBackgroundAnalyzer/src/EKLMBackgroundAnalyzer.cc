209921cb1e9747c86414855039d4b84765f92054
Timofey Uglov <uglov@itep.ru>
2011-11-08 03:09:22 +0000

    remove extra output, add some comments
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@2247 b38c6c8f-bee8-465d-8448-9abe6915d496

--- eklm/modules/eklmBackgroundAnalyzer/src/EKLMBackgroundAnalyzer.cc
+++ eklm/modules/eklmBackgroundAnalyzer/src/EKLMBackgroundAnalyzer.cc
@@ -72,8 +72,6 @@ void EKLMBackgroundAnalyzerModule::readAndSortBkgHits()
       it->second.push_back(bkgHitsArray[i]);
     }
   }
-//   cout << bkgHitsArray.getEntries() << " bkgHits read ";
-//   cout << "in " << m_HitVolumeMap.size() << " volumes" << endl;
 
   B2INFO("EKLMBackgroundAnalyzerModule::readAndSortBkgHits()  completed");
 }
@@ -89,8 +87,7 @@ void EKLMBackgroundAnalyzerModule::makeGraphs()
     // we have only tree graphs here, so edge is completely defined by it's track ID
     // map to store (edge  <--> hit) == (vertex <--> hit) correspondence
     map < int , EKLMBackHit*> hitMap;
-//     cout << endl;
-//     cout << "Name: " << endl << volumeIterator->first << endl;
+
     for (vector<EKLMBackHit*>::iterator i = volumeIterator->second.begin(); i != volumeIterator->second.end(); i++) {
       // ID key
       int key = (*i)->getTrackID();
@@ -117,13 +114,13 @@ void EKLMBackgroundAnalyzerModule::makeGraphs()
       }
     }
 
-
     //  Now we have map with key of the track ID
     // we are ready to create the graph
+
+    // since vertex numbering always starts from 0, we will use NUMBER OF TRACK IN THE MAP (started from 0) istead of Track ID itself (which could be quite large).
+    // this procedure drastically decreases number of graph vertices
+
     adjacency_list <> G;
-
-
-
 
     // used for parent IDs of track coming from abroad to remain graph splittable
     int non_exsisting_rtracks_counter = hitMap.size();
@@ -134,31 +131,19 @@ void EKLMBackgroundAnalyzerModule::makeGraphs()
 
       map < int , EKLMBackHit*>::iterator parentIterator =
         hitMap.find(hitIterator->second->getParentTrackID());
-//       cerr << "IT1: " << distance(hitMap.begin(), hitIterator) << endl;
-//       cerr << "IT2: " << distance(hitMap.begin(), parentIterator) << endl;
 
       if (parentIterator != hitMap.end()) {
-        //        cout << "yes" << endl;
+        // see comments ahead on the vertex numbering
         add_edge(distance(hitMap.begin(), hitIterator), distance(hitMap.begin(), parentIterator)  , G);
       } else {
-        //        cout << "no" << endl;
         add_edge(distance(hitMap.begin(), hitIterator), non_exsisting_rtracks_counter++  , G);
       }
     }
 
 
-    cerr << "GRAPH OK" << endl;
 
     graph_traits < adjacency_list <> >::vertex_iterator it, end;
     property_map < adjacency_list <>, vertex_index_t >::type  index_map = get(vertex_index, G);
-
-//     cout << "" << endl;
-//     cout << "vertices:" << endl;
-
-//     for (boost::tie(it, end) = vertices(G); it != end; ++it) {
-//       cout << get(index_map, *it) << endl;
-//     }
-//     cout << endl;
 
     // search for connected subgraphs
     vector<int> component(num_vertices(G));
@@ -171,8 +156,8 @@ void EKLMBackgroundAnalyzerModule::makeGraphs()
 
     // loop over the vertices
     for (map < int , EKLMBackHit*>::iterator hitIterator = hitMap.begin(); hitIterator != hitMap.end(); hitIterator++) {
-      // get EKLMBackHit corresponding to the current vertex
 
+      // get EKLMBackHit corresponding to the current vertex
       EKLMBackHit * backhit = hitIterator->second;
 
       // search for the current component in the map
@@ -200,29 +185,10 @@ void EKLMBackgroundAnalyzerModule::makeGraphs()
           current->second->setPDGCode(backhit->getPDG());
         }
       }
-      //  cout <<     "Vertex " << distance(hitMap.begin(),hitIterator) << " is in component " << component[distance(hitMap.begin(),hitIterator)] << endl;
     }
-    //    cout << endl;
 
 
 
-
-
-    /*
-
-
-    cout<<"*************** "<<hitMap.find(i)->first<<" "<<hitMap.find(i)->second<<endl;
-    cout<<"*pos: ("<<hitMap.find(i)->second->getPosition().x()<<","<<hitMap.find(i)->second->getPosition().y()<<","<<hitMap.find(i)->second->getPosition().z()<<")"<<endl;
-    cout<<"*time: "<<hitMap.find(i)->second->getTime()<<endl;
-    cout<<"*edep: "<<hitMap.find(i)->second->getEDep()<<endl;
-    cout<<"*pdg: "<<hitMap.find(i)->second->getPDG()<<endl;
-    cout<<"*ID : "<<hitMap.find(i)->second->getParentTrackID()<<" "<<hitMap.find(i)->second->getTrackID()<<endl;
-    cout<<"***************"<<endl;
-
-
-
-
-    */
 
 
 

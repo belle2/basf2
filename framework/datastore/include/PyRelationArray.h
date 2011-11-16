403c39badc1de6a4221d71d0936a9574410ffd06
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2011-11-16 13:14:16 +0000

    Add PyRelationArray::getToIndices(int from) for convenience
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@2274 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/datastore/include/PyRelationArray.h
+++ framework/datastore/include/PyRelationArray.h
@@ -6,10 +6,24 @@
 #include "TObject.h"
 
 #include <string>
+#include <vector>
+#include <map>
 
 namespace Belle2 {
-  /** a python wrapper for RelationArray.
+  /** a Python wrapper for RelationArray.
    *
+   * Note that constructing the map used by getToIndices() requires looping
+   * over all relations, so you'll want to have only one PyRelationArray per
+   * name and event.
+   *
+   * Example (Loop over MCParticles and find corresponding PXDSimHits):
+   *   mcpart_to_pxdhits = Belle2.PyRelationArray('MCParticlesToPXDSimHits')
+   *   #...
+   *   for mcpart_index in range(num_mcparticles):
+   *     pxdhits_for_current_particle = mcpart_to_pxdhits.getToIndices(mcpart_index)
+   *     for hit_index in pxdhits_for_current_particle:
+   *       hit = pxdhits[hit_index]
+   *       #do something with hit
    */
   class PyRelationArray : public TObject {
   public:
@@ -17,18 +31,38 @@ namespace Belle2 {
     * @param name Name of the relation array
     * @param durability 0: event, 1: run, 2: persistent
     */
-    PyRelationArray(const std::string &name, int durability = 0):
-        TObject(),
-        m_relations(name, DataStore::EDurability(durability)) { }
+    PyRelationArray(const std::string &name, int durability = 0);
 
     /** returns number of relations */
-    int getEntries() const { return m_relations.getEntries(); }
+    int getEntries() const {
+      return isValid() ? m_relations.getEntries() : 0;
+    }
 
-    /** returns the RelationElement at index i. */
-    const RelationElement& operator [](int i) const {return m_relations[i];}
+    /** returns true if the associated RelationArray exists, false otherwise.
+     *
+     * Note that getEntries() and getToIndices() are safe even for invalid
+     * arrays.
+     */
+    bool isValid() const { return (bool)m_relations; }
+
+    /** returns a vector of 'to' indices corresponding to the given 'from' index, or an empty list if 'from' cannot be found.
+     *
+     * This function internally uses a map to ensure fast lookups.
+     */
+    std::vector<unsigned int> getToIndices(unsigned int from) const;
+
+    /** returns the RelationElement at index i.
+     *
+     * @param i Index, should be in range [0, getEntries()-1]
+     */
+    const RelationElement& operator [](int i) const { return m_relations[i];}
 
   private:
-    RelationArray m_relations; /**< wrapped object */
+    /** wrapped object */
+    RelationArray m_relations;
+
+    /** maps from-index to to-indices */
+    std::map<unsigned int, std::vector<unsigned int> > m_toindices_map;
 
     ClassDef(PyRelationArray, 0);
   };
--- framework/datastore/src/PyRelationArray.cc
+++ framework/datastore/src/PyRelationArray.cc
@@ -2,4 +2,34 @@
 
 using namespace Belle2;
 
+PyRelationArray::PyRelationArray(const std::string &name, int durability):
+    TObject(),
+    m_relations(name, DataStore::EDurability(durability))
+{
+  if (!isValid())
+    return;
+
+  //build index
+  for (int i = 0; i < m_relations.getEntries(); i++) {
+    const RelationElement &relation = m_relations[i];
+    const int from = relation.getFromIndex();
+
+    if (m_toindices_map.find(from) != m_toindices_map.end()) {
+      //assuming that RelationArray has been consolidate()'ed
+      B2FATAL("PyRelationArray: key already exists!");
+    }
+
+    m_toindices_map[from] = relation.getToIndices();
+  }
+}
+
+std::vector<unsigned int> PyRelationArray::getToIndices(unsigned int from) const
+{
+  std::map<unsigned int, std::vector<unsigned int> >::const_iterator iter = m_toindices_map.find(from);
+  if (iter == m_toindices_map.end())
+    return std::vector<unsigned int>();
+
+  return iter->second;
+}
+
 ClassImp(PyRelationArray);

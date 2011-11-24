86226d763edd16888b4b04a6b338e8fce9b5c05d
Yanliang Han <hanyl@ihep.ac.cn>
2011-11-24 02:37:47 +0000

    Add directory tools and file set_env.sh
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@2303 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ gbasf2/tools/set_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Add gbasf2 path to the environment PATH
+gbasf2_root=`pwd`
+if [ -n "${PATH}" ]; then
+  export PATH=${gbasf2_root}/bin:${PATH}
+else
+  export PATH=${gbasf2_root}/bin
+fi
+

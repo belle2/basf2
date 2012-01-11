36fdba73c10feda0564fb93a8dddd69f571d4799
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2012-01-11 15:30:10 +0000

    add SimpleInput test
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@2469 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ framework/tests/simple_input.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+from basf2 import *
+
+#copy input file into current dir to avoid having the full path in .out file
+try:
+    shutil.copy(os.getenv('BELLE2_LOCAL_DIR')
+    + '/framework/tests/simple_input.root', '.')
+except:
+    pass  # we're probably in tests/ directory, no copy necessary
+
+input = register_module('SimpleInput')
+evtmetainfo = register_module('EvtMetaInfo')
+printcollections = register_module('PrintCollections')
+
+input.param('inputFileName', 'simple_input.root')
+#load all branches, minus PXDClusters
+input.param('branchNames', ['EventMetaData', 'PXDClustersToPXDDigits',
+'PXDClustersToPXDTrueHits', 'PXDDigits', 'PXDTrueHits'])
+
+# Create paths
+main = create_path()
+
+# Add modules to paths
+main.add_module(input)
+main.add_module(evtmetainfo)
+main.add_module(printcollections)
+
+# Process events
+process(main)

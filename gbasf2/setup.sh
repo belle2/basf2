c4fcf557b6adbcf944e8e0b8dd7b0bdc67116f16
Yanliang Han <hanyl@ihep.ac.cn>
2012-03-12 09:12:33 +0000

    debug the setup.sh: DIRAC client should be initialed separately
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@2753 b38c6c8f-bee8-465d-8448-9abe6915d496

--- gbasf2/setup.sh
+++ gbasf2/setup.sh
@@ -1,3 +1,6 @@
-source ~/dirac_ihep/bashrc 
-export PYTHONPATH=$PYTHONPATH:$PWD/lib/
-export PATH=$PATH:$PWD/bin/
+#set up environment used by gbasf2
+#gbasf2 is based on DIRAC, so before using gbasf2 we should make DIRAC available.
+#by Yanliang Han (hanyl@ihep.ac.cn)
+
+export PYTHONPATH=$PWD/lib/:$PYTHONPATH
+export PATH=$PWD/bin/:$PATH

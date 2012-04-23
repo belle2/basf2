47a118450be7f2edf48b6bab5c99edf21203d10c
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2012-04-23 00:54:13 +0000

    Added parallel processing flag to modules.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@2965 b38c6c8f-bee8-465d-8448-9abe6915d496

--- geometry/modules/src/GeometryModule.cc
+++ geometry/modules/src/GeometryModule.cc
@@ -21,6 +21,7 @@ namespace Belle2 {
   GeometryModule::GeometryModule()
   {
     setDescription("Setup geometry description");
+    setPropertyFlags(c_ParallelProcessingCertified | c_InitializeInProcess);
     addParam("geometryPath", m_geometryPath,
              "Path where the parameters for the Geometry can be found", string("/Detector"));
     addParam("geometryType", m_geometryType,

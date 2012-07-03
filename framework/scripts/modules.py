b5336a6d5673e4151841a3b461bdd85324a9fa0e
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2012-07-03 16:13:51 +0000

    Make 'basf2 -m' list only module names and descriptions.
    Use 'basf2 -m ModuleName' (case-sensitive) for parameter listing
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@3230 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/scripts/modules.py
+++ framework/scripts/modules.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
+import textwrap
 from basf2 import *
 
 
@@ -11,14 +13,37 @@ def printAllModules(moduleList):
     register them and print their information
     """
 
+    #how many characters of description to print per line?
+    try:
+        #get terminal width
+        columns = int(os.popen('stty size', 'r').read().split()[1])
+        if columns < 50:
+            columns = 50
+    except:
+        columns = 80
+
     for (moduleName, sharedLib) in sorted(moduleList.iteritems()):
         try:
             current_module = register_module(moduleName)
-            print_params(current_module, False, sharedLib)
         except:
             B2ERROR('The module could not be loaded. This is most likely '
                     + 'caused by a library with missing links.')
 
+        #use automatic word wrapping on module description
+        description = textwrap.wrap(current_module.description(), columns - 22)
+        first = True
+        for text in description:
+            if not first:
+                moduleName = ""
+            first = False
+            print "%-20s  %-s" % (moduleName, text)
+
+    print ''
+    print ('------------------------------------------------------'
+          + '-----------------------')
+    print ''
+    print 'To show detailed information on a module, including its parameters,'
+    print 'type \'basf2 -m ModuleName\'.'
 
 # Get the list of available modules
 avModList = fw.list_available_modules()
@@ -27,7 +52,11 @@ avModList = fw.list_available_modules()
 # Otherwise print all modules.
 if len(sys.argv) == 2:
     if sys.argv[1] in avModList:
-        current_module = register_module(sys.argv[1])
+        try:
+            current_module = register_module(sys.argv[1])
+        except:
+            B2ERROR('The module could not be loaded. This is most likely '
+                    + 'caused by a library with missing links.')
         print_params(current_module, False, avModList[sys.argv[1]])
     else:
         B2ERROR('Print module information: A module with the name "'

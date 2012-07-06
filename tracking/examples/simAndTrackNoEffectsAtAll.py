dc8d75af9ccf5b2a1c6c3a71e84d00701c25bd40
Moritz Nadler <moritz_nadler@gmx.de>
2012-07-06 13:11:08 +0000

    new examples files to show the fine controle off physics/material effects in Geant4 and Genfit via a basf2 steering file
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@3252 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/examples/simAndTrackNoEffectsAtAll.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# WARNING THIS EXAMPLES ONLY WORKS IF ONE MANULAY EDITS THE FILES SVD.xml IN svd/data AND PXD.xml IN pxd/data
+# MORE PRESISELY ONE HAS TO CHANGE THE LINE
+# <SeeNeutrons desc="Set to true to make sensitive detectors see neutrons">False</SeeNeutrons>
+# TO
+# <SeeNeutrons desc="Set to true to make sensitive detectors see neutrons">True</SeeNeutrons>
+# IN BOTH FILES
+
+import os
+from basf2 import *
+set_log_level(LogLevel.ERROR)
+set_random_seed(1)
+# Register necessary modules
+evtmetagen = register_module('EvtMetaGen')
+evtmetainfo = register_module('EvtMetaInfo')
+evtmetagen.param('ExpList', [0])
+evtmetagen.param('RunList', [1])
+evtmetagen.param('EvtNumList', [5000])
+
+# Geometry parameter loader
+gearbox = register_module('Gearbox')
+# Geometry builder
+geometry = register_module('Geometry')
+geometry.param('Components', ['MagneticField', 'PXD', 'SVD'])
+
+# Simulation
+pGun = register_module('ParticleGun')
+param_pGun = {  # this angle is only there to prevent curlers
+    'pdgCodes': [-211, 211],
+    'nTracks': 1,
+    'momentumGeneration': 'uniform',
+    'momentumParams': [1, 1],
+    'thetaGeneration': 'fixed',
+    'thetaParams': [110., 110.],
+    'phiGeneration': 'uniform',
+    'phiParams': [0, 360],
+    'vertexGeneration': 'uniform',
+    'xVertexParams': [0.0, 0.0],
+    'yVertexParams': [0.0, 0.0],
+    'zVertexParams': [0.0, 0.0],
+    }
+pGun.param(param_pGun)
+
+g4sim = register_module('FullSim')
+g4sim.param('UICommands', [  # "/process/inactivate     Transportation",
+                             # "/process/inactivate            nKiller",
+                             # "/process/inactivate G4ErrorStepLengthLimit",
+                             # "/process/inactivate ExtMagFieldLimitProcess",
+                             # "/process/inactivate          ExtNoHits",
+    '/process/list',
+    '/process/inactivate                msc',
+    '/process/inactivate              hIoni',
+    '/process/inactivate            ionIoni',
+    '/process/inactivate              eIoni',
+    '/process/inactivate              eBrem',
+    '/process/inactivate            annihil',
+    '/process/inactivate               phot',
+    '/process/inactivate              compt',
+    '/process/inactivate               conv',
+    '/process/inactivate             hBrems',
+    '/process/inactivate          hPairProd',
+    '/process/inactivate              muMsc',
+    '/process/inactivate             muIoni',
+    '/process/inactivate            muBrems',
+    '/process/inactivate         muPairProd',
+    '/process/inactivate        CoulombScat',
+    '/process/inactivate    PhotonInelastic',
+    '/process/inactivate     ElectroNuclear',
+    '/process/inactivate    PositronNuclear',
+    '/process/inactivate              Decay',
+    '/process/inactivate         hadElastic',
+    '/process/inactivate   NeutronInelastic',
+    '/process/inactivate           nCapture',
+    '/process/inactivate           nFission',
+    '/process/inactivate    ProtonInelastic',
+    '/process/inactivate  PionPlusInelastic',
+    '/process/inactivate PionMinusInelastic',
+    '/process/inactivate  KaonPlusInelastic',
+    '/process/inactivate KaonMinusInelastic',
+    '/process/inactivate KaonZeroLInelastic',
+    '/process/inactivate KaonZeroSInelastic',
+    '/process/inactivate AntiProtonInelastic',
+    '/process/inactivate AntiNeutronInelastic',
+    '/process/inactivate    LambdaInelastic',
+    '/process/inactivate AntiLambdaInelastic',
+    '/process/inactivate SigmaMinusInelastic',
+    '/process/inactivate AntiSigmaMinusInelastic',
+    '/process/inactivate SigmaPlusInelastic',
+    '/process/inactivate AntiSigmaPlusInelastic',
+    '/process/inactivate   XiMinusInelastic',
+    '/process/inactivate AntiXiMinusInelastic',
+    '/process/inactivate    XiZeroInelastic',
+    '/process/inactivate AntiXiZeroInelastic',
+    '/process/inactivate OmegaMinusInelastic',
+    '/process/inactivate AntiOmegaMinusInelastic',
+    '/process/inactivate CHIPSNuclearCaptureAtRest',
+    '/process/inactivate muMinusCaptureAtRest',
+    '/process/inactivate  DeuteronInelastic',
+    '/process/inactivate    TritonInelastic',
+    '/process/inactivate      ExtEnergyLoss',
+    '/process/inactivate       OpAbsorption',
+    '/process/inactivate         OpRayleigh',
+    '/process/inactivate            OpMieHG',
+    '/process/inactivate         OpBoundary',
+    '/process/inactivate              OpWLS',
+    '/process/inactivate           Cerenkov',
+    '/process/inactivate      Scintillation',
+    ])
+# "/process/inactivate        StepLimiter"
+
+mctrackfinder = register_module('MCTrackFinder')
+param_mctrackfinder = {
+    'UseCDCHits': 0,
+    'UseSVDHits': 1,
+    'UsePXDHits': 1,
+    'Smearing': 0,
+    }
+mctrackfinder.param(param_mctrackfinder)
+
+trackfitter = register_module('GenFitter2')
+trackfitter.logging.log_level = LogLevel.WARNING
+trackfitter.param('energyLossBetheBloch', False)
+trackfitter.param('noiseBetheBloch', False)
+trackfitter.param('noiseCoulomb', False)
+# you do not have to switch off bremsstrahlung in Genfit becuse it is only active for electrons (in Genfit!) anyway and this steering file uses only Pions
+trackfitter.param('filterIterations', 3)
+
+trackfitchecker = register_module('TrackFitChecker')
+trackfitchecker.logging.log_level = LogLevel.INFO  # the reults of the statistical tests will only show up at info or debug level
+trackfitchecker.param('testSi', False)
+trackfitchecker.param('writeToTextFile', True)
+trackfitchecker.param('writeToRootFile', True)
+trackfitchecker.param('inspectTracks', False)
+trackfitchecker.param('truthAvailable', False)
+trackfitchecker.param('outputFileName', 'simAndTrackNoEffectsAtAll')
+
+# Create paths
+main = create_path()
+
+# Add modules to paths
+main.add_module(evtmetagen)
+main.add_module(evtmetainfo)
+
+main.add_module(gearbox)
+main.add_module(geometry)
+main.add_module(pGun)
+main.add_module(g4sim)
+main.add_module(mctrackfinder)
+main.add_module(trackfitter)
+main.add_module(trackfitchecker)
+
+# Process events
+process(main)
+
+print statistics
---
+++ tracking/examples/simAndTrackNoEffectsBesidesELoss.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from basf2 import *
+set_log_level(LogLevel.ERROR)
+set_random_seed(1)
+# Register necessary modules
+evtmetagen = register_module('EvtMetaGen')
+evtmetainfo = register_module('EvtMetaInfo')
+evtmetagen.param('ExpList', [0])
+evtmetagen.param('RunList', [1])
+evtmetagen.param('EvtNumList', [5000])
+
+# Geometry parameter loader
+gearbox = register_module('Gearbox')
+# Geometry builder
+geometry = register_module('Geometry')
+geometry.param('Components', ['MagneticField', 'PXD', 'SVD', 'CDC'])
+
+# Simulation
+pGun = register_module('ParticleGun')
+param_pGun = {  # this angle is only there to prevent curlers
+    'pdgCodes': [-13, 13],
+    'nTracks': 1,
+    'momentumGeneration': 'uniform',
+    'momentumParams': [1, 1],
+    'thetaGeneration': 'fixed',
+    'thetaParams': [120., 120.],
+    'phiGeneration': 'uniform',
+    'phiParams': [0, 360],
+    'vertexGeneration': 'uniform',
+    'xVertexParams': [0.0, 0.0],
+    'yVertexParams': [0.0, 0.0],
+    'zVertexParams': [0.0, 0.0],
+    }
+pGun.param(param_pGun)
+
+g4sim = register_module('FullSim')
+
+g4sim.param('EnableVisualization', True)
+
+# in this example I am swiching off the effects only for Muons because engery loss for muons is still acitve which means the still produce seoncondary particles
+# in the detector if these particles also do not have any effects like decay the will curle in the detector for every and the simulation needs ages to compleate
+g4sim.param('UICommands', [  # "/process/inactivate     Transportation mu+",
+                             # "/process/inactivate             muIoni mu+",
+                             # "/process/inactivate            nKiller mu+",
+                             # "/process/inactivate      ExtEnergyLoss mu+",
+                             # "/process/inactivate G4ErrorStepLengthLimit mu+",
+                             # "/process/inactivate ExtMagFieldLimitProcess mu+",
+                             # "/process/inactivate        StepLimiter mu+",
+                             # "/process/inactivate     Transportation mu-",
+                             # "/process/inactivate             muIoni mu-",
+                             # "/process/inactivate            nKiller mu-",
+                             # "/process/inactivate      ExtEnergyLoss mu-",
+                             # "/process/inactivate G4ErrorStepLengthLimit mu-",
+                             # "/process/inactivate ExtMagFieldLimitProcess mu-",
+    '/process/list',
+    '/process/inactivate                msc mu+',
+    '/process/inactivate              hIoni mu+',
+    '/process/inactivate            ionIoni mu+',
+    '/process/inactivate              eIoni mu+',
+    '/process/inactivate              eBrem mu+',
+    '/process/inactivate            annihil mu+',
+    '/process/inactivate               phot mu+',
+    '/process/inactivate              compt mu+',
+    '/process/inactivate               conv mu+',
+    '/process/inactivate             hBrems mu+',
+    '/process/inactivate          hPairProd mu+',
+    '/process/inactivate              muMsc mu+',
+    '/process/inactivate            muBrems mu+',
+    '/process/inactivate         muPairProd mu+',
+    '/process/inactivate        CoulombScat mu+',
+    '/process/inactivate    PhotonInelastic mu+',
+    '/process/inactivate     ElectroNuclear mu+',
+    '/process/inactivate    PositronNuclear mu+',
+    '/process/inactivate              Decay mu+',
+    '/process/inactivate         hadElastic mu+',
+    '/process/inactivate   NeutronInelastic mu+',
+    '/process/inactivate           nCapture mu+',
+    '/process/inactivate           nFission mu+',
+    '/process/inactivate    ProtonInelastic mu+',
+    '/process/inactivate  PionPlusInelastic mu+',
+    '/process/inactivate PionMinusInelastic mu+',
+    '/process/inactivate  KaonPlusInelastic mu+',
+    '/process/inactivate KaonMinusInelastic mu+',
+    '/process/inactivate KaonZeroLInelastic mu+',
+    '/process/inactivate KaonZeroSInelastic mu+',
+    '/process/inactivate AntiProtonInelastic mu+',
+    '/process/inactivate AntiNeutronInelastic mu+',
+    '/process/inactivate    LambdaInelastic mu+',
+    '/process/inactivate AntiLambdaInelastic mu+',
+    '/process/inactivate SigmaMinusInelastic mu+',
+    '/process/inactivate AntiSigmaMinusInelastic mu+',
+    '/process/inactivate SigmaPlusInelastic mu+',
+    '/process/inactivate AntiSigmaPlusInelastic mu+',
+    '/process/inactivate   XiMinusInelastic mu+',
+    '/process/inactivate AntiXiMinusInelastic mu+',
+    '/process/inactivate    XiZeroInelastic mu+',
+    '/process/inactivate AntiXiZeroInelastic mu+',
+    '/process/inactivate OmegaMinusInelastic mu+',
+    '/process/inactivate AntiOmegaMinusInelastic mu+',
+    '/process/inactivate CHIPSNuclearCaptureAtRest mu+',
+    '/process/inactivate muMinusCaptureAtRest mu+',
+    '/process/inactivate  DeuteronInelastic mu+',
+    '/process/inactivate    TritonInelastic mu+',
+    '/process/inactivate          ExtNoHits mu+',
+    '/process/inactivate       OpAbsorption mu+',
+    '/process/inactivate         OpRayleigh mu+',
+    '/process/inactivate            OpMieHG mu+',
+    '/process/inactivate         OpBoundary mu+',
+    '/process/inactivate              OpWLS mu+',
+    '/process/inactivate           Cerenkov mu+',
+    '/process/inactivate      Scintillation mu+',
+    '/process/inactivate                msc mu-',
+    '/process/inactivate              hIoni mu-',
+    '/process/inactivate            ionIoni mu-',
+    '/process/inactivate              eIoni mu-',
+    '/process/inactivate              eBrem mu-',
+    '/process/inactivate            annihil mu-',
+    '/process/inactivate               phot mu-',
+    '/process/inactivate              compt mu-',
+    '/process/inactivate               conv mu-',
+    '/process/inactivate             hBrems mu-',
+    '/process/inactivate          hPairProd mu-',
+    '/process/inactivate              muMsc mu-',
+    '/process/inactivate            muBrems mu-',
+    '/process/inactivate         muPairProd mu-',
+    '/process/inactivate        CoulombScat mu-',
+    '/process/inactivate    PhotonInelastic mu-',
+    '/process/inactivate     ElectroNuclear mu-',
+    '/process/inactivate    PositronNuclear mu-',
+    '/process/inactivate              Decay mu-',
+    '/process/inactivate         hadElastic mu-',
+    '/process/inactivate   NeutronInelastic mu-',
+    '/process/inactivate           nCapture mu-',
+    '/process/inactivate           nFission mu-',
+    '/process/inactivate    ProtonInelastic mu-',
+    '/process/inactivate  PionPlusInelastic mu-',
+    '/process/inactivate PionMinusInelastic mu-',
+    '/process/inactivate  KaonPlusInelastic mu-',
+    '/process/inactivate KaonMinusInelastic mu-',
+    '/process/inactivate KaonZeroLInelastic mu-',
+    '/process/inactivate KaonZeroSInelastic mu-',
+    '/process/inactivate AntiProtonInelastic mu-',
+    '/process/inactivate AntiNeutronInelastic mu-',
+    '/process/inactivate    LambdaInelastic mu-',
+    '/process/inactivate AntiLambdaInelastic mu-',
+    '/process/inactivate SigmaMinusInelastic mu-',
+    '/process/inactivate AntiSigmaMinusInelastic mu-',
+    '/process/inactivate SigmaPlusInelastic mu-',
+    '/process/inactivate AntiSigmaPlusInelastic mu-',
+    '/process/inactivate   XiMinusInelastic mu-',
+    '/process/inactivate AntiXiMinusInelastic mu-',
+    '/process/inactivate    XiZeroInelastic mu-',
+    '/process/inactivate AntiXiZeroInelastic mu-',
+    '/process/inactivate OmegaMinusInelastic mu-',
+    '/process/inactivate AntiOmegaMinusInelastic mu-',
+    '/process/inactivate CHIPSNuclearCaptureAtRest mu-',
+    '/process/inactivate muMinusCaptureAtRest mu-',
+    '/process/inactivate  DeuteronInelastic mu-',
+    '/process/inactivate    TritonInelastic mu-',
+    '/process/inactivate          ExtNoHits mu-',
+    '/process/inactivate       OpAbsorption mu-',
+    '/process/inactivate         OpRayleigh mu-',
+    '/process/inactivate            OpMieHG mu-',
+    '/process/inactivate         OpBoundary mu-',
+    '/process/inactivate              OpWLS mu-',
+    '/process/inactivate           Cerenkov mu-',
+    '/process/inactivate      Scintillation mu-',
+    ])
+# "/process/inactivate        StepLimiter mu-"
+
+# digitizer
+cdcDigitizer = register_module('CDCDigi')
+
+# use one gaussian with resolution of 0.01 in the digitizer (to simplify the fitting)
+param_cdcdigi = {'Fraction': 1, 'Resolution1': 0.01, 'Resolution2': 0.0}
+cdcDigitizer.param(param_cdcdigi)
+
+mctrackfinder = register_module('MCTrackFinder')
+param_mctrackfinder = {
+    'UseCDCHits': 1,
+    'UseSVDHits': 1,
+    'UsePXDHits': 1,
+    'Smearing': 0,
+    }
+mctrackfinder.param(param_mctrackfinder)
+mctrackfinder.logging.log_level = LogLevel.DEBUG
+
+trackfitter = register_module('GenFitter2')
+trackfitter.logging.log_level = LogLevel.WARNING
+trackfitter.param('noiseCoulomb', False)
+# you do not have to switch off bremsstrahlung in Genfit becuse it is only active for electrons (in Genfit!) anyway and this steering file uses only Pions
+trackfitter.param('filterIterations', 3)
+
+trackfitchecker = register_module('TrackFitChecker')
+trackfitchecker.logging.log_level = LogLevel.INFO  # the reults of the statistical tests will only show up at info or debug level
+trackfitchecker.param('testSi', False)
+trackfitchecker.param('writeToTextFile', True)
+trackfitchecker.param('writeToRootFile', True)
+trackfitchecker.param('inspectTracks', False)
+trackfitchecker.param('truthAvailable', False)
+trackfitchecker.param('outputFileName', 'simAndTrackNoEffectsBesidesELoss')
+
+# Create paths
+main = create_path()
+
+# Add modules to paths
+main.add_module(evtmetagen)
+main.add_module(evtmetainfo)
+
+main.add_module(gearbox)
+main.add_module(geometry)
+main.add_module(pGun)
+main.add_module(g4sim)
+main.add_module(cdcDigitizer)
+main.add_module(mctrackfinder)
+main.add_module(trackfitter)
+main.add_module(trackfitchecker)
+
+# Process events
+process(main)
+
+print statistics

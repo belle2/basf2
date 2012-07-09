f210394269bf3c3f9f1e1235766fb398bff9d3cb
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2012-07-09 14:32:14 +0000

    Fix StoreArray::operator!=
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@3264 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/datastore/include/StoreArray.h
+++ framework/datastore/include/StoreArray.h
@@ -123,12 +123,14 @@ namespace Belle2 {
       DataStore::Instance().handleArray<T>(m_name, durability, m_storeArray);
     }
 
-
-    bool operator==(const StoreArray<T> &b) const {                /**< Check if two StoreArrays point to the same array. */
+    /** Check if two StoreArrays point to the same array. */
+    bool operator==(const StoreArray<T> &b) const {
       return (b.m_name == m_name) && (b.m_durability == m_durability);
     }
-    bool operator!=(const StoreArray<T> &b) const {                /**< ...or to different ones. */
-      return *this != b;
+
+    /** Check if two StoreArrays point to the different arrays. */
+    bool operator!=(const StoreArray<T> &b) const {
+      return !(*this == b);
     }
 
     /** Is this StoreArray's data safe to access? */

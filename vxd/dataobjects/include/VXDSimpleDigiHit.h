b45711716c54ca0d1eb4e2e97038f05e8c4d968c
Moritz Nadler <moritz_nadler@gmx.de>
2012-07-22 20:53:13 +0000

    fixing a dependency and some missing doxygen documentation
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@3378 b38c6c8f-bee8-465d-8448-9abe6915d496

--- vxd/dataobjects/include/VXDSimpleDigiHit.h
+++ vxd/dataobjects/include/VXDSimpleDigiHit.h
@@ -31,15 +31,19 @@ namespace Belle2 {
 
   public:
     /** Default constructor for ROOT IO */
-    VXDSimpleDigiHit(): m_sensorID(0), m_u(0), m_v(0), m_vxdTrueHitPtr(NULL) {}
+    VXDSimpleDigiHit(): m_sensorID(0), m_u(0), m_v(0), m_sigU(0), m_sigV(0), m_vxdTrueHitPtr(NULL), m_statusOfSigmas(false) {}
 
     /** Constructor
      * @param sensorID SensorID of the Sensor
      * @param u u coordinate of the hit in local coordinates
      * @param v v coordinate of the hit in local coordinates
+     * @param sigU Measurement error of u coordinate
+     * @param sigV Measurement error of v coordinate
+     * @param vxdTrueHitPtr Pointer to the VXDTrueHit if info in this VXDSimpleDigiHit came from a VXDTrueHit
+     * @param statusOfSigmas states if the sigU and sigV are the real errors or not
      */
-    VXDSimpleDigiHit(VxdID sensorID, float u, float v, float sigU, float sigV, const VXDTrueHit* vxdTrueHitPtr, bool statusOfVariance):
-      m_sensorID(sensorID), m_u(u), m_v(v), m_sigU(sigU), m_sigV(sigV), m_vxdTrueHitPtr(vxdTrueHitPtr), m_statusOfVariance(statusOfVariance)  {}
+    VXDSimpleDigiHit(VxdID sensorID, float u, float v, float sigU, float sigV, const VXDTrueHit* vxdTrueHitPtr, bool statusOfSigmas):
+      m_sensorID(sensorID), m_u(u), m_v(v), m_sigU(sigU), m_sigV(sigV), m_vxdTrueHitPtr(vxdTrueHitPtr), m_statusOfSigmas(statusOfSigmas)  {}
     /** Return the Sensor ID */
     VxdID getSensorID() const { return m_sensorID; }
     /** Return local u coordinate of hit */
@@ -50,23 +54,24 @@ namespace Belle2 {
     float getSigU() const { return m_sigU; }
     /** Return sigma v of hit */
     float getSigV() const { return m_sigV; }
+    /** Return pointer to TrueHit used for creation of this object. Can be NULL if this is a background hit */
     const VXDTrueHit* getTrueHit() const { return m_vxdTrueHitPtr; }
     /** Return  */
-    bool isVarianceCorrect() const { return m_statusOfVariance; }
+    bool isVarianceCorrect() const { return m_statusOfSigmas; }
 
   protected:
 
-    int m_sensorID;           /**< ID of the sensor */
+    VxdID m_sensorID;         /**< ID of the sensor */
     float m_u;                /**< Local u coordinate when crossing detector plane*/
     float m_v;                /**< Local v coordinate when crossing detector plane*/
-    float m_sigU;
-    float m_sigV;
+    float m_sigU;             /**< Measurement error of u coordinate */
+    float m_sigV;             /**< Measurement error of v coordinate */
 
     const VXDTrueHit* m_vxdTrueHitPtr; /**< Pointer to the VXDTrueHit that was used to create this VXDSimpleDigiHit*/
-    bool m_statusOfVariance; /**< set to true if this hit was smeared with the variance set in the recoHit*/
+    bool m_statusOfSigmas; /**< set to true if this hit was smeared with the variance set in the recoHit*/
 
 
-    ClassDef(VXDSimpleDigiHit, 1)
+    ClassDef(VXDSimpleDigiHit, 2)
   };
 
 } // end namespace Belle2

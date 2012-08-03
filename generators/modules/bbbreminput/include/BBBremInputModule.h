ae7432ca9b1f029a8b483cf723070f32f355698c
Andreas Moll <molland@mpp.mpg.de>
2012-08-03 18:23:35 +0000

    Added BBBrem input module. Use it to generate low angle radiative Bhabha scattering.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@3471 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ generators/modules/bbbreminput/include/BBBremInputModule.h
@@ -0,0 +1,68 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2012  Belle II Collaboration                              *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Andreas Moll                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef BBBREMINPUTMODULE_H
+#define BBBREMINPUTMODULE_H
+
+#include <framework/core/Module.h>
+#include <generators/bbbrem/BBBrem.h>
+
+#include <generators/dataobjects/MCParticle.h>
+#include <generators/dataobjects/MCParticleGraph.h>
+
+#include <string>
+
+namespace Belle2 {
+
+  /**
+   * The BBBrem Generator module.
+   * Generates low scattering angle radiative Bhabha events (Beam-Beam Bremsstrahlung).
+   */
+  class BBBremInputModule : public Module {
+
+  public:
+
+    /**
+     * Constructor.
+     * Sets the module parameters.
+     */
+    BBBremInputModule();
+
+    /** Destructor. */
+    virtual ~BBBremInputModule();
+
+    /** Initializes the module. */
+    virtual void initialize();
+
+    /** Method is called for each event. */
+    virtual void event();
+
+    /** Method is called at the end of the event processing. */
+    virtual void terminate();
+
+
+  protected:
+
+    /** Module parameters */
+    int m_boostMode;      /**< The mode of the boost (0 = no boost, 1 = Belle II, 2 = Belle). */
+    double m_photonEFrac; /**< Minimum photon energy fraction. */
+    bool m_unweighted;    /**< True if BBBrem should produce unweighted events. */
+    double m_maxWeight;   /**< The maximum weight. Used for the event rejection procedure to produce unweighted events. */
+
+    /** Variables */
+    BBBrem m_generator;        /**< The BBBrem generator. */
+    MCParticleGraph m_mcGraph; /**< The MCParticle graph object. */
+
+  };
+
+} // end namespace Belle2
+
+
+#endif /* BBBREMINPUTMODULE_H */
---
+++ generators/modules/bbbreminput/src/BBBremInputModule.cc
@@ -0,0 +1,96 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2012  Belle II Collaboration                              *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Andreas Moll                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <generators/modules/bbbreminput/BBBremInputModule.h>
+#include <generators/utilities/cm2LabBoost.h>
+
+#include <framework/core/Environment.h>
+#include <framework/logging/Logger.h>
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/dataobjects/EventMetaData.h>
+#include <framework/dataobjects/RunMetaData.h>
+#include <framework/gearbox/Unit.h>
+
+#include <TLorentzVector.h>
+
+using namespace std;
+using namespace Belle2;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(BBBremInput)
+
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+BBBremInputModule::BBBremInputModule() : Module()
+{
+  //Set module properties
+  setDescription("Generates low scattering angle radiative Bhabha events (Beam-Beam Bremsstrahlung).");
+
+  //Parameter definition
+  addParam("BoostMode", m_boostMode, "The mode of the boost (0 = no boost, 1 = Belle II, 2 = Belle)", 0);
+  addParam("MinPhotonEnergyFraction", m_photonEFrac, "Fraction of the minimum photon energy.", 0.000001);
+  addParam("Unweighted", m_unweighted, "Produce unweighted or weighted events.", true);
+  addParam("MaxWeight", m_maxWeight, "The max weight (only for Unweighted=True).", 2000.0);
+}
+
+
+BBBremInputModule::~BBBremInputModule()
+{
+
+}
+
+
+void BBBremInputModule::initialize()
+{
+  StoreObjPtr<RunMetaData>   runMetaData("RunMetaData",   DataStore::c_Run);
+  double centerOfMassEnergy = 2.0 * sqrt(runMetaData->getEnergyHER() * runMetaData->getEnergyLER());
+
+  m_generator.init(centerOfMassEnergy, m_photonEFrac, m_unweighted, m_maxWeight);
+
+  //Depending on the settings use the Belle II or Belle boost
+  if (m_boostMode == 1) {
+    m_generator.setBoost(getBoost(runMetaData->getEnergyHER(),     runMetaData->getEnergyLER(),
+                                  runMetaData->getCrossingAngle(), runMetaData->getAngleLER()));
+  } else {
+    if (m_boostMode == 2) {
+      m_generator.setBoost(getBoost(7.998213, 3.499218, 22.0 * Unit::mrad, 0.0));
+    }
+  }
+
+  m_generator.enableBoost(m_boostMode > 0);
+}
+
+
+void BBBremInputModule::event()
+{
+  StoreObjPtr<EventMetaData> evtMetaData("EventMetaData", DataStore::c_Event);
+
+  m_mcGraph.clear();
+  double weight = m_generator.generateEvent(m_mcGraph);
+  m_mcGraph.generateList("", MCParticleGraph::c_setDecayInfo | MCParticleGraph::c_checkCyclic);
+
+  evtMetaData->setGeneratedWeight(weight);
+}
+
+
+void BBBremInputModule::terminate()
+{
+  m_generator.term();
+
+  B2INFO("Maximum weight delivered: " << m_generator.getMaxWeightDelivered())
+  B2INFO("Cross-section:            " << m_generator.getCrossSection() << " +- " << m_generator.getCrossSectionError() << " [mb]")
+}

b08c004a336c78dc7bfcfa49bc82cdd3d7e3352f
Martin Heck <martin.heck@kit.edu>
2012-08-19 19:15:41 +0000

    I have created this already in my branch, but I need to test, if the doxygen does the right thing for me. So I have to add this in the trunk.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@3560 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/aaaDocumentation/include/aaaDoxyDocu.h
@@ -0,0 +1,22 @@
+// This is due to its name the first file in the tracking folder seen by Doxygen.
+// I want to define some groups and write the explanation for them.
+// Otherwise the first member of the group seems sometimes to be taken as the description line.
+
+/** @defgroup tracking_dataobjects
+ *  @ingroup dataobjects
+ *  @{
+ *  Dataobjects maintained by the tracking group.
+ *
+ *  @todo test, if this todo ends up in the description.
+ *  @}
+ */
+
+
+/** @defgroup tracking_modules
+ *  @ingroup modules
+ *  @{
+ *  basf2 Modules maintained by the tracking group.
+ *
+ *  @todo test, if this todo ends up in the description.
+ *  @}
+ */

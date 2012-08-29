5ecb0f33cff5279e5b5eba504545ac24dbb345ea
Yanliang Han <hanyl@ihep.ac.cn>
2012-08-29 13:30:40 +0000

    modify tools/set_env.sh so that it can generate script which will be used to set environment of gbasf2
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@3654 b38c6c8f-bee8-465d-8448-9abe6915d496

--- gbasf2/tools/set_env.sh
+++ gbasf2/tools/set_env.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #Add gbasf2 path to the environment PATH
-gbasf2_root=`pwd`/..
-if [ -n "${PATH}" ]; then
-  export PATH=${gbasf2_root}/bin:${PATH}
-else
-  export PATH=${gbasf2_root}/bin
-fi
+GBASF2ROOT=`pwd`/..
+GBASF2LIB=${GBASF2ROOT}/lib
+GBASF2BIN=${GBASF2ROOT}/bin
 
+echo GBASF2ROOT=${GBASF2ROOT} >setup.sh
+echo GBASF2LIB=${GBASF2LIB}   >>setup.sh
+echo GBASF2BIN=${GBASF2BIN}   >>setup.sh
+echo "export PYTHONPATH=${GBASF2ROOT}/lib/:$PYTHONPATH" >>setup.sh
+echo "export PATH=${GBASF2ROOT}/bin/:$PATH" >>setup.sh

09858497a858be17d6ffaa1299747feef10bac7b
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2012-08-30 13:19:15 +0000

    Add test for SeqRoot input module (use output of seqroot_output test)
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@3661 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ framework/tests/seqroot_input.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Test SeqRootInputModule. The input file is created by seqroot_output.py
+
+from basf2 import *
+
+import os
+import shutil
+
+# copy input file into current dir to avoid having the full path in .out file
+try:
+    shutil.copy(os.getenv('BELLE2_LOCAL_DIR')
+                + '/framework/tests/seqroot_input.sroot', '.')
+except:
+    pass  # we're probably in tests/ directory, no copy necessary
+
+# ============================================================================
+# Now lets create the necessary modules to perform a simulation
+
+# Create Event information
+# Show progress of processing
+progress = register_module('Progress')
+# Set input filename
+input = register_module('SeqRootInput')
+input.param('inputFileName', 'seqroot_input.sroot')
+input.logging.log_level = LogLevel.WARNING  # ignore read rate
+
+# ============================================================================
+# Do the simulation
+
+main = create_path()
+# init path
+main.add_module(input)
+main.add_module(register_module('PrintCollections'))
+main.add_module(progress)
+
+process(main)

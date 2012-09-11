97206a6af6f8814af4effe6c3ab87ea94ae43281
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2012-09-11 12:19:36 +0000

    Don't overwrite previous parameters in MCFitting.py example
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@3739 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/examples/MCFitting.py
+++ tracking/examples/MCFitting.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-###########################################################################################################################
+#############################################################################
 #
 # This steering file creates the Belle II detector geometry,
 # and perfoms the simulation and MC based track finding and fitting.
 #
-# EvtMetaGen and EvtMetaInfo generates and shows event meta data (see example in the framework package).
+# EvtMetaGen and EvtMetaInfo generates and shows event meta data (see
+# example in the framework package).
 # Gearbox and Geometry are used to create the Belle2 detector geometry.
-# The generator used in this example is geant4 particle gun (see example in the simulation or generator package).
+# The generator used in this example is geant4 particle gun (see example
+# in the simulation or generator package).
 # FullSim performs the full simulation.
 
 # CDCDigitizer creates the detector response in the CDC for the simulated Hits.
-# For the PXD and SVD currently the TrueHits are used (created directly by the sensitive detector), will be replaced by realistic clusters later on.
+# For the PXD and SVD currently the TrueHits are used (created directly by the
+# sensitive detector), will be replaced by realistic clusters later on.
 
-# MCTrackFinder creates relations between MCParticles and CDCHits/PXDTrueHits/SVDTrueHits produced by it.
-# GenFitter fits the found MCTracks and created two track collections: GFTracks (Genfit class) and Tracks (class with helix parametrization)
+# MCTrackFinder creates relations between MCParticles and CDCHits/PXDTrueHits
+# /SVDTrueHits produced by it.
+# GenFitter fits the found MCTracks and created two track collections: GFTracks
+# (Genfit class) and Tracks (class with helix parametrization)
 #
 # For details about module parameters just type > basf2 -m .
 #
-############################################################################################################################
+###############################################################################
 
 import os
 from basf2 import *
@@ -39,7 +44,7 @@ gearbox = register_module('Gearbox')
 geometry = register_module('Geometry')
 
 # simulate only tracking detectors
-# to simulate the whole detector included in BelleII.xml, comment the next line out
+# to simulate the whole detector included in BelleII.xml, comment the next line
 geometry.param('Components', ['MagneticField', 'BeamPipe', 'PXD', 'SVD', 'CDC'
                ])
 
@@ -73,7 +78,8 @@ g4sim.logging.log_level = LogLevel.ERROR
 # digitizer
 cdcDigitizer = register_module('CDCDigitizer')
 
-# use one gaussian with resolution of 0.01 in the digitizer (to simplify the fitting)
+# use one gaussian with resolution of 0.01 in the digitizer (to simplify
+# fitting)
 param_cdcdigi = {'Fraction': 1, 'Resolution1': 0.01, 'Resolution2': 0.0}
 cdcDigitizer.param(param_cdcdigi)
 
@@ -81,9 +87,13 @@ cdcDigitizer.param(param_cdcdigi)
 mctrackfinder = register_module('MCTrackFinder')
 
 # select which detectors you would like to use
-param_mctrackfinder = {'UseCDCHits': 1, 'UseSVDHits': 1, 'UsePXDHits': 1}
+param_mctrackfinder = {
+    'UseCDCHits': 1,
+    'UseSVDHits': 1,
+    'UsePXDHits': 1,
 # select which particles to use: primary particles
-param_mctrackfinder = {'WhichParticles': ['primary']}
+    'WhichParticles': ['primary']
+}
 mctrackfinder.param(param_mctrackfinder)
 
 # fitting

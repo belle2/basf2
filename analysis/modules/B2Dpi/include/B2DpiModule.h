3cdf3e3bc2cfa3c310c2ab880cc34f9413ff5de5
Tagir Aushev <aushev@itep.ru>
2012-11-01 14:43:08 +0000

    B2Dpi: TOP includes are removed
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@4108 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/modules/B2Dpi/include/B2DpiModule.h
+++ analysis/modules/B2Dpi/include/B2DpiModule.h
@@ -23,9 +23,6 @@
 #include <svd/dataobjects/SVDTrueHit.h>
 #include <cdc/dataobjects/CDCSimHit.h>
 
-#include <top/dataobjects/TOPTrack.h>
-#include <top/dataobjects/TOPLikelihoods.h>
-
 // What is the difference,
 // between RelationArray and RelationIndex
 #include <framework/datastore/RelationArray.h>

2c0d5c676a6d027e9fe3c71ab98f1d44a0635984
Poyuan Chen <poyuan@hep1.phys.ntu.edu.tw>
2012-11-16 04:35:59 +0000

    add ECL Hit module with dataobject ECLDebugHit
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@4210 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ ecl/dataobjects/include/ECLDebugHit.h
@@ -0,0 +1,76 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Poyuan Chen                                              *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef ECLDEBUGHIT_H
+#define ECLDEBUGHIT_H
+
+#include <framework/datastore/DataStore.h>
+
+#include <TObject.h>
+#include <TVector3.h>
+
+#include <generators/dataobjects/SimHitBase.h>
+
+namespace Belle2 {
+
+  /**
+    * ClassECLDebugHit - Geant4 simulated hit for the ECL.
+    *
+    * This class holds particle hit data from geant4 simulation. As the simulated
+    * hit classes are used to generate detector response, they contain _local_
+    * information.
+    */
+  class ECLDebugHit : public TObject {
+
+  public:
+
+
+    //! The cell id of this hit.
+    int m_cellId;
+
+    //! Deposited energy of this hit.
+    double m_edep;
+
+    //! Hit average time
+    double m_TimeAve;
+
+    //! The method to set cell id
+    void setCellId(int cellId) { m_cellId = cellId; }
+
+    //! The method to set deposited energy
+    void setEnergyDep(double edep) { m_edep = edep; }
+
+    //! The method to set hit average time
+    void setTimeAve(double TimeAve) { m_TimeAve = TimeAve; }
+
+    //! The method to get cell id
+    int getCellId() const { return m_cellId; }
+
+    //! The method to get deposited energy
+    double getEnergyDep() const { return m_edep; }
+
+    //! The method to get hit average time
+    double getTimeAve() const {return m_TimeAve;}
+
+
+
+
+    //! Empty constructor
+    /*! Recommended for ROOT IO
+    */
+    ECLDebugHit() {;}
+    /** ROOT Macro.*/
+    ClassDef(ECLDebugHit, 1);
+
+  };
+
+} // end namespace Belle2
+
+#endif

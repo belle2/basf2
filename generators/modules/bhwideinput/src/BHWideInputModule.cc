6d5723c76fd7d32f556cde00d32c1d44a7e8dc74
Jeremy Dalseno <jdalseno@mpp.mpg.de>
2012-11-17 09:07:53 +0000

    Replace RunMetaData stuff with GearBox stuff
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@4233 b38c6c8f-bee8-465d-8448-9abe6915d496

--- generators/modules/bhwideinput/src/BHWideInputModule.cc
+++ generators/modules/bhwideinput/src/BHWideInputModule.cc
@@ -16,7 +16,7 @@
 #include <framework/datastore/StoreObjPtr.h>
 #include <framework/dataobjects/EventMetaData.h>
 #include <framework/gearbox/Unit.h>
-#include <framework/utilities/RunMetaData.h>
+#include <framework/gearbox/GearDir.h>
 
 #include <TLorentzVector.h>
 
@@ -54,8 +54,11 @@ void BHWideInputModule::initialize()
 {
   //Depending on the settings, use the Belle II or Belle boost
   if (m_boostMode == 1) {
-    m_generator.setBoost(getBoost(RunMetaData::getEnergyHER(), RunMetaData::getEnergyLER(),
-                                  RunMetaData::getCrossingAngle(), RunMetaData::getAngleLER()));
+    GearDir ler("/Detector/SuperKEKB/LER/");
+    GearDir her("/Detector/SuperKEKB/HER/");
+
+    m_generator.setBoost(getBoost(her.getDouble("energy"), ler.getDouble("energy"),
+                                  her.getDouble("angle") - ler.getDouble("angle"), ler.getDouble("angle")));
   } else {
     if (m_boostMode == 2) {
 

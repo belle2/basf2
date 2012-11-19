3d61f7023f993c3ff8681979df3222df2445ed74
Yanliang Han <hanyl@ihep.ac.cn>
2012-11-19 05:29:14 +0000

    Remove the file setup.sh under GBASF2ROOT directory. We will only use tools/setup.(c)sh
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@4242 b38c6c8f-bee8-465d-8448-9abe6915d496

--- gbasf2/setup.sh
+++
@@ -1,6 +0,0 @@
-#set up environment used by gbasf2
-#gbasf2 is based on DIRAC, so before using gbasf2 we should make DIRAC available.
-#by Yanliang Han (hanyl@ihep.ac.cn)
-
-export PYTHONPATH=$PWD/lib/:$PYTHONPATH
-export PATH=$PWD/bin/:$PATH

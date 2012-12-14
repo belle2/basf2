772d955d50d3f7e6621d94c7ff9015c62d972819
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2012-12-14 06:06:45 +0000

    Examples to transfer data from COPPER to ReadoutPC. RecvStore1.py is an example to store received RawCOPPER objects in a ROOT file.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@4446 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/rawdata/examples/RecvStore1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+######################################################
+# This steering file shows how to run the framework
+# for different experiment, run and event numbers.
+#
+# In the example below, basf2 will run on and display
+# the following experiment, run and event numbers:
+#
+# Experiment 71, Run  3, 4 Events
+# Experiment 71, Run  4, 6 Events
+# Experiment 73, Run 10, 2 Events
+# Experiment 73, Run 20, 5 Events
+# Experiment 73, Run 30, 3 Events
+#
+# Example steering file - 2011 Belle II Collaboration
+######################################################
+
+from basf2 import *
+
+# Set the log level to show only error and fatal messages
+set_log_level(LogLevel.ERROR)
+# set_log_level(LogLevel.INFO)
+
+# Modules
+receiver = register_module('DeSerializer')
+dump = register_module('PrintCollections')
+perf = register_module('DAQPerf')
+output = register_module('RootOutput')
+
+# RxSocket
+receiver.param('Port', [99999])
+
+# Perf
+perf.param('Cycle', 100000)
+
+# Output
+output.param('outputFileName', 'rawcopperM.root')
+
+# Create main path
+main = create_path()
+
+# Add modules to main path
+main.add_module(receiver)
+# main.add_module(dump)
+main.add_module(output)
+main.add_module(perf)
+
+# Process all events
+process(main)

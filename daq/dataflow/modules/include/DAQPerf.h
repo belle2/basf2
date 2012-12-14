822b57e44e6dbae1a1d0644b15e6be09cec065e1
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2012-12-14 06:08:35 +0000

    DAQ performance monitor module added.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@4447 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/dataflow/modules/include/DAQPerf.h
@@ -0,0 +1,63 @@
+//+
+// File : DAQPerf.h
+// Description : Module to measure data transfer performance
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 4 - Jan - 2012
+//-
+
+#ifndef DAQPERF_H
+#define DAQPERF_H
+
+#include <framework/core/Module.h>
+
+#include <boost/shared_ptr.hpp>
+#include <string>
+#include <vector>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/datastore/StoreArray.h>
+
+#include <framework/dataobjects/EventMetaData.h>
+#include <daq/dataobjects/RawCOPPER.h>
+
+namespace Belle2 {
+
+  /*! A class definition of an input module for Sequential ROOT I/O */
+
+  class DAQPerfModule : public Module {
+
+    // Public functions
+  public:
+
+    //! Constructor / Destructor
+    DAQPerfModule();
+    virtual ~DAQPerfModule();
+
+    //! Module functions to be called from main process
+    virtual void initialize();
+
+    //! Module functions to be called from event process
+    virtual void beginRun();
+    virtual void event();
+    virtual void endRun();
+    virtual void terminate();
+
+    // Data members
+  private:
+    timeval m_t0;
+    int m_nevent;
+    int m_ncycle;
+    bool m_mon;
+    double m_totbytes;
+
+  };
+
+} // end namespace Belle2
+
+#endif // MODULEHELLO_H

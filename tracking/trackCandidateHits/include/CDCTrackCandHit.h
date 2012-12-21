d2876f1c4b00fb9df89282c5798e1b2c641b0541
Moritz Nadler <moritz_nadler@gmx.de>
2012-12-21 14:23:12 +0000

    add CDCTrackCandHit a class that can be used by a cdc track finder to pass the resolution of the l/r ambiguity to Genfit
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@4562 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackCandidateHits/include/CDCTrackCandHit.h
@@ -0,0 +1,48 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2012 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors:  Moritz Nadler                                           *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#ifndef CDCTrackCandHit_H
+#define CDCTrackCandHit_H
+
+#include <TObject.h>
+#include <GFTrackCandHit.h>
+
+namespace Belle2 {
+  /** Namespace for everything inside the tracking package that is not a module or a datastore class*/
+  namespace Tracking {
+    /** This class extends the capabilities of the GFTrackCandHit to store how the left right ambiguity of CDC hits should be resolved.
+     * Track finder that can determine this information should use this class and give it to the GFTrackCand. The information will then be extracted
+     * by the GenFitter module and given to Genfit*/
+    class CDCTrackCandHit : public GFTrackCandHit {
+    public:
+      /** Constructor. First 4 arguments are identical GFTrackCandHit's constructor. 5th argument tells who to resolve the */
+      CDCTrackCandHit(int detId = -1, int hitId = -1, int planeId = -1, double rho =  0., char leftOrRight = 0):
+        GFTrackCandHit(detId, hitId, planeId, rho), m_leftOrRight(leftOrRight) {
+
+      }
+      /** get the info how the left right ambiguity of CDC hits should be resolved*/
+      char getLeftRightResolution() const {return m_leftOrRight;}
+      /** set the info how the left right ambiguity of CDC hits should be resolved*/
+      void setLeftRightResolution(char leftOrRight) {
+        m_leftOrRight = leftOrRight;
+      }
+
+    protected:
+
+      char m_leftOrRight; /**< to store how the left right ambiguity of CDC hits should be resolved*/
+
+//public:
+//  ClassDef(CDCTrackCandHit, 1);
+
+    };
+
+  }
+}
+
+#endif
---
+++ tracking/trackCandidateHits/src/CDCTrackCandHit.cc
@@ -0,0 +1,16 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010-2011  Belle II Collaboration                         *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Moritz Nadler                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <tracking/trackCandidateHits/CDCTrackCandHit.h>
+
+using namespace Belle2;
+using namespace Tracking;
+
+//ClassImp(CDCTrackCandHit)

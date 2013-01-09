59f86f1b5437dfb135e0445651fc75d447d7498c
Kirill Chilikin <chilikin@lebedev.ru>
2013-01-09 11:51:36 +0000

    Fix missing documentation.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@4594 b38c6c8f-bee8-465d-8448-9abe6915d496

--- eklm/geometry/include/GeoESTRCreator.h
+++ eklm/geometry/include/GeoESTRCreator.h
@@ -25,70 +25,70 @@ namespace Belle2 {
   namespace EKLM {
 
     /**
-     * @struct GeometryParams
+     * @struct ESTRGeometryParams
      * @brief ESTR geometry parameters.
      *
-     * @var GeometryParams::phi
+     * @var ESTRGeometryParams::phi
      * The starting angle of the octagonal Endcap KLM shape.
      *
-     * @var GeometryParams::dphi
+     * @var ESTRGeometryParams::dphi
      * The opening angle (shape is extended from phi to phi+dphi).
      *
-     * @var GeometryParams::nsides
+     * @var ESTRGeometryParams::nsides
      * The number of sides (=8 : octagonal).
      *
-     * @var GeometryParams::nboundary
+     * @var ESTRGeometryParams::nboundary
      * The number of boundaries perpendicular to the z-axis.
      *
-     * @var GeometryParams::z
+     * @var ESTRGeometryParams::z
      * The z coordinate of the section specified by input id (=boundary id).
      *
-     * @var GeometryParams::rmin
+     * @var ESTRGeometryParams::rmin
      * The radius of the circle tangent to the sides of the inner polygon.
      *
-     * @var GeometryParams::rmax
+     * @var ESTRGeometryParams::rmax
      * The radius of the circle tangent to the sides of the outer polygon.
      *
-     * @var GeometryParams::zsub
+     * @var ESTRGeometryParams::zsub
      * The length of the tube.
      *
-     * @var GeometryParams::rminsub
+     * @var ESTRGeometryParams::rminsub
      * The inner radius of the tube.
      *
-     * @var GeometryParams::rmaxsub
+     * @var ESTRGeometryParams::rmaxsub
      * The outer radius of the tube.
      *
-     * @var GeometryParams::thick_eiron
+     * @var ESTRGeometryParams::thick_eiron
      * The thickness of the mother vessel of the Endcap KLM module slot.
      *
-     * @var GeometryParams::thick_eiron_meas
+     * @var ESTRGeometryParams::thick_eiron_meas
      * Measured thickness of the mother vessel of the Endcap KLM module slot.
      *
-     * @var GeometryParams::thick_eslot
+     * @var ESTRGeometryParams::thick_eslot
      * The thickness of the Endcap KLM module slot.
      *
-     * @var GeometryParams::thick_eslot_meas
+     * @var ESTRGeometryParams::thick_eslot_meas
      * Measured thickness of the Endcap KLM module slot.
      *
-     * @var GeometryParams::thick_emod
+     * @var ESTRGeometryParams::thick_emod
      * The thickness of the Endcap KLM module.
      *
-     * @var GeometryParams::rmin_emod
+     * @var ESTRGeometryParams::rmin_emod
      * The minimum radius of the Endcap KLM module.
      *
-     * @var GeometryParams::rmax_emod
+     * @var ESTRGeometryParams::rmax_emod
      * The maximum radius of the Endcap KLM module.
      *
-     * @var GeometryParams::rmin_eslot
+     * @var ESTRGeometryParams::rmin_eslot
      * The minimum radius of the Endcap KLM module slot.
      *
-     * @var GeometryParams::rmax_eslot
+     * @var ESTRGeometryParams::rmax_eslot
      * The maximum radius of the Endcap KLM module slot.
      *
-     * @var GeometryParams::rshift_eslot
+     * @var ESTRGeometryParams::rshift_eslot
      * The shift value in the radial direction of the Endcap KLM module slot.
      *
-     * @var GeometryParams::rmax_glass
+     * @var ESTRGeometryParams::rmax_glass
      * The maximum radius of the glass used in KLM module (not used now).
      */
     struct ESTRGeometryParams {

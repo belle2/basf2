ae6c2910fe0d525faa25126613b6b900aeece19a
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2013-01-16 17:24:05 +0000

    Remove getProbability() and related functions from DedxLikelihood
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@4636 b38c6c8f-bee8-465d-8448-9abe6915d496

--- reconstruction/dataobjects/include/DedxLikelihood.h
+++ reconstruction/dataobjects/include/DedxLikelihood.h
@@ -78,47 +78,9 @@ namespace Belle2 {
     /** corresponding setter for m_p */
     void setMomentum(float p) { m_p = p; }
 
-
-    /** returns a probability for a particle hypothesis.
-     *
-     * The probability for the hypothesis m is calculated as
-     * \f[
-       \frac { P_m(p) \mathcal{L}_m } { \sum_k P_k(p) \mathcal{L}_k },
-       \f]
-     * which is limited to values in the interval [0, 1] and guarant
-     * Where k iterates over all hypotheses, including m. \f$ P_m(p) \f$ is the
-     * prior probability for the hypothesis m, which will in general depend on
-     * the particle momentum p.
-     *
-     * If momentum distributions have not been set using loadMomentumPriorsFromFile()
-     * or setMomentumPriors() beforehand, equal prior probabilities will be assumed.
-     *
-     * @param type  The desired particle hypothesis.
-     */
-    double getProbability(Dedx::Particle type) const;
-
-    /** set the prior distributions for the momentum.
-     *
-     * If you want getProbability() to return meaningful values, you should probably call this.
-     * Since this is a static function, this only needs to be done once.
-     *
-     * @param momentumPrior Array of TH1F pointers with Dedx::c_num_particles entries.
-     *                      DedxLikelihood does not take ownership of the associated memory.
-     */
-    static void setMomentumPriors(TH1F** momentumPrior) { s_momentumPrior = momentumPrior; }
-
-    /** Load prior distributions from a file.
-     *
-     * For generic BB events, the PDF file in reconstruction/data/ contains suitable
-     * prior distributions.
-     */
-    static void loadMomentumPriorsFromFile(const std::string& filename);
-
   private:
     float m_logl[Dedx::c_num_particles]; /**< log likelihood for each particle, not including momentum prior */
     float m_p; /**< track momentum used for PDF lookups */
-
-    static TH1F** s_momentumPrior; /**< prior distributions for the momentum, an array with Dedx::c_num_particles elements */
 
     ClassDef(DedxLikelihood, 2); /**< Build ROOT dictionary */
   };

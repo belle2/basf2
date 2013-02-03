15f53a8b7d092ddfd42549e41fd78f8c2f50f3c8
Poyuan Chen <poyuan@hep1.phys.ntu.edu.tw>
2013-02-03 16:41:17 +0000

    add pi0 mass plots
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@4728 b38c6c8f-bee8-465d-8448-9abe6915d496

--- ecl/validation/ECLPi0.C
+++ ecl/validation/ECLPi0.C
@@ -8,10 +8,23 @@ void ECLPi0()
   TFile* output = TFile::Open("ECLPi0.root", "recreate");
 
   // an example of a 1D histogram
-  TH1F* hPi0s = new TH1F("hPi0s", "ECL Reconstructed Pi0 Energy for 1GeV Pi0", 100, 0.2, 1.2);
+  TH1F* hPi0s = new TH1F("hPi0s", "ECL Reconstructed Pi0 Energy for 1 GeV/c Pi0", 100, 0.2, 1.2);
   hPi0s->GetXaxis()->SetTitle("Pi0 Energy (GeV)");
   tree->Draw("ECLPi0s.m_Energy>>hPi0s");
   hPi0s->Write();
+
+  // an example of a 1D histogram
+  TH1F* hPi0mass = new TH1F("hPi0mass", "ECL combined Pi0 Mass for 1 GeV/c Pi0", 100, 0.08, 0.18);
+  hPi0mass->GetXaxis()->SetTitle("Pi0 combined mass (GeV/c^{2})");
+  tree->Draw("ECLPi0s.m_Mass>>hPi0mass");
+  hPi0mass->Write();
+
+  // an example of a 1D histogram
+  TH1F* hPi0massFit = new TH1F("hPi0massFit", "ECL constrainted  Pi0 Mass for 1 GeV/c Pi0", 100, 0.08, 0.18);
+  hPi0massFit->GetXaxis()->SetTitle("Pi0 constrainted  mass (GeV/c^{2})");
+  tree->Draw("ECLPi0s.m_MassFit>>hPi0massFit");
+  hPi0massFit->Write();
+
 
 
   delete output;

7ba0098187c5eabf35cc184a25f984f6a14bf741
Makoto Uchida <uchida@phys.titech.ac.jp>
2013-02-24 01:30:40 +0000

    The default position resolution is set to be 100 um.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@4955 b38c6c8f-bee8-465d-8448-9abe6915d496

--- cdc/modules/cdcDigitizer/src/CDCDigitizerModule.cc
+++ cdc/modules/cdcDigitizer/src/CDCDigitizerModule.cc
@@ -56,7 +56,8 @@ CDCDigitizerModule::CDCDigitizerModule() : Module()
   //float Gauss Parameters
   addParam("Fraction",                    m_fraction,    "Fraction of first Gaussian used to smear drift length in cm",    0.5710);
   addParam("Mean1",                       m_mean1,       "Mean value of first Gaussian used to smear drift length in cm",  0.0000);
-  addParam("Resolution1",                 m_resolution1, "Resolution of first Gaussian used to smear drift length in cm",  0.0089);
+  //  addParam("Resolution1",                 m_resolution1, "Resolution of first Gaussian used to smear drift length in cm",  0.0010);
+  addParam("Resolution1",                 m_resolution1, "Resolution of first Gaussian used to smear drift length in cm",  0.0086);
   addParam("Mean2",                       m_mean2,       "Mean value of second Gaussian used to smear drift length in cm", 0.0000);
   addParam("Resolution2",                 m_resolution2, "Resolution of second Gaussian used to smear drift length in cm", 0.0188);
 

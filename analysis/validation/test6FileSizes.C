1a32dbe0413e430f1092ef5a0e7644bbf6cb6226
Phillip Urquijo <purquijo@gmail.com>
2013-02-26 17:28:08 +0000

    validation summary macros
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@4991 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ analysis/validation/test6FileSizes.C
@@ -0,0 +1,37 @@
+void test6FileSizes(){
+
+ // open the file with simulated and reconstructed EvtGen particles
+  TFile* input = TFile::Open("../GENSIMRECtoDST.dst.root");
+  TTree* meta = (TTree*) input->Get("tree");
+  
+  TH1F* hEvents = new TH1F("hEvents", "number of processed events", 1, 0, 1);
+  hEvents->GetXaxis()->SetTitle("Event");
+  meta->Draw("EventMetaData.m_event>>hEvents");
+  double eventsize = input->GetSize()/1024 / hEvents->GetEntries();
+
+  TFile* inputm = TFile::Open("../DSTtoMDST.mdst.root");
+  TTree* metam = (TTree*) inputm->Get("tree");
+
+  TH1F* hEventsm = new TH1F("hEvents", "number of processed events", 1, 0, 1);
+  hEventsm->GetXaxis()->SetTitle("Event");
+  metam->Draw("EventMetaData.m_event>>hEvents");
+  double eventsizem = inputm->GetSize()/1024 / hEventsm->GetEntries();
+
+  TFile* inputu = TFile::Open("../MDSTtoUDST.udst.root");
+  TTree* metau = (TTree*) inputu->Get("tree");
+
+  TH1F* hEventsu = new TH1F("hEvents", "number of processed events", 1, 0, 1);
+  hEventsu->GetXaxis()->SetTitle("Event");
+  metau->Draw("EventMetaData.m_event>>hEvents");
+  double eventsizeu = inputu->GetSize()/1024 / hEventsu->GetEntries();
+
+
+
+  TFile* output = TFile::Open("FileEventSizes.root", "recreate");
+  TNtuple* bench = new TNtuple("DST event size in kB", "tree", "dst:mdst:udst");
+  bench->Fill(eventsize,eventsizem,eventsizeu);
+  bench->Write();
+  delete output;
+  
+
+}

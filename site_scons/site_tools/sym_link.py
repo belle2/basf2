dbe21085a30c77be3e2b79bb6402a38cc39754a9
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2013-03-10 05:32:38 +0000

    workaround for missing relpath in python versions below 2.6
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@5132 b38c6c8f-bee8-465d-8448-9abe6915d496

--- site_scons/site_tools/sym_link.py
+++ site_scons/site_tools/sym_link.py
@@ -4,6 +4,30 @@
 import os
 from SCons.Builder import Builder
 
+# define relpath for python < 2.6
+if not hasattr(os, 'relpath'):
+
+
+    def relpath(path, start=os.path.curdir):
+        """Return a relative version of a path"""
+
+        if not path:
+            raise ValueError('no path specified')
+
+        start_list = os.path.abspath(start).split(os.path.sep)
+        path_list = os.path.abspath(path).split(os.path.sep)
+
+        # Work out how much of the filepath is shared by start and path.
+        i = len(os.path.commonprefix([start_list, path_list]))
+
+        rel_list = [os.path.pardir] * (len(start_list) - i) + path_list[i:]
+        if not rel_list:
+            return os.path.curdir
+        return os.path.join(*rel_list)
+
+
+os.path.relpath = relpath
+
 
 def create_symlink(target, source, env):
     """Create a symbolic link from source to target"""

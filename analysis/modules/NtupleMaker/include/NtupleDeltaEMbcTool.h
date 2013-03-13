9ee913de643164a6f78ccc0527aa3eb393b2726b
Christian Oswald <oswald@physik.uni-bonn.de>
2013-03-13 09:44:26 +0000

    Fixed extra links in NtupleMaker.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@5161 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/modules/NtupleMaker/include/NtupleDeltaEMbcTool.h
+++ analysis/modules/NtupleMaker/include/NtupleDeltaEMbcTool.h
@@ -32,6 +32,7 @@ namespace Belle2 {
     /** Create branches in m_tree - this function should be called by the constructor only. */
     void setupTree();
   public:
+    /** Constructor. */
     NtupleDeltaEMbcTool(TTree* tree, DecayDescriptor& decaydescriptor) : NtupleFlatTool(tree, decaydescriptor) {setupTree();}
     /** Set branch variables to properties of the provided Particle. */
     void eval(const Particle* p);

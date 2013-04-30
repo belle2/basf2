b93d9de9fbbeda14e4573cdace48aecb4a8fa20e
Koji Hara <koji.hara@kek.jp>
2013-04-30 02:44:38 +0000

    B->D(*)taunu EvtGen model including new physics effects based on PRD87,034028(2013). The two EvtGen models for 2HDM Type2 (BSTD_2HDMTYPE2) and for the generic new physics case (BSTD) including vector/scalar/tensor contributions have been prepared.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@5474 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ generators/evtgen/models/include/EvtBSemiTauonicScalarMesonAmplitude.h
@@ -0,0 +1,34 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2013 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Koji Hara                                                *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef EVTBSEMITAUONICSCALARMESONAMPLITUDE_HH
+#define EVTBSEMITAUONICSCALARMESONAMPLITUDE_HH
+
+#include "generators/evtgen/models/EvtBSemiTauonicAmplitude.h"
+
+/** The class calculates the spin dependent amplitudes of B->Dtaunu decays for the BSemiTauonic model based on [M. Tanaka and R. Watanabe PRD87,034028(2013)].
+*/
+class EvtBSemiTauonicScalarMesonAmplitude : public EvtBSemiTauonicAmplitude {
+
+public:
+
+  /** The function calculates the spin dependent amplitude.
+   *
+   * @param parent a pointer to the parent particle.
+   * @param amp a pointer to fill the calculated spin dependent amplitude.
+   * @param CalcHelAmp a pointer to the calculator of the helicity dependent amplitude.
+   * The function calculate the spin dependent amplitude of the semi-tauonic decay to a scalar meson (D meson).
+   */
+  void CalcAmp(EvtParticle* parent, EvtAmp& amp,
+               EvtBSemiTauonicHelicityAmplitudeCalculator* HelicityAmplitudeCalculator);
+
+};
+
+#endif
---
+++ generators/evtgen/models/include/EvtBSemiTauonicVectorMesonAmplitude.h
@@ -0,0 +1,34 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2013 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Koji Hara                                                *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef EVTBSEMITAUONICVECTORMESONAMPLITUDE_HH
+#define EVTBSEMITAUONICVECTORMESONAMPLITUDE_HH
+
+#include "generators/evtgen/models/EvtBSemiTauonicAmplitude.h"
+
+/** The class calculates the spin dependent amplitudes of B->D*taunu decays for the BSemiTauonic model based on [M. Tanaka and R. Watanabe PRD87,034028(2013)].
+ */
+class EvtBSemiTauonicVectorMesonAmplitude : public EvtBSemiTauonicAmplitude {
+
+public:
+
+  /** The function calculates the spin dependent amplitude.
+   *
+   * @param parent a pointer to the parent particle.
+   * @param amp a pointer to fill the calculated spin dependent amplitude.
+   * @param HelicityAmplitudeCalculator a pointer to the calculator of the helicity dependent amplitude.
+   * The function calculate the spin dependent amplitude of the semi-tauonic decay to a vector meson (D* meson).
+   */
+  void CalcAmp(EvtParticle* parent, EvtAmp& amp,
+               EvtBSemiTauonicHelicityAmplitudeCalculator* HelicityAmplitudeCalculator);
+
+};
+
+#endif

6ad22ecc8bfd1a82e178acae520ad7d9d2e84b31
Phillip Urquijo <purquijo@gmail.com>
2013-05-05 20:40:18 +0000

    modified to allow the definitions to be imported to analyses, following the format of reconstruction and simulation.py
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@5528 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/scripts/modularAnalysis.py
+++ analysis/scripts/modularAnalysis.py
@@ -3,59 +3,69 @@
 
 from basf2 import *
 
-# importing of these definitions doesn't work yet!
-# have to find a proper way to do that!
 
+def Events(
+    path,
+    numlist,
+    runlist,
+    explist,
+    ):
 
-def Events(numlist, runlist, explist):
     evtmetagen = register_module('EvtMetaGen')
     evtmetagen.param({'EvtNumList': numlist, 'RunList': runlist, 'ExpList'
                      : explist})
     main.add_module(evtmetagen)
     progress = register_module('Progress')
-    main.add_module(progress)
+    path.add_module(progress)
 
 
-def Input(filename):
+def Input(path, filename):
     roinput = register_module('RootInput')
     roinput.param('inputFileName', filename)
-    main.add_module(roinput)
+    path.add_module(roinput)
 
 
-def Output(filename):
+def Output(path, filename):
     rooutput = register_module('RootOutput')
     rooutput.param('outputFileName', filename)
-    main.add_module(rooutput)
+    path.add_module(rooutput)
 
 
-def loadMCParticles():
+def loadMCParticles(path):
     ploader = register_module('ParticleLoader')
     ploader.param('UseMCParticles', True)
-    main.add_module(ploader)
+    path.add_module(ploader)
 
 
-def loadReconstructedParticles():
+def loadReconstructedParticles(path):
     ploader = register_module('ParticleLoader')
     ploader.param('UseMCParticles', False)
-    main.add_module(ploader)
+    path.add_module(ploader)
 
 
-def selectParticle(list_name, PDGcode, criteria):
+def selectParticle(
+    path,
+    list_name,
+    PDGcode,
+    criteria,
+    ):
+
     pselect = register_module('ParticleSelector')
     pselect.param('PDG', PDGcode)
     pselect.param('ListName', list_name)
     pselect.param('Select', criteria)
-    main.add_module(pselect)
+    path.add_module(pselect)
 
 
-def applyCuts(list_name, criteria):
+def applyCuts(path, list_name, criteria):
     pselect = register_module('ParticleSelector')
     pselect.param('ListName', list_name)
     pselect.param('Select', criteria)
-    main.add_module(pselect)
+    path.add_module(pselect)
 
 
 def makeParticle(
+    path,
     list_name,
     PDGcode,
     list_of_lists,
@@ -69,20 +79,20 @@ def makeParticle(
     pmake.param('InputListNames', list_of_lists)
     pmake.param('MassCutLow', mL)
     pmake.param('MassCutHigh', mH)
-    main.add_module(pmake)
+    path.add_module(pmake)
 
 
-def fitVertex(list_name, confidenceLevel):
+def fitVertex(path, list_name, confidenceLevel):
     pvfit = register_module('ParticleVertexFitter')
     pvfit.param('ListName', list_name)
     pvfit.param('ConfidenceLevel', confidenceLevel)
-    main.add_module(pvfit)
+    path.add_module(pvfit)
 
 
-def printList(list_name, full):
+def printList(path, list_name, full):
     prlist = register_module('ParticlePrinter')
     prlist.param('ListName', list_name)
     prlist.param('FullPrint', full)
-    main.add_module(prlist)
+    path.add_module(prlist)
 
 

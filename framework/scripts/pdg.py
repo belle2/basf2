9c86a7aff4e20125cc19a7b1f1ad2993418dfa3c
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2013-05-15 10:03:39 +0000

    add PDG <-> name conversion script by Martin Ritter, with some mods
    Example:
    >>> import pdg
    >>> pdg.from_name("pi+")
    211
    >>> pdg.from_names(["e+","e-","gamma"])
    [-11, 11, 22]
    >>> pdg.to_name(321)
    K+
    >>> pdg.to_names([11,-11,-211,3212])
    ['e-', 'e+', 'pi-', 'Sigma0']
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@5567 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ framework/scripts/pdg.py
@@ -0,0 +1,67 @@
+"""
+Small wrapper module to ease access to pdg codes for particle gun or similar
+usage in the steering file
+"""
+
+from ROOT import TDatabasePDG
+database = TDatabasePDG.Instance()
+
+
+def get(name):
+    """
+    Function to return particle information (TParticlePDG) from ROOT Database.
+
+    'name' can be either the name of the particle, or a pdg code.
+    Will throw an LookupError of no such particle exists.
+    """
+    p = database.GetParticle(name)
+    if not p:
+        raise LookupError("No particle with name '%s'" % name)
+
+    return p
+
+
+def from_name(name):
+    """
+    Function to return pdg code for the given particle name.
+
+    >>> pdg.from_name("pi+")
+    211
+    """
+
+    return get(name).PdgCode()
+
+
+def from_names(names):
+    """
+    for a list/tuple of particle names, return list of pdg codes.
+
+    >>> pdg.from_names(["e+","e-","gamma"])
+    [-11, 11, 22]
+    """
+    assert not isinstance(names, str), 'Argument is not a list!'
+
+    return [from_name(n) for n in names]
+
+
+def to_name(pdg):
+    """
+    Return particle name for given pdg code.
+
+    >>> pdg.to_name(321)
+    K+
+    """
+
+    return get(pdg).GetName()
+
+
+def to_names(pdg_codes):
+    """
+    for a list/tuple of pdg codes, return list of paricle names.
+
+    >>> pdg.to_names([11,-11,-211,3212])
+    ['e-', 'e+', 'pi-', 'Sigma0']
+    """
+    assert not isinstance(pdg_codes, int), 'Argument is not a list!'
+
+    return [to_name(pdg) for pdg in pdg_codes]

4c53fac4e17febe54ef7769cba71b11c2192a6f0
Martin Heck <martin.heck@kit.edu>
2013-05-25 14:18:56 +0000

    Test of the recently committed object.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@5623 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/tests/hitPatternCDC.cc
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+#include <tracking/dataobjects/HitPatternCDC.h>
+
+namespace Belle2 {
+#define EXPECT_FATAL(x) EXPECT_EXIT(x,::testing::KilledBySignal(SIGABRT),"");
+
+  /** Set up a few arrays and objects in the datastore */
+  class BitPatternCDCTest : public ::testing::Test {
+  protected:
+  };
+
+  /** Test simple Setters and Getters. */
+  TEST_F(BitPatternCDCTest, settersNGetters)
+  {
+    // Test proper initialization. ---------------------------------------------------------
+    long initValue = 66560; // python >>> 2 ** 16 + 2 ** 10
+    HitPatternCDC myPattern(initValue);
+    EXPECT_EQ(2,  myPattern.getNHits());
+
+    HitPatternCDC myPattern2;
+    EXPECT_EQ(0,  myPattern2.getNHits());
+
+    // Test setting and getting individual bits
+    short layer = 5;
+    myPattern2.setLayer(layer);
+    EXPECT_EQ(1,  myPattern2.getNHits());
+    EXPECT_EQ(1,  myPattern2.getLayer(layer));
+
+    // Test of SuperLayer getters; setters don't make sense.
+    HitPatternCDC bitPatternCDC;
+    bitPatternCDC.setLayer(9);   // SL 2
+    bitPatternCDC.setLayer(15);  // SL 3
+    bitPatternCDC.setLayer(21);  // SL 4
+
+    EXPECT_FALSE(bitPatternCDC.getSLayer(0));
+    EXPECT_TRUE(bitPatternCDC.getSLayer(1));
+
+  }
+}

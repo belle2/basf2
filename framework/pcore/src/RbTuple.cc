d63c91df8d7d5da49c3700a165cb5f6876fa2900
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2013-06-14 07:12:41 +0000

    New algorithm to merge histogram (TFileMerger) is implemented. Now compatible with new paralle processing scheme.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@5775 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/pcore/src/RbTuple.cc
+++ framework/pcore/src/RbTuple.cc
@@ -18,6 +18,7 @@
 #include <dirent.h>
 #include <errno.h>
 
+#include "TFileMerger.h"
 #include "TChain.h"
 #include "TFile.h"
 #include "TH1.h"
@@ -132,19 +133,13 @@ int RbTupleManager::hadd()
     return 0;
   }
 
-  // Make a list of ROOT file created by event processes
-  TList* filelist = new TList();
-  std::vector<std::string> filenames;
-
-  /* OLD
-  for (int i = 0; i < m_nproc; i++) {
-    char filename[1024];
-    sprintf(filename, "%s.%d", m_filename, i);
-    //    filelist->Add(TFile::Open(filename));
-    TFile* tf = new TFile(filename, "READ");
-    filelist->Add(tf) ;
+  // Set up merger with output file
+  TFileMerger merger(false, false);
+  if (! merger.OutputFile(m_filename)) {
+    //    printf ( "RbTupleManager:: error to open output file %s\n", m_filename );
+    B2ERROR("RbTupleManager:: error to open output file " << m_filename);
+    return -1;
   }
-  */
 
   // Open current directory
   std::string dir = ".";
@@ -155,35 +150,27 @@ int RbTupleManager::hadd()
     return errno;
   }
 
+  std::vector<std::string> filenames;
   // Scan the directory and register all histogram files
   std::string compfile = std::string(m_filename) + ".";
   while ((dirp = readdir(dp)) != NULL) {
     std::string curfile = std::string(dirp->d_name);
     if (curfile.compare(0, compfile.size(), compfile) == 0) {
-      //      printf ( "Opening file =%s\n", curfile.c_str() );
-      TFile* tf = new TFile(curfile.c_str(), "READ");
-      filelist->Add(tf) ;
+      //      printf ( "RbTupleManager:: adding  file =%s\n", curfile.c_str() );
+      merger.AddFile(curfile.c_str());
       filenames.push_back(curfile);
     }
   }
   closedir(dp);
 
-  // Open a root file to merge histograms
-  TFile* target = new TFile(m_filename, "recreate");
-
-  //  printf ( "Just sleeping before merging..... pid=%d\n", getpid() );
-  //  sleep ( 10 );
-  MergeRootfile((TDirectory*) target, filelist);
+  // Do Merge
+  if (!merger.Merge()) {
+    //    printf ( "RbTupleManager:: error to merge files\n" );
+    B2ERROR("RbTupleManager:: error to merge files");
+    return -1;
+  }
 
   // Delete temporary files
-  /* OLD
-  for (int i = 0; i < m_nproc; i++) {
-    char filename[1024];
-    sprintf(filename, "%s.%d", m_filename, i);
-    unlink(filename);
-  }
-  */
-
   vector<string>::iterator it;
   for (it = filenames.begin(); it != filenames.end(); ++it) {
     string& hfile = *it;
@@ -202,107 +189,6 @@ int RbTupleManager::hadd()
 
 void RbTupleManager::MergeRootfile(TDirectory* target, TList* sourcelist)
 {
-
-  //  cout << "Target path: " << target->GetPath() << endl;
-  TString path((char*)strstr(target->GetPath(), ":"));
-  path.Remove(0, 2);
-
-  TFile* first_source = (TFile*)sourcelist->First();
-  first_source->cd(path);
-  TDirectory* current_sourcedir = gDirectory;
-  //gain time, do not add the objects in the list in memory
-  Bool_t status = TH1::AddDirectoryStatus();
-  TH1::AddDirectory(kFALSE);
-
-  // loop over all keys in this directory
-  TChain* globChain = 0;
-  TIter nextkey(current_sourcedir->GetListOfKeys());
-  TKey* key, *oldkey = 0;
-  while ((key = (TKey*)nextkey())) {
-
-    //keep only the highest cycle number for each key
-    if (oldkey && !strcmp(oldkey->GetName(), key->GetName())) continue;
-
-    // read object from first source file
-    first_source->cd(path);
-    TObject* obj = key->ReadObj();
-
-    if (obj->IsA()->InheritsFrom("TH1")) {
-      // descendant of TH1 -> merge it
-
-      //      cout << "Merging histogram " << obj->GetName() << endl;
-      TH1* h1 = (TH1*)obj;
-
-      // loop over all source files and add the content of the
-      // correspondant histogram to the one pointed to by "h1"
-      TFile* nextsource = (TFile*)sourcelist->After(first_source);
-      while (nextsource) {
-
-        // make sure we are at the correct directory level by cd'ing to path
-        nextsource->cd(path);
-        TKey* key2 = (TKey*)gDirectory->GetListOfKeys()->FindObject(h1->GetName());
-        if (key2) {
-          TH1* h2 = (TH1*)key2->ReadObj();
-          h1->Add(h2);
-          delete h2;
-        }
-
-        nextsource = (TFile*)sourcelist->After(nextsource);
-      }
-    } else if (obj->IsA()->InheritsFrom("TTree")) {
-
-      // loop over all source files create a chain of Trees "globChain"
-      const char* obj_name = obj->GetName();
-
-      globChain = new TChain(obj_name);
-      globChain->Add(first_source->GetName());
-      TFile* nextsource = (TFile*)sourcelist->After(first_source);
-      //      const char* file_name = nextsource->GetName();
-      // cout << "file name  " << file_name << endl;
-      while (nextsource) {
-
-        globChain->Add(nextsource->GetName());
-        nextsource = (TFile*)sourcelist->After(nextsource);
-      }
-
-    } else if (obj->IsA()->InheritsFrom("TDirectory")) {
-      // it's a subdirectory
-
-      cout << "Found subdirectory " << obj->GetName() << endl;
-
-      // create a new subdir of same name and title in the target file
-      target->cd();
-      TDirectory* newdir = target->mkdir(obj->GetName(), obj->GetTitle());
-
-      // newdir is now the starting point of another round of merging
-      // newdir still knows its depth within the target file via
-      // GetPath(), so we can still figure out where we are in the recursion
-      MergeRootfile(newdir, sourcelist);
-
-    } else {
-
-      // object is of no type that we know or can handle
-      cout << "Unknown object type, name: "
-           << obj->GetName() << " title: " << obj->GetTitle() << endl;
-    }
-
-    // now write the merged histogram (which is "in" obj) to the target file
-    // note that this will just store obj in the current directory level,
-    // which is not persistent until the complete directory itself is stored
-    // by "target->Write()" below
-    target->cd();
-
-    //!!if the object is a tree, it is stored in globChain...
-    if (obj->IsA()->InheritsFrom("TTree"))
-      globChain->Merge(target->GetFile(), 0, "keep");
-    else
-      obj->Write(key->GetName());
-
-  } // while ( ( TKey *key = (TKey*)nextkey() ) )
-
-  // save modifications to target file
-  target->SaveSelf(kTRUE);
-  TH1::AddDirectory(status);
 }
 
 

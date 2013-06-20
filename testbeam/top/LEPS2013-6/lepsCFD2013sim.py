a3e83b9f7d2d2a8e6ecca287fd78ee66e46f5021
Marko Staric <marko.staric@ijs.si>
2013-06-20 15:10:57 +0000

    updated wedge shape, steering file + xml for LEPS tests with CFD electronics
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@5835 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ testbeam/top/LEPS2013-6/lepsCFD2013sim.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from basf2 import *
+
+# LEPS beam test 2013-june using 2.1 GeV/c e+ beam: runs with CFD electronics
+
+# Belle coordinate frame is used: bar along z-axis, perpendicular to x-axis at x=120cm
+# beam fired from x=0
+#
+#   x = z(LEPS)+120 cm
+#   y = -y(LEPS)
+#   z = x(LEPS)
+# ------------------------------------------
+
+# beam definition for exp001 (cosTheta=0.0)
+
+beam01 = {
+    'pdgCodes': [-11],
+    'nTracks': 1,
+    'varyNTracks': False,
+    'momentumGeneration': 'normal',
+    'momentumParams': [2.12, 0.21],
+    'thetaGeneration': 'normal',
+    'thetaParams': [89.4678, 0],
+    'phiGeneration': 'normal',
+    'phiParams': [0, 0],
+    'vertexGeneration': 'normal',
+    'xVertexParams': [0, 0],
+    'yVertexParams': [0, 0.5],
+    'zVertexParams': [123.035, 0.5],
+    }
+
+# beam definition for exp002 (cosTheta=0.39)
+
+beam02 = {
+    'pdgCodes': [-11],
+    'nTracks': 1,
+    'varyNTracks': False,
+    'momentumGeneration': 'normal',
+    'momentumParams': [2.12, 0.21],
+    'thetaGeneration': 'normal',
+    'thetaParams': [66.9446, 0],
+    'phiGeneration': 'normal',
+    'phiParams': [0, 0],
+    'vertexGeneration': 'normal',
+    'xVertexParams': [0, 0],
+    'yVertexParams': [0.1, 0.5],
+    'zVertexParams': [72.568, 0.5],
+    }
+
+# beam definition for exp003 (cosTheta=0.39, impact close to bar side)
+# (no alignment data yet!)
+
+beam03 = {
+    'pdgCodes': [-11],
+    'nTracks': 1,
+    'varyNTracks': False,
+    'momentumGeneration': 'normal',
+    'momentumParams': [2.12, 0.21],
+    'thetaGeneration': 'normal',
+    'thetaParams': [66.9, 0],
+    'phiGeneration': 'normal',
+    'phiParams': [0, 0],
+    'vertexGeneration': 'normal',
+    'xVertexParams': [0, 0],
+    'yVertexParams': [-19.9, 0.5],
+    'zVertexParams': [65.73, 0.5],
+    }
+
+# specify here the beam setup, number of events to simulate and output file
+# -------------------------------------------------------------------------
+beam = beam02
+nevents = [100]
+outfile = 'LEPS-CFDbeamtest02.root'
+# -------------------------------------------------------------------------
+
+# Number of events to generate
+evtmetagen = register_module('EvtMetaGen')
+evtmetagen.param({'evtNumList': nevents, 'runList': [1]})
+
+# particle gun
+particlegun = register_module('ParticleGun')
+particlegun.param(beam)
+print_params(particlegun)
+
+# Show progress of processing
+progress = register_module('Progress')
+
+# Gearbox
+gearbox = register_module('Gearbox')
+gearbox.param('fileName', 'testbeam/top/LEPS2013-6/TOP-CFD.xml')
+
+# Geometry
+geometry = register_module('Geometry')
+geometry.param('Components', ['TOP'])
+
+# Simulation
+simulation = register_module('FullSim')
+
+# TOP digitization
+TOPdigi = register_module('TOPDigitizer')
+TOPdigi.param('electronicJitter', 0)
+TOPdigi.param('timeZeroJitter', 30e-3)
+
+# Output
+output = register_module('RootOutput')
+output.param('outputFileName', outfile)
+
+# suppress messages and warnings during processing:
+set_log_level(LogLevel.ERROR)
+
+# Create path
+main = create_path()
+main.add_module(evtmetagen)
+main.add_module(progress)
+main.add_module(gearbox)
+main.add_module(geometry)
+main.add_module(particlegun)
+main.add_module(simulation)
+main.add_module(TOPdigi)
+main.add_module(output)
+
+# Process events
+process(main)
+
+# Print call statistics
+print statistics

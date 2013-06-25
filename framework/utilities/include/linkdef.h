2479e1785e4c9070500853cd4c57c414821d83ee
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2013-06-25 12:32:39 +0000

    Make XML serialization functions available to Python
    E.g.:
    from ROOT import gSystem
    gSystem.Load('libframework')
    gSystem.Load('libdataobjects')
    
    from ROOT import Belle2
    from ROOT import TVector3
    
    v = TVector3(1, 2, 3)
    
    xmlstring = Belle2.Stream.serialize(v)
    xmlstring = Belle2.Stream.escapeXML(xmlstring)
    print xmlstring
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@5900 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ framework/utilities/include/linkdef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+//not really a class, but it doesn't complain
+#pragma link C++ class Belle2::Stream;
+
+#endif

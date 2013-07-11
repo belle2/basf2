d4046c3ce25b31e329b94f21c681c19da15aa507
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2013-07-11 13:49:20 +0000

    Remove validation dE/dx curve for PXD (not in input file)
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@6035 b38c6c8f-bee8-465d-8448-9abe6915d496

--- reconstruction/validation/dedx1_curves.C
+++ reconstruction/validation/dedx1_curves.C
@@ -37,7 +37,7 @@ void plot(const TString &input_filename)
   const int num_detectors = 3;
   const char* detectors[] = { "PXD", "SVD", "CDC" };
   const double dedx_cutoff[] = { 2.5e6, 2.5e6, 150.0 };
-  for(int idet = 0; idet < num_detectors; idet++) { //PXD not in input file, anyway
+  for(int idet = 1; idet < num_detectors; idet++) { //PXD not in input file, anyway
     tree->Project(TString::Format("dedx_p_%d", idet), TString::Format("DedxTracks.m_dedx_avg_truncated[][%d]:m_p", idet),
         TString::Format("DedxTracks.m_p < 3 && DedxTracks.m_dedx_avg_truncated[][%d] < %f ", idet, dedx_cutoff[idet]));
     TH1* hist = (TH1*)output_file->Get(TString::Format("dedx_p_%d", idet));

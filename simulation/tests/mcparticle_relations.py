6dba62b9282055e5b4dbefcef4c409ef9ca96905
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2013-07-11 17:12:19 +0000

    check MCParticle relations created by SensitiveDetectors
    fixes #848
    Please revert if you don't like it
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@6040 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ simulation/tests/mcparticle_relations.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+from basf2 import *
+
+from basf2 import Module
+from ROOT import Belle2
+
+
+class TestModule(Module):
+    """Test to read relations."""
+
+    def __init__(self):
+        """constructor."""
+
+        super(TestModule, self).__init__()
+
+    def event(self):
+        """reimplementation of Module::event().
+
+        access all relations from/to MCParticles,
+        any invalid indices should be caught.
+        """
+
+        mcparticles = Belle2.PyStoreArray('MCParticles')
+        # this will generate an index internally, checking consistency
+        # (will die with a FATAL if something goes wrong)
+        from_relations = mcparticles[0].getRelationsFrom("ALL")
+        to_relations = mcparticles[0].getRelationsTo("ALL")
+
+
+evtmetagen = register_module('EvtMetaGen')
+evtmetagen.param('evtNumList', [10])
+
+gearbox = register_module('Gearbox')
+geometry = register_module('Geometry')
+pgun = register_module('ParticleGun')
+g4sim = register_module('FullSim')
+# make the simulation less noisy
+g4sim.logging.log_level = LogLevel.ERROR
+
+main = create_path()
+
+main.add_module(evtmetagen)
+main.add_module(gearbox)
+main.add_module(geometry)
+main.add_module(pgun)
+main.add_module(g4sim)
+main.add_module(TestModule())
+
+process(main)

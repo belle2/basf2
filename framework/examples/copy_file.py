060d309b0b54eb88aaecabd4ce9394fa6c3321cb
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2013-07-18 12:51:44 +0000

    Add framework/examples/copy_file.py
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@6136 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ framework/examples/copy_file.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# copy a file by reading it using RootInput and writing it again
+# both input and output file should be given as command line arguments, e.g.:
+# basf2 framework/examples/copy_file.py -i in.root -o out.root
+
+from basf2 import *
+
+#load additional dictionaries to avoid errors
+from ROOT import gSystem
+gSystem.Load('libvxd')
+gSystem.Load('libtracking')
+gSystem.Load('libgenfit')
+gSystem.Load('libgenfitRK')
+
+main = create_path()
+
+input = register_module('RootInput')
+main.add_module(input)
+
+output = register_module('RootOutput')
+main.add_module(output)
+
+process(main)
+
+print statistics

6ae09e50852433728f8ce5e92e6f8527c93e48de
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2013-07-27 15:51:51 +0000

    new function add_mc_reconstruction added for reconstruction with MC tracking
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@6207 b38c6c8f-bee8-465d-8448-9abe6915d496

--- reconstruction/scripts/reconstruction.py
+++ reconstruction/scripts/reconstruction.py
@@ -4,6 +4,60 @@
 from basf2 import *
 
 
+def add_posttracking_reconstruction(path, components=None):
+    """
+    This function adds the standard reconstruction modules after tracking to a path.
+    """
+
+    # TOP reconstruction
+    if components == None or 'TOP' in components:
+        top_rec = register_module('TOPReconstructor')
+        path.add_module(top_rec)
+
+    # ARICH reconstruction
+    if components == None or 'ARICH' in components:
+        arich_rec = register_module('ARICHReconstructor')
+        path.add_module(arich_rec)
+
+    # ECL reconstruction
+    if components == None or 'ECL' in components:
+
+        # shower reconstruction
+        ecl_shower_rec = register_module('ECLReconstructor')
+        path.add_module(ecl_shower_rec)
+
+        # gamma reconstruction
+        gamma_rec = register_module('ECLGammaReconstructor')
+        path.add_module(gamma_rec)
+
+        # pi0 reconstruction
+        pi0_rec = register_module('ECLPi0Reconstructor')
+        path.add_module(pi0_rec)
+
+        # MC matching
+        ecl_mc = register_module('ECLMCMatching')
+        path.add_module(ecl_mc)
+
+    # EKLM reconstruction
+    if components == None or 'EKLM' in components:
+        eklm_rec = register_module('EKLMReconstructor')
+        path.add_module(eklm_rec)
+
+        # Muon reconstruction
+        eklm_muon_rec = register_module('EKLMMuonReconstructor')
+        path.add_module(eklm_muon_rec)
+
+        # K0L reconstruction
+        eklm_k0l_rec = register_module('EKLMK0LReconstructor')
+        path.add_module(eklm_k0l_rec)
+
+    # charged particle PID
+    if components == None or 'PXD' in components or 'SVD' in components \
+        or 'CDC' in components:
+        mdstPID = register_module('MdstPID')
+        path.add_module(mdstPID)
+
+
 def add_reconstruction(path, components=None):
     """
     This function adds the standard reconstruction modules to a path.
@@ -64,53 +118,47 @@ def add_reconstruction(path, components=None):
         ext = register_module('Ext')
         path.add_module(ext)
 
-    # TOP reconstruction
-    if components == None or 'TOP' in components:
-        top_rec = register_module('TOPReconstructor')
-        path.add_module(top_rec)
+    # add further reconstruction modules
+    add_posttracking_reconstruction(path, components)
 
-    # ARICH reconstruction
-    if components == None or 'ARICH' in components:
-        arich_rec = register_module('ARICHReconstructor')
-        path.add_module(arich_rec)
 
-    # ECL reconstruction
-    if components == None or 'ECL' in components:
+def add_mc_reconstruction(path, components=None):
+    """
+    This function adds the standard reconstruction modules with MC tracking to a path.
+    """
 
-        # shower reconstruction
-        ecl_shower_rec = register_module('ECLReconstructor')
-        path.add_module(ecl_shower_rec)
-
-        # gamma reconstruction
-        gamma_rec = register_module('ECLGammaReconstructor')
-        path.add_module(gamma_rec)
-
-        # pi0 reconstruction
-        pi0_rec = register_module('ECLPi0Reconstructor')
-        path.add_module(pi0_rec)
-
-        # MC matching
-        ecl_mc = register_module('ECLMCMatching')
-        path.add_module(ecl_mc)
-
-    # EKLM reconstruction
-    if components == None or 'EKLM' in components:
-        eklm_rec = register_module('EKLMReconstructor')
-        path.add_module(eklm_rec)
-
-        # Muon reconstruction
-        eklm_muon_rec = register_module('EKLMMuonReconstructor')
-        path.add_module(eklm_muon_rec)
-
-        # K0L reconstruction
-        eklm_k0l_rec = register_module('EKLMK0LReconstructor')
-        path.add_module(eklm_k0l_rec)
-
-    # charged particle PID
+    # tracking
     if components == None or 'PXD' in components or 'SVD' in components \
         or 'CDC' in components:
-        mdstPID = register_module('MdstPID')
-        path.add_module(mdstPID)
+
+        # find MCTracks in CDC, SVD, and PXD
+        mc_trackfinder = register_module('MCTrackFinder')
+        if components == None or 'PXD' in components:
+            mc_trackfinder.param('UsePXDHits', 1)
+        if components == None or 'SVD' in components:
+            mc_trackfinder.param('UseSVDHits', 1)
+        if components == None or 'CDC' in components:
+            mc_trackfinder.param('UseCDCHits', 1)
+        path.add_module(mc_trackfinder)
+
+        # track fitting
+        trackfitter = register_module('GenFitter')
+        path.add_module(trackfitter)
+
+        # dE/dx PID
+        dEdxPID = register_module('DedxPID')
+        if components != None and 'SVD' not in components:
+            dEdxPID.param('useSVD', False)
+        if components != None and 'CDC' not in components:
+            dEdxPID.param('useCDC', False)
+        path.add_module(dEdxPID)
+
+        # track extrapolation
+        ext = register_module('Ext')
+        path.add_module(ext)
+
+    # add further reconstruction modules
+    add_posttracking_reconstruction(path, components)
 
 
 def add_mdst_output(path, mc=True):

bc8845a91949a8bbf838ee0adf0883361f112d90
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2013-08-20 15:59:18 +0000

    Add examples for displaying events sent over the network
    (requires daq/dataflow modules)
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@6403 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ display/examples/net_display_send.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import os
+import random
+from basf2 import *
+
+
+# register necessary modules
+evtmetagen = register_module('EvtMetaGen')
+evtmetagen.param('evtNumList', [5000])
+
+
+# create geometry
+gearbox = register_module('Gearbox')
+geometry = register_module('Geometry')
+geometry.param('Components', ['PXD', 'SVD', 'CDC', 'MagneticField'])
+
+particlegun = register_module('ParticleGun')
+
+
+# simulation
+g4sim = register_module('FullSim')
+# make the simulation less noisy
+g4sim.logging.log_level = LogLevel.ERROR
+
+tx = register_module('TxSocket')
+tx.param('DestHostName', 'localhost')
+#tx.param('DestPort', 1111)
+
+# create paths
+main = create_path()
+
+# add modules to paths
+main.add_module(evtmetagen)
+main.add_module(register_module('Progress'))
+
+main.add_module(gearbox)
+main.add_module(geometry)
+main.add_module(particlegun)
+main.add_module(g4sim)
+
+main.add_module(tx)
+
+process(main)
+print statistics

4aceaeebdf54e1c8e5c6dfdb6d036ac6a07d0f7f
Martin Ritter <martin.ritter@lmu.de>
2013-09-02 16:14:39 +0000

    Add PXDDigit sorting Module
    This module allows to sort an existing PXDDigit collection as the
    clusterizer needs the digits in sorted order.
    
    Readout and simulation produce the digits in a sorted way by default but
    in special cases this might be needed. Up to now, the Clusterizer was able
    to sort the digits but as it is almost never used we put it into a
    separate module.
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@6500 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ pxd/modules/pxdReconstruction/include/PXDDigitSorterModule.h
@@ -0,0 +1,65 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Martin Ritter                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef PXDDigitSorterModule_H
+#define PXDDigitSorterModule_H
+
+#include <framework/core/Module.h>
+#include <pxd/dataobjects/PXDDigit.h>
+#include <string>
+
+namespace Belle2 {
+  namespace PXD {
+    /** \addtogroup modules
+     * @{
+     */
+
+    /** The PXDDigitSorter module.
+     *
+     * This module sorts the existing PXDDigits collection and also updates the
+     * corresponding Relation to MCParticles and TrueHits. This is needed for
+     * unsorted pixel data as the Clusterizer expects sorted input.
+     *
+     * @see PXDClusterizerModule
+     */
+    class PXDDigitSorterModule : public Module {
+
+    public:
+      /** Constructor defining the parameters */
+      PXDDigitSorterModule();
+
+      /** Initialize the module */
+      virtual void initialize();
+      /** do the sorting */
+      virtual void event();
+
+    private:
+      /** Name of the collection to use for the PXDDigits */
+      std::string m_storeDigitsName;
+      /** Name of the collection to use for the PXDTrueHits */
+      std::string m_storeTrueHitsName;
+      /** Name of the collection to use for the MCParticles */
+      std::string m_storeMCParticlesName;
+      /** Name of the relation between PXDDigits and MCParticles */
+      std::string m_relDigitMCParticleName;
+      /** Name of the relation between PXDDigits and PXDTrueHits */
+      std::string m_relDigitTrueHitName;
+      /** Copy of the Digits needed for sorting */
+      std::vector<PXDDigit> m_digitcopy;
+      /** Mode: if true, merge duplicate pixels, otherwise delete all but the first occurence */
+      bool m_mergeDuplicates;
+    };//end class declaration
+
+    /** @}*/
+
+  } //end PXD namespace;
+} // end namespace Belle2
+
+#endif // PXDDigitSorterModule_H
---
+++ pxd/modules/pxdReconstruction/src/PXDDigitSorterModule.cc
@@ -0,0 +1,141 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Martin Ritter                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <pxd/modules/pxdReconstruction/PXDDigitSorterModule.h>
+
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/RelationArray.h>
+#include <framework/logging/Logger.h>
+
+#include <generators/dataobjects/MCParticle.h>
+#include <pxd/dataobjects/PXDTrueHit.h>
+#include <pxd/reconstruction/Pixel.h>
+
+using namespace std;
+using namespace Belle2;
+using namespace Belle2::PXD;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(PXDDigitSorter);
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+PXDDigitSorterModule::PXDDigitSorterModule() : Module()
+{
+  //Set module properties
+  setDescription("This module sorts the existing PXDDigits collection and also "
+                 "updates the corresponding Relation to MCParticles and TrueHits. This is "
+                 "needed for unsorted pixel data as the Clusterizer expects sorted input.");
+  setPropertyFlags(c_ParallelProcessingCertified);
+  addParam("merge", m_mergeDuplicates, "If true, merge Pixel information if more than one digit exists for the same address", true);
+  addParam("digits", m_storeDigitsName, "PXDDigit collection name", string(""));
+  addParam("truehits", m_storeTrueHitsName, "PXDTrueHit collection name", string(""));
+  addParam("particles", m_storeMCParticlesName, "MCParticle collection name", string(""));
+  addParam("digitsToTrueHits", m_relDigitTrueHitName, "Digits to TrueHit relation name",
+           string(""));
+  addParam("digitsToMCParticles", m_relDigitMCParticleName, "Digits to MCParticles relation name",
+           string(""));
+}
+
+void PXDDigitSorterModule::initialize()
+{
+  //Register collections
+  StoreArray<PXDDigit> storeDigits(m_storeDigitsName);
+  StoreArray<MCParticle> storeMCParticles(m_storeMCParticlesName);
+  StoreArray<PXDTrueHit> storeTrueHits(m_storeTrueHitsName);
+  storeDigits.required();
+  storeMCParticles.isOptional();
+  storeTrueHits.isOptional();
+
+  RelationArray relDigitMCParticles(storeDigits, storeMCParticles, m_relDigitMCParticleName);
+  RelationArray relDigitTrueHits(storeDigits, storeTrueHits, m_relDigitTrueHitName);
+  relDigitMCParticles.isOptional();
+  relDigitTrueHits.isOptional();
+
+  m_storeDigitsName = storeDigits.getName();
+  m_storeTrueHitsName = storeTrueHits.getName();
+  m_storeMCParticlesName = storeMCParticles.getName();
+
+  m_relDigitTrueHitName = relDigitTrueHits.getName();
+  m_relDigitMCParticleName = relDigitMCParticles.getName();
+}
+
+void PXDDigitSorterModule::event()
+{
+  StoreArray<PXDDigit> storeDigits(m_storeDigitsName);
+  // If not digits, nothing to do
+  if (!storeDigits || !storeDigits.getEntries()) return;
+
+  RelationArray relDigitMCParticle(m_relDigitMCParticleName);
+  RelationArray relDigitTrueHit(m_relDigitTrueHitName);
+
+  //List of mappings from old indices to new indices
+  std::vector<RelationArray::consolidation_type> relationIndices(storeDigits.getEntries());
+  //Mapping of Pixel information to sort according to VxdID, row, column
+  std::map<VxdID, std::multiset<Pixel>> sensors;
+
+  // Fill sensor information to get sorted PXDDigit indices
+  const int nPixels = storeDigits.getEntries();
+  for (int i = 0; i < nPixels; i++) {
+    const PXDDigit* const digit = storeDigits[i];
+    Pixel px(digit, i);
+    VxdID sensorID = digit->getSensorID();
+    sensors[sensorID].insert(px);
+  }
+
+  // Now we loop over sensors and reorder the digits list
+  // To do this, we create a copy of the existing digits
+  m_digitcopy.clear();
+  m_digitcopy.insert(end(m_digitcopy), begin(storeDigits), end(storeDigits));
+  // and a vector to remember which index changed into what
+  unsigned int index(0);
+  // And just loop over the sensors and assign the digits at the correct position
+  for (const auto & sensor : sensors) {
+    const PXD::Pixel* lastpx(0);
+    for (const PXD::Pixel & px : sensor.second) {
+      //Normal case: pixel has different address
+      if (!lastpx || px > *lastpx) {
+        //Overwrite the digit
+        *storeDigits[index] = m_digitcopy[px.getIndex()];
+        //Remember old and new index
+        relationIndices[px.getIndex()] = std::make_pair(index, false);
+        //increment next index
+        ++index;
+      } else {
+        //We already have a pixel at this address, see if we merge or drop the new one
+        if (m_mergeDuplicates) {
+          //Merge the two pixels. As the PXDDigit does not have setters we have to create a new object.
+          const PXDDigit& old = *storeDigits[index - 1];
+          *storeDigits[index - 1] = PXDDigit(old.getSensorID(), old.getUCellID(), old.getVCellID(), old.getUCellPosition(), old.getVCellPosition(), old.getCharge() + m_digitcopy[px.getIndex()].getCharge());
+          relationIndices[px.getIndex()] = std::make_pair(index - 1, false);
+        } else {
+          //Otherwise delete the second pixel by omitting it here and removing relation elements on consolidation
+          relationIndices[px.getIndex()] = std::make_pair(0, true);
+        }
+      }
+      lastpx = &px;
+    }
+  }
+  //Resize if we omitted/merged one or more elements
+  storeDigits.getPtr()->ExpandCreate(index);
+
+  // Finally we just need to reorder the RelationArrays and delete elements
+  // referencing pixels which were ignored because that adress was set more
+  // than once
+  RelationArray::ReplaceVec<> from(relationIndices);
+  RelationArray::Identity to;
+  if (relDigitMCParticle) relDigitMCParticle.consolidate(from, to, RelationArray::c_deleteElement);
+  if (relDigitTrueHit) relDigitTrueHit.consolidate(from, to, RelationArray::c_deleteElement);
+}

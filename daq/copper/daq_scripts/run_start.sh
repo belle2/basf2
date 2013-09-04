e08ddafe6961c55ff6f8d4b4ba9b48a749baac05
Satoru Yamada <satoru.yamada@kek.jp>
2013-09-04 00:30:56 +0000

    Add stuffs about kernel compilation
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@6532 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/copper/daq_scripts/run_start.sh
+++ daq/copper/daq_scripts/run_start.sh
@@ -3,7 +3,7 @@
 # basf2 on COPPER CPU
 #
 /usr/bin/xterm -fn 7x14 -geometry 102x10+0+642 -e ${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr006 1 &
-/usr/bin/xterm -fn 7x14 -geometry 102x10+750+642 -e ${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr007 2 &
+#/usr/bin/xterm -fn 7x14 -geometry 102x10+750+642 -e ${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr007 2 &
 #
 # event builder on ROPC
 #

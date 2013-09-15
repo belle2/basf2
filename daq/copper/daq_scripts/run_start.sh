f05e6f06aa6ae32c12a2d16302befdb9d60cb1fa
Satoru Yamada <satoru.yamada@kek.jp>
2013-09-15 02:02:31 +0000

    Update instructions
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@6713 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/copper/daq_scripts/run_start.sh
+++ daq/copper/daq_scripts/run_start.sh
@@ -2,12 +2,18 @@
 #
 # basf2 on COPPER CPU
 #
-/usr/bin/xterm -fn 7x14 -geometry 102x10+0+642 -e ${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr006 1 9 &
+/usr/bin/xterm -fn 7x14 -geometry 102x10+0+642 -e ${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr006 1 1 &
+#/usr/bin/xterm -fn 7x14 -geometry 102x10+0+642 -e sh -c '${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr006 1 1; read line' &
+#/usr/bin/xterm -fn 7x14 -geometry 102x10+0+642 -e sh -c 'source  ~/.bash_profile; basf2 ${BELLE2_LOCAL_DIR}/daq/rawdata/examples/RecvSendCOPPER.py cpr006 1 1 --no-stat; read line' &
+
 /usr/bin/xterm -fn 7x14 -geometry 102x10+750+642 -e ${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr007 1 1 &
+#/usr/bin/xterm -fn 7x14 -geometry 102x10+750+642 -e sh -c '${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr007 1 1; read line' &
+#/usr/bin/xterm -fn 7x14 -geometry 102x10+750+642 -e sh -c 'source  ~/.bash_profile; basf2 ${BELLE2_LOCAL_DIR}/daq/rawdata/examples/RecvSendCOPPER.py cpr007 1 1 --no-stat; read line' &
 #
 # event builder on ROPC
 #
-${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_eb0.sh &
+/usr/bin/xterm -fn 7x14 -geometry 102x10+0+342 -e ${BELLE2_LOCAL_DIR}/daq/eventbuilder/evb0/eb0 -n 2 cpr006 cpr007 -D -b &
+#${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_eb0.sh &
 
 #
 # basf2 on ROPC

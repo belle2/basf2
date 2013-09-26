a40eabbbc54402ea8e207bf9410edb221e5b3d65
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2013-09-26 18:12:23 +0000

    Update to handle popup
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@6822 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/javalib/b2rc/src/b2rc/java/ui/RCNodeViewPanel.java
+++ daq/slc/javalib/b2rc/src/b2rc/java/ui/RCNodeViewPanel.java
@@ -4,19 +4,32 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.SwingUtilities;
 
 import b2daq.ui.Updatable;
+import b2rc.core.RCCommand;
 import b2rc.core.RCNode;
+import b2rc.core.RCState;
+import b2rc.java.io.RCServerCommunicator;
+import b2rc.java.io.RunControlMessage;
 
 public class RCNodeViewPanel extends JPanel implements Updatable {
 
 	private static final long serialVersionUID = 1L;
 	private RCNode _node;
 	private RCNodeStateLabel _state;
-	
+
 	public RCNodeViewPanel(RCNode node) {
 		_node = node;
 		_state = new RCNodeStateLabel(node);
@@ -38,8 +51,58 @@ public class RCNodeViewPanel extends JPanel implements Updatable {
 				GridBagConstraints.LINE_START, new Insets(0, 5, 0, 5));
 		layout.setConstraints(_state, gbc);
 		add(_state);
+
+		addMouseListener(new MouseListener() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (SwingUtilities.isRightMouseButton(e)) {
+					JPopupMenu popup_menu = new JPopupMenu();
+					ArrayList<RCCommand> command_v = new ArrayList<RCCommand>();
+					if (_node.getState().equals(RCState.INITIAL_S)) {
+						command_v.add(RCCommand.BOOT);
+					} else if (_node.getState().equals(RCState.CONFIGURED_S)) {
+						command_v.add(RCCommand.REBOOT);
+						command_v.add(RCCommand.LOAD);
+					} else if (_node.getState().equals(RCState.READY_S)) {
+						command_v.add(RCCommand.RELOAD);
+						command_v.add(RCCommand.START);
+					} else if (_node.getState().equals(RCState.RUNNING_S)) {
+						command_v.add(RCCommand.PAUSE);
+					} else if (_node.getState().equals(RCState.PAUSED_S)) {
+						command_v.add(RCCommand.RESUME);
+					}
+					command_v.add(RCCommand.RECOVER);
+					JMenu menu = new JMenu("Command");
+					popup_menu.add(menu);
+					for (RCCommand command : command_v) {
+						JMenuItem item = new JMenuItem(command.getAlias());
+						item.addActionListener(new CommandMenuItemListener(
+								command));
+						menu.add(item);
+					}
+					popup_menu.show(e.getComponent(), e.getX(), e.getY());
+				}
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+			}
+
+		});
 	}
-	
+
 	public void update() {
 		_state.update(_node);
 	}
@@ -62,6 +125,24 @@ public class RCNodeViewPanel extends JPanel implements Updatable {
 	private void setGrid(GridBagConstraints gbc, int gridx, int gridy,
 			double weightx, double weighty, int fill, int anchor, Insets insets) {
 		setGrid(gbc, 1, 1, gridx, gridy, weightx, weighty, fill, anchor, insets);
+	}
+
+	private class CommandMenuItemListener implements ActionListener {
+		private RCCommand _command;
+
+		public CommandMenuItemListener(RCCommand command) {
+			_command = command;
+		}
+
+		public void actionPerformed(ActionEvent arg0) {
+			int [] value_v = new int[1];
+			value_v[0] = _node.getIndex();
+			try {
+				RCServerCommunicator.get().sendMessage(new RunControlMessage(_command, value_v));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }

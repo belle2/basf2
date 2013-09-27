c1fadab43191a6026d87693cc75592284cd836b6
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2013-09-27 02:44:35 +0000

    A module to read raw data from RingBuffer and restore them in RawXXX on DataStore
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@6836 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/rfarm/event/modules/include/Raw2DsModule.h
@@ -0,0 +1,92 @@
+//+
+// File : Raw2Ds.h
+// Description : Module to restore RawData in DataStore through RingBuffer
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 26 - Sep - 2013
+//-
+
+#ifndef RAW2DSMODULE_H
+#define RAW2DSMODULE_H
+
+#include <framework/core/Module.h>
+#include <framework/pcore/RingBuffer.h>
+
+#include <boost/shared_ptr.hpp>
+#include <string>
+#include <vector>
+
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/datastore/StoreArray.h>
+
+
+#include <daq/rawdata/modules/DAQConsts.h>
+#include <daq/dataobjects/RawCDC.h>
+#include <daq/dataobjects/RawSVD.h>
+#include <daq/dataobjects/RawECL.h>
+#include <daq/dataobjects/RawEPID.h>
+#include <daq/dataobjects/RawBPID.h>
+#include <daq/dataobjects/RawKLM.h>
+#include <daq/dataobjects/RawCOPPER.h>
+#include <daq/dataobjects/RawHeader.h>
+#include <daq/dataobjects/RawTrailer.h>
+#include <daq/dataobjects/SendHeader.h>
+#include <daq/dataobjects/SendTrailer.h>
+
+
+#include <framework/dataobjects/EventMetaData.h>
+
+#define RBUFSIZE   100000000
+#define MAXEVTSIZE  10000000
+
+#define DETECTOR_MASK 0xFFFFFFFF // tentative
+#define SVD_ID 0x00000001 // tentative
+#define CDC_ID 0x00000002 // tentative
+#define BPID_ID 0x00000003 // tentative
+#define EPID_ID 0x00000004 // tentative
+#define ECL_ID 0x00000005 // tentative
+#define KLM_ID 0x00000006 // tentative
+
+
+namespace Belle2 {
+
+  /*! A class definition of an input module for Sequential ROOT I/O */
+
+  class Raw2DsModule : public Module {
+
+    // Public functions
+  public:
+
+    //! Constructor / Destructor
+    Raw2DsModule();
+    virtual ~Raw2DsModule();
+
+    //! Module functions to be called from main process
+    virtual void initialize();
+
+    //! Module functions to be called from event process
+    virtual void beginRun();
+    virtual void event();
+    virtual void endRun();
+    virtual void terminate();
+
+  private:
+    //! function to register data buffer in DataStore as RawCOPPERs
+    void registerRawCOPPERs();
+
+    // Data members
+  private:
+
+    //! RingBuffer ID
+    std::string m_rbufname;
+    RingBuffer* m_rbuf;
+
+    //! No. of sent events
+    int m_nevt;
+
+  };
+
+} // end namespace Belle2
+
+#endif // MODULEHELLO_H
---
+++ daq/rfarm/event/modules/src/Raw2DsModule.cc
@@ -0,0 +1,176 @@
+//+
+// File : Raw2DsModule.cc
+// Description : Module to restore DataStore from RingBuffer
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 13 - Aug - 2010
+//-
+
+#include <daq/rfarm/event/modules/Raw2DsModule.h>
+#include <TSystem.h>
+#include <stdlib.h>
+
+#include "framework/datastore/StoreObjPtr.h"
+#include "framework/dataobjects/EventMetaData.h"
+
+using namespace std;
+using namespace Belle2;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(Raw2Ds)
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+Raw2DsModule::Raw2DsModule() : Module()
+{
+  //Set module properties
+  setDescription("Encode DataStore into RingBuffer");
+  setPropertyFlags(c_Input);
+
+  addParam("RingBufferName", m_rbufname, "Name of RingBuffer",
+           string("InputRbuf"));
+
+  m_rbuf = NULL;
+  m_nevt = 0;
+
+  //Parameter definition
+  B2INFO("Rx: Constructor done.");
+}
+
+
+Raw2DsModule::~Raw2DsModule()
+{
+}
+
+void Raw2DsModule::initialize()
+{
+  gSystem->Load("libdataobjects");
+
+  m_rbuf = new RingBuffer(m_rbufname.c_str());
+
+  // Initialize EvtMetaData
+  StoreObjPtr<EventMetaData>::registerPersistent();
+
+  // Initialize Array of RawCOPPER
+  StoreArray<RawCOPPER>::registerPersistent();
+  StoreArray<RawSVD>::registerPersistent();
+  StoreArray<RawCDC>::registerPersistent();
+  StoreArray<RawBPID>::registerPersistent();
+  StoreArray<RawEPID>::registerPersistent();
+  StoreArray<RawECL>::registerPersistent();
+  StoreArray<RawKLM>::registerPersistent();
+
+  // Read the first event in RingBuffer and restore in DataStore.
+  // This is necessary to create object tables before TTree initialization
+  // if used together with SimpleOutput.
+  //  ---- Prefetch the first event
+  registerRawCOPPERs();
+
+  B2INFO("Rx initialized.");
+}
+
+
+void Raw2DsModule::beginRun()
+{
+  B2INFO("beginRun called.");
+}
+
+
+void Raw2DsModule::event()
+{
+  registerRawCOPPERs();
+}
+
+void Raw2DsModule::registerRawCOPPERs()
+{
+  // Get a record from ringbuf
+  int size;
+
+  int* evtbuf = new int[MAXEVTSIZE];
+  while ((size = m_rbuf->remq((int*)evtbuf)) == 0) {
+    usleep(100);
+  }
+
+  B2INFO("Raw2Ds: got an event from RingBuffer, size=" << size <<
+         " (proc= " << (int)getpid() << ")");
+
+  // Unpack SendHeader
+  SendHeader sndhdr;
+  sndhdr.SetBuffer(evtbuf);
+  int npackedevts = sndhdr.GetNumEventsinPacket();
+  if (npackedevts != 1) {
+    B2FATAL("Raw2DsModule::number of events in packet is not 1");
+  }
+  int ncprs = sndhdr.GetNumNodesinPacket();
+  int nwords = sndhdr.GetTotalNwords() - SendHeader::SENDHDR_NWORDS - SendTrailer::SENDTRL_NWORDS;
+
+  // Get buffer header
+  int* bufbody = evtbuf + SendHeader::SENDHDR_NWORDS;
+
+  // Unpack buffer
+  RawCOPPER tempcpr;
+  tempcpr.SetBuffer(bufbody, nwords, false, npackedevts, ncprs);
+
+  // Store data contents in Corresponding RawXXXX
+  for (int cprid = 0; cprid < ncprs; cprid++) {
+    // Pick up one COPPER and copy data in a temporary buffer
+    int nwds_buf = tempcpr.GetCprBlockNwords(cprid);
+    int* cprbuf = new int[nwds_buf];
+    memcpy(cprbuf, tempcpr.GetBuffer(cprid), nwds_buf * 4);
+    // Get subsys id
+    int subsysid = tempcpr.GetSubsysId(cprid);
+    // Switch to each detector and register RawXXX
+    if (subsysid == CDC_ID) {
+      StoreArray<RawCDC> ary;
+      (ary.appendNew())->SetBuffer(cprbuf, nwds_buf, 1, 1, 1);
+    } else if (subsysid == SVD_ID) {
+      StoreArray<RawSVD> ary;
+      (ary.appendNew())->SetBuffer(cprbuf, nwds_buf, 1, 1, 1);
+    } else if (subsysid == ECL_ID) {
+      StoreArray<RawECL> ary;
+      (ary.appendNew())->SetBuffer(cprbuf, nwds_buf, 1, 1, 1);
+    } else if (subsysid == BPID_ID) {
+      StoreArray<RawBPID> ary;
+      (ary.appendNew())->SetBuffer(cprbuf, nwds_buf, 1, 1, 1);
+    } else if (subsysid == EPID_ID) {
+      StoreArray<RawEPID> ary;
+      (ary.appendNew())->SetBuffer(cprbuf, nwds_buf, 1, 1, 1);
+    } else if (subsysid == KLM_ID) {
+      StoreArray<RawKLM> ary;
+      (ary.appendNew())->SetBuffer(cprbuf, nwds_buf, 1, 1, 1);
+    } else {
+      StoreArray<RawCOPPER> ary;
+      (ary.appendNew())->SetBuffer(cprbuf, nwds_buf, 1, 1, 1);
+    }
+    //    delete[] cprbuf;
+  }
+
+  StoreObjPtr<EventMetaData> evtmetadata;
+  evtmetadata.create();
+  evtmetadata->setExperiment(1);
+  evtmetadata->setRun(1);
+  evtmetadata->setEvent(m_nevt);
+  m_nevt++;
+  delete[] evtbuf;
+
+  B2INFO("Raw2Ds: DataStore Restored!!");
+  return;
+}
+
+void Raw2DsModule::endRun()
+{
+  //fill Run data
+
+  B2INFO("Raw2Ds: endRun done.");
+}
+
+
+void Raw2DsModule::terminate()
+{
+  B2INFO("Raw2Ds: terminate called")
+}
+

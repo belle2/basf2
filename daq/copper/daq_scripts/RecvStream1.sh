a493a955b7d2afeb52db0f90ca21d57238229f52
Satoru Yamada <satoru.yamada@kek.jp>
2013-09-30 09:35:18 +0000

    Add option to use shared memory
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@6891 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/copper/daq_scripts/RecvStream1.sh
+++ daq/copper/daq_scripts/RecvStream1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-basf2 ${BELLE2_LOCAL_DIR}/daq/rawdata/examples/RecvStream1.py --no-stat 
+basf2 ${BELLE2_LOCAL_DIR}/daq/rawdata/examples/RecvStream1.py $1 --no-stat 
--- daq/copper/daq_scripts/copper.sh
+++ daq/copper/daq_scripts/copper.sh
@@ -6,4 +6,4 @@ source  ~/.bash_profile
 #
 # Read data form COPPER FIFO
 #
-basf2 ${BELLE2_LOCAL_DIR}/daq/rawdata/examples/RecvSendCOPPER.py $1 $2 $3 --no-stat
+basf2 ${BELLE2_LOCAL_DIR}/daq/rawdata/examples/RecvSendCOPPER.py $1 $2 $3 $4 --no-stat

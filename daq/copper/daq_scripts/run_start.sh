497fa157c218bb33ea1a32b1e14af37afa239886
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2013-10-17 18:51:38 +0000

    Before commenting out Contine=False
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7089 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/copper/daq_scripts/run_start.sh
+++ daq/copper/daq_scripts/run_start.sh
@@ -2,7 +2,9 @@
 #
 # basf2 on COPPER CPU
 #
-/usr/bin/xterm -fn 7x14 -geometry 102x10+0+642 -e ${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr006 1 1 0 &
+/usr/bin/xterm -fn 7x14 -geometry 102x10+0+642 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr001 1 1 0; sleep 3000000;" &
+/usr/bin/xterm -fn 7x14 -geometry 102x10+0+642 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr002 2 1 0; sleep 3000000;" &
+#/usr/bin/xterm -fn 7x14 -geometry 102x10+0+642 -e ${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr006 1 3 0 &
 #/usr/bin/xterm -fn 7x14 -geometry 102x10+0+642 -e sh -c '${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr006 1 1; read line' &
 #/usr/bin/xterm -fn 7x14 -geometry 102x10+0+642 -e sh -c 'source  ~/.bash_profile; basf2 ${BELLE2_LOCAL_DIR}/daq/rawdata/examples/RecvSendCOPPER.py cpr006 1 1 --no-stat; read line' &
 
@@ -12,11 +14,11 @@
 #
 # event builder on ROPC
 #
-/usr/bin/xterm -fn 7x14 -geometry 102x10+0+342 -e ${BELLE2_LOCAL_DIR}/daq/eventbuilder/evb0/eb0 -n 1 cpr006 -D -b &
+#/usr/bin/xterm -fn 7x14 -geometry 102x10+0+342 -e ${BELLE2_LOCAL_DIR}/daq/eventbuilder/evb0/eb0 -n 1 cpr006 -D -b &
 #/usr/bin/xterm -fn 7x14 -geometry 102x10+0+342 -e ${BELLE2_LOCAL_DIR}/daq/eventbuilder/evb0/eb0 -n 2 cpr006 cpr015 -D -b &
 
 
 #
 # basf2 on ROPC
 #
-${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/RecvStream1.sh 0 35000
+${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/RecvStream1.sh 0 5101
--- daq/copper/daq_scripts/start_copper.sh
+++ daq/copper/daq_scripts/start_copper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ssh -n -n -XY $1 ${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/copper.sh $1 $2 $3 $4
+ssh -n -n -XY -l${USER} $1 ${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/copper.sh $1 $2 $3 $4
 

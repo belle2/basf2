e9f9ca66042d21729036efd8edd18247510b3f76
Giulia Casarosa <giulia.casarosa@pi.infn.it>
2013-10-19 10:41:05 +0000

    add module to send ROI payload from basf2 to ring-buffer
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7106 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/modules/pxdDataReduction/include/ROISenderModule.h
@@ -0,0 +1,79 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2011 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Giulia Casarosa, Eugenio Paoloni                         *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef ROI_SENDER_H_
+#define ROI_SENDER_H_
+
+#include <framework/core/Module.h>
+#include <tracking/pxdDataReductionClasses/ROIrawID.h>
+#include <string>
+
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <mqueue.h>
+
+namespace Belle2 {
+
+  /** The ROI to ONSEN Module
+   *
+   * this module is used to check the payload produced by the ROItoOnsen Module
+   *
+   */
+
+  class ROISenderModule : public Module {
+
+  public:
+
+    /**
+     * Constructor of the module.
+     */
+    ROISenderModule();
+
+    /**
+     * Destructor of the module.
+     */
+    virtual ~ROISenderModule();
+
+    /**
+     *Initializes the Module.
+     */
+    virtual void initialize();
+
+    virtual void beginRun();
+
+    virtual void event();
+
+    virtual void endRun();
+
+    /**
+     * Termination action.
+     */
+    virtual void terminate();
+
+  protected:
+
+
+    std::string  m_messageQueueName;
+    const char* m_messageQueueNameCstring;
+    std::string  m_ROIpayloadName;
+
+    int          m_messageQueueDepth;
+    int          m_messageQueueMsgSize;
+
+  private:
+    mqd_t        m_messageQueue;
+
+
+    void openMessageQueue(const char* log_string);
+    void closeMessageQueue(const char* log_string);
+    void unlinkMessageQueue(const char* log_string);
+  };
+}
+#endif
---
+++ tracking/modules/pxdDataReduction/src/ROISenderModule.cc
@@ -0,0 +1,170 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2011 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Giulia Casarosa, Eugenio Paoloni                         *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <framework/datastore/StoreObjPtr.h>
+#include <tracking/modules/pxdDataReduction/ROISenderModule.h>
+#include <tracking/dataobjects/ROIpayload.h>
+#include <sys/stat.h>
+
+using namespace std;
+using namespace Belle2;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+
+REG_MODULE(ROISender)
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+ROISenderModule::ROISenderModule() :
+  Module(), m_messageQueueNameCstring(NULL)
+{
+  //Set module properties
+  setDescription("Send the ROI payload to the external ring buffer");
+  setPropertyFlags(c_InitializeInProcess);
+
+  addParam("MessageQueueName", m_messageQueueName, "name of the output message queue", std::string("/roi"));
+  addParam("ROIpayloadName", m_ROIpayloadName, "name of the payload of ROIs", std::string(""));
+  addParam("MessageQueueDepth", m_messageQueueDepth, "depth of the output message queue", 10);
+  addParam("MessageSize", m_messageQueueMsgSize, "max size of themessage", 8192);
+
+  B2INFO("ROI Sender created");
+}
+
+
+ROISenderModule::~ROISenderModule()
+{
+
+}
+
+
+void
+ROISenderModule::initialize()
+{
+  StoreObjPtr<ROIpayload>::required(m_ROIpayloadName);
+
+  m_messageQueueNameCstring =  m_messageQueueName.c_str();
+
+  bool slashFree = (NULL == strchr(m_messageQueueNameCstring + 1 , '/'));
+
+  if (! slashFree || m_messageQueueNameCstring[0] != '/')
+    B2FATAL(__FILE__ << ":" << __LINE__ <<
+            m_messageQueueName << " invalid. cfr: man mq_overview ");
+
+
+  unlinkMessageQueue("on initialize");
+  openMessageQueue("on initialize");
+
+}
+
+void
+ROISenderModule::beginRun()
+{
+
+
+}
+
+
+void
+ROISenderModule::event()
+{
+
+  StoreObjPtr<ROIpayload> payloadPtr(m_ROIpayloadName);
+  int length = payloadPtr->getLength();
+  const char* data = (const char*) payloadPtr->getRootdata();
+
+  mqd_t ret;
+
+  if (length <= m_messageQueueMsgSize) {
+    ret = mq_send(m_messageQueue, data, length, 0 /* priority */);
+
+    if (ret == (mqd_t) - 1)
+      B2FATAL(std::string(__FILE__) << ":" << __LINE__ <<
+              ": error: " <<
+              strerror(errno) <<
+              " on mq_send");
+  } else
+    B2WARNING(std::string(__FILE__) << ":" << __LINE__  <<
+              " ROI payload too long." << endl <<
+              " Payload length     = " << length << endl <<
+              " Message max lengtt = " << m_messageQueueMsgSize << endl);
+
+}
+
+
+void
+ROISenderModule::endRun()
+{
+}
+
+
+void
+ROISenderModule::terminate()
+{
+  closeMessageQueue("on terminate");
+  unlinkMessageQueue("on terminate");
+}
+
+void
+ROISenderModule::openMessageQueue(const char* log_string)
+{
+  struct mq_attr attr;
+  attr.mq_flags   = 0;
+  attr.mq_maxmsg  = 10;
+  attr.mq_msgsize = 8192; // bytes
+
+  int oflags = O_WRONLY | O_CREAT | O_EXCL;
+  mode_t mode =  S_IRUSR | S_IWUSR | S_IROTH |  S_IRGRP ;
+
+
+  mqd_t ret = mq_open(m_messageQueueNameCstring, oflags, mode, &attr);
+
+
+  if (ret == (mqd_t) - 1)
+    B2FATAL(std::string(__FILE__) << ":" <<
+            __LINE__ << ": error: " <<
+            strerror(errno) <<
+            " on mq_open " << log_string);
+
+  m_messageQueue = ret;
+
+}
+
+void
+ROISenderModule::closeMessageQueue(const char* log_string)
+{
+  mqd_t ret;
+
+  ret = mq_close(m_messageQueue);
+  if (ret == (mqd_t) - 1)
+    B2WARNING(std::string(__FILE__) << ":" <<
+              __LINE__ << ": error: " <<
+              strerror(errno) <<
+              " on mq_close " << log_string);
+
+}
+
+
+void
+ROISenderModule::unlinkMessageQueue(const char* log_string)
+{
+  mqd_t ret;
+  ret = mq_unlink(m_messageQueueNameCstring);
+  if (ret == (mqd_t) - 1)
+    B2WARNING(std::string(__FILE__) << ":" <<
+              __LINE__ << ": error: " <<
+              strerror(errno) <<
+              " on mq_unlink " << log_string);
+
+}
+

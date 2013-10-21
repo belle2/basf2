fabdc123280ea3d06df4e48baa1645e927047e8b
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2013-10-21 07:22:44 +0000

    Raw data handling added.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7117 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/rfarm/event/include/RawRevRb2Sock.h
@@ -0,0 +1,46 @@
+//+
+// File : Rb2Sock.h
+// Description : Pick an event from RingBuffer and send it to EvtSocket
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 10 - Dec - 2002, Orignal for RFARM
+//        26 - Apr - 2012, for Belle II RFARM
+//-
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <sys/time.h>
+
+#include <vector>
+#include <string>
+
+#include "framework/pcore/RingBuffer.h"
+#include "daq/dataflow/REvtSocket.h"
+#include "daq/rfarm/manager/RFFlowStat.h"
+
+#define RBUFSIZE 100000000
+
+class RawRevRb2Sock {
+public:
+  /*! Constuctor and Destructor */
+  RawRevRb2Sock(std::string rbuf, int port, std::string shmname, int id);
+  ~RawRevRb2Sock(void);
+
+  /*! Event function */
+  int SendEvent(void);
+
+private:
+  RingBuffer* m_rbuf;
+  RSocketSend* m_sock;
+  RFFlowStat* m_flow;
+  int* m_evtbuf;
+
+};
+
+
---
+++ daq/rfarm/event/include/RawRevSock2Rb.h
@@ -0,0 +1,47 @@
+//+
+// File : Sock2Rb.h
+// Description : Receive an event from EvtSocket and place it in Rbuf.
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 10 - Dec - 2002, Orignal for RFARM
+//        26 - Apr - 2012, for Belle II RFARM
+//-
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <sys/time.h>
+
+#include <vector>
+#include <string>
+
+#include "framework/pcore/RingBuffer.h"
+#include "daq/dataflow/REvtSocket.h"
+#include "daq/rfarm/manager/RFFlowStat.h"
+
+#define MAXBUFSIZE 20000000
+#define RBUFSIZE 100000000
+
+class RawRevSock2Rb {
+public:
+  /*! Constuctor and Destructor */
+  RawRevSock2Rb(std::string rbuf, std::string src, int port, std::string shmname, int id);
+  ~RawRevSock2Rb(void);
+
+  /*! Event function */
+  int ReceiveEvent(void);
+
+private:
+  RingBuffer* m_rbuf;
+  RSocketRecv* m_sock;
+  RFFlowStat* m_flow;
+  char* m_evtbuf;
+  int* m_buf;
+
+};
+

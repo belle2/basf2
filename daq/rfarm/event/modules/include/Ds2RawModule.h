4b0a0b53e637cd7941153c66102e58d588db4945
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2013-10-21 07:25:32 +0000

    Raw data handling added.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7119 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/rfarm/event/modules/include/Ds2RawModule.h
@@ -0,0 +1,77 @@
+//+
+// File : Ds2RawModule.h
+// Description : Stream DataStore contents and place in in RingBuffer
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 26 - Apr - 2012
+//-
+
+#ifndef DS2RBUFMODULE_H
+#define DS2RBUFMODULE_H
+
+#include <framework/core/Module.h>
+#include <framework/pcore/EvtMessage.h>
+#include <framework/pcore/MsgHandler.h>
+#include <framework/pcore/RingBuffer.h>
+
+#include <framework/pcore/DataStoreStreamer.h>
+//#include <framework/datastore/DataStore.h>
+
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/dataobjects/EventMetaData.h>
+#include <daq/dataobjects/SendHeader.h>
+#include <daq/dataobjects/SendTrailer.h>
+
+
+#include <string>
+
+#define RBUFSIZE 100000000
+#define HLT_SUBSYS_ID 300
+
+namespace Belle2 {
+
+  /*! A class definition of an input module for Sequential ROOT I/O */
+
+  class Ds2RawModule : public Module {
+
+    // Public functions
+  public:
+
+    //! Constructor / Destructor
+    Ds2RawModule();
+    virtual ~Ds2RawModule();
+
+    //! Module functions to be called from main process
+    virtual void initialize();
+
+    //! Module functions to be called from event process
+    virtual void beginRun();
+    virtual void event();
+    virtual void endRun();
+    virtual void terminate();
+
+    // Data members
+  private:
+
+    //!Compression parameter
+    int m_compressionLevel;
+
+    //! RingBuffer ID
+    std::string m_rbufname;
+    RingBuffer* m_rbuf;
+
+    //! Messaage handler (obsolete)
+    MsgHandler* m_msghandler;
+
+    //! DataStore streamer
+    DataStoreStreamer* m_streamer;
+
+    //! No. of sent events
+    int m_nsent;
+
+  };
+
+} // end namespace Belle2
+
+#endif // MODULEHELLO_H
---
+++ daq/rfarm/event/modules/src/Ds2RawModule.cc
@@ -0,0 +1,152 @@
+//+
+// File : Ds2RawModule.cc
+// Description : Module to encode DataStore and place it in Ringbuffer
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 13 - Aug - 2010
+//-
+
+#include <daq/rfarm/event/modules/Ds2RawModule.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+using namespace std;
+using namespace Belle2;
+
+//!! for debugging only
+// static int fdout;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(Ds2Raw)
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+Ds2RawModule::Ds2RawModule() : Module()
+{
+  //Set module properties
+  setDescription("Encode DataStore into RingBuffer");
+  setPropertyFlags(c_InitializeInProcess);
+
+  addParam("RingBufferName", m_rbufname, "Name of RingBuffer",
+           string("OutputRbuf"));
+  addParam("CompressionLevel", m_compressionLevel, "Compression level",
+           0);
+
+  m_rbuf = NULL;
+  m_nsent = 0;
+  m_compressionLevel = 0;
+
+  //Parameter definition
+  B2INFO("Ds2Raw: Constructor done.");
+}
+
+
+Ds2RawModule::~Ds2RawModule()
+{
+}
+
+void Ds2RawModule::initialize()
+{
+
+  //  m_rbuf = new RingBuffer(m_rbufname.c_str(), RBUFSIZE);
+  m_rbuf = new RingBuffer(m_rbufname.c_str());
+  //  m_msghandler = new MsgHandler(m_compressionLevel);
+  m_streamer = new DataStoreStreamer(m_compressionLevel);
+
+  //!! For debugging only
+  //!!  fdout = open ( "Ds2RawDump3.dat", O_CREAT|O_RDWR|O_TRUNC, 0666 );
+
+  B2INFO("Ds2Raw initialized.");
+}
+
+
+void Ds2RawModule::beginRun()
+{
+  B2INFO("Ds2Raw: beginRun called.");
+}
+
+
+void Ds2RawModule::event()
+{
+  // Stream DataStore in EvtMessage
+  EvtMessage* msg = m_streamer->streamDataStore(DataStore::c_Event);
+
+  // Event Meta Data
+  StoreObjPtr<EventMetaData> evtmeta;
+
+  // Fill Header and Trailer
+  SendHeader hdr;
+  SendTrailer trl;
+
+  // Number of total words
+  int msgsize = (msg->size() - 1) / 4 + 1;
+  int total_nwrds = msgsize +  hdr.GetHdrNwords() + trl.GetTrlNwords();
+
+  // Fill header and trailer
+  hdr.SetNwords(total_nwrds);
+  hdr.SetNumEventsinPacket(1);
+  hdr.SetNumNodesinPacket(1);
+  hdr.SetEventNumber(evtmeta->getEvent());
+  hdr.SetNodeID(HLT_SUBSYS_ID);
+  //  trl.SetMagicWord();
+
+  // Allocate raw buffer
+  int* buffer = new int[total_nwrds];
+
+  // Fill header
+  memcpy(buffer, hdr.GetBuffer(), hdr.GetHdrNwords()*sizeof(int));
+
+  // Fill EvtMessage
+  memcpy(buffer + hdr.GetHdrNwords(), msg->buffer(), msg->size());
+
+  // Fill trailer
+  memcpy(buffer + hdr.GetHdrNwords() + msgsize, trl.GetBuffer(),
+         trl.GetTrlNwords()*sizeof(int));
+  //  printf ( "trailer = %8.8x, %8.8x\n", *(buffer+hdr.GetHdrNwords()+msgsize),
+  //       *(buffer+hdr.GetHdrNwords()+msgsize+1) );
+
+  // Put the raw buffer in ring buffer
+  for (;;) {
+    int stat = m_rbuf->insq(buffer, total_nwrds);
+    if (stat >= 0) break;
+    //    usleep(200);
+    usleep(20);
+  }
+
+  //!! For debugging only
+  //!! int is = write ( fdout, buffer, total_nwrds*4 );
+
+  B2INFO("Ds2Raw: objs sent in buffer. Size = " << msg->size());
+
+  // Release EvtMessage buffer
+  delete[] buffer;
+
+  // return
+  m_nsent++;
+
+}
+
+void Ds2RawModule::endRun()
+{
+  //fill Run data
+
+  printf("Ds2Raw: endRun called.....\n");
+  B2INFO("Ds2Raw: endRun done.");
+}
+
+
+void Ds2RawModule::terminate()
+{
+  delete m_streamer;
+
+  // RingBuffer should not be deleted
+
+  B2INFO("Ds2Raw: terminate called")
+}
+

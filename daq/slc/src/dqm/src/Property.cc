a28f34e9bcd2aaeb8034b87244cae844506a0d7d
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2013-10-31 17:48:21 +0000

    Tempprally remove
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7245 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/src/dqm/src/Property.cc
+++
@@ -1,9 +0,0 @@
-#include "Property.hh"
-
-using namespace B2DQM;
-
-Property::Property(const MonColor& color) throw()
-  : _color(color), _label(""), _use_color(true) {}
-
-Property::Property() throw()
-  : _color(), _label(""), _use_color(false) {}

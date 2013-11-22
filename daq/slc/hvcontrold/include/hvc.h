58fab0eeec3c8613097f3d3d15c3e9db32ef9cab
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2013-11-22 01:52:20 +0000

    Fixed bugs
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7434 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/hvcontrold/include/hvc.h
+++
@@ -1,9 +0,0 @@
-#ifndef HVC_H
-#define HVC_H
-
-static const char HVC_HVOFF    = 0;
-static const char HVC_STANDBY1 = 1;
-static const char HVC_STANDBY2 = 2;
-static const char HVC_PEAK     = 3;
-
-#endif

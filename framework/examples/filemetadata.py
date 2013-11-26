c8937ab952e7541184ef1ec94569ce69e616ecdb
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2013-11-26 18:17:44 +0000

    hint to showmetadata tool added
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7493 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/examples/filemetadata.py
+++ framework/examples/filemetadata.py
@@ -3,12 +3,14 @@
 
 # example script to print FileMetaData for a given file
 # Usage: basf2 framework/examples/filemetadata.py -i inputfile.root
+# (Hint: for inspecting file metadata it is recommended to use the showmetadata tool)
 
 from basf2 import *
 from ROOT import Belle2
 
 
 class PrintFileMetaData(Module):
+
     """Print FileMetaData object."""
 
     def initialize(self):

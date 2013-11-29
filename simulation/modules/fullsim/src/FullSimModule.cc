d3ed4f8a0716328d0d03ebb4beace1ba567e708e
Takanori Hara <takanori.hara@kek.jp>
2013-11-29 04:00:08 +0000

    Default physics list is changed from QGSP_BERT to FTFP_BERT
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7581 b38c6c8f-bee8-465d-8448-9abe6915d496

--- simulation/modules/fullsim/src/FullSimModule.cc
+++ simulation/modules/fullsim/src/FullSimModule.cc
@@ -71,7 +71,7 @@ FullSimModule::FullSimModule() : Module(), m_visManager(NULL)
   addParam("ThresholdTrials", m_thresholdTrials, "Geant4 will try 'ThresholdTrials' times to move a particle which got 'stuck' and has an energy less than 'ThresholdImportantEnergy'.", 10);
   addParam("TrackingVerbosity", m_trackingVerbosity, "Tracking verbosity: 0=Silent; 1=Min info per step; 2=sec particles; 3=pre/post step info; 4=like 3 but more info; 5=proposed step length info.", 0);
   addParam("HadronProcessVerbosity", m_hadronprocessVerbosity, "Hadron Process verbosity: 0=Silent; 1=info level; 2=debug level", 0);
-  addParam("PhysicsList", m_physicsList, "The name of the physics list which is used for the simulation.", string("QGSP_BERT"));
+  addParam("PhysicsList", m_physicsList, "The name of the physics list which is used for the simulation.", string("FTFP_BERT"));
   addParam("RegisterOptics", m_optics, "If true, G4OpticalPhysics is registered in Geant4 PhysicsList.", true);
   addParam("ProductionCut", m_productionCut, "[cm] Apply continuous energy loss to primary particle which has no longer enough energy to produce secondaries which travel at least the specified productionCut distance.", 0.07);
   addParam("MaxNumberSteps", m_maxNumberSteps, "The maximum number of steps before the track transportation is stopped and the track is killed.", 100000);

b6e4dde6b13b7b1e8de4e5ceacdb329f4c49aaa4
Satoru Yamada <satoru.yamada@kek.jp>
2013-12-09 06:51:39 +0000

    move rawdata handling classes to rawdata/ directory
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7791 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/dataobjects/include/linkdef.h
+++ daq/dataobjects/include/linkdef.h
@@ -5,19 +5,7 @@
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
-#pragma link C++ class Belle2::RawDataBlock;
-#pragma link C++ class Belle2::RawCOPPER;
-#pragma link C++ class Belle2::RawHeader;
-#pragma link C++ class Belle2::RawTrailer;
-#pragma link C++ class Belle2::RawCDC;
-#pragma link C++ class Belle2::RawSVD;
-#pragma link C++ class Belle2::RawECL;
-#pragma link C++ class Belle2::RawBPID;
-#pragma link C++ class Belle2::RawEPID;
-#pragma link C++ class Belle2::RawKLM;
-#pragma link C++ class Belle2::RawFTSW;
 #pragma link C++ class Belle2::SendHeader;
 #pragma link C++ class Belle2::SendTrailer;
-#pragma link C++ class Belle2::ErrorMessage;
 
 #endif

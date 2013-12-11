70ad7a82322d5665fd85322c2656012e05dbed2c
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2013-12-11 04:38:27 +0000

    Output module to write data at recording node.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7856 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/storage/modules/include/StorageOutput.h
@@ -0,0 +1,110 @@
+//+
+// File : storageoutput.h
+// Description : Sequential ROOT I/O output module for DAQ
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 13 - Aug - 2010
+//        9  - Dec - 2013 ; update for DAQ
+//-
+
+#ifndef STORAGE_OUTPUT_H
+#define STORAGE_OUTPUT_H
+
+#include <framework/core/Module.h>
+#include <framework/pcore/EvtMessage.h>
+#include <framework/pcore/MsgHandler.h>
+#include <framework/pcore/SeqFile.h>
+#include <framework/pcore/DataStoreStreamer.h>
+#include <framework/pcore/RingBuffer.h>
+
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/dataobjects/EventMetaData.h>
+
+#include <string>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <cmath>
+#include <fcntl.h>
+
+
+namespace Belle2 {
+  /**
+   * \addtogroup modules
+   * @{
+   * \addtogroup framework_modules
+   * \ingroup modules
+   * @{ SeqRootOutputModule @} @}
+   */
+
+  /*! Class definition for the output module of Sequential ROOT I/O */
+
+  class StorageOutputModule : public Module {
+
+    // Public functions
+  public:
+
+    //! Constructor / Destructor
+    StorageOutputModule();
+    virtual ~StorageOutputModule();
+
+    //! Module functions to be called from main process
+    virtual void initialize();
+
+    //! Module functions to be called from event process
+    virtual void beginRun();
+    virtual void event();
+    virtual void endRun();
+    virtual void terminate();
+
+    //! open data file by looking at exp/run number
+    SeqFile* openDataFile();
+    //! Function to close file from outside
+    //    void closeDataFile();
+
+
+    // Data members
+  private:
+    //! Exp number, Run number
+    int m_exp;
+    int m_run;
+
+    //! Compression level
+    int m_compressionLevel;
+
+    //! Storage directory
+    std::string m_stordir;
+
+    //! Blocked file handler
+    SeqFile* m_file;
+
+    //! Total nr. of events in the file
+    int m_nevt;
+
+    //! Messaage handler
+    MsgHandler* m_msghandler;
+
+    //! DataStoreStreamer
+    DataStoreStreamer* m_streamer;
+
+    //! Time
+    struct timeval m_t0; /**< time at begin of current run. */
+    struct timeval m_tend; /**< time at end of current run. */
+
+    //! Data flow
+    double m_size; /**< total transferred data, in kB. */
+    double m_size2; /**< sum of squares of data transferred in each event, in kB^2. */
+
+    //! Ring Buffer to dump sampled output stream
+    std::string m_rbufname;
+    RingBuffer* m_rbufout;
+
+    //! Event interval to dump output in RingBuffer
+    int m_interval;
+
+  };
+} // end namespace Belle2
+
+#endif
---
+++ daq/storage/modules/src/StorageOutput.cc
@@ -0,0 +1,162 @@
+//+
+// File : storageoutput.cc
+// Description : Sequential ROOT output module for pbasf2
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 13 - Aug - 2010
+//         6 - Sep - 2012,  Use of DataStoreStreamer, clean up
+//         9 - Dec - 2013, Modification for DAQ use
+//-
+
+#include <daq/storage/modules/StorageOutput.h>
+
+using namespace std;
+using namespace Belle2;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(StorageOutput)
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+StorageOutputModule::StorageOutputModule() : Module()
+{
+  //Set module properties
+  setDescription("SeqROOT output module");
+  //  setPropertyFlags(c_Output | c_ParallelProcessingCertified);
+  m_file = 0;
+  m_msghandler = 0;
+  m_streamer = 0;
+
+  //Parameter definition
+  addParam("compressionLevel", m_compressionLevel, "Compression Level: 0 for no, 1 for low, 9 for high compression. Level 1 usually reduces size by 50%, higher levels have no noticable effect. NOTE: Because of a ROOT bug, enabling this currently causes memory leaks..", 0);
+  addParam("RingBufferName", m_rbufname, "Name of Ring Buffer to dump streamed events",
+           string("DumpRbuf"));
+  addParam("DumpInterval", m_interval, "Event interval to dump event in RingBuffer",
+           100);
+  addParam("StorageDir", m_stordir, "Directory to write output files", string(""));
+
+  B2DEBUG(1, "StorageOutput: Constructor done.");
+}
+
+
+StorageOutputModule::~StorageOutputModule() { }
+
+void StorageOutputModule::initialize()
+{
+  // Open output file
+  /*
+  m_file = new SeqFile(m_outputFileName.c_str(), "w");
+  if ( m_file == 0 )
+    B2FATAL ( "Error to open output file" );
+  */
+
+  // Message handler to encode serialized object
+  m_msghandler = new MsgHandler(m_compressionLevel);
+
+  // DataStoreStreamer
+  m_streamer = new DataStoreStreamer(m_compressionLevel);
+
+  // Ring Buffer
+  if (strcmp(m_rbufname.c_str(), "") == 0) {
+    m_rbufout = new RingBuffer(m_rbufname.c_str());
+  } else
+    m_rbufout = NULL;
+
+  B2INFO("StorageOutput: initialized.");
+
+}
+
+
+void StorageOutputModule::beginRun()
+{
+  // Statistics
+  gettimeofday(&m_t0, 0);
+  m_size = 0.0;
+  m_size2 = 0.0;
+  m_nevt = 0;
+
+  // Open data file by looking at exp/run number
+  if (m_file != NULL) delete m_file;
+  m_file = openDataFile();
+
+  B2INFO("StorageOutput: beginRun called.");
+}
+
+void StorageOutputModule::event()
+{
+  // Stream DataStore in EvtMessage
+  EvtMessage* msg = m_streamer->streamDataStore(DataStore::c_Event);
+
+  // Store EvtMessage
+  int stat = m_file->write(msg->buffer());
+  //  printf("StorageOuput : write = %d\n", stat);
+
+  // Queue EvtMessage in RingBuffer
+  if (m_rbufout != NULL) {
+    if (m_nevt % m_interval == 0) {
+      // Free running RingBuffer, no flow control
+      m_rbufout->insq((int*)msg->buffer(), (msg->size() - 1) / 4 + 1);
+    }
+  }
+  // Clean up EvtMessage
+  delete msg;
+
+  // Statistics
+  double dsize = (double)stat / 1000.0;
+  m_size += dsize;
+  m_size2 += dsize * dsize;
+  m_nevt++;
+}
+
+void StorageOutputModule::endRun()
+{
+  //fill Run data
+
+  // End time
+  gettimeofday(&m_tend, 0);
+  double etime = (double)((m_tend.tv_sec - m_t0.tv_sec) * 1000000 +
+                          (m_tend.tv_usec - m_t0.tv_usec));
+
+  // Statistics
+  // Sigma^2 = Sum(X^2)/n - (Sum(X)/n)^2
+
+  double flowmb = m_size / etime * 1000.0;
+  double avesize = m_size / (double)m_nevt;
+  double avesize2 = m_size2 / (double)m_nevt;
+  double sigma2 = avesize2 - avesize * avesize;
+  double sigma = sqrt(sigma2);
+
+  //  printf ( "m_size = %f, m_size2 = %f, m_nevt = %d\n", m_size, m_size2, m_nevt );
+  //  printf ( "avesize2 = %f, avesize = %f, avesize*avesize = %f\n", avesize2, avesize, avesize*avesize );
+  B2INFO("StorageOutput :  " << m_nevt << " events written with total bytes of " << m_size << " kB");
+  B2INFO("StorageOutput : flow rate = " << flowmb << " (MB/s)");
+  B2INFO("StorageOutput : event size = " << avesize << " +- " << sigma << " (kB)");
+
+  B2INFO("StorageOutput: endRun done.");
+}
+
+
+void StorageOutputModule::terminate()
+{
+  delete m_msghandler;
+  delete m_streamer;
+  delete m_file;
+
+  B2INFO("terminate called")
+}
+
+SeqFile* StorageOutputModule::openDataFile()
+{
+  StoreObjPtr<EventMetaData> evtmetadata;
+  int expno = evtmetadata->getExperiment();
+  int runno = evtmetadata->getRun();
+  char outfile[1024];
+  sprintf(outfile, "%se%4.4dr%6.6d.sroot", m_stordir.c_str(), expno, runno);
+  SeqFile* seqfile = new SeqFile(outfile, "w");
+  printf("StorageOutput : data file %s initialized\n", outfile);
+  return seqfile;
+}

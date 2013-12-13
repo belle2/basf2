e16c08a1fad40d65b18538cdf88671fda6e7c7e0
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2013-12-13 15:00:00 +0000

    Add VariablesToNtuple module
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7957 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ analysis/modules/VariablesToNtuple/include/VariablesToNtupleModule.h
@@ -0,0 +1,58 @@
+#pragma once
+/**************************************************************************
+* BASF2 (Belle Analysis Framework 2)                                     *
+* Copyright(C) 2013 - Belle II Collaboration                             *
+*                                                                        *
+* Author: The Belle II Collaboration                                     *
+* Contributors: Christian Pulvermacher                                   *
+*                                                                        *
+* This software is provided "as is" without any warranty.                *
+**************************************************************************/
+
+#include <framework/core/Module.h>
+#include <analysis/utility/VariableManager.h>
+
+#include <TNtuple.h>
+#include <TFile.h>
+
+#include <string>
+
+namespace Belle2 {
+
+  /** Module to calculate variables specified by the user for a given ParticleList
+   *  and save them into an Ntuple.
+   */
+  class VariablesToNtupleModule : public Module {
+  public:
+    /** Constructor. */
+    VariablesToNtupleModule();
+    /** Destructor. */
+    ~VariablesToNtupleModule() {}
+    /** Initialises the module.
+     *
+     * Prepares variables and sets up ntuple columns.
+     */
+    void initialize();
+    /** Method called for each event. */
+    void event();
+    /** Write TTree to file, and close file if necessary. */
+    void terminate();
+  private:
+    /** Name of particle list with reconstructed particles. */
+    std::string m_particleList;
+    /** List of variables to save. Variables are taken from VariableManager, and are identical to those available to e.g. ParticleSelector. */
+    std::vector<std::string> m_variables;
+    /** Name of ROOT file for output. */
+    std::string m_fileName;
+    /** Name of the TTree. */
+    std::string m_treeName;
+
+    /** ROOT file for output. */
+    TFile* m_file;
+    /** The ROOT TNtuple for output. */
+    TNtuple* m_tree;
+    /** List of function pointers corresponding to given variables. */
+    std::vector<VariableManager::FunctionPtr> m_functions;
+
+  };
+} // end namespace Belle2
---
+++ analysis/modules/VariablesToNtuple/src/VariablesToNtuple.cc
@@ -0,0 +1,108 @@
+/**************************************************************************
+* BASF2 (Belle Analysis Framework 2)                                     *
+* Copyright(C) 2013 - Belle II Collaboration                             *
+*                                                                        *
+* Author: The Belle II Collaboration                                     *
+* Contributors: Christian Pulvermacher                                   *
+*                                                                        *
+* This software is provided "as is" without any warranty.                *
+**************************************************************************/
+
+#include <analysis/modules/VariablesToNtuple/VariablesToNtupleModule.h>
+
+#include <analysis/dataobjects/ParticleList.h>
+#include <analysis/utility/VariableManager.h>
+#include <framework/logging/Logger.h>
+#include <framework/datastore/StoreObjPtr.h>
+
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+using namespace Belle2;
+
+// Register module in the framework
+REG_MODULE(VariablesToNtuple)
+
+
+VariablesToNtupleModule::VariablesToNtupleModule() : Module()
+{
+  //Set module properties
+  setDescription("Calculate variables specified by the user for a given ParticleList and save them into a TNtuple.");
+
+  vector<string> emptylist;
+  addParam("particleList", m_particleList, "Name of particle list with reconstructed particles.");
+  addParam("variables", m_variables, "List of variables to save. Variables are taken from VariableManager, and are identical to those available to e.g. ParticleSelector.", emptylist);
+
+  addParam("fileName", m_fileName, "Name of ROOT file for output.", string("VariablesToNtuple.root"));
+  addParam("treeName", m_treeName, "Name of the NTuple in the saved file.", string("ntuple"));
+}
+
+void VariablesToNtupleModule::initialize()
+{
+  StoreObjPtr<ParticleList>::required(m_particleList);
+
+  // Initializing the output root file
+  m_file = new TFile(m_fileName.c_str(), "RECREATE");
+  if (!m_file->IsOpen()) {
+    B2WARNING("Could not create file " << m_fileName);
+    return;
+  }
+
+  m_file->cd();
+
+  // check if TTree with that name already exists
+  if (m_file->Get(m_treeName.c_str())) {
+    B2WARNING("Tree with this name already exists: " << m_fileName);
+    return;
+  }
+
+  // root wants var1:var2:...
+  string varlist;
+  bool first = true;
+  for (const string & varStr : m_variables) {
+    if (!first)
+      varlist += ":";
+    varlist += varStr;
+    first = false;
+
+    //also collection function pointers
+    const VariableManager::Var* var = VariableManager::Instance().getVariable(varStr);
+    if (!var) {
+      B2ERROR("Variable '" << varStr << "' is not available in VariableManager!");
+    } else {
+      m_functions.push_back(var->function);
+    }
+  }
+
+  m_tree = new TNtuple(m_treeName.c_str(), "", varlist.c_str());
+}
+
+void VariablesToNtupleModule::event()
+{
+  StoreObjPtr<ParticleList> particlelist(m_particleList);
+  unsigned int nVars = m_variables.size();
+  std::vector<float> vars(nVars);
+
+  unsigned int nPart = particlelist->getListSize();
+  for (unsigned int iPart = 0; iPart < nPart; iPart++) {
+    const Particle* particle = particlelist->getParticle(iPart);
+
+    for (unsigned int iVar = 0; iVar < nVars; iVar++) {
+      vars[iVar] = m_functions[iVar](particle);
+    }
+
+    m_tree->Fill(vars.data());
+  }
+}
+
+void VariablesToNtupleModule::terminate()
+{
+  B2INFO("Writing NTuple " << m_treeName);
+  m_file->cd();
+  m_tree->Write();
+
+  B2INFO("Closing file " << m_fileName);
+  delete m_file;
+  m_tree = NULL;
+}

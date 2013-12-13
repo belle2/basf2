a94bf746412e55f9591e8fb31e2eeb5b15bb9ce2
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2013-12-13 05:20:16 +0000

    Old master renamed to rf_master_local
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7928 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/rfarm/manager/tools/rf_master_local.cc
@@ -0,0 +1,28 @@
+//+
+// File : rf_master.cc
+// Description : Master node of RFARM
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 31 - Jul - 2013
+//-
+#include "daq/rfarm/manager/RFMaster.h"
+#include "daq/rfarm/manager/RFNSM.h"
+
+using namespace std;
+using namespace Belle2;
+
+int main(int argc, char** argv)
+{
+  RFConf conf(argv[1]);
+
+  RFMaster* master = new RFMaster(argv[1]);
+  // Creation of event server instance. evs contains the instance
+  //  RFOutputServer& ots = RFOutputServer::Create ( argv[1] );
+
+  RFNSM nsm(conf.getconf("master", "nodename"), master);
+  nsm.AllocMem(conf.getconf("system", "nsmdata"));
+  master->SetNodeInfo(nsm.GetNodeInfo());
+
+  master->monitor_loop();
+
+}

03d84f3ec0c09da866e3d685e541bb28a9945d02
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2013-12-13 14:25:10 +0000

    Slow control for express reco
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@7956 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/apps/templated/tools/exprecod.cc
+++ daq/slc/apps/templated/tools/exprecod.cc
@@ -1,4 +1,4 @@
-#include "daq/slc/apps/templated/TemplateCallback.h"
+#include "daq/slc/apps/exprecod/ExpRecoCallback.h"
 
 #include <daq/slc/nsm/NSMNodeDaemon.h>
 
@@ -9,13 +9,13 @@ using namespace Belle2;
 int main(int argc, char** argv)
 {
   if (argc < 2) {
-    Belle2::debug("Usage : ./recvd <name>");
+    Belle2::debug("Usage : ./exprecod <name>");
     return 1;
   }
 
   const char* name = argv[1];
   NSMNode* node = new NSMNode(name);
-  TemplateCallback* callback = new TemplateCallback(node);
+  ExpRecoCallback* callback = new ExpRecoCallback(node);
   NSMNodeDaemon* daemon = new NSMNodeDaemon(callback);
   daemon->run();
 

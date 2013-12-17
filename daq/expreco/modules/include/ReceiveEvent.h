5589f54c0750c841445296a9bf45370f2054992e
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2013-12-17 08:50:19 +0000

    Added module to receive event from express reco.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@8022 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/expreco/modules/include/ReceiveEvent.h
@@ -0,0 +1,70 @@
+//+
+// File : ReceiveEvent.h
+// Description : Module to restore DataStore in RingBuffer
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 17 - Dec - 2013
+//-
+
+#ifndef RECEIVE_EVENT_MODULE_H
+#define RECEIVE_EVENT_MODULE_H
+
+#include <framework/core/Module.h>
+#include <framework/pcore/EvtMessage.h>
+#include <framework/pcore/MsgHandler.h>
+#include <daq/dataflow/EvtSocket.h>
+
+#include <boost/shared_ptr.hpp>
+#include <string>
+#include <vector>
+
+#include <framework/pcore/DataStoreStreamer.h>
+
+#include <TSystem.h>
+
+namespace Belle2 {
+
+  /*! A class definition of an input module for Sequential ROOT I/O */
+
+  class ReceiveEventModule : public Module {
+
+    // Public functions
+  public:
+
+    //! Constructor / Destructor
+    ReceiveEventModule();
+    virtual ~ReceiveEventModule();
+
+    //! Module functions to be called from main process
+    virtual void initialize();
+
+    //! Module functions to be called from event process
+    virtual void beginRun();
+    virtual void event();
+    virtual void endRun();
+    virtual void terminate();
+
+    // Data members
+  private:
+
+    //! Receiver Port
+    std::string m_host;
+    int m_port;
+
+    //! Reciever Socket
+    EvtSocketSend* m_recv;
+
+    //! DataStoreStreamer
+    DataStoreStreamer* m_streamer;
+
+    //! Compression Level
+    int m_compressionLevel;
+
+    //! No. of sent events
+    int m_nrecv;
+
+  };
+
+} // end namespace Belle2
+
+#endif // MODULEHELLO_H
---
+++ daq/expreco/modules/src/ReceiveEvent.cc
@@ -0,0 +1,121 @@
+//+
+// File : ReceiveEvent.cc
+// Description : Module to receive event from expreco
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 17 - Dec - 2013
+//-
+
+#include <daq/expreco/modules/ReceiveEvent.h>
+#include <stdlib.h>
+
+//#define MAXEVTSIZE 400000000
+
+using namespace std;
+using namespace Belle2;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(ReceiveEvent)
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+ReceiveEventModule::ReceiveEventModule() : Module()
+{
+  //Set module properties
+  setDescription("Receive Event from ExpReco");
+  //  setPropertyFlags(c_Input | c_ParallelProcessingCertified);
+
+  addParam("Host", m_host, "Receiver host", string("expreco"));
+  addParam("Port", m_port, "Receiver Port", 7111);
+  m_nrecv = 0;
+  m_compressionLevel = 0;
+
+  //Parameter definition
+  B2INFO("Rx: Constructor done.");
+}
+
+
+ReceiveEventModule::~ReceiveEventModule()
+{
+}
+
+void ReceiveEventModule::initialize()
+{
+  // Load data objects definitions
+  gSystem->Load("libdataobjects");
+
+  // Open receiving socekt
+  m_recv = new EvtSocketSend(m_host.c_str(), m_port);
+
+  // Initialize DataStoreStreamer
+  m_streamer = new DataStoreStreamer(m_compressionLevel);
+
+  // Prefetch first record in ReceiveEvent
+  EvtMessage* msg = m_recv->recv();
+  if (msg == NULL) {
+    return;
+  }
+  m_streamer->restoreDataStore(msg);
+
+  // Delete buffers
+  delete msg;
+
+  m_nrecv = -1;
+
+  B2INFO("Rx initialized.");
+}
+
+
+void ReceiveEventModule::beginRun()
+{
+  B2INFO("beginRun called.");
+}
+
+
+void ReceiveEventModule::event()
+{
+  m_nrecv++;
+  // First event is already loaded
+  if (m_nrecv == 0) return;
+
+  // Get a record from socket
+  EvtMessage* msg = m_recv->recv();
+  if (msg == NULL) {
+    return;
+  }
+  B2INFO("Rx: got an event from Socket, size=" << msg->size());
+  // Check for termination record
+  if (msg->type() == MSG_TERMINATE) {
+    B2INFO("Rx: got termination message. Exitting....");
+    return;
+    // Flag End Of File !!!!!
+    //    return msg->type(); // EOF
+  }
+
+  // Restore DataStore
+  m_streamer->restoreDataStore(msg);
+  B2INFO("Rx: DataStore Restored!!");
+
+  // Delete EvtMessage
+  delete msg;
+
+  return;
+}
+
+void ReceiveEventModule::endRun()
+{
+  //fill Run data
+
+  B2INFO("endRun done.");
+}
+
+
+void ReceiveEventModule::terminate()
+{
+  B2INFO("terminate called")
+}
+

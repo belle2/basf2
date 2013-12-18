4b1d7b870a365d068c4214b05a3febbfa1178ee7
Makoto Uchida <uchida@phys.titech.ac.jp>
2013-12-18 05:56:03 +0000

    CDC geom. typo fixed.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@8067 b38c6c8f-bee8-465d-8448-9abe6915d496

--- cdc/geometry/include/CDCGeometryPar.h
+++ cdc/geometry/include/CDCGeometryPar.h
@@ -565,7 +565,7 @@ namespace Belle2 {
 
     inline double CDCGeometryPar::momZ(int iBound) const
     {
-      returm m_momZ[iBound];
+      return m_momZ[iBound];
     }
 
     inline double CDCGeometryPar::momRmin(int iBound) const

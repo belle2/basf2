39ae85a48d1615798736e70801876f4956f8c1fe
Martin Ritter <martin.ritter@lmu.de>
2013-12-19 13:50:33 +0000

    Extend UserInfo to allow pointer to MCPaticleTrajectory.
    This allows the SteppingAction to store the step positions and momenta
    in the trajectory if defined
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@8116 b38c6c8f-bee8-465d-8448-9abe6915d496

--- simulation/kernel/include/UserInfo.h
+++ simulation/kernel/include/UserInfo.h
@@ -13,6 +13,7 @@
 
 #include <framework/core/FrameworkExceptions.h>
 #include <generators/dataobjects/MCParticleGraph.h>
+#include <simulation/dataobjects/MCParticleTrajectory.h>
 
 #include "G4VUserTrackInformation.hh"
 #include "G4VUserPrimaryParticleInformation.hh"
@@ -38,7 +39,7 @@ namespace Belle2 {
        * The constructor of the user info class.
        * @param data The data which should be payloaded to the class.
        */
-      UserInfo(Payload data) : Info(), m_data(data), m_status(0), m_fraction(1.0) {}
+      UserInfo(Payload data) : Info(), m_data(data), m_status(0), m_fraction(1.0), m_trajectory(nullptr) {}
 
       /**
        * Returns the payloaded data.
@@ -64,6 +65,9 @@ namespace Belle2 {
        */
       double getFraction() {return m_fraction;}
 
+      /** Get optional particle trajectory */
+      MCParticleTrajectory* getTrajectory() { return m_trajectory; }
+
       /**
        * Set status of optical photon (used for performance speed-ups)
        * @param status status
@@ -75,6 +79,9 @@ namespace Belle2 {
        * @param fraction fraction
        */
       void setFraction(double fraction) { m_fraction = fraction;}
+
+      /** Set the optional particle trajectory */
+      void setTrajectory(MCParticleTrajectory* trajectory) { m_trajectory = trajectory; }
 
       /**
        * Prints information, not implemented in this class.
@@ -100,6 +107,7 @@ namespace Belle2 {
 
       int m_status;      /**< optical photon: status */
       double m_fraction; /**< optical photon: propagation fraction */
+      MCParticleTrajectory* m_trajectory; /**< Particle trajectory, if any */
 
     };
 

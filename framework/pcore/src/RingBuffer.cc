f058fe77ec750add8a1ef65a88a2590b52855c84
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2013-12-29 20:41:55 +0000

    Updated to leave temp file also for named RingBuffer
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@8264 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/pcore/src/RingBuffer.cc
+++ framework/pcore/src/RingBuffer.cc
@@ -102,6 +102,18 @@ RingBuffer::RingBuffer(const char* name, unsigned int size)
     close(m_pathfd);
   }
 
+  // Leave id of shm and semaphore in file name
+  if (m_new) {
+    m_strbuf = new char[1024];
+    sprintf(m_strbuf, "/tmp/SHM%d-SEM%d", m_shmid, m_semid);
+    int fd = open(m_strbuf, O_CREAT | O_TRUNC | O_RDWR, 0644);
+    if (fd < 0) {
+      B2INFO("RingBuffer ID file could not be created.");
+    } else {
+      close(fd);
+    }
+  }
+
   B2INFO("RingBuffer initialization done with shm=" << m_shmid);
 }
 
@@ -182,9 +194,8 @@ void RingBuffer::cleanup()
     if (m_file) {
       //      close(m_pathfd);
       unlink(m_pathname.c_str());
-    } else {
-      unlink(m_strbuf);
     }
+    unlink(m_strbuf);
   }
 }
 

36b59d810116fd5bb0766d72f694da91d9b72d07
Jakob Lettenbichler <jkl@jodoschka.com>
2014-01-08 13:12:13 +0000

    updated expreco-file for testbeam
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@8445 b38c6c8f-bee8-465d-8448-9abe6915d496

--- testbeam/vxd/online/expreco.py
+++ testbeam/vxd/online/expreco.py
@@ -1,11 +1,12 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
 import sys
 
 from basf2 import *
-#from simulation import register_simulation
-#from reconstruction import register_reconstruction
+# from simulation import register_simulation
+# from reconstruction import register_reconstruction
 
 set_log_level(LogLevel.WARNING)
 
@@ -18,7 +19,7 @@ print argvs[3]
 print argvs[4]
 print argc
 
-#field off
+# field off
 fieldOn = False
 
 # Load Geometry module
@@ -42,41 +43,91 @@ else:
     SVDClust.param('TanLorentz_electrons', 0.)
 
 # SVD DQM module
-SVD_DQM = register_module("SVDDQM")
+SVD_DQM = register_module('SVDDQM')
+
+# VXDTF:
+## parameters:
+secSetup = ['testBeamMini6GeVSVD-moreThan1500MeV_SVD']
+qiType = 'circleFit'
+filterOverlaps = 'hopfield'
+#
+vxdtf = register_module('VXDTF')
+vxdtf.logging.log_level = LogLevel.INFO
+vxdtf.logging.debug_level = 2
+param_vxdtf = {
+    'activateBaselineTF': 1,
+    'tccMinState': [2],
+    'tccMinLayer': [3],
+    'standardPdgCode': -11,
+    'sectorSetup': secSetup,
+    'calcQIType': qiType,
+    'killEventForHighOccupancyThreshold': 75,
+    'cleanOverlappingSet': False,
+    'filterOverlappingTCs': filterOverlaps,
+    'TESTERexpandedTestingRoutines': True,
+    'qiSmear': False,
+    'smearSigma': 0.000001,
+    'GFTrackCandidatesColName': 'caTracks',
+    'tuneCutoffs': 5,
+    'activateDistanceXY': [False],
+    'activateDistance3D': [True],
+    'activateAngles3DHioC': [False],
+    'activateAnglesXYHioC': [False],
+    'activateDeltaSlopeRZHioC': [False],
+    'activateDistance2IPHioC': [False],
+    'activatePTHioC': [False],
+    'activateHelixFitHioC': [False],
+    'activateDeltaPtHioC': [False],
+    'activateDeltaDistance2IPHioC': [False],
+    'activateAngles3D': [True],
+    'activateAnglesXY': [False],
+    'activateAnglesRZ': [False],
+    'activateDeltaSlopeRZ': [False],
+    'activateDistance2IP': [False],
+    'activatePT': [False],
+    'activateHelixFit': [False],
+    'activateZigZagXY': [False],
+    'activateDeltaPt': [True],
+    'activateCircleFit': [False],
+    'tuneCircleFit': [0.00000001],
+    'tuneAngles3D': [-0.1],
+    'tuneDistance3D': [-0.1],
+    }
+vxdtf.param(param_vxdtf)
 
 # create the main path
 main = create_path()
 
 # Add input module
-#input = register_module("SeqRootInput")
-#input.param ( "inputFileName", "/fcdisk1-1/data/sim/sim-evtgen.sroot")
-#main.add_module(input)
+# input = register_module("SeqRootInput")
+# input.param ( "inputFileName", "/fcdisk1-1/data/sim/sim-evtgen.sroot")
+# main.add_module(input)
 
 # Add Rbuf2Ds
-#rbuf2ds = register_module("Rbuf2Ds")
-rbuf2ds = register_module("FastRbuf2Ds")
-rbuf2ds.param("RingBufferName", argvs[1])
-rbuf2ds.param("NumThreads", 2)
+# rbuf2ds = register_module("Rbuf2Ds")
+rbuf2ds = register_module('FastRbuf2Ds')
+rbuf2ds.param('RingBufferName', argvs[1])
+rbuf2ds.param('NumThreads', 2)
 main.add_module(rbuf2ds)
 
 # Add DqmHistoManager
-hman = register_module("DqmHistoManager")
-hman.param("HostName", argvs[2])
-hman.param("Port", int(argvs[3]))
-#main.add_module(hman)
+hman = register_module('DqmHistoManager')
+hman.param('HostName', argvs[2])
+hman.param('Port', int(argvs[3]))
+# main.add_module(hman)
 
 # Add Progress
-progress = register_module("Progress")
+progress = register_module('Progress')
 main.add_module(progress)
 
 # Add Elapsed Time
-elapsed = register_module("ElapsedTime")
-elapsed.param("EventInterval", 10000)
+elapsed = register_module('ElapsedTime')
+elapsed.param('EventInterval', 10000)
 main.add_module(elapsed)
 
 # Add Ds2Raw
-ds2sample = register_module("Ds2Sample")
-ds2sample.param("RingBufferName", argvs[4])
+ds2sample = register_module('Ds2Sample')
+ds2sample.param('RingBufferName', argvs[4])
 main.add_module(ds2sample)
 
 # Add Gearbox and geometry
@@ -89,10 +140,12 @@ main.add_module(SVDClust)
 # Add SVD DQM module
 main.add_module(SVD_DQM)
 
+main.add_module(vxdtf)
+
 # Test seqrootoutput
-#output = register_module("SeqRootOutput" )
-#output.param ( "outputFileName", "/dev/null" )
-#main.add_module(output)
+# output = register_module("SeqRootOutput" )
+# output.param ( "outputFileName", "/dev/null" )
+# main.add_module(output)
 
 # Run
 process(main)

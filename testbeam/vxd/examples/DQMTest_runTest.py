e8982de1cc830b8c01c21ae06bc9c3982fc57095
Jakob Lettenbichler <jkl@jodoschka.com>
2014-01-12 22:02:22 +0000

    updated steering files for testbeam
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@8549 b38c6c8f-bee8-465d-8448-9abe6915d496

--- testbeam/vxd/examples/DQMTest_runTest.py
+++ testbeam/vxd/examples/DQMTest_runTest.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 from basf2 import *
 
-#Register modules
-input = register_module("RootInput")
+# Register modules
+input = register_module('RootInput')
 input.param('inputFileName', 'TBSimulation.root')
 
 # Histogram manager immediately after master module
-histo = register_module("HistoManager")
-histo.param("histoFileName", "DQM-VXD-histo.root")  # File to save histograms
+histo = register_module('HistoManager')
+histo.param('histoFileName', 'DQM-VXD-histo.root')  # File to save histograms
 
 # Report progress of processing
 progress = register_module('Progress')
@@ -34,15 +37,74 @@ SVDClust.param('TanLorentz_holes', 0.)
 SVDClust.param('TanLorentz_electrons', 0.)
 
 # PXD DQM module
-pxd_dqm = register_module("PXDDQM")
+pxd_dqm = register_module('PXDDQM')
 
 # SVD DQM module
-svd_dqm = register_module("SVDDQM")
+svd_dqm = register_module('SVDDQM')
 
-#Create paths
+# VXDTF:
+## parameters:
+secSetup = ['testBeamMini6GeVSVD-moreThan1500MeV_SVD']
+qiType = 'circleFit'
+filterOverlaps = 'hopfield'
+#
+vxdtf = register_module('VXDTF')
+vxdtf.logging.log_level = LogLevel.INFO
+vxdtf.logging.debug_level = 2
+param_vxdtf = {
+    'activateBaselineTF': 1,
+    'tccMinState': [2],
+    'tccMinLayer': [3],
+    'standardPdgCode': -11,
+    'sectorSetup': secSetup,
+    'calcQIType': qiType,
+    'killEventForHighOccupancyThreshold': 75,
+    'cleanOverlappingSet': False,
+    'filterOverlappingTCs': filterOverlaps,
+    'TESTERexpandedTestingRoutines': False,
+    'qiSmear': False,
+    'smearSigma': 0.000001,
+    'GFTrackCandidatesColName': 'caTracks',
+    'tuneCutoffs': 5,
+    'activateDistanceXY': [False],
+    'activateDistance3D': [True],
+    'activateAngles3DHioC': [False],
+    'activateAnglesXYHioC': [False],
+    'activateDeltaSlopeRZHioC': [False],
+    'activateDistance2IPHioC': [False],
+    'activatePTHioC': [False],
+    'activateHelixFitHioC': [False],
+    'activateDeltaPtHioC': [False],
+    'activateDeltaDistance2IPHioC': [False],
+    'activateAngles3D': [True],
+    'activateAnglesXY': [False],
+    'activateAnglesRZ': [False],
+    'activateDeltaSlopeRZ': [False],
+    'activateDistance2IP': [False],
+    'activatePT': [False],
+    'activateHelixFit': [False],
+    'activateZigZagXY': [False],
+    'activateDeltaPt': [True],
+    'activateCircleFit': [False],
+    'tuneCircleFit': [0.00000001],
+    'tuneAngles3D': [-0.1],
+    'tuneDistance3D': [-0.1],
+    }
+vxdtf.param(param_vxdtf)
+
+trackfitter = register_module('GenFitter')
+# trackfitter.logging.log_level = LogLevel.WARNING
+trackfitter.param('GFTrackCandidatesColName', 'caTracks')
+trackfitter.param('FilterId', 'Kalman')
+trackfitter.param('UseClusters', True)
+
+# VXDTF DQM module
+vxdtf_dqm = register_module('VXDTFDQM')
+
+# Create paths
 main = create_path()
 
-#Add modules to paths
+# Add modules to paths
 main.add_module(input)
 main.add_module(histo)  # immediately after master module
 main.add_module(progress)
@@ -52,6 +114,9 @@ main.add_module(PXDClust)
 main.add_module(SVDClust)
 main.add_module(pxd_dqm)
 main.add_module(svd_dqm)
+main.add_module(vxdtf)
+main.add_module(trackfitter)
+main.add_module(vxdtf_dqm)
 
-#Process events
+# Process events
 process(main)

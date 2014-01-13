e598504c9ca2015fe839a14cbf3a9eb03782221b
Christian Oswald <oswald@physik.uni-bonn.de>
2014-01-13 16:12:51 +0000

    NtupleTool with custom choice of float variables added (missing files).
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@8574 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ analysis/NtupleTools/include/NtupleCustomFloatsTool.h
@@ -0,0 +1,45 @@
+/**************************************************************************
+* BASF2 (Belle Analysis Framework 2)                                     *
+* Copyright(C) 2010 - Belle II Collaboration                             *
+*                                                                        *
+* Author: The Belle II Collaboration                                     *
+* Contributors: Christian Oswald                                         *
+*                                                                        *
+* This software is provided "as is" without any warranty.                *
+**************************************************************************/
+
+#ifndef NTUPLECUSTOMFLOATSTOOL_H
+#define NTUPLECUSTOMFLOATSTOOL_H
+#include <analysis/NtupleTools/NtupleFlatTool.h>
+#include <analysis/dataobjects/Particle.h>
+#include <analysis/DecayDescriptor/DecayDescriptor.h>
+#include <analysis/utility/VariableManager.h>
+#include <TTree.h>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace Belle2;
+
+namespace Belle2 {
+
+  /** NtupleTool to write a custom set of float variables to a tree. */
+  class NtupleCustomFloatsTool : public NtupleFlatTool {
+  private:
+    /** The float variables */
+    float* m_fVars;
+    /** Names of the float variables. */
+    std::vector<std::string> m_strVarNames;
+    /** List of function pointers corresponding to given variables. */
+    std::vector<VariableManager::FunctionPtr> m_functions;
+    /** Create branches in m_tree - this function should be called by the constructor only. */
+    void setupTree();
+  public:
+    /** Constuctor. */
+    NtupleCustomFloatsTool(TTree* tree, DecayDescriptor& decaydescriptor, std::string strOptions) : NtupleFlatTool(tree, decaydescriptor, strOptions) {setupTree();}
+    /** Set branch variables to properties of the provided Particle. */
+    void eval(const Particle* p);
+  };
+} // namepspace Belle2
+
+#endif // NTUPLECUSTOMFLOATSTOOL_H
---
+++ analysis/NtupleTools/src/NtupleCustomFloatsTool.cc
@@ -0,0 +1,64 @@
+/**************************************************************************
+* BASF2 (Belle Analysis Framework 2)                                     *
+* Copyright(C) 2010 - Belle II Collaboration                             *
+*                                                                        *
+* Author: The Belle II Collaboration                                     *
+* Contributors: Christian Oswald                                         *
+*                                                                        *
+* This software is provided "as is" without any warranty.                *
+**************************************************************************/
+
+#include <analysis/NtupleTools/NtupleCustomFloatsTool.h>
+#include <TBranch.h>
+#include <boost/algorithm/string.hpp>
+
+void NtupleCustomFloatsTool::setupTree()
+{
+
+  // Initialise function pointers of the variables
+  if (m_strOption.empty()) {
+    B2ERROR("No variables specified for the NtupleCustomFloatsTool!");
+  }
+  B2INFO("Option is: " << m_strOption);
+  boost::split(m_strVarNames, m_strOption, boost::is_any_of(":"));
+
+  int nVars = m_strVarNames.size();
+  for (int iVar = 0; iVar < nVars; iVar++) {
+    const VariableManager::Var* var = VariableManager::Instance().getVariable(m_strVarNames[iVar]);
+    if (!var) {
+      B2ERROR("Variable '" << m_strVarNames[iVar] << "' is not available in VariableManager!");
+    } else {
+      m_functions.push_back(var->function);
+    }
+  }
+
+  vector<string> strNames = m_decaydescriptor.getSelectionNames();
+  int nDecayProducts = strNames.size();
+
+  m_fVars = new float[nDecayProducts * nVars];
+
+  for (int iProduct = 0; iProduct < nDecayProducts; iProduct++) {
+    for (int iVar = 0; iVar < nVars; iVar++) {
+      int iPos = iProduct * nDecayProducts + iVar;
+      m_tree->Branch((strNames[iProduct] + "__" + m_strVarNames[iVar]).c_str(), &m_fVars[iPos], (strNames[iProduct] + "__" + m_strVarNames[iVar] + "/F").c_str());
+    }
+  }
+}
+
+void NtupleCustomFloatsTool::eval(const Particle* particle)
+{
+  if (!particle) {
+    printf("NtupleCustomFloatsTool::eval - ERROR, no Particle found!\n");
+    return;
+  }
+
+  vector<const Particle*> selparticles = m_decaydescriptor.getSelectionParticles(particle);
+  int nDecayProducts = selparticles.size();
+  int nVars = m_strVarNames.size();
+  for (int iProduct = 0; iProduct < nDecayProducts; iProduct++) {
+    for (int iVar = 0; iVar < nVars; iVar++) {
+      int iPos = iProduct * nDecayProducts + iVar;
+      m_fVars[iPos] = m_functions[iVar](selparticles[iProduct]);
+    }
+  }
+}

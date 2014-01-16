005f0179b9b6b6e630c47099b6f13011c9efddf9
Peter Kvasnicka <kvasnicka@ipnp.troja.mff.cuni.cz>
2014-01-16 16:03:29 +0000

    Added SVDIgnoredStripsMap class with demo application and sample file (by cloning Eugenio's SVDOnlineToOfflineMap class)
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@8682 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ svd/online/tools/read_svd_ignored_map.cc
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <svd/online/SVDIgnoredStripsMap.h>
+#include <vxd/dataobjects/VxdID.h>
+#include <set>
+
+
+int
+main(int argc, char** argv)
+{
+  if (argc != 2) {
+    printf("Hello!\n");
+    printf("Usage: %s filename.xml\n read filename.xml\n", argv[0]);
+    return 0;
+  }
+
+  Belle2::SVDIgnoredStripsMap test(argv[1]);
+  for (int plane = 3; plane < 7; ++ plane) {
+    Belle2::VxdID sensorID(plane, 1, plane);
+    printf("Plane %i, side v:\n", plane);
+    for (unsigned short ignored : test.getIgnoredStrips(sensorID))
+      printf("Ignored: %i\n", ignored);
+    sensorID.setSegmentNumber(1);
+    printf("Plane %i, side u:\n", plane);
+    for (unsigned short ignored : test.getIgnoredStrips(sensorID))
+      printf("Ignored: %i\n", ignored);
+  }
+}

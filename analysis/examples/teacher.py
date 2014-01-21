d63e5cffdae7f510557f1469cff1ebd7c33ee3ed
Thomas Keck <t.keck@online.de>
2014-01-21 15:48:17 +0000

    Fixed teacher.py example file
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@8818 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/examples/teacher.py
+++ analysis/examples/teacher.py
@@ -49,16 +49,18 @@ variables = [
     'chiProb',
     ]
 
-methods = {
-    'Fisher': 'H:!V:Fisher:VarTransform=None:CreateMVAPdfs:PDFInterpolMVAPdf=Spline2:NbinsMVAPdf=50:NsmoothMVAPdf=10',
-    'BDTGradient': '!H:!V:CreateMVAPdfs:NTrees=1000:BoostType=Grad:Shrinkage=0.10:UseBaggedGrad:GradBaggingFraction=0.5:nCuts=20:MaxDepth=2',
-    'PDEFoamBoost': '!H:!V:CreateMVAPdfs:Boost_Num=30:Boost_Transform=linear:SigBgSeparate=F:MaxDepth=4:UseYesNoCell=T:DTLogic=MisClassificationError:FillFoamWithOrigWeights=F:TailCut=0:nActiveCells=500:nBin=20:Nmin=400:Kernel=None:Compress=T',
-    'NeuroBayes': '!H:V:CreateMVAPdfs:NTrainingIter=50:TrainingMethod=BFGS:NBIndiPreproFlagByVarname=' \
-        + '=34,'.join([s.replace('*', 'C') for s in variables]) + '=34',
-    }
+methods = [('Fisher', 'Fisher',
+           'H:!V:Fisher:VarTransform=None:CreateMVAPdfs:PDFInterpolMVAPdf=Spline2:NbinsMVAPdf=50:NsmoothMVAPdf=10'
+           ), ('BDTGradient', 'BDT',
+           '!H:!V:CreateMVAPdfs:NTrees=1000:BoostType=Grad:Shrinkage=0.10:UseBaggedGrad:GradBaggingFraction=0.5:nCuts=20:MaxDepth=2'
+           ), ('PDEFoamBoost', 'PDEFoam',
+           '!H:!V:CreateMVAPdfs:Boost_Num=30:Boost_Transform=linear:SigBgSeparate=F:MaxDepth=4:UseYesNoCell=T:DTLogic=MisClassificationError:FillFoamWithOrigWeights=F:TailCut=0:nActiveCells=500:nBin=20:Nmin=400:Kernel=None:Compress=T'
+           ), ('NeuroBayes', 'NeuroBayes',
+           '!H:V:CreateMVAPdfs:NTrainingIter=50:TrainingMethod=BFGS:NBIndiPreproFlagByVarname='
+            + '=34,'.join([s.replace('*', 'C') for s in variables]) + '=34')]
 
 teacher = register_module('TMVATeacher')
-teacher.param('file', 'TMVA')
+teacher.param('identifier', 'TMVA')
 teacher.param('methods', methods)
 teacher.param('variables', variables)
 teacher.param('target', 'truth')

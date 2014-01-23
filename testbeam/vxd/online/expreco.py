9502b86d8c20f1ed8cc959479200c84e0b07c8ff
Peter Kvasnicka <kvasnicka@ipnp.troja.mff.cuni.cz>
2014-01-23 03:23:46 +0000

    Resolved merge conflict with expreco.py
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@8862 b38c6c8f-bee8-465d-8448-9abe6915d496

--- testbeam/vxd/online/expreco.py
+++ testbeam/vxd/online/expreco.py
@@ -5,29 +5,76 @@ import os
 import sys
 
 from basf2 import *
-# from simulation import register_simulation
-# from reconstruction import register_reconstruction
-
+# This is testbeam/vxd/scripts/setup_vxdtf.py
 from setup_vxdtf import setup_vxdtf
+#from simulation import register_simulation
+#from reconstruction import register_reconstruction
 
-set_log_level(LogLevel.WARNING)
+set_log_level(LogLevel.ERROR)
 
 argvs = sys.argv
 argc = len(argvs)
 
-print argvs[1]
-print argvs[2]
-print argvs[3]
-print argvs[4]
-print argc
+#print argvs[1]
+#print argvs[2]
+#print argvs[3]
+#print argvs[4]
+#print argc
 
-# field off
+# B-field
+# fieldOn = True
 fieldOn = False
+
+# flag to use if the PXD is in place and should be used in the Reconstruction
+havePXD = True
+
+# data file
+inputFileName = '/path/to/srrot/files/e0000r000rrr.sroot'
+
+# histogram file
+histoFileName = 'DQM-VXD-histo.root'
+
+# which geometry
+if havePXD:
+    geometry_file = 'testbeam/vxd/FullTelescopeVXDTB_v2.xml'
+else:
+    geometry_file = 'testbeam/vxd/FullTelescopeVXDTB_v1.xml'
+
+
+# Register modules
+input = register_module('SeqRootInput')
+input.param('inputFileName', inputFileName)
+
+# Histogram manager immediately after master module
+histo = register_module('HistoManager')
+histo.param('histoFileName', histoFileName)  # File to save histograms
+
+# Add Rbuf2Ds
+#rbuf2ds = register_module("Rbuf2Ds")
+#rbuf2ds = register_module("FastRbuf2Ds")
+#rbuf2ds.param("RingBufferName", argvs[1])
+#rbuf2ds.param("NumThreads", 1 )
+#main.add_module(rbuf2ds)
+
+# Add DqmHistoManager
+#hman = register_module("DqmHistoManager")
+#hman.param("HostName", argvs[2] );
+#hman.param("Port", int(argvs[3]) );
+#main.add_module(hman)
+
+# Add Ds2Raw
+#ds2sample = register_module("Ds2Sample")
+#ds2sample.param("RingBufferName", argvs[4])
+#main.add_module(ds2sample)
+
+# Report progress of processing
+progress = register_module('Progress')
 
 # Load Geometry module
 gearbox = register_module('Gearbox')
-# Telescopes, magnetic field, daemon PXD (air), SVD
-gearbox.param('fileName', 'testbeam/vxd/FullTelescopeVXDTB_v1.xml')
+# Telescopes, magnetic field, PXD, SVD
+# Need to select the
+gearbox.param('fileName', geometry_file)
 
 geometry = register_module('Geometry')
 if fieldOn:
@@ -35,35 +82,31 @@ if fieldOn:
 else:
     geometry.param('components', ['TB'])
 
-# PXD rawhit converter
-PXDSort = register_module('PXDRawHitSorter')
-# If the same pixel appears again in data, merge the two by adding their
-# charges, or discard the following occurrences?
-# Setting to True may lead to pixels with over-range charge when there are
-# several repetitions of data.
-PXDSort.param('mergeDuplicates', False)
-# True is conservative with respect to downstream processing chain in case
-# some parts do not support PXD frames. There is always a single frame,
-# though information on number of frames and start rows is propagated.
-PXDSort.param('mergeFrames', False)
-# PXD Unpacker currently doesn't provide valid sensor IDs. So the following
-# feature is enabled by default, but I mention it explicitly:
-PXDSort.param('assignID', True)
+if havePXD:
+    # PXD unpacker
+    PXDUnpack = register_module('PXDUnpacker')
+    PXDUnpack.param('DHHCmode', True)
+    PXDUnpack.param('HeaderEndianSwap', True)
+    # PXD rawhit sorter: convert PXDRawHits to PXDDigits. .
+    PXDSort = register_module('PXDRawHitSorter')
+    PXDSort.param('mergeDuplicates', False)
+    PXDSort.param('mergeFrames', True)
+    # PXD clusterizer
+    PXDClust = register_module('PXDClusterizer')
+    if fieldOn:
+        PXDClust.param('TanLorentz', 0.16)
+    else:
+        PXDClust.param('TanLorentz', 0.)
+        # PXD DQM modules
+    PXD_DQM = register_module('PXDDQM')
+    raw_pxd_dqm = register_module('pxdRawDQM')
 
-# PXD clusterizer
-PXDClust = register_module('PXDClusterizer')
+SVDUnpack = register_module('SVDUnpacker')
+SVDUnpack.param('xmlMapFileName', 'testbeam/vxd/data/SVD-OnlineOfflineMap.xml')
 
-# PXD DQM module
-PXD_DQM = register_module('PXDDQMModule')
-
-# SVD digit sorter
 SVDSort = register_module('SVDDigitSorter')
 SVDSort.param('mergeDuplicates', False)
-# Map of ignored strips - working, but not approved widely
-#SVDSort.param('ignoredStripsListName', \
-#    'testbeam/svd/data/SVD-IgnoredStripsList.xml')
 
-# SVD clusterizer
 SVDClust = register_module('SVDClusterizer')
 if fieldOn:
     SVDClust.param('TanLorentz_holes', 0.052)
@@ -71,82 +114,59 @@ if fieldOn:
 else:
     SVDClust.param('TanLorentz_holes', 0.)
     SVDClust.param('TanLorentz_electrons', 0.)
-
 # SVD DQM module
 SVD_DQM = register_module('SVDDQM')
 
 # VXDTF:
-# get a VXDTF with standard settings that writes TrackCands to caTracks
-vxdtf = setup_vxdtf('caTracks')
+
+# Depending on whether we have the PXD, we include it in trackfinding or don't
+if havePXD:
+    VXDTF = setup_vxdtf('caTracks', ['testBeamMini6GeVVXD-moreThan1500MeV_VXD'
+                        ])
+else:
+    VXDTF = setup_vxdtf('caTracks', ['testBeamMini6GeVSVD-moreThan1500MeV_SVD'
+                        ])
 
 # VXDTF DQM module
-vxdtf_dqm = register_module('VXDTFDQM')
+VXDTF_DQM = register_module('VXDTFDQM')
 
 trackfitter = register_module('GenFitter')
-# trackfitter.logging.log_level = LogLevel.WARNING
+trackfitter.param('GFTrackCandidatesColName', 'caTracks')
 trackfitter.param('FilterId', 'Kalman')
 trackfitter.param('UseClusters', True)
+trackfitter.param('NMinIterations', 3)
+trackfitter.param('NMaxIterations', 3)
 
 trackfit_dqm = register_module('TrackfitDQM')
+trackfit_dqm.param('GFTrackCandidatesColName', 'caTracks')
 
-# create the main path
+# Create paths
 main = create_path()
 
-# Add input module
-# input = register_module("SeqRootInput")
-# input.param ( "inputFileName", "/fcdisk1-1/data/sim/sim-evtgen.sroot")
-# main.add_module(input)
-
-# Add Rbuf2Ds
-# rbuf2ds = register_module("Rbuf2Ds")
-rbuf2ds = register_module('FastRbuf2Ds')
-rbuf2ds.param('RingBufferName', argvs[1])
-rbuf2ds.param('NumThreads', 2)
-main.add_module(rbuf2ds)
-
-# Add DqmHistoManager
-hman = register_module('DqmHistoManager')
-hman.param('HostName', argvs[2])
-hman.param('Port', int(argvs[3]))
-# main.add_module(hman)
-
-# Add Progress
-progress = register_module('Progress')
-main.add_module(progress)
-
-# Add Elapsed Time
-elapsed = register_module('ElapsedTime')
-elapsed.param('EventInterval', 10000)
-main.add_module(elapsed)
-
-# Add Ds2Raw
-ds2sample = register_module('Ds2Sample')
-ds2sample.param('RingBufferName', argvs[4])
-main.add_module(ds2sample)
-
-# Add Gearbox and geometry
+# Add modules to paths
+#main.add_module(rbuf2ds)
+main.add_module(input)
+main.add_module(histo)
+#main.add_module(hman)  # immediately after master module
 main.add_module(gearbox)
 main.add_module(geometry)
+#main.add_module(ds2sample)
+if havePXD:
+    main.add_module(PXDUnpack)
+    main.add_module(PXDSort)
+    main.add_module(PXDClust)
+    main.add_module(raw_pxd_dqm)
+    main.add_module(PXD_DQM)
 
-# PXD: sorter, clusterizer, dqm
-main.add_module(PXDSort)
-main.add_module(PXDClust)
-main.add_module(PXD_DQM)
-
-# SVD: clusterizer, dqm
+main.add_module(SVDUnpack)
 main.add_module(SVDSort)
 main.add_module(SVDClust)
 main.add_module(SVD_DQM)
-
-main.add_module(vxdtf)
+main.add_module(VXDTF)
 main.add_module(trackfitter)
-main.add_module(vxdtf_dqm)
+main.add_module(VXDTF_DQM)
 main.add_module(trackfit_dqm)
+main.add_module(progress)
 
-# Test seqrootoutput
-# output = register_module("SeqRootOutput" )
-# output.param ( "outputFileName", "/dev/null" )
-# main.add_module(output)
-
-# Run
+# Process events
 process(main)

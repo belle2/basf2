ddf7976e905e853f5c9c7a8e557cfe3a302164b7
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2014-01-27 14:27:55 +0000

    Make show_tmva_results script path-independent
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@9002 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/tools/show_tmva_results.sh
+++ analysis/tools/show_tmva_results.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 # show_tma_results.sh TMVA.root
 # Shows Control Plots for a training stored in root file created by TMVATeacher
-root -l "$BELLE2_EXTERNALS_DIR/src/root/tmva/test/TMVAGui.C(\"$1\")"
+
+#get full path
+FILE=`readlink -f "$1"`
+
+cd $BELLE2_EXTERNALS_DIR/src/root/tmva/test/
+if [ $? -ne 0 ]
+then
+  echo "Directory src/root/tmva/test not found in externals. Please re-install from source."
+  exit 1
+fi
+root -l "$BELLE2_EXTERNALS_DIR/src/root/tmva/test/TMVAGui.C(\"$FILE\")"

cd87c36ad3a36df2c2e220439f159414b875b257
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2014-01-27 13:55:34 +0000

    Fix fullrecon_input.py example
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@8999 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/examples/fullrecon_input.py
+++ analysis/examples/fullrecon_input.py
@@ -34,10 +34,15 @@ DVars = ['prodChildProb', 'p', 'pt', 'p_CMS', 'pt_CMS']
 BVars = ['prodChildProb', 'p', 'pt', 'p_CMS', 'pt_CMS']
 
 methods = [('BDTGradient_100', 'BDT',
-           '!H:CreateMVAPdfs:!V:NTrees=100:BoostType=Grad:Shrinkage=0.10:UseBaggedGrad:GradBaggingFraction=0.5:nCuts=10:MaxDepth=2'
-           ), ('NeuroBayes', 'NeuroBayes',
-           '!H:CreateMVAPdfs:V:NTrainingIter=50:TrainingMethod=BFGS')]  # 'BDTGradient_1000_20_5': '!H:CreateMVAPdfs:!V:NTrees=1000:BoostType=Grad:
-                                                                        # Shrinkage=0.10:UseBaggedGrad:GradBaggingFraction=0.5:nCuts=20:MaxDepth=5',
+           '!H:CreateMVAPdfs:!V:NTrees=100:BoostType=Grad:Shrinkage=0.10:'
+           'UseBaggedGrad:GradBaggingFraction=0.5:nCuts=10:MaxDepth=2'
+           ),
+           #('NeuroBayes', 'NeuroBayes',
+           #'!H:CreateMVAPdfs:V:NTrainingIter=50:TrainingMethod=BFGS')
+           ]
+# old syntax, needs to be updated
+# 'BDTGradient_1000_20_5': '!H:CreateMVAPdfs:!V:NTrees=1000:BoostType=Grad:
+# Shrinkage=0.10:UseBaggedGrad:GradBaggingFraction=0.5:nCuts=20:MaxDepth=5',
 
 fr = FullReconstruction()
 
@@ -48,46 +53,46 @@ fr.addParticle(Particle('pi+', chargedTrackVars, methods))
 fr.addParticle(Particle('K+', chargedTrackVars, methods))
 
 p = Particle('D0', DVars, methods)
-fr.addChannel(['K-', 'pi+'])
-fr.addChannel(['K-', 'pi+', 'pi+', 'pi-'])
-fr.addChannel(['K-', 'pi+', 'pi0'])
+p.addChannel(['K-', 'pi+'])
+p.addChannel(['K-', 'pi+', 'pi+', 'pi-'])
+p.addChannel(['K-', 'pi+', 'pi0'])
 fr.addParticle(p)
 
 p = Particle('D+', DVars, methods)
-fr.addChannel(['K-', 'pi+', 'pi+'])
-fr.addChannel(['K-', 'K+', 'pi+'])
-fr.addChannel(['K-', 'pi+', 'pi+', 'pi0'])
-fr.addChannel(['K-', 'K+', 'pi+', 'pi0'])
+p.addChannel(['K-', 'pi+', 'pi+'])
+p.addChannel(['K-', 'K+', 'pi+'])
+p.addChannel(['K-', 'pi+', 'pi+', 'pi0'])
+p.addChannel(['K-', 'K+', 'pi+', 'pi0'])
 fr.addParticle(p)
 
 p = Particle('D*+', DVars, methods)
-fr.addChannel(['D0', 'pi+'])
-fr.addChannel(['D+', 'pi0'])
+p.addChannel(['D0', 'pi+'])
+p.addChannel(['D+', 'pi0'])
 fr.addParticle(p)
 
 p = Particle('D*0', DVars, methods)
-fr.addChannel(['D0', 'pi0'])
-fr.addChannel(['D0', 'gamma'])
+p.addChannel(['D0', 'pi0'])
+p.addChannel(['D0', 'gamma'])
 fr.addParticle(p)
 
 p = Particle('B+', BVars, methods)
-fr.addChannel(['D0', 'pi+'])
-fr.addChannel(['D+', 'pi0'])
-fr.addChannel(['D*-', 'pi+', 'pi+'])
+p.addChannel(['D0', 'pi+'])
+p.addChannel(['D+', 'pi0'])
+p.addChannel(['D*-', 'pi+', 'pi+'])
 fr.addParticle(p)
 
 p = Particle('B0', BVars, methods)
-fr.addChannel(['D0', 'pi0'])
-fr.addChannel(['D-', 'pi+'])
-fr.addChannel(['D-', 'pi0', 'pi+'])
-fr.addChannel(['D*-', 'pi+', 'pi+', 'pi-'])
-fr.addChannel(['D*-', 'pi+', 'pi+', 'pi-', 'pi0'])
+p.addChannel(['D0', 'pi0'])
+p.addChannel(['D-', 'pi+'])
+p.addChannel(['D-', 'pi0', 'pi+'])
+p.addChannel(['D*-', 'pi+', 'pi+', 'pi-'])
+p.addChannel(['D*-', 'pi+', 'pi+', 'pi-', 'pi0'])
 fr.addParticle(p)
 
 main = create_path()
 main.add_module(register_module('RootInput'))
 main.add_module(register_module('ParticleLoader'))
-main.add_module(register_module('Progress'))
+main.add_module(register_module('ProgressBar'))
 
 fr.run(main)
 

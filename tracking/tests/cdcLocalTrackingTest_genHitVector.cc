43760401b65d3864ff8a930c1f66e12ba646e9a8
Oliver Frost <oliver.frost@desy.de>
2014-02-05 22:01:45 +0000

    CDCLocalTracking: Added all methods from poormans template to CDCGenHitVector as well as some test code
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@9204 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/tests/cdcLocalTrackingTest_genHitVector.cc
+++ tracking/tests/cdcLocalTrackingTest_genHitVector.cc
@@ -15,6 +15,8 @@
 #include <framework/datastore/StoreObjPtr.h>
 
 #include <tracking/cdcLocalTracking/eventdata/entities/CDCWireHit.h>
+#include <tracking/cdcLocalTracking/eventdata/entities/CDCGenHit.h>
+#include <tracking/cdcLocalTracking/eventdata/entities/CDCGenHitVector.h>
 #include <tracking/cdcLocalTracking/eventdata/collections/CDCWireHitVector.h>
 #include <tracking/cdcLocalTracking/topology/CDCWire.h>
 #include <tracking/cdcLocalTracking/topology/CDCWireTopology.h>
@@ -35,8 +37,8 @@ TEST_F(CDCLocalTrackingTest, CDCGenHitVector_WithCDCWireHits)
 
   CDCWireHit wirehit(firstWire, 0.0);
 
-  //CDCGenHitVector<CDCWireHit> collection;
-  CDCWireHitVector collection;
+  CDCGenHitVector<CDCWireHit> collection;
+  //CDCWireHitVector collection;
 
   ASSERT_TRUE(collection.empty());
 
@@ -46,6 +48,57 @@ TEST_F(CDCLocalTrackingTest, CDCGenHitVector_WithCDCWireHits)
 
 }
 
+
+TEST_F(CDCLocalTrackingTest, CDCGenHitVector_Sorting)
+{
+  CDCGenHitVector<CDCGenHit> collection;
+  EXPECT_TRUE(collection.isSorted()) << "Empty collection is not sorted";
+  EXPECT_TRUE(collection.checkSorted()) << "Empty collection was checked to be not sorted";
+  collection.sort(); //Just check if those run
+  collection.ensureSorted(); //Just check if those run
+
+  CDCGenHit lowestGenHit;
+  CDCGenHit secondLowestGenHit(WireID(0, 0, 1), Vector2D(0.0, 0.0));
+  CDCGenHit thirdLowestGenHit(WireID(0, 0, 1), Vector2D(0.0, 1.0));
+  CDCGenHit fourthLowestGenHit(WireID(0, 1, 1), Vector2D(0.0, 1.0));
+
+  //Add the second hit first
+  collection.push_back(secondLowestGenHit);
+  EXPECT_TRUE(collection.isSorted()) << "Single element collection is not sorted";
+  EXPECT_TRUE(collection.checkSorted()) << "Single element collection was check to be not sorted";
+  collection.ensureSorted(); //Just check if those run
+  collection.sort();  //Just check if those run
+
+  //Add the third hit - collection should still be sorted
+  collection.push_back(thirdLowestGenHit);
+  EXPECT_TRUE(collection.isSorted()) << "Collection is not sorted after the third hit was added.";
+  EXPECT_TRUE(collection.checkSorted()) << "Collection is not sorted after the third hit was added.";;
+  collection.ensureSorted(); //Just check if those run
+  collection.sort();  //Just check if those run
+
+  //Now add the first hit
+  //Collection should not be sorted anymore
+  collection.push_back(lowestGenHit);
+  EXPECT_FALSE(collection.isSorted());
+  EXPECT_FALSE(collection.checkSorted());
+  collection.ensureSorted();
+  EXPECT_TRUE(collection.isSorted());
+  EXPECT_TRUE(collection.checkSorted());
+
+  //Now add the fourth hit - collection should still be sorted
+  collection.push_back(fourthLowestGenHit);
+  EXPECT_TRUE(collection.isSorted()) << "Collection is not sorted after the fourth hit was added.";
+  EXPECT_TRUE(collection.checkSorted()) << "Collection was checked not sorted after the fourth hit was added.";
+  collection.ensureSorted();
+
+  collection.push_back(thirdLowestGenHit);
+  collection.push_back(secondLowestGenHit);
+  EXPECT_FALSE(collection.isSorted()) << "Collection is not sorted after adding two unsorted hits.";
+  EXPECT_FALSE(collection.checkSorted()) << "Collection was checked not sorted after adding two unsorted hits.";
+  collection.clear();
+  EXPECT_TRUE(collection.isSorted()) << "Collection is not sorted after clearing.";
+  EXPECT_TRUE(collection.checkSorted()) << "Collection was checked not sorted after clearing.";
+}
 
 
 

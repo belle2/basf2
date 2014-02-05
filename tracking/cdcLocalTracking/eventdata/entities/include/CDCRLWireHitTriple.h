4b53d3d646af12f0549d0f63b28851a5407a9ff5
Oliver Frost <oliver.frost@desy.de>
2014-02-05 22:19:32 +0000

    CDCLocalTracking: Added a type for a triple of oriented wire hits which will become a base class of CDCRecoFacet factoring a great deal of functionality.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@9240 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/cdcLocalTracking/eventdata/entities/include/CDCRLWireHitTriple.h
@@ -0,0 +1,192 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2014 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#ifndef CDCRLWIREHITTRIPLE_H
+#define CDCRLWIREHITTRIPLE_H
+
+#include <utility>
+
+#include <tracking/cdcLocalTracking/mockroot/MockRoot.h>
+#include <tracking/cdcLocalTracking/typedefs/BasicTypes.h>
+
+#include "CDCRLWireHit.h"
+#include "CDCRLWireHitPair.h"
+
+namespace Belle2 {
+  namespace CDCLocalTracking {
+
+    /// Class representing a triple of neighboring wire hits.
+    /** Class representing a triple of neighboring wire hits.
+     *  The first is taken as a single, while the second and the third are group into a pair.
+     *  In this way we can save some object creations during lookups of a triple following this one,
+     *  since the projection to the last pair can be done by reference.*/
+    class CDCRLWireHitTriple : public UsedTObject {
+
+    public:
+
+      enum Shape { ILLSHAPED = 0 , ORTHO = 1, META = 2, PARA = 3, };
+
+    public:
+
+      /// Default constructor for ROOT compatibility.
+      CDCRLWireHitTriple();
+
+      /// Constructor taking three oriented wire hits
+      CDCRLWireHitTriple(
+        const CDCRLWireHit* startRLWireHit,
+        const CDCRLWireHit* middleRLWireHit,
+        const CDCRLWireHit* endRLWireHit
+      );
+
+      /// Empty destructor
+      ~CDCRLWireHitTriple() {;}
+
+
+
+    public:
+      bool operator==(const CDCRLWireHitTriple& other) const
+      { return getStartRLWireHit() == other.getStartRLWireHit() and getRearRLWireHitPair() == other.getRearRLWireHitPair(); }
+
+
+      /// Establish a total ordering  based on the three oriented wire hits
+      bool operator< (const CDCRLWireHitTriple& other) const {
+        return getStartRLWireHit() <  other.getStartRLWireHit() or
+               (getStartRLWireHit() == other.getStartRLWireHit() and getRearRLWireHitPair() < other.getRearRLWireHitPair());
+      }
+
+      /// Standard output operator for debugging purposes
+      friend std::ostream& operator<<(std::ostream& output, const CDCRLWireHitTriple& rlWireHitTriple) {
+        return output << "Start : "  << rlWireHitTriple.getStartWireHit() << " with rl " << rlWireHitTriple.getStartRLInfo() <<
+               " Middle : " << rlWireHitTriple.getMiddleWireHit() << " with rl " << rlWireHitTriple.getMiddleRLInfo() <<
+               " End : "    << rlWireHitTriple.getEndWireHit()  << " with rl " << rlWireHitTriple.getEndRLInfo() ;
+      }
+
+
+
+
+      /// Access the object methods and methods from a pointer in the same way.
+      /** In situations where the type is not known to be a pointer or a reference there is no way to tell \n
+       *  if one should use the dot '.' or operator '->' for method look up. \n
+       *  So this function defines the -> operator for the object. \n
+       *  No matter you have a pointer or an object access is given with '->'*/
+      const CDCRLWireHitTriple* operator->() const { return this; }
+
+
+
+      /// Getter for the shape of this tiple if all three oriented wire hits are neighbors. Else INVALIDSHAPE
+      Shape getShape() const;
+
+
+
+      /// Estimate the transvers travel distance on the given circle to the first oriented wire hit
+      FloatType getStartPerpS(const CDCTrajectory2D& trajectory2D) const
+      { return getStartRLWireHit().getStartPerpS(trajectory2D); }
+
+      /// Estimate the transvers travel distance on the given circle to the third oriented wire hit
+      FloatType getEndPerpS(const CDCTrajectory2D& trajectory2D) const
+      { return getRearRLWireHitPair().getEndPerpS(trajectory2D); }
+
+      /// Calculates the squared distance of the three oriented wire hits to a circle as see from the transvers plane.
+      /** Returns the sum of the squared distances to the three oriented wire hits */
+      FloatType getSquaredDist2D(const CDCTrajectory2D& trajectory2D) const
+      { return getStartRLWireHit().getSquaredDist2D(trajectory2D) + getRearRLWireHitPair().getSquaredDist2D(trajectory2D);}
+
+
+
+      /// Getter for the common superlayer id of the pair
+      ILayerType getISuperLayer() const {
+        ILayerType result = getStartRLWireHit().getISuperLayer();
+        return result == getRearRLWireHitPair().getISuperLayer() ? result : INVALIDSUPERLAYER;
+      }
+
+      /// Getter for the common axial type of the superlayer of the pair
+      AxialType getAxialType() const
+      { return getISuperLayer() == INVALIDSUPERLAYER ? INVALID_AXIALTYPE : getStartRLWireHit().getAxialType(); }
+
+
+
+      /// Getter for the wire the first oriented wire hit is based on
+      const CDCWire& getStartWire() const
+      { return getStartRLWireHit().getWire(); }
+
+      /// Getter for the wire the second oriented wire hit is based on
+      const CDCWire& getMiddleWire() const
+      { return getMiddleRLWireHit().getWire(); }
+
+      /// Getter for the wire the third oriented wire hit is based on
+      const CDCWire& getEndWire() const
+      { return getEndRLWireHit().getWire(); }
+
+      /// Indicator if any of the three oriented wire hits is based on the given wire
+      bool hasWire(const CDCWire& wire) const
+      { return getStartRLWireHit()->hasWire(wire) or getRearRLWireHitPair()->hasWire(wire); }
+
+
+
+      /// Getter for the right left passage information of the first oriented wire hit
+      const RightLeftInfo& getStartRLInfo() const
+      { return getStartRLWireHit().getRLInfo(); }
+
+      /// Getter for the right left passage information of the second oriented wire hit
+      const RightLeftInfo& getMiddleRLInfo() const
+      { return getMiddleRLWireHit().getRLInfo(); }
+
+      /// Getter for the right left passage information of the third oriented wire hit
+      const RightLeftInfo& getEndRLInfo() const
+      { return getEndRLWireHit().getRLInfo(); }
+
+
+
+      /// Getter for the hit wire of the first oriented wire hit
+      const CDCWireHit& getStartWireHit() const
+      { return getStartRLWireHit().getWireHit(); }
+
+      /// Getter for the hit wire of the second oriented wire hit
+      const CDCWireHit& getMiddleWireHit() const
+      { return getMiddleRLWireHit().getWireHit(); }
+
+      /// Getter for the hit wire of the third oriented wire hit
+      const CDCWireHit& getEndWireHit() const
+      { return getEndRLWireHit().getWireHit(); }
+
+      /// Indicator if any of the three oriented wire hits is based on the given wire hit
+      bool hasWireHit(const CDCWireHit& wirehit) const
+      { return getStartWireHit()->hasWireHit(wirehit) or getRearRLWireHitPair()->hasWireHit(wirehit); }
+
+
+
+      /// Getter for the first oriented wire hit
+      const CDCRLWireHit& getStartRLWireHit() const
+      { return *m_startRLWireHit; }
+
+      /// Getter for the second oriented wire hit
+      const CDCRLWireHit& getMiddleRLWireHit() const
+      { return getRearRLWireHitPair().getFromRLWireHit(); }
+
+      /// Getter for the third oriented wire hit
+      const CDCRLWireHit& getEndRLWireHit() const
+      { return getRearRLWireHitPair().getToRLWireHit(); }
+
+      /// Getter for the pair of second and third oriented wire hit
+      const CDCRLWireHitPair& getRearRLWireHitPair() const
+      { return m_rearRLWireHitPair; }
+
+
+
+    private:
+      const CDCRLWireHit* m_startRLWireHit; ///< Memory for the reference to the start oriented wire hit
+      CDCRLWireHitPair m_rearRLWireHitPair; ///< Memory for the reference to the remaining two oriented wire hits
+
+      /// ROOT Macro to make CDCRLWireHitTriple a ROOT class.
+      ClassDefInCDCLocalTracking(CDCRLWireHitTriple, 1);
+
+    }; //end class CDCRLWireHitTriple
+  } // end namespace CDCLocalTracking
+} // end namespace Belle2
+#endif // CDCRECOFACET_H
---
+++ tracking/cdcLocalTracking/eventdata/entities/src/CDCRLWireHitTriple.cc
@@ -0,0 +1,64 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2014 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include "../include/CDCRLWireHitTriple.h"
+
+using namespace std;
+using namespace Belle2;
+using namespace CDCLocalTracking;
+
+ClassImpInCDCLocalTracking(CDCRLWireHitTriple)
+
+
+
+CDCRLWireHitTriple::CDCRLWireHitTriple():
+  m_startRLWireHit(nullptr),
+  m_rearRLWireHitPair(nullptr, nullptr)
+{
+  B2WARNING("CDCRLWireHitTiple initialized with three nullptr");
+}
+
+
+
+CDCRLWireHitTriple::CDCRLWireHitTriple(
+  const CDCRLWireHit* startRLWireHit,
+  const CDCRLWireHit* middleRLWireHit,
+  const CDCRLWireHit* endRLWireHit
+):
+  m_startRLWireHit(startRLWireHit),
+  m_rearRLWireHitPair(middleRLWireHit, endRLWireHit)
+{
+  if (startRLWireHit == nullptr) { B2ERROR("CDCRLWireHitPair initialized with nullptr as first oriented wire hit"); }
+  if (middleRLWireHit == nullptr) { B2ERROR("CDCRLWireHitPair initialized with nullptr as second oriented wire hit"); }
+  if (endRLWireHit == nullptr) { B2ERROR("CDCRLWireHitPair initialized with nullptr as third oriented wire hit"); }
+}
+
+
+
+CDCRLWireHitTriple::Shape CDCRLWireHitTriple::getShape() const
+{
+
+  const CDCWire& startWire = getStartWire();
+  const CDCWire& middleWire = getMiddleWire();
+  const CDCWire& endWire = getEndWire();
+
+  WireNeighborType middleToStartNeighborType = middleWire.isNeighborWith(startWire);
+  WireNeighborType middleToEndNeighborType   = middleWire.isNeighborWith(endWire);
+
+  if (middleToStartNeighborType and middleToEndNeighborType) {
+    if (startWire.isNeighborWith(endWire)) return ORTHO;
+    else if (abs(middleToStartNeighborType - middleToEndNeighborType) == 6) return PARA;
+    else return META;
+  }
+  //else
+  return Shape::ILLSHAPED;
+}
+
+

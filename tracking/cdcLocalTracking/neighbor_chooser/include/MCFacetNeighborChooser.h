ae05fce19f769d99f1f4f5444503e10bd3670c43
Oliver Frost <oliver.frost@desy.de>
2014-02-05 22:30:56 +0000

    Removed some comments and fixed some includes in MCFacetNeighborChooser
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@9262 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/cdcLocalTracking/neighbor_chooser/include/MCFacetNeighborChooser.h
+++ tracking/cdcLocalTracking/neighbor_chooser/include/MCFacetNeighborChooser.h
@@ -13,10 +13,9 @@
 
 #include <tracking/cdcLocalTracking/typedefs/BasicTypes.h>
 #include <tracking/cdcLocalTracking/typedefs/BasicConstants.h>
-#include <tracking/cdcLocalTracking/typedefs/UsedDataHolders.h>
+#include <tracking/cdcLocalTracking/eventdata/entities/CDCRecoFacet.h>
 
 #include <tracking/cdcLocalTracking/creator_filters/MCFacetFilter.h>
-
 #include "BaseFacetNeighborChooser.h"
 
 namespace Belle2 {
@@ -39,14 +38,10 @@ namespace Belle2 {
         const CDCRecoFacet& neighborFacet
       ) const {
 
-
-
         //the last wire of the neighbor should not be the same as the start wire of the facet
         if (facet.getStartWire() == neighborFacet.getEndWire()) {
           return NOT_A_NEIGHBOR;
         }
-
-
 
         // Despite of that two facets are neighbors if both are true facets
         // That also implies the correct tof alignment of the hits not common to both facets
@@ -55,7 +50,6 @@ namespace Belle2 {
 
         bool mcDecision = (not isNotACell(facetWeight)) and (not isNotACell(neighborWeight));
 
-
         // the weight must be -2 because the overlap of the facets is two points
         // so the amount of two facets is 4 points hence the cellular automat
         // must calculate 3 + (-2) + 3 = 4 as cellstate
@@ -63,58 +57,12 @@ namespace Belle2 {
         // ( together with the facet creator filter)
         return mcDecision ? -2.0 : NOT_A_NEIGHBOR;
 
-
-        /*
-        const CDCWireHit * startWireHit   = facet.getStartWireHit();
-
-        const CDCWireHit * middleWireHit  = facet.getMiddleWireHit();
-              //  == neighborFacet.getStartWireHit();
-
-        const CDCWireHit * endWireHit     = facet.getEndWireHit();
-              //  == neighborFacet.getMiddleWireHit();
-
-        const CDCWireHit * neighborStartWireHit   = facet.getStartWireHit();
-              //  == facet.getMiddleWireHit();
-
-        const CDCWireHit * neighborMiddleWireHit  = facet.getMiddleWireHit();
-              //  == facet.getEndWireHit();
-
-        const CDCWireHit * neighborEndWireHit     = facet.getEndWireHit();
-
-
-        ITrackType iTrack
-            = m_mcLookUp.getMajorMCTrackId(startWireHit,
-                                           middleWireHit,
-                                           endWireHit);
-
-
-        ITrackType neighborITrack
-            = m_mcLookUp.getMajorMCTrackId(neighborStartWireHit,
-                                           neighborMiddleWireHit,
-                                           neighborEndWireHit);
-
-        if ( iTrack == INVALID_ITRACK or
-             neighborITrack == INVALID_ITRACK or
-             neighborITrack != iTrack ){
-
-          //B2DEBUG(200,"Rejected facet neighbor because major track id does not match");
-
-        }
-
-
-        return -2;
-        */
       }
 
     private:
       const MCFacetFilter m_mcFacetFilter;
-      //const CDCMCLookUp & m_mcLookUp;
-
-
 
     }; // end class
-
-
   } //end namespace CDCLocalTracking
 } //end namespace Belle2
 

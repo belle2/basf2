a38dc0635061fd0486d82d4c8cfbe595294c0f90
Martin Heck <martin.heck@kit.edu>
2014-02-21 14:25:04 +0000

    Movin the get function to the header, so the function can be inlined.
    Fixstyle
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@9537 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/gfbfield/include/GFGeant4Field.h
+++ tracking/gfbfield/include/GFGeant4Field.h
@@ -3,25 +3,38 @@
  * Copyright(C) 2011  Belle II Collaboration                              *
  *                                                                        *
  * Author: The Belle II Collaboration                                     *
- * Contributors: Andreas Moll                                             *
+ * Contributors: Andreas Moll, Martin Heck                                *
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/
-
 #pragma once
-#include <genfit/AbsBField.h>
 #include <geometry/bfieldmap/BFieldMap.h>
+#include <genfit/AbsBField.h>
 
 /** Interface of the Belle II B-field with GenFit.
  */
 class GFGeant4Field : public genfit::AbsBField {
 public:
+  /** Constructor.
+   *
+   *  The same B field as in the simulation is used.
+   *  If you want to use a different field in reconstruction, please run two jobs and change
+   *  the used B field in the geometry data (Belle2.xml file).
+   */
   GFGeant4Field(): genfit::AbsBField(), m_bField(Belle2::BFieldMap::Instance()) {}
-  TVector3 get(const TVector3& pos) const;
+
+  /** Getter for the magnetic field.
+   *
+   *  As Genfit uses kGauss, but we use T in basf2, we need to apply a factor 10 in the calculation.
+   *  @param position   Position at which the magnetic field should be evaluated.
+   */
+  TVector3 get(const TVector3& position) const {
+    return (10.0 * m_bField.getBField(position));
+  }
 
 
 protected:
-  Belle2::BFieldMap& m_bField; /**< Reference to the central magnetic field map.*/
-
+  /** Reference to the central magnetic field map. */
+  Belle2::BFieldMap& m_bField;
 };
 

c9a5ccc5240478a92fbca05caa531ca26b24767d
Tadeas Bilka <tadeas.bilka@gmail.com>
2014-03-02 16:50:52 +0000

    Complete processing chain example for standalone run alignment. Set important settings in python files (geometry, alignment, field, sectormaps, masking...). Look for fixed parameters in steering files. Get merged digits (see eutel/examples) and run (from the examples folder, or copy .py and steering files to new dir) Digits2Tracks. Then run FitForAlignment. Finaly set STAGE=2 in FitForAlignment and run again to align again for checking.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@9646 b38c6c8f-bee8-465d-8448-9abe6915d496

--- testbeam/vxd/examples/FitForAlignment.py
+++ testbeam/vxd/examples/FitForAlignment.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# This script (despite its name) contains the whole
-# processing chain including alignment.
-# It is intented as direct next step after digit merging (see eutel/examples).
-# Telescopes are clusterized but the track finder ignores them so far
+# This script refits a file produced by Digits2Tracks.py
+# and also (despite its name) does the alignment.
 #
-# This script starts with custom alignment file
-# and finishes with resulting file with sum initial and computed alignment
+# It is intented as next step after initial alignment is computed
+# in Digits2Tracks.py. This alignment is here loaded and track fitting
+# (and optionaly track finding) is performed again.
+# Also the alignment is computed again, this time corrections should be small
 #
 # Sorry for mismatch of usual and Gear paths. Only geometry and masks use Gear paths
 
@@ -23,6 +23,12 @@ release = str(os.getenv('BELLE2_LOCAL_DIR')) + '/'
 # -------------------------------------------------
 #          IMPORTANT PROCESSING SETTINGS
 # -------------------------------------------------
+# Starting with stage 1, each next stage takes
+# results as previous one as input
+STAGE = 1
+# STAGE = 2
+# WARNING: You must set correct mille binary name in MP2 steering
+# Here only stage 1 and 2 are prepared
 
 # Geometry file: _v1 for GeoID=0, _v2 for GeoID=1
 geometry = 'testbeam/vxd/FullTelescopeVXDTB_v2.xml'  # Gear path
@@ -31,39 +37,39 @@ geometry = 'testbeam/vxd/FullTelescopeVXDTB_v2.xml'  # Gear path
 # Custom alignment must be set at the very begining as it modifies main xml
 # during execution of basf2
 # NOTE: You need a separated (from main xml) file for playing with alignment
-alignment = release + 'testbeam/vxd/data/nominal_alignment.xml'
+alignment = 'alignment_stage_' + str(STAGE) + '.xml'
 
 # Turn ON/OFF 1T constant mag. field, affects: geometry/tracking, clustering, track finding
 PCMAG_ON = True  # only 1T/0T supported this way
 
+# If last alignment corrections were large, better to repeat track finding
+# However, once you find "final" alignment, you should repeat the whole chain from Digits2Tracks.py
+redo_trackfinding = True
 # This sector map must reflect field ON/OFF and nominal beam energy ... look for it or create your own
 sectormap = 'TB4GeV1TRun500VXD-moreThan1500MeV_VXD'
 # sectormap = 'TB0T3GeVVXD-moreThan1500MeV_VXD'
 
-# Masking for sensors
-pxd_mask = 'testbeam/vxd/data/PXD-IgnoredPixelsList.xml'  # Gear path
-svd_mask = 'testbeam/vxd/data/SVD-IgnoredStripsList.xml'  # Gear path
-
 # ------------- INPUT/OUTPUT SETTINGS ------------
 
 # Input root file with merged digits + event data (nothing else)
-input_root = 'MRun_500.root'
+input_root = 'Digits2Tracks.root'
 # Output file with added clusters and tracks
-output_root = 'Digits2Tracks.root'
+output_root = 'FitForAlignment_stage_' + str(STAGE) + '.root'
 # Complete DQM for digits/clusters/correlations/track finding/Kalman fitting...
-output_dqm = 'DQM_Digits2Tracks.root'
+output_dqm = 'DQM_FitForAlignment_stage_' + str(STAGE) + '.root'
 # Output Mille binary file with data for alignment
-alignment_binary = 'millefile_stage_0.dat'
+alignment_binary = 'millefile_stage_' + str(STAGE) + '.dat'
 
 # Often you do not want to run Millepede automatically. Then set this to False
 do_alignment = True
 # File with final alignment after MP2 correction (+ initial)
-output_alignment = 'alignment_stage_1.xml'
+output_alignment = 'alignment_stage_' + str(STAGE + 1) + '.xml'
 # File with alignment steering
 # NOTE: You have to link the alignment_binary (or more such files) from the steering file
-alignment_steering = 'steer_stage_0.txt'
+alignment_steering = 'steer_stage_' + str(STAGE) + '.txt'
 # This is just intermediate step btw. MP2 and sum of alignment ... just translated millepede.res file
-output_alignment_corrections = 'alingment_corrections_stage_0to1.xml'
+output_alignment_corrections = 'alingment_corrections_stage_' + str(STAGE) \
+    + 'to' + str(STAGE + 1) + '.xml'
 
 # ------------------------------------------------
 # ------------------------------------------------
@@ -90,57 +96,31 @@ else:
 input = register_module('RootInput')
 input.param('inputFileName', input_root)
 
-# Masking in fact:
-PXDDSort = register_module('PXDDigitSorter')
-PXDDSort.param('ignoredPixelsListName', pxd_mask)
-
-# PXD clusterizer
-PXDClust = register_module('PXDClusterizer')
-if PCMAG_ON:
-    PXDClust.param('TanLorentz', 0.1625)  # value scaled from 0.25 for 1.5T to 0.975T
-else:
-    PXDClust.param('TanLorentz', 0.)
-
-# PXD DQM module
-PXD_DQM = register_module('PXDDQM')
-
-# SVD Digit sorter ... masking
-SVDSort = register_module('SVDDigitSorter')
-SVDSort.param('mergeDuplicates', False)
-# No gearbox conventions, VERBATIM path!
-SVDSort.param('ignoredStripsListName', svd_mask)
-
-# SVD clusterizer
-SVDClust = register_module('SVDClusterizer')
-if PCMAG_ON:
-    SVDClust.param('TanLorentz_holes', 0.052)  # value scaled from 0.08 for 1.5T to 0.975T
-    SVDClust.param('TanLorentz_electrons', 0.)
-else:
-    SVDClust.param('TanLorentz_holes', 0.)
-    SVDClust.param('TanLorentz_electrons', 0.)
-# SVDClust.param('applyTimeWindow', True)
-# SVDClust.param('triggerTime', -20.0)
-# SVDClust.param('acceptanceWindowSize', 20.0)
-
-# SVD DQM module
-svd_dqm = register_module('SVDDQM')
-
-# EUDET clusterizer
-TelClust = register_module('TelClusterizer')
-TelClust.param('Clusters', 'TelClusters')
-
-# TEL DQM module
-tel_dqm = register_module('TelDQM')
-tel_dqm.param('Clusters', 'TelClusters')
-
-# TB DQM module adds correlations between telescopes and nearest VXD sensors.
-telvxd_dqm = register_module('TelxVXD')
+# Clean-up collections (remove genfit stuff), we need just this:
+branchNames = [
+    'EventMetaData',
+    'PXDClusters',
+    'PXDClustersToPXDDigits',
+    'PXDDigits',
+    'SVDClusters',
+    'SVDClustersToSVDDigits',
+    'SVDDigits',
+    'TelEventInfo',
+    'TelDigits',
+    'TelClusters',
+    'TelClustersToTelDigits',
+    ]
+# ... and caTracks if we do not want to repeat track finding (=hit matching + mom. seed)
+if not redo_trackfinding:
+    branchNames.append('caTracks')
+input.param('branchNames', branchNames)
 
 # Track finding by VXDTF
-if PCMAG_ON:
-    VXDTF = setup_vxdtf1T('caTracks', [sectormap])
-else:
-    VXDTF = setup_vxdtf('caTracks', [sectormap])
+if redo_trackfinding:
+    if PCMAG_ON:
+        VXDTF = setup_vxdtf1T('caTracks', [sectormap])
+    else:
+        VXDTF = setup_vxdtf('caTracks', [sectormap])
 
 # Track finding DQM
 vxdtfdqm = register_module('VXDTFDQM')
@@ -162,9 +142,9 @@ tfdqm.param('GFTrackCandidatesColName', 'caTracks')
 # GBL fitting -> produces binary data for alignment
 gbl = register_module('GBLfit')
 gbl.param('GFTrackCandidatesColName', 'caTracks')
-# This removes cut on hit Chi squared. Use it if geometry is heavily misaligned
-# Default value is 50.    ... use this once first alignment step is finished
-gbl.param('chi2Cut', 0.)
+# Suppose first alignment step is finished, apply chi squared cut
+# Default value is 50. (used also in MP2)
+gbl.param('chi2Cut', 50.0)
 gbl.param('FilterId', 'Kalman')
 gbl.param('internalIterations', '')
 gbl.param('milleFileName', alignment_binary)
@@ -217,20 +197,11 @@ main.add_module(histo)
 main.add_module(set_alignment)
 main.add_module(gearbox)
 main.add_module(geometry)
-main.add_module(PXDDSort)
-main.add_module(PXDClust)
-main.add_module(PXD_DQM)
-main.add_module(SVDSort)
-main.add_module(SVDClust)
-main.add_module(svd_dqm)
-main.add_module(TelClust)
-main.add_module(tel_dqm)
-main.add_module(telvxd_dqm)
-main.add_module(VXDTF)
+if redo_trackfinding:
+    main.add_module(VXDTF)
 main.add_module(vxdtfdqm)
 main.add_module(trackfitter)
 main.add_module(tfdqm)
-#
 main.add_module(gbl)
 if do_alignment:
     main.add_module(mp2)

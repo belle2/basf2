143fa794ac3b144491ac01d12676289a5a95ea88
Oliver Frost <oliver.frost@desy.de>
2014-03-06 13:07:40 +0000

    Added documentation strings to the CDCSVGPlotter.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@9736 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/cdcLocalTracking/python_code/cdcdisplay/svgdrawing/__init__.py
+++ tracking/cdcLocalTracking/python_code/cdcdisplay/svgdrawing/__init__.py
@@ -10,24 +10,38 @@ import primitives
 
 class CDCSVGPlotter:
 
+    """
+    Helper class to generated the svg image from the various tracking objects.
+    """
+
     def __init__(self, animate=False):
+        """
+        Constructor methode.
+        @param animate switch indicating if an animated SVG should be generated
+        """
+
         default_bound = primitives.Bound(top=-112, left=-112, right=112,
                 bottom=112)
         default_width = 1120
         default_height = 1120
         default_viewbox = default_bound.viewbox
 
+        # # Switch to indicating if an animated SVG should be generated
         self.animate = animate
 
-    # Create the xml.dom.minidom document object set up with the right headers
-    # The viewbox is set to a default value and is to be updated later
+        # Create the xml.dom.minidom document object set up with the right headers
+        # The viewbox is set to a default value and is to be updated later
         document = primitives.createSVGDocument(default_width, default_height,
                 default_viewbox)
+
+        # # xml.dom.minidom.Document which holds the linked representation of all svg elements to be written to file
         self.document = document
 
-    # Create helper object to convert the various dataobjects to svg
-    # primitive element objects
-    # The document serves as a factory for the xml.dom.minidom elements
+        # Create helper object to convert the various dataobjects to svg
+        # primitive element objects
+        # The document serves as a factory for the xml.dom.minidom elements
+
+        # # Helper object to translate the various tracking objects to xml.dom.minidom objects
         self.converter = converter.CDCDataobjectsConverter(document,
                 animate=animate)
 
@@ -38,17 +52,29 @@ class CDCSVGPlotter:
         self.document.documentElement.appendChild(defsElement)
 
     def clone(self):
+        """
+        Make a copy of the current status of the plotter.
+        """
+
         clonedPlotter = CDCSVGPlotter()
         clonedPlotter.document = self.document.cloneNode(deep=True)
         return clonedPlotter
 
     def append(self, obj, **kwd):
+        """
+        Take the tracking object, translate it to svg and append it to the svg document
+        """
+
         svgElement = self.converter.toSVG(obj, **kwd)
         if svgElement:
             mainElement = self.document.documentElement
             mainElement.appendChild(svgElement)
 
     def updateViewBox(self):
+        """
+        Adjust the view box to accomodate the current content of the document.
+        """
+
         clipBound = primitives.Bound.fromSVG(self.document.documentElement)
 
         if not clipBound:
@@ -62,12 +88,16 @@ class CDCSVGPlotter:
         mainElement.setAttribute('viewBox', viewbox)
 
     def toxml(self, totalPoints=1120 * 1120):
+        """
+        Generate the xml string for the current dom object representation.
+        """
+
         mainElement = self.document.documentElement
 
         viewbox = mainElement.getAttribute('viewBox')
         bound = primitives.Bound.fromViewBox(viewbox)
 
-    # set the image to proper aspect ratio
+        # set the image to proper aspect ratio
         svgHeight = round(math.sqrt(totalPoints * float(bound.height)
                           / bound.width))
         svgWidth = round(math.sqrt(totalPoints * float(bound.width)
@@ -79,6 +109,10 @@ class CDCSVGPlotter:
         return self.document.toprettyxml()
 
     def saveSVGFile(self, svgFileName='display.svg'):
+        """
+        Save the current dom object representation to disk.
+        """
+
         output = open(svgFileName, 'w')
         output.write(self.toxml())
         output.close()

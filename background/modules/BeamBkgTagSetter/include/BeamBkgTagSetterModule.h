dbc5862a9f08d5e37b99ee38196d07df0db488af
Marko Staric <marko.staric@ijs.si>
2014-03-10 21:27:01 +0000

    New module to set background tag in SimHits
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@9783 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ background/modules/BeamBkgTagSetter/include/BeamBkgTagSetterModule.h
@@ -0,0 +1,100 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Marko Staric                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef BEAMBKGTAGSETTERMODULE_H
+#define BEAMBKGTAGSETTERMODULE_H
+
+#include <framework/core/Module.h>
+#include <framework/datastore/StoreArray.h>
+#include <simulation/dataobjects/SimHitBase.h>
+#include <string>
+
+namespace Belle2 {
+
+  /**
+   * A module that sets m_backgroundTag variable in SimHits (see SimHitBase.h).
+   * @return true if at least one SimHit store array has entries.
+   */
+  class BeamBkgTagSetterModule : public Module {
+
+  public:
+
+    /**
+     * Constructor
+     */
+    BeamBkgTagSetterModule();
+
+    /**
+     * Destructor
+     */
+    virtual ~BeamBkgTagSetterModule();
+
+    /**
+     * Initialize the Module.
+     * This method is called at the beginning of data processing.
+     */
+    virtual void initialize();
+
+    /**
+     * Called when entering a new run.
+     * Set run dependent things like run header parameters, alignment, etc.
+     */
+    virtual void beginRun();
+
+    /**
+     * Event processor.
+     */
+    virtual void event();
+
+    /**
+     * End-of-run action.
+     * Save run-related stuff, such as statistics.
+     */
+    virtual void endRun();
+
+    /**
+     * Termination action.
+     * Clean-up, close files, summarize statistics, etc.
+     */
+    virtual void terminate();
+
+    /**
+     * Prints module parameters.
+     */
+    void printModuleParams() const;
+
+  private:
+
+    std::string m_backgroundType; /**< beam background type */
+    SimHitBase::BG_TAG m_backgroundTag; /**< background tag to set */
+
+    /**
+     * functions that set background tag in SimHits
+     * @param simHits a reference to DataStore SimHits
+     * @return number of tagged SimHit entries
+     */
+    template<class SIMHIT>
+    int setBackgroundTag(StoreArray<SIMHIT>& simHits) {
+      if (!simHits.isValid()) return 0;
+
+      int numEntries = simHits.getEntries();
+      for (int i = 0; i < numEntries; i++) {
+        SIMHIT* simHit = simHits[i];
+        simHit->setBackgroundTag(m_backgroundTag);
+      }
+      return numEntries;
+    }
+
+
+  };
+
+} // Belle2 namespace
+
+#endif
---
+++ background/modules/BeamBkgTagSetter/src/BeamBkgTagSetterModule.cc
@@ -0,0 +1,150 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Marko Staric                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+// Own include
+#include <background/modules/BeamBkgTagSetter/BeamBkgTagSetterModule.h>
+
+#include <framework/core/ModuleManager.h>
+
+// framework - DataStore
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/datastore/RelationArray.h>
+
+// framework aux
+#include <framework/gearbox/Unit.h>
+#include <framework/gearbox/Const.h>
+#include <framework/logging/Logger.h>
+
+// SimHits
+#include <pxd/dataobjects/PXDSimHit.h>
+#include <svd/dataobjects/SVDSimHit.h>
+#include <cdc/dataobjects/CDCSimHit.h>
+#include <top/dataobjects/TOPSimHit.h>
+#include <arich/dataobjects/ARICHSimHit.h>
+#include <ecl/dataobjects/ECLSimHit.h>
+#include <bklm/dataobjects/BKLMSimHit.h>
+#include <eklm/dataobjects/EKLMSimHit.h>
+
+// EventMetaData
+#include <framework/dataobjects/EventMetaData.h>
+
+#include <unordered_map>
+using namespace std;
+
+namespace Belle2 {
+
+  //-----------------------------------------------------------------
+  //                 Register module
+  //-----------------------------------------------------------------
+
+  REG_MODULE(BeamBkgTagSetter)
+
+  //-----------------------------------------------------------------
+  //                 Implementation
+  //-----------------------------------------------------------------
+
+  BeamBkgTagSetterModule::BeamBkgTagSetterModule() : Module(),
+    m_backgroundTag(SimHitBase::bg_none)
+
+  {
+    // set module description (e.g. insert text)
+    setDescription("Sets beam background tag variable in SimHits; returns true if at least one of the SimHit store arrays has entries. Return value can be used to discard empty events at output.");
+    setPropertyFlags(c_ParallelProcessingCertified | c_InitializeInProcess);
+
+    // Add parameters
+    addParam("backgroundType", m_backgroundType, "one of: Coulomb_LER, Coulomb_HER, RBB_LER, RBB_HER, Touschek_LER, Touschek_HER, twoPhoton", string(""));
+
+  }
+
+  BeamBkgTagSetterModule::~BeamBkgTagSetterModule()
+  {
+  }
+
+  void BeamBkgTagSetterModule::initialize()
+  {
+    std::unordered_map<std::string, SimHitBase::BG_TAG> tag;
+    tag["Coulomb_LER"] = SimHitBase::bg_Coulomb_LER;
+    tag["Coulomb_HER"] = SimHitBase::bg_Coulomb_HER;
+    tag["RBB_LER"] = SimHitBase::bg_RBB_LER;
+    tag["RBB_HER"] = SimHitBase::bg_RBB_HER;
+    tag["Touschek_LER"] = SimHitBase::bg_Touschek_LER;
+    tag["Touschek_HER"] = SimHitBase::bg_Touschek_HER;
+    tag["twoPhoton"] = SimHitBase::bg_twoPhoton;
+
+    m_backgroundTag = tag[m_backgroundType];
+
+    if (m_backgroundTag == 0) {
+      B2WARNING("Unknown beam background: " << m_backgroundType <<
+                ", will be tagged as SimHitBase::bg_other");
+      m_backgroundTag = SimHitBase::bg_other;
+    }
+
+    StoreArray<PXDSimHit>::optional();
+    StoreArray<SVDSimHit>::optional();
+    StoreArray<CDCSimHit>::optional();
+    StoreArray<TOPSimHit>::optional();
+    StoreArray<ARICHSimHit>::optional();
+    StoreArray<ECLSimHit>::optional();
+    StoreArray<BKLMSimHit>::optional();
+    StoreArray<EKLMSimHit>::optional();
+  }
+
+  void BeamBkgTagSetterModule::beginRun()
+  {
+  }
+
+  void BeamBkgTagSetterModule::event()
+  {
+    StoreArray<PXDSimHit> pxdSimHits;
+    StoreArray<SVDSimHit> svdSimHits;
+    StoreArray<CDCSimHit> cdcSimHits;
+    StoreArray<TOPSimHit> topSimHits;
+    StoreArray<ARICHSimHit> arichSimHits;
+    StoreArray<ECLSimHit> eclSimHits;
+    StoreArray<BKLMSimHit> bklmSimHits;
+    StoreArray<EKLMSimHit> eklmSimHits;
+
+    int n = setBackgroundTag(pxdSimHits);
+    n += setBackgroundTag(svdSimHits);
+    n += setBackgroundTag(cdcSimHits);
+    n += setBackgroundTag(topSimHits);
+    n += setBackgroundTag(arichSimHits);
+    n += setBackgroundTag(eclSimHits);
+    n += setBackgroundTag(bklmSimHits);
+    n += setBackgroundTag(eklmSimHits);
+
+    setReturnValue(n > 0);
+
+    StoreObjPtr<EventMetaData> evtMetaData;
+    B2INFO("Exp " << evtMetaData->getExperiment() <<
+           " Run " << evtMetaData->getRun() <<
+           " Event " << evtMetaData->getEvent() <<
+           " number of SimHits = " << n);
+
+  }
+
+
+  void BeamBkgTagSetterModule::endRun()
+  {
+  }
+
+  void BeamBkgTagSetterModule::terminate()
+  {
+  }
+
+  void BeamBkgTagSetterModule::printModuleParams() const
+  {
+  }
+
+
+} // end Belle2 namespace
+

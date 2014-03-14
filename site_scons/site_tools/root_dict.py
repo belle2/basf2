85a2eb3220b027ad12a48621d34735eecf1a601b
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2014-03-14 16:43:39 +0000

    Improved detection of header files for dictionary generation, resolves #1110
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@9842 b38c6c8f-bee8-465d-8448-9abe6915d496

--- site_scons/site_tools/root_dict.py
+++ site_scons/site_tools/root_dict.py
@@ -17,7 +17,8 @@ def linkdef_emitter(target, source, env):
     # add all header files for classes which are listed in the linkdef file
 
     # determine the right include directory
-    include_dir = os.path.dirname(str(source[0]))
+    source_dir = os.path.dirname(str(source[0]))
+    include_dir = source_dir
     if include_dir.endswith('include'):
         include_dir = os.path.dirname(include_dir)
     include_dir = os.path.join(env['INCDIR'], include_dir)
@@ -27,6 +28,12 @@ def linkdef_emitter(target, source, env):
     contents = source[0].get_text_contents()
     for entry in linkdef_class_re.findall(contents):
         include_base = entry[1].split('<')[0] + '.h'
+        header_file = os.path.join(source_dir, include_base)
+        if not os.path.isfile(header_file):
+            header_file = os.path.join(os.environ.get('BELLE2_RELEASE_DIR', ''
+                                       ), header_file)
+        if not os.path.isfile(header_file):
+            include_base = entry[0].split(':')[0] + '.h'
         include_file = os.path.join(include_dir, include_base)
         if not include_file in sources:
             sources.append(include_file)

f1e8791c32157002fe551ea572fd2511bc2075e2
Viktor Trusov <viktor.trusov@kit.edu>
2014-03-14 13:21:46 +0000

    Changed m_rc in CDCLegendreFastHough which defines maximal radius of curlers
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@9828 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/cdcLegendreTracking/include/CDCLegendreFastHough.h
+++ tracking/cdcLegendreTracking/include/CDCLegendreFastHough.h
@@ -49,7 +49,7 @@ namespace Belle2 {
     int m_maxLevel; /**< Maximum Level of FastHough Algorithm*/
 
     static constexpr double m_PI = 3.1415926535897932384626433832795; /**< pi is exactly three*/
-    static constexpr double m_rc = 0.0176991150442477874; /**< threshold of r, which defines curlers*/
+    static constexpr double m_rc = 0.01667; /**< threshold of r, which defines curlers*/
     int m_nbinsTheta; /**< Number of bins in theta, derived from m_maxLevel*/
     double m_rMin; /**< Minimum in r direction, initialized in initializer list of the module*/
     double m_rMax; /**< Maximum in r direction, initialized in initializer list of the module*/

858139bc1f8972988d8a8305bd28435bd874f66b
Jakob Lettenbichler <jkl@jodoschka.com>
2014-03-25 12:27:39 +0000

    current state of spacePointCreator for the VXDTF
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@9928 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/modules/spacePointCreator/include/SpacePointCreatorModule.h
@@ -0,0 +1,36 @@
+#pragma once
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2013  Belle II Collaboration                              *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Christian Pulvermacher                                   *
+ *                                                                        *
+ **************************************************************************/
+
+#include <framework/core/Module.h>
+#include <tracking/spacePointCreation/SpacePoint.h>
+
+namespace Belle2 {
+  /**
+   * Imports Clusters of the silicon detectors and converts them to spacePoints.
+   *
+   */
+  class SpacePointCreatorModule : public Module {
+
+  public:
+
+    /** Constructor */
+    SpacePointCreatorModule();
+
+    /** Init the module */
+    virtual void initialize();
+    /** Show progress */
+    virtual void event();
+    /** Don't break the terminal */
+    virtual void terminate();
+
+  protected:
+
+  };
+} // end namespace Belle2
---
+++ tracking/modules/spacePointCreator/src/SpacePointCreatorModule.cc
@@ -0,0 +1,46 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2013  Belle II Collaboration                              *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Christian Pulvermacher                                   *
+ *                                                                        *
+ **************************************************************************/
+
+#include <tracking/modules/spacePointCreator/SpacePointCreatorModule.h>
+
+#include <framework/datastore/StoreArray.h>
+
+
+using namespace std;
+using namespace Belle2;
+
+
+REG_MODULE(SpacePointCreator)
+
+SpacePointCreatorModule::SpacePointCreatorModule() : Module()
+{
+  setDescription("Imports Clusters of the silicon detectors and converts them to spacePoints.");
+}
+
+void SpacePointCreatorModule::initialize()
+{
+  StoreArray<PXDCluster>::optional();
+  StoreArray<SpacePoint>::registerPersistent();
+}
+
+void SpacePointCreatorModule::event()
+{
+  StoreArray<PXDCluster> pxdClusters;
+  StoreArray<SpacePoint> spacePoints;
+  spacePoints.create();
+
+  for (unsigned int i = 0; i < uint(pxdClusters.getEntries()); ++i) {
+    spacePoints.appendNew((*pxdClusters[i]), i);
+  }
+}
+
+void SpacePointCreatorModule::terminate()
+{
+  cerr << "\n";
+}

b72a02f0b69412a737c7eba436f6ecc78ec3ad71
Jakob Lettenbichler <jkl@jodoschka.com>
2014-03-27 23:33:21 +0000

    replaced CollectorTestModule by simplistic dummy to show that the current basf2 -m problem can not be caused by the Module - where to search next?
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@9999 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/modules/trackFinderVXDTests/include/CollectorTestModule.h
+++ tracking/modules/trackFinderVXDTests/include/CollectorTestModule.h
@@ -1,105 +1,28 @@
-/**************************************************************************
- * BASF2 (Belle Analysis Framework 2)                                     *
- * Copyright(C) 2011 - Belle II Collaboration                             *
- *                                                                        *
- * Author: The Belle II Collaboration                                     *
- * Contributors: Stefan Ferstl                                            *
- *                                                                        *
- * This software is provided "as is" without any warranty.                *
- **************************************************************************/
-
 #pragma once
 
 #include <framework/core/Module.h>
-#include <tracking/dataobjects/CollectorTFInfo.h>
-//#include <gtest/gtest.h>
-
-// #include <fstream>
-//#include <string>
-
 
 namespace Belle2 {
-
-  /** CollectorTestModule
-   *
-   * simply tests the Collector (CollectorTFInfo and its ingredients) of the TrackFinder
+  /**
+   * CollectorTestModules
    *
    */
   class CollectorTestModule : public Module {
 
   public:
 
-    /**
-     * Constructor of the module.
-     */
+    /** Constructor */
     CollectorTestModule();
 
-    /** Destructor of the module. */
-    virtual ~CollectorTestModule();
-
-    /** Initializes the Module.
-     */
+    /** Init the module */
     virtual void initialize();
-
-    /**
-     * Prints a header for each new run.
-     */
-    virtual void beginRun();
-
-    /** Prints the full information about the event, run and experiment number. */
+    /** Show progress */
     virtual void event();
-
-    /**
-     * Prints a footer for each run which ended.
-     */
-    virtual void endRun();
-
-    /**
-     * Termination action.
-     */
+    /** Don't break the terminal */
     virtual void terminate();
 
-
-    void import_sectors_loop();
-    void import_sectors_standard();
-
-    void import_clusters_loop();
-    void import_clusters_standard();
-
-    void import_hit_loop();
-    void import_hit_standard();
-
-    void import_cell_loop();
-    void import_cell_standard();
-
-    void import_tfc_loop();
-    void import_tfc_standard();
-
-    void getAllCells();
-    void getAllHits();
-    void getAllClusters();
-    void getAllTC();
-    void getAllSectors();
-
-    void testSilentKill();
-    void testOverlapped();
-    void testAllInformationLoop();
-    void testAllInformationStandard();
-
-
   protected:
-//    CollectorTFInfo m_collector = CollectorTFInfo();
-    CollectorTFInfo m_collector; /**< carries complete information about hits, cells and the whole rest */
-    // Pass Ids
-    std::vector<int> pass_sector_ids; /**< carries the IDs of the passes for fast access */
-    int pass_sector_id_single; /**< @ Stefan: please explain ;) */
-
-    int m_eventCounter; /**< knows current event number */
-//   int m_stepSize; /**< Informes the user that  event: (eventCounter-modulo(stepSize)) is currently executed */
-    int m_pxdClusterCounter; /**< counts total number of pxd clusters occured */
-    int m_svdClusterCounter; /**< counts total number of pxd clusters occured */
-
-  private:
 
   };
-}
+} // end namespace Belle2
+

f236ae32076725b3068540e3e280d1978222ac9d
Vishal Bhardwaj <vishstar@gmail.com>
2014-04-04 09:36:52 +0000

    Redesign the ECLCluster format bool m_isTrack (to be remove in future) use isNeutral()
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10105 b38c6c8f-bee8-465d-8448-9abe6915d496

--- mdst/dataobjects/include/ECLCluster.h
+++ mdst/dataobjects/include/ECLCluster.h
@@ -35,37 +35,36 @@ namespace Belle2 {
      *default constructor : all values are set to 0
      */
     ECLCluster() {
-
+      m_isTrack = false; /**< To store relation to tracks [true] or not [false] (for timebeing, Pleaseuse function  isNeutral() [as it is for future] */
       m_Energy = 0;   /**< Corrected energy (GeV) */
       m_EnedepSum = 0; /**< Uncorrected Energy Deposited in a shower (GeV) */
-      m_Px = 0;       /**< Px Momentum of Gamma (GeV/c) */
-      m_Py = 0;       /**< Py Momentum of Gamma (GeV/c) */
-      m_Pz = 0;       /**< Pz Momentum of Gamma (GeV/c) */
-
+      m_Theta = 0;     /**< Theta (in radian) */
+      m_Phi = 0;       /**< Phi (in radian) */
+      m_R = 0;        /**<  R (in cm) */
       m_E9oE25 = 0;   /**< E9/E25: photon-like & isolation cut */
-
 
       // TODO
       m_Timing = 0;   /**< Timing information (at present study going on)*/
       m_ErrorTiming = 0;  /**< Timing Error (at present study going on)*/
       m_HighestE = 0; /**< Highest Energy stored in a Crystal in a shower (GeV) */
       m_LAT = 0; /**< Lateral distribution parameter (Not available now) */
-
       m_NofCrystals = 0; /**< Number of Crystals in a shower (Not available now) */
-      m_Pi0Likelihood = 0; /**< Pi0 Likelihood (greater than 200 MeV) (Not available now)*/
-      m_EtaLikelihood = 0; /**< Eta Likelihood (greater than 200 MeV) (Not available now) */
-      m_NofTracks = 0; /**< Number of Tracks related to this shower (being studied)*/
-      m_deltaL = 0; /**< arXiv:0711.1593 (Not available now)*/
-      m_beta = 0; /**< arXiv:0711.1593 (Not available now)*/
       m_CrystHealth = 0; /**< Crystal Health (Not available now)
           -100 : healthy
           10 : Bad
           20 : Broken
           30 : Dead  */
-      m_HimomentumPi0 = 0; /**< Likelihood of being High momentum Pi0 (Not available now)
-            0 to 1 : Can be high momentum pi0
+      m_MergedPi0 = 0; /**< Likelihood of being Merged Pi0
+            (Not available now)
+            0 to 1 : Can be a Merged Pi0
          */
 
+      m_Error[0] = 0;
+      m_Error[1] = 0;
+      m_Error[2] = 0;
+      m_Error[3] = 0;
+      m_Error[4] = 0;
+      m_Error[5] = 0;
 
     }
 
@@ -77,17 +76,17 @@ namespace Belle2 {
      */
     void setEnergy(float Energy) {m_Energy = Energy;}
 
-    /*! Set X component of Momentum (GeV/c)
+    /*! Set Theta of Shower (radian)
      */
-    void setPx(float Px) { m_Px = Px;}
+    void setTheta(float Theta) { m_Theta = Theta;}
 
-    /*! Set Y component of Momentum (GeV/c)
+    /*! Set Phi of Shower (radian)
      */
-    void setPy(float Py) { m_Py = Py;}
+    void setPhi(float Phi) { m_Phi = Phi;}
 
-    /*! Set Z component of Momentum (GeV/c)
+    /*! Set R (in cm)
      */
-    void setPz(float Pz) { m_Pz = Pz;}
+    void setR(float R) { m_R = R; }
 
     /*! Set Uncorrect Energy deposited (GeV)
      */
@@ -105,18 +104,6 @@ namespace Belle2 {
      */
     void setE9oE25(float E9oE25) { m_E9oE25 = E9oE25; }
 
-    /*! Set  Error Matrix (7x7)  for momentum TMatrixFSym class
-     */
-    void setErrorMatrix(TMatrixFSym& errorMatrix) {
-      m_errorMatrix.ResizeTo(7, 7);
-      for (int i = 0; i < 7; i++) {
-        for (int j = 0; j <= i ; j++) {
-          m_errorMatrix[i][j] = errorMatrix[i][j];
-        }
-      }
-    }
-
-
 
 
     /*! Set Highest Energy stored in a Crystal in a shower (GeV)
@@ -132,35 +119,6 @@ namespace Belle2 {
      */
     void setNofCrystals(int NofCrystals) {m_NofCrystals = NofCrystals;}
 
-    /*! Set Pi0 Likelihood
-     */
-    void setPi0Likelihood(float Pi0Likelihood) {
-      m_Pi0Likelihood = Pi0Likelihood;
-    }
-
-    /*! Set Eta Likelihood
-     */
-    void setEtaLikelihood(float EtaLikelihood) {
-      m_EtaLikelihood = EtaLikelihood;
-    }
-
-    /*! Set Number of charged tracks identified with corresponsing shower
-     */
-    void setNofTracks(int NofTracks) {
-      m_NofTracks = NofTracks;
-    }
-
-    /*! Set Delta L for charged track
-     */
-    void setdeltaL(float deltaL) {
-      m_deltaL = deltaL;
-    }
-
-    /*! Set beta  for charged tracks
-     */
-    void setbeta(float beta) {
-      m_beta = beta;
-    }
 
     /*! Set Crystal health (Not now)
      */
@@ -170,12 +128,28 @@ namespace Belle2 {
 
     /*! Set high momentum pi0 likelihood
      */
-    void setHimomentumPi0(float HimomentumPi0) {
-      m_HimomentumPi0 = HimomentumPi0;
+    void setMergedPi0(float MergedPi0) {
+      m_MergedPi0 = MergedPi0;
     }
 
 
+    /*! Set  Error Array(3x3)  for
+      [0]->Error on Energy
+      [2]->Error on Phi
+      [5]->Error on Theta
+    */
+    void setError(float ErrorArray[6]) {
+      for (int i = 0; i < 6; ++i) {
+        m_Error[i] = ErrorArray[i];
+      }
+    }
 
+
+    /*! Set m_isTrack true if the cluster matches with cluster
+     */
+    void setisTrack(bool istrack) {
+      m_isTrack = istrack;
+    }
 
 
     // Get the value
@@ -185,17 +159,29 @@ namespace Belle2 {
      */
     float getEnergy() const {return m_Energy;}
 
-    /*! Return X component of Momentum (GeV/c)
+    /*! Return Theta of Shower (radian)
      */
-    float getPx() const {return m_Px;}
+    float getTheta() const {return m_Theta;}
 
-    /*! Return Y component of Momentum (GeV/c)
+    /*! Return Phi of Shower (radian)
      */
-    float getPy() const { return m_Py;}
+    float getPhi() const { return m_Phi;}
+    /*! Return R
+     */
+    float getR() const { return m_R ; }
 
-    /*! Return Z component of Momentum (GeV/c)
+    /*! Return Error on Energy
      */
-    float getPz() const { return m_Pz;}
+    float getErrorEnergy() const {return m_Error[0];}
+
+    /*! Return Error on Theta of Shower
+     */
+    float getErrorTheta() const {return m_Error[5];}
+
+    /*! Return Error on Phi of Shower
+     */
+    float getErrorPhi() const { return m_Error[2];}
+
 
     /*! Return Uncorrect Energy deposited (GeV)
      */
@@ -226,26 +212,6 @@ namespace Belle2 {
      */
     int getNofCrystals() const {return m_NofCrystals;}
 
-    /*! Return Pi0 Likelihood  of a shower
-     */
-    float getPi0Likelihood() const { return m_Pi0Likelihood; }
-
-    /*! Return Eta Likelihood  of a shower
-     */
-    float getEtaLikelihood() const { return m_EtaLikelihood;}
-
-
-    /*! Return Number of charged tracks identified with corresponsing shower
-     */
-    int getNofTracks() const { return m_NofTracks;}
-
-    /*! Return Delta L for charged track
-     */
-    float getdeltaL() const { return m_deltaL;}
-
-    /*! Set Return  for charged tracks
-     */
-    float getbeta() const { return m_beta;}
 
     /*! Return Crystal health (Not now)
      */
@@ -254,66 +220,231 @@ namespace Belle2 {
 
     /*! Return high momentum pi0 likelihood
      */
-    float getHimomentumPi0() const { return m_HimomentumPi0;}
+    float getMergedPi0() const { return m_MergedPi0;}
 
     // The method to get return TMatrixFSym  7 Momentum Error Matrix
-    /*!   TMatrixFSym  7 Momentum Error Matrix
-     * @return Error Matrix from E, theta, phi and their errors
-     */
-    TMatrixFSym getErrorMatrix() const { return m_errorMatrix;}
 
+
+
+    //................
+    // Return here Px, Py and Pz
+
+    /*! Return Px (GeV/c)
+     */
+    float getPx() {
+      return float(m_Energy * sin(m_Theta) * cos(m_Phi));
+    }
+
+    /*! Return Py (GeV/c)
+     */
+    float getPy() {
+      return float(m_Energy * sin(m_Theta) * sin(m_Phi));
+    }
+
+    /*! Return Pz (GeV/c)
+     */
+    float getPz() {
+      return float(m_Energy * cos(m_Theta));
+    }
+
+    ///.......
 
 
     /*! Return TVector3 momentum (Px,Py,Pz)
      */
     TVector3 getMomentum() const {
-      return TVector3(m_Px, m_Py, m_Pz);
+
+      return TVector3(float(m_Energy * sin(m_Theta) * cos(m_Phi)), float(m_Energy * sin(m_Theta) * sin(m_Phi)), float(m_Energy * cos(m_Theta)));
     }
+
 
 
     /*! Return 4Vector  (Px,Py,Pz,E)
      */
     TLorentzVector get4Vector() const {
-      return TLorentzVector(m_Px, m_Py, m_Pz, m_Energy);
+      return TLorentzVector(float(m_Energy * sin(m_Theta) * cos(m_Phi)), float(m_Energy * sin(m_Theta) * sin(m_Phi)), float(m_Energy * cos(m_Theta)), m_Energy);
     }
 
-    /*! Return TMatrixFsym 4x4 error matrix for px,py,pz,E
+
+
+    /*! Return TVector3 on cluster position /Shower center (x,y,z)
      */
+    TVector3 getclusterPosition() const {
+      float cluster_x =  m_R * sin(m_Phi) * cos(m_Theta);
+      float cluster_y =  m_R * sin(m_Phi) * sin(m_Theta);
+      float cluster_z =  m_R * cos(m_Phi);
+      return TVector3(cluster_x, cluster_y, cluster_z);
+    }
+
+    /*! Return TVector3 on  position on gamma's production
+      By default the position of gamma's production is (0,0,0)
+    */
+    TVector3 getPosition() const {
+      float x, y, z;
+      x = y = z = 0.0;
+      return TVector3(x, y, z);
+    }
+
+
+    //.................
+    // For filling error matrix on Px,Py and Pz
+
+    /*! Return TMatrixFsym 4x4  error matrix
+    */
 
     TMatrixFSym getError4x4() const {
-      TMatrixFSym MomentumError;
-      MomentumError.ResizeTo(4, 4);
+      TMatrixFSym errorecl(3);
+      errorecl[0][0] = m_Error[0];  //Energy
+      errorecl[1][0] = m_Error[1];
+      errorecl[1][1] = m_Error[2]; // Phi
+      errorecl[2][0] = m_Error[3];
+      errorecl[2][1] = m_Error[4];
+      errorecl[2][2] = m_Error[5]; // Theta
+
+      TMatrixF  jacobian(4, 3);
+      float  cosPhi = cos(m_Phi);
+      float  sinPhi = sin(m_Phi);
+      float  cosTheta = cos(m_Theta);
+      float  sinTheta = sin(m_Theta);
+      float   E = m_Energy;
+
+      jacobian[ 0 ][ 0 ] =            cosPhi * sinTheta;
+      jacobian[ 0 ][ 1 ] =  -1.0 * E * sinPhi * sinTheta;
+      jacobian[ 0 ][ 2 ] =        E * cosPhi * cosTheta;
+      jacobian[ 1 ][ 0 ] =            sinPhi * sinTheta;
+      jacobian[ 1 ][ 1 ] =        E * cosPhi * sinTheta;
+      jacobian[ 1 ][ 2 ] =        E * sinPhi * cosTheta;
+      jacobian[ 2 ][ 0 ] =                     cosTheta;
+      jacobian[ 2 ][ 1 ] =           0.0;
+      jacobian[ 2 ][ 2 ] =  -1.0 * E          * sinTheta;
+      jacobian[ 3 ][ 0 ] =           1.0;
+      jacobian[ 3 ][ 1 ] =           0.0;
+      jacobian[ 3 ][ 2 ] =           0.0;
+      TMatrixFSym errCart(4);
+      errCart = errorecl.Similarity(jacobian);
+      return errCart;
+    }
+    //__________________________
+
+
+    /*! Return TMatrixFsym 7x7  error matrix
+     */
+    TMatrixFSym getError7x7() const {
+      TMatrixFSym errorecl(3);
+      errorecl[0][0] = m_Error[0]; //Energy
+      errorecl[1][0] = m_Error[1];
+      errorecl[1][1] = m_Error[2]; // Phi
+      errorecl[2][0] = m_Error[3];
+      errorecl[2][1] = m_Error[4];
+      errorecl[2][2] = m_Error[5]; // Theta
+
+      TMatrixFSym ErrorMatrix(7);
+      TMatrixF  jacobian(4, 3);
+      float  cosPhi = cos(m_Phi);
+      float  sinPhi = sin(m_Phi);
+      float  cosTheta = cos(m_Theta);
+      float  sinTheta = sin(m_Theta);
+      float   E = m_Energy;
+
+      jacobian[ 0 ][ 0 ] =            cosPhi * sinTheta;
+      jacobian[ 0 ][ 1 ] =  -1.0 * E * sinPhi * sinTheta;
+      jacobian[ 0 ][ 2 ] =        E * cosPhi * cosTheta;
+      jacobian[ 1 ][ 0 ] =            sinPhi * sinTheta;
+      jacobian[ 1 ][ 1 ] =        E * cosPhi * sinTheta;
+      jacobian[ 1 ][ 2 ] =        E * sinPhi * cosTheta;
+      jacobian[ 2 ][ 0 ] =                     cosTheta;
+      jacobian[ 2 ][ 1 ] =           0.0;
+      jacobian[ 2 ][ 2 ] =  -1.0 * E          * sinTheta;
+      jacobian[ 3 ][ 0 ] =           1.0;
+      jacobian[ 3 ][ 1 ] =           0.0;
+      jacobian[ 3 ][ 2 ] =           0.0;
+      TMatrixFSym errCart(4);
+      errCart = errorecl.Similarity(jacobian);
       for (int i = 0; i < 4; i++) {
         for (int j = 0; j <= i ; j++) {
-          MomentumError[i][j] = m_errorMatrix[i][j];
+          ErrorMatrix[i][j] = errCart[i][j];
         }
       }
-      return MomentumError;
+      for (int i = 4; i <= 6; ++i) {
+        ErrorMatrix[i][i] = 1.0; // 1.0*1.0 cm^2 (default treatment as Belle ?)
+      }
+      return ErrorMatrix;
     }
 
 
-    TMatrixFSym getError7x7() const {
-      TMatrixFSym Errormatrix;
-      Errormatrix.ResizeTo(7, 7);
-      for (int i = 0; i < 7; i++) {
-        for (int j = 0; j <= i ; j++) {
-          Errormatrix[i][j] = m_errorMatrix[i][j];
-        }
-      }
-      return Errormatrix;
+
+
+
+
+    /*! Return TMatrixFsym 3x3 error matrix for E, Phi and Theta
+     */
+
+    TMatrixFSym getError3x3() const {
+      TMatrixFSym errorecl(3);
+      errorecl[0][0] = m_Error[0]; //Energy
+      errorecl[1][0] = m_Error[1];
+      errorecl[1][1] = m_Error[2]; // Phi
+      errorecl[2][0] = m_Error[3];
+      errorecl[2][1] = m_Error[4];
+      errorecl[2][2] = m_Error[5]; // Theta
+      return errorecl;
     }
+
+    /*! Return m_isTrack true if the cluster matches with cluster, otherwise
+      false (for time being). Please use isNeutral function (as it will be kept
+      for future)
+     */
+    bool getisTrack() const {
+      return m_isTrack;
+    }
+
+    /*! Return true if cluster has no match with cluster, otherwise
+      return false if cluster has match with track.
+     */
+    bool isNeutral() const {
+      if (m_isTrack) {
+        return false;
+      } else { return true; }
+    }
+
+
+    //..... For FUTURE (to DO)
+
+    /*! Return pi0Likelihood for a shower (for future, Not available now)
+     */
+    float getpi0Likelihood() {
+      return 0.5;
+    }
+    /*! Return etaLikelihood for a shower (for future, Not available now)
+     */
+    float getetaLikelihood() {
+      return 0.5;
+    }
+
+    /*! Return deltaL for shower shape (for future, Not available now)
+     */
+    float getdeltaL() {
+      return 0;
+    }
+    /*! Return beta for shower shape (for future, Not available now)
+     */
+    float getbeta() {
+      return 0;
+    }
+
 
 
 
   private:
 
-    TMatrixFSym m_errorMatrix;   /**< Error Matrix 7x7 */
-
-
+    float m_Error[6]; /**< Error Array for Energy->[0], Phi->[2], Theta->[5] */
+    bool m_isTrack;  /**< To store relation to tracks or not (for timebeing, Pleaseuse function  isNeutral() [as it is for future] */
     float  m_Energy;     /**< Corrected energy (GeV) */
-    float  m_Px;         /**< Px Momentum of Gamma (GeV/c) */
-    float  m_Py;         /**< Py Momentum of Gamma (GeV/c) */
-    float  m_Pz;         /**< Pz Momentum of Gamma (GeV/c) */
+    float  m_Theta;         /**< Theta of Shower (radian) */
+    float  m_Phi;         /**< Phi of Shower (radian)  */
+
+
+    float  m_R;         /**< R (cm) */
     float  m_EnedepSum;  /**< Uncorrected  Energy Deposited (GeV) */
     float  m_Timing;     /**< Timing information (study going on) */
     float  m_ErrorTiming;    /**< Timing Error (NA) */
@@ -322,17 +453,12 @@ namespace Belle2 {
     //TODO
     float m_LAT;  /**< Lateral distribution parameter (Not available now) */
     int   m_NofCrystals; /**< Number of Crystals in a shower (Not available now) */
-    float m_Pi0Likelihood; /**< Pi0 Likelihood (greater than 200 MeV) (Not available now)*/
-    float m_EtaLikelihood; /**< Eta Likelihood (greater than 200 MeV) (Not available now) */
-    int   m_NofTracks; /**< Number of Tracks related to this shower (being studied)*/
-    float m_deltaL; /**< DeltaL charged track arXiv:0711.1593 (Not available now)*/
-    float m_beta; /**< Beta charged track arXiv:0711.1593 (Not available now)*/
     int   m_CrystHealth;/**< Crystal Health (Not available now)
           -100 : healthy
           10 : Bad
           20 : Broken
           30 : Dead  */
-    float m_HimomentumPi0; /**< Likelihood of being High momentum Pi0 (Not available now) */
+    float m_MergedPi0; /**< Likelihood of being Merged Pi0 (Not available now) */
 
 
 

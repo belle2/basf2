e1e1e90b12971e65c98a78a6a7eaa9dcd33f4a52
Jakob Lettenbichler <jkl@jodoschka.com>
2014-04-08 16:28:01 +0000

    expanded SpacePoint- and SectorTools-functionality
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10165 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingVXD/sectorMapTools/include/SectorTools.h
+++ tracking/trackFindingVXD/sectorMapTools/include/SectorTools.h
@@ -16,6 +16,7 @@
 // includes - stl:
 #include <vector>
 #include <utility> // std::pair
+#include <limits>       // std::numeric_limits
 
 // includes - tf-related stuff:
 // includes - general fw stuff:
@@ -25,30 +26,116 @@
 namespace Belle2 {
 
 
-  /**SectorTools - contains often needed transformer-functions.
-   * -
+  /** SectorTools - contains often needed transformer-functions.
    **/
   class SectorTools {
   public:
     /** .first is uValue, .second is vValue (both defined between 0-1) */
-    typedef std::pair<float, float> RelCoords;
+    typedef std::pair<float, float> NormCoords;
+
+
 
     /** shortcut for unsigned int short */
     typedef unsigned short uShort;
 
-    BELLE2_DEFINE_EXCEPTION(Out_of_bounds, "The relative coordinates (sectorConfig or coordinates themselves) are not defined between 0-1!");
+
+
+    /** shortcut for unsigned int */
+    typedef unsigned int uInt;
+
+
+
+    /** exception shall be thrown if value is not between 0-1 and therefore not normalized */
+    BELLE2_DEFINE_EXCEPTION(Out_of_bounds, "The normalized coordinates (sectorConfig or coordinates themselves) are not defined between 0-1!");
+
+
 
     /** calculates the sector ID for current hit.
-     * for given configuration for u and v coordinates, it takes a hit in relative coordinates (sensor-independent) and calculates the sectorID */
-    static uShort calcSecID(const std::vector<float>& uConfig, const std::vector<float>& vConfig, RelCoords coords) {
-      return 0; /*dummy*/
+     * for given configuration for u and v coordinates, it takes a hit in normalized coordinates (sensor-independent within sensor-boundaries) and calculates the sectorID.
+     * if no secID is found, it returns unsigned short :: max
+     */
+    static uShort calcSecID(const std::vector<float>& uConfig, const std::vector<float>& vConfig, NormCoords coords) {
+      // safety checks
+      throwBoundary(coords.first);
+      throwBoundary(coords.second);
+      throwBoundaryVector(uConfig);
+      throwBoundaryVector(vConfig);
+
+      // needed for determining the sectorID of this sensor
+      uShort uCount = 0,
+             vCount = 0,
+             secID = std::numeric_limits<uShort>::max();
+      bool foundSectorU = false,
+           foundSectorV = false;
+      unsigned int uMax = uConfig.size() - 1,
+                   vMax = vConfig.size() - 1;
+
+      // searching for sector-slot in u-direction:
+      for (; uCount < uMax; ++uCount) {
+        if (coords.first > uConfig[uCount] and coords.first < uConfig[uCount + 1]) {
+          foundSectorU = true;
+          break;
+        }
+      }
+
+      // searching for sector-slot in v-direction:
+      for (; vCount < uMax; ++vCount) {
+        if (coords.second > vConfig[vCount] and coords.second < vConfig[vCount + 1]) {
+          foundSectorU = true;
+          break;
+        }
+      }
+
+      // on a sensor represented as a matrix, the uCount carries the lineID and the vCount the rowID
+      if (foundSectorU == true and foundSectorV == true) { secID = vCount + 1 + uCount * vMax; }
+
+      return secID;
     }
 
-    /** for a given point  */
-    static RelCoords calcRelativeSectorPoint(const std::vector<float>& uConfig, const std::vector<float>& vConfig, uShort secID, RelCoords coords) {
-      return std::make_pair(0, 0); /*dummy*/
+
+
+    /** calculates the normalized coordinates for a point in given sectorID.
+     *
+     * first parameter is a vector containing the cuts for the sectors in u-direction (in consecutive and increasing order, have to be normalized).
+     * second parameter is a vector containing the cuts for the sectors in v-direction (in consecutive and increasing order, have to be normalized).
+     * third parameter is the sectorID in which the normalized point is calculated
+     * fourth parameter is the point for which the normalized sector point shall be calculated.
+     *  WARNING: this value is normalized not within the boundaries of the sensor but within the boundaries of the sector!
+     *   Example: if coords are (0.5,0.5), the coordinate of the middle of the sector is calculated, not the coordinate of the middle of the sensor.
+     *   Example2: the sector-edges can be retrieved by using e.g.: (0,0), (0,1), (1,0) and (1,1)
+     */
+    static NormCoords calcNormalizedSectorPoint(const std::vector<float>& uConfig, const std::vector<float>& vConfig, uShort secID, NormCoords coords) {
+      // safety checks
+      throwBoundary(coords.first);
+      throwBoundary(coords.second);
+      throwBoundaryVector(uConfig);
+      throwBoundaryVector(vConfig);
+
+      uInt vMax = vConfig.size() - 1 ;
+//       // reconstructing indices for the u and v cuts by using the sectorID:
+      uInt vIndex = (secID - 1) % vMax ;
+      uInt uIndex = (secID - 1 - vIndex) / vMax;
+
+      float uPosInSector = (uConfig[uIndex + 1] - uConfig[uIndex]) * coords.first;
+      throwBoundary(uPosInSector);
+      float vPosInSector = (vConfig[vIndex + 1] - vConfig[vIndex]) * coords.second;
+      throwBoundary(vPosInSector);
+
+      return std::make_pair(uConfig[uIndex] + uPosInSector, vConfig[vIndex] + vPosInSector);
     }
+
+
 
   protected:
+    /** returns true if value is between 0-1, false if not */
+    static void throwBoundary(float value) {
+      if (value > 1 or value < 0) throw Out_of_bounds;
+    }
+
+
+    /** returns true if value is between 0-1, false if not */
+    static void throwBoundaryVector(std::vector<float>& vec) {
+      for (float value : vec) throwBoundary(value);
+    }
   };
 } //Belle2 namespace

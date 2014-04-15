7c1d1a2e15b24648a5776391f0829b1f9b20a4fa
Satoru Yamada <satoru.yamada@kek.jp>
2014-04-15 08:26:03 +0000

    Add document in CRCCalculator.h
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10276 b38c6c8f-bee8-465d-8448-9abe6915d496

--- rawdata/include/CRCCalculator.h
+++ rawdata/include/CRCCalculator.h
@@ -4,6 +4,11 @@
 #include <cstddef>
 
 namespace Belle2 {
+  /**
+   * Function to calculate CRC16
+   * Belle2link(HSLB/FEE) trailers.are supposed to contain CRC information.
+   * This function will be used to check them in DAQ system.
+   */
   unsigned short CalcCRC16(unsigned short crc16, const char buf[], size_t size);
 }
 #endif

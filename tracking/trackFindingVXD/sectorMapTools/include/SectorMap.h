b3330dd79f8d71cbc79669aa6bca812d271987da
Jakob Lettenbichler <jkl@jodoschka.com>
2014-05-05 16:05:31 +0000

    some new features and bugfixes for sectorMap-related stuff
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10446 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingVXD/sectorMapTools/include/SectorMap.h
+++ tracking/trackFindingVXD/sectorMapTools/include/SectorMap.h
@@ -12,9 +12,16 @@
 
 // includes - rootStuff:
 #include <TObject.h>
+#include <TVector3.h>
 
 // includes - stl:
+#include <string>
+#include <vector>
+#include <utility> // std::pair
+
 // includes - tf-related stuff:
+// #include <tracking/dataobjects/TrackFinderVXDTypedefs.h>
+
 // includes - general fw stuff:
 
 
@@ -27,11 +34,57 @@ namespace Belle2 {
    **/
   class SectorMap : public TObject {
   public:
+    typedef std::pair<unsigned int, double> SectorDistance; /**< stores distance to origin (.second) for sector (.first) */
+    typedef std::vector< SectorDistance > SectorDistancesMap; /**< stores vector of SectorDistanceInfo */
 
     /** constructor */
     SectorMap() {}
 
   protected:
+
+    /** Name of the sectorMap */
+    std::string m_nameOfSecMap;
+
+
+    /** Name of the detectorType (PXD, SVD, VXD) */
+    std::string m_detectorType;
+
+
+    /** allows defining the the config of the sectors in U direction value is valid for each sensor of chosen detector setup, minimum 2 values between 0.0 and 1.0 */
+    std::vector<double> m_sectorConfigU;
+
+
+    /** allows defining the the config of the sectors in V direction value is valid for each sensor of chosen detector setup, minimum 2 values between 0.0 and 1.0 */
+    std::vector<double> m_sectorConfigV;
+
+
+    /** defines the position of the assumed primary vertex */
+    TVector3 m_origin;
+
+
+    /** strength of magnetic field in Tesla, standard is 1.5T */
+    double m_magneticFieldStrength;
+
+
+    /** this variable is reserved for extra info which shall be stored in the container, e.g. date of production or other useful info for the user(it shall be formatted before storing it), this info will be displayed by the VXDTF on Info-level */
+    std::string m_additionalInfo;
+
+
+    /** stores the secID in .first and the value for the distances in .second */
+    SectorDistancesMap m_dist2OriginMap;
+
+
+    /** defines minimal accepted distance for sectors to the origin */
+    double m_minDistance2origin;
+
+
+    /** defines maximum accepted distance for sectors to the origin */
+    double m_maxDistance2origin;
+
+
+    /** if true, filterBadSectorCombis filters sectors by origin not by layerID */
+    bool m_sortByDistance2origin;
+
 
     ClassDef(SectorMap, 1)
   };
--- tracking/trackFindingVXD/sectorMapTools/include/SectorTools.h
+++ tracking/trackFindingVXD/sectorMapTools/include/SectorTools.h
@@ -33,7 +33,7 @@ namespace Belle2 {
 
   public:
     /** .first is uValue, .second is vValue (both defined between 0-1) */
-    typedef std::pair<float, float> NormCoords;
+    typedef std::pair<double, double> NormCoords;
 
 
 
@@ -56,10 +56,10 @@ namespace Belle2 {
      * for given configuration for u and v coordinates, it takes a hit in normalized coordinates (sensor-independent within sensor-boundaries) and calculates the sectorID.
      * if no secID is found, it returns unsigned short :: max
      */
-    static uShort calcSecID(const std::vector<float>& uConfig, const std::vector<float>& vConfig, NormCoords coords) {
+    static uShort calcSecID(const std::vector<double>& uConfig, const std::vector<double>& vConfig, NormCoords coords) {
       // safety checks
-      throwBoundary(float(coords.first));
-      throwBoundary(float(coords.second));
+      throwBoundary(coords.first);
+      throwBoundary(coords.second);
       throwBoundaryVector(uConfig);
       throwBoundaryVector(vConfig);
 
@@ -75,7 +75,7 @@ namespace Belle2 {
 
       // searching for sector-slot in u-direction:
       for (; uCount < uMax; ++uCount) {
-        if (coords.first > uConfig.at(uCount) and coords.first < uConfig.at(uCount + 1)) {
+        if (coords.first >= uConfig.at(uCount) and coords.first <= uConfig.at(uCount + 1)) {
           foundSectorU = true;
           break;
         }
@@ -83,7 +83,7 @@ namespace Belle2 {
 
       // searching for sector-slot in v-direction:
       for (; vCount < vMax; ++vCount) {
-        if (coords.second > vConfig.at(vCount) and coords.second < vConfig.at(vCount + 1)) {
+        if (coords.second >= vConfig.at(vCount) and coords.second <= vConfig.at(vCount + 1)) {
           foundSectorV = true;
           break;
         }
@@ -94,7 +94,8 @@ namespace Belle2 {
 
       // on a sensor represented as a matrix, the uCount carries the lineID and the vCount the rowID
       if (foundSectorU == true and foundSectorV == true) {
-        secID = vCount + 1 + uCount * vMax;
+        secID = vCount + uCount * vMax; // 0-based
+//  secID = vCount + 1 + uCount * vMax; // 1-based
       }
 //       B2INFO("secID: " << secID );
 
@@ -113,26 +114,29 @@ namespace Belle2 {
      *   Example: if coords are (0.5,0.5), the coordinate of the middle of the sector is calculated, not the coordinate of the middle of the sensor.
      *   Example2: the sector-edges can be retrieved by using e.g.: (0,0), (0,1), (1,0) and (1,1)
      */
-    static NormCoords calcNormalizedSectorPoint(const std::vector<float>& uConfig, const std::vector<float>& vConfig, uShort secID, NormCoords coords) {
+    static NormCoords calcNormalizedSectorPoint(const std::vector<double>& uConfig, const std::vector<double>& vConfig, uShort secID, NormCoords coords) {
       // safety checks
-      throwBoundary(float(coords.first));
-      throwBoundary(float(coords.second));
+      throwBoundary(coords.first);
+      throwBoundary(coords.second);
       throwBoundaryVector(uConfig);
       throwBoundaryVector(vConfig);
 
 //       B2INFO ("SECID: " << secID);
 
-      uInt vMax = vConfig.size() - 1 ;
+      uInt vMax = 0;
+      if (uInt(vConfig.size()) > 0) { vMax = vConfig.size() - 1 ; }
+
 //       // reconstructing indices for the u and v cuts by using the sectorID:
-      uInt vIndex = (secID - 1) % vMax ;
-      uInt uIndex = (secID - 1 - vIndex) / vMax;
+      uInt vIndex = (secID /*- 1*/) % vMax ;
+      uInt uIndex = (secID /*- 1*/ - vIndex) / vMax;
 
-//        B2INFO ("vIndex: " << vIndex << ", uIndex: " << uIndex);
+      B2DEBUG(100, "SECID: " << secID << ", vIndex: " << vIndex << ", uIndex: " << uIndex << ", vMax:" << vMax);
+      B2DEBUG(100, "uConfig.at(uIndex + 1): " << uConfig.at(uIndex + 1) << ", uConfig.at(uIndex): " << uConfig.at(uIndex));
+      B2DEBUG(100, "vConfig.at(vIndex + 1): " << vConfig.at(vIndex + 1) << ", vConfig.at(vIndex): " << vConfig.at(vIndex));
 
-
-      float uPosInSector = (uConfig.at(uIndex + 1) - uConfig.at(uIndex)) * coords.first;
+      double uPosInSector = (uConfig.at(uIndex + 1) - uConfig.at(uIndex)) * coords.first;
       throwBoundary(uPosInSector);
-      float vPosInSector = (vConfig.at(vIndex + 1) - vConfig.at(vIndex)) * coords.second;
+      double vPosInSector = (vConfig.at(vIndex + 1) - vConfig.at(vIndex)) * coords.second;
       throwBoundary(vPosInSector);
 
       return std::make_pair(uConfig.at(uIndex) + uPosInSector, vConfig.at(vIndex) + vPosInSector);
@@ -140,18 +144,16 @@ namespace Belle2 {
 
   protected:
     /** returns true if value is between 0-1, false if not */
-    static void throwBoundary(float value) {
+    static void throwBoundary(double value) {
       if (value > 1 or value < 0) {
-
-        // TO DO: Activate
-//   throw Out_of_bounds;
+        //throw Out_of_bounds();
       }
     }
 
 
     /** returns true if value is between 0-1, false if not */
-    static void throwBoundaryVector(const std::vector<float>& vec) {
-      for (float value : vec) throwBoundary(value);
+    static void throwBoundaryVector(const std::vector<double>& vec) {
+      for (double value : vec) throwBoundary(value);
     }
 
 

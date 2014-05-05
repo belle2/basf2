f12bac98d6474590a8982b5569527d2cdecb30c3
Bjoern Spruck <bjoern.spruck@exp2.physik.uni-giessen.de>
2014-05-05 11:00:09 +0000

    Comments
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10425 b38c6c8f-bee8-465d-8448-9abe6915d496

--- pxd/dataobjects/include/PXDRawROIs.h
+++ pxd/dataobjects/include/PXDRawROIs.h
@@ -21,7 +21,9 @@ namespace Belle2 {
 
   /** The PXD Raw ROIs class
     * This class stores information about the ROIs processed by ONSEN
-    * and makes them available in a root tree
+    * and makes them available in a root tree.
+    * This is mainly for debugging and crosscheck.
+    * Attention: Format is depending on hardware implementation and not yet fixed!
     */
   class PXDRawROIs : public TObject {
   public:
@@ -31,8 +33,13 @@ namespace Belle2 {
       m_2timesNrROIs(0), m_header1(0), m_header2(0), m_header3(0), m_header4(0), m_rois(0) {};
 
 
-    /**
-     * @param nr rois.
+    /** Constructor with data
+     * @param nrroi Nr of Rois to follow in data ptr.
+     * @param h1 Header 1 - HLT Header
+     * @param h2 Header 2 - HLT Trigger Nr
+     * @param h3 Header 3 - DATCON Header
+     * @param h4 Header 4 - DATCON Trigger Nr
+     * @param data pointer to ROIs .
      */
     PXDRawROIs(unsigned int nrroi, unsigned int h1, unsigned int h2, unsigned int h3, unsigned int h4, unsigned int* data):
       m_2timesNrROIs(2 * nrroi), m_header1(h1), m_header2(h2), m_header3(h3), m_header4(h4), m_rois(0) {
@@ -41,43 +48,73 @@ namespace Belle2 {
     };
 
     /** Get the nr of ROIs.
+     * @param j Index of ROI
      * @return Nr of ROIs.
      */
     unsigned int getNrROIs() const {
       return m_2timesNrROIs / 2;
     }
 
+    /** Return DHH ID of ROI j
+     * @param j Index of ROI
+     * @return DHH ID
+     */
     int getDHHID(int j) const {
       if (j < 0 || j >= (int)m_2timesNrROIs / 2) return -1;
       return (m_rois[2 * j] >> 4) & 0x3F; // & 0x3F0
     }
+
+    /** Return Row 1 of ROI j
+     * @param j Index of ROI
+     * @return  Row 1
+     */
     int getRow1(int j) const {
       if (j < 0 || j >= (int)m_2timesNrROIs / 2) return -1;
       return ((m_rois[2 * j] << 6) & 0x3C0) | ((m_rois[2 * j + 1] >> 26) & 0x3F) ;//  & 0x00F , & 0xFC000000
     }
+
+    /** Return Row 2 of ROI j
+     * @param j Index of ROI
+     * @return Row 2
+     */
     int getRow2(int j) const {
       if (j < 0 || j >= (int)m_2timesNrROIs / 2) return -1;
       return (m_rois[2 * j + 1] >> 8) & 0x3FF;  // & 0x0003FF00
     }
+
+    /** Return Col 1 of ROI j
+     * @param j Index of ROI
+     * @return Column 1
+     */
     int getCol1(int j) const {
       if (j < 0 || j >= (int)m_2timesNrROIs / 2) return -1;
       return (m_rois[2 * j + 1] >> 18) & 0xFF; // & 0x03FC0000
     }
+
+    /** Return Col 1 of ROI j
+     * @param j Index of ROI
+     * @return Column 2
+     */
     int getCol2(int j) const {
       if (j < 0 || j >= (int)m_2timesNrROIs / 2) return -1;
       return (m_rois[2 * j + 1]) & 0xFF;
     }
+
+    /** Return Type (Datcon or HLT) of ROI j
+     * @param j Index of ROI
+     * @return Type of Roi
+     */
     int getType(int j) const {
       if (j < 0 || j >= (int)m_2timesNrROIs / 2) return -1;
       return (m_rois[2 * j] >> 10) & 0x1; //  & 0x400
     }
 
   protected:
-    unsigned int m_2timesNrROIs;
-    unsigned int m_header1;
-    unsigned int m_header2;
-    unsigned int m_header3;
-    unsigned int m_header4;
+    unsigned int m_2timesNrROIs;/**< Number of ROIs times two (size of one ROI is 2*32bit) */
+    unsigned int m_header1;/**< HLT Header */
+    unsigned int m_header2;/**< HTL Trigger NR */
+    unsigned int m_header3;/**< DATCON Header */
+    unsigned int m_header4;/**< DATCON trigger NR */
     int* m_rois; // [m_2timesNrROIs] /**< Buffer of size 2*m_NrROIs ints  */
 
 

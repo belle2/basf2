f0eb2c7cf9b0b4d22989daab191521f0799bffe4
Jakob Lettenbichler <jkl@jodoschka.com>
2014-05-06 12:23:01 +0000

    removed another relative path for includes
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10461 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/vxdCaTracking/src/GlobalNames.cc
+++ tracking/vxdCaTracking/src/GlobalNames.cc
@@ -8,7 +8,7 @@
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/
 
-#include "../include/GlobalNames.h"
+#include "tracking/vxdCaTracking/GlobalNames.h"
 
 
 using namespace std;

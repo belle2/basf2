054f36e6716c05dac451f2caec2e4993f1150a9d
Luigi Li Gioi <ligioi@mpp.mpg.de>
2014-05-18 15:52:55 +0000

    First version of the Vertex data object
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10644 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ analysis/dataobjects/include/Vertex.h
@@ -0,0 +1,146 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Luigi Li Gioi                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef VERTEX_H
+#define VERTEX_H
+
+#include <framework/datastore/RelationsObject.h>
+
+#include <TVector3.h>
+#include <TMatrixFSym.h>
+
+#include <vector>
+#include <set>
+
+namespace Belle2 {
+
+  /** \addtogroup dataobjects
+   * @{
+   */
+
+
+  /**
+   * Vertex data object: contains Btag Vertex and DeltaT
+   */
+
+  class Vertex : public RelationsObject {
+
+  public:
+
+    /**
+      * Default constructor.
+      * All private members are set to 0 (all vectors are empty).
+      */
+    Vertex() {
+      m_tagVertex(0) = 0; m_tagVertex(1) = 0; m_tagVertex(2) = 0;
+      m_tagVertexPval = 0;
+      m_deltaT = 0;
+      m_MCtagV(0) = 0; m_MCtagV(1) = 0; m_MCtagV(2) = 0;
+      m_mcPDG = 0;
+      resetTagVertexErrorMatrix();
+    }
+
+
+    /**
+     * Returns BTag Vertex
+     */
+    TVector3 getTagVertex();
+
+    /**
+     * Returns BTag Vertex (3x3) error matrix
+     */
+    TMatrixFSym getTagVertexErrMatrix();
+
+    /**
+     * Returns BTag Vertex P value
+     */
+    float getTagVertexPval();
+
+    /**
+     * Returns DeltaT
+     */
+    float getDeltaT();
+
+    /**
+     * Returns generated BTag Vertex
+     */
+    TVector3 getMCTagVertex();
+
+    /**
+     * Returns generated Btag PDG code
+     */
+    int getMCTagBFlavor();
+
+    /**
+     * Returns generated DeltaT
+     */
+    float getMCDeltaT();
+
+    /**
+     * Set BTag Vertex
+     */
+    void setTagVertex(TVector3 TagVertex);
+
+    /**
+     *  Set BTag Vertex (3x3) error matrix
+     */
+    void setTagVertexErrMatrix(TMatrixFSym TagVertexErrMatrix);
+
+    /**
+     * Set BTag Vertex P value
+     */
+    void setTagVertexPval(float TagVertexPval);
+
+    /**
+     * Set DeltaT
+     */
+    void setDeltaT(float DeltaT);
+
+    /**
+     * Set generated BTag Vertex
+     */
+    void setMCTagVertex(TVector3 MCTagVertex);
+
+    /**
+     * Set generated Btag PDG code
+     */
+    void setMCTagBFlavor(int MCTagBFlavor);
+
+    /**
+     * Set generated DeltaT
+     */
+    void setMCDeltaT(float DeltaT);
+
+  private:
+    TVector3 m_tagVertex;               /**< Btag vertex */
+    TMatrixFSym m_tagVertexErrMatrix;   /**< Btag vertex (3x3) error matrix */
+    float m_tagVertexPval;              /**< Btag vertex P value */
+    float m_deltaT;                     /**< Delta t */
+    TVector3 m_MCtagV;                  /**< generated Btag vertex */
+    int m_mcPDG;                        /**< generated tag side B flavor (PDG code) */
+    float m_MCdeltaT;                   /**< generated Delta t */
+
+
+    /**
+     * Resets 3x3 tag vertex error matrix
+     * All elements are set to 0.0
+     */
+    void resetTagVertexErrorMatrix();
+
+
+    ClassDef(Vertex, 1) /**< class definition */
+
+  };
+
+
+
+} // end namespace Belle2
+
+#endif
---
+++ analysis/dataobjects/src/Vertex.cc
@@ -0,0 +1,100 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Luigi Li Gioi                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <analysis/dataobjects/Vertex.h>
+
+#include <framework/datastore/StoreArray.h>
+
+#include <framework/logging/Logger.h>
+#include <iostream>
+
+using namespace Belle2;
+
+
+TVector3 Vertex::getTagVertex()
+{
+  return m_tagVertex;
+}
+
+TMatrixFSym Vertex::getTagVertexErrMatrix()
+{
+  return m_tagVertexErrMatrix;
+}
+
+float Vertex::getTagVertexPval()
+{
+  return m_tagVertexPval;
+}
+
+float Vertex::getDeltaT()
+{
+  return m_deltaT;
+}
+
+TVector3 Vertex::getMCTagVertex()
+{
+  return m_MCtagV;
+}
+
+int Vertex::getMCTagBFlavor()
+{
+  return m_mcPDG;
+}
+
+float Vertex::getMCDeltaT()
+{
+  return m_MCdeltaT;
+}
+
+
+void Vertex::setTagVertex(TVector3 TagVertex)
+{
+  m_tagVertex = TagVertex;
+}
+
+void Vertex::setTagVertexErrMatrix(TMatrixFSym TagVertexErrMatrix)
+{
+  m_tagVertexErrMatrix = TagVertexErrMatrix;
+}
+
+void Vertex::setTagVertexPval(float TagVertexPval)
+{
+  m_tagVertexPval = TagVertexPval;
+}
+
+void Vertex::setDeltaT(float DeltaT)
+{
+  m_deltaT = DeltaT;
+}
+
+void Vertex::setMCTagVertex(TVector3 MCTagVertex)
+{
+  m_MCtagV = MCTagVertex;
+}
+
+void Vertex::setMCTagBFlavor(int MCTagBFlavor)
+{
+  m_mcPDG = MCTagBFlavor;
+}
+
+void Vertex::setMCDeltaT(float MCDeltaT)
+{
+  m_MCdeltaT = MCDeltaT;
+}
+
+
+void  Vertex::resetTagVertexErrorMatrix()
+{
+  TMatrixFSym temp(3);
+  m_tagVertexErrMatrix.ResizeTo(temp);
+  m_tagVertexErrMatrix = temp;
+}
+
+ClassImp(Vertex)

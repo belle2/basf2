60b03bee11ceb18247cbfe0c9cdf3b7f21a12671
Stefan Ferstl <ferstl_stefan@gmx.at>
2014-05-20 15:31:33 +0000

    Collector for Tracking, moved to new directory => displayInterfaceTF
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10686 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/dataobjects/include/linkdef.h
+++ tracking/dataobjects/include/linkdef.h
@@ -5,14 +5,53 @@
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
+// #pragma link C++ all namespaces;
 
-#pragma link C++ class Belle2::BaseTFInfo+;
-#pragma link C++ class Belle2::CellTFInfo+;
-#pragma link C++ class Belle2::SectorTFInfo+;
-#pragma link C++ class Belle2::HitTFInfo+;
-#pragma link C++ class Belle2::CollectorTFInfo+;
-#pragma link C++ class Belle2::AnalizerCollectorTFInfo+;
-#pragma link C++ class Belle2::TrackCandidateTFInfo+;
-#pragma link C++ class Belle2::ClusterTFInfo+;
+#pragma link C++ class Belle2::TrackingOutput+;
+#pragma link C++ class Belle2::ExtHit+;
+#pragma link C++ class Belle2::Muid+;
+#pragma link C++ class Belle2::MuidHit+;
+#pragma link C++ class Belle2::VXDTFInfoBoard+;
+#pragma link C++ class Belle2::VXDTFSecMap+;
+
+#pragma link C++ class pair <unsigned int, pair < double, double > >+;
+#pragma link C++ class vector < pair <unsigned int, pair < double, double > > >+;
+#pragma link C++ class pair < unsigned int, vector < pair <unsigned int, pair < double, double > > > >+;
+#pragma link C++ class vector < pair < unsigned int, vector < pair <unsigned int, pair < double, double > > > > >+;
+#pragma link C++ class pair < unsigned int, vector < pair < unsigned int, vector < pair <unsigned int, pair < double, double > > > > > >+;
+#pragma link C++ class vector < pair < unsigned int, vector < pair < unsigned int, vector < pair <unsigned int, pair < double, double > > > > > > >+;
+#pragma link C++ class pair <unsigned int, list < double > >+;
+#pragma link C++ class vector < pair <unsigned int, list < double > > >+;
+#pragma link C++ class pair < unsigned int, vector < pair <unsigned int, list < double > > > >+;
+#pragma link C++ class vector < pair < unsigned int, vector < pair <unsigned int, list < double > > > > >+;
+#pragma link C++ class pair < unsigned int, vector < pair < unsigned int, vector < pair <unsigned int, list < double > > > > > >+;
+#pragma link C++ class vector < pair < unsigned int, vector < pair < unsigned int, vector < pair <unsigned int, list < double > > > > > > >+;
+#pragma link C++ class pair <unsigned int, double >+;
+#pragma link C++ class vector < pair <unsigned int, double > >+;
+
+#pragma link C++ class Belle2::VXDTFRawSecMap+;
+#pragma link C++ class pair <string, Belle2::VXDTFRawSecMap >+;
+#pragma link C++ class vector < pair <string, Belle2::VXDTFRawSecMap > >+;
+
+#pragma link C++ class Belle2::SecMapVector+;
+#pragma link C++ class Belle2::ROIid+;
+#pragma link C++ class Belle2::ROIpayload+;
+#pragma link C++ class Belle2::PXDIntercept+;
+#pragma link C++ class Belle2::RelationsInterface<genfit::Track>+;
+#pragma link C++ class set < unsigned short >+;
+#pragma link C++ class Belle2::RecoTrack+;
+
+// #pragma link C++ class Belle2::BaseTFInfo+;
+// #pragma link C++ class Belle2::CellTFInfo+;
+// #pragma link C++ class Belle2::SectorTFInfo+;
+// #pragma link C++ class Belle2::HitTFInfo+;
+// #pragma link C++ class Belle2::CollectorTFInfo+;
+// #pragma link C++ class Belle2::AnalizerCollectorTFInfo+;
+// #pragma link C++ class Belle2::TrackCandidateTFInfo+;
+// #pragma link C++ class Belle2::ClusterTFInfo+;
+
+// #pragma link C++ namespace Belle2::VXDTFRawSecMapTypedef+;
+// #pragma link C++ namespace Belle2::VXDTFSecMapTypedef+;
+
 
 #endif

2da4301903e767a37f603fa7eab92ca9b4896332
Jakob Lettenbichler <jkl@jodoschka.com>
2014-05-22 19:59:27 +0000

    repaired script for TB
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10732 b38c6c8f-bee8-465d-8448-9abe6915d496

--- testbeam/vxd/scripts/setup_vxdtf.py
+++ testbeam/vxd/scripts/setup_vxdtf.py
@@ -3,6 +3,7 @@
 
 from basf2 import *
 
+
 # This returns a VXDTF module with current 'best' options
 # Argument candName can be used to define a non-standard name for the
 # output genfit::TrackCand StoreArray
@@ -10,16 +11,16 @@ from basf2 import *
 # The idea of returning the module instead of adding it to a path is
 # that the user can change the settings.
 
-
 def setup_vxdtf(
     candName='',
-    secSetup=['TB6GeVNoMagnetSVD-moreThan1500MeV_SVD'],
+    secSetup=['TB4GeVNoMagnetNoAlignedSource2014May21SVD-moreThan1500MeV_SVD'
+              ],
     filterOverlaps='hopfield',
     baseLineTF=1,
     ):
 
     # VXDTF:
-    # # parameters:
+    ## parameters:
     qiType = 'circleFit'
     #
     vxdtf = register_module('VXDTF')
@@ -41,31 +42,35 @@ def setup_vxdtf(
         'qiSmear': False,
         'smearSigma': 0.000001,
         'GFTrackCandidatesColName': candName,
-        'tuneCutoffs': 4,
+        'tuneCutoffs': 0.51,
         'activateDistanceXY': [False],
-        'activateDistanceZ': [True],
         'activateDistance3D': [True],
-        'activateAngles3DHioC': [False],
-        'activateAnglesXYHioC': [False],
-        'activateDeltaSlopeRZHioC': [False],
-        'activateDistance2IPHioC': [False],
-        'activatePTHioC': [False],
-        'activateHelixFitHioC': [False],
-        'activateDeltaPtHioC': [False],
-        'activateDeltaDistance2IPHioC': [False],
+        'activateDistanceZ': [False],
+        'activateSlopeRZ': [False],
+        'activateNormedDistance3D': [False],
         'activateAngles3D': [True],
         'activateAnglesXY': [False],
-        'activateAnglesRZ': [True],
+        'activateAnglesRZ': [False],
         'activateDeltaSlopeRZ': [False],
         'activateDistance2IP': [False],
         'activatePT': [False],
-        'activateHelixFit': [False],
+        'activateHelixParameterFit': [False],
+        'activateAngles3DHioC': [True],
+        'activateAnglesXYHioC': [True],
+        'activateAnglesRZHioC': [False],
+        'activateDeltaSlopeRZHioC': [False],
+        'activateDistance2IPHioC': [False],
+        'activatePTHioC': [False],
+        'activateHelixParameterFitHioC': [False],
+        'activateDeltaPtHioC': [False],
+        'activateDeltaDistance2IPHioC': [False],
         'activateZigZagXY': [False],
+        'activateZigZagRZ': [False],
         'activateDeltaPt': [False],
         'activateCircleFit': [True],
         'tuneCircleFit': [0],
-        'tuneDistanceZ': [1.2],
         }
+        # 'tuneDistanceZ': [1.2],
     vxdtf.param(param_vxdtf)
     return vxdtf
 
@@ -78,7 +83,7 @@ def setup_vxdtf1T(
     ):
 
     # VXDTF:
-    # # parameters:
+    ## parameters:
     qiType = 'circleFit'
     #
     vxdtf = register_module('VXDTF')

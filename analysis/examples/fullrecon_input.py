53eda2f97241a67a3a8e47a04db660e12a4ced77
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2014-05-23 14:59:09 +0000

    Add some fairly important variables, reenable channels with pi0
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10761 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/examples/fullrecon_input.py
+++ analysis/examples/fullrecon_input.py
@@ -27,54 +27,63 @@ chargedTrackVars = [
     'pt',
     'p_CMS',
     'pt_CMS',
+    'M',
     'chiProb']
 
-DVars = ['daughterProductOf(getExtraInfo(SignalProbability))', 'p', 'pt', 'p_CMS', 'pt_CMS']
-BVars = ['daughterProductOf(getExtraInfo(SignalProbability))', 'p', 'pt', 'p_CMS', 'pt_CMS']
+DVars = ['daughterProductOf(getExtraInfo(SignalProbability))', 'p', 'pt', 'p_CMS', 'pt_CMS', 'M']
+BVars = ['daughterProductOf(getExtraInfo(SignalProbability))', 'p', 'pt', 'p_CMS', 'pt_CMS', 'M']
 method = ('FastBDT', 'Plugin', '!H:CreateMVAPdfs:!V:NTrees=400:Shrinkage=0.10:RandRatio=0.5:NCutLevel=8:NTreeLayers=3')
+#method = (
+#    'BDTGradient_100', 'BDT',
+#    '!H:CreateMVAPdfs:!V:NTrees=100:BoostType=Grad:Shrinkage=0.10:'
+#    'UseBaggedGrad:GradBaggingFraction=0.5:nCuts=10:MaxDepth=2'
+#)
+#nb = (
+#    'NeuroBayes', 'Plugin',
+#    '!H:CreateMVAPdfs:V:NTrainingIter=50:TrainingMethod=BFGS'
+#)
 
 particles = []
-
-# Add FSP
-#particles.append( Particle('gamma', ['p','pt','p_CMS','pt_CMS'], method))
-#particles.append( Particle('pi0', ['p','pt','p_CMS','pt_CMS'], method))
 particles.append(Particle('pi+', chargedTrackVars, method))
 particles.append(Particle('K+', chargedTrackVars, method))
+particles.append(Particle('gamma', ['p', 'pt', 'clusterE9E25'], method))
+particles.append(Particle('pi0', ['p', 'pt', 'daughterAngle', 'M'], method))
+
 
 p = Particle('D0', DVars, method).addChannel(['K-', 'pi+'])
 p.addChannel(['K-', 'pi+', 'pi+', 'pi-'])
-#p.addChannel(['K-', 'pi+','pi0'])
+p.addChannel(['K-', 'pi+', 'pi0'])
 particles.append(p)
 
 p = Particle('D+', DVars, method)
 p.addChannel(['K-', 'pi+', 'pi+'])
 p.addChannel(['K-', 'K+', 'pi+'])
-#p.addChannel(['K-', 'pi+', 'pi+', 'pi0'])
-#p.addChannel(['K-', 'K+', 'pi+', 'pi0'])
+p.addChannel(['K-', 'pi+', 'pi+', 'pi0'])
+p.addChannel(['K-', 'K+', 'pi+', 'pi0'])
 particles.append(p)
 
 p = Particle('D*+', DVars, method)
 p.addChannel(['D0', 'pi+'])
-#p.addChannel(['D+', 'pi0'])
+p.addChannel(['D+', 'pi0'])
 particles.append(p)
 
-#p = Particle('D*0', DVars, method)
-#p.addChannel(['D0', 'pi0'])
-#p.addChannel(['D0', 'gamma'])
-#particles.append(p)
+p = Particle('D*0', DVars, method)
+p.addChannel(['D0', 'pi0'])
+p.addChannel(['D0', 'gamma'])
+particles.append(p)
 
 p = Particle('B+', BVars, method)
 p.addChannel(['D0', 'pi+'])
-#p.addChannel(['D+', 'pi0'])
+p.addChannel(['D+', 'pi0'])
 p.addChannel(['D*-', 'pi+', 'pi+'])
 particles.append(p)
 
 p = Particle('B0', BVars, method)
-#p.addChannel(['D0', 'pi0'])
+p.addChannel(['D0', 'pi0'])
 p.addChannel(['D-', 'pi+'])
-#p.addChannel(['D-', 'pi0', 'pi+'])
+p.addChannel(['D-', 'pi0', 'pi+'])
 p.addChannel(['D*-', 'pi+', 'pi+', 'pi-'])
-#p.addChannel(['D*-', 'pi+', 'pi+', 'pi-', 'pi0'])
+p.addChannel(['D*-', 'pi+', 'pi+', 'pi-', 'pi0'])
 particles.append(p)
 
 main = create_path()

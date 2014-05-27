63da42dbd983326afa54566dabfaae58258ecfa4
Jakob Lettenbichler <jkl@jodoschka.com>
2014-05-27 15:18:19 +0000

    new easy-to-use setups for the VXDTF optimized for TBRun 470
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10810 b38c6c8f-bee8-465d-8448-9abe6915d496

--- testbeam/vxd/scripts/setup_vxdtf.py
+++ testbeam/vxd/scripts/setup_vxdtf.py
@@ -136,3 +136,237 @@ def setup_vxdtf1T(
     return vxdtf
 
 
+############################################################################################################
+
+def setup_vxdtfR470VXD(
+    candName='CaVXDTCs',
+    secSetup=['R470TB3GeVNoMagnetNoAlignedSource2014May27PXDSVD-moreThan1500MeV_PXDSVD'
+              ],
+    filterOverlaps='hopfield',
+    qiType='straightLine',
+    baseLineTF=0,
+    seedType='straightLine',
+    ):
+
+    #
+    vxdtf = register_module('VXDTF')
+    # vxdtf.logging.log_level = LogLevel.INFO
+    vxdtf.logging.log_level = LogLevel.DEBUG
+    vxdtf.logging.debug_level = 1
+    param_vxdtf = {  # 7
+                     # ### noMagnet
+                     # 'activateAnglesXY': [False], #### withMagnet
+                     # 'activateAnglesRZHioC': [False], #### noMagnet
+                     # ### withMagnet r51x
+                     # True
+                     # 'tuneCircleFit': [0.0000000001],
+        'activateBaselineTF': baseLineTF,
+        'tccMinState': [2],
+        'tccMinLayer': [5],
+        'reserveHitsThreshold': [0.],
+        'highestAllowedLayer': [6],
+        'telClustersName': '',
+        'standardPdgCode': -11,
+        'artificialMomentum': 3,
+        'sectorSetup': secSetup,
+        'calcQIType': qiType,
+        'calcSeedType': seedType,
+        'killEventForHighOccupancyThreshold': 1000,
+        'highOccupancyThreshold': 70,
+        'cleanOverlappingSet': False,
+        'filterOverlappingTCs': filterOverlaps,
+        'TESTERexpandedTestingRoutines': False,
+        'qiSmear': False,
+        'smearSigma': 0.000001,
+        'GFTrackCandidatesColName': candName,
+        'tuneCutoffs': 0.4,
+        'activateDistanceXY': [False],
+        'activateDistance3D': [True],
+        'activateDistanceZ': [True],
+        'activateSlopeRZ': [False],
+        'activateNormedDistance3D': [False],
+        'activateAngles3D': [True],
+        'activateAnglesXY': [True],
+        'activateAnglesRZ': [False],
+        'activateDeltaSlopeRZ': [False],
+        'activateDistance2IP': [False],
+        'activatePT': [False],
+        'activateHelixParameterFit': [False],
+        'activateAngles3DHioC': [True],
+        'activateAnglesXYHioC': [True],
+        'activateAnglesRZHioC': [False],
+        'activateDeltaSlopeRZHioC': [False],
+        'activateDistance2IPHioC': [False],
+        'activatePTHioC': [False],
+        'activateHelixParameterFitHioC': [False],
+        'activateDeltaPtHioC': [False],
+        'activateDeltaDistance2IPHioC': [False],
+        'activateZigZagXY': [False],
+        'activateZigZagRZ': [False],
+        'activateDeltaPt': [False],
+        'activateCircleFit': [False],
+        'activateDeltaDistance2IP': [False],
+        'tuneAngles3D': [0.1],
+        'tuneDistance3D': [0.1],
+        'tuneDistanceZ': [2.5],
+        }
+    vxdtf.param(param_vxdtf)
+    return vxdtf
+
+
+def setup_vxdtfR470InnerTEL(
+    candName='CaInnerTELTCs',
+    secSetup=['R470TB3GeVNoMagnetNoAlignedSource2014May27InnerTEL-moreThan1500MeV_TEL'
+              ],
+    filterOverlaps='hopfield',
+    qiType='straightLine',
+    baseLineTF=0,
+    seedType='straightLine',
+    ):
+
+    vxdtf = register_module('VXDTF')
+    # vxdtf.logging.log_level = LogLevel.INFO
+    vxdtf.logging.log_level = LogLevel.DEBUG
+    vxdtf.logging.debug_level = 1
+    param_vxdtf = {  #   'artificialMomentum': 5.,
+                     # 7
+                     # VXDTFInfoBoards
+                     # 'activateAnglesXY': [True],  #### noMagnet
+                     # ### withMagnet
+                     # ### noMagnet
+                     # 'activateAnglesRZHioC': [False], #### withMagnet r51x
+                     # True
+                     # 'tuneCircleFit': [0.0000000001],
+        'activateBaselineTF': baseLineTF,
+        'debugMode': 0,
+        'tccMinState': [1],
+        'tccMinLayer': [7],
+        'reserveHitsThreshold': [0.],
+        'highestAllowedLayer': [7],
+        'telClustersName': '',
+        'standardPdgCode': -11,
+        'artificialMomentum': 4,
+        'sectorSetup': secSetup,
+        'calcQIType': qiType,
+        'calcSeedType': seedType,
+        'killEventForHighOccupancyThreshold': 1250,
+        'highOccupancyThreshold': 70,
+        'cleanOverlappingSet': False,
+        'filterOverlappingTCs': filterOverlaps,
+        'TESTERexpandedTestingRoutines': False,
+        'qiSmear': False,
+        'smearSigma': 0.000001,
+        'GFTrackCandidatesColName': candName,
+        'tuneCutoffs': 0.4,
+        'activateDistanceXY': [False],
+        'activateDistance3D': [True],
+        'activateDistanceZ': [True],
+        'activateSlopeRZ': [False],
+        'activateNormedDistance3D': [False],
+        'activateAngles3D': [True],
+        'activateAnglesXY': [False],
+        'activateAnglesRZ': [False],
+        'activateDeltaSlopeRZ': [False],
+        'activateDistance2IP': [False],
+        'activatePT': [False],
+        'activateHelixParameterFit': [False],
+        'activateAngles3DHioC': [True],
+        'activateAnglesXYHioC': [True],
+        'activateAnglesRZHioC': [False],
+        'activateDeltaSlopeRZHioC': [False],
+        'activateDistance2IPHioC': [False],
+        'activatePTHioC': [False],
+        'activateHelixParameterFitHioC': [False],
+        'activateDeltaPtHioC': [False],
+        'activateDeltaDistance2IPHioC': [False],
+        'activateZigZagXY': [False],
+        'activateZigZagRZ': [False],
+        'activateDeltaPt': [False],
+        'activateCircleFit': [False],
+        'activateDeltaDistance2IP': [False],
+        'tuneAngles3D': [0.1],
+        'tuneDistance3D': [0.1],
+        'tuneDistanceZ': [2.5],
+        }
+   #     'tuneDistanceZ': [1.2],
+    vxdtf.param(param_vxdtf)
+    return vxdtf
+
+
+def setup_vxdtfR470OuterTEL(
+    candName='CaOuterTELTCs',
+    secSetup=['R470TB3GeVNoMagnetNoAlignedSource2014May27OuterTEL-moreThan1500MeV_TEL'
+              ],
+    filterOverlaps='hopfield',
+    qiType='straightLine',
+    baseLineTF=0,
+    seedType='straightLine',
+    ):
+
+    vxdtf = register_module('VXDTF')
+    # vxdtf.logging.log_level = LogLevel.INFO
+    vxdtf.logging.log_level = LogLevel.DEBUG
+    vxdtf.logging.debug_level = 1
+    param_vxdtf = {  # 7
+                     # 'activateAnglesXY': [True],  #### noMagnet
+                     # ### withMagnet
+                     # ### noMagnet
+                     # 'activateAnglesRZHioC': [False], #### withMagnet r51x
+                     # True
+                     # 'tuneCircleFit': [0.0000000001],
+        'activateBaselineTF': baseLineTF,
+        'debugMode': 0,
+        'tccMinState': [1],
+        'tccMinLayer': [7],
+        'reserveHitsThreshold': [0.],
+        'highestAllowedLayer': [7],
+        'telClustersName': '',
+        'standardPdgCode': -11,
+        'artificialMomentum': 4,
+        'sectorSetup': secSetup,
+        'calcQIType': qiType,
+        'calcSeedType': seedType,
+        'killEventForHighOccupancyThreshold': 1250,
+        'highOccupancyThreshold': 70,
+        'cleanOverlappingSet': False,
+        'filterOverlappingTCs': filterOverlaps,
+        'TESTERexpandedTestingRoutines': False,
+        'qiSmear': False,
+        'smearSigma': 0.000001,
+        'GFTrackCandidatesColName': candName,
+        'tuneCutoffs': 0.4,
+        'activateDistanceXY': [False],
+        'activateDistance3D': [True],
+        'activateDistanceZ': [True],
+        'activateSlopeRZ': [False],
+        'activateNormedDistance3D': [False],
+        'activateAngles3D': [True],
+        'activateAnglesXY': [False],
+        'activateAnglesRZ': [False],
+        'activateDeltaSlopeRZ': [False],
+        'activateDistance2IP': [False],
+        'activatePT': [False],
+        'activateHelixParameterFit': [False],
+        'activateAngles3DHioC': [True],
+        'activateAnglesXYHioC': [True],
+        'activateAnglesRZHioC': [False],
+        'activateDeltaSlopeRZHioC': [False],
+        'activateDistance2IPHioC': [False],
+        'activatePTHioC': [False],
+        'activateHelixParameterFitHioC': [False],
+        'activateDeltaPtHioC': [False],
+        'activateDeltaDistance2IPHioC': [False],
+        'activateZigZagXY': [False],
+        'activateZigZagRZ': [False],
+        'activateDeltaPt': [False],
+        'activateCircleFit': [False],
+        'activateDeltaDistance2IP': [False],
+        'tuneAngles3D': [0.1],
+        'tuneDistance3D': [0.1],
+        'tuneDistanceZ': [2.5],
+        }
+   #     'tuneDistanceZ': [1.2],
+    vxdtf.param(param_vxdtf)
+    return vxdtf
+
+

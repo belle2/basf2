9821e17f8196f673892c514def3e43d30585ded9
Martin Ritter <martin.ritter@lmu.de>
2014-05-30 20:39:18 +0000

    Simple run-dependency for Gearbox on file level
    Allow to have some run dependency in xml files by replacing {EXP} and
    {RUN} in the file backend uri with the actual experiment and run number
    respectively. Optionally, the number of leading digits can be specified
    separated by a colon, e.g. {EXP:4} to have the experiment number
    starting as 0001.  In addition, if the file cannot be found the
    directory separators "/" will be successivley replaced by "-" to allow
    for flat file structure.
    
    To use this feature, use something like
    
    >> gearbox.param("backends", ["file:/rundata-{EXP:3}-{RUN:4}/","file:"])
    
    in the steering file to search for run-dependent files for experiment 3,
    run 1 in  in rundata-003-0001/ and fall back on the default files if no
    run-dependent version exists. It is also possible to supply only one,
    either {EXP} or {RUN} in the backend string.
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10856 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/gearbox/include/FileHandler.h
+++ framework/gearbox/include/FileHandler.h
@@ -8,12 +8,13 @@
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/
 
-#ifndef GEARBOX_FILEHANDLER_H
-#define GEARBOX_FILEHANDLER_H
+#pragma once
+#ifndef FRAMEWORK_GEARBOX_FILEHANDLER_H
+#define FRAMEWORK_GEARBOX_FILEHANDLER_H
 
 #include <framework/gearbox/InputHandler.h>
 #include <boost/iostreams/filtering_stream.hpp>
-
+#include <boost/format.hpp>
 
 namespace Belle2 {
   namespace gearbox {
@@ -42,26 +43,49 @@ namespace Belle2 {
      * This Handler will try to find the XML files as plain files in the file
      * system.  If a file with the exact name cannot be found, the Handler
      * tries to append .gz to the filename and decompress it with gzip.
+     *
+     * The uri is the base dir for all files to be searched and can contain the
+     * placeholders {EXP} and {RUN} to be replaced with the experiment and run
+     * number respectively. Optionally, the placeholders can contain the number
+     * of digits the result should have separated by a colon, filled with
+     * zeros, e.g. {EXP:4} to have the experiment number starting as 0001
+     *
+     * If an file cannot be found (with or without .gz extension), the
+     * directory separators "/" will be successively replaced by dashes "-"
+     * (starting at the back) to allow for flat file structure. For example, if
+     * the uri is /data/ and the filename to be opened is geometry/Belle2.xml,
+     * the inputhandler will try to find
+     * /data/geometry/Belle2.xml
+     * /data/geometry/Belle2.xml.gz
+     * /data/geometry-Belle2.xml
+     * /data/geometry-Belle2.xml.gz
+     * /data-geometry-Belle2.xml
+     * /data-geometry-Belle2.xml.gz
+     * in that order the search path, stopping at the first success.
      */
     class FileHandler: public InputHandler {
     public:
-      /** Instantiate a new file hanlder, using the uri as base search path.
-       * If uri is empty, the default data search path will be used
-       * @param uri path to search files in
+      /** Instantiate a new file handler, using the uri as base search path.
+       * If uri is empty, the default data search path will be used.
+       * @param uri * path to search files in
        */
       FileHandler(const std::string& uri);
       /** empty, virtual destructor */
       virtual ~FileHandler() {}
-      /** create a new FileContext by searching the file system for a file named like path.
+      /** create a new FileContext by searching the file system for a file
+       * named like path.
        * @param path name of the file, relative to the FileHandler search path
        */
       virtual InputContext* open(const std::string& path);
     protected:
       /** Search path to look for files */
       std::string m_path;
+      /** format object in case of run-dependent data path */
+      boost::format m_pathformat;
+      /** bool indicating whether the data path is run-dependent */
+      bool m_rundependence {false};
     };
+  } //namespace gearbox
+} //namespace Belle2
 
-  }
-}
-
-#endif
+#endif //FRAMEWORK_GEARBOX_FILEHANDLER_H
--- framework/gearbox/src/FileHandler.cc
+++ framework/gearbox/src/FileHandler.cc
@@ -12,7 +12,10 @@
 #include <framework/gearbox/Gearbox.h>
 #include <framework/logging/Logger.h>
 #include <framework/utilities/FileSystem.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/dataobjects/EventMetaData.h>
 
+#include <boost/regex.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/iostreams/device/file.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
@@ -32,20 +35,68 @@ namespace Belle2 {
 
     FileHandler::FileHandler(const std::string& uri): InputHandler(uri), m_path(uri)
     {
-      if (m_path.empty()) m_path = "/data/";
+
+      if (m_path.empty()) {
+        m_path = "/data/";
+      } else {
+        //Check if we have placeholder for experiment or run information
+        boost::regex exp("\\{EXP(?::(\\d+))?\\}");
+        boost::regex run("\\{RUN(?::(\\d+))?\\}");
+        m_rundependence = boost::regex_search(m_path, exp) || boost::regex_search(m_path, run);
+        if (m_rundependence) {
+          //Apparently we do have placeholders, replace them by something
+          //boost::format will understand. The placeholder is something like
+          //{EXP} or {EXP:<n>} where <n> is an integer to make the number
+          //zero-filled with <n> digits, so we replace it with %1$0d or
+          //%1$0<n>d to denote to the first argument in a boost format string.
+          //Same for {RUN} and {RUN:<n>} but as second argument
+          StoreObjPtr<EventMetaData> eventMetaDataPtr;
+          eventMetaDataPtr.isRequired();
+          std::string tmp = boost::regex_replace(m_path, exp, std::string("%1$$0$1d"));
+          tmp = boost::regex_replace(tmp, run, std::string("%2$$0$1d"));
+          B2DEBUG(300, "Found run-dependence in file path, resulting in " << tmp);
+          m_pathformat = boost::format(tmp);
+          m_pathformat.exceptions(boost::io::all_error_bits ^
+                                  (boost::io::too_many_args_bit | boost::io::too_few_args_bit));
+        }
+      }
       B2DEBUG(300, "Created FileHandler for directory " << m_path);
     }
 
     InputContext* FileHandler::open(const std::string& path)
     {
+      if (m_rundependence) {
+        StoreObjPtr<EventMetaData> eventMetaDataPtr;
+        if (!eventMetaDataPtr.isValid()) {
+          B2DEBUG(300, "No run info, probably initialize? Skipping backend");
+          return 0;
+        }
+        int exp = eventMetaDataPtr->getExperiment();
+        int run = eventMetaDataPtr->getRun();
+        m_path = (m_pathformat % exp % run).str();
+      }
       fs::path basedir(m_path);
-      fs::path filename = basedir / path;
-      std::string fullpath = FileSystem::findFile(filename.string());
-      if (!fullpath.empty()) return new FileContext(fullpath, false);
+      std::string filename = (basedir / path).string();
+      bool repeat(false);
+      do {
+        //Ok, let's try to find the file
+        B2DEBUG(350, "Trying to find " << filename);
 
-      filename = basedir / (path + ".gz");
-      fullpath = FileSystem::findFile(filename.string());
-      if (!fullpath.empty()) return new FileContext(fullpath, true);
+        std::string fullpath = FileSystem::findFile(filename);
+        if (!fullpath.empty()) return new FileContext(fullpath, false);
+        fullpath = FileSystem::findFile(filename + ".gz");
+        if (!fullpath.empty()) return new FileContext(fullpath, true);
+
+        //did not work, replace last slash by a - and try again if such a
+        //replacement was successful. This allows flattening the directory
+        //structure from e.g. data/pxd/PXD-Alignment.xml to
+        //data-pxd-PXD-Alignment.xml to more easily override single files
+        size_t last_slash = filename.find_last_of('/');
+        repeat = last_slash != 0 && last_slash != std::string::npos;
+        if (repeat) {
+          filename[last_slash] = '-';
+        }
+      } while (repeat);
       return 0;
     }
 

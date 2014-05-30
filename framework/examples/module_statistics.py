895d522b5569734740349cbc507b49ae012a732c
Martin Ritter <martin.ritter@lmu.de>
2014-05-30 21:01:21 +0000

    Update module_statistics.py example
    Updated module_statistics example to reflect new ModuleStatistics
    capabilities
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@10865 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/examples/module_statistics.py
+++ framework/examples/module_statistics.py
@@ -13,8 +13,8 @@ from basf2 import *
 # EventInfoSetter - generate event meta data
 eventinfosetter = register_module('EventInfoSetter')
 eventinfosetter.param({'expList': [71, 71, 73, 73, 73],
-                 'runList': [3, 4, 10, 20, 30],
-                 'evtNumList': [4, 6, 2, 5, 3]})
+                       'runList': [3, 4, 10, 20, 30],
+                       'evtNumList': [40, 600, 20, 500, 301]})
 # eventinfo - show event meta info
 eventinfo = register_module('EventInfoPrinter')
 
@@ -44,55 +44,60 @@ print statistics(statistics.TOTAL)
 # Change name of modules
 statistics.set_name(eventinfosetter, 'Foo')
 
+# Make a list of all available statistic counters
+statistic_counters = [
+    (statistics.INIT, "initialize()"),
+    (statistics.BEGIN_RUN, "beginRun()"),
+    (statistics.EVENT, "event()"),
+    (statistics.END_RUN, "endRun()"),
+    (statistics.TERM, "terminate()"),
+    (statistics.TOTAL, "*total*"),
+]
+
 # Print detailed statistics for each module
 for stats in statistics.modules:
     print 'Module %s:' % stats.name
-    print ' -> initialize(): %10.3f ms, %3d calls, %10.3f ms/call' \
-        % (stats.time(statistics.INIT) * 1e3, stats.calls(statistics.INIT),
-           stats.time(statistics.INIT) * 1e3 / stats.calls(statistics.INIT))
-    print ' -> beginRun():   %10.3f ms, %3d calls, %10.3f ms/call' \
-        % (stats.time(statistics.BEGIN_RUN) * 1e3,
-           stats.calls(statistics.BEGIN_RUN), stats.time(statistics.BEGIN_RUN)
-           * 1e3 / stats.calls(statistics.BEGIN_RUN))
-    print ' -> event():      %10.3f ms, %3d calls, %10.3f ms/call' \
-        % (stats.time() * 1e3, stats.calls(), stats.time() * 1e3
-           / stats.calls())
-    print ' -> endRun():     %10.3f ms, %3d calls, %10.3f ms/call' \
-        % (stats.time(statistics.END_RUN) * 1e3,
-           stats.calls(statistics.END_RUN), stats.time(statistics.END_RUN)
-           * 1e3 / stats.calls(statistics.END_RUN))
-    print ' -> terminate():  %10.3f ms, %3d calls, %10.3f ms/call' \
-        % (stats.time(statistics.TERM) * 1e3, stats.calls(statistics.TERM),
-           stats.time(statistics.TERM) * 1e3 / stats.calls(statistics.TERM))
+    for stat_counter, stat_name in statistic_counters:
+        print ' -> %12s: %10.3f ms, %4d calls, %10.3f +-%10.3f ms/call' % (
+            stat_name,
+            #Time is in the default time unit which happens to be ns
+            stats.time_sum(stat_counter) / 1e6,
+            stats.calls(stat_counter),
+            stats.time_mean(stat_counter) / 1e6,
+            stats.time_stddev(stat_counter) / 1e6,
+        )
     print
 
 print 'Memory statistics'
 for stats in statistics.modules:
     print 'Module %s:' % stats.name
-    print ' -> initialize(): %10d KB' \
-        % (stats.memory(statistics.INIT))
-    print ' -> beginRun():   %10d KB' \
-        % (stats.memory(statistics.BEGIN_RUN))
-    print ' -> event():      %10d KB' \
-        % (stats.memory())
-    print ' -> endRun():     %10d KB' \
-        % (stats.memory(statistics.END_RUN))
-    print ' -> terminate():  %10d KB' \
-        % (stats.memory(statistics.TERM))
+    for stat_counter, stat_name in statistic_counters:
+        print ' -> %12s: %10d KB, %4d calls, %10d +-%10.3f KB/call' % (
+            stat_name,
+            stats.memory_sum(stat_counter),
+            stats.calls(stat_counter),
+            stats.memory_mean(stat_counter),
+            stats.memory_stddev(stat_counter),
+        )
 
 # Get Statistics for single module
 stats = statistics.get(eventinfosetter)
-print 'EventInfoSetter needed %.3f ms in total' % (1e3
-        * stats.time(statistics.TOTAL))
+eventinfo_total = stats.time_sum(statistics.TOTAL)
+print 'EventInfoSetter needed %.3f ms in total' % (eventinfo_total / 1e6)
+#Now we can also set the name since we have the correct object
+stats.name = "FooBar"
 
 # Print total processing time
-print 'Total processing time: %.3f ms' % (1e3
-        * statistics.framework.time(statistics.TOTAL))
+framework_total = statistics.framework.time_sum(statistics.TOTAL)
+print 'Total processing time: %.3f ms' % (framework_total / 1e6)
 
 # Calculate estimate for framework overhead
-modules_total = sum(e.time(statistics.TOTAL) for e in statistics.modules)
-overhead = statistics.framework.time(statistics.TOTAL) - modules_total
-print 'Framework overhead: %.3f ms' % (1e3 * overhead)
+modules_total = sum(e.time_sum(statistics.TOTAL) for e in statistics.modules)
+overhead = framework_total - modules_total
+print 'Framework overhead: %.3f ms (%.2f %%)' % (
+    overhead / 1e6,
+    100 * overhead / framework_total,
+)
 print
 
 # Clear statistics

bf233ed84655437f7bd44abd6bffdfb414dd05fd
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2014-06-10 09:40:11 +0000

    Use standard simulation/reconstruction
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@11014 b38c6c8f-bee8-465d-8448-9abe6915d496

--- display/examples/evtgen_display.py
+++ display/examples/evtgen_display.py
@@ -5,6 +5,8 @@
 # including fitted tracks.
 
 from basf2 import *
+from simulation import add_simulation
+from reconstruction import add_reconstruction
 
 # register necessary modules
 eventinfosetter = register_module('EventInfoSetter')
@@ -14,19 +16,12 @@ eventinfoprinter = register_module('EventInfoPrinter')
 
 # create geometry
 gearbox = register_module('Gearbox')
-geometry = register_module('Geometry')
 
 # EvtGen to provide generic BB events
 evtgeninput = register_module('EvtGenInput')
 
 evtgeninput.param('boost2LAB', True)
 
-# simulation
-g4sim = register_module('FullSim')
-# make the simulation less noisy
-g4sim.logging.log_level = LogLevel.ERROR
-# and make it store the particle trajectories so we can draw them
-g4sim.param("trajectoryStore", 2)
 
 # create paths
 main = create_path()
@@ -36,26 +31,12 @@ main.add_module(eventinfosetter)
 main.add_module(eventinfoprinter)
 
 main.add_module(gearbox)
-main.add_module(geometry)
 main.add_module(evtgeninput)
-main.add_module(g4sim)
 
-cdcdigi = register_module('CDCDigitizer')
-main.add_module(cdcdigi)
+add_simulation(main)
 
-pxd_digi = register_module('PXDDigitizer')
-main.add_module(pxd_digi)
-
-main.add_module(register_module('PXDClusterizer'))
-
-mctrackfinder = register_module('TrackFinderMCTruth')
-mctrackfinder.param('UsePXDHits', True)
-mctrackfinder.param('UseSVDHits', True)
-mctrackfinder.param('UseCDCHits', True)
-main.add_module(mctrackfinder)
-
-genfit = register_module('GenFitter')
-main.add_module(genfit)
+# reconstruction
+add_reconstruction(main)
 
 # default parameters
 display = register_module('Display')

5e47581ef323f164cbbb51943902dd5876f26d64
Thomas Keck <t.keck@online.de>
2014-06-11 14:54:24 +0000

    Fixed BtagBsigReconstruction example
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@11045 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/examples/BtagBsigReconstruction.py
+++ analysis/examples/BtagBsigReconstruction.py
@@ -9,12 +9,10 @@ from reconstruction import add_reconstruction
 myMain = create_path()
 
 # generation of 100 events according to the specified DECAY table
-# Y(4S) -> Btag- Bsig+
-# Btag- -> D0 pi-; D0 -> K- pi+
-# Bsig+ -> pi0 e+ nu_e
-generateY4S(20, os.environ['BELLE2_LOCAL_DIR']
-            + '/analysis/examples/exampleEvtgenDecayFiles/Btag2Dpi_Bsig2pi0enu.dec'
-            , myMain)
+# Y(4S) -> B-:tag- B+:sig
+# B-:tag -> D0 pi-; D0 -> K- pi+
+# B+:sig -> pi0 e+ nu_e
+generateY4S(100, os.environ['BELLE2_LOCAL_DIR'] + '/analysis/examples/exampleEvtgenDecayFiles/Btag2Dpi_Bsig2pi0enu.dec', myMain)
 
 # simulation
 add_simulation(myMain)
@@ -26,81 +24,53 @@ add_reconstruction(myMain)
 loadReconstructedParticles(myMain)
 
 # create final state particle lists
-selectParticle('K-', -321, [], True, myMain)
-selectParticle('pi+', 211, [], True, myMain)
-selectParticle('pi-', -211, [], True, myMain)
-selectParticle('pi0', 111, [], True, myMain)
-selectParticle('e+', -11, [], True, myMain)
-selectParticle('g', 22, [], True, myMain)
+selectParticle('K-', [], True, myMain)
+selectParticle('pi+', [], True, myMain)
+selectParticle('pi-', [], True, myMain)
+selectParticle('e+', [], True, myMain)
+selectParticle('gamma', [], True, myMain)
+
+# reconstruct pi0 -> gamma gamma decay
+makeParticle('pi0 -> gamma gamma', {'M': (0.05, 1.7)}, True, myMain)
 
 # reconstruct D0 -> K- pi+ decay (and c.c.)
-makeParticle(
-    'D0',
-    421,
-    ['K-', 'pi+'],
-    1.800,
-    1.900,
-    True,
-    myMain,
-    )
+makeParticle('D0 -> K- pi+', {'M': (1.800, 1.900)}, True, myMain)
 
 # reconstruct Btag -> D0 pi- (and c.c.)
-makeParticle(
-    'B-',
-    -521,
-    ['D0', 'pi-'],
-    5.000,
-    6.000,
-    True,
-    myMain,
-    )
+makeParticle('B-:tag -> D0 pi-', {'M': (5.000, 6.000)}, True, myMain)
 
 # reconstruct Bsig -> pi0 e+ [nu_e] (and c.c.)
-makeParticle(
-    'B+',
-    521,
-    ['pi0', 'e+'],
-    0.000,
-    6.000,
-    True,
-    myMain,
-    )
+makeParticle('B+:sig -> pi0 e+', {'M': (0.000, 6.000)}, True, myMain)
 
 # reconstruct Y(4S) -> Btag Bsig
-makeParticle(
-    'Upsilon(4S)',
-    300553,
-    ['B-', 'B+'],
-    0.000,
-    11.000,
-    True,
-    myMain,
-    )
+makeParticle('Upsilon(4S) -> B-:tag B+:sig', {'M': (0.000, 11.000)}, True, myMain)
 
 # perform MC matching
+matchMCTruth('pi0', myMain)
+matchMCTruth('B-:tag', myMain)
+matchMCTruth('B+:sig', myMain)
 matchMCTruth('Upsilon(4S)', myMain)
 
 # create and fill RestOfEvent for B- and Y(4S) particles
-buildRestOfEvent('B-', myMain)
+buildRestOfEvent('B-:tag', myMain)
 buildRestOfEvent('Upsilon(4S)', myMain)
 
 # define what should be dumped to ntuple for Btag
-toolsB = ['MCTruth', '^B- -> D0 pi-']
-toolsB += ['DeltaEMbc', '^B- -> D0 pi-']
-toolsB += ['ROEMultiplicities', '^B- -> D0 pi-']
+toolsB = ['MCTruth', '^B-:tag -> D0 pi-']
+toolsB += ['DeltaEMbc', '^B-:tag -> D0 pi-']
+toolsB += ['ROEMultiplicities', '^B-:tag -> D0 pi-']
 
 # define what should be dumped to ntuple for Upsilon(4S)
-tools4S = ['MCTruth', '^Upsilon(4S) -> ^B- ^B+']
-tools4S += ['DeltaEMbc', 'Upsilon(4S) -> ^B- ^B+']
-tools4S += ['ROEMultiplicities', '^Upsilon(4S) -> B- B+']
-tools4S += ['RecoilKinematics', '^Upsilon(4S) -> B- B+']
-tools4S += ['ExtraEnergy', '^Upsilon(4S) -> B- B+']
+tools4S = ['MCTruth', '^Upsilon(4S) -> ^B-:tag ^B+:sig']
+tools4S += ['DeltaEMbc', 'Upsilon(4S) -> ^B-:tag ^B+:sig']
+tools4S += ['ROEMultiplicities', '^Upsilon(4S) -> B-:tag B+:sig']
+tools4S += ['RecoilKinematics', '^Upsilon(4S) -> B-:tag B+:sig']
+tools4S += ['ExtraEnergy', '^Upsilon(4S) -> B-:tag B+:sig']
 
 # write flat ntuples
 ntupleFile('restOfEvent_pi0enu.root', myMain)
-ntupleTree('btag', 'B-', toolsB, myMain)
+ntupleTree('btag', 'B-:tag', toolsB, myMain)
 ntupleTree('btagbsig', 'Upsilon(4S)', tools4S, myMain)
 
 process(myMain)
 print statistics
-

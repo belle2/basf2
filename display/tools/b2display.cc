201c8091c905a7c763b1470deda984a318d92bd1
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2014-06-23 13:37:51 +0000

    Update b2display help, do proper escaping of argument to allow wildcards
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@11239 b38c6c8f-bee8-465d-8448-9abe6915d496

--- display/tools/b2display.cc
+++ display/tools/b2display.cc
@@ -9,13 +9,17 @@
  */
 int main(int argc, char* argv[])
 {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " input_file.root\n\n";
-    std::cerr << "  Show data in given input file using the event display.\n";
+  if (argc != 2 or std::string(argv[1]) == "--help") {
+    std::cerr << "Usage: " << argv[0] << " input_file.root\n\n"
+              "  Show data in given input file using the event display.\n"
+              "  You can also open multiple files using wildcards, though you have to\n"
+              "  pass the string with wildcards to b2display, e.g.\n"
+              "    b2display \"my/files/mdst_*.root\".\n"
+              "  Using http:// or root:// URLs is also possible.\n";
     return 1;
   }
   using namespace Belle2::FileSystem;
   std::string fullPath = findFile("/display/examples/display.py");
-  std::string s = "basf2 " + fullPath + " -i " + std::string(argv[1]);
+  std::string s = "basf2 " + fullPath + " -i \"" + std::string(argv[1]) + "\"";
   return system(s.c_str());
 }

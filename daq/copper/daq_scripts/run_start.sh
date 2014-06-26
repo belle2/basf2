abca7c7fafcf590a9b27d96b2b795c76cad33ff4
Satoru Yamada <satoru.yamada@kek.jp>
2014-06-26 05:05:12 +0000

    Update scripts
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@11331 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/copper/daq_scripts/run_start.sh
+++ daq/copper/daq_scripts/run_start.sh
@@ -2,8 +2,8 @@
 #
 # basf2 on COPPER CPU
 #
-/usr/bin/xterm -fn 7x14 -geometry 102x10+0+242 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr013 1 1 0 1; sleep 3000000;" &
-/usr/bin/xterm -fn 7x14 -geometry 102x10+0+442 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr014 1 1 0 2; sleep 3000000;" &
+/usr/bin/xterm -fn 7x14 -geometry 102x10+0+242 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr015 1 1 0 1; sleep 3000000;" &
+#/usr/bin/xterm -fn 7x14 -geometry 102x10+0+442 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr014 1 1 0 2; sleep 3000000;" &
 #/usr/bin/xterm -fn 7x14 -geometry 102x10+0+242 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr005 1 8 0 1; sleep 3000000;" &
 #
 # event builder on ROPC

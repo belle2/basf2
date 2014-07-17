9d2b55cb0cd7f81b3290bb0525ab8b45c97c3344
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2014-07-17 13:16:45 +0000

    don't print variables on import
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@11679 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/scripts/variables.py
+++ analysis/scripts/variables.py
@@ -5,8 +5,10 @@ gSystem.Load('libanalysis.so')
 from ROOT import Belle2
 variables = Belle2.Variable.Manager.Instance()
 
-print 80 * '='
-print 'Available variables in Variable::Manager:'
-print 80 * '='
-variables.printList()
-print 80 * '='
+
+if __name__ == "__main__":
+    print 80 * '='
+    print 'Available variables in Variable::Manager:'
+    print 80 * '='
+    variables.printList()
+    print 80 * '='

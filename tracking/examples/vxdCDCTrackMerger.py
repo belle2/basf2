eff3baa4a179e7afea4ef96384280a9c3a8f36c6
Benjamin Oberhof <benjamin.oberhof@pi.infn.it>
2014-07-17 16:13:55 +0000

    fixes
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@11684 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/examples/vxdCDCTrackMerger.py
+++ tracking/examples/vxdCDCTrackMerger.py
@@ -3,7 +3,10 @@
 
 # adapted from ExtrapCDCTracksToVXD.py
 import os
+import random
 from basf2 import *
+from ROOT import Belle2
+from modularAnalysis import *
 
 # register necessary modules
 eventinfosetter = register_module('EventInfoSetter')
@@ -11,7 +14,7 @@ eventinfosetter = register_module('EventInfoSetter')
 # generate one event
 eventinfosetter.param('expList', [0])
 eventinfosetter.param('runList', [1])
-eventinfosetter.param('evtNumList', [1000])
+eventinfosetter.param('evtNumList', [500])
 eventinfoprinter = register_module('EventInfoPrinter')
 
 # create geometry
@@ -20,32 +23,38 @@ geometry = register_module('Geometry')
 
 # simulate only tracking detectors to simulate the whole detector included in
 # BelleII.xml, comment the next line out
-geometry.param('components', ['MagneticField', 'BeamPipe', 'PXD', 'SVD', 'CDC'
-               ])
+geometry.param('components', [
+    'MagneticField',
+    'BeamPipe',
+    'PXD',
+    'SVD',
+    'CDC',
+    'ECL',
+    ])
 
-# Realistic evt generator
+# EvtGen generator
 evtgeninput = register_module('EvtGenInput')
 evtgeninput.logging.log_level = LogLevel.WARNING
 
 # shoot particles in the detector
 pGun = register_module('ParticleGun')
 
-# choose the particles you want to simulate
+# choose the particles you want to simulate with pGun
 param_pGun = {  # ---    'momentumParams': [0.4, 1.6],
                 # fixed, uniform
     'pdgCodes': [13, -13],
     'nTracks': 10,
     'varyNTracks': 0,
-    'momentumGeneration': 'fixed',
-    'momentumParams': [2],
+    'momentumGeneration': 'uniform',
+    'momentumParams': [0, 5],
     'thetaGeneration': 'uniform',
-    'thetaParams': [60., 120.],
+    'thetaParams': [0., 180.],
     'phiGeneration': 'uniform',
     'phiParams': [0, 360],
     'vertexGeneration': 'uniform',
-    'xVertexParams': [0.0, 0.0],
-    'yVertexParams': [0.0, 0.0],
-    'zVertexParams': [0.0, 0.0],
+    'xVertexParams': [0., 0.],
+    'yVertexParams': [0., 0.],
+    'zVertexParams': [0., 0.],
     }
 pGun.param(param_pGun)
 
@@ -56,10 +65,15 @@ g4sim.logging.log_level = LogLevel.ERROR
 
 # digitizer
 cdcDigitizer = register_module('CDCDigitizer')
+cdcDigitizer.logging.log_level = LogLevel.ERROR
 pxdDigitizer = register_module('PXDDigitizer')
+pxdDigitizer.logging.log_level = LogLevel.ERROR
 svdDigitizer = register_module('SVDDigitizer')
+svdDigitizer.logging.log_level = LogLevel.ERROR
 pxdClusterizer = register_module('PXDClusterizer')
+pxdClusterizer.logging.log_level = LogLevel.ERROR
 svdClusterizer = register_module('SVDClusterizer')
+svdClusterizer.logging.log_level = LogLevel.ERROR
 
 # trackfinders
 si_mctrackfinder = register_module('TrackFinderMCTruth')
@@ -141,7 +155,7 @@ track_splitter.param(track_splitter_param)
 
 # MERGING
 # track merger
-svd_cdcTracksMerger = register_module('VXDCDCTrackMerger')
+vxd_cdcTracksMerger = register_module('VXDCDCTrackMerger')
 trackMerger_param = {  # (in cm) use cdc inner wall
                        #    'CDC_wall_radius':        16.29,  #(in cm) use cdc outer wall
                        # default False
@@ -158,11 +172,11 @@ trackMerger_param = {  # (in cm) use cdc inner wall
     }
 #    'root_output_filename': 'VXD_CDC_trackmerger_test.root',
 
-svd_cdcTracksMerger.param(trackMerger_param)
+vxd_cdcTracksMerger.param(trackMerger_param)
 
 # MERGING ANALYSIS
 # track merger analysis
-svd_cdcTracksMergerAnalysis = register_module('VXDCDCTrackMergerAnalysis')
+vxd_cdcTracksMergerAnalysis = register_module('VXDCDCTrackMergerAnalysis')
 trackMergerAnalysis_param = {  # (in cm) use cdc inner wall
                                #    'CDC_wall_radius':        16.29,  #(in cm) use cdc outer wall
                                # default False
@@ -172,12 +186,50 @@ trackMergerAnalysis_param = {  # (in cm) use cdc inner wall
     'CDCGFTracksColName': 'CDCTracks',
     'GFTracksColName': 'GFTracks',
     'TrackCandColName': 'TracksCand',
+    'UnMergedCands': 'UnMergedCand',
     'root_output_filename': 'VXD_CDC_trackmerger_test.root',
+    'chi2_max': 100,
+    'merge_radius': 2.0,
     }
-svd_cdcTracksMergerAnalysis.param(trackMergerAnalysis_param)
-# output
-# ---output = register_module('RootOutput')
-# ---output.param('outputFileName', 'SiCDCTrackMerger.root')
+vxd_cdcTracksMergerAnalysis.param(trackMergerAnalysis_param)
+vxd_cdcTracksMergerAnalysis.logging.log_level = LogLevel.DEBUG
+
+
+class HighlighterModule(Module):
+
+    """Select all things related to a Particle"""
+
+    def initialize(self):
+        """reimplementation of Module::initialize()."""
+
+        Belle2.PyStoreObj('DisplayData').registerAsPersistent()
+
+    def event(self):
+        """reimplementation of Module::event()."""
+
+        displayData = Belle2.PyStoreObj('DisplayData')
+        displayData.create()
+
+        merged = Belle2.PyStoreArray('MergedTracks')
+        for p in merged:
+            # if abs(p.getPDGCode()) == 413:
+            B2WARNING('Highlighting merged tracks')
+            displayData.obj().select(p)
+                # daughters = p.getFinalStateDaughters()
+                # for d in daughters:
+                    # selecting the MCParticle also gets the tracks
+                    # mcp = d.getRelated('MCParticles')
+                    # displayData.obj().select(mcp)
+            break  # only one
+
+
+display = register_module('Display')
+display.param('useClusters', True)
+display.param('showTrackLevelObjects', True)
+    # 'showTrackCandidates' : True)
+display.param('GFTrackCandidatesColName', 'UnMergedCand')
+display.param('options', 'DHMPS')
+# display.param(display_param)
 
 # Create paths
 main = create_path()
@@ -201,8 +253,11 @@ main.add_module(cand_merger)
 # main.add_module(si_fitting)
 main.add_module(fitting)
 main.add_module(track_splitter)
-main.add_module(svd_cdcTracksMerger)
-main.add_module(svd_cdcTracksMergerAnalysis)
+main.add_module(vxd_cdcTracksMerger)
+main.add_module(vxd_cdcTracksMergerAnalysis)
+# main.add_module(HighlighterModule())
+# main.add_module(display)
+
 # ---main.add_module(output)
 
 # Process events

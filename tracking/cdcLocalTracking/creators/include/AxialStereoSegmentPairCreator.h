2680c792c583c97cefceca718c72c4c8d2502934
Oliver Frost <oliver.frost@desy.de>
2014-07-18 18:01:18 +0000

    Setup logic for segment creation based on segment pairs.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@11705 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/cdcLocalTracking/creators/include/AxialStereoSegmentPairCreator.h
@@ -0,0 +1,192 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2014 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef AXIALSTEREOSEGMENTPAIRCREATOR_H
+#define AXIALSTEREOSEGMENTPAIRCREATOR_H
+
+#include <boost/range/iterator_range.hpp>
+
+#include <tracking/cdcLocalTracking/topology/CDCWireTopology.h>
+#include <tracking/cdcLocalTracking/eventdata/tracks/CDCAxialStereoSegmentPair.h>
+
+
+
+namespace Belle2 {
+
+  namespace CDCLocalTracking {
+
+
+
+    /// Class providing construction combinatorics for the segment triples.
+    template<class AxialStereoSegmentPairFilter>
+    class AxialStereoSegmentPairCreator {
+
+    private:
+      /// Iterator type of the input segments;
+      typedef std::vector<CDCRecoSegment2D>::const_iterator CDCRecoSegmentIterator;
+
+      /// Iterator range type usable with range based for of input segments.
+      typedef boost::iterator_range<CDCRecoSegmentIterator> CDCRecoSegmentRange;
+
+      /// Storage structure type for axial segements by their superlayer id
+      typedef CDCRecoSegmentRange SegmentRangesBySuperLayer[CDCWireTopology::N_SUPERLAYERS];
+
+
+    public:
+      /// Empty constructor.
+      AxialStereoSegmentPairCreator() {;}
+
+      /// Empty destructor.
+      ~AxialStereoSegmentPairCreator() {;}
+
+
+
+      /// Forwards the initialize method of the module to the filters
+      void initialize() {
+        m_axialStereoSegmentPairFilter.initialize();
+      }
+
+      /// Forwards the terminate method of the module to the filters
+      void terminate() {
+        m_axialStereoSegmentPairFilter.terminate();
+      }
+
+
+
+      bool checkSegmentsSortedBySuperLayer(const std::vector<CDCRecoSegment2D>& segments) const {
+        if (segments.empty()) return true;
+
+        ISuperLayerType lastISuperLayer = INNER_ISUPERLAYER;
+
+        for (const CDCRecoSegment2D & segment : segments) {
+          if (lastISuperLayer > segment.getISuperLayer()) return false;
+          lastISuperLayer = segment.getISuperLayer();
+        }
+        return true;
+      }
+
+
+
+      /// Create the segment triples by combining close by segments in the combination axial-stereo-axial based on the filter selection criteria.
+      inline void create(const std::vector<CDCRecoSegment2D>& segments,
+                         std::vector<CDCAxialStereoSegmentPair>& axialStereoSegmentPairs) const {
+
+        if (not checkSegmentsSortedBySuperLayer(segments)) {
+          B2ERROR("Given CDCRecoSegments are not sorted by superlayer");
+          return;
+        }
+
+        SegmentRangesBySuperLayer segmentRangesBySuperLayer;
+
+        CDCRecoSegmentIterator itBeginSegmentsInSuperLayer = std::begin(segments);
+        const CDCRecoSegmentIterator itEndSegments = std::end(segments);
+
+        for (ISuperLayerType iSuperLayer = 0; iSuperLayer < CDCWireTopology::N_SUPERLAYERS; ++iSuperLayer) {
+          std::pair<CDCRecoSegmentIterator, CDCRecoSegmentIterator> itPairSegmentsInSuperLayer = std::equal_range(itBeginSegmentsInSuperLayer, itEndSegments, iSuperLayer);
+          segmentRangesBySuperLayer[iSuperLayer] = CDCRecoSegmentRange(itPairSegmentsInSuperLayer.first, itPairSegmentsInSuperLayer.second);
+          //End iterator is the start of the next superlayer
+          itBeginSegmentsInSuperLayer = itPairSegmentsInSuperLayer.second;
+        }
+
+        create(segmentRangesBySuperLayer, axialStereoSegmentPairs);
+
+
+      }
+
+
+    private:
+      /// Creates the axial stereo segment pairs from the segments, which have been grouped by their superlayer id.
+      inline void create(const SegmentRangesBySuperLayer& segmentRangesBySuperLayer,
+                         std::vector<CDCAxialStereoSegmentPair>& axialStereoSegmentPairs) const {
+
+        //clear the remembered fits
+        m_axialStereoSegmentPairFilter.clear();
+
+        //Make pairs of closeby superlayers
+        for (ISuperLayerType iSuperLayer = 0; iSuperLayer < CDCWireTopology::N_SUPERLAYERS; ++iSuperLayer) {
+
+          const CDCRecoSegmentRange& startSegments = segmentRangesBySuperLayer[iSuperLayer];
+
+          //make pairs of this superlayer and the superlayer more to the inside
+          {
+            ILayerType iSuperLayerIn = iSuperLayer - 1;
+            if (isValidISuperLayer(iSuperLayerIn)) {
+              const CDCRecoSegmentRange& endSegments = segmentRangesBySuperLayer[iSuperLayerIn];
+              create(startSegments, endSegments, axialStereoSegmentPairs);
+            }
+          }
+
+          //make pairs of this superlayer and the superlayer more to the outside
+          {
+            ILayerType iSuperLayerOut = iSuperLayer + 1;
+            if (isValidISuperLayer(iSuperLayerOut)) {
+              const CDCRecoSegmentRange& endSegments = segmentRangesBySuperLayer[iSuperLayerOut];
+              create(startSegments, endSegments, axialStereoSegmentPairs);
+            }
+          }
+
+        } // end for iSuperLayer
+      }
+
+
+      /// Creates segment tiples from a combination of start segment, middle segments and end segments.
+      inline void create(const CDCRecoSegmentRange& startSegments,
+                         const CDCRecoSegmentRange& endSegments,
+                         std::vector<CDCAxialStereoSegmentPair>& axialStereoSegmentPairs) const {
+
+        CDCAxialStereoSegmentPair axialStereoSegmentPair;
+
+        for (const CDCRecoSegment2D & startSegment : startSegments) {
+          for (const CDCRecoSegment2D & endSegment : endSegments) {
+
+            const CDCRecoSegment2D* ptrStartSegment = &startSegment;
+            const CDCRecoSegment2D* ptrEndSegment = &endSegment;
+
+            if (&ptrStartSegment == &ptrEndSegment) continue; //Just for safety
+            axialStereoSegmentPair.setSegments(ptrStartSegment, ptrEndSegment);
+            axialStereoSegmentPair.clearTrajectories();
+
+            if (axialStereoSegmentPair.getTrajectory2D().isFitted()) {
+              B2ERROR("CDCAxialAxialSegmentPair still fitted after clearing.")
+              continue;
+            }
+            if (axialStereoSegmentPair.getTrajectorySZ().isFitted()) {
+              B2ERROR("CDCAxialAxialSegmentPair still fitted after clearing.")
+              continue;
+            }
+
+            if (not axialStereoSegmentPair.checkSegments()) {
+              B2ERROR("CDCAxialAxialSegmentPair containing nullptr encountered in AxialStereoSegmentPairCreator");
+              continue;
+            }
+
+            CellWeight pairWeight = m_axialStereoSegmentPairFilter.isGoodAxialStereoSegmentPair(axialStereoSegmentPair);
+            bool pairIsGood = not isNotACell(pairWeight);
+            if (pairIsGood) {
+              axialStereoSegmentPair.getAutomatonCell().setCellWeight(pairWeight);
+              axialStereoSegmentPairs.push_back(axialStereoSegmentPair);
+            }
+
+          }
+        }
+      }
+
+    private:
+
+      /// Instance of the axial to axial stereo filter.
+      mutable AxialStereoSegmentPairFilter m_axialStereoSegmentPairFilter;
+
+    };
+
+  } //end namespace CDCLocalTracking
+
+} //end namespace Belle2
+
+#endif // AXIALSTEREOSEGMENTPAIRCREATOR_H
---
+++ tracking/cdcLocalTracking/workers/include/SegmentPairTrackingWorker.h
@@ -0,0 +1,204 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2014 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef SEGMENTPAIRTRACKINGWORKER_H
+#define SEGMENTPAIRTRACKINGWORKER_H
+
+#include <vector>
+
+#include <framework/datastore/StoreArray.h>
+
+#include <tracking/cdcLocalTracking/algorithms/WeightedNeighborhood.h>
+#include <tracking/cdcLocalTracking/algorithms/NeighborhoodBuilder.h>
+
+#include <tracking/cdcLocalTracking/algorithms/Clusterizer.h>
+#include <tracking/cdcLocalTracking/algorithms/MultipassCellularPathFinder.h>
+
+#include <tracking/cdcLocalTracking/eventdata/CDCEventData.h>
+
+#include <tracking/cdcLocalTracking/creators/AxialStereoSegmentPairCreator.h>
+
+#include <tracking/cdcLocalTracking/creators/TrackCreator.h>
+#include <tracking/cdcLocalTracking/creators/SingleSegmentTrackCreator.h>
+#include <tracking/cdcLocalTracking/creators/TrackOrientator.h>
+#include <tracking/cdcLocalTracking/creators/GFTrackCandCreator.h>
+
+
+namespace Belle2 {
+  namespace CDCLocalTracking {
+
+    /// Worker for building reconstructed tracks form reconstructed segments using segment triples
+    template<class AxialStereoSegmentPairFilter, class AxialStereoSegmentPairNeighorChooser>
+    class SegmentPairTrackingWorker {
+
+    public:
+      /// Constructor.
+      SegmentPairTrackingWorker(): m_cellularPathFinder(0.0) {;}
+
+
+
+      /// Destructor.
+      ~SegmentPairTrackingWorker() {;}
+
+
+
+      /// Forwards the initialize methods of the module to the segment creator and the neighborhood builder.
+      void initialize() {
+
+#ifdef CDCLOCALTRACKING_USE_ROOT
+        StoreArray < CDCAxialStereoSegmentPairs >::registerTransient("CDCAxialStereoSegmentPairs");
+        StoreArray < CDCTrack >::registerTransient("CDCTracks");
+#endif
+
+        m_axialStereoSegmentPairCreator.initialize();
+        m_axialStereoSegmentPairNeighborhoodBuilder.initialize();
+
+      }
+
+
+
+      /// Forwards the terminate method of the module to the segment creator and the neighborhood builder.
+      void terminate() {
+        m_axialStereoSegmentPairCreator.terminate();
+        m_axialStereoSegmentPairNeighborhoodBuilder.terminate();
+      }
+
+
+
+      /// Generates tracks from the given segments.
+      inline void apply(const std::vector< CDCRecoSegment2D >& segments,
+                        StoreArray < genfit::TrackCand >& storedGFTrackCands) {
+
+        //create the segment pairs
+        B2DEBUG(100, "Combining CDCReco2DSegments to CDCAxialStereoSegmentPairs");
+        m_axialStereoSegmentPairs.clear();
+        m_axialStereoSegmentPairCreator.create(segments, m_axialStereoSegmentPairs);
+        B2DEBUG(100, "  Created " << m_axialStereoSegmentPairs.size()  << " CDCAxialStereoSegmentPair");
+
+        //create the segment pair neighborhood
+        B2DEBUG(100, "Creating the CDCAxialStereoSegmentPair neighborhood");
+        m_axialStereoSegmentPairNeighborhood.clear();
+        m_axialStereoSegmentPairNeighborhoodBuilder.create(m_axialStereoSegmentPairs, m_axialStereoSegmentPairNeighborhood);
+        B2DEBUG(100, "  Created " << m_axialStereoSegmentPairNeighborhood.size()  << " AxialStereoPairNeighborhoods");
+
+        //multiple passes if growMany is active and one track is created at a time
+        //no best candidate analysis needed
+        m_axialStereoSegmentPairTracks.clear();
+        m_cellularPathFinder.apply(m_axialStereoSegmentPairs, m_axialStereoSegmentPairNeighborhood, m_axialStereoSegmentPairTracks);
+
+        B2DEBUG(100, "  Created " << m_axialStereoSegmentPairTracks.size()  << " SegmentTripleTracks");
+
+        //reduce to plain tracks
+        B2DEBUG(100, "Reducing the AxialStereoPairTracks to CDCTracks");
+        m_tracks.clear();
+        m_trackCreator.create(m_axialStereoSegmentPairTracks, m_tracks);
+        B2DEBUG(100, "  Created " << m_tracks.size()  << " CDCTracks");
+
+        //m_singleSegmentTrackCreator.append(recoSegments, m_tracks);
+
+        m_trackOrientator.markOrientation(m_tracks);
+
+        //create the gftracks
+        B2DEBUG(100, "Creating the genfit::TrackCands");
+        m_gfTrackCandCreator.create(m_tracks, storedGFTrackCands);
+        B2DEBUG(100, "  Created " << storedGFTrackCands.getEntries()  << " genfit::TrackCands");
+
+        copyToDataStoreForDebug();
+
+
+      }
+
+
+
+
+
+    private:
+      /// Helper function to copy intermediate objects to the data store for analysis from python.
+      void copyToDataStoreForDebug() const {
+
+#ifdef CDCLOCALTRACKING_USE_ROOT
+
+        // IO for monitoring in python
+        // IO segment triples
+        B2DEBUG(100, "  Creating the StoreArray for the CDCAxialStereoSegmentPair");
+        StoreArray < CDCAxialStereoSegmentPair > storedAxialStereoSegmentPairs("CDCAxialStereoSegmentPairs");
+        storedSegmentTriples.create();
+        for (const CDCAxialStereoSegmentPair & axialStereoSegmentPair :  m_axialStereoSegmentPairs) {
+          storedAxialStereoSegmentPairs.appendNew(axialStereoSegmentPair);
+        }
+        B2DEBUG(100, "  Created " << storedAxialStereoSegmentPairs.getEntries()  << " CDCAxialStereoSegmentPairs");
+
+        // IO tracks
+        B2DEBUG(100, "  Creating the StoreArray for the CDCTracks");
+        StoreArray < CDCTrack > storedTracks("CDCTracks");
+        storedTracks.create();
+        B2DEBUG(100, "  Copying the CDCTracks to the StoreArray");
+        for (const CDCTrack & track : m_tracks) {
+          storedTracks.appendNew(track);
+        }
+
+#endif
+
+      }
+
+
+
+
+
+    private:
+      //object pools
+      /// Memory for the axial stereo segment pairs.
+      std::vector<CDCAxialStereoSegmentPair> m_axialStereoSegmentPairs;
+
+      /// Memory for the axial stereo segment pair neighborhood.
+      WeightedNeighborhood<const CDCAxialStereoSegmentPair> m_axialStereoSegmentPairNeighborhood;
+
+      /// Memory for the segment triple paths generated from the graph.
+      std::vector< std::vector<const CDCAxialStereoSegmentPair*> > m_axialStereoSegmentPairTracks;
+
+      /// Memory for the tracks generated from the paths.
+      std::vector<CDCTrack> m_tracks;
+
+
+
+      //object creators
+      /// Instance of the axial stereo segment pair creator.
+      AxialStereoSegmentPairCreator<AxialStereoSegmentPairFilter> m_axialStereoSegmentPairCreator;
+
+      //neighborhood builders
+      /// Instance of the axial stereo pair neighborhoods builder
+      NeighborhoodBuilder <CDCAxialStereoSegmentPair, AxialStereoSegmentPairNeighorChooser > m_axialStereoSegmentPairNeighborhoodBuilder;
+
+
+
+      //cellular automat
+      /// Instance of the cellular automaton.
+      MultipassCellularPathFinder<CDCAxialStereoSegmentPair> m_cellularPathFinder;
+
+      /// Instance of the track creator from paths.
+      TrackCreator m_trackCreator;
+
+      /// Instance of the leftover single segment tracks creator.
+      SingleSegmentTrackCreator m_singleSegmentTrackCreator;
+
+      /// Instance of the orientation fixer
+      TrackOrientator m_trackOrientator;
+
+      /// Instance of track to the genfit translater
+      GFTrackCandCreator m_gfTrackCandCreator;
+
+
+
+    }; // end class SegmentPairTrackingWorker
+
+  } //end namespace CDCLocalTracking
+} //end namespace Belle2
+
+#endif // SEGMENTPAIRTRACKINGWORKER_H

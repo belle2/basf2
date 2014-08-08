bcca161cc3f13c15c039b296852d432a59e39a21
Jakob Lettenbichler <jkl@jodoschka.com>
2014-08-08 09:18:09 +0000

    forgot the index file for the vxdtf-related tests in tracking
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@12154 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/tests/vxdtfTests.cc
@@ -0,0 +1,27 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2014 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Jakob Lettenbichler                                      *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include "vxdtf/trackletFilters.cc"
+#include "vxdtf/sector.cc"
+#include "vxdtf/secMapVector.cc"
+#include "vxdtf/hittfinfo.cc"
+#include "vxdtf/filterID.cc"
+#include "vxdtf/collectortfinfo.cc"
+#include "vxdtf/twohitfilters.cc"
+#include "vxdtf/trackcandidatetfinfo.cc"
+#include "vxdtf/threehitfilters.cc"
+#include "vxdtf/sectortools.cc"
+#include "vxdtf/sectortfinfo.cc"
+#include "vxdtf/fullSecID.cc"
+#include "vxdtf/fourhitfilters.cc"
+#include "vxdtf/basetfinfo.cc"
+#include "vxdtf/clustertfinfo.cc"
+#include "vxdtf/celltfinfo.cc"
+#include "vxdtf/vXDTFRawSecMap.cc"

7e85aab63474da0bf09edf9023e1c49e54e14b0c
Oliver Frost <oliver.frost@desy.de>
2014-08-13 16:42:16 +0000

    Extended to sz line fit to estimate the covariance matrix.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@12253 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/cdcLocalTracking/fitting/include/CDCSZFitter.h
+++ tracking/cdcLocalTracking/fitting/include/CDCSZFitter.h
@@ -14,19 +14,19 @@
 #include <tracking/cdcLocalTracking/typedefs/BasicTypes.h>
 #include <tracking/cdcLocalTracking/eventdata/CDCEventData.h>
 
+#include "CDCObservations2D.h"
+
 namespace Belle2 {
   namespace CDCLocalTracking {
 
-    /// Class implementing the z coordinate over travel distance fit.
+    /// Class implementing the z coordinate over travel distance line fit.
     class CDCSZFitter : public CDCLocalTracking::UsedTObject {
     public:
 
-      /** Constructor. */
-      /** This is as well the parameter free I/O constructor.
-       */
+      /// Default constructor for ROOT.
       CDCSZFitter();
 
-      /** Destructor. */
+      /// Empty destructor.
       ~CDCSZFitter();
 
       /// Getter for a standard sz line fitter instance.
@@ -40,60 +40,51 @@ namespace Belle2 {
         return trajectorySZ;
       }
 
+
+
       /// Update the given sz trajectory reconstructing the stereo segment with a near by axial segment
       void update(CDCTrajectorySZ& trajectorySZ,
                   const CDCStereoRecoSegment2D& stereoSegment,
                   const CDCTrajectory2D& axialTrajectory2D) const;
 
-
       /// Update the trajectory with a fit in the sz direction to the three dimensional hits
-      void update(CDCTrajectorySZ& trajectory, const CDCRecoHit3DVector& recohits) const {
-        std::vector<FloatType> observations;
-        fillObservations(recohits, observations);
-        update(trajectory, observations);
+      void update(CDCTrajectorySZ& trajectory,
+                  const CDCRecoHit3DVector& recoHits3D) const {
+        CDCObservations2D observationsSZ;
+        appendSZ(observationsSZ, recoHits3D);
+        update(trajectory, observationsSZ);
       }
 
-    private:
-      /// Appends the s and z values of all given hits to the given vector.
-      void fillObservations(const CDCRecoHit3DVector& recoHits3D, std::vector<FloatType>& observations) const {
-        for (const CDCRecoHit3D & recoHit3D : recoHits3D) {
-          fillObservation(recoHit3D, observations);
-        }
-      }
-
-      /// Appends the s and z value of the given hits to the given vector.
-      void fillObservation(const Belle2::CDCLocalTracking::CDCRecoHit3D& recohit,
-                           std::vector<FloatType>& observations) const {
-        observations.push_back(recohit.getPerpS());
-        observations.push_back(recohit.getRecoPos3D().z());
-      }
 
 
     private:
-      /// Update the trajectory with a fit in the sz direction to the vector of s and z values setup by fillObservations.
-      void update(CDCTrajectorySZ& trajectorySZ,
-                  std::vector<FloatType>& observations) const {
+      /// Appends the s and z values of all given hits to the observation matrix
+      void appendSZ(CDCObservations2D& observationsSZ, const CDCRecoHit3DVector& recoHits3D) const
+      { for (const CDCRecoHit3D & recoHit3D : recoHits3D) appendSZ(observationsSZ, recoHit3D); }
 
-        size_t nObservations = observations.size() / 2;
-        FloatType* rawObservations = &(observations.front());
-        updateOptimizeZDistance(trajectorySZ, rawObservations, nObservations);
+      /// Appends the s and z value of the given hit to the observation matrix
+      void appendSZ(CDCObservations2D& observationsSZ, const Belle2::CDCLocalTracking::CDCRecoHit3D& recohit) const
+      { appendSZ(observationsSZ, recohit.getPerpS(), recohit.getRecoPos3D().z()); }
 
-      }
+      /// Appends the s and z value of the given hit to the observation matrix
+      void appendSZ(CDCObservations2D& observationsSZ, const FloatType& s, const FloatType& z, const FloatType& weight = 1.0) const
+      { observationsSZ.append(s, z, 0.0, weight); }
 
-      /// Update the trajectory with a fit in the sz direction to the vector of s and z values setup by fillObservations optimizing the direct euclidian distance in sz space.
-      void updateOptimizeSZDistance(CDCTrajectorySZ& trajectorySZ,
-                                    FloatType* observations,
-                                    size_t nObservations) const;
 
-      /// Update the trajectory with a fit in the sz direction to the vector of s and z values setup by fillObservations optimizing the z distance in sz space.
-      void updateOptimizeZDistance(CDCTrajectorySZ& trajectorySZ,
-                                   FloatType* observations,
-                                   size_t nObservations) const;
 
+    public:
+      /// Update the trajectory with a fit to the observations.
+      void update(CDCTrajectorySZ& trajectorySZ, CDCObservations2D& observationsSZ) const;
+
+
+
+    private:
       /// ROOT Macro to make CDCSZFitter a ROOT class.
       ClassDefInCDCLocalTracking(CDCSZFitter, 1);
+
+
 
     }; //class
   } // end namespace CDCLocalTracking
 } // namespace Belle2
-#endif // CDCSZFITTER
+#endif // CDCSZFITTER_H
--- tracking/cdcLocalTracking/fitting/src/CDCSZFitter.cc
+++ tracking/cdcLocalTracking/fitting/src/CDCSZFitter.cc
@@ -21,11 +21,18 @@ using namespace Eigen;
 
 ClassImpInCDCLocalTracking(CDCSZFitter)
 
+
+
+
+
 CDCSZFitter::CDCSZFitter()
 {
 }
 
-/** Destructor. */
+
+
+
+
 CDCSZFitter::~CDCSZFitter()
 {
 }
@@ -54,82 +61,117 @@ const CDCSZFitter& CDCSZFitter::getFitter()
 
 
 
+namespace {
+  UncertainSZLine fitZ(const Matrix< FloatType, 3, 3 >& sumMatrixWSZ)
+  {
+    // Solve the normal equation X * n = y
+
+    Matrix< FloatType, 2, 2> sumMatrixWS = sumMatrixWSZ.block<2, 2>(0, 0);
+    Matrix< FloatType, 2, 2> invSumMatrixWS = sumMatrixWS.inverse();
+
+    Matrix< FloatType, 2, 1> sumVectorZOverWS = sumMatrixWSZ.block<2, 1>(0, 2);
+    Matrix< FloatType, 2, 1> nZOverWS = invSumMatrixWS * sumVectorZOverWS;
+
+    FloatType z0Intercept = nZOverWS(0);
+    FloatType szSlope = nZOverWS(1);
+
+    SZCovariance szCovariance;
+
+    szCovariance(iSZ, iSZ) = invSumMatrixWS(1, 1);
+    szCovariance(iZ0, iSZ) = invSumMatrixWS(0, 1); // Should be symmetric.
+    szCovariance(iSZ, iZ0) = invSumMatrixWS(1, 0); // Should be symmetric.
+    szCovariance(iZ0, iZ0) = invSumMatrixWS(0, 0);
+
+    FloatType chi2 = sumMatrixWSZ(2, 2) - z0Intercept * sumMatrixWSZ(0, 2) -  sumMatrixWSZ(1, 2) * szSlope;
+
+    return UncertainSZLine(szSlope, z0Intercept, szCovariance, chi2);
+  }
+
+
+  /// Variant without drift circles and seperating the offset before the matrix solving
+  UncertainSZLine fitSZ(const Matrix< FloatType, 3, 3 >& sumMatrixWSZ)
+  {
+
+    // Matrix of averages
+    Matrix< FloatType, 3, 3> averageMatrixWSZ = sumMatrixWSZ / sumMatrixWSZ(0);
+
+    // Measurement means
+    Matrix< FloatType, 3, 1> meansWSZ = averageMatrixWSZ.row(0);
+
+    // Covariance matrix
+    Matrix< FloatType, 3, 3> covMatrixWSZ = averageMatrixWSZ - meansWSZ * meansWSZ.transpose();
+
+
+    Matrix< FloatType, 2, 2> covMatrixSZ = covMatrixWSZ.block<2, 2>(1, 1);
+    SelfAdjointEigenSolver< Matrix<FloatType, 2, 2> > eigensolver(covMatrixSZ);
+    if (eigensolver.info() != Success) {
+      B2WARNING("SelfAdjointEigenSolver could not compute the eigen values of the observation matrix");
+    }
+
+    // the eigenvalues are generated in increasing order
+    // we are interested in the lowest one since we want to compute the normal vector of the line here
+
+    Matrix<FloatType, 3, 1> nWSZ;
+    nWSZ.middleRows<2>(1) = eigensolver.eigenvectors().col(0);
+    nWSZ(0) = -meansWSZ.middleRows<2>(1).transpose() * nWSZ.middleRows<2>(1);
+
+    Line2D szLine(nWSZ(0), nWSZ(1), nWSZ(2));
+
+
+    Matrix< FloatType, 3, 3> covN = covMatrixWSZ.inverse();
+    Matrix< FloatType, 2, 3> jacobianNToSlopeIntercept;
+
+    jacobianNToSlopeIntercept(0, 0) = -1.0 / nWSZ(2);
+    jacobianNToSlopeIntercept(1, 1) = -1.0 / nWSZ(2);
+
+    jacobianNToSlopeIntercept(0, 2) = nWSZ(0) / nWSZ(2) / nWSZ(2);
+    jacobianNToSlopeIntercept(1, 2) = nWSZ(1) / nWSZ(2) / nWSZ(2);
+
+    Matrix<FloatType, 2, 2 > covInterceptSlope = jacobianNToSlopeIntercept * covN * jacobianNToSlopeIntercept.transpose();
+
+    SZCovariance szCovariance;
+
+    szCovariance(iSZ, iSZ) = covInterceptSlope(1, 1);
+    szCovariance(iZ0, iSZ) = covInterceptSlope(0, 1); // Should be symmetric.
+    szCovariance(iSZ, iZ0) = covInterceptSlope(1, 0); // Should be symmetric.
+    szCovariance(iZ0, iZ0) = covInterceptSlope(0, 0);
+
+    FloatType chi2 = nWSZ.transpose() * sumMatrixWSZ * nWSZ;
+
+    return UncertainSZLine(szLine, szCovariance, chi2);
+
+  }
+
+}
+
+
+
+
+
 void CDCSZFitter::update(CDCTrajectorySZ& trajectorySZ,
                          const CDCStereoRecoSegment2D& stereoSegment,
                          const CDCTrajectory2D& axialTrajectory2D) const
 {
   //recostruct the stereo segment
-  CDCRecoSegment3D reconstructedStereoSegment;
+  CDCObservations2D observationsSZ;
   for (const CDCRecoHit2D & recoHit2D : stereoSegment) {
     CDCRecoHit3D recoHit3D = CDCRecoHit3D::reconstruct(recoHit2D, axialTrajectory2D);
-    reconstructedStereoSegment.push_back(recoHit3D);
+    appendSZ(observationsSZ, recoHit3D);
   }
 
-  update(trajectorySZ, reconstructedStereoSegment);
+  update(trajectorySZ, observationsSZ);
 
 }
 
 
 
-void CDCSZFitter::updateOptimizeSZDistance(CDCTrajectorySZ& trajectorySZ,
-                                           FloatType* observations,
-                                           size_t nObservations) const
+
+
+void CDCSZFitter::update(CDCTrajectorySZ& trajectorySZ,
+                         CDCObservations2D& observations2D) const
 {
-
-  Map< Matrix< FloatType, Dynamic, Dynamic, RowMajor > > eigenObservation(observations, nObservations, 2);
-
-  //do a normal line fit
-  Matrix<FloatType, Dynamic, 2> points = eigenObservation.leftCols<2>();
-
-  Matrix<FloatType, 1, 2> pointMean;
-  //RowVector2d pointMean;
-  pointMean << 0.0, 0.0;
-
-  // subtract the offset from the origin
-  pointMean = points.colwise().mean();
-
-  points = points.rowwise() - pointMean;
-
-  Matrix<FloatType, 2, 2> covarianceMatrix = points.transpose() * points;
-
-  SelfAdjointEigenSolver< Matrix<FloatType, 2, 2> > eigensolver(covarianceMatrix);
-
-  if (eigensolver.info() != Success) B2WARNING("SelfAdjointEigenSolver could not compute the eigen values of the observation matrix");
-
-  //the eigenvalues are generated in increasing order
-  //we are interested in the lowest one since we want to compute the normal vector of the line
-
-  Matrix<FloatType, 2, 1> normalToLine = eigensolver.eigenvectors().col(0);
-  //Matrix<FloatType,2,1> parallelToLine = eigensolver.eigenvectors().col(1);
-
-  FloatType offset = -pointMean * normalToLine;
-
-  // line equation is normalToLine(0) * s + normalToLine(1) * z + offset = 0
-  // the point mean lies in the line
-
-  trajectorySZ.setN(offset, normalToLine(0) , normalToLine(1));
-
+  // Matrix of weighted sums
+  Matrix< FloatType, 3, 3 > sumMatrixWSZ = observations2D.getWXYSumMatrix();
+  UncertainSZLine uncertainSZLine = fitZ(sumMatrixWSZ);
+  trajectorySZ.setSZLine(uncertainSZLine);
 }
-
-
-void CDCSZFitter::updateOptimizeZDistance(CDCTrajectorySZ& trajectorySZ,
-                                          FloatType* observations,
-                                          size_t nObservations) const
-{
-
-  Map< Matrix< FloatType, Dynamic, Dynamic, RowMajor > > eigenObservation(observations, nObservations, 2);
-
-  Matrix< FloatType, Dynamic, 1 > zObservations = eigenObservation.rightCols<1>();
-  Matrix< FloatType, Dynamic, Dynamic > offsetAndS(nObservations, 2);
-
-  //all coordiates
-  offsetAndS.col(0) = Matrix<FloatType, Dynamic, 1>::Constant(nObservations, 1.0);
-  offsetAndS.col(1) = eigenObservation.leftCols<1>();
-
-  Matrix< FloatType, 2, 1> interceptAndSlope = offsetAndS.jacobiSvd(ComputeThinU | ComputeThinV).solve(zObservations);
-
-  trajectorySZ.setSlopeIntercept(interceptAndSlope(1), interceptAndSlope(0));
-
-}
-
-

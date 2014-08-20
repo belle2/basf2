02f7d4b0c511eb346474122b39ac66a19f081e29
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2014-08-20 01:54:06 +0000

    added start up scripts
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@12416 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/apps/rocontrold/tools/rocontrold.cc
+++ daq/slc/apps/rocontrold/tools/rocontrold.cc
@@ -12,7 +12,7 @@ using namespace Belle2;
 
 int main(int argc, char** argv)
 {
-  ConfigFile config(argv[1]);
+  ConfigFile config("slowcontrol", argv[1]);
   const std::string hostname = config.get("nsm.local.host");
   const int port = config.getInt("nsm.local.port");
   const std::string name = config.get("nsm.nodename");

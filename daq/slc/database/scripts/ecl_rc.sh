623545ded96bc87c09bafbb3957aaf2aade4e9b2
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2014-08-23 09:57:53 +0000

    Updated table configuration
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@12500 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/database/scripts/ecl_rc.sh
+++ daq/slc/database/scripts/ecl_rc.sh
@@ -24,6 +24,9 @@ addParamConfig ECL ropc ${DIR}/ecl.ropc.csv
 addParamConfig DAQ hlt ${DIR}/hlt.csv
 addParamConfig DAQ storage ${DIR}/storage.csv
 addParamConfig DAQ ttd ${DIR}/ttd.csv
+addParamConfig DAQ runcontrol.node ${DIR}/ecl0102.runcontrol.node.csv
+addParamConfig DAQ runcontrol.nsmdata ${DIR}/ecl0102.runcontrol.nsmdata.csv
+addParamConfig DAQ runcontrol ${DIR}/ecl0102.runcontrol.csv
 addParamConfig DAQ runcontrol.node ${DIR}/ecl.runcontrol.node.csv
 addParamConfig DAQ runcontrol.nsmdata ${DIR}/ecl.runcontrol.nsmdata.csv
 addParamConfig DAQ runcontrol ${DIR}/ecl.runcontrol.csv

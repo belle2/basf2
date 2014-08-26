a07c46ebb69ab94383118f16d71c7965d9b38e10
Thomas Keck <t.keck@online.de>
2014-08-26 07:35:48 +0000

    Removed ParticleLoader from example files, since the loader is automatically loaded frmo the FEI
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@12596 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/examples/FEI/B+.py
+++ analysis/examples/FEI/B+.py
@@ -301,7 +301,6 @@ particles.append(p)
 
 main = create_path()
 main.add_module(register_module('RootInput'))
-main.add_module(register_module('ParticleLoader'))
 
 FullEventInterpretation(main, particles)
 
--- analysis/examples/FEI/B0.py
+++ analysis/examples/FEI/B0.py
@@ -302,7 +302,6 @@ particles.append(p)
 
 main = create_path()
 main.add_module(register_module('RootInput'))
-main.add_module(register_module('ParticleLoader'))
 
 FullEventInterpretation(main, particles)
 

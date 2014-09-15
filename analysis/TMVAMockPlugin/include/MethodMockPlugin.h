93eff36d5eb80658ea271990fe64971ca1810039
Thomas Keck <t.keck@online.de>
2014-09-15 21:05:33 +0000

    Add dummy documentation to TMVAInterface MockPlugin and MockPluginInspector
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@12880 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/TMVAMockPlugin/include/MethodMockPlugin.h
+++ analysis/TMVAMockPlugin/include/MethodMockPlugin.h
@@ -15,38 +15,95 @@ namespace Belle2 {
 
 namespace TMVA {
 
+  /**
+   * Mock TMVA Plugin for writing tests
+   */
   class MethodMockPlugin : public MethodBase {
 
   public:
+    /**
+     * Constructor
+     */
     MethodMockPlugin(const TString& jobName,
                      const TString& methodTitle,
                      DataSetInfo& theData,
                      const TString& theOption = "",
                      TDirectory* theTargetDir = 0);
 
+    /**
+     * Constructor
+     */
     MethodMockPlugin(DataSetInfo& theData,
                      const TString& theWeightFile,
                      TDirectory* theTargetDir = NULL);
 
+    /**
+     * Destructor
+     */
     virtual ~MethodMockPlugin();
 
+    /**
+     * Method
+     */
     virtual Bool_t HasAnalysisType(Types::EAnalysisType type, UInt_t numberClasses, UInt_t numberTargets);
+
+    /**
+     * Method
+     */
     void Train();
+
+    /**
+     * Method
+     */
     void Init();
+
+    /**
+     * Method
+     */
     void Reset();
 
-
+    /**
+     * Method
+     */
     void AddWeightsXMLTo(void* parent) const;
+
+    /**
+     * Method
+     */
     void ReadWeightsFromXML(void* parent);
 
     using MethodBase::ReadWeightsFromStream;
+
+    /**
+     * Method
+     */
     void ReadWeightsFromStream(std::istream& istr);
 
+
+    /**
+     * Method
+     */
     Double_t GetMvaValue(Double_t* err = 0, Double_t* errUpper = 0);
+
+    /**
+     * Method
+     */
     const Ranking* CreateRanking();
 
+
+    /**
+     * Method
+     */
     void DeclareOptions();
+
+    /**
+     * Method
+     */
     void ProcessOptions();
+
+    /**
+     * Method
+     */
     void GetHelpMessage() const;
 
   private:
--- analysis/TMVAMockPluginInspector/include/MockPluginInspector.h
+++ analysis/TMVAMockPluginInspector/include/MockPluginInspector.h
@@ -8,25 +8,57 @@
 
 namespace Belle2 {
 
+
+  /**
+   * Class inspects MockPlugin class, only used to write TMVAInterface tests
+   */
   class MockPluginInspector {
 
   public:
+
+    /**
+     * Method
+     */
     static MockPluginInspector& GetInstance();
 
+
+    /**
+     * Method
+     */
     void SetMvaValue(double _mva_value) { mva_value = _mva_value; }
 
+
+    /**
+     * Method
+     */
     std::vector< std::vector<float> > GetTrainEvents() { return train_events; }
+
+    /**
+     * Method
+     */
     std::vector<float> GetTrainEvent(unsigned int iEvent) { return train_events[iEvent]; }
+
+    /**
+     * Method
+     */
     std::vector<float> GetTestEvent() { return test_event; }
 
   private:
+
+    /**
+     * Method
+     */
     MockPluginInspector(const MockPluginInspector&);
+
+    /**
+     * Method
+     */
     MockPluginInspector() : mva_value(0) { }
 
   public:
-    float mva_value;
-    std::vector<float> test_event;
-    std::vector< std::vector<float> > train_events;
+    float mva_value; /**< returned mva value */
+    std::vector<float> test_event; /**< passed test event */
+    std::vector< std::vector<float> > train_events; /**< passed train events */
 
   };
 

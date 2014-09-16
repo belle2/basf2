e16c46d9cc108c0be0eaa60636b7e1d566cc4fd9
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2014-09-16 23:36:05 +0000

    Modified database server to new one
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@12913 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/database/scripts/init.sh
+++ daq/slc/database/scripts/init.sh
@@ -4,5 +4,5 @@ DIR=${BELLE2_LOCAL_DIR}/daq/slc/data/database
 
 cleanTables
 
-psql -U b2daq -d b2slow -h ropc02.kek.jp < ${BELLE2_LOCAL_DIR}/daq/slc/database/scripts/init.sql
+psql -U b2daq -d b2slow -h b2db1.daqnet.kek.jp < ${BELLE2_LOCAL_DIR}/daq/slc/database/scripts/init.sql
 

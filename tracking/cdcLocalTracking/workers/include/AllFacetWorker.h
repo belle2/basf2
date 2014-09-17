c177531d57d9f76e0382f5aa554eeafb69df8aad
Oliver Frost <oliver.frost@desy.de>
2014-09-17 13:49:46 +0000

    Introduced a worker that constructs all unfiltered facets to the DataStore.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@12928 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/cdcLocalTracking/workers/include/AllFacetWorker.h
@@ -0,0 +1,68 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2014 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#ifndef ALLFACETWORKER_H
+#define ALLFACETWORKER_H
+
+
+#include <tracking/cdcLocalTracking/eventdata/CDCEventData.h>
+
+#include <tracking/cdcLocalTracking/eventtopology/CDCWireHitTopology.h>
+#include <tracking/cdcLocalTracking/topology/CDCWireTopology.h>
+
+#include <framework/datastore/StoreArray.h>
+
+#include <vector>
+
+namespace Belle2 {
+  namespace CDCLocalTracking {
+
+    /// Worker for building all facets without a filter applied. This is mainly a helper to evaluate a filter.
+    class AllFacetWorker : public UsedTObject {
+
+    public:
+      /** Constructor. */
+      AllFacetWorker() {;}
+
+      /** Destructor.*/
+      ~AllFacetWorker() {;}
+
+      /// Forwards the initialize method of the module to the facet creator and the neighborhood chooser
+      void initialize() {
+#ifdef CDCLOCALTRACKING_USE_ROOT
+        StoreArray < CDCRecoFacet >::registerTransient();
+#endif
+      }
+
+      /// Forwards the terminate method of the module to the facet creator and the neighborhood chooser. Does nothing here.
+      void terminate() {;}
+
+      /// Generates all possible facets without filtering them from the event topology.
+      void generate();
+
+
+
+    private:
+      /// Helper function to copy intermediate objects to the data store for analysis from python.
+      void copyToDataStoreForDebug() const;
+
+
+    public:
+      const CDCRecoFacetCollection& getRecoFacets() const
+      { return m_recoFacets; }
+
+    private:
+      /// Memory for the constructed facets.
+      CDCRecoFacetCollection m_recoFacets;
+
+    }; // end class AllFacetWorker
+  } //end namespace CDCLocalTracking
+} //end namespace Belle2
+
+#endif //FACETSEGMENTWORKER_H_
---
+++ tracking/cdcLocalTracking/workers/src/AllFacetWorker.cc
@@ -0,0 +1,123 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2014 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include "../include/AllFacetWorker.h"
+
+#include <tracking/cdcLocalTracking/creators/FacetCreator.h>
+#include <tracking/cdcLocalTracking/algorithms/Clusterizer.h>
+
+#include <tracking/cdcLocalTracking/filters/CDCFilters.h>
+#include <tracking/cdcLocalTracking/filters/wirehit_wirehit/WireHitNeighborChooser.h>
+
+#include <framework/logging/Logger.h>
+
+using namespace std;
+using namespace Belle2;
+using namespace CDCLocalTracking;
+
+
+
+
+
+void AllFacetWorker::generate()
+{
+  // Clear all facets and regenerate them
+  m_recoFacets.clear();
+
+  const CDCWireTopology& wireTopology = CDCWireTopology::getInstance();
+  const CDCWireHitTopology& wireHitTopology = CDCWireHitTopology::getInstance();
+
+  for (const CDCWireSuperLayer & wireSuperLayer : wireTopology.getWireSuperLayers()) {
+
+    // Examine only the hit in the current superlayer
+    const CDCWireHitTopology::CDCWireHitRange wireHitsInSuperlayer = wireHitTopology.getWireHits(wireSuperLayer);
+
+    // Create the neighborhood
+    B2DEBUG(100, "Creating the CDCWireHit neighborhood");
+    WeightedNeighborhood<const CDCWireHit> m_wirehitNeighborhood;
+
+    B2DEBUG(100, "  Append clockwise neighborhood");
+    m_wirehitNeighborhood.appendUsing<WireHitNeighborChooser<CW_NEIGHBOR>>(wireHitsInSuperlayer);
+
+    B2DEBUG(100, "  Append clockwise out neighborhood");
+    m_wirehitNeighborhood.appendUsing<WireHitNeighborChooser<CW_OUT_NEIGHBOR>>(wireHitsInSuperlayer);
+
+    B2DEBUG(100, "  Append clockwise in neighborhood");
+    m_wirehitNeighborhood.appendUsing<WireHitNeighborChooser<CW_IN_NEIGHBOR>>(wireHitsInSuperlayer);
+
+    B2DEBUG(100, "  Append counter clockwise neighborhood");
+    m_wirehitNeighborhood.appendUsing<WireHitNeighborChooser<CCW_NEIGHBOR>>(wireHitsInSuperlayer);
+
+    B2DEBUG(100, "  Append counter clockwise out neighborhood");
+    m_wirehitNeighborhood.appendUsing<WireHitNeighborChooser<CCW_OUT_NEIGHBOR>>(wireHitsInSuperlayer);
+
+    B2DEBUG(100, "  Append counter clockwise in neighborhood");
+    m_wirehitNeighborhood.appendUsing<WireHitNeighborChooser<CCW_IN_NEIGHBOR>>(wireHitsInSuperlayer);
+
+    // Security check if the neighbor of hits is symmetric
+    bool isSymmetric = m_wirehitNeighborhood.isSymmetric();
+    B2DEBUG(100, "  Check symmetry " << isSymmetric);
+    B2DEBUG(100, "  allWirehitNeighbors.size() = " << m_wirehitNeighborhood.size());
+
+    if (not isSymmetric) {
+      B2WARNING("  The wire neighborhood is not symmetric");
+    }
+
+    // Create the clusters
+    B2DEBUG(100, "Creating the CDCWireHit clusters");
+    std::vector<CDCWireHitCluster> m_clustersInSuperLayer;
+    m_clustersInSuperLayer.reserve(20);
+
+    Clusterizer<CDCWireHit, CDCWireHitCluster> m_wirehitClusterizer;
+    m_wirehitClusterizer.create(wireHitsInSuperlayer, m_wirehitNeighborhood, m_clustersInSuperLayer);
+
+    B2DEBUG(100, "Created " << m_clustersInSuperLayer.size() << " CDCWireHit clusters in superlayer");
+
+
+    for (CDCWireHitCluster & cluster : m_clustersInSuperLayer) {
+
+      B2DEBUG(100, "Cluster size: " << cluster.size());
+      B2DEBUG(100, "Wire hit neighborhood size: " << m_wirehitNeighborhood.size());
+
+      // Create the facets
+      B2DEBUG(100, "Creating the CDCRecoFacets");
+
+      FacetCreator<AllFacetFilter> m_allFacetCreator;
+      m_allFacetCreator.createFacets(cluster, m_wirehitNeighborhood, m_recoFacets);
+
+      B2DEBUG(100, "  Created " << m_recoFacets.size()  << " CDCRecoFacets");
+
+    } // end for cluster
+  } // end for superlayer
+
+  copyToDataStoreForDebug();
+
+}
+
+
+
+
+
+void AllFacetWorker::copyToDataStoreForDebug() const
+{
+
+#ifdef CDCLOCALTRACKING_USE_ROOT
+  // IO for monitoring in python
+  StoreArray < CDCRecoFacet > storedRecoFacets;
+  storedRecoFacets.create(true); //Force all content to be cleared.
+  for (const CDCRecoFacet & recoFacet : m_recoFacets) {
+    storedRecoFacets.appendNew(recoFacet);
+  }
+#endif
+
+}
+
+
+

8e530928955c8f935ab9858e92c27f2dd7693305
Michael Ziegler <michael.ziegler2@kit.edu>
2014-09-19 12:46:04 +0000

    Added script to generate data for added pt=4.0 GeV
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@12960 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/validation/100_trackingEfficiency_createData.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# !/usr/bin/env python
+
+#################################################################
+#                                                               #
+#    script to simulate 10 charged muon tracks with             #
+#    fixed transverse momentum using the ParticleGun            #
+#                                                               #
+#    written by Michael Ziegler, KIT                            #
+#    michael.ziegler2@kit.edu                                   #
+#                                                               #
+#################################################################
+
+from basf2 import *
+from tracking_efficiency_helpers import run_simulation, get_generated_pt_value
+
+set_random_seed(123460)
+
+pt_value = get_generated_pt_value(8)
+
+output_filename = '../trackingEfficiency_pt_%.2fGeV.root' % pt_value
+
+print output_filename
+
+path = create_path()
+
+run_simulation(path, pt_value, output_filename)
+
+process(path)
+

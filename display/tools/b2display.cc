88a774b35cdc54ae513f277a371967ec3ed243aa
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2014-09-24 13:50:41 +0000

    Improve b2display help
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13032 b38c6c8f-bee8-465d-8448-9abe6915d496

--- display/tools/b2display.cc
+++ display/tools/b2display.cc
@@ -11,11 +11,17 @@ int main(int argc, char* argv[])
 {
   if (argc != 2 or std::string(argv[1]) == "--help") {
     std::cerr << "Usage: " << argv[0] << " input_file.root\n\n"
-              "  Show data in given input file using the event display.\n"
+              "  Show data in given input file using the event display.\n\n"
+
               "  You can also open multiple files using wildcards, though you have to\n"
-              "  pass the string with wildcards to b2display, e.g.\n"
-              "    b2display \"my/files/mdst_*.root\".\n"
-              "  Using http:// or root:// URLs is also possible.\n";
+              "  pass the string with wildcards to b2display to prevent expansion by the shell, e.g.\n"
+              "    b2display \"my/files/mdst_*.root\".\n\n"
+
+              "  Using http:// or root:// URLs is also possible.\n\n"
+
+              "  Configuration of the display can be changed from the defaults by\n"
+              "  editing display/examples/display.py.\n"
+              "  See output of 'basf2 -m Display' for available options.\n";
     return 1;
   }
   using namespace Belle2::FileSystem;

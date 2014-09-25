8eba53b40c36dbeadba69a7825a488c36ee27c1a
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2014-09-25 16:17:24 +0000

    SubEvent: Allow saving loop object to file while in subeventPath
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13063 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/core/src/SubEventModule.cc
+++ framework/core/src/SubEventModule.cc
@@ -84,10 +84,14 @@ void SubEventModule::initialize()
   //register loop object
   const DataStore::StoreEntry& arrayEntry = DataStore::Instance().getStoreEntryMap(DataStore::c_Event).at(m_loopOver.getName());
   TClass* arrayClass = static_cast<TClonesArray*>(arrayEntry.object)->GetClass();
-  DataStore::Instance().registerEntry(m_objectName, DataStore::c_Event, arrayClass, false, DataStore::c_DontWriteOut | DataStore::c_ErrorIfAlreadyRegistered);
+  DataStore::Instance().registerEntry(m_objectName, DataStore::c_Event, arrayClass, false, DataStore::c_ErrorIfAlreadyRegistered);
 
   m_moduleList = m_path->buildModulePathList();
   processInitialize(m_moduleList);
+
+  //set c_DontWriteOut to disable writing the object
+  DataStore::StoreEntry& objectEntry = DataStore::Instance().getStoreEntryMap(DataStore::c_Event).at(m_objectName);
+  objectEntry.dontWriteOut = true;
 
   //don't screw up statistics for this module
   processStatistics->startModule();

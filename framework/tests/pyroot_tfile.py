518956caac6175df73a0143ee0679a80213fd4ec
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2014-10-03 12:42:41 +0000

    add test for pyroot regression found by Thomas
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13182 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ framework/tests/pyroot_tfile.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This test catches a regression in PyRoot (in root 5.34/19+ ?)
+from basf2 import *
+import ROOT
+from ROOT import Belle2
+
+path = Belle2.FileSystem.findFile('framework/tests/chaintest_2.root')
+f = ROOT.TFile(path)
+tree = f.Get('tree')
+
+tree.GetEntry(0)
+hits = tree.PXDSimHits
+nentries = hits.GetEntriesFast()
+print nentries
+hit = hits[0]
+hit.Dump()

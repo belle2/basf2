e7027c5bc4ac9364ef6137e7280f0f929b92b8a9
Luis Pesantez <pesantez@uni-bonn.de>
2014-10-10 06:11:26 +0000

    Add rho gamma to validation tests
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13246 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ analysis/validation/test0_Bd_rho0gamma_GENSIMRECtoDST.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from basf2 import *
+from simulation import add_simulation
+from reconstruction import add_reconstruction
+
+set_random_seed(12345)
+
+main = create_path()
+
+# specify number of events to be generated
+eventinfosetter = register_module('EventInfoSetter')
+eventinfosetter.param('evtNumList', [500])
+eventinfosetter.param('runList', [1])
+eventinfosetter.param('expList', [1])
+main.add_module(eventinfosetter)
+
+# generate Bd->Kstgamma events
+evtgeninput = register_module('EvtGenInput')
+evtgeninput.param('boost2LAB', True)
+evtgeninput.param('userDECFile', os.environ['BELLE2_LOCAL_DIR']
+                  + '/generators/evtgen/decayfiles/Bd_rho0gamma,pipi.dec')
+
+main.add_module(evtgeninput)
+
+# detector simulation,  digitisers and clusterisers
+add_simulation(main)
+
+# reconstruction
+add_reconstruction(main)
+
+# write out the full dst output
+output = register_module('RootOutput')
+output.param('outputFileName', '../Bd_rho0gamma_GENSIMRECtoDST.dst.root')
+main.add_module(output)
+
+# Go!
+process(main)
+
+# Print call statistics
+print statistics

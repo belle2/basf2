ee73905aaeaef70977c84e558c59eaf4209725ba
Marko Staric <marko.staric@ijs.si>
2014-10-10 13:07:02 +0000

    tags for additional BG types added to SimHitBase.h
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13253 b38c6c8f-bee8-465d-8448-9abe6915d496

--- simulation/dataobjects/include/SimHitBase.h
+++ simulation/dataobjects/include/SimHitBase.h
@@ -25,32 +25,46 @@ namespace Belle2 {
     */
   class SimHitBase : public RelationsObject {
   public:
-    /** Enum for background tags. */
-    enum BG_TAG { bg_none         = 0, /**< No background.*/
-                  bg_Coulomb_LER  = 1, /**< Coulomb LER.*/
-                  bg_Coulomb_HER  = 2, /**< Coulomb HER.*/
-                  bg_RBB_LER      = 3, /**< RBB LER.*/
-                  bg_RBB_HER      = 4, /**< RBB_HER.*/
-                  bg_Touschek_LER = 5, /**< Touschek LER.*/
-                  bg_Touschek_HER = 6, /**< Touschek HER.*/
-                  bg_twoPhoton    = 7, /**< 2-photon */
-                  bg_other        = 99 /**< Other type of background.*/
+    /**
+     * Enum for background tags.
+     */
+    enum BG_TAG { bg_none             = 0,  /**< No background */
+                  bg_Coulomb_LER      = 1,  /**< Coulomb LER */
+                  bg_Coulomb_HER      = 2,  /**< Coulomb HER */
+                  bg_RBB_LER          = 3,  /**< Radiative Bhabha LER */
+                  bg_RBB_HER          = 4,  /**< Radiative Bhabha HER */
+                  bg_Touschek_LER     = 5,  /**< Touschek LER */
+                  bg_Touschek_HER     = 6,  /**< Touschek HER */
+                  bg_twoPhoton        = 7,  /**< 2-photon */
+                  bg_RBB_gamma        = 8,  /**< Gammas from radiative Bhabha */
+                  bg_RBB_LER_far      = 9,  /**< Radiative Bhabha far LER */
+                  bg_RBB_HER_far      = 10, /**< Radiative Bhabha far HER */
+                  bg_Touschek_LER_far = 11, /**< Touschek far LER */
+                  bg_Touschek_HER_far = 12, /**< Touschek far HER */
+                  bg_SynchRad_LER     = 13, /**< Synchrotron radiation LER */
+                  bg_SynchRad_HER     = 14, /**< Synchrotron radiation HER */
+                  bg_other            = 99  /**< Other type of background */
                 };
-    /** Constructor */
+    /**
+     * Constructor
+     */
     SimHitBase(): m_backgroundTag(bg_none) {}
 
-    /** Set the background tag.
+    /**
+     * Set background tag.
      * @param backgroundTag The desired setting of the background tag.
      */
     virtual void setBackgroundTag(unsigned int backgroundTag)
     { m_backgroundTag = static_cast<unsigned short>(backgroundTag); }
 
-    /** Get the background tag.
+    /**
+     * Get background tag.
      * @return The background tag value.
      */
     virtual unsigned short getBackgroundTag() const { return m_backgroundTag; }
 
-    /** The method to shift the SimHit in time.
+    /**
+     * The method to shift the SimHit in time.
      * Re-implement for your subdetector's SimHit class if time-aware background
      * is needed.
      * @param delta The desired time shift.
@@ -58,8 +72,8 @@ namespace Belle2 {
     virtual void shiftInTime(float /*delta*/) {}
 
   private:
-    /** Background tag */
-    unsigned short m_backgroundTag;
+
+    unsigned short m_backgroundTag; /**< Background tag */
 
     ClassDef(SimHitBase, 2)
   };

90de42479e7d1dbd98ee7bfa7166c99a3692030e
Marko Staric <marko.staric@ijs.si>
2014-10-13 12:21:07 +0000

    histogram names modified - all differ now
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13289 b38c6c8f-bee8-465d-8448-9abe6915d496

--- top/validation/makePlots.C
+++ top/validation/makePlots.C
@@ -61,66 +61,66 @@ void kaonID(TTree* top)
 
   TH1* h = 0;
 
-  h = new TH1F("h01", "log likelihood difference for pions (p < 2 GeV/c)", 
+  h = new TH1F("h101", "log likelihood difference for pions (p < 2 GeV/c)", 
 		      100, -200, 200);
   h->GetXaxis()->SetTitle("log L_{K} - log L_{#pi}");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("logL.K-logL.pi >> h01", select && TCut("abs(PDG) == 211 && p < 2"));
+  top->Draw("logL.K-logL.pi >> h101", select && TCut("abs(PDG) == 211 && p < 2"));
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Difference between kaon and pion log likelihoods for true pions below 2 GeV/c"));
   h->GetListOfFunctions()->Add(new TNamed("Check", 
       "Most of the entries should be distributed at negative values."));
   h->Write();
 
-  h = new TH1F("h02", "log likelihood difference for kaons (p < 2 GeV/c)", 
+  h = new TH1F("h102", "log likelihood difference for kaons (p < 2 GeV/c)", 
 		      100, -200, 200);
   h->GetXaxis()->SetTitle("log L_{K} - log L_{#pi}");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("logL.K-logL.pi >> h02", select && TCut("abs(PDG) == 321 && p < 2"));
+  top->Draw("logL.K-logL.pi >> h102", select && TCut("abs(PDG) == 321 && p < 2"));
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Difference between kaon and pion log likelihoods for true kaons below 2 GeV/c"));
   h->GetListOfFunctions()->Add(new TNamed("Check", 
       "Most of the entries should be distributed at positive values."));
   h->Write();
 
-  h = new TH1F("h03", "log likelihood difference for pions (p > 2 GeV/c)", 
+  h = new TH1F("h103", "log likelihood difference for pions (p > 2 GeV/c)", 
 		      100, -100, 100);
   h->GetXaxis()->SetTitle("log L_{K} - log L_{#pi}");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("logL.K-logL.pi >> h03", select && TCut("abs(PDG) == 211 && p > 2"));
+  top->Draw("logL.K-logL.pi >> h103", select && TCut("abs(PDG) == 211 && p > 2"));
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Difference between kaon and pion log likelihoods for true pions above 2 GeV/c"));
   h->GetListOfFunctions()->Add(new TNamed("Check", 
       "Most of the entries should be distributed at negative values."));
   h->Write();
 
-  h = new TH1F("h04", "log likelihood difference for kaons (p > 2 GeV/c)", 
+  h = new TH1F("h104", "log likelihood difference for kaons (p > 2 GeV/c)", 
 		      100, -100, 100);
   h->GetXaxis()->SetTitle("log L_{K} - log L_{#pi}");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("logL.K-logL.pi >> h04", select && TCut("abs(PDG) == 321 && p > 2"));
+  top->Draw("logL.K-logL.pi >> h104", select && TCut("abs(PDG) == 321 && p > 2"));
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Difference between kaon and pion log likelihoods for true kaons above 2 GeV/c"));
   h->GetListOfFunctions()->Add(new TNamed("Check", 
       "Most of the entries should be distributed at positive values."));
   h->Write();
 
-  h = new TH2F("h05", "log likelihood difference vs. momentum for pions", 
+  h = new TH2F("h105", "log likelihood difference vs. momentum for pions", 
 		      100, 0, 4, 100, -200, 200);
   h->GetXaxis()->SetTitle("p [GeV/c]");
   h->GetYaxis()->SetTitle("log L_{K} - log L_{#pi}");
-  top->Draw("logL.K-logL.pi:p >> h05", select && TCut("abs(PDG) == 211"));
+  top->Draw("logL.K-logL.pi:p >> h105", select && TCut("abs(PDG) == 211"));
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Difference between kaon and pion log likelihoods versus momentum for true pions."));
   h->GetListOfFunctions()->Add(new TNamed("Check", 
     "Most of the entries should be distributed below zero."));
   h->Write();
 
-  h = new TH2F("h06", "log likelihood difference vs. momentum for kaons", 
+  h = new TH2F("h106", "log likelihood difference vs. momentum for kaons", 
 		      100, 0, 4, 100, -200, 200);
   h->GetXaxis()->SetTitle("p [GeV/c]");
   h->GetYaxis()->SetTitle("log L_{K} - log L_{#pi}");
-  top->Draw("logL.K-logL.pi:p >> h06", select && TCut("abs(PDG) == 321"));
+  top->Draw("logL.K-logL.pi:p >> h106", select && TCut("abs(PDG) == 321"));
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Difference between kaon and pion log likelihoods versus momentum for true kaons."));
   h->GetListOfFunctions()->Add(new TNamed("Check", 
@@ -139,11 +139,11 @@ void numberOfPhotons(TTree* top)
 
   TH1* h = 0;
 
-  h = new TH1F("h1", "number of detected photons per track", 
+  h = new TH1F("h201", "number of detected photons per track", 
 		      100, 0, 200);
   h->GetXaxis()->SetTitle("number of photons");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("numPhot >> h1", select); 
+  top->Draw("numPhot >> h201", select); 
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Number of detected photons per track."));
   h->GetListOfFunctions()->Add(new TNamed("Check", 
@@ -151,11 +151,11 @@ void numberOfPhotons(TTree* top)
     "long tail is normal."));
   h->Write();
 
-  h = new TH2F("h2", "number of detected photons per track vs polar angle", 
+  h = new TH2F("h202", "number of detected photons per track vs polar angle", 
 	       100, -0.6, 0.9, 100, 0, 100);
   h->GetXaxis()->SetTitle("cos #theta");
   h->GetYaxis()->SetTitle("number of photons");
-  top->Draw("numPhot:cth >> h2", select); 
+  top->Draw("numPhot:cth >> h202", select); 
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Number of detected photons per track versus polar angle."));
   h->GetListOfFunctions()->Add(new TNamed("Check", 
@@ -179,11 +179,11 @@ void trackResolutions(TTree* top)
 
   trackSelect = select && "abs(extHit.y - barHit.y) < 0.01 && p < 2";
 
-  h = new TH1F("h01", "extrapolated track resolution in z for p < 2 GeV/c", 
+  h = new TH1F("h301", "extrapolated track resolution in z for p < 2 GeV/c", 
 		      100, -10, 10);
   h->GetXaxis()->SetTitle("#Deltaz [mm]");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("(extHit.z - barHit.z)*10 >> h01", trackSelect);
+  top->Draw("(extHit.z - barHit.z)*10 >> h301", trackSelect);
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Extrapolated minus true z of the track at the bar inner surface "
     "for p < 2 GeV/c."));
@@ -192,11 +192,11 @@ void trackResolutions(TTree* top)
     "and may cause severe PID performance degradation."));
   h->Write();
 
-  h = new TH1F("h02", "extrapolated track resolution in x (e.g. r#phi) for p < 2 GeV/c", 
+  h = new TH1F("h302", "extrapolated track resolution in x (e.g. r#phi) for p < 2 GeV/c", 
 		      100, -10, 10);
   h->GetXaxis()->SetTitle("#Deltax [mm]");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("(extHit.x - barHit.x)*10 >> h02", trackSelect);
+  top->Draw("(extHit.x - barHit.x)*10 >> h302", trackSelect);
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Extrapolated minus true x of the track at the bar inner surface "
     "for p < 2 GeV/c."));
@@ -205,11 +205,11 @@ void trackResolutions(TTree* top)
     "and may cause severe PID performance degradation."));
   h->Write();
 
-  h = new TH1F("h03", "extrapolated track resolution in #theta for p < 2 GeV/c", 
+  h = new TH1F("h303", "extrapolated track resolution in #theta for p < 2 GeV/c", 
 		      100, -20, 20);
   h->GetXaxis()->SetTitle("#Delta#theta [mrad]");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("(extHit.theta - barHit.theta)*1000 >> h03", trackSelect);
+  top->Draw("(extHit.theta - barHit.theta)*1000 >> h303", trackSelect);
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Extrapolated minus true theta of the track at the bar inner surface "
     "for p < 2 GeV/c."));
@@ -218,11 +218,11 @@ void trackResolutions(TTree* top)
     "and may cause severe PID performance degradation."));
   h->Write();
 
-  h = new TH1F("h04", "extrapolated track resolution in #phi for p < 2 GeV/c", 
+  h = new TH1F("h304", "extrapolated track resolution in #phi for p < 2 GeV/c", 
 		      100, -20, 20);
   h->GetXaxis()->SetTitle("#Delta#phi [mrad]");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("(extHit.phi - barHit.phi)*1000 >> h04", trackSelect);
+  top->Draw("(extHit.phi - barHit.phi)*1000 >> h304", trackSelect);
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Extrapolated minus true phi of the track at the bar inner surface "
     "for p < 2 GeV/c."));
@@ -231,11 +231,11 @@ void trackResolutions(TTree* top)
     "and may cause severe PID performance degradation."));
   h->Write();
 
-  h = new TH1F("h05", "extrapolated track resolution in time-of-flight for p < 2 GeV/c", 
+  h = new TH1F("h305", "extrapolated track resolution in time-of-flight for p < 2 GeV/c", 
 		      100, -100, 100);
   h->GetXaxis()->SetTitle("#Deltat [ps]");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("(extHit.time - barHit.time)*1000 >> h05", trackSelect && 
+  top->Draw("(extHit.time - barHit.time)*1000 >> h305", trackSelect && 
 	    TCut("extHit.PDG == barHit.PDG"));
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Extrapolated minus true time-of-flight of the track at the bar inner surface "
@@ -249,11 +249,11 @@ void trackResolutions(TTree* top)
 
   trackSelect = select && "abs(extHit.y - barHit.y) < 0.01 && p > 2";
 
-  h = new TH1F("h06", "extrapolated track resolution in z for p > 2 GeV/c", 
+  h = new TH1F("h306", "extrapolated track resolution in z for p > 2 GeV/c", 
 		      100, -10, 10);
   h->GetXaxis()->SetTitle("#Deltaz [mm]");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("(extHit.z - barHit.z)*10 >> h06", trackSelect);
+  top->Draw("(extHit.z - barHit.z)*10 >> h306", trackSelect);
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Extrapolated minus true z of the track at the bar inner surface "
     "for p > 2 GeV/c."));
@@ -262,11 +262,11 @@ void trackResolutions(TTree* top)
     "and may cause severe PID performance degradation."));
   h->Write();
 
-  h = new TH1F("h07", "extrapolated track resolution in x (e.g. r#phi) for p > 2 GeV/c", 
+  h = new TH1F("h307", "extrapolated track resolution in x (e.g. r#phi) for p > 2 GeV/c", 
 		      100, -10, 10);
   h->GetXaxis()->SetTitle("#Deltax [mm]");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("(extHit.x - barHit.x)*10 >> h07", trackSelect);
+  top->Draw("(extHit.x - barHit.x)*10 >> h307", trackSelect);
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Extrapolated minus true x of the track at the bar inner surface "
     "for p > 2 GeV/c."));
@@ -275,11 +275,11 @@ void trackResolutions(TTree* top)
     "and may cause severe PID performance degradation."));
   h->Write();
 
-  h = new TH1F("h08", "extrapolated track resolution in #theta for p > 2 GeV/c", 
+  h = new TH1F("h308", "extrapolated track resolution in #theta for p > 2 GeV/c", 
 		      100, -20, 20);
   h->GetXaxis()->SetTitle("#Delta#theta [mrad]");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("(extHit.theta - barHit.theta)*1000 >> h08", trackSelect);
+  top->Draw("(extHit.theta - barHit.theta)*1000 >> h308", trackSelect);
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Extrapolated minus true theta of the track at the bar inner surface "
     "for p > 2 GeV/c."));
@@ -288,11 +288,11 @@ void trackResolutions(TTree* top)
     "and may cause severe PID performance degradation."));
   h->Write();
 
-  h = new TH1F("h09", "extrapolated track resolution in #phi for p > 2 GeV/c", 
+  h = new TH1F("h309", "extrapolated track resolution in #phi for p > 2 GeV/c", 
 		      100, -20, 20);
   h->GetXaxis()->SetTitle("#Delta#phi [mrad]");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("(extHit.phi - barHit.phi)*1000 >> h09", trackSelect);
+  top->Draw("(extHit.phi - barHit.phi)*1000 >> h309", trackSelect);
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Extrapolated minus true phi of the track at the bar inner surface "
     "for p > 2 GeV/c."));
@@ -301,11 +301,11 @@ void trackResolutions(TTree* top)
     "and may cause severe PID performance degradation."));
   h->Write();
 
-  h = new TH1F("h10", "extrapolated track resolution in time-of-flight for p > 2 GeV/c", 
+  h = new TH1F("h310", "extrapolated track resolution in time-of-flight for p > 2 GeV/c", 
 		      100, -100, 100);
   h->GetXaxis()->SetTitle("#Deltat [ps]");
   h->GetYaxis()->SetTitle("entries/bin");
-  top->Draw("(extHit.time - barHit.time)*1000 >> h10", trackSelect && 
+  top->Draw("(extHit.time - barHit.time)*1000 >> h310", trackSelect && 
 	    TCut("extHit.PDG == barHit.PDG"));
   h->GetListOfFunctions()->Add(new TNamed("Description", 
     "Extrapolated minus true time-of-flight of the track at the bar inner surface "

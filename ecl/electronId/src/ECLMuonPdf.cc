a55a7d49f1867f71c8c0893eb63dc40ddd11bcee
Kenkichi Miyabayashi <miyabaya@cc.nara-wu.ac.jp>
2014-10-16 03:11:46 +0000

    Marko Staric suggestion, treatment for large p.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13353 b38c6c8f-bee8-465d-8448-9abe6915d496

--- ecl/electronId/src/ECLMuonPdf.cc
+++ ecl/electronId/src/ECLMuonPdf.cc
@@ -26,6 +26,7 @@ void  ECLMuonPdf::init(const char* parametersFileName)
 
 double ECLMuonPdf::pdf(double eop, double p) const
 {
+  if (p > 100.) p = 100.; // very large p can not fit into int().
   int i = int(p / 0.250);
   if (i == 0) i = 1;
   if (i > 7) i = 7;

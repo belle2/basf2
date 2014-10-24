26466df124b07c80fad960e784743597a798c185
Satoru Yamada <satoru.yamada@kek.jp>
2014-10-24 06:23:10 +0000

    Add Convert2RawDet.cc for request from KLM group
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13457 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/rawdata/modules/include/Convert2RawDet.h
@@ -0,0 +1,69 @@
+//+
+// File : Convert2RawDet.h
+// Description : Module to convert from RawCOPPER or RawDataBlock to RawDetector objects
+//
+// Author : Satoru Yamada Itoh, IPNS, KEK
+// Date : 24 - Oct - 2014
+//-
+
+#ifndef CONVERT2RAWDET_H
+#define CONVERT2RAWDET_H
+
+#include <framework/core/Module.h>
+
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/datastore/StoreArray.h>
+
+#include <daq/rawdata/modules/DAQConsts.h>
+#include <rawdata/dataobjects/RawFTSW.h>
+#include <rawdata/dataobjects/RawTLU.h>
+#include <rawdata/dataobjects/RawCDC.h>
+#include <rawdata/dataobjects/RawSVD.h>
+#include <rawdata/dataobjects/RawECL.h>
+#include <rawdata/dataobjects/RawEPID.h>
+#include <rawdata/dataobjects/RawBPID.h>
+#include <rawdata/dataobjects/RawKLM.h>
+#include <rawdata/dataobjects/RawCOPPER.h>
+
+#include <framework/dataobjects/EventMetaData.h>
+
+
+namespace Belle2 {
+
+  /*! A class definition of a module to convert from RawCOPPER or RawDataBlock to RawDetector objects */
+
+  class Convert2RawDetModule : public Module {
+
+    // Public functions
+  public:
+
+    //! Constructor / Destructor
+    Convert2RawDetModule();
+    virtual ~Convert2RawDetModule();
+
+    //! Module functions to be called from main process
+    virtual void initialize();
+
+    //! Module functions to be called from event process
+    virtual void beginRun();
+    virtual void event();
+    virtual void endRun();
+    virtual void terminate();
+
+  private:
+
+    //! function to register data buffer in DataStore as RawDetector
+    void convertDataObject(RawDataBlock* raw_dblk);
+
+    // Data members
+  private:
+
+    //! No. of sent events
+    int m_nevt;
+
+  };
+
+} // end namespace Belle2
+
+#endif // MODULEHELLO_H

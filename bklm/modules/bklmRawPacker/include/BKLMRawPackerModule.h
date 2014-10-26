c74d35f8d374f54e119f8d2e978bb275dad9c79a
Anselm Vossen <avossen@indiana.edu>
2014-10-26 20:07:52 +0000

    added dummy raw packer
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13499 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ bklm/modules/bklmRawPacker/include/BKLMRawPackerModule.h
@@ -0,0 +1,97 @@
+//+
+// File : BKLMRawPackerModule.h
+// Description : Module to store dummy data in RawKLM object
+//
+// Author : Anselm Vossen, based on Satoru Yamada's DummyDataPacker
+//-
+
+#ifndef BKLMRAWPACKER_H
+#define BKLMRAWPACKER_H
+
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include <framework/core/Module.h>
+#include <framework/pcore/EvtMessage.h>
+#include <framework/pcore/MsgHandler.h>
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/dataobjects/EventMetaData.h>
+
+#include <rawdata/dataobjects/RawCOPPER.h>
+#include <rawdata/dataobjects/RawKLM.h>
+
+
+namespace Belle2 {
+
+  /*! A class definition of an input module for Sequential ROOT I/O */
+
+  class BKLMRawPackerModule : public Module {
+
+    // Public functions
+  public:
+
+    //! Constructor / Destructor
+    BKLMRawPackerModule();
+    virtual ~BKLMRawPackerModule();
+
+    //! Module functions to be called from main process
+    virtual void initialize();
+
+    //! Module functions to be called from event process
+    virtual void event();
+
+
+  protected :
+    //! check data contents
+    //    virtual void checkData(RawDataBlock* raw_datablk, unsigned int* eve_copper_0);
+
+    //    StoreArray<RawFTSW> raw_ftswarray;
+
+    //! # of events in a run
+    int max_nevt;
+
+    //! Node ID
+    int m_nodeid;
+
+    //! Number of events
+    int n_basf2evt;
+
+    //! Messaage handler
+    //    MsgHandler* m_msghandler;
+
+    /*     //! Compression Level */
+    /*     int m_compressionLevel; */
+
+    //! Event Meta Data
+    StoreObjPtr<EventMetaData> m_eventMetaDataPtr;
+
+    //! RawKLM array
+    StoreArray<RawKLM> rawklmarray;
+    //! format the data
+    //! @param[in] channel rpc channel
+    //! @param[in] axis phi or z
+    //! @param[in] lane slot in the crate for the rpc
+    //! @param[in] tdc measured tdc value
+    //! @param[in] charge num photons for the scintillators
+    //! @param[in] ctime the ctime (or is this already in the copper header)
+    //! @param[out] bword1 first 16bit word
+    //! @param[out] bword2 second 16bit word
+    //! @param[out] bword3 third 16bit word
+    //! @param[out] bword4 forth 16bit word
+    void formatData(int channel, int axis, int lane, int tdc, int charge, int ctime, unsigned short& bword1, unsigned short& bword2, unsigned short& bword3, unsigned short& bword4);
+
+    //    //! put datapackets for a track together
+    //    //! param[in] channel The channel that will be set in all layers
+    //     //! param[out] bword1-bword4 the words
+    //    void getTrack(int channel, short& bword1, short& bword2, short& bword3, short& bword4);
+
+
+  };
+
+} // end namespace Belle2
+
+#endif // MODULEHELLO_H
---
+++ bklm/modules/bklmRawPacker/src/BKLMRawPackerModule.cc
@@ -0,0 +1,228 @@
+//+
+// File : BKLMRawPackerModule.cc
+// Description : Module to store dummy data in RawKLM object
+//
+// Author : Anselm Vossen, based on Satoru Yamada's BKLMRawPackerModule
+//-
+
+#include <bklm/modules/bklmRawPacker/BKLMRawPackerModule.h>
+
+using namespace std;
+using namespace Belle2;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(BKLMRawPacker)
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+BKLMRawPackerModule::BKLMRawPackerModule() : Module()
+{
+  ///Set module properties
+  setDescription("an Example to pack data to a RawKLM object");
+
+  ///  maximum # of events to produce( -1 : inifinite)
+  addParam("MaxEventNum", max_nevt, "Maximum event number to make", -1);
+
+  ///  maximum # of events to produce( -1 : inifinite)
+  addParam("NodeID", m_nodeid, "Node ID", 0);
+
+  B2INFO("BKLMRawPackerModule: Constructor done.");
+
+  // initialize event #
+  n_basf2evt = 0;
+
+
+}
+
+
+
+BKLMRawPackerModule::~BKLMRawPackerModule()
+{
+
+}
+
+
+void BKLMRawPackerModule::initialize()
+{
+  B2INFO("BKLMRawPackerModule: initialize() started.");
+
+  // Open message handler
+  //  m_msghandler = new MsgHandler(m_compressionLevel);
+
+  /// Initialize EvtMetaData
+  m_eventMetaDataPtr.registerInDataStore();
+
+  rawklmarray.registerPersistent();
+
+  B2INFO("BKLMRawPackerModule: initialize() done.");
+}
+
+
+
+
+void BKLMRawPackerModule::event()
+{
+  cout << "pack the event.." << endl;
+  //    Make RawKLM array
+  rawklmarray.create();
+
+  //
+  // Fill event info (These values will be stored in RawHeader )
+  //
+  RawCOPPERPackerInfo rawcprpacker_info;
+  rawcprpacker_info.exp_num = 1;
+  rawcprpacker_info.run_subrun_num = 2; // run number : 14bits, subrun # : 8bits
+  rawcprpacker_info.eve_num = n_basf2evt;
+  rawcprpacker_info.node_id = m_nodeid;
+  rawcprpacker_info.tt_ctime = 0x7123456;
+  rawcprpacker_info.tt_utime = 0xF1234567;
+  rawcprpacker_info.b2l_ctime = 0x7654321;
+
+  //
+  // Prepare buffer to fill dummy data
+  //
+
+  //hopefully we can fill as many as fill as many as we want, lets do one per layer to make tracks
+  for (int iLay = 1; iLay < 14; iLay++)    {
+    for (int iAx = 0; iAx < 2; iAx++) {
+      for (int ifwd = 0; ifwd < 2; ifwd++) {
+        RawKLM* raw_klm = rawklmarray.appendNew();
+
+        int* buf1, *buf2, *buf3, *buf4;
+        int nwords_1st = 2, nwords_2nd = 2, nwords_3rd = 2, nwords_4th = 2;
+        //basf2 words are 32 bits, whereas in the dataformat documentation for the KLM assumes 16bits
+
+        unsigned short bword1 = 0;
+        unsigned short bword2 = 0;
+        unsigned short bword3 = 0;
+        unsigned short bword4 = 0;
+
+        buf1 = new int[ nwords_1st];
+        buf1[0] = 0;
+        buf1[1] = 0;
+        int iChannelNr = 10;
+        if (ifwd == 1)
+          iChannelNr = 20;
+        //tdc, charge, ctime
+        formatData(iChannelNr, iAx, iLay, 20, 30, 70, bword1, bword2, bword3, bword4);
+        cout << "buf1: " << bword1 << ", " << bword2 << ", " << bword3 << ", " << bword4 << endl;
+        buf1[0] |= bword1;
+        buf1[0] |= ((bword2 << 16));
+        buf1[1] |= bword3;
+        buf1[1] |= ((bword4 << 16));
+
+        cout << "word1: " << buf1[0] << " word2: " << buf1[1] << endl;
+        buf2 = new int[ nwords_2nd];
+        buf2[0] = 0;
+        buf2[1] = 0;
+
+        iChannelNr = 25;
+        if (ifwd == 1)
+          iChannelNr = 30;
+        formatData(iChannelNr, iAx, iLay, 20, 30, 70, bword1, bword2, bword3, bword4);
+        buf2[0] |= bword1;
+        buf2[0] |= ((bword2 << 16) & 0xFFFF0000);
+        buf2[1] |= bword3;
+        buf2[1] |= ((bword4 << 16) & 0xFFFF0000);
+        cout << "buf2: " << bword1 << ", " << bword2 << ", " << bword3 << ", " << bword4 << endl;
+        cout << "word1: " << buf2[0] << " word2: " << buf2[1] << endl;
+        buf3 = new int[ nwords_3rd];
+        buf3[0] = 0;
+        buf3[1] = 0;
+
+        formatData(iChannelNr, iAx, iLay, 20, 30, 70, bword1, bword2, bword3, bword4);
+        iChannelNr = 35;
+        if (ifwd == 1)
+          iChannelNr = 40;
+        formatData(iChannelNr, iAx, iLay, 20, 30, 70, bword1, bword2, bword3, bword4);
+        cout << "buf3: " << bword1 << ", " << bword2 << ", " << bword3 << ", " << bword4 << endl;
+        cout << "word1: " << buf3[0] << " word2: " << buf3[1] << endl;
+        buf3[0] |= bword1;
+        buf3[0] |= ((bword2 << 16) & 0xFFFF0000);
+        buf3[1] |= bword3;
+        buf3[1] |= ((bword4 << 16) & 0xFFFF0000);
+
+
+        buf4 = new int[ nwords_4th];
+        buf4[0] = 0;
+        buf4[1] = 0;
+
+        iChannelNr = 17;
+        if (ifwd == 1)
+          iChannelNr = 18;
+        formatData(iChannelNr, iAx, iLay, 20, 30, 70, bword1, bword2, bword3, bword4);
+        cout << "buf4: " << bword1 << ", " << bword2 << ", " << bword3 << ", " << bword4 << endl;
+        cout << "word1: " << buf4[0] << " word2: " << buf4[1] << endl;
+        buf4[0] |= bword1;
+        buf4[0] |= ((bword2 << 16) & 0xFFFF0000);
+        buf4[1] |= bword3;
+        buf4[1] |= ((bword4 << 16) & 0xFFFF0000);
+
+
+
+
+        raw_klm->PackDetectorBuf(buf1, nwords_1st,
+                                 buf2, nwords_2nd,
+                                 buf3, nwords_3rd,
+                                 buf4, nwords_4th,
+                                 rawcprpacker_info);
+
+        delete [] buf1;
+        delete [] buf2;
+        delete [] buf3;
+        delete [] buf4;
+      }
+    }
+  }
+  //
+  // Update EventMetaData : Not affect on the output
+  //
+  m_eventMetaDataPtr.create();
+  m_eventMetaDataPtr->setExperiment(rawcprpacker_info.exp_num);
+  m_eventMetaDataPtr->setRun(rawcprpacker_info.run_subrun_num);
+  m_eventMetaDataPtr->setEvent(n_basf2evt);
+
+  printf("Event # %.8d\n", n_basf2evt);
+  fflush(stdout);
+
+  //
+  // Monitor
+  //
+  if (max_nevt >= 0) {
+    if (n_basf2evt >= max_nevt && max_nevt > 0) {
+      printf("[DEBUG] RunStop was detected. ( Setting:  Max event # %d ) Processed Event %d \n",
+             max_nevt , n_basf2evt);
+      fflush(stdout);
+      m_eventMetaDataPtr->setEndOfData();
+    }
+  }
+
+  n_basf2evt++;
+  return;
+}
+
+
+
+void BKLMRawPackerModule::formatData(int channel, int axis, int lane, int tdc, int charge, int ctime, unsigned short& bword1, unsigned short& bword2, unsigned short& bword3, unsigned short& bword4)
+{
+
+  bword1 = 0;
+  bword2 = 0;
+  bword3 = 0;
+  bword4 = 0;
+  bword1 |= (channel & 0x7F);
+  bword1 |= ((axis & 1) << 7);
+  bword1 |= ((lane & 0x1F) << 8);
+  bword2 |= (ctime & 0xFFFF);
+  bword3 |= (tdc & 0x07FF);
+  bword4 |= (charge & 0xFFF);
+
+}
+//    void getTrack(int channel, short& bword1, short& bword2, short& bword3, short& bword4)
+//    {
+//
+//    }

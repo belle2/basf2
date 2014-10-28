2e67f8a8ba1ad49f175a1c1160f57e21e5dc5961
Satoru Yamada <satoru.yamada@kek.jp>
2014-10-28 09:41:17 +0000

    Add RecvStream0.py to run basf2 program before eb0
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13518 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/rawdata/examples/RecvStream0.py
+++ daq/rawdata/examples/RecvStream0.py
@@ -4,15 +4,15 @@
 ######################################################
 #
 # RecvStream0.py : DAQ comoponent running on a readout PC.
-# This component receive data from eb0 and send data to eb1tx.
+# This component receive data from COPPER and send data to eb0 process.
 #
 ######################################################
 
 from basf2 import *
 import sys
 argvs = sys.argv
-if len(argvs) != 4:
-    print 'Usage : RecvStream1.py <Use NSM(Network Shared Memory)? yes=1/no=0> <port # of eb0> <NSM nodename>'
+if len(argvs) != 6:
+    print 'Usage : RecvStream0.py <COPPER hostname> <port # of COPPER> <Use NSM(Network Shared Memory)? yes=1/no=0> <port # of eb0> <NSM nodename>'
     sys.exit()
 
 # Set the log level to show only error and fatal messages
@@ -20,58 +20,37 @@ set_log_level(LogLevel.ERROR)
 # set_log_level(LogLevel.INFO)
 
 # Modules
-receiver = register_module('DeSerializerPC')
+receiver = register_module('DeSerializerPrePC')
 dump = register_module('RootOutput')
-perf = register_module('DAQPerf')
 output = register_module('PrintData')
 
 # Receiver
 receiver.param('NodeID', 3)  # ROPC node ID (only used for NSM)
 receiver.param('NumConn', 1)
-receiver.param('HostNameFrom', ['localhost', 'cpr007'])
+receiver.param('HostNameFrom', [argvs[1], 'aiueohost'])
 receiver.param('PortFrom', [int(argvs[2]), int(argvs[2])])
 receiver.param('EventDataBufferWords', 4801)
 receiver.param('MaxTime', -1.)
 receiver.param('MaxEventNum', -1)
-receiver.param('NodeName', argvs[3])  # node name for NSM
-use_shm_flag = int(argvs[1])
+receiver.param('NodeName', argvs[5])  # node name for NSM
+use_shm_flag = int(argvs[3])
 receiver.param('UseShmFlag', use_shm_flag)
-
-# Histo Module
-# histo = register_module('HistoManager')
-# main.add_module (histo)
-histo = register_module('DqmHistoManager')
-histo.param('HostName', 'ropc01')
-histo.param('Port', 9991)
-histo.param('DumpInterval', 1000)
-histo.param('HistoFileName', 'ropc_histofile.root')
-
-# Monitor module
-monitor = register_module('MonitorDataCOPPER')
 
 # Dump
 dump.param('outputFileName', 'root_output.root')
 
 # Sender
 sender = register_module('Serializer')
-sender.param('DestPort', 36000)
+sender.param('DestPort', int(argvs[4]))
 sender.param('LocalHostName', 'localhost')
-
-# Perf
-perf.param('Cycle', 100000)
-perf.param('MonitorSize', True)
 
 # Create main path
 main = create_path()
 
 # Add modules to main path
 main.add_module(receiver)
-# main.add_module(histo)
-# main.add_module(monitor)
 # main.add_module(dump)
 main.add_module(sender)
-# main.add_module(output)
-# main.add_module(perf)
 
 # Process all events
 process(main)

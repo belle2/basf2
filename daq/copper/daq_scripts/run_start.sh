9ade25523fb7be83b34a3a878418d92eb39ec99f
Satoru Yamada <satoru.yamada@kek.jp>
2014-10-29 01:39:50 +0000

    Update for uing DeSerializerPrePC.cc
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13539 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/copper/daq_scripts/run_start.sh
+++ daq/copper/daq_scripts/run_start.sh
@@ -7,12 +7,21 @@
 # slot a : 1, slot b : 2, slot c : 4, slot d : 8
 # e.g. slot abcd -> bitflag=15, slot bd -> bit flag=10
 
-#/usr/bin/xterm -fn 7x14 -geometry 102x10+0+242 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr001 1 1 0 hogehoge1; sleep 3000000;" &
-/usr/bin/xterm -fn 7x14 -geometry 102x10+0+442 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr002 5002 3 0 hogehoge2; sleep 3000000;" &
-#/usr/bin/xterm -fn 7x14 -geometry 102x10+0+242 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr005 1 8 0 hogehoge3; sleep 3000000;" &
+#
+# basf2 on COPPER
+#
+/usr/bin/xterm -fn 7x14 -geometry 102x10+0+142 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr001 5001 1 0 hogehoge2; sleep 3000000;" &
+/usr/bin/xterm -fn 7x14 -geometry 102x10+0+342 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh cpr007 5007 1 0 hogehoge2; sleep 3000000;" &
 
 #
-# basf2 on ROPC
+# basf2 program BEFORE eb0(event builder 0) on a readout PC
 #
-#    print 'Usage : RecvStream1.py <Use NSM(Network Shared Memory)? yes=1/no=0> <port # of eb0> <NSM nodename>'
+#    print 'Usage : RecvStream0.py <COPPER hostname> <Use NSM(Network Shared Memory)? yes=1/no=0> <port # of eb0> <NSM nodename>
+/usr/bin/xterm -fn 7x14 -geometry 102x10+400+242 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/RecvStream0.sh cpr001 0 32001 hogehoge1000; sleep 3000000;" &
+/usr/bin/xterm -fn 7x14 -geometry 102x10+400+442 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/RecvStream0.sh cpr007 0 32007 hogehoge1000; sleep 3000000;" &
+
+#
+# basf2 program AFTER eb0(event builder 0) on a readout PC
+#
+#    print 'Usage : RecvStream1.py <Use NSM(Network Shared Memory)? yes=1/no=0> <port # of eb0> <NSM nodename>
 ${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/RecvStream1.sh 0 5101 hogehoge1000

633490c57530daaa437f085bb3472184fedc987c
Thomas Keck <t.keck@online.de>
2014-11-02 14:10:19 +0000

    Add some docu
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13719 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/modules/TMVAExpert/src/TMVAExpertModule.cc
+++ analysis/modules/TMVAExpert/src/TMVAExpertModule.cc
@@ -46,8 +46,8 @@ namespace Belle2 {
     addParam("signalProbabilityName", m_signalProbabilityName, "Name under which the signal probability is stored in the ExtraInfo of the Particle object.");
     addParam("signalClass", m_signalClass, "Class which is considered as signal. e.g. the pdg of the Particle which is considered signal if "
              "you trained the method with pdg as target variable. Or 1 if you trained with isSignal as target.", 1);
-    addParam("signalFraction", m_signalFraction, "signalFraction to calculate probability, -1 if no transformation of the method output should be performed, -2 if training signal/background ratio should be used. If you want to use "
-             "this feature, you have to set the option createMVAPDFs in the TMVATeacher or the method config string", -1.0f);
+    addParam("signalFraction", m_signalFraction, "signalFraction to calculate probability, -1 if no transformation of the method output should be performed, -2 if training signal/background ratio should be used. WARNING: If you want to use "
+             "this feature (so any other value than -1), you have to set the options createMVAPDFs and NbinsMVAPdf in the method config string provided to TMVATeacher to reasonable values! The default values of TMVA will result in a unusable classifer output!", -1.0f);
     std::map<int, unsigned int> defaultInverseSamplingRates;
     addParam("inverseSamplingRates", m_inverseSamplingRates, "Map of class id and inverse sampling rate for this class.", defaultInverseSamplingRates);
 

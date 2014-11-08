6fa17aa5fb9387ad48b37bd45dc2f069c4ad1d41
Martin Ritter <martin.ritter@lmu.de>
2014-11-08 06:16:22 +0000

    Make add_simulation more robust
    - add a function check_simulation() which does some basic sanity checks on
      the simulation path: It checks that Gearbox, Geometry and FullSim are
      on the path exactly once and in the right order.
    - add_simulation(): if Gearbox, Geometry or FullSim are already present
      on the path do not add them again. Also call check_simulation() on the
      path to make sure that the required modules are in the necessary order
    - no checks are performed for BeamBkgMixer and the Digitizers as they
      are not necessary for running simulation jobs and it should be
      possible to have them in the path more than once
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13802 b38c6c8f-bee8-465d-8448-9abe6915d496

--- simulation/scripts/simulation.py
+++ simulation/scripts/simulation.py
@@ -4,29 +4,70 @@
 from basf2 import *
 
 
-def add_simulation(
-    path,
-    components=None,
-    bkgfiles=None,
-    bkgcomponents=None,
-    ):
+def _has_module(path, module_type):
+    "Check if a module of a given type is already in the path"
+    result = module_type in (e.type() for e in path.modules())
+    if result:
+        B2WARNING("add_simulation: Module '%s' already present in path, "
+                  "skipping" % module_type)
+    return result
+
+
+def check_simulation(path):
+    """Check if the minimum number of modules required for simulation are in
+    the path and in the correct order"""
+    required = ['Gearbox', 'Geometry', 'FullSim']
+    found = []
+
+    # make a list of all required modules in the path
+    for module in path.modules():
+        module_type = module.type()
+        # if required add to list of found modules
+        if module_type in required:
+            # unless it is already in there
+            if module_type in found:
+                B2ERROR("Duplicate module in path: %s" % module_type)
+            else:
+                found.append(module.type())
+
+    if len(required) != len(found):
+        #Apparently at least one module is missing
+        for r in required:
+            if r not in found:
+                B2ERROR("No '%s' module found but needed for simulation" % r)
+    #We have all modules but do they have the correct order?
+    elif required != found:
+        B2ERROR("Simulation modules in wrong order. Should be '%s' but is '%s'"
+                % (", ".join(required), ", ".join(found)))
+
+
+def add_simulation(path, components=None, bkgfiles=None, bkgcomponents=None):
     """
     This function adds the standard simulation modules to a path.
     """
 
     # geometry parameter database
-    gearbox = register_module('Gearbox')
-    path.add_module(gearbox)
+    if not _has_module(path, 'Gearbox'):
+        gearbox = register_module('Gearbox')
+        path.add_module(gearbox)
 
     # detector geometry
-    geometry = register_module('Geometry')
-    if components:
-        geometry.param('components', components)
-    path.add_module(geometry)
+    if not _has_module(path, 'Geometry'):
+        geometry = register_module('Geometry')
+        if components:
+            geometry.param('components', components)
+        path.add_module(geometry)
 
     # detector simulation
-    g4sim = register_module('FullSim')
-    path.add_module(g4sim)
+    if not _has_module(path, 'FullSim'):
+        g4sim = register_module('FullSim')
+        path.add_module(g4sim)
+
+    check_simulation(path)
+
+    # no checks are performed for BeamBkgMixer and the Digitizers as they are
+    # not necessary for running simulation jobs and it should be possible to
+    # have them in the path more than once
 
     # background mixing
     if bkgfiles:
@@ -40,47 +81,45 @@ def add_simulation(
         path.add_module(bkgmixer)
 
     # PXD digitization
-    if components == None or 'PXD' in components:
+    if components is None or 'PXD' in components:
         pxd_digitizer = register_module('PXDDigitizer')
         path.add_module(pxd_digitizer)
         pxd_clusterizer = register_module('PXDClusterizer')
         path.add_module(pxd_clusterizer)
 
     # SVD digitization
-    if components == None or 'SVD' in components:
+    if components is None or 'SVD' in components:
         svd_digitizer = register_module('SVDDigitizer')
         path.add_module(svd_digitizer)
         svd_clusterizer = register_module('SVDClusterizer')
         path.add_module(svd_clusterizer)
 
     # CDC digitization
-    if components == None or 'CDC' in components:
+    if components is None or 'CDC' in components:
         cdc_digitizer = register_module('CDCDigitizer')
         path.add_module(cdc_digitizer)
 
     # TOP digitization
-    if components == None or 'TOP' in components:
+    if components is None or 'TOP' in components:
         top_digitizer = register_module('TOPDigitizer')
         path.add_module(top_digitizer)
 
     # ARICH digitization
-    if components == None or 'ARICH' in components:
+    if components is None or 'ARICH' in components:
         arich_digitizer = register_module('ARICHDigitizer')
         path.add_module(arich_digitizer)
 
     # ECL digitization
-    if components == None or 'ECL' in components:
+    if components is None or 'ECL' in components:
         ecl_digitizer = register_module('ECLDigitizer')
         path.add_module(ecl_digitizer)
 
     # BKLM digitization
-    if components == None or 'BKLM' in components:
+    if components is None or 'BKLM' in components:
         bklm_digitizer = register_module('BKLMDigitizer')
         path.add_module(bklm_digitizer)
 
     # EKLM digitization
-    if components == None or 'EKLM' in components:
+    if components is None or 'EKLM' in components:
         eklm_digitizer = register_module('EKLMDigitizer')
         path.add_module(eklm_digitizer)
-
-

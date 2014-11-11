43ce8bdcc1e744a5734614730f095e01b5b2eaa9
Oliver Frost <oliver.frost@desy.de>
2014-11-11 17:15:40 +0000

    More cosmics validation.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13876 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/validation/cosmicsTrackingValidation.py
+++ tracking/validation/cosmicsTrackingValidation.py
@@ -14,6 +14,7 @@ CONTACT = 'oliver.frost@desy.de'
 N_EVENTS = 100000
 
 import basf2
+basf2.set_random_seed(1337)
 
 import os
 import sys
@@ -46,9 +47,9 @@ class ValidationPlot(object):
         self.lower_bound = lower_bound
         self.n_bins = n_bins
 
-        self.description = description
-        self.check = check
-        self.contact = contact
+        self._description = description
+        self._check = check
+        self._contact = contact
 
         self._xlabel = ''
         self._ylabel = ''
@@ -56,29 +57,60 @@ class ValidationPlot(object):
 
         self.histogram = None
 
-    def fill(self, xs, ys=None):
+    @staticmethod
+    def is_binary(xs):
+        is_boolean = all(isinstance(x, bool) for x in xs)
+        is_one_or_zero = all(x == 0 or x == 1 for x in xs)
+        return is_boolean or is_one_or_zero
+
+    def fill(
+        self,
+        xs,
+        ys=None,
+        weights=None,
+        ):
+
         # Handle data content of the histgram
         name = self.name
 
-        if self.lower_bound is None:
-            self.lower_bound = min(xs)
+        if self.is_binary(xs):
+            # if xs is a binary variable setup the histogram to
+            # have only two bins with the right boundaries.
+            if self.lower_bound is None:
+                self.lower_bound = 0
+
+            if self.upper_bound is None:
+                self.upper_bound = 1.0000000000000002
+
+            if self.n_bins is None:
+                self.n_bins = 2
+        else:
+
+            if self.lower_bound is None:
+                self.lower_bound = min(xs)
+
+            if self.upper_bound is None:
+                self.upper_bound = max(xs)
+
+            if self.n_bins is None:
+                n_data = len(x)
+                rice_n_bins = ceil(2.0 * pow(n_data, 1.0 / 3.0))
+                self.n_bins = rice_n_bins
+
         lower_bound = self.lower_bound
-
-        if self.upper_bound is None:
-            self.upper_bound = max(xs)
         upper_bound = self.upper_bound
-
-        if self.n_bins is None:
-            n_data = len(x)
-            rice_n_bins = ceil(2.0 * pow(n_data, 1.0 / 3.0))
-            self.n_bins = rice_n_bins
         n_bins = self.n_bins
 
         if ys is not None:
             histogram = ROOT.TProfile(name, '', self.n_bins, lower_bound,
                                       upper_bound)
-            for (x, y) in zip(xs, ys):
-                histogram.Fill(float(x), float(y))
+            Fill = histogram.Fill
+            if weights is None:
+                for (x, y) in zip(xs, ys):
+                    Fill(float(x), float(y))
+            else:
+                for (x, y, weight) in zip(xs, ys, weights):
+                    Fill(float(x), float(y), float(weight))
 
             max_y = max(ys)
             min_y = min(ys)
@@ -90,8 +122,13 @@ class ValidationPlot(object):
 
             histogram = ROOT.TH1F(name, '', self.n_bins, lower_bound,
                                   upper_bound)
-            for x in xs:
-                histogram.Fill(float(x))
+            Fill = histogram.Fill
+            if weights is None:
+                for x in xs:
+                    Fill(float(x))
+            else:
+                for (x, weight) in zip(xs, weights):
+                    Fill(float(x), float(weight))
 
         # Now handle to style of the histogram
         root_description = ROOT.TNamed('Descrition', self.description)
@@ -145,6 +182,42 @@ class ValidationPlot(object):
         self._ylabel = ylabel
         if self.histogram is not None:
             self.histogram.SetYTitle(ylabel)
+
+    @property
+    def contact(self):
+        return self._contact
+
+    @contact.setter
+    def contact(self, contact):
+        self._contact = contact
+        if self.histogram is not None:
+            found_obj = self.histogram.FindObject('Contact')
+            if found_obj:
+                found_obj.SetTitle(contact)
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, description):
+        self._description = description
+        if self.histogram is not None:
+            found_obj = self.histogram.FindObject('Description')
+            if found_obj:
+                found_obj.SetTitle(description)
+
+    @property
+    def check(self):
+        return self._check
+
+    @check.setter
+    def check(self, check):
+        self._check = check
+        if self.histogram is not None:
+            found_obj = self.histogram.FindObject('Check')
+            if found_obj:
+                found_obj.SetTitle(check)
 
     def draw(self):
         self.histogram.Draw()
@@ -230,10 +303,12 @@ class CosmicsTrackingValidationModule(basf2.Module):
     def initialize(self):
         self.trackMatchLookUp = Belle2.TrackMatchLookUp('MCTrackCands')
 
-        self.isMatchedPRTracks = []
+        self.pr_clones_and_matches = []
+        self.pr_matches = []
+        self.pr_seen_in_vxds = []
 
-        self.isMatchedMCTracks = []
-        self.mcAbsTransversMomentums = []
+        self.mc_transverse_momenta = []
+        self.mc_matches = []
 
     def event(self):
         """Event method"""
@@ -242,62 +317,105 @@ class CosmicsTrackingValidationModule(basf2.Module):
 
         trackCands = Belle2.PyStoreArray('TrackCands')
         if trackCands:
-            print 'Number of pattern recognition tracks', \
-                trackCands.getEntries()
+            # print 'Number of pattern recognition tracks', \
+            #    trackCands.getEntries()
 
             for (iTrackCand, trackCand) in enumerate(trackCands):
-                isMatched = trackMatchLookUp.isMatchedPRTrackCand(trackCand)
-                self.isMatchedPRTracks.append(isMatched)
+                is_matched = trackMatchLookUp.isMatchedPRTrackCand(trackCand)
+                is_clone = trackMatchLookUp.isClonePRTrackCand(trackCand)
+                seen_in_vxd = False
+                if is_matched or is_clone:
+                    mcParticle = \
+                        trackMatchLookUp.getRelatedMCParticle(trackCand)
+                    if mcParticle:
+                        seenInDetector = mcParticle.getSeenInDetector()
+
+                        pxd_detector_set = \
+                            Belle2.Const.DetectorSet(Belle2.Const.PXD)
+                        svd_detector_set = \
+                            Belle2.Const.DetectorSet(Belle2.Const.SVD)
+
+                        seen_in_pxd = seenInDetector.contains(pxd_detector_set)
+                        seen_in_svd = seenInDetector.contains(svd_detector_set)
+                        seen_in_vxd = seen_in_pxd | seen_in_vxd
+
+                self.pr_clones_and_matches.append(is_matched or is_clone)
+                self.pr_matches.append(is_matched)
+                self.pr_seen_in_vxds.append(seen_in_vxd)
 
         mcTrackCands = Belle2.PyStoreArray('MCTrackCands')
         if trackCands:
-            print 'Number of Monte Carlo tracks', mcTrackCands.getEntries()
+            # print 'Number of Monte Carlo tracks', mcTrackCands.getEntries()
 
             for mcTrackCand in mcTrackCands:
-                isMatched = trackMatchLookUp.isMatchedMCTrackCand(mcTrackCand)
-                self.isMatchedMCTracks.append(isMatched)
+                is_matched = trackMatchLookUp.isMatchedMCTrackCand(mcTrackCand)
 
                 mcParticle = trackMatchLookUp.getRelatedMCParticle(mcTrackCand)
                 momentum = mcParticle.getMomentum()
-                absTransversMomentum = momentum.Perp()
-                self.mcAbsTransversMomentums.append(absTransversMomentum)
+                transverse_momentum = momentum.Perp()
+
+                self.mc_matches.append(is_matched)
+                self.mc_transverse_momenta.append(transverse_momentum)
 
     def terminate(self):
-        track_finding_efficiency = 1.0 * sum(self.isMatchedMCTracks) \
-            / len(self.isMatchedMCTracks)
+        track_finding_efficiency = 1.0 * sum(self.mc_matches) \
+            / len(self.mc_matches)
 
-        fake_rate = 1.0 - 1.0 * sum(self.isMatchedPRTracks) \
-            / len(self.isMatchedPRTracks)
+        fake_rate = 1.0 - 1.0 * sum(self.pr_clones_and_matches) \
+            / sum(self.pr_clones_and_matches)
+
+        clone_rate = 1.0 - 1.0 * sum(self.pr_matches) \
+            / sum(self.pr_clones_and_matches)
 
         cosmics_figures_of_merit = \
             ValidationFiguresOfMerit('Cosmics_track_finding_efficiency')
         cosmics_figures_of_merit['finding_efficiency'] = \
             track_finding_efficiency
         cosmics_figures_of_merit['fake_rate'] = fake_rate
+        cosmics_figures_of_merit['clone_rate'] = clone_rate
         cosmics_figures_of_merit.description = \
             """
 finding_efficiency - the ratio of matched Monte Carlo tracks to all Monte Carlo tracks <br/>
-fake_rate - ratio of unmatched (clone, background, ghost) pattern recognition track to all pattern recognition tracks
+fake_rate - ratio of  pattern recognition tracks that are not related to a particle (background, ghost) to all pattern recognition tracks <br/>
+clone_rate - ratio of clones divided the number of tracks that are related to a particle (clones and matches) <br/>
 """
         cosmics_figures_of_merit.check = \
             'The efficiency should be well above 0.9. The fake rate should be well below 0.1'
         cosmics_figures_of_merit.contact = CONTACT
 
+        print cosmics_figures_of_merit
+
         efficiency_profile = \
             ValidationPlot('Cosmics_track_finding_efficiency_by_momentum',
                            n_bins=50)
-        efficiency_profile.fill(self.mcAbsTransversMomentums,
-                                self.isMatchedMCTracks)
+        efficiency_profile.fill(self.mc_transverse_momenta, self.mc_matches)
+
         efficiency_profile.xlabel = 'Momentum (GeV)'
-        efficiency_profile.xlabel = 'Efficiency'
+        efficiency_profile.ylabel = 'Efficiency'
         efficiency_profile.description = 'Not a serious plot yet.'
         efficiency_profile.check = ''
         efficiency_profile.contact = CONTACT
         efficiency_profile.draw()
 
+        clone_rate_by_impact = \
+            ValidationPlot('Cosmics_clone_rate_by_seen_in_vxd')
+        clone_rate_by_impact.fill(self.pr_seen_in_vxds, [not x for x in
+                                  self.pr_matches],
+                                  weights=self.pr_clones_and_matches)
+
+        clone_rate_by_impact.xlabel = 'Seen in vxd (bool)'
+        clone_rate_by_impact.ylabel = 'Clone rate'
+
+        clone_rate_by_impact.description = 'Not a serious plot yet.'
+        clone_rate_by_impact.check = ''
+        clone_rate_by_impact.contact = CONTACT
+
+        clone_rate_by_impact.draw()
+
         output_file = ROOT.TFile(VALIDATION_OUTPUT_FILE, 'recreate')
         cosmics_figures_of_merit.write()
         efficiency_profile.write()
+        # clone_rate_by_impact.write()
         output_file.Close()
 
 

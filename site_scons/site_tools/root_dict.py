11755493919190298d1f36596a17a5cf684b8abb
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2014-11-15 11:04:07 +0000

    improve building of root dictionaries
    allows link requests to namespaces, structs
    warn if header cannot be found. May be O.K. in some cases, can be suppressed via '//implicit' comment
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@13974 b38c6c8f-bee8-465d-8448-9abe6915d496

--- site_scons/site_tools/root_dict.py
+++ site_scons/site_tools/root_dict.py
@@ -6,9 +6,18 @@ import re
 from SCons.Builder import Builder
 from SCons.Scanner.C import CScanner
 
+# everything that has #pragma link C++ .* Belle2::.*
+linkdef_everything = \
+    re.compile(r'^#pragma\s+link\s+C\+\+\s+[\w]*\s+Belle2::.*$', re.M)
+
 # regular expression to find class names in linkdef files
 linkdef_class_re = \
-    re.compile('^#pragma\\s+link\\s+C\\+\\+\\s+class\\s+Belle2::([\\w]*::)?([\\w<>,\\*]+)[+-]?\\!?;.*$'
+    re.compile(r'^#pragma\s+link\s+C\+\+\s+[\w]*\s+Belle2::([\w]*::)?([\w]*).*[+-]?\!?;.*$'
+               , re.M)
+
+# link requests with '// implicit' comment (doesn't require header file)
+linkdef_implicit = \
+    re.compile(r'^#pragma\s+link\s+C\+\+\s+[\w]*\s+Belle2::.*;\s*//\s*implicit\s*$'
                , re.M)
 
 
@@ -26,14 +35,40 @@ def linkdef_emitter(target, source, env):
     # loop over class names and construct the corresponding header file names
     sources = []
     contents = source[0].get_text_contents()
-    for entry in linkdef_class_re.findall(contents):
-        include_base = entry[1].split('<')[0] + '.h'
+    for line in contents.split('\n'):
+        # first check if this is looks like an actual request to create a dictionary
+        if linkdef_everything.search(line) is None:
+            continue
+
+        match = linkdef_class_re.search(line)
+        if match is None:
+            raise RuntimeError("%s contains '%s' which we couldn't parse. The syntax may be incorrect, or the build system may lack support for the feature you're using."
+                                % (str(source[0]), line))
+
+        namespace = match.group(1)  # or possibly a class, but it might match the header file
+        classname = match.group(2)
+        if not classname:
+            raise RuntimeError("%s contains '%s' without class name?"
+                               % (str(source[0]), str(line)))
+
+        is_implicit = not linkdef_implicit.search(line) is None
+        if is_implicit:
+            continue  # do not look for a header for this request
+
+        include_base = classname + '.h'
         header_file = os.path.join(source_dir, include_base)
         if not os.path.isfile(header_file):
             header_file = os.path.join(os.environ.get('BELLE2_RELEASE_DIR', ''
                                        ), header_file)
         if not os.path.isfile(header_file):
-            include_base = entry[0].split(':')[0] + '.h'
+            if not namespace:
+                # no //implicit for suppression found...
+                print "%s contains '%s' where we couldn't find a header file. If dictionary compilation fails, this might be the reason. For classes residing in other directories and already included via other link requests, add '// implicit' at the end to suppress this message." \
+                    % (str(source[0]), str(line))
+                continue
+
+            # remove trailing '::', add '.h'
+            include_base = namespace.split(':')[0] + '.h'
         include_file = os.path.join(include_dir, include_base)
         if not include_file in sources:
             sources.append(include_file)

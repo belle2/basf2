f466f119462c1d0b146077ff02614c4a915ebcd0
Eugenio Paoloni <eugenio.paoloni@pi.infn.it>
2014-11-20 15:57:40 +0000

    New features: additional shortucts for equality test
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@14096 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingVXD/FilterTools/include/Shortcuts.h
+++ tracking/trackFindingVXD/FilterTools/include/Shortcuts.h
@@ -14,6 +14,7 @@
 #include "tracking/trackFindingVXD/FilterTools/Range.h"
 #include "tracking/trackFindingVXD/FilterTools/LowerBoundedSet.h"
 #include "tracking/trackFindingVXD/FilterTools/UpperBoundedSet.h"
+#include "tracking/trackFindingVXD/FilterTools/SingleElementSet.h"
 
 #include "tracking/trackFindingVXD/FilterTools/VoidObserver.h"
 #include "tracking/trackFindingVXD/FilterTools/Filter.h"
@@ -57,6 +58,27 @@ namespace Belle2 {
   operator > (Arithmetic  upperBound, const Var&)
   {
     return Filter< Var, UpperBoundedSet<Arithmetic>, VoidObserver >(UpperBoundedSet<Arithmetic> (upperBound));
+  }
+
+  template < class Var, class Val  >
+  // the following typename expands to Filter< Var, SingleElementSet<Val>, VoidObserver>
+  // if Var derives from SelectionVariable
+  typename std::enable_if < std::is_base_of< SelectionVariable<typename Var::argumentType, typename Var::variableType>, Var>::value,
+           Filter< Var, SingleElementSet<Val>, VoidObserver> >::type
+           operator == (const Var& ,  Val v)
+  {
+    return Filter< Var, SingleElementSet<Val>, VoidObserver >(SingleElementSet<Val> (v));
+  }
+
+
+  template < class Var, class Val  >
+  // the following typename expands to Filter< Var, SingleElementSet<Val>, VoidObserver>
+  // if Var derives from SelectionVariable
+  typename std::enable_if < std::is_base_of< SelectionVariable<typename Var::argumentType, typename Var::variableType>, Var>::value,
+           Filter< Var, SingleElementSet<Val>, VoidObserver> >::type
+           operator == (Val val, const Var& var)
+  {
+    return var == val;
   }
 
 

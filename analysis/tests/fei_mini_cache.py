c3a04b555536b1676f1ed73fd7909b232b53c7b6
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2014-11-27 13:33:07 +0000

    add FEI test with actor caching
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@14163 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ analysis/tests/fei_mini_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+import tempfile
+import shutil
+import glob
+from basf2 import *
+from ROOT import Belle2
+
+inputFile = Belle2.FileSystem.findFile('analysis/tests/mdst_r10142.root')
+steeringFile = Belle2.FileSystem.findFile('analysis/tests/fei_mini_steeringfile.internal')
+
+tempdir = tempfile.mkdtemp()
+print tempdir
+os.chdir(tempdir)
+
+shutil.copy(inputFile, "inputdata.root")
+
+cmd = "basf2 " + steeringFile + " -i inputdata.root -o inputdata.root -- -cache cache.pkl --preload"
+
+#fsp variablestontuple
+assert 0 == os.system(cmd)
+#fsp TMVATeacher
+assert 0 == os.system(cmd)
+#fsp training and D hists
+assert 0 == os.system(cmd)
+assert len(glob.glob('weights/*')) == 4
+assert len(glob.glob('CutHistograms_D0*.root')) == 1
+#D channels ignored.
+assert 0 == os.system(cmd)
+assert len(glob.glob('D0*.root')) == 0
+
+shutil.rmtree(tempdir)

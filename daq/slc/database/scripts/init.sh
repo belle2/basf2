8ebaafa1800910423e1f9b97623ea0d4d6369f9a
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2014-12-03 07:38:23 +0000

    fixed to used database.host as database hostname for init script
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@14295 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/database/scripts/init.sh
+++ daq/slc/database/scripts/init.sh
@@ -4,5 +4,7 @@ DIR=${BELLE2_LOCAL_DIR}/daq/slc/data/database
 
 cleanTables
 
-psql -U b2daq -d b2slow -h b2db1.daqnet.kek.jp < ${BELLE2_LOCAL_DIR}/daq/slc/database/scripts/init.sql
+dbhost=`getconfig slowcontrol database.host`
+
+psql -U b2daq -d b2slow -h ${dbhost} < ${BELLE2_LOCAL_DIR}/daq/slc/database/scripts/init.sql
 

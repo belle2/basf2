df7ea958bcb724712592fae7616a4b35dc437c2d
Oliver Frost <oliver.frost@desy.de>
2014-12-19 15:26:20 +0000

    Factored validation run such that it can be specialised for different finder setups.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@14548 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/validation/cosmicsTrackingValidation.py
+++ tracking/validation/cosmicsTrackingValidation.py
@@ -13,6 +13,8 @@ VALIDATION_OUTPUT_FILE = 'CosmicsTrackingValidation.root'
 CONTACT = 'oliver.frost@desy.de'
 N_EVENTS = 10000
 
+TRACKING_MAILING_LIST = 'tracking@belle2.kek.jp'
+
 import basf2
 basf2.set_random_seed(1337)
 
@@ -36,8 +38,6 @@ from ROOT import Belle2
 
 
 def main():
-    """Function to be executed during the validation run"""
-
     argument_parser = argparse.ArgumentParser()
 
     # Indication if tracks should be fitted.
@@ -51,99 +51,162 @@ def main():
                                  ], default='Geant4',
                                  help='Geometry to be used with Genfit.')
 
-    argument_parser.add_argument('-p', '--pulls', action='store_true',
-                                 help='Plot pulls of the curvature.')
-
     argument_parser.add_argument('-s', '--show', action='store_true',
                                  help='Show generated plots in a TBrowser immediatly.'
                                  )
 
     arguments = argument_parser.parse_args()
 
-    # Compose basf2 module path #
-    #############################
-    main_path = basf2.create_path()
-
-    # Master module
-    eventInfoSetterModule = basf2.register_module('EventInfoSetter')
-    eventInfoSetterModule.param({'evtNumList': [N_EVENTS], 'runList': [1],
-                                'expList': [1]})
-    main_path.add_module(eventInfoSetterModule)
-
-    # Progress module
-    progressModule = basf2.register_module('Progress')
-    main_path.add_module(progressModule)
-
-    # Cosmics generator
-    cosmicsModule = basf2.register_module('Cosmics')
-    main_path.add_module(cosmicsModule)
-
-    # Simulation
-    components = ['MagneticFieldConstant4LimitedRCDC', 'CDC']
-    simulation.add_simulation(main_path, components=components)
-
-    # Setup track finder
-    trackFinderModule = basf2.register_module('CDCLocalTracking')
-    main_path.add_module(trackFinderModule)
-
+    cosmics_validation_run = Cosmics()
     if arguments.fit:
-        # Prepare Genfit extrapolation
-        setupGenfitExtrapolationModule = \
-            basf2.register_module('SetupGenfitExtrapolation')
-        setupGenfitExtrapolationModule.param({'whichGeometry': arguments.geometry})
-        main_path.add_module(setupGenfitExtrapolationModule)
+        cosmics_validation_run.fit_geometry = arguments.geometry
+    else:
+        cosmics_validation_run.fit_geometry = None
 
-        # Fit tracks
-        genFitterModule = basf2.register_module('GenFitter')
-        genFitterModule.param({'PDGCodes': [13]})
-        main_path.add_module(genFitterModule)
-
-    # Reference Monte Carlo tracks
-    trackFinderMCTruthModule = basf2.register_module('TrackFinderMCTruth')
-    trackFinderMCTruthModule.param({
-        'UseCDCHits': True,
-        'UseSVDHits': False,
-        'UsePXDHits': False,
-        'WhichParticles': ['primary'],
-        'EnergyCut': 0.1,
-        'GFTrackCandidatesColName': 'MCTrackCands',
-        })
-    main_path.add_module(trackFinderMCTruthModule)
-
-    # Track matcher
-    mcTrackMatcherModule = basf2.register_module('MCTrackMatcher')
-    mcTrackMatcherModule.param({
-        'UseCDCHits': True,
-        'UseSVDHits': False,
-        'UsePXDHits': False,
-        'MCGFTrackCandsColName': 'MCTrackCands',
-        'MinimalPurity': 0.66,
-        'RelateClonesToMCParticles': True,
-        })
-
-    main_path.add_module(mcTrackMatcherModule)
-
-    # Validation module generating plots
-    trackingValidationModule = TrackingValidationModule('Cosmics',
-            fit=arguments.fit, pulls=arguments.pulls)
-    main_path.add_module(trackingValidationModule)
-
-    # Run basf2 module path #
-    #########################
-    print 'Start processing'
-    basf2.process(main_path)
-    print basf2.statistics
+    cosmics_validation_run.execute()
 
     # Show plots #
     ##############
     if arguments.show:
-        output_file_name = trackingValidationModule.output_file_name
+        output_file_name = cosmics_validation_run.output_file_name
         tFile = ROOT.TFile(output_file_name)
         tBrowser = ROOT.TBrowser()
         tBrowser.BrowseObject(tFile)
         tBrowser.Show()
         raw_input('Press enter to close.')
         tFile.Close()
+
+
+class TrackingValidationRun(object):
+
+    n_events = 10000
+    generator_module = 'EvtGenInput'
+    components = ['PXD', 'SVD', 'CDC', 'BeamPipe',
+                  'MagneticFieldConstant4LimitedRCDC']
+    finder_module = None  # To be specified
+    tracking_coverage = {'UsePXDHits': True, 'UseSVDHits': True,
+                         'UseCDCHits': True}
+    fit_geometry = None
+    pulls = True
+    contact = TRACKING_MAILING_LIST
+    output_file_name = 'StandardTrackingValidation.root'
+
+    # Allow override from instances only in these field names to prevent some spelling errors
+    __slots__ = [
+        'n_events',
+        'generator_module',
+        'components',
+        'finder_module',
+        'tracking_coverage',
+        'fit_geometry',
+        'contract',
+        'output_file_name',
+        ]
+
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+    def execute(self):
+        # Compose basf2 module path #
+        #############################
+        main_path = basf2.create_path()
+
+        # Master module
+        eventInfoSetterModule = basf2.register_module('EventInfoSetter')
+        eventInfoSetterModule.param({'evtNumList': [self.n_events],
+                                    'runList': [1], 'expList': [1]})
+        main_path.add_module(eventInfoSetterModule)
+
+        # Progress module
+        progressModule = basf2.register_module('Progress')
+        main_path.add_module(progressModule)
+
+        # Generator
+        generatorModule = get_basf2_module(self.generator_module)
+        main_path.add_module(generatorModule)
+
+        # Simulation
+        components = self.components
+        simulation.add_simulation(main_path, components=components)
+
+        # Setup track finder
+        trackFinderModule = get_basf2_module(self.finder_module)
+        main_path.add_module(trackFinderModule)
+
+        if self.fit_geometry:
+            # Prepare Genfit extrapolation
+            setupGenfitExtrapolationModule = \
+                basf2.register_module('SetupGenfitExtrapolation')
+            setupGenfitExtrapolationModule.param({'whichGeometry': self.fit_geometry})
+            main_path.add_module(setupGenfitExtrapolationModule)
+
+            # Fit tracks
+            genFitterModule = basf2.register_module('GenFitter')
+            genFitterModule.param({'PDGCodes': [13]})
+            main_path.add_module(genFitterModule)
+
+        # Reference Monte Carlo tracks
+        trackFinderMCTruthModule = basf2.register_module('TrackFinderMCTruth')
+        trackFinderMCTruthModule.param({'WhichParticles': ['primary'],
+                                       'EnergyCut': 0.1,
+                                       'GFTrackCandidatesColName': 'MCTrackCands'
+                                       })
+        trackFinderMCTruthModule.param(self.tracking_coverage)
+        main_path.add_module(trackFinderMCTruthModule)
+
+        # Track matcher
+        mcTrackMatcherModule = basf2.register_module('MCTrackMatcher')
+        mcTrackMatcherModule.param({'MCGFTrackCandsColName': 'MCTrackCands',
+                                   'MinimalPurity': 0.66,
+                                   'RelateClonesToMCParticles': True})
+        mcTrackMatcherModule.param(self.tracking_coverage)
+        main_path.add_module(mcTrackMatcherModule)
+
+        # Validation module generating plots
+        fit = bool(self.fit_geometry)
+        pulls = self.pulls
+        contact = self.contact
+        output_file_name = self.output_file_name
+        trackingValidationModule = TrackingValidationModule(self.name,
+                contact=contact, fit=fit, pulls=pulls,
+                output_file_name=output_file_name)
+        main_path.add_module(trackingValidationModule)
+
+        # Run basf2 module path #
+        #########################
+        print 'Start processing'
+        basf2.process(main_path)
+        print basf2.statistics
+
+
+def get_basf2_module(module_or_module_name):
+    if isinstance(module_or_module_name, str):
+        module_name = module_or_module_name
+        module = basf2.register_module(module_name)
+        return module
+    elif isinstance(module_or_module_name, basf2.Module):
+        module = module_or_module_name
+        return module
+    else:
+        message_template = \
+            '%s of type %s is neither a module nor the name of module. Expected str or basf2.Module instance.'
+        raise ValueError(message_template % (module_or_module_name,
+                         type(module_or_module_name)))
+
+
+class Cosmics(TrackingValidationRun):
+
+    n_events = N_EVENTS
+    generator_module = 'Cosmics'
+    components = ['CDC', 'MagneticFieldConstant4LimitedRCDC']
+    finder_module = 'CDCLocalTracking'
+    tracking_coverage = {'UsePXDHits': False, 'UseSVDHits': False,
+                         'UseCDCHits': True}
+    fit_geometry = None
+    pulls = True
+    contact = CONTACT
+    output_file_name = 'CosmicsTrackingValidation.root'
 
 
 # Analysis module #
@@ -156,18 +219,19 @@ class TrackingValidationModule(basf2.Module):
     def __init__(
         self,
         name,
+        contact=TRACKING_MAILING_LIST,
         fit=False,
         pulls=False,
+        output_file_name=None,
         ):
 
         super(TrackingValidationModule, self).__init__()
         self.name = name
+        self.contact = contact
         self.fit = fit
         self.pulls = pulls
-
-    @property
-    def output_file_name(self):
-        return self.name + 'TrackingValidation.root'
+        self.output_file_name = output_file_name or self.name \
+            + 'TrackingValidation.root'
 
     def initialize(self):
         self.trackMatchLookUp = Belle2.TrackMatchLookUp('MCTrackCands')

81231aa58fcdc517872144185c323cbf779d086f
Phillip Urquijo <purquijo@gmail.com>
2014-12-23 03:19:36 +0000

    setting up decfiles
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@14593 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ decfiles/scripts/colours.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# --------------------------------------
+# Class holding colours for printing coloured text to terminal and printing functions, also handles the general log
+# Written by Phillip Urquijo
+# December 2014
+# --------------------------------------
+
+import settings
+import os
+
+# settings imported from the settings file
+logfile = settings.logfile
+
+# handle the log filename
+if not settings.overwrite:
+    if os.path.exists(logfile):
+        for i in range(10000):
+            if not os.path.exists(logfile + '.' + str(i)):
+                logfile = logfile + '.' + str(i)
+                break
+
+# open the log
+log = open(logfile, 'w')
+failed = False
+
+
+# --------------------------------------
+# Class holding colours for printing coloured text to terminal
+# --------------------------------------
+
+class bcolours:
+
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[100;93m'
+    FAIL = '\033[100;91m'
+    ENDC = '\033[0m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
+
+    def enable(self):
+        self.HEADER = '\033[95m'
+        self.OKBLUE = '\033[94m'
+        self.OKGREEN = '\033[92m'
+        self.WARNING = '\033[93m'
+        self.FAIL = '\033[91m'
+        self.ENDC = '\033[0m'
+
+
+if not settings.enable_colours:
+    bcolours.disable()
+
+
+# --------------------------------------
+# Text Functions
+# --------------------------------------
+
+def query(string):
+
+# print a query, wait, at the end of the dots for either fail() or done()
+
+    string = str(string) + (60 - len(str(string))) * '.'
+    print string,
+
+    # try to log it
+    try:
+        log.write(string)
+    except:
+        pass
+
+    failed = False
+
+
+def openlog():
+
+# open the logfile manually again after it has been closed
+
+    globals()['log'] = open(logfile, 'a')
+    query('Opening logfile <' + logfile + '>')
+    done()
+
+
+query('Opening logfile <' + logfile + '> and loading colours')
+
+
+def mesg(string):
+
+# Simple announcing information debug function, in white
+
+    string = str(string)
+    print string
+    log.write(string + '\n')
+
+
+def stop(string):
+
+# Most serious error announcing function, in red
+
+    string = str(string)
+    print bcolours.FAIL + string + bcolours.ENDC
+            # log it
+    log.write(string + '\n')
+
+
+def done():
+
+# Prints DONE in green letters - to be used in conjunction with query() above
+
+    print bcolours.OKGREEN + 'DONE' + bcolours.ENDC
+
+    # try to log it
+    try:
+        log.write('DONE\n')
+    except:
+        pass
+
+
+def fail(list):
+
+# Prints FAIL in red letters and follows with debug information if they were passed in the list[strings] as an argument, to be used in conjunction with query() above
+
+    print bcolours.FAIL + 'FAIL' + bcolours.ENDC
+    log.write('FAIL\n')
+
+    # print the debug info
+    if list != []:
+        print bcolours.WARNING + 'Printing debug info:\n'
+
+        # log it
+        log.write('Debug info:\n')
+        for foo in list:
+            print foo
+            log.write(str(foo) + '\n')
+        print bcolours.ENDC
+
+
+def warning(string):
+
+# Prints a yellow warning string that is passed as the argument, moderate problem
+
+    string = str(string)
+    # print if runlevel is 1 or higher
+    print bcolours.WARNING + string + bcolours.ENDC
+    log.write(string + '\n')
+
+
+def closeLog():
+
+# closes log after it's been opened manually
+
+    log.close()
+
+
+done()
---
+++ decfiles/scripts/settings.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+logfile = os.path.dirname(os.path.abspath(sys.argv[0])) + '/decparse.log'
+enable_colours = True
+overwrite = True
+dkfilespath = 'dec/'
+obsoletepath = 'doc/'
+partdictpath = os.path.dirname(os.path.abspath(sys.argv[0])) \
+    + '/particle_dictionary'
+
+use_url = False
+dec_url = \
+    'https://belle2.cc.kek.jp/browse/viewvc.cgi/svn/trunk/software/decfiles/dec/'
+obs_url = \
+    'https://belle2.cc.kek.jp/browse/viewvc.cgi/svn/trunk/software/decfiles/doc/table_obsolete.sql?format=txt'
+cuts_url = \
+    'https://belle2.cc.kek.jp/browse/viewvc.cgi/svn/trunk/software/decfiles/doc/cuts'
+table_url = 'http://svn.cern.ch/guest/evtgen/tags/R01-03-00/evt.pdl'
+
+groups = [
+    'SL',
+    'EWP',
+    'TCPV',
+    'HAD',
+    'CHARM',
+    'ONIA',
+    'TAU',
+    ]
+
+terminators = [
+    'VSS',
+    'VSS_BMIX',
+    'PHOTOS',
+    'VLL',
+    'VVPIPI',
+    'PARTWAVE',
+    'HELAMP',
+    'PYTHIA',
+    'HQET',
+    'HQET2',
+    'ISGW2',
+    'GOITY_ROBERTS',
+    'VUB',
+    'PHSP',
+    'SVS',
+    'SVV_HELAMP',
+    'BTOXSGAMMA',
+    'BTOSLLBALL',
+    'BTOXSLL',
+    'BTO3PI_CP',
+    'STS',
+    'SLN',
+    'CB3PI-P00',
+    'CB3PI-MPP',
+    'VSP_PWAVE',
+    'SVP_HELAMP',
+    'BTOSLLALI',
+    'TAULNUNU',
+    'TAUSCALARNU',
+    'TAUHADNU',
+    'TAUVECTORNU',
+    'D_DALITZ',
+    'VVS_PWAVE',
+    'TVS_PWAVE',
+    'TSS',
+    'PI0_DALITZ',
+    'ETA_DALITZ',
+    'OMEGA_DALITZ',
+    'VVP',
+    'BTOSLLMS',
+    ]
+
+longlived = [
+    'pi+',
+    'pi-',
+    'K+',
+    'K-',
+    'K_L0',
+    'p+',
+    'anti-p-',
+    'n0',
+    'anti-n0',
+    'e+',
+    'e-',
+    'mu+',
+    'mu-',
+    ]
+
+nickslation = {'B0': 'Bd', 'B+': 'Bu', 'D*+': 'Dst'}
+
+# Obsolete
+descripslation = {'D*+': 'D*(2010)+'}
+                   # "K_S0" : "KS0"

415f89b6daf58dd560be158f29376fb7e89b533b
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2015-01-02 17:57:42 +0000

    parser -> variableCutParser
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@14687 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/tools/variableCutParser.cc
+++ analysis/tools/variableCutParser.cc
@@ -18,8 +18,8 @@ using namespace Belle2;
 
 int main()
 {
-
   std::string condition;
+  std::cout << "This program allows you to test variable conditions (see https://belle2.cc.kek.jp/~twiki/bin/view/Physics/VariableManager ).\n";
   std::cout << "Please input a condition " << std::flush;
   std::getline(std::cin, condition);
 

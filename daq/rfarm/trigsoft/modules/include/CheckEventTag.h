8549b4ebcd1369e4a79df3d031d4c059df324c79
Satoru Yamada <satoru.yamada@kek.jp>
2015-01-08 23:10:23 +0000

    replace BPID/EPID with TOP/ARICH
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@14763 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/rfarm/trigsoft/modules/include/CheckEventTag.h
+++ daq/rfarm/trigsoft/modules/include/CheckEventTag.h
@@ -24,8 +24,8 @@
 #include <rawdata/dataobjects/RawCDC.h>
 #include <rawdata/dataobjects/RawSVD.h>
 #include <rawdata/dataobjects/RawECL.h>
-#include <rawdata/dataobjects/RawEPID.h>
-#include <rawdata/dataobjects/RawBPID.h>
+#include <rawdata/dataobjects/RawARICH.h>
+#include <rawdata/dataobjects/RawTOP.h>
 #include <rawdata/dataobjects/RawKLM.h>
 #include <rawdata/dataobjects/RawCOPPER.h>
 //#include <rawdata/dataobjects/RawHeader.h>

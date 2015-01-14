2635860d8c123b0cd0c59dffdbb3073271e4445d
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2015-01-14 15:42:22 +0000

    add run_distributed.sh example, update distributed_fei.sh
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@14882 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/examples/FEI/distributed_fei.sh
+++ analysis/examples/FEI/distributed_fei.sh
@@ -1,36 +1,31 @@
-#!/usr/bin/env bash
+#Not actually an example file, sourced from run_distributed.sh
 
-collectionDirectory="FEI"
-jobDirectory="jobs"
-persistentDirectory="persistent"
-nJobs="1000"
-steeringFile="B_generic.py"
-
-set -a
-source /opt/ogs/ekpcluster/common/settings.sh
-set +a
+#set -a
+#source /opt/ogs/ekpcluster/common/settings.sh
+#set +a
 
 
 function create_play {
-
-  local allMcFiles=( "$@" )
- 
   echo "Recreate directories"
   rm -rf "$persistentDirectory"
   mkdir -p "$persistentDirectory"
   rm -rf "$collectionDirectory"
+  rm -rf "$collectionDirectoryReal"
   mkdir -p "$collectionDirectory"
+  mkdir -p "$collectionDirectoryReal"
   rm -rf "$jobDirectory"
   mkdir -p "$jobDirectory"
-  
+
   echo "Create persistent job files"
   for i in $(seq "$nJobs")
   do
+    echo "Creating setup for job $i..."
     unset mcFiles
     declare -a mcFiles
     local count=0
-    for mcFile in "${allMcFiles[@]}"
+    for mcFile in ${allMcFiles[@]}
     do
+        #echo "test: $mcFile"
       count=$((count + 1))
       if [ $((count % nJobs)) -eq $((i-1)) ]
       then
@@ -40,14 +35,52 @@ function create_play {
     mkdir -p  "$jobDirectory"/"$i"
     mkdir -p  "$persistentDirectory"/"$i"
     cd "$persistentDirectory"/"$i"
-    hadd basf2_input.root "${mcFiles[@]}"
+    #hadd basf2_input.root "${mcFiles[@]}"
+    for mcFile in ${mcFiles[@]}
+    do
+        basename=`basename "$mcFile"`
+        ln -s "$mcFile" "rawinput_$basename"
+    done
+    cd -
+  done
+}
+
+function create_ntuples_with_finished_training {
+  
+  echo "Create persistent job files"
+  for i in $(seq "$nJobs")
+  do
+    echo "Creating setup for job $i..."
+    unset mcFiles
+    declare -a mcFiles
+    local count=0
+    for mcFile in ${allMcFiles[@]}
+    do
+        #echo "test: $mcFile"
+      count=$((count + 1))
+      if [ $((count % nJobs)) -eq $((i-1)) ]
+      then
+        mcFiles[count]="$mcFile"
+      fi
+    done
+    mkdir -p  "$jobDirectory"/"$i"
+    mkdir -p  "$persistentDirectory"/"$i"
+    cd "$persistentDirectory"/"$i"
+    for mcFile in ${mcFiles[@]}
+    do
+        basename=`basename "$mcFile"`
+        ln -s "$mcFile" "rawinput_$basename"
+    done
     cd -
 
     cd "$jobDirectory"/"$i"
-    echo -n "basf2 -l warning --execute-path basf2_path.pickle " > basf2_script.sh
-    echo -n "-i ../../$persistentDirectory/$i/basf2_input.root " >> basf2_script.sh
-    echo -n "-o ../../$persistentDirectory/$i/basf2_output.root " >> basf2_script.sh
+    echo -n "time basf2 -l warning --execute-path basf2_path.pickle " > basf2_script.sh
+    echo -n "-i $persistentDirectory/$i/rawinput_\*.root " >> basf2_script.sh
+    echo -n "-o $persistentDirectory/$i/basf2_output.root " >> basf2_script.sh
+    #echo -n " &> /home/belle/pulver/joboutput/${i}_my_output_hack.log " >> basf2_script.sh
+    echo -n " &> my_output_hack.log " >> basf2_script.sh
     echo -n "&& touch basf2_finished_successfully" >> basf2_script.sh
+    chmod +x basf2_script.sh
     cd -
   done
     
@@ -55,8 +88,7 @@ function create_play {
 
 function run_basf2 {
   cd "$collectionDirectory"
-  basf2 ../$steeringFile --dump-path basf2_path.pickle -- -ve -nproc 2 -preload -cache mycachefile.pkl
-  #basf2 ../$steeringFile --dump-path basf2_path.pickle -- -ve -preload -cache mycachefile.pkl
+  basf2 $steeringFile --dump-path basf2_path.pickle -i "$persistentDirectory"/1/basf2_input.root -- -ve -nproc 20 -cache cache.pkl --preload -summary || return 1
   cd -
 }
 
@@ -68,6 +100,32 @@ function setup_jobDirectory {
       local absolutePath=$(readlink -f "$file")
       for i in $(seq "$nJobs")
       do
+            mkdir -p  "$jobDirectory"/"$i"
+            cd "$jobDirectory"/"$i"
+            #here doc with no quotes to allow variable substitution
+            cat << EOF > basf2_script.sh
+#abort on error
+set -e
+
+if [ -f "$persistentDirectory/$i/basf2_input.root" ]; then
+  INPUT="$persistentDirectory/$i/basf2_input.root"
+else
+  INPUT="$persistentDirectory/$i/rawinput_*.root"
+fi
+time basf2 -l warning --execute-path basf2_path.pickle -i "\$INPUT" -o $persistentDirectory/$i/basf2_output.root &> my_output_hack.log || touch basf2_job_error
+for f in *.root;
+do
+  #ignore symlinks
+  test -h "\$f" && continue
+
+  mv "\$f" $persistentDirectory/$i/
+  ln -s "$persistentDirectory/$i/\$f" .
+done
+touch basf2_finished_successfully
+EOF
+            chmod +x basf2_script.sh
+            cd -
+
             ln -s "$absolutePath" "$jobDirectory"/"$i"/
       done
     fi
@@ -78,17 +136,27 @@ function submit_jobs {
   
   for i in $(seq "$nJobs")
   do
+    echo "submitting $i"
     cd "$jobDirectory"/"$i"
-    qsub -cwd -q express,short -e error.log -o output.log -V basf2_script.sh | cut -f 3 -d ' ' > basf2_jobid
+    #qsub -cwd -q express,short,medium,long -e error.log -o output.log -V basf2_script.sh | cut -f 3 -d ' ' > basf2_jobid
+    #bsub -q s -e error.log -o output.log ./basf2_script.sh | cut -f 2 -d ' ' | sed 's/<//' | sed 's/>//' > basf2_jobid
+    bsub -q s -e /home/belle/pulver/joboutput -o /home/belle/pulver/joboutput ./basf2_script.sh | cut -f 2 -d ' ' | sed 's/<//' | sed 's/>//' > basf2_jobid
     cd -
   done
 }
 
 function wait_for_jobs {
 
-  while [ $(ls -l "$jobDirectory"/*/basf2_finished_successfully | wc -l) -lt "$nJobs" ]
+  while [ $(ls -l "$jobDirectory"/*/basf2_finished_successfully 2> /dev/null | wc -l ) -lt "$nJobs" ]
   do
-    sleep 30
+    if [ $(ls -l "$jobDirectory"/*/basf2_job_error 2> /dev/null | wc -l) -ne 0 ]
+    then
+      echo "basf2 execution failed! Error occurred in:"
+      ls -l "$jobDirectory"/*/basf2_job_error
+      return 1
+    fi
+    echo -n "."
+    sleep 40
   done
 }
 
@@ -102,7 +170,8 @@ function merge_root_files {
     for file in "$jobDirectory"/1/*.root
     do
       file=$(basename "$file")
-      hadd "$collectionDirectory"/"$file" "$jobDirectory"/*/"$file"
+      hadd -f "$collectionDirectoryReal"/"$file" "$jobDirectory"/*/"$file" || return 1
+      ln -s "$collectionDirectoryReal"/"$file" "$collectionDirectory"/"$file"
     done
   fi
 
@@ -113,7 +182,7 @@ function update_input_files {
   for i in $(seq "$nJobs")
   do
     cd "$persistentDirectory"/"$i"
-    mv basf2_output.root basf2_input.root
+    command mv basf2_output.root basf2_input.root
     cd -
   done
 
@@ -123,21 +192,24 @@ function clean_jobDirectory {
   
   find $jobDirectory -maxdepth 2 -type l -delete
   find $jobDirectory -maxdepth 2 -name basf2_finished_successfully -delete
+  find $jobDirectory -maxdepth 2 -name error.log -delete
+  find $jobDirectory -maxdepth 2 -name output.log -delete
+  find $jobDirectory -maxdepth 2 -name my_output_hack.log -delete
   find $jobDirectory -maxdepth 2 -name *.root -delete
   
 }
 
 function next_act {
   
-  run_basf2
+  run_basf2 || return 1
   echo "Finished basf2 here -- wait 3 seconds"
   sleep 3
   setup_jobDirectory
   submit_jobs
-  wait_for_jobs
+  wait_for_jobs || return 1
   echo "Finished basf2 cluster -- wait 3 seconds"
   sleep 3
-  merge_root_files
+  merge_root_files || return 1
   update_input_files
   clean_jobDirectory
 
---
+++ analysis/examples/FEI/run_distributed.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# Trains FEI on a provided input sample, running on KEKCC cluster
+
+set -e
+
+
+################################################################################
+# User settings
+nJobs="2000"
+steeringFile="/home/belle/pulver/basf2/analysis/examples/FEI/B_generic.py"
+
+#collects training output, plots, etc.
+#note: MUST be on home, not HSM (larger files only stored temporarily)
+collectionDirectory="/home/belle/pulver/B_generic/collection"
+
+#contains working directories for jobs
+#note: MUST be on home, not HSM (larger files only stored temporarily)
+jobDirectory="/home/belle/pulver/B_generic/jobs"
+
+#use HSM for large amounts of data:
+collectionDirectoryReal="/ghi/fs01/belle2/bdata/pulver/B_generic_daughter0mass/collection"
+persistentDirectory="/ghi/fs01/belle2/bdata/pulver/B_generic/persistent"
+
+#Data files to use as input:
+#streams 0-39
+#allMcFiles=(/group/belle2/MC/generic/{mixed,charged,ccbar,ssbar,ddbar,uubar}/mcprod1405/BGx0/*_s{0{0..9},{10..39}}/*.root)
+#10 independent streams
+allMcFiles=(/group/belle2/MC/generic/{mixed,charged,ccbar,ssbar,ddbar,uubar}/mcprod1405/BGx0/*_s{40..49}/*.root)
+#all 50 streams
+#allMcFiles=(/group/belle2/MC/generic/{mixed,charged,ccbar,ssbar,ddbar,uubar}/mcprod1405/BGx1/*_s{0{0..9},{10..49}}/*.root)
+################################################################################
+
+
+
+source $BELLE2_LOCAL_DIR/analysis/examples/FEI/distributed_fei.sh
+
+create_play
+
+while [ ! -f $collectionDirectory/FEIsummary.tex ]; do
+  next_act
+  sleep 2
+done

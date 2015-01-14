d8ee8276f45c665628fb61728f3036c41ae20c49
Oliver Frost <oliver.frost@desy.de>
2015-01-14 13:45:26 +0000

    Started to unify the drawing of eventdisplays.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@14874 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingCDC/display/include/BoundingBox.h
@@ -0,0 +1,113 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#ifndef BOUNDINGBOX_H_
+#define BOUNDINGBOX_H_
+
+// #include <tracking/trackFindingCDC/rootification/SwitchableRootificationBase.h>
+
+#include <algorithm>
+#include <cmath>
+#include <utility>
+
+namespace Belle2 {
+  namespace TrackFindingCDC {
+
+    /// A two dimensional rectangle that keeps track of the extend of a drawing
+    class BoundingBox {
+
+    public:
+
+      /// Default constructor for ROOT compatibility. Cell weight defaults to 1
+      BoundingBox() :
+        m_left(NAN),
+        m_bottom(NAN),
+        m_right(NAN),
+        m_top(NAN)
+      {;}
+
+      /// Initialises the bounds of the rectangle to the given values. Automatically swaps the x or y values if they are given in a decreasing.
+      BoundingBox(const float& x1, const float& y1, const float& x2, const float& y2) :
+        m_left(std::min(x1, x2)),
+        m_bottom(std::min(y1, y2)),
+        m_right(std::max(x1, x2)),
+        m_top(std::max(y1, y2))
+      {;}
+
+      /// Expands the bounding box such that it also covers the given bounding box
+      void operator&=(const BoundingBox& other) {
+
+        m_left = std::isnan(getLeft()) ? other.getLeft() : std::min(getLeft(), other.getLeft());
+        m_bottom = std::isnan(getBottom()) ? other.getBottom() : std::min(getBottom(), other.getBottom());
+
+        m_right = std::isnan(getRight()) ? other.getRight() : std::max(getRight(), other.getRight());
+        m_top = std::isnan(getTop()) ? other.getTop() : std::max(getTop(), other.getTop());
+
+      }
+
+      /// Getter for the width of the bounding box rectangle.
+      float getWidth()
+      { return getRight() - getLeft(); }
+
+      /// Getter for the height of the bounding box rectangle.
+      float getHeight()
+      { return getTop() - getBottom(); }
+
+      /// Getter for the location of the left of the bounding box rectangle (lower x bound). NAN if unset.
+      const float& getLeft() const
+      { return m_left; }
+
+      /// Getter for the location of the bottom of the bounding box rectangle (lower y bound). NAN if unset.
+      const float& getBottom() const
+      { return m_bottom; }
+
+      /// Getter for the location of the right of the bounding box rectangle (upper x bound). NAN if unset.
+      const float& getRight() const
+      { return m_right; }
+
+      /// Getter for the location of the top of the bounding box rectangle (upper y bound). NAN if unset.
+      const float& getTop() const
+      { return m_top; }
+
+      /// Clears all bounds to NAN.
+      void clear() {
+        m_left = NAN;
+        m_bottom = NAN;
+        m_right = NAN;
+        m_top = NAN;
+      }
+
+    private:
+      /// Memory for the lower x bound of the rectangle.
+      float m_left;
+
+      /// Memory for the lower y bound of the rectangle.
+      float m_bottom;
+
+      /// Memory for the upper x bound of the rectangle.
+      float m_right;
+
+      /// Memory for the upper y bound of the rectangle.
+      float m_top;
+
+    }; //class
+
+  } // namespace TrackFindingCDC
+} // namespace Belle2
+#endif // BOUNDINGBOX_H_
+
+
+
+
+
+
+
+
+
+
---
+++ tracking/trackFindingCDC/display/include/linkdef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class Belle2::TrackFindingCDC::BoundingBox-;
+
+#ifdef TRACKFINDINGCDC_USE_ROOT
+
+#endif
+
+#endif

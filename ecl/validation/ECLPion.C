474d5525ef2a4f2b04bca783b3c14d50c7936455
Benjamin Oberhof <benjamin.oberhof@pi.infn.it>
2015-01-16 12:48:27 +0000

    test version
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@14921 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ ecl/validation/ECLPion.C
@@ -0,0 +1,28 @@
+void ECLPion()
+{
+
+  TFile* inputFile = TFile::Open("../ECLPionOutput.root");
+  TTree* tree = (TTree*) inputFile->Get("m_tree");
+
+  //  TH1F* hShowers = new TH1F("hShowers", "Reconstructed Shower Energy for 1GeV Muon, the typical energy is 0.2 GeV", 100, 0., 1.2);
+  TH1F* hClusterE = new TH1F("hClusterE", "Reconstructed Shower Energy Pions", 100, 0., 1.2);
+  hClusterE->GetXaxis()->SetTitle("Shower Energy (GeV)");
+  m_tree->Draw("eclShowerEnergy>>hClusterE","eclShowerEnergy>0");
+  //hShowers->Write();
+
+  //  TH1F* hGammas = new TH1F("hGammas", "Fake Reconstructed Gamma from  1000 muons should be less than 5%", 10,0,10);
+  TH1F* hClusterMultip = new TH1F("hClusterMultip", "Cluster Multiplicity Pions", 10,0,10);
+  hClusterMultip->GetXaxis()->SetTitle("Cluster Multiplicity");
+  m_tree->Draw("eclClusterMultip>>hClusterMultip","eclClusterMultip>0");
+  //hGammas->Write();
+
+  //TH1F* hE9oE25_500 = new TH1F("hE9oE25_500","E9/E25", 100, 0., 1.05);
+  //hE9oE25_500->GetListOfFunctions()->Add(new TNamed("Description", "Energy deposited in 3x3 matrix around most energetic deposit over energy deposited in 5x5 matrix around most energetic deposit 500 MeV/c single photons"));
+
+  // open the output file for the validation histograms
+  TFile* output = TFile::Open("ECLPion.root", "recreate");
+  hClusterE->Write();
+  hClusterMultip->Write();
+  
+  delete output;
+}

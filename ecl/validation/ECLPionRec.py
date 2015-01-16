f4913c771ea702272fbbeda38188d0377dc84fad
Benjamin Oberhof <benjamin.oberhof@pi.infn.it>
2015-01-16 12:48:08 +0000

    test version
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@14920 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ ecl/validation/ECLPionRec.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+########################################################
+# This steering file generates an ntuple for validation
+# purposes
+########################################################
+
+import os
+from basf2 import *
+from simulation import add_simulation
+from reconstruction import add_reconstruction
+
+set_log_level(LogLevel.ERROR)
+
+# Register necessary modules
+eventinfosetter = register_module('EventInfoSetter')
+# Set the number of events to be processed (1000 events)
+eventinfosetter.param({'evtNumList': [1000], 'runList': [1]})
+
+eventinfoprinter = register_module('EventInfoPrinter')
+
+import random
+intseed = random.randint(1, 10000000)
+
+pGun = register_module('ParticleGun')
+param_pGun = {
+    'pdgCodes': [211],
+    'nTracks': 1,
+    'momentumGeneration': 'uniform',
+    'momentumParams': [0, 1],
+    'thetaGeneration': 'uniform',
+    'thetaParams': [0., 180.],
+    'phiGeneration': 'uniform',
+    'phiParams': [0, 360],
+    'vertexGeneration': 'uniform',
+    'xVertexParams': [0., 0.],
+    'yVertexParams': [0., 0.],
+    'zVertexParams': [0., 0.],
+    }
+pGun.param(param_pGun)
+
+# simpleoutput = register_module('RootOutput')
+# simpleoutput.param('outputFileName', '../ECLPionOutput.root')
+eclanalysis = register_module('ECLDataAnalysis')
+eclanalysis.param('rootFileName', '../ECLPionOutput.root')
+# eclanalysis.param('doTracking', 1)
+
+# Create paths
+main = create_path()
+main.add_module(eventinfosetter)
+main.add_module(eventinfoprinter)
+main.add_module(pGun)
+add_simulation(main)
+add_reconstruction(main)
+main.add_module(eclanalysis)
+# main.add_module(simpleoutput)
+
+process(main)
+print statistics

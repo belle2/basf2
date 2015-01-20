dfe57199994e6eedbb219eb4c6c353d91b7a5419
Benjamin Oberhof <benjamin.oberhof@pi.infn.it>
2015-01-20 11:03:25 +0000

    added description and check
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@14951 b38c6c8f-bee8-465d-8448-9abe6915d496

--- ecl/validation/ECLPion.C
+++ ecl/validation/ECLPion.C
@@ -5,14 +5,22 @@ void ECLPion()
   TTree* tree = (TTree*) inputFile->Get("m_tree");
 
   //  TH1F* hShowers = new TH1F("hShowers", "Reconstructed Shower Energy for 1GeV Muon, the typical energy is 0.2 GeV", 100, 0., 1.2);
-  TH1F* hClusterE = new TH1F("hClusterE", "Reconstructed Shower Energy Pions", 100, 0., 1.2);
+  TH1F* hClusterE = new TH1F("hClusterE", "ECL shower energy, charged pions", 100, 0., 1.2);
   hClusterE->GetXaxis()->SetTitle("Shower Energy (GeV)");
+  hClusterE->GetListOfFunctions()->Add(new TNamed("Description",
+    "Reconstructed shower energy for single 1GeV charged pion")); 
+  hClusterE->GetListOfFunctions()->Add(new TNamed("Check",
+    "Typical energy should be 0.2 GeV"));
   m_tree->Draw("eclShowerEnergy>>hClusterE","eclShowerEnergy>0");
   //hShowers->Write();
 
   //  TH1F* hGammas = new TH1F("hGammas", "Fake Reconstructed Gamma from  1000 muons should be less than 5%", 10,0,10);
   TH1F* hClusterMultip = new TH1F("hClusterMultip", "Cluster Multiplicity Pions", 10,0,10);
-  hClusterMultip->GetXaxis()->SetTitle("Cluster Multiplicity");
+  hClusterMultip->GetXaxis()->SetTitle("ECL cluster multiplicity charged pion");
+  hClusterE->GetListOfFunctions()->Add(new TNamed("Description",
+    "ECL cluster multiplicity for 1 GeV charged pion")); 
+  hClusterE->GetListOfFunctions()->Add(new TNamed("Check",
+    "Cluster multiplicity should be low"));
   m_tree->Draw("eclClusterMultip>>hClusterMultip","eclClusterMultip>0");
   //hGammas->Write();
 

2c0011ad2244c023f3bd4960c4eac8153a4ba094
Tobias Schluter <tobias.schlueter@physik.uni-muenchen.de>
2015-01-23 11:22:35 +0000

    tracking/validation: svn add new files, previously missing.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15034 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/validation/12_trackingEfficiency_createData.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# !/usr/bin/env python
+
+#################################################################
+#                                                               #
+#    script to simulate 10 charged muon tracks with             #
+#    fixed transverse momentum using the ParticleGun            #
+#                                                               #
+#    written by Michael Ziegler, KIT                            #
+#    michael.ziegler2@kit.edu                                   #
+#                                                               #
+#################################################################
+
+"""
+<header>
+    <output>trackingEfficiency_pt_4.00GeV.root</output>
+    <contact>michael.ziegler2@kit.edu</contact>
+    <description>Create events with 10 muon tracks with fixed pt value.</description>
+</header>
+"""
+
+from basf2 import *
+from tracking.validation.tracking_efficiency_helpers import run_simulation, get_generated_pt_value
+
+set_random_seed(123460)
+
+pt_value = get_generated_pt_value(9)
+
+output_filename = '../trackingEfficiency_pt_%.2fGeV.root' % pt_value
+
+print output_filename
+
+path = create_path()
+
+run_simulation(path, pt_value, output_filename)
+
+process(path)

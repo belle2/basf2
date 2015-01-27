a060272e8b94cd70c33fa7b09bebd271ceba8a39
Igal Jaegle <igjaegle@gmail.com>
2015-01-27 16:22:24 +0000

    Add Sam tubeHit
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15102 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ beast/he3tube/dataobjects/include/He3tubeHit.h
@@ -0,0 +1,68 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010-2011  Belle II Collaboration                         *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Martin Ritter, Igal Jaegle                               *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef He3tubeHIT_H
+#define He3tubeHIT_H
+
+#include <simulation/dataobjects/SimHitBase.h>
+//#include <framework/datastore/RelationsObject.h>
+
+// ROOT
+#include <TObject.h>
+#include <TVector3.h>
+#include <vector>
+
+namespace Belle2 {
+
+  class He3tubeHit : public SimHitBase {
+  public:
+    //typedef std::vector<unsigned int>::iterator iterator;
+    //typedef std::vector<unsigned int>::const_iterator const_iterator;
+
+    /** default constructor for ROOT */
+    He3tubeHit(): m_energyDep(0), m_detNb(0), m_peakV(0), m_time(0) {}
+
+
+    /** Standard constructor
+     * @param energyDep Deposited energy in electrons
+     */
+    He3tubeHit(double energyDep, int detNb, double peakV, double time) {
+      m_energyDep = energyDep;
+      m_detNb = detNb;
+      m_peakV = peakV;
+      m_time = time;
+    }
+
+    /** Return the energy deposition in electrons */
+    double getEnergyDep()      const { return m_energyDep; }
+    /** Return the tube number */
+    int getdetNb()  const { return m_detNb; }
+    /** Return peak */
+    double getPeakV() const { return m_peakV; }
+    /** Return time of hit */
+    double getTime() const { return m_time; }
+
+  private:
+    /** Deposited energy in electrons */
+    double m_energyDep;
+    /** Detector Number */
+    int m_detNb;
+    /** waveform peak */
+    double m_peakV;
+    /** time of hit */
+    double m_time;
+
+
+    ClassDef(He3tubeHit, 1)
+  };
+
+} // end namespace Belle2
+
+#endif

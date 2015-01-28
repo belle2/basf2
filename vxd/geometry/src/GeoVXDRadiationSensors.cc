d061ae0a9e609c55eb3bd243451eb3c1ca9bd979
Martin Ritter <martin.ritter@lmu.de>
2015-01-28 15:26:25 +0000

    Fix bug in Diamond sensor placement
    Position was of by 90 degree (phi=0 was placed at 90degree).
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15152 b38c6c8f-bee8-465d-8448-9abe6915d496

--- vxd/geometry/src/GeoVXDRadiationSensors.cc
+++ vxd/geometry/src/GeoVXDRadiationSensors.cc
@@ -54,7 +54,7 @@ namespace Belle2 {
         BkgSensitiveDetector* sensitive = new BkgSensitiveDetector(m_subdetector.c_str(), id);
         volume->SetSensitiveDetector(sensitive);
         //and place it at the correct position
-        G4Transform3D transform = G4RotateZ3D(phi) * G4Translate3D(0, r * CLHEP::cm, z * CLHEP::cm) * G4RotateX3D(-M_PI / 2 - theta);
+        G4Transform3D transform = G4RotateZ3D(phi - M_PI / 2) * G4Translate3D(0, r * CLHEP::cm, z * CLHEP::cm) * G4RotateX3D(-M_PI / 2 - theta);
         new G4PVPlacement(transform, volume, name, top, false, 1);
       }
     }

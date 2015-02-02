38bf1105c67d43a319d2d213892bae68f17dab18
Oliver Frost <oliver.frost@desy.de>
2015-02-02 17:49:39 +0000

    TrackFindingCDC: Made common header files to include all available filters.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15306 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingCDC/filters/facet/include/FacetFilters.h
@@ -0,0 +1,15 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include "BaseFacetFilter.h"
+#include "AllFacetFilter.h"
+#include "MCFacetFilter.h"
+#include "SimpleFacetFilter.h"
+
---
+++ tracking/trackFindingCDC/filters/facet_facet/include/FacetNeighborChoosers.h
@@ -0,0 +1,14 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include "BaseFacetNeighborChooser.h"
+#include "AllFacetNeighborChooser.h"
+#include "MCFacetNeighborChooser.h"
+#include "SimpleFacetNeighborChooser.h"

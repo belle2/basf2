4d48f69d5f573fec9e34edd15b68fc9780768b5d
Jakob Lettenbichler <jkl@jodoschka.com>
2015-02-02 01:50:08 +0000

    improved test-quality for the secMapVector
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15241 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/tests/vxdtf/secMapVector.cc
+++ tracking/tests/vxdtf/secMapVector.cc
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 
 #include <string>
-#include <utility> // std::pair
+#include <utility> // std::pair, std::move(objects)
 #include <stdio.h> // remove(file)
 
 #include "TFile.h"
@@ -26,49 +26,66 @@ namespace Belle2 {
   /** Filling and reading SecMapVector in/from root file */
   TEST_F(SecMapVectorTest, readAndWriteTObjectToRootFile)
   {
+    string fName = "demo.root";
+
     /// create SecMapVector, fill with some fake VXDTFTemporarySecMaps (setting only the name to check whether everything went fine or not)
-    B2INFO("creating a secMap named aSecMap:")
+    string aSecMapName = "aSecMap";
     SecMapVector aSecMapVector;
     VXDTFRawSecMap aSecMap;
-    aSecMap.setMapName(string("aSecMap"));
-    SecMapVector::MapPack firstPack = make_pair(string("aSecMap"), aSecMap);
+    aSecMap.setMapName(aSecMapName);
+    aSecMap.setLowerMomentumThreshold(0.23);
+    SecMapVector::MapPack firstPack = make_pair(aSecMapName, aSecMap);
     aSecMapVector.push_back(firstPack);
-    B2INFO("creating a secMap named anotherSecMap:")
+    B2INFO("creating a secMap named " << aSecMap.getMapName() << " having " << aSecMap.getMaxDistance2origin() << " as maxDistance2Origin- and " << aSecMap.getLowerMomentumThreshold() << " for lowerMomentum-threshold")
+
+    std::string anotherSecMapName = "anotherSecMap";
     VXDTFRawSecMap anotherSecMap;
-    anotherSecMap.setMapName(string("anotherSecMap"));
-    SecMapVector::MapPack secondPack = make_pair(string("anotherSecMap"), anotherSecMap);
+    anotherSecMap.setMapName(anotherSecMapName);
+    anotherSecMap.setLowerMomentumThreshold(0.23);
+    SecMapVector::MapPack secondPack = make_pair(anotherSecMapName, anotherSecMap);
     aSecMapVector.push_back(secondPack);
+    B2INFO("creating a secMap named " << anotherSecMap.getMapName() << " having " << anotherSecMap.getMaxDistance2origin() << " as maxDistance2Origin- and " << anotherSecMap.getLowerMomentumThreshold() << " for lowerMomentum-threshold")
+
+    for (const SecMapVector::MapPack & aMap : aSecMapVector.getFullVector()) {
+      B2INFO("vector stored map with name " << aMap.second.getMapName() << " and lower threshold " << aMap.second.getLowerMomentumThreshold())
+    }
 
     /// create root file, fill SecMapVector into file, store it, close
     B2INFO("opening root file:");
-    TFile f("demo.root", "recreate");
+    TFile f(fName.c_str(), "recreate");
     B2INFO("storing data to root file:")
     aSecMapVector.Write();
+    B2INFO("closing file:")
     f.Close();
 
     /// repeat first step to check, whether files can be filled repeatedly
-    B2INFO("creating a secMap named aThirdSecMap:")
+    string aThirdSecMapName = "aThirdSecMap";
     SecMapVector aSecMapVector2;
     VXDTFRawSecMap aThirdSecMap;
-    aThirdSecMap.setMapName(string("aThirdSecMap"));
-    SecMapVector::MapPack thirdPack = make_pair(string("aThirdSecMap"), aThirdSecMap);
+    aThirdSecMap.setMapName(aThirdSecMapName);
+    aThirdSecMap.setLowerMomentumThreshold(0.23);
+    SecMapVector::MapPack thirdPack = make_pair(aThirdSecMapName, aThirdSecMap);
     aSecMapVector2.push_back(thirdPack);
-    B2INFO("creating a secMap named aFourthSecMap:")
+    B2INFO("creating a secMap named " << aThirdSecMap.getMapName() << " having " << aThirdSecMap.getMaxDistance2origin() << " as maxDistance2Origin- and " << aThirdSecMap.getLowerMomentumThreshold() << " for lowerMomentum-threshold")
+
+    string aFourthSecMapName = "aFourthSecMap";
     VXDTFRawSecMap aFourthSecMap;
-    aFourthSecMap.setMapName(string("aFourthSecMap"));
-    SecMapVector::MapPack fourthPack = make_pair(string("aFourthSecMap"), aFourthSecMap);
+    aFourthSecMap.setMapName(aFourthSecMapName);
+    aFourthSecMap.setLowerMomentumThreshold(0.23);
+    SecMapVector::MapPack fourthPack = make_pair(aFourthSecMapName, aFourthSecMap);
     aSecMapVector2.push_back(fourthPack);
+    B2INFO("creating a secMap named " << aFourthSecMap.getMapName() << " having " << aFourthSecMap.getMaxDistance2origin() << " as maxDistance2Origin- and " << aFourthSecMap.getLowerMomentumThreshold() << " for lowerMomentum-threshold")
 
     /// reopen root file, fill SecMapVector into file, store it, close
     B2INFO("opening root file:");
-    TFile f3("demo.root", "update");
+    TFile f3(fName.c_str(), "update");
     B2INFO("storing data to root file:")
     aSecMapVector2.Write();
     f3.Close();
 
 
     /// reopen file, read entries and check them
-    TFile f2("demo.root");
+    TFile f2(fName.c_str());
     if (f2.IsZombie()) { B2ERROR("file could not be reopened!"); }
     else {
       SecMapVector* retrievedVector;
@@ -79,26 +96,28 @@ namespace Belle2 {
       while ((key = (TKey*)next())) {
 
         try {
-          retrievedVector = dynamic_cast<SecMapVector*>(key->ReadObj());
+          retrievedVector = static_cast<SecMapVector*>(key->ReadObj());
         } catch (exception& e) {
           B2WARNING("Key was not a SecMapVector, therefore error message: " << e.what() << "\n Skipping this key...")
           continue;
         }
 
-        for (auto entry : retrievedVector->getFullVector()) {
+        for (SecMapVector::MapPack & entry : retrievedVector->getFullVector()) {
+          EXPECT_FLOAT_EQ(-1, entry.second.getMaxDistance2origin());
+          EXPECT_FLOAT_EQ(1.5, entry.second.getMagneticFieldStrength());
+          EXPECT_FLOAT_EQ(0.23, entry.second.getLowerMomentumThreshold());
           B2INFO(".first is: " << entry.first << ", .second->getMapName() is:" << entry.second.getMapName())
         }
       }
       f2.Close();
     }
 
-    /// delete file from disk
-    if (remove("demo.root") != 0)
-    { B2ERROR("could not delete file!") }
-    else
-    { B2INFO("File successfully deleted") }
+//     /// delete file from disk
+//     if (remove(fName.c_str()) != 0)
+//     { B2ERROR("could not delete file!") }
+//     else
+//     { B2INFO("File successfully deleted") }
 
-//     EXPECT_DOUBLE_EQ(2.329208242689889, aFilter.calcCircleDist2IP());
   }
 
 }  // namespace

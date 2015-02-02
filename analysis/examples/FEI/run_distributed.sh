e4fa05227870b9859e9a7294f5109e3a7a9cd418
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2015-02-02 02:51:57 +0000

    minor fixes to example
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15258 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/examples/FEI/run_distributed.sh
+++ analysis/examples/FEI/run_distributed.sh
@@ -19,17 +19,17 @@ collectionDirectory="/home/belle/pulver/B_generic/collection"
 jobDirectory="/home/belle/pulver/B_generic/jobs"
 
 #use HSM for large amounts of data (.root files from training, ntuples)
-collectionDirectoryReal="/ghi/fs01/belle2/bdata/pulver/B_generic_daughter0mass/collection"
+collectionDirectoryReal="/ghi/fs01/belle2/bdata/pulver/B_generic/collection"
 #cache intermediate information (probably needs at least as much space as input data, should be on HSM)
 persistentDirectory="/ghi/fs01/belle2/bdata/pulver/B_generic/persistent"
 
 #Data files to use as input:
 #streams 0-39
-#allMcFiles=(/group/belle2/MC/generic/{mixed,charged,ccbar,ssbar,ddbar,uubar}/mcprod1405/BGx0/*_s{0{0..9},{10..39}}/*.root)
+#allMcFiles=(/group/belle2/MC/generic/{mixed,charged}/mcprod1405/BGx0/*_s{0{0..9},{10..39}}/*.root)
 #10 independent streams
-allMcFiles=(/group/belle2/MC/generic/{mixed,charged,ccbar,ssbar,ddbar,uubar}/mcprod1405/BGx0/*_s{40..49}/*.root)
+allMcFiles=(/group/belle2/MC/generic/{mixed,charged}/mcprod1405/BGx0/*_s{40..49}/*.root)
 #all 50 streams
-#allMcFiles=(/group/belle2/MC/generic/{mixed,charged,ccbar,ssbar,ddbar,uubar}/mcprod1405/BGx1/*_s{0{0..9},{10..49}}/*.root)
+#allMcFiles=(/group/belle2/MC/generic/{mixed,charged}/mcprod1405/BGx1/*_s{0{0..9},{10..49}}/*.root)
 ################################################################################
 
 

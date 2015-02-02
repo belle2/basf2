7347fbae572ed330b44a9a80639d44e35b0d2718
Oliver Frost <oliver.frost@desy.de>
2015-02-02 17:44:52 +0000

    TrackFindingCDC: Extracted segment workers as modules for the first stage.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15289 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingCDC/modules/segmentation/include/SegmentFinderCDCMCTruthModule.h
@@ -0,0 +1,33 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef SEGMENTFINDERCDCMCTRUTHMODULE_H_
+#define SEGMENTFINDERCDCMCTRUTHMODULE_H_
+
+#include <tracking/trackFindingCDC/modules/segmentation/SegmentFinderCDCBaseModule.h>
+
+#include <framework/core/Module.h>
+
+namespace Belle2 {
+  namespace TrackFindingCDC {
+
+    /// Worker for building reconstructed segments form wirehits using reconstructed facets
+    class SegmentFinderCDCMCTruthModule : public SegmentFinderCDCBaseModule {
+
+    public:
+      /// Generates the segment from Monte Carlo information. Default orientation is the flight direction.
+      virtual void generate(std::vector<Belle2::TrackFindingCDC::CDCRecoSegment2D>& segments);
+
+    }; // end class SegmentFinderCDCMCTruthModule
+
+  } //end namespace TrackFindingCDC
+} //end namespace Belle2
+
+#endif // SEGMENTFINDERCDCMCTRUTHMODULE_H_
---
+++ tracking/trackFindingCDC/modules/segmentation/src/SegmentFinderCDCMCTruthModule.cc
@@ -0,0 +1,50 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <tracking/trackFindingCDC/modules/segmentation/SegmentFinderCDCMCTruthModule.h>
+
+#include <tracking/trackFindingCDC/mclookup/CDCMCManager.h>
+#include <tracking/trackFindingCDC/mclookup/CDCMCTrackStore.h>
+#include <tracking/trackFindingCDC/mclookup/CDCSimHitLookUp.h>
+
+using namespace std;
+using namespace Belle2;
+using namespace TrackFindingCDC;
+
+REG_MODULE(SegmentFinderCDCMCTruth);
+
+void SegmentFinderCDCMCTruthModule::generate(std::vector<CDCRecoSegment2D>& outputSegments)
+{
+  CDCMCManager::getInstance().fill();
+
+  const CDCMCTrackStore& mcTrackStore = CDCMCTrackStore::getInstance();
+  const CDCSimHitLookUp& simHitLookUp = CDCSimHitLookUp::getInstance();
+
+  typedef Belle2::TrackFindingCDC::CDCMCTrackStore::CDCHitVector CDCHitVector;
+
+  const std::map<ITrackType, std::vector<CDCHitVector>>& mcSegmentsByMCParticleIdx = mcTrackStore.getMCSegmentsByMCParticleIdx();
+  for (const std::pair<ITrackType, std::vector<CDCHitVector>>& mcSegmentsAndMCParticleIdx : mcSegmentsByMCParticleIdx) {
+
+    const std::vector<CDCHitVector>& mcSegments =  mcSegmentsAndMCParticleIdx.second;
+
+    // Reserve double the space if both orientations shall be generated
+    outputSegments.reserve(mcSegments.size());
+
+    for (const CDCHitVector & mcSegment : mcSegments) {
+      outputSegments.push_back(CDCRecoSegment2D());
+      CDCRecoSegment2D& recoSegment2D = outputSegments.back();
+
+      for (const CDCHit * ptrHit : mcSegment) {
+        CDCRecoHit2D recoHit2D = simHitLookUp.getClosestPrimaryRecoHit2D(ptrHit);
+        recoSegment2D.push_back(recoHit2D);
+      }
+    }
+  }
+}

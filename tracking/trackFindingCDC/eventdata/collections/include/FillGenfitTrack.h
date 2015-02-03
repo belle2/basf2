563def953cd75d925b0b1a3354b1f887ecff03a6
Oliver Frost <oliver.frost@desy.de>
2015-02-03 18:08:51 +0000

    TrackFindingCDC: Extracted filling of Genfit track to a separate function.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15343 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingCDC/eventdata/collections/include/FillGenfitTrack.h
@@ -0,0 +1,79 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#ifndef FILLGENFITTRACK_H_
+#define FILLGENFITTRACK_H_
+
+#include <tracking/trackFindingCDC/eventdata/entities/CDCRLWireHit.h>
+#include <tracking/trackFindingCDC/eventdata/entities/CDCWireHit.h>
+#include <tracking/trackFindingCDC/topology/CDCWire.h>
+
+#include <genfit/TrackCand.h>
+#include <genfit/WireTrackCandHit.h>
+#include <framework/gearbox/Const.h>
+
+namespace Belle2 {
+  namespace TrackFindingCDC {
+
+
+    /** Translates a range of hits and inserts them in the genfit::TrackCand
+     *
+     * @param hits              A range of hits, which elements support ->getRLWireHit() to access a wire hit including a right left passage hypotheses.
+     * @param[out] gfTrackCand  Genfit track candidate to be filled
+     * @param reverseRLInfo     Switch to indicate that the reverse right left passage shall be forwarded. */
+    template<class HitRange>
+    void fillHitsInto(const HitRange& hits, genfit::TrackCand& gfTrackCand, bool reverseRLInfo = false)
+    {
+      // the sorting parameter is just used to reinforce the order in the range.
+      int sortingParameter = -1;
+
+      for (const auto & genHit : hits) {
+        ++sortingParameter;
+
+        const CDCRLWireHit& rlWireHit = genHit->getRLWireHit();
+        const CDCWireHit& wireHit = rlWireHit.getWireHit();
+        const CDCWire& wire = rlWireHit.getWire();
+
+        // the hit id correspondes to the index in the TClonesArray
+        // this is stored in the wirehit the recohit is based on
+        unsigned int storeIHit = wireHit.getStoreIHit();
+
+        // the plain id serves to mark competition between two or more hits
+        // use the wire id here which is unique for all the hits in the track
+        // but it may also serve the fact that a track can only be responsable for a single hit on each wire
+        // double hits should correspond to multiple tracks
+        unsigned int planeId = wire.getEWire();
+
+        // Right left ambiguity resolution
+        RightLeftInfo rlInfo = rlWireHit.getRLInfo();
+
+        // Check if reversion is requested
+        if (reverseRLInfo) {
+          rlInfo = reversed(rlInfo);
+        }
+
+        // Note:  rlInfo < 0 means LEFT,   rlInfo > 0 means RIGHT,  which is the same as in Genfit
+        signed char genfitLeftRight = rlInfo;
+
+        //do not delete! the genfit::TrackCand has ownership
+        genfit::WireTrackCandHit* cdcTrackCandHit = new genfit::WireTrackCandHit(Const::CDC,
+            storeIHit,
+            planeId,
+            sortingParameter,
+            genfitLeftRight);
+
+        gfTrackCand.addHit(cdcTrackCandHit);
+
+      }
+    }
+
+  } // namespace TrackFindingCDC
+} // namespace Belle2
+
+#endif // FILLGENFITTRACK_H_
--- tracking/trackFindingCDC/eventdata/tracks/src/CDCTrack.cc
+++ tracking/trackFindingCDC/eventdata/tracks/src/CDCTrack.cc
@@ -10,8 +10,7 @@
 
 #include "../include/CDCTrack.h"
 
-#include <genfit/WireTrackCandHit.h>
-#include <framework/gearbox/Const.h>
+#include <tracking/trackFindingCDC/eventdata/collections/FillGenfitTrack.h>
 
 using namespace std;
 using namespace Belle2;
@@ -19,9 +18,6 @@ using namespace TrackFindingCDC;
 using namespace genfit;
 
 TRACKFINDINGCDC_SwitchableClassImp(CDCTrack)
-
-
-
 
 void CDCTrack::fillInto(genfit::TrackCand& trackCand) const
 {
@@ -32,80 +28,15 @@ void CDCTrack::fillInto(genfit::TrackCand& trackCand) const
     getStartTrajectory3D().fillInto(trackCand);
   }
 
-
-
   // Add the hits
   if (getFBInfo() == BACKWARD) {
-
-    // Calculate the travel distance relative to the end of the track
-    FloatType endPrepS = getEndRecoHit3D().getPerpS();
-
-    for (const CDCRecoHit3D & recoHit3D : reverseRange()) {
-      // the hit id correspondes to the index in the TClonesArray
-      // this is stored in the wirehit the recohit is based on
-      unsigned int storeIHit = recoHit3D.getWireHit().getStoreIHit();
-
-      // the plain id serves to mark competition between two or more hits
-      // use the wire id here which is unique for all the hits in the track
-      // but it may also serve the fact that a track can only be responsable for a single hit on each wire
-      // double hits should correspond to multiple tracks
-      unsigned int planeId = recoHit3D.getWire().getEWire();
-
-      // the sorting parameter is used to sort the cdchits accoring to the direction of flight
-      // we assign the estimated flight distance to them
-      double sortingParameter = endPrepS - recoHit3D.getPerpS(); // translate it to a scale from the end of the track
-
-      // Right left ambiguity resolution from the hypotheses stored in the reconstructed hit must be reversed to
-      RightLeftInfo rlInfo = reversed(recoHit3D.getRLInfo());
-      // Note:  rlInfo < 0 means LEFT,   rlInfo > 0 means RIGHT,  which is the same as in Genfit
-      char genfitLeftRight = rlInfo;
-
-
-      //do not delete! the genfit::TrackCand has ownership
-      WireTrackCandHit* aCdcTrackCandHit = new WireTrackCandHit(Const::CDC,
-                                                                storeIHit,
-                                                                planeId,
-                                                                sortingParameter,
-                                                                genfitLeftRight);
-
-      trackCand.addHit(aCdcTrackCandHit);
-
-    }
+    bool reverseRLInfo = true;
+    fillHitsInto(reverseRange(), trackCand, reverseRLInfo);
 
   } else {
-    for (const CDCRecoHit3D & recoHit3D : *this) {
-      //the hit id correspondes to the index in the TClonesArray
-      //this is stored in the wirehit the recohit is based on
-      int storeIHit = recoHit3D.getWireHit().getStoreIHit();
-
-      //the plain id serves to mark competition between two or more hits
-      //use the wire id here which is unique for all the hits in the track
-      //but it may also serve the fact that a track can only be responsable for a single hit on each wire
-      //double hits should correspond to multiple tracks
-      int planeId = recoHit3D.getWire().getEWire();
-
-      // the rho parameter is used to sort the cdchits accoring to the direction of flight
-      // we assign the estimated flight distance to them
-      double sortingParameter = recoHit3D.getPerpS();
-
-      // Right left ambiguity resolution from the hypotheses stored in the reconstructed hit not reversed in the normal case
-      RightLeftInfo rlInfo = recoHit3D.getRLInfo();
-      // Note:  rlInfo < 0 means LEFT,   rlInfo > 0 means RIGHT,  which is the same as in Genfit
-      char genfitLeftRight = rlInfo;
-
-      //do not delete! the genfit::TrackCand has ownership
-      WireTrackCandHit* aCdcTrackCandHit = new WireTrackCandHit(Const::CDC,
-                                                                storeIHit,
-                                                                planeId,
-                                                                sortingParameter,
-                                                                genfitLeftRight);
-
-      trackCand.addHit(aCdcTrackCandHit);
-
-    }
-
+    bool reverseRLInfo = false;
+    fillHitsInto(*this, trackCand, reverseRLInfo);
   }
-
 }
 
 

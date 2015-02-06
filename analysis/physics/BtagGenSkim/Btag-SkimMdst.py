60cbbe24619465ce6f114d5352688e03f252d745
Matic Lubej <matic.lubej@ijs.si>
2015-02-06 09:16:13 +0000

    Applied B-tag scripts fix
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15412 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/physics/BtagGenSkim/Btag-SkimMdst.py
+++ analysis/physics/BtagGenSkim/Btag-SkimMdst.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 
 ######################################################
-# Reconstruction of several possible decay modes of charged B mesons.
+# Reconstruction of several possible decay modes of B mesons.
 # Charged B mesons are reconstructed in imp_BC_decayRec.py
+# Neutral B mesons are reconstructed in imp_BN_decayRec.py
 # D mesons are reconstructed in imp_D_decayRec.py
 # ccbar events are reconstructed in imp_cc_decayRec.py
-# decay mode names are written in imp_chNames.py
 #
 # Contributors: A. Zupanc (August 2014)
-#               M. Lubej (December 2014)
-#               A. Polsak (December 2014)
+#               M. Lubej (January 2014)
+#               A. Polsak (January 2014)
 ######################################################
 
 # load all necessary scripts
@@ -32,24 +32,23 @@ from modularAnalysis import cutAndCopyList
 from imp_D_decayRec import *
 from imp_cc_decayRec import *
 from imp_BC_decayRec import *
+from imp_BN_decayRec import *
 import sys
 
-# input parameters: e.g. basf2 BS301-SkimMdstCharged.py input.root output.root 1
+# input parameters: e.g. basf2 Btag-SkimMdst.py input.root output.root 1
 # "0": include only most common decay modes
 # "1": include all written decay modes
 
 if len(sys.argv) != 4:
-    sys.exit('Must provide enough arguments: [input file name(s)] [output file name] [0/1]'
+    sys.exit('Must provide enough arguments: [input file name] [output file name] [0/1]'
              )
 
-inputNames = sys.argv[1]
+inputName = sys.argv[1]
 outputName = sys.argv[2]
 recLong = sys.argv[3]
 
-inputFiles = [inputNames]
-
 # load files
-inputMdstList(inputFiles)
+inputMdstList(inputName)
 
 # create lists of FSPs
 pions = ('pi+:all', '')
@@ -70,7 +69,6 @@ fillParticleListsFromMC([
     kshort,
     ], True)
 
-# next lines do the same as the lines for pi0 and K_S0
 # reconstruct charm
 reconstructD0()
 reconstructDP()
@@ -84,7 +82,7 @@ reconstructJpsi()
 reconstructPsi2S()
 reconstructChi_cy()
 
-# reconstruct B+B- mesons
+# reconstruct B mesons
 q = int(recLong)
 if q != 0 and q != 1:
     sys.exit('Must provide argument 0 or 1')
@@ -96,27 +94,40 @@ reconstructBPDST0YPS1(q)
 reconstructBPccbarYPS1(q)
 reconstructBPmisc(q)
 
+reconstructB0DMYPS0(q)
+reconstructB0DMYPS1(q)
+reconstructB0DSTMYPS0(q)
+reconstructB0DSTMYPS1(q)
+reconstructB0ccbarY0S1(q)
+reconstructB0misc(q)
+
 # merge individual B+ lists
-# the same is already done for D mesons and ccbar, but in their own files, B mesons are here separately for ease of access
 copyLists('B+:antiD0Y+', ['B+:antiD0Y+S0', 'B+:antiD0Y+S1'])
 copyLists('B+:antiD*0Y+', ['B+:antiD*0Y+S0', 'B+:antiD*0Y+S1'])
 copyLists('B+:rest', ['B+:ccbarYPS1', 'B+:misc'])
 
 copyLists('B+:all', ['B+:antiD0Y+', 'B+:antiD*0Y+', 'B+:rest'])
 
+# merge individual B0 lists
+copyLists('B0:D-Y+', ['B0:D-Y+S0', 'B0:D-Y+S1'])
+copyLists('B0:D*-Y+', ['B0:D*-Y+S0', 'B0:D*-Y+S1'])
+copyLists('B0:rest', ['B0:ccbarY0S1', 'B0:misc'])
+
+copyLists('B0:all', ['B0:D-Y+', 'B0:D*-Y+', 'B0:rest'])
+
 # add SkimFilter module to set condition variable based on the number of reconstructed B-tag mesons
 skim = register_module('SkimFilter')
-skim.param('particleLists', ['B+:all'])
+skim.param('particleLists', ['B+:all', 'B0:all'])
 analysis_main.add_module(skim)
 
 # save event to a new mdst file if it contained at least one B-tag meson
-new_analysis_main = create_path()
-add_mdst_output(new_analysis_main, True, outputName)
-skim.if_true(new_analysis_main, AfterConditionPath.CONTINUE)
+empty_path = create_path()
+skim.if_false(empty_path)
+add_mdst_output(analysis_main, True, outputName)
 
 # print out some further info
 summaryOfLists(['B+:all'])
-summaryOfLists(['B+:antiD0Y+', 'B+:antiD*0Y+', 'B+:rest'])
+summaryOfLists(['B0:all'])
 
 # process the events
 process(analysis_main)

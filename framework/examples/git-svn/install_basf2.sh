4eeb8aff733f7c11adad78c1741881e915296104
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2015-02-07 07:37:00 +0000

    Add git-svn scripts to framework/examples
    Includes update to pre-commit hook by Martin Ritter to check changes in
    index
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15443 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ framework/examples/git-svn/install_basf2.sh
@@ -0,0 +1,144 @@
+#!/usr/bin/env bash
+# A script to do a fully automatic installation of basf2 and externals (using git-svn for basf2)
+
+#TODO: could start cloning at beginning, while tools are being installed...
+#TODO: just get externals from .externals in HEAD
+#TODO v00-05-04 has a different set of binaries (ubuntu1204 gone, 1404, sl5-32bit added)
+
+#abort on errors
+set -e
+
+BASF2_SVN="https://belle2.cc.kek.jp/svn/trunk/software"
+
+echo "This script will install the basf2 software, including tools and externals, into `pwd`,"
+echo "creating the directories tools/, externals/ and basf2/."
+echo "The basf2/ folder will be a git repository containing the entire history imported from SVN,"
+echo "see https://belle2.cc.kek.jp/~twiki/bin/view/Software/GitSvn for details."
+echo ""
+echo "Press return to continue, Ctrl-c to abort."
+read
+
+if ! svn help >& /dev/null
+then
+  echo "subversion is not installed, please install it and try again."
+  exit 1
+fi
+
+if ! git help svn >& /dev/null
+then
+  echo "git-svn is not installed, please install it and try again."
+  exit 1
+fi
+
+if [ ! -d tools/ ]
+then
+  svn co https://belle2.cc.kek.jp/svn/trunk/tools
+  ./tools/prepare_belle2.sh || true # optional packages still cause bad return value...
+  ./tools/install.sh
+fi
+
+echo "Tools installed."
+source tools/setup_belle2
+
+echo "================================================================================"
+CURRENT_REV=`svn log https://belle2.cc.kek.jp/svn/trunk/software 2>/dev/null | head -2 | tail -1 | awk '{print $1}'`
+echo "How much of the SVN history do you want to import? Select $CURRENT_REV for a quick checkout with history starting from the current revision, or 'all' for the entire history (.git requires about 200MB for 100 commits, or 900MB for all). Ctrl-c to abort."
+select SVN_CLONE_FROM in "all" $CURRENT_REV; do
+  echo "Selected: $SVN_CLONE_FROM"
+  break
+done
+
+echo "Looking for available externals..."
+AVAILABLE_EXTERNALS=`get_externals.sh | head -n -1` #skip development, added manually
+
+echo "================================================================================"
+echo "Please select your preferred externals version. Ctrl-c to abort."
+select EXTERNALS_VERSION in $AVAILABLE_EXTERNALS development; do
+  echo "Selected: $EXTERNALS_VERSION"
+  break
+done
+
+if [ ! -d externals/$EXTERNALS_VERSION ]
+then
+  if [ "$EXTERNALS_VERSION" == "development" ]; then
+    BINARY_VARIANT=""
+  else
+    echo "================================================================================"
+    echo "Select a binary variant of the externals for your system, or 'source' to compile them from source. All variants are for 64 bit unless mentioned otherwise. Ctrl-c to abort."
+    select BINARY_VARIANT in "source" sl5 debian6 fedora16 ubuntu1004 ubuntu1204 ubuntu1204_32bit; do
+      if [ "$BINARY_VARIANT" == "source" ]; then
+        BINARY_VARIANT=""
+      fi
+      break
+    done
+  fi
+fi
+
+echo "================================================================================"
+if [ ! -d externals/ ]
+then
+  echo "Creating externals/ directory..."
+  mkdir externals
+fi
+
+if [ ! -d externals/$EXTERNALS_VERSION ]
+then
+  echo "Now installing the externals... (logging to get_externals.log)"
+  get_externals.sh $EXTERNALS_VERSION $BINARY_VARIANT >& get_externals.log &
+  EXTERNALS_PID=$!
+else
+  echo "Externals $EXTERNALS_VERSION already installed, nothing to do."
+  EXTERNALS_PID=$!
+fi
+
+echo "Cloning SVN repository into basf2/... (this will take a while)"
+if [ "$SVN_CLONE_FROM" == "all" ]; then
+  GIT_SVN_CLONE_ARGS=""
+else
+  GIT_SVN_CLONE_ARGS="-$SVN_CLONE_FROM:HEAD"
+fi
+
+git svn clone $GIT_SVN_CLONE_ARGS $BASF2_SVN basf2
+
+# remaining setup
+pushd .
+cd basf2
+ln -s site_scons/SConstruct .
+echo "head" > .release
+echo "$EXTERNALS_VERSION" > .externals
+
+echo "Installing pre-commit hook"
+cp framework/examples/git-svn/pre-commit .git/hooks/
+chmod 755 .git/hooks/pre-commit
+
+
+echo "Installing hooks to deal with svn:externals (e.g. genfit2 package)"
+cp framework/examples/git-svn/svnexternals.py .git/hooks/
+pushd .git/hooks > /dev/null
+chmod 755 svnexternals.py
+ln -s svnexternals.py post-rewrite
+ln -s svnexternals.py post-checkout
+popd > /dev/null
+
+
+echo "git svn clone finished. Waiting for externals install..."
+
+if wait $EXTERNALS_PID; then
+  echo "Externals install successful!"
+
+  setuprel
+  echo "Fetching svn:externals..."
+  ./.git/hooks/svnexternals.py
+
+  popd
+else
+  echo "Something went wrong during installation of externals, please check get_externals.log!"
+  exit 1
+fi
+
+echo "================================================================================"
+echo "Installation finished. You can now set up your basf2 environment using"
+echo "  source tools/setup_belle2"
+echo "  cd basf2"
+echo "  setuprel"
+echo "and compile basf2 using 'scons' (use -j4 to compile with e.g. 4 cores)"
---
+++ framework/examples/git-svn/svnexternals.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# coding: utf8
+
+import sys
+import os
+import re
+from subprocess import check_output, call, CalledProcessError
+
+
+def parse_external(line):
+    """Parse svn:externals property.
+
+    assume that the externals are either defined as
+    <folder> [-r<revision>] <url>
+    <folder>[@<revision>] <url>
+
+    return folder, revision, url
+    revision might be None
+    """
+    match = re.match("""
+        ^(?P<folder>[^@ \t]*)                # match the folder
+        (?:(?:@|\s+-r)(?P<revision>[0-9]*))? # optionally, match the revision
+        \s*(?P<url>.*)$                      # finally, match the rest as url
+        """, line.strip(), re.VERBOSE)
+    if not match:
+        raise ValueError("svn::externals line could not be parsed")
+    return match.groups()
+
+
+def find_svnrevision(commit="HEAD"):
+    """Find the svn revision the current branch is based on.
+    git-svn history should be very linear but we need to now on which svn
+    revision the current branch is based. To do this, we traverse the parents
+    of the current commit until git svn finds a corresponding svn revision
+
+    return svn revision or None if none could be found
+    """
+    revlist = check_output(["git", "rev-list", "--parents", commit])
+    for line in revlist.splitlines():
+        parents = line.split()
+        revision = check_output(["git", "svn", "find-rev", parents[0]])
+        try:
+            revision = int(revision)
+            return revision
+        except ValueError:
+            pass
+
+    return None
+
+# Get Belle2 environment variables
+try:
+    LOCAL_DIR = os.environ["BELLE2_LOCAL_DIR"]
+except KeyError:
+    print "Belle II software not correctly set up, aborting"
+    sys.exit(1)
+
+# Checking all folders takes far to long, lets provide a list of dirs to check
+# as environment variable
+externals = os.environ.get("BELLE2_SVN_EXTERNALS", "genfit2").split()
+
+# Check arguments: See if we are called as post-rewrite or post checkout and if
+# so determine if we need to do anything
+basename = os.path.basename(sys.argv[0])
+if basename == "post-checkout":
+    prev_head, new_head, branch_switch = sys.argv[1:]
+    if not branch_switch or prev_head == new_head:
+        # No branch real switch, nothing to do
+        sys.exit(0)
+
+    prev_revision = find_svnrevision(prev_head)
+    current_revision = find_svnrevision(new_head)
+    if prev_revision == current_revision:
+        # Nothing to do, both branches are based on the same svn revision
+        sys.exit(0)
+else:
+    if basename == "post-rewrite" and sys.argv[1] != "rebase":
+        # Nothing to do
+        sys.exit(0)
+
+    # Find svn revision current head is based on
+    current_revision = find_svnrevision()
+
+if current_revision is None:
+    print "Cannot determine SVN revision, cannot check for externals"
+    sys.exit(1)
+
+# Let's change into the release dir
+os.chdir(LOCAL_DIR)
+
+# See if there is already an gitignore file, if so read it
+gitignore = []
+if os.path.exists(".gitignore"):
+    f = open(".gitignore")
+    gitignore = f.readlines()
+    f.close()
+
+# Get the latest revision we checked out. This might not be the revision the
+# current branch is based on but finding that revision is more difficult.
+current_revision = find_svnrevision()
+print "Checking svn:externals for r%s" % current_revision
+
+for e in externals:
+    print "directory '%s'" % e
+    try:
+        prop = check_output(["git", "svn", "propget",
+                             "-r%s" % current_revision, "svn:externals", e])
+    except CalledProcessError:
+        # Apparently no externals here, move on
+        continue
+
+    # Fetch all externals
+    for line in prop.split("\n"):
+        if not line.strip():
+            continue
+
+        dirname, revision, url = parse_external(line)
+        abspath = os.path.join(LOCAL_DIR, e, dirname)
+        # Build command line for svn
+        args = ["svn"]
+        if os.path.exists(abspath):
+            # Seems to exist already, so update to specified revision
+            print "Updating", os.path.join(e, dirname),
+            args.append("update")
+        else:
+            # Not there yet, checkout using svn
+            print "Fetching", os.path.join(e, dirname),
+            print "from", url,
+            args += ["checkout", url]
+
+        # If revision is specified, add it after the svn command
+        if revision is not None:
+            print "r" + revision,
+            args.insert(2, "-r" + revision)
+
+        # finish line
+        print
+        call(args + [abspath])
+
+        # Check if it is already excluded
+        exclude = "/%s/" % os.path.join(e, dirname).strip("/")
+        if not any(e.strip() == exclude for e in gitignore):
+            # If not, add it to the gitignore
+            gitignore += ["# ignore directory containing svn:externals\n",
+                          exclude + "\n"]
+
+# Write gitignore file
+f = open(".gitignore", "w")
+f.writelines(gitignore)
+f.close()

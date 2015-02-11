ab88a69fb2d9b05240b0ea743ca263728b65e1d8
Oliver Frost <oliver.frost@desy.de>
2015-02-11 14:39:39 +0000

    TrackFindingCDC: Made common header file to include all segment pair filter / choosers
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15531 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingCDC/filters/axial_stereo/include/AxialStereoSegmentPairFilters.h
@@ -0,0 +1,15 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include "BaseAxialStereoSegmentPairFilter.h"
+#include "AllAxialStereoSegmentPairFilter.h"
+#include "MCAxialStereoSegmentPairFilter.h"
+#include "SimpleAxialStereoSegmentPairFilter.h"
+
---
+++ tracking/trackFindingCDC/filters/axial_stereo_axial_stereo/include/AxialStereoSegmentPairNeighborChoosers.h
@@ -0,0 +1,14 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include "BaseAxialStereoSegmentPairNeighborChooser.h"
+#include "AllAxialStereoSegmentPairNeighborChooser.h"
+#include "MCAxialStereoSegmentPairNeighborChooser.h"
+#include "SimpleAxialStereoSegmentPairNeighborChooser.h"

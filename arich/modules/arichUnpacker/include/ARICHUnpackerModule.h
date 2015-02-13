ee250d393e66508234821f2abcb09616acce62f6
Luka Santelj <luka.santelj@ijs.si>
2015-02-13 05:50:11 +0000

    First version of arich raw data packer and unpacker.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15594 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ arich/modules/arichUnpacker/include/ARICHUnpackerModule.h
@@ -0,0 +1,82 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Luka Santelj                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef ARICHUNPACKERMODULE_H
+#define ARICHUNPACKERMODULE_H
+
+#include <framework/core/Module.h>
+#include <arich/geometry/ARICHGeometryPar.h>
+
+namespace Belle2 {
+
+  /**
+   * Raw data unpacker for ARICH
+   */
+  class ARICHUnpackerModule : public Module {
+
+  public:
+
+    /**
+     * Constructor
+     */
+    ARICHUnpackerModule();
+
+    /**
+     * Destructor
+     */
+    virtual ~ARICHUnpackerModule();
+
+    /**
+     * Initialize the Module.
+     * This method is called at the beginning of data processing.
+     */
+    virtual void initialize();
+
+    /**
+     * Called when entering a new run.
+     * Set run dependent things like run header parameters, alignment, etc.
+     */
+    virtual void beginRun();
+
+    /**
+     * Event processor.
+     */
+    virtual void event();
+
+    /**
+     * End-of-run action.
+     * Save run-related stuff, such as statistics.
+     */
+    virtual void endRun();
+
+    /**
+     * Termination action.
+     * Clean-up, close files, summarize statistics, etc.
+     */
+    virtual void terminate();
+
+  private:
+
+    /**
+     * Unpack raw data given in production format
+     * @param buffer raw data buffer
+     * @param bufferSize buffer size
+     */
+    void unpackSuppressedData(const int* buffer, int bufferSize);
+    void unpackUnsuppressedData(const int* buffer, int bufferSize);
+    unsigned m_bitMask;
+    int m_debug;
+    ARICHGeometryPar* m_arichgp;  /**< geometry parameters */
+
+  };
+
+} // Belle2 namespace
+
+#endif
---
+++ arich/modules/arichUnpacker/src/ARICHUnpackerModule.cc
@@ -0,0 +1,189 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Luka Santelj                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+// Own include
+#include <arich/modules/arichUnpacker/ARICHUnpackerModule.h>
+
+#include <framework/core/ModuleManager.h>
+
+// framework - DataStore
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/StoreObjPtr.h>
+
+// framework aux
+#include <framework/gearbox/Unit.h>
+#include <framework/gearbox/Const.h>
+#include <framework/logging/Logger.h>
+
+// Dataobject classes
+#include <framework/dataobjects/EventMetaData.h>
+#include <rawdata/dataobjects/RawARICH.h>
+#include <arich/dataobjects/ARICHDigit.h>
+
+using namespace std;
+
+namespace Belle2 {
+
+  //using namespace ARICH;
+
+  //-----------------------------------------------------------------
+  //                 Register module
+  //-----------------------------------------------------------------
+
+  REG_MODULE(ARICHUnpacker)
+
+  //-----------------------------------------------------------------
+  //                 Implementation
+  //-----------------------------------------------------------------
+
+  ARICHUnpackerModule::ARICHUnpackerModule() : Module(), m_bitMask(0), m_debug(0),
+    m_arichgp(ARICHGeometryPar::Instance())
+
+  {
+    // set module description
+    setDescription("Raw data unpacker for ARICH");
+    addParam("bitMask", m_bitMask, "hit bit mask (4 bits/channel)", (unsigned)0xF);
+    addParam("debug", m_debug, "print data words bitmap", 0);
+
+    setPropertyFlags(c_ParallelProcessingCertified);
+
+  }
+
+  ARICHUnpackerModule::~ARICHUnpackerModule()
+  {
+  }
+
+  void ARICHUnpackerModule::initialize()
+  {
+
+    StoreArray<RawARICH> rawData;
+    rawData.isRequired();
+
+    StoreArray<ARICHDigit> digits;
+    digits.registerInDataStore();
+
+  }
+
+  void ARICHUnpackerModule::beginRun()
+  {
+  }
+
+  void ARICHUnpackerModule::event()
+  {
+
+    StoreArray<RawARICH> rawData;
+    StoreArray<ARICHDigit> digits;
+    digits.clear();
+
+
+    for (auto & raw : rawData) {
+      for (int finesse = 0; finesse < 4; finesse++) {
+        const int* buffer = raw.GetDetectorBuffer(0, finesse);
+        int bufferSize = raw.GetDetectorNwords(0, finesse);
+
+        if (bufferSize < 1)
+          continue;
+
+        unsigned dataFormat = (buffer[0] >> 24) & 0xFF;
+
+        switch (dataFormat) {
+          case 1:
+            unpackSuppressedData(buffer, bufferSize);
+            break;
+          case 2:
+            unpackUnsuppressedData(buffer, bufferSize);
+            break;
+          default:
+            B2ERROR("ARICHUnpacker: unknown data format type = " << dataFormat);
+        }
+      }
+    }
+  }
+
+  void ARICHUnpackerModule::unpackUnsuppressedData(const int* buffer, int bufferSize)
+  {
+
+    StoreArray<ARICHDigit> digits;
+
+    unsigned mergerID = (buffer[0] >> 8) & 0xFF;
+
+    unsigned version = (buffer[0] >> 16) & 0xFF;
+
+    switch (version) {
+      case 0:
+        for (int i = 1; i < bufferSize;) {
+          unsigned boardN = buffer[i]  & 0xFF;
+          int boardID = m_arichgp->getBoardFromMerger(mergerID, boardN);
+          i += 2;
+          for (int j = 0; j < 144; j++) {
+            unsigned chan = buffer[i] & (m_bitMask << (j % 8) * 4);
+            if (chan) digits.appendNew(boardID, j, 0);
+            if ((j + 1) % 8 == 0) i++;
+          }
+        }
+        break;
+
+      default:
+        B2ERROR("ARICHUnpacker::unpackData: unknown data format version = " << version);
+    }
+  }
+
+
+  void ARICHUnpackerModule::unpackSuppressedData(const int* buffer, int bufferSize)
+  {
+
+    StoreArray<ARICHDigit> digits;
+
+    unsigned short mergerID = (buffer[0] >> 8) & 0xFF;
+    unsigned version = (buffer[0] >> 16) & 0xFF;
+
+    switch (version) {
+      case 0:
+        for (int i = 1; i < bufferSize;) {
+          unsigned boardN = buffer[i]  & 0xFF;
+          int boardID = m_arichgp->getBoardFromMerger(mergerID, boardN);
+          i++;
+          int nhits = buffer[i] / 2;
+          i++;
+
+          // if the FEB is not assigned, skip its data
+          if (boardID < 0) {i += ceil(double(nhits) / 2.); continue;}
+
+          for (int j = 1; j < nhits + 1; j++) {
+            unsigned word = 0;
+            if (j % 2) word = (buffer[i] >> 16);
+            else {word = buffer[i]; i++;}
+            unsigned is_hit = word & m_bitMask;
+            unsigned chn = (word >> 8) & (0xFF);
+            if (is_hit) digits.appendNew(boardID, chn, 0);
+          }
+          if (nhits % 2) i++;
+        }
+        break;
+
+      default:
+        B2ERROR("ARICHUnpacker::unpackData: unknown data format version = " << version);
+    }
+
+  }
+
+
+  void ARICHUnpackerModule::endRun()
+  {
+  }
+
+  void ARICHUnpackerModule::terminate()
+  {
+  }
+
+
+} // end Belle2 namespace
+

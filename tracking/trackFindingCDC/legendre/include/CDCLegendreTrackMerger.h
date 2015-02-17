f519611a32f80281225f6523210cab05d4f5d0ff
Nils Braun <nils.braun@student.kit.edu>
2015-02-17 08:43:38 +0000

    Reworked the merging procedure. Commented out some unused methods
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15665 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/legendre/include/CDCLegendreTrackMerger.h
+++ tracking/trackFindingCDC/legendre/include/CDCLegendreTrackMerger.h
@@ -46,73 +46,104 @@ namespace Belle2 {
       void doTracksMerging();
 
       /** Try to merge given track with tracks in tracklist. */
-      void tryToMergeTrack(TrackCandidate* cand1);
+      void tryToMergeTrackWithOtherTracks(TrackCandidate* cand1);
 
-      /** Trying to merge tracks */
-      double tryToMergeAndFit(TrackCandidate* cand1, TrackCandidate* cand2, bool remove_hits = true);
+      /** Trying select core of the tracks and fit them together, then add good hits
+       * UNUSED */
+      //double selectCoreMergeFit(TrackCandidate* cand1, TrackCandidate* cand2);
 
-      /** Trying select core of the tracks and fit them together, then add good hits */
-      double selectCoreMergeFit(TrackCandidate* cand1, TrackCandidate* cand2);
+      /** Calculate distance between track and hit
+       * UNUSED */
+      //double checkDist(TrackHit* hit, TrackCandidate* cand);
 
-      /** Calculate distance between track and hit */
-      double checkDist(TrackHit* hit, TrackCandidate* cand);
-
-      /** Split tracks into positive and negative parts */
-      void splitTracks();
+      /** Split tracks into positive and negative parts
+       * UNUSED */
+      //void splitTracks();
 
       /** Check whether tracks are overlapping;
        * for overlapping tracks try to merge them or make clear separation
+       * UNUSED
        */
-      void checkOverlapping();
+      //void checkOverlapping();
 
       /**
        * @brief Function to merge two track candidates
        * All hits of track 2 are assigned to track 1 and the mean of the r and theta values of the two tracks are assigned to track 1
        * Track 2 is deleted.
+       * UNUSED
        */
-      bool earlyCandidateMerge(std::pair<std::vector<TrackHit*>, std::pair<double, double> >& track, std::set<TrackHit*>& hits_set, bool fitTracksEarly);
+      //bool earlyCandidateMerge(std::pair<std::vector<TrackHit*>, std::pair<double, double> >& track, std::set<TrackHit*>& hits_set, bool fitTracksEarly);
 
       /**
        * @brief Function to merge two track candidates
        * All hits of track 2 are assigned to track 1 and the mean of the r and theta values of the two tracks are assigned to track 1
        * Track 2 is deleted.
+       * UNUSED
        */
-      void mergeTracks(TrackCandidate* cand1, const std::pair<std::vector<TrackHit*>, std::pair<double, double> >& track, std::set<TrackHit*>& hits_set);
-
-      /**
-       * @brief Function to merge two track candidates
-       * All hits of track 2 are assigned to track 1 and the mean of the r and theta values of the two tracks are assigned to track 1
-       * Track 2 is deleted.
-       */
-      void mergeTracks(TrackCandidate* cand1, TrackCandidate* cand2, bool remove_hits = false);
+      //void mergeTracks(TrackCandidate* cand1, const std::pair<std::vector<TrackHit*>, std::pair<double, double> >& track, std::set<TrackHit*>& hits_set);
 
       /**
        * Function which adds stereo tracklets to the track
        * Uses simple combinations of tracks
        * also theta angle determination implemented; for each tracklet which possibly can belong to the track theta angle are
        * calculated, and then voting for better theta angle done
+       * UNUSED
        */
-      void addStereoTracklesToTrack();
+      //void addStereoTracklesToTrack();
 
       /** Set stereowire z position
        * first, estimate z-position of wires, fine ajustment of z position using info about drift time
+       * UNUSED
        */
-      double fitStereoTrackletsToTrack(TrackCandidate*, TrackCandidate*);
+      //double fitStereoTrackletsToTrack(TrackCandidate*, TrackCandidate*);
 
-      /** Extend tracklet using conformal transformation with respect to given point */
-      void extendTracklet(TrackCandidate* tracklet, std::vector<TrackHit*>& m_AxialHitList);
+      /** Extend tracklet using conformal transformation with respect to given point
+       * UNUSED */
+      //void extendTracklet(TrackCandidate* tracklet, std::vector<TrackHit*>& m_AxialHitList);
 
 
     private:
 
+
+      typedef std::pair<TrackCandidate*, double> BestMergePartner;
+
+      /**
+       * @brief Function to merge two track candidates
+       */
+      void mergeTracks(TrackCandidate* cand1, TrackCandidate* cand2, bool remove_hits = false);
+
+      /**
+       * Marks hits away from the trajectory as bad and refit. This method is used for calculating the chi2 of the tracks to be merged.
+       * @param factor gives a number how far the hit is allowed to be.
+       * @return the chi2 of the new merged tracks. The input parameters like track_par and ref_point are reassigned.
+       */
+      double removeStrangeHits(double factor, std::vector<TrackHit*>& trackHits, std::pair<double, double>& track_par, std::pair<double , double>& ref_point);
+
+      /** Try to merge the two tracks
+       * For this, build a common hit list and do a fast fit.
+       * Then, throw away hits with a very high distance and fit again. Repeat this process three times.
+       * As a result. the reduced chi2 is given.
+       * The bad hits are marked but none of them is deleted!
+       * This method does not do the actual merging. */
+      double doTracksFitTogether(TrackCandidate* cand1, TrackCandidate* cand2, bool remove_hits = true);
+
+      /**
+       * Searches for the best candidate to merge this track to.
+       * @param trackCandidateToBeMerged
+       * @param probabilityToBeMerged output with the best probability.
+       * @return a pointer to the best fit candidate.
+       */
+      BestMergePartner calculateBestTrackToMerge(TrackCandidate* trackCandidateToBeMerged);
+
       std::list<TrackCandidate*>& m_trackList; /**< List of track candidates. Mainly used for memory management! */
-      std::list<TrackCandidate*>& __attribute__((unused)) m_trackletList; /**< List of tracklets. */
-      std::list<TrackCandidate*>& m_stereoTrackletList; /**< List of tracklets. */
       TrackFitter* m_cdcLegendreTrackFitter; /**< Track fitter */
-      FastHough* m_cdcLegendreFastHough;  /**< Fast Hough finder */
-      TrackProcessor* m_cdcLegendreTrackProcessor; /**< Track creator */
 
+      const double m_minimum_probability_to_be_merged = 0.8;
 
+      std::list<TrackCandidate*>& __attribute__((unused)) m_trackletList; /**< List of tracklets. */
+      std::list<TrackCandidate*>& __attribute__((unused)) m_stereoTrackletList; /**< List of tracklets. */
+      TrackProcessor* __attribute__((unused)) m_cdcLegendreTrackProcessor; /**< Track creator */
+      FastHough* __attribute__((unused)) m_cdcLegendreFastHough;  /**< Fast Hough finder */
     };
   }
 }

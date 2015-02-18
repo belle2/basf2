bf3101d075b98d12a586a7dabc8b41ad03bfd52f
Benjamin Oberhof <benjamin.oberhof@pi.infn.it>
2015-02-18 17:24:12 +0000

    bug fixes
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15721 b38c6c8f-bee8-465d-8448-9abe6915d496

--- ecl/validation/ECLPion.C
+++ ecl/validation/ECLPion.C
@@ -4,30 +4,23 @@ void ECLPion()
   TFile* inputFile = TFile::Open("../ECLPionOutput.root");
   TTree* tree = (TTree*) inputFile->Get("m_tree");
 
-  //  TH1F* hShowers = new TH1F("hShowers", "Reconstructed Shower Energy for 1GeV Muon, the typical energy is 0.2 GeV", 100, 0., 1.2);
-  TH1F* hClusterE = new TH1F("hClusterE", "ECL shower energy, charged pions", 100, 0., 1.2);
-  hClusterE->GetXaxis()->SetTitle("Shower Energy (GeV)");
-  hClusterE->GetListOfFunctions()->Add(new TNamed("Description",
-    "Reconstructed shower energy for single 1GeV charged pion")); 
-  hClusterE->GetListOfFunctions()->Add(new TNamed("Check",
-    "Typical energy should be 0.2 GeV"));
-  m_tree->Draw("eclShowerEnergy>>hClusterE","eclShowerEnergy>0");
-  //hShowers->Write();
+  TH1F* hClusterE = new TH1F("PiClusterE", "Cluster energy, 1 GeV charged pion", 100, 0., 1.2);
+  hClusterE->GetXaxis()->SetTitle("Cluster energy (GeV)");
+  hClusterE->GetListOfFunctions()->Add(new TNamed("Description","Reconstructed cluster energy for single 1GeV charged pion")); 
+  hClusterE->GetListOfFunctions()->Add(new TNamed("Check","Typical energy should be 0.2 GeV"));
+  hClusterE->GetListOfFunctions()->Add(new TNamed("Contact","ecl2ml@bpost.kek.jp")); 
+  //hClusterE->GetListOfFunctions()->Add(new TNamed("Check","Typical energy should be 0.2 GeV"));
+  m_tree->Draw("eclClusterEnergy>>hClusterE","eclShowerEnergy>0");
 
-  //  TH1F* hGammas = new TH1F("hGammas", "Fake Reconstructed Gamma from  1000 muons should be less than 5%", 10,0,10);
-  TH1F* hClusterMultip = new TH1F("hClusterMultip", "Cluster Multiplicity Pions", 10,0,10);
-  hClusterMultip->GetXaxis()->SetTitle("ECL cluster multiplicity charged pion");
+  TH1F* hClusterMultip = new TH1F("PiClusterMultip", "Cluster multiplicity, 1GeV charged pion", 10,0,10);
+  hClusterMultip->GetXaxis()->SetTitle("ECL cluster multiplicity 1 GeV charged pion");
   hClusterMultip->GetListOfFunctions()->Add(new TNamed("Description",
     "ECL cluster multiplicity for 1 GeV charged pion")); 
   hClusterMultip->GetListOfFunctions()->Add(new TNamed("Check",
     "Cluster multiplicity should be low"));
   m_tree->Draw("eclClusterMultip>>hClusterMultip","eclClusterMultip>0");
-  //hGammas->Write();
 
-  //TH1F* hE9oE25_500 = new TH1F("hE9oE25_500","E9/E25", 100, 0., 1.05);
-  //hE9oE25_500->GetListOfFunctions()->Add(new TNamed("Description", "Energy deposited in 3x3 matrix around most energetic deposit over energy deposited in 5x5 matrix around most energetic deposit 500 MeV/c single photons"));
-
-  // open the output file for the validation histograms
+  // write output file for the validation histograms
   TFile* output = TFile::Open("ECLPion.root", "recreate");
   hClusterE->Write();
   hClusterMultip->Write();

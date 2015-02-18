c2bf5bf43031fd97cc37945c5709dbba5db26ee5
Jeter Hall <jeter.hall@pnnl.gov>
2015-02-18 20:49:24 +0000

    Added argparse arguments to the example steering file.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15725 b38c6c8f-bee8-465d-8448-9abe6915d496

--- topcaf/example/topcaf_itop_makeped.py
+++ topcaf/example/topcaf_itop_makeped.py
@@ -4,35 +4,77 @@
 from basf2 import *
 import sys
 
+import argparse
+
+parser = \
+    argparse.ArgumentParser(description='Calculate the pedestal calibration. Typically, a dedicated pedestal run that includes data for all channels is used as the input to create this calibration.'
+                            ,
+                            usage='basf2 %(prog)s -n <number of events> --arg [option1] --arg [option2]'
+                            )
+
+parser.add_argument('--inputRun',
+                    metavar='InputRun (i.e. file name = InputRun.dat)',
+                    required=True,
+                    help='the root name for the input data files.  myInputRun.dat would have a root name of myInputRun.  This parameter is REQUIRED.'
+                    )
+
+parser.add_argument('--inputDir', metavar='InputDirectory (path)',
+                    required=True,
+                    help='the path to the data files (IRS and CAMAC) used for the analysis. This parameter is REQUIRED.'
+                    )
+
+parser.add_argument('--Output', metavar='Output File (path/filename)',
+                    help='the output file name.  A default based on the input will be created if this argument is not supplied'
+                    )
+
+parser.add_argument('--Conditions', action='store_true',
+                    help='Use the conditions service to store this calibration.'
+                    )
+
+parser.add_argument('--IOVi', metavar='Run_initial', default='NULL',
+                    help='The initial run in the conditions service interval of validity.'
+                    )
+
+parser.add_argument('--IOVf', metavar='Run_final', default='NULL',
+                    help='The final run in the conditions service interval of validity.'
+                    )
+
+args = parser.parse_args()
+
+print 'data file     = ' + args.inputDir + args.inputRun + '.dat'
+
+if args.Conditions:
+    print 'Using conditions service with IOVi = ' + args.IOVi + ' and IOVf = ' \
+        + args.IOVf
+    Conditions = 1
+else:
+    print 'Not using conditions service.'
+    Conditions = 0
+
+WriteFile = 1
+if args.Output:
+    OutputFile = args.Output
+else:
+    OutputFile = args.inputRun + '_PedestalCalibration.root'
+print 'Writing output calibration file to ' + OutputFile
+
 eventinfosetter = register_module('EventInfoSetter')
-# eventinfosetter.param('expList', [900000001])
-# eventinfosetter.param('evtNumList', [100000])
 eventinfoprinter = register_module('EventInfoPrinter')
 
 # register topcaf modules
 
-if len(sys.argv) == 3:
-    InitialRun = 'NULL'
-    FinalRun = 'NULL'
-elif len(sys.argv) == 4:
-    FinalRun = 'NULL'
-else:
-    print 'Usage: basf2 topcaf_itop_makeped.py -n <number of events> --arg <Path to Files> <Input Pedestal Filename> <Initial run for interval of validity> <Final run for interval of validity>'
-    print '         NULL for IOV if not known/desired (IOV optional)'
-    sys.exit()
-
 itopeventconverter = register_module('iTopRawConverter')
-itopeventconverter.param('InputFileName', str(sys.argv[2]))
-itopeventconverter.param('InputDirectory', str(sys.argv[1]))
+itopeventconverter.param('InputFileName', args.inputRun + '.dat')
+itopeventconverter.param('InputDirectory', args.inputDir)
 
 pedmodule = register_module('Pedestal')
 pedestalDict = {
-    'OutputFileName': sys.argv[2] + '_pedestals.root',
+    'OutputFileName': OutputFile,
     'Mode': 0,
-    'WriteFile': 1,
-    'Conditions': 0,
-    'IOV_initialRun': InitialRun,
-    'IOV_finalRun': FinalRun,
+    'WriteFile': WriteFile,
+    'Conditions': Conditions,
+    'IOV_initialRun': args.IOVi,
+    'IOV_finalRun': args.IOVf,
     }
 pedmodule.param(pedestalDict)
 

4b015c75c3af94697aa6155fc74c1dbc9ce8705a
Jakob Lettenbichler <jkl@jodoschka.com>
2015-02-20 12:31:00 +0000

    adding missing linkdef-file for B2Vector3
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15743 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/vectorTools/include/linkdef.h
@@ -0,0 +1,15 @@
+#ifdef __CINT__
+#include <framework/gearbox/Const.h>
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+// //
+// //
+// // // #pragma link C++ class Belle2::B2Vector3+;
+#pragma link C++ class Belle2::B2Vector3<float>+;
+// // #pragma link C++ class Belle2::B2Vector3<double>+;
+// //
+// //
+#endif

c8b164d6e204454011ef84ef54dc51832e7788a8
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2015-02-20 03:32:07 +0000

    Modify run_start_assembly.sh to use RecvStream2.sh
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15737 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/copper/daq_scripts/run_start_assembly.sh
+++ daq/copper/daq_scripts/run_start_assembly.sh
@@ -2,9 +2,13 @@
 #
 # For KLM, CDC, ECL COPPERs slot A,B
 #
-/usr/bin/xterm -fn 7x14 -geometry 102x10+0+242 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh ${1} 0 ${2} 0 0; sleep 3000000;" &
+/usr/bin/xterm -fn 7x14 -geometry 40x7+500+542 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/start_copper.sh ${1} 5026 ${2} 0 33000; sleep 3000000;" &
+
+# Note : argv[3] is a port # speciefd in ~b2daq/eb/eb0.sh
+/usr/bin/xterm -fn 7x14 -geometry 40x10+400+442 -e "${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/RecvStream2.sh ${1} 0 34000 33000; sleep 3000000;" &
 
 #
 # basf2 on ROPC
 #
-${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/RecvStream1.sh 0 5101 10
+${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/RecvStream1.sh 0 5101 10 > ~b2daq/yamadas/cprtest14May/${3}/cpr${4}_slot${2}_basf2_ropc1.txt
+

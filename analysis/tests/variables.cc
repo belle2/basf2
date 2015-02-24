9ad2fc6d4e03df195373db54f5fd9d2b1e3ea4ff
Thomas Keck <t.keck@online.de>
2015-02-24 10:19:50 +0000

    Connectected gearbox in variables tests, and fixed kinematic variable tests in CMS
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15790 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/tests/variables.cc
+++ analysis/tests/variables.cc
@@ -2,6 +2,7 @@
 #include <analysis/dataobjects/Particle.h>
 #include <framework/utilities/TestHelpers.h>
 #include <framework/logging/Logger.h>
+#include <framework/gearbox/Gearbox.h>
 
 #include <gtest/gtest.h>
 
@@ -14,6 +15,14 @@ namespace {
   /** test kinematic Variable. */
   TEST(KinematicVariableTest, Variable)
   {
+
+    // Connect gearbox for CMS variables
+
+    Gearbox& gearbox = Gearbox::getInstance();
+    gearbox.setBackends({std::string("file:")});
+    gearbox.close();
+    gearbox.open("geometry/Belle2.xml", false);
+
     {
       Particle p({ 0.1 , -0.4, 0.8, 1.0 }, 11);
       EXPECT_FLOAT_EQ(0.9, particleP(&p));
@@ -25,14 +34,15 @@ namespace {
       EXPECT_FLOAT_EQ(0.8 / 0.9, particleCosTheta(&p));
       EXPECT_FLOAT_EQ(-1.325817664, particlePhi(&p));
 
-      EXPECT_FLOAT_EQ(0.0, particleP_CMS(&p));
-      EXPECT_FLOAT_EQ(0.0, particleE_CMS(&p));
-      EXPECT_FLOAT_EQ(0.0, particlePx_CMS(&p));
-      EXPECT_FLOAT_EQ(0.0, particlePy_CMS(&p));
-      EXPECT_FLOAT_EQ(0.0, particlePz_CMS(&p));
-      EXPECT_FLOAT_EQ(0.0, particlePt_CMS(&p));
-      EXPECT_FLOAT_EQ(1.0, particleCosTheta_CMS(&p));
-      EXPECT_FLOAT_EQ(0.0, particlePhi_CMS(&p));
+      EXPECT_FLOAT_EQ(0.68176979, particleP_CMS(&p));
+      EXPECT_FLOAT_EQ(0.80920333, particleE_CMS(&p));
+      // FIXME Why is Px different in CMS?!
+      EXPECT_FLOAT_EQ(0.058562335, particlePx_CMS(&p));
+      EXPECT_FLOAT_EQ(-0.40000001, particlePy_CMS(&p));
+      EXPECT_FLOAT_EQ(0.54898131, particlePz_CMS(&p));
+      EXPECT_FLOAT_EQ(0.40426421, particlePt_CMS(&p));
+      EXPECT_FLOAT_EQ(0.80522972, particleCosTheta_CMS(&p));
+      EXPECT_FLOAT_EQ(-1.4254233, particlePhi_CMS(&p));
     }
 
     {

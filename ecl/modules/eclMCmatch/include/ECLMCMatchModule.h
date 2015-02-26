df94e634542d3df0d67cf22e5398018a42b976af
Benjamin Oberhof <benjamin.oberhof@pi.infn.it>
2015-02-26 19:17:50 +0000

    test module with better pile-up rejection
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15862 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ ecl/modules/eclMCmatch/include/ECLMCMatchModule.h
@@ -0,0 +1,80 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Poyuan Chen                                              *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+
+#ifndef ECLMCMATCHMODULE_H_
+#define ECLMCMATCHMODULE_H_
+
+//basf2 framework headers
+#include <framework/core/Module.h>
+#include <ecl/geometry/ECLGeometryPar.h>
+#include <ecl/dataobjects/ECLSimHit.h>
+
+//C++/C standard lib elements.
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+
+//ROOT
+#include <TRandom3.h>
+
+
+namespace Belle2 {
+  namespace ECL {
+
+    /** Class to represent the hit of one cell */
+    class ECLMCMatchModule : public Module {
+
+    public:
+      /** Constructor.*/
+      ECLMCMatchModule();
+
+      /** Destructor.*/
+      virtual ~ECLMCMatchModule();
+
+      /** Initialize variables, print info, and start CPU clock. */
+      virtual void initialize();
+
+      /** Nothing so far.*/
+      virtual void beginRun();
+
+      /** Actual digitization of all hits in the ECL.
+       *
+       *  The digitized hits are written into the DataStore.
+       */
+      virtual void event();
+
+      /** Nothing so far. */
+      virtual void endRun();
+
+      /** Stopping of CPU clock.*/
+      virtual void terminate();
+
+      /** define a map for Primary Track*/
+      typedef std::map< int, int>  PrimaryTrackMap;
+      /** define a multimap for hits in showers */
+      typedef std::multimap< int, int> MultiMap;
+
+
+    private:
+
+      /** CPU time     */
+      double m_timeCPU;
+      /** Run number   */
+      int    m_nRun;
+      /** Event number */
+      int    m_nEvent;
+
+    };
+  } //ECL
+} // end of Belle2 namespace
+
+#endif // ECLDIGI_H

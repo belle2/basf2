0e0896b5499220e6def986a343e5a97a722bb0c1
Igal Jaegle <igjaegle@gmail.com>
2015-02-27 01:31:59 +0000

    add overlapping check steering file
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15866 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ beast/examples/CheckGeometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+######################################################
+# This steering file creates the Belle II detector
+# geometry and checks for overlaps
+######################################################
+
+from basf2 import *
+
+evtmeta = register_module('EventInfoSetter')
+
+# Geometry parameter loader
+gearbox = register_module('Gearbox')
+gearbox.param('fileName', '/geometry/Beast2_phase2.xml')
+
+# Geometry builder
+geometry = register_module('Geometry')
+
+# Overlap checker
+overlapchecker = register_module('OverlapChecker')
+
+# Create main path
+main = create_path()
+
+# Add modules to main path
+main.add_module(evtmeta)
+main.add_module(gearbox)
+main.add_module(geometry)
+main.add_module(overlapchecker)
+
+# Process one event
+process(main)

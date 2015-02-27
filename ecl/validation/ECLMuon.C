d294a395b05403e021d393a087e9241bace7d214
Benjamin Oberhof <benjamin.oberhof@pi.infn.it>
2015-02-27 12:16:48 +0000

    various fixes
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@15873 b38c6c8f-bee8-465d-8448-9abe6915d496

--- ecl/validation/ECLMuon.C
+++ ecl/validation/ECLMuon.C
@@ -8,16 +8,23 @@ void ECLMuon()
   TFile* output = TFile::Open("ECLMuon.root", "recreate");
 
   // an example of a 1D histogram
-  TH1F* hShowers = new TH1F("hShowers", "Reconstructed Shower Energy for 1GeV Muon, the typical energy is 0.2 GeV", 100, 0., 1.2);
-  hShowers->GetXaxis()->SetTitle("Shower Energy (GeV)");
-  tree->Draw("ECLShowers.m_Energy>>hShowers");
-  hShowers->Write();
+  TH1F* hMuonsE = new TH1F("hMuonsE", "Reconstructed Shower Energy for 1GeV Muon, the typical energy is 0.2 GeV", 100, 0., 1.2);
+  hMuonsE->GetXaxis()->SetTitle("Shower Energy (GeV)");
+  hMuonsE->GetListOfFunctions()->Add(new TNamed("Description","Energy release in the ECL for 1 GeV muons")); 
+  hMuonsE->GetListOfFunctions()->Add(new TNamed("Check","Should be peaked at 200 MeV"));
+  hMuonsE->GetListOfFunctions()->Add(new TNamed("Contact","ecl2ml@bpost.kek.jp")); 
+  tree->Draw("ECLShowers.m_Energy>>hMuonsE");
+  hMuonsE->Write();
 
   // an example of a 1D histogram
-  TH1F* hGammas = new TH1F("hGammas", "Fake Reconstructed Gamma from  1000 muons should be less than 5%", 10,0,10);
-  hGammas->GetXaxis()->SetTitle("Shower Id");
-  tree->Draw("ECLGammas.m_ShowerId>>hGammas");
-  hGammas->Write();
+  TH1F* hMuonsFake = new TH1F("hMuonsFake", "Fake Reconstructed camma for 1000 muons should be less than 5%", 10,0,10);
+  hMuonsFake->SetMaximum(6);
+  hMuonsFake->GetXaxis()->SetTitle("Shower Id");
+  hMuonsFake->GetListOfFunctions()->Add(new TNamed("Description","Fake reconstructed gamma from 1000 muons")); 
+  hMuonsFake->GetListOfFunctions()->Add(new TNamed("Check","Should be low"));
+  hMuonsFake->GetListOfFunctions()->Add(new TNamed("Contact","ecl2ml@bpost.kek.jp")); 
+  tree->Draw("ECLGammas.m_ShowerId>>hMuonsFake");
+  hMuonsFake->Write();
 
 
 
--- ecl/validation/ECLPi0.C
+++ ecl/validation/ECLPi0.C
@@ -10,22 +10,26 @@ void ECLPi0()
   // an example of a 1D histogram
   TH1F* hPi0s = new TH1F("hPi0s", "ECL Reconstructed Pi0 Energy for 1 GeV/c Pi0", 100, 0.2, 1.2);
   hPi0s->GetXaxis()->SetTitle("Pi0 Energy (GeV)");
+  hPi0s->GetListOfFunctions()->Add(new TNamed("Description","Reconstructed energy for 1 GeV Pi0s")); 
+  hPi0s->GetListOfFunctions()->Add(new TNamed("Check","Should not be biased"));
+  hPi0s->GetListOfFunctions()->Add(new TNamed("Contact","ecl2ml@bpost.kek.jp"));
   tree->Draw("ECLPi0s.m_Energy>>hPi0s");
   hPi0s->Write();
 
   // an example of a 1D histogram
   TH1F* hPi0mass = new TH1F("hPi0mass", "ECL combined Pi0 Mass for 1 GeV/c Pi0", 100, 0.08, 0.18);
   hPi0mass->GetXaxis()->SetTitle("Pi0 combined mass (GeV/c^{2})");
+  hPi0mass->GetListOfFunctions()->Add(new TNamed("Description","Reconstructed mass for 1 GeV Pi0s")); 
+  hPi0mass->GetListOfFunctions()->Add(new TNamed("Check","Should not be biased"));
+  hPi0mass->GetListOfFunctions()->Add(new TNamed("Contact","ecl2ml@bpost.kek.jp"));
   tree->Draw("ECLPi0s.m_Mass>>hPi0mass");
   hPi0mass->Write();
 
   // an example of a 1D histogram
-  TH1F* hPi0massFit = new TH1F("hPi0massFit", "ECL constrainted  Pi0 Mass for 1 GeV/c Pi0", 100, 0.08, 0.18);
-  hPi0massFit->GetXaxis()->SetTitle("Pi0 constrainted  mass (GeV/c^{2})");
-  tree->Draw("ECLPi0s.m_MassFit>>hPi0massFit");
-  hPi0massFit->Write();
-
-
+  //TH1F* hPi0massFit = new TH1F("hPi0massFit", "ECL constrainted  Pi0 Mass for 1 GeV/c Pi0", 100, 0.08, 0.18);
+  //hPi0massFit->GetXaxis()->SetTitle("Pi0 constrainted  mass (GeV/c^{2})");
+  //tree->Draw("ECLPi0s.m_MassFit>>hPi0massFit");
+  //hPi0massFit->Write();
 
   delete output;
 }

63fcb86a8381b90c62169c7ca5d027c92cdca09c
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2015-03-07 06:38:33 +0000

    Added config files
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@16184 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/slc/database/tools/dbprovider.cc
@@ -0,0 +1,58 @@
+#include <daq/slc/database/PostgreSQLInterface.h>
+#include <daq/slc/database/DBObjectLoader.h>
+
+#include <daq/slc/system/Daemon.h>
+#include <daq/slc/system/TCPServerSocket.h>
+#include <daq/slc/system/TCPSocketReader.h>
+#include <daq/slc/system/TCPSocketWriter.h>
+#include <daq/slc/system/LogFile.h>
+
+#include <daq/slc/base/ConfigFile.h>
+#include <daq/slc/base/StringUtil.h>
+
+#include <cstdlib>
+
+using namespace Belle2;
+
+int main(int argc, char** argv)
+{
+  if (Daemon::start(argv[1], argc, argv, 1, "<config>")) {
+    ConfigFile config("slowcontrol", argv[1]);
+    const int port = config.getInt("provider.port");
+    const std::string dbtable = config.get("dbtable");
+    TCPServerSocket server("0.0.0.0", port);
+    try {
+      server.open();
+    } catch (const IOException& e) {
+      LogFile::error("failed to open server socket %d", port);
+      exit(1);
+    }
+    PostgreSQLInterface db(config.get("database.host"),
+                           config.get("database.dbname"),
+                           config.get("database.user"),
+                           config.get("database.password"),
+                           config.getInt("database.port"));
+    while (true) {
+      TCPSocket socket;
+      try {
+        socket = server.accept();
+        TCPSocketReader reader(socket);
+        const std::string path = reader.readString();
+        StringList s = StringUtil::split(path, '/');
+        const std::string table = (s.size() > 0) ? s[0] : dbtable;
+        const std::string config = (s.size() > 0) ? s[1] : path;
+        DBObject obj(DBObjectLoader::load(db, dbtable, config));
+        LogFile::debug("loaded %s from %s", obj.getName().c_str(), table.c_str());
+        obj.print();
+        TCPSocketWriter writer(socket);
+        writer.writeObject(obj);
+      } catch (const IOException& e) {
+        LogFile::error(e.what());
+      }
+      socket.close();
+      db.close();
+    }
+  }
+  return 0;
+}
+

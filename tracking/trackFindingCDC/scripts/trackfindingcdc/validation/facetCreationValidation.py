1dce78b548adefbf3624565357fa27fe63df21eb
Oliver Frost <oliver.frost@desy.de>
2015-03-12 19:01:39 +0000

    TrackFindingCDC: Updated facet creation validation with new classification refiner.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@16311 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/scripts/trackfindingcdc/validation/facetCreationValidation.py
+++ tracking/trackFindingCDC/scripts/trackfindingcdc/validation/facetCreationValidation.py
@@ -74,7 +74,7 @@ class FacetCreationValidationModule(harvesting.HarvestingModule):
 
     """Module to collect information about the facet creation cuts and compose validation plots on terminate."""
 
-    cos_cut_value = math.cos(math.pi / 180.0 * 9)
+    cos_cut_value = 0.980
 
     def __init__(self, output_file_name):
         super(FacetCreationValidationModule, self).__init__(foreach="CDCRecoFacetVector",
@@ -117,13 +117,57 @@ class FacetCreationValidationModule(harvesting.HarvestingModule):
         # Fit the tangent lines
         self.fit(facet)
 
+        start_rl_wirehit = facet.getStartRLWireHit()
+        start_drift_length_var = start_rl_wirehit.getRefDriftLengthVariance()
+        start_drift_length_std = math.sqrt(start_drift_length_var)
+
+        middle_rl_wirehit = facet.getMiddleRLWireHit()
+        middle_drift_length_var = middle_rl_wirehit.getRefDriftLengthVariance()
+        middle_drift_length_std = math.sqrt(middle_drift_length_var)
+
+        end_rl_wirehit = facet.getEndRLWireHit()
+        end_drift_length_var = end_rl_wirehit.getRefDriftLengthVariance()
+        end_drift_length_std = math.sqrt(end_drift_length_var)
+
         start_to_middle_line = facet.getStartToMiddleLine()
         start_to_end_line = facet.getStartToEndLine()
         middle_to_end_line = facet.getMiddleToEndLine()
 
-        start_cos = start_to_middle_line.tangential().cosWith(start_to_end_line.tangential())
-        middle_cos = start_to_middle_line.tangential().cosWith(middle_to_end_line.tangential())
-        end_cos = start_to_end_line.tangential().cosWith(middle_to_end_line.tangential())
+        start_to_middle_tangential_vector = start_to_middle_line.tangential()
+        start_to_end_tangential_vector = start_to_end_line.tangential()
+        middle_to_end_tangential_vector = middle_to_end_line.tangential()
+
+        start_to_middle_length = start_to_middle_tangential_vector.norm()
+        start_to_end_length = start_to_end_tangential_vector.norm()
+        middle_to_end_length = middle_to_end_tangential_vector.norm()
+
+        start_cos = start_to_middle_tangential_vector.cosWith(start_to_end_tangential_vector)
+        middle_cos = start_to_middle_tangential_vector.cosWith(middle_to_end_tangential_vector)
+        end_cos = start_to_end_tangential_vector.cosWith(middle_to_end_tangential_vector)
+
+        start_phi = math.acos(start_cos)
+        middle_phi = math.acos(middle_cos)
+        end_phi = math.acos(end_cos)
+
+        # Unexact sigmas up to a common factor equivalent to the simple drift length std
+        start_phi_pseudo_sigma = math.hypot(1.0 / start_to_middle_length, 1.0 / start_to_end_length)
+        middle_phi_pseudo_sigma = math.hypot(1.0 / start_to_middle_length, 1.0 / middle_to_end_length)
+        end_phi_pseudo_sigma = math.hypot(1.0 / start_to_end_length, 1.0 / middle_to_end_length)
+
+        start_phi_sigma = math.hypot(start_drift_length_std / start_to_middle_length, start_drift_length_std / start_to_end_length)
+        middle_phi_sigma = math.hypot(middle_drift_length_std / start_to_middle_length, middle_drift_length_std / middle_to_end_length)
+        end_phi_sigma = math.hypot(end_drift_length_std / start_to_end_length, end_drift_length_std / middle_to_end_length)
+
+        start_phi_pseudo_pull = start_phi / start_phi_pseudo_sigma
+        middle_phi_pseudo_pull = start_phi / middle_phi_pseudo_sigma
+        end_phi_pseudo_pull = start_phi / end_phi_pseudo_sigma
+
+        start_phi_pull = start_phi / start_phi_sigma
+        middle_phi_pull = start_phi / middle_phi_sigma
+        end_phi_pull = start_phi / end_phi_sigma
+
+        select_pull = start_phi_pull < 20 and middle_phi_pull < 12.5 and end_phi_pull < 20
+        select_pseudo_pull = start_phi_pseudo_pull < 0.3 and middle_phi_pseudo_pull < 0.2 and end_phi_pseudo_pull < 0.3
 
         crops = dict(
             start_cos=start_cos,
@@ -133,6 +177,17 @@ class FacetCreationValidationModule(harvesting.HarvestingModule):
             min_cos=min(start_cos, middle_cos, end_cos),
             no_middle_max_cos=max(start_cos, end_cos),
             no_middle_min_cos=min(start_cos, end_cos),
+            start_phi=start_phi,
+            midlde_phi=middle_phi,
+            end_phi=end_phi,
+            start_phi_pseudo_pull=start_phi_pseudo_pull,
+            middle_phi_pseudo_pull=middle_phi_pseudo_pull,
+            end_phi_pseudo_pull=end_phi_pseudo_pull,
+            start_phi_pull=start_phi_pull,
+            middle_phi_pull=middle_phi_pull,
+            end_phi_pull=end_phi_pull,
+            select_pull=select_pull,
+            select_pseudo_pull=select_pseudo_pull,
         )
         crops.update(fitless_crops)
 
@@ -188,7 +243,13 @@ class FacetCreationValidationModule(harvesting.HarvestingModule):
         return True
 
     def select(self, crops):
-        return crops["no_middle_min_cos"] > self.cos_cut_value and crops["select_fitless_hard"]
+        return (
+            crops["select_fitless_hard"] and
+            crops["no_middle_max_cos"] > 0.994 and
+            crops["no_middle_min_cos"] > 0.980 and
+            crops["middle_cos"] > 0.980
+        )
+        # return crops["no_middle_min_cos"] > self.cos_cut_value and crops["select_fitless_hard"]
 
     def real_select(self, facet):
         real_weigth = self.real_facet_filter.isGoodFacet(facet)
@@ -202,10 +263,68 @@ class FacetCreationValidationModule(harvesting.HarvestingModule):
     save_histograms = refiners.save_histograms(outlier_z_score=5.0, allow_discrete=True, folder_name="histograms")
     save_tree = refiners.save_tree(folder_name="tree")
 
-    # Investigate the preselection
-    save_selection_variables_histograms = refiners.save_histograms(
-        select=[
-            "mc_decision",
+    # Analyse the python selection quality on Monte Carlo
+    save_select_quality = refiners.save_classification_analysis(
+        folder_name="quality_select",
+        truth_name="mc_decision",
+        estimate_name="select",
+    )
+
+    # Analyse the python pseudo pull selection quality on Monte Carlo
+    save_select_cut_quality = refiners.save_classification_analysis(
+        folder_name="quality_cuts",
+        truth_name="mc_decision",
+        estimate_name=["select_pseudo_pull", "select_pull", ]
+    )
+
+    # Compare the python selection with the real c++ filter
+    save_compare_select = refiners.save_classification_analysis(
+        folder_name="compare_real",
+        truth_name="real_select",
+        estimate_name="select"
+    )
+
+    # Fitless preselection
+    # Validate that the c++ implementation is in sync with the python one
+    save_compare_select_fitless = refiners.save_classification_analysis(
+        folder_name="compare_real_fitless",
+        truth_name="real_select_fitless",
+        estimate_name=["select_fitless", "select_fitless_hard"]
+    )
+
+    # Analyse the python fitless selection quality on Monte Carlo
+    save_select_fitless_quality = refiners.save_classification_analysis(
+        folder_name="quality_fitless",
+        truth_name="mc_decision",
+        estimate_name=["select_fitless", "select_fitless_hard", ]
+    )
+
+    # Classification curves #
+    #########################
+    save_select_phi_pull = refiners.save_classification_analysis(
+        groupby=[None, "abs_shape"],
+        folder_name="quality_phi_pull/{groupby_addition}",
+        truth_name="mc_decision",
+        estimate_name=["start_phi_pull", "middle_phi_pull", "end_phi_pull", ],
+        cut_direction=1,
+        cut=20,
+        upper_bound=30,
+    )
+
+    save_select_phi_pseudo_pull = refiners.save_classification_analysis(
+        groupby=[None, "abs_shape"],
+        folder_name="quality_phi_pseudo_pull/{groupby_addition}",
+        truth_name="mc_decision",
+        estimate_name=["start_phi_pseudo_pull", "middle_phi_pseudo_pull", "end_phi_pseudo_pull", ],
+        cut_direction=1,
+        cut=0.3,
+        upper_bound=0.5,
+    )
+
+    save_selection_variables_histograms = refiners.save_classification_analysis(
+        groupby=[None, "abs_shape"],
+        folder_name="quality_cos/{groupby_addition}",
+        estimate_name=[
             "start_cos",
             "middle_cos",
             "end_cos",
@@ -214,59 +333,35 @@ class FacetCreationValidationModule(harvesting.HarvestingModule):
             "no_middle_max_cos",
             "no_middle_min_cos",
         ],
-        stackby="mc_decision",
-        lower_bound=cos_cut_value,
+        truth_name="mc_decision",
+        cut_direction=-1,  # cut low values
+        cut=0.980,
+        lower_bound=0.980,
         upper_bound=1,
-        folder_name="selection_variables",
     )
 
-    save_select_quality_histograms = refiners.save_histograms(
-        select=[
-            "mc_decision",
-            "select",
-        ],
-        stackby="mc_decision",
-        allow_discrete=True,
-        folder_name="quality"
-    )
+    @refiners.context(filter_on="mc_decision", folder_name="scatter")
+    def save_scatter_cos(self, crops, tdirectory, **kwds):
+        max_versus_min_no_middle = ValidationPlot("max_versus_min_no_middle")
+        max_versus_min_no_middle.scatter(
+            crops["no_middle_max_cos"], crops["no_middle_min_cos"],
+            lower_bound=(0.99, 0.90),
+        )
+        max_versus_min_no_middle.write(tdirectory)
 
-    save_compare_select_histograms = refiners.save_histograms(
-        select=[
-            "real_select",
-            "select",
-        ],
-        stackby="real_select",
-        allow_discrete=True,
-        folder_name="compare_real"
-    )
+        max_versus_middle = ValidationPlot("max_versus_middle")
+        max_versus_middle.scatter(
+            crops["no_middle_max_cos"], crops["middle_cos"],
+            lower_bound=(0.99, 0.90),
+        )
+        max_versus_middle.write(tdirectory)
 
-    # Fitless preselection
-    # Validate that the c++ implementation is in sync with the python one
-    save_compare_fitless_select_histograms = refiners.save_histograms(
-        select=[
-            "real_select_fitless",
-            "select_fitless",
-            "select_fitless_hard",
-        ],
-        stackby="real_select_fitless",
-        allow_discrete=True,
-        folder_name="compare_real_fitless"
-    )
-
-    # Show how well this selects
-    save_fitless_select_quality_histograms = refiners.cd(
-        refiners.save_histograms(
-            select=[
-                "mc_decision",
-                "select_fitless",
-                "select_fitless_hard",
-            ],
-            stackby="mc_decision",
-            allow_discrete=True,
-            groupby=[None, "abs_shape"],
-        ),
-        folder_name="quality_fitless"
-    )
+        min_versus_middle = ValidationPlot("min_versus_middle")
+        min_versus_middle.scatter(
+            crops["no_middle_min_cos"], crops["middle_cos"],
+            lower_bound=(0.90, 0.90),
+        )
+        min_versus_middle.write(tdirectory)
 
     @refiners.context()
     def print_signal_number(self, crops, tdirectory, **kwds):

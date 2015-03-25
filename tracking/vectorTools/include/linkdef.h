0ddbf4b63c9da80bf33408fd42319c7df97a42b7
Jakob Lettenbichler <jkl@jodoschka.com>
2015-03-25 16:31:01 +0000

    removed B2Vector3<bool> and B2Vector3<int> from linkdef file
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@16612 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/vectorTools/include/linkdef.h
+++ tracking/vectorTools/include/linkdef.h
@@ -8,9 +8,9 @@
 // //
 // //
 // // // #pragma link C++ class Belle2::B2Vector3+;
-#pragma link C++ class Belle2::B2Vector3<bool>+;
-// #pragma link C++ class Belle2::B2Vector3<unsigned int>+;
-#pragma link C++ class Belle2::B2Vector3<int>+;
+// #pragma link C++ class Belle2::B2Vector3<bool>+;
+// // // // #pragma link C++ class Belle2::B2Vector3<unsigned int>+;
+// #pragma link C++ class Belle2::B2Vector3<int>+;
 #pragma link C++ class Belle2::B2Vector3<float>+;
 #pragma link C++ class Belle2::B2Vector3<double>+;
 // #pragma link C++ class Belle2::B2Vector3F+; // implicit

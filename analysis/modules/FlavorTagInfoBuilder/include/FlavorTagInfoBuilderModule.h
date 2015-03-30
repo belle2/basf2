68bb5e5f8b6285781e2bcaab53a470cb5182d6fa
Anze Zupanc <anze.zupanc@ijs.si>
2015-03-30 12:08:15 +0000

    removed extra link and duplicated include statements.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@16733 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/modules/FlavorTagInfoBuilder/include/FlavorTagInfoBuilderModule.h
+++ analysis/modules/FlavorTagInfoBuilder/include/FlavorTagInfoBuilderModule.h
@@ -13,11 +13,7 @@
 
 #include <framework/core/Module.h>
 
-#include <analysis/dataobjects/Particle.h>
-#include <analysis/dataobjects/RestOfEvent.h>
-
 #include <string>
-#include <vector>
 
 namespace Belle2 {
 

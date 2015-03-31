0d9e77b190064890b1daef96d9792d1cc3d94b18
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2015-03-31 06:02:58 +0000

    PostgreSQL is enabled by default
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@16782 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/setenv.sh
+++ daq/slc/setenv.sh
@@ -5,8 +5,7 @@ export BELLE2_DAQ_SLC=$PWD
 export PATH=$BELLE2_DAQ_SLC/bin:$PATH
 export PGSQL_LIB_PATH=/usr/pgsql-9.3/
 export LD_LIBRARY_PATH=$BELLE2_DAQ_SLC/lib:$PGSQL_LIB_PATH/lib:$LD_LIBRARY_PATH
-#export NSM2_INCDIR=$BELLE2_LOCAL_DIR/slc/data/nsm
 export NSM2_INCDIR=$BELLE2_DAQ_SLC/data/nsm
 
 ## set NOT_USE_PSQL yes if postreSQL is not available
-export NOT_USE_PSQL=yes
+#export NOT_USE_PSQL=yes

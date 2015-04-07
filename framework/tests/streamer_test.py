010d120307b16d2b9fe039be4bb06758554c4675
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2015-04-07 13:36:47 +0000

    remove unnecessary workaround (done in basf2_env)
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@16939 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/tests/streamer_test.py
+++ framework/tests/streamer_test.py
@@ -3,10 +3,6 @@
 
 from basf2 import *
 
-# avoid race conditions beetween pyroot and GUI thread
-from ROOT import PyConfig
-PyConfig.StartGuiThread = False
-
 from simulation import add_simulation
 from reconstruction import add_reconstruction
 
@@ -29,14 +25,13 @@ main = create_path()
 main.add_module(eventinfosetter)
 main.add_module(particlegun)
 
-# detecor simulation
+# detector simulation
 components = ['MagneticField',
               'BeamPipe',
-              #'PXD',
-              #'SVD',
+              # 'PXD',
+              # 'SVD',
               'CDC']
 add_simulation(main, components)
-# or add_simulation(main) to simulate all detectors
 
 add_reconstruction(main, components)
 
@@ -46,13 +41,13 @@ main.add_module(output)
 process(main)
 
 
-#load file and see if it can be read properly
+# load file and see if it can be read properly
 from ROOT import TFile
 from ROOT import TTree
 file = TFile('streamer_test.root')
 tree = file.Get('tree')
 
-#seems to be ok, most of the time
+# seems to be ok, most of the time
 tree.Project("", "MCParticles.m_pdg")
 
 # Used to create problems when genfit::AbsTrackRep didn't have schema

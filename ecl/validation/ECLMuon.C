2c1ffe7d40d2b7f80fba553ba3e727dfd4b68f9d
Benjamin Oberhof <benjamin.oberhof@pi.infn.it>
2015-04-10 07:11:15 +0000

    fixes
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17040 b38c6c8f-bee8-465d-8448-9abe6915d496

--- ecl/validation/ECLMuon.C
+++ ecl/validation/ECLMuon.C
@@ -16,31 +16,28 @@ void ECLMuon()
 {
   // open the file with simulated and reconstructed EvtGen particles
   TFile* input = TFile::Open("../ECLMuonOutput.root");
-  TTree* tree = (TTree*) input->Get("tree");
+  TTree* tree = (TTree*) input->Get("m_tree");
 
   // open the output file for the validation histograms
   TFile* output = TFile::Open("ECLMuon.root", "recreate");
 
   // an example of a 1D histogram
-  TH1F* hMuonsE = new TH1F("hMuonsE", "Reconstructed Shower Energy for 1GeV Muon, the typical energy is 0.2 GeV", 100, 0., 1.2);
-  hMuonsE->GetXaxis()->SetTitle("Shower Energy (GeV)");
+  TH1F* hMuonsE = new TH1F("hMuonsE", "Reconstructed cluster energy for 1GeV muons, the typical energy is 0.2 GeV", 100, 0., 1.2);
+  hMuonsE->GetXaxis()->SetTitle("Cluster Energy (GeV)");
   hMuonsE->GetListOfFunctions()->Add(new TNamed("Description","Energy release in the ECL for 1 GeV muons")); 
   hMuonsE->GetListOfFunctions()->Add(new TNamed("Check","Should be peaked at 200 MeV"));
   hMuonsE->GetListOfFunctions()->Add(new TNamed("Contact","ecl2ml@bpost.kek.jp")); 
-  tree->Draw("ECLShowers.m_Energy>>hMuonsE");
+  tree->Draw("eclClusterEnergy>>hMuonsE","");
   hMuonsE->Write();
 
   // an example of a 1D histogram
-  TH1F* hMuonsFake = new TH1F("hMuonsFake","Fake Reconstructed camma for 1000 muons should be less than 5%", 10,0,10);
-  hMuonsFake->SetMaximum(6);
-  hMuonsFake->GetXaxis()->SetTitle("Shower Id");
-  hMuonsFake->GetListOfFunctions()->Add(new TNamed("Description", 
-    "Fake reconstructed gamma from 1000 muons")); 
-  hMuonsFake->GetListOfFunctions()->Add(new TNamed("Check", 
-    "Should be low"));
-  hMuonsFake->GetListOfFunctions()->Add(new TNamed("Contact", 
-    "ecl2ml@bpost.kek.jp")); 
-  tree->Draw("ECLGammas.m_ShowerId>>hMuonsFake");
+  TH1F* hMuonsFake = new TH1F("hMuonsFake","Fake reconstructed gamma for 1000 muons should be less than 5%", 10,0,10);
+  // hMuonsFake->SetMaximum(6);
+  hMuonsFake->GetXaxis()->SetTitle("Gamma Idx");
+  hMuonsFake->GetListOfFunctions()->Add(new TNamed("Description", "Fake reconstructed gamma every 1000 muons")); 
+  hMuonsFake->GetListOfFunctions()->Add(new TNamed("Check", "Should be low"));
+  hMuonsFake->GetListOfFunctions()->Add(new TNamed("Contact", "ecl2ml@bpost.kek.jp")); 
+  tree->Draw("eclGammaIdx>>hMuonsFake","eclGammaMultip>0");
   hMuonsFake->Write();
 
   delete output;
--- ecl/validation/ECLMuon.py
+++ ecl/validation/ECLMuon.py
@@ -10,109 +10,51 @@
 
 import os
 from basf2 import *
+from simulation import add_simulation
+from reconstruction import add_reconstruction
 
 set_log_level(LogLevel.ERROR)
 
 # Register necessary modules
 eventinfosetter = register_module('EventInfoSetter')
-eventinfoprinter = register_module('EventInfoPrinter')
-
-# Create geometry
-# Geometry parameter loader
-gearbox = register_module('Gearbox')
-
-# Geometry builder
-geometry = register_module('Geometry')
-
-# Simulation
-g4sim = register_module('FullSim')
-
 # Set the number of events to be processed (1000 events)
 eventinfosetter.param({'evtNumList': [1000], 'runList': [1]})
 
-# import random
-# intseed = random.randint(1, 10000000)
+eventinfoprinter = register_module('EventInfoPrinter')
+
+# Fixed random seed
 set_random_seed(123456)
 
 pGun = register_module('ParticleGun')
 param_pGun = {
     'pdgCodes': [13],
     'nTracks': 1,
-    'momentumGeneration': 'uniform',
-    'momentumParams': [1., 1.],
+    'momentumGeneration': 'fixed',
+    'momentumParams': [1],
     'thetaGeneration': 'uniform',
-    'thetaParams': [50., 130.],
+    'thetaParams': [0., 180.],
     'phiGeneration': 'uniform',
     'phiParams': [0, 360],
     'vertexGeneration': 'uniform',
-    'xVertexParams': [0.0, 0.0],
-    'yVertexParams': [0.0, 0.0],
-    'zVertexParams': [0.0, 0.0],
+    'xVertexParams': [0., 0.],
+    'yVertexParams': [0., 0.],
+    'zVertexParams': [0., 0.],
     }
-
 pGun.param(param_pGun)
 
-eclDigi = register_module('ECLDigitizer')
-eclRecShower = register_module('ECLReconstructor')
-makeGamma = register_module('ECLGammaReconstructor')
-makePi0 = register_module('ECLPi0Reconstructor')
-makeMatch = register_module('ECLMCMatching')
-param_Gamma = {
-    'gammaEnergyCut': 0.02,
-    'gammaE9o25Cut': 0.75,
-    'gammaWidthCut': 6.0,
-    'gammaNhitsCut': 0,
-    }
-
-makeGamma.param(param_Gamma)
-
-cdcDigitizer = register_module('CDCDigitizer')
-param_cdcdigi = {'Fraction': 1, 'Resolution1': 0.01, 'Resolution2': 0.0}
-cdcDigitizer.param(param_cdcdigi)
-
-ext = register_module('Ext')
-
-genfit = register_module('GenFitter')
-mctrackfinder = register_module('TrackFinderMCTruth')
-param_mctrackfinder = {
-    'UseCDCHits': 1,
-    'UseSVDHits': 1,
-    'UsePXDHits': 1,
-    'Smearing': 0,
-    }
-mctrackfinder.param(param_mctrackfinder)
-
-trackfitter = register_module('GenFitter')
-trackfitter.logging.log_level = LogLevel.WARNING
-# trackfitter.param('NIterations', 3)
-
-trackfitchecker = register_module('TrackFitChecker')
-trackfitchecker.logging.log_level = LogLevel.INFO
-trackfitchecker.param('testSi', True)
-trackfitchecker.param('testCdc', False)
-trackfitchecker.param('writeToTextFile', True)
+eclanalysis = register_module('ECLDataAnalysis')
+eclanalysis.param('rootFileName', '../ECLMuonOutput.root')
+eclanalysis.param('doTracking', 1)
 
 # Create paths
 main = create_path()
 main.add_module(eventinfosetter)
-main.add_module(eventinfoprinter)
-main.add_module(gearbox)
-main.add_module(geometry)
+# main.add_module(eventinfoprinter)
 main.add_module(pGun)
-main.add_module(g4sim)
-main.add_module(cdcDigitizer)
-main.add_module(mctrackfinder)
-main.add_module(trackfitter)
-# main.add_module(trackfitchecker)
-main.add_module(ext)
-main.add_module(eclDigi)
-main.add_module(eclRecShower)
-main.add_module(makeGamma)
-main.add_module(makePi0)
-main.add_module(makeMatch)
-simpleoutput = register_module('RootOutput')
-simpleoutput.param('outputFileName', '../ECLMuonOutput.root')
-main.add_module(simpleoutput)
+add_simulation(main)
+add_reconstruction(main)
+main.add_module(eclanalysis)
+# main.add_module(simpleoutput)
 
 process(main)
 print statistics

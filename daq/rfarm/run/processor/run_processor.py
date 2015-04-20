4670b1c32eaf3d45924498e276534b231c606874
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2015-04-20 08:46:55 +0000

    Processor scripts added.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17219 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/rfarm/run/processor/run_processor.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import signal
+import subprocess
+
+global proc
+
+
+# Signal Handler
+def cleanup(num, frame):
+    os.kill(proc.pid, signal.SIGINT)
+    os.kill(proc.pid, signal.SIGINT)
+    exit()
+
+# main
+
+# Signal Handler
+signal.signal(signal.SIGINT, cleanup)
+signal.signal(signal.SIGTERM, cleanup)
+
+# Obtain input/output RingBuffer names
+argvs = sys.argv
+argc = len(argvs)
+
+inputrb = argvs[1]
+outputrb = argvs[2]
+histport = argvs[3]
+ncore = argvs[4]
+
+script = "processor.py"
+
+# run basf2
+proc = subprocess.Popen("basf2 " + script + " " + inputrb + " " + outputrb + " " + histport + " " + ncore, shell=True)
+
+# Just sleep
+proc.wait()

d13c54b9cedf71adaff8623c9953fe4d4ee25a98
Jeter Hall <jeter.hall@pnnl.gov>
2015-04-21 20:47:53 +0000

    Type definitions to support various change of basis in electronics numbering.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17302 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ topcaf/utils/iTopTypedef.h
@@ -0,0 +1,62 @@
+#ifndef iTopTypedef_H
+#define iTopTypedef_H
+
+#include <string>
+#include <utility>
+#include <map>
+#include <vector>
+
+
+
+
+//typedef unsigned int packet_word_t;
+typedef std::string TopElectronicConstructionName;
+typedef unsigned int TopPixelNumber;
+typedef unsigned int TopElectronicModuleNumber;
+
+// Pairs
+typedef std::pair<TopElectronicConstructionName, TopElectronicModuleNumber> TopElectronicModule;
+typedef std::pair<TopElectronicConstructionName, TopPixelNumber> TopPixel;
+
+// Maps
+typedef std::map<TopPixel, unsigned int> TopPixelRefMap;
+typedef std::map<unsigned int, TopPixel> TopPixelRetMap;
+typedef std::map<TopPixel, TopElectronicModule> TopPixelRefElectronicRetMap;
+typedef std::map<unsigned int, unsigned int> TopUnsignedMap;
+typedef std::map<TopElectronicModule, unsigned int> TopElectronicRefMap;
+typedef std::map<unsigned int, TopElectronicModule> TopElectronicRetMap;
+
+
+
+
+
+#ifdef __CINT__
+/*
+#pragma link C++ class TopElectronicModule+;
+#pragma link C++ class TopPixel+;
+
+#pragma link C++ class std::pair<std::string, unsigned int>;
+
+#pragma link C++ class TopUnsignedMap+;
+#pragma link C++ class std::pair<unsigned int, unsigned int>+;
+
+#pragma link C++ class TopPixelRefMap+;
+#pragma link C++ class std::pair<TopPixel,unsigned int>+;
+
+#pragma link C++ class TopPixelRetMap+;
+#pragma link C++ class std::pair<unsigned int, TopPixel>+;
+
+#pragma link C++ class TopPixelRefElectronicRetMap+;
+#pragma link C++ class std::pair<TopPixel, TopElectronicModule>+;
+
+#pragma link C++ class TopElectronicRefMap+;
+#pragma link C++ class std::pair<TopElectronicModule, unsigned int>+;
+
+#pragma link C++ class TopElectronicRetMap+;
+#pragma link C++ class std::pair<unsigned int, TopElectronicRetMap>+;
+
+*/
+#endif
+
+
+#endif

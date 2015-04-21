26a10e28dac2ffd6e85b7f1abe958538c0cf0f95
Jeter Hall <jeter.hall@pnnl.gov>
2015-04-21 19:17:34 +0000

    Added more channel number conversions.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17298 b38c6c8f-bee8-465d-8448-9abe6915d496

--- topcaf/utils/iTopUtils.h
+++ topcaf/utils/iTopUtils.h
@@ -5,13 +5,53 @@
 #include <sstream>
 
 
-typedef unsigned int packet_word_t;
-
 template <typename T> std::string NumberToString(T Number)
 {
   std::ostringstream ss;
   ss << Number;
   return ss.str();
 }
+
+unsigned int pixelNumber_to_pixelRow(unsigned int pixel)
+{
+  return (pixel - 1) / 64 + 1;
+}
+unsigned int pixelNumber_to_pixelColumn(unsigned int pixel)
+{
+  return (pixel - 1) % 64 + 1;
+}
+unsigned int pixelNumber_to_PMTNumber(unsigned int pixel)
+{
+  return ((pixel - 1) / 4) % 16 + ((pixel - 1) / 256) * 16 + 1;
+}
+unsigned int pixelNumber_to_channelNumber(unsigned int pixel)
+{
+  return (pixel - 1) % 4 + (((pixel - 1) % 256) / 64) * 4 + 1;
+}
+unsigned int channelNumber_to_pixelNumber(unsigned int PMT, unsigned int channel)
+{
+  return (channel - 1) % 4 + (PMT - 1) * 4 + ((channel - 1) / 4) * 64 + ((PMT - 1) / 16) * 192 + 1;
+}
+unsigned int pixel_to_electronicsModuleNumber(unsigned int pixel)
+{
+  return ((pixel - 1) % 64) / 16;
+}
+unsigned int pixel_to_asicRow(unsigned int pixel)
+{
+  return (pixel - 1) / 128;
+}
+unsigned int pixel_to_asicColumn(unsigned int pixel)
+{
+  return ((pixel - 1) % 16) / 4;
+}
+unsigned int pixel_to_asicChannel(unsigned int pixel)
+{
+  return 2 * ((3 - (pixel - 1) % 4)) + ((pixel - 1) % 128) / 64;
+}
+unsigned int asic_to_pixel(unsigned int electronicsModule, unsigned int asicRow, unsigned int asicColumn, unsigned int asicChannel)
+{
+  return 4 - (asicChannel % 8) / 2 + (asicChannel % 2) * 64 + asicColumn * 4 + electronicsModule * 16 + asicRow * 128;
+}
+
 
 #endif

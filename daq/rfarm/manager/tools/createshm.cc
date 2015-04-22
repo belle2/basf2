300217f8fcfc58753ce82304b0c2029a915c75b7
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2015-04-22 09:14:32 +0000

    Utilities for RFARM
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17312 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/rfarm/manager/tools/createshm.cc
@@ -0,0 +1,30 @@
+//+
+// File : createshm.cc
+// Description : Create Shared Memory for flow logging
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 7 - Apr - 2015
+//-
+#include <string>
+#include <vector>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "daq/rfarm/manager/RFSharedMem.h"
+
+using namespace Belle2;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  if (argc < 2) {
+    printf("createrb : rbufname \n");
+    exit(-1);
+  }
+  RFSharedMem* shm = new RFSharedMem(argv[1]);
+}
+
+
+

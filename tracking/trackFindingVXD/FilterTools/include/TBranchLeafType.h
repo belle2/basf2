1ec17c5f87d97f4185bb2e55c69820d5b17559e9
Oliver Frost <oliver.frost@desy.de>
2015-04-23 13:47:24 +0000

    Mark definitions in header file as inline to prevent a multiply defined symbol conflict when included in several compilation units.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17349 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingVXD/FilterTools/include/TBranchLeafType.h
+++ tracking/trackFindingVXD/FilterTools/include/TBranchLeafType.h
@@ -13,40 +13,40 @@
 
 namespace Belle2 {
 
-  char
+  inline char
   TBranchLeafType(const char*) { return 'C'; };
 
-  char
+  inline char
   TBranchLeafType(const Char_t&) { return 'B'; };
 
-  char
+  inline char
   TBranchLeafType(const unsigned char&) { return 'b'; };
 
-  char
+  inline char
   TBranchLeafType(const short&) { return 'S'; };
 
-  char
+  inline char
   TBranchLeafType(const unsigned short&) { return 's'; };
 
-  char
+  inline char
   TBranchLeafType(const Int_t&) { return 'I'; };
 
-  char
+  inline char
   TBranchLeafType(const UInt_t&) { return 'i'; };
 
-  char
+  inline char
   TBranchLeafType(const Float_t&) { return 'F'; };
 
-  char
+  inline char
   TBranchLeafType(const Double_t&) { return 'D'; };
 
-  char
+  inline char
   TBranchLeafType(const long int&) { return 'L'; };
 
-  char
+  inline char
   TBranchLeafType(const unsigned long int&) { return 'l'; };
 
-  char
+  inline char
   TBranchLeafType(const bool&) { return 'O'; };
 
 

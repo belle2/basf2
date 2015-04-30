369e51bd30d320e20369c78755e00707e552a0f3
Malachi Schram <malachi.schram@pnnl.gov>
2015-04-30 23:43:05 +0000

    Update condition db code
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17545 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/modules/conditions/src/ConditionsModule.cc
+++ framework/modules/conditions/src/ConditionsModule.cc
@@ -16,6 +16,17 @@ ConditionsModule::ConditionsModule(): Module()
   addParam("RunName", m_run_name, "Run name", std::string("r000545"));
   addParam("RESTBaseName", m_rest_basename, "Base name for REST services", std::string("http://belle2db.hep.pnnl.gov/b2s/rest/v1/"));
   addParam("FileBaseName", m_file_basename, "Base name for conditions files", std::string("http://belle2db.hep.pnnl.gov/"));
+  addParam("FileBaseLocal", m_file_local, "Directory name for local conditions files copies", std::string("/tmp/"));
+
+  /// Parameters for custom payload additions.  See framework/conditions/scripts/add_payload.py example.
+  addParam("FileName", m_filename,
+           "Filename for manual payload addition.  This payload will be added to conditions on intialization.", std::string());
+  addParam("Package", m_package, "Package for manual payload addition.  This payload will be added to conditions on intialization.",
+           std::string());
+  addParam("Module", m_module, "Module for manual payload addition.  This payload will be added to conditions on intialization.",
+           std::string());
+  addParam("Comment", m_comment, "Comment for manual payload addition.  This payload will be added to conditions on intialization.",
+           std::string());
 
 }
 
@@ -26,8 +37,15 @@ void ConditionsModule::initialize()
 
   ConditionsService::GetInstance()->SetRESTbasename(m_rest_basename);
   ConditionsService::GetInstance()->SetFILEbasename(m_file_basename);
+  ConditionsService::GetInstance()->SetFILEbaselocal(m_file_local);
+
+  if (m_filename.length() > 0) {
+    B2INFO("custom payload add requested : " << m_filename);
+    ConditionsService::GetInstance()->WritePayloadFile(m_filename, m_package, m_module);
+  }
 
   B2INFO("Initialized: ");
+
 }
 
 void ConditionsModule::beginRun()
@@ -39,3 +57,4 @@ void ConditionsModule::terminate()
 {
 
 }
+

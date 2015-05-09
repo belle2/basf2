0b0be34fdb8f4d4589d2d2c14d473aa48a773439
Michal Kristof <michail.kristof@gmail.com>
2015-05-09 11:32:32 +0000

    more speed optimalisation uses a) native js functions b) css instead of show/hide. Load time reduction ~ 5-10% measured.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17749 b38c6c8f-bee8-465d-8448-9abe6915d496

--- validation/html/js/loadcontent.js
+++ validation/html/js/loadcontent.js
@@ -17,12 +17,12 @@ function loadcontent() {
 	       in accordance with the checkbox  */
 	    if( $('.displaydescriptions').is(':checked') ){
 		$('.wrap_boxes').each(function(){
-			$(this).show();
+			$(this).css("display", "block");
 	        });
 	    }
 	    else {
             $('.wrap_boxes').each(function(){
-			$(this).hide();
+			$(this).css("display", "none");
 	      	});
 	    }
 		if( $('.package_overview').is(':checked') ){
@@ -38,38 +38,37 @@ function loadcontent() {
 	    
 			}).done( function(){
 	    $("input.matrix-toggle").click(function(){
-			$("span#"+$(this).attr("value")).toggle();
+			$("span#"+this.value).toggle();
 	    });
 	    
 	    $("input.plot-size").on("input", function(){
-			var range_val = $(this).val();
-			var range_id = $(this).attr("id");
-			$(".imidzes_"+range_id).width((range_val-2)+"%");
+			var range_val = this.value;
+			var range_id = this.id;
+			$(".imidzes_"+range_id).css("width", (range_val-2)+"%");
 			$("#slidernumber_"+range_id).text(range_val);
 			$("input.slidernumber_"+range_id).val(range_val);
         });
         
         $(".hide").on("click", function(){
-			var text = $(this).closest(".hidetext").text();
-			$(".wrap_"+$(this).attr("id")).toggle();
-			$(this).closest(".hidetext").text(text == "(hide)" ? "(show)" : "(hide)");
+			$(".wrap_"+this.id).toggle();
+			$(this).text($(this).text() == "(hide)" ? "(show)" : "(hide)");
 	    });
     $('.selectall').on("click", function () {
 		var checked = this.checked;
         $(this).closest('form').find(':checkbox').each(function(){
 			$(this).prop("checked", checked);
 			if(checked){
-				$("#"+$(this).attr("value")).css("display", "block");
+				$("#"+this.value).css("display", "block");
 			} else {
-				$("#"+$(this).attr("value")).css("display", "none");
+				$("#"+this.value).css("display", "none");
 			}
 	});
     });
 		$('.imidz').on("click", function() {
 				if(!$(this).attr("id")){
-					var plot_show_id = $(this).attr("name");
+					var plot_show_id = this.name;
 				} else {
-					var plot_show_id = $(this).attr("id");
+					var plot_show_id = this.id;
 				}
 				if($("."+plot_show_id).css("display") == "none") {
 					if($('input[type="checkbox"][name="overview"]').prop('checked')) {
@@ -85,5 +84,5 @@ function loadcontent() {
 		 
     });
     });
-    $("#outer").hide();
+    $("#outer").css("display", "none");
 }

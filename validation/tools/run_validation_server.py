2aca912d596dc6a27925cd6b1bf0bafb6a46e00d
Michal Kristof <michail.kristof@gmail.com>
2015-05-11 17:11:39 +0000

    added colour distinction for failed and skipped scripts, also -p and --port cmd line arguments now can define the port on which the server will run
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17802 b38c6c8f-bee8-465d-8448-9abe6915d496

--- validation/tools/run_validation_server.py
+++ validation/tools/run_validation_server.py
@@ -23,8 +23,8 @@ from validationplots import create_plots
 
 def parse_cmd_line_arguments():
         """!
-        Sets up a parser for command line arguments, parses them and returns the
-        arguments.
+        Sets up a parser for command line arguments,
+        parses them and returns the arguments.
         @return: An object containing the parsed command line arguments.
         Arguments are accessed like they are attributes of the object,
         i.e. [name_of_object].[desired_argument]
@@ -37,6 +37,9 @@ def parse_cmd_line_arguments():
         parser.add_argument("-ip", "--ip", help="The IP address on which the"
                             "server starts. Default is 'localhost'.",
                             type=str, default='localhost')
+        parser.add_argument("-p", "--port", help="The port number on which"
+                            " the server starts. Default is '8000'.",
+                            type=str, default=8000)
 
         # Return the parsed arguments!
         return parser.parse_args()
@@ -89,7 +92,8 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def parse_POST(self):
         """!
-        A method that parses header sent to the server and returns POST method html form data.
+        A method that parses header sent to the server and returns
+        POST method html form data.
         """
 
         ctype, pdict = parse_header(self.headers['content-type'])
@@ -124,10 +128,13 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
                 self.send_response(404)
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
-                self.wfile.write(json.dumps({"error": "Missing input parameter"}))
+                self.wfile.write(json.dumps({"error":
+                                             "Missing input parameter"}))
                 return
 
-            (respCode, respContent, respContType) = self.do_GET_JSON(data['input'])
+            (respCode,
+             respContent,
+             respContType) = self.do_GET_JSON(data['input'])
 
             self.send_response(respCode)
             self.send_header('Content-type', respContType)
@@ -138,45 +145,59 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 
             return
 
-        elif (contentType.startswith('multipart/form-data') or
-              contentType.startswith('application/x-www-form-urlencoded')) and "matrix_save" in self.path:
+        elif ((contentType.startswith('multipart/form-data') or
+               contentType.startswith('application/x-www-form-urlencoded')) and
+              "matrix_save" in self.path):
             postvars = self.parse_POST()
             images = []
             pakidz = set()
             for element in postvars['matrix']:
                 pakidz.add(element.split("__")[0])
                 images.append(element.split("__")[1])
-            if len(pakidz) > 1 or len(postvars['size']) > 1 or len(postvars['package']) > 1:
-                sys.exit("Error: Two or more packages selected in /save_matrix")
+            if (len(pakidz) > 1 or
+                    len(postvars['size']) > 1 or
+                    len(postvars['package']) > 1):
+                sys.exit("Error: Two or more packages"
+                         " selected in /save_matrix")
             if "pdf" in postvars["type"]:
                 try:
-                    if merge_multiple_plots("".join(postvars['package']), images):
-                        log.debug("POST: Requested plots successfully merged and the pdf saved.")
+                    if merge_multiple_plots("".join(postvars['package']),
+                                            images):
+                        log.debug("POST: Requested plots "
+                                  "successfully merged and the pdf saved.")
                         self.send_response(301)
-                        self.send_header('Location', './plots/merged_plots.pdf')
+                        self.send_header('Location',
+                                         './plots/merged_plots.pdf')
                         self.end_headers()
                     else:
-                        log.debug("POST: Something went wrong, pdf could not be saved.")
+                        log.debug("POST: Something went wrong, pdf could not"
+                                  " be saved.")
                         sys.exit()
                 except ImportError:
-                    log.debug("ImportError - the module 'PyPDF2' is missing. \
-                              Please install it by running commands 'source activate' and 'pip install PyPDF2'")
+                    log.debug("ImportError - the module 'PyPDF2' is missing."
+                              "Please install it by running commands "
+                              "'source activate' and 'pip install PyPDF2'")
                     self.send_response(301)
                     self.send_header('Location', './import_error_pdf.html')
                     self.end_headers()
             else:
                 try:
-                    if create_image_matrix(images, "".join(postvars['package']), int(max(postvars['size']))):
-                        log.debug("POST: Requested plot matrix successfully created and saved.")
+                    if create_image_matrix(images,
+                                           "".join(postvars['package']),
+                                           int(max(postvars['size']))):
+                        log.debug("POST: Requested plot matrix"
+                                  " successfully created and saved.")
                         self.send_response(301)
                         self.send_header('Location', './plots/matrix.png')
                         self.end_headers()
                     else:
                         log.debug("POST: Plot matrix could not be created.")
-                        self.wfile.write("Error: Requested plot matrix could not be created.")
+                        self.wfile.write("Error: Requested plot matrix "
+                                         "could not be created.")
                 except ImportError:
-                    log.debug("ImportError - the module 'pillow' is missing. \
-                              Please install it by running commands 'source activate' and 'pip install pillow'")
+                    log.debug("ImportError - the module 'pillow' is missing. "
+                              "Please install it by running commands"
+                              " 'source activate' and 'pip install pillow'")
                     self.send_response(301)
                     self.send_header('Location', './import_error.html')
                     self.end_headers()
@@ -186,7 +207,8 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps({"error": "ContentType not supported"}))
+            self.wfile.write(json
+                             .dumps({"error": "ContentType not supported"}))
             return
 
     def do_GET_FILE(self):
@@ -210,11 +232,14 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
             file_type = 'text/html'
 
         if self.path.startswith('../'):
-            log.debug('GET: "%s" trying to escape from the sandbox' % self.path)
+            log.debug('GET: "%s" trying to escape from the sandbox' % self
+                      .path)
             docFile = open(errorPath, 'r')
             code = 404
-        elif docPath.endswith('/') and os.path.exists("%sindex.html" % docPath):
-            log.debug('GET: Serving %sindex.html for %s' % (docPath, self.path))
+        elif docPath.endswith('/') and os.path.exists("%sindex.html" %
+                                                      docPath):
+            log.debug('GET: Serving %sindex.html for %s' % (docPath,
+                                                            self.path))
             docFile = open("%sindex.html" % docPath, 'r')
             code = 200
         elif os.path.exists(docPath):
@@ -257,35 +282,44 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
             folders = ['./results/' + __ for __ in os.listdir('./results')
                        if os.path.isdir('./results/' + __)]
 
-            # The folder containing the data from the most recent validation run
+            # The folder containing the data from
+            # the most recent validation run
             newest = sorted(folders, key=os.path.getctime)[-1]
 
             # Read in the failed scripts
-            failedscripts_path = newest + "/__general__/list_of_failed_scripts.log"
+            failedscripts_path = (newest +
+                                  "/__general__/list_of_failed_scripts.log")
             if os.path.exists(failedscripts_path):
                 # If a list of failed scripts exists, read it in
                 with open(failedscripts_path, 'r') as f:
                     failed_scripts = f.readlines()
+                    failed_scripts = [line.strip()[:-3] for line in
+                                      failed_scripts]
             else:
                 # Otherwise use an empty list
                 failed_scripts = []
 
             # Read in the skipped scripts
-            skippedscripts_path = newest + "/__general__/list_of_skipped_scripts.log"
+            skippedscripts_path = (newest +
+                                   "/__general__/list_of_skipped_scripts.log")
             if os.path.exists(skippedscripts_path):
                 # If a list of skipped scripts exists, read it in
                 with open(skippedscripts_path, 'r') as f:
                     skipped_scripts = f.readlines()
+                    skipped_scripts = [line.strip()[:-3] for line in
+                                       skipped_scripts]
             else:
                 # Otherwise use an empty list
                 skipped_scripts = []
 
             # Add some general information to the HTML
             html.append('<strong>General information:</strong><br>')
-            html.append('Execution failed for <strong>{0}</strong> scripts!<br>'
-                        .format(len(failed_scripts)))
-            html.append('Execution skipped <strong>{0}</strong> scripts!<br>'
-                        .format(len(skipped_scripts)))
+            html.append('Execution failed for '
+                        '<strong style="color:red">{0}</strong> scripts!'
+                        '<br>'.format(len(failed_scripts)))
+            html.append('Execution skipped '
+                        '<strong style="color:orange">{0}</strong>'
+                        ' scripts!<br>'.format(len(skipped_scripts)))
             html.append('<br>')
 
             # A dict of all logfiles.
@@ -295,8 +329,12 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
                 if os.path.isdir(newest + '/' + _):
                     logfiles[_] = []
             for pkg in logfiles:
-                logfiles[pkg] = [newest + '/' + pkg + '/' + _ for _ in os.listdir(newest + '/' + pkg) if _.endswith('.log')]
+                logfiles[pkg] = [newest + '/' + pkg +
+                                 '/' + _ for _ in os.listdir(newest + '/' +
+                                                             pkg)
+                                 if _.endswith('.log')]
 
+            print failed_scripts
             # Parse the dict into HTML
             for pkg in sorted(logfiles.keys()):
                 html.append('<strong>{0}</strong><br>'.format(pkg))
@@ -306,8 +344,21 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
                     filename = os.path.basename(logfile)
                     filepath = logfile
 
-                    # Add the entry to the html list
-                    html.append('<span class="loglink" name="{0}">{1}</span></br>'.format(filepath, filename))
+                    if filename[:-7] in failed_scripts:
+                        html.append('<span class="loglink" name="{0}" '
+                                    'style="color: red">&bull;&nbsp;{1}'
+                                    '</span></br>'.format(filepath, filename))
+                    elif filename[:-7] in skipped_scripts:
+                        html.append('<span class="loglink" name="{0}" '
+                                    'style="color:orange">&bull;&nbsp;{1}'
+                                    '</span></br>'.format(filepath, filename))
+
+                    else:
+                        # Add the entry to the html list
+                        html.append('<span class="loglink" name="{0}">'
+                                    '<span style="color:green">&bull;</span>'
+                                    ' {1}</span></br>'
+                                    .format(filepath, filename))
                 html.append('<br>')
 
             return (200, json.dumps('\n'.join(html)), 'application/json')
@@ -319,18 +370,22 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
             folders = ['./results/' + __ for __ in os.listdir('./results')
                        if os.path.isdir('./results/' + __)]
 
-            # The folder containing the data from the most recent validation run
+            # The folder containing the data from
+            # the most recent validation run
             newest = sorted(folders, key=os.path.getctime)[-1]
 
             # Get the number of failed/skipped scripts
-            number_of_failed_scripts = sum(1 for line in open(newest + '/__general__/list_of_failed_scripts.log'))
-            number_of_skipped_scripts = sum(1 for line in open(newest + '/__general__/list_of_skipped_scripts.log'))
+            failpth = newest + '/__general__/list_of_failed_scripts.log'
+            shippth = newest + '/__general__/list_of_skipped_scripts.log'
+            number_of_failed_scripts = sum(1 for line in open(failpth))
+            number_of_skipped_scripts = sum(1 for line in open(skippth))
 
             # Turn that into the result we want to deliver to the website
             if number_of_failed_scripts + number_of_skipped_scripts == 0:
                 result = ''
             else:
-                result = '{0}+{1}'.format(number_of_failed_scripts, number_of_skipped_scripts)
+                result = '{0}+{1}'.format(number_of_failed_scripts,
+                                          number_of_skipped_scripts)
 
             # Answer the AJAX request
             return (200, json.dumps(result), 'application/json')
@@ -368,7 +423,8 @@ if __name__ == '__main__':
 
     # Make sure the output of validate_basf2.py is there
     if not os.path.isdir('html/results'):
-        sys.exit('Error: No html/results dir found! Run validate_bash2.py first.')
+        sys.exit('Error: No html/results '
+                 'dir found! Run validate_basf2.py first.')
 
     # Go to the html directory
     os.chdir('html')
@@ -378,14 +434,14 @@ if __name__ == '__main__':
                     format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%H:%M:%S')
 
-    # Define the server address
+    # Define the server address and port
     ip = cmd_arguments.ip
-    port = 8000
+    port = int(cmd_arguments.port)
 
     # Start the server!
     try:
         log.info("Server: Starting HTTP server on %s:%d" % (ip, port))
-        run(ip=ip)
+        run(ip=ip, port=port)
         log.info("Server: Terminating")
     # Terminate upon KeyboardInterrupt
     except KeyboardInterrupt, k:

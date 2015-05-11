9a778b9e89bf4aef9f78e95813cbb5e576274e2d
Thomas Hauth <thomas.hauth@kit.edu>
2015-05-11 14:53:48 +0000

    added test to ensure the reconstruction chain can be executed without the simulation being loaded before
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17788 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ reconstruction/tests/ExecuteStandaloneReco.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Test to ensure the reconstruction can be run standalone without load evtgen/simulation
+performed before in the same process.
+"""
+
+import os
+from ROOT import Belle2
+
+evtgen_steering = Belle2.FileSystem.findFile('reconstruction/tests/evtgen.py_noexec')
+reco_steering = Belle2.FileSystem.findFile('reconstruction/tests/reco.py_noexec')
+
+# run generator & simulation
+assert(0 == os.system("basf2 " + evtgen_steering))
+# run reconstruction only
+assert(0 == os.system("basf2 " + reco_steering))

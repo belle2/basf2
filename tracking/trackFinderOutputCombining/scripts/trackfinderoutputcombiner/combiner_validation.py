4e4e078db79bc6d7617e1a2cd6a9dd5c8d8f9117
Nils Braun <nils.braun@student.kit.edu>
2015-05-11 05:41:43 +0000

    Deleted python combiner
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17764 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFinderOutputCombining/scripts/trackfinderoutputcombiner/combiner_validation.py
+++ tracking/trackFinderOutputCombining/scripts/trackfinderoutputcombiner/combiner_validation.py
@@ -36,169 +36,6 @@ def get_logger():
 CONTACT = "ucddn@student.kit.edu"
 
 
-class TestingModule(basf2.Module):
-
-    def calculate_matching_tracks(self, segment, perpS_of_tracks):
-        front = segment.front()
-        back = segment.back()
-
-        list_of_matches = list()
-
-        for track in perpS_of_tracks:
-
-            # Calculate distance
-            trajectory = perpS_of_tracks[track][0]
-
-            distance = trajectory.getDist2D(front.getRecoPos2D())
-            if abs(distance) > 10:
-                continue
-
-            # Calculate perpS
-            perpS_min = perpS_of_tracks[track][1]
-            perpS_max = perpS_of_tracks[track][2]
-
-            frontRecoPos3D = front.reconstruct3D(trajectory)
-            backRecoPos3D = back.reconstruct3D(trajectory)
-
-            forwardZ = front.getWire().getSkewLine().forwardZ()
-            backwardZ = front.getWire().getSkewLine().backwardZ()
-
-            if frontRecoPos3D.z() > forwardZ or frontRecoPos3D.z() < backwardZ:
-                continue
-
-            perpS_segment = [trajectory.calcPerpS(frontRecoPos3D.xy()), trajectory.calcPerpS(backRecoPos3D.xy())]
-
-            if np.min(perpS_segment) > perpS_min and np.max(perpS_segment) < perpS_max:
-                wireHits = perpS_of_tracks[track][3]
-                perpS_list = perpS_of_tracks[track][4]
-                superlayer_segment = segment.getISuperLayer()
-
-                hits_in_same_superlayer = 0
-
-                for wireHit, perpS in zip(wireHits, perpS_list):
-                    if wireHit.getISuperLayer() == superlayer_segment and perpS > 0.5 * \
-                            np.min(perpS_segment) and perpS < 1.5 * np.max(perpS_segment):
-                        hits_in_same_superlayer += 1
-
-                if hits_in_same_superlayer > 3:
-                    continue
-            else:
-                continue
-
-            list_of_matches.append([track, [np.min(perpS_segment), np.max(perpS_segment)]])
-
-        return list_of_matches
-
-    def calculate_perpS_list_of_tracks(self):
-
-        tracks = Belle2.PyStoreArray("LegendreTrackCands")
-        cdcHits = Belle2.PyStoreArray("CDCHits")
-
-        perpS_of_tracks = dict()
-        wireHitTopology = Belle2.TrackFindingCDC.CDCWireHitTopology.getInstance()
-        for track in tracks:
-            perpS_list = list()
-            pos = track.getPosSeed()
-            mom = track.getMomSeed()
-            trajectory = Belle2.TrackFindingCDC.CDCTrajectory2D(Belle2.TrackFindingCDC.Vector2D(pos.X(), pos.Y()),
-                                                                Belle2.TrackFindingCDC.Vector2D(mom.X(), mom.Y()),
-                                                                track.getChargeSeed())
-            wireHits = [wireHitTopology.getWireHit(cdcHits[cdcHitID]) for cdcHitID in track.getHitIDs(Belle2.Const.CDC)]
-            perpS_list = [trajectory.calcPerpS(wireHit.getRefPos2D()) for wireHit in wireHits if wireHit.getStereoType() == 0]
-            perpS_of_tracks.update({track: [trajectory, np.min(perpS_list), np.max(perpS_list), wireHits, perpS_list]})
-
-        return perpS_of_tracks
-
-    def event(self):
-        perpS_of_tracks = self.calculate_perpS_list_of_tracks()
-
-        items = Belle2.PyStoreObj("CDCRecoSegment2DVector")
-        wrapped_vector = items.obj()
-        vector = wrapped_vector.get()
-
-        circleFitter = Belle2.TrackFindingCDC.CDCRiemannFitter.getFitter()
-
-        track_to_segment_dict = {item: [] for item in perpS_of_tracks}
-
-        for segment in vector:
-            if segment.getStereoType() != 0:
-
-                matching_tracks = self.calculate_matching_tracks(segment, perpS_of_tracks)
-
-                if len(matching_tracks) == 1:
-                    track_to_segment_dict[matching_tracks[0][0]].append([segment, matching_tracks[0][1]])
-
-        for track, segment_list in track_to_segment_dict.items():
-            segment = None
-
-            if len(segment_list) == 1:
-                self.add_segment_to_track(segment_list[0][0], track)
-            elif len(segment_list) > 1:
-                # we have more than one possibility
-
-                segments_by_superlayer = {i: [] for i in xrange(9)}
-
-                for segment in segment_list:
-                    segments_by_superlayer[segment[0].getISuperLayer()].append(segment)
-
-                for id, segment_list in segments_by_superlayer.items():
-                    if len(segment_list) == 1:
-                        self.add_segment_to_track(segment_list[0][0], track)
-                    elif len(segment_list) > 1:
-                        # Put together segments that create a whole track part
-                        perpS_segments = [segment[1] for segment in segment_list]
-
-                        def is_non_overlapping_segment_list(l):
-                            return reduce(operator.and_, [y[0] - x[1] > 0 for x, y in zip(l[:-1], l[1:])], True)
-
-                        is_good = [
-                            is_non_overlapping_segment_list(
-                                np.sort(c)) for c in chain.from_iterable(
-                                combinations(
-                                    perpS_segments,
-                                    r) for r in xrange(
-                                    2,
-                                    len(segment_list) + 1))]
-                        all_combinations = chain.from_iterable(
-                            combinations(
-                                segment_list,
-                                r) for r in xrange(
-                                2,
-                                len(segment_list) +
-                                1))
-                        possibly_track_parts = [comb for comb, flag in zip(all_combinations, is_good) if flag]
-
-                        if len(possibly_track_parts) == 0:
-
-                            trajectory = perpS_of_tracks[track][0]
-                            perpS_list = perpS_of_tracks[track][4]
-                            wireHits = perpS_of_tracks[track][3]
-
-                            distances = []
-
-                            for possible_match in segment_list:
-                                perpS_segment_front, perpS_segment_back = possible_match[1]
-                                distances.append(np.min(abs(np.subtract(perpS_segment_front, perpS_list))) +
-                                                 np.min(abs(np.subtract(perpS_segment_back, perpS_list))))
-
-                            segment = segment_list[np.argmin(distances)][0]
-
-                            self.add_segment_to_track(segment, track)
-
-                        elif len(possibly_track_parts) == 1:
-                            for segment in possibly_track_parts[0]:
-                                self.add_segment_to_track(segment[0], track)
-
-                        else:
-                            print possibly_track_parts
-
-    def add_segment_to_track(self, segment, track):
-        for recoHit in segment.items():
-            track.addHit(Belle2.Const.CDC, recoHit.getWireHit().getStoreIHit())
-
-        # segment.clear()
-
-
 class CombinerValidationRun(LegendreTrackFinderRun, AddValidationMethod):
 
     local_track_cands_store_array_name = "LocalTrackCands"
@@ -207,7 +44,7 @@ class CombinerValidationRun(LegendreTrackFinderRun, AddValidationMethod):
         # Sets up a path that plays back pregenerated events or generates events
         # based on the properties in the base class.
 
-        self.stereo_assignment = True
+        self.stereo_assignment = False
 
         main_path = super(CombinerValidationRun, self).create_path()
 
@@ -228,7 +65,7 @@ class CombinerValidationRun(LegendreTrackFinderRun, AddValidationMethod):
 
         display_module = CDCSVGDisplayModule()
         display_module.draw_segments_id = True
-        # main_path.add_module(display_module)
+        main_path.add_module(display_module)
 
         combiner_module = basf2.register_module("SegmentTrackCombiner")
         combiner_module.param({'WriteGFTrackCands': False,
@@ -238,26 +75,16 @@ class CombinerValidationRun(LegendreTrackFinderRun, AddValidationMethod):
         # combiner_module.set_log_level(basf2.LogLevel.DEBUG)
         main_path.add_module(combiner_module)
 
-        second_local_track_finder = basf2.register_module('TrackFinderCDCSegmentTripleAutomaton')
-        second_local_track_finder.param({'WriteGFTrackCands': True,
-                                         'SkipHitsPreparation': True,
-                                         'GFTrackCandsStoreArrayName': "ResultTrackCands",
-                                         'TracksStoreObjNameIsInput': True})
-        main_path.add_module(second_local_track_finder)
-
         display_module = CDCSVGDisplayModule()
         display_module.draw_track_trajectories = True
         display_module.draw_tracks = True
-        # main_path.add_module(display_module)
+        main_path.add_module(display_module)
 
         display_module = CDCSVGDisplayModule()
         display_module.draw_gftrackcand_trajectories = True
         display_module.draw_gftrackcands = True
         display_module.track_cands_store_array_name = "MCTrackCands"
         # main_path.add_module(display_module)
-
-        self.create_validation(main_path, track_candidates_store_array_name="TrackCands", output_file_name="Legendre.root")
-        self.create_validation(main_path, track_candidates_store_array_name="ResultTrackCands", output_file_name="Combiner.root")
 
         return main_path
 

fdc3d5361b3058446b7684513befb515aebecaf9
Oliver Frost <oliver.frost@desy.de>
2015-05-18 14:14:10 +0000

    Introduced a variable set for reconstructed segments.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18014 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingCDC/filters/segment/include/CDCRecoSegment2DVarSet.h
@@ -0,0 +1,66 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+#ifndef CDCRECOSEGMENT2DVARSET_H
+#define CDCRECOSEGMENT2DVARSET_H
+
+#include <tracking/trackFindingCDC/varsets/VarNames.h>
+#include <tracking/trackFindingCDC/varsets/VarSet.h>
+
+#include <tracking/trackFindingCDC/rootification/IfNotCint.h>
+
+namespace Belle2 {
+  namespace TrackFindingCDC {
+
+    /// Forward declaration.
+    class CDCRecoSegment2D;
+
+    /// Names of the variables to be generated.
+    IF_NOT_CINT(constexpr)
+    static char const* const segmentVarNames[] = {
+      "size",
+      "superlayer_id",
+
+    };
+
+    /** Class that specifies the names of the variables
+     *  that should be generated from a segment
+     */
+    class CDCRecoSegment2DVarNames : public VarNames<CDCRecoSegment2D> {
+
+    public:
+      /// Number of variables to be generated.
+      static const size_t nNames = size(segmentVarNames);
+
+      /// Getter for the name a the given index
+      IF_NOT_CINT(constexpr)
+      static char const* getName(int iName)
+      {
+        return segmentVarNames[iName];
+      }
+
+    };
+
+    /** Class that computes floating point variables from a segment.
+     *  that can be forwarded to a flat TNtuple or a TMVA method
+     */
+    class CDCRecoSegment2DVarSet : public VarSet<CDCRecoSegment2DVarNames> {
+
+    public:
+      /// Construct the varset and take an optional prefix to be prepended to all variable names.
+      CDCRecoSegment2DVarSet(const std::string& prefix = "");
+
+      /// Generate and assign the variables from the facet relation
+      virtual bool extract(const CDCRecoSegment2D* ptrSegment) IF_NOT_CINT(override final);
+    };
+  }
+}
+
+#endif // CDCRECOSEGMENT2DVARSET_H

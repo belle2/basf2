0a4c597c21445a8562add4a4c41d0c4171fdcbb2
Oliver Frost <oliver.frost@desy.de>
2015-05-18 13:07:13 +0000

    Introduced a quick generation of overview plots for variables in a TTree versus a classification target variable.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17998 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/scripts/trackfindingcdc/tmva/train.py
+++ tracking/trackFindingCDC/scripts/trackfindingcdc/tmva/train.py
@@ -3,12 +3,29 @@ import basf2
 from ROOT import gSystem
 import ROOT
 
+import numpy as np
+
+import tracking.validation.classification as classification
+from tracking.validation.utilities import root_cd
 
 import logging
 
 
 def get_logger():
     return logging.getLogger(__name__)
+
+
+def get_truth_name(variable_names):
+    """Selects the shortest variable name form containing the 'truth'."""
+    truth_names = [name for name in variable_names if "truth" in name]
+
+    # select the shortest
+    try:
+        truth_name = min(truth_names, key=len)
+    except ValueError:
+        raise ValueError("variable_names='%s' does not contain a truth variable" % variable_names)
+    else:
+        return truth_name
 
 
 class FastBDTClassifier(object):
@@ -107,5 +124,101 @@ class FastBDTClassifier(object):
             output_file.Close()
 
 
-if __name__ == "__main__":
-    main()
+class ClassificationOverview:
+
+    """Class to generate overview plots for the classification power of various variables from a TTree.
+
+    In order to get an overview, which variables from a diverse set, generated from a recording filter
+    or some other sort of validation, perform well in classification task.
+
+    """
+
+    def __init__(self, output_file_name, truth_name=None, select=[], exclude=[]):
+        self.output_file_name = output_file_name
+        self.truth_name = truth_name
+        self.select = select
+        self.exclude = exclude
+
+        self.classification_analyses = []
+
+    def train(self, input_tree):
+        """Main method feed with a TTree containing the truth variable and the variables to be investigated.
+
+        Branches that contain "truth" in the name are considered to directly contain information about
+        true classification target and are not analysed here.
+
+        Args:
+            input_tree (ROOT.TTree) : Tree containing the variables to be investigated
+                as well as the classification target.
+            truth_name (str, optional) : Name of the branch of the classification target.
+                If not given the Branch with the shortest name containing "truth" is selected.
+        """
+
+        if isinstance(self.output_file_name, str):
+            output_file = ROOT.TFile(self.output_file_name, "RECREATE")
+        else:
+            output_file = self.output_file_name
+
+        output_file.cd()
+
+        column_names = [leave.GetName() for leave in input_tree.GetListOfLeaves()]
+
+        tree_name = input_tree.GetName()
+
+        truth_name = self.truth_name
+
+        if truth_name is None:
+            truth_name = get_truth_name(column_names)
+
+        if truth_name not in column_names:
+            raise KeyError("Truth column {truth} not in tree {tree}".format(truth=truth_name,
+                                                                            tree=tree_name))
+        variable_names = [name for name in column_names if name != truth_name]
+
+        exclude = self.exclude
+        select = self.select
+
+        if select:
+            variable_names = [name for name in variable_names if name in select]
+
+        if exclude:
+            variable_names = [name for name in variable_names if name not in exclude]
+
+        # Remove the variables that have Monte Carlo truth information unless explicitly selected
+        variable_names = [name for name
+                          in variable_names
+                          if "truth" not in name or name in select]
+
+        print "Truth name", truth_name
+        print "Variable names", variable_names
+
+        import root_numpy
+        print "Loading tree"
+        branch_names = variable_names + [truth_name]
+        input_record_array = root_numpy.tree2rec(input_tree, branches=branch_names)
+        print "Loaded tree"
+        truths = input_record_array[truth_name]
+
+        for variable_name in variable_names:
+            print 'Analyse', variable_name
+
+            # Get the truths as a numpy array
+            estimates = input_record_array[variable_name]
+
+            classification_analysis = classification.ClassificationAnalysis(
+                contact="",
+                quantity_name=variable_name,
+                outlier_z_score=5.0,
+                allow_discrete=True,
+            )
+            classification_analysis.analyse(estimates, truths)
+
+            with root_cd(variable_name) as tdirectory:
+                classification_analysis.write(tdirectory)
+
+            self.classification_analyses.append(classification_analysis)
+
+        if isinstance(self.output_file_name, str):
+            output_file.Close()
+
+        print "Saved overviews completely"

8354fafe872d0ca7e5423d026f98d6494f892562
Oliver Frost <oliver.frost@desy.de>
2015-05-18 13:01:59 +0000

    Change docstrings to google style for a test. Introduce module for conditional execution if a StoreArray is present.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@17985 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/scripts/tracking/metamodules.py
+++ tracking/scripts/tracking/metamodules.py
@@ -4,14 +4,16 @@
 """This file contains python modules that generally decorate other modules and paths
 to serve a slightly changed purpose or circumstance.
 
-Note
-----
-A great deal of the these modules are quite general purpose helper constructs,
-which might be helpful in other parts of the BASF2, and might therefore be better
-placed in the framework folder.
+Note:
+  A great deal of the these modules are quite general purpose helper constructs,
+  which might be helpful in other parts of the BASF2, and might therefore be better
+  placed in the framework folder.
 """
 
 import basf2
+import ROOT
+from ROOT import Belle2
+
 import cProfile
 import pstats
 
@@ -26,22 +28,21 @@ def get_logger():
 
 class WrapperModule(basf2.Module):
 
-    """Wrapping module base class that generally wraps a single module to slightly change its behaviour.
+    """Wrapping module base class that wraps a single module to slightly change its behaviour.
 
     The base implementation does nothing, but forward the relevant methods to the wrapped module.
 
-    Notes
-    -----
-    Implements the decorator/wrapper pattern.
+    Note:
+      Implements the decorator/wrapper pattern.
 
-    Attributes
-    ----------
-    module : basf2.Module
-        The wrapped module instance.
+    Attributes:
+      module (basf2.Module): The wrapped module instance.
     """
 
     def __init__(self, module):
         super(WrapperModule, self).__init__()
+
+        #: The wrapped module
         self.module = module
 
     @property
@@ -54,8 +55,13 @@ class WrapperModule(basf2.Module):
         return self.module.param
 
     def name(self):
-        """Forwards the name"""
-        return "{wrapper_name}({module_name})".format(module_name=self.module.name(), wrapper_name=self.wrapper_name)
+        """Compose a name that indicates the wrapped module."""
+        return "{wrapper_name}({module_name})".format(module_name=self.module.name(),
+                                                      wrapper_name=self.wrapper_name)
+
+    def getName(self):
+        """Forwards the name()."""
+        return self.name()
 
     def initialize(self):
         """Initialize method of the module"""
@@ -84,15 +90,15 @@ class PyProfilingModule(WrapperModule):
 
     Uses cProfile.
 
-    Attributes
-    ----------
-    module : basf2.Module
-        The wrapped module that should be profiled. Should be a module written in Python,
-        since the profile interacts with the interpretor for the measurements,
-        but cannot look into c++ implementations.
-    output_file_name : str, optional
-        Path to the file where the profiling information shall be stored. Defaults to profile.txt.
-    profiler : cProf
+    Attributes:
+      module (basf2.Module): The wrapped module that should be profiled.
+        Should be a module written in Python, since the profile interacts
+        with the interpretor for the measurements, but cannot look into c++ implementations.
+
+      output_file_name (str, optional): Path to the file where the profiling information
+        shall be stored. Defaults to profile.txt.
+
+      profiler (cProfile.Profile): Profiler instance to manage and extract the profiling statistics.
     """
 
     default_output_file_name = "profile.txt"
@@ -122,7 +128,8 @@ class PyProfilingModule(WrapperModule):
         super(PyProfilingModule, self).terminate()
         sortby = 'cumulative'
         with open(self.output_file_name, 'w') as profile_output_file:
-            profile_stats = pstats.Stats(self.profiler, stream=profile_output_file).sort_stats(sortby)
+            profile_stats = pstats.Stats(self.profiler, stream=profile_output_file)
+            profile_stats.sort_stats(sortby)
             profile_stats.print_stats()
 
 
@@ -132,71 +139,127 @@ class IfModule(WrapperModule):
 
     There are two ways to specify the condition.
     One way is to override the condtion(self) method in a subclass.
-    The second way is to give a function as the second argument to the module constructor, which is called
-    each event.
+    The second way is to give a function as the second argument to the module constructor,
+    which is called each event.
 
-    Attributes
-    ----------
-    module : basf2.Module
-         The module executed, if the condition is met.
-    condition : function() -> bool, optional
-         Function executed at each event to determine, if the given module shall be executed.
-         If None call the condition method instead, which can be overridden by subclasses
+    Attributes:
+      module (basf2.Module): The module executed, if the condition is met.
+
+      condition (function() -> bool, optional): Function executed at each event to determine,
+        if the given module shall be executed.
+        If None call the condition method instead, which can be overridden by subclasses.
+
     """
 
     def __init__(self, module, condition=None):
-        """Initialisation method taking the module instance to
+        """Initialisation method taking the module instance to be wrapped.
 
-        Parameters
-        ----------
-        module : basf2.Module
-            The module executed, if the condition is met.
-        condition : function() -> bool, optional
-            Function executed at each event to determine, if the given module shall be executed.
+        Args:
+          module (basf2.Module): The module executed, if the condition is met.
+          condition  (function() -> bool, optional): Function executed at each event to determine,
+            if the given module shall be executed.
             If None call the condition method instead, which can be overridden by subclasses.
         """
 
         super(IfModule, self).__init__(module)
         if condition is not None:
-            # Condition function called at each event to determine if wrapped module should be executed
+            #: Condition function called at each event to determine
+            # if wrapped module should be executed
             self.condition = condition
 
     def condition(self):
         """Condition method called if not given a condition function during construction.
 
-        Can be overridden by a concrete subclass to specify under which condition the wrapped module should be executed.
+        Can be overridden by a concrete subclass to specify
+        under which condition the wrapped module should be executed.
         It can also be shadowed by a condition function given to the constructor of this module.
 
-        Returns
-        -------
-        bool
-            The indication if the wrapped module should be executed. Always True in the base implementation
+        Returns:
+          bool: The indication if the wrapped module should be executed.
+            Always True in the base implementation
         """
         return True
 
     def event(self):
         """Event method of the module
 
-        Evaluates the condition and sets the return value of this module to trigger the execution of the wrapped module.
+        Evaluates the condition and sets the return value of this module
+        to trigger the execution of the wrapped module.
         """
 
         if self.condition():
             super(IfModule, self).event()
 
 
+class IfStoreArrayPresentModule(IfModule):
+
+    """Conditional execution of the wrapped module based if a StoreArray is present.
+
+    Attributes:
+      storearray_name (str): The name of the StoreArray which presence has to be checked.
+      storearray_durability (int): The durability of the StoreArray
+      storearray_is_present (bool): The flag whether the StoreArray is present.
+        Set during initialisation.
+    """
+
+    def __init__(self,
+                 module,
+                 storearray_name,
+                 storearray_durability=0):
+        """
+        Args:
+          module (basf2.Module): The module executed, if the condition is met.
+          storearray_name (str): The name of the StoreArray which presence has to be checked.
+          storearray_durability (int, optional): The durability of the StoreArray. Default 0.
+        """
+
+        super(IfStoreArrayPresentModule, self).__init__(module)
+
+        #: Name of the StoreArray to be checked (str).
+        self.storearray_name = storearray_name
+
+        #: Durability of the StoreArray to be checked (int).
+        self.storearray_durability = storearray_durability
+
+        #: Flag that the StoreArray is present (bool).
+        self.storearray_is_present = None
+
+    def condition(self):
+        """Returns the flag if the StoreArray is present"""
+        if self.storearray_is_present is None:
+            storearray = Belle2.PyStoreArray(self.storearray_name, self.storearray_durability)
+            if storearray:
+                self.storearray_is_present = True
+            else:
+                self.storearray_is_present = False
+
+        return self.storearray_is_present
+
+
+class IfMCParticlesPresentModule(IfStoreArrayPresentModule):
+
+    """Conditional execution of the wrapped module based on the presence of Monte Carlo information.
+    """
+
+    def __init__(self, module):
+        """
+        Args:
+          module (basf2.Module): The module executed, if the condition is met.
+        """
+        super(IfMCParticlesPresentModule, self).__init__(module, "MCParticles")
+
+
 class PathModule(basf2.Module):
 
     """Wrapper for a BASF2 path into a module such that
-    it can be passed around and added to a BASF2 path as BASF2 module.
+    it can be passed around and added to a BASF2 path as a BASF2 module.
 
     The wrapper is implement in such a way that it unconditionally executes
     the contained path by means of a positive return value.
     The calling path is continued after the execution of the wrapped path.
 
-    Attributes
-    ----------
-    _path : basf2.Path
-        The wrapped execution path.
+    Attributes:
+      _path (basf2.Path): The wrapped execution path.
     """
 
     def __init__(self, path=None, modules=None):
@@ -205,12 +268,11 @@ class PathModule(basf2.Module):
         May also give a list of modules that should be appended to the path.
         If the path is omitted a new basf2.Path is constructed.
 
-        Parameters
-        ----------
-        path : basf2.Path, optional
-            The execution path to be wrapped. Of no path is given create a new one.
-        modules : iterable of basf2.Module, optional
-            Module to be added to the path.
+        Args:
+          path (basf2.Path): The execution path to be wrapped.
+            If no path is given create a new one.
+
+          modules (iterable of basf2.Module): Module to be added to the path.
         """
 
         super(PathModule, self).__init__()

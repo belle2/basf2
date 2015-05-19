66fddd9074872637d6cf646df26f41573817b404
YunTsung Lai <ytlai@hep1.phys.ntu.edu.tw>
2015-05-19 08:27:05 +0000

    TRGGRLModule uploaded
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18040 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ trg/grl/modules/trggrl/include/TRGGRLModule.h
@@ -0,0 +1,85 @@
+//-----------------------------------------------------------------------------
+// $Id$
+//-----------------------------------------------------------------------------
+// Filename : TRGGRLModule.h
+// Section  : TRG GRL
+// Owner    : Yoshihito Iwasaki
+// Email    : yoshihito.iwasaki@kek.jp
+//-----------------------------------------------------------------------------
+// Description : A trigger module for GRL
+//-----------------------------------------------------------------------------
+// 0.00 : 2013/12/13 : First version
+//-----------------------------------------------------------------------------
+
+#ifndef TRGGRLModule_H
+#define TRGGRLModule_H
+
+#include <string>
+#include "framework/core/Module.h"
+#include "trg/grl/TRGGRL.h"
+
+namespace Belle2 {
+
+/// A module to simulate the Global Decision Logic. This module
+/// requires sub-trigger simulation data (CDC, ECL, TOP, and KLM). The
+/// output is GRL response.
+
+class TRGGRLModule : public Module {
+
+  public:
+
+    /// Constructor
+    TRGGRLModule();
+//  TRGGRLModule(const std::string & type);
+
+    /// Destructor
+    virtual ~TRGGRLModule();
+
+    /// Initilizes TRGGRLModule.
+    virtual void initialize();
+
+    /// Called when new run started.
+    virtual void beginRun();
+
+    /// Called event by event.
+    virtual void event();
+
+    /// Called when run ended.
+    virtual void endRun();
+
+    /// Called when processing ended.
+    virtual void terminate();
+
+  public:
+
+    /// returns version of TRGGRLModule.
+    std::string version(void) const;
+
+  private: // Parameters
+
+    /// Debug level.
+    int _debugLevel;
+
+    /// Config. file name.
+    std::string _configFilename;
+
+    /// Mode for TRGGRL simulation. 0th bit : fast simulation switch,
+    /// 1st bit : firmware simulation switch.
+    int _simulationMode;
+
+    /// Switch for the fast simulation. 0:do everything, 1:stop after
+    /// the track segment simulation. Default is 0.
+    int _fastSimulationMode;
+
+    /// Switch for the firmware simulation. 0:do nothing, 1:do everything
+    int _firmwareSimulationMode;
+
+  private:
+
+    /// A pointer to a TRGGRL;
+    static TRGGRL * _grl;
+};
+
+} // namespace Belle2
+
+#endif // TRGGRLModule_H
---
+++ trg/grl/modules/trggrl/src/TRGGRLModule.cc
@@ -0,0 +1,142 @@
+//-----------------------------------------------------------------------------
+// $Id$
+//-----------------------------------------------------------------------------
+// Filename : TRGGRLModule.cc
+// Section  : TRG GRL
+// Owner    : Yoshihito Iwasaki
+// Email    : yoshihito.iwasaki@kek.jp
+//-----------------------------------------------------------------------------
+// Description : A trigger module for GRL
+//-----------------------------------------------------------------------------
+// 0.00 : 2013/12/13 : First version
+//-----------------------------------------------------------------------------
+
+#define TRGGRL_SHORT_NAMES
+
+#include <stdlib.h>
+#include <iostream>
+#include "framework/core/ModuleManager.h"
+#include "trg/trg/Debug.h"
+#include "trg/grl/modules/trggrl/TRGGRLModule.h"
+
+using namespace std;
+
+namespace Belle2 {
+
+REG_MODULE(TRGGRL);
+
+TRGGRL *
+TRGGRLModule::_grl = 0;
+
+string
+TRGGRLModule::version() const {
+    return string("TRGGRLModule 0.00");
+}
+
+TRGGRLModule::TRGGRLModule()
+    : Module::Module(),
+      _debugLevel(0),
+      _configFilename("TRGGDLConfig.dat"),
+      _simulationMode(2),
+      _fastSimulationMode(0),
+      _firmwareSimulationMode(0) {
+    
+    string desc = "TRGGRLModule(" + version() + ")";
+    setDescription(desc);
+    setPropertyFlags(c_ParallelProcessingCertified);
+    
+    addParam("DebugLevel", _debugLevel, "TRGGRL debug level", _debugLevel);
+    addParam("ConfigFile",
+             _configFilename,
+             "The filename of CDC trigger config file",
+             _configFilename);
+    addParam("SimulationMode",
+             _simulationMode,
+             "TRGGRL simulation switch",
+             _simulationMode);
+    addParam("FastSimulationMode",
+             _fastSimulationMode,
+             "TRGGRL fast simulation mode",
+             _fastSimulationMode);
+    addParam("FirmwareSimulationMode",
+             _firmwareSimulationMode,
+             "TRGGRL firmware simulation mode",
+             _firmwareSimulationMode);
+    
+    if (TRGDebug::level())
+        cout << "TRGGRLModule ... created" << endl;
+}
+
+TRGGRLModule::~TRGGRLModule() {
+
+    if (_grl)
+        TRGGRL::getTRGGRL("good-bye");
+
+    if (TRGDebug::level())
+        cout << "TRGGRLModule ... destructed " << endl;
+}
+
+void
+TRGGRLModule::initialize() {
+
+    TRGDebug::level(_debugLevel);
+
+    if (TRGDebug::level()) {
+	cout << "TRGGRLModule::initialize ... options" << endl;
+	cout << TRGDebug::tab(4) << "debug level = " << TRGDebug::level()
+	     << endl;
+    }
+}
+
+void
+TRGGRLModule::beginRun() {
+
+    //...GDL config. name...
+    string cfn = _configFilename;
+
+    //...GRL...
+    if (_grl == 0) { 
+	_grl = TRGGRL::getTRGGRL(cfn,
+				 _simulationMode,
+				 _fastSimulationMode,
+				 _firmwareSimulationMode);
+    }
+    else if (cfn != _grl->configFile()) {
+	_grl = TRGGRL::getTRGGRL(cfn,
+				 _simulationMode,
+				 _fastSimulationMode,
+				 _firmwareSimulationMode);
+    }
+
+    if (TRGDebug::level()) {
+        cout << "TRGGDLModule ... beginRun called " << endl;
+        cout << "                 configFile = " << cfn << endl;
+    }
+}
+
+void
+TRGGRLModule::event() {
+    TRGDebug::enterStage("TRGGRLModule event");
+
+    //...GRL simulation...
+    _grl->update(true);
+    _grl->simulate();
+
+    TRGDebug::leaveStage("TRGGRLModule event");
+}
+
+void
+TRGGRLModule::endRun() {
+    if (TRGDebug::level())
+        cout << "TRGGRLModule ... endRun called " << endl;
+}
+
+
+void
+TRGGRLModule::terminate() {
+    _grl->terminate();
+    if (TRGDebug::level())
+	cout << "TRGGRLModule ... terminate called " << endl;
+}
+
+} // namespace Belle2

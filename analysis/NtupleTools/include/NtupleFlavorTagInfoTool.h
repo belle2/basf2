77b43516935be0c5a572b83d779a164561bf3632
Christian Roca <crisroc@mpp.mpg.de>
2015-05-20 14:49:41 +0000

    Added new functionalities and compatibility with FlavorTagInfo DataObject
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18097 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/NtupleTools/include/NtupleFlavorTagInfoTool.h
+++ analysis/NtupleTools/include/NtupleFlavorTagInfoTool.h
@@ -19,16 +19,35 @@
 
 namespace Belle2 {
 
-  /** Writes tag vertex position and error of B candidate to flat ntuple. */
+  /** Writes all the Flavor Tag Information to flat ntuple. All members are arrays,
+   whose elements correspond to all the categories used in the TMVA algorithm*/
   class NtupleFlavorTagInfoTool : public NtupleFlatTool {
   private:
-    /** reconstructed tag vertex x component. */
+    /** probability of the selected track to be daugther of the Btag (given by TMVA) */
     float* m_targetP;
-    /** reconstructed tag vertex y component. */
+    /** probability of the selected track to belong to each category (given by TMVA)*/
     float* m_categoryP;
-    float* m_isB;
-    /** reconstructed tag vertex z component. */
-    std::string m_categories[8] = {"Electron", "Muon", "KinLepton", "Kaon", "SlowPion", "FastPion", "Lambda", "MaximumP*"};
+    /** absolute momentum **/
+    float* m_P;
+    /** 0 - not from Btag, 1-5 - code of the parent meson  **/
+    float* m_isFromB;
+    float* m_D0;
+    float* m_Z0;
+    float* m_MCvertex;
+
+    float m_isFromBGeneral;
+    float m_ROEComesFromB;
+    float m_goodTracksPurityFT;
+    float m_goodBadTracksRatioFT;
+    float m_goodTracksPurityROE;
+    float m_goodBadTracksRatioROE;
+    float m_badTracksROE;
+    float m_goodTracksROE;
+    float m_badTracksFT;
+    float m_goodTracksFT;
+    float m_FTandROEGoodTracksRatio;
+    /** categories used in the TMVA */
+    std::string m_categories[8] = {"Electron", "Muon", "KinLepton", "Kaon", "SlowPion", "FastPion", "Lambda", "MaximumP"};
 
     /** Create branches in m_tree - this function should be called by the constructor only. */
     void setupTree();
--- analysis/NtupleTools/src/NtupleFlavorTagInfoTool.cc
+++ analysis/NtupleTools/src/NtupleFlavorTagInfoTool.cc
@@ -22,21 +22,64 @@ void NtupleFlavorTagInfoTool::setupTree()
 
   m_targetP = new float[8]();
   m_categoryP = new float[8]();
-  m_isB = new float[8]();
+  m_isFromB = new float[8]();
+  m_D0 = new float[8]();
+  m_Z0 = new float[8]();
+  m_P = new float[8]();
+  m_MCvertex = new float[8]();
 
+  m_isFromBGeneral = 0;
+  m_ROEComesFromB = 0;
+  m_goodTracksPurityFT = 0;
+  m_goodBadTracksRatioFT = 0;
+  m_FTandROEGoodTracksRatio = 0;
+  m_goodTracksPurityROE = 0;
+  m_goodBadTracksRatioROE = 0;
+  m_badTracksROE = 0;
+  m_goodTracksROE = 0;
+  m_badTracksFT = 0;
+  m_goodTracksFT = 0;
 
   for (unsigned i = 0; i < sizeof(m_targetP); i++) {
-    B2INFO("FILLING:");
-    B2INFO(m_targetP[i]);
-    B2INFO(m_categoryP[i]);
-    B2INFO(m_categories[i]);
+    +
     m_tree->Branch((strNames[0] + "_" + m_categories[i] + "_targetP").c_str(), &m_targetP[i],
                    (strNames[0] + "_" + m_categories[i] + "_targetP/F").c_str());
     m_tree->Branch((strNames[0] + "_" + m_categories[i] + "_categoryP").c_str(), &m_categoryP[i],
                    (strNames[0] + "_" + m_categories[i] + "_categoryP/F").c_str());
-    m_tree->Branch((strNames[0] + "_" + m_categories[i] + "_comesFromB").c_str(), &m_isB[i],
+    m_tree->Branch((strNames[0] + "_" + m_categories[i] + "_comesFromB").c_str(), &m_isFromB[i],
                    (strNames[0] + "_" + m_categories[i] + "_comesFromB/F").c_str());
+    m_tree->Branch((strNames[0] + "_" + m_categories[i] + "_P").c_str(), &m_P[i],
+                   (strNames[0] + "_" + m_categories[i] + "_P/F").c_str());
+    m_tree->Branch((strNames[0] + "_" + m_categories[i] + "_D0").c_str(), &m_D0[i],
+                   (strNames[0] + "_" + m_categories[i] + "_D0/F").c_str());
+    m_tree->Branch((strNames[0] + "_" + m_categories[i] + "_Z0").c_str(), &m_Z0[i],
+                   (strNames[0] + "_" + m_categories[i] + "_Z0/F").c_str());
+    m_tree->Branch((strNames[0] + "_" + m_categories[i] + "_MCvertex").c_str(), &m_MCvertex[i],
+                   (strNames[0] + "_" + m_categories[i] + "_MCvertex/F").c_str());
   }
+  m_tree->Branch((strNames[0] + "_comesFromBGeneral").c_str(), &m_isFromBGeneral,
+                 (strNames[0] + "_comesFromBGeneral/F").c_str());
+  m_tree->Branch((strNames[0] + "_ROEComesFromB").c_str(), &m_ROEComesFromB,
+                 (strNames[0] + "_ROEComesFromB/F").c_str());
+  m_tree->Branch((strNames[0] + "_goodTracksPurityFT").c_str(), &m_goodTracksPurityFT,
+                 (strNames[0] + "_goodTracksPurityFT/F").c_str());
+  m_tree->Branch((strNames[0] + "_goodBadTracksRatioFT").c_str(), &m_goodBadTracksRatioFT,
+                 (strNames[0] + "_goodBadTracksRatioFT").c_str());
+  m_tree->Branch((strNames[0] + "_goodTracksPurityROE").c_str(), &m_goodTracksPurityROE,
+                 (strNames[0] + "_goodTracksPurityROE/F").c_str());
+  m_tree->Branch((strNames[0] + "_goodBadTracksRatioROE").c_str(), &m_goodBadTracksRatioROE,
+                 (strNames[0] + "_goodBadTracksRatioROE/F").c_str());
+  m_tree->Branch((strNames[0] + "_FTandROEGoodTracksRatio").c_str(), &m_FTandROEGoodTracksRatio,
+                 (strNames[0] + "_FTandROEGoodTracksRatio/F").c_str());
+
+  m_tree->Branch((strNames[0] + "_goodTracksROE").c_str(), &m_goodTracksROE,
+                 (strNames[0] + "_goodTracksROE/F").c_str());
+  m_tree->Branch((strNames[0] + "_badTracksROE").c_str(), &m_badTracksROE,
+                 (strNames[0] + "_badTracksROE/F").c_str());
+  m_tree->Branch((strNames[0] + "_goodTracksFT").c_str(), &m_goodTracksFT,
+                 (strNames[0] + "_goodTracksFT/F").c_str());
+  m_tree->Branch((strNames[0] + "_badTracksFT").c_str(), &m_badTracksFT,
+                 (strNames[0] + "_badTracksFT/F").c_str());
 }
 
 void NtupleFlavorTagInfoTool::eval(const Particle* particle)
@@ -54,8 +97,24 @@ void NtupleFlavorTagInfoTool::eval(const Particle* particle)
     for (unsigned i = 0; i < flavTag->getCategories().size(); i++) {
       m_targetP[i] = flavTag->getTargProb()[i];
       m_categoryP[i] = flavTag->getCatProb()[i];
-      m_isB[i] = float(flavTag->getIsB()[i]);
+      m_isFromB[i] = flavTag->getIsFromB()[i];
+      m_P[i] = flavTag->getP()[i];
+      m_Z0[i] = flavTag->getZ0()[i];
+      m_D0[i] = flavTag->getD0()[i];
+      m_MCvertex[i] = flavTag->getProdVertexMotherZ()[i];
     }
+    m_isFromBGeneral = float(flavTag->getIsFromBGeneral());
+    m_ROEComesFromB = float(flavTag->getROEComesFromB());
+    m_goodTracksPurityFT = flavTag->getGoodTracksPurityFT();
+    m_goodBadTracksRatioFT = flavTag->getGoodBadTracksRatioFT();
+    m_goodTracksPurityROE = flavTag->getGoodTracksPurityROE();
+    m_goodBadTracksRatioROE = flavTag->getGoodBadTracksRatioROE();
+    m_FTandROEGoodTracksRatio = flavTag->getFTandROEGoodTracksRatio();
+    m_badTracksROE = float(flavTag->getBadTracksROE());
+    m_goodTracksROE = float(flavTag->getGoodTracksROE());
+    m_badTracksFT = float(flavTag->getBadTracksFT());
+    m_goodTracksFT = float(flavTag->getGoodTracksFT());
+
   } else {
     B2ERROR("FlavorTagInfo Object does not exist");
   }

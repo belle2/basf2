2379d099b6f46e201c42bcd7cf05aff78cff69d8
Torben Ferber <ferber@physics.ubc.ca>
2015-05-20 08:08:54 +0000

    convert root output to hepevt
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18064 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ generators/examples/RootToHepEvt.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+########################################################
+# Convert basf2 Root output to HepEvt
+#
+# Example steering file
+########################################################
+
+from basf2 import *
+
+# Set the global log level
+set_log_level(LogLevel.INFO)
+
+main = create_path()
+
+# Register the input module
+myinput = register_module('RootInput')
+myinput.param('inputFileName', './input.root')
+
+# Register the output module
+myoutput = register_module('HepevtOutput')
+myoutput.param('OutputFilename', './output.hepevt')
+
+# Register the Progress module and the Python histogram module
+myprogress = register_module('Progress')
+
+main = create_path()
+main.add_module(myprogress)
+main.add_module(myinput)
+main.add_module(myoutput)
+main.add_module("PrintMCParticles", logLevel=LogLevel.DEBUG, onlyPrimaries=False)
+
+# generate events
+process(main)
+
+# show call statistics
+print statistics

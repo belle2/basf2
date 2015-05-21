56dc6da2d9bb007cb6f5eb66e5d3e71122b24cfc
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2015-05-21 07:02:29 +0000

    essage=Adde error messages
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18130 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/readout/src/ProcessListener.cc
+++ daq/slc/readout/src/ProcessListener.cc
@@ -23,17 +23,13 @@ void ProcessListener::run()
     LogFile::fatal("Failed to wait processed process %s", process_name.c_str());
     return;
   }
-  LogFile::debug("%s:%d", __FILE__, __LINE__);
   m_con->lock();
-  LogFile::debug("%s:%d", __FILE__, __LINE__);
   RCCallback& callback(*(m_con->getCallback()));
   NSMNode& node(callback.getNode());
   if (m_con->getInfo().isAvailable()) {
     unsigned int state = m_con->getInfo().getState();
-    LogFile::debug("%s:%d", __FILE__, __LINE__);
     switch (state) {
       case RunInfoBuffer::RUNNING:
-        LogFile::debug("%s:%d", __FILE__, __LINE__);
         if (node.getState() == RCState::RUNNING_S ||
             node.getState() == RCState::LOADING_TS ||
             node.getState() == RCState::STARTING_TS) {
@@ -41,20 +37,21 @@ void ProcessListener::run()
           LogFile::error(emsg);
           callback.replyLog(LogFile::ERROR, emsg);
           m_con->getInfo().reportError(RunInfoBuffer::PROCESS_DOWN);
+        } else if (node.getState() == RCState::READY_S) {
+          std::string emsg = StringUtil::form("Foked process %s (pid = %d) was not started", process_name.c_str(), pid);
+          LogFile::error(emsg);
+          callback.replyLog(LogFile::ERROR, emsg);
+          m_con->getInfo().reportError(RunInfoBuffer::PROCESS_DOWN);
         }
-        LogFile::debug("%s:%d", __FILE__, __LINE__);
         break;
       case RunInfoBuffer::READY: {
-        LogFile::debug("%s:%d", __FILE__, __LINE__);
         std::string emsg = StringUtil::form("Foked process %s (pid = %d) was not started", process_name.c_str(), pid);
         LogFile::error(emsg);
         callback.replyLog(LogFile::ERROR, emsg);
         m_con->getInfo().reportError(RunInfoBuffer::PROCESS_DOWN);
-        LogFile::debug("%s:%d", __FILE__, __LINE__);
       } break;
       case RunInfoBuffer::NOTREADY:
       default: {
-        LogFile::debug("%s:%d", __FILE__, __LINE__);
         if (node.getState() == RCState::LOADING_TS) {
           std::string emsg = StringUtil::form("Foked process %s (pid = %d) was not booted", process_name.c_str(), pid);
           LogFile::error(emsg);
@@ -65,14 +62,9 @@ void ProcessListener::run()
           LogFile::debug(emsg);
           callback.replyLog(LogFile::INFO, emsg);
         }
-        LogFile::debug("%s:%d", __FILE__, __LINE__);
       } break;
     }
-    LogFile::debug("%s:%d", __FILE__, __LINE__);
   }
-  LogFile::debug("%s:%d", __FILE__, __LINE__);
   process.set_id(-1);
-  LogFile::debug("%s:%d", __FILE__, __LINE__);
   m_con->unlock();
-  LogFile::debug("%s:%d", __FILE__, __LINE__);
 }

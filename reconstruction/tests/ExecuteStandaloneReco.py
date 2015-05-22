b5e044a695b141b0fe75be41601270b8c4e0a4ec
Thomas Hauth <thomas.hauth@kit.edu>
2015-05-22 13:06:46 +0000

    executing reconstruction test in temporary folder now
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18257 b38c6c8f-bee8-465d-8448-9abe6915d496

--- reconstruction/tests/ExecuteStandaloneReco.py
+++ reconstruction/tests/ExecuteStandaloneReco.py
@@ -7,12 +7,21 @@ performed before in the same process.
 """
 
 import os
+import tempfile
+import shutil
 from ROOT import Belle2
 
 evtgen_steering = Belle2.FileSystem.findFile('reconstruction/tests/evtgen.py_noexec')
 reco_steering = Belle2.FileSystem.findFile('reconstruction/tests/reco.py_noexec')
 
+# create and move to temporary directory
+tempdir = tempfile.mkdtemp()
+print "Moving to temporary directory " + str(tempdir)
+os.chdir(tempdir)
+
 # run generator & simulation
 assert(0 == os.system("basf2 " + evtgen_steering))
 # run reconstruction only
 assert(0 == os.system("basf2 " + reco_steering))
+
+shutil.rmtree(tempdir)

04eba8b1340c9d1222fc2c0a807a433f0c4752fd
Eugenio Paoloni <eugenio.paoloni@pi.infn.it>
2015-05-22 10:06:44 +0000

    A quick and very minimal Observer for the filter tools
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18223 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingVXD/FilterTools/include/Observer.h
@@ -0,0 +1,40 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2013 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Eugenio Paoloni                                          *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+#include <string>
+#include <tuple>
+#include <iostream>
+
+
+#include "tracking/vxdCaTracking/VXDTFHit.h"
+#include "tracking/trackFindingVXD/FilterTools/Range.h"
+#include "tracking/trackFindingVXD/TwoHitFilters/Distance3DSquared.h"
+#include <iostream>
+
+#include "tracking/vxdCaTracking/VXDTFHit.h"
+
+namespace Belle2 {
+
+  class Observer {
+  public:
+
+    template< class T>
+    //    static void notify( Belle2::Distance3DSquared<Belle2::VXDTFHit> , double val, const Belle2::VXDTFHit &,
+    static void notify(T , double val, const Belle2::VXDTFHit&,
+                       const Belle2::VXDTFHit&, Belle2::Range< float, float> m_range)
+    {
+      std::cout << m_range.getInf() << " < " << val  << " < " << m_range.getSup() << " | ";
+    };
+
+    Observer() {};
+  };
+}

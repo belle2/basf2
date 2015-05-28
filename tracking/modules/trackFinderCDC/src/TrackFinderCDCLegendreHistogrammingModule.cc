1963abeca1967ee1330b860e00e1e918ec7c91a9
Nils Braun <nils.braun@student.kit.edu>
2015-05-28 06:15:50 +0000

    Refit the tracks after stereo assignment
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18408 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/modules/trackFinderCDC/src/TrackFinderCDCLegendreHistogrammingModule.cc
+++ tracking/modules/trackFinderCDC/src/TrackFinderCDCLegendreHistogrammingModule.cc
@@ -1,6 +1,8 @@
 #include <tracking/modules/trackFinderCDC/TrackFinderCDCLegendreHistogrammingModule.h>
 #include <tracking/trackFindingCDC/legendre/stereohits/CDCLegendreStereohitsProcesser.h>
 
+#include <tracking/trackFindingCDC/fitting/CDCSZFitter.h>
+#include <tracking/trackFindingCDC/fitting/CDCObservations2D.h>
 #include <tracking/trackFindingCDC/eventdata/tracks/CDCTrack.h>
 
 using namespace std;
@@ -15,10 +17,26 @@ void CDCLegendreHistogrammingModule::generate(std::vector<Belle2::TrackFindingCD
   //create object which will add stereohits to tracks
   StereohitsProcesser stereohitsProcesser;
 
-  B2DEBUG(100, "Processing new track; assigning stereohits.");
-
   for (CDCTrack& track : tracks) {
     //assign stereohits to the track
     stereohitsProcesser.makeHistogramming(track);
+  }
+
+  // Refit the tracks
+  const CDCSZFitter& fitter = CDCSZFitter::getFitter();
+
+  for (CDCTrack& track : tracks) {
+    CDCObservations2D szObservations;
+    for (const CDCRecoHit3D& recoHit : track) {
+      szObservations.append(recoHit.getPerpS(), recoHit.getRecoZ());
+    }
+
+    if (szObservations.size() > 2) {
+      CDCTrajectorySZ szTrajectory;
+      fitter.update(szTrajectory, track);
+
+      CDCTrajectory3D trajectory3D(track.getStartTrajectory3D().getTrajectory2D(), szTrajectory);
+      track.setStartTrajectory3D(trajectory3D);
+    }
   }
 }

20850668a021440a99ad9800b0875e5c8b71c28c
Nils Braun <nils.braun@student.kit.edu>
2015-05-29 07:56:07 +0000

    Added new modules to the testing path
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18442 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/scripts/trackfindingcdc/validation/test_full_path.py
+++ tracking/trackFindingCDC/scripts/trackfindingcdc/validation/test_full_path.py
@@ -9,23 +9,27 @@ import sys
 
 import basf2
 import IPython
+import ROOT
 from ROOT import Belle2
 from ROOT import genfit
 from trackfindingcdc.cdcdisplay import CDCSVGDisplayModule
 
 import numpy as np
 
+from trackfindingcdc.validation.harvester import ReconstructionPositionHarvester
+from tracking.modules import CDCBackgroundHitFinder
+
 
 class TestingModule(basf2.Module):
+    #: A small track cleaner module to reset all tracls that were fitted by
+    #: genfit (to see only not fitted tracks in the event display
 
     def __init__(self, track_store_array_name="TrackCands"):
         super(TestingModule, self).__init__()
-        self.number_fit_failed = 0
-        self.number_fits = 0
-        self.chi2_cutoff = 2
         self.track_store_array_name = track_store_array_name
 
     def event(self):
+        #: Reset all fitted tracks
         tracks = Belle2.PyStoreArray(self.track_store_array_name)
         for track in tracks:
             rel = Belle2.DataStore.getRelationsWithObj(track, "TrackFitResults")
@@ -33,14 +37,21 @@ class TestingModule(basf2.Module):
                 print "Deleting fitted tracks"
                 track.swap(genfit.TrackCand())
 
-    def terminate(self):
-        print "%d, failed %d" % (self.number_fit_failed, self.number_fits)
-
 
 class HitCleaner(basf2.Module):
+    #: A small hit cleaner module to set the track information according to mc information.
+    #: This is surely not for later usage but for testing the genfitter module
+
+    def __init__(self):
+        super(HitCleaner, self).__init__()
+
+        self.number_of_tracks = 0
+        self.number_of_deleted_hits = 0
+        self.number_of_hits = 0
 
     def initialize(self):
         self.cdc_hit_look_up = Belle2.TrackFindingCDC.CDCMCHitLookUp()
+        self.mc_matcher_lookup = Belle2.TrackMatchLookUp("MCTrackCands", "TrackCands")
 
     def event(self):
         tracks = Belle2.PyStoreArray("TrackCands")
@@ -49,6 +60,10 @@ class HitCleaner(basf2.Module):
 
         cdc_hit_lookup = self.cdc_hit_look_up
         cdc_hit_lookup.fill()
+
+        mc_matcher_lookup = self.mc_matcher_lookup
+
+        self.number_of_tracks += tracks.getEntries()
 
         for track in tracks:
             # Store all Hit IDs and reset the track
@@ -74,32 +89,70 @@ class HitCleaner(basf2.Module):
                 if current_mc_track == should_belong_to_track:
                     good_hits.append(hitID)
 
-            track.reset()
-            sorting_parameter = 0
-            for hitID in good_hits:
-                track.addHit(Belle2.Const.CDC, hitID, cdc_hit_lookup.getRLInfo(cdc_hits[hitID]), sorting_parameter)
-                sorting_parameter += 1
+            self.number_of_deleted_hits += deleted_hits
+            self.number_of_hits += len(hitIDs)
 
-            print "having deleted", deleted_hits, "hits"
+            # Set the position and momentum
+            mc_track = mc_matcher_lookup.getMatchedMCTrackCand(track)
+
+            if mc_track:
+                mc_trajectory = Belle2.TrackFindingCDC.CDCTrajectory3D(Belle2.TrackFindingCDC.Vector3D(mc_track.getPosSeed()),
+                                                                       Belle2.TrackFindingCDC.Vector3D(mc_track.getMomSeed()),
+                                                                       mc_track.getChargeSeed())
+                startingPosition = Belle2.TrackFindingCDC.Vector3D(track.getPosSeed().X(), track.getPosSeed().Y(), 0)
+                sStartingPosition = mc_trajectory.calcPerpS(startingPosition)
+                zStartingPosition = mc_trajectory.getTrajectorySZ().mapSToZ(sStartingPosition)
+                mc_trajectory.setLocalOrigin(Belle2.TrackFindingCDC.Vector3D(startingPosition.xy(), zStartingPosition))
+
+                pos = ROOT.TVector3(mc_trajectory.getSupport().x(), mc_trajectory.getSupport().y(), mc_trajectory.getSupport().z())
+                mom = ROOT.TVector3(
+                    mc_trajectory.getMom3DAtSupport().x(),
+                    mc_trajectory.getMom3DAtSupport().y(),
+                    mc_trajectory.getMom3DAtSupport().z())
+
+                # track.setPosMomSeedAndPdgCode(pos, mom , int(mc_track.getChargeSeed() * 211))
+                track.setPdgCode(int(track.getChargeSeed() * 211))
+
+            else:
+                # track.reset()
+                track.setPdgCode(int(track.getChargeSeed() * 211))
+
+    def terminate(self):
+        print("Number of tracks in total: %d" % self.number_of_tracks)
+        print("Number of hits in total: %d" % self.number_of_hits)
+        print("Number of deleted hits: %d" % self.number_of_deleted_hits)
+
+        print("Number of deleted hits per track: %f" % (1.0 * self.number_of_deleted_hits / self.number_of_tracks))
+        print("Ratio of deleted hits: %f" % (100.0 * self.number_of_deleted_hits / self.number_of_hits))
 
 
 class FullRun(StandardEventGenerationRun):
-    # Tester module for all cdc tracking in on path (plus combiners)
+    #: Tester module for all cdc tracking in on path (plus combiners)
 
     n_events = 10
     show_not_fitted_tracks = False
 
     def create_path(self):
-        # Creates the path with all the modules in a row
+        #: Creates the path with all the modules in a row
         main_path = super(FullRun, self).create_path()
 
         add_mc_track_finder(main_path, mc_track_cands_store_array_name="MCTrackCands")
-        main_path.add_module(modules.CDCFullFinder(output_track_cands_store_array_name="TrackCands"))
+        main_path.add_module(modules.CDCBackgroundHitFinder())
+        main_path.add_module(modules.CDCLocalTrackFinder())
+        main_path.add_module(modules.CDCBackgroundHitFinder())
+        main_path.add_module(modules.CDCLegendreTrackFinder())
+        main_path.add_module(modules.CDCStereoSegmentTrackMatcher())
+        main_path.add_module(modules.CDCNotAssignedHitsCombiner(output_track_cands_store_array_name="TrackCands"))
 
-        main_path.add_module(HitCleaner())
+        main_path.add_module(modules.CDCHitUniqueAssumer())
 
-        # main_path.add_module(basf2.register_module("Trasan"))
-        main_path.add_module(modules.CDCFitter(input_track_cands_store_array_name="TrackCands"))
+        # Enable these modules for further testing
+        # main_path.add_module(modules.CDCMCFiller())
+        # main_path.add_module(ReconstructionPositionHarvester(tracks_store_vector_name="CDCTrackVector",
+        #                                                      output_file_name="reconstruction_position.root"))
+        # main_path.add_module(HitCleaner())
+        # main_path.add_module(modules.CDCEventDisplay(full_display=False))
+        # main_path.add_module(modules.CDCFitter(input_track_cands_store_array_name="TrackCands"))
 
         if self.show_not_fitted_tracks:
             main_path.add_module(TestingModule(track_store_array_name="TrackCands"))
@@ -113,11 +166,12 @@ class FullRun(StandardEventGenerationRun):
             main_path.add_module(
                 modules.CDCValidation(
                     track_candidates_store_array_name="TrackCands",
-                    output_file_name="output_with_cleaner.root"))
+                    output_file_name="output.root"))
 
         return main_path
 
 if __name__ == "__main__":
+    #: Main function
     logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(levelname)s:%(message)s')
     run = FullRun()
     run.configure_and_execute_from_commandline()

544396b33ef4599f493db667dc3907dd83ec3380
Nils Braun <nils.braun@student.kit.edu>
2015-06-03 08:53:03 +0000

    Documentation
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18609 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/scripts/trackfindingcdc/quadtree/quadTreePlotter.py
+++ tracking/trackFindingCDC/scripts/trackfindingcdc/quadtree/quadTreePlotter.py
@@ -21,8 +21,12 @@ from trackfindingcdc.cdcdisplay.svgdrawing import attributemaps
 
 
 class QuadTreePlotter(basf2.Module):
+    #: This Module is able to draw the content coming from a QuadTreeImplementation with debugOutput = True.
 
     def __init__(self):
+        """
+        Do not forget to set the ranges! Otherwise you will end up with an empty plot..
+        """
         basf2.Module.__init__(self)
         self.file_name_of_quad_tree_content = "quadtree_content.root"
         self.draw_quad_tree_content = True and False
@@ -32,6 +36,9 @@ class QuadTreePlotter(basf2.Module):
         self.range_y_max = 0
 
     def plot_quad_tree_content(self):
+        """
+        Draw the quad tree content coming from the root file if enabled.
+        """
         if not self.draw_quad_tree_content:
             return
 
@@ -67,12 +74,20 @@ class QuadTreePlotter(basf2.Module):
             plt.gca().add_patch(patch)
 
     def save_and_show_file(self):
+        """
+        Save the plot to a svg and show it (maybe a png would be better?)
+        """
         fileName = "/tmp/" + datetime.now().isoformat() + '.svg'
         plt.savefig(fileName)
         procDisplay = subprocess.Popen(['eog', fileName])
         raw_input("Press Enter to continue..")
 
     def init_plotting(self):
+        """
+        Initialize the figure with the plot ranges
+        We need to implement axes labels later!
+        """
+
         plt.clf()
         plt.xlim(self.range_x_min, self.range_x_max)
         plt.ylim(self.range_y_min, self.range_y_max)
@@ -80,12 +95,19 @@ class QuadTreePlotter(basf2.Module):
         plt.yticks([])
 
     def event(self):
+        """
+        Draw everything
+        """
         self.init_plotting()
         self.plot_quad_tree_content()
         self.save_and_show_file()
 
 
 class SegmentQuadTreePlotter(QuadTreePlotter):
+
+    """
+    Implementation of a quad tree plotter for SegmentQuadTrees
+    """
 
     draw_segment_intersection = True and False
     draw_segment = True and False
@@ -98,6 +120,14 @@ class SegmentQuadTreePlotter(QuadTreePlotter):
     maximum_theta = np.pi
 
     def calculateIntersectionInQuadTreePicture(self, first, second):
+        """
+        Calculate the point where the two given hits intersect
+
+        params
+        ------
+        first: hit
+        second: hit
+        """
         positionFront = first.getRecoPos2D().conformalTransformed()
         positionBack = second.getRecoPos2D().conformalTransformed()
 
@@ -115,6 +145,13 @@ class SegmentQuadTreePlotter(QuadTreePlotter):
         return theta_cut, r_cut
 
     def calculatePositionInQuadTreePicture(self, position):
+        """
+        Transform a given normal coordinate position to a legendre position (conformal transformed)
+
+        params
+        ------
+        position: TrackFindingCDC.Vector2D
+        """
         position = position.conformalTransformed()
 
         theta = np.linspace(-self.maximum_theta / 2, self.maximum_theta / 2, 100)
@@ -123,6 +160,13 @@ class SegmentQuadTreePlotter(QuadTreePlotter):
         return theta, r
 
     def forAllAxialSegments(self, f):
+        """
+        Loop over all segments and execute a function
+
+        params
+        ------
+        f: function
+        """
         items = Belle2.PyStoreObj("CDCRecoSegment2DVector")
         wrapped_vector = items.obj()
         vector = wrapped_vector.get()
@@ -132,6 +176,15 @@ class SegmentQuadTreePlotter(QuadTreePlotter):
                 f(quad_tree_item)
 
     def convertToQuadTreePicture(self, phi, mag, charge):
+        """
+        Convert given track parameters into a point in the legendre space
+
+        params
+        ------
+        phi: phi of the track
+        mag: magnitude of pt
+        charge: charge of the track
+        """
         theta = phi + np.pi / 2
         r = 1 / mag * 1.5 * 0.00299792458 * charge
         if self.theta_shifted:
@@ -147,6 +200,18 @@ class SegmentQuadTreePlotter(QuadTreePlotter):
         return theta, r
 
     def event(self):
+        """
+        Draw everything according to the given options
+
+        Attributes
+        ----------
+        draw_segment_intersection
+        draw_segment
+        draw_segment_averaged
+        draw_segment_fitted
+        draw_mc_information
+        draw_mc_hits
+        """
         if self.theta_shifted:
             self.range_x_min = -self.maximum_theta / 2
             self.range_x_max = self.maximum_theta / 2
@@ -239,6 +304,10 @@ class SegmentQuadTreePlotter(QuadTreePlotter):
 
 class StereoQuadTreePlotter(QuadTreePlotter):
 
+    """
+    Implementation of a quad tree plotter for StereoHitAssignment
+    """
+
     draw_mc_hits = False
     draw_mc_tracks = False
     draw_track_hits = False
@@ -246,6 +315,13 @@ class StereoQuadTreePlotter(QuadTreePlotter):
     delete_bad_hits = False
 
     def create_trajectory_from_track(self, track):
+        """
+        Convert a genfit::TrackCand into a TrackFindingCDC.CDCTrajectory3D
+
+        params
+        ------
+        track: genfit::TrackCand
+        """
         Vector3D = Belle2.TrackFindingCDC.Vector3D
         Trajectory3D = Belle2.TrackFindingCDC.CDCTrajectory3D
 
@@ -256,6 +332,15 @@ class StereoQuadTreePlotter(QuadTreePlotter):
         return Trajectory3D(position, momentum, charge)
 
     def create_reco_hit(self, cdcHit, trajectory3D, rlInfo):
+        """
+        Use a cdc hit and a trajectory to reconstruct a CDCRecoHit3D
+
+        params
+        ------
+        cdcHit: CDCHit
+        trajectory3D: TrackFindingCDC.CDCTrajectory3D
+        rlInfo: RightLeftInfo ( = short)
+        """
         wireHitTopology = Belle2.TrackFindingCDC.CDCWireHitTopology.getInstance()
 
         CDCRecoHit3D = Belle2.TrackFindingCDC.CDCRecoHit3D
@@ -267,6 +352,17 @@ class StereoQuadTreePlotter(QuadTreePlotter):
             return None
 
     def event(self):
+        """
+        Draw the hit content according to the attributes
+
+        Attributes
+        ----------
+        draw_mc_hits
+        draw_mc_tracks
+        draw_track_hits
+        draw_last_track
+        delete_bad_hits
+        """
         self.draw_quad_tree_content = True
 
         self.range_x_min = -2 - np.sqrt(3)

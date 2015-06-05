a52bdc07630d7af9c538f6fef910edd8a137a187
Nils Braun <nils.braun@student.kit.edu>
2015-06-05 07:40:32 +0000

    Check for tmva output
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18670 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingCDC/tmva/scripts/trackfindingcdc/scan_stereo_segment_tmva_cut.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+from tracking.run.event_generation import StandardEventGenerationRun
+from tracking import modules
+from trackfinderoutputcombiner.validation import add_mc_track_finder
+
+import logging
+import sys
+import numpy as np
+
+from multiprocessing import Pool
+
+
+class FullRun(StandardEventGenerationRun):
+    #: Tester module for all cdc tracking in on path (plus combiners)
+
+    n_events = 1000
+    tmva_cut = 0.1
+
+    def create_path(self):
+        #: Creates the path with all the modules in a row
+        main_path = super(FullRun, self).create_path()
+
+        add_mc_track_finder(main_path)
+        main_path.add_module(modules.CDCFullFinder(output_track_cands_store_array_name="TrackCands", stereo_tmva_cut=self.tmva_cut))
+
+        main_path.add_module(
+            modules.CDCValidation(
+                track_candidates_store_array_name="TrackCands",
+                output_file_name="output_%f.root" % self.tmva_cut))
+
+        return main_path
+
+
+class ParameterScanner():
+
+    def __init__(self, lambda_function, parameter_ranges):
+        self.lambda_function = lambda_function
+        self.parameter_ranges = parameter_ranges
+
+    def scan_parameter_space(self):
+        import itertools
+
+        every_parameter_combination = itertools.product(*(self.parameter_ranges))
+        p = Pool(8)
+        p.map(extract_parameters, every_parameter_combination)
+        p.close()
+
+
+def extract_parameters(x):
+    return main(*x)
+
+
+def main(tmva_cut):
+    run = FullRun()
+    run.tmva_cut = tmva_cut
+    run.configure_and_execute_from_commandline()
+
+if __name__ == "__main__":
+    #: Main function
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(levelname)s:%(message)s')
+
+    short_scanner = ParameterScanner(main,
+                                     [np.arange(0.8, 1.0, 0.1)])
+    short_scanner.scan_parameter_space()

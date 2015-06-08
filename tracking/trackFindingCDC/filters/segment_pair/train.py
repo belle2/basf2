2be2fd335b883b231d1ed84993907cff21b0480b
Oliver Frost <oliver.frost@desy.de>
2015-06-08 10:23:24 +0000

    Made training procedure take input parameters.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18706 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/filters/segment_pair/train.py
+++ tracking/trackFindingCDC/filters/segment_pair/train.py
@@ -5,6 +5,9 @@ import os
 import sys
 import os.path
 import basf2
+
+import tracking.utilities as utilities
+
 from tracking.run.event_generation import StandardEventGenerationRun
 from trackfindingcdc.tmva.train import FastBDTClassifier, ClassificationOverview
 
@@ -14,16 +17,46 @@ import logging
 
 
 def main():
-    overview = ClassificationOverview("CDCSegmentPairOverview.root")
+
+    argument_parser = utilities.DefaultHelpArgumentParser()
+
+    argument_parser.add_argument(
+        "records_file_path",
+        help="ROOT file containing the TTree of records on which to train a boosted decision tree.",
+    )
+
+    argument_parser.add_argument(
+        "-t",
+        "--input-tree-name",
+        default="records",
+        help="Name of the input TTree in the ROOT file",
+    )
+
+    arguments = argument_parser.parse_args()
+
+    records_file_path = arguments.records_file_path
+    records_folder_path, records_file_name = os.path.split(records_file_path)
+
+    records_file_basename, ext = os.path.splitext(records_file_name)
+
+    weights_file_name = records_file_basename + ".weights" + ext
+    weights_file_path = os.path.join(records_folder_path, weights_file_name)
+
+    overview_file_name = records_file_basename + ".overview" + ext
+    overview_file_path = os.path.join(records_folder_path, overview_file_name)
+
+    decision_tree_name = records_file_basename
+
+    overview = ClassificationOverview(overview_file_name)
     classifer = FastBDTClassifier(
-        "CDCSegmentPair",
-        "CDCSegmentPairWeights.root",
+        decision_tree_name,
+        weights_file_path,
         replace_nan=-999,
     )
 
-    input_file_name = "CDCSegmentPairTruthRecords.root"
+    input_file_name = records_file_name
     input_file = ROOT.TFile(input_file_name, "READ")
-    input_tree_name = "records"
+    input_tree_name = arguments.input_tree_name
     input_tree = input_file.Get(input_tree_name)
 
     overview.train(input_tree)

10be74e07bc827c8e7dbeab13208169c86609ebc
Oliver Frost <oliver.frost@desy.de>
2015-06-08 10:22:26 +0000

    Implemented a variable set adaptor for rough cuts.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18702 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingCDC/varsets/include/SkimmedVarSet.h
@@ -0,0 +1,52 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+#include <string>
+
+namespace Belle2 {
+  namespace TrackFindingCDC {
+    /** Generic class that generates the same variables from a each of a pair of instances.
+     **/
+    template<class BaseVarSet>
+    class SkimmedVarSet : public BaseVarSet {
+
+    private:
+      typedef BaseVarSet Super;
+
+    public:
+      /// Object type from which the variables shall be extracted
+      typedef typename BaseVarSet::Object Object;
+
+    public:
+      /// Constructure taking a optional prefix that can be attached to the names if request.
+      SkimmedVarSet(const std::string& prefix = "") :
+        Super(prefix)
+      {;}
+
+      /// Main method that extracts the variable values from the complex object.
+      virtual bool extract(const Object* obj)
+      {
+        bool extracted = Super::extract(obj);
+        return extracted and accept(obj);
+      }
+
+      /** Method to be specialised in a derived class after to accept or reject the extracted values.
+       *
+       *  The method may use variables of the object as well as already extracted values.
+       */
+      virtual bool accept(const Object*)
+      {
+        return true;
+      }
+
+    }; //end class
+  } //end namespace TrackFindingCDC
+} //end namespace Belle2

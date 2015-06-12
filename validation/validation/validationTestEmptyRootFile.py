9279138134932ff36741ea4091b8a7a031a2f141
Thomas Hauth <thomas.hauth@kit.edu>
2015-06-12 10:36:42 +0000

    added two scripts to the validation test site to probe for edge cases ( no root file, exit code != 0 )
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18828 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ validation/validation/validationTestEmptyRootFile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This file will not generate an output root file to probe
+the validation suite properly handles this case.
+"""
+
+VALIDATION_OUTPUT_FILE = 'validationTestEmptyRootFile.root'
+N_EVENTS = 10
+ACTIVE = True
+
+import array
+from ROOT import TFile, TNtuple, TH1F, TF1, TRandom3
+
+# make sure we are able to always create the same plots
+import basf2
+basf2.set_random_seed(1337)
+
+tfile = TFile(VALIDATION_OUTPUT_FILE, "RECREATE")
+tfile.Close()
---
+++ validation/validation/validationZTestFail_runlast.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This file will exit with an exit code != 0 to probe the
+validation correctly detects and reports this.
+"""
+
+import sys
+
+print "This script will exit with code 20 on purpose"
+
+sys.exit(20)

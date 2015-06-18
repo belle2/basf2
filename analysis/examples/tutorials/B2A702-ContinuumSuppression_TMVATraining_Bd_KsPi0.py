510b868aac073d41b4ba51a424237d900d990315
Pablo Goldenzweig <pablo.goldenzweig@kit.edu>
2015-06-18 09:24:01 +0000

    Updated tutorials with new skims and additional user options.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18961 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/examples/tutorials/B2A702-ContinuumSuppression_TMVATraining_Bd_KsPi0.py
+++ analysis/examples/tutorials/B2A702-ContinuumSuppression_TMVATraining_Bd_KsPi0.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from basf2 import *
-from modularAnalysis import *
-from stdFSParticles import stdPi0
-from stdV0s import stdKshorts
 
-#######################################################
+################################################################################
 #
 # This tutorial demonstrates how to perform a
 # TMVA training on continuum suppression variables
@@ -22,7 +18,7 @@ from stdV0s import stdKshorts
 # B2A703-ContinuumSuppression_TMVAExpert_Bd_KsPi0.py
 #
 # Note: This example uses a privately produced skim of
-# the continuum MC sample created with no beam background in
+# the continuum MC sample created with no beam background (BGx0) in
 # MC campaign 3.5, therefore it can be run only on KEKCC computers.
 #
 # To run NeuroBayes on KEKCC execute the following before running:
@@ -30,36 +26,69 @@ from stdV0s import stdKshorts
 #  export PHIT_LICENCE_SERVER=bweb3.cc.kek.jp:16820
 #  source /sw/belle/local/neurobayes/belle_default_64bit/setup_neurobayes.sh
 #
+# Usage:
+#  ./B2A702-ContinuumSuppression_TMVATraining_Bd_KsPi0.py <test,full>
+#
+# This will produce /training_qq_<test,full> directories containing
+# the training output
+#
 # Contributors: P. Goldenzweig (January 2015)
 #
-######################################################
+################################################################################
 
-### SKIM
-filelistSIG = \
-    ['/gpfs/fs02/belle/users/pablog/mcGeneration_belle2/Bd_KsPi0_noBeamBkgd_500k/root/*'
-     ]
-filelistCC = \
-    ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_K0Pi0_noBeamBkgd/Bd_KsPi0/mdst/ccbar/*'
-     ]
-filelistSS = \
-    ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_K0Pi0_noBeamBkgd/Bd_KsPi0/mdst/ssbar/*'
-     ]
-filelistDD = \
-    ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_K0Pi0_noBeamBkgd/Bd_KsPi0/mdst/ddbar/*'
-     ]
-filelistUU = \
-    ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_K0Pi0_noBeamBkgd/Bd_KsPi0/mdst/uubar/*'
-     ]
-inputMdstList(filelistSIG + filelistCC + filelistSS + filelistDD + filelistUU)
+
+import sys
+
+# Command-line usage
+if len(sys.argv) != 2:
+    sys.exit(
+        '\nSpecify sampleSize to process. Either `test` or `full`. \n\n'
+        '`test` will train on a subset of the skim (~400 B->KsPi0 signal and ~400 qq bkgd) and will take ~2 min to complete. \n'
+        '       WARNING: Not a good training!! Only useful for testing. \n\n'
+        '`full` will train on the full skim and will take ~1 hour to complete. \n\n'
+        'Usage: ./training_Bd_KsPi0.py sampleSize \n')
+
+sampleSize = str(sys.argv[1])
+
+import os
+from basf2 import *
+from modularAnalysis import *
+from stdFSParticles import stdPi0
+from stdV0s import stdKshorts
+
+
+# _____________________________________________________________________________________________________________________________________
+# Test skim of BGx0 MC 3.5    -    (2 min to process)
+testSIG1 = [
+    '/gpfs/fs02/belle/users/pablog/mcGeneration_belle2/Bd_KsPi0_BGx0_2014_500k/root/EvtGenSimRec_Bd_KsPi0_noBeamBkgd_*10*.root']
+testSIG2 = [
+    '/gpfs/fs02/belle/users/pablog/mcGeneration_belle2/Bd_KsPi0_BGx0_2014_500k/root/EvtGenSimRec_Bd_KsPi0_noBeamBkgd_*20*.root']
+testCC = ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_K0Pi0_BGx0_MC35_2014/Bd_KsPi0/mdst/ccbar/mc35_ccbar_BGx0_s01.mdst.root']
+testSS = ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_K0Pi0_BGx0_MC35_2014/Bd_KsPi0/mdst/ssbar/mc35_ssbar_BGx0_s01.mdst.root']
+testDD = ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_K0Pi0_BGx0_MC35_2014/Bd_KsPi0/mdst/ddbar/mc35_ddbar_BGx0_s01.mdst.root']
+testUU = ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_K0Pi0_BGx0_MC35_2014/Bd_KsPi0/mdst/uubar/mc35_uubar_BGx0_s01.mdst.root']
+
+# Full skim of BGx0 MC 3.5    -    (1 hour to process)
+fullSIG = ['/gpfs/fs02/belle/users/pablog/mcGeneration_belle2/Bd_KsPi0_BGx0_2014_500k/root/*']
+fullCC = ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_K0Pi0_BGx0_MC35_2014/Bd_KsPi0/mdst/ccbar/*']
+fullSS = ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_K0Pi0_BGx0_MC35_2014/Bd_KsPi0/mdst/ssbar/*']
+fullDD = ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_K0Pi0_BGx0_MC35_2014/Bd_KsPi0/mdst/ddbar/*']
+fullUU = ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_K0Pi0_BGx0_MC35_2014/Bd_KsPi0/mdst/uubar/*']
+# _____________________________________________________________________________________________________________________________________
+
+
+if sampleSize == 'test':
+    inputMdstList(testSIG1 + testSIG2 + testCC + testSS + testDD + testUU)
+elif sampleSize == 'full':
+    inputMdstList(fullSIG + fullCC + fullSS + fullDD + fullUU)
+else:
+    sys.exit('sampleSize does not match any of the available samples: `test` or `full`')
+
 
 # Do the analysis
 fillParticleList('gamma:all', '')
-fillParticleList('pi+:good',
-                 'chiProb > 0.001 and piid > 0.5 and abs(dz) < 4.0 and abs(dr) < 0.2'
-                 )
-fillParticleList('pi-:good',
-                 'chiProb > 0.001 and piid > 0.5 and abs(dz) < 4.0 and abs(dr) < 0.2'
-                 )
+fillParticleList('pi+:good', 'chiProb > 0.001 and piid > 0.5 and abs(dz) < 4.0 and abs(dr) < 0.2')
+fillParticleList('pi-:good', 'chiProb > 0.001 and piid > 0.5 and abs(dz) < 4.0 and abs(dr) < 0.2')
 
 reconstructDecay('K_S0 -> pi+:good pi-:good', '0.480<=M<=0.516', 1)
 reconstructDecay('pi0  -> gamma:all gamma:all', '0.115<=M<=0.152', 1)
@@ -69,7 +98,8 @@ matchMCTruth('B0')
 buildRestOfEvent('B0')
 buildContinuumSuppression('B0')
 
-# Define the input variables.
+
+# Define the input variables for training.
 variables = [
     'R2',
     'cosTBTO',
@@ -100,26 +130,33 @@ variables = [
     'CleoCone(5)',
     'CleoCone(6)',
     'CleoCone(7)',
-    'CleoCone(8)',
-    ]
+    'CleoCone(8)'
+]
 
-# Define the methods.
-methods = [('FastBDT', 'Plugin',
-           'H:V:CreateMVAPdfs:NTrees=100:Shrinkage=0.10:RandRatio=0.5:NCutLevel=8:NTreeLayers=3'
-           ), ('NeuroBayes', 'Plugin',
-           'H:V:CreateMVAPdfs:NtrainingIter=20:Preprocessing=612:ShapeTreat=DIAG:TrainingMethod=BFGS'
-           )]
+
+# Define the methods
+methods = [
+    ('FastBDT', 'Plugin', 'H:V:CreateMVAPdfs:NTrees=100:Shrinkage=0.10:RandRatio=0.5:NCutLevel=8:NTreeLayers=3'),
+    ('NeuroBayes', 'Plugin', 'H:V:CreateMVAPdfs:NtrainingIter=20:Preprocessing=612:ShapeTreat=DIAG:TrainingMethod=BFGS')
+]
+
+
+# Create directory for TMVA teacher output
+outDirForTMVA = 'training_qq_' + sampleSize
+if not os.path.exists(outDirForTMVA):
+    os.makedirs(outDirForTMVA)
+
 
 # TMVA training/testing
-teacher = register_module('TMVATeacher')
+teacher = register_module('TMVAOnTheFlyTeacher')
 teacher.param('prefix', 'Bd_KsPi0_TMVA')
 teacher.param('methods', methods)
 teacher.param('variables', variables)
 teacher.param('target', 'isSignal')
 teacher.param('listNames', ['B0'])
-teacher.param('workingDirectory', 'training')
+teacher.param('workingDirectory', outDirForTMVA)
 analysis_main.add_module(teacher)
+
 
 process(analysis_main)
 print statistics
-

c46a3275b00b09a06b1c2ea17143656112ca22f6
Pablo Goldenzweig <pablo.goldenzweig@kit.edu>
2015-06-18 09:41:51 +0000

    An example of how to create a skim of continuum MC. Output is used as input for tutorials B2A702(3) to run TMVA training(expert).
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@18963 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/examples/tutorials/B2A701-ContinuumSuppressionExample.py
+++ analysis/examples/tutorials/B2A701-ContinuumSuppressionExample.py
@@ -1,138 +1,86 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#######################################################
+################################################################################
 #
 # This tutorial demonstrates how to perform a
-# TMVA training on continuum suppression variables
-# for B0 candidates in B0->PhiK*0 decays using the following
-# input variables:
-#  KSFW moments, thrust related quantities,
-#  R2, and CLEO Cones.
+# skim of continuum MC for B0 candidates in Bd->KsPi0 decays.
 #
-# 4 TMVA methods are used: Likelihood, BDT, FastBDT, and NBayes
+# One can then run the following 2 continuum suppression tutorials
+# (B2A702 and B2A703) over these skims, to apply TMVA training
+# and expert.
 #
-# Note: This example uses a privately produced skim of
-# the continuum MC sample created with no beam background in
-# MC campaign 3.5, therefore it can be run only on KEKCC computers.
+# Usage:
+#  -Command-line (for a single input file specified below):
+#    ./B2A701-ContinuumSuppressionExample.py mc35_ccbar_BGx0_s00.root mc35_ccbar_BGx0_s00.mdst.root
+#  -Queue (for the entire /mc35_ccbar_BGx0_s00/* directory):
+#    bsub -q l -e error_mc35_ccbar_BGx0_s00.log -o output_mc35_ccbar_BGx0_s00.log
+#     'basf2 ./rec_Bd_KsPi0.py mc35_ccbar_BGx0_s00.root mc35_ccbar_BGx0_s00.mdst.root
+#      -i "/hsm/belle2/bdata/MC/generic/ccbar/mcprod1405/BGx0/mc35_ccbar_BGx0_s00/*"'
+#
+# This will produce a flat ntuple along with a skimmed mdst.root file
 #
 # Contributors: P. Goldenzweig (July 2014)
 #
-######################################################
+################################################################################
 
+import os.path
+import sys
 from basf2 import *
-from modularAnalysis import inputMdst
-from modularAnalysis import reconstructDecay
-from modularAnalysis import matchMCTruth
-from modularAnalysis import analysis_main
-from modularAnalysis import ntupleFile
-from modularAnalysis import ntupleTree
-from stdFSParticles import goodPhoton
-
 from modularAnalysis import *
+from stdFSParticles import stdPi0
+from stdV0s import stdKshorts
 
-## FULL SKIM
-filelistSIG = \
-    ['/gpfs/fs02/belle/users/pablog/mcGeneration_belle2/Bd_PhiKst_noBeamBkgd/fL_1/root/*'
-     ]
-filelistCC = \
-    ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_PhiKst_noBeamBkgd_18July2014/mdst/ccbar/*'
-     ]
-filelistSS = \
-    ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_PhiKst_noBeamBkgd_18July2014/mdst/ssbar/*'
-     ]
-filelistDD = \
-    ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_PhiKst_noBeamBkgd_18July2014/mdst/ddbar/*'
-     ]
-filelistUU = \
-    ['/gpfs/fs02/belle/users/pablog/analysis/rec_Bd_PhiKst_noBeamBkgd_18July2014/mdst/uubar/*'
-     ]
-inputMdstList(filelistSIG + filelistCC + filelistSS + filelistDD + filelistUU)
 
-fillParticleList('K+',
-                 'chiProb > 0.001 and Kid  > 0.5 and -4.0 < dz < 4.0 and -0.2 < dr < 0.2'
-                 )
-fillParticleList('pi-',
-                 'chiProb > 0.001 and piid > 0.5 and -4.0 < dz < 4.0 and -0.2 < dr < 0.2'
-                 )
+outTuple = str(sys.argv[1])
+outSkim = str(sys.argv[2])
 
-reconstructDecay('K*0 -> K+ pi-', '0.75 < M < 1.25')
-reconstructDecay('phi -> K+ K-', '1.00 < M < 1.05')
+main = create_path()
 
-reconstructDecay('B0 -> K*0 phi', '5.20 < M < 5.40')
+inputMdstList(['/hsm/belle2/bdata/MC/generic/ccbar/mcprod1405/BGx0/mc35_ccbar_BGx0_s00/ccbar_e0001r0973_s00_BGx0.mdst.root'])
 
-applyCuts('B0', '-0.05 < deltaE < 0.05')
-applyCuts('B0', ' 5.24 < Mbc    < 5.29')
+# Do the analysis
+fillParticleList('gamma:all', '')
+fillParticleList('pi+:good', 'chiProb > 0.001 and piid > 0.5 and abs(dz) < 4.0 and abs(dr) < 0.2')
+fillParticleList('pi-:good', 'chiProb > 0.001 and piid > 0.5 and abs(dz) < 4.0 and abs(dr) < 0.2')
+
+reconstructDecay('K_S0 -> pi+:good pi-:good', '0.480<=M<=0.516', 1)
+reconstructDecay('pi0  -> gamma:all gamma:all', '0.115<=M<=0.152', 1)
+reconstructDecay('B0   -> K_S0 pi0', '5.2 < Mbc < 5.3 and -0.3 < deltaE < 0.2')
 
 matchMCTruth('B0')
 buildRestOfEvent('B0')
 buildContinuumSuppression('B0')
+TagV('B0', 'breco')
 
-# Define the input variables.
-variables = [
-    'R2',
-    'cosTBTO',
-    'KSFWVariables(k0hso02)',
-    'KSFWVariables(k0hso12)',
-    'cosTBz',
-    'CleoCone(9)',
-    'thrustBm',
-    'thrustOm',
-    'KSFWVariables(k0et)',
-    'KSFWVariables(k0mm2)',
-    'KSFWVariables(k0hso00)',
-    'KSFWVariables(k0hso04)',
-    'KSFWVariables(k0hso10)',
-    'KSFWVariables(k0hso14)',
-    'KSFWVariables(k0hso20)',
-    'KSFWVariables(k0hso22)',
-    'KSFWVariables(k0hso24)',
-    'KSFWVariables(k0hoo0)',
-    'KSFWVariables(k0hoo1)',
-    'KSFWVariables(k0hoo2)',
-    'KSFWVariables(k0hoo3)',
-    'KSFWVariables(k0hoo4)',
-    'CleoCone(1)',
-    'CleoCone(2)',
-    'CleoCone(3)',
-    'CleoCone(4)',
-    'CleoCone(5)',
-    'CleoCone(6)',
-    'CleoCone(7)',
-    'CleoCone(8)',
-    ]
 
-# Define the methods.
-# In this case Likelihood, BDT, FastBDT, and NBayes are defined.
-# The last arguments are TMVA options.
-# Refer to TMVA manual for instructions.
-methods = [('LPCA', 'Likelihood',
-           'H:V:!TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmooth=5:NAvEvtPerBin=50:VarTransform=PCA'
-           ), ('BDTG', 'BDT',
-           'H:V:CreateMVAPdfs:NTrees=100:Shrinkage=0.10:UseBaggedBoost:GradBaggingFraction=0.5:nCuts=256:MaxDepth=3:BoostType=Grad'
-           ), ('FastBDT', 'Plugin',
-           'H:V:CreateMVAPdfs:NTrees=100:Shrinkage=0.10:RandRatio=0.5:NCutLevel=8:NTreeLayers=3'
-           ), ('NeuroBayes', 'Plugin',
-           'H:V:CreateMVAPdfs:NtrainingIter=20:Preprocessing=612:ShapeTreat=DIAG:TrainingMethod=BFGS'
-           )]
-               # fei match
-               # fei
-               # ov
+toolsB = ['EventMetaData', '^B0']
+toolsB += ['RecoStats', '^B0']
+toolsB += ['DeltaEMbc', '^B0']
+toolsB += ['ContinuumSuppression', '^B0']
+toolsB += ['TagVertex', '^B0']
+toolsB += ['MCTagVertex', '^B0']
+toolsB += ['DeltaT', '^B0']
+toolsB += ['MCDeltaT', '^B0']
+toolsB += ['InvMass[BeforeFit]', '^B0 -> [ ^K_S0 -> ^pi+:good ^pi-:good ]   ^pi0                             ']
+toolsB += ['PID', ' B0 -> [  K_S0 -> ^pi+:good ^pi-:good ]    pi0                             ']
+toolsB += ['Kinematics', '^B0 -> [ ^K_S0 -> ^pi+:good ^pi-:good ] [ ^pi0 -> ^gamma:all ^gamma:all ]']
+toolsB += ['MCTruth', '^B0 -> [ ^K_S0 -> ^pi+:good ^pi-:good ] [ ^pi0 -> ^gamma:all ^gamma:all ]']
+toolsB += ['MCHierarchy', '^B0 -> [ ^K_S0 -> ^pi+:good ^pi-:good ] [ ^pi0 -> ^gamma:all ^gamma:all ]']
 
-# At the moment the python wrapper function for the TMVATeacher module
-# does not exist, therefore we have to register the module by ourselves
-# and set all module parameters.
-teacher = register_module('TMVATeacher')
-teacher.param('prefix', 'Bd_PhiKst_TMVA')
-teacher.param('methods', methods)
-teacher.param('variables', variables)
-teacher.param('target', 'isSignal')
-teacher.param('listNames', ['B0'])
-teacher.param('workingDirectory',
-              '/home/belle/pablog/work/belle2/basf2/analysis/examples/Bd_PhiKst/training'
-              )
-analysis_main.add_module(teacher)
+
+# Write flat ntuples
+ntupleFile(outTuple)
+ntupleTree('ntupB0', 'B0', toolsB)
+
+# Write skim mdst files
+skimMdst = register_module('SkimFilter')
+skimMdst.param('particleLists', ['B0'])
+analysis_main.add_module(skimMdst)
+
+save_path = create_path()
+skimMdst.if_true(save_path)
+outputMdst(outSkim, path=save_path)
 
 process(analysis_main)
 print statistics
-

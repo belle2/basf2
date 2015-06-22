5718e881b7865aa5f70bd5d72472ba85898a2a46
Martin Ritter <martin.ritter@lmu.de>
2015-06-22 07:36:14 +0000

    Add generator for initial beam particles
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19039 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ generators/utilities/include/InitialParticleGeneration.h
@@ -0,0 +1,74 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Martin Ritter                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+#ifndef GENERATORS_UTILITIES_INITIALPARTICLEGENERATION_H
+#define GENERATORS_UTILITIES_INITIALPARTICLEGENERATION_H
+
+#include <framework/datastore/StoreObjPtr.h>
+//#include <framework/database/DBObjPtr.h>
+#include <framework/utilities/MultivariateNormalGenerator.h>
+#include <framework/dataobjects/BeamParameters.h>
+#include <framework/dataobjects/MCInitialParticles.h>
+
+namespace Belle2 {
+
+  /** Generate Collision.
+   * This class is meant to be a common interface to be used by physics
+   * generators for generating the initial beam collision.
+   *
+   * A generator module should have this class as a member. In each event, a call
+   * to generate() will return the initial event information and also
+   * automatically put it in the datastore.
+   */
+  class InitialParticleGeneration {
+  public:
+    /** constructor
+     * @param allowedFlags allowed generation flags from MCInitialParticles.
+     *        Default is to allow no smearing.
+     */
+    InitialParticleGeneration(int allowedFlags = 0): m_allowedFlags(allowedFlags) {};
+
+    /** Generate a new event */
+    MCInitialParticles& generate();
+
+    /** function to be executed on initialize() */
+    void initialize();
+
+  private:
+    /** generate the vertex
+     * @param initial nominal vertex position
+     * @param cov covariance of the vertex position
+     * @param gen multivariate normal generator to be used
+     */
+    TVector3 generateVertex(const TVector3& initial, const TMatrixDSym& cov, MultivariateNormalGenerator& gen);
+    /** generate 4 vector for one beam
+     * @param initial beam
+     * @param cov covariance of the beam momentum (E, theta_x, theta_y)
+     * @param gen multivariate normal generator to be used
+     */
+    TLorentzVector generateBeam(const TLorentzVector& initial, const TMatrixDSym& cov, MultivariateNormalGenerator& gen);
+    /** Datastore object containing the nominal beam parameters */
+    StoreObjPtr<BeamParameters> m_beamParams{"", DataStore::c_Persistent};
+    /** Datastore object containing the generated event */
+    StoreObjPtr<MCInitialParticles> m_event;
+    /** Generator for HER */
+    MultivariateNormalGenerator m_generateHER;
+    /** Generator for LER */
+    MultivariateNormalGenerator m_generateLER;
+    /** Generator for Vertex */
+    MultivariateNormalGenerator m_generateVertex;
+    /** Allowed generation flags */
+    int m_allowedFlags;
+  };
+
+
+} //Belle2 namespace
+#endif // GENERATORS_UTILITIES_INITIALPARTICLEGENERATION_H
---
+++ generators/utilities/src/InitialParticleGeneration.cc
@@ -0,0 +1,96 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Martin Ritter                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <generators/utilities/InitialParticleGeneration.h>
+#include <framework/gearbox/Const.h>
+
+namespace Belle2 {
+
+  void InitialParticleGeneration::initialize()
+  {
+    m_beamParams.required("", DataStore::c_Persistent);
+    m_event.registerInDataStore();
+  }
+
+  TVector3 InitialParticleGeneration::generateVertex(const TVector3& initial, const TMatrixDSym& cov,
+                                                     MultivariateNormalGenerator& gen)
+  {
+    if (m_event->hasGenerationFlags(BeamParameters::c_smearVertex)) {
+      if (!gen.size()) gen.setMeanCov(initial, cov);
+      return gen.generateVec3();
+    }
+    return initial;
+  }
+
+  TLorentzVector InitialParticleGeneration::generateBeam(const TLorentzVector& initial, const TMatrixDSym& cov,
+                                                         MultivariateNormalGenerator& gen)
+  {
+    if (!(m_event->getGenerationFlags() & BeamParameters::c_smearBeam)) {
+      //no smearing, nothing to do
+      return initial;
+    }
+    if (gen.size() != 3) gen.setMeanCov(TVector3(initial.E(), 0, 0), cov);
+    // generate the beam parameters: energy and horizontal angle and vertical
+    // angle with respect to nominal direction
+    // p[0] = energy
+    // p[1] = horizontal angle
+    // p[2] = vertical angle
+    Eigen::VectorXd p = gen.generate();
+    // if we don't smear beam energy set it to nominal values
+    if (!m_event->hasGenerationFlags(BeamParameters::c_smearBeamEnergy)) {
+      p[0] = initial.E();
+    }
+    // if we don't smear beam direction set angles to 0
+    if (!m_event->hasGenerationFlags(BeamParameters::c_smearBeamDirection)) {
+      p[1] = 0;
+      p[2] = 0;
+    }
+    // calculate TLorentzvector from p
+    const double pz = std::sqrt(p[0] * p[0] - Const::electronMass * Const::electronMass);
+    // Rotate around theta_x and theta_y. Same as result.RotateX(p[1]);
+    // result.RotateY(p[2]) but as we know it's a 0,0,pz vector this can be
+    // optimized.
+    const double sx = sin(p[1]);
+    const double cx = cos(p[1]);
+    const double sy = sin(p[2]);
+    const double cy = cos(p[2]);
+    const double px = sy * cx * pz;
+    const double py = -sx * pz;
+    TLorentzVector result(px, py, cx * cy * pz, p[0]);
+    // And rotate into the direction of the nominal beam
+    result.RotateY(initial.Theta());
+    return result;
+  }
+
+  MCInitialParticles& InitialParticleGeneration::generate()
+  {
+    if (!m_event) {
+      m_event.create();
+    }
+    /*if(m_beamParams.hasChanged()){
+      m_generateHER.reset();
+      m_generateLER.reset();
+      m_generateVertex.reset();
+    }*/
+    m_event->setGenerationFlags(m_beamParams->getGenerationFlags() & m_allowedFlags);
+    TLorentzVector her = generateBeam(m_beamParams->getHER(), m_beamParams->getCovHER(), m_generateHER);
+    TLorentzVector ler = generateBeam(m_beamParams->getLER(), m_beamParams->getCovLER(), m_generateLER);
+    TVector3 vtx = generateVertex(m_beamParams->getVertex(), m_beamParams->getCovVertex(), m_generateVertex);
+    m_event->set(her, ler, vtx);
+    //Check if we want to go to CMS, if so boost both
+    if (m_beamParams->hasGenerationFlags(BeamParameters::c_generateCMS)) {
+      her = m_event->getLabToCMS() * her;
+      ler = m_event->getLabToCMS() * ler;
+      m_event->set(her, ler, vtx);
+    }
+    return *m_event;
+  }
+
+} //Belle2 namespace

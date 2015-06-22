df94b375d4f323b7761f4b8bd5bf27408849ceb7
Martin Ritter <martin.ritter@lmu.de>
2015-06-22 07:36:05 +0000

    Change MultivariateNormalGenerator to use LDLT transformation and add some setters/getters
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19037 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/utilities/include/MultivariateNormalGenerator.h
+++ framework/utilities/include/MultivariateNormalGenerator.h
@@ -15,11 +15,39 @@
 #include <Eigen/Dense>
 #include <TRandom.h>
 #include <TVectorT.h>
+#include <TVector3.h>
 #include <TMatrixTBase.h>
 
 namespace Belle2 {
   /** Class to generate normal distributed, correlated random numbers given the
    * mean values and the covariance matrix of all dimensions.
+   *
+   * This class can be used to generate normal distributed random values
+   * according to a given covariance matrix (assuming the covariance matrix is
+   * positive semi-definite).
+   *
+   * To use it first set the desired mean values and covariance matrix using
+   * setMeanCov() and then call generate() to generate one set of values.
+   *
+   * \warning: setMeanCov() will not work for all matrices. Please check the
+   * return value when setting the covariance matrix.
+   *
+   * To generate normal distributed random values according to a covariance
+   * matrix we need to decompose the covariance matrix M into M= A A^T. Given
+   * the vector of mean values as \mu and a vector of standard normal
+   * distributed random values (\mu=0, \sigma=1) as n we can obtain a set of
+   * correlated random values x = \mu + A * n.
+   *
+   * Usually the Cholesky decomposition is chosen as it computes M = L L^T.
+   * However the Cholesky composition only works for positive definite matrices
+   * so it does not work if for example one of the values is fixed and has no
+   * error.
+   *
+   * To ease this restriction a little we use the LDLT decomposition given as
+   * M = P^T L D L^T P where P is a permutation matrix and D a diagonal matrix.
+   * We then can use A = P^T L \sqrt(D) to caluclate the correlated values also
+   * for positive semi-definite covariance matrices if the elements of D are
+   * positive.
    */
   class MultivariateNormalGenerator {
   public:
@@ -50,15 +78,20 @@ namespace Belle2 {
      */
     Eigen::VectorXd generate() const
     {
-      //To get the correlated multivariate normal distribution, we
-      //multiply standard normal distributed values (mean=0, sigma=1)
-      //with the cholesky decomposition and add the mean values. Since we
-      //know the Cholesky to be a lower triangle matrix we use the
-      //triangularView to tell Eigen to use optimized operations.
+      //To get the correlated multivariate normal distribution, we multiply
+      //standard normal distributed values (mean=0, sigma=1) with a
+      //transformation matrix we obtained from an LDLT decomposition and add
+      //the mean values.
       Eigen::VectorXd x(m_mean.rows());
       for (int i = 0; i < m_mean.rows(); ++i) x(i) = gRandom->Gaus();
-      return m_mean + (m_cholesky.triangularView<Eigen::Lower>() * x);
+      return m_mean + (m_transform * x);
     }
+    /** reset the generator setting the size to 0. Subsequent calls to generate
+     * will return 0-sized results until the generator is reinitialized using
+     * setMeanCov() */
+    void reset();
+    /** Return the number of elements to be generated on generate() */
+    size_t size() const { return m_mean.rows(); }
     /** Generate a set of correlated random numbers with the previouly set
      * mean and covariance and store them in buffer output.
      * @param output pointer to array where generated values will be stored.
@@ -66,53 +99,102 @@ namespace Belle2 {
     void generate(double* output) const
     {
       Eigen::VectorXd x = generate();
-      for (int i = 0; i < x.rows(); ++i) { output[i] = x(i); }
+      for (unsigned int i = 0; i < x.rows(); ++i) { output[i] = x(i); }
+    }
+
+    /** Generate a set of correlated random numbers with the previouly set
+     * mean and covariance and return a TVector3. Optimally, the set mean and
+     * covariance matrix should be of dimension three, otherwise just the first
+     * size() elements of the TVector3 are set and the remaining elements are
+     * zero. If size() is bigger than 3 the remaining values will be discarded.
+     */
+    TVector3 generateVec3()
+    {
+      Eigen::VectorXd x = generate();
+      TVector3 output(0, 0, 0);
+      for (unsigned int i = 0; i < std::min(3u, (unsigned int)size()); ++i) {
+        output[i] = x(i);
+      }
+      return output;
+    }
+
+    /** Generate a set of correlated random numbers with the previouly set
+     * mean and covariance and return a TVectorT<double>
+     */
+    TVectorD generateVecT()
+    {
+      Eigen::VectorXd x = generate();
+      TVectorD output(x.rows());
+      output.SetElements(x.data());
+      return output;
     }
 
     /** set the mean and covariance for the distribution with array
      * interface: mean and covariance are passed as double arrays where the
      * covariance is expected to be an NxN matrix in row major layout
+     *
      * @param n dimensionality
      * @param mean pointer to the n mean values of the distribution
      * @param cov pointer to the n*n covariance values in row major layout
+     * @return true if covariance could be decomposited, false otherwise
      */
-    void setMeanCov(int n, const double* mean, const double* cov);
+    bool setMeanCov(int n, const double* mean, const double* cov);
 
     /** set the mean and covariance for the distribution.
      * @param mean Vector of mean values
      * @param cov Matrix containing the covariance values
+     * @return true if covariance could be decomposited, false otherwise
      */
-    void setMeanCov(const Eigen::VectorXd& mean, const Eigen::MatrixXd& cov);
+    bool setMeanCov(const Eigen::VectorXd& mean, const Eigen::MatrixXd& cov);
 
     /** set mean and covariance matrix from ROOT vector/matrix objects, e.g.
      * TMatrixD, TMatrixF, TMatrixDSym and so forth
      * @param mean Vector of mean values
      * @param cov Matrix containing the covariance values
+     * @return true if covariance could be decomposited, false otherwise
      */
-    template<class value_type> void setMeanCov(const TVectorT<value_type>& mean,
+    template<class value_type> bool setMeanCov(const TVectorT<value_type>& mean,
+                                               const TMatrixTBase<value_type>& cov);
+
+    /** set mean and covariance matrix from ROOT vector/matrix objects, e.g.
+     * TMatrixD, TMatrixF, TMatrixDSym and so forth but with exactly three dimensions
+     * @param mean Vector of mean values
+     * @param cov Matrix containing the covariance values
+     * @return true if covariance could be decomposited, false otherwise
+     */
+    template<class value_type> bool setMeanCov(const TVector3& mean,
                                                const TMatrixTBase<value_type>& cov);
   private:
     /** Member to store the mean values of the distribution */
     Eigen::VectorXd m_mean;
-    /** Member to store the lower triangle of the Cholesky decomposition of
-     * the covariance matrix */
-    Eigen::MatrixXd m_cholesky;
+    /** Member to store the transformation matrix for standard normal
+     * distributed random values */
+    Eigen::MatrixXd m_transform;
   };
 
-  template<class value_type> void MultivariateNormalGenerator::setMeanCov(
+  template<class value_type> bool MultivariateNormalGenerator::setMeanCov(
     const TVectorT<value_type>& mean, const TMatrixTBase<value_type>& cov)
   {
-    Eigen::VectorXd emean(mean.getNrows());
-    Eigen::MatrixXd ecov(cov.getNrows(), cov.getcols());
-    for (int i = 0; i < mean.getNrows(); ++i) { emean(i) = mean(i); }
-    for (int i = 0; i < cov.getNrows(); ++i) {
-      for (int j = 0; j < cov.getNcols(); ++j) {
+    Eigen::VectorXd emean(mean.GetNrows());
+    Eigen::MatrixXd ecov(cov.GetNrows(), cov.GetNcols());
+    for (int i = 0; i < mean.GetNrows(); ++i) { emean(i) = mean(i); }
+    for (int i = 0; i < cov.GetNrows(); ++i) {
+      for (int j = 0; j < cov.GetNcols(); ++j) {
         ecov(i, j) = cov(i, j);
       }
     }
-    setMeanCov(emean, ecov);
+    return setMeanCov(emean, ecov);
   }
 
+  template<class value_type> bool MultivariateNormalGenerator::setMeanCov(
+    const TVector3& mean, const TMatrixTBase<value_type>& cov)
+  {
+    TVectorT<value_type> tmean(3);
+    for (int i = 0; i < 3; ++i) {
+      tmean[i] = mean[i];
+    }
+    return setMeanCov(tmean, cov);
+  }
 }
 
 #endif // FRAMEWORK_UTILITIES_MULTIVARIATENORMALGENERATOR_H
--- framework/utilities/src/MultivariateNormalGenerator.cc
+++ framework/utilities/src/MultivariateNormalGenerator.cc
@@ -10,11 +10,17 @@
 
 #include <framework/utilities/MultivariateNormalGenerator.h>
 #include <framework/logging/Logger.h>
-#include <Eigen/Cholesky>
+#include <iostream>
 
 using namespace Belle2;
 
-void MultivariateNormalGenerator::setMeanCov(int n, const double* mean, const double* cov)
+void MultivariateNormalGenerator::reset()
+{
+  m_mean.resize(0);
+  m_transform.resize(0, 0);
+}
+
+bool MultivariateNormalGenerator::setMeanCov(int n, const double* mean, const double* cov)
 {
   Eigen::VectorXd emean(n);
   Eigen::MatrixXd ecov(n, n);
@@ -26,31 +32,47 @@ void MultivariateNormalGenerator::setMeanCov(int n, const double* mean, const do
     }
   }
   //And delegate to the correct function
-  setMeanCov(emean, ecov);
+  return setMeanCov(emean, ecov);
 }
 
 
-void MultivariateNormalGenerator::setMeanCov(const Eigen::VectorXd& mean, const Eigen::MatrixXd& cov)
+bool MultivariateNormalGenerator::setMeanCov(const Eigen::VectorXd& mean, const Eigen::MatrixXd& cov)
 {
-  m_mean.resize(0);
-  m_cholesky.resize(0, 0);
+  reset();
   if (mean.rows() != cov.rows()) {
     B2ERROR("Mean values and covariance matrix need to be of the same dimension");
-    return;
+    return false;
   }
   if (cov.rows() != cov.cols()) {
     B2ERROR("Covariance matrix needs to be a square matrix");
-    return;
+    return false;
   }
-  //Retrieve Cholesky decomposition and save it if possible
-  Eigen::LLT<Eigen::MatrixXd> choleskyDecomposition(cov);
-  if (choleskyDecomposition.info() != Eigen::Success) {
-    B2ERROR("Cannot compute Cholesky decomposition of covariance "
-            "matrix, maybe not positive definite?")
-    return;
+
+  // The usual way to calulate multivariate normal distributed values is to use
+  // the cholesky decomposition C = ll' and then calculate y = mean + l * x
+  // where x is a vector of standard normal distributed values. However this
+  // only works for positive definite matrices so we use the LDLT composition
+  // which gives us C = P'LDL'P and we compute L=P'Lsqrt(D) to get the same
+  // result but in a more robust way which works also for semi-definite
+  // matrices
+  auto ldlt = cov.ldlt();
+  if (ldlt.info() != Eigen::Success) {
+    B2ERROR("Cannot compute LDLT decomposition of covariance "
+            "matrix, maybe not positive semi-definite?")
+    return false;
   }
-  m_cholesky = choleskyDecomposition.matrixL();
+  Eigen::MatrixXd L = ldlt.matrixL();
+  Eigen::MatrixXd D = ldlt.vectorD().asDiagonal();
+  if (D.minCoeff() < 0) {
+    B2ERROR("MultivariateNormalGenerator: Negative values when computing LDL^T "
+            "decomposition, cannot compute M=AA^T, resulting random numbers "
+            "will not be correct");
+    return false;
+  }
+  auto P = ldlt.transpositionsP().transpose();
+  m_transform = P * L * D.cwiseSqrt();
   //And save the mean values
   m_mean = mean;
+  return true;
 }
 

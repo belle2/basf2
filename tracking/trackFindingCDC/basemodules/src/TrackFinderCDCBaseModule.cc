b4ae9b8fd81e53e7fc1585ea8ccd669b3d96375e
Nils Braun <nils.braun@student.kit.edu>
2015-07-03 10:12:57 +0000

    Resort the hits in the end
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19300 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/basemodules/src/TrackFinderCDCBaseModule.cc
+++ tracking/trackFindingCDC/basemodules/src/TrackFinderCDCBaseModule.cc
@@ -268,7 +268,21 @@ void TrackFinderCDCBaseModule::event()
     // Create the gftracks
     StoreArray<genfit::TrackCand> storedGFTrackCands(m_param_gfTrackCandsStoreArrayName);
     storedGFTrackCands.create();
-    for (const CDCTrack& track : outputTracks) {
+    for (CDCTrack& track : outputTracks) {
+      // Postprocess track
+      // 1. Recalculate the reconstructed position of all hits
+      const CDCTrajectory3D& trajectory3D = track.getStartTrajectory3D();
+      const CDCTrajectory2D& trajectory2D = trajectory3D.getTrajectory2D();
+      for (CDCRecoHit3D& recoHit : track) {
+        // TODO: Also change the position of stereo hits?
+        double reconstructedPerpS = trajectory2D.calcPerpS(recoHit.getRecoPos2D());
+        recoHit.setPerpS(reconstructedPerpS);
+      }
+
+      // 2. Sort the hits according to their reconstructed perpS
+      track.sort();
+
+
       genfit::TrackCand gfTrackCand;
       if (track.fillInto(gfTrackCand)) {
         storedGFTrackCands.appendNew(gfTrackCand);

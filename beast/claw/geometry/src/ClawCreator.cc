9598f9e29f5fa33fdeb0deca9b320b5851427d74
Igal Jaegle <igjaegle@gmail.com>
2015-07-03 22:04:15 +0000

    phase 1 claw: update geometry
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19323 b38c6c8f-bee8-465d-8448-9abe6915d496

--- beast/claw/geometry/src/ClawCreator.cc
+++ beast/claw/geometry/src/ClawCreator.cc
@@ -113,7 +113,7 @@ namespace Belle2 {
 
         G4double r = activeParams.getLength("r_claw") * CLHEP::cm;
         G4double z = activeParams.getLength("z_claw") * CLHEP::cm;
-        G4double phi = activeParams.getAngle("Phi") + 180. * CLHEP::deg;
+        G4double phi = activeParams.getAngle("Phi");
         G4double thetaZ = activeParams.getAngle("ThetaZ");
         G4double dx_board = activeParams.getLength("dx_board") / 2.*CLHEP::cm;
         G4double dy_board = activeParams.getLength("dy_board") / 2.*CLHEP::cm;
@@ -126,58 +126,69 @@ namespace Belle2 {
         G4double dx_Al = dx_scint + Al_width;
         G4double dy_Al = dy_scint + Al_width;
         G4double dz_Al = dz_scint + Al_width;
+        G4double dx_air = dx_Al;
+        G4double dy_air = dy_Al * 8.;
+        G4double dz_air = dz_board + 2. * dz_Culayer + dz_Al;
+        //cout << " dx sol 1 " << dx_air << " sol 2 " << dx_board << endl;
+        //cout << " dy sol 1 " << dy_air << " sol 2 " << dy_board << endl;
 
-        /*
         //create air volume engloging ladder + scintillator + Al foil
-        G4Box* s_air = new G4Box("s_air",dx_board,dy_board,dz_board+dz_Culayer+dz_Al);
-
-        G4LogicalVolume* l_air = new G4LogicalVolume(s_air, geometry::Materials::get("G4_Air"), "l_air");
-
-        G4Transform3D transform = G4RotateZ3D(phi) * G4Translate3D(0, r, z) * G4RotateX3D(-M_PI / 2 - thetaZ);
+        G4Box* s_air = new G4Box("s_air", dx_air, dy_air, dz_air);
+        G4LogicalVolume* l_air = new G4LogicalVolume(s_air, geometry::Materials::get("G4_AIR"), "l_air");
+        G4Transform3D transform = G4RotateZ3D(-M_PI / 2 + phi) * G4Translate3D(0, r, z) * G4RotateX3D(-M_PI / 2 - thetaZ);
         new G4PVPlacement(transform, l_air, "p_air", &topVolume, false, 1);
-        */
+
         //create claw G10 board
         G4Box* s_board = new G4Box("s_board", dx_board, dy_board, dz_board);
-
         G4LogicalVolume* l_board = new G4LogicalVolume(s_board, geometry::Materials::get("G10"), "l_board");
         l_board->SetVisAttributes(green);
-
-        G4Transform3D transform = G4RotateZ3D(phi) * G4Translate3D(0, r, z) * G4RotateX3D(-M_PI / 2 - thetaZ);
-        new G4PVPlacement(transform, l_board, "p_board", &topVolume, false, 1);
+        //G4Transform3D transform = G4RotateZ3D(phi - M_PI / 2) * G4Translate3D(0, r, z) * G4RotateX3D(-M_PI / 2 - thetaZ);
+        //new G4PVPlacement(transform, l_board, "p_board", &topVolume, false, 1);
+        //G4double r_board = -(dz_board + 2. * dz_Culayer + dz_scint + Al_width) / 2. + 2. * dz_Culayer + dz_board;
+        G4double r_board = dz_air - 2. * dz_Culayer - dz_board;
+        new G4PVPlacement(0, G4ThreeVector(0, 0, r_board), l_board, "p_board", l_air, false, 1);
 
         //create copper layer on each side of the G10 board
         G4Box* s_Culayer = new G4Box("s_Culayer", dx_board, dy_board, dz_Culayer);
-
         G4LogicalVolume* l_Culayer = new G4LogicalVolume(s_Culayer, geometry::Materials::get("MetalCopper"), "l_Culayer");
         l_Culayer->SetVisAttributes(coppercolor);
-        //transform = G4Translate3D(0, 0, 0);
-        transform = G4RotateZ3D(phi) * G4Translate3D(0, r + dz_board + dz_Culayer, z) * G4RotateX3D(-M_PI / 2 - thetaZ);
+        /*
+        transform = G4RotateZ3D(phi - M_PI / 2) * G4Translate3D(0, r + dz_board + dz_Culayer, z) * G4RotateX3D(-M_PI / 2 - thetaZ);
         new G4PVPlacement(transform, l_Culayer, "p_Culayer_1", &topVolume, false, 1);
-        //transform = G4Translate3D(0, 0, 0);
-        transform = G4RotateZ3D(phi) * G4Translate3D(0, r - dz_board - dz_Culayer, z) * G4RotateX3D(-M_PI / 2 - thetaZ);
+        transform = G4RotateZ3D(phi - M_PI / 2) * G4Translate3D(0, r - dz_board - dz_Culayer, z) * G4RotateX3D(-M_PI / 2 - thetaZ);
         new G4PVPlacement(transform, l_Culayer, "p_Culayer_2", &topVolume, false, 1);
+        */
+        G4double r_Culayer_bot = r_board - dz_board - dz_Culayer;
+        G4double r_Culayer_top = r_board + dz_board + dz_Culayer;
+        //cout << " cu bot sol 1 " << r_Culayer_bot << " sol 2 " << -dz_air + dz_Culayer<< endl;
+        new G4PVPlacement(0, G4ThreeVector(0, 0, r_Culayer_bot), l_Culayer, "p_Culayer_bot", l_air, false, 1);
+        new G4PVPlacement(0, G4ThreeVector(0, 0, r_Culayer_top), l_Culayer, "p_Culayer_top", l_air, false, 1);
 
         //create scintillator and Al foil around it
         G4VSolid* s_scint = new G4Box("s_scint", dx_scint, dy_scint, dz_scint);
         G4VSolid* s_Al = new G4Box("s_Al", dx_Al, dy_Al, dz_Al);
         s_Al = new G4SubtractionSolid("s_Al", s_Al, s_scint, 0, G4ThreeVector(0, 0, 0));
-
         G4LogicalVolume* l_Al = new G4LogicalVolume(s_Al, geometry::Materials::get("Aluminum"), "l_Al");
         l_Al->SetVisAttributes(gray);
-
-
 
         G4LogicalVolume* l_scint = new G4LogicalVolume(s_scint, geometry::Materials::get("G4_POLYSTYRENE"), "l_scint", 0, m_sensitive);
         l_scint->SetVisAttributes(red);
         //Lets limit the Geant4 stepsize inside the volume
         l_scint->SetUserLimits(new G4UserLimits(stepSize));
-        double z_0 = z - dy_board + dy_scint;
+
+        G4double r_Al = r_Culayer_bot - dz_Culayer - dz_Al;
+        //cout << " dz_air " << dz_air << " x2 " << 2. * dz_air << " r_Al " << r_Al << " tot " << r_Al + dz_Al << " sol3 " << r_Culayer_top + dz_Culayer + dz_Al << endl;
+        double z_0 = -dy_air + dy_Al;
 
         for (int i = 0; i < 8; i++) {
-          double i_z = z_0 + i * 2. * (dy_scint + Al_width);
-          transform = G4RotateZ3D(phi) * G4Translate3D(0, r - dz_board - 2.*dz_Culayer - dz_Al, i_z) * G4RotateX3D(-M_PI / 2 - thetaZ);
+          double i_z = z_0 + i * 2. * dy_Al;
+          /*
+          transform = G4RotateZ3D(phi - M_PI / 2) * G4Translate3D(0, r - dz_board - 2.*dz_Culayer - dz_Al, i_z) * G4RotateX3D(-M_PI / 2 - thetaZ);
           new G4PVPlacement(transform, l_Al, "p_Al", &topVolume, false, 1);
           new G4PVPlacement(transform, l_scint, "p_scint", &topVolume, false, detID);
+          */
+          new G4PVPlacement(0, G4ThreeVector(0, i_z, r_Al), l_Al, "p_Al", l_air, false, 1);
+          new G4PVPlacement(0, G4ThreeVector(0, i_z, r_Al), l_scint, "p_scint", l_air, false, detID);
           detID++;
         }
       }

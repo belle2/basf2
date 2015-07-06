30ac95cea107f0dfcb22e5f2ca4c071bb557d19c
Markus Prim <markus.prim@kit.edu>
2015-07-06 08:35:43 +0000

    UnitTests for ECLHit
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19330 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ ecl/tests/eclHit.cc
@@ -0,0 +1,55 @@
+#include <ecl/dataobjects/ECLHit.h>
+#include <framework/gearbox/Const.h>
+#include <framework/logging/Logger.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/StoreObjPtr.h>
+
+#include <utility>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+namespace Belle2 {
+
+  /** Set up a few arrays and objects in the datastore */
+  class ECLHitTest : public ::testing::Test {
+  };
+
+  /** Test Constructors. */
+  TEST_F(ECLHitTest, Constructors)
+  {
+    ECLHit myHit;
+    EXPECT_EQ(myHit.getCellId(), 0);
+    EXPECT_EQ(myHit.getEnergyDep(), 0);
+    EXPECT_EQ(myHit.getTimeAve(), 0);
+
+    const int cellId = 1;
+    const float edep = 1.234;
+    const float timeAve = 4.321;
+    ECLHit myHit2(cellId, edep, timeAve);
+    EXPECT_EQ(myHit2.getCellId(), cellId);
+    EXPECT_EQ(myHit2.getEnergyDep(), edep);
+    EXPECT_EQ(myHit2.getTimeAve(), timeAve);
+  } // Testcases for Something
+
+  /** Test Setters and Getters. */
+  TEST_F(ECLHitTest, SettersAndGetters)
+  {
+    const int cellId = 1;
+    const float edep = 1.234;
+    const float timeAve = 4.321;
+    const float deltaTimeShift = 2.5;
+    ECLHit myHit;
+    myHit.setCellId(cellId);
+    myHit.setEnergyDep(edep);
+    myHit.setTimeAve(timeAve);
+    EXPECT_EQ(myHit.getCellId(), cellId);
+    EXPECT_EQ(myHit.getEnergyDep(), edep);
+    EXPECT_EQ(myHit.getTimeAve(), timeAve);
+    myHit.shiftInTime(deltaTimeShift);
+    EXPECT_EQ(myHit.getTimeAve(), timeAve + deltaTimeShift);
+
+  } // Testcases for Setters and Getters
+
+}  // namespace

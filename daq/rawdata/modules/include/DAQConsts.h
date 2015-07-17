617193ed291f193812e269c468114e20e27fe11d
Satoru Yamada <satoru.yamada@kek.jp>
2015-07-17 09:09:19 +0000

    Declear g_run_** only in NONSTOP scheme
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19688 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/rawdata/modules/include/DAQConsts.h
+++ daq/rawdata/modules/include/DAQConsts.h
@@ -28,10 +28,12 @@
 
 // global variables for non-stop DAQ
 
+#ifdef NONSTOP
 extern int g_run_restarting;
 extern int g_run_stop;
 extern int g_run_recovery;
 extern int g_run_error;
+#endif
 
 const int sp8test_run_starttime[93] = {
   -1, 1381029053

6a64b6438e4b68d81b01feaa790b82bd5d655ff0
Giacomo Caria <gcaria@student.unimelb.edu.au>
2015-07-21 05:10:37 +0000

    New version of steering file for updated validation modules
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19811 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ svd/validation/SVDValidationSteering.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#####################################################################
+#
+# History:
+# 21 Jul 2015, Commit
+#
+#####################################################################
+
+
+"""
+<header>
+  <output>SVDValidationOutput.root</output>
+  <contact>G. Caria, gcaria@student.unimelb.edu.au</contact>
+  <description>This is the SVD validation steering file.</description>
+</header>
+"""
+
+from basf2 import *
+# Individual validation packages
+from SVDValidationTTree import *
+from SVDValidationTTreeDigit import *
+from SVDValidationTTreeSimhit import *
+from SVDValidationTTreeEfficiency import *
+
+set_random_seed(12345)
+
+# Particle gun module
+# particlegun = register_module('ParticleGun')
+# particlegun.param({
+#    'nTracks': 1,
+#    'varyNTracks': False,
+#    'pdgCodes': [13, -13],
+#    'momentumGeneration': 'normal',
+#    'momentumParams': [3, 1],
+#    'phiGeneration': 'uniform',
+#    'phiParams': [0, 360],
+#    'thetaGeneration': 'uniformCos',
+#    'thetaParams': [15, 150],
+#    'vertexGeneration': 'normal',
+#    'xVertexParams': [0.0, 0.02],
+#    'yVertexParams': [0.0, 0.02],
+#    'zVertexParams': [0.0, 0.02],
+#    'independentVertices': False,
+#    })
+
+# Create Event information
+eventinfosetter = register_module('EventInfoSetter')
+# Specify number of events to be generated
+eventinfosetter.param({'evtNumList': [10], 'runList': [1]})
+
+# Show progress of processing
+progress = register_module('Progress')
+
+# Load parameters
+gearbox = register_module('Gearbox')
+
+# Create geometry
+geometry = register_module('Geometry')
+# Select subdetectors to be built
+geometry.param('components', ['MagneticField', 'PXD', 'SVD'])
+# geometry.param("excludedComponents", "MagneticField")
+
+# generate BBbar events
+evtgeninput = register_module('EvtGenInput')
+evtgeninput.param('boost2LAB', True)
+
+# Run simulation
+fullsim = register_module('FullSim')
+
+# SVD modules
+svddigitizer = register_module('SVDDigitizer')
+svdclusterizer = register_module('SVDClusterizer')
+# OldValues = { 'NoiseSN'  : 2.5,
+#             'SeedSN'   : 5.0,
+#             'ClusterSN': 8.0 }
+# svdclusterizer.param(OldValues)
+
+# Save output of simulation
+output = register_module('RootOutput')
+# Set output filename
+output.param('outputFileName', 'simulationOutput.root')
+
+# SVD validation modules
+svdvalidation = SVDValidationTTree()
+svdvalidationdigit = SVDValidationTTreeDigit()
+svdvalidationsimhit = SVDValidationTTreeSimhit()
+svdefficiency = SVDValidationTTreeEfficiency()
+
+# ============================================================================
+# create processing path
+main = create_path()
+main.add_module(eventinfosetter)
+main.add_module(progress)
+# main.add_module(particlegun)
+main.add_module(gearbox)
+main.add_module(geometry)
+main.add_module(evtgeninput)
+main.add_module(fullsim)
+main.add_module(svddigitizer)
+main.add_module(svdclusterizer)
+# main.add_module(output)
+main.add_module(svdvalidation)
+main.add_module(svdvalidationdigit)
+main.add_module(svdvalidationsimhit)
+main.add_module(svdefficiency)
+
+process(main)
+
+# Print call statistics
+print statistics

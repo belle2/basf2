c7ceeaad5af368634350214f41fa08f98f84ade4
Martin Ritter <martin.ritter@lmu.de>
2015-07-21 11:58:33 +0000

    Revert examples to before PythonTidy-Gate
    and update them to newer add_module() possibilities.
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19825 b38c6c8f-bee8-465d-8448-9abe6915d496

--- simulation/examples/MaterialScan.py
+++ simulation/examples/MaterialScan.py
@@ -1,117 +1,103 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from basf2 import logging, LogLevel, register_module, create_path, process
+from basf2 import logging, LogLevel, create_path, process
 # Don't show all the info messages
 logging.log_level = LogLevel.ERROR
 
+# create path
+main = create_path()
+
 # We need to process one event so we need the EventInfoSetter
-eventinfosetter = register_module('EventInfoSetter')
-eventinfosetter.param({'runList': [1], 'evtNumList': [1]})
+main.add_module("EventInfoSetter", evtNumList=[1])
 
 # We need the geometry parameters
-gearbox = register_module('Gearbox')
+main.add_module("Gearbox")
 
-# as well as the geometry
-geometry = register_module('Geometry')
-geometry.set_log_level(LogLevel.INFO)
-# Restrict Geometry to certain components, in this case only PXD and SVD
-geometry.param('components', ['BeamPipe', 'PXD', 'SVD'])
+# as well as the geometry. We assign regions to each creator which allows to
+# split the material budget by region instead of material. Also, we restrict
+# the geometry to certain sub detectors only, in this case just the central
+# beampipe, the PXD and the SVD.
+geometry = main.add_module("Geometry", logLevel=LogLevel.INFO, assignRegions=True,
+                           components=['BeamPipe', 'PXD', 'SVD'])
 
 # MaterialScan uses the Geant4 setup which is created by the FullSim module so
 # we need this as well
-simulation = register_module('FullSim')
+main.add_module('FullSim')
 
 # And finally the MaterialScan module
-materialscan = register_module('MaterialScan')
-materialscan.set_log_level(LogLevel.INFO)
+materialscan = main.add_module("MaterialScan", logLevel=LogLevel.INFO)
 
 # Create a detailed Materialbudget for the beampipe
-# 1) 1000x1000 raster starting from IP looking at the acceptance
-# 2) 2000x2000 raster of the ZX plane between Z=-50 to 60 cm and X=-16 to
+# 1) 100x100 raster starting from IP looking at the acceptance
+# 2) 200x200 raster of the ZX plane between Z=-50 to 60 cm and X=-16 to
 #    16 cm, starting at Y=-16cm and scanning the next 32cm along Y
 
-materialscan.param({  # Filename for output File
-                      # Do Spherical scan?
-                      # Origin of the spherical scan
-                      # Number of steps in theta
-                      # Minimal theta value
-                      # Maximal theta value
-                      # If set to true, the scan will be done uniform in cos(theta)
-                      # Number of steps in theta
-                      # Minimal theta value
-                      # Maximal theta value
-                      # Depth of the scan: 0 for scan the whole defined geometry. Any value >0
-                      # will stop the ray after reaching the given distance from the start point
-                      # Split output by Material names instead of by Region
-                      # Specify the names of Materials to ignore in the scan.
-                      # Default is Air and Vacuum
-                      # Do Planar scan?
-                      # Name of the plane for scanning.  One of 'XY', 'XZ', 'YX', 'YZ', 'ZX',
-                      # 'ZY' or 'custom' to define the plane directly
-                      # Define a custom plane for the scan, only used if planar.plane='custom'.
-                      # This is a list of 9 values where the first three values specify the
-                      # origin, the second 3 values specify the first axis and the last 3 values
-                      # specify the second axis. In this case we scan the ZX plane but the origin
-                      # is not at the IP at y=-1 to create a symmetric cut view in |y|< 1 cm
-                      # Origin
-                      # First axis
-                      # Second axis
-                      # Depth of the scan: 0 for scan the whole defined geometry. Any value >0
-                      # will stop the ray after reaching the given distance from the start point
-                      # Number of steps along the first axis
-                      # Minimum value for the first axis
-                      # Maximum value for the first axis
-                      # Number of steps along the second axis
-                      # Minimum value for the second axis
-                      # Maximum value for the second axis
-                      # Split output by Material names insted of by Region
-                      # Specify the names of Materials to ignore in the scan.
-                      # Default is Air and Vacuum
+materialscan.param({
+    # Filename for output File
     'Filename': 'MaterialScan.root',
+    # Do Spherical scan?
     'spherical': True,
+    # Origin of the spherical scan
     'spherical.origin': [0, 0, 0],
-    'spherical.nTheta': 1000,
+    # Number of steps in theta
+    'spherical.nTheta': 100,
+    # Minimal theta value
     'spherical.minTheta': 17,
+    # Maximal theta value
     'spherical.maxTheta': 150,
+    # If set to true, the scan will be done uniform in cos(theta)
     'spherical.cosTheta': True,
-    'spherical.nPhi': 1000,
+    # Number of steps in theta
+    'spherical.nPhi': 100,
+    # Minimal theta value
     'spherical.minPhi': 0,
+    # Maximal theta value
     'spherical.maxPhi': 360,
+    # Depth of the scan: 0 for scan the whole defined geometry. Any value >0
+    # will stop the ray after reaching the given distance from the start point
     'spherical.maxDepth': 0,
+    # Split output by Material names instead of by Region
     'spherical.splitByMaterials': False,
+    # Specify the names of Materials to ignore in the scan.
+    # Default is Air and Vacuum
     'spherical.ignored': ['Air', 'Vacuum', 'G4_AIR', 'ColdAir'],
+    # Do Planar scan?
     'planar': True,
+    # Name of the plane for scanning.  One of 'XY', 'XZ', 'YX', 'YZ', 'ZX',
+    # 'ZY' or 'custom' to define the plane directly
     'planar.plane': 'custom',
+    # Define a custom plane for the scan, only used if planar.plane='custom'.
+    # This is a list of 9 values where the first three values specify the
+    # origin, the second 3 values specify the first axis and the last 3 values
+    # specify the second axis. In this case we scan the ZX plane but the origin
+    # is not at the IP at y=-1 to create a symmetric cut view in |y|< 1 cm
     'planar.custom': [
-        0,
-        -16,
-        0,
-        0,
-        0,
-        1,
-        1,
-        0,
-        0,
+        0, -16, 0,  # Origin
+        0,   0, 1,  # First axis
+        1,   0, 0,  # Second axis
     ],
+    # Depth of the scan: 0 for scan the whole defined geometry. Any value >0
+    # will stop the ray after reaching the given distance from the start point
     'planar.maxDepth': 32,
-    'planar.nU': 2000,
+    # Number of steps along the first axis
+    'planar.nU': 200,
+    # Minimum value for the first axis
     'planar.minU': -50.0,
+    # Maximum value for the first axis
     'planar.maxU': 60,
-    'planar.nV': 2000,
+    # Number of steps along the second axis
+    'planar.nV': 200,
+    # Minimum value for the second axis
     'planar.minV': -16,
+    # Maximum value for the second axis
     'planar.maxV': 16,
+    # Split output by Material names insted of by Region
     'planar.splitByMaterials': True,
+    # Specify the names of Materials to ignore in the scan.
+    # Default is Air and Vacuum
     'planar.ignored': ['Air', 'G4_AIR', 'ColdAir'],
 })
-
-# Create a path and add all modules
-main = create_path()
-main.add_module(eventinfosetter)
-main.add_module(gearbox)
-main.add_module(geometry)
-main.add_module(simulation)
-main.add_module(materialscan)
 
 # Do the MaterialScan, can take some time depending on the number of steps
 process(main)
--- simulation/examples/VisualizeGeometry+DAWN.py
+++ simulation/examples/VisualizeGeometry+DAWN.py
@@ -4,49 +4,40 @@
 import os
 from basf2 import *
 logging.log_level = LogLevel.WARNING
-# logging.log_level = LogLevel.INFO
 
-# Set the environment variable to automatically start the VRML viewer
-# for example http://freewrl.sourceforge.net/
-os.environ['G4VRMLFILE_VIEWER'] = 'freewrl'
-
-eventinfosetter = register_module('EventInfoSetter')
+# create path
+main = create_path()
+# process one event
+main.add_module('EventInfoSetter', evtNumList=[1])
 # Load XML parameters
+main.add_module('Gearbox')
+# Create Geometry and show info messages for that
+main.add_module('Geometry', logLevel=LogLevel.INFO)
+particlegun = main.add_module('ParticleGun')
+simulation = main.add_module('FullSim')
 
-gearbox = register_module('Gearbox')
-
-# Create Geometry
-geobuilder = register_module('Geometry')
-geobuilder.log_level = LogLevel.INFO
-
-pguninput = register_module('ParticleGun')
-
-g4sim = register_module('FullSim')
-
-eventinfosetter.param('evtNumList', [1])
-
-g4sim.param('EnableVisualization', True)
-g4sim.param('UICommands', ['/vis/open DAWNFILE', '/vis/drawVolume',
-                           '/vis/viewer/set/lineSegmentsPerCircle 100',
-                           '/vis/scene/add/axes 0 0 0 100 mm'])
-#            '/vis/scene/add/axes 0 0 0 100 mm',
-#            '/vis/scene/add/trajectories smooth',
-#            '/vis/modeling/trajectories/create/drawByCharge'])
+simulation.param('EnableVisualization', True)
+simulation.param('UICommands', [
+    # Use VRML2 backend
+    '/vis/open DAWNFILE',
+    # Draw the geometry
+    '/vis/drawVolume',
+    # Draw coordinate axes at the origin with a length of 100mm in each direction
+    '/vis/scene/add/axes 0 0 0 100 mm',
+    # Draw simulated tracks
+    '/vis/scene/add/trajectories smooth',
+    '/vis/modeling/trajectories/create/drawByCharge'
+    # Uncomment the following two lines to have yellow dots at each step boundary
+    # along the trajectory
+    # '/vis/modeling/trajectories/drawByCharge-0/default/setDrawStepPts true',
+    # '/vis/modeling/trajectories/drawByCharge-0/default/setStepPtsSize 0.5',
+])
 
 # Parameters for particle gun
-pguninput.param('nTracks', 1)
-pguninput.param('pdgCodes', [211])
-pguninput.param('momentumParams', [2.0, 5.0])
-pguninput.param('thetaParams', [85.0, 95.0])
-pguninput.param('phiParams', [-5.0, 5.0])
-
-main = create_path()
-
-main.add_module(eventinfosetter)
-main.add_module(gearbox)
-main.add_module(geobuilder)
-# Without Particle Gun there are no tracks
-main.add_module(pguninput)
-main.add_module(g4sim)
+particlegun.param('nTracks', 1)
+particlegun.param('pdgCodes', [211])
+particlegun.param('momentumParams', [2.0, 5.0])
+particlegun.param('thetaParams', [85.0, 95.0])
+particlegun.param('phiParams', [-5.0, 5.0])
 
 process(main)

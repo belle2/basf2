0737ebdf08bac844be7f57063686ef9887a78775
Guglielmo De Nardo <denardo@na.infn.it>
2015-07-23 16:17:06 +0000

    Ntuple maker for calculation covariance matrix in waveform fit
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19885 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ ecl/modules/eclDataAnalysis/include/EclCovMatrixNtupleModule.h
@@ -0,0 +1,70 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Peter Lukin, Alexander Bobrov, Guglielmo De Nardo                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef ECLCOVMATRIXNTUPLEMODULE_H
+#define ECLCOVMATRIXNTUPLEMODULE_H
+
+#include <framework/core/Module.h>
+
+#include <string>
+#include <TTree.h>
+#include <TFile.h>
+
+using namespace Belle2;
+
+namespace Belle2 {
+
+  class EclCovMatrixNtupleModule : public Module {
+
+  public:
+
+    /**
+     * Constructor.
+     * Sets the module parameters.
+     */
+    EclCovMatrixNtupleModule();
+
+    /** Destructor. */
+    virtual ~EclCovMatrixNtupleModule() {}
+
+    /** Initializes the module. */
+    virtual void initialize();
+
+    virtual void terminate();
+
+    /** Method is called for each event. */
+    virtual void event();
+
+  protected:
+    /** output root file name (given as Module parameter) */
+    std::string m_dataOutFileName;
+
+    /** Root tree and file for saving the output */
+    TTree* m_tree;
+    TFile* m_rootFile;
+
+    double m_energy;
+    double m_theta;
+    double m_phi;
+
+    int m_nevt;
+    int m_nhits;
+
+    int m_cellID[8736];
+    int m_DspHit[8736][31];
+    double m_hitE[8736];
+    double m_hitTime[8736];
+    int m_DigiTime[8736];
+    double m_DeltaT[8736];
+  };
+
+} // end namespace Belle2
+
+#endif
---
+++ ecl/modules/eclDataAnalysis/src/EclCovMatrixNtupleModule.cc
@@ -0,0 +1,135 @@
+/*******************************************************************************
+ * Module: EclCovMatrixNtuple                                                  *
+ *                                                                             *
+ * Contributors: Peter Lukin, Alexander Bobrov, Guglielmo De Nardo             *
+ *                                                                             *
+ * Description: This module write ECL digi information in a root tuple         *
+ *              to study amplitude and time info                               *
+ *                                                                             *
+ * This software is provided "as is" without any warranty. Due to the          *
+ * limited knowledge of BASF2 by the author some parts of the module might     *
+ * be written more elegantly. Eventualy also the reconstrcution/analysis tools *
+ * will be improved and parts of the code might become obsolete or could be    *
+ * performed in a better way. If you find a better solution to anything below  *
+ * please fill free to modify it.                                              *
+ *******************************************************************************/
+
+#include <ecl/modules/eclDataAnalysis/EclCovMatrixNtupleModule.h>
+#include <ecl/dataobjects/ECLDigit.h>
+#include <ecl/dataobjects/ECLDsp.h>
+#include <ecl/dataobjects/ECLTrig.h>
+
+#include <framework/datastore/StoreArray.h>
+
+using namespace std;
+using namespace Belle2;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(EclCovMatrixNtuple)
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+EclCovMatrixNtupleModule::EclCovMatrixNtupleModule() : Module()
+{
+  //Set module properties
+  setDescription("EclCovMatrixNtuple: write ECL waveform and fitted time and amplitude in a root file");
+
+  //Parameters definition
+  addParam("outputFileName", m_dataOutFileName,
+           "Output root file name of this module. Suffixes to distinguish them will be added automatically", string("EclCovMatrixNtuple"));
+
+}
+
+void EclCovMatrixNtupleModule::initialize()
+{
+  B2INFO("[EclCovMatrixNtuple Module]: Starting initialization of EclCovMatrixNtuple Module.");
+
+  // Initialize store arrays
+  // Why is it necessary to do here????
+
+  // Initializing the output root file
+  string dataFileName = m_dataOutFileName + ".root";
+
+  m_rootFile = new TFile(dataFileName.c_str(), "RECREATE");
+  m_tree     = new TTree("m_tree", "EclCovMatrixNtuple tree");
+
+  m_tree->Branch("energy",     &m_energy,     "energy/D");
+  m_tree->Branch("theta",      &m_theta,      "theta/D");
+  m_tree->Branch("phi",        &m_phi,        "phi/D");
+  m_tree->Branch("nhits",      &m_nhits,      "nhits/I");
+  m_tree->Branch("cellID",     m_cellID,      "cellID[nhits]/I");
+  m_tree->Branch("hitA",       m_DspHit,      "hitA[nhits][31]/I");
+  m_tree->Branch("hitT",       m_hitTime,     "hitT[nhits]/D");
+  m_tree->Branch("deltaT",     m_DeltaT,      "deltaT[nhits]/D");
+
+  m_tree->Branch("digiE",       m_hitE,       "hitE[nhits]/D");
+  m_tree->Branch("digiT",      m_DigiTime,    "digiT[nhits]/I");
+
+
+  B2INFO("[EclCovMatrixNtuple Module]: Finished initialising the Time Information Study in Gamma Reconstruction Module.");
+}
+
+void EclCovMatrixNtupleModule::terminate()
+{
+  m_rootFile->cd();
+  m_tree->Write();
+  m_rootFile->Close();
+}
+
+void EclCovMatrixNtupleModule::event()
+{
+  StoreArray<ECLDsp> eclDspArray;
+  StoreArray<ECLDigit> eclDigiArray;
+  StoreArray<ECLTrig> eclTrigArray;
+
+  m_nevt++;
+  m_energy = 0;
+  m_theta  = 0;
+  m_phi    = 0;
+
+
+  m_nhits  = 0;
+
+  for (int i = 0; i < 8736; i++) {
+    m_cellID[i]  = 0;
+    m_hitTime[i] = 0;
+    m_hitE[i]    = 0.0;
+    m_hitTime[i] = 0.0;
+    m_DigiTime[i] = 0;
+    m_DeltaT[i]   = 0.0;
+    for (int j = 0; j < 31; j++) {
+      m_DspHit[i][j] = 0;
+    }
+  }
+
+
+  m_nhits = eclDigiArray.getEntries();
+
+  // There is only 1 ECLTrig per event
+  assert(eclTrigArray.getEntries() == 1);
+  for (const auto& digit : eclDigiArray) {
+    size_t cellIndex = static_cast<size_t>(digit.getCellId() - 1);
+    m_cellID[cellIndex] = cellIndex;
+    m_hitE[cellIndex]     = digit.getAmp();
+    m_DigiTime[cellIndex] = digit.getTimeFit();
+    // The following DeltaT IS DIFFERENT DeltaT in igitizer by the last factor 12.!
+    double deltaT = eclTrigArray[0]->getTimeTrig() * 508.0 / 12.0;
+    m_DeltaT[cellIndex] = deltaT;
+    m_hitTime[cellIndex] = 1520 - digit.getTimeFit() - 64 * deltaT * 12.0 * 24.0 / 508.0 / 1536.0;
+  }
+
+  //The following works only because position of an ECLDigit in the ECLDigiArray
+  //is the same of position of the corresponding ECLDsp in the ECLDspArray
+  //Since the two a tightly related this must be enforced in a safer way
+  //in the ecl data objects model (Guglielmo De Nardo)
+  assert(eclDspArray.getEntries() == eclDigiArray.getEntries());
+  for (const auto& eclDsp : eclDspArray) {
+    size_t cellIndex = static_cast<size_t>(eclDsp.getCellId() - 1);
+    eclDsp.getDspA(m_DspHit[cellIndex]);
+  }
+  m_tree->Fill();
+}
+

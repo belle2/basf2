9ff7cd5cf0f8f01cc770b26a3c644db847e04791
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2015-07-24 13:01:02 +0000

    add small test for default channels
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19916 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ analysis/tests/fei_default_channels.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Christian Pulvermacher 2015
+
+from fei import *
+from basf2 import *
+from modularAnalysis import *
+
+# test wether we can actually get the default channels
+particles_charged = get_default_channnels(neutralB=False)
+particles_neutral = get_default_channnels(chargedB=False)
+particles_hadronic = get_default_channnels(semileptonicB=False)
+particles_withextracut = get_default_channnels(BlevelExtraCut='Mbc > 5.27')
+
+particles_all = get_default_channnels()
+feistate = fullEventInterpretation(None, particles_all)
+
+assert len(particles_all) > len(particles_charged)
+assert len(particles_all) > len(particles_neutral)
+assert len(particles_all) > len(particles_hadronic)
+assert len(particles_all) == len(particles_withextracut)
+
+
+# show constructed path
+print feistate.path
+
+# no actual basf2 execution

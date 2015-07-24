007286a4a0492f776af8cd0c2f03f775c24e19f3
Ian James Watson <iwatson@hep.phys.s.u-tokyo.ac.jp>
2015-07-24 03:20:28 +0000

    Simple implementation of the Combinatorial Kalman Filter (as I understand it)
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19893 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/modules/cdcToVXDExtrapolator/include/CKF.h
@@ -0,0 +1,65 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Ian J. Watson                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+#include<vector>
+
+namespace genfit { class Track; class AbsMeasurement; }
+
+namespace Belle2 {
+
+  /**
+   * Extrapolate a track, collect compatible hits, update, filter, refit
+   *
+   * Given a track and a method to ask for compatible hits, the CKF
+   * extrapolates the track to each of the hits, runs a Kalman update
+   * to give a new track for each hit, filters out bad updates, and
+   * repeats until no more hits are available. After this, it filters
+   * out a best track (or tracks?) and does a full refit.
+   *
+   */
+  class CKF {
+
+  public:
+
+    /**
+     * Pass in an existing track to be fit, and a way to ask for new
+     * hits. findHits should take in a track, and an integer
+     * representing the number of steps already processed. data will
+     * be passed unmolested to each invocation of findHits
+     */
+    CKF(genfit::Track* track, bool (*findHits)(genfit::Track*, unsigned, std::vector<genfit::AbsMeasurement*>&, void*), void* data);
+
+    /// find hits, run extrapolations, trim outputs, find a best track candidate
+    genfit::Track* processTrack();
+
+  private:
+    genfit::Track* seedTrack;
+    bool (*findHits)(genfit::Track*, unsigned, std::vector<genfit::AbsMeasurement*>&, void*);
+
+    /// refit track, assumes a TrackRep for the track already exists
+    bool refitTrack(genfit::Track* track);
+
+    /// finds the best track of all the candidates
+    genfit::Track* bestTrack(std::vector<genfit::Track*>*);
+
+    /// after adding an additional hit but before updating, check that the track passes some selections (e.g. check for holes)
+    bool passPreUpdateTrim(genfit::Track*, unsigned step);
+    /// after adding an additional hit and updating, check that the track passes some selections (e.g. check the updates chi2 increment)
+    bool passPostUpdateTrim(genfit::Track*, unsigned step);
+
+    /// data needed by the findHits functions
+    void* data;
+
+    /// how many findHits steps we've already taken
+    unsigned step;
+  };
+}
---
+++ tracking/modules/cdcToVXDExtrapolator/src/CKF.cc
@@ -0,0 +1,251 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Ian J. Watson                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <framework/logging/Logger.h>
+#include <tracking/modules/cdcToVXDExtrapolator/CKF.h>
+
+#include <genfit/Track.h>
+#include <genfit/TrackCand.h>
+#include <genfit/RKTrackRep.h>
+#include <genfit/AbsKalmanFitter.h>
+#include <genfit/KalmanFitter.h>
+#include <genfit/DAF.h>
+#include <genfit/KalmanFitterInfo.h>
+#include <genfit/KalmanFitterRefTrack.h>
+#include <genfit/FitStatus.h>
+#include <genfit/KalmanFitStatus.h>
+#include <genfit/Exception.h>
+
+using namespace Belle2;
+
+CKF::CKF(genfit::Track* _track, bool (*_findHits)(genfit::Track*, unsigned, std::vector<genfit::AbsMeasurement*>&, void*),
+         void* _data) :
+  seedTrack(_track),
+  findHits(_findHits),
+  data(_data),
+  step(0)
+{
+}
+
+genfit::Track* CKF::processTrack()
+{
+  auto tracks = new std::vector<genfit::Track*>;
+  tracks->push_back(new genfit::Track(*seedTrack));
+
+  auto fitter = new genfit::KalmanFitter();
+  fitter->setMinIterations(3);
+  fitter->setMaxIterations(10);
+  fitter->setMultipleMeasurementHandling(genfit::unweightedClosestToPredictionWire);
+  fitter->setMaxFailedHits(5);
+
+  B2DEBUG(90, "- In CKF::processTrack(): new track");
+  std::vector<genfit::AbsMeasurement*> allHits;
+  for (step = 0; true; ++step) {
+    // only process steps where we find new hits, take it from the
+    // seed track because we assume we have a very wide window.
+    std::vector<genfit::AbsMeasurement*> newHits;
+    if (!findHits(seedTrack, step, newHits, data))
+      break;
+
+    if (newHits.size() == 0) {
+      B2DEBUG(90, "-- In CKF::processTrack(): At step " << step << " no hits found, continuing search");
+      continue;
+    }
+    for (unsigned i = 0; i < newHits.size(); ++i)
+      allHits.push_back(newHits[i]);
+
+    bool any = false;
+    std::vector<genfit::Track*>* newtracks = new std::vector<genfit::Track*>;
+
+    for (auto& track : *tracks) {
+      B2DEBUG(90, "-- In CKF::processTrack(): At step " << step << " given " << newHits.size() << " hits");
+      // copy the no-added-hit case
+      newtracks->push_back(track);
+      /// if the track is bad, don't try to add hits
+      if (!passPreUpdateTrim(track, step)) {
+        continue;
+      }
+      any = true;
+      if (newHits.size() != 0) {
+        for (auto& hit : newHits) {
+          genfit::Track* newtrack = new genfit::Track(*track);
+          newtrack->insertMeasurement(hit->clone(), 0);
+          try {
+            fitter->processTrackPartially(newtrack, newtrack->getCardinalRep(), 1, 0);
+            auto up0 = newtrack->getPointWithMeasurementAndFitterInfo(0, newtrack->getCardinalRep())->getKalmanFitterInfo(0)->getUpdate(-1);
+            //auto up1 = newtrack->getPointWithMeasurementAndFitterInfo(1, newtrack->getCardinalRep())->getKalmanFitterInfo(0)->getUpdate(-1);
+
+            if (passPostUpdateTrim(newtrack, step)) {
+              TMatrixDSym cov(6, 6), cov2(6, 6);
+              TVector3 pos, mom, pos2, mom2;
+              track->getFittedState().getPosMomCov(pos, mom, cov);
+              up0->getPosMomCov(pos2, mom2, cov2);
+              B2DEBUG(70, "--- In CKF::processTrack(): Added partial track step " << newtrack->getFitStatus(
+                        newtrack->getCardinalRep())->getChi2() <<
+                      "[" << newtrack->getFitStatus(newtrack->getCardinalRep())->getNdf() << "] " << up0->getChiSquareIncrement() << "{" << up0->getNdf()
+                      << "}" << //" " << up1->getChiSquareIncrement() <<
+                      " - " << pos2.X() << " " << pos2.Y() << " " << pos2.Z() << " " << " - " << pos.X() << " " << pos.Y() << " " << pos.Z() << " ");
+              newtracks->push_back(newtrack);
+            } else
+              delete newtrack;
+          } catch (...) {
+            B2DEBUG(80, "--- In CKF::processTrack(): Track extrapolation failed");
+            delete newtrack;
+          }
+        }
+      }
+    }
+
+    if (!any) {
+      delete newtracks;
+      break;
+    }
+
+    B2DEBUG(90, "-- In CKF::processTrack(): At step " << step << " have " << newtracks->size() << " track extrapolations");
+    // for (unsigned i = 0; i < tracks->size(); ++i)
+    //   delete tracks->at(i);
+    delete tracks;
+    tracks = newtracks;
+  }
+
+  B2DEBUG(90, "- In CKF::processTrack(): Finished search, have " << tracks->size() << " track extrapolations");
+  genfit::Track* best = bestTrack(tracks);
+  genfit::Track* ret = 0;
+  if (best)
+    ret = new genfit::Track(*best);
+  for (unsigned i = 0; i < tracks->size(); ++i)
+    delete tracks->at(i);
+  delete tracks;
+  delete fitter;
+
+  // everything is deleted so i hope we can delete the hits!
+  for (unsigned i = 0; i < allHits.size(); ++i)
+    delete allHits[i];
+
+
+  if (ret) {
+    B2DEBUG(90, "- In CKF::processTrack(): Found a best");
+    if (refitTrack(ret)) {
+      return ret;
+    } else {
+      B2DEBUG(90, "- In CKF::processTrack(): Refit failed. Returning seed track.");
+      delete ret;
+      return seedTrack;
+    }
+  } else {
+    B2DEBUG(90, "- In CKF::processTrack(): No best");
+    return seedTrack;
+  }
+}
+
+bool CKF::passPreUpdateTrim(genfit::Track* track, unsigned step)
+{
+  // Check for holes
+  int nadded = track->getNumPointsWithMeasurement() - seedTrack->getNumPointsWithMeasurement();
+  int nholes = step - nadded;
+  if (nholes > 3) {
+    B2DEBUG(70, "--- In CKF::preUpdateTrim(): Filtering track with nholes " << nholes << "{" << step << " " <<
+            track->getNumPointsWithMeasurement() << " " << seedTrack->getNumPointsWithMeasurement() << " " << nadded << "}");
+    return false;
+  }
+  return true;
+}
+
+bool CKF::passPostUpdateTrim(genfit::Track* track, unsigned)
+{
+  auto up0 = track->getPointWithMeasurementAndFitterInfo(0, track->getCardinalRep())->getKalmanFitterInfo(0)->getUpdate(-1);
+  if ((up0->getChiSquareIncrement() / up0->getNdf()) > 20) {
+    B2DEBUG(70, "--- In CKF::postUpdateTrim(): Filtering track with chi2 " << up0->getChiSquareIncrement() << "{" << up0->getNdf() <<
+            "}");
+    return false;
+  }
+  return true;
+}
+
+void chiSqNdf(genfit::Track* track, double& chi2, int& ndf)
+{
+  chi2 = 0; ndf = 0;
+  for (int i = 0; i < track->getNumPointsWithMeasurement(); ++i) {
+    auto up = track->getPointWithMeasurementAndFitterInfo(i, track->getCardinalRep())->getKalmanFitterInfo(0)->getUpdate(-1);
+    chi2 += up->getChiSquareIncrement();
+    ndf += up->getNdf();
+  }
+}
+
+genfit::Track* CKF::bestTrack(std::vector<genfit::Track*>* tracks)
+{
+  if (!tracks || tracks->size() == 0)
+    return 0;
+  unsigned iBest = 0;
+  double chi2Best = tracks->at(0)->getFitStatus(tracks->at(0)->getCardinalRep())->getChi2();
+  for (unsigned i = 0; i < tracks->size(); ++i) {
+    double thisChi2(0); int ndf(0); // tracks->at(i)->getFitStatus(tracks->at(i)->getCardinalRep())->getChi2();
+    chiSqNdf(tracks->at(i), thisChi2, ndf);
+    B2DEBUG(90, "-- In CKF::bestTrack(): @ " << i << " " << thisChi2 << " " << ndf << " " << thisChi2 / ndf);
+    thisChi2 = thisChi2 / ndf;
+    if (thisChi2 < chi2Best) {
+      chi2Best = thisChi2;
+      iBest = i;
+    }
+  }
+  B2DEBUG(90, "-- In CKF::bestTrack(): Best track @ " << iBest);
+
+  return tracks->at(iBest);
+}
+
+bool CKF::refitTrack(genfit::Track* track)
+{
+  bool fitSuccess = false;
+  // following the GenFitter.cc code
+  // genfit::AbsKalmanFitter* fitter = 0;
+  // if (m_useKalman) {
+  genfit::KalmanFitterRefTrack* fitter = new genfit::KalmanFitterRefTrack();
+  fitter->setMinIterations(3);
+  fitter->setMaxIterations(10);
+  fitter->setMultipleMeasurementHandling(genfit::unweightedClosestToPredictionWire);
+  fitter->setMaxFailedHits(5);
+  // } else {
+  //   fitter = new genfit::DAF(true);
+  //   ((genfit::DAF*) fitter)->setProbCut(0.001);
+  //   fitter->setMaxFailedHits(5);
+  // }
+  try {
+    if (!track->checkConsistency()) {
+      B2DEBUG(50, "Inconsistent track found, attempting to sort!")
+      bool sorted = track->sort();
+      if (!sorted) {
+        B2DEBUG(50, "Track points NOT SORTED! Still inconsistent, I can't be held responsible for assertion failure!");
+      } else {
+        B2DEBUG(50, "Track points SORTED!! Hopefully this works now!");
+      }
+    }
+    fitter->processTrack(track);
+
+    genfit::FitStatus* fs = 0;
+    genfit::KalmanFitStatus* kfs = 0;
+    fitSuccess = track->hasFitStatus();
+    if (fitSuccess) {
+      fs = track->getFitStatus();
+      fitSuccess = fitSuccess && fs->isFitted();
+      fitSuccess = fitSuccess && fs->isFitConverged();
+      kfs = dynamic_cast<genfit::KalmanFitStatus*>(fs);
+      fitSuccess = fitSuccess && kfs;
+      B2DEBUG(100, "> Track fit. isFitted: " << fs->isFitted() << " isFitConverged:" << fs->isFitConverged() << " isFitConverged(false):"
+              << fs->isFitConverged(false) << " nFailedPoints:" << fs->getNFailedPoints() << " numPointsWithMeasurement (from track): " <<
+              track->getNumPointsWithMeasurement());
+    } else {
+      B2WARNING("Bad Fit in CKFCdcToVxdModule");
+    }
+  } catch (...) {
+    B2WARNING("Track fitting has failed.");
+  }
+  delete fitter;
+  return fitSuccess;
+}

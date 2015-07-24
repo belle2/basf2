d52a6a2ee9061e3d23510571d91dfc16da06cd52
Nils Braun <nils.braun@student.kit.edu>
2015-07-24 09:01:11 +0000

    Changed to new optimized parameters sets
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19901 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/scripts/tracking/modules.py
+++ tracking/scripts/tracking/modules.py
@@ -100,11 +100,11 @@ class CDCFullFinder(metamodules.PathModule):
 
     def __init__(self, output_track_cands_store_array_name="TrackCands",
                  tmva_cut=0.1,
-                 first_filter="tmva", first_tmva_cut=0.70,
-                 background_filter="tmva", background_filter_tmva_cut=0.3,
+                 first_filter="tmva", first_tmva_cut=0.75,
+                 background_filter="tmva", background_filter_tmva_cut=0.7,
                  new_segments_filter="none", new_segments_filter_tmva_cut=0,
                  second_filter="none", second_tmva_cut=0,
-                 track_filter="tmva", track_filter_cut=0.3,
+                 track_filter="all", track_filter_cut=0,
                  use_pair_finder=True):
 
         modules = [

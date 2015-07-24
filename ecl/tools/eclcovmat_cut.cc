b9da57cfda513259951f6db1ebb9c46e5940ea0c
Guglielmo De Nardo <denardo@na.infn.it>
2015-07-24 15:28:39 +0000

    start porting to C++ and reducing warnings
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@19921 b38c6c8f-bee8-465d-8448-9abe6915d496

--- ecl/tools/eclcovmat_cut.cc
+++ ecl/tools/eclcovmat_cut.cc
@@ -4,12 +4,14 @@
 #include <TH1.h>
 #include <TF1.h>
 #include <TMath.h>
-#include <stdio.h>
 #include <iostream>
+#include <fstream>
+#include <cassert>
 
 TH2F* Box = new TH2F("Box", "box", 8736, 0., 8736., 2900, 2000, 4900);
 TH1F* Ms = new TH1F("Ms", "ev", 2900, 2000., 4900.);
 
+using namespace std;
 
 Double_t glog(Double_t* x, Double_t* par)
 {
@@ -31,88 +33,29 @@ Double_t glog(Double_t* x, Double_t* par)
   return fitval;
 }
 
-void cut(char* inputRootFile)
+void cut(const char* inputRootFilename, const char* outputCutFilename)
 {
 
   TChain fChain("m_tree");
-  char treeroot[256];
-
-  FILE* McoIN;
-  Char_t Min[256];
-
-  FILE* BMcoIN;
-  //  Char_t BMin[256];
-
   Int_t poq;
+  cout << "!!! file for calibration:" << inputRootFilename << endl;
+  fChain.Add(inputRootFilename);
 
-
-  //  sprintf(BMin,"/gpfs/home/belle/avbobrov/belle2/j15/ecl/examples/rootfile.txt");
-  char* BMin = inputRootFile;
-  if ((BMcoIN = fopen(BMin, "r")) == NULL) {
-    printf(" file %s is absent \n", BMin);
-    exit(1);
-  }
-
-  while (!feof(BMcoIN)) {
-    fscanf(BMcoIN, "%s", &treeroot);
-  }
-  fclose(BMcoIN);
-  std::cout << "!!! file for calibration:" << treeroot << std::endl;
-
-  fChain.Add(treeroot);
-
-
-  Double_t        energy;
-  Double_t        theta;
-  Double_t        phi;
   Int_t           nhits;
-  Int_t           cellID[8736];   //[nhits]
-  Int_t           hitA[8736][31];   //[nhits]
-  Double_t        hitT[8736];   //[nhits]
-  Int_t           digiT[8736];   //[nhits]
-  Double_t        digiE[8736];   //[nhits]
-  Double_t        deltaT[8736];   //[nhits]
-  Int_t           necl;
-  Int_t           cID[500000];   //[necl]
-  Float_t         Edep[500000];   //[necl]
-  Float_t         TimeAve[500000];   //[necl]
+  Int_t          hitA[8736][31];
 
-  // List of branches
-  TBranch*        b_energy;   //!
-  TBranch*        b_theta;   //!
-  TBranch*        b_phi;   //!
-  TBranch*        b_nhits;   //!
-  TBranch*        b_cellID;   //!
-  TBranch*        b_hitA;   //!
-  TBranch*        b_hitT;   //!
-  TBranch*        b_digiE;   //!
-  TBranch*        b_digiT;   //!
-  TBranch*        b_DeltaT;   //!
-  TBranch*        b_deltaT;   //!
-  TBranch*        b_necl;   //!
-  TBranch*        b_cID;   //!
-  TBranch*        b_Edep;   //!
-  TBranch*        b_TimeAve;   //!
 
-  fChain.SetBranchAddress("energy", &energy, &b_energy);
-  fChain.SetBranchAddress("theta", &theta, &b_theta);
-  fChain.SetBranchAddress("phi", &phi, &b_phi);
+  TBranch*        b_nhits;
+  TBranch*        b_hitA;
+
   fChain.SetBranchAddress("nhits", &nhits, &b_nhits);
-  fChain.SetBranchAddress("cellID", cellID, &b_cellID);
   fChain.SetBranchAddress("hitA", hitA, &b_hitA);
-  fChain.SetBranchAddress("hitT", hitT, &b_hitT);
-  fChain.SetBranchAddress("digiE", digiE, &b_digiE);
-  fChain.SetBranchAddress("digiT", digiT, &b_digiT);
-  fChain.SetBranchAddress("deltaT", deltaT, &b_deltaT);
 
   Int_t k;
-
   Int_t u;
 
   Double_t InT;
-
   Double_t mug;
-
   Double_t rg;
 
   Double_t Mu[8736];
@@ -120,10 +63,9 @@ void cut(char* inputRootFile)
   Double_t As[8736];
   Double_t Sr[8736];
   Double_t Sl[8736];
-
-  Double_t Chi[8736];
+  Double_t Chi[8736]
+  ;
   Double_t cc = TMath::Sqrt(2.*TMath::Log(2.));
-
 
   Int_t nevent = fChain.GetEntries();
   std::cout << "! nevent=" << nevent << std::endl;
@@ -178,16 +120,23 @@ void cut(char* inputRootFile)
 
   // write data
 
-  sprintf(Min, "/home/belle/avbobrov/binp2/chdatuXX.txt");
-  McoIN = fopen(Min, "w");
+  ofstream outputCutFile(outputCutFilename);
   for (poq = 0; poq < 8736; poq++) {
-    fprintf(McoIN, "%d %f %f %f %f %f \n  ", poq, Mu[poq], Sg[poq], Sl[poq], Sr[poq], As[poq]);
+    outputCutFile << poq << " " <<  Mu[poq] << " "
+                  << Sg[poq] << " " <<  Sl[poq]  << " "
+                  <<  Sr[poq]  << " " <<  As[poq] << endl;
+    // fprintf(McoIN, "%d %f %f %f %f %f \n  ", poq, Mu[poq], Sg[poq], Sl[poq], Sr[poq], As[poq]);
   }
-  fclose(McoIN);
+  outputCutFile.close();
 }
 
 int main(int argc, char** argv)
 
 {
-  cut("/gpfs/home/belle/avbobrov/belle2/j15/ecl/examples/rootfile.txt");
+  assert(argc == 3);
+  // first argument is input root file
+  // second argument is output cut text file
+  cut(argv[1], argv[2]);
+  //cut("/gpfs/home/belle/avbobrov/belle2/j15/ecl/examples/rootfile.txt");
+  // sprintf(Min, "/home/belle/avbobrov/binp2/chdatuXX.txt");
 }

3344e135f5d828e807496b1f458bbc94355a9aff
Satoru Yamada <satoru.yamada@kek.jp>
2015-07-30 08:25:59 +0000

    Add comment for GetNodeID()
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20090 b38c6c8f-bee8-465d-8448-9abe6915d496

--- rawdata/dataobjects/include/RawCOPPERFormat.h
+++ rawdata/dataobjects/include/RawCOPPERFormat.h
@@ -190,7 +190,8 @@ namespace Belle2 {
     //! Add Detected Error bitflag
     virtual void AddErrorBitFlag(int n, unsigned int error_bit_flag) = 0;
 
-    virtual unsigned int GetNodeID(int n) = 0;     //! get node-ID from data
+    //! get node-ID from data
+    virtual unsigned int GetNodeID(int n) = 0;
 
     //
     // Get information from 13words "COPPER header" attached by COPPER board

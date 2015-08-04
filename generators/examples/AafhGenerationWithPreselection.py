ec8c93be8fd2233213c02f80a30ddc09b6944421
Torben Ferber <ferber@physics.ubc.ca>
2015-08-04 11:39:54 +0000

    reformatted the comments
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20179 b38c6c8f-bee8-465d-8448-9abe6915d496

--- generators/examples/AafhGenerationWithPreselection.py
+++ generators/examples/AafhGenerationWithPreselection.py
@@ -29,32 +29,31 @@ beamparameters.param("generateCMS", False)
 
 # generator
 aafh = register_module('AafhInput')
-aafh.param({  # decay mode to generate.
-              # 1: e+e- -> mu+mu-L+L- where L is a user defined particle (default: tau)
-              # 2: e+e- -> mu+mu-mu+mu-
-              # 3: e+e- -> e+e-mu+mu-
-              # 4: e+e- -> e+e-L+L- where L is a user defined particle (default: tau)
-              # 5: e+e- -> e+e-e+e-
-              # to set the particle for modes 1 and 4 use set parameter "particle"
-              # rejection scheme to generate unweighted events
-              # 1: use rejection once for the final event weight
-              # 2: use rejection per sub generator and then for the final event
-              # max subgenerator event weight, only used if rejection is set to 2
-              # (default). If this value is to low the generation will produce errors. If
-              # it is to high generation runs slower.
-              # max final event weight which is always used. If this value is to low the
-              # generation will produce errors. If it is to high generation runs slower.
-              # ==> should be around 2-4
-              # adjust subgenerator weights so that each sub generator has same
-              # probability to be called and the maximum weight is equal as well. These
-              # values are printed at the end of generation when output level is set to
-              # INFO. These weights strongly depend on the mode
-              # set to awfully precise
+aafh.param({
+    # decay mode to generate.
+    # 1: e+e- -> mu+mu-L+L- where L is a user defined particle (default: tau)
+    # 2: e+e- -> mu+mu-mu+mu-
+    # 3: e+e- -> e+e-mu+mu-
+    # 4: e+e- -> e+e-L+L- where L is a user defined particle (default: tau)
+    # 5: e+e- -> e+e-e+e-
     'mode': 5,
-    'minMass': 0.50,
+    # to set the particle for modes 1 and 4 use set parameter "particle"
+    # rejection scheme to generate unweighted events
+    # 1: use rejection once for the final event weight
+    # 2: use rejection per sub generator and then for the final event
     'rejection': 2,
+    # max subgenerator event weight, only used if rejection is set to 2
+    # (default). If this value is to low the generation will produce errors. If
+    # it is to high generation runs slower.
     'maxSubgeneratorWeight': 1.0,
+    # max final event weight which is always used. If this value is to low the
+    # generation will produce errors. If it is to high generation runs slower.
+    # ==> should be around 2-4
     'maxFinalWeight': 3.0,
+    # adjust subgenerator weights so that each sub generator has same
+    # probability to be called and the maximum weight is equal as well. These
+    # values are printed at the end of generation when output level is set to
+    # INFO. These weights strongly depend on the mode
     'subgeneratorWeights': [
         1.0,
         7.986e+01,
@@ -64,9 +63,12 @@ aafh.param({  # decay mode to generate.
         1.664e+00,
         2.812e+00,
         7.321e-01,
-        ],
+    ],
+    # set to awfully precise
     'suppressionLimits': [1e100] * 4,
-    })
+    # minimum invariant mass of the secondary pair
+    'minMass': 0.50,
+})
 aafh.logging.log_level = LogLevel.INFO
 
 # preselection to reject no tag events

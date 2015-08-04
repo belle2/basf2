2b084630cde6f29e1e8e748acf189afaea566792
Thomas Keck <t.keck@online.de>
2015-08-04 13:09:53 +0000

    Export binom_error and poisson_error from B2Plot module
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20187 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/scripts/B2Plot/__init__.py
+++ framework/scripts/B2Plot/__init__.py
@@ -5,7 +5,8 @@ Thomas Keck 2013-2015
 # @cond internal
 # only public interfaces, to make package doc nicer. Also the only things imported by 'from B2Plot import *'
 __all__ = ['Plotter', 'PurityOverEfficiency', 'RejectionOverEfficiency', 'Overtraining', 'Correlation',
-           'Box', 'Distribution', 'Difference', 'VerboseDistribution', 'Diagonal', 'Histograms']
+           'Box', 'Distribution', 'Difference', 'VerboseDistribution', 'Diagonal', 'Histograms',
+           'binom_error', 'poisson_error']
 # @endcond
 
 from B2Plot.b2plot import *

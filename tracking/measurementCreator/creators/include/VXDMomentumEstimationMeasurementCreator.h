0f21527c2a19bcfe791e6fbd48b927771b960754
Nils Braun <nils.braun@student.kit.edu>
2015-08-05 08:01:06 +0000

    Made the fit parameters configurable
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20250 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/measurementCreator/creators/include/VXDMomentumEstimationMeasurementCreator.h
+++ tracking/measurementCreator/creators/include/VXDMomentumEstimationMeasurementCreator.h
@@ -19,9 +19,36 @@ namespace Belle2 {
   class VXDMomentumEstimationMeasurementCreator : public BaseMeasurementCreatorFromCoordinateMeasurement<HitType, detector> {
   public:
     VXDMomentumEstimationMeasurementCreator(const genfit::MeasurementFactory<genfit::AbsMeasurement>& measurementFactory) :
-      BaseMeasurementCreatorFromCoordinateMeasurement<HitType, detector>(measurementFactory) {}
+      BaseMeasurementCreatorFromCoordinateMeasurement<HitType, detector>(measurementFactory), m_minimumMomentum(0.1) {}
 
     virtual ~VXDMomentumEstimationMeasurementCreator() { }
+
+    void setParameter(const std::string& parameterName, const std::string& parameterValue) override
+    {
+      // there are only double values, so this should be ok:
+      double value = std::stod(parameterValue);
+      if (parameterName == "MinimumMomentum") {
+        m_minimumMomentum = value;
+      } else if (parameterName == "aE") {
+        m_fitParameters.aE = value;
+      } else if (parameterName == "bE") {
+        m_fitParameters.bE = value;
+      } else if (parameterName == "cE") {
+        m_fitParameters.cE = value;
+      } else if (parameterName == "dE") {
+        m_fitParameters.dE = value;
+      } else if (parameterName == "aM") {
+        m_correctionFitParameters.aM = value;
+      } else if (parameterName == "bM") {
+        m_correctionFitParameters.bM = value;
+      } else if (parameterName == "cM") {
+        m_correctionFitParameters.cM = value;
+      } else if (parameterName == "dM") {
+        m_correctionFitParameters.dM = value;
+      } else {
+        B2FATAL("A parameter with the name " << parameterName << " and the value " << parameterValue << " could not be set.")
+      }
+    }
 
   protected:
     /**
@@ -31,7 +58,6 @@ namespace Belle2 {
         const RecoTrack& recoTrack, const RecoHitInformation&,
         const std::pair<genfit::AbsMeasurement*, genfit::TrackCandHit*>& coordinateMeasurement) const override
     {
-
       genfit::AbsMeasurement* absCoordinateMeasurement = coordinateMeasurement.first;
       genfit::PlanarMeasurement* planarMeasurement = dynamic_cast<genfit::PlanarMeasurement*>(absCoordinateMeasurement);
       if (planarMeasurement == nullptr) {
@@ -44,9 +70,10 @@ namespace Belle2 {
       const TVector3& position = recoTrack.getPosition();
       short charge = recoTrack.getCharge();
 
-      if (momentum.Mag() < 0.1) {
+      if (momentum.Mag() < m_minimumMomentum) {
         TVectorD rawHitCoordinates(1);
-        double estimatedMomentum = momentumEstimation.estimateQOverP(*hit, momentum, position, charge);
+        double estimatedMomentum = momentumEstimation.estimateQOverP(*hit, momentum, position, charge, m_fitParameters,
+                                   m_correctionFitParameters);
 
         if (not std::isnan(estimatedMomentum)) {
           rawHitCoordinates(0) = estimatedMomentum;
@@ -64,5 +91,9 @@ namespace Belle2 {
       return {};
     }
 
+  private:
+    typename VXDMomentumEstimation<HitType>::FitParameters m_fitParameters;
+    typename VXDMomentumEstimation<HitType>::CorrectionFitParameters m_correctionFitParameters;
+    double m_minimumMomentum;
   };
 }
--- tracking/vxdMomentumEstimation/include/VXDMomentumEstimation.h
+++ tracking/vxdMomentumEstimation/include/VXDMomentumEstimation.h
@@ -22,43 +22,55 @@ namespace Belle2 {
     VXDMomentumEstimation& operator = (const VXDMomentumEstimation&);
 
   public:
+    struct FitParameters {
+      double aE = 1.080493149e+11;
+      double bE = -752991.2151;
+      double cE = 0.03636985211;
+      double dE = -3.020359066e-09;
+    };
+
+    struct CorrectionFitParameters {
+      double aM = 41.21477882;
+      double bM = -2.650950176;
+      double cM = 0.05576940304;
+      double dM = -206.4416227;
+    };
+
     static const VXDMomentumEstimation& getInstance()
     {
       static VXDMomentumEstimation instance;
       return instance;
     }
 
-    double estimateQOverP(const ClusterType& cluster, const TVector3& momentum, const TVector3& position, short charge) const
+    double estimateQOverP(const ClusterType& cluster, const TVector3& momentum, const TVector3& position, short charge,
+                          const FitParameters& fitParameters, const CorrectionFitParameters& correctionFitParameters) const
     {
-      double momentumEstimation = estimateMomentum(cluster, momentum, position, charge);
+      double momentumEstimation = estimateMomentum(cluster, momentum, position, charge, fitParameters, correctionFitParameters);
       double QOverP = charge / momentumEstimation;
 
       return QOverP;
     }
 
   private:
-    double estimateMomentum(const ClusterType& cluster, const TVector3& momentum, const TVector3& position, short charge) const
+    double estimateMomentum(const ClusterType& cluster, const TVector3& momentum, const TVector3& position, short charge,
+                            const FitParameters& fitParameters, const CorrectionFitParameters& correctionFitParameters) const
     {
       const VXDMomentumEstimationTools<ClusterType>& tools = VXDMomentumEstimationTools<ClusterType>::getInstance();
 
       double dEdX = tools.getDEDX(cluster, momentum, position, charge);
 
       // These numbers are fit results from the Analyse_Hitwise.ipynb ipython notebook and can not be calculated.
-      double aE = 1.049925575e+11;
-      double bE = -738645.4502;
-      double cE = 0.03691275027;
-      double dE = -3.155086281e-09;
-      double estimation = aE / (dEdX - bE) / (dEdX - bE) + cE + dE * dEdX;
+      double estimation = fitParameters.aE / (dEdX - fitParameters.bE) / (dEdX - fitParameters.bE) + fitParameters.cE + fitParameters.dE *
+                          dEdX;
+      B2INFO(estimation);
 
-
-      double aM = 103.3845985;
-      double bM = -4.812561153;
-      double cM = 0.08154437323;
-      double mediumCorrection = aM * estimation * estimation + bM * estimation + cM;
+      double mediumCorrection = correctionFitParameters.aM * estimation * estimation + correctionFitParameters.bM * estimation +
+                                correctionFitParameters.cM + correctionFitParameters.dM * estimation * estimation * estimation;
       double estimationWithMediumCalibration = estimation - mediumCorrection;
 
+      B2INFO(estimationWithMediumCalibration);
 
-      return estimation;
+      return estimationWithMediumCalibration;
     }
 
   };

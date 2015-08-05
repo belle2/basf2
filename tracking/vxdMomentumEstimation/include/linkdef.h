26344fff9f6f3198db829c6d5f2a91e2f15a8c2a
Nils Braun <nils.braun@student.kit.edu>
2015-08-05 07:59:11 +0000

    Added the tools to python too
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20241 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/vxdMomentumEstimation/include/linkdef.h
+++ tracking/vxdMomentumEstimation/include/linkdef.h
@@ -5,6 +5,9 @@
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
+#pragma link C++ class Belle2::VXDMomentumEstimationTools<Belle2::PXDCluster>-;
+#pragma link C++ class Belle2::VXDMomentumEstimationTools<Belle2::SVDCluster>-;
+
 #pragma link C++ class Belle2::VXDMomentumEstimation<Belle2::PXDCluster>-;
 #pragma link C++ class Belle2::VXDMomentumEstimation<Belle2::SVDCluster>-;
 

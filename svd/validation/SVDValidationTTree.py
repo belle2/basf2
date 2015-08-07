ac6d1b8532c94447032b8da8f4684db0db493b33
Giacomo Caria <gcaria@student.unimelb.edu.au>
2015-08-07 12:03:21 +0000

    Fixed list of output files in header
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20383 b38c6c8f-bee8-465d-8448-9abe6915d496

--- svd/validation/SVDValidationTTree.py
+++ svd/validation/SVDValidationTTree.py
@@ -3,7 +3,6 @@
 
 """
 <header>
-  <output> SVDValidationTTree.root </output>
   <contact> G. Caria, gcaria@student.unimelb.edu.au </contact>
   <description>
     This module is used for the SVD validation.

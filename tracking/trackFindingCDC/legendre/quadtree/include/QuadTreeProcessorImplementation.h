f8e27be0e0f01629fb2067ff7ec19ee00aca43f4
Thomas Hauth <thomas.hauth@kit.edu>
2015-08-07 14:19:35 +0000

    added the final keyword in the QuadTreeProcessorImplementation to aid the compiler with devirtualization.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20392 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/legendre/quadtree/include/QuadTreeProcessorImplementation.h
+++ tracking/trackFindingCDC/legendre/quadtree/include/QuadTreeProcessorImplementation.h
@@ -25,7 +25,7 @@ namespace Belle2 {
      * The two axis are the inverse slope in z direction and the z0.
      * We loop over the z0 and calculate the z-slope with the x-y-z-position the recoHit has.
      *  */
-    class StereoHitQuadTreeProcessor : public QuadTreeProcessorTemplate<float, float, const CDCRecoHit3D, 2, 2> {
+    class StereoHitQuadTreeProcessor final : public QuadTreeProcessorTemplate<float, float, const CDCRecoHit3D, 2, 2> {
 
     public:
       StereoHitQuadTreeProcessor(unsigned char lastLevel, const ChildRanges& ranges,
@@ -37,7 +37,7 @@ namespace Belle2 {
        * Do only insert the hit into a node if the slope and z information calculated from this hit belongs into this node
        */
       bool insertItemInNode(QuadTree* node, const CDCRecoHit3D* hit, unsigned int /*slope_index*/,
-                            unsigned int /*z0_index*/) const override
+                            unsigned int /*z0_index*/) const override final
       {
         float dist[2][2];
 
@@ -60,7 +60,7 @@ namespace Belle2 {
 
 
     /** A QuadTreeProcessor for RecoSegments */
-    class QuadTreeProcessorSegments : public QuadTreeProcessorTemplate<int, float, CDCRecoSegment2D, 2, 2> {
+    class QuadTreeProcessorSegments final : public QuadTreeProcessorTemplate<int, float, CDCRecoSegment2D, 2, 2> {
 
     public:
       /**
@@ -74,7 +74,8 @@ namespace Belle2 {
        * Insert only if the sinogram of the front or the back part of the segment goes through the node.
        * Also we can intersect the sinograms of the front and the back and get an intersection point, which should lay in the node also.
        */
-      bool insertItemInNode(QuadTree* node, CDCRecoSegment2D* recoItem, unsigned int /*t_index*/, unsigned int /*r_index*/) const override
+      bool insertItemInNode(QuadTree* node, CDCRecoSegment2D* recoItem, unsigned int /*t_index*/,
+                            unsigned int /*r_index*/) const override final
       {
         float dist[2][2];
 

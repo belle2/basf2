f0a8b1e99ad66ffa1d60c8f85fb0a6527e3dba59
Luka Santelj <luka.santelj@ijs.si>
2015-08-07 02:03:11 +0000

    Update of example file ARICHSteering.py
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20358 b38c6c8f-bee8-465d-8448-9abe6915d496

--- arich/examples/ARICHSteering.py
+++ arich/examples/ARICHSteering.py
@@ -4,140 +4,111 @@
 ##############################################################################
 #
 # This is an example steering file to run ARICH part of Belle2 simulation.
-# It uses ParicleGun module to generate tracks,
-# (see "generators/example/ParticleGunFull.py" for detailed usage)
-# needs reconstructed tracks(Tracks) extrapolated to ARICH (ExtHits).
-# The module builds geometry, performs geant4 simulation, does ARICH
-# reconstruction and stores output (for each track a value of likelihood
-# for different particle hypotheses) in an output root file
-# defined in RootOutput options below.
+# It builds inner detectors geometry (CDC,PXD,SVD,ARICH), uses ParticleGun module
+# to shoot particles, performs geant4 simulation, reconstructs tracks and performs
+# ARICH reconstruction. Output containing PID likelihood is by default stored in
+# 'ARICHEvents.root' file, additionally all objects from the DataStore
+# (like detector hits, MCParticles, etc.) can be stored by uncommenting few lines
+# near the end of this script
 #
+# 7/8/2015 Luka Santelj
 ##############################################################################
 
 from basf2 import *
+from optparse import OptionParser
+from simulation import add_simulation
+from reconstruction import add_mc_reconstruction
+import glob
+
+# Options from command line
+parser = OptionParser()
+parser.add_option('-n', '--nevents', dest='nevents', default=100,
+                  help='Number of events to process')
+parser.add_option('-f', '--file', dest='filename',
+                  default='ARICHEvents.root')
+parser.add_option('-d', '--debug', dest='debugLevel', default=10)
+(options, args) = parser.parse_args()
+nevents = int(options.nevents)
+filename = options.filename
+debugLevel = int(options.debugLevel)
 
 # suppress messages and warnings during processing DEBUG, INFO, WARNING, ERROR
-set_log_level(LogLevel.ERROR)
+set_log_level(LogLevel.INFO)
 
-# to run the framework the used modules need to be registered
+# Create path
+main = create_path()
 
-# Create Event information
+# specify number of events to be generated
 eventinfosetter = register_module('EventInfoSetter')
-# Show progress of processing
-progress = register_module('Progress')
-# Load parameters
-gearbox = register_module('Gearbox')
-# Create geometry
-geometry = register_module('Geometry')
+eventinfosetter.param({'evtNumList': [nevents], 'runList': [1]})
+main.add_module(eventinfosetter)
+
 # Particle gun module
 particlegun = register_module('ParticleGun')
-# Run simulation
-simulation = register_module('FullSim')
-# CDC digitizer
-cdcDigitizer = register_module('CDCDigitizer')
-# MC track finder (for simplicity)
-mctrackfinder = register_module('TrackFinderMCTruth')
-# Track fitting
-cdcfitting = register_module('GenFitter')
-# Track extrapolation
-ext = register_module('Ext')
-# ARICH digitization module
-arichDIGI = register_module('ARICHDigitizer')
-# ARICH reconstruction module
-arichRECO = register_module('ARICHReconstructor')
-# Save output of simulation
-output = register_module('RootOutput')
-
-# EventInfoSetter parameters
-# Set the number of events to be processed (10 event)
-eventinfosetter.param({'evtNumList': [100], 'runList': [1]})
-# ============================================================================
-
-# Set output filename
-output.param('outputFileName', 'ParticleGunOutput.root')
-# ============================================================================
-
-# ============================================================================
-# ParticleGun parameters
 # Setting the random seed for particle generation:
-set_random_seed(1028307)
+set_random_seed(123456)
 # Setting the list of particle codes (PDG codes) for the generated particles
-particlegun.param('pdgCodes', [-211, 211, 11, 321])
+# pions and kaons
+particlegun.param('pdgCodes', [-211, 211, 321, -321])
 # Setting the number of tracks to be generated per event:
 particlegun.param('nTracks', 1)
-# if you set nTracks to 0, then for each PDG code in pdgCodes list a track
-# will be generated on each event.
 # Setting the parameters for the random generation
 # of particles momenta:
 particlegun.param('momentumGeneration', 'uniform')
-particlegun.param('momentumParams', [0.5, 4])
+particlegun.param('momentumParams', [0.5, 3.5])
 # Setting the parameters for the random generation
 # of the particle polar angle:
 particlegun.param('thetaGeneration', 'uniformCos')
-particlegun.param('thetaParams', [17, 35])
+particlegun.param('thetaParams', [17.0, 35.0])
+particlegun.param('vertexGeneration', 'fixed')
+particlegun.param('xVertexParams', [0.0, 0.0])
+particlegun.param('yVertexParams', [0.0, 0.0])
+particlegun.param('zVertexParams', [0.0, 0.0])
+particlegun.param('independentVertices', False)
 # Print the parameters of the particle gun
 print_params(particlegun)
-# ============================================================================
-
-# ============================================================================
-# Geometry parameters
-# Select subdetectors to be built
-geometry.param('components', [
-    'MagneticField',
-    'BeamPipe',
-    'PXD',
-    'SVD',
-    'CDC',
-    'ARICH',
-])
-# If you comment this out all subdetectors will be built. If you want to
-# include just some of them do for example ['ARICH','TOP','CDC'].
-# ============================================================================
-
-# ============================================================================
-# Simulation parameters
-# To speed up the simulation you can propagate only a selected fraction of
-# photons.
-simulation.param('PhotonFraction', 0.3)
-# !!! NOTE: if you use ARICH digitization module this must be set to 0.3
-# since HAPD q.e. curve is scaled to that value
-# (will be solved in one of next releases)
-# Here you can select visualization driver and visualization commands.
-# You can use any visualization supported by geant4:
-# "http://geant4.web.cern.ch/geant4/UserDocumentation/UsersGuides/
-#  ForApplicationDeveloper/html/ch08.html"
-# Uncomment following lines  to create VRML file that can be examined with any
-# vrml viewer (freewrl,...)
-# simulation.param('EnableVisualization', True)
-# simulation.param('UICommands', ['/vis/open VRML2FILE', '/vis/drawVolume',
-#                            '/vis/scene/add/axes 0 0 0 100 mm',
-#                            '/vis/scene/add/trajectories smooth',
-#                            '/vis/modeling/trajectories/create/drawByCharge'])
-# =============================================================================
-
-# =============================================================================
-# ARICH Reconstruction parameters
-# arichRECO.logging.log_level = LogLevel.DEBUG
-arichRECO.param('inputTrackType', 0)
-arichRECO.param('storeHist', 10000)
-# =============================================================================
-
-# Do the simulation
-# =============================================================================
-main = create_path()
-main.add_module(eventinfosetter)
-main.add_module(progress)
-main.add_module(gearbox)
-main.add_module(geometry)
 main.add_module(particlegun)
-main.add_module(simulation)
-main.add_module(cdcDigitizer)
-main.add_module(mctrackfinder)
-main.add_module(cdcfitting)
+
+# Add simulation
+components = ['MagneticField', 'CDC', 'PXD', 'SVD', 'ARICH']
+# choose second line if you want to add background
+add_simulation(main, components)
+# add_simulation(main, components, bkgfiles=glob.glob('/sw/belle2/bkg/ARICH*.root'))
+
+# Add reconstruction
+components_nr = ['CDC', 'PXD', 'SVD']
+add_mc_reconstruction(main, components_nr)
+
+# Add track extrapolation module
+ext = register_module('Ext')
 main.add_module(ext)
-main.add_module(arichDIGI)
+
+# Add module that creates relation between ExtHits and ARICHAeroHits
+arichREL = register_module('ARICHRelate')
+main.add_module(arichREL)
+
+# Add ARICH reconstruction module
+arichRECO = register_module('ARICHReconstructor')
+# track information is obtain from tracking system
+# (set to 1 to use MC track information)
+arichRECO.param('inputTrackType', 0)
 main.add_module(arichRECO)
-main.add_module(output)
+
+# Add module fpr ARICH efficiency analysis
+arichEfficiency = register_module('ARICHAnalysis')
+arichEfficiency.logging.log_level = LogLevel.DEBUG
+arichEfficiency.logging.debug_level = debugLevel
+arichEfficiency.param('outputFile', filename)
+main.add_module(arichEfficiency)
+
+# Show progress of processing
+progress = register_module('Progress')
+main.add_module(progress)
+
+# uncomment for full DataStore outout
+# output = register_module('RootOutput')
+# output.param('outputFileName', 'FullOutput.root')
+# main.add_module(output)
 
 # Process events
 process(main)

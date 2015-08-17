6489e0f6195bf0a5327a462592eaa32d9381f474
Oliver Frost <oliver.frost@desy.de>
2015-08-17 14:36:21 +0000

    Rebased the phi0 curv hough on the general box division hough.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20658 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/hough/phi0_curv/include/Phi0CurvHough.h
+++ tracking/trackFindingCDC/hough/phi0_curv/include/Phi0CurvHough.h
@@ -12,8 +12,7 @@
 #include <tracking/trackFindingCDC/hough/perigee/DiscretePhi0.h>
 #include <tracking/trackFindingCDC/hough/perigee/DiscreteCurv.h>
 
-#include <tracking/trackFindingCDC/hough/WeightedFastHoughTree.h>
-#include <tracking/trackFindingCDC/hough/LinearDivision.h>
+#include <tracking/trackFindingCDC/hough/BoxDivisionHoughTree.h>
 #include <tracking/trackFindingCDC/hough/DiscreteAngles.h>
 
 #include <cmath>
@@ -26,19 +25,13 @@ namespace Belle2 {
     template<class ItemPtr,
              size_t phi0Divisions = 2, // Division at each level
              size_t curvDivisions = 2> // Division at each level
-    class Phi0CurvHough {
+    class Phi0CurvHough :
+      public BoxDivisionHoughTree<ItemPtr, Phi0CurvBox, phi0Divisions, curvDivisions> {
 
     public:
-      /// Type of the box division strategy
-      using Phi0CurvBoxDivision = LinearDivision<Phi0CurvBox, phi0Divisions, curvDivisions>;
+      /// Type of the base class
+      using Super = BoxDivisionHoughTree<ItemPtr, Phi0CurvBox, phi0Divisions, curvDivisions>;
 
-      /// Type of the fast hough tree structure
-      using Phi0CurvFastHoughTree = WeightedFastHoughTree<ItemPtr, Phi0CurvBox, Phi0CurvBoxDivision>;
-
-      /// Type of the nodes used in the tree for the search.
-      using Node = typename  Phi0CurvFastHoughTree::Node;
-
-    public:
       /// Constructor using the default values
       Phi0CurvHough() {;}
 
@@ -46,7 +39,7 @@ namespace Belle2 {
       Phi0CurvHough(size_t maxLevel,
                     double minCurv = -0.018,
                     double maxCurv = 0.13) :
-        m_maxLevel(maxLevel),
+        Super(maxLevel),
         m_minCurv(minCurv),
         m_maxCurv(maxCurv)
       {;}
@@ -55,93 +48,70 @@ namespace Belle2 {
       /// Initialise the algorithm by constructing the hough tree from the parameters
       virtual void initialize()
       {
-        // Setup the discrete values for phi0
-        const size_t nPhi0Bins = std::pow(phi0Divisions, m_maxLevel);
-        m_discretePhi0s = DiscretePhi0Array::forBinsWithOverlaps(nPhi0Bins,
-                                                                 m_discretePhi0Width,
-                                                                 m_discretePhi0Overlap);
+        {
+          size_t maxLevel = this->getMaxLevel();
 
-        // Setup the discrete values for the two dimensional curvature
-        assert(m_discreteCurvWidth > m_discreteCurvOverlap);
-        const size_t nCurvBins = std::pow(curvDivisions, m_maxLevel);
-        if (m_minCurv == 0) {
-          m_discreteCurvs =
-          DiscreteCurvArray::forPositiveBinsWithOverlap([](double curv) { return curv;},
-          m_maxCurv,
-          nCurvBins,
-          m_discreteCurvWidth,
-          m_discreteCurvOverlap);
-        } else {
-          m_discreteCurvs =
-          DiscreteCurvArray::forBinsWithOverlap([](double curv) { return curv;},
-          m_minCurv,
-          m_maxCurv,
-          nCurvBins,
-          m_discreteCurvWidth,
-          m_discreteCurvOverlap);
+          // Setup the discrete values for phi0
+          const size_t nPhi0Bins = std::pow(phi0Divisions, maxLevel);
+          DiscretePhi0Array discretePhi0s =
+            DiscretePhi0Array::forBinsWithOverlaps(nPhi0Bins,
+                                                   m_discretePhi0Width,
+                                                   m_discretePhi0Overlap);
+          this-> template assignArray<0>(std::move(discretePhi0s), m_discretePhi0Overlap);
+
+          // Setup the discrete values for the two dimensional curvature
+          assert(m_discreteCurvWidth > m_discreteCurvOverlap);
+          const size_t nCurvBins = std::pow(curvDivisions, maxLevel);
+          if (m_minCurv == 0) {
+            DiscreteCurvArray discreteCurvs =
+            DiscreteCurvArray::forPositiveBinsWithOverlap([](double curv) { return curv;},
+            m_maxCurv,
+            nCurvBins,
+            m_discreteCurvWidth,
+            m_discreteCurvOverlap);
+
+            this->template assignArray<1>(std::move(discreteCurvs), m_discreteCurvOverlap);
+
+          } else {
+            DiscreteCurvArray discreteCurvs =
+            DiscreteCurvArray::forBinsWithOverlap([](double curv) { return curv;},
+            m_minCurv,
+            m_maxCurv,
+            nCurvBins,
+            m_discreteCurvWidth,
+            m_discreteCurvOverlap);
+
+            this->template assignArray<1>(std::move(discreteCurvs), m_discreteCurvOverlap);
+          }
         }
 
-        // Compose the hough space
-        Phi0CurvBox phi0CurvHoughPlain(DiscretePhi0::getRange(m_discretePhi0s),
-                                       DiscreteCurv::getRange(m_discreteCurvs));
-
-        Phi0CurvBox::Delta phi0CurvOverlaps{m_discretePhi0Overlap, m_discreteCurvOverlap};
-        Phi0CurvBoxDivision phi0CurvBoxDivision(phi0CurvOverlaps);
-
-        m_phi0CurvFastHoughTree.reset(new Phi0CurvFastHoughTree(phi0CurvHoughPlain,
-                                                                phi0CurvBoxDivision));
-
+        const DiscretePhi0Array& discretePhi0s = this->template getArray<0>();
+        const DiscreteCurvArray& discreteCurvs = this->template getArray<1>();
 
         B2INFO("Initialize hough plan");
-        B2INFO("First curvature bin lower bound " << m_discreteCurvs[0].getValue());
-        B2INFO("First curvature bin upper bound " << m_discreteCurvs[m_discreteCurvWidth].getValue());
-        B2INFO("Curvature bin width " << m_discreteCurvs[m_discreteCurvWidth].getValue() - m_discreteCurvs[0].getValue());
-        B2INFO("Curvature bin overlap " << m_discreteCurvs[m_discreteCurvOverlap].getValue() - m_discreteCurvs[0].getValue());
+        B2INFO("First curvature bin lower bound " << discreteCurvs[0].getValue());
+        B2INFO("First curvature bin upper bound " << discreteCurvs[m_discreteCurvWidth].getValue());
+        B2INFO("Curvature bin width " << discreteCurvs[m_discreteCurvWidth].getValue() - discreteCurvs[0].getValue());
+        B2INFO("Curvature bin overlap " << discreteCurvs[m_discreteCurvOverlap].getValue() - discreteCurvs[0].getValue());
 
-        double phi0Width = m_discretePhi0s[m_discretePhi0Width].getAngleVec().phi() - m_discretePhi0s[0].getAngleVec().phi();
+        double phi0Width = discretePhi0s[m_discretePhi0Width].getAngle() - discretePhi0s[0].getAngle();
         // need modulo 2 * Pi because of the wrap around of the lowest bin.
         phi0Width = std::remainder(phi0Width, 2 * PI);
 
-        double phi0Overlap = m_discretePhi0s[m_discretePhi0Overlap].getAngleVec().phi() - m_discretePhi0s[0].getAngleVec().phi();
+        double phi0Overlap = discretePhi0s[m_discretePhi0Overlap].getAngleVec().phi() - discretePhi0s[0].getAngle();
         // need modulo 2 * Pi because of the wrap around of the lowest bin.
         phi0Overlap = std::remainder(phi0Overlap, 2 * PI);
 
         B2INFO("Phi0 bin width " << phi0Width);
         B2INFO("Phi0 bin overlap " << phi0Overlap);
+
+        Super::initialize();
       }
-
-      /// Prepare the leave finding by filling the top node with given hits
-      template<class ItemPtrs>
-      void seed(ItemPtrs& items)
-      {
-        if (not m_phi0CurvFastHoughTree) { initialize(); }
-        m_phi0CurvFastHoughTree->seed(items);
-      }
-
-      /// Terminates the processing by striping all hit information from the tree
-      virtual void fell()
-      { m_phi0CurvFastHoughTree->fell(); }
-
-      /// Release all memory that the tree aquired during the runs.
-      virtual void raze()
-      { m_phi0CurvFastHoughTree->raze(); }
-
-    public:
-      /// Getter for the tree used in the search in the hough plane.
-      Phi0CurvFastHoughTree* getTree() const
-      { return m_phi0CurvFastHoughTree.get(); }
-
-      /// Getter for the currently set maximal level
-      size_t getMaxLevel() const
-      { return m_maxLevel; }
 
     private:
       // Default parameters
       // TODO: Cross check this values with old implementation, room further tuning?
       // TODO: Expose these parameters to the constructor?
-
-      /// Maximal level of the tree to investigate
-      size_t m_maxLevel = 13;
 
       /// Overlap of the leaves in phi0 counted in number of discrete values.
       size_t m_discretePhi0Overlap = 1;
@@ -163,17 +133,6 @@ namespace Belle2 {
 
       /// Curvature below which a trajectory is considered non curling
       double m_curlCurv = 0.018;
-
-      // Dummy initialisation of the other constructs
-      /// Space for the discrete values that mark the usable bin bound in phi0
-      DiscretePhi0Array m_discretePhi0s{NAN, NAN, 1};
-
-      /// Space for the discrete values that mark the usable bin bound in the curvature
-      DiscreteCurvArray m_discreteCurvs{NAN, NAN, 1};
-
-      /// Dynamic hough tree structure traversed in the leave search.
-      std::unique_ptr<Phi0CurvFastHoughTree> m_phi0CurvFastHoughTree{nullptr};
-
     };
   }
 }

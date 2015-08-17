df69524170a88eca8bd5ef82f062a6e95b856157
Oliver Frost <oliver.frost@desy.de>
2015-08-17 14:50:45 +0000

    Adding a test for phi0 impact and curvature hough finding with segments on a more realistic event.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20706 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/hough/perigee/tests/Phi0ImpactCurv.test.cc
+++ tracking/trackFindingCDC/hough/perigee/tests/Phi0ImpactCurv.test.cc
@@ -188,4 +188,75 @@ namespace {
     saveDisplay(svgFileName);
     timeItResult.printSummary();
   }
+
+
+  TEST_F(TrackFindingCDCTestWithSimpleSimulation, hough_perigee_phi0_impact_curv_prepared_event_segments)
+  {
+    std::string svgFileName = "phi0_impact_curv_on_prepared_event_segments.svg";
+    loadPreparedEvent();
+    saveDisplay(svgFileName);
+
+    using SimpleSegmentPhi0ImpactCurvHoughTree =
+      SimpleSegmentHoughTree<InPhi0ImpactCurvBox, phi0Divisions, impactDivisions, curvDivisions>;
+
+    using HoughBox = SimpleSegmentPhi0ImpactCurvHoughTree::HoughBox;
+    SimpleSegmentPhi0ImpactCurvHoughTree houghTree(maxLevel);
+
+    houghTree.assignArray<DiscretePhi0>(phi0BinsSpec.constructArray(),
+                                        phi0BinsSpec.getNOverlap());
+    houghTree.assignArray<ContinuousImpact>({{minImpact, maxImpact}},
+    impactBinsSpec.getOverlap()); // Continuous
+    houghTree.assignArray<DiscreteCurv>(curvBinsSpec.constructArray(),
+                                        curvBinsSpec.getNOverlap());
+
+    houghTree.initialize();
+    const double minWeight = 30.0;
+
+    // Execute the finding a couple of time to find a stable execution time.
+    vector< pair<HoughBox, vector<const CDCRecoSegment2D*> > > candidates;
+
+    // Is this still C++? Looks like JavaScript to me :-).
+    TimeItResult timeItResult = timeIt(1, true, [&]() {
+      // Exclude the timing of the resource release for comparision with the legendre test.
+      houghTree.fell();
+      houghTree.seed(m_mcAxialSegment2Ds);
+
+      // candidates = houghTree.find(minWeight, maxCurvAcceptance);
+      candidates = houghTree.findBest(minWeight, maxCurvAcceptance);
+
+      ASSERT_EQ(m_mcTracks.size(), candidates.size());
+      // Check for the parameters of the track candidates
+      // The actual hit numbers are more than 30, but this is somewhat a lower bound
+      for (size_t iCand = 0; iCand < candidates.size(); ++iCand) {
+        EXPECT_GE(candidates[iCand].second.size(), 3);
+      }
+    });
+
+    /// Test idiom to output statistics about the tree.
+    std::size_t nNodes = houghTree.getTree()->getNNodes();
+    B2DEBUG(100, "Tree generated " << nNodes << " nodes");
+    houghTree.fell();
+    houghTree.raze();
+
+    size_t iColor = 0;
+    for (std::pair<HoughBox, std::vector<const CDCRecoSegment2D*> >& candidate : candidates) {
+      const HoughBox& houghBox = candidate.first;
+      const std::vector<const CDCRecoSegment2D*>& segments = candidate.second;
+
+      B2DEBUG(100, "Candidate");
+      B2DEBUG(100, "size " << segments.size());
+      B2DEBUG(100, "Phi0 " << houghBox.getLowerBound<DiscretePhi0>()->phi());
+      B2DEBUG(100, "Curv " << houghBox.getLowerBound<DiscreteCurv>());
+      B2DEBUG(100, "Impact " << houghBox.getLowerBound<ContinuousImpact>());
+
+      for (const CDCRecoSegment2D* recoSegment2D : segments) {
+        EventDataPlotter::AttributeMap strokeAttr {{"stroke", m_colors[iColor % m_colors.size()] }};
+        draw(*recoSegment2D, strokeAttr);
+      }
+      ++iColor;
+    }
+    saveDisplay(svgFileName);
+    timeItResult.printSummary();
+  }
+
 }

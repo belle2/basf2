7f2533579cf5be04192cbd7b96913cccce1537c7
Oliver Frost <oliver.frost@desy.de>
2015-08-17 14:36:07 +0000

    Factored out the general box division part from the phi0 curv hough.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20657 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingCDC/hough/include/BoxDivisionHoughTree.h
@@ -0,0 +1,179 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+#include <tracking/trackFindingCDC/hough/perigee/DiscretePhi0.h>
+#include <tracking/trackFindingCDC/hough/perigee/DiscreteCurv.h>
+#include <tracking/trackFindingCDC/hough/WeightedFastHoughTree.h>
+#include <tracking/trackFindingCDC/hough/LinearDivision.h>
+
+#include <tracking/trackFindingCDC/utilities/GenIndices.h>
+
+#include <tuple>
+#include <array>
+#include <memory>
+#include <cmath>
+
+namespace Belle2 {
+  namespace TrackFindingCDC {
+
+    /** A fast hough algorithm with rectangular boxes, which are split linearly by a fixed number of
+     *  divisions in each coordinate up to a maximal level. */
+    template<class ItemPtr,
+             class HoughBox,
+             size_t ... divisions>
+    class BoxDivisionHoughTree {
+
+    public:
+      /// Type of the box division strategy
+      using BoxDivision = LinearDivision<HoughBox, divisions...>;
+
+      /// Type of the fast hough tree structure
+      using HoughTree = WeightedFastHoughTree<ItemPtr, HoughBox, BoxDivision>;
+
+      /// Type of the width in coordinate I.
+      template<size_t I>
+      using Width = typename HoughBox::template Width<I>;
+
+      /// Type of the nodes used in the tree for the search.
+      using Node = typename  HoughTree::Node;
+
+    public:
+      /// Constructor using the given maximal level.
+      template<class ... RangeSpecOverlap>
+      BoxDivisionHoughTree(size_t maxLevel) :
+        m_maxLevel(maxLevel),
+        m_overlaps((divisions * 0) ...),
+        m_arrays{ { -PI, PI, static_cast<size_t>(std::pow(divisions, m_maxLevel))} ... }
+      {;}
+
+    private:
+      /// Type of the discrete value array to coordinate index I.
+      template<size_t I>
+      using Array = typename std::tuple_element<I, typename HoughBox::Point>::type::Array;
+
+      /// Tuple type of the discrete value arrays
+      using Arrays = MapGenIndices<Array, sizeof...(divisions)>;
+
+    public:
+      /// Getter the number of divisions at each level for coordinate index I.
+      size_t getDivision(const size_t i) const
+      { return m_divisions[i]; }
+
+      /** Construct the discrete value array at coordinate index I
+       *  @param lowerBound  Lower bound of the value range
+       *  @param upperBound  Upper bound of the value range
+       *  @param overlap     Overlap of neighboring bins. Default is no overlap.
+       *                     Usuallly this is counted in number of discrete values
+       *  @param width       Width of the bins at lowest level. Default is width of 1.
+       *                     Usually this is counted in numbers of discrete values
+       */
+      template<size_t I>
+      void constructArray(const double& lowerBound,
+                          const double& upperBound,
+                          const Width<I>& overlap = 0,
+                          Width<I> width = 0)
+      {
+        const size_t division = getDivision(I);
+        const size_t nBins = std::pow(division, m_maxLevel);
+
+        if (width == 0) {
+          width = overlap + 1;
+        }
+        B2ASSERT("Width " << width << "is not bigger than overlap " << overlap, overlap < width);
+
+        const auto nPositions = (width - overlap) * nBins + overlap + 1;
+        std::get<I>(m_arrays) = Array<I>(lowerBound, upperBound, nPositions);
+        std::get<I>(m_overlaps) = overlap;
+
+        const Array<I>& array  = std::get<I>(m_arrays);
+        using ValueType = typename Array<I>::ValueType;
+
+        B2INFO("Constructing array for coordinate " << I);
+        B2INFO("Lower value " << ValueType(array.front()));
+        B2INFO("Upper value " << ValueType(array.back()));
+      }
+
+      /// Provide an externally constructed array
+      template<size_t I>
+      void assignArray(Array<I> array, Width<I> overlap)
+      {
+        // Double move assignment
+        std::get<I>(m_arrays) = std::move(array);
+        std::get<I>(m_overlaps) = std::move(overlap);
+      }
+
+    public:
+      /// Initialise the algorithm by constructing the hough tree from the parameters
+      virtual void initialize()
+      {
+        // Compose the hough space
+        HoughBox houghPlane = constructHoughPlane();
+        BoxDivision boxDivision(m_overlaps);
+        m_houghTree.reset(new HoughTree(houghPlane, boxDivision));
+      }
+
+      /// Prepare the leave finding by filling the top node with given hits
+      template<class ItemPtrs>
+      void seed(ItemPtrs& items)
+      {
+        if (not m_houghTree) { initialize(); }
+        m_houghTree->seed(items);
+      }
+
+      /// Terminates the processing by striping all hit information from the tree
+      virtual void fell()
+      { m_houghTree->fell(); }
+
+      /// Release all memory that the tree aquired during the runs.
+      virtual void raze()
+      { m_houghTree->raze(); }
+
+    public:
+      /// Getter for the tree used in the search in the hough plane.
+      HoughTree* getTree() const
+      { return m_houghTree.get(); }
+
+      /// Getter for the currently set maximal level
+      size_t getMaxLevel() const
+      { return m_maxLevel; }
+
+      /// Getter for the array of discrete value for coordinate I.
+      template<size_t I>
+      const Array<I>& getArray() const
+      { return std::get<I>(m_arrays); }
+
+    private:
+      /// Construct the box of the top node of the tree. Implementation unroling the indices.
+      template<size_t ... Is>
+      HoughBox constructHoughPlaneImpl(const IndexSequence<Is...>&)
+      { return HoughBox{ HoughBox::template Type<Is>::getRange(getArray<Is>())... }; }
+
+      /// Construct the box of the top node of the tree.
+      HoughBox constructHoughPlane()
+      { return constructHoughPlaneImpl(GenIndices<sizeof ...(divisions)>()); }
+
+    private:
+      /// Number of the maximum tree level.
+      size_t m_maxLevel;
+
+      /// Array of the number of divisions at each level
+      const std::array<size_t, sizeof ...(divisions)> m_divisions{{divisions ...}};
+
+      /// An tuple of division overlaps in each coordinate.
+      typename HoughBox::Delta m_overlaps;
+
+      /// A tuple of value arrays providing the memory for the discrete bin bounds.
+      Arrays m_arrays;
+
+      /// Dynamic hough tree structure traversed in the leaf search.
+      std::unique_ptr<HoughTree> m_houghTree{nullptr};
+    };
+  }
+}

6919957315268130cacc7021ffdd439d72b0ee54
Alexei Sibidanov <Alexei.Sibidanov@sydney.edu.au>
2015-08-21 01:13:55 +0000

    DspSamplingArray is replaced by ECLDigitizerModule::signalsample_t::Accumulate member function. It should be much faster in calculating hit response.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20874 b38c6c8f-bee8-465d-8448-9abe6915d496

--- ecl/digitization/include/algorithms.h
+++ ecl/digitization/include/algorithms.h
@@ -5,7 +5,6 @@ namespace Belle2 {
     double Sv123(double t, double t01, double tb1, double t02, double tb2, double td1, double ts1);
     double ShaperDSP(double time);
     double ShaperDSP_F(double time, float* parameters);
-    double DspSamplingArray(int templateSignalNBins, double time, double templateSignalBinLen, const double* templateFun);
     void shapeFitter(short int* id, int* f, int* f1, int* fg41, int* fg43, int* fg31, int* fg32, int* fg33, int* y,
                      int* ttrig2, int* n16,  int* lar, int* ltr, int* lq);
   }

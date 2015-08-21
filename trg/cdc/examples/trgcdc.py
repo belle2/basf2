1c0605f08ab72c200ec2abf92b4111a0456b3f95
YunTsung Lai <ytlai@hep1.phys.ntu.edu.tw>
2015-08-21 07:37:34 +0000

    files restored
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20883 b38c6c8f-bee8-465d-8448-9abe6915d496

--- trg/cdc/examples/trgcdc.py
+++ trg/cdc/examples/trgcdc.py
@@ -21,20 +21,20 @@ basf2datadir = os.path.join(os.environ.get('BELLE2_LOCAL_DIR', None), 'data')
 
 ##########################################################
 # Register modules
-evtmetagen        = register_module('EventInfoSetter')
-evtmetainfo       = register_module('Progress')
-paramloader       = register_module('Gearbox')
-geobuilder        = register_module('Geometry')
-particlegun       = register_module('ParticleGun')
-evtgeninput       = register_module('EvtGenInput')
-kkgeninput        = register_module('KKGenInput')
+evtmetagen = register_module('EventInfoSetter')
+evtmetainfo = register_module('Progress')
+paramloader = register_module('Gearbox')
+geobuilder = register_module('Geometry')
+particlegun = register_module('ParticleGun')
+evtgeninput = register_module('EvtGenInput')
+kkgeninput = register_module('KKGenInput')
 mcparticleprinter = register_module('PrintMCParticles')
-g4sim             = register_module('FullSim')
-bkgmixer          = register_module('BeamBkgMixer')
-cdcdigitizer      = register_module('CDCDigitizer')
-cdctrg            = register_module("TRGCDC")
-rootOut           = register_module('RootOutput')
-rootIn            = register_module('RootInput')
+g4sim = register_module('FullSim')
+bkgmixer = register_module('BeamBkgMixer')
+cdcdigitizer = register_module('CDCDigitizer')
+cdctrg = register_module("TRGCDC")
+rootOut = register_module('RootOutput')
+rootIn = register_module('RootInput')
 
 
 ##########################################################
@@ -42,15 +42,14 @@ rootIn            = register_module('RootInput')
 
 #...Global settings...
 #simulatedComponents = ['MagneticField', 'CDC'
-#                 ,'PXD', 'SVD', 'BeamPipe'
-#                ]
+# ,'PXD', 'SVD', 'BeamPipe'
+# ]
 simulatedComponents = ['MagneticField', 'CDC'
-                ]
-
+]
 
 #...EventInfoSetter...
 # Set number of events and runs
-evtmetagen.param({'evtNumList': [1000], 'runList': [1]})
+evtmetagen.param({'evtNumList': [10], 'runList': [1]})
 
 #...Geometry...
 # Set what dectectors to simulate.
@@ -65,7 +64,7 @@ particlegun.param('nTracks', 1)
 particlegun.param('momentumGeneration', 'uniformPt')
 #particlegun.param('momentumGeneration', 'uniform')
 #particlegun.param('momentumParams', [0.2,0.4])
-particlegun.param('momentumParams', [1.0, 3.0])
+particlegun.param('momentumParams', [0.9, 0.9])
 #particlegun.param('thetaGeneration', 'uniform')
 #particlegun.param('thetaParams', [35, 127])
 particlegun.param('thetaParams', [90, 90])
@@ -95,37 +94,37 @@ mcparticleprinter.param('maxLevel',-1)
 
 #...FullSim...
 # Turn off physics processes
-#    "physics.mac" is located at "trg/examples/".
+# "physics.mac" is located at "trg/examples/".
 #g4sim.param('UICommands',['/control/execute physics.mac'])
 # or below line can be used when trgcdc.py is not in trg/examples directory //JB
 g4sim.param('UICommands',['/control/execute ' + os.path.join(os.environ.get('BELLE2_LOCAL_DIR', None),"trg/cdc/examples/physics.mac")])
 
 #...BeamBkgMixer...
 # Mix background (From beamBkgMixer.py)
-dir = '/sw/belle2/bkg/'  # change the directory name if you don't run on KEKCC
+dir = '/sw/belle2/bkg/' # change the directory name if you don't run on KEKCC
 #bkg_files = [
-#    dir + 'Coulomb_HER_100us.root',
-#    dir + 'Coulomb_LER_100us.root',
-#    dir + 'RBB_HER_100us.root',
-#    dir + 'RBB_LER_100us.root',
-#    dir + 'Touschek_HER_100us.root',
-#    dir + 'Touschek_LER_100us.root',
+# dir + 'Coulomb_HER_100us.root',
+# dir + 'Coulomb_LER_100us.root',
+# dir + 'RBB_HER_100us.root',
+# dir + 'RBB_LER_100us.root',
+# dir + 'Touschek_HER_100us.root',
+# dir + 'Touschek_LER_100us.root',
 #]
 bkg_files = glob.glob(dir + '/*.root')
 bkgScaleFactor = 1
 bkgmixer.param('backgroundFiles', bkg_files)
 bkgmixer.param('components', simulatedComponents)
-bkgmixer.param('scaleFactors', [('Coulomb_LER', bkgScaleFactor), ('Coulomb_HER', bkgScaleFactor) 
-                             ,('RBB_LER', bkgScaleFactor), ('RBB_HER', bkgScaleFactor)
-                             ,('Touschek_LER',bkgScaleFactor), ('Touschek_HER', bkgScaleFactor)
-                             ])
+bkgmixer.param('scaleFactors', [('Coulomb_LER', bkgScaleFactor), ('Coulomb_HER', bkgScaleFactor)
+,('RBB_LER', bkgScaleFactor), ('RBB_HER', bkgScaleFactor)
+,('Touschek_LER',bkgScaleFactor), ('Touschek_HER', bkgScaleFactor)
+])
 
 #...CDCDigitizer...
 #set digitizer to no smearing
 param_cdcdigi = {'Fraction': 1,
-                 'Resolution1': 0.,
-                 'Resolution2': 0.,
-                 'Threshold': -10.0}
+'Resolution1': 0.,
+'Resolution2': 0.,
+'Threshold': -10.0}
 cdcdigitizer.param(param_cdcdigi)
 cdcdigitizer.param('AddInWirePropagationDelay', True)
 cdcdigitizer.param('AddTimeOfFlight', True)
@@ -137,7 +136,7 @@ cdcdigitizer.param('UseSimpleDigitization', True)
 cdctrg.param('ConfigFile', os.path.join(basf2datadir,"trg/cdc/TRGCDCConfig_0_20101111_1051_2013beamtest.dat"))
 #cdctrg.param('DebugLevel', 1)
 cdctrg.param('CurlBackStop', 1)
-cdctrg.param('SimulationMode', 1)        # 1:fast, 2:firm, 3:fast and firm
+cdctrg.param('SimulationMode', 1) # 1:fast, 2:firm, 3:fast and firm
 cdctrg.param('FastSimulationMode', 0)
 #cdctrg.param('SimulationMode',0x11)
 #cdctrg.param('TRGCDCRootFile',1)
@@ -147,18 +146,15 @@ cdctrg.param('FastSimulationMode', 0)
 cdctrg.param('InnerTSLUTFile', os.path.join(basf2datadir,"trg/cdc/innerLUT_v2.2.coe"))
 cdctrg.param('OuterTSLUTFile', os.path.join(basf2datadir,"trg/cdc/outerLUT_v2.2.coe"))
 # 0: (Default) Logic TSF, 1: LUT TSF
-cdctrg.param('TSFLogicLUT', 1)
+#cdctrg.param('TSFLogicLUT', 1)
 #cdctrg.param('TSFRootFile',1)
 #---2D finder settings---
 cdctrg.param('HoughFinderMappingFileMinus', os.path.join(basf2datadir,"trg/cdc/HoughMappingMinus20140808.dat"))
-#cdctrg.param('HoughFinderMappingFilePlus', os.path.join(basf2datadir,"trg/cdc/HoughMappingPlus20140807.dat"))
-cdctrg.param('HoughFinderMappingFilePlus', os.path.join(basf2datadir,"trg/cdc/Plus_v3.dat"))
-#cdctrg.param('HoughFinderMappingFilePlus', os.path.join(basf2datadir,"trg/cdc/HoughMappingPlus20150731.dat"))
-cdctrg.param('2DFinderPerfect',0)
-cdctrg.param('HoughFinderMeshX',160)
-cdctrg.param('HoughFinderMeshY',26)
-cdctrg.param('HoughFinderPeakMin',5)
-#cdctrg.param('HoughFinderDoit', 4)
+cdctrg.param('HoughFinderMappingFilePlus', os.path.join(basf2datadir,"trg/cdc/HoughMappingPlus20140807.dat"))
+cdctrg.param('2DFinderPerfect',1)
+#cdctrg.param('HoughFinderMeshX',160)
+#cdctrg.param('HoughFinderMeshY',26)
+#cdctrg.param('HoughFinderPeakMin',4)
 #---3D finder settings---
 #cdctrg.param('Hough3DRootFile',1)
 # 0: perfect finder, 1: Hough3DFinder, 2: (Default) GeoFinder, 3: VHDL GeoFinder
@@ -176,7 +172,7 @@ rootIn.param('inputFileName', 'basf2.root')
 
 
 ##########################################################
-#  Path settings.
+# Path settings.
 
 # For full simulation.
 fullMain = create_path()
@@ -190,7 +186,7 @@ fullMain.add_module(particlegun)
 #fullMain.add_module(kkgeninput)
 fullMain.add_module(mcparticleprinter)
 fullMain.add_module(g4sim)
-#fullMain.add_module(bkgmixer)
+fullMain.add_module(bkgmixer)
 fullMain.add_module(cdcdigitizer)
 fullMain.add_module(cdctrg)
 
@@ -259,3 +255,4 @@ process(fullMain)
 
 # Print call statistics
 print statistics
+

a8f8f661f6ac5e46cab4a89e6532d966414b7690
Oliver Frost <oliver.frost@desy.de>
2015-08-23 22:24:10 +0000

    Early access axial hough finder module over three coordinate (phi0, curvature and d0) using segments.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@20914 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/validation/cosmicsAxialHoughTrackingValidation.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+<header>
+  <contact>tracking@belle2.kek.jp</contact>
+  <description>Validates the axial hough finder working on segments in cosmics events.</description>
+</header>
+"""
+
+VALIDATION_OUTPUT_FILE = 'CosmicsAxialHoughTrackingValidation.root'
+N_EVENTS = 1000
+ACTIVE = False
+
+import basf2
+basf2.set_random_seed(1337)
+
+import os
+import trackfindingcdc.cdcdisplay as cdcdisplay
+
+import logging
+
+from tracking.validation.run import TrackingValidationRun
+
+
+class CosmicsAxialHough(TrackingValidationRun):
+    n_events = N_EVENTS
+    generator_module = 'Cosmics'
+    components = ['CDC', 'MagneticFieldConstant4LimitedRCDC']
+
+    segment_finder_module = basf2.register_module('SegmentFinderCDCFacetAutomaton')
+    segment_finder_module.param("SegmentOrientation", "downwards")
+
+    axial_hough_module = basf2.register_module('TrackFinderCDCAxialSegmentHough')
+    finder_module = [segment_finder_module, axial_hough_module, ]
+
+    del segment_finder_module  # do not let the names show up in the class name space
+    del axial_hough_module  # do not let the names show up in the class name space
+
+    interactive_display = False
+    if interactive_display:
+        cdc_display_module = cdcdisplay.CDCSVGDisplayModule(os.getcwd(), interactive=True)
+
+        cdc_display_module.draw_gftrackcands = True
+        cdc_display_module.draw_gftrackcand_trajectories = True
+        finder_module.append(cdc_display_module)
+        del cdc_display_module  # do not let the names show up in the class name space
+
+    tracking_coverage = {'UsePXDHits': False, 'UseSVDHits': False,
+                         'UseCDCHits': True, 'UseOnlyAxialCDCHits': True}
+    fit_geometry = None
+    pulls = True
+    output_file_name = VALIDATION_OUTPUT_FILE
+
+
+def main():
+    validation_run = CosmicsAxialHough()
+    validation_run.configure_and_execute_from_commandline()
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    if ACTIVE:
+        main()

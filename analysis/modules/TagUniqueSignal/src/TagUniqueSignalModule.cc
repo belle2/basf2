0d1fb437f7a180506973d888af470b1c2c330e0f
Thomas Keck <t.keck@online.de>
2015-08-30 20:44:19 +0000

    Hopefully fix automaticReporting MVA ranking generation on KEKCC and switched back to kfitter as standard fitter for FEI
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@21072 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/modules/TagUniqueSignal/src/TagUniqueSignalModule.cc
+++ analysis/modules/TagUniqueSignal/src/TagUniqueSignalModule.cc
@@ -64,7 +64,7 @@ void TagUniqueSignalModule::event()
         extraInfoValue = 1.0;
     }
     if (part->hasExtraInfo(m_extraInfoName)) {
-      B2FATAL("Extra Info with given name is already set! This module can only be used once per particle.");
+      B2WARNING("Extra Info with given name is already set! This module can only be used once per particle.");
     } else {
       part->addExtraInfo(m_extraInfoName, extraInfoValue);
     }

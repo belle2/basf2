36403a8f412f1db2c7225507ce73d39511aabf59
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2015-09-02 02:38:26 +0000

    Update DB
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@21131 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/extra/epics/nsm2cad/iocBoot/iocnsm2cad/st.cmd
+++ daq/slc/extra/epics/nsm2cad/iocBoot/iocnsm2cad/st.cmd
@@ -12,10 +12,10 @@ dbLoadDatabase "dbd/nsm2cad.dbd"
 nsm2cad_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-#dbLoadRecords("db/nsmnode.db","nodename=ROPC501")
+dbLoadRecords("db/nsmnode.db","nodename=PXD")
 #dbLoadRecords("db/nsmnode.db","nodename=STORAGE")
 #dbLoadRecords("db/ropc.db","dataname=ROPC501_STATUS")
-dbLoadRecords("db/nsmdata_storage.db","dataname=STORE01C_STATUS")
+#dbLoadRecords("db/nsmdata_storage.db","dataname=STORE01C_STATUS")
 
 nsmConfig("NSM2CA","ropc02.b2nsm.kek.jp.", 9122)
 

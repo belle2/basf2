bb288427156909b98ffec3b77fa0a6068d759dcc
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2015-09-04 15:32:28 +0000

    Silence warnings when reading ModuleStatistics::m_stats from root5
    Note: objects are empty, but they are of very minor importance and not loaded by default.
    The rule is applied properly, but somehow just doesn't read any data.
    
    Fixes #1675
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@21250 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/core/include/linkdef.h
+++ framework/core/include/linkdef.h
@@ -13,4 +13,19 @@
 #pragma link C++ class Belle2::Environment+;
 #pragma link C++ class Belle2::RandomGenerator+;
 
+//avoid warning when reading m_stats written by root 5
+//(which is saved with wrong type Belle2::ModuleStatistics::CalcMeanCov<2,value_type>)
+//NOTE: It still cannot be read, and the resulting objects will be empty.
+#pragma read sourceClass="Belle2::ModuleStatistics" \
+  source="Belle2::CalcMeanCov<2, double> m_stats[6]" \
+  targetClass="Belle2::ModuleStatistics" target="m_stats" \
+  code="{ \
+    m_stats[0] = onfile.m_stats[0]; \
+    m_stats[1] = onfile.m_stats[1]; \
+    m_stats[2] = onfile.m_stats[2]; \
+    m_stats[3] = onfile.m_stats[3]; \
+    m_stats[4] = onfile.m_stats[4]; \
+    m_stats[5] = onfile.m_stats[5]; \
+  }"
+
 #endif

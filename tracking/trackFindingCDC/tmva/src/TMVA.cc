988b1a26028053ae792c8b754c52f283c2aa32ca
Thomas Hauth <thomas.hauth@kit.edu>
2015-09-04 12:32:24 +0000

    added correct namespace qualifiers because ROOT 6 is more strict with plugin method signatures
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@21236 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/tmva/src/TMVA.cc
+++ tracking/trackFindingCDC/tmva/src/TMVA.cc
@@ -11,8 +11,8 @@ namespace Belle2 {
         std::string regexp2 = std::string(".*") + name + std::string(".*");
         std::string className = std::string("TMVA::Method") + name;
         std::string pluginName = std::string("TMVA") + name;
-        std::string ctor1 = std::string("Method") + name + std::string("(DataSetInfo&,TString)");
-        std::string ctor2 = std::string("Method") + name + std::string("(TString&,TString&,DataSetInfo&,TString&)");
+        std::string ctor1 = std::string("Method") + name + std::string("(TMVA::DataSetInfo&,TString)");
+        std::string ctor2 = std::string("Method") + name + std::string("(TString&,TString&,TMVA::DataSetInfo&,TString&)");
 
         gPluginMgr->AddHandler(base.c_str(), regexp1.c_str(), className.c_str(), pluginName.c_str(), ctor1.c_str());
         gPluginMgr->AddHandler(base.c_str(), regexp2.c_str(), className.c_str(), pluginName.c_str(), ctor2.c_str());

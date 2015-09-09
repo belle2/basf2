5127322c6dc9ff0a2a4134e122bebad24ec97022
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2015-09-09 11:25:47 +0000

    add test for -i & --dry-run
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@21349 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/tests/basf2_args.py
+++ framework/tests/basf2_args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from basf2 import *
+from ROOT import Belle2
 import os
 
 assert 0 == os.system('basf2 --info')
@@ -12,6 +14,13 @@ assert 0 == os.system('basf2 --help')
 assert 0 == os.system('basf2 -m')
 
 assert 0 == os.system('basf2 -m RootOutput')
+
+testdir = Belle2.FileSystem.findFile('/framework/tests/')
+os.chdir(testdir)
+assert 0 == os.system('basf2 root_input.py --dry-run -i miep.root')
+assert 0 == os.system('basf2 --dry-run root_input.py')
+# TODO: check output from --dry-run to see if it changes
+
 
 # some failing things
 # exact return code of system() not defined, just unsuccesful

40785ec603c9983c20a04b03c76ec964381cdfd3
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2015-09-17 12:06:51 +0000

    minor doc changes
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@21550 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/utility/include/MCMatching.h
+++ analysis/utility/include/MCMatching.h
@@ -26,10 +26,10 @@ namespace Belle2 {
     enum MCErrorFlags {
       c_Correct             = 0,  /**< This Particle and all its daughters are perfectly reconstructed. */
       c_MissFSR             = 1, /**< A Final State Radiation (FSR) photon is not reconstructed. Note that distinction from c_MissGamma is based only on number of other daughters and may be wrong. */
-      c_MissingResonance    = 2, /**< The associated MCParticle decay contained additional particles (e.g. a rho) that weren't reconstructed. This is probably O.K. in most cases*/
+      c_MissingResonance    = 2, /**< The associated MCParticle decay contained additional non-final-state particles (e.g. a rho) that weren't reconstructed. This is probably O.K. in most cases*/
       c_DecayInFlight       = 4, /**< A Particle was reconstructed from the secondary decay product of the actual particle. This means that a wrong hypothesis was used to reconstruct it, which e.g. for tracks might mean a pion hypothesis was used for a secondary electron. */
-      c_MissNeutrino        = 8, /**< A Neutrino is missing (not reconstructed). */
-      c_MissGamma           = 16, /**< A Photon (not FSR) is missing (not reconstructed). */
+      c_MissNeutrino        = 8, /**< A neutrino is missing (not reconstructed). */
+      c_MissGamma           = 16, /**< A photon (not FSR) is missing (not reconstructed). */
       c_MissMassiveParticle = 32, /**< A generated massive FSP is missing (not reconstructed). */
       c_MissKlong           = 64, /**< A Klong is missing (not reconstructed). */
       c_MisID               = 128, /**< One of the charged final state particles is mis-identified. */

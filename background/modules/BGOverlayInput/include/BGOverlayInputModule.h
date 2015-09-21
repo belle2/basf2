a82a3c3b4f2ad2172274a4f149cf464572e8c7ed
Marko Staric <marko.staric@ijs.si>
2015-09-21 14:23:14 +0000

    Reader for BG overlay data.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@21603 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ background/modules/BGOverlayInput/include/BGOverlayInputModule.h
@@ -0,0 +1,89 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Marko Staric                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+#include <framework/core/Module.h>
+#include <framework/datastore/DataStore.h>
+#include <string>
+#include "TChain.h"
+
+namespace Belle2 {
+
+  /**
+   * Beam BG data input, either in form of Digits or raw data.
+   * Both kinds of data are kept at the DataStore in a separate StoreArrays
+   */
+  class BGOverlayInputModule : public Module {
+
+  public:
+
+    /**
+     * Constructor
+     */
+    BGOverlayInputModule();
+
+    /**
+     * Destructor
+     */
+    virtual ~BGOverlayInputModule();
+
+    /**
+     * Initialize the Module.
+     * This method is called at the beginning of data processing.
+     */
+    virtual void initialize();
+
+    /**
+     * Called when entering a new run.
+     * Set run dependent things like run header parameters, alignment, etc.
+     */
+    virtual void beginRun();
+
+    /**
+     * Event processor.
+     */
+    virtual void event();
+
+    /**
+     * End-of-run action.
+     * Save run-related stuff, such as statistics.
+     */
+    virtual void endRun();
+
+    /**
+     * Termination action.
+     * Clean-up, close files, summarize statistics, etc.
+     */
+    virtual void terminate();
+
+  private:
+
+    typedef std::vector<DataStore::StoreEntry*> StoreEntries;   /**< DataStore entries */
+
+    /**
+     * Connect branches of the given event tree to the data store,
+     * except EventMetaData and all relations
+     *
+     * @return True if the branches are connected successfully.
+     */
+    bool connectBranches();
+
+
+    std::vector<std::string> m_inputFileNames; /**< list of file names */
+    TChain* m_tree = 0;            /**< tree pointer */
+    unsigned m_numEvents = 0;      /**< number of events (tree entries) in the sample */
+    unsigned m_eventCount = 0;     /**< current event (tree entry) */
+    StoreEntries m_storeEntries;   /**< store entries with connected branches */
+
+  };
+
+} // Belle2 namespace
+
---
+++ background/modules/BGOverlayInput/src/BGOverlayInputModule.cc
@@ -0,0 +1,202 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Marko Staric                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+// Own include
+#include <background/modules/BGOverlayInput/BGOverlayInputModule.h>
+
+#include <framework/core/ModuleManager.h>
+
+// framework - DataStore
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/StoreObjPtr.h>
+
+// framework aux
+#include <framework/gearbox/Unit.h>
+#include <framework/gearbox/Const.h>
+#include <framework/logging/Logger.h>
+
+// root
+#include <framework/io/RootIOUtilities.h>
+#include <TClonesArray.h>
+#include <TFile.h>
+
+
+#include <iostream>
+
+using namespace std;
+
+namespace Belle2 {
+
+  using namespace RootIOUtilities;
+
+  //-----------------------------------------------------------------
+  //                 Register module
+  //-----------------------------------------------------------------
+
+  REG_MODULE(BGOverlayInput)
+
+  //-----------------------------------------------------------------
+  //                 Implementation
+  //-----------------------------------------------------------------
+
+  BGOverlayInputModule::BGOverlayInputModule() : Module()
+
+  {
+    // module description
+    setDescription("Input for BG overlay, either in form of Digits or raw data.");
+
+    // Add parameters
+    addParam("inputFileNames", m_inputFileNames,
+             "List of files with measured beam background ");
+
+  }
+
+  BGOverlayInputModule::~BGOverlayInputModule()
+  {
+  }
+
+  void BGOverlayInputModule::initialize()
+  {
+
+    loadDictionaries();
+
+    // expand possible wildcards
+    m_inputFileNames = expandWordExpansions(m_inputFileNames);
+    if (m_inputFileNames.empty()) {
+      B2FATAL("No valid files specified!");
+    }
+
+    // check files
+    TDirectory* dir = gDirectory;
+    for (const string& fileName : m_inputFileNames) {
+      TFile* f = TFile::Open(fileName.c_str(), "READ");
+      if (!f or !f->IsOpen()) {
+        B2FATAL("Couldn't open input file " + fileName);
+      }
+      delete f;
+    }
+    dir->cd();
+
+    // get event TTree
+    m_tree = new TChain(c_treeNames[DataStore::c_Event].c_str());
+    for (const string& fileName : m_inputFileNames) {
+      m_tree->AddFile(fileName.c_str());
+    }
+    m_numEvents = m_tree->GetEntries();
+    if (m_numEvents == 0) B2ERROR(c_treeNames[DataStore::c_Event] << " has no entires");
+    m_eventCount = 0;
+
+    // connect selected branches to DataStore
+    bool ok = connectBranches();
+    if (!ok) {
+      B2ERROR("No branches found to be connected");
+    }
+
+  }
+
+
+  void BGOverlayInputModule::beginRun()
+  {
+  }
+
+
+  void BGOverlayInputModule::event()
+  {
+
+    for (auto entry : m_storeEntries) {
+      entry->resetForGetEntry();
+    }
+
+    m_tree->GetEntry(m_eventCount);
+    m_eventCount++;
+    if (m_eventCount >= m_numEvents) {
+      m_eventCount = 0;
+      B2INFO("BGOverlayInput: events for BG overlay will be re-used");
+    }
+
+    for (auto entry : m_storeEntries) {
+      if (entry->object) {
+        entry->ptr = entry->object;
+      } else {
+        entry->recoverFromNullObject();
+        entry->ptr = 0;
+      }
+    }
+
+  }
+
+
+  void BGOverlayInputModule::endRun()
+  {
+  }
+
+  void BGOverlayInputModule::terminate()
+  {
+
+    delete m_tree;
+
+  }
+
+
+  bool BGOverlayInputModule::connectBranches()
+  {
+
+    auto durability = DataStore::c_Event;
+    auto storeFlags = DataStore::c_DontWriteOut | DataStore::c_ErrorIfAlreadyRegistered;
+    auto& map = DataStore::Instance().getStoreEntryMap(durability);
+
+    const TObjArray* branches = m_tree->GetListOfBranches();
+    if (!branches) return false;
+
+    for (int jj = 0; jj < branches->GetEntriesFast(); jj++) {
+      TBranch* branch = static_cast<TBranch*>(branches->At(jj));
+      if (!branch) continue;
+      const std::string branchName = branch->GetName();
+
+      TObject* objectPtr = 0;
+      branch->SetAddress(&objectPtr);
+      branch->GetEntry();
+      bool array = (string(branch->GetClassName()) == "TClonesArray");
+      if (!array) {               // only arrays will be read-in
+        m_tree->SetBranchStatus(branchName.c_str(), 0);
+        branch->ResetAddress();
+        delete objectPtr;
+        continue;
+      }
+      TClass* objClass = (static_cast<TClonesArray*>(objectPtr))->GetClass();
+      branch->ResetAddress();
+      delete objectPtr;
+
+      const std::string className = objClass->GetName();
+      if (className.find("Belle2::") == std::string::npos) { // only Belle2 classes
+        m_tree->SetBranchStatus(branchName.c_str(), 0);
+        continue;
+      }
+
+      std::string name = branchName + "_BG";
+      bool ok = DataStore::Instance().registerEntry(name, durability, objClass,
+                                                    array, storeFlags);
+      if (!ok) {
+        m_tree->SetBranchStatus(branchName.c_str(), 0);
+        continue;
+      }
+      DataStore::StoreEntry& entry = (map.find(name))->second;
+      m_tree->SetBranchAddress(branchName.c_str(), &(entry.object));
+      m_storeEntries.push_back(&entry);
+
+    }
+
+    return !m_storeEntries.empty();
+  }
+
+
+} // end Belle2 namespace
+

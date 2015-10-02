c4fea2e5e5a844b9fb01e1ea15823cd65b0ae3e8
Martin Ritter <martin.ritter@lmu.de>
2015-10-02 13:27:23 +0000

    python3: override print() function to flush by default
    Python3 does not flush after each print statement which makes the order
    of C++ and python output inconsistent. Fix this by flushing by default.
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@21876 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/scripts/basf2.py
+++ framework/scripts/basf2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # -----------------------------------------------
@@ -10,3 +10,20 @@ from basf2_env import *
 #    Import global basf2 function definitions
 # -----------------------------------------------
 from basf2_def import *
+
+# -----------------------------------------------------
+# replace print function with a flushing print function
+# -----------------------------------------------------
+
+import builtins
+
+
+def flush_print(*args, **argk):
+    """print function which flushes by default"""
+    argk.setdefault("flush", True)
+    builtins._print(*args, **argk)
+
+# remember original print function
+builtins._print = print
+# and replace it with the flushing one
+builtins.print = flush_print

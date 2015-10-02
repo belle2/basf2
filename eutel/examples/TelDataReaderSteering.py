6448e08822a117fbb08fb9d7af0d17fd28de75ab
Martin Ritter <martin.ritter@lmu.de>
2015-10-02 13:42:29 +0000

    python3: fix eutel package (2to3 + fixstyle)
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@21894 b38c6c8f-bee8-465d-8448-9abe6915d496

--- eutel/examples/TelDataReaderSteering.py
+++ eutel/examples/TelDataReaderSteering.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from basf2 import *
 
 # Set the log level to show only warning, error and, fatal messages
--- eutel/examples/TelDataReaderTestSteering.py
+++ eutel/examples/TelDataReaderTestSteering.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from basf2 import *
 
 set_log_level(LogLevel.INFO)
@@ -6,7 +9,7 @@ set_log_level(LogLevel.INFO)
 dataReader = register_module('TelDataReader')
 # use 4394 or 3 or (better 313)
 p_dataReader = {'inputFileName': '/home/benjamin/TB_2014_VXD/run000005.raw',
-                  'maxNumEntries': 15000}
+                'maxNumEntries': 15000}
 dataReader.param(p_dataReader)
 
 # data reader test/validation

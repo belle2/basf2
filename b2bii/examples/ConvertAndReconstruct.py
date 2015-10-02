7213e89bc9c7c0ac541cb846ffb46225ac72ad01
Martin Ritter <martin.ritter@lmu.de>
2015-10-02 13:31:40 +0000

    python3: fix b2bii package (2to3)
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@21883 b38c6c8f-bee8-465d-8448-9abe6915d496

--- b2bii/examples/ConvertAndReconstruct.py
+++ b2bii/examples/ConvertAndReconstruct.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import os
@@ -85,4 +85,4 @@ analysis_main.add_module(progress)
 process(analysis_main)
 
 # Print call statistics
-print statistics
+print(statistics)

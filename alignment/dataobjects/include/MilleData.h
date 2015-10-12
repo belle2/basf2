535dea870081e6b2bfda6237324e2586970f194c
Tadeas Bilka <tadeas.bilka@gmail.com>
2015-10-12 20:18:07 +0000

    Move definitions into source. Remove using namespace std from header.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@22118 b38c6c8f-bee8-465d-8448-9abe6915d496

--- alignment/dataobjects/include/MilleData.h
+++ alignment/dataobjects/include/MilleData.h
@@ -2,17 +2,11 @@
 
 #include <string>
 #include <vector>
-#include <map>
 
 #include <genfit/MilleBinary.h>
 #include <genfit/GblTrajectory.h>
-#include <framework/utilities/FileSystem.h>
 #include <framework/pcore/Mergeable.h>
 
-#include <TCollection.h>
-#include <TFileMergeInfo.h>
-
-using namespace std;
 namespace Belle2 {
   /// Mergeable class holding list of so far opened mille binaries and providing the binaries
   class MilleData : public Mergeable {
@@ -23,63 +17,27 @@ namespace Belle2 {
     virtual ~MilleData() { close(); }
 
     /// Implementation of merging
-    virtual void merge(const Mergeable* other)
-    {
-      auto* data = static_cast<const MilleData*>(other);
-      const vector<string>& files = data->getFiles();
-      for (auto& file : files) {
-        bool exists = false;
-        for (auto& myfile : m_files) {
-          if (myfile == file) {
-            exists = true;
-            break;
-          }
-        }
-        if (!exists)
-          m_files.push_back(file);
-      }
-    }
+    virtual void merge(const Mergeable* other);
     /// Implementation of clearing
     virtual void clear() { m_files.clear(); }
 
     /// Open a new file and remember it. Filename should encode also process id!
-    void open(string filename)
-    {
-      if (m_binary) {
-        close();
-      }
-      m_binary = new gbl::MilleBinary(filename);
-      m_files.push_back(filename);
-    }
+    void open(std::string filename);
     /// Is some file already open?
     bool isOpen() { return !!m_binary; }
-    void fill(gbl::GblTrajectory& trajectory)
-    {
-      if (m_binary) {
-        trajectory.milleOut(*m_binary);
-      }
-    }
+    /// Write a GBL trajectory to the binary file
+    void fill(gbl::GblTrajectory& trajectory);
     /// Close current mille binary if opened
-    void close()
-    {
-      if (m_binary) {
-        delete m_binary;
-        m_binary = nullptr;
-      }
-    }
+    void close();
     /// Get the list of all created files
-    const vector<string>& getFiles() const { return m_files; }
+    const std::vector<std::string>& getFiles() const { return m_files; }
     /// Copy by assignment
-    MilleData& operator=(const MilleData& other)
-    {
-      close();
-      m_files = other.m_files;
-      return *this;
-    }
+    MilleData& operator=(const MilleData& other);
     /// Construct from other object
     MilleData(const MilleData& other) : m_files(other.m_files), m_binary(nullptr) {}
   private:
-    vector<string> m_files = {}; /**< List of already created file names */
+    std::vector<std::string> m_files = {}; /**< List of already created file names */
+    /// Pointer to current binary file
     gbl::MilleBinary* m_binary = nullptr; //! Pointer to opened binary file (not streamed)
 
     ClassDef(MilleData, 1) /**< Mergeable list of opened mille binaries */
--- alignment/dataobjects/src/MilleData.cc
+++ alignment/dataobjects/src/MilleData.cc
@@ -1,5 +1,56 @@
 #include <alignment/dataobjects/MilleData.h>
 
+#include <framework/utilities/FileSystem.h>
+
+using namespace std;
 using namespace Belle2;
 
-ClassImp(MilleData)
+void MilleData::merge(const Mergeable* other)
+{
+  auto* data = static_cast<const MilleData*>(other);
+  const vector<string>& files = data->getFiles();
+  for (auto& file : files) {
+    bool exists = false;
+    for (auto& myfile : m_files) {
+      if (myfile == file) {
+        exists = true;
+        break;
+      }
+    }
+    if (!exists)
+      m_files.push_back(file);
+  }
+}
+
+MilleData& MilleData::operator=(const MilleData& other)
+{
+  close();
+  m_files = other.m_files;
+  return *this;
+}
+void MilleData::close()
+{
+  if (m_binary) {
+    delete m_binary;
+    m_binary = nullptr;
+  }
+}
+
+void MilleData::fill(gbl::GblTrajectory& trajectory)
+{
+  if (m_binary) {
+    trajectory.milleOut(*m_binary);
+  }
+}
+
+void MilleData::open(string filename)
+{
+  if (m_binary) {
+    close();
+  }
+  m_binary = new gbl::MilleBinary(filename);
+  m_files.push_back(filename);
+}
+
+
+

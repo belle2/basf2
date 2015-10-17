c67d2acc46d3a077d4e123040affe2276005b073
Thomas Hauth <thomas.hauth@kit.edu>
2015-10-17 11:40:41 +0000

    changed the cdc packer\unpacker test to use the full CDC mapping file, removed usage of the CDC Hit filter and sort hits before comparison.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@22247 b38c6c8f-bee8-465d-8448-9abe6915d496

--- cdc/tests/cdc_packer_unpacker.py
+++ cdc/tests/cdc_packer_unpacker.py
@@ -1,26 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-##############################################################################
-# This steering file which shows all usage options for the particle gun module
-# in the generators package. The generated particles from the particle gun are
-# then fed through a full Geant4 simulation and the output is stored in a root
-# file.
-#
-# The different options for the particle gun are explained below.
-# Uncomment/comment different lines to get the wanted settings
-#
-# Example steering file - 2012 Belle II Collaboration
-##############################################################################
-
 from basf2 import *
 from ROOT import Belle2
-import os
 
 import simulation
 
-cdc_mapping_file = "cdc/examples/ch_map.dat"
-cdc_hits_filtered = "CDCHits_filtered"
+cdc_mapping_file = "cdc/data/ch_map.dat"
+cdc_hits = "CDCHits"
 cdc_hits_pack_unpack_collection = "CDCHits_test_output"
 set_random_seed(42)
 
@@ -31,12 +18,32 @@ class PackerUnpackerTestModule(Module):
     module which checks if two collection of CDCHits are equal
     """
 
+    def sortHits(self, unsortedPyStoreArray):
+        """ use a some information to sort the CDCHits list
+            Returns a python-list containing the CDCHits
+        """
+
+        # first convert to a python-list to be able to sort
+        py_list = [x for x in unsortedPyStoreArray]
+
+        # sort via a hierachy of sort keys
+        return sorted(
+            py_list,
+            key=lambda x: (
+                x.getILayer(),
+                x.getISuperLayer(),
+                x.getIWire()))
+
     def event(self):
         # load the cdcHits and the collection which results from the packer and unpacker
         # processed by packer and unpacker
-        cdcHitsPackedUnpacked = Belle2.PyStoreArray(cdc_hits_pack_unpack_collection)
+        cdcHitsPackedUnpacked_unsorted = Belle2.PyStoreArray(cdc_hits_pack_unpack_collection)
         # direct from simulation
-        cdcHits = Belle2.PyStoreArray(cdc_hits_filtered)
+        cdcHits_unsorted = Belle2.PyStoreArray(cdc_hits)
+
+        # sort the hits, because they have been shuffled in the packing/unpacking process
+        cdcHitsPackedUnpacked = self.sortHits(cdcHitsPackedUnpacked_unsorted)
+        cdcHits = self.sortHits(cdcHits_unsorted)
 
         if not len(cdcHits) == len(cdcHitsPackedUnpacked):
             B2FATAL("CDC Hit count not equal after packing and unpacking")
@@ -54,53 +61,26 @@ class PackerUnpackerTestModule(Module):
             assert hit.getADCCount() == hitPackedUnpacked.getADCCount()
             assert hit.getTDCCount2ndHit() == hitPackedUnpacked.getTDCCount2ndHit()
             assert hit.getStatus() == hitPackedUnpacked.getStatus()
-        pass
+
+main = create_path()
+# Create Event information
+eventinfosetter = register_module('EventInfoSetter')
+eventinfosetter.param({'evtNumList': [10], 'runList': [1]})
+main.add_module(eventinfosetter)
 
 # to run the framework the used modules need to be registered
 particlegun = register_module('ParticleGun')
 particlegun.param('pdgCodes', [13, -13])
-
-# limit the range of the particle gun to a 20 degree segment
-# because only this range is covered by the mapping file
-# cdc/examples/ch_map.dat yet.
-# This limiation can be removed as soon as the mapping files contains all
-# super layers
-particlegun.param('phiGeneration', 'uniform')
-particlegun.param('phiParams', [0, 20])
-
 particlegun.param('nTracks', 10)
-
-# Create Event information
-eventinfosetter = register_module('EventInfoSetter')
-eventinfosetter.param({'evtNumList': [10], 'runList': [1]})
-# Show progress of processing
-progress = register_module('Progress')
-
-main = create_path()
-# init path
-main.add_module(eventinfosetter)
 main.add_module(particlegun)
+
 # add simulation for CDC only
 simulation.add_simulation(main, components=['CDC'])
-
-main.add_module(progress)
-
-# the range of super layers/layers/wire, which are listed
-# in the cdc/examples/ch_map.dat mapping file yet.
-# This limiation can be removed as soon as the mapping files contains all
-# super layers
-filter_module = register_module('CDCHitFilter')
-filter_module.param("inputCDCHitListName", "CDCHits")
-filter_module.param("outputCDCHitListName", cdc_hits_filtered)
-filter_module.param("filterSuperLayer", 4)
-filter_module.param("filterLayerMax", 5)
-filter_module.param("filterWireMax", 15)
-main.add_module(filter_module)
 
 # add the packer which packs the CDCHits resulting from the simulation
 cdc_packer = register_module('CDCPacker')
 cdc_packer.param('xmlMapFileName', cdc_mapping_file)
-cdc_packer.param('cdcHitName', "CDCHits_filtered")
+cdc_packer.param('cdcHitName', "CDCHits")
 main.add_module(cdc_packer)
 
 # add the unpacker which unpacks the RawCDC hits and stores

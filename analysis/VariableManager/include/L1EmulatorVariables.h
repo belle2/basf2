1c11f06696d23379164c60210038afbbc3566fb9
Chunhua Li <chunhua.li@unimelb.edu.au>
2015-10-28 10:56:46 +0000

    add variables
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@22487 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/VariableManager/include/L1EmulatorVariables.h
+++ analysis/VariableManager/include/L1EmulatorVariables.h
@@ -18,172 +18,10 @@ namespace Belle2 {
   namespace Variable {
 
     /**
-     * The variables used in LE are all defined based on the
+     * The vetos used in LE are all defined based on the
      * the good charged tracks and the ECL clusters passing the selection
      * criteria
      */
-
-
-    /**
-     * return the number of the charged tracks
-     */
-    double nTracksLE(const Particle*);
-
-
-    /**
-     * return the number of the tracks matched to ECL Clusters
-     */
-    double nECLMatchTracksLE(const Particle*);
-
-
-    /**
-     * return the number of the tracks matched to KLM Clusters
-     */
-    double nKLMMatchTracksLE(const Particle*);
-
-    /** trk1 is the track with the largest momentum*/
-    /** trk2 is the track with the second largest momentum*/
-
-    /**
-     * return trk1's momentum
-     */
-    double P1BhabhaLE(const Particle*);
-
-    /**
-     * return trk2's momentum
-     */
-    double P2BhabhaLE(const Particle*);
-
-    /**
-     * return trk2's polar angle
-     */
-    double Theta2BhabhaLE(const Particle*);
-
-    /**
-     * return trk2's azimuthal angle
-     */
-    double Phi2BhabhaLE(const Particle*);
-
-
-    /**
-     * return trk1's polar angle
-     */
-    double Theta1BhabhaLE(const Particle*);
-
-    /**
-     * return trk1's azimuthal angle
-     */
-    double Phi1BhabhaLE(const Particle*);
-
-    /**
-     * return the energy of ECL cluster matched to trk1
-     */
-    double E1BhabhaLE(const Particle*);
-
-    /**
-     * return the energy of ECL cluster matched to trk2
-     */
-    double E2BhabhaLE(const Particle*);
-
-    /**
-     * return trk1's charge
-     */
-    double Charge1BhabhaLE(const Particle*);
-
-
-    /**
-     * return trk2's charge
-     */
-    double Charge2BhabhaLE(const Particle*);
-
-    /**
-     * return the maximum angle between tracks
-     */
-    double maxAngleTTLE(const Particle*);
-    /**
-     * return the number of the ECL clusters
-     */
-    double nClustersLE(const Particle*);
-
-    /**
-     * return the sum energy of the  ECL clusters
-     */
-    double EtotLE(const Particle*);
-
-    /**
-     * return the number of KLM Clusters
-     */
-    double nKLMClustersLE(const Particle*);
-
-
-    /**C1 is the most energnetic ECL Cluster*/
-    /**C2 is the second most energnetic ECL Cluster*/
-
-    /**
-     * return C1's energy
-     */
-    double EC1LE(const Particle*);
-
-
-    /**
-     * return C1's polar angle
-     */
-    double ThetaC1LE(const Particle*);
-
-    /**
-     * return C1's azimuthal angle
-     */
-    double PhiC1LE(const Particle*);
-
-    /**
-     * return the distance C1 and IP
-     */
-    double RC1LE(const Particle*);
-
-    /**
-     * return C2's energy
-     */
-    double EC2LE(const Particle*);
-
-    /**
-     * return C2's polar angle
-     */
-    double ThetaC2LE(const Particle*);
-
-    /**
-     * return C2's azimuthal angle
-     */
-    double PhiC2LE(const Particle*);
-
-    /**
-     * return the distance C2 and IP
-     */
-    double RC2LE(const Particle*);
-
-    /**
-     * return C1
-     */
-    const ECLCluster*  ECLClusterC1LE(const Particle*);
-
-    /**
-     * return C2
-     */
-    const ECLCluster*  ECLClusterC2LE(const Particle*) ;
-
-    /**
-     * return the angle between C1 and C2
-     */
-    double AngleGGLE(const Particle*);
-
-    /**
-     * return trk1
-     */
-    const Particle* T1(const Particle*);
-
-    /**
-    * return trk2
-    */
-    const Particle* T2(const Particle*);
 
     /**
     * return ecl Bhabha veto result
@@ -196,9 +34,15 @@ namespace Belle2 {
     double BhabhaVeto(const Particle*);
 
     /**
+    * return Bhabha veto result with single track
+    */
+    double SBhabhaVeto(const Particle*);
+
+    /**
     * return gg veto result
     */
     double ggVeto(const Particle*);
+
 
   }
 

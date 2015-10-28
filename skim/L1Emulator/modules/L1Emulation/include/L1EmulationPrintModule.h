f6acecd3c9e02efd1a71305651806f877ce85ca2
Chunhua Li <chunhua.li@unimelb.edu.au>
2015-10-28 11:05:39 +0000

    print the result of L1 trigger
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@22503 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ skim/L1Emulator/modules/L1Emulation/include/L1EmulationPrintModule.h
@@ -0,0 +1,59 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2014 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Chunhua LI                                               *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef L1EMULATIONPRINTMODULE_H
+#define L1EMULATIONPRINTMODULE_H
+
+#include <framework/core/Module.h>
+#include <string>
+#include <analysis/dataobjects/Particle.h>
+#include <analysis/VariableManager/Utility.h>
+
+
+namespace Belle2 {
+  /**
+   * physics trigger
+   */
+
+  class L1EmulationPrintModule : public Module {
+
+  public:
+
+    /**
+     * Constructor: Sets the description, the properties and the parameters of the module.
+     */
+    L1EmulationPrintModule();
+
+    /** Destructor */
+    virtual ~L1EmulationPrintModule();
+
+    /** Initialize the parameters */
+    virtual void initialize();
+
+
+    /** Event processor. */
+    virtual void event();
+
+    /** Termination action. */
+    virtual void terminate();
+
+  private:
+    /**the sum of the weight of the processed events*/
+    double m_weightcount;
+
+    /**the sum of the weight of the triggered events*/
+    double m_weightcounttrigger;
+
+    /**the number of trigger pathes*/
+    int m_ntrg;
+  };
+}
+
+#endif /* L1EMULATIONPRINTMODULE_H */

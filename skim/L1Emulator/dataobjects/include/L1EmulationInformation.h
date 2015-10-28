995c61f9ae85b09176bebdbfdaf5d5eb8a97d312
Chunhua Li <chunhua.li@unimelb.edu.au>
2015-10-28 11:03:51 +0000

    change the structure of the module
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@22501 b38c6c8f-bee8-465d-8448-9abe6915d496

--- skim/L1Emulator/dataobjects/include/L1EmulationInformation.h
+++ skim/L1Emulator/dataobjects/include/L1EmulationInformation.h
@@ -22,16 +22,27 @@ namespace Belle2 {
 
   public:
 
-    L1EmulationInformation(): m_eclBhabha(0), m_BhabhaVeto(0), m_ggVeto(0)
+    L1EmulationInformation(): m_eclBhabha(0), m_BhabhaVeto(0), m_SBhabhaVeto(0), m_ggVeto(0), m_ntrg(0), m_weight(0.)
     {
-      m_summary.clear();
+      for (int i = 0; i < 50; i++) {
+        m_subweight[i] = 0;
+        m_summary[i] = 0;
+      }
     }
 
     ~L1EmulationInformation() {}
 
 //setters
     /**set trigger results*/
-    void setSummary(int Summary) {m_summary.push_back(Summary);}
+    void setSummary(int i, double Summary) {m_summary[i] = Summary;}
+    /**set total weight*/
+    void settotWeight(double w) {m_weight = w;}
+
+    /**set total weight of sub trigger*/
+    void setsubWeight(int i, double w) { m_subweight[i] = w;}
+
+    /**set the number of triggers*/
+    void setnTrg(int n = 1) {m_ntrg += n;}
 
     /**set ecl Bhabha veto*/
     void setECLBhabha(int eclBhabha) {m_eclBhabha = eclBhabha;}
@@ -39,11 +50,14 @@ namespace Belle2 {
     /**set Bhabha veto*/
     void setBhabhaVeto(int BhabhaVeto) {m_BhabhaVeto = BhabhaVeto;}
 
+    /**set Bhabha veto*/
+    void setSBhabhaVeto(int SBhabhaVeto) {m_SBhabhaVeto = SBhabhaVeto;}
+
     /**set diphoton veto*/
     void setggVeto(int ggVeto) {m_ggVeto = ggVeto;}
 
     /**get trigger results*/
-    std::vector<int> getSummary() const {return m_summary;}
+    int getSummary(int i) const {return m_summary[i];}
 
     /**get ecl Bhabha veto*/
     int getECLBhabha() const {return m_eclBhabha;}
@@ -51,8 +65,20 @@ namespace Belle2 {
     /**get Bhabha veto*/
     int getBhabhaVeto() const {return m_BhabhaVeto;}
 
+    /**get SBhabha veto*/
+    int getSBhabhaVeto() const {return m_SBhabhaVeto;}
+
     /**get diphoton veto*/
     int getggVeto() const {return m_ggVeto;}
+
+    /**get the numbre of triggers*/
+    int getnTrg() const {return m_ntrg;}
+
+    /**get total weight*/
+    double gettotWeight() const {return m_weight;}
+
+    /**get total weight of sub trigger*/
+    double getsubWeight(int i) const {return m_subweight[i];}
 
   private:
 
@@ -62,13 +88,25 @@ namespace Belle2 {
     /**Bhabha veto by using CDC and ECL information*/
     int m_BhabhaVeto;
 
-    /**diphoton veto by using CDC and ECL information*/
+    /**single track Bhabha veto by using CDC and ECL information*/
+    int m_SBhabhaVeto;
+
+    /**gamma gamma veto by using CDC and ECL information*/
     int m_ggVeto;
 
     /**the trigger results*/
-    std::vector<int> m_summary;
+    double m_summary[50];
 
-    ClassDef(L1EmulationInformation, 1)
+    /**the number of triggers*/
+    int m_ntrg;
+
+    /**the total weight*/
+    double m_weight;
+
+    /**the sub weight*/
+    double m_subweight[50];
+
+    ClassDef(L1EmulationInformation, 2)
   };
 
 }

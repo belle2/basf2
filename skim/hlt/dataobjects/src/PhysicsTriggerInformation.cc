4597a570d1ff736b11ff73bb3005e1126665bd02
Chunhua Li <chunhua.li@unimelb.edu.au>
2015-10-28 11:00:11 +0000

    change the structure of the module
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@22494 b38c6c8f-bee8-465d-8448-9abe6915d496

--- skim/hlt/dataobjects/src/PhysicsTriggerInformation.cc
+++ skim/hlt/dataobjects/src/PhysicsTriggerInformation.cc
@@ -13,6 +13,16 @@
 //using namespace std;
 using namespace Belle2;
 
-ClassImp(PhysicsTriggerInformation)
+PhysicsTriggerInformation::PhysicsTriggerInformation(): m_ngoodTracks(0), m_ngoodECLClusters(0), m_Pzvis(0.), m_Evis(0.),
+  m_Esum(0.),
+  m_MaxAngleTT(-10.), m_AngleTT(-10.), m_AngleGG(-10.), m_P1(0.), m_P2(0.), m_E1(0.), m_E2(0.), m_nKLMClusters(0), m_ptsummary(0),
+  m_ntottrg(0)
+{
+  for (int i = 0; i < 16; i++) {
+    m_trgresult[i] = 0;
+    m_nsubtrg[i] = 0;
+  }
+}
+//ClassImp(PhysicsTriggerInformation)
 
 

e93340f31409b78cd6fcb993d31dfe1336f51f78
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2015-11-02 05:37:03 +0000

    Update path to pgsql for external
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@22671 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/setenv.sh
+++ daq/slc/setenv.sh
@@ -3,9 +3,16 @@
 export BELLE2_DAQ_SLC=$PWD
 export PATH=$BELLE2_DAQ_SLC/bin:$PATH
 if [ -z ${BELLE2_LOCAL_DIR+x} ]; then
+  if [ -d $BELLE2_EXTERNALS_DIR/lib/$BELLE2_EXTERNALS_SUBDIR/ ]; then
+    export PGSQL_LIB_PATH=$BELLE2_EXTERNALS_DIR/lib/$BELLE2_EXTERNALS_SUBDIR
+  else 
+    export PGSQL_LIB_PATH=$BELLE2_EXTERNALS_DIR/$BELLE2_EXTERNALS_SUBDIR/lib
+  fi
+  export PGSQL_INC_PATH=$BELLE2_EXTERNALS_DIR/include/pgsql
   export LD_LIBRARY_PATH=$BELLE2_DAQ_SLC/lib:$LD_LIBRARY_PATH
 else
-  export PGSQL_LIB_PATH=/usr/pgsql-9.3/
+  export PGSQL_LIB_PATH=/usr/pgsql-9.3/lib
+  export PGSQL_INC_PATH=/usr/pgsql-9.3/include
   export LD_LIBRARY_PATH=$BELLE2_DAQ_SLC/lib:$PGSQL_LIB_PATH/lib:$LD_LIBRARY_PATH
 fi
 export NSM2_INCDIR=$BELLE2_DAQ_SLC/data/nsm

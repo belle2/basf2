187adec042d8ebcb2dab70197df24aa88169bbad
Chunhua Li <chunhua.li@unimelb.edu.au>
2015-11-06 10:20:19 +0000

    remove useless lines and add trigger pathes
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@22845 b38c6c8f-bee8-465d-8448-9abe6915d496

--- skim/L1Emulator/examples/L1emulator_example_usercustom.py
+++ skim/L1Emulator/examples/L1emulator_example_usercustom.py
@@ -21,7 +21,9 @@ emptypath = create_path()
 
 filelistSIG = \
     [
-        '../../../../release-head-head/MC/Analysis_L1emulator/MC/tauTomunn-tau1-prong/BGx0-noVXD/rootFiles/*root'
+        #    '../../../../release-head-head/MC/Analysis_L1emulator/MC/tauTomunn-tau1-prong/BGx0-noVXD/rootFiles/*root'
+        '../../../../release-head-head/MC/Analysis_L1emulator/MC/Bhabha/BGx0-noVXD/rootFiles/*root'
+        #    '../../../../release-head-head/MC/Analysis_L1emulator/MC/eeee_pre/BGx0-noVXD/rootFiles-nopreselection/*root'
     ]
 
 inputMdstList(filelistSIG)
@@ -33,14 +35,30 @@ fillParticleList('pi+:HLT', 'pt>0.2')
 fillParticleList('gamma:HLT', 'E>0.1')
 
 
-# add_L1Emulation(analysis_main)
-# Users can develop their own trugger path as shown below
+# Users can develop their own trigger path as shown below
+# The four veto logics  eclBhabha, BhabhaVeto, SBhabhaVeto, and ggVeto have to be defined firstly.
 
-# Define the veto logic firstly, user can also define their own veto logic. The first four trigger pathes
-# are defined as eclBhabha, BhabhaVeto, SBhabhaVeto, and ggVeto by default.
-L1Emualtion_Veto(analysis_main)
+# eclBhabhaVetoHLT
+L1Emulation('eclBhabhaVeto', 'EC1HLT>0 and EC2HLT>0.0 and EC12HLT>7.0 and AngGGHLT>100', analysis_main, [1])
 
-# Then user could define the trigger path, the total pathes should be less than 50
+# BhabhaVetoHLT, the logic "eclBhabhaVetoHLT" could is available here
+L1Emulation('BhabhaVeto', 'eclBhabhaVetoHLT==1 and NtHLT==2 and NtcHLT==2', analysis_main, [1])
+
+# Bhabha veto with single track
+L1Emulation(
+    'SBhabhaVeto',
+    'EC1HLT > 0 and EC2HLT > 0.0 and EC12HLT > 5.0 and AngGGHLT > 100 and' +
+    ' NtHLT == 1 and NtcHLT == 1 and ENeutralHLT > 0.5 and PT1HLT > 2.5 and ET1HLT > 2.0',
+    analysis_main,
+    [1])
+
+# ggVetoHLT
+L1Emulation('ggVeto', 'eclBhabhaVetoHLT==1 and NtHLT==0', analysis_main, [1])
+
+# User could also use the following default veto definations instead of the above four custom definations.
+# L1Emulation_Veto(analysis_main)
+
+# Then user could define the physical trigger path, the total pathes should be less than 50
 # parameters 1: the name of the path given by user 2: the trigger logic 3: the path  4: the scale factor. if there
 # is no scale for the path, set [1]. if you set the array with more than one component, e.g. [1000,200,50,10], the
 # trigger path will be scaled as a function of the election's theta in lab frame by default.
@@ -48,6 +66,7 @@ L1Emulation('TwoTracks', 'NltHLT==2 and BhabhaVetoHLT==0', analysis_main, [1])
 L1Emulation('TwoTracksScale', 'NltHLT==2', analysis_main, [2000])
 L1Emulation('OneTrkOneMu', 'NtHLT>=1 and Layer1KLMHLT>=3 and maxAngTMHLT>45', analysis_main, [1])
 L1Emulation('Hadron1', 'NtHLT>=3 and NltHLT>=2', analysis_main, [1])
+L1Emulation('Neutral', 'NcHLT>=3 and AngGGHLT>20 and AngGGHLT<170 and eclBhabhaVetoHLT==0', analysis_main, [1])
 
 # Finally, load L1EmulationPrint module to print the reigger result on screen.
 L1EmulationPrint(analysis_main)
@@ -59,27 +78,12 @@ toolsY = ['EventMetaData', 'pi+']
 # Summary[1],[2],[3],[4] are corresponding to the trigger result of eclBhabha, BhabhaVeto, SBhabhaVeto, and ggVeto.
 # Summary[5-50] are the trigger results of the trigger pathes.
 toolsY += ['LETRG', 'pi+']
-# The CDC, ECL, and KLM information used in TRGi are stored in Ntuples LECDC, LEECL, LEKLM
+# The CDC, ECL, and KLM information are stored in Ntuples LECDC, LEECL, LEKLM
 # The defination of variables in these ntuples could be found in link
 toolsY += ['LECDC', 'pi+']
 toolsY += ['LEECL', 'pi+']
 toolsY += ['LEKLM', 'pi+']
 
-
-recotools = [
-    'EventMetaData',
-    '^pi+',
-    'Kinematics',
-    '^pi+',
-    'MCKinematics',
-    '^pi+',
-    'Track',
-    '^pi+',
-    'MCTruth',
-    '^pi+',
-    'MCKinematics',
-    '^pi+',
-]
 
 # These ntuples are event level based, the parameters 'pi+' means nothing here.
 ntupleFile('BB.root')

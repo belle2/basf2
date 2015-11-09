cc1aa4afc35eef29ac3ea1a1624c5f1b29d1f05a
Anze Zupanc <anze.zupanc@ijs.si>
2015-11-09 13:38:20 +0000

    Added conversion monitoring script. Creates conversion monitoring histograms within BASF, which are to be compared with the monitoring histogram created in BASF.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@22912 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ b2bii/examples/Monitor.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from basf2 import *
+from modularAnalysis import inputMdstList
+from modularAnalysis import reconstructDecay
+from modularAnalysis import matchMCTruth
+from modularAnalysis import analysis_main
+from modularAnalysis import ntupleFile
+from modularAnalysis import ntupleTree
+from modularAnalysis import fillParticleList
+from modularAnalysis import fillConvertedPhotonsList
+from modularAnalysis import loadGearbox
+from modularAnalysis import vertexKFit
+from modularAnalysis import vertexRave
+from modularAnalysis import printVariableValues
+from b2biiConversion import convertBelleMdstToBelleIIMdst
+from b2biiMonitors import addBeamParamsConversionMonitors
+from b2biiMonitors import addTrackConversionMonitors
+from b2biiMonitors import addKshortConversionMonitors
+
+if len(sys.argv) != 3:
+    sys.exit('Must provide two input parameters: [input_Belle_MDST_file][output_BelleII_ROOT_file].\n'
+             'A small example Belle MDST file can be downloaded from'
+             'http://www-f9.ijs.si/~zupanc/evtgen_exp_07_BptoD0pip-D0toKpipi0-0.mdst')
+
+inputBelleMDSTFile = sys.argv[1]
+outputBelle2ROOTFile = sys.argv[2]
+
+# set the location of the Belle DB server
+# options are: ekpbelle.physik.uni-karlsruhe.de
+# or ekpbelle.physik.uni-karlsruhe.de (if you're running outside KEKCC computers)
+os.putenv('BELLE_POSTGRES_SERVER', 'can51')
+os.putenv('USE_GRAND_REPROCESS_DATA', '1')
+
+# Convert
+convertBelleMdstToBelleIIMdst(inputBelleMDSTFile)
+
+# Reconstruct
+# first the gearbox needs to be loaded
+loadGearbox()
+
+# Create monitoring histograms
+addBeamParamsConversionMonitors()
+addTrackConversionMonitors()
+addKshortConversionMonitors()
+
+# progress
+progress = register_module('Progress')
+analysis_main.add_module(progress)
+
+process(analysis_main)
+
+# Print call statistics
+print(statistics)

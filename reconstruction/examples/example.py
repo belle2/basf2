70b70673ea0a59ba14162a2fff6abe75d00363cf
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2015-11-13 17:42:27 +0000

    HLT included in example
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@23009 b38c6c8f-bee8-465d-8448-9abe6915d496

--- reconstruction/examples/example.py
+++ reconstruction/examples/example.py
@@ -3,15 +3,14 @@
 
 from basf2 import *
 from simulation import add_simulation
-from reconstruction import add_reconstruction
+from reconstruction import add_reconstruction, add_mdst_output
+from HLTTrigger import add_HLT_Y4S
 
 main = create_path()
 
 # specify number of events to be generated in job
 eventinfosetter = register_module('EventInfoSetter')
 eventinfosetter.param('evtNumList', [10])  # we want to process 10 events
-eventinfosetter.param('runList', [1])  # from run number 1
-eventinfosetter.param('expList', [1])  # and experiment number 1
 main.add_module(eventinfosetter)
 
 # generate BBbar events
@@ -21,30 +20,26 @@ add_beamparameters(main, "Y4S")
 main.add_module('EvtGenInput')
 
 # detecor simulation
-components = [
-    'MagneticField',
-    'BeamPipe',
-    'PXD',
-    'SVD',
-    'CDC',
-    'TOP',
-    'ARICH',
-    'BKLM',
-    'ECL',
-]
-add_simulation(main, components)
-# or add_simulation(main) to simulate all detectors
+add_simulation(main)
+# or add_simulation(main, components) to simulate a selection of detectors
+
+# HLT L3 simulation
+main.add_module('Level3')
 
 # reconstruction
-add_reconstruction(main, components)
-# or add_reconstruction(main) to run the reconstruction of all detectors
+add_reconstruction(main)
+# or add_reconstruction(main, components) to run the reconstruction of a selection of detectors
 
-# output
-output = register_module('RootOutput')
-output.param('outputFileName', 'output.root')
-main.add_module(output)
+# HLT physics trigger
+add_HLT_Y4S(main)
+
+# full output
+main.add_module('RootOutput', outputFileName='output.root')
+
+# mdst output
+add_mdst_output(main)
+
 main.add_module('ProgressBar')
-
 process(main)
 
 # Print call statistics

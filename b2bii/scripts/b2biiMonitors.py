0d679ebc4fbba64802384dcb2b5a535ded2e3f17
Anze Zupanc <anze.zupanc@ijs.si>
2015-11-16 10:44:21 +0000

    Added remaining KS monitoring histograms. Ks monitoring is considered to be ready.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@23032 b38c6c8f-bee8-465d-8448-9abe6915d496

--- b2bii/scripts/b2biiMonitors.py
+++ b2bii/scripts/b2biiMonitors.py
@@ -3,7 +3,10 @@
 
 from basf2 import *
 from modularAnalysis import fillParticleList
+from modularAnalysis import copyParticles
+from modularAnalysis import matchMCTruth
 from modularAnalysis import analysis_main
+from vertex import vertexKFit
 
 
 def addBeamParamsConversionMonitors(outputRootFile='b2biiBeamParamsConversionMonitors.root', path=analysis_main):
@@ -73,10 +76,16 @@ def addKshortConversionMonitors(outputRootFile='b2biiKshortConversionMonitors.ro
     @param outputRootFile name of the output ROOT file to which the histograms are saved.
     @param path modules are added to this path
     """
+    # copy KShorts from 'K_S0:mdst' list. We don't want to mess with them.
+    copyParticles('K_S0:b2bii_monitor', 'K_S0:mdst', False, path)
+    vertexKFit('K_S0:b2bii_monitor', -1, '', '', path)
+    matchMCTruth('K_S0:b2bii_monitor', path)
 
     # register VariablesToHistogram and fill it with monitored variables
     kshorts2hist = register_module('VariablesToHistogram')
     kshorts2hist.param('particleList', 'K_S0:mdst')
+    fittdKS2hist = register_module('VariablesToHistogram')
+    fittdKS2hist.param('particleList', 'K_S0:b2bii_monitor')
 
     # define variables that are monitored and specify
     # the corresponding histogram (#bins, low, high)
@@ -119,8 +128,73 @@ def addKshortConversionMonitors(outputRootFile='b2biiKshortConversionMonitors.ro
                                      kshorts2hist_d1x, kshorts2hist_d1y, kshorts2hist_d1z,
                                      kshorts2hist_gks, kshorts2hist_vlike, kshorts2hist_nolam, kshorts2hist_stnrd])
 
+    fittdKS2hist_mass = ('M', 100, 0.46, 0.53)
+    fittdKS2hist_px = ('px', 100, -2.5, 2.5)
+    fittdKS2hist_py = ('py', 100, -2.5, 2.5)
+    fittdKS2hist_pz = ('pz', 100, -1.5, 3.5)
+    fittdKS2hist_e = ('E', 100, 0.0, 4.0)
+    fittdKS2hist_x = ('x', 100, -50.0, 50.0)
+    fittdKS2hist_y = ('y', 100, -50.0, 50.0)
+    fittdKS2hist_z = ('z', 100, -50.0, 50.0)
+
+    fittdKS2hist_pvale = ('chiProb', 120, -1.1, 1.1)
+
+    fittdKS2hist_err00 = ('momVertCovM(0,0)', 100, 0.0, 0.00010)
+    fittdKS2hist_err01 = ('momVertCovM(0,1)', 100, 0.0, 0.00005)
+    fittdKS2hist_err02 = ('momVertCovM(0,2)', 100, 0.0, 0.00005)
+    fittdKS2hist_err03 = ('momVertCovM(0,3)', 100, 0.0, 0.00010)
+    fittdKS2hist_err04 = ('momVertCovM(0,4)', 100, 0.0, 0.00020)
+    fittdKS2hist_err05 = ('momVertCovM(0,5)', 100, 0.0, 0.00050)
+    fittdKS2hist_err06 = ('momVertCovM(0,6)', 100, 0.0, 0.00100)
+    fittdKS2hist_err11 = ('momVertCovM(1,1)', 100, 0.0, 0.00010)
+    fittdKS2hist_err12 = ('momVertCovM(1,2)', 100, 0.0, 0.00005)
+    fittdKS2hist_err13 = ('momVertCovM(1,3)', 100, 0.0, 0.00010)
+    fittdKS2hist_err14 = ('momVertCovM(1,4)', 100, 0.0, 0.00050)
+    fittdKS2hist_err15 = ('momVertCovM(1,5)', 100, 0.0, 0.00020)
+    fittdKS2hist_err16 = ('momVertCovM(1,6)', 100, 0.0, 0.00100)
+    fittdKS2hist_err22 = ('momVertCovM(2,2)', 100, 0.0, 0.00020)
+    fittdKS2hist_err23 = ('momVertCovM(2,3)', 100, 0.0, 0.00020)
+    fittdKS2hist_err24 = ('momVertCovM(2,4)', 100, 0.0, 0.00050)
+    fittdKS2hist_err25 = ('momVertCovM(2,5)', 100, 0.0, 0.00050)
+    fittdKS2hist_err26 = ('momVertCovM(2,6)', 100, 0.0, 0.00005)
+    fittdKS2hist_err33 = ('momVertCovM(3,3)', 100, 0.0, 0.00030)
+    fittdKS2hist_err34 = ('momVertCovM(3,4)', 100, 0.0, 0.00050)
+    fittdKS2hist_err35 = ('momVertCovM(3,5)', 100, 0.0, 0.00050)
+    fittdKS2hist_err36 = ('momVertCovM(3,6)', 100, 0.0, 0.00050)
+    fittdKS2hist_err44 = ('momVertCovM(4,4)', 100, 0.0, 0.00100)
+    fittdKS2hist_err45 = ('momVertCovM(4,5)', 100, 0.0, 0.00100)
+    fittdKS2hist_err46 = ('momVertCovM(4,6)', 100, 0.0, 0.00100)
+    fittdKS2hist_err55 = ('momVertCovM(5,5)', 100, 0.0, 0.00100)
+    fittdKS2hist_err56 = ('momVertCovM(5,6)', 100, 0.0, 0.00100)
+    fittdKS2hist_err66 = ('momVertCovM(6,6)', 100, 0.0, 0.00100)
+
+    # mc truth information
+    fittdKS2hist_ksd0MCPDG = ('abs(daughter(0,mcPDG))', 250, 0.0, 2499.0)
+    fittdKS2hist_ksd1MCPDG = ('abs(daughter(1,mcPDG))', 250, 0.0, 2499.0)
+    fittdKS2hist_ksd0MCP = ('abs(daughter(0,mcP))', 100, 0.0, 3.5)
+    fittdKS2hist_ksd1MCP = ('abs(daughter(1,mcP))', 100, 0.0, 3.5)
+
+    fittdKS2hist.param('variables', [fittdKS2hist_mass, fittdKS2hist_px, fittdKS2hist_py, fittdKS2hist_pz,
+                                     fittdKS2hist_e, fittdKS2hist_x, fittdKS2hist_y, fittdKS2hist_z,
+                                     fittdKS2hist_pvale,
+                                     fittdKS2hist_err00, fittdKS2hist_err01, fittdKS2hist_err02, fittdKS2hist_err03,
+                                     fittdKS2hist_err04, fittdKS2hist_err05, fittdKS2hist_err06,
+                                     fittdKS2hist_err11, fittdKS2hist_err12, fittdKS2hist_err13,
+                                     fittdKS2hist_err14, fittdKS2hist_err15, fittdKS2hist_err16,
+                                     fittdKS2hist_err22, fittdKS2hist_err23, fittdKS2hist_err24,
+                                     fittdKS2hist_err25, fittdKS2hist_err26,
+                                     fittdKS2hist_err33, fittdKS2hist_err34, fittdKS2hist_err35, fittdKS2hist_err36,
+                                     fittdKS2hist_err44, fittdKS2hist_err45, fittdKS2hist_err46,
+                                     fittdKS2hist_err55, fittdKS2hist_err56,
+                                     fittdKS2hist_err66,
+                                     fittdKS2hist_ksd0MCPDG, fittdKS2hist_ksd1MCPDG,
+                                     fittdKS2hist_ksd0MCP, fittdKS2hist_ksd1MCP])
+
     kshorts2hist.param('fileName', outputRootFile)
+    outputRootFileAVF = outputRootFile.replace(".root", "-AVF.root")
+    fittdKS2hist.param('fileName', outputRootFileAVF)
     path.add_module(kshorts2hist)
+    path.add_module(fittdKS2hist)
 
 
 def addTrackConversionMonitors(outputRootFile='b2biiTrackConversionMonitors.root', path=analysis_main):
@@ -134,7 +208,7 @@ def addTrackConversionMonitors(outputRootFile='b2biiTrackConversionMonitors.root
     """
 
     # create charged pions from all charged tracks
-    fillParticleList('pi+:b2bii_monitor', '')
+    fillParticleList('pi+:b2bii_monitor', '', False, path)
 
     # register VariablesToHistogram and fill it with monitored variables
     tracks2hist = register_module('VariablesToHistogram')

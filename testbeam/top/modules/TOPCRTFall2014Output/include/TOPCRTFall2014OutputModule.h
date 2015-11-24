1fbc0140f93f9cb5cea037d78f03665b3e56da1b
Matthew Barrett <mattb@phys.hawaii.edu>
2015-11-24 18:56:32 +0000

    testbeam/top: Added output module for TOP cosmic testing for Fall 2014.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@23216 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ testbeam/top/modules/TOPCRTFall2014Output/include/TOPCRTFall2014OutputModule.h
@@ -0,0 +1,96 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Marko Staric, Matthew Barrett                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef TOPCRTFALL2014OUTPUTMODULE_H
+#define TOPCRTFALL2014OUTPUTMODULE_H
+
+#include <framework/core/Module.h>
+#include <string>
+#include <top/geometry/TOPGeometryPar.h>
+#include <testbeam/top/modules/TOPCRTFall2014Output/CRT2014Tree.h>
+#include "TFile.h"
+
+namespace Belle2 {
+
+  /**
+   * Output of TOP Cosmic test fall 2014 test simulation to a specific root ntuple with
+   * a data format matching parameters used in data root files ("top tree")
+   */
+  class TOPCRTFall2014OutputModule : public Module {
+
+  public:
+
+    /**
+     * Constructor
+     */
+    TOPCRTFall2014OutputModule();
+
+    /**
+     * Destructor
+     */
+    virtual ~TOPCRTFall2014OutputModule();
+
+    /**
+     * Initialize the Module.
+     * This method is called at the beginning of data processing.
+     */
+    virtual void initialize();
+
+    /**
+     * Called when entering a new run.
+     * Set run dependent things like run header parameters, alignment, etc.
+     */
+    virtual void beginRun();
+
+    /**
+     * Event processor.
+     */
+    virtual void event();
+
+    /**
+     * End-of-run action.
+     * Save run-related stuff, such as statistics.
+     */
+    virtual void endRun();
+
+    /**
+     * Termination action.
+     * Clean-up, close files, summarize statistics, etc.
+     */
+    virtual void terminate();
+
+    /**
+     * Prints module parameters.
+     */
+    void printModuleParams() const;
+
+  private:
+
+    std::string m_outputFileName; /**< Output file name */
+    bool m_randomize;             /**< Randomize within TDC bin */
+    double m_t0;   /**< Offset time to add for adjusting photon TDC times */
+    bool m_multipleHits; /**< Allow multiple hits in a single channel within an event */
+
+    TFile* m_file;      /**< TFile */
+    TTree* m_treeTop;   /**< TTree top */
+    TOP::TopTree m_top; /**< A struct containin the top tree structure */
+
+    TOP::TOPGeometryPar* m_topgp; /**< geometry parameters */
+
+    int m_numPMTchannels; /**< number of PMT channels */
+    int m_numChannels;    /**< number of all channels */
+    float m_tdcWidth;     /**< TDC bin width [ps] */
+    int m_tdcOverflow;    /**< TDC overflow value */
+
+  };
+
+} // Belle2 namespace
+
+#endif

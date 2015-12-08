8563a34a58bb18c51df95ce7037d9b21c0e8bde5
Guglielmo De Nardo <denardo@na.infn.it>
2015-12-08 15:58:11 +0000

    Fixed a mismatch of one channel between filling and fitting adc content
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@23476 b38c6c8f-bee8-465d-8448-9abe6915d496

--- ecl/digitization/include/EclConfigurationPure.h
+++ ecl/digitization/include/EclConfigurationPure.h
@@ -13,7 +13,7 @@ namespace Belle2 {
       static constexpr int    m_nch      = EclConfiguration::m_nch; // total number of electronic channels (crystals) in calorimeter
 
       static constexpr int    m_nsmp     = EclConfiguration::m_nsmp; // number of ADC measurements for signal fitting
-      static constexpr double m_tmin     = EclConfiguration::m_tmin; // lower range of the signal fitting region in ADC clocks
+      static constexpr double m_tmin     = -16; // lower range of the signal fitting region in ADC clocks
       static constexpr int    m_ntrg     = EclConfiguration::m_ntrg; // number of trigger counts per ADC clock tick
       static double           m_tickPure;
       static constexpr int    m_nlPure   = EclConfiguration::m_nl * 15; // length of samples signal in number of ADC clocks

d794b0fb28d1c66aebf9f48c644e2e3943824938
Oliver Frost <oliver.frost@desy.de>
2015-12-10 14:42:32 +0000

    Added a description for the segment fitter.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@23601 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/findlets/minimal/include/SegmentFitter.h
+++ tracking/trackFindingCDC/findlets/minimal/include/SegmentFitter.h
@@ -27,6 +27,12 @@ namespace Belle2 {
       typedef Findlet<CDCRecoSegment2D> Super;
 
     public:
+      /// Short description of the findlet
+      virtual std::string getDescription() override
+      {
+        return "Fits each segment with the Riemann method";
+      }
+
       /** Add the parameters of the filter to the module */
       void exposeParameters(ModuleParamList* moduleParamList) override final
       {

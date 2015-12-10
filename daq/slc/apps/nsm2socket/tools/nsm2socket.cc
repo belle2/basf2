d881dacc6d6c524be03a2bed91446c0fdd4a2454
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2015-12-10 05:21:20 +0000

    Added client mode
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@23512 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/apps/nsm2socket/tools/nsm2socket.cc
+++ daq/slc/apps/nsm2socket/tools/nsm2socket.cc
@@ -31,24 +31,39 @@ using namespace Belle2;
 int main(int argc, char** argv)
 {
   int port = (argc > 1) ? atoi(argv[1]) : -1;
+  std::string hostname = "";
   if (port <= 0) {
     LogFile::debug("usage: %s <port> [-d]", argv[0]);
     return 1;
   }
-  if (argc > 2) daemon(0, 0);
-  LogFile::open("nsm2socket/nsm2scoket");
-  TCPServerSocket server_socket("0.0.0.0", port);
-  try {
-    server_socket.open();
-    while (true) {
-      TCPSocket socket = server_socket.accept();
-      Process(new NSM2SocketBridge(socket));
-      socket.close();
-      PThread(new ProcessMonitor());
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-c") == 0) {
+      i++;
+      if (i < argc) hostname = argv[i];
     }
-  } catch (const IOException& e) {
-    LogFile::error(e.what());
+    if (strcmp(argv[i], "-d") == 0) {
+      daemon(0, 0);
+    }
   }
-  LogFile::debug("Terminate process");
+  LogFile::open("nsm2socket/nsm2scoket");
+  if (hostname.size() == 0) {
+    TCPServerSocket server_socket("0.0.0.0", port);
+    try {
+      server_socket.open();
+      while (true) {
+        TCPSocket socket = server_socket.accept();
+        Process(new NSM2SocketBridge(socket));
+        socket.close();
+        PThread(new ProcessMonitor());
+      }
+    } catch (const IOException& e) {
+      LogFile::error(e.what());
+    }
+    LogFile::debug("Terminate process");
+  } else {
+    TCPSocket socket(hostname, port);
+    socket.connect();
+    NSM2SocketBridge(socket).run();
+  }
   return 0;
 }

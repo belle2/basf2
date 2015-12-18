cc19585c25fd3dbbdacd61b3516f6ec7c579a6da
Martin Ritter <martin.ritter@lmu.de>
2015-12-18 11:10:51 +0000

    Add test ROOT for return values in python
    Apparently this is a thing: we have to check that ROOT returns the
    correct values when calling a c++ function from python :/
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@23818 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ framework/tests/root_returnvalues.py
@@ -0,0 +1,95 @@
+from __future__ import print_function
+
+import sys
+import ROOT
+import numpy as np
+
+testclass = [
+    "#include <cstdint>",
+    "#include <limits>",
+    "struct ReturnValueTests {"
+]
+
+results = []
+# what integer sizes to check
+integral_types = [8, 16, 32, 64]
+# which floating types to check
+floating_types = ["float", "double"]
+# nan and inf need to be expressed differently so make a mapping
+floating_expressions = {
+    -np.inf: "-std::numeric_limits<{0}>::infinity()",
+    np.inf: "std::numeric_limits<{0}>::infinity()",
+    np.nan: "std::numeric_limits<{0}>::quiet_NaN()",
+}
+
+for width in integral_types:
+    # check signed types, we want min, max -1, 0 and 1
+    for i, value in enumerate([-2**(width-1), -1, 0, 1, 2**(width-1) - 1]):
+        # create a unique member name
+        func_name = "int{0}test{1}".format(width, i)
+        # do we need a prefix to the literal?
+        prefix = "ull" if width > 32 else ""
+        # append it to the test class
+        testclass.append("int{0}_t {1}() const {{ return {2}{3}; }}".format(width, func_name, value, prefix))
+        testclass.append("int{0}_t& ref_{1}() {{ static int{0}_t val = {2}{3}; return val; }}".format(
+            width, func_name, value, prefix))
+        # and remember name and result for checking
+        results.append([func_name, value])
+        results.append(['ref_' + func_name, value])
+
+    # check unsigned types, just 0, 1, and max
+    for i, value in enumerate([0, 1, 2**(width) - 1]):
+        # create a unique member name
+        func_name = "uint{0}test{1}".format(width, i)
+        # do we need a prefix to the literal?
+        prefix = "ull" if width > 32 else ""
+        # append it to the test class
+        testclass.append("uint{0}_t {1}() const {{ return {2}ull; }}".format(width, func_name, value, prefix))
+        testclass.append("uint{0}_t& ref_{1}() {{ static uint{0}_t val = {2}{3}; return val; }}".format(
+            width, func_name, value, prefix))
+        # and remember name and result for checking
+        results.append([func_name, value])
+        results.append(['ref_' + func_name, value])
+
+# now add floating types
+for t in floating_types:
+    # and exploit that numpy offers a numeric_limits equivalent
+    info = np.finfo(t[0])
+
+    # check some values
+    for i, value in enumerate([-np.inf, info.min, -1, 0, info.tiny, info.eps, 1, info.max, np.inf, np.nan]):
+        func_name = "{0}test{1}".format(t, i)
+        # see if we can just have the literal or if we need a mapping
+        expression = repr(value) if value not in floating_expressions else floating_expressions[value].format(t)
+        testclass.append("{0} {1}() const {{ return {2}; }}".format(t, func_name, expression))
+        testclass.append("{0}& ref_{1}() {{ static {0} val = {2}; return val; }}".format(t, func_name, expression))
+        results.append([func_name, value])
+        results.append(['ref_' + func_name, value])
+
+
+# compile the test class
+testclass.append("};")
+ROOT.gROOT.ProcessLine("\n".join(testclass))
+# get an instance
+tests = ROOT.ReturnValueTests()
+# and do all the checks
+failures = 0
+for func, value in results:
+    ret = getattr(tests, func)()
+    # char is odd and sometimes returns strings for positive values. Fix that
+    if isinstance(ret, str):
+        ret = ord(ret[0])
+
+    # print the test
+    print("check %s(): %r == %r: " % (func, value, ret), end="")
+    # nan needs special care because it cannot be compared to itself
+    if np.isnan(value):
+        passed = np.isnan(ret)
+    else:
+        passed = (ret == value)
+
+    print("\033[32mOK\033[0m" if passed else "\033[31mFAIL\033[0m")
+    if not passed:
+        failures += 1
+
+sys.exit(failures)

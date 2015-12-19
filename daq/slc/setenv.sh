ea0c2944b0d624f41808b243181928018695beb8
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2015-12-19 05:59:37 +0000

    Added functions to set/check NSM2 related environment variables
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@23841 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/setenv.sh
+++ daq/slc/setenv.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+## set NOT_USE_PSQL yes if postreSQL is not available
+#export NOT_USE_PSQL=yes
+
 export BELLE2_DAQ_SLC=$PWD
 export PATH=$BELLE2_DAQ_SLC/bin:$PATH
 if ! test "$BELLE2_LOCAL_DIR" = "" ; then
-#if [ -z ${BELLE2_LOCAL_DIR+x} ]; then
   if [ -d $BELLE2_EXTERNALS_DIR/lib/$BELLE2_EXTERNALS_SUBDIR/ ]; then
     export PGSQL_LIB_PATH=$BELLE2_EXTERNALS_DIR/lib/$BELLE2_EXTERNALS_SUBDIR
   else 
@@ -17,7 +19,34 @@ else
 fi
 export LD_LIBRARY_PATH=$BELLE2_DAQ_SLC/lib:$PGSQL_LIB_PATH/lib:$LD_LIBRARY_PATH
 export NSM2_INCDIR=$BELLE2_DAQ_SLC/data/nsm
-## set NOT_USE_PSQL yes if postreSQL is not available
-#export NOT_USE_PSQL=yes
-#!/bin/bash
 
+function setslc () {
+    if [ $# -ne 1 ]; then
+	echo "Current slowcontrol : " ${BELLE2_SLC_FILE}
+	return 1
+    fi
+    export BELLE2_SLC_FILE=$1
+    echo "Set slowcontrol : " ${BELLE2_SLC_FILE}
+    return 0
+}
+
+function setnsmhost () {
+    if [ $# -ne 1 ]; then
+	echo "Current NSM2_HOST : " ${NSM2_HOST}
+	return 1
+    fi
+    export NSM2_HOST=$1
+    echo "Set NSM2_HOST : " ${NSM2_HOST}
+    return 0
+}
+
+function setnsmport () {
+    if [ $# -ne 1 ]; then
+	echo "Current NSM2_PORT : " ${NSM2_PORT}
+	return 1
+    fi
+    export NSM2_PORT=$1
+    export NSM2_SHMKEY=$1
+    echo "Set NSM2_PORT : " ${NSM2_PORT}
+    return 0
+}

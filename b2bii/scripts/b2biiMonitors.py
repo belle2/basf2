744cce242653f576a815ab22e9ac525f1a952776
Anze Zupanc <anze.zupanc@ijs.si>
2015-12-30 11:37:50 +0000

    Added monitors for Lambdas and converted photons.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@24018 b38c6c8f-bee8-465d-8448-9abe6915d496

--- b2bii/scripts/b2biiMonitors.py
+++ b2bii/scripts/b2biiMonitors.py
@@ -177,8 +177,8 @@ def addKshortConversionMonitors(outputRootFile='b2biiKshortConversionMonitors.ro
     # mc truth information
     fittdKS2hist_ksd0MCPDG = ('abs(daughter(0,mcPDG))', 250, 0.0, 2499.0)
     fittdKS2hist_ksd1MCPDG = ('abs(daughter(1,mcPDG))', 250, 0.0, 2499.0)
-    fittdKS2hist_ksd0MCP = ('abs(daughter(0,mcP))', 100, 0.0, 3.5)
-    fittdKS2hist_ksd1MCP = ('abs(daughter(1,mcP))', 100, 0.0, 3.5)
+    fittdKS2hist_ksd0MCP = ('daughter(0,mcP)', 100, 0.0, 3.5)
+    fittdKS2hist_ksd1MCP = ('daughter(1,mcP)', 100, 0.0, 3.5)
 
     fittdKS2hist.param('variables', [fittdKS2hist_mass, fittdKS2hist_px, fittdKS2hist_py, fittdKS2hist_pz,
                                      fittdKS2hist_e, fittdKS2hist_x, fittdKS2hist_y, fittdKS2hist_z,
@@ -201,6 +201,254 @@ def addKshortConversionMonitors(outputRootFile='b2biiKshortConversionMonitors.ro
     fittdKS2hist.param('fileName', outputRootFileAVF)
     path.add_module(kshorts2hist)
     path.add_module(fittdKS2hist)
+
+
+def addLambda0ConversionMonitors(outputRootFile='b2biiLambda0ConversionMonitors.root', path=analysis_main):
+    """
+    Creates copies of Lambda0 particles from 'Lambda0:mdst' ParticleList and c.c. and fills them to a new ParticleList called
+    'Lambda0:b2bii_monitor'. For each Lambda0 candidate several quantities are stored to histograms with 'VariablesToHistogram'
+    module for monitoring purpuses: e.g. to be compared with same distributions obtained within BASF.
+
+    @param outputRootFile name of the output ROOT file to which the histograms are saved.
+    @param path modules are added to this path
+    """
+
+    copyParticles('Lambda0:b2bii_monitor', 'Lambda0:mdst', False, path)
+    vertexKFit('Lambda0:b2bii_monitor', -1, '', '', path)
+    matchMCTruth('Lambda0:b2bii_monitor', path)
+
+    # register VariablesToHistogram and fill it with monitored variables
+    lambdas2hist = register_module('VariablesToHistogram')
+    lambdas2hist.param('particleList', 'Lambda0:mdst')
+    fittdL02hist = register_module('VariablesToHistogram')
+    fittdL02hist.param('particleList', 'Lambda0:b2bii_monitor')
+
+    # define variables that are monitored and specify
+    # the corresponding histogram (#bins, low, high)
+    # ('variable_name', number_of_bins, x_low, x_high)
+    lambdas2hist_mass = ('M', 100, 1.095, 1.135)
+    lambdas2hist_px = ('px', 100, -2.5, 2.5)
+    lambdas2hist_py = ('py', 100, -2.5, 2.5)
+    lambdas2hist_pz = ('pz', 100, -1.5, 3.5)
+    lambdas2hist_e = ('E', 100, 0.0, 4.0)
+    lambdas2hist_x = ('x', 100, -50.0, 50.0)
+    lambdas2hist_y = ('y', 100, -50.0, 50.0)
+    lambdas2hist_z = ('z', 100, -50.0, 50.0)
+
+    lambdas2hist_d0px = ('daughter(0,px)', 100, -2.0, 2.0)
+    lambdas2hist_d0py = ('daughter(0,py)', 100, -2.0, 2.0)
+    lambdas2hist_d0pz = ('daughter(0,pz)', 100, -2.0, 3.0)
+    lambdas2hist_d0e = ('daughter(0,E)', 100, 0.0, 3.0)
+    lambdas2hist_d0x = ('daughter(0,x)', 100, -50.0, 50.0)
+    lambdas2hist_d0y = ('daughter(0,y)', 100, -50.0, 50.0)
+    lambdas2hist_d0z = ('daughter(0,z)', 100, -50.0, 50.0)
+
+    lambdas2hist_d1px = ('daughter(1,px)', 100, -2.0, 2.0)
+    lambdas2hist_d1py = ('daughter(1,py)', 100, -2.0, 2.0)
+    lambdas2hist_d1pz = ('daughter(1,pz)', 100, -2.0, 3.0)
+    lambdas2hist_d1e = ('daughter(1,E)', 100, 0.0, 3.0)
+    lambdas2hist_d1x = ('daughter(1,x)', 100, -50.0, 50.0)
+    lambdas2hist_d1y = ('daughter(1,y)', 100, -50.0, 50.0)
+    lambdas2hist_d1z = ('daughter(1,z)', 100, -50.0, 50.0)
+
+    lambdas2hist.param('variables', [lambdas2hist_mass, lambdas2hist_px, lambdas2hist_py, lambdas2hist_pz,
+                                     lambdas2hist_e, lambdas2hist_x, lambdas2hist_y, lambdas2hist_z,
+                                     lambdas2hist_d0px, lambdas2hist_d0py, lambdas2hist_d0pz, lambdas2hist_d0e,
+                                     lambdas2hist_d0x, lambdas2hist_d0y, lambdas2hist_d0z,
+                                     lambdas2hist_d1px, lambdas2hist_d1py, lambdas2hist_d1pz, lambdas2hist_d1e,
+                                     lambdas2hist_d1x, lambdas2hist_d1y, lambdas2hist_d1z])
+
+    fittdL02hist_mass = ('M', 100, 1.095, 1.135)
+    fittdL02hist_px = ('px', 100, -2.5, 2.5)
+    fittdL02hist_py = ('py', 100, -2.5, 2.5)
+    fittdL02hist_pz = ('pz', 100, -1.5, 3.5)
+    fittdL02hist_e = ('E', 100, 0.0, 4.0)
+    fittdL02hist_x = ('x', 100, -50.0, 50.0)
+    fittdL02hist_y = ('y', 100, -50.0, 50.0)
+    fittdL02hist_z = ('z', 100, -50.0, 50.0)
+
+    fittdL02hist_pvale = ('chiProb', 120, -1.1, 1.1)
+
+    fittdL02hist_err00 = ('momVertCovM(0,0)', 100, 0.0, 0.00010)
+    fittdL02hist_err01 = ('momVertCovM(0,1)', 100, 0.0, 0.00005)
+    fittdL02hist_err02 = ('momVertCovM(0,2)', 100, 0.0, 0.00005)
+    fittdL02hist_err03 = ('momVertCovM(0,3)', 100, 0.0, 0.00010)
+    fittdL02hist_err04 = ('momVertCovM(0,4)', 100, 0.0, 0.00020)
+    fittdL02hist_err05 = ('momVertCovM(0,5)', 100, 0.0, 0.00050)
+    fittdL02hist_err06 = ('momVertCovM(0,6)', 100, 0.0, 0.00100)
+    fittdL02hist_err11 = ('momVertCovM(1,1)', 100, 0.0, 0.00010)
+    fittdL02hist_err12 = ('momVertCovM(1,2)', 100, 0.0, 0.00005)
+    fittdL02hist_err13 = ('momVertCovM(1,3)', 100, 0.0, 0.00010)
+    fittdL02hist_err14 = ('momVertCovM(1,4)', 100, 0.0, 0.00050)
+    fittdL02hist_err15 = ('momVertCovM(1,5)', 100, 0.0, 0.00020)
+    fittdL02hist_err16 = ('momVertCovM(1,6)', 100, 0.0, 0.00100)
+    fittdL02hist_err22 = ('momVertCovM(2,2)', 100, 0.0, 0.00020)
+    fittdL02hist_err23 = ('momVertCovM(2,3)', 100, 0.0, 0.00020)
+    fittdL02hist_err24 = ('momVertCovM(2,4)', 100, 0.0, 0.00050)
+    fittdL02hist_err25 = ('momVertCovM(2,5)', 100, 0.0, 0.00050)
+    fittdL02hist_err26 = ('momVertCovM(2,6)', 100, 0.0, 0.00005)
+    fittdL02hist_err33 = ('momVertCovM(3,3)', 100, 0.0, 0.00030)
+    fittdL02hist_err34 = ('momVertCovM(3,4)', 100, 0.0, 0.00050)
+    fittdL02hist_err35 = ('momVertCovM(3,5)', 100, 0.0, 0.00050)
+    fittdL02hist_err36 = ('momVertCovM(3,6)', 100, 0.0, 0.00050)
+    fittdL02hist_err44 = ('momVertCovM(4,4)', 100, 0.0, 0.00100)
+    fittdL02hist_err45 = ('momVertCovM(4,5)', 100, 0.0, 0.00100)
+    fittdL02hist_err46 = ('momVertCovM(4,6)', 100, 0.0, 0.00100)
+    fittdL02hist_err55 = ('momVertCovM(5,5)', 100, 0.0, 0.00100)
+    fittdL02hist_err56 = ('momVertCovM(5,6)', 100, 0.0, 0.00100)
+    fittdL02hist_err66 = ('momVertCovM(6,6)', 100, 0.0, 0.00100)
+
+    # mc truth information
+    fittdL02hist_ksd0MCPDG = ('abs(daughter(0,mcPDG))', 250, 0.0, 2499.0)
+    fittdL02hist_ksd1MCPDG = ('abs(daughter(1,mcPDG))', 250, 0.0, 2499.0)
+    fittdL02hist_ksd0MCP = ('daughter(0,mcP)', 100, 0.0, 3.5)
+    fittdL02hist_ksd1MCP = ('daughter(1,mcP)', 100, 0.0, 3.5)
+
+    fittdL02hist.param('variables', [fittdL02hist_mass, fittdL02hist_px, fittdL02hist_py, fittdL02hist_pz,
+                                     fittdL02hist_e, fittdL02hist_x, fittdL02hist_y, fittdL02hist_z,
+                                     fittdL02hist_pvale,
+                                     fittdL02hist_err00, fittdL02hist_err01, fittdL02hist_err02, fittdL02hist_err03,
+                                     fittdL02hist_err04, fittdL02hist_err05, fittdL02hist_err06,
+                                     fittdL02hist_err11, fittdL02hist_err12, fittdL02hist_err13,
+                                     fittdL02hist_err14, fittdL02hist_err15, fittdL02hist_err16,
+                                     fittdL02hist_err22, fittdL02hist_err23, fittdL02hist_err24,
+                                     fittdL02hist_err25, fittdL02hist_err26,
+                                     fittdL02hist_err33, fittdL02hist_err34, fittdL02hist_err35, fittdL02hist_err36,
+                                     fittdL02hist_err44, fittdL02hist_err45, fittdL02hist_err46,
+                                     fittdL02hist_err55, fittdL02hist_err56,
+                                     fittdL02hist_err66,
+                                     fittdL02hist_ksd0MCPDG, fittdL02hist_ksd1MCPDG,
+                                     fittdL02hist_ksd0MCP, fittdL02hist_ksd1MCP])
+
+    lambdas2hist.param('fileName', outputRootFile)
+    outputRootFileAVF = outputRootFile.replace(".root", "-AVF.root")
+    fittdL02hist.param('fileName', outputRootFileAVF)
+    path.add_module(lambdas2hist)
+    path.add_module(fittdL02hist)
+
+
+def addConvertedPhotonConversionMonitors(outputRootFile='b2biiConvertedPhotonConversionMonitors.root', path=analysis_main):
+    """
+    Creates copies of gamma particles from 'gamma:v0mdst' ParticleList and fills them to a new ParticleList called
+    'gamma:v0_b2bii_monitor'. For each gamma candidate several quantities are stored to histograms with 'VariablesToHistogram'
+    module for monitoring purpuses: e.g. to be compared with same distributions obtained within BASF.
+
+    @param outputRootFile name of the output ROOT file to which the histograms are saved.
+    @param path modules are added to this path
+    """
+
+    copyParticles('gamma:v0_b2bii_monitor', 'gamma:v0mdst', False, path)
+    vertexKFit('gamma:v0_b2bii_monitor', -1, '', '', path)
+    matchMCTruth('gamma:v0_b2bii_monitor', path)
+
+    # register VariablesTohistogram and fill it with monitored variables
+    convgam2hist = register_module('VariablesToHistogram')
+    convgam2hist.param('particleList', 'gamma:v0mdst')
+    fittdCG2hist = register_module('VariablesToHistogram')
+    fittdCG2hist.param('particleList', 'gamma:v0_b2bii_monitor')
+
+    # define variables that are monitored and specify
+    # the corresponding histogram (#bins, low, high)
+    # ('variable_name', number_of_bins, x_low, x_high)
+    convgam2hist_mass = ('M', 100, 0.0, 0.050)
+    convgam2hist_px = ('px', 100, -2.5, 2.5)
+    convgam2hist_py = ('py', 100, -2.5, 2.5)
+    convgam2hist_pz = ('pz', 100, -1.5, 3.5)
+    convgam2hist_e = ('E', 100, 0.0, 4.0)
+    convgam2hist_x = ('x', 100, -50.0, 50.0)
+    convgam2hist_y = ('y', 100, -50.0, 50.0)
+    convgam2hist_z = ('z', 100, -50.0, 50.0)
+
+    convgam2hist_d0px = ('daughter(0,px)', 100, -2.0, 2.0)
+    convgam2hist_d0py = ('daughter(0,py)', 100, -2.0, 2.0)
+    convgam2hist_d0pz = ('daughter(0,pz)', 100, -2.0, 3.0)
+    convgam2hist_d0e = ('daughter(0,E)', 100, 0.0, 3.0)
+    convgam2hist_d0x = ('daughter(0,x)', 100, -50.0, 50.0)
+    convgam2hist_d0y = ('daughter(0,y)', 100, -50.0, 50.0)
+    convgam2hist_d0z = ('daughter(0,z)', 100, -50.0, 50.0)
+
+    convgam2hist_d1px = ('daughter(1,px)', 100, -2.0, 2.0)
+    convgam2hist_d1py = ('daughter(1,py)', 100, -2.0, 2.0)
+    convgam2hist_d1pz = ('daughter(1,pz)', 100, -2.0, 3.0)
+    convgam2hist_d1e = ('daughter(1,E)', 100, 0.0, 3.0)
+    convgam2hist_d1x = ('daughter(1,x)', 100, -50.0, 50.0)
+    convgam2hist_d1y = ('daughter(1,y)', 100, -50.0, 50.0)
+    convgam2hist_d1z = ('daughter(1,z)', 100, -50.0, 50.0)
+
+    convgam2hist.param('variables', [convgam2hist_mass, convgam2hist_px, convgam2hist_py, convgam2hist_pz,
+                                     convgam2hist_e, convgam2hist_x, convgam2hist_y, convgam2hist_z,
+                                     convgam2hist_d0px, convgam2hist_d0py, convgam2hist_d0pz, convgam2hist_d0e,
+                                     convgam2hist_d0x, convgam2hist_d0y, convgam2hist_d0z,
+                                     convgam2hist_d1px, convgam2hist_d1py, convgam2hist_d1pz, convgam2hist_d1e,
+                                     convgam2hist_d1x, convgam2hist_d1y, convgam2hist_d1z])
+
+    fittdCG2hist_mass = ('M', 100, 0.0, 0.050)
+    fittdCG2hist_px = ('px', 100, -2.5, 2.5)
+    fittdCG2hist_py = ('py', 100, -2.5, 2.5)
+    fittdCG2hist_pz = ('pz', 100, -1.5, 3.5)
+    fittdCG2hist_e = ('E', 100, 0.0, 4.0)
+    fittdCG2hist_x = ('x', 100, -50.0, 50.0)
+    fittdCG2hist_y = ('y', 100, -50.0, 50.0)
+    fittdCG2hist_z = ('z', 100, -50.0, 50.0)
+
+    fittdCG2hist_pvale = ('chiProb', 120, -1.1, 1.1)
+
+    fittdCG2hist_err00 = ('momVertCovM(0,0)', 100, 0.0, 0.00010)
+    fittdCG2hist_err01 = ('momVertCovM(0,1)', 100, 0.0, 0.00005)
+    fittdCG2hist_err02 = ('momVertCovM(0,2)', 100, 0.0, 0.00005)
+    fittdCG2hist_err03 = ('momVertCovM(0,3)', 100, 0.0, 0.00010)
+    fittdCG2hist_err04 = ('momVertCovM(0,4)', 100, 0.0, 0.00020)
+    fittdCG2hist_err05 = ('momVertCovM(0,5)', 100, 0.0, 0.00050)
+    fittdCG2hist_err06 = ('momVertCovM(0,6)', 100, 0.0, 0.00100)
+    fittdCG2hist_err11 = ('momVertCovM(1,1)', 100, 0.0, 0.00010)
+    fittdCG2hist_err12 = ('momVertCovM(1,2)', 100, 0.0, 0.00005)
+    fittdCG2hist_err13 = ('momVertCovM(1,3)', 100, 0.0, 0.00010)
+    fittdCG2hist_err14 = ('momVertCovM(1,4)', 100, 0.0, 0.00050)
+    fittdCG2hist_err15 = ('momVertCovM(1,5)', 100, 0.0, 0.00020)
+    fittdCG2hist_err16 = ('momVertCovM(1,6)', 100, 0.0, 0.00100)
+    fittdCG2hist_err22 = ('momVertCovM(2,2)', 100, 0.0, 0.00020)
+    fittdCG2hist_err23 = ('momVertCovM(2,3)', 100, 0.0, 0.00020)
+    fittdCG2hist_err24 = ('momVertCovM(2,4)', 100, 0.0, 0.00050)
+    fittdCG2hist_err25 = ('momVertCovM(2,5)', 100, 0.0, 0.00050)
+    fittdCG2hist_err26 = ('momVertCovM(2,6)', 100, 0.0, 0.00005)
+    fittdCG2hist_err33 = ('momVertCovM(3,3)', 100, 0.0, 0.00030)
+    fittdCG2hist_err34 = ('momVertCovM(3,4)', 100, 0.0, 0.00050)
+    fittdCG2hist_err35 = ('momVertCovM(3,5)', 100, 0.0, 0.00050)
+    fittdCG2hist_err36 = ('momVertCovM(3,6)', 100, 0.0, 0.00050)
+    fittdCG2hist_err44 = ('momVertCovM(4,4)', 100, 0.0, 0.00100)
+    fittdCG2hist_err45 = ('momVertCovM(4,5)', 100, 0.0, 0.00100)
+    fittdCG2hist_err46 = ('momVertCovM(4,6)', 100, 0.0, 0.00100)
+    fittdCG2hist_err55 = ('momVertCovM(5,5)', 100, 0.0, 0.00100)
+    fittdCG2hist_err56 = ('momVertCovM(5,6)', 100, 0.0, 0.00100)
+    fittdCG2hist_err66 = ('momVertCovM(6,6)', 100, 0.0, 0.00100)
+
+    # mc truth information
+    fittdCG2hist_ksd0MCPDG = ('abs(daughter(0,mcPDG))', 250, 0.0, 2499.0)
+    fittdCG2hist_ksd1MCPDG = ('abs(daughter(1,mcPDG))', 250, 0.0, 2499.0)
+    fittdCG2hist_ksd0MCP = ('daughter(0,mcP)', 100, 0.0, 3.5)
+    fittdCG2hist_ksd1MCP = ('daughter(1,mcP)', 100, 0.0, 3.5)
+
+    fittdCG2hist.param('variables', [fittdCG2hist_mass, fittdCG2hist_px, fittdCG2hist_py, fittdCG2hist_pz,
+                                     fittdCG2hist_e, fittdCG2hist_x, fittdCG2hist_y, fittdCG2hist_z,
+                                     fittdCG2hist_pvale,
+                                     fittdCG2hist_err00, fittdCG2hist_err01, fittdCG2hist_err02, fittdCG2hist_err03,
+                                     fittdCG2hist_err04, fittdCG2hist_err05, fittdCG2hist_err06,
+                                     fittdCG2hist_err11, fittdCG2hist_err12, fittdCG2hist_err13,
+                                     fittdCG2hist_err14, fittdCG2hist_err15, fittdCG2hist_err16,
+                                     fittdCG2hist_err22, fittdCG2hist_err23, fittdCG2hist_err24,
+                                     fittdCG2hist_err25, fittdCG2hist_err26,
+                                     fittdCG2hist_err33, fittdCG2hist_err34, fittdCG2hist_err35, fittdCG2hist_err36,
+                                     fittdCG2hist_err44, fittdCG2hist_err45, fittdCG2hist_err46,
+                                     fittdCG2hist_err55, fittdCG2hist_err56,
+                                     fittdCG2hist_err66,
+                                     fittdCG2hist_ksd0MCPDG, fittdCG2hist_ksd1MCPDG,
+                                     fittdCG2hist_ksd0MCP, fittdCG2hist_ksd1MCP])
+
+    convgam2hist.param('fileName', outputRootFile)
+    outputRootFileAVF = outputRootFile.replace(".root", "-AVF.root")
+    fittdCG2hist.param('fileName', outputRootFileAVF)
+    path.add_module(convgam2hist)
+    path.add_module(fittdCG2hist)
 
 
 def addTrackConversionMonitors(outputRootFile='b2biiTrackConversionMonitors.root', path=analysis_main):

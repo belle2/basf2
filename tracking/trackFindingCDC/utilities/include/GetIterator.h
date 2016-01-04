b6c7390cb8da07a6c3c0b57923b433b263933f66
Oliver Frost <oliver.frost@desy.de>
2016-01-04 19:07:28 +0000

    Introducing a type function to get the iterator type of a range and a type predicate to check if a type is an iterable range.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@24077 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingCDC/utilities/include/GetIterator.h
@@ -0,0 +1,23 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+#include <iterator>
+#include <type_traits>
+
+namespace Belle2 {
+  namespace TrackFindingCDC {
+
+    /// Type function to extract the iterator type from a range of values
+    template<class Ts>
+    using GetIterator = typename std::remove_reference < decltype(std::begin(std::declval<Ts&>())) >::type;
+
+  } // end namespace TrackFindingCDC
+} // end namespace Belle2
---
+++ tracking/trackFindingCDC/utilities/include/IsIterable.h
@@ -0,0 +1,28 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+#include <tracking/trackFindingCDC/utilities/GetIterator.h>
+#include <type_traits>
+
+namespace Belle2 {
+  namespace TrackFindingCDC {
+
+    /**
+     *  Type predicate to check if Ts is iterable by means of std::begin().
+     *  Result type is equivalent to std::true_type or does not compile,
+     *  because it attempts to find the iterator type of the range or fails to do so.
+     *  Therefore, the expression is currently limited to be used within enable_if type expressions.
+     */
+    template<class Ts>
+    using IsIterable = std::integral_constant<bool, not std::is_void<GetIterator<Ts> >() >;
+
+  } // end namespace TrackFindingCDC
+} // end namespace Belle2

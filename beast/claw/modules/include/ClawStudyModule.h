481a54b46d598729a8dbc66ad7ee3de271dcf450
Igal Jaegle <igjaegle@gmail.com>
2016-01-08 19:35:13 +0000

    phase 1 claws: update study
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@24196 b38c6c8f-bee8-465d-8448-9abe6915d496

--- beast/claw/modules/include/ClawStudyModule.h
+++ beast/claw/modules/include/ClawStudyModule.h
@@ -66,6 +66,20 @@ namespace Belle2 {
     private:
 
       /** Time distribution */
+      //TH2F* h_time;
+      /** Time distribution weighted per the energy deposited */
+      //TH2F* h_timeWeighted;
+      /** Time distribution with energy threshold applied */
+      //TH2F* h_timeThres;
+      /** Time distribution weighted per the energy deposited with energy threshold applied */
+      //TH2F* h_timeWeightedThres;
+      /** Energy deposited per time bin */
+      //TH2F* h_edep;
+      /** Energy deposited above threshold per time bin */
+      //TH2F* h_edepThres;
+      /** Energy deposited vs z */
+      //TH1F* h_zvedep[2];
+      /** Time distribution */
       TH2F* h_time;
       /** Time distribution weighted per the energy deposited */
       TH2F* h_timeWeighted;
@@ -75,10 +89,70 @@ namespace Belle2 {
       TH2F* h_timeWeightedThres;
       /** Energy deposited per time bin */
       TH2F* h_edep;
+      /** Energy deposited per time bin */
+      TH2F* h_Edep[10];
+      /** Energy deposited per time bin */
+      TH2F* h_edep1;
+      /** Energy deposited per time bin */
+      TH2F* h_edep2;
+      /** Energy deposited per time bin */
+      TH2F* h_edep3;
+
       /** Energy deposited above threshold per time bin */
       TH2F* h_edepThres;
+      /** Energy deposited above threshold per time bin */
+      TH2F* h_edepThres1;
+      /** Energy deposited above threshold per time bin */
+      TH2F* h_edepThres2;
+      /** Energy deposited above threshold per time bin */
+      TH2F* h_edepThres3;
+
+
       /** Energy deposited vs z */
       TH1F* h_zvedep[2];
+      /** Energy vs y vs z */
+      TH2F* h_yvzvedep[2];
+      /** Energy vs x vs z */
+      TH2F* h_xvzvedep[2];
+      /** Energy vs x vs y */
+      TH2F* h_xvyvedep[2];
+
+      /** Energy deposited vs z */
+      TH1F* h_zvedepW[2];
+      /** Energy vs y vs z */
+      TH2F* h_yvzvedepW[2];
+      /** Energy vs x vs z */
+      TH2F* h_xvzvedepW[2];
+      /** Energy vs x vs y */
+      TH2F* h_xvyvedepW[2];
+
+      /** Energy deposited vs z */
+      TH1F* h_zvedepT[2];
+      /** Energy vs y vs z */
+      TH2F* h_yvzvedepT[2];
+      /** Energy vs x vs z */
+      TH2F* h_xvzvedepT[2];
+      /** Energy vs x vs y */
+      TH2F* h_xvyvedepT[2];
+
+      /** Energy deposited vs z */
+      TH1F* h_zvedepWT[2];
+      /** Energy vs y vs z */
+      TH2F* h_yvzvedepWT[2];
+      /** Energy vs x vs z */
+      TH2F* h_xvzvedepWT[2];
+      /** Energy vs x vs y */
+      TH2F* h_xvyvedepWT[2];
+
+      /** Energy vs x vs y */
+      TH2F* h_rvzvedep[2];
+      /** Energy vs x vs y */
+      TH2F* h_rvzvedepT[2];
+
+      /** Energy vs x vs y */
+      TH2F* h_rvzvedepW[2];
+      /** Energy vs x vs y */
+      TH2F* h_rvzvedepWT[2];
 
     };
 

c3fd23f1b357a2e4ccfd8ed760e1a2ce9f860c1a
Oliver Frost <oliver.frost@desy.de>
2016-01-14 15:58:14 +0000

    Switched to proper finder in the debug display.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@24321 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/scripts/trackfindingcdc/run/debug_display.py
+++ tracking/trackFindingCDC/scripts/trackfindingcdc/run/debug_display.py
@@ -9,21 +9,20 @@ import logging
 
 
 class CDCDebugDisplayRun(CDCDisplayRun):
-    # finder_module = basf2.register_module("TrackFinderCDCAutomatonDev")
-    finder_module = basf2.register_module("SegmentFinderCDCFacetAutomaton")
-    finder_module.param({
-        "ClusterFilter": "tmva",
-        "ClusterFilterParameters": {"cut": 0.2},
-        "WriteSuperClusters": True,
-        "WriteClusters": True,
-        "RemainingCDCHitsStoreArrayName": "GoodCDCHits",
-        "FacetFilter": "none",
-        "FacetRelationFilter": "none",
-        # "CreateGFTrackCands" : True,
-        # "SegmentOrientation": "none",
-    })
+    finder_module = basf2.register_module("TrackFinderCDCAutomaton")
+    # finder_module = basf2.register_module("SegmentFinderCDCFacetAutomaton")
+    # finder_module.param({
+    #     "ClusterFilter": "tmva",
+    #     "ClusterFilterParameters": {"cut": 0.2},
+    #     "WriteSuperClusters": True,
+    #     "WriteClusters": True,
+    #     "FacetFilter": "none",
+    #     "FacetRelationFilter": "none",
+    #     "exportSegment" : True,
+    #     "exportSegmentInto": "none",
+    # })
 
-    finder_module = [finder_module, "PrintCollections"]
+    finder_module = ["WireHitTopologyPreparer", finder_module, "PrintCollections"]
     show_all_drawoptions = True
     # Also show draw options that are related to the cellular automaton track finder
     # on the command line!

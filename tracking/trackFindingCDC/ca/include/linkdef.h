43bc3debce05907d10872233cae8795fae73d9d4
Oliver Frost <oliver.frost@desy.de>
2016-01-14 15:53:28 +0000

    Introducing dictionaries for relation objects.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@24304 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/ca/include/linkdef.h
+++ tracking/trackFindingCDC/ca/include/linkdef.h
@@ -10,5 +10,33 @@
 
 #pragma link C++ class Belle2::TrackFindingCDC::AutomatonCell-;
 
+#pragma extra_include <tracking/trackFindingCDC/ca/WeightedRelation.h>;
+
+/// Some dictionaries for weighted relations that will be needed
+#pragma extra_include <tracking/trackFindingCDC/eventdata/hits/CDCWireHit.h>;
+#pragma extra_include <tracking/trackFindingCDC/eventdata/hits/CDCFacet.h>;
+#pragma extra_include <tracking/trackFindingCDC/eventdata/segments/CDCRecoSegment2D.h>;
+#pragma extra_include <tracking/trackFindingCDC/eventdata/tracks/CDCSegmentPair.h>;
+#pragma extra_include <tracking/trackFindingCDC/eventdata/tracks/CDCSegmentTriple.h>;
+#pragma extra_include <tracking/trackFindingCDC/eventdata/tracks/CDCTrack.h>;
+
+#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCWireHit>-;
+#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCWireHit> >-;
+
+#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCFacet>-;
+#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCFacet> >-;
+
+#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCRecoSegment2D>-;
+#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCRecoSegment2D> >-;
+
+#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCSegmentPair>-;
+#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCSegmentPair> >-;
+
+#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCSegmentTriple>-;
+#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCSegmentTriple> >-;
+
+#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCTrack>-;
+#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCTrack> >-;
+
 #endif
 

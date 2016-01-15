46d7d629e12b83151724deceb9d7db6ebcc20d64
Phillip Urquijo <purquijo@gmail.com>
2016-01-15 07:31:10 +0000

    replace stdLightMesons and add stdCharm
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@24348 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/scripts/stdLightMesons.py
+++ analysis/scripts/stdLightMesons.py
@@ -4,24 +4,92 @@
 from basf2 import *
 from modularAnalysis import *
 
-
-def stdLightMesons(path=analysis_main):
-
-    stdLooseKS0(path)
-    stdRho(path)
-    stdKstar(path)
+# Call to build all light mesons. Not recommended to use this general function as it creates many candidates
 
 
-def stdLooseKS0(path=analysis_main):
-    reconstructDecay('K_S0:loose -> pi-:loose pi+:loose', '0.4 < M < 0.6', 1,
-                     True, path)
+def loadStdLightMesons(persistent=True, path=analysis_main):
+
+    loadStdLooseRho0(persistent, path)
+    loadStdLooseRhoPlus(persistent, path)
+    loadStdLooseKstar(persistent, path)
+    loadStdLoosePhi(persistent, path)
+    loadStdLooseF_0(persistent, path)
+    loadStdLooseOmega(persistent, path)
+    loadStdLooseEta(persistent, path)
+    loadStdLooseEtaPrime(persistent, path)
+
+# NoCut mesons not to be included by default
 
 
-def stdRho(path=analysis_main):
-    reconstructDecay('rho0:loose -> pi-:loose pi+:loose', '0.5 < M < 1.0', 1,
-                     True, path)
+def loadStdLooseRho0(persistent=True, path=analysis_main):
+    reconstructDecay('rho0:loose -> pi-:loose pi+:loose', '0.47 < M < 1.07', 1, persistent, path)
+    return 'rho0:loose'
 
 
-def stdKstar(path=analysis_main):
-    reconstructDecay('K*0:loose -> pi-:loose K+:loose', '0.8 < M < 1.1', 1,
-                     True, path)
+def loadStdAllRho0(persistent=True, path=analysis_main):
+    reconstructDecay('rho0:all -> pi-:all pi+:all', '0.47 < M < 1.07', 1, persistent, path)
+    return 'rho0:all'
+
+
+def loadStdLooseRhoPlus(persistent=True, path=analysis_main):
+    reconstructDecay('rho+:loose -> pi0:all pi+:loose', '0.47 < M < 1.07', 1, persistent, path)
+    return 'rho+:loose'
+
+
+def loadStdAllRhoPlus(persistent=True, path=analysis_main):
+    reconstructDecay('rho+:all -> pi0:all pi+:all', '0.47 < M < 1.07', 1, persistent, path)
+    return 'rho+:all'
+
+
+def loadStdLooseKstar(persistent=True, path=analysis_main):
+    reconstructDecay('K*0:loose -> pi-:loose K+:loose', '0.74 < M < 1.04', 1, persistent, path)
+    return 'K*0:loose'
+
+
+def loadStdAllKstar(persistent=True, path=analysis_main):
+    reconstructDecay('K*0:all -> pi-:all K+:all', '0.74 < M < 1.04', 1, persistent, path)
+    return 'K*0:all'
+
+
+def loadStdAllPhi(persistent=True, path=analysis_main):
+    reconstructDecay('phi:all -> K+:all K-:all', '0.97 < M < 1.1', 1, persistent, path)
+    return 'phi:all'
+
+
+def loadStdLoosePhi(persistent=True, path=analysis_main):
+    reconstructDecay('phi:loose -> K+:loose K-:loose', '0.97 < M < 1.1', 1, persistent, path)
+    return 'phi:loose'
+
+
+def loadStdAllF_0(persistent=True, path=analysis_main):
+    reconstructDecay('f_0:all -> pi+:all pi-:all', '0.78 < M < 1.18', 1, persistent, path)
+    return 'f_0:all'
+
+
+def loadStdLooseF_0(persistent=True, path=analysis_main):
+    reconstructDecay('f_0:loose -> pi+:loose pi-:loose', '0.78 < M < 1.18', 1, persistent, path)
+    return 'f_0:loose'
+
+
+def loadStdAllOmega(persistent=True, path=analysis_main):
+    reconstructDecay('omega:all -> pi0:all pi-:all pi+:all', '0.73 < M < 0.83', 1, persistent, path)
+    return 'omega:all'
+
+
+def loadStdLooseOmega(persistent=True, path=analysis_main):
+    reconstructDecay('omega:loose -> pi0:all pi-:loose pi+:loose', '0.73 < M < 0.83', 1, persistent, path)
+    return 'omega:loose'
+
+
+def loadStdLooseEta(persistent=True, path=analysis_main):
+    reconstructDecay('eta:loose1 -> gamma:all gamma:all', '0.4 < M < 0.6', 1, persistent, path)
+    reconstructDecay('eta:loose2 -> pi0:all pi-:loose pi+:loose', '0.4 < M < 0.6', 2, persistent, path)
+    copyLists('eta:loose', ['eta:loose1', 'eta:loose2'], persistent, path)
+    return 'eta:loose'
+
+
+def loadStdLooseEtaPrime(persistent=True, path=analysis_main):
+    reconstructDecay('eta\':loose1 -> pi+:loose pi-:loose gamma:all', '0.8 < M < 1.1', 1, persistent, path)
+    reconstructDecay('eta\':loose2 -> pi+:loose pi-:loose eta:loose', '0.8 < M < 1.1', 2, persistent, path)
+    copyLists('eta\':loose', ['eta\':loose1', 'eta\':loose2'], persistent, path)
+    return 'eta\':loose'

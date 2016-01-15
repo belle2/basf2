2978f4d51415c62fa06983223be441d1dc2c87dd
Phillip Urquijo <purquijo@gmail.com>
2016-01-15 06:59:10 +0000

    separate out the photon criteria into stdPhotons
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@24345 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ analysis/scripts/stdPhotons.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from basf2 import *
+from modularAnalysis import *
+# Prepare all standard final state particles
+
+
+def loadStdAllPhoton(path=analysis_main):
+    fillParticleList('gamma:all', '', True, path)
+    calibratePhotonEnergy('gamma:all', 0.030, path)
+
+
+def loadStdPhoton(path=analysis_main):
+    loadStdAllPhoton(path)
+
+
+def loadStdGoodPhoton(path=analysis_main):
+    loadStdAllPhoton(path)
+    cutAndCopyList('gamma:good', 'gamma:all', '0.5 < goodGamma < 1.5', True, path)
+
+
+def loadStdPhotonE12(path=analysis_main):
+    loadStdAllPhoton(path)
+    cutAndCopyList('gamma:E12', 'gamma:all', '1.2 < E < 100', True, path)
+
+
+def loadStdPhotonE15(path=analysis_main):
+    loadStdAllPhoton(path)
+    cutAndCopyList('gamma:E15', 'gamma:all', '1.5 < E < 100', True, path)

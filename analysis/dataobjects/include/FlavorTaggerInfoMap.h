d1200c5ad177350b25508d9684a8a04949e35f8b
Fernando Abudinen <abudinen@mpp.mpg.de>
2016-01-19 16:45:31 +0000

    Added FlavorTaggerInfoMap for each method
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@24426 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ analysis/dataobjects/include/FlavorTaggerInfoMap.h
@@ -0,0 +1,200 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Fernando Abudinen                                        *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef FLAVORTAGGERINFOMAP_H
+#define FLAVORTAGGERINFOMAP_H
+
+#include <framework/datastore/RelationsObject.h>
+#include <analysis/dataobjects/Particle.h>
+
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+
+namespace Belle2 {
+
+  // forward declarations
+
+  class Track;
+
+  /**
+   * This class stores the Flavor Tagger information for a specific method and particle filled in the
+   * Flavor Tagger Module.
+   *
+   *
+   * The way the information is stored is into maps for each Level: Track, event and Combiner.
+   * there are two maps per level: One saving the pointer to the tracks and the other saving the probabilities.
+   * Each category has its own key.
+   *
+   */
+
+  class FlavorTaggerInfoMap : public RelationsObject {
+
+  public:
+
+    /**
+     * Default constructor.
+     * All private members are set to 0 (all vectors are empty).
+     */
+    FlavorTaggerInfoMap()
+    {
+      m_targetTrackLevel = {};
+      m_probTrackLevel = {};
+      m_targetEventLevel = {};
+      m_probEventLevel = {};
+      m_qrCategory = {};
+      m_qrCombined = -2;
+      m_B0Probability = -2;
+      m_B0barProbability = -2;
+    };
+
+
+    /**
+     * SETTERS
+     * The setters are expected to fill a map whose element corresponds to the probabilities and tracks of a
+     * given category. The categories are set by the Flavor Tagger.
+     * The setting of all elements need to be performed in a loop. For example, setTargetTrackLevel() need to be called
+     * once per each category so that the vector is filled progressively. The same holds for all the other
+     * setters described as map fillers.
+     */
+
+    /**
+    * Map filler: Set the category name and the pointer of the track with the highest target track probability
+    * for the corresponding category in Event Level.
+    * @param category string name of the given category
+    * @param track pointer to track object
+    */
+    void setTargetTrackLevel(std::string category, Belle2::Track* track);
+
+    /**
+    * Map filler: Set the category name and the corresponding highest target track probability.
+    * @param category string name of the given category
+    * @param probability highest target track probability
+    */
+    void setProbTrackLevel(std::string category, float probability);
+
+    /**
+    * Map filler: Set the category name and the pointer of the track with the highest category probability
+    * for the corresponding category in Event Level.
+    * @param category string name of the given category
+    * @param track pointer to track object
+    */
+    void setTargetEventLevel(std::string category, Belle2::Track* track);
+
+    /**
+    * Map filler: Set the category name and the highest category probability for the corresponding
+    * category in Event Level.
+    * @param category string name of the given category
+    * @param probability highest category probability
+    */
+    void setProbEventLevel(std::string category, float probability);
+
+    /**
+    * Map filler: Set the category name and the corresponding qr Output, i.e. the Combiner input value.
+    * They could be used for independent tags.
+    * @param category string name of the given category
+    * @param qr output of the given category
+    */
+    void setQrCategory(std::string category, float qr);
+
+    /**
+    * Saves qr Output of the Combiner. Output of the FlavorTagger after the complete process.
+    * @param qr final FlavorTagger output
+    */
+    void setQrCombined(float qr);
+
+    /**
+    * Saves the B0Probability output of the Combiner.
+    * @param B0Probability probability of being a B0
+    */
+    void setB0Probability(float B0Probability);
+
+    /**
+    * Saves the B0barProbability output of the Combiner.
+    * @param B0barProbability Probability of being a B0bar
+    */
+    void setB0barProbability(float B0barProbability);
+
+
+
+    /**
+     * GETTERS
+     */
+
+    /**< map containing the category name and the corresponding pointer to the track with highest target probability in Track Level
+    * @return map
+    */
+    std::map<std::string, Belle2::Track*> getTargetTrackLevel();
+
+    /**< map containing the category name and thecorresponding highest target track probability in Track Level
+    * @return map
+    */
+    std::map<std::string, float> getProbTrackLevel();
+
+    /**< map containing the category name and the corresponding pointer to the track with highest category probability in Event Level
+    * @return map
+    */
+    std::map<std::string, Belle2::Track*> getTargetEventLevel();
+
+    /**< map containing the category name and the corresponding highest category probability in Event Level
+    * @return map
+    */
+    std::map<std::string, float> getProbEventLevel();
+
+    /**< map containing the category name and the corresponding qr Output
+    * @return map
+    */
+    std::map<std::string, float> getQrCategory();
+
+    /**< qr Output of the Combiner
+    * @return qr
+    */
+    float getQrCombined();
+
+    /**Probability of being a B0.
+    * @return B0Probability
+    */
+    float getB0Probability();
+
+    /**Probability of being a B0bar.
+    * @return B0barProbability
+    */
+    float getB0barProbability(); /**< Direct Output of the Combiner: Probability of being a B0bar).*/
+
+
+  private:
+
+    // persistent data members
+
+    // Track, Event and Combiner Level Flavor Tagger Info
+
+    std::map<std::string, Belle2::Track*>
+    m_targetTrackLevel; /**< map containing the category name and the corresponding pointer to the track with highest target probability in Track Level*/
+    std::map<std::string, float>
+    m_probTrackLevel; /**< map containing the category name and thecorresponding highest target track probability in Track Level*/
+    std::map<std::string, Belle2::Track*>
+    m_targetEventLevel; /**< map containing the category name and the corresponding pointer to the track with highest category probability in Event Level*/
+    std::map<std::string, float>
+    m_probEventLevel; /**< map containing the category name and the corresponding highest category probability in Event Level*/
+    std::map<std::string, float>
+    m_qrCategory; /**< map containing the category name and the corresponding qr Output, i.e. the Combiner input value. They could be used for independent tags.*/
+
+    float m_qrCombined; /**< qr Output of the Combiner. Output of the FlavorTagger after the complete process*/
+    float m_B0Probability; /**< Direct Output of the Combiner: Probability of being a B0.*/
+    float m_B0barProbability; /**< Direct Output of the Combiner: Probability of being a B0bar).*/
+
+    ClassDef(FlavorTaggerInfoMap, 3) /**< class definition */
+  };
+
+
+} // end namespace Belle2
+
+#endif
---
+++ analysis/dataobjects/src/FlavorTaggerInfoMap.cc
@@ -0,0 +1,116 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Christian Roca and Fernando Abudinen                     *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <analysis/dataobjects/FlavorTaggerInfoMap.h>
+
+#include <framework/datastore/StoreArray.h>
+
+#include <mdst/dataobjects/Track.h>
+#include <mdst/dataobjects/MCParticle.h>
+
+#include <framework/logging/Logger.h>
+#include <iostream>
+
+using namespace Belle2;
+
+
+
+/************************SETTERS***********************/
+
+
+void FlavorTaggerInfoMap::setTargetTrackLevel(std::string category, Track* track)
+{
+  m_targetTrackLevel.insert(std::pair<std::string, Track*>(category, track));
+}
+
+
+void FlavorTaggerInfoMap::setProbTrackLevel(std::string category, float probability)
+{
+  m_probTrackLevel.insert(std::pair<std::string, float>(category, probability));
+}
+
+
+void FlavorTaggerInfoMap::setTargetEventLevel(std::string category, Belle2::Track* track)
+{
+  m_targetEventLevel.insert(std::pair<std::string, Track*>(category, track));
+}
+
+
+void FlavorTaggerInfoMap::setProbEventLevel(std::string category, float probability)
+{
+  m_probEventLevel.insert(std::pair<std::string, float>(category, probability));
+}
+
+
+void FlavorTaggerInfoMap::setQrCategory(std::string category, float qr)
+{
+  m_qrCategory.insert(std::pair<std::string, float>(category, qr));
+}
+
+void FlavorTaggerInfoMap::setQrCombined(float qr)
+{
+  m_qrCombined = qr;
+}
+
+
+void FlavorTaggerInfoMap::setB0Probability(float B0Probability)
+{
+  m_B0Probability = B0Probability;
+}
+
+
+void FlavorTaggerInfoMap::setB0barProbability(float B0barProbability)
+{
+  m_B0barProbability = B0barProbability;
+}
+
+
+/************************GETTERS***********************/
+
+
+std::map<std::string, Belle2::Track*> FlavorTaggerInfoMap::getTargetTrackLevel()
+{
+  return m_targetTrackLevel;
+}
+
+std::map<std::string, float> FlavorTaggerInfoMap::getProbTrackLevel()
+{
+  return m_probTrackLevel;
+}
+
+std::map<std::string, Belle2::Track*> FlavorTaggerInfoMap::getTargetEventLevel()
+{
+  return m_targetEventLevel;
+}
+
+std::map<std::string, float> FlavorTaggerInfoMap::getProbEventLevel()
+{
+  return m_probEventLevel;
+}
+
+std::map<std::string, float> FlavorTaggerInfoMap::getQrCategory()
+{
+  return m_qrCategory;
+}
+
+float FlavorTaggerInfoMap::getQrCombined()
+{
+  return m_qrCombined;
+}
+
+float FlavorTaggerInfoMap::getB0Probability()
+{
+  return m_B0Probability;
+}
+
+float FlavorTaggerInfoMap::getB0barProbability()
+{
+  return m_B0barProbability;
+}

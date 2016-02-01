9f30bc99683e70cda4e021b20b90b8aa98620dc5
Anze Zupanc <anze.zupanc@ijs.si>
2016-02-01 14:39:10 +0000

    Module checks if the ROE object in the for_each ROE path is related to any of the Particles from the input list.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@24951 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ analysis/modules/SignalSideParticleFilter/include/SignalSideParticleFilterModule.h
@@ -0,0 +1,59 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Anze Zupanc                                              *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef SIGNALSIDEPARTICLEFILTERMODULE_H
+#define SIGNALSIDEPARTICLEFILTERMODULE_H
+
+#include <framework/core/Module.h>
+#include <analysis/VariableManager/Utility.h>
+
+#include <analysis/dataobjects/ParticleList.h>
+#include <framework/datastore/StoreObjPtr.h>
+
+#include <string>
+#include <memory>
+
+namespace Belle2 {
+  /**
+   * The module returns true if the current RestOfEvent object is related to any of the Particles from the input ParticleList.
+   *
+   * This module should be executed in for_each Rest of Event path only. The ROE objects in DataStore might be created just for Particles
+   * from one ParticleList, or they might be created for particles from multiple ParticleLists, or particles reconstructed in multiple
+   * decay modes. With this module we can select only those ROE objects that are related to Particles that we are currently interested in.
+   * The additional selection criteria is optional.
+   */
+  class SignalSideParticleFilterModule : public Module {
+
+  public:
+
+    /**
+     * Constructor: Sets the description, the properties and the parameters of the module.
+     */
+    SignalSideParticleFilterModule();
+
+    /**  */
+    virtual void initialize();
+
+    /**  */
+    virtual void event();
+
+
+  private:
+
+    std::string m_particleListName;  /**< Input ParticleList name */
+    StoreObjPtr<ParticleList> m_inputList; /**< input particle list */
+
+    std::string m_selection;  /**< Additional selection criteria */
+    std::unique_ptr<Variable::Cut> m_cut; /**< cut object which performs the cuts */
+
+  };
+}
+
+#endif /* SIGNALSIDEPARTICLEFILTERMODULE_H */
---
+++ analysis/modules/SignalSideParticleFilter/src/SignalSideParticleFilterModule.cc
@@ -0,0 +1,73 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2013 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Anze Zupanc                                              *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <analysis/modules/SignalSideParticleFilter/SignalSideParticleFilterModule.h>
+
+#include <framework/datastore/StoreArray.h>
+#include <analysis/dataobjects/Particle.h>
+#include <analysis/dataobjects/RestOfEvent.h>
+
+using namespace Belle2;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(SignalSideParticleFilter)
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+SignalSideParticleFilterModule::SignalSideParticleFilterModule() : Module()
+{
+  // Set module properties
+  setDescription("The module returns true if the current RestOfEvent object is related to\n"
+                 "any of the Particles from the input ParticleList and passes selection criteria.\n"
+                 "The module should be executed only in the for_each ROE path.");
+
+  // Parameter definitions
+  addParam("particleListName", m_particleListName, "Input ParticleList name", std::string(""));
+  addParam("selection", m_selection, "Additional selection criteria", std::string(""));
+}
+
+void SignalSideParticleFilterModule::initialize()
+{
+  StoreArray<Particle>::required();
+  m_inputList.isRequired(m_particleListName);
+
+  m_cut = Variable::Cut::Compile(m_selection);
+
+}
+
+void SignalSideParticleFilterModule::event()
+{
+  setReturnValue(false);
+  if (!m_inputList) {
+    B2WARNING("Input list " << m_inputList.getName() << " was not created?");
+    return;
+  }
+
+  const unsigned int numParticles = m_inputList->getListSize();
+  if (numParticles == 0)
+    return;
+
+  bool inTheList = false;
+  StoreObjPtr<RestOfEvent> roe("RestOfEvent");
+  if (roe.isValid()) {
+    const Particle* particle = roe->getRelated<Particle>();
+    if (particle)
+      if (m_inputList->contains(particle) && m_cut->check(particle))
+        inTheList = true;
+  }
+
+  setReturnValue(inTheList);
+}
+
+

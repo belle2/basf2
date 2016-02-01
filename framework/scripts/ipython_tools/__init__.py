9e646697abfc6af502abc78c3dc720cb94d1ba46
Nils Braun <nils.braun@student.kit.edu>
2016-02-01 07:29:25 +0000

    Deleted obsolete comments.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@24932 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/scripts/ipython_tools/__init__.py
+++ framework/scripts/ipython_tools/__init__.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# The needed scrips from this package
-# System imports
 
-# basf2 imports
-
-# multiprocessing imports
-
-# SVN imports
 from ipython_tools.ipython_handler import IPythonHandler
 
 #: Create a single instance

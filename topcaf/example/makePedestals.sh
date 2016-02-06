74032ca03171ef67ee5305b8072a317682fb801f
Jan Fridolf Strube <jan.strube@pnnl.gov>
2016-02-06 05:09:40 +0000

    makePedestals now creates a tmp dir for operation
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@25097 b38c6c8f-bee8-465d-8448-9abe6915d496

--- topcaf/example/makePedestals.sh
+++ topcaf/example/makePedestals.sh
@@ -2,13 +2,19 @@
 
 ### Usage: ./makePedestals nFrames file1 file2 file3
 
+mkdir garbleStrube
+cd garbleStrube
+
 for infile in ${2} ${3} ${4}
 do
 	inputRun=$(basename ${infile})
 	inputDir=$(dirname ${infile})
-	basf2 topcaf_itop_makeped_IRSXGIGE_pocketDAQ.py -n ${1} --arg --inputRun ${inputRun/.dat/} --arg --inputDir ${inputDir}
+	basf2 ../topcaf_itop_makeped_IRSXGIGE_pocketDAQ.py -n ${1} --arg --inputRun ${inputRun/.dat/} --arg --inputDir ${inputDir}
 done
 
 hadd $(basename ${2/.dat/})_Pedestals.root *PedestalCalibration.root
 rm *PedestalCalibration.root
+mv *_Pedestals.root ..
+cd ..
+rm -r garbleStrube
 

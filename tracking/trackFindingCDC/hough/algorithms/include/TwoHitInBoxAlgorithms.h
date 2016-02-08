a0b31e0c8a61b5491bddb1043009a75df71a653b
Nils Braun <nils.braun@student.kit.edu>
2016-02-08 15:14:54 +0000

    Add an algorithm for mixing two HitInBox algorithms.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@25196 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingCDC/hough/algorithms/include/TwoHitInBoxAlgorithms.h
@@ -0,0 +1,52 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Nils Braun                                               *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#include <tracking/trackFindingCDC/numerics/numerics.h>
+
+
+namespace Belle2 {
+  namespace TrackFindingCDC {
+
+    /**
+     * Predicate class to check for the containment of items in a hough space part with two algorithms.
+     * The resulting weight is added. If one of the two algorithms gives NaN, this particular result is
+     * not used (only the other). If both give NaN, the result will also be NaN.
+     */
+    template<class AHitInBoxAlgorithm, class AnotherHitInBoxAlgorithm>
+    class TwoHitInBoxAlgorithm {
+    public:
+      /// The type of the underlaying HoughBox (copied from the first underlying hit algorithm)
+      typedef typename AHitInBoxAlgorithm::HoughBox HoughBox;
+
+      /**
+       * Returns the sum of the resulting weights of both algorithms (of not NAN).
+       */
+      template<class AObject>
+      inline Weight operator()(const AObject& object,
+                               const HoughBox* box)
+      {
+        AHitInBoxAlgorithm hitInBoxAlgorithm;
+        AnotherHitInBoxAlgorithm anotherHitInBoxAlgorithm;
+
+        const Weight& firstResult = hitInBoxAlgorithm(object, box);
+        const Weight& secondResult = anotherHitInBoxAlgorithm(object, box);
+
+        if (std::isnan(firstResult) and std::isnan(secondResult)) {
+          return NAN;
+        } else if (std::isnan(firstResult)) {
+          return secondResult;
+        } else if (std::isnan(secondResult)) {
+          return firstResult;
+        } else {
+          return firstResult + secondResult;
+        }
+      }
+    };
+  }
+}

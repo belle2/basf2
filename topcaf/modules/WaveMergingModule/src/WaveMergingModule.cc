d984874cea7ecf0a50d75a422e675c44964b2b68
Jan Fridolf Strube <jan.strube@pnnl.gov>
2016-02-19 09:28:16 +0000

    updates to satisfy static analysis tools
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@25542 b38c6c8f-bee8-465d-8448-9abe6915d496

--- topcaf/modules/WaveMergingModule/src/WaveMergingModule.cc
+++ topcaf/modules/WaveMergingModule/src/WaveMergingModule.cc
@@ -31,54 +31,55 @@ void WaveMergingModule::event()
 
 
 
-  if (evtwaves_ptr) {
-
-    std::map<unsigned int, EventWaveformPackets > ch2wp;
-    for (int w = 0; w < evtwaves_ptr.getEntries(); w++) {
-      unsigned int channel_id = evtwaves_ptr[w]->GetChannelID();
-
-      if (ch2wp[channel_id].size() == 0) {
-        EventWaveformPackets ch_waves;
-        ch_waves.push_back(evtwaves_ptr[w]);
-        ch2wp[channel_id] = ch_waves;
-        //B2INFO("Wave packets found with channel_id "<<channel_id);
-      } else {
-        EventWaveformPackets ch_waves = ch2wp[channel_id];
-        ch_waves.push_back(evtwaves_ptr[w]);
-        ch2wp[channel_id] = ch_waves;
-      }
-    }
-    //Create new event with merged waveforms
-    EventWaveformPackets corrected_waves;
-    std::map<unsigned int, EventWaveformPackets>::iterator it = ch2wp.begin();
-    for (; it != ch2wp.end(); ++it) {
-      //Sort waveform (?)
-      //Assumed it's already in the right order!!!
-      EventWaveformPacket* merged_wave;
-      std::vector< double > merged_samples;
-      //std::cout << "ch_id/waves: " << it->first << "/" << it->second.size()  << std::endl;
-      for (unsigned int p = 0; p < it->second.size(); p++) {
-        if (p == 0) {
-          merged_wave = new EventWaveformPacket(*it->second.at(p));
-        }
-        std::vector< double > samples = it->second.at(p)->GetSamples();
-        for (unsigned int s = 0; s < samples.size(); s++) {
-          merged_samples.push_back(samples.at(s));
-        }
-      }
-      merged_wave->SetSamples(merged_samples);
-      corrected_waves.push_back(merged_wave);
-    }
-
-    /// Now clear the old waveform partials and store the new merged waveforms.
-    evtwaves_ptr.clear();
-    EventWaveformPackets::iterator wave_it = corrected_waves.begin();
-    for (; wave_it != corrected_waves.end(); ++wave_it) {
-      evtwaves_ptr.appendNew(EventWaveformPacket(**wave_it));
-      delete *wave_it;
-    }
-
+  if (not evtwaves_ptr) {
+    return;
   }
+
+  std::map<unsigned int, EventWaveformPackets > ch2wp;
+  for (int w = 0; w < evtwaves_ptr.getEntries(); w++) {
+    unsigned int channel_id = evtwaves_ptr[w]->GetChannelID();
+
+    if (ch2wp[channel_id].size() == 0) {
+      EventWaveformPackets ch_waves;
+      ch_waves.push_back(evtwaves_ptr[w]);
+      ch2wp[channel_id] = ch_waves;
+      //B2INFO("Wave packets found with channel_id "<<channel_id);
+    } else {
+      EventWaveformPackets ch_waves = ch2wp[channel_id];
+      ch_waves.push_back(evtwaves_ptr[w]);
+      ch2wp[channel_id] = ch_waves;
+    }
+  }
+  //Create new event with merged waveforms
+  EventWaveformPackets corrected_waves;
+  std::map<unsigned int, EventWaveformPackets>::iterator it = ch2wp.begin();
+  for (; it != ch2wp.end(); ++it) {
+    if (it->second.empty()) {
+      continue;
+    }
+    //Sort waveform (?)
+    //Assumed it's already in the right order!!!
+    EventWaveformPacket* merged_wave = new EventWaveformPacket(*it->second.at(0));
+    std::vector< double > merged_samples;
+    //std::cout << "ch_id/waves: " << it->first << "/" << it->second.size()  << std::endl;
+    for (unsigned int p = 0; p < it->second.size(); p++) {
+      std::vector< double > samples = it->second.at(p)->GetSamples();
+      for (unsigned int s = 0; s < samples.size(); s++) {
+        merged_samples.push_back(samples.at(s));
+      }
+    }
+    merged_wave->SetSamples(merged_samples);
+    corrected_waves.push_back(merged_wave);
+  }
+
+  /// Now clear the old waveform partials and store the new merged waveforms.
+  evtwaves_ptr.clear();
+  EventWaveformPackets::iterator wave_it = corrected_waves.begin();
+  for (; wave_it != corrected_waves.end(); ++wave_it) {
+    evtwaves_ptr.appendNew(EventWaveformPacket(**wave_it));
+    delete *wave_it;
+  }
+
 }
 
 void WaveMergingModule::terminate()

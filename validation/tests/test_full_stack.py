a27baf50d3175a6411bd4a075e43ed4d619ec4de
Thomas Hauth <thomas.hauth@kit.edu>
2016-02-19 15:46:13 +0000

    test will fail now, if validate_basf2 returns an error code
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@25573 b38c6c8f-bee8-465d-8448-9abe6915d496

--- validation/tests/test_full_stack.py
+++ validation/tests/test_full_stack.py
@@ -16,16 +16,18 @@ except ImportError:
     sys.exit(0)
 
 
-def check_excecute(cmd):
+def check_excecute(cmd, terminate_on_error=True):
     """
     Executes a shell commands and check for =! 0 return codes
     """
 
     print("Executing command '{}'".format(cmd))
     res = os.system(cmd)
-    print("Command '{}' exited with code ".format(res))
+    print("Command '{}' exited with code {}".format(cmd, res))
     if not res == 0:
-        print("Exit code is not 0")
+        print("FATAL: Exit code is not 0")
+        if terminate_on_error:
+            sys.exit(res)
         return False
 
     return True

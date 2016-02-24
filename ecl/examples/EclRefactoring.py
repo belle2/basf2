cb5a2165b082e84acecf56ff8f320298a532a846
Torben Ferber <ferber@physics.ubc.ca>
2016-02-24 00:16:26 +0000

    Script to run the full simulation and reconstruction with the refatcored ECL code (which itself is under development)
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@25707 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ ecl/examples/EclRefactoring.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+########################################################
+#
+# This steering file includes all modules of the
+# refactored ECL code.
+#
+# Example steering file - 2016 Belle II Collaboration
+# Author(s): Torben Ferber (ferber@physics.ubc.ca)
+#
+########################################################
+
+import os
+from basf2 import *
+from simulation import add_simulation
+from reconstruction import add_tracking_reconstruction, add_prune_tracks, add_mdst_output
+
+import sys
+import glob
+
+# user input
+withbg = 0  # add beam background boolean
+bgfolder = '.'  # folder that holds beam background
+bgfrac = 0.5  # fracion of nominal background
+seed = 12345  # seed for random numbers
+mdstfile = 'out.root'  # output file
+
+# set log level
+set_log_level(LogLevel.INFO)
+
+# fix random seed
+set_random_seed(seed)
+
+# create main path
+main = create_path()
+
+# add event infosetter
+eventinfosetter = register_module('EventInfoSetter')
+main.add_module(eventinfosetter)
+
+# add generator
+evtgeninput = register_module('EvtGenInput')
+main.add_module(evtgeninput)
+
+# add default full simulation and digitization
+if (withbg == 1):
+    bg = glob.glob(bgfolder + '/*.root')
+    add_simulation(main, bkgfiles=bg, bkgfraction=bgfrac)
+else:
+    add_simulation(main)
+
+# add reconstruction
+# tracking
+add_tracking_reconstruction(main)
+# post tracking
+
+# CDC PID
+CDCdEdxPID = register_module('CDCDedxPID')
+main.add_module(CDCdEdxPID)
+
+# VXD PID
+VXDdEdxPID = register_module('VXDDedxPID')
+main.add_module(VXDdEdxPID)
+
+# prune tracks
+add_prune_tracks(main)
+
+# extrapolation
+ext = register_module('Ext')
+main.add_module(ext)
+
+# top
+top_rec = register_module('TOPReconstructor')
+main.add_module(top_rec)
+
+# arich
+arich_rec = register_module('ARICHReconstructor')
+main.add_module(arich_rec)
+
+# ECL digit calibration
+ecl_digit_calibration = register_module('ECLDigitCalibrator')
+main.add_module(ecl_digit_calibration)
+
+# ECL shower reconstruction
+ecl_shower_rec = register_module('ECLReconstructor')
+main.add_module(ecl_shower_rec)
+
+# ECL track shower matching
+ecl_track_match = register_module('ECLTrackShowerMatch')
+main.add_module(ecl_track_match)
+
+# ECL electron ID
+electron_id = register_module('ECLElectronId')
+main.add_module(electron_id)
+
+# ECL MC matching
+ecl_mc = register_module('MCMatcherECLClusters')
+main.add_module(ecl_mc)
+
+# EKLM
+eklm_rec = register_module('EKLMReconstructor')
+main.add_module(eklm_rec)
+
+# K0L reconstruction
+eklm_k0l_rec = register_module('EKLMK0LReconstructor')
+main.add_module(eklm_k0l_rec)
+
+# BKLM
+bklm_rec = register_module('BKLMReconstructor')
+main.add_module(bklm_rec)
+
+# K0L reconstruction
+bklm_k0l_rec = register_module('BKLMK0LReconstructor')
+main.add_module(bklm_k0l_rec)
+
+# muon identifcation
+muid = register_module('Muid')
+main.add_module(muid)
+
+# charged particle PID
+mdstPID = register_module('MdstPID')
+main.add_module(mdstPID)
+
+# add output file with most of the available information
+add_mdst_output(
+    main,
+    mc=True,
+    filename=mdstfile,
+    additionalBranches=[
+        'ECLDigits',
+        'ECLCalDigits',
+        'ECLConnectedRegions',
+        'ECLShowers',
+        'ECLEventInformation'])
+
+# Show progress of processing
+progress = register_module('ProgressBar')
+main.add_module(progress)
+
+process(main)
+print(statistics)

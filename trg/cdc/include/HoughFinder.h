cea466bfe4427ec6b18e0352b642d4999a5036f6
Sara Neuhaus <sneuhaus@mpp.mpg.de>
2016-02-24 11:44:08 +0000

    fixstyle
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@25719 b38c6c8f-bee8-465d-8448-9abe6915d496

--- trg/cdc/include/HoughFinder.h
+++ trg/cdc/include/HoughFinder.h
@@ -27,27 +27,27 @@
 
 namespace Belle2 {
 
-class TRGCDC;
-class TRGCDCPeakFinder;
-class TRGCDCTrack;
-class TRGCDCLink;
-class TRGCDCJSignal;  
-class TRGCDCJLUT;  
-class TRGCDCJSignalData;
+  class TRGCDC;
+  class TRGCDCPeakFinder;
+  class TRGCDCTrack;
+  class TRGCDCLink;
+  class TRGCDCJSignal;
+  class TRGCDCJLUT;
+  class TRGCDCJSignalData;
 
 /// A class to find tracks using Hough algorithm
-class TRGCDCHoughFinder {
+  class TRGCDCHoughFinder {
 
   public:
 
     /// Contructor.
-    TRGCDCHoughFinder(const std::string & name,
-                      const TRGCDC &,
+    TRGCDCHoughFinder(const std::string& name,
+                      const TRGCDC&,
                       unsigned nX,
                       unsigned nY,
-		      unsigned peakMin,
-                      const std::string & mappingFilePlus,
-                      const std::string & mappingFileMinus,
+                      unsigned peakMin,
+                      const std::string& mappingFilePlus,
+                      const std::string& mappingFileMinus,
                       unsigned doit);
 
     /// Destructor
@@ -65,8 +65,8 @@ class TRGCDCHoughFinder {
     void terminate();
 
     /// do track finding and fitting.
-    int doit(std::vector<TRGCDCTrack *> & trackList2D,
-             std::vector<TRGCDCTrack *> & trackList2DFitted);
+    int doit(std::vector<TRGCDCTrack*>& trackList2D,
+             std::vector<TRGCDCTrack*>& trackList2DFitted);
 
   public:
 
@@ -76,73 +76,73 @@ class TRGCDCHoughFinder {
   private:
 
     /// do track finding and fitting (Trasan version).
-    int doit1(std::vector<TRGCDCTrack *> & trackList2D,
-              std::vector<TRGCDCTrack *> & trackList2DFitted);
+    int doit1(std::vector<TRGCDCTrack*>& trackList2D,
+              std::vector<TRGCDCTrack*>& trackList2DFitted);
 
     /// do track finding and fitting (Kaiyu version).
-    int doit2(std::vector<TRGCDCTrack *> & trackList2D,
-              std::vector<TRGCDCTrack *> & trackList2DFitted);
+    int doit2(std::vector<TRGCDCTrack*>& trackList2D,
+              std::vector<TRGCDCTrack*>& trackList2DFitted);
 
     /// do track finding and fitting (development version)
-    int doit3(std::vector<TRGCDCTrack *> & trackList2D,
-              std::vector<TRGCDCTrack *> & trackList2DFitted);
+    int doit3(std::vector<TRGCDCTrack*>& trackList2D,
+              std::vector<TRGCDCTrack*>& trackList2DFitted);
 
     /// selects the best(fastest) hits in each super layer.
-    std::vector<TRGCDCLink *> selectBestHits(
-	const std::vector<TRGCDCLink *> & links) const;
+    std::vector<TRGCDCLink*> selectBestHits(
+      const std::vector<TRGCDCLink*>& links) const;
 
     /// creates mappings by a file. (Kaiyu's original)
-    void mappingByFile(const std::string & mappingFilePlus,
-                       const std::string & mappingFileMinus);
+    void mappingByFile(const std::string& mappingFilePlus,
+                       const std::string& mappingFileMinus);
 
     /// creates mappings by a file.
-    void mappingByFile2(const std::string & mappingFilePlus,
-                        const std::string & mappingFileMinus);
+    void mappingByFile2(const std::string& mappingFilePlus,
+                        const std::string& mappingFileMinus);
 
     /// do track finding. (trasan version)
     int doFinding(std::vector<unsigned> peaks[],
-                  std::vector<TRGCDCTrack *> & trackList2D) const;
+                  std::vector<TRGCDCTrack*>& trackList2D) const;
 
     /// do track finding. (kaiyu version)
     int doFinding2(std::vector<std::vector<unsigned>> peaks[],
-                   std::vector<TRGCDCTrack *> & trackList2D);
+                   std::vector<TRGCDCTrack*>& trackList2D);
 
     /// do track fitting. (old trasan version)
     int doFitting(std::vector<unsigned> peaks[],
-                  std::vector<TRGCDCTrack *> & trackList2DFitted) const;
+                  std::vector<TRGCDCTrack*>& trackList2DFitted) const;
 
     /// do track fitting. (kaiyu original)
-    int doFitting2(std::vector<TRGCDCTrack *> & trackList2D,
-                   std::vector<TRGCDCTrack *> & trackList2DFitted);
+    int doFitting2(std::vector<TRGCDCTrack*>& trackList2D,
+                   std::vector<TRGCDCTrack*>& trackList2DFitted);
 
     /// do track fitting.
-    int doFitting3(std::vector<TRGCDCTrack *> & trackList2D,
-                   std::vector<TRGCDCTrack *> & trackList2DFitted);
+    int doFitting3(std::vector<TRGCDCTrack*>& trackList2D,
+                   std::vector<TRGCDCTrack*>& trackList2DFitted);
 
     /// Make a track from serial ID in Hough plane. (no fit, segment
     /// hit attached)
-    TRGCDCTrack * makeTrack(const unsigned serialID, const unsigned pm) const;
+    TRGCDCTrack* makeTrack(const unsigned serialID, const unsigned pm) const;
 
     ///  Utility functions.
-    static double calPhi(TRGCDCSegmentHit const * segmentHit,
+    static double calPhi(TRGCDCSegmentHit const* segmentHit,
                          double eventTime);
 
     /// Calculate Cos Sin ?
     static void calCosPhi(std::map<std::string, Belle2::TRGCDCJSignal>
                           & mSignalStorage,
-                          std::map<std::string, Belle2::TRGCDCJLUT * >
+                          std::map<std::string, Belle2::TRGCDCJLUT* >
                           & mLutStorage);
 
     /// Calculate Cos Sin ?
     static void calSinPhi(std::map<std::string, Belle2::TRGCDCJSignal>
                           & mSignalStorage,
-                          std::map<std::string, Belle2::TRGCDCJLUT * >
+                          std::map<std::string, Belle2::TRGCDCJLUT* >
                           & mLutStorage);
 
     /// Calculate r * phi ?
     static void rPhi(std::map<std::string, Belle2::TRGCDCJSignal>
                      & mSignalStorage,
-                     std::map<std::string, Belle2::TRGCDCJLUT * >
+                     std::map<std::string, Belle2::TRGCDCJLUT* >
                      & mLutStorage);
   private:
 
@@ -150,10 +150,10 @@ class TRGCDCHoughFinder {
     const std::string _name;
 
     /// CDCTRG.
-    const TRGCDC & _cdc;
+    const TRGCDC& _cdc;
 
     /// Hough planes, for + and - charges.
-    TRGCDCHoughPlaneMulti2 * _plane[2];
+    TRGCDCHoughPlaneMulti2* _plane[2];
 
     /// Circle Hough transformtion.
     TRGCDCHoughTransformationCircle _circleH;
@@ -166,7 +166,7 @@ class TRGCDCHoughFinder {
 
     /// Min. peak height for the peak finder.
     const unsigned _peakMin;
-    
+
     /// Map to hold double values for Fitter2D.
     std::map<std::string, double> m_mDouble;
 
@@ -192,16 +192,17 @@ class TRGCDCHoughFinder {
     std::map<std::string, TRGCDCJLUT*> m_mLutStorage;
 
     /// For VHDL code.
-    TRGCDCJSignalData * _commonData;
-};
+    TRGCDCJSignalData* _commonData;
+  };
 
 //-----------------------------------------------------------------------------
 
-inline
-std::string
-TRGCDCHoughFinder::name(void) const {
+  inline
+  std::string
+  TRGCDCHoughFinder::name(void) const
+  {
     return _name;
-}
+  }
 
 
 } // namespace Belle2

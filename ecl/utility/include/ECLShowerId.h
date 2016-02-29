513dd8d1374be6be346efae8ab4fd9fc9d3a5502
Torben Ferber <ferber@physics.ubc.ca>
2016-02-29 23:33:53 +0000

    added utility function to unpack the shower id, hypothesis and seed from the unique shower id
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@25900 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ ecl/utility/include/ECLShowerId.h
@@ -0,0 +1,73 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Torben Ferber                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef ECLSHOWERID_H
+#define ECLSHOWERID_H
+
+
+namespace Belle2 {
+
+  /**
+   * Class to convert the shower id into CR, CS and Seed and vice versa
+   */
+  class ECLShowerId {
+
+  public:
+
+    /**
+     * Constructor
+     */
+    ECLShowerId();
+
+    /**
+     * Returns connected region for a given shower ID
+     */
+    int getCRID(const int showerid) const
+    {
+      return showerid / m_CRMultiplier;
+    }
+
+    /**
+     * Returns hypothesis for a given shower ID
+     */
+    int getHypothesis(const int showerid) const
+    {
+      return (showerid % m_CRMultiplier) / m_HypothesisMultiplier;
+    }
+
+    /**
+     * Returns seed for a given shower ID
+     */
+    int getSeed(const int showerid) const
+    {
+      return ((showerid % m_CRMultiplier) % m_HypothesisMultiplier) / m_SeedMultiplier;
+    }
+
+    /**
+     * Returns shower identifier for given CRID, hypothesis and seed
+     */
+    int getShowerId(const int crid, const int hypothesis, const int seed) const
+    {
+      return m_CRMultiplier * crid + m_HypothesisMultiplier * hypothesis + m_SeedMultiplier * seed;
+    }
+
+
+  private:
+    const int m_CRMultiplier = 100000;
+    const int m_HypothesisMultiplier = 1000;
+    const int m_SeedMultiplier = 1;
+  };
+
+} // Belle2 namespace
+
+#endif
+
+
+
---
+++ ecl/utility/src/ECLShowerId.cc
@@ -0,0 +1,17 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Torben Ferber                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <ecl/utility/ECLShowerId.h>
+
+using namespace Belle2;
+
+ECLShowerId::ECLShowerId()
+{ }
+

3eda8b495b139a458673a48afd655e0a4bc848b9
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2016-03-01 15:04:54 +0000

    update fei_default_channels test
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@25927 b38c6c8f-bee8-465d-8448-9abe6915d496

--- analysis/tests/fei_default_channels.py
+++ analysis/tests/fei_default_channels.py
@@ -8,21 +8,19 @@ from basf2 import *
 from modularAnalysis import *
 
 # test wether we can actually get the default channels
-particles_charged = get_default_channels(neutralB=False)
-particles_neutral = get_default_channels(chargedB=False)
-particles_hadronic = get_default_channels(semileptonicB=False)
-particles_withextracut = get_default_channels(BlevelExtraCut='Mbc > 5.27')
+particles_hadronic = get_default_channels(semileptonic=False)
+particles_noklong = get_default_channels(KLong=False)
+particles_withextracut = get_default_channels(B_extra_cut='Mbc > 5.27')
 
 particles_all = get_default_channels()
+
+assert len(particles_all) > len(particles_hadronic)
+assert len(particles_all) > len(particles_noklong)
+assert len(particles_all) == len(particles_withextracut)
+
 path = create_path()
 path.add_module('RootInput')
 feistate = fullEventInterpretation(None, path, particles_all)
-
-assert len(particles_all) > len(particles_charged)
-assert len(particles_all) > len(particles_neutral)
-assert len(particles_all) > len(particles_hadronic)
-assert len(particles_all) == len(particles_withextracut)
-
 
 # show constructed path
 print(feistate.path)

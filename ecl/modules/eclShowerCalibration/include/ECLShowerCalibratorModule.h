a58956c264e030e64ce99b4d35f7e539462397f2
Torben Ferber <ferber@physics.ubc.ca>
2016-03-04 01:17:41 +0000

    refactoring part: shower calibration (skeleton module)
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@25993 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ ecl/modules/eclShowerCalibration/include/ECLShowerCalibratorModule.h
@@ -0,0 +1,71 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                             *
+ *                                                                        *
+ * This module performs the calibration for EM shower. This calibration   *
+ * corrects for differences between data and MC: corr = data / MC         *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Torben Ferber (ferber@physics.ubc.ca)                    *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#ifndef ECLSHOWERCALIBRATORMODULE_H_
+#define ECLSHOWERCALIBRATORMODULE_H_
+
+#include <framework/core/Module.h>
+
+namespace Belle2 {
+  namespace ECL {
+
+    /** Class to perform the shower correction */
+    class ECLShowerCalibratorModule : public Module {
+
+    public:
+      /** Constructor. */
+      ECLShowerCalibratorModule();
+
+      /** Destructor. */
+      ~ECLShowerCalibratorModule();
+
+      /** Initialize. */
+      virtual void initialize();
+
+      /** Begin run. */
+      virtual void beginRun();
+
+      /** Event. */
+      virtual void event();
+
+      /** End run. */
+      virtual void endRun();
+
+      /** Terminate. */
+      virtual void terminate();
+
+    private:
+
+    public:
+      /** We need names for the data objects to differentiate between PureCsI and default*/
+      virtual const char* eclShowerArrayName() const
+      { return "ECLShowers" ; }
+
+      virtual const char* eclConnectedRegionArrayName() const
+      { return "ECLConnectedRegions" ; }
+    }; // end of ECLCRFinderAndSplitterModule
+
+
+    /** The very same module but for PureCsI */
+    class ECLShowerCalibratorPureCsIModule : public ECLShowerCalibratorModule {
+    public:
+      virtual const char* eclShowerArrayName() const override
+      { return "ECLShowersPureCsI" ; }
+
+      virtual const char* eclConnectedRegionArrayName() const override
+      { return "ECLConnectedRegionsPureCsI" ; }
+    }; // end of ECLCRFinderAndSplitterPureCsIModule
+  } // end of ECL namespace
+} // end of Belle2 namespace
+
+#endif
---
+++ ecl/modules/eclShowerCalibration/src/ECLShowerCalibratorModule.cc
@@ -0,0 +1,90 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                             *
+ *                                                                        *
+ * This module performs the calibration for EM shower. This calibration   *
+ * corrects for differences between data and MC: corr = data / MC         *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Torben Ferber (ferber@physics.ubc.ca)                    *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+// THIS MODULE
+#include <ecl/modules/eclShowerCalibration/ECLShowerCalibratorModule.h>
+
+// FRAMEWORK
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/RelationArray.h>
+#include <framework/gearbox/Unit.h>
+#include <framework/logging/Logger.h>
+
+// ECL
+#include <ecl/dataobjects/ECLConnectedRegion.h>
+#include <ecl/dataobjects/ECLShower.h>
+
+using namespace Belle2;
+using namespace ECL;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(ECLShowerCalibrator)
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+ECLShowerCalibratorModule::ECLShowerCalibratorModule() : Module()
+{
+  // Set description
+  setDescription("ECLShowerCalibratorModule: Corrects for data to MC differences");
+  setPropertyFlags(c_ParallelProcessingCertified);
+
+}
+
+ECLShowerCalibratorModule::~ECLShowerCalibratorModule()
+{
+}
+
+
+void ECLShowerCalibratorModule::initialize()
+{
+  // Register in datastore
+  StoreArray<ECLShower> eclShowers(eclShowerArrayName());
+  StoreArray<ECLConnectedRegion> eclCRs(eclConnectedRegionArrayName());
+
+  eclShowers.registerInDataStore();
+  eclCRs.registerInDataStore();
+}
+
+void ECLShowerCalibratorModule::beginRun()
+{
+  // Do not use this for Database updates
+  ;
+}
+
+void ECLShowerCalibratorModule::event()
+{
+  // input array
+  StoreArray<ECLShower> eclShowers(eclShowerArrayName());
+
+  // loop over all ECLShowers
+  for (const auto& eclShower : eclShowers) {
+    // check hypothesis, do other things...
+
+  }
+
+}
+
+void ECLShowerCalibratorModule::endRun()
+{
+  ;
+}
+
+void ECLShowerCalibratorModule::terminate()
+{
+  ;
+}

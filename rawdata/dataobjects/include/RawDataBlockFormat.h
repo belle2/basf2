844ad7038ca0c452edf6043ba79fb819a7b757ed
Satoru Yamada <satoru.yamada@kek.jp>
2016-03-05 05:41:44 +0000

    Add RawDataBlockFormat
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@26021 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ rawdata/dataobjects/include/RawDataBlockFormat.h
@@ -0,0 +1,105 @@
+//+
+// File : RawDataBlockFormat.h
+// Description : Module to handle raw data from COPPER
+//
+// Author : Satoru Yamada, IPNS, KEK
+// Date : 2 - Aug - 2013
+//-
+
+#ifndef RAWDATABLOCKFORMAT_H
+#define RAWDATABLOCKFORMAT_H
+
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+
+namespace Belle2 {
+
+  /**
+   * The RawDataBlockFormat class
+   * Format information for rawdata handling
+   */
+  class RawDataBlockFormat  {
+  public:
+    //! Default constructor
+    RawDataBlockFormat();
+
+    //! Destructor
+    virtual ~RawDataBlockFormat() {};
+
+    //! set buffer ( delete_flag : m_buffer is freeed( = 0 )/ not freeed( = 1 ) in Destructer )
+    virtual void SetBuffer(int* bufin, int nwords, int delete_flag, int num_events, int num_nodes);
+
+    //! Get total length of m_buffer
+    virtual int TotalBufNwords();
+
+    //! get position of data block in word
+    virtual int GetBufferPos(int n);
+
+    //! get nth buffer pointer
+    virtual int* GetBuffer(int n);
+
+    //! get pointer to  buffer(m_buffer)
+    virtual int* GetWholeBuffer();
+
+    //! get # of data blocks = (# of nodes)*(# of events)
+    virtual int GetNumEntries() { return m_num_events * m_num_nodes; }
+
+    //! get # of data sources(e.g. # of COPPER boards) in m_buffer
+    virtual int GetNumNodes() { return m_num_nodes; }
+
+    //! get # of events in m_buffer
+    virtual int GetNumEvents() { return m_num_events; }
+
+    /*     //! get malloc_flag */
+    /*     virtual int GetPreAllocFlag() { return m_use_prealloc_buf; } */
+
+    //! get size of a data block
+    virtual int GetBlockNwords(int n);
+
+    //! get FTSW ID to check whether this data block is FTSW data or not
+    virtual int CheckFTSWID(int n);
+
+    //! get FTSW ID to check whether this data block is FTSW data or not
+    virtual int CheckTLUID(int n);
+
+    //! Copy one datablock to buffer
+    virtual void CopyBlock(int n, int* buf_to);
+
+    //! print data
+    virtual void PrintData(int* buf, int nwords);
+
+    enum {
+      POS_NWORDS = 0,
+      POS_NODE_ID = 6
+    };
+
+    enum {
+      // Tentatively needed to distinguish new and old FTSW format, which will be changed in Nov. 2013
+      POS_FTSW_ID_OLD = 5,
+      TEMP_POS_NWORDS_HEADER = 1,
+      OLD_FTSW_NWORDS_HEADER = 6
+    };
+
+  protected :
+    /// number of words of buffer
+    int m_nwords;
+
+    /// number of nodes in this object
+    int m_num_nodes;
+
+    /// number of events in this object
+    int m_num_events;
+
+    /// Buffer
+    int* m_buffer; //! not recorded
+
+    /*     /// To drive from TObject */
+    /*     ClassDef(RawDataBlockFormat, 1); */
+  };
+
+}
+
+#endif
---
+++ rawdata/dataobjects/src/RawDataBlockFormat.cc
@@ -0,0 +1,163 @@
+//+
+// File : RawDataBlockFormat.cc
+// Description : Module to handle raw data from COPPER.
+//
+// Author : Satoru Yamada, IPNS, KEK
+// Date : 2 - Aug - 2013
+//-
+
+#include <rawdata/dataobjects/RawDataBlockFormat.h>
+
+using namespace std;
+using namespace Belle2;
+
+
+
+RawDataBlockFormat::RawDataBlockFormat()
+{
+  m_nwords = 0;
+  //  m_use_prealloc_buf = 0;
+  m_buffer = NULL;
+  m_num_nodes = 0;
+  m_num_events = 0;
+}
+
+int RawDataBlockFormat::GetBufferPos(int n)
+{
+  if (m_buffer == NULL || m_nwords <= 0) {
+    char err_buf[500];
+    sprintf(err_buf, "[FATAL] [ERROR] RawPacket buffer(%p) is not available or length(%d) is not set.\n %s %s %d\n",
+            m_buffer, m_nwords, __FILE__, __PRETTY_FUNCTION__, __LINE__);
+    string err_str = err_buf;     throw (err_str);
+  }
+
+  if (n >= (m_num_events * m_num_nodes)) {
+    char err_buf[500];
+    sprintf(err_buf, "[FATAL] Invalid COPPER block No. (%d : max %d ) is specified. Exiting... \n %s %s %d\n",
+            n, (m_num_events * m_num_nodes), __FILE__, __PRETTY_FUNCTION__, __LINE__);
+    string err_str = err_buf;     throw (err_str);
+  }
+
+  int pos_nwords = 0;
+  for (int i = 1; i <= n ; i++) {
+    if (m_buffer[ pos_nwords ] <= 0) {
+      char err_buf[500];
+      sprintf(err_buf,
+              "[FATAL] CORRUPTED DATA: length of this data block is strange ( %d words ). Maybe data is corrupted or RawHeader info has not been filled yet. Exiting...",
+              m_buffer[ pos_nwords ]);
+      string err_str = err_buf;     throw (err_str);
+    } else {
+      pos_nwords +=      m_buffer[ pos_nwords ];
+    }
+    if (pos_nwords >= m_nwords) {
+      char err_buf[500];
+      sprintf(err_buf, "CORRUPTED DATA: value of pos_nwords(%d) is larger than m_nwords(%d). Exiting...\n %s %s %d\n",
+              pos_nwords, m_nwords, __FILE__, __PRETTY_FUNCTION__, __LINE__);
+      string err_str = err_buf;     throw (err_str);
+      //      exit(1);
+    }
+  }
+  return pos_nwords;
+
+}
+
+
+int RawDataBlockFormat::CheckFTSWID(int n)
+{
+  int pos = POS_NODE_ID;
+  if (m_buffer[ GetBufferPos(n) + TEMP_POS_NWORDS_HEADER ] == OLD_FTSW_NWORDS_HEADER) {
+    pos =  POS_FTSW_ID_OLD;
+  }
+
+  if (m_buffer[ GetBufferPos(n) + pos ] == 0x54544420) { // "TTD "
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+
+int RawDataBlockFormat::CheckTLUID(int n)
+{
+  int pos = POS_NODE_ID;
+  if (m_buffer[ GetBufferPos(n) + TEMP_POS_NWORDS_HEADER ] == OLD_FTSW_NWORDS_HEADER) {
+    pos =  POS_FTSW_ID_OLD;
+  }
+  if (m_buffer[ GetBufferPos(n) + pos ] == 0x544c5520) { // "TLU "
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int RawDataBlockFormat::TotalBufNwords()
+{
+  return m_nwords;
+}
+
+
+int RawDataBlockFormat::GetBlockNwords(int n)
+{
+  int size;
+  if (n == (m_num_events * m_num_nodes) - 1) {
+    size =  m_nwords - GetBufferPos(n);
+  } else {
+    size = GetBufferPos(n + 1) - GetBufferPos(n);
+  }
+  return size;
+}
+
+
+int* RawDataBlockFormat::GetWholeBuffer()
+{
+  return m_buffer;
+}
+
+int* RawDataBlockFormat::GetBuffer(int n)
+{
+  int pos_nwords = GetBufferPos(n);
+  return &(m_buffer[ pos_nwords ]);
+}
+
+
+void RawDataBlockFormat::SetBuffer(int* bufin, int nwords, int delete_flag, int num_events, int num_nodes)
+{
+  if (delete_flag  == 1) {
+    printf("RawCOPPER format class does not delete m_buffer. Please specify 0 for delete_flag. Exiting...\n");
+    exit(1);
+  }
+
+  if (bufin == NULL) {
+    printf("[DEBUG] bufin is NULL. Exting...\n");
+    exit(1);
+  }
+
+  m_nwords = nwords;
+  m_buffer = bufin;
+
+  m_num_nodes = num_nodes;
+  m_num_events = num_events;
+
+  return;
+
+}
+
+
+
+void RawDataBlockFormat::PrintData(int* buf, int nwords)
+{
+  printf("[DEBUG] ");
+  for (int i = 0; i < nwords; i++) {
+    printf("%.8x ", buf[ i ]);
+    if (i % 10 == 9) printf("\n[DEBUG] ");
+  }
+  printf("\n[DEBUG] ");
+  printf("\n");
+  return;
+}
+
+void RawDataBlockFormat::CopyBlock(int n, int* buf_to)
+{
+  memcpy(buf_to, GetBuffer(n), GetBlockNwords(n) * sizeof(int));
+  return;
+}

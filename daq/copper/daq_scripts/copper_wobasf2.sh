08594e27b9b2249a093d20ab1b577aaed24e1cbf
Satoru Yamada <satoru.yamada@kek.jp>
2016-03-10 06:08:07 +0000

    Add new scripts
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@26177 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/copper/daq_scripts/copper_wobasf2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# setup basf2 environment (See "Setup of Software Tools" at https://belle2.cc.kek.jp/~twiki/bin/view/Software/SoftwareInstallation
+#
+source  ~/.bash_profile
+#
+# Read data form COPPER FIFO
+#
+#${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/RecvSendCOPPER.sh $1 $2 $3 $4 $5 # Read from COPPER FIFO
+
+#
+# Use dummy data instead of data from COPPER FIFO
+#
+#${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/RecvSendCOPPERDummy.sh $1 $2  $3 $4 $5 # Dummy data produced by COPPER CPU
+#${BELLE2_LOCAL_DIR}/daq/copper/daq_scripts/RecvSendCOPPER.sh $1 $2  $3 $4 $5 #
+${BELLE2_LOCAL_DIR}/daq/ropc/des_ser_COPPER_main $1 $2  $3 $4 $5 #
+#${BELLE2_LOCAL_DIR}/daq/rawdata/src/dummy_data_src $2 

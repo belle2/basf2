90c420076df82a34ce29d058271e6539a8a46a18
Torben Ferber <ferber@physics.ubc.ca>
2016-03-10 20:59:46 +0000

    Geometry routine to return a list of neighbour IDs for a given crystal.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@26230 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ ecl/geometry/include/ECLNeighbours.h
@@ -0,0 +1,79 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                             *
+ *                                                                        *
+ * Returns the list of neighbours for a given crystal ID.                 *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Torben Ferber (ferber@physics.ubc.ca)                    *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#ifndef ECLNEIGHBOURS_H
+#define ECLNEIGHBOURS_H
+
+#include <vector>
+#include <string>
+
+namespace Belle2 {
+  namespace ECL {
+
+    /**
+     * Class to get the neighbours for a given crystal id.
+     */
+    class ECLNeighbours {
+
+    public:
+
+      /**  Constructor: Default. */
+//      ECLNeighbours();
+
+      /**  Constructor: Fix number of neighbours ("N") in the seed theta ring or radius ("R") with par = n or par = radius [cm]. */
+      ECLNeighbours(const std::string neighbourDef, const float par);
+
+      /**  Destructor. */
+      ~ECLNeighbours();
+
+      /** Return the neighbours for a given crystal ID.*/
+      std::vector<short  int> getNeighbours(short int cid);
+
+    private:
+      bool m_isInitialized; // true if the neighbourmap is set up
+      std::vector < std::vector < short int > > m_neighbourMap; // list of list of neighbour cids
+      std::vector < std::vector < short int > > m_neighbourMapTemp; // temp list of list of neighbour cids
+
+      const short m_crystalsPerRing[69] = {
+        48, 48, 64, 64, 64, 96, 96, 96, 96, 96, 96, 144, 144, //FWD up to 13
+        144, 144, 144, 144, 144, 144, 144,  // BARREL up to 20
+        144, 144, 144, 144, 144, 144, 144, 144, 144, 144, // ...30
+        144, 144, 144, 144, 144, 144, 144, 144, 144, 144, // ...40
+        144, 144, 144, 144, 144, 144, 144, 144, 144, 144, // ...50
+        144, 144, 144, 144, 144, 144, 144, 144, 144, // BARREL up to 59
+        144, 144, 96, 96, 96, 96, 96, 64, 64, 64
+      }; //BWD
+
+
+      /**  initialize the mask neighbour list. */
+      void initializeN(const int nneighbours);
+
+      /**  initialize the radius neighbout list. */
+      void initializeR(const float radius);
+
+      /**  return the previous phi id. */
+      short int decreasePhiId(const short int phiid, const short int thetaid, const short int n);
+
+      /**  return the next phi id. */
+      short int increasePhiId(const short int phiid, const short int thetaid, const short int n);
+
+      /** return a list of phi ids between two phi ids */
+      std::vector<short int> getPhiIdsInBetween(const short int phiInc, const short int phiDec, const short int theta);
+
+      /** return the chord length between crystals */
+      float getDistance(float alpha, float R);
+
+    };
+
+  } // end of namespace ECL
+} // end of namespace Belle2
+
+#endif
---
+++ ecl/geometry/src/ECLNeighbours.cc
@@ -0,0 +1,197 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                             *
+ *                                                                        *
+ * Returns the list of neighbours for a given crystal ID.                 *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Torben Ferber (ferber@physics.ubc.ca)                    *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+// ECL GEOMETRY
+#include <ecl/geometry/ECLNeighbours.h>
+#include <ecl/geometry/ECLGeometryPar.h> // for geometry
+
+// FRAMEWORK
+#include <framework/logging/Logger.h>
+#include <framework/gearbox/Unit.h>
+
+// OTHER
+#include "TMath.h" // for Nint
+#include "TVector3.h"
+
+using namespace Belle2;
+using namespace ECL;
+
+// Constructor.
+ECLNeighbours::ECLNeighbours(const std::string neighbourDef, const float par)
+{
+  // resize the vector
+  std::vector<short int> fakeneighbours;
+  fakeneighbours.push_back(0); // insert one fake to avoid the "0" entry
+  m_neighbourMap.push_back(fakeneighbours);
+
+  int parToInt = TMath::Nint(par);
+
+  // fixed number of neighbours:
+  if (neighbourDef == "N") {
+    B2INFO("ECLNeighbours::ECLNeighbours: initialize " << neighbourDef << ", n x n: " << parToInt << " x " << parToInt);
+    if ((parToInt >= 0) && (parToInt < 6)) initializeN(parToInt);
+    else B2FATAL("ECLNeighbours::ECLNeighbours: " << parToInt << " is an invalid parameter (must be between 0 and 5)!");
+  }
+  // or neighbours depend on the distance:
+  else if (neighbourDef == "R") {
+    B2INFO("ECLNeighbours::ECLNeighbours: initialize " << neighbourDef << ", radius: " << par << " cm");
+    if ((par > 0.0) && (par < 30.0 * Belle2::Unit::cm)) initializeR(par);
+    else B2FATAL("ECLNeighbours::ECLNeighbours: " << par << " is an invalid parameter (must be between 0 cm and 30 cm)!");
+  }
+  // or wrong type:
+  else {
+    B2FATAL("ECLNeighbours::ECLNeighbours (constructor via std::string): Invalid option: " << neighbourDef <<
+            " (valid: N1, N2, N3, N4, N5 or R");
+  }
+
+}
+
+ECLNeighbours::~ECLNeighbours()
+{
+  ;
+}
+
+void ECLNeighbours::initializeR(float radius)
+{
+  // resize the vector
+  std::vector<short int> fakeneighbours;
+  fakeneighbours.push_back(0); // insert one fake to avoid the "0" entry
+  m_neighbourMapTemp.push_back(fakeneighbours);
+
+  // distance calculations take a long time, so reduce the number of candidates first:
+  initializeN(6);
+
+  // ECL geometry
+  ECLGeometryPar* geom = ECLGeometryPar::Instance();
+
+  for (int i = 0; i < 8736; i++) {
+    // get the central one
+    TVector3 direction = geom->GetCrystalVec(i);
+    TVector3 position  = geom->GetCrystalPos(i);
+
+    // get all nearby crystals
+    std::vector<short int> neighbours = getNeighbours(i + 1);
+    std::vector<short int> neighboursTemp;
+
+    // ... and calculate the shortest distance between the central one and all possible neighbours (of the reduced set)
+    for (auto const& id : neighbours) {
+      TVector3 directionNeighbour = geom->GetCrystalVec(id - 1);
+      const float alpha = direction.Angle(directionNeighbour);
+      const float R = position.Mag();
+      const float distance = getDistance(alpha, R);
+
+//      B2INFO(i << " " << id << " " << distance << " (radius = " << radius << ")");
+
+      if (distance <= radius) neighboursTemp.push_back(id);
+    }
+
+    m_neighbourMapTemp.push_back(neighboursTemp);
+  }
+
+  // all done, reoplace the original map
+  m_neighbourMap = m_neighbourMapTemp;
+
+}
+
+void ECLNeighbours::initializeN(int n)
+{
+  // ECL geometry
+  ECLGeometryPar* geom = ECLGeometryPar::Instance();
+
+  // This is the "NxN-edges" case (in the barrel)
+  // in the endcaps we project the neighbours to the outer and inner rings.
+  for (int i = 0; i < 8736; i++) {
+
+//    B2INFO(i);
+
+    // this vector will hold all neighbours for the i-th crystal
+    std::vector<short int> neighbours;
+
+    // phi and theta coordinates of the central crystal
+    geom->Mapping(i);
+    //const short cid = i;
+    const short tid = geom->GetThetaID();
+    const short pid = geom->GetPhiID();
+
+    // 'left' and 'right' extremal neighbours in the same theta ring
+    const short int phiInc = increasePhiId(pid, tid, n);
+    const short int phiDec = decreasePhiId(pid, tid, n);
+    const float fractionalPhiInc = static_cast < float >(phiInc) / m_crystalsPerRing [ tid ];
+    const float fractionalPhiDec = static_cast < float >(phiDec) / m_crystalsPerRing [ tid ];
+//    B2INFO("   phiInc = " << phiInc << " phiDec = " << phiDec);
+//    B2INFO("   fractionalPhiInc = " << fractionalPhiInc << " fractionalPhiDec = " << fractionalPhiDec);
+
+    // loop over all possible theta rings and add neighbours
+    for (int theta = tid - n; theta <= tid + n; theta++) {
+      if ((theta > 68) || (theta < 0)) continue;     // valid theta ids are 0..68 (69 in total)
+//      B2INFO("   " << theta);
+
+      short int thisPhiInc = TMath::Nint(fractionalPhiInc * m_crystalsPerRing [ theta ]);
+
+      short int thisPhiDec = TMath::Nint(fractionalPhiDec * m_crystalsPerRing [ theta ]);
+      if (thisPhiDec == m_crystalsPerRing [ theta ]) thisPhiDec = 0;
+
+//      B2INFO("          thisPhiInc = " << thisPhiInc << " thisPhiDec = " << thisPhiDec);
+
+      const std::vector<short int> phiList = getPhiIdsInBetween(thisPhiInc, thisPhiDec, theta);
+
+      for (unsigned int k = 0; k < phiList.size(); ++k) {
+        neighbours.push_back(geom->GetCellID(theta , phiList.at(k)) + 1);
+      }
+
+    }
+
+    // push back the final vector of IDs
+    m_neighbourMap.push_back(neighbours);
+
+  }
+}
+
+std::vector<short int> ECLNeighbours::getNeighbours(const short int cid)
+{
+  return m_neighbourMap.at(cid);
+}
+
+// decrease the phi id by "n" integers numbers (valid ids range from 0 to m_crystalsPerRing[thetaid] - 1)
+short int ECLNeighbours::decreasePhiId(const short int phiid, const short int thetaid, const short int n)
+{
+  short int previousPhiId = ((phiid - n < 0) ? m_crystalsPerRing[thetaid] + phiid - n : phiid - n);     // "underflow"
+  return previousPhiId;
+}
+
+// increase the phi id by "n" integers numbers (valid ids range from 0 to m_crystalsPerRing[thetaid] - 1)
+short int ECLNeighbours::increasePhiId(const short int phiid, const short int thetaid, const short int n)
+{
+  short int nextPhiId = (((phiid + n) > (m_crystalsPerRing[thetaid] - 1)) ? phiid + n - m_crystalsPerRing[thetaid] : phiid +
+                         n);       // "overflow"
+  return nextPhiId;
+}
+
+std::vector<short int> ECLNeighbours::getPhiIdsInBetween(const short int phi0, const short int phi1, const short int theta)
+{
+  std::vector<short int> phiList;
+  short int loop = phi0;
+
+  while (1) {
+    phiList.push_back(loop);
+    if (loop == phi1) break;
+    loop = decreasePhiId(loop, theta, 1);
+  }
+
+  return phiList;
+}
+
+float ECLNeighbours::getDistance(float alpha, float R)
+{
+  return 2.0 * R * TMath::Sin(alpha / 2.0);
+}
+

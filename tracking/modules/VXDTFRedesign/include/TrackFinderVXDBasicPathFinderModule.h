4f330ca72b5209c88f3788212c36678ff907a4d5
Jakob Lettenbichler <jkl@jodoschka.com>
2016-03-11 07:43:34 +0000

    adding two more modules for the redesigned version of the VXDTF, one allows to remove a virtual IP from the SPTCs and the other is a simplified trackFinder as a possible bypass in some situations (especially for debugging purposes).
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@26237 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/modules/VXDTFRedesign/include/TrackFinderVXDBasicPathFinderModule.h
@@ -0,0 +1,158 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Jakob Lettenbichler                                      *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+// fw
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/core/Module.h>
+
+// tracking
+#include <tracking/spacePointCreation/SpacePointTrackCand.h>
+
+#include <tracking/dataobjects/FullSecID.h>
+#include <tracking/dataobjects/SectorMapConfig.h>
+
+#include <tracking/trackFindingVXD/algorithms/CellularAutomaton.h>
+#include <tracking/trackFindingVXD/algorithms/PathCollectorRecursive.h>
+#include <tracking/trackFindingVXD/algorithms/CALogger.h>
+#include <tracking/trackFindingVXD/algorithms/CAValidator.h>
+#include <tracking/trackFindingVXD/algorithms/NodeCompatibilityCheckerBase.h>
+
+#include <tracking/trackFindingVXD/segmentNetwork/CACell.h>
+#include <tracking/trackFindingVXD/segmentNetwork/DirectedNodeNetworkContainer.h>
+
+#include <tracking/trackFindingVXD/sectorMapTools/SectorMap.h>
+
+#include <tracking/trackFindingVXD/tcTools/SpacePointTrackCandCreator.h>
+
+
+namespace Belle2 {
+  /** The TrackFinderVXDBasicPathFinder is a low momentum Si-only trackfinder.
+   *
+   * It uses the output produced by the SegmentNetworkProducerModule to create SpacePointTrackCands by simply storing all possible paths stored in the SegmentNetwork.
+   */
+  class TrackFinderVXDBasicPathFinderModule : public Module {
+
+  public:
+
+
+    /** Constructor */
+    TrackFinderVXDBasicPathFinderModule();
+
+
+    /** Destructor */
+    virtual ~TrackFinderVXDBasicPathFinderModule() {}
+
+
+    /** initialize */
+    virtual void initialize();
+
+
+    /** beginRun */
+    virtual void beginRun() {}
+
+
+    /** event */
+    virtual void event();
+
+
+    /** endRun */
+    virtual void endRun() {}
+
+
+    /** terminate */
+    virtual void terminate() {}
+
+
+
+    /** *************************************+************************************* **/
+    /** ***********************************+ + +*********************************** **/
+    /** *******************************+ functions +******************************* **/
+    /** ***********************************+ + +*********************************** **/
+    /** *************************************+************************************* **/
+
+
+
+  protected:
+
+/// module parameters
+
+
+    /** name for StoreArray< SpacePointTrackCand> to be filled */
+    std::string m_PARAMSpacePointTrackCandArrayName;
+
+
+    /** name for StoreObjPtr< DirectedNodeNetwork> which contains the networks needed */
+    std::string m_PARAMNetworkName;
+
+
+    /** the name of the SectorMap used for this instance. */
+    std::string m_PARAMsecMapName;
+
+
+    /** If true for each event and each network created a file with a graph is created. */
+    bool m_PARAMprintNetworks;
+
+
+    /** Regulates if every subset of sufficient length of a path shall be collected as separate path or not.
+     *
+     * if true, only one path per possibility is collected, if false subsets are collected too. */
+    bool m_PARAMstrictSeeding;
+
+    /** If true, the virtual interaction Point will be removed from the track candidates. */
+    bool m_PARAMremoveVirtualIP = false;
+
+/// member variables
+
+    /** CA algorithm */
+    CellularAutomaton<Belle2::DirectedNodeNetwork< Belle2::Segment<Belle2::TrackNode>, Belle2::CACell >, Belle2::CAValidator<Belle2::CACell>, Belle2::CALogger>
+    m_cellularAutomaton;
+
+    /** algorithm for finding paths of segments */
+    PathCollectorRecursive <
+    Belle2::DirectedNodeNetwork< Belle2::Segment<Belle2::TrackNode>, Belle2::CACell >,
+           Belle2::DirectedNode<Belle2::Segment<Belle2::TrackNode>, Belle2::CACell>,
+           std::vector<Belle2::DirectedNode<Belle2::Segment<Belle2::TrackNode>, Belle2::CACell>*>,
+           Belle2::NodeCompatibilityCheckerBase<Belle2::DirectedNode<Belle2::Segment<Belle2::TrackNode>, Belle2::CACell>>
+           > m_pathCollector;
+
+    /** tool for creating SPTCs, fills storeArray directly */
+    SpacePointTrackCandCreator<StoreArray<Belle2::SpacePointTrackCand>> m_sptcCreator;
+
+
+    /// input containers
+
+    /** access to the DirectedNodeNetwork, which contains the network needed for creating TrackCandidates */
+    StoreObjPtr<Belle2::DirectedNodeNetworkContainer> m_network;
+
+
+    /** contains the sectorMap (only needed for loading the configuration). */
+    StoreObjPtr< SectorMap<SpacePoint> > m_sectorMap = StoreObjPtr< SectorMap<SpacePoint> >("", DataStore::c_Persistent);
+
+
+    /** contains the configuration-settings for this run. */
+    SectorMapConfig m_config;
+
+
+    /// output containers
+
+    /** StoreArray for the TCs created in this module */
+    StoreArray<Belle2::SpacePointTrackCand> m_TCs;
+
+
+    /// counters and other debug stuff:
+    /** counts event numbers */
+    unsigned int m_eventCounter = 0;
+
+  private:
+  };
+} // Belle2 namespace
---
+++ tracking/modules/spacePointCreator/include/SPTCvirtualIPRemoverModule.h
@@ -0,0 +1,108 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2011 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Jakob Lettenbichler                                      *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+#include <tracking/spacePointCreation/SpacePointTrackCand.h>
+#include <framework/datastore/StoreArray.h>
+
+#include <framework/core/Module.h>
+#include <string>
+#include <vector>
+
+
+namespace Belle2 {
+
+  /** A module for checking and removing the virtual IP if wanted. */
+  class SPTCvirtualIPRemoverModule : public Module {
+
+  public:
+
+
+    /**
+     * Constructor of the module.
+     */
+    SPTCvirtualIPRemoverModule();
+
+
+    /** Initializes the Module.
+     */
+    virtual void initialize()
+    {
+      InitializeCounters();
+      m_spacePointTrackCands.isRequired(m_PARAMtcArrayName);
+    }
+
+
+    /**
+     * Prints a header for each new run.
+     */
+    virtual void beginRun()
+    {
+      InitializeCounters();
+    }
+
+
+    /** Applies the circleFit at given sets of TCs. */
+    virtual void event();
+
+
+    /** Prints a footer for each run which ended. */
+    virtual void endRun();
+
+
+    /** initialize variables to avoid nondeterministic behavior */
+    void InitializeCounters()
+    {
+      m_eventCounter = 0;
+      m_nTCsTotal = 0;
+      m_nVIPsTotal = 0;
+      m_nVIPsRemoved = 0;
+    }
+
+
+  protected:
+
+    // module parameters
+
+
+    /** sets the name of expected StoreArray with SpacePointTrackCand in it. */
+    std::string m_PARAMtcArrayName;
+
+    /** if true, no vIP is removed, but only nVIPs are counted. */
+    bool m_PARAMdoCheckOnly;
+
+    /** If you want to keep the vIP only for short TCs, then set this value to the number of hits a TC is maximally allowed to have to not loose its vIP.
+     *
+     * (number of hits without counting the vIP). */
+    unsigned int m_PARAMmaxTCLengthForVIPKeeping;
+
+
+    // member variables
+
+
+    /** knows current event number. */
+    unsigned int m_eventCounter;
+
+    /** total number of TCs found in SpacePointTrackCand-container. */
+    unsigned int m_nTCsTotal;
+
+    /** total number virtualIPs found. */
+    unsigned int m_nVIPsTotal;
+
+    /** total number of TCs removed from TCs. */
+    unsigned int m_nVIPsRemoved;
+
+    /** the storeArray for SpacePointTrackCands as member, is faster than recreating link for each event. */
+    StoreArray<SpacePointTrackCand> m_spacePointTrackCands;
+
+  private:
+  };
+}

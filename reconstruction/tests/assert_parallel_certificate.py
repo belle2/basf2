db23c767b65a94055c8591a916aa36ec0c855d13
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2016-03-17 15:31:12 +0000

    Added a test for the parallel certified in reconstruction. [by Nils]
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@26492 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ reconstruction/tests/assert_parallel_certificate.py
@@ -0,0 +1,21 @@
+# Test for the parallel certified flag in all modules in the standard reconstruction.
+# It creates a path and fills it with all reconstruction modules, without anything else
+# (this path can never be executed, but we do not want that anyway).
+# Then it goes through all modules and checks it flag.
+#
+#
+# Author: Nils Braun
+
+import basf2
+import reconstruction
+
+# Create an empty path
+path = basf2.create_path()
+
+# Add only the reconstruction
+reconstruction.add_reconstruction(path)
+
+# Assert that all modules have a parallel processing certified flag.
+modules = path.modules()
+for m in modules:
+    assert m.has_properties(basf2.ModulePropFlags.PARALLELPROCESSINGCERTIFIED)

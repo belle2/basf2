1e8a77e3a3364dd3b35224c306ba42877d4a8f00
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2016-03-23 18:55:21 +0000

    typo
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@26695 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/modules/logging/src/ToFileLogger.cc
+++ framework/modules/logging/src/ToFileLogger.cc
@@ -19,8 +19,8 @@ ToFileLoggerModule::ToFileLoggerModule():
   m_fileDeviceOfStdout(0),
   m_fileDeviceOfStderr(0)
 {
-  setDescription("For better handling of non BASF2-Logging (e.g. cout or printf()), you can insert this module."
-                 "It redirects all output to the console into the filename given as parameter."
+  setDescription("For better handling of non BASF2-Logging (e.g. cout or printf()), you can insert this module. "
+                 "It redirects all output to the console into the filename given as parameter. "
                  "Using the basf2-own logging framework (logging.add_file()) you can even write the basf2-related logging"
                  "into a separate file.");
 

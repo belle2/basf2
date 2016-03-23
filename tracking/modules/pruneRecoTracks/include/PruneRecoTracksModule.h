8968f0c0569ab4395f7f49dd82846d7b049522d3
Thomas Hauth <thomas.hauth@kit.edu>
2016-03-23 09:28:46 +0000

    added module to prune RecoTracks before storing to disk
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@26668 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/modules/pruneRecoTracks/include/PruneRecoTracksModule.h
@@ -0,0 +1,43 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2012 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Thomas Hauth                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+#include <framework/core/Module.h>
+
+/**
+ * This module calls RecoTrack::prune to remove hits before
+ * the track gets stored to disk to save storage space.
+ * By default, this module will remove all but the first and last hits of
+ * the track.
+ */
+
+namespace Belle2 {
+
+  /// Depricated alias for TrackFinderCDCAutomatonModule
+  class PruneRecoTracksModule : public Module {
+
+  public:
+    /// Constructor of the module. Setting up parameters and description.
+    PruneRecoTracksModule();
+
+    /// Event processing, prunes the RecoTracks contained in each event
+    virtual void event() override;
+
+  private:
+
+    // flags used for pruning, See genfit::Track class for possible settings
+    std::string m_pruneFlags;
+
+    // name of the StoreArray to prune
+    std::string m_storeArrayName;
+
+  }; // end class
+} // end namespace Belle2
+
---
+++ tracking/modules/pruneRecoTracks/src/PruneRecoTracksModule.cc
@@ -0,0 +1,42 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2012 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Thomas Hauth                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <tracking/modules/pruneRecoTracks/PruneRecoTracksModule.h>
+#include <framework/datastore/StoreArray.h>
+#include <tracking/dataobjects/RecoTrack.h>
+
+using namespace Belle2;
+
+REG_MODULE(PruneRecoTracks);
+
+PruneRecoTracksModule::PruneRecoTracksModule() :
+  Module()
+{
+  setDescription("Prunes RecoTracks");
+  setPropertyFlags(c_ParallelProcessingCertified);
+
+  addParam("pruneFlags", m_pruneFlags,
+           "Pruning options passed on to genfit. See genfit::Track class for a detailed description",
+           std::string("FL"));
+
+  addParam("storeArrayName", m_storeArrayName,
+           "Name of the StoreArray which is pruned",
+           std::string("RecoTracks"));
+
+}
+
+void PruneRecoTracksModule::event()
+{
+  auto tracks = Belle2::StoreArray<RecoTrack>(m_storeArrayName);
+  for (auto& t : tracks) {
+    t.prune(m_pruneFlags.c_str());
+  }
+}
+

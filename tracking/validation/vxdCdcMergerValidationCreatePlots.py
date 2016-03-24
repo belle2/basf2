34e9805ecd3609550cc523992a2ec6f1a758b83c
Thomas Hauth <thomas.hauth@kit.edu>
2016-03-24 17:25:20 +0000

    added new vxdCdc merger validation
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@26741 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/validation/vxdCdcMergerValidationCreatePlots.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+<header>
+  <contact>tracking@belle2.kek.jp</contact>
+  <input>VxdCdcValidationHarvested.root</input>
+  <description>This module creates efficiency plots for the V0 validation.</description>
+</header>
+"""
+
+
+import numpy
+import ROOT
+
+
+class VxdCdcMergerValidationPlots:
+
+    def __init__(self, input_file='../vxdCdcValidationHarvested.root', output_file='VxdCdcMergerValidation.root'):
+        self.input_file = input_file
+        self.output_file = output_file
+
+        self.hist_merged_hits = ROOT.TH1F("merged_hits", "Merged Hits", 70, 0, 140)
+        self.hist_good_over_pt = ROOT.TProfile("good_over_pt", "Good Merge over Pt", 50, 0, 4)
+        self.hist_good_over_theta = ROOT.TProfile("good_over_theta", "Good Merge over Theta", 50, 0, 4)
+
+    def collect_histograms(self):
+        input_root_file = ROOT.TFile.Open(self.input_file, "READ")
+
+        for event in input_root_file.VxdCdcMergerHarvester_tree:
+            self.hist_merged_hits.Fill(event.PR_NHITS)
+            self.hist_good_over_pt.Fill(event.MC_PT, event.GOOD_MERGE)
+            self.hist_good_over_theta.Fill(event.MC_THETA, event.GOOD_MERGE)
+
+        input_root_file.Close()
+        return self
+
+    @staticmethod
+    def histogram_plot(hist, title, x_variable, x_unit=None, description='', check='', contact='', meta_options=''):
+        hist.SetName("".join(title.split()))
+        xlabel = '{} / ({})'.format(x_variable, x_unit) if x_unit is not None else '{}'.format(x_variable)
+        ylabel = 'Entries / ({} {})'.format((hist.GetXaxis().GetXmax() -
+                                             hist.GetXaxis().GetXmin()) /
+                                            hist.GetNbinsX(), x_unit) if x_unit is not None \
+            else 'Entries / ({})'.format((hist.GetXaxis().GetXmax() - hist.GetXaxis().GetXmin()) / hist.GetNbinsX())
+        hist.SetTitle("{};{};{}".format(title, xlabel, ylabel))
+        hist.GetListOfFunctions().Add(ROOT.TNamed('Description', description))
+        hist.GetListOfFunctions().Add(ROOT.TNamed('Check', check))
+        hist.GetListOfFunctions().Add(ROOT.TNamed('Contact', contact))
+        hist.GetListOfFunctions().Add(ROOT.TNamed('MetaOptions', meta_options))
+        return hist
+
+    def plot(self):
+        output_root_file = ROOT.TFile.Open(self.output_file, "RECREATE")
+
+        VxdCdcMergerValidationPlots.histogram_plot(self.hist_merged_hits, "Number of hits of merged tracks", "Number of Hits", None,
+                                                   description='Number of hits of merged tracks',
+                                                   check='',
+                                                   contact='',
+                                                   meta_options='').Write()
+
+        VxdCdcMergerValidationPlots.histogram_plot(self.hist_good_over_pt, "Good Merge over Pt", "MC Track Pt (GeV)", None,
+                                                   description='Good Merge over Pt',
+                                                   check='',
+                                                   contact='',
+                                                   meta_options='').Write()
+
+        VxdCdcMergerValidationPlots.histogram_plot(self.hist_good_over_theta, "Good Merge over Theta", "MC Track Theta (1)", None,
+                                                   description='Good Merge over Theta',
+                                                   check='',
+                                                   contact='',
+                                                   meta_options='').Write()
+
+        output_root_file.Write()
+        output_root_file.Close()
+        return self
+
+
+if __name__ == '__main__':
+    VxdCdcMergerValidationPlots().collect_histograms().plot()
---
+++ tracking/validation/vxdCdcMergerValidationGenerateSample.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+<header>
+  <contact>tracking@belle2.kek.jp</contact>
+  <output>VxdCdcValidationHarvested.root</output>
+  <description>This module generates events for the V0 validation.</description>
+</header>
+"""
+
+import basf2
+from ROOT import Belle2
+from simulation import add_simulation
+from reconstruction import add_mdst_output, add_reconstruction
+from modularAnalysis import generateY4S
+from tracking import add_track_finding, add_prune_tracks
+from tracking.validation.harvesting import HarvestingModule
+from tracking.validation import refiners, tracking_efficiency_helpers
+import numpy
+
+
+def run():
+    """
+    Generate and reconstruct Generic BBar
+    and evaluate the merger performance
+    """
+    components = tracking_efficiency_helpers.get_simulation_components()
+
+    basf2.set_random_seed(1337)
+    path = basf2.create_path()
+
+    # generateY4S(1000, path=path)
+
+    path.add_module('EventInfoSetter',
+                    evtNumList=[1000],
+                    runList=[1],
+                    expList=[1]
+                    )
+
+    path.add_module('EvtGenInput')
+
+    path.add_module('Gearbox')
+    add_simulation(path, components=components)
+    add_reconstruction(path, components=components)
+
+    # convert to TrackCands so tracks can be matched
+    path.add_module("GenfitTrackCandidatesCreator", recoTracksStoreArrayName='VXDRecoTracks',
+                    genfitTrackCandsStoreArrayName='VXDGFTrackCands')
+    path.add_module("GenfitTrackCandidatesCreator", recoTracksStoreArrayName='CDCRecoTracks',
+                    genfitTrackCandsStoreArrayName='CDCGFTrackCands')
+
+    # add additional matching for vxd and cdc only tracks here
+
+    # for VXD only track
+    mctrackmatcher_vxd = basf2.register_module('MCMatcherTracks')
+    mctrackmatcher_vxd.param('MCGFTrackCandsColName', 'MCTrackCands')
+    mctrackmatcher_vxd.param('PRGFTrackCandsColName', 'VXDGFTrackCands')
+    mctrackmatcher_vxd.param('UseCDCHits', False)
+    path.add_module(mctrackmatcher_vxd)
+
+    # for CDC only tracks
+    mctrackmatcher_cdc = basf2.register_module('MCMatcherTracks')
+    mctrackmatcher_cdc.param('MCGFTrackCandsColName', 'MCTrackCands')
+    mctrackmatcher_cdc.param('PRGFTrackCandsColName', 'CDCGFTrackCands')
+    mctrackmatcher_cdc.param('UsePXDHits', False)
+    mctrackmatcher_cdc.param('UseSVDHits', False)
+    path.add_module(mctrackmatcher_cdc)
+
+    path.add_module(VxdCdcMergerHarvester())
+
+    basf2.process(path)
+    print(basf2.statistics)
+
+
+class VxdCdcMergerHarvester(HarvestingModule):
+
+    def __init__(self):
+        """
+        Init harvester
+        """
+        HarvestingModule.__init__(self, foreach="MCParticles", output_file_name="../VxdCdcValidationHarvested.root")
+
+        #: matcher used for the final MCTrack list
+        self.mc_track_matcher = Belle2.TrackMatchLookUp("MCTrackCands")
+        #: matcher used for the MCTracks from the CDC
+        self.mc_track_matcher_cdc = Belle2.TrackMatchLookUp("MCTrackCands", "CDCGFTrackCands")
+        #: matcher used for the MCTracks from the VXD
+        self.mc_track_matcher_vxd = Belle2.TrackMatchLookUp("MCTrackCands", "VXDGFTrackCands")
+
+    def pick(self, mc_particle):
+        # mc_track = mc_particle.getRelated("MCTrackCands")
+        # return mc_track is not None
+        mc_track = mc_particle.getRelatedFrom("MCTrackCands")
+        if mc_track:
+            return True
+        else:
+            return False
+
+    def peel(self, mc_particle):
+        # mc is a genfit::TrackCand
+
+        mc_track = mc_particle.getRelatedFrom("MCTrackCands")
+
+        mc_nhits = mc_track.getNHits()
+        mc_pt = mc_particle.getMomentum().Pt()
+        mc_theta = mc_particle.getMomentum().Theta()
+
+        reco_pxd_hits = Belle2.PyStoreArray('PXDRecoHits')
+        reco_svd_hits = Belle2.PyStoreArray('SVDRecoHits')
+        reco_cdc_hits = Belle2.PyStoreArray('CDCHits')
+
+        reco_tracks = Belle2.PyStoreArray('TrackCands')
+
+        this_best_track_cdc = self.mc_track_matcher_cdc.getRelatedPRTrackCand(mc_track)
+        this_best_track_vxd = self.mc_track_matcher_vxd.getRelatedPRTrackCand(mc_track)
+
+        # here we know, the above tracks should have been merged because they stem
+        # from the same MC Track. Have they?
+
+        this_best_track_merged = self.mc_track_matcher.getRelatedPRTrackCand(mc_track)
+
+        # todo: once moved to RecoTrack: use more elaborate comparison method to indentify vxd/cdc tracks
+        # in merged tracks
+        # cdc_merge_fraction = sameHitFractionCdc( this_best_track_merged, this_best_track_cdc, reco_cdc_hits )
+        # vxd_merge_fraction = sameHitFractionVxd( this_best_track_merged, this_best_track_vxd, reco_pxd_hits, reco_svd_hits)
+
+        good_merge = False
+        vxd_hits = 0
+        cdc_hits = 0
+
+        merged_hits = 0
+
+        if this_best_track_merged:
+            if this_best_track_cdc:
+                cdc_hits = this_best_track_cdc.getNHits()
+            if this_best_track_vxd:
+                vxd_hits = this_best_track_vxd.getNHits()
+
+            merged_hits = vxd_hits + cdc_hits
+            good_merge = merged_hits == this_best_track_merged.getNHits()
+
+        return {
+            "MC_PT": mc_pt,
+            "MC_THETA": mc_theta,
+            "MC_NHITS": mc_nhits,
+            "PR_NHITS": merged_hits,
+            "PR_CDC_NHITS": cdc_hits,
+            "PR_VXD_NHITS": vxd_hits,
+            "GOOD_MERGE": good_merge
+        }
+
+    save_tree = refiners.SaveTreeRefiner()
+
+
+if __name__ == '__main__':
+    run()

a3a6004e2b1596c07e183cf6599bb175e0ac5d09
Thomas Hauth <thomas.hauth@kit.edu>
2016-03-24 17:46:57 +0000

    added author to header
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@26744 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/dataobjects/include/RecoTrack.h
+++ tracking/dataobjects/include/RecoTrack.h
@@ -3,7 +3,7 @@
  * Copyright(C) 2014 - Belle II Collaboration                             *
  *                                                                        *
  * Author: The Belle II Collaboration                                     *
- * Contributors: Martin Heck, Nils Braun                                  *
+ * Contributors: Martin Heck, Nils Braun, Thomas Hauth                    *
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/

1244b83a54ff1ae64038f468959fb2519f2bac15
Thomas Hauth <thomas.hauth@kit.edu>
2016-03-29 11:55:22 +0000

    removed anonymous namespace because this fixes unused var warning on GCC 5.0
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@26851 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/topology/tests/topology.cc
+++ tracking/trackFindingCDC/topology/tests/topology.cc
@@ -18,247 +18,245 @@
 using namespace Belle2;
 using namespace TrackFindingCDC;
 
-namespace {
-  class SecondaryWireNeighborhoodTest : public TrackFindingCDCTestWithTopology,
-    public ::testing::WithParamInterface<int> {
-  };
+class SecondaryWireNeighborhoodTest : public TrackFindingCDCTestWithTopology,
+  public ::testing::WithParamInterface<int> {
+};
 
-  TEST_P(SecondaryWireNeighborhoodTest, IsSymmetric)
-  {
-    WireNeighborKind testNeighborKind(2, GetParam());
-    short oClockDirection = testNeighborKind.getOClockDirection();
-    short reverseOClockDirection = modulo(testNeighborKind.getOClockDirection() + 6, 12);
+TEST_P(SecondaryWireNeighborhoodTest, IsSymmetric)
+{
+  WireNeighborKind testNeighborKind(2, GetParam());
+  short oClockDirection = testNeighborKind.getOClockDirection();
+  short reverseOClockDirection = modulo(testNeighborKind.getOClockDirection() + 6, 12);
 
-    const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
+  const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
 
-    for (const CDCWire& wire : theWireTopology.getWires()) {
-      MayBePtr<const CDCWire> neighbor = wire.getSecondaryNeighbor(oClockDirection);
-      if (neighbor != nullptr) {
-        MayBePtr<const CDCWire> neighbor_of_neighbor =
-          neighbor->getSecondaryNeighbor(reverseOClockDirection);
+  for (const CDCWire& wire : theWireTopology.getWires()) {
+    MayBePtr<const CDCWire> neighbor = wire.getSecondaryNeighbor(oClockDirection);
+    if (neighbor != nullptr) {
+      MayBePtr<const CDCWire> neighbor_of_neighbor =
+        neighbor->getSecondaryNeighbor(reverseOClockDirection);
 
-        EXPECT_EQ(*neighbor_of_neighbor, wire);
+      EXPECT_EQ(*neighbor_of_neighbor, wire);
 
-        WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
-        EXPECT_TRUE(neighborKind.isValid());
-        EXPECT_EQ(testNeighborKind.getCellDistance(), neighborKind.getCellDistance());
-        EXPECT_EQ(oClockDirection, neighborKind.getOClockDirection());
-        EXPECT_EQ(neighbor->getILayer() - wire.getILayer(), neighborKind.getILayerDifference());
-      }
+      WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
+      EXPECT_TRUE(neighborKind.isValid());
+      EXPECT_EQ(testNeighborKind.getCellDistance(), neighborKind.getCellDistance());
+      EXPECT_EQ(oClockDirection, neighborKind.getOClockDirection());
+      EXPECT_EQ(neighbor->getILayer() - wire.getILayer(), neighborKind.getILayerDifference());
     }
   }
+}
 
-  INSTANTIATE_TEST_CASE_P(SecondaryWireNeighborhoodTest_IsSymmetric,
-                          SecondaryWireNeighborhoodTest,
-                          ::testing::Range(0, 12));
+INSTANTIATE_TEST_CASE_P(SecondaryWireNeighborhoodTest_IsSymmetric,
+                        SecondaryWireNeighborhoodTest,
+                        ::testing::Range(0, 12));
 
 
-  TEST_F(TrackFindingCDCTestWithTopology, topology_WireNeighborSymmetry_CWInwards)
-  {
+TEST_F(TrackFindingCDCTestWithTopology, topology_WireNeighborSymmetry_CWInwards)
+{
 
-    const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
+  const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
 
-    for (const CDCWire& wire : theWireTopology.getWires()) {
-      MayBePtr<const CDCWire> neighbor = wire.getNeighborCWInwards();
-      if (neighbor != nullptr) {
-        MayBePtr<const CDCWire> neighbor_of_neighbor = neighbor->getNeighborCCWOutwards();
-        EXPECT_EQ(*neighbor_of_neighbor, wire);
+  for (const CDCWire& wire : theWireTopology.getWires()) {
+    MayBePtr<const CDCWire> neighbor = wire.getNeighborCWInwards();
+    if (neighbor != nullptr) {
+      MayBePtr<const CDCWire> neighbor_of_neighbor = neighbor->getNeighborCCWOutwards();
+      EXPECT_EQ(*neighbor_of_neighbor, wire);
 
-        WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
-        EXPECT_EQ(1, neighborKind.getCellDistance());
-        EXPECT_EQ(5, neighborKind.getOClockDirection());
-        EXPECT_EQ(-1, neighborKind.getILayerDifference());
-      }
+      WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
+      EXPECT_EQ(1, neighborKind.getCellDistance());
+      EXPECT_EQ(5, neighborKind.getOClockDirection());
+      EXPECT_EQ(-1, neighborKind.getILayerDifference());
     }
   }
+}
 
-  TEST_F(TrackFindingCDCTestWithTopology, topology_WireNeighborSymmetry_CCWInwards)
-  {
+TEST_F(TrackFindingCDCTestWithTopology, topology_WireNeighborSymmetry_CCWInwards)
+{
 
-    const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
+  const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
 
-    for (const CDCWire& wire : theWireTopology.getWires()) {
-      MayBePtr<const CDCWire> neighbor = wire.getNeighborCCWInwards();
-      if (neighbor != nullptr) {
-        MayBePtr<const CDCWire> neighbor_of_neighbor = neighbor->getNeighborCWOutwards();
-        EXPECT_EQ(*neighbor_of_neighbor, wire);
+  for (const CDCWire& wire : theWireTopology.getWires()) {
+    MayBePtr<const CDCWire> neighbor = wire.getNeighborCCWInwards();
+    if (neighbor != nullptr) {
+      MayBePtr<const CDCWire> neighbor_of_neighbor = neighbor->getNeighborCWOutwards();
+      EXPECT_EQ(*neighbor_of_neighbor, wire);
 
-        WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
-        EXPECT_EQ(1, neighborKind.getCellDistance());
-        EXPECT_EQ(7, neighborKind.getOClockDirection());
-        EXPECT_EQ(-1, neighborKind.getILayerDifference());
-      }
+      WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
+      EXPECT_EQ(1, neighborKind.getCellDistance());
+      EXPECT_EQ(7, neighborKind.getOClockDirection());
+      EXPECT_EQ(-1, neighborKind.getILayerDifference());
     }
   }
+}
 
-  TEST_F(TrackFindingCDCTestWithTopology, topology_WireNeighborSymmetry_CWOutwards)
-  {
+TEST_F(TrackFindingCDCTestWithTopology, topology_WireNeighborSymmetry_CWOutwards)
+{
 
-    const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
+  const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
 
-    for (const CDCWire& wire : theWireTopology.getWires()) {
-      MayBePtr<const CDCWire> neighbor = wire.getNeighborCWOutwards();
-      if (neighbor != nullptr) {
-        MayBePtr<const CDCWire> neighbor_of_neighbor = neighbor->getNeighborCCWInwards();
-        EXPECT_EQ(*neighbor_of_neighbor, wire);
+  for (const CDCWire& wire : theWireTopology.getWires()) {
+    MayBePtr<const CDCWire> neighbor = wire.getNeighborCWOutwards();
+    if (neighbor != nullptr) {
+      MayBePtr<const CDCWire> neighbor_of_neighbor = neighbor->getNeighborCCWInwards();
+      EXPECT_EQ(*neighbor_of_neighbor, wire);
 
-        WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
-        EXPECT_EQ(1, neighborKind.getCellDistance());
-        EXPECT_EQ(1, neighborKind.getOClockDirection());
-        EXPECT_EQ(1, neighborKind.getILayerDifference());
-      }
+      WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
+      EXPECT_EQ(1, neighborKind.getCellDistance());
+      EXPECT_EQ(1, neighborKind.getOClockDirection());
+      EXPECT_EQ(1, neighborKind.getILayerDifference());
     }
   }
+}
 
-  TEST_F(TrackFindingCDCTestWithTopology, topology_WireNeighborSymmetry_CCWOutwards)
-  {
+TEST_F(TrackFindingCDCTestWithTopology, topology_WireNeighborSymmetry_CCWOutwards)
+{
 
-    const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
+  const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
 
-    for (const CDCWire& wire : theWireTopology.getWires()) {
-      MayBePtr<const CDCWire> neighbor = wire.getNeighborCCWOutwards();
-      if (neighbor != nullptr) {
-        MayBePtr<const CDCWire> neighbor_of_neighbor = neighbor->getNeighborCWInwards();
-        EXPECT_EQ(*neighbor_of_neighbor, wire);
+  for (const CDCWire& wire : theWireTopology.getWires()) {
+    MayBePtr<const CDCWire> neighbor = wire.getNeighborCCWOutwards();
+    if (neighbor != nullptr) {
+      MayBePtr<const CDCWire> neighbor_of_neighbor = neighbor->getNeighborCWInwards();
+      EXPECT_EQ(*neighbor_of_neighbor, wire);
 
-        WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
-        EXPECT_EQ(1, neighborKind.getCellDistance());
-        EXPECT_EQ(11, neighborKind.getOClockDirection());
-        EXPECT_EQ(1, neighborKind.getILayerDifference());
-      }
+      WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
+      EXPECT_EQ(1, neighborKind.getCellDistance());
+      EXPECT_EQ(11, neighborKind.getOClockDirection());
+      EXPECT_EQ(1, neighborKind.getILayerDifference());
     }
   }
+}
 
-  TEST_F(TrackFindingCDCTestWithTopology, topology_WireNeighborSymmetry_CCW)
-  {
+TEST_F(TrackFindingCDCTestWithTopology, topology_WireNeighborSymmetry_CCW)
+{
 
-    const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
+  const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
 
-    for (const CDCWire& wire : theWireTopology.getWires()) {
-      MayBePtr<const CDCWire> neighbor = wire.getNeighborCCW();
-      if (neighbor != nullptr) {
-        MayBePtr<const CDCWire> neighbor_of_neighbor = neighbor->getNeighborCW();
-        EXPECT_EQ(*neighbor_of_neighbor, wire);
+  for (const CDCWire& wire : theWireTopology.getWires()) {
+    MayBePtr<const CDCWire> neighbor = wire.getNeighborCCW();
+    if (neighbor != nullptr) {
+      MayBePtr<const CDCWire> neighbor_of_neighbor = neighbor->getNeighborCW();
+      EXPECT_EQ(*neighbor_of_neighbor, wire);
 
-        WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
-        EXPECT_EQ(1, neighborKind.getCellDistance());
-        EXPECT_EQ(9, neighborKind.getOClockDirection());
-        EXPECT_EQ(0, neighborKind.getILayerDifference());
-      }
+      WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
+      EXPECT_EQ(1, neighborKind.getCellDistance());
+      EXPECT_EQ(9, neighborKind.getOClockDirection());
+      EXPECT_EQ(0, neighborKind.getILayerDifference());
     }
   }
+}
 
-  TEST_F(TrackFindingCDCTestWithTopology, topology_WireNeighborSymmetry_CW)
-  {
+TEST_F(TrackFindingCDCTestWithTopology, topology_WireNeighborSymmetry_CW)
+{
 
-    const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
+  const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
 
-    for (const CDCWire& wire : theWireTopology.getWires()) {
-      MayBePtr<const CDCWire> neighbor = wire.getNeighborCW();
-      if (neighbor != nullptr) {
-        MayBePtr<const CDCWire> neighbor_of_neighbor = neighbor->getNeighborCCW();
-        EXPECT_EQ(*neighbor_of_neighbor, wire);
+  for (const CDCWire& wire : theWireTopology.getWires()) {
+    MayBePtr<const CDCWire> neighbor = wire.getNeighborCW();
+    if (neighbor != nullptr) {
+      MayBePtr<const CDCWire> neighbor_of_neighbor = neighbor->getNeighborCCW();
+      EXPECT_EQ(*neighbor_of_neighbor, wire);
 
-        WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
-        EXPECT_EQ(1, neighborKind.getCellDistance());
-        EXPECT_EQ(3, neighborKind.getOClockDirection());
-        EXPECT_EQ(0, neighborKind.getILayerDifference());
-      }
+      WireNeighborKind neighborKind = wire.getNeighborKind(*neighbor);
+      EXPECT_EQ(1, neighborKind.getCellDistance());
+      EXPECT_EQ(3, neighborKind.getOClockDirection());
+      EXPECT_EQ(0, neighborKind.getILayerDifference());
     }
   }
+}
 
-  TEST_F(TrackFindingCDCTestWithTopology, topology_CDCWire_stereoAngle)
-  {
-    // Test if the all wires in the same superlayer have similar skew parameters.
+TEST_F(TrackFindingCDCTestWithTopology, topology_CDCWire_stereoAngle)
+{
+  // Test if the all wires in the same superlayer have similar skew parameters.
 
-    double tanThetaByICLayer[CDCWireTopology::c_NLayers];
-    double stereoAngleByICLayer[CDCWireTopology::c_NLayers];
-    double refCylindricalRByICLayer[CDCWireTopology::c_NLayers];
+  double tanThetaByICLayer[CDCWireTopology::c_NLayers];
+  double stereoAngleByICLayer[CDCWireTopology::c_NLayers];
+  double refCylindricalRByICLayer[CDCWireTopology::c_NLayers];
 
-    const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
-    for (const CDCWireLayer& wireLayer : theWireTopology.getWireLayers()) {
-      const ILayer iCLayer = wireLayer.getICLayer();
+  const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
+  for (const CDCWireLayer& wireLayer : theWireTopology.getWireLayers()) {
+    const ILayer iCLayer = wireLayer.getICLayer();
 
-      const CDCWire& firstWire = wireLayer.front();
-      tanThetaByICLayer[iCLayer] = firstWire.getTanStereoAngle();
-      stereoAngleByICLayer[iCLayer] = firstWire.getStereoAngle();
-      refCylindricalRByICLayer[iCLayer] = firstWire.getRefCylindricalR();
+    const CDCWire& firstWire = wireLayer.front();
+    tanThetaByICLayer[iCLayer] = firstWire.getTanStereoAngle();
+    stereoAngleByICLayer[iCLayer] = firstWire.getStereoAngle();
+    refCylindricalRByICLayer[iCLayer] = firstWire.getRefCylindricalR();
 
-      for (const CDCWire& wire : wireLayer) {
-        EXPECT_NEAR(tanThetaByICLayer[iCLayer], wire.getTanStereoAngle(), 10e-6);
-        EXPECT_NEAR(stereoAngleByICLayer[iCLayer], wire.getStereoAngle(), 10e-6);
-        EXPECT_NEAR(refCylindricalRByICLayer[iCLayer], wire.getRefCylindricalR(), 10e-6);
-      }
-
-      B2INFO("ICLayer : " << iCLayer);
-      B2INFO("   Tan Theta : " << tanThetaByICLayer[iCLayer]);
-      B2INFO("   Stereo angle : " << stereoAngleByICLayer[iCLayer]);
-      B2INFO("   Z range : " << wireLayer.getBackwardZ() << " to " << wireLayer.getForwardZ());
-      B2INFO("   Ref. cylindricalR : " << refCylindricalRByICLayer[iCLayer]);
-      B2INFO("   Max abs displacement : " << wireLayer.getWire(0).getWireVector().xy().norm());
+    for (const CDCWire& wire : wireLayer) {
+      EXPECT_NEAR(tanThetaByICLayer[iCLayer], wire.getTanStereoAngle(), 10e-6);
+      EXPECT_NEAR(stereoAngleByICLayer[iCLayer], wire.getStereoAngle(), 10e-6);
+      EXPECT_NEAR(refCylindricalRByICLayer[iCLayer], wire.getRefCylindricalR(), 10e-6);
     }
 
+    B2INFO("ICLayer : " << iCLayer);
+    B2INFO("   Tan Theta : " << tanThetaByICLayer[iCLayer]);
+    B2INFO("   Stereo angle : " << stereoAngleByICLayer[iCLayer]);
+    B2INFO("   Z range : " << wireLayer.getBackwardZ() << " to " << wireLayer.getForwardZ());
+    B2INFO("   Ref. cylindricalR : " << refCylindricalRByICLayer[iCLayer]);
+    B2INFO("   Max abs displacement : " << wireLayer.getWire(0).getWireVector().xy().norm());
   }
 
+}
 
 
-  TEST_F(TrackFindingCDCTestWithTopology, topology_RefCylindricalRVersusZInSuperLayers)
-  {
-    const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
-    for (const CDCWireSuperLayer& wireSuperLayer : theWireTopology.getWireSuperLayers()) {
-      if (wireSuperLayer.getStereoKind() == EStereoKind::c_Axial) {
-        EXPECT_EQ(0.0, wireSuperLayer.getRefTanLambda());
-      }
 
-      B2INFO("ISuperLayer : " << wireSuperLayer.getISuperLayer() <<
-             " Inner ref. z : " << wireSuperLayer.getInnerRefZ() <<
-             " Outer ref. z : " << wireSuperLayer.getOuterRefZ() <<
-             " CylindricalR Z slope : " << wireSuperLayer.getRefTanLambda()
-            );
-
+TEST_F(TrackFindingCDCTestWithTopology, topology_RefCylindricalRVersusZInSuperLayers)
+{
+  const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
+  for (const CDCWireSuperLayer& wireSuperLayer : theWireTopology.getWireSuperLayers()) {
+    if (wireSuperLayer.getStereoKind() == EStereoKind::c_Axial) {
+      EXPECT_EQ(0.0, wireSuperLayer.getRefTanLambda());
     }
+
+    B2INFO("ISuperLayer : " << wireSuperLayer.getISuperLayer() <<
+           " Inner ref. z : " << wireSuperLayer.getInnerRefZ() <<
+           " Outer ref. z : " << wireSuperLayer.getOuterRefZ() <<
+           " CylindricalR Z slope : " << wireSuperLayer.getRefTanLambda()
+          );
+
   }
+}
 
-  TEST_F(TrackFindingCDCTestWithTopology, topology_ShowCurlCurv)
-  {
-    const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
+TEST_F(TrackFindingCDCTestWithTopology, topology_ShowCurlCurv)
+{
+  const CDCWireTopology& theWireTopology  = CDCWireTopology::getInstance();
 
-    double outerR = theWireTopology.getOuterCylindricalR();
-    double innerR = theWireTopology.getInnerCylindricalR();
+  double outerR = theWireTopology.getOuterCylindricalR();
+  double innerR = theWireTopology.getInnerCylindricalR();
 
-    double innerOriginCurv = 1 / (innerR / 2);
-    double outerOriginCurv = 1 / (outerR / 2);
+  double innerOriginCurv = 1 / (innerR / 2);
+  double outerOriginCurv = 1 / (outerR / 2);
 
-    double innerCurlCurv = 1 / innerR;
-    double outerCurlCurv = 1 / outerR;
+  double innerCurlCurv = 1 / innerR;
+  double outerCurlCurv = 1 / outerR;
 
-    B2INFO("Maximal curvature reaching the CDC from IP : " << innerOriginCurv);
-    B2INFO("Minimal momentum reaching the CDC from IP : " << curvatureToAbsMom2D(innerOriginCurv, 1.5));
+  B2INFO("Maximal curvature reaching the CDC from IP : " << innerOriginCurv);
+  B2INFO("Minimal momentum reaching the CDC from IP : " << curvatureToAbsMom2D(innerOriginCurv, 1.5));
 
-    B2INFO("Maximal curvature leaving the CDC from IP : " << outerOriginCurv);
-    B2INFO("Minimal momentum leaving the CDC from IP : " << curvatureToAbsMom2D(outerOriginCurv, 1.5));
-
-
-    B2INFO("Minimal curvature not reaching the CDC from VXD : " << innerCurlCurv);
-    B2INFO("Maximal momentum not reaching the CDC from VXD : " << curvatureToAbsMom2D(innerCurlCurv, 1.5));
-
-    B2INFO("Minimal curvature not leaving the CDC from inside the CDC : " << outerCurlCurv);
-    B2INFO("Maximal momentum not leaving the CDC from inside the CDC : " << curvatureToAbsMom2D(outerCurlCurv, 1.5));
-  }
+  B2INFO("Maximal curvature leaving the CDC from IP : " << outerOriginCurv);
+  B2INFO("Minimal momentum leaving the CDC from IP : " << curvatureToAbsMom2D(outerOriginCurv, 1.5));
 
 
+  B2INFO("Minimal curvature not reaching the CDC from VXD : " << innerCurlCurv);
+  B2INFO("Maximal momentum not reaching the CDC from VXD : " << curvatureToAbsMom2D(innerCurlCurv, 1.5));
 
-  TEST_F(TrackFindingCDCTestWithTopology, topology_CDCGeometryPar_cellId)
-  {
-    // Testing for a discrepancy in the cellId method of CDCGeometryPar
-    // Example in layer four
-    unsigned iCLayer = 4;
+  B2INFO("Minimal curvature not leaving the CDC from inside the CDC : " << outerCurlCurv);
+  B2INFO("Maximal momentum not leaving the CDC from inside the CDC : " << curvatureToAbsMom2D(outerCurlCurv, 1.5));
+}
 
-    // Middle of the layer four
-    TVector3 posInMiddle(0, 0, 20.8);
 
-    unsigned iWireInFourthLayer = CDC::CDCGeometryPar::Instance().cellId(iCLayer, posInMiddle);
-    EXPECT_TRUE(iWireInFourthLayer < 160);
-    EXPECT_LT(iWireInFourthLayer, 160);
-  }
+
+TEST_F(TrackFindingCDCTestWithTopology, topology_CDCGeometryPar_cellId)
+{
+  // Testing for a discrepancy in the cellId method of CDCGeometryPar
+  // Example in layer four
+  unsigned iCLayer = 4;
+
+  // Middle of the layer four
+  TVector3 posInMiddle(0, 0, 20.8);
+
+  unsigned iWireInFourthLayer = CDC::CDCGeometryPar::Instance().cellId(iCLayer, posInMiddle);
+  EXPECT_TRUE(iWireInFourthLayer < 160);
+  EXPECT_LT(iWireInFourthLayer, 160);
 }

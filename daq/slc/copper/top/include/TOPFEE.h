722f3ecd9af62829d74b9d4f9bc20fdd65c872be
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2016-03-30 07:21:45 +0000

    Added TOP FEE
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@26901 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ daq/slc/copper/top/include/TOPFEE.h
@@ -0,0 +1,23 @@
+#ifndef _Belle2_TOPFEE_h
+#define _Belle2_TOPFEE_h
+
+#include <daq/slc/copper/FEE.h>
+
+namespace Belle2 {
+
+  class TOPFEE : public FEE {
+
+  public:
+    TOPFEE();
+    virtual ~TOPFEE() throw() {}
+
+  public:
+    virtual void init(RCCallback& callback, HSLB& hslb);
+    virtual void boot(HSLB& hslb, const DBObject& obj);
+    virtual void load(HSLB& hslb, const DBObject& obj);
+
+  };
+
+}
+
+#endif
---
+++ daq/slc/copper/top/src/TOPFEE.cc
@@ -0,0 +1,55 @@
+#include "daq/slc/copper/top/TOPFEE.h"
+#include "daq/slc/copper/FEEHandler.h"
+
+#include <daq/slc/system/File.h>
+#include <daq/slc/system/LogFile.h>
+
+#include <mgt/hsreg.h>
+
+#define REG0_ADDR 0x01
+
+using namespace Belle2;
+
+TOPFEE::TOPFEE()
+{
+}
+
+void TOPFEE::init(RCCallback& callback, HSLB& hslb)
+{
+  std::string vname = StringUtil::form("top[%d].", hslb.get_finid());
+  callback.add(new FEEStreamHandler(vname + "steam", callback, hslb, ""));
+  callback.add(new FEE8Handler(vname + "reg0", callback, hslb, *this, REG0_ADDR));
+}
+
+void TOPFEE::boot(HSLB& hslb,  const DBObject& obj)
+{
+
+}
+
+void TOPFEE::load(HSLB& hslb, const DBObject& obj)
+{
+
+  // writing stream file to FEE
+  if (obj.hasText("stream")) {
+    const std::string stream = obj.getText("stream");
+    if (File::exist(stream)) {
+      //hslb.writestream(stream.c_str());
+    } else {
+      LogFile::warning("file %s not exsits", stream.c_str());
+    }
+  }
+
+  // write access to register "reg0"
+  if (obj.hasValue("reg0")) {
+    //  int val = obj.getInt("reg0");
+    //  LogFile::debug("writefee8 val=%d to reg0(adr=%d)", val, REG0_ADDR);
+    //  hslb.writefee8(REG0_ADDR, val);
+  }
+}
+
+extern "C" {
+  void* getTOPFEE()
+  {
+    return new Belle2::TOPFEE();
+  }
+}

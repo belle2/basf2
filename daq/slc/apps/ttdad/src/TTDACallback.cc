b3b618c475c04a3287d4cda2f68c7a3be421bb7d
Tomoyuki Konno <konno@hepmail.phys.se.tmu.ac.jp>
2016-03-31 05:18:53 +0000

    Added new ftprogs
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@26959 b38c6c8f-bee8-465d-8448-9abe6915d496

--- daq/slc/apps/ttdad/src/TTDACallback.cc
+++ daq/slc/apps/ttdad/src/TTDACallback.cc
@@ -7,12 +7,29 @@
 #include <daq/slc/base/StringUtil.h>
 #include <daq/slc/base/Date.h>
 
-#include <unistd.h>
+#define STATFT_NSM
 
 #include <ftprogs/ftsw.h>
 #include <ftprogs/ftstat.h>
+#include <ftprogs/pocket_ttd.h>
+#include <ftprogs/pocket_ttd_fast.h>
+#include <ftprogs/ft2u067.h>
 
-ftsw_t* g_ftsw = NULL;
+#include <unistd.h>
+#include <sys/time.h>
+
+#define VERSION 20160330
+
+typedef struct pocket_ttd_fast fast_t;
+typedef struct pocket_ttd      slow_t;
+
+extern "C" {
+  void summary2u067(struct timeval* tvp, fast_t* f, slow_t* s);
+  void color2u067(fast_t* f, slow_t* s);
+}
+
+unsigned int prev_u = 0;
+unsigned int prev_c = 0;
 
 namespace Belle2 {
 
@@ -107,13 +124,15 @@ bool TTDACallback::setTriggerType(const std::string& type) throw()
 void TTDACallback::initialize(const DBObject& obj) throw(RCHandlerException)
 {
   configure(obj);
+  openData("TTDFAST", "pocket_ttd_fast", pocket_ttd_fast_revision);
+  openData("TTDSLOW", "pocket_ttd", pocket_ttd_revision);
 }
 
 void TTDACallback::configure(const DBObject& obj) throw(RCHandlerException)
 {
   add(new NSMVHandlerInt("ftsw", true, false, m_ftswid));
   add(new NSMVHandlerTrigft(*this, "trigft"));
-  add(new NSMVHandlerTriggerType(*this, "trigger_type", m_trigger_type));//obj.getText("trigger_type")));
+  add(new NSMVHandlerTriggerType(*this, "trigger_type", m_trigger_type));
   add(new NSMVHandlerInt("trigger_limit", true, true, m_trigger_limit));
   add(new NSMVHandlerInt("dummy_rate", true, true, m_dummy_rate));
   add(new NSMVHandlerInt("expno", true, false, 0));
@@ -138,12 +157,6 @@ void TTDACallback::configure(const DBObject& obj) throw(RCHandlerException)
   add(new NSMVHandlerInt("trun", true, false, 0));
   add(new NSMVHandlerText("ftstate", true, false, "UNKNOWN"));
   add(new NSMVHandlerStatFT(*this, "statft"));
-
-  try {
-    //trigft();
-  } catch (const RCHandlerException& e) {
-    LogFile::warning(e.what());
-  }
 }
 
 void TTDACallback::monitor() throw(RCHandlerException)
@@ -163,33 +176,49 @@ void TTDACallback::monitor() throw(RCHandlerException)
     } catch (const NSMHandlerException& e) {
       LogFile::error(e.what());
     }
-  } else {
-    if (g_ftsw == NULL) {
-      g_ftsw = open_ftsw(m_ftswid, 0x01);
-    }
-    statft(g_ftsw, m_ftswid);
-    set("expno", (int)g_ftstat.exp);
-    set("runno", (int)g_ftstat.run);
-    set("subno", (int)g_ftstat.sub);
-    set("busy", g_ftstat.busy);
-    set("reset", g_ftstat.reset);
-    //LogFile::info("reset=%s", g_ftstat.reset);
-    set("stafifo", g_ftstat.stafifo);
-    set("tincnt", (float)g_ftstat.tincnt);
-    set("toutcnt", (float)g_ftstat.toutcnt);
-    set("atrigc", (float)g_ftstat.atrigc);
-    set("rateall", g_ftstat.rateall);
-    set("raterun", g_ftstat.raterun);
-    set("rateout", g_ftstat.rateout);
-    set("ratein", g_ftstat.ratein);
-    set("tlimit", (int)g_ftstat.tlimit);
-    set("tlast", (int)g_ftstat.tlast);
-    set("err", g_ftstat.err);
-    set("errport", g_ftstat.errport);
-    set("tstart_s", Date(g_ftstat.tstart).toString());
-    set("tstart", (int)g_ftstat.tstart);
-    set("trun", (int)g_ftstat.trun);
-    set("ftstate", g_ftstat.state);
+    NSMData& data_fast(getData("TTDFAST"));
+    NSMData& data_slow(getData("TTDSLOW"));
+    try {
+      if (data_fast.isAvailable() && data_slow.isAvailable()) {
+        fast_t* f = (fast_t*)data_fast.get();
+        slow_t* s = (slow_t*)data_slow.get();
+        struct timeval tv;
+        gettimeofday(&tv, 0);
+        struct tm* tp = localtime(&tv.tv_sec);
+        if (prev_u != f->utime || prev_c != f->ctime) {
+          sprintf(g_ftstat.statft,
+                  "statft version %d FTSW(NSM) - "
+                  "%04d.%02d.%02d %02d:%02d:%02d.%03d\n",
+                  VERSION, tp->tm_year + 1900, tp->tm_mon + 1, tp->tm_mday,
+                  tp->tm_hour, tp->tm_min, tp->tm_sec, (int)(tv.tv_usec / 1000));
+          color2u067(f, s);
+          summary2u067(&tv, f, s);
+          set("expno", (int)g_ftstat.exp);
+          set("runno", (int)g_ftstat.run);
+          set("subno", (int)g_ftstat.sub);
+          set("busy", g_ftstat.busy);
+          set("reset", g_ftstat.reset);
+          set("stafifo", g_ftstat.stafifo);
+          set("tincnt", (float)g_ftstat.tincnt);
+          set("toutcnt", (float)g_ftstat.toutcnt);
+          set("atrigc", (float)g_ftstat.atrigc);
+          set("rateall", g_ftstat.rateall);
+          set("raterun", g_ftstat.raterun);
+          set("rateout", g_ftstat.rateout);
+          set("ratein", g_ftstat.ratein);
+          set("tlimit", (int)g_ftstat.tlimit);
+          set("tlast", (int)g_ftstat.tlast);
+          set("err", g_ftstat.err);
+          set("errport", g_ftstat.errport);
+          set("tstart_s", Date(g_ftstat.tstart).toString());
+          set("tstart", (int)g_ftstat.tstart);
+          set("trun", (int)g_ftstat.trun);
+          set("ftstate", g_ftstat.state);
+        }
+        prev_u = f->utime;
+        prev_c = f->ctime;
+      }
+    } catch (const NSMHandlerException& e) {}
   }
 }
 
@@ -200,8 +229,18 @@ void TTDACallback::load(const DBObject&) throw(RCHandlerException)
 
 void TTDACallback::start(int expno, int runno) throw(RCHandlerException)
 {
+  int dummy_rate = 0, trigger_limit = 0;
+  get("trigger_type", m_trigger_type);
+  get("dummy_rate", dummy_rate);
+  get("trigger_limit", trigger_limit);
   if (m_ttdnode.getName().size() > 0) {
-    int pars[2] = { expno, runno };
+    int pars[3];
+    pars[0] = m_trgcommands[m_trigger_type];
+    pars[1] = dummy_rate;
+    pars[2] = trigger_limit;
+    send(NSMMessage(m_ttdnode, NSMCommand(11, "TRIGFT"), 3, pars));
+    pars[0] = expno;
+    pars[1] = runno;
     send(NSMMessage(m_ttdnode, NSMCommand(12, "START"), 2, pars));
   } else {
     std::string cmd = StringUtil::form("regft -%d 160 0x%x", m_ftswid, (expno << 22) + ((runno - 1) << 8));
@@ -209,26 +248,20 @@ void TTDACallback::start(int expno, int runno) throw(RCHandlerException)
     system(cmd.c_str());
     sleep(1);
     trigft();
-    //cmd = StringUtil::form("/home/usr/nakao/bin/trigft -%d %s", m_ftswid, m_trigger_type.c_str());
-    //cmd = StringUtil::form("trigft -%d %s", m_ftswid, m_trigger_type.c_str());
-    int dummy_rate = 0, trigger_limit = 0;
-    get("trigger_type", m_trigger_type);
-    get("dummy_rate", dummy_rate);
-    get("trigger_limit", trigger_limit);
     cmd = StringUtil::form("trigft -%d %s %d %d", m_ftswid,
                            m_trigger_type.c_str(), dummy_rate, trigger_limit);
-    DBObject& obj(getDBObject());
-    obj.addInt("ftsw", m_ftswid);
-    obj.addText("trigger_type", m_trigger_type);
-    obj.addInt("dummy_rate", dummy_rate);
-    obj.addInt("trigger_limit", trigger_limit);
     LogFile::debug(cmd);
     system(cmd.c_str());
-    monitor();
-    obj.addText("tstart_s", Date(g_ftstat.tstart).toString());
-    obj.addInt("tstart", g_ftstat.tstart);
-    obj.addInt("trun", g_ftstat.trun);
   }
+  monitor();
+  DBObject& obj(getDBObject());
+  obj.addInt("ftsw", m_ftswid);
+  obj.addText("trigger_type", m_trigger_type);
+  obj.addInt("dummy_rate", dummy_rate);
+  obj.addInt("trigger_limit", trigger_limit);
+  obj.addText("tstart_s", Date(g_ftstat.tstart).toString());
+  obj.addInt("tstart", g_ftstat.tstart);
+  obj.addInt("trun", g_ftstat.trun);
 }
 
 void TTDACallback::stop() throw(RCHandlerException)
@@ -236,23 +269,22 @@ void TTDACallback::stop() throw(RCHandlerException)
   if (m_ttdnode.getName().size() > 0) {
     send(NSMMessage(m_ttdnode, NSMCommand(13, "STOP")));
   } else {
-    //std::string cmd = StringUtil::form("/home/usr/nakao/bin/trigft -%d reset", m_ftswid);
-    int dummy_rate = 0, trigger_limit = 0;
-    get("trigger_type", m_trigger_type);
-    get("dummy_rate", dummy_rate);
-    get("trigger_limit", trigger_limit);
-    DBObject& obj(getDBObject());
-    obj.addInt("ftsw", m_ftswid);
-    obj.addText("trigger_type", m_trigger_type);
-    obj.addInt("dummy_rate", dummy_rate);
-    obj.addInt("trigger_limit", trigger_limit);
-    obj.addText("tstart_s", Date(g_ftstat.tstart).toString());
-    obj.addInt("tstart", g_ftstat.tstart);
-    obj.addInt("trun", g_ftstat.trun);
     std::string cmd = StringUtil::form("trigft -%d reset", m_ftswid);
     LogFile::debug(cmd);
     system(cmd.c_str());
   }
+  int dummy_rate = 0, trigger_limit = 0;
+  get("trigger_type", m_trigger_type);
+  get("dummy_rate", dummy_rate);
+  get("trigger_limit", trigger_limit);
+  DBObject& obj(getDBObject());
+  obj.addInt("ftsw", m_ftswid);
+  obj.addText("trigger_type", m_trigger_type);
+  obj.addInt("dummy_rate", dummy_rate);
+  obj.addInt("trigger_limit", trigger_limit);
+  obj.addText("tstart_s", Date(g_ftstat.tstart).toString());
+  obj.addInt("tstart", g_ftstat.tstart);
+  obj.addInt("trun", g_ftstat.trun);
 }
 
 bool TTDACallback::pause() throw(RCHandlerException)
@@ -281,11 +313,6 @@ void TTDACallback::recover(const DBObject&) throw(RCHandlerException)
 void TTDACallback::abort() throw(RCHandlerException)
 {
   stop();
-  /*
-  if (m_ttdnode.getName().size() > 0) {
-    send(NSMMessage(m_ttdnode, NSMCommand(13, "STOP")));
-  }
-  */
 }
 
 void TTDACallback::trigft() throw(RCHandlerException)
@@ -299,16 +326,10 @@ void TTDACallback::trigft() throw(RCHandlerException)
     if (m_ttdnode.getName().size() > 0) {
       send(NSMMessage(m_ttdnode, NSMCommand(11, "TRIGFT"), 3, pars));
     } else {
-      //std::string cmd = StringUtil::form("/home/usr/nakao/bin/utimeft -%d", m_ftswid);
-      //LogFile::debug(cmd);
-      //system(cmd.c_str());
-      //sleep(1);
       std::string cmd = StringUtil::form("trigft -%d reset", m_ftswid);
       LogFile::debug(cmd);
       system(cmd.c_str());
       sleep(1);
-      //cmd = StringUtil::form("trigft -%d %s %d %d", m_ftswid,
-      //           m_trigger_type.c_str(), pars[1], pars[2]);
     }
   } catch (const std::out_of_range& e) {
     LogFile::error(e.what());
@@ -356,7 +377,7 @@ void TTDACallback::error(const char* nodename, const char* data) throw()
       m_ttdnode.getName() == nodename) {
     LogFile::debug("ERROR from %s : %s", nodename, data);
     try {
-      setState(RCState::NOTREADY_S);
+      //setState(RCState::NOTREADY_S);
     } catch (const std::out_of_range& e) {}
   }
 }

46eb997fc0dee599f21b3ff7139fbd95d27feb82
Makoto Uchida <uchida@phys.titech.ac.jp>
2016-04-04 09:52:44 +0000

    The steering file for CDC database importer.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@27111 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ cdc/examples/CDCDatabaseImporter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+CDC Database importer.
+Simple example.
+"""
+
+from basf2 import *
+import ROOT
+from ROOT.Belle2 import CDCDatabaseImporter
+from ROOT.Belle2 import FileSystem
+import os
+import sys
+import glob
+import subprocess
+from fnmatch import fnmatch
+
+mapfile = FileSystem.findFile('data/cdc/ch_map.dat')
+use_local_database("cdc_crt/database.txt", "cdc_crt")
+
+main = create_path()
+
+# Event info setter - execute single event
+eventinfosetter = register_module('EventInfoSetter')
+eventinfosetter.param({'evtNumList': [1], 'runList': [1]})
+main.add_module(eventinfosetter)
+
+# Gearbox - access to xml files
+gearbox = register_module('Gearbox')
+main.add_module(gearbox)
+
+# process single event
+process(main)
+
+dbImporter = CDCDatabaseImporter()
+dbImporter.importChannelMap(mapfile)
+dbImporter.importTimeZero()
+# dbImporter.printChannelMap()
+# dbImporter.printTimeZero()

e21ab50ef01079d180b3792cd17bb6049a7d3de9
Bjoern Spruck <bjoern.spruck@exp2.physik.uni-giessen.de>
2016-04-12 20:17:56 +0000

    modified debug ROI generator to suit TB needs. Changed default parameters to useful ones.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@27525 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/modules/pxdDataReduction/src/ROIGeneratorModule.cc
+++ tracking/modules/pxdDataReduction/src/ROIGeneratorModule.cc
@@ -38,16 +38,16 @@ ROIGeneratorModule::ROIGeneratorModule() : Module()
   addParam("nROIs", m_nROIs, "number of generated ROIs", 1);
 
   addParam("TrigDivider", m_divider, "Generates one ROI every TrigDivider events", 2);
-  addParam("Layer"      , m_layer  , "on layer", 2);
+  addParam("Layer"      , m_layer  , "on layer", 1);
   addParam("Ladder"     , m_ladder , " ladder " , 1);
   addParam("Sensor"     , m_sensor , " sensor " , 1);
 
   addParam("MinU"       , m_minU   , " min U (pixel column hopefully) ", 0);
-  addParam("MaxU"       , m_maxU   , " max U (pixel column hopefully) ", 479);
+  addParam("MaxU"       , m_maxU   , " max U (pixel column hopefully) ", 786 - 1);
 
 
   addParam("MinV"       , m_minV   , " min V (pixel column hopefully) ", 0);
-  addParam("MaxV"       , m_maxV   , " max v (pixel column hopefully) ", 192);
+  addParam("MaxV"       , m_maxV   , " max v (pixel column hopefully) ", 250 - 1);
 
 
 }
@@ -76,9 +76,10 @@ void ROIGeneratorModule::event()
   StoreArray<ROIid> ROIList(m_ROIListName);
 
   StoreObjPtr<EventMetaData> eventMetaDataPtr;
-  int iROI = eventMetaDataPtr->getEvent(); // trigger number
+  int tNr = eventMetaDataPtr->getEvent(); // trigger number
 
-  if (iROI % m_divider != 0)
+  // Only if divider tells us to...
+  if (tNr % m_divider != 0)
     return ;
 
   //  ROIList.create(true);
@@ -90,22 +91,7 @@ void ROIGeneratorModule::event()
   sensorID.setLadderNumber(m_ladder);
   sensorID.setSensorNumber(m_sensor);
 
-  /*
-  int minU;
-  int minV;
-  int maxU;
-  int maxV;
-
-  //  for (int iROI = 0; iROI < m_nROIs; iROI++) {
-
-  //    minU =  (0 + iROI*16 ) % 250;
-  //  minU =  iROI % 250;
-  //  maxU = min(249, minU + 5);
-  //    minV =  (0 + iROI*32 ) % 768;
-  //  minV = (iROI / 250) % 768;
-  //  maxV = min(767 , minV + 10);
-  */
-
+  // Always create one FULL size ROI
   tmp_ROIid.setMinUid(m_minU);
   tmp_ROIid.setMinVid(m_minV);
   tmp_ROIid.setMaxUid(m_maxU);
@@ -113,7 +99,28 @@ void ROIGeneratorModule::event()
   tmp_ROIid.setSensorID(sensorID);
 
   ROIList.appendNew(tmp_ROIid);
-  //  }
+
+  if (m_nROIs > 1) {
+    // ... plus additional ones for debugging.
+    // maybe we should do it depending on the triggernr lateron...
+    int dU = (m_maxU - m_minU) / (m_nROIs + 1);
+    int dV = (m_maxV - m_minV) / (m_nROIs + 1);
+    for (int iROI = 1; iROI < m_nROIs; iROI++) {
+      // Create a chain of ROIs from top left to bottom right
+      int minU = m_minU + dU * iROI;
+      int maxU = minU + dU;
+      int minV = m_minV + dV * iROI;
+      int maxV = minV + dV;
+
+      tmp_ROIid.setMinUid(minU);
+      tmp_ROIid.setMinVid(minV);
+      tmp_ROIid.setMaxUid(maxU);
+      tmp_ROIid.setMaxVid(maxV);
+      tmp_ROIid.setSensorID(sensorID);
+
+      ROIList.appendNew(tmp_ROIid);
+    }
+  }
 }
 
 

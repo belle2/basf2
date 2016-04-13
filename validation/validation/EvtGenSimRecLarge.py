b7223b7a032c627180238f3aeec85f43d890a57e
Thomas Hauth <thomas.hauth@kit.edu>
2016-04-13 08:54:33 +0000

    added long-running EvtGenSimRec to release validation
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@27537 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ validation/validation/EvtGenSimRecLarge.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+<header>
+  <contact>tkuhr</contact>
+  <description>This steering file produces 10000 generic BBbar events with EvtGen,
+  runs the detector simulation with mixed in background, and performs the standard reconstruction.
+  It will only be run for release validation in order to test the software on a larger set of events to
+  be more closer to the typical runtime on a grid site.</description>
+  <interval>release</interval>
+</header>
+"""
+
+from basf2 import *
+from simulation import add_simulation
+from reconstruction import add_reconstruction
+from beamparameters import add_beamparameters
+import glob
+
+set_random_seed(12345)
+
+main = create_path()
+
+# specify number of events to be generated
+eventinfosetter = register_module('EventInfoSetter')
+# will roughly run for 10 hours
+eventinfosetter.param('evtNumList', [40000])
+eventinfosetter.param('runList', [1])
+eventinfosetter.param('expList', [1])
+main.add_module(eventinfosetter)
+
+# beam parameters
+beamparameters = add_beamparameters(main, "Y4S")
+# beamparameters.param("smearVertex", False)
+
+# generate BBbar events
+evtgeninput = register_module('EvtGenInput')
+main.add_module(evtgeninput)
+
+# detector simulation
+bg = None
+if 'BELLE2_BACKGROUND_DIR' in os.environ:
+    bg = glob.glob(os.environ['BELLE2_BACKGROUND_DIR'] + '/*.root')
+add_simulation(main, bkgfiles=bg)
+
+# reconstruction
+add_reconstruction(main)
+
+# memory profile
+main.add_module(register_module('Profile'))
+
+# do not output to save storage space
+process(main)
+
+# Print call statistics
+print(statistics)
+
+from validation import *
+statistics_plots('EvtGenSimRecLarge_statistics.root', contact='tkuhr',
+                 jobDesc='a standard simulation and reconstruction job with generic EvtGen events', prefix='EvtGenSimRecLarge')
+event_timing_plot('../EvtGenSimRecLarge.root', 'EvtGenSimRecLarge_statistics.root',
+                  contact='tkuhr',
+                  jobDesc='a standard simulation and reconstruction job with generic EvtGen events', prefix='EvtGenSimRecLarge')

259f96a2c85e9138ed4af83d2d12a0ef7451d928
Tadeas Bilka <tadeas.bilka@gmail.com>
2016-04-24 00:19:58 +0000

    Add missing documentation
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@27977 b38c6c8f-bee8-465d-8448-9abe6915d496

--- testbeam/vxd/modules/unpacking/include/PXDTriggerShifter.h
+++ testbeam/vxd/modules/unpacking/include/PXDTriggerShifter.h
@@ -14,19 +14,25 @@
 namespace Belle2 {
 
   namespace PXD {
-
+    /// Module to match PXD and ONSET trigger number which differ by fixed offset
     class PXDTriggerShifterModule: public Module {
     private:
+      /// MRU cache size
       enum {MAX_EVENTSHIFT = 100};
+      /// MRU cache with last couple of events
       MRUCache<int, TClonesArray> m_previous_events{MAX_EVENTSHIFT};
+      /// The store array with RawPXDs to match
       StoreArray<RawPXD> m_storeRaw;
+      /// The trigger offset
       int m_offset;
 
     public:
       PXDTriggerShifterModule();
       void initialize(void);
       void event(void);
+      /// Get the DHH and HLT trigger number from RawPXD
       bool getTrigNr(RawPXD& px, unsigned int& innerDHH, unsigned int& outerHLT);
+      /// Unpack DHE(C) frame in dataptr
       bool unpack_dhc_frame(void* data, unsigned int& innerDHH, unsigned int& outerHLT);
     };
   }

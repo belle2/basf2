4a262fe11c2d03dbd653fe4fb06498bb949788ed
Chunhua Li <chunhua.li@unimelb.edu.au>
2016-04-26 06:12:23 +0000

    remove  fillParticleList from the script
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@28055 b38c6c8f-bee8-465d-8448-9abe6915d496

--- skim/hlt/examples/physicstrigger_example_mc_rec.py
+++ skim/hlt/examples/physicstrigger_example_mc_rec.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-################################################################################################
-#
-# This file shows how to execute high level trigger (hlt) module
-# The data flow isGenerator-->Simulation-->Level3 trigger-->Reconstruction->Physics trigger
-#
-# For reconstruction module, events are reconstructed with information form all
-# detectors except PXD
-#
-#
-# Contributor: Chunhua LI
-#
-##################################################################################################
-
 from basf2 import *
-from simulation import add_simulation
-from reconstruction import add_reconstruction
 from modularAnalysis import inputMdstList
 from modularAnalysis import fillParticleList
 from modularAnalysis import analysis_main
 from modularAnalysis import generateY4S
 from HLTTrigger import add_HLT_Y4S
-from beamparameters import add_beamparameters
-from modularAnalysis import generateY4S
 from ROOT import Belle2
 
 logging.log_level = LogLevel.INFO
@@ -31,16 +14,10 @@ emptypath = create_path()
 
 filelistSIG = \
     [
-        '../../../../release-head-head/MC/Analysis_L1emulator/MC/tauToegamma/BGx0-noVXD/rootFiles/*root'
+        'inputfile'
     ]
 
 inputMdstList(filelistSIG)
-
-# create charged tracks list
-fillParticleList('pi+:HLT', '')
-
-# create gamma list
-fillParticleList('gamma:HLT', '')
 
 # load the PhysicsTrigger Module
 add_HLT_Y4S(analysis_main)

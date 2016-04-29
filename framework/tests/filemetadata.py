8f8ab056c99bc4ce62c47cb19099b9793f501b04
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2016-04-29 10:53:09 +0000

    actually check FileMetaData contents in test
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@28236 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/tests/filemetadata.py
+++ framework/tests/filemetadata.py
@@ -10,11 +10,8 @@ set_random_seed("something important")
 testFile = tempfile.NamedTemporaryFile(prefix='b2filemetadata_')
 
 # Register modules
-eventinfosetter = register_module('EventInfoSetter')
+eventinfosetter = register_module('EventInfoSetter', evtNumList=[9, 1], runList=[1, 15], expList=[7, 7])
 output = register_module('RootOutput')
-
-# Set module parameters
-eventinfosetter.param('evtNumList', 10)
 
 output.param('outputFileName', testFile.name)
 output.param('updateFileCatalog', False)
@@ -29,10 +26,47 @@ main.add_module(output)
 # Process events
 process(main)
 
-# Check the file meta data
+# Check the file meta data (via DataStore)
+from ROOT import Belle2
+metadata = Belle2.PyStoreObj('FileMetaData', 1)
+
+# print (metadata.getLfn()) #?
+assert 10 == metadata.getNEvents()
+
+assert 7 == metadata.getExperimentLow()
+assert 1 == metadata.getRunLow()
+assert 0 == metadata.getEventLow()
+assert 7 == metadata.getExperimentHigh()
+assert 15 == metadata.getRunHigh()
+assert 0 == metadata.getEventHigh()  # highest evt. no in highest run
+assert metadata.containsEvent(7, 1, 0)
+assert metadata.containsEvent(7, 1, 8)
+assert metadata.containsEvent(7, 15, 0)
+assert metadata.containsEvent(7, 1, 9)  # does not exist, but would be plausible
+assert metadata.containsEvent(7, 3, 5)  # same
+assert not metadata.containsEvent(7, 0, 9)  # does not exist, but would be plausible
+assert not metadata.containsEvent(7, 16, 5)  # same
+assert not metadata.containsEvent(0, 15, 5)
+
+assert 0 == metadata.getNParents()
+
+# print (metadata.getDate())
+# assert socket.gethostname() == metadata.getSite()
+# print (metadata.getUser()) #different env variables, not checked
+assert "something important" == metadata.getRandomSeed()
+
+assert basf2version == metadata.getRelease()
+assert metadata.getSteering().startswith('#!/usr/bin/env python3')
+assert metadata.getSteering().strip().endswith('dummystring')
+assert 10 == metadata.getMcEvents()
+
+
+# Check the file meta data (via file)
 assert 0 == os.system('showmetadata ' + testFile.name)
 
 os.system('touch Belle2FileCatalog.xml')
 assert 0 == os.system('addmetadata --lfn /logical/file/name ' + testFile.name)
 
 assert 0 == os.system('showmetadata ' + testFile.name)
+
+# dummystring

fa46bc9515ea57218f6785784fe3b29a9002cfdb
Oliver Frost <oliver.frost@desy.de>
2016-05-07 20:58:11 +0000

    Added optional deletion of higher order trajectory loops to the standard wire hit preparation.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@28373 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/findlets/combined/include/WireHitTopologyPreparer.h
+++ tracking/trackFindingCDC/findlets/combined/include/WireHitTopologyPreparer.h
@@ -10,9 +10,12 @@
 #pragma once
 
 #include <tracking/trackFindingCDC/findlets/minimal/WireHitCreator.h>
+#include <tracking/trackFindingCDC/findlets/minimal/WireHitMCMultiLoopBlocker.h>
 #include <tracking/trackFindingCDC/findlets/minimal/WireHitTopologyFiller.h>
 
 #include <tracking/trackFindingCDC/eventdata/hits/CDCWireHit.h>
+
+#include <tracking/trackFindingCDC/utilities/StringManipulation.h>
 
 #include <string>
 #include <vector>
@@ -42,16 +45,37 @@ namespace Belle2 {
                "Also set all CDCWireHits as unused.";
       }
 
-      /// Expose the parameters of the cluster filter to a module
-      virtual void exposeParameters(ModuleParamList*, const std::string& = "") override
+      /// Expose the parameters of the wire hit preparation
+      virtual void exposeParameters(ModuleParamList* moduleParamList,
+                                    const std::string& prefix = "") override
       {
-        // Chose to not expose the parameters
+        moduleParamList->addParameter(prefixed(prefix, "mcFirstLoop"),
+                                      m_param_mcFirstLoop,
+                                      "Switch to delete hits that are not from "
+                                      "the first loop of the tracks before track finding starts. "
+                                      "This is useful for tuning the hit triplet filters "
+                                      "to the first earliest hits in the track and neglects "
+                                      "the more cumbersome later hits",
+                                      m_param_mcFirstLoop);
+      }
+
+      /// Signals the start of the event processing
+      virtual void initialize() override final
+      {
+        // Add the multi loop blocking to the signal chain if it is active.
+        if (m_param_mcFirstLoop) {
+          addProcessingSignalListener(&m_wireHitMCMultiLoopBlocker);
+        }
+        Super::initialize();
       }
 
       /// Generates the segment.
       virtual void apply(std::vector<CDCWireHit>& outputWireHits) override final
       {
         m_wireHitCreator.apply(outputWireHits);
+        if (m_param_mcFirstLoop) {
+          m_wireHitMCMultiLoopBlocker.apply(outputWireHits);
+        }
         m_wireHitTopologyFiller.apply(outputWireHits);
       }
 
@@ -60,8 +84,15 @@ namespace Belle2 {
       /// Creates the wire hits from CDCHits attaching geometry information.
       WireHitCreator m_wireHitCreator;
 
+      /// Creates the wire hits from CDCHits attaching geometry information.
+      WireHitMCMultiLoopBlocker m_wireHitMCMultiLoopBlocker;
+
       /// Publishes the created wire hits to the wire hit topology.
       WireHitTopologyFiller m_wireHitTopologyFiller;
+
+    private:
+      /// Parameter: Switch to delete hits that are not from the first loop of the tracks before track finding starts
+      bool m_param_mcFirstLoop = false;
 
     }; // end class WireHitTopologyPreparer
 

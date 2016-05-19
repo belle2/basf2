28d54c7953439f315c029835767ce79bb13faecb
Christian Pulvermacher <christian.pulvermacher@kit.edu>
2016-05-19 08:43:02 +0000

    make utils test more robust
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@28705 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/tests/utils.cc
+++ framework/tests/utils.cc
@@ -11,13 +11,14 @@ namespace {
   TEST(Utilities, MemSize)
   {
     //compare memory measurements done by root and ourselves
+    //(should differ by less than 1MB even though function calls are a bit different)
     ProcInfo_t meminfo;
     gSystem->GetProcInfo(&meminfo);
-    auto v = Utils::getVirtualMemoryKB();
-    ASSERT_EQ(v, meminfo.fMemVirtual);
+    long v = Utils::getVirtualMemoryKB();
+    ASSERT_TRUE(abs(v - meminfo.fMemVirtual) < 1000);
 
     gSystem->GetProcInfo(&meminfo);
-    auto r = Utils::getRssMemoryKB();
-    ASSERT_EQ(r, meminfo.fMemResident);
+    long r = Utils::getRssMemoryKB();
+    ASSERT_TRUE(abs(r - meminfo.fMemResident) < 1000);
   }
 }

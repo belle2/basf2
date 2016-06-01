3812cda4e834dec3ed211239abd403566ce5ef94
Peter Kvasnicka <kvasnicka@ipnp.troja.mff.cuni.cz>
2016-06-01 10:05:18 +0000

    Temporarily moved pxdUnpackerDQM module to pxdUnpacking to resolve inter-module dependency problem
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@28977 b38c6c8f-bee8-465d-8448-9abe6915d496

--- pxd/modules/pxdUnpacking/src/PXDUnpackerDQMModule.cc
+++ pxd/modules/pxdUnpacking/src/PXDUnpackerDQMModule.cc
@@ -8,7 +8,7 @@
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/
 
-#include <pxd/modules/pxdDQM/PXDUnpackerDQMModule.h>
+#include <pxd/modules/pxdUnpacking/PXDUnpackerDQMModule.h>
 
 #include "TDirectory.h"
 #include <TAxis.h>

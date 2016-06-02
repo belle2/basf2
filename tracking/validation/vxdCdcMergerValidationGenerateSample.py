6157507f3d7afa9034b1f950a5db2d3eb9b12be0
Nils Braun <nils.braun@kit.edu>
2016-06-02 11:01:35 +0000

    Had to adjust the cdc vxd merger validation script.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@29047 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/validation/vxdCdcMergerValidationGenerateSample.py
+++ tracking/validation/vxdCdcMergerValidationGenerateSample.py
@@ -37,25 +37,19 @@ def run():
 
     add_reconstruction(path, components=components, pruneTracks=False)
 
-    # convert to TrackCands so tracks can be matched
-    path.add_module("GenfitTrackCandidatesCreator", recoTracksStoreArrayName='VXDRecoTracks',
-                    genfitTrackCandsStoreArrayName='VXDGFTrackCands')
-    path.add_module("GenfitTrackCandidatesCreator", recoTracksStoreArrayName='CDCRecoTracks',
-                    genfitTrackCandsStoreArrayName='CDCGFTrackCands')
-
     # add additional matching for vxd and cdc only tracks here
 
     # for VXD only track
-    mctrackmatcher_vxd = basf2.register_module('MCTrackMatcher')
-    mctrackmatcher_vxd.param('MCGFTrackCandsColName', 'MCTrackCands')
-    mctrackmatcher_vxd.param('PRGFTrackCandsColName', 'VXDGFTrackCands')
+    mctrackmatcher_vxd = basf2.register_module('MCRecoTracksMatcher')
+    mctrackmatcher_vxd.param('mcRecoTracksStoreArrayName', 'MCRecoTracks')
+    mctrackmatcher_vxd.param('prRecoTracksStoreArrayName', 'VXDRecoTracks')
     mctrackmatcher_vxd.param('UseCDCHits', False)
     path.add_module(mctrackmatcher_vxd)
 
     # for CDC only tracks
-    mctrackmatcher_cdc = basf2.register_module('MCTrackMatcher')
-    mctrackmatcher_cdc.param('MCGFTrackCandsColName', 'MCTrackCands')
-    mctrackmatcher_cdc.param('PRGFTrackCandsColName', 'CDCGFTrackCands')
+    mctrackmatcher_cdc = basf2.register_module('MCRecoTracksMatcher')
+    mctrackmatcher_cdc.param('mcRecoTracksStoreArrayName', 'MCRecoTracks')
+    mctrackmatcher_cdc.param('prRecoTracksStoreArrayName', 'CDCRecoTracks')
     mctrackmatcher_cdc.param('UsePXDHits', False)
     mctrackmatcher_cdc.param('UseSVDHits', False)
     path.add_module(mctrackmatcher_cdc)
@@ -75,16 +69,16 @@ class VxdCdcMergerHarvester(HarvestingModule):
         HarvestingModule.__init__(self, foreach="MCParticles", output_file_name="../VxdCdcValidationHarvested.root")
 
         #: matcher used for the final MCTrack list
-        self.mc_track_matcher = Belle2.TrackMatchLookUp("MCTrackCands")
+        self.mc_track_matcher = Belle2.TrackMatchLookUp("MCRecoTracks")
         #: matcher used for the MCTracks from the CDC
-        self.mc_track_matcher_cdc = Belle2.TrackMatchLookUp("MCTrackCands", "CDCGFTrackCands")
+        self.mc_track_matcher_cdc = Belle2.TrackMatchLookUp("MCRecoTracks", "CDCGFTrackCands")
         #: matcher used for the MCTracks from the VXD
-        self.mc_track_matcher_vxd = Belle2.TrackMatchLookUp("MCTrackCands", "VXDGFTrackCands")
+        self.mc_track_matcher_vxd = Belle2.TrackMatchLookUp("MCRecoTracks", "VXDGFTrackCands")
 
     def pick(self, mc_particle):
-        # mc_track = mc_particle.getRelated("MCTrackCands")
+        # mc_track = mc_particle.getRelated("MCRecoTracks")
         # return mc_track is not None
-        mc_track = mc_particle.getRelatedFrom("MCTrackCands")
+        mc_track = mc_particle.getRelatedFrom("MCRecoTracks")
         if mc_track:
             return True
         else:
@@ -93,9 +87,9 @@ class VxdCdcMergerHarvester(HarvestingModule):
     def peel(self, mc_particle):
         # mc is a genfit::TrackCand
 
-        mc_track = mc_particle.getRelatedFrom("MCTrackCands")
+        mc_track = mc_particle.getRelatedFrom("MCRecoTracks")
 
-        mc_nhits = mc_track.getNHits()
+        mc_nhits = mc_track.getNumberOfTotalHits()
         mc_pt = mc_particle.getMomentum().Pt()
         mc_theta = mc_particle.getMomentum().Theta()
 
@@ -103,7 +97,7 @@ class VxdCdcMergerHarvester(HarvestingModule):
         reco_svd_hits = Belle2.PyStoreArray('SVDRecoHits')
         reco_cdc_hits = Belle2.PyStoreArray('CDCHits')
 
-        reco_tracks = Belle2.PyStoreArray('TrackCands')
+        reco_tracks = Belle2.PyStoreArray('RecoTracks')
 
         this_best_track_cdc = self.mc_track_matcher_cdc.getRelatedPRTrackCand(mc_track)
         this_best_track_vxd = self.mc_track_matcher_vxd.getRelatedPRTrackCand(mc_track)
@@ -126,12 +120,12 @@ class VxdCdcMergerHarvester(HarvestingModule):
 
         if this_best_track_merged:
             if this_best_track_cdc:
-                cdc_hits = this_best_track_cdc.getNHits()
+                cdc_hits = this_best_track_cdc.getNumberOfTotalHits()
             if this_best_track_vxd:
-                vxd_hits = this_best_track_vxd.getNHits()
+                vxd_hits = this_best_track_vxd.getNumberOfTotalHits()
 
             merged_hits = vxd_hits + cdc_hits
-            good_merge = merged_hits == this_best_track_merged.getNHits()
+            good_merge = merged_hits == this_best_track_merged.getNumberOfTotalHits()
 
         return {
             "MC_PT": mc_pt,

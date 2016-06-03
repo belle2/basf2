38a4cf8bac585a75fc935d7b706b9ef6c5132801
Oliver Frost <oliver.frost@desy.de>
2016-06-03 10:04:11 +0000

    Added missing docstrings in trackFindingCDC/testFixtures
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@29078 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/testFixtures/include/TrackFindingCDCTestWithTopology.h
+++ tracking/trackFindingCDC/testFixtures/include/TrackFindingCDCTestWithTopology.h
@@ -17,32 +17,41 @@
 
 namespace Belle2 {
   namespace TrackFindingCDC {
-    /** This class provides the declaration of the common test fixture to all
-       test of the track finding in the CDC code.
-
-       Reminder: You can filter for specific test by running
-
-       $ test_tracking --gtest_filter="TrackFindingCDCTest*"
-
-       which runs only for track finding in the CDC (positiv match).
-
-       Run
-
-       $ test_tracking --gtest_filter="-TrackFindingCDCTest*"
-
-       to exclude test with this fixture. Also consider
-       test_tracking --help for more details
-
-       Alternativelly it is now possible to run
-
-       $ test_tracking_trackFindingCDC
-
-       as a seperate executable.
-    */
+    /**
+     *  This class provides the declaration of the common test fixture to all
+     *  test of the track finding in the CDC code.
+     *
+     *  Reminder: You can filter for specific test by running
+     *
+     *  $ test_tracking --gtest_filter="TrackFindingCDCTest*"
+     *
+     *  which runs only for track finding in the CDC (positiv match).
+     *
+     *  Run
+     *
+     *  $ test_tracking --gtest_filter="-TrackFindingCDCTest*"
+     *
+     *  to exclude test with this fixture. Also consider
+     *  test_tracking --help for more details
+     *
+     *  Alternativelly it is now possible to run
+     *
+     *  $ test_tracking_trackFindingCDC
+     *
+     *  as a seperate executable.
+     */
     class TrackFindingCDCTestWithTopology : public TestHelpers::TestWithGearbox {
 
     public:
+      /**
+       *  Implementation of hook method to setup the environment for all tests in this test case
+       *  Unpacks the wire topology from the CDCGeometryPar.
+       */
       static void SetUpTestCase();
+
+      /**
+       *  Implementation of hook method to clean up the environment for all tests in this test case.
+       */
       static void TearDownTestCase();
 
     }; //end class

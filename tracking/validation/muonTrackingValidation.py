06c80ba5b649330163f23fbc533df07b108a5fc4
Thomas Hauth <thomas.hauth@kit.edu>
2016-06-10 13:06:32 +0000

    added tracking validation for large muon only sample
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@29275 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/validation/muonTrackingValidation.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+<header>
+  <contact>Thomas.Hauth@kit.edu</contact>
+  <input>MuonGenSimNoBkg.root</input>
+  <output>MuonTrackingValidation.root</output>
+  <description>This script validates the full track finding chain in easy 1 muon events.</description>
+</header>
+"""
+
+VALIDATION_OUTPUT_FILE = 'MuonTrackingValidation.root'
+N_EVENTS = 10000
+ACTIVE = True
+
+import basf2
+basf2.set_random_seed(1337)
+
+import logging
+import tracking
+
+from tracking.validation.run import TrackingValidationRun
+
+
+class Full(TrackingValidationRun):
+    n_events = N_EVENTS
+    root_input_file = '../MuonGenSimNoBkg.root'
+    finder_module = staticmethod(tracking.add_tracking_reconstruction)
+    tracking_coverage = {
+        'UsePXDHits': True,
+        'UseSVDHits': True,
+        'UseCDCHits': True,
+    }
+    fit_geometry = "default"
+    # tracks will be already fitted by
+    # add_tracking_reconstruction finder module set above
+    fit_tracks = False
+    use_expert_folder = False
+    pulls = True
+    resolution = True
+    output_file_name = VALIDATION_OUTPUT_FILE
+
+
+def main():
+    validation_run = Full()
+    validation_run.configure_and_execute_from_commandline()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    if ACTIVE:
+        main()
---
+++ validation/validation/MuonGenSimNoBkg.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+<header>
+  <output>MuonGenSimNoBkg.root</output>
+  <contact>tkuhr</contact>
+  <cacheable/>
+  <description>This steering file produces 10000 events with one Muon each
+  and runs the detector simulation without mixing in background.</description>
+</header>
+"""
+
+from basf2 import *
+from simulation import add_simulation
+from beamparameters import add_beamparameters
+import glob
+
+set_random_seed(12345)
+
+main = create_path()
+
+# specify number of events to be generated
+eventinfosetter = register_module('EventInfoSetter')
+eventinfosetter.param('evtNumList', [10000])
+eventinfosetter.param('runList', [1])
+eventinfosetter.param('expList', [1])
+main.add_module(eventinfosetter)
+
+# beam parameters
+beamparameters = add_beamparameters(main, "Y4S")
+
+# generate Muon events
+# Particle gun
+particlegun = register_module('ParticleGun')
+particlegun.param('nTracks', 1)
+particlegun.param('pdgCodes', [13, -13])
+particlegun.param('momentumGeneration', 'uniform')
+particlegun.param('momentumParams', [0.1, 4])
+main.add_module(particlegun)
+
+# detector simulation, no background files
+add_simulation(main)
+
+# memory profile
+main.add_module(register_module('Profile'))
+
+# output
+output = register_module('RootOutput')
+output.param('outputFileName', '../MuonGenSimNoBkg.root')
+main.add_module(output)
+
+process(main)
+
+# Print call statistics
+print(statistics)
+
+from validation import *
+statistics_plots('MuonGenSimNoBkg_statistics.root', contact='tkuhr',
+                 jobDesc='a standard simulation job with generic MuonGenSimNoBkg events', prefix='MuonGenSimNoBkg')
+event_timing_plot('../MuonGenSimNoBkg.root', 'MuonGenSimNoBkg_statistics.root',
+                  contact='tkuhr',
+                  jobDesc='a standard simulation job with Muon events', prefix='MuonGenSimNoBkg')

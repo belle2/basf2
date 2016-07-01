fd9bbb2a08e614fdbbc0d749ce9c0bf8dc5ee721
Oliver Frost <oliver.frost@desy.de>
2016-07-01 14:17:50 +0000

    Clean up the calculation of the covariance of CDCSZFitter.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@29705 b38c6c8f-bee8-465d-8448-9abe6915d496

--- tracking/trackFindingCDC/fitting/src/CDCSZFitter.cc
+++ tracking/trackFindingCDC/fitting/src/CDCSZFitter.cc
@@ -31,29 +31,25 @@ namespace {
   UncertainSZLine fitZ(const Matrix< double, 3, 3 >& sumMatrixWSZ)
   {
     // Solve the normal equation X * n = y
-
     Matrix< double, 2, 2> sumMatrixWS = sumMatrixWSZ.block<2, 2>(0, 0);
-    Matrix< double, 2, 2> invSumMatrixWS = sumMatrixWS.inverse();
-
     Matrix< double, 2, 1> sumVectorZOverWS = sumMatrixWSZ.block<2, 1>(0, 2);
-    Matrix< double, 2, 1> nZOverWS = invSumMatrixWS * sumVectorZOverWS;
-
-    double z0Intercept = nZOverWS(0);
-    double tanLambda = nZOverWS(1);
-
-    SZCovariance szCovariance;
+    Matrix< double, 2, 1> nZOverWS = sumMatrixWS.llt().solve(sumVectorZOverWS);
+    double chi2 = sumMatrixWSZ(2, 2) - nZOverWS.transpose() * sumVectorZOverWS;
 
     using namespace NSZParameterIndices;
-    szCovariance(c_TanL, c_TanL) = invSumMatrixWS(1, 1);
-    szCovariance(c_Z0, c_TanL) = invSumMatrixWS(0, 1); // Should be symmetric.
-    szCovariance(c_TanL, c_Z0) = invSumMatrixWS(1, 0); // Should be symmetric.
-    szCovariance(c_Z0, c_Z0) = invSumMatrixWS(0, 0);
+    SZParameters szParameters;
+    szParameters(c_TanL) = nZOverWS(1);
+    szParameters(c_Z0) = nZOverWS(0);
 
-    double chi2 = sumMatrixWSZ(2, 2) - z0Intercept * sumMatrixWSZ(0, 2) -  sumMatrixWSZ(1, 2) * tanLambda;
+    SZPrecision szPrecision;
+    szPrecision(c_TanL, c_TanL) = sumMatrixWS(1, 1);
+    szPrecision(c_Z0, c_TanL)   = sumMatrixWS(0, 1);
+    szPrecision(c_TanL, c_Z0)   = sumMatrixWS(1, 0);
+    szPrecision(c_Z0, c_Z0)     = sumMatrixWS(0, 0);
 
-    return UncertainSZLine(tanLambda, z0Intercept, szCovariance, chi2);
+    SZCovariance szCovariance = szPrecision.inverse();
+    return UncertainSZLine(szParameters, szCovariance, chi2);
   }
-
 
   // Declare function as currently unused to avoid compiler warning
   UncertainSZLine fitSZ(const Matrix< double, 3, 3 >& sumMatrixWSZ) __attribute__((__unused__));
@@ -71,8 +67,9 @@ namespace {
     // Covariance matrix
     Matrix< double, 3, 3> covMatrixWSZ = averageMatrixWSZ - meansWSZ * meansWSZ.transpose();
 
-
     Matrix< double, 2, 2> covMatrixSZ = covMatrixWSZ.block<2, 2>(1, 1);
+    Matrix< double, 2, 1> meansSZ     = meansWSZ.segment<2>(1);
+
     SelfAdjointEigenSolver< Matrix<double, 2, 2> > eigensolver(covMatrixSZ);
     if (eigensolver.info() != Success) {
       B2WARNING("SelfAdjointEigenSolver could not compute the eigen values of the observation matrix");
@@ -80,39 +77,33 @@ namespace {
 
     // the eigenvalues are generated in increasing order
     // we are interested in the lowest one since we want to compute the normal vector of the line here
-
+    Matrix<double, 2, 1> nSZ = eigensolver.eigenvectors().col(0);
+    B2INFO("nSZ " << nSZ);
+    nSZ /= std::copysign(nSZ.norm(), -nSZ(1)); // Making n2 negative to normalize to forward along s
     Matrix<double, 3, 1> nWSZ;
-    nWSZ.middleRows<2>(1) = eigensolver.eigenvectors().col(0);
-    nWSZ(0) = -meansWSZ.middleRows<2>(1).transpose() * nWSZ.middleRows<2>(1);
-
-    Line2D szLine(nWSZ(0), nWSZ(1), nWSZ(2));
-
-
-    Matrix< double, 3, 3> covN = covMatrixWSZ.inverse();
-    Matrix< double, 2, 3> jacobianNToSlopeIntercept;
-
-    jacobianNToSlopeIntercept(0, 0) = -1.0 / nWSZ(2);
-    jacobianNToSlopeIntercept(1, 1) = -1.0 / nWSZ(2);
-
-    jacobianNToSlopeIntercept(0, 2) = nWSZ(0) / nWSZ(2) / nWSZ(2);
-    jacobianNToSlopeIntercept(1, 2) = nWSZ(1) / nWSZ(2) / nWSZ(2);
-
-    Matrix<double, 2, 2 > covInterceptSlope = jacobianNToSlopeIntercept * covN * jacobianNToSlopeIntercept.transpose();
-
-    SZCovariance szCovariance;
-
-    using namespace NSZParameterIndices;
-    szCovariance(c_TanL, c_TanL) = covInterceptSlope(1, 1);
-    szCovariance(c_Z0, c_TanL) = covInterceptSlope(0, 1); // Should be symmetric.
-    szCovariance(c_TanL, c_Z0) = covInterceptSlope(1, 0); // Should be symmetric.
-    szCovariance(c_Z0, c_Z0) = covInterceptSlope(0, 0);
+    nWSZ << -meansSZ.transpose() * nSZ, nSZ;
+    B2INFO("nWSZ " << nWSZ);
 
     double chi2 = nWSZ.transpose() * sumMatrixWSZ * nWSZ;
 
-    return UncertainSZLine(szLine, szCovariance, chi2);
+    PrecisionMatrix<3>  precN = sumMatrixWSZ;
 
+    using namespace NSZParameterIndices;
+    SZParameters szParameters;
+    szParameters(c_TanL) = - nWSZ(1) / nWSZ(2);
+    szParameters(c_Z0)   = - nWSZ(0) / nWSZ(2);
+
+    Matrix<double, 3, 2> ambiguitySZToN = Matrix<double, 3, 2>::Zero();
+    ambiguitySZToN(0, c_Z0)   = -nWSZ(2);
+    ambiguitySZToN(0, c_TanL) = -nWSZ(1) * nWSZ(0);
+    ambiguitySZToN(1, c_TanL) = -nWSZ(2) * nWSZ(2) * nWSZ(2);
+    ambiguitySZToN(2, c_TanL) = nWSZ(1) * nWSZ(2) * nWSZ(2);
+
+    SZPrecision szPrecision = ambiguitySZToN.transpose() * precN * ambiguitySZToN;
+
+    SZCovariance szCovariance = szPrecision.inverse();
+    return UncertainSZLine(szParameters, szCovariance, chi2);
   }
-
 }
 
 CDCTrajectorySZ CDCSZFitter::fitUsingSimplifiedTheilSen(const CDCRecoSegment3D& segment3D) const
@@ -131,10 +122,11 @@ CDCTrajectorySZ CDCSZFitter::fitUsingSimplifiedTheilSen(const CDCRecoSegment3D& 
     for (unsigned int i = 0; i < observationsSZ.size(); i++) {
       for (unsigned int j = 0; j < observationsSZ.size(); j++) {
         if (i != j) {
-          observationsSZFiltered.fill(observationsSZ.getX(j), observationsSZ.getY(j), observationsSZ.getDriftLength(j),
+          observationsSZFiltered.fill(observationsSZ.getX(j),
+                                      observationsSZ.getY(j),
+                                      observationsSZ.getDriftLength(j),
                                       observationsSZ.getWeight(j));
         }
-
       }
 
       update(trajectorySZ, observationsSZFiltered);
@@ -142,11 +134,13 @@ CDCTrajectorySZ CDCSZFitter::fitUsingSimplifiedTheilSen(const CDCRecoSegment3D& 
       meanStartZ += trajectorySZ.getStartZ();
     }
 
-    return CDCTrajectorySZ(meanTanLambda / observationsSZ.size(), meanStartZ / observationsSZ.size());
+    return CDCTrajectorySZ(meanTanLambda / observationsSZ.size(),
+                           meanStartZ / observationsSZ.size());
   } else {
     return CDCTrajectorySZ::basicAssumption();
   }
 }
+
 
 void CDCSZFitter::update(const CDCSegmentPair& segmentPair) const
 {
@@ -165,8 +159,6 @@ void CDCSZFitter::update(const CDCSegmentPair& segmentPair) const
   CDCTrajectory3D trajectory3D(axialTrajectory2D, trajectorySZ);
   segmentPair.setTrajectory3D(trajectory3D);
 }
-
-
 
 
 void CDCSZFitter::update(CDCTrajectorySZ& trajectorySZ,
@@ -192,17 +184,13 @@ void CDCSZFitter::update(CDCTrajectorySZ& trajectorySZ,
     return;
   }
 
+  // Determine NDF : Line fit eats up 2 degrees of freedom.
+  size_t ndf = observations2D.size() - 2;
+
   // Matrix of weighted sums
   Matrix< double, 3, 3 > sumMatrixWSZ = observations2D.getWXYSumMatrix();
   UncertainSZLine uncertainSZLine = fitZ(sumMatrixWSZ);
 
-  // Determine NDF : Line fit eats up 2 degrees of freedom.
-  size_t ndf = observations2D.size() - 2;
   uncertainSZLine.setNDF(ndf);
-
   trajectorySZ.setSZLine(uncertainSZLine);
-
-
-
-
 }

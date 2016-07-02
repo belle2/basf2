9b283a7009928bba1f6061ef3b7266fd9cf2205b
Umberto Tamponi <tamponi@to.infn.it>
2016-07-02 13:55:00 +0000

    Module completely rewritten. The useless loops have been removed, and the main loop for the calpulse search has been simplified. The module can now process both positive and negative pulses, with different thresholds. The hard coded parameters have been removed, and no check is done on the time distance bewteen two pulses anymore. The documentation part has been updated accordin to the doxigen standards.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@29725 b38c6c8f-bee8-465d-8448-9abe6915d496

--- topcaf/modules/DoubleCalPulseModule/src/DoubleCalPulseModule.cc
+++ topcaf/modules/DoubleCalPulseModule/src/DoubleCalPulseModule.cc
@@ -1,3 +1,15 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Umberto Tamponi  (tamponi@to.infn.it)                    *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+
+
 #include <topcaf/modules/DoubleCalPulseModule/DoubleCalPulseModule.h>
 #include <topcaf/dataobjects/EventWaveformPacket.h>
 #include <topcaf/dataobjects/EventHeaderPacket.h>
@@ -21,23 +33,18 @@ DoubleCalPulseModule::DoubleCalPulseModule() : Module()
 {
   setDescription("This module adjusts the timing of itop hits by searhing for a double calibration pulse in channel 7 of each asic.");
 
-  addParam("calibrationChannel", m_cal_ch, "Channel in each ASIC that holds the calibration pulse", 7);
-  addParam("calibrationTimeMin", m_tmin, "Earliest time to look for a calibration pulse", 0.);
-  addParam("calibrationTimeMax", m_tmax, "Latest time to look for a calibration pulse", 2000.);
-  addParam("calibrationWidthMin", m_wmin, "Minimum width of a calibration pulse", 1.3);
-  addParam("calibrationWidthMax", m_wmax, "Maximum width of a calibration pulse", 3.0);
-  addParam("calibrationADCThreshold", m_adcmin, "Min ADC count threshold for calibration pulse", 400.);
-  addParam("calibrationADCThreshold_max", m_adcmax, "Max ADC count threshold for calibration pulse", 1000.);
+  addParam("calibrationChannel", m_calCh, "Channel in each ASIC that holds the calibration pulse", 7);
+  addParam("calibrationWidthMin", m_wMin, "Minimum width of a calibration pulse", 12.);
+  addParam("calibrationWidthMax", m_wMax, "Maximum width of a calibration pulse", 30.);
+  addParam("calibrationADCThresholdMin", m_adcMin, "Min ADC count threshold for a positive calibration pulse", 400.);
+  addParam("calibrationADCThresholdMax", m_adcMax, "Max ADC count threshold for a positive calibration pulse", 1000.);
+  addParam("calibrationADCThresholdMinNeg", m_adcMinNeg, "Min ADC count threshold for a negative calibration pulse", -1000.);
+  addParam("calibrationADCThresholdMaxNeg", m_adcMaxNeg, "Max ADC count threshold for a negative calibration pulse", -400.);
 
 }
 
 
 DoubleCalPulseModule::~DoubleCalPulseModule() {}
-
-//void DoubleCalPulseModule::defineHisto()
-//{
-//  m_occupancy = new TH2F("WaveFormOccupancy", "WaveFormOccupancy", 64, 1, 65, 8, 1, 9);
-//}
 
 void DoubleCalPulseModule::initialize()
 {
@@ -50,145 +57,80 @@ void DoubleCalPulseModule::beginRun()
 }
 
 
-void DoubleCalPulseModule::event()
+void  DoubleCalPulseModule::event()
 {
-
-  //Get Waveform from datastore
+  //Get hits from datastore
   StoreArray<TOPCAFDigit> digit_ptr;
   digit_ptr.isRequired();
 
   StoreObjPtr<EventHeaderPacket> evtheader_ptr;
   evtheader_ptr.isRequired();
 
-  map<topcaf_channel_id_t, double> asic_ref_time;
+  double asic_ref_time[1000];
+  int asic_ref_calpulsenum[1000];
 
-  map<topcaf_channel_id_t, double> asic_ref_flag;
+  for (int k = 0; k < 1000; k++)
+    asic_ref_calpulsenum[k] = 0;
+
 
   if (digit_ptr) {
-    //Look over all waveforms for calibration times.
-
+    cout << "new event" << endl;
+    // first loop to look for the pulse candidates
     for (int w = 0; w < digit_ptr.getEntries(); w++) {
-      topcaf_channel_id_t hardwareID = digit_ptr[w]->GetChannelID();
-      topcaf_channel_id_t asicKey = (hardwareID / 1000000);
-      asicKey *= 1000000;
-      int asic = digit_ptr[w]->GetASIC();
-      asicKey += asic * 10000;
-      int asic_ch = digit_ptr[w]->GetASICChannel();
 
-      //Special case for Tsukuba negative cal pulse when threshold is set as negative
-      if ((m_adcmin < 0) && (digit_ptr[w]->GetTDCBin() > m_tmin) && (digit_ptr[w]->GetTDCBin() < m_tmax)
-          && (digit_ptr[w]->GetWidth() > m_wmin)
-          && (digit_ptr[w]->GetWidth() < m_wmax) && (digit_ptr[w]->GetADCHeight() < m_adcmin) && (digit_ptr[w]->GetADCHeight() > m_adcmax)) {
-        if ((asic_ch == m_cal_ch) && ((digit_ptr[w]->GetTDCBin() < asic_ref_time[asicKey]) || (asic_ref_time[asicKey] == 0))) {
-          asic_ref_time[asicKey] = digit_ptr[w]->GetTDCBin();
-          digit_ptr[w]->SetFlag(10);
+      int asicKey = 100 * digit_ptr[w]->GetBoardstack() + 10 * digit_ptr[w]->GetCarrier() + digit_ptr[w]->GetASIC();
+      int asic_ch = digit_ptr[w]->GetASICChannel();
+      digit_ptr[w]->SetFlag(0); // unprocessed hit has flag = 0
+      //First applies the width selection and the channel ID requirement
+      if (digit_ptr[w]->GetWidth() > m_wMin && digit_ptr[w]->GetWidth() < m_wMax && asic_ch == m_calCh) {
+        // we don't distinguish bewteen positive and negative candidates, but we use different thresholds
+        if ((digit_ptr[w]->GetADCHeight() > m_adcMin && digit_ptr[w]->GetADCHeight() < m_adcMax)
+            || (digit_ptr[w]->GetADCHeight() > m_adcMinNeg && digit_ptr[w]->GetADCHeight() < m_adcMaxNeg)) {
+          // if this is the first calpulse candidate, then use it as reference
+          if (asic_ref_calpulsenum[asicKey] == 0)
+            asic_ref_time[asicKey] = digit_ptr[w]->GetTDCBin();
+
+          asic_ref_calpulsenum[asicKey]++;
+
+          digit_ptr[w]->SetFlag(100 + asic_ref_calpulsenum[asicKey]);
         }
-        //      B2INFO(hardwareID<<"\ttdc: "<<digit_ptr[w]->GetTDCBin()<<"\tasic_ref_time: "<<asic_ref_time[asicKey]);
-      }
-      //Special case for Tsukuba negative cal pulse when threshold is set as negative
-      if ((m_adcmin < 0) && (digit_ptr[w]->GetTDCBin() > m_tmin) && (digit_ptr[w]->GetTDCBin() < m_tmax)
-          && (digit_ptr[w]->GetWidth() > m_wmin)
-          && (digit_ptr[w]->GetWidth() < m_wmax) && (digit_ptr[w]->GetADCHeight() < m_adcmin) && (digit_ptr[w]->GetADCHeight() > m_adcmax)) {
-        if ((asic_ch == m_cal_ch) && ((digit_ptr[w]->GetTDCBin() < asic_ref_time[asicKey]) || (asic_ref_time[asicKey] == 0))) {
-          asic_ref_time[asicKey] = digit_ptr[w]->GetTDCBin();
-          digit_ptr[w]->SetFlag(10);
-        }
-        //      B2INFO(hardwareID<<"\ttdc: "<<digit_ptr[w]->GetTDCBin()<<"\tasic_ref_time: "<<asic_ref_time[asicKey]);
-      }
-      if ((digit_ptr[w]->GetTDCBin() > m_tmin) && (digit_ptr[w]->GetTDCBin() < m_tmax) && (digit_ptr[w]->GetWidth() > m_wmin)
-          && (digit_ptr[w]->GetWidth() < m_wmax) && (digit_ptr[w]->GetADCHeight() > m_adcmin) && (digit_ptr[w]->GetADCHeight() < m_adcmax)) {
-        if ((asic_ch == m_cal_ch) && ((digit_ptr[w]->GetTDCBin() < asic_ref_time[asicKey]) || (asic_ref_time[asicKey] == 0))) {
-          asic_ref_time[asicKey] = digit_ptr[w]->GetTDCBin();
-          digit_ptr[w]->SetFlag(10);
-        }
-        //      B2INFO(hardwareID<<"\ttdc: "<<digit_ptr[w]->GetTDCBin()<<"\tasic_ref_time: "<<asic_ref_time[asicKey]);
       }
     }
-
 
 
     //Loop again to apply calibration times
     for (int w = 0; w < digit_ptr.getEntries(); w++) {
-      topcaf_channel_id_t hardwareID = digit_ptr[w]->GetChannelID();
-      topcaf_channel_id_t asicKey = (hardwareID / 1000000);
-      asicKey *= 1000000;
-      int asic = digit_ptr[w]->GetASIC();
-      asicKey += asic * 10000;
-      int asic_ch = digit_ptr[w]->GetASICChannel();
 
-      double corr_time = digit_ptr[w]->GetTDCBin() - asic_ref_time[asicKey];
-      digit_ptr[w]->SetTime(corr_time);
-      digit_ptr[w]->SetQuality(asic_ref_time[asicKey]);
-      if (asic_ref_time[asicKey] > 0) {
-        if (digit_ptr[w]->GetFlag() != 10) digit_ptr[w]->SetFlag(1); // i.e. calibrated to a pulse.
-        if ((corr_time > 45.) && (corr_time < 70.)
-            && (asic_ch == m_cal_ch)) { // then mark this as the second calibration pulse. hard coded numbers again...
-          digit_ptr[w]->SetFlag(11); // second calibration pulse.
-          if (asic_ref_flag[asicKey] > 9) {
-            asic_ref_flag[asicKey] = 11;
-          } else {
-            asic_ref_flag[asicKey] = 1;
-          }
-        }
-        if (((corr_time < -4. && corr_time > -125.) || (corr_time > 4. && corr_time < 15.))
-            && (asic_ch == m_cal_ch)) {  // then mark this as a bad cal pulse hit
-          digit_ptr[w]->SetFlag(20); // bad cal hit
-          if (asic_ref_flag[asicKey] < 10) {
-            asic_ref_flag[asicKey] = asic_ref_flag[asicKey] + 10;
-          }
-        }
+      int asicKey = 100 * digit_ptr[w]->GetBoardstack() + 10 * digit_ptr[w]->GetCarrier() + digit_ptr[w]->GetASIC();
+
+      // no calibration pulse has been found!
+      if (asic_ref_calpulsenum[asicKey] == 0) {
+        digit_ptr[w]->SetTime(0);
+        digit_ptr[w]->SetQuality(0);
+        digit_ptr[w]->SetFlag(-1000 - digit_ptr[w]->GetFlag()); // not calibrated hit
       }
-      //      B2INFO(hardwareID<<"\ttdc: "<<digit_ptr[w]->GetTDCBin()<<"\tasicKey: "<<asicKey<<"\tasic_ref_time: "<<asic_ref_time[asicKey]<<"\tcorr_time: "<<corr_time);
-
-    }
-    //Flag to confirm other pulses found
-    for (int w = 0; w < digit_ptr.getEntries(); w++) {
-      topcaf_channel_id_t hardwareID = digit_ptr[w]->GetChannelID();
-      topcaf_channel_id_t asicKey = (hardwareID / 1000000);
-      asicKey *= 1000000;
-      int asic = digit_ptr[w]->GetASIC();
-      asicKey += asic * 10000;
-
-
-      //int asic_ch = digit_ptr[w]->GetASICChannel();
-
-      if (asic_ref_flag[asicKey] == 1) {
-        //double pulse confirmed
-        digit_ptr[w]->SetFlag(digit_ptr[w]->GetFlag() + 100);
+      // only one calpulse candidate found
+      if (asic_ref_calpulsenum[asicKey] == 1) {
+        digit_ptr[w]->SetTime(digit_ptr[w]->GetTDCBin() - asic_ref_time[asicKey]);
+        digit_ptr[w]->SetQuality(asic_ref_time[asicKey]);
+        digit_ptr[w]->SetFlag(1000 + digit_ptr[w]->GetFlag()); // possibly problematic hit
       }
-      if (asic_ref_flag[asicKey] == 10) {
-        //bad ch7 digit confirmed
-        digit_ptr[w]->SetFlag(digit_ptr[w]->GetFlag() + 1000);
+      // two calpulse found. Should be the correct situation
+      if (asic_ref_calpulsenum[asicKey] == 2) {
+        digit_ptr[w]->SetTime(digit_ptr[w]->GetTDCBin() - asic_ref_time[asicKey]);
+        digit_ptr[w]->SetQuality(asic_ref_time[asicKey]);
+        digit_ptr[w]->SetFlag(2000 + digit_ptr[w]->GetFlag()); // good hit
       }
-      if (asic_ref_flag[asicKey] == 10) {
-        //both
-        digit_ptr[w]->SetFlag(digit_ptr[w]->GetFlag() + 1100);
+      // more than two calpulse candidates found...
+      if (asic_ref_calpulsenum[asicKey] > 2) {
+        digit_ptr[w]->SetTime(digit_ptr[w]->GetTDCBin() - asic_ref_time[asicKey]);
+        digit_ptr[w]->SetQuality(asic_ref_time[asicKey]);
+        digit_ptr[w]->SetFlag(3000 + digit_ptr[w]->GetFlag()); // possibly problematic hit
       }
     }
-    //Flag to confirm other pulses found
-    for (int w = 0; w < digit_ptr.getEntries(); w++) {
-      topcaf_channel_id_t hardwareID = digit_ptr[w]->GetChannelID();
-      topcaf_channel_id_t asicKey = (hardwareID / 1000000);
-      asicKey *= 1000000;
-      int asic = digit_ptr[w]->GetASIC();
-      asicKey += asic * 10000;
-      //int asic_ch = digit_ptr[w]->GetASICChannel();
-
-      if (asic_ref_flag[asicKey] == 1) {
-        //double pulse confirmed
-        digit_ptr[w]->SetFlag(digit_ptr[w]->GetFlag() + 100);
-      }
-      if (asic_ref_flag[asicKey] == 10) {
-        //bad ch7 digit confirmed
-        digit_ptr[w]->SetFlag(digit_ptr[w]->GetFlag() + 1000);
-      }
-      if (asic_ref_flag[asicKey] == 10) {
-        //both
-        digit_ptr[w]->SetFlag(digit_ptr[w]->GetFlag() + 1100);
-      }
-    }
-  }//End loop
+  }
 }
+
 
 
 void DoubleCalPulseModule::terminate()

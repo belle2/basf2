ff47c8ebae584a3cdb813b14b58958271e6c75d0
Oliver Frost <oliver.frost@desy.de>
2016-07-04 11:06:42 +0000

    Implemented findlet to adjust the flight time to the start point of tracks.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@29747 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/trackFindingCDC/findlets/minimal/include/TrackFlightTimeAdjuster.h
@@ -0,0 +1,60 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+
+#include <tracking/trackFindingCDC/findlets/base/Findlet.h>
+
+#include <tracking/trackFindingCDC/geometry/Vector3D.h>
+#include <vector>
+
+namespace Belle2 {
+  class ModuleParamList;
+
+  namespace TrackFindingCDC {
+
+    class CDCTrack;
+    /**
+     *  Findlet to adjust the flight time of tracks relative to a trigger point.
+     *
+     *  The flight time of tracks is only adjusted in the start trajectories of each given track
+     *  and the time which is need to reach the trigger point with the speed of flight
+     *  on the curved trajectory is set as the flight time.
+     */
+    class TrackFlightTimeAdjuster:
+      public Findlet<CDCTrack&> {
+
+    private:
+      /// Type of the base class
+      typedef Findlet<CDCTrack> Super;
+
+    public:
+      /// Short description of the findlet
+      virtual std::string getDescription() override;
+
+      /** Add the parameters of the filter to the module */
+      void exposeParameters(ModuleParamList* moduleParamList, const std::string& prefix = "") override final;
+
+      /// Initialize at the start of the event processing
+      virtual void initialize() override;
+
+      /// Write give tracks into track store array
+      virtual void apply(std::vector<CDCTrack>& tracks) override final;
+
+    private:
+      /// Parameter: Point relative to which the flight time should be estimated.
+      std::tuple<double, double, double> m_param_triggerPoint{0.0, 0.0, 0.0};
+
+      /// Point relative to which the flight time should be estimated.
+      Vector3D m_triggerPoint{0.0, 0.0, 0.0};
+
+    }; // end class
+  } // end namespace TrackFindingCDC
+} // end namespace Belle2
---
+++ tracking/trackFindingCDC/findlets/minimal/src/TrackFlightTimeAdjuster.cc
@@ -0,0 +1,53 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#include <tracking/trackFindingCDC/findlets/minimal/TrackFlightTimeAdjuster.h>
+
+#include <tracking/trackFindingCDC/eventdata/tracks/CDCTrack.h>
+#include <tracking/trackFindingCDC/utilities/StringManipulation.h>
+
+#include <framework/gearbox/Const.h>
+
+#include <vector>
+
+using namespace Belle2;
+using namespace TrackFindingCDC;
+
+std::string TrackFlightTimeAdjuster::getDescription()
+{
+  return "Adjusts the flight time of tracks relative to a trigger point";
+}
+
+void TrackFlightTimeAdjuster::exposeParameters(ModuleParamList* moduleParamList, const std::string& prefix)
+{
+  moduleParamList->addParameter(prefixed(prefix, "triggerPoint"),
+                                m_param_triggerPoint,
+                                "Point relative to which the flight times of tracks should be adjusted",
+                                m_param_triggerPoint);
+}
+
+void TrackFlightTimeAdjuster::initialize()
+{
+  m_triggerPoint = Vector3D(std::get<0>(m_param_triggerPoint),
+                            std::get<1>(m_param_triggerPoint),
+                            std::get<2>(m_param_triggerPoint));
+}
+
+void TrackFlightTimeAdjuster::apply(std::vector<CDCTrack>& tracks)
+{
+  for (CDCTrack& track : tracks) {
+    CDCTrajectory3D startTrajectory3D = track.getStartTrajectory3D();
+    double arcLength2D = startTrajectory3D.calcArcLength2D(m_triggerPoint);
+    double arcLength3D = arcLength2D * std::hypot(1, startTrajectory3D.getTanLambda());
+    double flightTime = arcLength3D / Const::speedOfLight;
+    // Insert negative drift time such that the time at the trigger point is 0.
+    startTrajectory3D.setFlightTime(-flightTime);
+    track.setStartTrajectory3D(startTrajectory3D);
+  }
+}

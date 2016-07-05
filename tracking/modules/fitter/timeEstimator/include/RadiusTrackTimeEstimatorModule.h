3932f6871008212128428cc46da5470187f56d07
Nils Braun <nils.braun@kit.edu>
2016-07-05 07:14:07 +0000

    Implemented a Radius track time seed estimator.
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@29766 b38c6c8f-bee8-465d-8448-9abe6915d496

---
+++ tracking/modules/fitter/timeEstimator/include/RadiusTrackTimeEstimatorModule.h
@@ -0,0 +1,35 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Nils Braun                                               *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+#include <tracking/modules/fitter/timeEstimator/BaseTrackTimeEstimatorModule.h>
+#include <TVector3.h>
+
+namespace Belle2 {
+
+  /** Module estimating the track time of RecoTracks - before or after the fit.
+   * It extrapolates the helix/state to the radius and corrects for the traveled time.  */
+  class RadiusTrackTimeEstimatorModule : public BaseTrackTimeEstimatorModule {
+
+  public:
+    /** Initialize the module parameters. */
+    RadiusTrackTimeEstimatorModule();
+
+  private:
+    /// Radius used for extrapolation. Please be aware that if the RecoTrack does not reach this radius, the results are wrong.
+    double m_param_radiusForExtrapolation = 0;
+
+    /// Extrapolate the measured state on plane to the given radius.
+    double estimateFlightLengthUsingFittedInformation(genfit::MeasuredStateOnPlane& measuredStateOnPlane) const;
+
+    /// Use some helix functionality to calculate the distance to given the radius.
+    double estimateFlightLengthUsingSeedInformation(const RecoTrack& recoTrack) const;
+  };
+}
---
+++ tracking/modules/fitter/timeEstimator/src/RadiusTrackTimeEstimatorModule.cc
@@ -0,0 +1,52 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                           *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Nils Braun                                               *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#include <tracking/modules/fitter/timeEstimator/RadiusTrackTimeEstimatorModule.h>
+
+#include <tracking/dataobjects/RecoTrack.h>
+#include <framework/dataobjects/Helix.h>
+#include <geometry/bfieldmap/BFieldMap.h>
+
+using namespace std;
+using namespace Belle2;
+
+REG_MODULE(RadiusTrackTimeEstimator)
+
+RadiusTrackTimeEstimatorModule::RadiusTrackTimeEstimatorModule() : BaseTrackTimeEstimatorModule()
+{
+  addParam("radiusForExtrapolation", m_param_radiusForExtrapolation,
+           "Radius used for extrapolation. Please be aware that if the RecoTrack does not reach this radius, "
+           "the results are wrong.", m_param_radiusForExtrapolation);
+}
+
+double RadiusTrackTimeEstimatorModule::estimateFlightLengthUsingFittedInformation(genfit::MeasuredStateOnPlane&
+    measuredStateOnPlane)
+const
+{
+  try {
+    const double s = measuredStateOnPlane.extrapolateToCylinder(m_param_radiusForExtrapolation);
+    return s;
+  } catch (const genfit::Exception& e) {
+    B2WARNING("Extrapolation failed: " << e.what());
+    return 0;
+  }
+}
+
+double RadiusTrackTimeEstimatorModule::estimateFlightLengthUsingSeedInformation(const RecoTrack& recoTrack) const
+{
+  const TVector3& momentum = recoTrack.getMomentumSeed();
+  const short int charge = recoTrack.getChargeSeed();
+  const TVector3& position = recoTrack.getPositionSeed();
+
+  const double bZ = BFieldMap::Instance().getBField(TVector3(0, 0, 0)).Z();
+  const Helix h(position, momentum, charge, bZ);
+  const double arcLengthOfIntersection = h.getArcLength2DAtCylindricalR(m_param_radiusForExtrapolation);
+  const double s = arcLengthOfIntersection * hypot(1, h.getTanLambda());
+  return s;
+}

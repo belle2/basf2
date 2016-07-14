f9569b830527d4f2ba3fde74e2fc201ab5bdbc3e
Nils Braun <nils.braun@kit.edu>
2016-07-14 12:54:06 +0000

    Ups, accidentily removed the header.
    Conflicts:
    	framework/utilities/include/GeneralCut.h
    
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/software@29926 b38c6c8f-bee8-465d-8448-9abe6915d496

--- framework/utilities/include/GeneralCut.h
+++ framework/utilities/include/GeneralCut.h
@@ -1,3 +1,15 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Thomas Keck, Nils Braun                                  *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+
 #include <analysis/VariableManager/Manager.h>
 #include <framework/logging/Logger.h>
 #include <framework/utilities/Conversion.h>

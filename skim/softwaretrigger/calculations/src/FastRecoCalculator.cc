02e814a59ec760d924c12a98873e5817840ff901
Nils Braun <nils.braun@kit.edu>
2016-08-16 16:42:18 +0200

    Added also the rest of the calculations. Will have to clean up some of them after the study is finished.

--- skim/softwaretrigger/calculations/src/FastRecoCalculator.cc
+++ skim/softwaretrigger/calculations/src/FastRecoCalculator.cc
@@ -64,17 +64,28 @@ namespace Belle2 {
 
     void FastRecoCalculator::doCalculation(SoftwareTriggerObject& calculationResult) const
     {
+      // Prepare some cache objects.
       std::vector<TVector3> momenta;
-      momenta.reserve(m_cdcRecoTracks.getEntries());
+      momenta.reserve(static_cast<unsigned long>(m_cdcRecoTracks.getEntries()));
 
       for (const RecoTrack& cdcRecoTrack : m_cdcRecoTracks) {
         momenta.push_back(cdcRecoTrack.getMomentumSeed());
       }
 
+      std::vector<double> sortedECLEnergies;
+      sortedECLEnergies.reserve(static_cast<unsigned long>(m_eclClusters.getEntries()));
+
+      for (const ECLCluster& eclCluster : m_eclClusters) {
+        sortedECLEnergies.push_back(eclCluster.getEnergy());
+      }
+
+      std::sort(sortedECLEnergies.begin(), sortedECLEnergies.end(), std::greater<double>());
+
       // TODO: Do only return the first (we do not need more).
       const std::vector<double>& sortedRhoECLEnergyList = getSortedEnergiesFrom(m_eclClusters, m_transformer);
       const std::vector<double>& sortedRhoCDCEnergyList = getSortedEnergiesFrom(m_cdcRecoTracks, m_transformer);
 
+      // Calculate the visible energy
       double visibleEnergy = 0;
       if (not momenta.empty()) {
         visibleEnergy = std::accumulate(momenta.begin(), momenta.end(), visibleEnergy,
@@ -91,25 +102,62 @@ namespace Belle2 {
 
       calculationResult["visible_energy"] = visibleEnergy;
 
-      if (sortedRhoECLEnergyList.empty()) {
-        calculationResult["highest_1_ecl"] = 0;
-        calculationResult["energy_sum_of_high_energy_ecl"] = 0;
-      } else {
-        calculationResult["highest_1_ecl"] = sortedRhoECLEnergyList.front();
-        calculationResult["energy_sum_of_high_energy_ecl"] = std::accumulate(std::begin(m_eclClusters), std::end(m_eclClusters), 0,
-        [](const double & value, const ECLCluster & eclCluster) {
-          const double& energy = eclCluster.getEnergy();
-          if (energy > 0.05) {
-            return value + energy;
-          } else {
-            return value;
-          }
-        });
-      }
+      calculationResult["energy_sum_of_high_energy_ecl"] =
+        std::accumulate(std::begin(m_eclClusters), std::end(m_eclClusters), 0,
+      [](const double & value, const ECLCluster & eclCluster) {
+        const double& energy = eclCluster.getEnergy();
+        if (energy > 0.05) {
+          return value + energy;
+        } else {
+          return value;
+        }
+      });
+      calculationResult["energy_sum_of_ecl"] =
+        std::accumulate(std::begin(m_eclClusters), std::end(m_eclClusters), 0,
+      [](const double & value, const ECLCluster & eclCluster) {
+        return value + eclCluster.getEnergy();
+      });
 
+      calculationResult["number_of_high_energy_ecl_clusters"] =
+        std::count_if(std::begin(m_eclClusters), std::end(m_eclClusters),
+      [](const ECLCluster & eclCluster) {
+        return eclCluster.getEnergy() > 0.05;
+      });
 
       calculationResult["number_of_cdc_tracks"] = momenta.size();
       calculationResult["number_of_ecl_cluster"] = m_eclClusters.getEntries();
+
+      calculationResult["highest_1_ecl"] = 0;
+      calculationResult["highest_2_ecl"] = 0;
+      calculationResult["highest_3_ecl"] = 0;
+
+      if (sortedECLEnergies.size() > 0) {
+        calculationResult["highest_1_ecl"] = sortedECLEnergies[0];
+
+        if (sortedECLEnergies.size() > 1) {
+          calculationResult["highest_2_ecl"] = sortedECLEnergies[0] + sortedECLEnergies[1];
+
+          if (sortedECLEnergies.size() > 2) {
+            calculationResult["highest_3_ecl"] = sortedECLEnergies[0] + sortedECLEnergies[1] + sortedECLEnergies[2];
+          }
+        }
+      }
+
+      calculationResult["highest_1_ecl_rho"] = 0;
+      calculationResult["highest_2_ecl_rho"] = 0;
+      calculationResult["highest_3_ecl_rho"] = 0;
+
+      if (sortedRhoECLEnergyList.size() > 0) {
+        calculationResult["highest_1_ecl_rho"] = sortedRhoECLEnergyList[0];
+
+        if (sortedRhoECLEnergyList.size() > 1) {
+          calculationResult["highest_2_ecl_rho"] = sortedRhoECLEnergyList[0] + sortedRhoECLEnergyList[1];
+
+          if (sortedRhoECLEnergyList.size() > 2) {
+            calculationResult["highest_3_ecl_rho"] = sortedRhoECLEnergyList[0] + sortedRhoECLEnergyList[1] + sortedRhoECLEnergyList[2];
+          }
+        }
+      }
 
       if (momenta.empty()) {
         calculationResult["max_pt"] = 0;
@@ -138,10 +186,21 @@ namespace Belle2 {
         }) / momenta.size();
       }
 
-      if (sortedRhoCDCEnergyList.empty()) {
-        calculationResult["first_highest_cdc_energies"] = 0;
-      } else {
-        calculationResult["first_highest_cdc_energies"] = sortedRhoCDCEnergyList.front();
+      calculationResult["highest_1_cdc_rho"] = 0;
+      calculationResult["highest_2_cdc_rho"] = 0;
+      calculationResult["highest_3_cdc_rho"] = 0;
+
+      if (sortedRhoCDCEnergyList.size() > 0) {
+        calculationResult["highest_1_cdc_rho"] = sortedRhoCDCEnergyList[0];
+
+        if (sortedRhoCDCEnergyList.size() > 1) {
+          calculationResult["highest_2_cdc_rho"] = sortedRhoCDCEnergyList[0] + sortedRhoCDCEnergyList[1];
+
+          if (sortedRhoCDCEnergyList.size() > 2) {
+            calculationResult["highest_3_cdc_rho"] = sortedRhoCDCEnergyList[0] + sortedRhoCDCEnergyList[1] +
+                                                     sortedRhoCDCEnergyList[2];
+          }
+        }
       }
     }
   }

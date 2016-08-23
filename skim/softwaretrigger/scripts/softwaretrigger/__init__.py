34268e5020a6e9c587ac3206bffc144578a128f0
Nils Braun <nils.braun@kit.edu>
2016-08-23 10:23:14 +0200

    Started implementing a default steering file for software trigger usage.

---
+++ skim/softwaretrigger/scripts/softwaretrigger/__init__.py
@@ -0,0 +1,75 @@
+from ROOT import Belle2
+
+import basf2
+import reconstruction
+
+
+def add_unpackers(path):
+    # Add unpackers
+    path.add_module("SVDUnpacker", shutUpFTBError=0)
+    path.add_module("CDCUnpacker",
+                    xmlMapFileName=Belle2.FileSystem.findFile("data/cdc/ch_map.dat"),
+                    enablePrintOut=False)
+    path.add_module("TOPUnpacker")
+    path.add_module("ARICHUnpacker")
+    path.add_module("BKLMUnpacker")
+
+    # TODO: ECLUnpacker, EKLMUnpacker
+
+    # add clusterizer
+    path.add_module("SVDClusterizer")
+
+
+def add_softwaretrigger_reconstruction(path):
+    # Add fast reco reconstruction
+    reconstruction.add_reconstruction(path, trigger_mode="fast_reco")
+
+    # In the following, we will need two paths:
+    # (1) A "store-metadata" path (only storing the trigger tags and some metadata)
+    store_metadata_path = get_store_metadata_path()
+    # (2) A "store-all" path (store the raw data with the metadata), that leads back to the store-metadata path
+    store_rawdata_path = get_store_rawdata_path(store_metadata_path)
+
+    # Add fast reco cuts
+    fast_reco_cut_module = path.add_module("SoftwareTrigger", baseIdentifier="fast_reco",
+                                           cutIdentifiers=["reject_ee", "accept_ee", "reject_bkg"])
+
+    # There are three possibilities for the output of this module
+    # (1) the event is dismissed -> only store the metadata
+    fast_reco_cut_module.if_value("==-1", store_metadata_path)
+    # (2) the event is immediately accepted -> store everything
+    fast_reco_cut_module.if_value("==1", store_rawdata_path)
+    # (3) we do not know what to do -> go on with the reconstruction
+
+    path.add_module("PrintCollections")
+
+
+def get_store_metadata_path():
+    store_metadata_path = basf2.create_path()
+    store_metadata_path.add_module("SeqRootOutput", outputFileName="metadata.sroot",
+                                   saveObjs=["EventMetaData", "SoftwareTriggerResult"])
+
+    return store_metadata_path
+
+
+def get_store_rawdata_path(store_metadata_path):
+    store_rawdata_path = basf2.create_path()
+    store_rawdata_path.add_module("SeqRootOutput", outputFileName="rawdata.sroot",
+                                  saveObjs=["EventMetaData", "SoftwareTriggerResult",
+                                            "RawSVDs", "RawCDCs", "RawTOPs", "RawARICHs", "RawKLMs",
+                                            "EKLMDigits", "ECLDigits"])
+
+    basf2.B2WARNING("The ECL and EKLM raw data is not handled properly!")
+
+    store_rawdata_path.add_path(store_metadata_path)
+    return store_metadata_path
+
+
+if __name__ == '__main__':
+    path = basf2.create_path()
+    path.add_module("SeqRootInput")
+
+    add_unpackers(path)
+    add_softwaretrigger_reconstruction(path)
+
+    basf2.print_path(path)

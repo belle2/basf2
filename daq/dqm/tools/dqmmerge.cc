39d7ca4fcc478f09df670ea03c8ece34adb24b1a
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2016-09-02 18:34:59 +0900

    NTuple/TTree collection added.

---
+++ daq/dqm/tools/dqmmerge.cc
@@ -0,0 +1,52 @@
+//+
+// File : dqmmerge.cc
+// Description : Merge DQM histogram files
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 29 - Aug - 2016
+//-
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <TFileMerger.h>
+#include <TFile.h>
+#include <Riostream.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  if (argc < 5) {
+    printf("Usage : dqmmerge topdir histofile nnodes startnode\n");
+    exit(-1);
+  }
+  string topdir = string(argv[1]);
+  string file = string(argv[2]);
+  int nnodes = atoi(argv[3]);
+  int startnode = atoi(argv[4]);
+
+  // Set up merger with output file
+  TFileMerger merger(false, false);
+  if (!merger.OutputFile(file.c_str())) {
+    printf("RbTupleManager:: error to open output file %s\n", file.c_str());
+    return -1;
+  }
+
+  // Make file list
+  for (int i = 0; i < nnodes; i++) {
+    char histofile[1024];
+    sprintf(histofile, "%s/evp_hltwk%2.2d/%s",
+            topdir.c_str(), startnode + i,
+            file.c_str());
+    merger.AddFile(histofile);
+  }
+
+  // Do Merge
+  if (!merger.Merge()) {
+    printf("RbTupleManager:: error to merge files\n");
+    return -1;
+  }
+}
+
+

76f09792ef0f5b0265ac5d2c73096bbbf2368da2
Nils Braun <nils.braun@kit.edu>
2016-09-12 16:47:15 +0200

    Adjusted examples.

--- hlt/hlt/examples/prod_gen_sim_hlt_reco.sh
+++ hlt/hlt/examples/prod_gen_sim_hlt_reco.sh
@@ -3,9 +3,9 @@
 # and run the hlt and the offlin reconstruction 
 
 # generate and simulate events
-basf2 skim/hlt/examples/prod_gen_sim.py
+basf2 hlt/hlt/examples/prod_gen_sim.py
 # hlt reco and only the HLTTags are stored to the root output file
-basf2 skim/hlt/examples/prod_hlt.py
+basf2 hlt/hlt/examples/prod_hlt.py
 # uses the gen+sim for offline reco and combines with the HLTTags from the previous
 # steering file
-basf2 skim/hlt/examples/prod_reco.py
+basf2 hlt/hlt/examples/prod_reco.py

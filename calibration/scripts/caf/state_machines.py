872b70205adf1ff82888ae899cfe672edae89463
David Dossett <david.dossett@desy.de>
2016-09-20 00:00:12 +1000

    Added basic CalibrationMachine states and transitions. Added a graphviz plotter to view machine visually.

--- calibration/scripts/caf/state_machines.py
+++ calibration/scripts/caf/state_machines.py
@@ -7,10 +7,9 @@ class Machine():
     Base class for a final state machine wrapper.
     Implements the framwork that a more complex machine can inherit from.
 
-    * 'model' attribute is compulsory and represents another class instance
-    that contains information that will set up your inherited class. Currently
-    this base class won't use the attribute so you can set it to a dummy object if
-    you want.
+    * 'model' attribute represents another class instance that contains information
+    that will set up your Machine and allow your machine to make descisions. Currently
+    this base class won't use the attribute.
 
     * 'states' attribute is a list of possible states of the machine. (strings)
 
@@ -20,13 +19,13 @@ class Machine():
     an allowed state to an allowed state. A condition is optional but must be a callable
     that returns True or False based on some inputs/state of the machine.
     """
-    def __init__(self, model, states=None, transitions=None, initial_state=None):
+    def __init__(self, model=None, states=None, transitions=None, initial_state=None):
         """
         Basic Setup
         """
         self.model = model
-        self.state = initial_state
         self.states = []
+        self.state = initial_state
         self.transitions = {}
         if states:
             self.states = states
@@ -87,7 +86,33 @@ class Machine():
             if condition():
                 self.state = transition_dict["dest"]
 
+    def save_graph(self, filename, graphname):
+        with open(filename, "w") as dotfile:
+            dotfile.write("digraph "+graphname+" {\n")
+            for state in self.states:
+                dotfile.write('"'+state+'" [shape=ellipse, color=black]\n')
+            for transition, transition_dict in self.transitions.items():
+                dotfile.write('"'+transition_dict["source"]+'" -> "'+transition_dict["dest"]+'" [label="'+transition+'"]\n')
+            dotfile.write("}\n")
+
 
 class CalibrationMachine(Machine):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.states.extend(["init",
+                            "running_collector",
+                            "collector_failed",
+                            "collector_completed",
+                            "running_algorithm",
+                            "algorithm_failed",
+                            "algorithm_completed",
+                            "completed"])
+
+        self.add_transition("submit_collector", "init", "running_collector")
+        self.add_transition("fail_collector", "running_collector", "collector_failed")
+        self.add_transition("complete_collector", "running_collector", "collector_completed")
+        self.add_transition("run_algorithm", "collector_completed", "running_algorithm")
+        self.add_transition("complete_algorithm", "running_algorithm", "algorithm_completed")
+        self.add_transition("fail_algorithm", "running_algorithm", "algorithm_failed")
+        self.add_transition("iterate", "algorithm_completed", "init")
+        self.add_transition("finish", "algorithm_completed", "completed")

c02dc6babf5dc92ad3c86a6ae13c3d659ab6e2e4
Oliver Frost <oliver.frost@desy.de>
2016-09-21 00:15:24 +0200

    Sketching a retrain procedure for the segment pair filters.

--- tracking/trackFindingCDC/studies/segmentPair/record.py
+++ tracking/trackFindingCDC/studies/segmentPair/record.py
@@ -14,85 +14,126 @@ import logging
 class CDCSegmentPairRecordingRun(RecordingRun):
     """Run for recording segment pairs encountered at the filter"""
     n_events = 10000
-    # n_loops = 0.5
-    # bkg_files = os.path.join(os.environ["VO_BELLE2_SW_DIR"], "bkg")
+
+    #: Select a recording task
+    task = ""
+
+    #: Change the default orientation of segments
+    segment_orientation = ""
 
     @property
     def root_output_file_name(self):
+        if self.task:
+            file_name = self.task + "_"
+        else:
+            file_name = ""
+
         if self.root_input_file:
-            file_name = os.path.split(self.root_input_file)[1]
+            file_name += os.path.split(self.root_input_file)[1]
             return file_name
         else:
-            return "CDCSegmentPairTruthRecords.root"
+            if file_name:
+                file_name += ".root"
+                return file_name
+            else:
+                return "CDCSegmentPairTruthRecords.root"
 
-    # Train feasible
-    # n_events = 1000
-    # n_loops = 0.5
-    # skim = ""
-    # varsets = ["feasible", "filter(truth)",]
+    @property
+    def recording_finder_module(self):
+        finder_module = basf2.register_module(
+            "TrackFinderCDCAutomatonDev",
+            SegmentPairFilter="unionrecording",
+            SegmentPairRelationFilter="none",
+            SegmentPairFilterParameters={
+                "rootFileName": self.root_output_file_name,
+                "varSets": self.varsets,
+                "skim": self.skim,
+            },
+        )
+        if self.segment_orientation:
+            finder_module.param(dict(SegmentOrientation=self.segment_orientation))
 
-    # Validate feasiblity cut
-    # n_events = 10000
-    # # n_loops = float("nan")
-    # n_loops = 1
-    # skim = ""
-    # varsets = ["feasible", "filter(simple)", "filter(fitless)", "filter(feasible)", "filter(truth)"]
+        return finder_module
 
-    # investigate next level
-    # n_events = 10000
-    # n_loops = 1.0
-    # skim = "feasible"
-    # varsets = [
-    #     "basic",
-    #     # "feasible",
-    #     "fitless",
-    #     "pre_fit",
-    #     # "fit",
-    #     "truth",
-    #     # "old_fit",
-    #     # "filter(fitless)",
-    #     # "filter(simple)",
-    #     # "filter(feasible)",
-    #     "filter(truth)",
-    #     ]
+    def create_argument_parser(self, **kwds):
+        argument_parser = super().create_argument_parser(**kwds)
+        argument_parser.add_argument(
+            "--task",
+            choices=["train_feasible", "eval_feasible", "train_realistic", "eval_realistic", ],
+            default=self.task,
+            dest="task",
+            help=("Select a perpared recording task")
+        )
+        return argument_parser
 
-    # Train final cut
-    # n_events = 10000
-    # n_loops = 1
-    # skim = "feasible"
-    # varsets = [
-    #     "realistic",
-    #     "filter(truth)",
-    #     "truth"
-    #     ]
+    def setup_task(self, task):
+        if task == "train_feasible":
+            self.n_events = 10000
+            self.n_loops = 0.5
+            self.flight_time_estimation = "outwards"
+            self.segment_orientation = "outwards"
+            self.skim = ""
+            self.varsets = [
+                "feasible",
+                "filter(truth)",
+            ]
 
-    # validate final cut
-    n_events = 10000
-    n_loops = 1.0
-    # n_loops = float("nan")
-    skim = "feasible"
-    varsets = [
-        "filter(fitless)",
-        "filter(simple)",
-        "filter(feasible)",
-        "filter(realistic)",
-        "filter(truth)",
-    ]
+        elif task == "eval_feasible":
+            self.n_events = 10000
+            self.n_loops = 0.5
+            self.flight_time_estimation = "outwards"
+            self.segment_orientation = "outwards"
+            self.skim = ""
+            self.varsets = [
+                "filter(feasible)",
+                "filter(truth)"
+            ]
 
-    recording_filter_parameter_name = "SegmentPairFilterParameters"
-    recording_finder_module = basf2.register_module("TrackFinderCDCAutomatonDev",
-                                                    # SegmentOrientation="outwards",
-                                                    SegmentOrientation="none",
-                                                    )
+        elif task == "train_realistic":
+            self.n_events = 10000
+            self.n_loops = 1
+            self.skim = "feasible"
+            self.varsets = [
+                "realistic",
+                "filter(truth)",
+                "truth"
+            ]
 
-    recording_finder_module.param({
-        "SegmentPairFilter": "unionrecording",
-        "SegmentPairFilterParameters": {
-            # "rootFileName": root_output_file_name,
-            "varSets": varsets,
-        },
-        "SegmentPairRelationFilter": "none"
-    })
+        elif task == "eval_realistic":
+            self.n_events = 10000
+            self.n_loops = 1
+            # self.n_loops = float("nan")
+            self.skim = "feasible"
+            self.varsets = [
+                "filter(fitless)",
+                "filter(simple)",
+                "filter(feasible)",
+                "filter(realistic)",
+                "filter(truth)",
+            ]
+
+        else:
+            # investigate next level
+            self.n_events = 10000
+            self.n_loops = 1.0
+            self.skim = "feasible"
+            self.varsets = [
+                "basic",
+                # "feasible",
+                "fitless",
+                "pre_fit",
+                # "fit",
+                "truth",
+                # "old_fit",
+                # "filter(fitless)",
+                # "filter(simple)",
+                # "filter(feasible)",
+                "filter(truth)",
+            ]
+
+    def configure(self, arguments):
+        super().configure(arguments)
+        self.setup_task(self.task)
 
 
 def main():

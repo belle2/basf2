c750ef905008b95bfeb6df80c814eada995d8751
Pablo Goldenzweig <pablo.goldenzweig@kit.edu>
2016-10-07 17:07:09 +0200

    Updated continuum suppression tutorials for new MVA package

---
+++ analysis/examples/tutorials/B2A701-ContinuumSuppression_Input.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# This tutorial runs over skims of centrally produced B->KsPi0 and continuum MC
+# mdst files and creates flat NTuples of B->KsPi0 decays, which are used in
+# tutorials B2A702 AND B2A703 for training, testing, and applying the MVAExpert.
+#
+# Usage:
+#   basf2 B2A701-ContinuumSuppression_Input.py <train,test,apply_signal,apply_qqbar>
+#
+# Contributors: P. Goldenzweig (October 2016)
+#
+################################################################################
+
+from modularAnalysis import *
+
+set_log_level(LogLevel.ERROR)
+
+# --I/O----------------------------------------------------------------------------------------
+step = str(sys.argv[1])
+
+path = '/gpfs/fs02/belle2/users/pablog/analysis/rec_Bd_K0Pi0_BGx0_MC5_2015/Bd_KsPi0/mdst/'
+input = ''
+
+if step == 'train':
+    input = [path + '*_train/*']
+elif step == 'test':
+    input = [path + '*_test/*']
+elif step == 'apply_signal':
+    input = [path + 'Bd_KsPi0_expert/*']
+elif step == 'apply_qqbar':
+    input = [path + '*bar_expert/*']
+else:
+    sys.exit('Step does not match any of the available samples: `train`, `test`, `apply_signal`or `apply_qqbar`')
+
+outfile = step + '.root'
+# ---------------------------------------------------------------------------------------------
+
+# Perform analysis.
+main = create_path()
+
+inputMdstList('MC5', input, path=main)
+
+fillParticleList('gamma:all', '', path=main)
+fillParticleList('pi+:good', 'chiProb > 0.001 and piid > 0.5 and abs(dz) < 4.0 and abs(dr) < 0.2', path=main)
+fillParticleList('pi-:good', 'chiProb > 0.001 and piid > 0.5 and abs(dz) < 4.0 and abs(dr) < 0.2', path=main)
+
+reconstructDecay('K_S0 -> pi+:good pi-:good', '0.480<=M<=0.516', 1, path=main)
+reconstructDecay('pi0  -> gamma:all gamma:all', '0.115<=M<=0.152', 1, path=main)
+reconstructDecay('B0   -> K_S0 pi0', '5.2 < Mbc < 5.3 and -0.3 < deltaE < 0.2', path=main)
+
+matchMCTruth('B0', path=main)
+buildRestOfEvent('B0', path=main)
+buildContinuumSuppression('B0', path=main)
+
+# Define the variables for training.
+trainVars = [
+    'R2',
+    'thrustBm',
+    'thrustOm',
+    'cosTBTO',
+    'cosTBz',
+    'KSFWVariables(et)',
+    'KSFWVariables(mm2)',
+    'KSFWVariables(hso00)',
+    'KSFWVariables(hso02)',
+    'KSFWVariables(hso04)',
+    'KSFWVariables(hso10)',
+    'KSFWVariables(hso12)',
+    'KSFWVariables(hso14)',
+    'KSFWVariables(hso20)',
+    'KSFWVariables(hso22)',
+    'KSFWVariables(hso24)',
+    'KSFWVariables(hoo0)',
+    'KSFWVariables(hoo1)',
+    'KSFWVariables(hoo2)',
+    'KSFWVariables(hoo3)',
+    'KSFWVariables(hoo4)',
+    'CleoCone(1)',
+    'CleoCone(2)',
+    'CleoCone(3)',
+    'CleoCone(4)',
+    'CleoCone(5)',
+    'CleoCone(6)',
+    'CleoCone(7)',
+    'CleoCone(8)',
+    'CleoCone(9)'
+]
+
+# Save target variable necessary for training.
+targetVar = ['isNotContinuumEvent']
+
+# Create output file.
+variablesToNTuple('B0', trainVars + targetVar, treename='tree', filename=outfile, path=main)
+
+process(main)
+print(statistics)
---
+++ analysis/examples/tutorials/B2A703-ContinuumSuppression_MVAExpert.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+# This tutorial runs over skims of centrally produced B->KsPi0 or continuum MC
+# mdst files and reconstructs B->KsPi0 decays, applies the MVAExpert module,
+# and writes out flat NTuples containing all variables used in the continuum
+# suppression training + the (transformed) network output distribution.
+#
+# This module requires the weightfile produced in B2A702 (MVAFastBDT.root).
+#
+# Usage:
+#   basf2 B2A703-ContinuumSuppression_MVAExpert.py <signal,qqbar>
+#
+# Contributors: P. Goldenzweig (October 2016)
+#
+################################################################################
+
+from modularAnalysis import *
+
+set_log_level(LogLevel.ERROR)
+
+# --I/O----------------------------------------------------------------------------------------
+step = str(sys.argv[1])
+
+path = '/gpfs/fs02/belle2/users/pablog/analysis/rec_Bd_K0Pi0_BGx0_MC5_2015/Bd_KsPi0/mdst/'
+input = ''
+
+if step == 'signal':
+    input = [path + 'Bd_KsPi0_expert/*']
+elif step == 'qqbar':
+    input = [path + '*bar_expert/*']
+else:
+    sys.exit('Step does not match any of the available samples: `signal` or `qqbar`')
+
+outfile = 'MVAExpert_fullNTuple_' + step + '.root'
+# ---------------------------------------------------------------------------------------------
+
+# Perform analysis.
+main = create_path()
+
+inputMdstList('MC5', input, path=main)
+
+fillParticleList('gamma:all', '', path=main)
+fillParticleList('pi+:good', 'chiProb > 0.001 and piid > 0.5 and abs(dz) < 4.0 and abs(dr) < 0.2', path=main)
+fillParticleList('pi-:good', 'chiProb > 0.001 and piid > 0.5 and abs(dz) < 4.0 and abs(dr) < 0.2', path=main)
+
+reconstructDecay('K_S0 -> pi+:good pi-:good', '0.480<=M<=0.516', 1, path=main)
+reconstructDecay('pi0  -> gamma:all gamma:all', '0.115<=M<=0.152', 1, path=main)
+reconstructDecay('B0   -> K_S0 pi0', '5.2 < Mbc < 5.3 and -0.3 < deltaE < 0.2', path=main)
+
+matchMCTruth('B0', path=main)
+buildRestOfEvent('B0', path=main)
+buildContinuumSuppression('B0', path=main)
+
+# Variables used in training.
+trainVars = [
+    'R2',
+    'thrustBm',
+    'thrustOm',
+    'cosTBTO',
+    'cosTBz',
+    'KSFWVariables(et)',
+    'KSFWVariables(mm2)',
+    'KSFWVariables(hso00)',
+    'KSFWVariables(hso02)',
+    'KSFWVariables(hso04)',
+    'KSFWVariables(hso10)',
+    'KSFWVariables(hso12)',
+    'KSFWVariables(hso14)',
+    'KSFWVariables(hso20)',
+    'KSFWVariables(hso22)',
+    'KSFWVariables(hso24)',
+    'KSFWVariables(hoo0)',
+    'KSFWVariables(hoo1)',
+    'KSFWVariables(hoo2)',
+    'KSFWVariables(hoo3)',
+    'KSFWVariables(hoo4)',
+    'CleoCone(1)',
+    'CleoCone(2)',
+    'CleoCone(3)',
+    'CleoCone(4)',
+    'CleoCone(5)',
+    'CleoCone(6)',
+    'CleoCone(7)',
+    'CleoCone(8)',
+    'CleoCone(9)'
+]
+
+# Target variable used in training.
+targetVar = ['isNotContinuumEvent']
+
+# MVAExpert
+main.add_module('MVAExpert', listNames=['B0'], extraInfoName='FastBDT', identifier='MVAFastBDT.root')
+
+# Variables from MVAExpert.
+expertVars = ['extraInfo(FastBDT)', 'transformedNetworkOutput(FastBDT,0.1,1.0)']
+
+# Create output file with all sets of variables.
+variablesToNTuple('B0', trainVars + targetVar + expertVars, treename='tree', filename=outfile, path=main)
+
+process(main)
+print(statistics)

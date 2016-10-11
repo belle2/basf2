0d13218e778ea417cb4d26a82e8327554d06b9a5
CASAROSA Giulia <giulia.casarosa@pi.infn.it>
2016-10-11 18:03:02 +0900

    updated

--- analysis/skim/Charm2BodyHadronic_List.py
+++ analysis/skim/Charm2BodyHadronic_List.py
@@ -13,7 +13,7 @@ from basf2 import *
 from modularAnalysis import *
 
 
-def Charm2BodyCharged():
+def D0ToHpJm():
     charmcuts = '1.78 < M < 1.94'
     D0_Channels = ['K-:loose pi+:loose',
                    'pi+:loose pi-:loose',
@@ -22,8 +22,24 @@ def Charm2BodyCharged():
 
     D0List = []
     for chID, channel in enumerate(D0_Channels):
-        reconstructDecay('D0:Hadronic' + str(chID) + ' -> ' + channel, charmcuts, chID)
-        D0List.append('D0:Hadronic' + str(chID))
+        reconstructDecay('D0:' + str(chID) + ' -> ' + channel, charmcuts, chID)
+        vertexRave('D0:' + str(chID), 0.001)
+        D0List.append('D0:' + str(chID))
 
     Lists = D0List
     return Lists
+
+
+def DstToD0PiD0ToHpJm():
+
+    D0List = D0ToHpJm()
+
+    Dstcuts = '0 < Q < 0.04'
+
+    DstList = []
+    for chID, channel in enumerate(D0List):
+        reconstructDecay('D*+:' + str(chID) + ' -> pi+:all ' + channel, Dstcuts, chID)
+        vertexRave('D*+:' + str(chID), 0.001)
+        DstList.append('D*+:' + str(chID))
+
+    return DstList
--- analysis/skim/Charm2BodyHadronic_Skim_Standalone.py
+++ analysis/skim/Charm2BodyHadronic_Skim_Standalone.py
@@ -8,27 +8,61 @@
 #
 ######################################################
 
+from ROOT import Belle2
 from basf2 import *
 from modularAnalysis import *
 from stdCharged import *
 from stdFSParticles import *
 
+reset_database()
+use_local_database(Belle2.FileSystem.findFile("data/framework/database.txt"), "", True, LogLevel.ERROR)
+
 set_log_level(LogLevel.INFO)
 
-filelist = \
+ccbar_wBG = \
     ['/ghi/fs01/belle2/bdata/MC/fab/sim/release-00-07-00/DBxxxxxxxx/MC6/prod00000198/s00/e0000/4S/r00000/ccbar/sub00/' +
-     'mdst_0005*_prod00000198_task000005*.root'
-     ]
+     'mdst_00051*_prod00000198_task0000051*.root']
 
+ccbar_noBG = \
+    ['/ghi/fs01/belle2/bdata/MC/fab/sim/release-00-07-00/DBxxxxxxxx/MC6/prod00000194/s00/e0000/4S/r00000/ccbar/sub00/' +
+     'mdst_00051*_prod00000194_task0000051*.root']
+
+mixed_wBG = \
+    ['/ghi/fs01/belle2/bdata/MC/fab/sim/release-00-07-00/DBxxxxxxxx/MC6/prod00000188/s00/e0000/4S/r00000/mixed/sub00/' +
+     'mdst_00051*_prod00000188_task0000051*.root']
+
+charged_wBG = \
+    ['/ghi/fs01/belle2/bdata/MC/fab/sim/release-00-07-00/DBxxxxxxxx/MC6/prod00000189/s00/e0000/4S/r00000/charged/sub00/' +
+     'mdst_00051*_prod00000189_task0000051*.root']
+
+uubar_wBG = \
+    ['/ghi/fs01/belle2/bdata/MC/fab/sim/release-00-07-00/DBxxxxxxxx/MC6/prod00000195/s00/e0000/4S/r00000/uubar/sub00/' +
+     'mdst_00051*_prod00000195_task0000051*.root']
+
+ddbar_wBG = \
+    ['/ghi/fs01/belle2/bdata/MC/fab/sim/release-00-07-00/DBxxxxxxxx/MC6/prod00000196/s00/e0000/4S/r00000/ddbar/sub00/' +
+     'mdst_00051*_prod00000196_task0000051*.root']
+
+ssbar_wBG = \
+    ['/ghi/fs01/belle2/bdata/MC/fab/sim/release-00-07-00/DBxxxxxxxx/MC6/prod00000197/s00/e0000/4S/r00000/ssbar/sub00/' +
+     'mdst_00051*_prod00000197_task0000051*.root']
+
+filelist = mixed_wBG
 inputMdstList('default', filelist)
 
 stdFSParticles()
 loadStdCharged()
 
 from Charm2BodyHadronic_List import *
-Charm2BodyChargedList = Charm2BodyCharged()
-skimOutputUdst('Charm2BodyCharged_Standalone', Charm2BodyChargedList)
-summaryOfLists(Charm2BodyChargedList)
+
+# D0ToHpJmList = D0ToHpJm()
+# skimOutputUdst('D0ToHpJm_Standalone_ssbar_wBG', D0ToHpJmList)
+# summaryOfLists(D0ToHpJmList)
+
+DstToD0PiD0ToHpJmList = DstToD0PiD0ToHpJm()
+skimOutputUdst('DstToD0PiD0ToHpJm_Standalone_mixed_wBG', DstToD0PiD0ToHpJmList)
+summaryOfLists(DstToD0PiD0ToHpJmList)
+
 
 process(analysis_main)
 

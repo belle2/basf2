a8af28c1e53fe54ad9b561827584ab012f49396d
Oliver Frost <oliver.frost@desy.de>
2016-10-14 01:05:24 +0200

    Pass by references to eliminate cppcheck warning + cosmetics.

--- tracking/trackFindingCDC/display/include/Colorizer.h
+++ tracking/trackFindingCDC/display/include/Colorizer.h
@@ -19,11 +19,13 @@ namespace Belle2 {
     /**
      * Class template for Colorizer classes.
      *
-     * This class is meant to work as a base class for interfaces to the ColorMap classes and handles
+     * This class is meant to work as a base class for interfaces to the ColorMap classes and
+     * handles
      * the mapping from the colormapping-method name given as a string to the actual ColorMap class.
-     * It needs to now the type of the Object to be drawn and the base ColorMap class for this Object (defaults to CDCHitColorMap).
+     * It needs to now the type of the Object to be drawn and the base ColorMap class for this
+     * Object (defaults to CDCHitColorMap).
      */
-    template<class AObject, class AColorMap = CDCHitColorMap>
+    template <class AObject, class AColorMap = CDCHitColorMap>
     class Colorizer {
 
     private:
@@ -34,18 +36,31 @@ namespace Belle2 {
       /**
        * Default Constructor.
        *
-       * Only needed in case there exists only one AColorMap class for this item. Initializes m_listOfMappingNames with an empty map.
+       * Only needed in case there exists only one AColorMap class for this item. Initializes
+       * m_listOfMappingNames with an empty map.
        */
-      Colorizer(): m_strokeSet(false), m_strokeWidthSet(false), m_strokeMethodPtr(nullptr), m_strokeWidthMethodPtr(nullptr),
-        m_listOfMappingNames( {}) {}
+      Colorizer()
+        : m_strokeSet(false)
+        , m_strokeWidthSet(false)
+        , m_strokeMethodPtr(nullptr)
+        , m_strokeWidthMethodPtr(nullptr)
+        , m_listOfMappingNames( {})
+      {
+      }
 
       /**
        * Constructor.
-       * @param map A string to MethodPtr map assigning a AColorMap class to its name given as a string. \see m_listOfMappingNames
+       * @param map A string to MethodPtr map assigning a AColorMap class to its name given as a
+       * string. \see m_listOfMappingNames
        */
-      explicit Colorizer(std::map<std::string, MethodPtr> map): m_strokeSet(false), m_strokeWidthSet(false), m_strokeMethodPtr(nullptr),
-        m_strokeWidthMethodPtr(nullptr),
-        m_listOfMappingNames(map) {}
+      explicit Colorizer(const std::map<std::string, MethodPtr>& map)
+        : m_strokeSet(false)
+        , m_strokeWidthSet(false)
+        , m_strokeMethodPtr(nullptr)
+        , m_strokeWidthMethodPtr(nullptr)
+        , m_listOfMappingNames(map)
+      {
+      }
 
       /**
        * Maps the object and its id to a color.
@@ -91,14 +106,18 @@ namespace Belle2 {
         m_strokeSet = true;
         if (m_listOfMappingNames.count(methodName) == 1) {
           m_strokeMethodPtr = (this->*m_listOfMappingNames[methodName])();
-        } else m_strokeMethodPtr = std::unique_ptr<AColorMap>(new ReturnInputString<AObject, AColorMap>(methodName));
+        } else {
+          m_strokeMethodPtr =
+            std::unique_ptr<AColorMap>(new ReturnInputString<AObject, AColorMap>(methodName));
+        }
       }
 
       /**
        * Sets the method defining the stroke-width to be used.
        * @param methodName  The Name of the AColorMap class to be used. The translation from name
        *                    to AColorMap class must be defined in m_listOfMappingNames. If there is
-       *                    no such translation, it is assumed that methodName already is the stroke-width
+       *                    no such translation, it is assumed that methodName already is the
+       * stroke-width
        *                    to be used.
        */
       void setStrokeWidth(const std::string& methodName)
@@ -106,13 +125,16 @@ namespace Belle2 {
         m_strokeWidthSet = true;
         if (m_listOfMappingNames.count(methodName) == 1) {
           m_strokeWidthMethodPtr = (this->*m_listOfMappingNames[methodName])();
-        } else m_strokeWidthMethodPtr = std::unique_ptr<AColorMap>(new ReturnInputString<AObject, AColorMap>(methodName));
+        } else {
+          m_strokeWidthMethodPtr =
+            std::unique_ptr<AColorMap>(new ReturnInputString<AObject, AColorMap>(methodName));
+        }
       }
 
       /**
        * Returns information on the translation from object and object id to color.
        */
-      std:: string printStrokeAttribute()
+      std::string printStrokeAttribute()
       {
         try {
           return m_strokeMethodPtr->info();
@@ -124,17 +146,16 @@ namespace Belle2 {
       /**
        * Returns information on the translation from object and object id to color.
        */
-      std:: string printStrokeWidthAttribute()
+      std::string printStrokeWidthAttribute()
       {
         return m_strokeWidthMethodPtr->info();
       }
-
 
     protected:
       /**
        * Construct and return a pointer to the AColorMap.
        */
-      template<class T>
+      template <class T>
       std::unique_ptr<AColorMap> constructMappingClass()
       {
         std::unique_ptr<AColorMap> mappingClass(new T);
@@ -164,7 +185,7 @@ namespace Belle2 {
     /**
      * Class template for coloring objects with colors defined manually with a string.
      */
-    template<class AObject>
+    template <class AObject>
     class InputValueColorizer : public Colorizer<AObject, ReturnInputValue<AObject>> {
     public:
       /**
@@ -173,8 +194,8 @@ namespace Belle2 {
       void setStroke(std::string stroke)
       {
         Colorizer<AObject, ReturnInputValue<AObject>>::m_strokeSet = true;
-        Colorizer<AObject, ReturnInputValue<AObject>>::m_strokeMethodPtr = std::unique_ptr<ReturnInputValue<AObject>>
-                                                   (new ReturnInputValue<AObject>(stroke));
+        Colorizer<AObject, ReturnInputValue<AObject>>::m_strokeMethodPtr =
+                                                     std::unique_ptr<ReturnInputValue<AObject>>(new ReturnInputValue<AObject>(stroke));
       }
 
       /**
@@ -183,13 +204,14 @@ namespace Belle2 {
       void setStrokeWidth(std::string strokeWidth)
       {
         Colorizer<AObject, ReturnInputValue<AObject>>::m_strokeWidthSet = true;
-        Colorizer<AObject, ReturnInputValue<AObject>>::m_strokeWidthMethodPtr = std::unique_ptr<ReturnInputValue<AObject>>
-                                                   (new ReturnInputValue<AObject>(strokeWidth));
+        Colorizer<AObject, ReturnInputValue<AObject>>::m_strokeWidthMethodPtr =
+                                                     std::unique_ptr<ReturnInputValue<AObject>>(new ReturnInputValue<AObject>(strokeWidth));
       }
     };
 
     /**
-     * This Class handles the mapping from the colormapping-method name given as a string to the actual AColorMap for CDCSegments.
+     * This Class handles the mapping from the colormapping-method name given as a string to the
+     * actual AColorMap for CDCSegments.
      */
     class CDCSegmentColorizer : public Colorizer<CDCRecoSegment2D, CDCSegmentColorMap> {
     public:
@@ -202,7 +224,8 @@ namespace Belle2 {
     };
 
     /**
-     * This Class handles the mapping from the colormapping-method name given as a string to the actual AColorMap for CDCHits.
+     * This Class handles the mapping from the colormapping-method name given as a string to the
+     * actual AColorMap for CDCHits.
      */
     class CDCHitColorizer : public Colorizer<CDCHit, CDCHitColorMap> {
     public:
@@ -217,7 +240,7 @@ namespace Belle2 {
     /**
      * Class template for coloring objects with colors from ListColorsColorMap.
      */
-    template<class AObject>
+    template <class AObject>
     class ListColorsColorizer : public Colorizer<AObject, ListColorsColorMap<AObject>> {
     public:
       /**
@@ -225,16 +248,15 @@ namespace Belle2 {
        *
        * Sets the Method on how to match a color to a stroke to ListColorsColorMap.
        */
-      ListColorsColorizer() : Colorizer<AObject, ListColorsColorMap<AObject>>()
+      ListColorsColorizer()
+        : Colorizer<AObject, ListColorsColorMap<AObject>>()
       {
-        Colorizer<AObject, ListColorsColorMap<AObject>>::m_strokeMethodPtr = std::unique_ptr<ListColorsColorMap<AObject>>
-                                                     (new ListColorsColorMap<AObject>());
+        Colorizer<AObject, ListColorsColorMap<AObject>>::m_strokeMethodPtr =
+                                                       std::unique_ptr<ListColorsColorMap<AObject>>(new ListColorsColorMap<AObject>());
         Colorizer<AObject, ListColorsColorMap<AObject>>::m_strokeSet = true;
       }
     };
-  }//TrackFindingCDC
-}//Belle2
-
-
+  } // TrackFindingCDC
+} // Belle2
 
 #endif /* COLORIZER_H_ */

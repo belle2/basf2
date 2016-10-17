a08c145f1e891c8ea06813ee8500f11301aaa2fe
Thomas Keck <thomas.keck2@kit.edu>
2016-10-17 14:02:54 +0200

    Add namespace Belle2 to WorkingDirectoryManager class

--- framework/utilities/include/WorkingDirectoryManager.h
+++ framework/utilities/include/WorkingDirectoryManager.h
@@ -9,30 +9,31 @@
 
 #include <TSystem.h>
 
-/**
- RAII helper class, changing the ROOT working directory
- and restoring the old one as soon as the object of this class is out of scope
-*/
-class WorkingDirectoryManager {
-public:
+namespace Belle2 {
   /**
-   * Change ROOT working directory to given directory
-   * @param workingDirectory
-   */
-  explicit WorkingDirectoryManager(std::string workingDirectory) : oldDirectory(gSystem->WorkingDirectory())
-  {
-    gSystem->ChangeDirectory(workingDirectory.c_str());
-  }
+   RAII helper class, changing the ROOT working directory
+   and restoring the old one as soon as the object of this class is out of scope
+  */
+  class WorkingDirectoryManager {
+  public:
+    /**
+     * Change ROOT working directory to given directory
+     * @param workingDirectory
+     */
+    explicit WorkingDirectoryManager(std::string workingDirectory) : oldDirectory(gSystem->WorkingDirectory())
+    {
+      gSystem->ChangeDirectory(workingDirectory.c_str());
+    }
 
-  /**
-   * Change ROOT working directory back to old working directory
-   */
-  ~WorkingDirectoryManager()
-  {
-    gSystem->ChangeDirectory(oldDirectory.c_str());
-  }
+    /**
+     * Change ROOT working directory back to old working directory
+     */
+    ~WorkingDirectoryManager()
+    {
+      gSystem->ChangeDirectory(oldDirectory.c_str());
+    }
 
-private:
-  std::string oldDirectory; /**< Old working directory */
-};
-
+  private:
+    std::string oldDirectory; /**< Old working directory */
+  };
+}

a8032cd43ba1c8098551dfd40c943332acf7821e
Christian Pulvermacher <christian.pulvermacher@kek.jp>
2016-10-17 04:35:53 +0200

    Use PWD in FileSystem::fileExists() etc.
    Previously, it would use the working directory during basf2 startup,
    which might cause 'interesting' problems when doing e.g.
      if (FileSystem::fileExists(f))
        open(f)
    
    findFile() was not affected by this, so its behaviour is unchanged.
    Enhanced the documentation a bit to avoid other problems related to
    changed working directories.
    
    Fixes BII-1746

--- framework/utilities/include/FileSystem.h
+++ framework/utilities/include/FileSystem.h
@@ -7,7 +7,25 @@ namespace Belle2 {
   /** Utility functions related to filename validation and filesystem access */
   class FileSystem {
   public:
-    /** Check if a given filename exits */
+    /**
+     * Search for given file or directory in local or central release directory,
+     * and return absolute path if found.
+     * If the file isn't found in either of these, absolute paths and paths
+     * relative to the current working directory are also accepted.
+     *
+     * It is recommended to replace any relative paths you have with the
+     * return value of this function during initialization.
+     *
+     * @param path path to file/directory, assuming it's installed locally
+     *             (e.g. /data/geometry/Belle2.xml). Leading slash is not
+     *             strictly required.
+     * @return absolute path to file in local directory, if it exists,
+     *         otherwise abs. path to file in central release directory,
+     *         or empty if file wasn't found.
+     */
+    static std::string findFile(const std::string& path);
+
+    /** Check if the file with given filename exists */
     static bool fileExists(const std::string& filename);
     /** Check if the dir containing the filename exists */
     static bool fileDirExists(const std::string& filename);
@@ -27,21 +45,6 @@ namespace Belle2 {
      *                 to lib<library>.so
      */
     static bool loadLibrary(std::string library, bool fullname = true);
-
-    /**
-     * Search for given file or directory in local or central release directory,
-     * and return absolute path if found.
-     * If the file isn't found in either of these, absolute paths and paths
-     * relative to the current working directory are also accepted.
-     *
-     * @param path path to file/directory, assuming it's installed locally
-     *             (e.g. /data/geometry/Belle2.xml). Leading slash is not
-     *             strictly required.
-     * @return absolute path to file in local directory, if it exists,
-     *         otherwise abs. path to file in central release directory,
-     *         or empty if file wasn't found.
-     */
-    static std::string findFile(const std::string& path);
 
     /**
      * Helper class for locking a file

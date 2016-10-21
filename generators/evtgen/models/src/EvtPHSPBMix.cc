917cf6847725c491c8807bd4f387e1a53fdfdd38
Martin Ritter <martin.ritter@desy.de>
2016-10-21 04:14:57 +0200

    Remove compiler warnings
    This commit removes some dynamic stack arrays and adds a return
    statement.

--- generators/evtgen/models/src/EvtPHSPBMix.cc
+++ generators/evtgen/models/src/EvtPHSPBMix.cc
@@ -253,7 +253,7 @@ void EvtPHSPBBMix::decay(EvtParticle* p)
   if (pr) prlp(103);
   if (NCC) {
     if (pr) prlp(2);
-    EvtId tempDaug[getNDaug() - 2];
+    std::vector<EvtId> tempDaug(getNDaug() - 2);
     tempDaug[0] = getDaug(0);
     tempDaug[1] = getDaug(1);
     if (getNDaug() == 5)
@@ -264,7 +264,7 @@ void EvtPHSPBBMix::decay(EvtParticle* p)
     //  for (int i(0);i<getNDaug()-2;++i)
     //  std::cout<<"Daug "<<i<<" : "<<EvtPDL::name(tempDaug[i]).c_str()<<std::endl;
 
-    p->initializePhaseSpace(getNDaug() - 2, tempDaug);
+    p->initializePhaseSpace(getNDaug() - 2, tempDaug.data());
 
     if (pr) prlp(222);
   } else { //nominal case.
@@ -361,7 +361,8 @@ EvtComplex EvtPHSPBBMix::Amplitude(const double& t1, const double& t2, bool B1_i
     else
       return EvtComplex(0., -sin(f * (t2 + _C * t1) / 2.) * _C / sqrt(2.));
   }
-
+  // no way to reach this but compiler complains without a return statement
+  return EvtComplex(0., 0.);
 }
 
 
@@ -474,7 +475,7 @@ void EvtPHSPBMix::decay(EvtParticle* p)
 
   //  const bool BBpipi(getNDaug()!=4);//true if BBpipi, if not BBpi
   if (pr)    std::cout << "decay B_MIX (0)" << std::endl;
-  EvtId tempDaug[getNDaug() - 1];
+  std::vector<EvtId> tempDaug(getNDaug() - 1);
   tempDaug[0] = getDaug(0);
   tempDaug[1] = getDaug(1);
   tempDaug[2] = getDaug(2);
@@ -484,7 +485,7 @@ void EvtPHSPBMix::decay(EvtParticle* p)
   if (pr)    std::cout << "decay B_MIX (1)" << std::endl;
 
 
-  p->initializePhaseSpace(getNDaug() - 1, tempDaug);
+  p->initializePhaseSpace(getNDaug() - 1, tempDaug.data());
 
   EvtParticle* s1;
 

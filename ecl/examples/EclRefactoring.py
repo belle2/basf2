27534ae35cc255fcc2c3024522d4bcf929997f79
Torben Ferber <ferber@physics.ubc.ca>
2016-10-28 20:33:37 +0200

    Modified examples to work with the new reconstruction, removed obsolote scripts.

--- ecl/examples/EclRefactoring.py
+++ ecl/examples/EclRefactoring.py
@@ -4,7 +4,7 @@
 ########################################################
 #
 # This steering file includes all modules of the
-# refactored ECL code.
+# ECL code.
 #
 # Example steering file - 2016 Belle II Collaboration
 # Author(s): Torben Ferber (ferber@physics.ubc.ca)
@@ -14,7 +14,7 @@
 import os
 from basf2 import *
 from simulation import add_simulation
-from reconstruction import add_tracking_reconstruction, add_prune_tracks, add_mdst_output
+from reconstruction import *
 
 import sys
 import glob
@@ -50,65 +50,13 @@ if (withbg == 1):
 else:
     add_simulation(main)
 
-# --------------------------------------------------
-# --------------------------------------------------
-# ECL digit calibration
-ecl_digit_calibration = register_module('ECLDigitCalibrator')
-main.add_module(ecl_digit_calibration)
-
-# ECL Connected Region Finder
-ecl_crfinder = register_module('ECLCRFinder')
-main.add_module(ecl_crfinder)
-
-# ECL Local Maximum Finder
-ecl_lmfinder = register_module('ECLLocalMaximumFinder')
-main.add_module(ecl_lmfinder)
-
-# ECL Splitter(s)
-ecl_n1 = register_module('ECLSplitterN1')
-main.add_module(ecl_n1)
-
-ecl_n2 = register_module('ECLSplitterN2')
-main.add_module(ecl_n2)
-
-# ECL Shower Correction
-ecl_showercorrection = register_module('ECLShowerCorrector')
-main.add_module(ecl_showercorrection)
-
-# ECL Shower Calibration
-ecl_showercalibration = register_module('ECLShowerCalibrator')
-main.add_module(ecl_showercalibration)
-
-# ECL Shower Shape
-ecl_showershape = register_module('ECLShowerShape')
-# ecl_showershape.set_log_level(LogLevel.DEBUG)
-# ecl_showershape.set_debug_level(175)
-main.add_module(ecl_showershape)
-
-# ECL covariance matrix
-ecl_covariance = register_module('ECLCovarianceMatrix')
-main.add_module(ecl_covariance)
-
-# ECL finalize
-ecl_finalize = register_module('ECLFinalizer')
-main.add_module(ecl_finalize)
-
-# ECL track shower matching
-ecl_track_match = register_module('ECLTrackShowerMatch')
-main.add_module(ecl_track_match)
-
-# ECL electron ID
-electron_id = register_module('ECLElectronId')
-main.add_module(electron_id)
-
-# ECL MC matching
-ecl_mc = register_module('MCMatcherECLClusters')
-main.add_module(ecl_mc)
+# add reconstruction
+add_reconstruction(main)
 
 # --------------------------------------------------
 # --------------------------------------------------
 
-# add output file with most of the available information
+# add output file with all of the available ECL information
 add_mdst_output(
     main,
     mc=True,

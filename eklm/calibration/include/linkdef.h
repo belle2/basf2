ba9973cd1386b80f222369a880fc23df9202173a
Kirill Chilikin <chilikin@lebedev.ru>
2016-11-01 00:05:52 +0300

    Database importer for simulation and digitization parameters.

--- eklm/calibration/include/linkdef.h
+++ eklm/calibration/include/linkdef.h
@@ -5,6 +5,7 @@
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
+#pragma link C++ class Belle2::EKLMDatabaseImporter+;
 #pragma link C++ class Belle2::EKLMTimeCalibrationAlgorithm+;
 
 #endif

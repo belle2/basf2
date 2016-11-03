c25dfb60a006bf1ca39de5b83c2bada886ffffcd
Eugenio Paoloni <eugenio.paoloni@pi.infn.it>
2016-11-03 17:40:55 +0100

    get rid of the CollalAllFilterVariable function in favour of a more flexible approach.

--- tracking/trackFindingVXD/sectorMap/filterFramework/tests/filters.cc
+++ tracking/trackFindingVXD/sectorMap/filterFramework/tests/filters.cc
@@ -12,7 +12,6 @@
 
 
 #include <tracking/trackFindingVXD/sectorMap/filterFramework/Shortcuts.h>
-#include <tracking/trackFindingVXD/sectorMap/training/CollectAllFilterVariables.h>
 #include <tuple>
 #include <iostream>
 #include <math.h>
@@ -430,26 +429,4 @@ namespace VXDTFfilterTest {
 
   }
 
-  /** explains how to use the CollectAllFilterVariables function **/
-  TEST_F(FilterTest, CollectAllFilterVariables)
-  {
-    spacePoint x1(0.0f , 0.0f, 0.0f);
-    spacePoint x2(1.0f , 0.0f, 0.0f);
-
-
-    auto filter = !(SquaredDistance3D() > 1.);
-
-    auto filter2 = !(SquaredDistance3D() > 1.) &&
-                   (SquaredDistance2Dxy() < -2);
-
-
-    std::map< std::string, double > collectedData;
-    CollectAllFilterVariables(filter , collectedData, x1, x2);
-    CollectAllFilterVariables(filter2, collectedData, x1, x2);
-
-    for (auto variablePair : collectedData)
-      std::cout << " " << variablePair.first << " = " <<
-                variablePair.second << std::endl;
-
-  }
 }

ad8750861b8a929961e1e31ee0d9dea25bf945a1
Oliver Frost <oliver.frost@desy.de>
2016-11-10 18:52:01 +0100

    Fixing intel build by using the less elegant explicit template instantiation.

--- tracking/trackFindingCDC/filters/base/src/ChooseableFilter.cc
+++ tracking/trackFindingCDC/filters/base/src/ChooseableFilter.cc
@@ -26,26 +26,26 @@ void AssignParameterVisitor::operator()(const T& t) const
   m_moduleParamList->getParameter<T>(m_paramName).setDefaultValue(t);
 }
 
-namespace Belle2 {
-  namespace TrackingFindingCDC {
-    /// Make explicit template function instances of
-    /// template<typename T> AssignParameterVisitor::operator()(const T&) const;
-    /// for each of the parameter types in FilterParamVariant
-    void instantiate_AssignParameterVisitor_operator() __attribute__((unused));
-    void instantiate_AssignParameterVisitor_operator()
-    {
-      ModuleParamList moduleParamList;
-      const std::string name = "";
-      AssignParameterVisitor::FilterParamVariant value;
-      AssignParameterVisitor psv(&moduleParamList, name);
-      boost::apply_visitor(psv, value);
-    }
-  }
-}
+// Make explicit template function instances of
+// namespace Belle2 {
+//   namespace TrackingFindingCDC {
+//     /// template<typename T> AssignParameterVisitor::operator()(const T&) const;
+//     /// for each of the parameter types in FilterParamVariant
+//     void instantiate_AssignParameterVisitor_operator() __attribute__((unused));
+//     void instantiate_AssignParameterVisitor_operator()
+//     {
+//       ModuleParamList moduleParamList;
+//       const std::string name = "";
+//       AssignParameterVisitor::FilterParamVariant value;
+//       AssignParameterVisitor psv(&moduleParamList, name);
+//       boost::apply_visitor(psv, value);
+//     }
+//   }
+// }
 // Does the same as above but explicit. The above however reacts to changes in the variant automatically.
 // Make explicit template instances for each of the parameter types in FilterParamVariant
-// template void AssignParameterVisitor::operator()(const bool&) const;
-// template void AssignParameterVisitor::operator()(const int&) const;
-// template void AssignParameterVisitor::operator()(const double&) const;
-// template void AssignParameterVisitor::operator()(const std::string&) const;
-// template void AssignParameterVisitor::operator()(const std::vector<std::string>&) const;
+template void AssignParameterVisitor::operator()(const bool&) const;
+template void AssignParameterVisitor::operator()(const int&) const;
+template void AssignParameterVisitor::operator()(const double&) const;
+template void AssignParameterVisitor::operator()(const std::string&) const;
+template void AssignParameterVisitor::operator()(const std::vector<std::string>&) const;

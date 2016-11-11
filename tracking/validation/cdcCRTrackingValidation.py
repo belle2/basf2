195cb2a5c0237930886d6b43174df59a4499382f
Oliver Frost <oliver.frost@desy.de>
2016-11-11 15:54:17 +0100

    Introducing a validation for the track finding setup provided to the CDC cosmic ray tests.

--- tracking/validation/cdcCRTrackingValidation.py
+++ tracking/validation/cdcCRTrackingValidation.py
@@ -5,12 +5,12 @@
 <header>
   <contact>oliver.frost@desy.de</contact>
   <input>CosmicsSimNoBkg.root</input>
-  <output>CosmicsTrackingValidation.root</output>
+  <output>CDCCRTrackingValidation.root</output>
   <description>This module validates that track finding is capable of reconstructing tracks in cosmics run.</description>
 </header>
 """
 
-VALIDATION_OUTPUT_FILE = 'CosmicsTrackingValidation.root'
+VALIDATION_OUTPUT_FILE = 'CDCCRTrackingValidation.root'
 CONTACT = 'oliver.frost@desy.de'
 N_EVENTS = 10000
 ACTIVE = True
@@ -25,7 +25,7 @@ from tracking.adjustments import adjust_module
 from tracking.validation.run import TrackingValidationRun
 
 
-class Cosmics(TrackingValidationRun):
+class CDCCR(TrackingValidationRun):
     n_events = N_EVENTS
     root_input_file = '../CosmicsSimNoBkg.root'
     components = None
@@ -33,15 +33,14 @@ class Cosmics(TrackingValidationRun):
     def finder_module(self, path):
         tracking.add_cdc_cr_track_finding(path)
 
-    def adjust_path(self, path):
-        adjust_module(path, 'DAFRecoFitter',
-                      pdgCodesToUseForFitting=[13],
-                      )
+        path.add_module('DAFRecoFitter',
+                        pdgCodesToUseForFitting=[13],
+                        )
 
-        adjust_module(path, 'TrackCreator',
-                      defaultPDGCode=13,
-                      useClosestHitToIP=True,
-                      )
+        path.add_module('TrackCreator',
+                        defaultPDGCode=13,
+                        useClosestHitToIP=True,
+                        )
 
     tracking_coverage = {
         'UsePXDHits': False,
@@ -59,7 +58,7 @@ class Cosmics(TrackingValidationRun):
 
 
 def main():
-    validation_run = Cosmics()
+    validation_run = CDCCR()
     validation_run.configure_and_execute_from_commandline()
 
 

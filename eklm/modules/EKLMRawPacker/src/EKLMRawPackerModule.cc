150f0124aa667335cafde363821beccfe90de799
Kirill Chilikin <chilikin@lebedev.ru>
2016-11-15 13:01:41 +0300

    Better formatting of packer and unpacker sources.

--- eklm/modules/EKLMRawPacker/src/EKLMRawPackerModule.cc
+++ eklm/modules/EKLMRawPacker/src/EKLMRawPackerModule.cc
@@ -31,34 +31,24 @@ EKLMRawPackerModule::EKLMRawPackerModule() : Module()
   setPropertyFlags(c_ParallelProcessingCertified);
   ///  maximum # of events to produce( -1 : inifinite)
   addParam("MaxEventNum", max_nevt, "Maximum event number to make", -1);
-  addParam("useDefaultModuleId", m_useDefaultElectId, "use default elect id if not found in mapping", true);
-
+  addParam("useDefaultModuleId", m_useDefaultElectId,
+           "Use default elect id if not found in mapping", true);
   B2INFO("EKLMRawPackerModule: Constructor done.");
-
   n_basf2evt = 0;
 }
-
-
 
 EKLMRawPackerModule::~EKLMRawPackerModule()
 {
 }
 
-
 void EKLMRawPackerModule::initialize()
 {
   B2INFO("EKLMRawPackerModule: initialize() started.");
-
   m_eventMetaDataPtr.registerInDataStore();
-
   rawklmarray.registerPersistent();
-
   B2INFO("EKLMRawPackerModule: initialize() done.");
   loadMap();
 }
-
-
-
 
 void EKLMRawPackerModule::event()
 {
@@ -67,14 +57,13 @@ void EKLMRawPackerModule::event()
   vector<uint32_t> data_words[4][4];//4 copper, 16 finesse
   data_words[4][4].clear();
   //int tot_num_hits=digits.getEntries();
-
   B2INFO("EKLMRawPackerModule:: entries of eklmdigits " << digits.getEntries());
   ///fill data_words
   int n_Gdidgits = 0;
   for (int d = 0; d < digits.getEntries(); d++) {
-    if (!(digits[d]->isGood())) continue;
+    if (!(digits[d]->isGood()))
+      continue;
     n_Gdidgits++;
-
     int* buf = new int[2];//for one hit, hit length is 2;
     buf[0] = 0;
     buf[1] = 0;
@@ -85,10 +74,8 @@ void EKLMRawPackerModule::event()
     int   iSector  = digit->getSector();
     int   iPlane   = digit->getPlane();
     int   iStrip   = digit->getStrip();
-
     int   iNPE  = digit->getNPE();
     float fTime    = digit->getTime();
-
 //    float fEDep    = digit->getEDep();
 //    float fMCTime  = digit->getSiPMMCTime();
 //    float fGTime   = digit->getGlobalTime();
@@ -99,20 +86,20 @@ void EKLMRawPackerModule::event()
                    | ((iLayer - 1)   << 2)
                    | ((iForward - 1) << 6);
 //                 | ((iPlane-1)   << 7);            //Do we need plane?
-
     if (m_ModuleIdToelectId.find(moduleId) == m_ModuleIdToelectId.end()) {
-      B2INFO("EKLMRawPacker::can not find in mapping for moduleId " << moduleId << " isForward? " << iForward <<
-             " , layer " << iLayer << " , sector " << iSector  << endl);
-      if (m_useDefaultElectId) electId = 17;     //Defaults: copper=1; finesse=1;
-      else                    continue;
-    } else   electId = m_ModuleIdToelectId[moduleId];
-
+      B2INFO("EKLMRawPacker::can not find in mapping for moduleId " <<
+             moduleId << " isForward? " << iForward << " , layer " << iLayer <<
+             " , sector " << iSector  << endl);
+      if (m_useDefaultElectId)
+        electId = 17;     //Defaults: copper=1; finesse=1;
+      else
+        continue;
+    } else
+      electId = m_ModuleIdToelectId[moduleId];
     int copperId = electId & 0xF;
     int finesse = (electId >> 4) & 0xF;
-
 //    B2INFO("EKLMRawPacker::copperId " << copperId << " F: " << iForward << " Lay: " << iLayer << " Sect: " << iSector << " Pl: " <<
 //           iPlane << " Str: " << iStrip << " Cha: " << iNPE << " T: " << fTime << endl);
-
 //MAKE WORDS WITH INFORMATION
     uint16_t bword1 = 0;
     uint16_t bword2 = 0;
@@ -125,76 +112,62 @@ void EKLMRawPackerModule::event()
     buf[1] |= ((bword3 << 16));
     data_words[copperId][finesse].push_back(buf[0]);
     data_words[copperId][finesse].push_back(buf[1]);
-
     delete [] buf;
   }
-
 //  B2INFO("EKLMRawPackerModule:: N_good_eklmdigits " << n_Gdidgits);
-
   RawCOPPERPackerInfo rawcprpacker_info;
   for (int i = 0 ; i < 4; i++) {
     // Fill event info (These values will be stored in RawHeader )
     rawcprpacker_info.exp_num = 1;
-    rawcprpacker_info.run_subrun_num = 2; // run number : 14bits, subrun # : 8bits
+    /* Run number : 14bits, subrun number : 8bits. */
+    rawcprpacker_info.run_subrun_num = 2;
     rawcprpacker_info.eve_num = n_basf2evt;
-    rawcprpacker_info.node_id = EKLM_ID + i;                               //?????????????????????????????????
+    rawcprpacker_info.node_id = EKLM_ID + i; //?????????????????????????????????
     rawcprpacker_info.tt_ctime = 0x7123456;
     rawcprpacker_info.tt_utime = 0xF1234567;
     rawcprpacker_info.b2l_ctime = 0x7654321;
-
     //one call per copper
     RawKLM* raw_klm = rawklmarray.appendNew();
-
     int* buf1, *buf2, *buf3, *buf4;
     int nwords_1st = data_words[i][0].size();
     int nwords_2nd = data_words[i][1].size();
     int nwords_3rd = data_words[i][2].size();
     int nwords_4th = data_words[i][3].size();
-
     buf1 = NULL;
     buf2 = NULL;
     buf3 = NULL;
     buf4 = NULL;
-    buf1 = new int[ nwords_1st + 1];
-    buf2 = new int[ nwords_2nd + 1];
-    buf3 = new int[ nwords_3rd + 1];
-    buf4 = new int[ nwords_4th + 1];
-
+    buf1 = new int[nwords_1st + 1];
+    buf2 = new int[nwords_2nd + 1];
+    buf3 = new int[nwords_3rd + 1];
+    buf4 = new int[nwords_4th + 1];
     for (int j = 0; j < nwords_1st; j++) {
       buf1[j] = data_words[i][0][j];
     }
     buf1[nwords_1st] = 0; // DIVOT
-
     for (int j = 0; j < nwords_2nd; j++) {
       buf2[j] = data_words[i][1][j];
     }
     buf2[nwords_2nd] = 0; // DIVOT
-
     for (int j = 0; j < nwords_3rd; j++) {
       buf3[j] = data_words[i][2][j];
     }
     buf3[nwords_3rd] = 0; // DIVOT
-
     for (int j = 0; j < nwords_4th; j++) {
       buf4[j] = data_words[i][3][j];
     }
     buf4[nwords_4th] = 0; // DIVOT
-
-
     raw_klm->PackDetectorBuf(buf1, nwords_1st + 1,
                              buf2, nwords_2nd + 1,
                              buf3, nwords_3rd + 1,
                              buf4, nwords_4th + 1,
                              rawcprpacker_info);
-
     delete [] buf1;
     delete [] buf2;
     delete [] buf3;
     delete [] buf4;
   }
-
   B2INFO("Event # " << n_basf2evt);
-
   // Monitor
   if (max_nevt >= 0) {
     if (n_basf2evt >= max_nevt && max_nevt > 0) {
@@ -203,29 +176,38 @@ void EKLMRawPackerModule::event()
       m_eventMetaDataPtr->setEndOfData();
     }
   }
-
   n_basf2evt++;
   return;
 }
 
-
-
+/*
+ * Data format:
+ * Word 1: Bit 1 - empty.
+ *         Bit 2 - endcap number.
+ *         Bits 3-6 - layer number.
+ *         Bits 7-8 - sector number.
+ *         Bit 9 - plane number.
+ *         Bits 10-16 - strip number.
+ * Word 2: 15 bits of time.
+ * Word 3: Want to write TDC in 15 bits or less.
+ * Word 4: 15 bits of charge.
+ */
 void EKLMRawPackerModule::formatData(
   int endcap, int layer, int sector, int plane, int strip, int charge,
   float ctime, uint16_t& bword1, uint16_t& bword2, uint16_t& bword3,
   uint16_t& bword4)
 {
-  bword1 = 0; //Mapping(1st bit - empty; 2nd bit - forward; 3,4,5,6 bits - layer; 7,8 - sector; 9 - plane; 10,11,12,13,14,15,16 - strip)
-  bword2 = 0; //15 bits of time
-  bword3 = 0; //Want to write TDC in 15 bits or less
-  bword4 = 0; //15 bits of charge
+  bword1 = 0;
+  bword2 = 0;
+  bword3 = 0;
+  bword4 = 0;
   bword1 |= (strip & 0x7F);
   bword1 |= (((plane - 1) & 1) << 7);
   bword1 |= (((sector - 1) & 3) << 8);
   bword1 |= (((layer - 1) & 0xF) << 10);
   bword1 |= (((endcap - 1) & 1) << 14);
   bword2 |= (((int)ctime) & 0x7FFF);
-//  bword3 |= (TDC & 0x7FFF);                              //DO WE NEED TDC ????????????????
+  //bword3 |= (TDC & 0x7FFF); //DO WE NEED TDC ????????????????
   bword4 |= (charge & 0x7FFF);
 }
 
@@ -237,11 +219,12 @@ void EKLMRawPackerModule::loadMap()
         //Do we need loop over planes as well?
         int copperId = 1;
         int finesseId = 1;
-
-        //Here we somehow get coppers and finesses from the position of sector, layer, etc.
+        /*
+         * Here we somehow get coppers and finesses from the position of
+         * sector, layer, etc.
+         */
         //copper=?????????????????????????????????????????????
         //finesse=?????????????????????????????????????????????
-
         int elecid = (copperId & 0xF) | ((finesseId & 0xF) << 4);
         int moduleId = (sector - 1)
                        | ((layer - 1)   << 2)
@@ -253,3 +236,4 @@ void EKLMRawPackerModule::loadMap()
     }
   }
 }
+

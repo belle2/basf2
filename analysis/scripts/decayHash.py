88110cf95a7e8b6e15b287f85832ec45bbe6998f
Thomas Keck <thomas.keck2@kit.edu>
2016-11-15 10:34:46 +0100

    Added documentation to decayHash script in analysis package

--- analysis/scripts/decayHash.py
+++ analysis/scripts/decayHash.py
@@ -19,11 +19,29 @@ import collections
 
 DecayNode = collections.namedtuple('DecayNode', "p,d,m")
 DecayNode.__new__.__defaults__ = (0, None, False)
+DecayNode.__doc__ = """
+    DecayNode describes the decay of a particle p, into
+    list of daughters d. In addition it can have list of connections
+    or match m to another DecayNode (e.g. between MC and reconstructed tree)
+    """
 
 
 class DecayTree(object):
+    """
+    Consists of a tree of DecayNodes.
+    Can be constructed from the output of the ParticleMCDecayString module
+    """
     def __init__(self, decaystring):
+        """
+        Builds a new DecayTree using a decaystring.
+        The string should look like this:
+         300553 (--> -521 (--> 15 (--> 11)) 521 (--> -423 (--> -421 (--> 321 -211) 111 (--> 22 22)) -13))
+        @param decaystring part of the output of the ParticleMCDecayString
+        """
+        #: List of decay nodes in the order of their appearance in the decay string for fast access
         self.particles = []
+        #: Decay nodes of the tree, for a well formed decaystring the list should have only one node
+        self.nodes = []
         if 'No match' in decaystring:
             self.nodes = [DecayNode()]
         else:
@@ -32,6 +50,11 @@ class DecayTree(object):
             _, self.nodes = self.build_tree(decaylist)
 
     def build_tree(self, decaylist, i=0):
+        """
+        Recursively build a new tree
+        @param decaylist linear list of tokens from the decaystring
+        @param i position of current token
+        """
         nodes = []
         while i < len(decaylist):
             x = decaylist[i]
@@ -48,6 +71,17 @@ class DecayTree(object):
         return i, nodes
 
     def nodes_match(self, node1, node2):
+        """
+        Check if two DecayNodes are compatible.
+        To be compatible they must:
+        - Have the same mother particle p
+        - Have the same number of daughters particles or one of the daughter particles list have to be None
+        - The daughter particles have to fullfill the same conditions pairwise
+        In particular the order auf the daughter particles matter!
+          423 (--> 421 13) is not the same as 423 (--> 13 421)
+        @param node1 first node
+        @param node2 second node
+        """
         if node1.p == node2.p:
             if node1.d is None or node2.d is None:
                 return True
@@ -58,6 +92,15 @@ class DecayTree(object):
         return False
 
     def find_decay(self, decay, nodes=None):
+        """
+        Check if the decay tree contains the given decay tree.
+        @param decay DecayTree or DecayNode object describing the decay
+        @parma nodes used to call the method recursively
+        """
+        if isinstance(decay, DecayTree):
+            if len(decay.nodes) != 1:
+                raise RuntimeError("Given decay tree does not contain exactly one decay!")
+            decay = decay.nodes[0]
         if nodes is None:
             nodes = self.nodes
         for node in nodes:
@@ -68,6 +111,9 @@ class DecayTree(object):
         return False
 
     def print_node(self, node, indent=0):
+        """
+        Recirsively print DecayNode with some indent
+        """
         output = '  ' * indent
         output += str(node.p) + '\n'
         for d in node.d:
@@ -75,6 +121,9 @@ class DecayTree(object):
         return output
 
     def __str__(self):
+        """
+        Convert DecayTree to a string
+        """
         output = ''
         for node in self.nodes:
             output += self.print_node(node) + '\n'
@@ -82,11 +131,26 @@ class DecayTree(object):
 
 
 def _bitwiseConversion(value, i='f', o='i'):
+    """
+    Bitwise conversion between to python types
+    This is equivalently to
+    union {
+        Type_i i;
+        Type_o o;
+    } conversion;
+    conversion.i = input;
+    return conversion.o
+    @param i input data type (e.g. f for float)
+    @param o output data type (e.g. i for integer)
+    """
     s = struct.pack('>' + i, value)
     return struct.unpack('>' + o, s)[0]
 
 
 def _decayHashFloatToInt(decayHash, decayHashExtended):
+    """
+    Convert decayHash and decayHashExtended 32 bit floats to an 64 bit integer
+    """
     decayHashInt = _bitwiseConversion(np.float32(decayHash))
     decayHashExtendedInt = _bitwiseConversion(np.float32(decayHashExtended))
     decayHashFullInt = decayHashInt << 32
@@ -95,11 +159,22 @@ def _decayHashFloatToInt(decayHash, decayHashExtended):
 
 
 class DecayHashMap(object):
+    """
+    DecayHashMap contains all the DecayTrees for all decays written out
+    by the ParticleMCDecayString module
+    """
     def __init__(self, rootfile):
+        """
+        Create a new DecayHashMap using a root file
+        @param rootfile a ROOT file outputted by the ParticleMCDecayString module
+        """
         import root_numpy
         ntuple = root_numpy.root2array(rootfile)
+        #: Dict Int -> DecayStrings
         self._string = {}
+        #: Dict Int -> Reconstructed DecayTree
         self._reconstructed_decay = {}
+        #: Dict Int -> Original (MC) Decay Tree
         self._original_decay = {}
         for decayHash, decayHashExtended, decayString in ntuple:
             decayInt = _decayHashFloatToInt(decayHash, decayHashExtended)
@@ -121,15 +196,35 @@ class DecayHashMap(object):
                     self._original_decay[decayInt].particles[k].m.append(self._reconstructed_decay[decayInt].particles[j])
 
     def get_string(self, decayHash, decayHashExtended):
+        """
+        Return DecayString given the decayHash and decayHashExtended
+        @param decayHash output of extraInfo(decayHash)
+        @param decayHashExtended output of extraInfo(decayHashExtended)
+        """
         return self._string[_decayHashFloatToInt(decayHash, decayHashExtended)]
 
     def get_original_decays(self, decayHash, decayHashExtended):
+        """
+        Return original (MC) DecayTree given the decayHash and decayHashExtended
+        @param decayHash output of extraInfo(decayHash)
+        @param decayHashExtended output of extraInfo(decayHashExtended)
+        """
         return self._original_decay[_decayHashFloatToInt(decayHash, decayHashExtended)]
 
     def get_reconstructed_decay(self, decayHash, decayHashExtended):
+        """
+        Return reconstructed DecayTree given the decayHash and decayHashExtended
+        @param decayHash output of extraInfo(decayHash)
+        @param decayHashExtended output of extraInfo(decayHashExtended)
+        """
         return self._reconstructed_decay[_decayHashFloatToInt(decayHash, decayHashExtended)]
 
     def print_hash(self, decayHash, decayHashExtended):
+        """
+        Print the DecayString in a fancy way given the decayHash and decayHashExtended
+        @param decayHash output of extraInfo(decayHash)
+        @param decayHashExtended output of extraInfo(decayHashExtended)
+        """
         entry = self.get_string(decayHash, decayHashExtended)
         entries = entry.split('|')
         all_particles = re.findall(r"(-?[0-9]+)", entries[0])
@@ -147,7 +242,11 @@ class DecayHashMap(object):
         basf2.pretty_print_table(table, column_widths=[6, '*'])
 
 
-def pdg_to_name(x):
+def _pdg_to_name(x):
+    """
+    Convert PDG code to a name
+    @param a pdg code
+    """
     selected = False
     if x[0] == '^':
         selected = True
@@ -169,7 +268,12 @@ def pdg_to_name(x):
 
 
 def prettify_pdg_codes(text):
-    text = re.sub(r"(\^?-?[0-9]+)", lambda x: pdg_to_name(x.group(0)), text)
+    """
+    Prettifiy a string containing PDG codes by replacing PDG codes
+    with their corresponding names.
+    @param text the text
+    """
+    text = re.sub(r"(\^?-?[0-9]+)", lambda x: _pdg_to_name(x.group(0)), text)
     text = text.replace('gamma', 'g').replace('--> ', '').replace('anti-', 'a-')
     text = text.replace('Upsilon', 'Y').replace(') ', ')').replace(' (', '(')
     return text

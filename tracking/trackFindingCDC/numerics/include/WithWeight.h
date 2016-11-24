716f14ec464f22c2bf38c333e6cd0509b57ea68e
Oliver Frost <oliver.frost@desy.de>
2016-11-24 21:05:18 +0100

    Replacing Ptr wrapper class with more general Scalar wrapper class.

--- tracking/trackFindingCDC/numerics/include/WithWeight.h
+++ tracking/trackFindingCDC/numerics/include/WithWeight.h
@@ -12,30 +12,35 @@
 
 #include <tracking/trackFindingCDC/numerics/Weight.h>
 
-#include <tracking/trackFindingCDC/utilities/Ptr.h>
+#include <tracking/trackFindingCDC/utilities/Scalar.h>
 
 namespace Belle2 {
   namespace TrackFindingCDC {
 
     /// A mixin class to attach a weight to an object.
     template<class T>
-    class WithWeight : public StarToPtr<T> {
+    class WithWeight : public ScalarToClass<T> {
 
     private:
       /// Type of the base class
-      using Super = StarToPtr<T>;
+      using Super = ScalarToClass<T>;
 
     public:
       /// Make the constructor of the base type available
       using Super::Super;
 
       /// Also forward the copy constructor form the base class object.
-      explicit WithWeight(const T& t): Super(t) {}
+      explicit WithWeight(const T& t)
+        : Super(t)
+      {
+      }
 
       /// Constructor which may also initialise the weight to a desired value
-      WithWeight(const T& t, Weight weight):
-        Super(t), m_weight(weight)
-      {}
+      WithWeight(const T& t, Weight weight)
+        : Super(t)
+        , m_weight(weight)
+      {
+      }
 
       /// Comparison operator establishing an ordering considering the pointer first and the weight second
       bool operator<(const WithWeight<T>& other) const
@@ -57,10 +62,15 @@ namespace Belle2 {
         m_weight = weight;
       }
 
+      /// Mutable getter for the weight
+      Weight& weight()
+      {
+        return m_weight;
+      }
+
     private:
       /// Memory for the weight.
       Weight m_weight = 0;
-
     };
   }
 }

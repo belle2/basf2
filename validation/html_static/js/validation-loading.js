4afe3c5f18d78e81a6121acb20977b8e990b8910
Thomas Hauth <Thomas.Hauth@desy.de>
2016-11-24 19:31:26 +0100

    added protections is case no tags are selected

--- validation/html_static/js/validation-loading.js
+++ validation/html_static/js/validation-loading.js
@@ -241,6 +241,10 @@ function getNewestRevision( rev_data) {
 
 function setupRactiveFromRevision(rev_data, rev_string, rev_list)
 {
+	// don't event attempt to show comparisons for empty revisions
+	if (rev_string == "")
+		return;
+	
     // make dynamic
     var comparisonLoadPath = "../comparisons/" + rev_string
     var createComparisonUrl = "../create_comparison"
@@ -327,9 +331,18 @@ function setupRactiveFromRevision(rev_data, rev_string, rev_list)
     });
 }
 
+/* this function call is triggered by the button 
+   under the revisions list
+*/
 function loadSelectedRevisions(data) {
+	
     rev_string = get_selected_revs_string();
     rev_list = get_selected_revs_list();
+    
+    if (rev_string == "") {
+    	alert("Please select at least one tag!");
+    } 
+    
     console.log("Loading rev via string " + rev_string );
 
     setupRactiveFromRevision(data, rev_string, rev_list);

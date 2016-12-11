75aaaf4969308bc209e1628401208ecf27ed87e2
Oliver Frost <oliver.frost@desy.de>
2016-12-11 01:13:04 +0100

    Moving the assignment logic of variant parameters to a module parameter list to its own file.

---
+++ tracking/trackFindingCDC/utilities/include/ParameterVariant.h
@@ -0,0 +1,56 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+#include <boost/variant.hpp>
+#include <string>
+#include <vector>
+#include <map>
+
+namespace Belle2 {
+  class ModuleParamList;
+
+  namespace TrackFindingCDC {
+
+    /// Type variant with allowed parameters types
+    using ParameterVariant =
+      boost::variant<bool, int, double, std::string, std::vector<double>, std::vector<std::string>>;
+
+    /// A helper class to unpack a boost::variant parameter value and set it in the parameter list.
+    struct AssignParameterVisitor : public boost::static_visitor<> {
+
+      template <class ... T>
+      static void update(ModuleParamList* moduleParamList,
+                         const std::map<std::string, boost::variant<T...> >& valuesByName)
+      {
+        for (auto& nameAndValue : valuesByName) {
+          const std::string& name = nameAndValue.first;
+          const boost::variant<T...>& value = nameAndValue.second;
+          AssignParameterVisitor visitor(moduleParamList, name);
+          boost::apply_visitor(visitor, value);
+        }
+      }
+
+      /// Constructor taking the module parameter list and the name of the parameter to be set from the boost::variant.
+      AssignParameterVisitor(ModuleParamList* moduleParamList, const std::string& paramName);
+
+      /// Function call that receives the parameter value from the boost::variant with the correct type.
+      template <class T>
+      void operator()(const T& t) const;
+
+    private:
+      /// Parameter list which contains the parameter to be set
+      ModuleParamList* m_moduleParamList;
+
+      /// Name of the parameter to be set.
+      std::string m_paramName;
+    };
+  }
+}

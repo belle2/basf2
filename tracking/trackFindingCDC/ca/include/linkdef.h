6e31488a291d372a935e3ec856083fef62f22f4d
Oliver Frost <oliver.frost@desy.de>
2016-12-12 21:38:44 +0100

    Moving linkdef definitions next to their actual data type.

--- tracking/trackFindingCDC/ca/include/linkdef.h
+++ tracking/trackFindingCDC/ca/include/linkdef.h
@@ -10,33 +10,4 @@
 
 #pragma link C++ class Belle2::TrackFindingCDC::AutomatonCell+;
 
-#pragma extra_include <tracking/trackFindingCDC/ca/WeightedRelation.h>;
-
-/// Some dictionaries for weighted relations that will be needed
-#pragma extra_include <tracking/trackFindingCDC/eventdata/hits/CDCWireHit.h>;
-#pragma extra_include <tracking/trackFindingCDC/eventdata/hits/CDCFacet.h>;
-#pragma extra_include <tracking/trackFindingCDC/eventdata/segments/CDCSegment2D.h>;
-#pragma extra_include <tracking/trackFindingCDC/eventdata/tracks/CDCSegmentPair.h>;
-#pragma extra_include <tracking/trackFindingCDC/eventdata/tracks/CDCSegmentTriple.h>;
-#pragma extra_include <tracking/trackFindingCDC/eventdata/tracks/CDCTrack.h>;
-
-#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCWireHit>+;
-#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<Belle2::TrackFindingCDC::CDCWireHit> >+;
-
-#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCFacet>+;
-#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCFacet> >+;
-
-#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCSegment2D>+;
-#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCSegment2D> >+;
-
-#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCSegmentPair>+;
-#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCSegmentPair> >+;
-
-#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCSegmentTriple>+;
-#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCSegmentTriple> >+;
-
-#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCTrack>+;
-#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCTrack> >+;
-
 #endif
-
--- tracking/trackFindingCDC/eventdata/tracks/include/linkdef.h
+++ tracking/trackFindingCDC/eventdata/tracks/include/linkdef.h
@@ -19,5 +19,30 @@
 #pragma link C++ class std::vector<Belle2::TrackFindingCDC::CDCSegmentTriple>+;
 #pragma link C++ class std::vector<Belle2::TrackFindingCDC::CDCTrack>+;
 
-#endif
+// Some dictionaries for WeightedRelations that will be needed
+#pragma extra_include <tracking/trackFindingCDC/ca/WeightedRelation.h>;
 
+#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCSegmentPair>+; // implicit
+#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCSegmentPair> >+; // implicit
+
+#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCSegmentTriple>+; // implicit
+#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCSegmentTriple> >+; // implicit
+
+#pragma link C++ class Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCTrack>+; // implicit
+#pragma link C++ class std::vector<Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCTrack> >+; // implicit
+
+// Define IO types as we want to put them in the DataStore
+#pragma extra_include <tracking/trackFindingCDC/rootification/StoreWrapper.h>;
+
+#pragma link C++ class Belle2::TrackFindingCDC::StoreWrapper<std::vector<Belle2::TrackFindingCDC::CDCSegmentPair> >+; // implicit
+#pragma link C++ class Belle2::TrackFindingCDC::StoreWrapper<std::vector<Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCSegmentPair> > >+; // implicit
+
+#pragma link C++ class Belle2::TrackFindingCDC::StoreWrapper<std::vector<Belle2::TrackFindingCDC::CDCAxialSegmentPair> >+; // implicit
+
+#pragma link C++ class Belle2::TrackFindingCDC::StoreWrapper<std::vector<Belle2::TrackFindingCDC::CDCSegmentTriple> >+; // implicit
+#pragma link C++ class Belle2::TrackFindingCDC::StoreWrapper<std::vector<Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCSegmentTriple> > >+; // implicit
+
+#pragma link C++ class Belle2::TrackFindingCDC::StoreWrapper<std::vector<Belle2::TrackFindingCDC::CDCTrack> >+; // implicit
+#pragma link C++ class Belle2::TrackFindingCDC::StoreWrapper<std::vector<Belle2::TrackFindingCDC::WeightedRelation<const Belle2::TrackFindingCDC::CDCTrack> > >+; // implicit
+
+#endif

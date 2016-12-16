a439b3df141d6f0f5d21bca706a583e4394178dc
Martin Ritter <martin.ritter@desy.de>
2016-12-16 16:35:40 +0100

    More merge_basf2_file tests
    We now also check that
     - NEvents and McEvents gets summed correctly
     - parentLFNs are combined correctly (set of all seend parentLFNs)
     - low/high event numbers are set correctly
     - merged file passes check_basf2_file

--- framework/tests/merge_files.py
+++ framework/tests/merge_files.py
@@ -4,8 +4,10 @@
 import os
 import sys
 import subprocess
+import itertools
 import tempfile
 import ROOT
+from ROOT.Belle2 import FileMetaData, EventMetaData
 from contextlib import contextmanager
 # we don't really need basf2 but it fixes the print buffering problem
 import basf2
@@ -40,6 +42,30 @@ def create_testfile(name, steering, release=None, exp=0, run=0, events=100, bran
 
     subprocess.call(["basf2", "-o", name, "--experiment", str(exp), "--run", str(run),
                     "-n", str(events), steering_file] + branchNames, env=env)
+
+
+def create_testfile_direct(name, metadata):
+    """similar to create_testfile but does it manually without running basf2 for
+    full control over the FileMetaData"""
+    f = ROOT.TFile(name, "RECREATE")
+    t = ROOT.TTree("persistent", "persistent")
+    t.Branch("FileMetaData", metadata)
+    t.Fill()
+    t.Write()
+    t = ROOT.TTree("tree", "tree")
+    event_meta = ROOT.Belle2.EventMetaData()
+    t.Branch("EventMetaData", event_meta)
+    t.Fill()
+    t.Write()
+    f.Close()
+
+
+def get_metadata(name="output.root"):
+    """Get the metadata out of a root file"""
+    out = ROOT.TFile(name)
+    t = out.Get("persistent")
+    t.GetEntry(0)
+    return FileMetaData(t.FileMetaData)
 
 
 def merge_files(*args, output="output.root"):
@@ -139,7 +165,7 @@ def check_12_noeventtree():
     """Check that merging fails without event tree"""
     f = ROOT.TFile("test1.root", "RECREATE")
     t = ROOT.TTree("persistent", "persistent")
-    meta = ROOT.Belle2.FileMetaData()
+    meta = FileMetaData()
     t.Branch("FileMetaData", meta)
     t.Fill()
     t.Write()
@@ -151,7 +177,7 @@ def check_13_noeventbranches():
     """Check that merging fails without event tree"""
     f = ROOT.TFile("test1.root", "RECREATE")
     t = ROOT.TTree("persistent", "persistent")
-    meta = ROOT.Belle2.FileMetaData()
+    meta = FileMetaData()
     t.Branch("FileMetaData", meta)
     t.Fill()
     t.Write()
@@ -165,7 +191,7 @@ def check_14_nonmergeable():
     """Check that merging fails without persistent tree"""
     f = ROOT.TFile("test1.root", "RECREATE")
     t = ROOT.TTree("persistent", "persistent")
-    meta = ROOT.Belle2.FileMetaData()
+    meta = FileMetaData()
     t.Branch("FileMetaData", meta)
     t.Branch("AnotherMetaData", meta)
     t.Fill()
@@ -178,11 +204,100 @@ def check_14_nonmergeable():
     return merge_files("test1.root") != 0
 
 
+def check_15_checkparentLFN():
+    """Check that parent LFN get merged correctly"""
+    parents = [("a", "b", "c"), ("a", "c", "d")]
+    m1 = FileMetaData()
+    m2 = FileMetaData()
+    lfn1 = ROOT.std.vector("std::string")()
+    lfn2 = ROOT.std.vector("std::string")()
+    for e in parents[0]:
+        lfn1.push_back(e)
+    for e in parents[1]:
+        lfn2.push_back(e)
+    m1.setParents(lfn1)
+    m2.setParents(lfn2)
+    m1.setRandomSeed("1")
+    m2.setRandomSeed("2")
+    create_testfile_direct("test1.root", m1)
+    create_testfile_direct("test2.root", m2)
+    merge_files("test1.root", "test2.root")
+    meta = get_metadata()
+    should_be = [e for e in sorted(set(parents[0] + parents[1]))]
+    is_actual = [meta.getParent(i) for i in range(meta.getNParents())]
+    return should_be == is_actual
+
+
+def check_16_checkEventNr():
+    """Check that event and mc numbers are summed correctly"""
+    evtNr = [10, 1243, 232, 1272, 25]
+    mcNr = [120, 821, 23, 923, 1]
+    files = []
+    for i, (e, m) in enumerate(zip(evtNr, mcNr)):
+        meta = FileMetaData()
+        meta.setNEvents(e)
+        meta.setMcEvents(m)
+        meta.setRandomSeed(str(i))
+        files.append("test%d.root" % i)
+        create_testfile_direct(files[-1], meta)
+    merge_files(*files)
+    meta = get_metadata()
+    return sum(evtNr) == meta.getNEvents() and sum(mcNr) == meta.getMcEvents()
+
+
+def check_17_lowhigh():
+    """Check that the low/high event numbers are merged correctly"""
+    lowhigh = [
+        (0, 0, 0),
+        (0, 0, 1),
+        (0, 1, 0),
+        (1, 0, 0),
+        (1, 1, 1),
+    ]
+    files = []
+    for i, e in enumerate(lowhigh):
+        meta = FileMetaData()
+        meta.setRandomSeed(str(i))
+        meta.setLow(e[0], e[1], e[2])
+        meta.setHigh(e[0], e[1], e[2])
+        files.append("test%d.root" % i)
+        create_testfile_direct(files[-1], meta)
+
+    # test all possible combinations taking 2 elements from the list plus the
+    # full list in one go
+    indices = range(len(files))
+    tests = list(itertools.permutations(indices, 2)) + [indices]
+    for indices in tests:
+        low = min(lowhigh[i] for i in indices)
+        high = max(lowhigh[i] for i in indices)
+        if merge_files("-f", "--no-catalog", *(files[i] for i in indices)) != 0:
+            return False
+        meta = get_metadata()
+        if meta.getExperimentLow() != low[0] or meta.getRunLow() != low[1] or meta.getEventLow() != low[2]:
+            print("low event should be", low)
+            meta.Print()
+            return False
+        if meta.getExperimentHigh() != high[0] or meta.getRunHigh() != high[1] or meta.getEventHigh() != high[2]:
+            print("high event should be", high)
+            meta.Print()
+            return False
+    return True
+
+
+def check_18_test_file():
+    """Check that a merged file passes the check_basf2_file program"""
+    create_testfile("test1.root", steering, events=1111)
+    create_testfile("test2.root", steering, events=123)
+    merge_files("test1.root", "test2.root")
+    return subprocess.call(["check_basf2_file", "-n", "1234", "--mcevents", "1234",
+                            "output.root", "EventMetaData", "MCParticles"]) == 0
+
+
 def check_XX_filemetaversion():
     """Check that the Version of the FileMetaData hasn't changed.
     If this check fails please check that the changes to FileMetaData don't
     affect merge_basf2_files and adapt the correct version number here."""
-    return ROOT.Belle2.FileMetaData.Class().GetClassVersion() == 8
+    return FileMetaData.Class().GetClassVersion() == 8
 
 
 if __name__ == "__main__":

23424b739ec05d74701359838f07075c1973f50f
Makoto Uchida <uchida@phys.titech.ac.jp>
2016-12-18 17:23:19 +0900

    Update the CDCHit object to enable the BG overlay.

--- cdc/dataobjects/src/CDCHit.cc
+++ cdc/dataobjects/src/CDCHit.cc
@@ -22,3 +22,24 @@ CDCHit::CDCHit(unsigned short tdcCount, unsigned short charge,
   setTDCCount2ndHit(tdcCount2ndHit);
   setStatus(status);
 }
+
+
+DigitBase::EAppendStatus CDCHit::addBGDigit(const DigitBase* bg)
+{
+  const auto* bgDigit = static_cast<const CDCHit*>(bg);
+
+  int diff  = static_cast<int>(m_tdcCount) - static_cast<int>(bgDigit->getTDCCount());
+
+  // If the BG hit is faster than the true hit, the TDC count is replaced,
+  // and the true hit is moved to the second TDC count.
+  // ADC counts are summed.
+  if (diff < 0) {
+    m_tdcCount2ndHit = m_tdcCount;
+    m_tdcCount = bgDigit->getTDCCount();
+  } else {
+    m_tdcCount2ndHit = bgDigit->getTDCCount();
+  }
+  m_adcCount += bgDigit->getADCCount();
+  return DigitBase::c_DontAppend;
+
+}

08821d4c13b31fd62b532fb66bd9abd80d2853df
Makoto Uchida <uchida@phys.titech.ac.jp>
2016-12-21 18:01:55 +0900

    Add scripts to study the performance of the CDC with cosmic data.

---
+++ cdc/examples/performance/submitAll.py
@@ -0,0 +1,21 @@
+import glob
+import subprocess
+
+'''
+Simple script submitting jobs to process all data.
+Usage:
+python3 submitAll.py
+'''
+
+# Experimantl number
+exp = 'exp00'
+# Data directory
+dir = '/ghi/fs01/belle2/bdata/group/detector/CDC/unpacked/'
+
+files = glob.glob(dir + exp + '/cr.cdc.*.root')
+for f in files:
+    input = f  # input file name.
+    name = f.split('/')
+    output = 'output_' + name[-1]  # output file name.
+    #    print('basf2 runReconstruction.py '+'input='+input+' output='+output)
+    subprocess.run(["bsub", "-q", "l", "basf2", "runReconstruction.py", input, output])

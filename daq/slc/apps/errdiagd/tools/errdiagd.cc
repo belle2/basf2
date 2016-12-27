ef457fa71a11316f1d2a7d1cbaadc3bc79412c6b
Satoru YAMADA <satoru.yamada@kek.jp>
2016-12-27 11:06:32 +0900

    Modify constructor for errdiagd.

--- daq/slc/apps/errdiagd/tools/errdiagd.cc
+++ daq/slc/apps/errdiagd/tools/errdiagd.cc
@@ -10,17 +10,14 @@ using namespace Belle2;
 
 int main(int argc, char** argv)
 {
-  if (Daemon::start(argv[1], argc, argv, 2, "<config> <xml path>")) {
+  if (Daemon::start(argv[1], argc, argv, 2, "<config>")) {
     ConfigFile config("slowcontrol", argv[1]);
     std::string host = config.get("nsm.host");
     int port = config.getInt("nsm.port");
     std::string name = config.get("nsm.nodename");
     int timeout = config.getInt("timeout");
     if (timeout <= 0) timeout = 5;
-    //    std::string xmlpath = "/home/usr/yamadas/slc/database/tools/temp2.xml";
-    std::string xmlpath = argv[2];
-    //    printf("errdiag : %s\n", xmlpath.c_str() );
-    NSMNodeDaemon(new ErrdiagCallback(name, xmlpath), host, port).run();
+    NSMNodeDaemon(new ErrdiagCallback(name), host, port).run();
   }
   return 0;
 }

8aece12e09885e78a736ab4881588bdfe0f9b587
Nils Braun <nils.braun@desy.de>
2017-01-16 10:18:19 +0100

    Use the new any algorithm and do not remove the doubled hits but just mark them as masked

--- tracking/trackFindingCDC/collectors/selectors/include/SegmentTrackBestMatchSelector.h
+++ tracking/trackFindingCDC/collectors/selectors/include/SegmentTrackBestMatchSelector.h
@@ -12,6 +12,7 @@
 #include <tracking/trackFindingCDC/collectors/selectors/SingleMatchSelector.h>
 #include <tracking/trackFindingCDC/eventdata/tracks/CDCTrack.h>
 #include <tracking/trackFindingCDC/eventdata/segments/CDCSegment2D.h>
+#include <tracking/trackFindingCDC/utilities/Algorithms.h>
 
 namespace Belle2 {
   namespace TrackFindingCDC {
@@ -20,35 +21,33 @@ namespace Belle2 {
      which should later be merged.
      It inherits the functionality of the SingleMatchSelector, meaning it will only output those
      combinations, which are best for all segments and tracks.
-     Additionaly, it will remove all shared hits between segments and tracks, which are *not* part of the best
-     combinations. This means, if the algorithm determines that a segment should belong to a certain track,
-     all hits that are part of other tracks are removed from those tracks. This increases the hit purity.
+     Additionaly, it will mark all shared hits between segments and tracks, which are *not* part of the best
+     combinations as masked. This means, if the algorithm determines that a segment should belong to a certain track,
+     all hits that are part of other tracks can later be removed from those tracks. This will increase the hit purity.
      */
     class SegmentTrackBestMatchSelector : public SingleMatchSelector<CDCTrack, CDCSegment2D> {
       /// The parent class
       using Super = SingleMatchSelector<CDCTrack, CDCSegment2D>;
 
     public:
-      /// Remove all hits that are in another segment
+      /**
+       * Mark all hits that are in another, non-optimal segment as masked. This information will later be used to
+       * remove those hits from the tracks.
+       */
       void resetNonBestMatches(const typename Super::WeightedRelationItem& notBestMatch) override
       {
         CDCTrack& track = *(notBestMatch.getFrom());
         const CDCSegment2D& segment = *(notBestMatch.getTo());
 
-        // This part is a bit ugly: what we want to do is to remove all hits in the track, which are also part
-        // in the segment. Additionally, we unset the taken flag of these hits.
-        const auto& hitIsPartOfSegment = [&segment](const CDCRecoHit3D & trackWireHit) {
-          bool trackHitIsInSegment = std::find_if(segment.begin(), segment.end(), [&trackWireHit](const CDCRecoHit2D & segmentWireHit) {
+        for (const CDCRecoHit3D& trackWireHit : track) {
+          const auto& hitIsInSegment = [&trackWireHit](const CDCRecoHit2D & segmentWireHit) {
             return segmentWireHit.getWireHit() == trackWireHit.getWireHit();
-          }) != segment.end();
+          };
+          const bool trackHitIsInSegment = any(segment, hitIsInSegment);
           if (trackHitIsInSegment) {
-            trackWireHit.getWireHit().getAutomatonCell().unsetTakenFlag();
+            trackWireHit.getWireHit().getAutomatonCell().setMaskedFlag();
           }
-
-          return trackHitIsInSegment;
         };
-
-        erase_remove_if(track, hitIsPartOfSegment);
       }
     };
   }

fb76039356cf3cf3afb5415cc4b582ca2131274e
Oliver Frost <oliver.frost@desy.de>
2017-01-18 01:28:39 +0100

    Remove operator-> from CDCFacet and CDCTangent

--- tracking/trackFindingCDC/eventdata/hits/include/CDCTangent.h
+++ tracking/trackFindingCDC/eventdata/hits/include/CDCTangent.h
@@ -64,14 +64,6 @@ namespace Belle2 {
         return output;
       }
 
-      /// Access the object methods and methods from a pointer in the same way.
-      /** In situations where the type is not known to be a pointer or a reference there is no way to tell
-       *  if one should use the dot '.' or operator '->' for method look up.
-       *  So this function defines the -> operator for the object.
-       *  No matter you have a pointer or an object access is given with '->'.*/
-      const CDCTangent* operator->() const
-      { return this; }
-
       /// Getter for the touching point of the tangent to the first drift circle.
       const Vector2D& getFromRecoPos2D() const
       { return getLine().support(); }

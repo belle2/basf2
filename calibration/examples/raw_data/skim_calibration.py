5a622348f1f5a5c780b542557bb2411550fc0919
David Dossett <david.dossett@desy.de>
2017-01-18 18:14:55 +1100

    Some initial RAW data production/flow scripts for getting calibration flagged events out of SROOT data during conversion. The TriggerSkim module may be a placeholder until the hlt package supports something similar, or needs error handling for missing lines if it's to be used.

---
+++ calibration/examples/raw_data/skim_calibration.py
@@ -0,0 +1,44 @@
+# Add the cut module to the pat#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# An example of how to skim off events that pass a set of trigger lines (using a custom module for now)
+# and how to also convert SROOT->ROOT at the same time. This is roughly the planned data flow after the DAQ
+# finishes with the data and outputs SROOT files. We want to copy/skim off calibration events (muon trigger here
+# is the stand-in since the calib triggers aren't ready) into their own ROOT files while allowing ALL events
+# to be output to RAW ROOT files.
+
+# The TriggerSkim module allows us to query SoftwareTriggerResult objects in the RAW output without having to
+# re-do the reconstruction/calculation again i.e. using the Trigger decisions already done by the HLT.
+# Notice that we NEVER UNPACK the RAW data during this step, making this very fast.
+
+from basf2 import *
+from ROOT import Belle2
+
+# Create path
+main = create_path()
+
+# Root input
+roinput = register_module('SeqRootInput')
+main.add_module(roinput)
+
+# Output only events that pass
+output_calib_path = create_path()
+output = register_module('RootOutput')
+output.param('outputFileName', "calib_mumu.root")
+output_calib_path.add_module(output)
+
+cut_decision = register_module("TriggerSkim")
+cut_decision.param('triggerLines', ['software_trigger_cut&hlt&accept_2_tracks',
+                                    'software_trigger_cut&hlt&accept_mu_mu'])
+cut_decision.if_value("==1", output_calib_path, AfterConditionPath.CONTINUE)
+main.add_module(cut_decision)
+
+# Output all events but in ROOT (not SeqROOT) format
+output_path = create_path()
+output = register_module('RootOutput')
+output.param('outputFileName', "raw.root")
+main.add_module(output)
+
+# Process events
+process(main)
+print(statistics)

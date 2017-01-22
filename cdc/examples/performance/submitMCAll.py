1a6041454d7a054a282ebd21cedf6cd174b82711
Makoto Uchida <uchida@phys.titech.ac.jp>
2017-01-22 17:37:17 +0900

    Change the arg of bsub command.

--- cdc/examples/performance/submitMCAll.py
+++ cdc/examples/performance/submitMCAll.py
@@ -9,4 +9,4 @@ python3 submitAll.py
 
 for i in range(100):
     subprocess.run(["bsub", "-q", "l", "basf2", "runSimulation+Reconstruction.py",
-                    str(i), '10000', '201609'])
+                    str(i), '10000'])

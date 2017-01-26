af36ab90ea3c8dffdb8d33c3067f6a23668b0ad0
tlueck <lueck@pi.infn.it>
2017-01-26 19:18:54 +0100

    forgot to add some files in my last commit

---
+++ tracking/trackFindingVXD/filterTools/src/ObserverCheckFilters.cc
@@ -0,0 +1,23 @@
+/**************************************************************************
+* BASF2 (Belle Analysis Framework 2)                                     *
+* Copyright(C) 2016 - Belle II Collaboration                             *
+*                                                                        *
+* Author: The Belle II Collaboration                                     *
+* Contributors: Thomas Lueck                                             *
+*                                                                        *
+* this is a modified copy of                                             *
+* tracking/trackFindingVXD/filterTools/include/ObserverCheckMCPurity.h   *
+*                                                                        *
+*                                                                        *
+* This software is provided "as is" without any warranty.                *
+**************************************************************************/
+
+#include <tracking/trackFindingVXD/filterTools/ObserverCheckFilters.h>
+
+using namespace Belle2;
+
+// NULL pointer
+StoreArray<ObserverInfo> ObserverCheckFilters::s_storeArray = StoreArray<ObserverInfo>();
+
+//initialize with an empty object
+ObserverInfo ObserverCheckFilters::s_observerInfo = ObserverInfo();

4d825bed0dec816abb03264eacebb444fbe0fa0c
Felix Metzner <felix.metzner@kit.edu>
2017-01-27 14:10:50 +0100

    Changed DataStore attribute for m_PARAMSpacePointTrackCandArrayName from WriteOut back to DontWriteOut

--- tracking/modules/vxdtfRedesign/src/TrackFinderVXDCellOMatModule.cc
+++ tracking/modules/vxdtfRedesign/src/TrackFinderVXDCellOMatModule.cc
@@ -72,7 +72,7 @@ void TrackFinderVXDCellOMatModule::initialize()
   m_spacePoints.isRequired(m_spacePointsName);
 
   m_network.isRequired(m_PARAMNetworkName);
-  m_TCs.registerInDataStore(m_PARAMSpacePointTrackCandArrayName, DataStore::c_WriteOut);
+  m_TCs.registerInDataStore(m_PARAMSpacePointTrackCandArrayName, DataStore::c_DontWriteOut);
 
   //Relations SpacePoints and SpacePointTCs:
   m_TCs.registerRelationTo(m_spacePoints, DataStore::c_Event, DataStore::c_DontWriteOut);

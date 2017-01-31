c21cb308cabd87ab6e3d64875f35a216f12fcfcd
Christian Pulvermacher <christian.pulvermacher@kek.jp>
2017-01-31 12:44:22 +0100

    more bamboo testing

--- framework/tests/bamboo_sigquit.py
+++ framework/tests/bamboo_sigquit.py
@@ -2,23 +2,26 @@ import signal
 import os
 import sys
 
+got_signal = False
+
 
 def sigquithandler(sig, frame):
-    print('SIGQUIT handler called')
-    print("dummy abort")
-    sys.exit(0)
+    global got_signal
+    print('handler called for ', sig)
+    got_signal = True
 
 # TODO investigating test failure on bamboo. (fails to die on SIGQUIT in single process.)
-prevsig = signal.signal(signal.SIGQUIT, sigquithandler)
-print('handler installed, previous handler was: ', prevsig)
-pid = os.getpid()
-print("Killing %s in init (sig %d)" % (pid, signal.SIGQUIT))
-os.kill(pid, signal.SIGQUIT)
+signals = [signal.SIGUSR1, signal.SIGUSR2, signal.SIGQUIT]
+for sig in signals:
+    got_signal = False
+    prevsig = signal.signal(sig, sigquithandler)
+    print('handler installed, previous handler was: ', prevsig)
+    pid = os.getpid()
+    print("Killing %s in init (sig %d)" % (pid, sig))
+    os.kill(pid, sig)
 
-print("didn't work, try numeric value")
-# this wouldn't change a thing, right?
-os.kill(pid, 3)
+    if not got_signal:
+        print("signal missed, abort!")
+        sys.exit(1)
 
-print("yup, signal handling is broken")
-# if handler is called we never end up here.
-sys.exit(1)
+print('Everything fine.')

719791762fd6d7d556a807ce2d74e792a305fe50
Thomas Hauth <Thomas.Hauth@desy.de>
2017-02-01 13:17:09 +0100

    made lookup of background files more verbose and fail in case there are no background files.

--- validation/validation/BabayagaNLOBhabhaSimRec.py
+++ validation/validation/BabayagaNLOBhabhaSimRec.py
@@ -15,7 +15,7 @@ from basf2 import *
 from simulation import add_simulation
 from reconstruction import add_reconstruction
 from beamparameters import add_beamparameters
-import glob
+import validationtools
 
 set_random_seed(12345)
 
@@ -58,9 +58,7 @@ main.add_module(generatorpreselection)
 generatorpreselection.if_value('<1', emptypath)
 
 # detector simulation
-bg = None
-if 'BELLE2_BACKGROUND_DIR' in os.environ:
-    bg = glob.glob(os.environ['BELLE2_BACKGROUND_DIR'] + '/*.root')
+bg = validationtools.get_background_files()
 add_simulation(main, bkgfiles=bg)
 
 # reconstruction

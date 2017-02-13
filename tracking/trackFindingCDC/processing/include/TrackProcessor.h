db32cd213fea0a31402ce2932bf048018049377f
Oliver Frost <oliver.frost@desy.de>
2017-02-13 22:19:48 +0100

    Sorting header file, remove explicit deletion of constructors.

--- tracking/trackFindingCDC/processing/include/TrackProcessor.h
+++ tracking/trackFindingCDC/processing/include/TrackProcessor.h
@@ -7,43 +7,31 @@
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/
-
 #pragma once
 
 #include <vector>
 #include <list>
 
-
 namespace Belle2 {
-
   namespace TrackFindingCDC {
-
     class CDCTrack;
     class CDCWireHit;
     class CDCTrajectory2D;
 
     /// Class with high-level track (post)processing.
     class TrackProcessor {
+
     public:
-      /// Static use only.
-      TrackProcessor() = delete;
-
-      /// Do not copy this class.
-      TrackProcessor(const TrackProcessor& copy) = delete;
-
-      /// Do not copy this class.
-      TrackProcessor& operator=(const TrackProcessor&) = delete;
-
       /// Create CDCTrack using CDCWireHit hits and store it in the list. Then call the postprocessing on it.
       static void addCandidateFromHitsWithPostprocessing(const std::vector<const CDCWireHit*>& foundAxialWireHits,
                                                          const std::vector<const CDCWireHit*>& allAxialWireHits,
                                                          std::list<CDCTrack>& cdcTrackList);
 
-      /// Assign new hits to all tracks (using the assignNewHitsToTrack(CDCTrack&) method of the HitsProcessor).
-      static void assignNewHits(const std::vector<const CDCWireHit*>& allWireHits, std::list<CDCTrack>& cdcTrackList);
-
       /// Perform all track postprocessing - return whether the track is considered good after the postprocessing
       static bool postprocessTrack(CDCTrack& track, const std::vector<const CDCWireHit*>& allAxialWireHits);
+
+      /// Assign new hits to all tracks (using the assignNewHitsToTrack(CDCTrack&) method of the HitsProcessor).
+      static void assignNewHits(const std::vector<const CDCWireHit*>& allWireHits, std::list<CDCTrack>& cdcTrackList);
 
       /// Finalize the tracks after the legendre track finder is done - includes a merging step of found tracks
       static void mergeAndFinalizeTracks(std::list<CDCTrack>& cdcTrackList,

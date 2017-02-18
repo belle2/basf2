3b21086a985601b8760717a6828cbbdda8b57f43
Wang, Boqun <boqunwg@ucmail.uc.edu>
2017-02-18 09:18:14 +0900

    Added documentation and copyright to TOPDataQualityOnline module
    * The name of this module will be changed later after discussion

--- top/modules/TOPDataQualityOnline/include/TOPDataQualityOnlineModule.h
+++ top/modules/TOPDataQualityOnline/include/TOPDataQualityOnlineModule.h
@@ -1,3 +1,15 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2017 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Boqun Wang                                               *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
 #include <framework/core/Module.h>
 #include <top/dataobjects/TOPRawWaveform.h>
 #include <framework/datastore/StoreObjPtr.h>
@@ -14,34 +26,83 @@
 #include <utility>
 
 namespace Belle2 {
+  /**
+   * Module for online data quality monitoring
+   */
   class TOPDataQualityOnlineModule : public Module {
+    /**
+     * Define the channel id type
+     */
     typedef unsigned long long topcaf_channel_id_t;
   public:
+    /**
+     * Constructor
+     */
     TOPDataQualityOnlineModule();
 
+    /**
+     * Destructor
+     */
+    ~TOPDataQualityOnlineModule() {};
+
+    /**
+     * Defining the histograms
+     */
     void defineHisto();
+
+    /**
+     * Initialize the module
+     */
     void initialize();
+
+    /**
+     * Called when entering a new run
+     */
     void beginRun();
+
+    /**
+     * Event processor
+     */
     void event();
+
+    /**
+     * End-of-run action
+     */
     void endRun();
+
+    /**
+     * Termination action
+     */
     void terminate();
+
+    /**
+     * Clear the graphs when necessary
+     */
     void clear_graph();
+
+    /**
+     * Update the graphs when new histograms are available
+     */
     void update_graph();
+
+    /**
+     * Draw wavefroms for each event (deprecated)
+    * @param wave The pointer to the TOPRawWaveform
+     */
     void drawWaveforms(TOPRawWaveform* wave);
-    //void basicDebuggingPlots(TOPRawWaveform* wave);
 
   private:
-    int m_iFrame;
-    int m_iEvent;
-    int m_refreshCount;
-    int m_framesPerEvent;
-    int m_nhits;
-    //map: scrod_id --> (asic_id --> TMultiGraph)
-    std::map<int, std::map<int, TMultiGraph*>> m_channels;
-    std::map<int, std::map<int, std::set<int>>> m_channelLabels;
-    std::map<int, TCanvas*> m_canvas; // map: scrod_id --> TCanvas
-    TCanvas* m_canvas_nhits;
-    TH1F* m_h_nhits;
+    int m_iFrame = 0; /**< Number of frames within one event */
+    int m_iEvent = 0; /**< Number of events that has been processed */
+    int m_refreshCount = 0; /**< For how many events should the graphs be updated */
+    int m_framesPerEvent = 0; /**< Number of frames within one event */
+    int m_nhits = 0; /**< Number of hits within one event */
+
+    std::map<int, std::map<int, TMultiGraph*>> m_channels; /**< map: scrod_id --> (asic_id --> TMultiGraph) */
+    std::map<int, std::map<int, std::set<int>>> m_channelLabels; /**< map: scrod_id --> (asic_id --> channels within one asic) */
+    std::map<int, TCanvas*> m_canvas; /**< map: scrod_id --> TCanvas */
+    TCanvas* m_canvas_nhits = NULL; /**< TCanvas for displaying waveform */
+    TH1F* m_h_nhits = NULL; /**< Histogram for number of hits within on event */
   };
 
-}
+} //namespace Belle2

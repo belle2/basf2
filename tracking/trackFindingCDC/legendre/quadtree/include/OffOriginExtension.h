fbdbfdbb2972a9de53eb2d8dd46cf0ae9f6254b5
Oliver Frost <oliver.frost@desy.de>
2017-02-20 02:47:11 +0100

    Splitting into header and source file.

--- tracking/trackFindingCDC/legendre/quadtree/include/OffOriginExtension.h
+++ tracking/trackFindingCDC/legendre/quadtree/include/OffOriginExtension.h
@@ -9,16 +9,14 @@
  **************************************************************************/
 #pragma once
 
-#include <tracking/trackFindingCDC/legendre/quadtree/AxialHitQuadTreeProcessorWithNewReferencePoint.h>
-#include <tracking/trackFindingCDC/processing/AxialTrackUtil.h>
-
-#include <tracking/trackFindingCDC/fitting/CDCKarimakiFitter.h>
 #include <tracking/trackFindingCDC/eventdata/tracks/CDCTrack.h>
 
 #include <vector>
 
 namespace Belle2 {
   namespace TrackFindingCDC {
+    class CDCWireHit;
+    class Vector2D;
 
     /**
      *  Class performs extension (adding new hits) of given candidate using conformal transformation w.r.t point on the trajectory
@@ -27,74 +25,13 @@ namespace Belle2 {
 
     public:
       /// Constructor
-      OffOriginExtension(std::vector<const CDCWireHit*> allAxialWireHits, double levelPrecision = 9)
-        : m_allAxialWireHits(std::move(allAxialWireHits))
-        , m_levelPrecision(levelPrecision)
-      {
-      }
+      OffOriginExtension(std::vector<const CDCWireHit*> allAxialWireHits, double levelPrecision = 9);
 
       /// Main entry point for the post processing call from the QuadTreeProcessor
-      void operator()(const std::vector<const CDCWireHit*>& inputWireHits,
-                      void* qt  __attribute__((unused)))
-      {
-        // Unset the taken flag and let the postprocessing decide
-        for (const CDCWireHit* wireHit : inputWireHits) {
-          (*wireHit)->setTakenFlag(false);
-        }
-
-        std::vector<const CDCWireHit*> candidateHits = roadSearch(inputWireHits);
-        AxialTrackUtil::addCandidateFromHitsWithPostprocessing(candidateHits, m_allAxialWireHits, m_tracks);
-      }
+      void operator()(const std::vector<const CDCWireHit*>& inputWireHits, void* qt);
 
       /// Perform transformation for set of given hits; reference position taken as POCA of the fitted trajectory
-      std::vector<const CDCWireHit*> roadSearch(const std::vector<const CDCWireHit*>& wireHits)
-      {
-        const CDCKarimakiFitter& fitter = CDCKarimakiFitter::getNoDriftVarianceFitter();
-        CDCTrajectory2D trackTrajectory2D = fitter.fit(wireHits);
-
-        double chi2 = trackTrajectory2D.getChi2();
-        Vector2D refPos = trackTrajectory2D.getGlobalPerigee();
-
-        // change sign of the curvature; should be the same as the charge of the candidate
-        double curv = trackTrajectory2D.getCurvature();
-
-        // theta is the vector normal to the trajectory at the perigee
-        double theta = trackTrajectory2D.getGlobalCircle().phi0() + M_PI_2;
-
-        // Hide the current hits from the road search
-        for (const CDCWireHit* hit : wireHits) {
-          hit->getAutomatonCell().setTakenFlag(true);
-        }
-
-        std::vector<const CDCWireHit*> newWireHits = getHitsWRTtoRefPos(refPos, curv, theta);
-
-        // Unhide the current hits from the road search
-        for (const CDCWireHit* hit : wireHits) {
-          hit->getAutomatonCell().setTakenFlag(false);
-        }
-
-        if (newWireHits.size() == 0) return wireHits;
-
-        std::vector<const CDCWireHit*> combinedWireHits;
-
-        for (const CDCWireHit* hit : wireHits) {
-          combinedWireHits.push_back(hit);
-        }
-
-        for (const CDCWireHit* hit : newWireHits) {
-          combinedWireHits.push_back(hit);
-        }
-
-        CDCTrajectory2D combinedTrajectory2D = fitter.fit(wireHits);
-        double combinedChi2 = combinedTrajectory2D.getChi2();
-
-        if (combinedChi2 < chi2 * 2.) {
-          return combinedWireHits;
-        }
-
-        return wireHits;
-      }
-
+      std::vector<const CDCWireHit*> roadSearch(const std::vector<const CDCWireHit*>& wireHits);
       /**
        * @brief Get hits which are compatible with given trajectory
        *
@@ -103,21 +40,7 @@ namespace Belle2 {
        * @param theta angle between x-axis and vector to the center of the circle which represents trajectory
        * @return vector of CDCWireHit objects which satisfy legendre transformation with respect to the given parameters
        */
-      std::vector<const CDCWireHit*> getHitsWRTtoRefPos(Vector2D refPos, float curv, float theta)
-      {
-        float thetaPrecision = 3.1415 / (pow(2., m_levelPrecision + 1));
-        float curvPrecision = 0.15 / (pow(2., m_levelPrecision));
-
-        using XYSpans = AxialHitQuadTreeProcessorWithNewReferencePoint::XYSpans;
-        XYSpans ranges({theta - thetaPrecision, theta + thetaPrecision},
-        {curv - curvPrecision, curv + curvPrecision});
-
-        AxialHitQuadTreeProcessorWithNewReferencePoint qtProcessor(ranges, std::make_pair(refPos.x(), refPos.y()));
-        qtProcessor.seed(m_allAxialWireHits);
-
-        std::vector<const CDCWireHit*> newWireHits = qtProcessor.getAssignedHits();
-        return newWireHits;
-      }
+      std::vector<const CDCWireHit*> getHitsWRTtoRefPos(const Vector2D& refPos, float curv, float theta);
 
       /// Get the collected tracks
       const std::vector<CDCTrack>& getTracks() const

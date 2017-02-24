a339ec1c0b6d0ce3067cc0b45c25646bc0d6163b
Francesco Tenchini <francesco.tenchini@unimelb.edu.au>
2017-02-25 07:17:01 +0900

    Fixed changes in EWP skims that ruined compatibility with combined skimming. Updated stdLightMesons to use pi0:loose, as pi0:all is deprecated

--- analysis/skim/EWP_Skim_Standalone.py
+++ analysis/skim/EWP_Skim_Standalone.py
@@ -24,39 +24,14 @@ filelist = \
      ]  # This is an MC7 file used for testing since we don't have MC8 yet; please modify accordingly
 inputMdstList('default', filelist)
 
-stdPi0s('loose')
-stdKshorts()
 stdPi('95eff')
 stdK('95eff')
-stdMu('95eff')  # this benchmark is missing, will load mu+:all
-stdE('95eff')  # this benchmark is missing, will load e+:all
+# lepton efficiency benchmark is missing, will load mu+:all and e+:all instead
 stdPhotons('loose')
-stdPhotons('all')  # only used for eta lists
-
-# rename to use in EWP_List.py
-# this is slightly inefficient, but makes the code more modular
-cutAndCopyList('pi0:ewp', 'pi0:loose', '', True)
-cutAndCopyList('pi+:ewp', 'pi+:95eff', '', True)
-cutAndCopyList('K+:ewp', 'K+:95eff', '', True)
-#
-
-# Loose cuts to compensate for missing leptons benchmarks;
-cutAndCopyList('e+:ewp', 'e+:all', 'pt > 0.4 and abs(d0)<2 and abs(z0)<4 and chiProb > 0.001', True)
-cutAndCopyList('mu+:ewp', 'mu+:all', 'pt > 0.7 and abs(d0)<2 and abs(z0)<4 and chiProb > 0.001', True)
-#
-
-# 20 MeV width; 'all' is 0.45<M<0.55; this selection could be optimised
-cutAndCopyList('K_S0:ewp', 'K_S0:all', '0.4776 < M < 0.5176', True)
-
-# We don't have light meson standard particle lists yet
-reconstructDecay('eta:ewp1 -> gamma:all gamma:all', '0.4 < M < 0.6', 1, True)
-reconstructDecay('eta:ewp2 -> pi0:ewp pi-:ewp pi+:ewp', '0.4 < M < 0.6', 2, True)
-copyLists('eta:ewp', ['eta:ewp1', 'eta:ewp2'], True)
-reconstructDecay('rho0:ewp -> pi-:ewp pi+:ewp', '0.47 < M < 1.07', 1, True)
-reconstructDecay('rho+:ewp -> pi0:ewp pi+:ewp', '0.47 < M < 1.07', 1, True)
-reconstructDecay('phi:ewp -> K+:ewp K-:ewp', '0.97 < M < 1.1', 1, True)
-reconstructDecay('omega:ewp -> pi0:ewp pi-:ewp pi+:ewp', '0.73 < M < 0.83', 1, True)
-#
+stdPi0s('loose')
+stdKshorts()
+loadStdCharged()  # needed for light mesons; also loads mu+:all and e+:all
+loadStdLightMesons()
 
 # EWP Skim
 from EWP_List import *

2eff98616b59134a7cc535b9fdac93fbe5d77431
Christian Pulvermacher <christian.pulvermacher@kek.jp>
2017-03-09 06:16:26 +0100

    basf2 --version: print BELLE2_RELEASE, too

--- framework/scripts/basf2_version.py
+++ framework/scripts/basf2_version.py
@@ -2,16 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import os
-import basf2
-import ROOT
 
-from ROOT import Belle2
-ROOT.gSystem.Load('libframework_io')
-ROOT.gROOT.LoadMacro(Belle2.FileSystem.findFile("include/framework/io/RootIOUtilities.h"))
+version = ''
 try:
+    from ROOT import Belle2
+    import ROOT
+    ROOT.gSystem.Load('libframework_io')
+    ROOT.gROOT.LoadMacro(Belle2.FileSystem.findFile("include/framework/io/RootIOUtilities.h"))
     version = Belle2.RootIOUtilities.getCommitID()
 except AttributeError:
-    version = os.environ.get('BELLE2_RELEASE', 'unknown')
+    pass
+
+release = (os.environ.get('BELLE2_RELEASE', 'unknown'))
 
 if __name__ == "__main__":
-    print('BASF2, Version', version)
+    print('BASF2, Version: %s (Release: %s)' % (version, release))

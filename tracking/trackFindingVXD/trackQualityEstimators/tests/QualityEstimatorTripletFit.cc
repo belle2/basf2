08697aed378f41b09297bb14d5334cdb5871936b
Jonas Ferdinand Wagner <jonas.wagner@desy.de>
2017-03-21 13:27:12 +0100

    Added a test to verify tripletfit interface behaviour

--- tracking/trackFindingVXD/trackQualityEstimators/tests/QualityEstimatorTripletFit.cc
+++ tracking/trackFindingVXD/trackQualityEstimators/tests/QualityEstimatorTripletFit.cc
@@ -14,6 +14,7 @@
 #include "tracking/trackFindingVXD/trackQualityEstimators/QualityEstimatorTripletFit.h"
 #include <vector>
 #include <utility> // pair
+#include <math.h>
 
 using namespace Belle2;
 const static double allowed_chi2_deviation = 1e-20;
@@ -22,10 +23,10 @@ const static double allowed_chi2_deviation = 1e-20;
 namespace QualityEstimatorTests {
   TEST(TripletFitTests, CircleChi2)
   {
-    // Setup QualityEstimators Class
+    // Setup QualityEstimator
     auto qualityEstimator = QualityEstimatorTripletFit(1.5);
-    // Some hitpoints that are exactly on a circle in x,y with radius 1 and m=(1,0,0)
 
+    // Some hitpoints that are exactly on a circle in x,y with radius 1 and m=(1,0,0)
     auto position1 = TVector3(0., 0., 0.);
     auto position2 = TVector3(1., 1., 0.);
     auto position3 = TVector3(2., 0., 0.);
@@ -43,7 +44,6 @@ namespace QualityEstimatorTests {
     ASSERT_NEAR(result, 0., allowed_chi2_deviation) << "1st perfect circle";
 
     // Some hitpoints that are exactly on a circle in rotated 2D-plane with radius 1
-
     position1 = TVector3(0., 0., 0.);
     position2 = TVector3(1., 1., 1.);
     position3 = TVector3(2., 0., 2.);
@@ -59,7 +59,6 @@ namespace QualityEstimatorTests {
     ASSERT_NEAR(result, 0., allowed_chi2_deviation) << "2nd perfect circle";
 
     // Move one hit away from circle
-
     position3 = TVector3(2., 0., 2. + 1e-6);
 
     hitlist.clear();
@@ -72,7 +71,6 @@ namespace QualityEstimatorTests {
     ASSERT_TRUE(result > allowed_chi2_deviation) << "1st imperfect circle";
 
     // Move one hit away from circle
-
     position3 = TVector3(2. + 1e-6, 0., 2.);
 
     hitlist.clear();
@@ -87,10 +85,10 @@ namespace QualityEstimatorTests {
 
   TEST(TripletFitTests, CombinedCircleChi2)
   {
-    // Setup QualityEstimators Class
-    auto qualityEstimators = QualityEstimatorTripletFit(1.5);
-    // Some hitpoints that are exactly on a circle in x,y with radius 1 and m=(1,0,0)
+    // Setup QualityEstimator
+    auto qualityEstimator = QualityEstimatorTripletFit(1.5);
 
+    // Some hitpoints that are exactly on a circle in x,y with radius 1 and m=(1,0,0)
     auto position1 = TVector3(0., 0., 0.);
     auto position2 = TVector3(1., 1., 0.);
     auto position3 = TVector3(2., 0., 0.);
@@ -104,12 +102,11 @@ namespace QualityEstimatorTests {
     hitlist.emplace_back(position3, sigma);
     hitlist.emplace_back(position4, sigma);
     // Perform fit
-    auto result = qualityEstimators.calcChiSquared(hitlist);
+    auto result = qualityEstimator.calcChiSquared(hitlist);
 
     ASSERT_NEAR(result, 0., allowed_chi2_deviation) << "1st perfect combined circle";
 
     // Combine two triplets with different radii;
-
     position4 = TVector3(1., -2., 0.);
     hitlist.clear();
     hitlist.reserve(4);
@@ -118,8 +115,55 @@ namespace QualityEstimatorTests {
     hitlist.emplace_back(position3, sigma);
     hitlist.emplace_back(position4, sigma);
 
-    result = qualityEstimators.calcChiSquared(hitlist);
+    result = qualityEstimator.calcChiSquared(hitlist);
 
     ASSERT_TRUE(result > allowed_chi2_deviation) << "Incompatible circle triplets";
   }
+
+  TEST(TripletFitTests, Interface)
+  {
+    // Setup QualityEstimator Class
+    auto qualityEstimator = QualityEstimatorTripletFit(1.5);
+
+    // Some hitpoints
+    auto position1 = TVector3(0., 0., 0.);
+    auto position2 = TVector3(1., 1., 0.);
+    auto position3 = TVector3(2., 0., 0.);
+    TVector3 sigma;
+
+    std::vector<Measurement> hitlist;
+    hitlist.reserve(4);
+    hitlist.emplace_back(position1, sigma);
+    hitlist.emplace_back(position2, sigma);
+    hitlist.emplace_back(position3, sigma);
+
+    // Test if calcChiSquared and calcCompleteResults return the same values
+    auto result = qualityEstimator.calcChiSquared(hitlist);
+    auto resultObject = qualityEstimator.calcCompleteResults(hitlist);
+    ASSERT_FLOAT_EQ(resultObject.chiSquared, result);
+
+    // check if the correct optional values are set
+    ASSERT_FALSE(resultObject.p);
+    ASSERT_FALSE(resultObject.p_sigma);
+
+    ASSERT_TRUE(resultObject.pt);
+    ASSERT_TRUE(resultObject.pt_sigma);
+    ASSERT_TRUE(resultObject.curvatureSign);
+
+    // Test if tripletFit handles empty measurements vector correct
+    hitlist.clear();
+
+    result = qualityEstimator.calcChiSquared(hitlist);
+    resultObject = qualityEstimator.calcCompleteResults(hitlist);
+    ASSERT_TRUE(isnan(result));
+    ASSERT_TRUE(isnan(resultObject.chiSquared));
+
+    // check if the optional values got reset
+    ASSERT_FALSE(resultObject.p);
+    ASSERT_FALSE(resultObject.p_sigma);
+    ASSERT_FALSE(resultObject.pt);
+    ASSERT_FALSE(resultObject.pt_sigma);
+    ASSERT_FALSE(resultObject.curvatureSign);
+  }
 }
+

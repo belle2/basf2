d6ac217109889e73fa1c5eb0da4c7b58ce9f6569
Oliver Frost <oliver.frost@desy.de>
2017-03-24 18:24:58 +0100

    Allow chi2 facet relation filter to distinguish cut values dependent on the superlayer.

--- tracking/trackFindingCDC/filters/facetRelation/src/Chi2FacetRelationFilter.cc
+++ tracking/trackFindingCDC/filters/facetRelation/src/Chi2FacetRelationFilter.cc
@@ -21,9 +21,11 @@
 using namespace Belle2;
 using namespace TrackFindingCDC;
 
+Chi2FacetRelationFilter::Chi2FacetRelationFilter() = default;
+
 Chi2FacetRelationFilter::Chi2FacetRelationFilter(double chi2Cut, double penaltyWidth)
-  : m_param_chi2Cut(chi2Cut)
-  , m_param_penaltyWidth(penaltyWidth)
+  : m_param_chi2CutByISuperLayer{chi2Cut,}
+  , m_param_penaltyFactor(penaltyWidth / chi2Cut)
 {
 }
 
@@ -31,14 +33,37 @@ void Chi2FacetRelationFilter::exposeParameters(ModuleParamList* moduleParamList,
                                                const std::string& prefix)
 {
   Super::exposeParameters(moduleParamList, prefix);
-  moduleParamList->addParameter(prefixed(prefix, "chi2Cut"),
-                                m_param_chi2Cut,
-                                "Acceptable chi2 fit value",
-                                m_param_chi2Cut);
-  moduleParamList->addParameter(prefixed(prefix, "penaltyWidth"),
-                                m_param_penaltyWidth,
-                                "Width used in translation from chi2 values to weight penalties",
-                                m_param_penaltyWidth);
+
+  moduleParamList->addParameter(prefixed(prefix, "chi2CutByISuperLayer"),
+                                m_param_chi2CutByISuperLayer,
+                                "Acceptable chi2 values by superlayer id",
+                                m_param_chi2CutByISuperLayer);
+
+  moduleParamList->addParameter(prefixed(prefix, "penaltyFactor"),
+                                m_param_penaltyFactor,
+                                "Factor for cut value to obtain the width used in translation from chi2 values to weight penalties",
+                                m_param_penaltyFactor);
+}
+
+void Chi2FacetRelationFilter::initialize()
+{
+  if (m_param_chi2CutByISuperLayer.size() == 1) {
+    for (int iSL = 0; iSL < ISuperLayerUtil::c_N; ++iSL) {
+      m_chi2CutByISuperLayer[iSL] = m_param_chi2CutByISuperLayer[0];
+      m_penaltyWidthByISuperLayer[iSL] = m_param_chi2CutByISuperLayer[0] * m_param_penaltyFactor;
+    }
+  } else if (m_param_chi2CutByISuperLayer.size() == ISuperLayerUtil::c_N) {
+    for (int iSL = 0; iSL < ISuperLayerUtil::c_N; ++iSL) {
+      m_chi2CutByISuperLayer[iSL] = m_param_chi2CutByISuperLayer[iSL];
+      m_penaltyWidthByISuperLayer[iSL] = m_param_chi2CutByISuperLayer[iSL] * m_param_penaltyFactor;
+    }
+  } else {
+    B2ERROR("Expected chi2CutByISuperLayer to be of length 1 or "
+            << ISuperLayerUtil::c_N
+            << " received "
+            << m_param_chi2CutByISuperLayer.size());
+  }
+
 }
 
 Weight Chi2FacetRelationFilter::operator()(const CDCFacet& fromFacet, const CDCFacet& toFacet)
@@ -49,11 +74,12 @@ Weight Chi2FacetRelationFilter::operator()(const CDCFacet& fromFacet, const CDCF
   const UncertainParameterLine2D fitLine = FacetFitter::fit(fromFacet, toFacet, nSteps);
   const double chi2 = fitLine.chi2();
 
-  if (chi2 > m_param_chi2Cut or std::isnan(chi2)) {
+  ISuperLayer iSL = fromFacet.getISuperLayer();
+  if (chi2 > m_chi2CutByISuperLayer[iSL] or std::isnan(chi2)) {
     return NAN;
   } else {
     // Introducing a mini penilty to distiguish better facets.
-    double penalty = std::erf(chi2 / m_param_penaltyWidth);
+    double penalty = std::erf(chi2 / m_penaltyWidthByISuperLayer[iSL]);
 
     // The facets contain three hits of the track each.
     // However they have 2 common hits which we have to substract

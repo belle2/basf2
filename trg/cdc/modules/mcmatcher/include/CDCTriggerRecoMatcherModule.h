144152f2e2662d95ae58adfa1fb6624a1ef59424
Sara Neuhaus <sneuhaus@mpp.mpg.de>
2017-03-29 08:08:37 +0200

    matcher for CDCTriggerTracks <-> RecoTracks

---
+++ trg/cdc/modules/mcmatcher/include/CDCTriggerRecoMatcherModule.h
@@ -0,0 +1,39 @@
+#ifndef CDCTRIGGERRECOMATCHERMODULE_H
+#define CDCTRIGGERRECOMATCHERMODULE_H
+
+#include <framework/core/Module.h>
+
+namespace Belle2 {
+
+  /** A module to match CDCTriggerTracks to RecoTracks.
+   */
+  class CDCTriggerRecoMatcherModule : public Module {
+  public:
+    /** Constructor, for setting module description and parameters. */
+    CDCTriggerRecoMatcherModule();
+
+    /** Destructor. */
+    virtual ~CDCTriggerRecoMatcherModule() {}
+
+    /** Initialize the module. */
+    virtual void initialize();
+
+    /** Called once for each event. */
+    virtual void event();
+
+  protected:
+    /** Name of the RecoTrack StoreArray to be matched */
+    std::string m_RecoTrackCollectionName;
+    /** Name of the CDCTriggerTrack Store Array to be matched */
+    std::string m_TrgTrackCollectionName;
+    /** switch for 2D matching */
+    bool m_axialOnly;
+    /** minimum purity */
+    double m_minPurity;
+    /** minimum efficiency */
+    double m_minEfficiency;
+    /** switch for creating relations for clones and merged tracks */
+    bool m_relateClonesAndMerged;
+  };
+}
+#endif

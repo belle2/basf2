3225f7094c6c2c9d77fe4f07307f1c924c986636
Jonas Ferdinand Wagner <jonas.wagner@desy.de>
2017-04-10 14:18:37 +0200

    Module to collect data required for a mva training based on the classic QualityEstimators

---
+++ tracking/modules/qualityEstimatorVXD/include/QETrainingDataCollectorModule.h
@@ -0,0 +1,81 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2017 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Jonas Wagner                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+#include <tracking/spacePointCreation/SpacePointTrackCand.h>
+#include <tracking/trackFindingVXD/trackQualityEstimators/QualityEstimatorBase.h>
+#include <tracking/trackFindingVXD/mva/SimpleVariableRecorder.h>
+#include <tracking/trackFindingVXD/utilities/VariableSet.h>
+
+
+#include <framework/datastore/StoreArray.h>
+#include <framework/core/Module.h>
+
+#include <memory>
+
+
+namespace Belle2 {
+  class QETrainingDataCollectorModule : public Module {
+
+  public:
+    /** Constructor of the module. */
+    QETrainingDataCollectorModule();
+
+    /** Initializes the Module. */
+    virtual void initialize();
+
+    /** Applies the selected quality estimation method for a given set of TCs */
+    virtual void event();
+
+    virtual void terminate();
+
+
+  protected:
+
+    // module parameters
+
+    /** Identifier which estimation method to use. Valid identifiers are:
+     * CircleFit
+     * TripletFit
+     * Random
+     */
+    std::string m_EstimationMethod;
+
+    /** sets the name of the expected StoreArray containing SpacePointTrackCands */
+    std::string m_SpacePointTrackCandsStoreArrayName;
+
+    /** sets the name of the expected StoreArray containing MCRecoTracks. Only required for MCInfo method */
+    std::string m_MCRecoTracksStoreArrayName;
+
+    /** Only required for MCInfo method */
+    bool m_MCStrictQualityEstimator;
+
+
+    // member variables
+
+    /** the storeArray for SpacePointTrackCands as member, is faster than recreating link for each event */
+    StoreArray<SpacePointTrackCand> m_spacePointTrackCands;
+
+
+    /** pointer to the selected QualityEstimator */
+    std::unique_ptr<QualityEstimatorBase> m_estimator;
+
+    /** QualityEstimatorMC as target for training */
+    std::unique_ptr<QualityEstimatorBase> m_estimatorMC;
+
+    std::string m_TrainingDataOutputName;
+
+    std::unique_ptr<SimpleVariableRecorder> m_recorder;
+
+    MVAVariableSet m_variableSet;
+
+  };
+}
---
+++ tracking/modules/qualityEstimatorVXD/src/QETrainingDataCollectorModule.cc
@@ -0,0 +1,101 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2017 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Jonas Wagner                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <tracking/modules/qualityEstimatorVXD/QETrainingDataCollectorModule.h>
+#include <tracking/trackFindingVXD/trackQualityEstimators/QualityEstimatorTripletFit.h>
+#include <tracking/trackFindingVXD/trackQualityEstimators/QualityEstimatorRiemannHelixFit.h>
+#include <tracking/trackFindingVXD/trackQualityEstimators/QualityEstimatorMC.h>
+#include <tracking/trackFindingVXD/trackQualityEstimators/QualityEstimatorCircleFit.h>
+#include <tracking/trackFindingVXD/trackQualityEstimators/QualityEstimatorRandom.h>
+#include <tracking/trackFindingVXD/utilities/UniquePointerHelper.h>
+#include <framework/logging/Logger.h>
+#include <geometry/bfieldmap/BFieldMap.h>
+#include <math.h>
+#include <numeric>
+
+
+using namespace Belle2;
+
+
+REG_MODULE(QETrainingDataCollector)
+
+QETrainingDataCollectorModule::QETrainingDataCollectorModule() : Module()
+{
+  //Set module properties
+  setDescription("Module to collect training data for a specified qualityEstimator and store it in a root file.");
+  setPropertyFlags(c_ParallelProcessingCertified | c_TerminateInAllProcesses);
+
+  addParam("EstimationMethod", m_EstimationMethod,
+           "Identifier which estimation method to use. Valid identifiers are: [CircleFit, TripletFit, Random]", std::string(""));
+
+  addParam("SpacePointTrackCandsStoreArrayName", m_SpacePointTrackCandsStoreArrayName,
+           "Name of StoreArray containing the SpacePointTrackCandidates to be estimated.", std::string(""));
+
+  addParam("MCRecoTracksStoreArrayName", m_MCRecoTracksStoreArrayName,
+           "Name of StoreArray containing MCRecoTracks. Only required for MCInfo method", std::string("MCRecoTracks"));
+
+  addParam("MCStrictQualityEstimator", m_MCStrictQualityEstimator,
+           "Only required for MCInfo method. If false combining several MCTracks is allowed.", bool(true));
+
+  addParam("TrainingDataOutputName", m_TrainingDataOutputName, "Name of the output rootfile.", std::string("QETrainingOutput.root"));
+}
+
+void QETrainingDataCollectorModule::initialize()
+{
+  m_spacePointTrackCands.isRequired(m_SpacePointTrackCandsStoreArrayName);
+
+  // BField is required by all QualityEstimators
+  double bFieldZ = BFieldMap::Instance().getBField(TVector3(0, 0, 0)).Z();
+
+  m_estimatorMC = make_unique<QualityEstimatorMC>(bFieldZ, m_MCRecoTracksStoreArrayName, m_MCStrictQualityEstimator);
+
+  // create pointer to chosen estimator
+  if (m_EstimationMethod == "TripletFit") {
+    m_estimator = make_unique<QualityEstimatorTripletFit>(bFieldZ);
+  } else if (m_EstimationMethod == "CircleFit") {
+    m_estimator = make_unique<QualityEstimatorCircleFit>(bFieldZ);
+  } else if (m_EstimationMethod == "HelixFit") {
+    m_estimator = make_unique<QualityEstimatorRiemannHelixFit>(bFieldZ);
+  }
+  B2ASSERT("Not all QualityEstimators could be initialized!", m_estimator);
+
+  // prepare recorder
+  m_variableSet.setVariable("NHits", -1);
+  m_variableSet.setVariable("truth", -1);
+  m_variableSet.setVariables(m_EstimationMethod, QualityEstimationResults());
+
+
+  m_recorder = make_unique<SimpleVariableRecorder>(m_variableSet, m_TrainingDataOutputName, "tree");
+}
+
+void QETrainingDataCollectorModule::event()
+{
+  for (SpacePointTrackCand& aTC : m_spacePointTrackCands) {
+
+    std::vector<SpacePoint const*> const sortedHits = aTC.getSortedHits();
+    m_variableSet.setVariable("NHits", sortedHits.size());
+
+    float qi = m_estimatorMC->estimateQuality(sortedHits);
+    qi = qi > 0 ? 1 : 0;
+    m_variableSet.setVariable("truth", qi);
+
+    QualityEstimationResults results = m_estimator->estimateQualityAndProperties(sortedHits);
+    m_variableSet.setVariables(m_EstimationMethod, results);
+
+    // record variables
+    m_recorder->record();
+  }
+}
+
+void QETrainingDataCollectorModule::terminate()
+{
+  m_recorder->write();
+  m_recorder.reset();
+}

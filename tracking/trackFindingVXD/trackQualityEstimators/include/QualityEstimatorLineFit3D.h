3d8d9cc033c2046f4de9e173fa04e12ea1e777d5
Jonas Ferdinand Wagner <jonas.wagner@desy.de>
2017-04-10 13:14:29 +0200

    LineFit3D implemented in new interface

---
+++ tracking/trackFindingVXD/trackQualityEstimators/include/QualityEstimatorLineFit3D.h
@@ -0,0 +1,41 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2017 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Jakob Lettenbichler, Felix Metzner, Jonas Wagner         *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+#include <tracking/trackFindingVXD/trackQualityEstimators/QualityEstimatorBase.h>
+
+namespace Belle2 {
+
+  /** Testbeam:
+   * Coords:   Sensors:
+   * ^        ./| ./| ./|
+   * |   ^    | | | | | |
+   * |Y /Z    | | | | | |
+   * |/       |/  |/  |/
+   * -------> X
+   *
+   * beam parallel to x. Measurement errors in y & z (v&u)
+   * With these conditions, the following approach using 2 independent 2D line fits is acceptable (if rotation is the same for all sensors):
+   * Modells:
+   * Y_i = a*X_i + b        Z_i = c*X_i + d
+   * */
+  class QualityEstimatorLineFit3D : public QualityEstimatorBase {
+
+  public:
+
+    QualityEstimatorLineFit3D(double bFieldValue): QualityEstimatorBase(bFieldValue) {};
+
+    virtual double estimateQuality(std::vector<SpacePoint const*> const& measurements) final;
+
+  protected:
+  };
+}
+

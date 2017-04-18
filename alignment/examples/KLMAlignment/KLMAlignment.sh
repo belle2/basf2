4d0ffe30d088d1a021c7f76d9e6e6d314579edec
Kirill Chilikin <chilikin@lebedev.ru>
2017-04-18 16:25:36 +0300

    Use random alignment only for simulation.
    Alignment is performed starting from zero displacements.

--- alignment/examples/KLMAlignment/KLMAlignment.sh
+++ alignment/examples/KLMAlignment/KLMAlignment.sh
@@ -2,13 +2,33 @@
 
 # Example of KLM alignment.
 
+# Displacements.
+mkdir displacement
+cd displacement
+
+# Generation of zero displacements.
+mkdir zero
+cd zero
+basf2 ${BELLE2_LOCAL_DIR}/eklm/examples/Alignment/ZeroDisplacement.py
+cd ..
+
+# Generation of random displacements.
+mkdir random
+cd random
+basf2 ${BELLE2_LOCAL_DIR}/eklm/examples/Alignment/RandomDisplacement.py
+cd ../..
+
 # Generation of 100000 e+ e- -> mu+ mu- events.
 basf2 -n 100000 ${BELLE2_LOCAL_DIR}/eklm/examples/TimeCalibration/Generation.py eetomumu_gen.root
 
 # Simulation and reconstruction.
+cp -R displacement/random/localdb .
 basf2 ${BELLE2_LOCAL_DIR}/alignment/examples/KLMAlignment/SimulationReconstruction.py eetomumu_gen.root eetomumu_rec.root
 
 # Millipede collection.
+rm -rf localdb
+cp -R displacement/zero/localdb .
+# If an iteration is necessary, restart from here.
 basf2 ${BELLE2_LOCAL_DIR}/alignment/examples/KLMAlignment/Collection.py eetomumu_rec.root eetomumu_col.root
 
 # Alignment.
@@ -16,3 +36,4 @@ basf2 ${BELLE2_LOCAL_DIR}/alignment/examples/KLMAlignment/Alignment.py eetomumu_
 
 # Comparison of the alignment results and initial displacement.
 root -b -q ${BELLE2_LOCAL_DIR}'/alignment/examples/KLMAlignment/CompareAlignment.C("EKLMDisplacement.root","alignment.root","comparison.root")'
+

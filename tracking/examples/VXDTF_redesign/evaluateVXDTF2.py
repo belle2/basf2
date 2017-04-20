c1096031ca19e4d8bbad78b33009ffd85a4c874a
Jonas Ferdinand Wagner <jonas.wagner@desy.de>
2017-04-20 10:32:00 +0200

    Fix default value in example script evaluateVXDTF2.py

--- tracking/examples/VXDTF_redesign/evaluateVXDTF2.py
+++ tracking/examples/VXDTF_redesign/evaluateVXDTF2.py
@@ -78,7 +78,7 @@ setup_VXDTF2(path=path,
              use_pxd=usePXD,
              secmap_name=secmap_name,
              overlap_filter='hopfield',
-             quality_estimator='circleFit')
+             quality_estimator='CircleFit')
 
 if performFit:
     genFitExtrapolation = register_module('SetupGenfitExtrapolation')

964372b298ddab6106dd42748c19020abd1b85b4
Jake Bennett <jake.bennett@desy.de>
2017-04-20 11:49:52 -0400

    Updating dE/dx calibration in CAF and adding example scripts

--- reconstruction/modules/DedxCorrection/src/DedxCorrectionModule.cc
+++ reconstruction/modules/DedxCorrection/src/DedxCorrectionModule.cc
@@ -156,11 +156,14 @@ double DedxCorrectionModule::RunGainCorrection(double& dedx) const
 double DedxCorrectionModule::WireGainCorrection(int wireID, double& dedx) const
 {
 
-  if (m_valid[wireID] && m_wireGain[wireID] != 0) {
-    double newDedx = dedx / m_wireGain[wireID];
+  /*
+  B2INFO("Rescaling wire gains for " << wireID << " = " << m_wireGains[wireID]->getWireID() << " by " << m_wireGains[wireID]->getWireGain());
+
+  if (m_wireGains[wireID]->getWireID() == wireID) {
+    double newDedx = dedx / m_wireGains[wireID]->getWireGain();
     return newDedx;
-  } else
-    return dedx;
+    } else*/
+  return dedx;
 }
 
 double DedxCorrectionModule::HadronCorrection(double costheta, double dedx) const

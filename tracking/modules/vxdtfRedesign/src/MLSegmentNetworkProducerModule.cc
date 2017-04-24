d18c4db817104dc5f2bdca90c5bfed808a2ee884
Jonas Ferdinand Wagner <jonas.wagner@desy.de>
2017-04-24 17:48:46 +0200

    Recreated deleted files and fixed access to DirectedNodeNetworks

---
+++ tracking/modules/vxdtfRedesign/src/MLSegmentNetworkProducerModule.cc
@@ -0,0 +1,160 @@
+/*************************************************************************
+* BASF2 (Belle Analysis Framework 2)                                     *
+* Copyright(C) 2016 - Belle II Collaboration                             *
+*                                                                        *
+* Author: The Belle II Collaboration                                     *
+* Contributors: Thomas Madlener                                          *
+*                                                                        *
+* This software is provided "as is" without any warranty.                *
+**************************************************************************/
+
+#include <tracking/modules/vxdtfRedesign/MLSegmentNetworkProducerModule.h>
+
+#include <framework/logging/Logger.h>
+
+#include <fstream>
+
+using namespace Belle2;
+using namespace std;
+
+REG_MODULE(MLSegmentNetworkProducer)
+
+MLSegmentNetworkProducerModule::MLSegmentNetworkProducerModule() : Module()
+{
+  setDescription("SegmentNetwork Producer Module with a machine learning classifier as three hit filter.");
+
+  addParam("FBDTFileName", m_PARAMfbdtFileName, "file where the FastBDT classifier is stored");
+  // addParam("collectMode", m_PARAMcollectMode, "set to true for collecting training data, false for applying the filter", false);
+
+  addParam("networkInputName",
+           m_PARAMnetworkInputName,
+           "Name of the StoreObjPtr where the network container used in this module is stored", std::string(""));
+
+  addParam("sectorMapName", m_PARAMsecMapName,
+           "The name of the SectorMap used for this instance.", string("testMap"));
+
+  addParam("cutValue", m_PARAMcutVal,
+           "Cut value to be used for dividing the classifier output into signal (above) and background (below)",
+           0.5);
+
+}
+
+void MLSegmentNetworkProducerModule::initialize()
+{
+  m_network.isRequired(m_PARAMnetworkInputName);
+
+  if (m_PARAMcutVal < 0. || m_PARAMcutVal > 1.) {
+    B2ERROR("cutValue set to " << m_PARAMcutVal << " but has to be in [0,1]!");
+  }
+
+  setupClassifier(m_PARAMfbdtFileName);
+  setupFilter();
+}
+
+void MLSegmentNetworkProducerModule::event()
+{
+  DirectedNodeNetwork<TrackNode, VoidMetaInfo>& hitNetwork = m_network->accessHitNetwork();
+  DirectedNodeNetwork<Segment<TrackNode>, CACell>& segmentNetwork = m_network->accessSegmentNetwork();
+  vector<Segment<TrackNode>* >& segments = m_network->accessSegments();
+
+  unsigned nAccepted{}, nRejected{}, nLinked{};
+
+  for (const auto& outerHitPair : hitNetwork.getNodes()) {
+    auto outerHit = outerHitPair.second;
+
+    for (const auto& centerHit : outerHit->getInnerNodes()) {
+      bool alreadyAdded = false; // skip adding Nodes twice into the network
+      for (const auto& innerHit : centerHit->getInnerNodes()) {
+        bool accepted = m_filter->accept(*(innerHit->getEntry().spacePoint),
+                                         *(centerHit->getEntry().spacePoint),
+                                         *(outerHit->getEntry().spacePoint));
+
+        if (LogSystem::Instance().isLevelEnabled(LogConfig::c_Debug, 499, PACKAGENAME())) {
+          const auto& sp1 = innerHit->getEntry().spacePoint;
+          const auto& sp2 = centerHit->getEntry().spacePoint;
+          const auto& sp3 = outerHit->getEntry().spacePoint;
+          std::array<double, 9> coords{{ sp1->X(), sp1->Y(), sp1->Z(), sp2->X(), sp2->Y(), sp2->Z(), sp3->X(), sp3->Y(), sp3->Z() }};
+          double classOut = m_classifier->analyze(coords);
+          B2DEBUG(499, "Classifier output: " << classOut << ", cutValue: " << m_PARAMcutVal);
+        }
+
+        if (!accepted) { nRejected++; continue; } // don't store combinations which have not been accepted
+        nAccepted++;
+
+        Segment<TrackNode>* innerSegment = new Segment<TrackNode>(centerHit->getEntry().sector->getFullSecID(),
+                                                                  innerHit->getEntry().sector->getFullSecID(),
+                                                                  &centerHit->getEntry(),
+                                                                  &innerHit->getEntry());
+        B2DEBUG(999, "buildSegmentNetwork: innerSegment: " << innerSegment->getName());
+        DirectedNode<Segment<TrackNode>, CACell>* tempInnerSegmentnode = segmentNetwork.getNode(innerSegment->getName());
+        if (tempInnerSegmentnode == nullptr) {
+          segments.push_back(innerSegment);
+          segmentNetwork.addNode(innerSegment->getName(), *innerSegment);
+        } else {
+          delete innerSegment;
+          innerSegment = &(tempInnerSegmentnode->getEntry());
+        }
+
+        if (!alreadyAdded) {
+          // create outerSector
+          Segment<TrackNode>* outerSegment = new Segment<TrackNode>(outerHit->getEntry().sector->getFullSecID(),
+                                                                    centerHit->getEntry().sector->getFullSecID(),
+                                                                    &outerHit->getEntry(),
+                                                                    &centerHit->getEntry());
+          B2DEBUG(999, "buildSegmentNetwork: outerSegment(freshly created): " << outerSegment->getName() <<
+                  " to be linked with inner segment: " << innerSegment->getName());
+
+          DirectedNode<Segment<TrackNode>, CACell>* tempOuterSegmentnode = segmentNetwork.getNode(outerSegment->getName());
+          if (tempOuterSegmentnode == nullptr) {
+            segments.push_back(outerSegment);
+            segmentNetwork.addNode(outerSegment->getName(), *outerSegment);
+          } else {
+            delete outerSegment;
+            outerSegment = &(tempOuterSegmentnode->getEntry());
+          }
+
+          B2DEBUG(999, "buildSegmentNetwork: outerSegment (after duplicate check): " << outerSegment->getName() <<
+                  " to be linked with inner segment: " << innerSegment->getName());
+          segmentNetwork.linkNodes(outerSegment->getName(), innerSegment->getName());
+          nLinked++;
+          alreadyAdded = true;
+          continue;
+        }
+        segmentNetwork.addInnerToLastOuterNode(innerSegment->getName());
+      } // end inner loop
+    } // end center loop
+  } // end outer loop
+
+
+  B2DEBUG(10, "MLSegmentNetworkProducerModule::buildSegmentNetwork(): nAccepted/nRejected: " << nAccepted << "/" << nRejected <<
+          ", size of nLinked/hitNetwork: " << nLinked << "/" << segmentNetwork.size());
+
+}
+
+void MLSegmentNetworkProducerModule::terminate()
+{
+
+}
+
+
+void MLSegmentNetworkProducerModule::setupClassifier(const std::string& filename)
+{
+  ifstream filestr(filename);
+  if (!filestr.is_open())  {
+    B2FATAL("Could not open file: " << filename << " for reading in a FBDTClassifier");
+  }
+
+  auto classifier = std::unique_ptr<FBDTClassifier<9> >(new FBDTClassifier<9>());
+  classifier->readFromStream(filestr);
+  filestr.close();
+
+  m_classifier = std::move(classifier);
+}
+
+void MLSegmentNetworkProducerModule::setupFilter()
+{
+  using RangeT = MLRange<FBDTClassifier<9>, 9, double>;
+
+  auto filter = std::unique_ptr<MLFilter>(new MLFilter(RangeT(m_classifier.get(), m_PARAMcutVal)));
+  m_filter = std::move(filter);
+}
---
+++ tracking/modules/vxdtfRedesign/src/SegmentNetworkAnalyzerModule.cc
@@ -0,0 +1,169 @@
+/*************************************************************************
+* BASF2 (Belle Analysis Framework 2)                                     *
+* Copyright(C) 2016 - Belle II Collaboration                             *
+*                                                                        *
+* Author: The Belle II Collaboration                                     *
+* Contributors: Thomas Madlener                                          *
+*                                                                        *
+* This software is provided "as is" without any warranty.                *
+**************************************************************************/
+
+#include <tracking/modules/vxdtfRedesign/SegmentNetworkAnalyzerModule.h>
+
+// #include <tracking/trackFindingVXD/segmentNetwork/Segment.h>
+#include <tracking/spacePointCreation/SpacePoint.h>
+#include <tracking/spacePointCreation/PurityCalculatorTools.h>
+// #include <tracking/spacePointCreation/MCVXDPurityInfo.h>
+
+#include <vector>
+
+using namespace Belle2;
+using namespace std;
+
+REG_MODULE(SegmentNetworkAnalyzer);
+
+SegmentNetworkAnalyzerModule::SegmentNetworkAnalyzerModule() :
+  Module(),
+  m_rFilePtr(nullptr),
+  m_treePtr(nullptr)
+{
+  setDescription("Module for analyzing the SegmentNetwork.");
+
+  addParam("networkInputName", m_PARAMnetworkName, "name of the StoreObjPtr to the DNN Container");
+  addParam("rootFileName", m_PARAMrootFileName, "name of the output root file name",
+           std::string("SegmentNetworkAnalyzer_output.root"));
+
+}
+
+void SegmentNetworkAnalyzerModule::initialize()
+{
+  B2INFO("SegmentNetworkAnalyzer::initialize() ------------------------------");
+  m_network.isRequired(m_PARAMnetworkName);
+  m_mcParticles = StoreArray<MCParticle>("");
+  m_mcParticles.isRequired();
+
+  m_rFilePtr = new TFile(m_PARAMrootFileName.c_str(), "recreate");
+  m_treePtr = new TTree("SegmentNetworkAnalyzer", "segment network analysis output");
+
+  makeBranches();
+}
+
+void SegmentNetworkAnalyzerModule::event()
+{
+  m_rootVariables = RootVariables(); // clear rootVariables
+
+  auto& hitNetwork = m_network->accessHitNetwork();
+  auto& segmentNetwork = m_network->accessSegmentNetwork();
+  m_rootVariables.networkSize = segmentNetwork.size();
+  m_rootVariables.networkConnections = getNConnections(segmentNetwork);
+
+  for (const auto& outerHitPair : hitNetwork.getNodes()) {
+    auto outerHit = outerHitPair.second;
+    for (const auto& centerHit : outerHit->getInnerNodes()) {
+      for (const auto& innerHit : centerHit->getInnerNodes()) {
+
+        Segment<TrackNode>* innerSegment = new Segment<TrackNode>(centerHit->getEntry().sector->getFullSecID(),
+                                                                  innerHit->getEntry().sector->getFullSecID(),
+                                                                  &centerHit->getEntry(),
+                                                                  &innerHit->getEntry());
+        Segment<TrackNode>* outerSegment = new Segment<TrackNode>(outerHit->getEntry().sector->getFullSecID(),
+                                                                  centerHit->getEntry().sector->getFullSecID(),
+                                                                  &outerHit->getEntry(),
+                                                                  &centerHit->getEntry());
+
+        bool passed = false;
+        // check if the outerSegment is in the network and then look if the inner is connected to it
+        if (auto outerNode = segmentNetwork.getNode(outerSegment->getName())) {
+          for (const auto& connectedNode : outerNode->getInnerNodes()) {
+            if (connectedNode->getEntry() == *innerSegment) {
+              passed = true;
+              break;
+            }
+          }
+        }
+        analyzeCombination(*innerSegment, *outerSegment, passed);
+
+      } // end inner loop
+    } // end center loop
+  } // end outer loop
+
+  m_treePtr->Fill();
+}
+
+void SegmentNetworkAnalyzerModule::terminate()
+{
+  // write and close root file
+  if (m_rFilePtr && m_treePtr) {
+    m_rFilePtr->cd();
+    m_rFilePtr->Write();
+    m_rFilePtr->Close();
+  }
+}
+
+void SegmentNetworkAnalyzerModule::makeBranches()
+{
+  m_treePtr->Branch("phi", &m_rootVariables.phi);
+  m_treePtr->Branch("theta", &m_rootVariables.theta);
+  m_treePtr->Branch("pT", &m_rootVariables.pT);
+  m_treePtr->Branch("signal", &m_rootVariables.signal);
+  m_treePtr->Branch("passed", &m_rootVariables.passed);
+  m_treePtr->Branch("pdg", &m_rootVariables.pdg);
+  m_treePtr->Branch("virtualIP", &m_rootVariables.virtualIP);
+  m_treePtr->Branch("networkSize", &m_rootVariables.networkSize);
+  m_treePtr->Branch("networkConns", &m_rootVariables.networkConnections);
+}
+
+void SegmentNetworkAnalyzerModule::analyzeCombination(const Belle2::Segment<Belle2::TrackNode>& outer,
+                                                      const Belle2::Segment<Belle2::TrackNode>& inner,
+                                                      bool passed)
+{
+  m_rootVariables.passed.push_back(passed);
+
+  std::vector<const Belle2::SpacePoint*> combinationSPs; // order in which they are stored does not really matter
+  combinationSPs.push_back(outer.getOuterHit()->spacePoint);
+  combinationSPs.push_back(outer.getInnerHit()->spacePoint);
+  combinationSPs.push_back(inner.getInnerHit()->spacePoint);
+
+  const vector<MCVXDPurityInfo> purityInfo = createPurityInfosVec(combinationSPs);
+  auto mcId = purityInfo[0].getPurity();
+  bool signal = mcId.first >= 0 && mcId.second == 1;
+  m_rootVariables.signal.push_back((int) signal);
+
+  // define some default values for non-signal combinations
+  int pdg = 0;
+  double pT = -999;
+
+  if (signal) {
+    const MCParticle* part = m_mcParticles[mcId.first];
+    pdg = part->getPDG();
+    pT = part->getMomentum().Pt();
+  }
+
+  m_rootVariables.pdg.push_back(pdg);
+  m_rootVariables.pT.push_back(pT);
+
+  // check if the innermost hit is virtualIP, if so get the outer hit to retrieve the position information from it
+  auto spacePoint = inner.getInnerHit()->spacePoint;
+  if (spacePoint->getType() == VXD::SensorInfoBase::VXD) {
+    m_rootVariables.virtualIP.push_back(1);
+    spacePoint = inner.getOuterHit()->spacePoint;
+  } else m_rootVariables.virtualIP.push_back(0);
+
+  // angles are determined from inner most (accessible) hit in combination
+  auto position = spacePoint->getPosition();
+  m_rootVariables.phi.push_back(position.Phi());
+  m_rootVariables.theta.push_back(position.Theta());
+
+  B2DEBUG(100, "Collected combination with: phi " << position.Phi() << ", theta " << position.Theta() <<
+          ", pdg " << pdg << ", pT " << pT << ", signal " << signal << ", passed " << passed << ", mcId " << mcId.first);
+}
+
+template<typename EntryType, typename MetaInfoType >
+size_t SegmentNetworkAnalyzerModule::getNConnections(Belle2::DirectedNodeNetwork<EntryType, MetaInfoType>& network) const
+{
+  size_t nLinks{};
+  for (const auto& outerNodes : network) {
+    nLinks += outerNodes.second->getInnerNodes().size();
+  }
+  return nLinks;
+}

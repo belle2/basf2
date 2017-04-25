98fc75f6798b71fb75628bd7d1eba4fa55f1f47d
Makoto Uchida <uchida@phys.titech.ac.jp>
2017-04-25 17:28:06 +0900

    Add a new module, CDCCosmicTrackMerge, to merge two tracks in to one single track. Two tracks mean that one is reconstucted with upper half of CDC, and nother is reconstructed with lower half of CDC.

---
+++ cdc/modules/cdcRecoTrackFilter/include/CDCCosmicTrackMergeModule.h
@@ -0,0 +1,40 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2015-2016  Belle II Collaboration                         *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors:    Dong Van Thanh                                        *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+#include <framework/core/Module.h>
+
+namespace Belle2 {
+  /**
+   * Module use to select two cosmic tracks event and merger these two tracks become one.
+   * If the deleteOtherRecoTracks flag is on, it will clean up the
+   * input store array afterwards.
+   */
+  class CDCCosmicTrackMergerModule : public Module {
+  public:
+    /// Create a new instance of the module.
+    CDCCosmicTrackMergerModule();
+
+    /// Register the store arrays and store obj pointers.
+    void initialize() override;
+
+    /// Do the selection.
+    void event() override;
+
+  private:
+    /// StoreArray name from which to read the reco tracks.
+    std::string m_param_recoTracksStoreArrayName = "";
+    /// StoreArray name where the merged reco track is written.
+    std::string m_param_MergedRecoTracksStoreArrayName = "__MergedRecoTracks";
+    /// Flag to delete the not RecoTracks from the input StoreArray.
+    bool m_param_deleteOtherRecoTracks = true;
+    /// Minium NDF required for each track (up and down).
+    double m_MinimumNumHitCut = 40;
+  };
+}

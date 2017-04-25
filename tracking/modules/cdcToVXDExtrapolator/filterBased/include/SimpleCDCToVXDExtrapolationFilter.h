05b13593298141b0e867f233599163b32f970333
Nils Braun <nils.braun@desy.de>
2017-04-25 13:11:12 +0200

    Use the implemented state in the simple filter

--- tracking/modules/cdcToVXDExtrapolator/filterBased/include/SimpleCDCToVXDExtrapolationFilter.h
+++ tracking/modules/cdcToVXDExtrapolator/filterBased/include/SimpleCDCToVXDExtrapolationFilter.h
@@ -11,16 +11,28 @@
 
 #include <tracking/modules/cdcToVXDExtrapolator/filterBased/CDCTrackSpacePointCombinationVarSet.h>
 #include <tracking/trackFindingCDC/filters/base/FilterOnVarSet.h>
+#include <tracking/trackFindingCDC/utilities/StringManipulation.h>
 
 namespace Belle2 {
   class SimpleCDCToVXDExtrapolationFilter : public TrackFindingCDC::FilterOnVarSet<CDCTrackSpacePointCombinationVarSet> {
   public:
     void exposeParameters(ModuleParamList* moduleParamList,
                           const std::string& prefix) final {
-      moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "maximumXYNorm"),
-      m_param_maximumXYNorm, "", m_param_maximumXYNorm);
-      moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "maximumZNorm"),
-      m_param_maximumZNorm, "", m_param_maximumZNorm);
+      moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "maximumXYDistance"),
+      m_param_maximumXYDistance, "", m_param_maximumXYDistance);
+      moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "maximumDistance"),
+      m_param_maximumDistance, "", m_param_maximumDistance);
+      moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "maximumChi2Difference"),
+      m_param_maximumChi2Difference, "", m_param_maximumChi2Difference);
+    }
+
+    void initialize() override
+    {
+      TrackFindingCDC::FilterOnVarSet<CDCTrackSpacePointCombinationVarSet>::initialize();
+
+      B2ASSERT("You need to provide exactly 4 maximal norms (for four layers).", m_param_maximumXYDistance.size() == 4);
+      B2ASSERT("You need to provide exactly 4 maximal norms (for four layers).", m_param_maximumDistance.size() == 4);
+      B2ASSERT("You need to provide exactly 4 maximal norms (for four layers).", m_param_maximumChi2Difference.size() == 4);
     }
 
     TrackFindingCDC::Weight operator()(const BaseCDCTrackSpacePointCombinationFilter::Object& currentState) final {
@@ -31,20 +43,46 @@ namespace Belle2 {
         return std::nan("");
       }
 
-      const Float_t* xyDistance = getVarSet().find("xy_distance");
-      const Float_t* zDistance = getVarSet().find("z_distance");
-      const Float_t* sameHemisphere = getVarSet().find("same_hemisphere");
+      const double& sameHemisphere = *(getVarSet().find("same_hemisphere"));
 
-      if (*xyDistance > m_param_maximumXYNorm or fabs(*zDistance) > m_param_maximumZNorm or * sameHemisphere != 1)
+      if (sameHemisphere != 1)
       {
         return std::nan("");
       }
+
+      const unsigned int& state = currentState.getState();
+      const double& layer = *(getVarSet().find("layer"));
+
+      if (state == 0)
+      {
+        const double& xy_distance = *(getVarSet().find("xy_distance"));
+        if (xy_distance > m_param_maximumXYDistance[layer - 3]) {
+          return std::nan("");
+        }
+      } else if (state == 1)
+      {
+        const double& distance = *(getVarSet().find("distance"));
+        if (distance > m_param_maximumDistance[layer - 3]) {
+          return std::nan("");
+        }
+      } else if (state == 2)
+      {
+        const double& lastChi2 = currentState.getLastChi2();
+        const double& chi2 = currentState.getChi2();
+        if (chi2 - lastChi2 > m_param_maximumChi2Difference[layer - 3]) {
+          return std::nan("");
+        }
+      } else {
+        B2FATAL("Invalid state encountered");
+      }
+
 
       return 1;
     }
 
   private:
-    double m_param_maximumXYNorm = 0.5;
-    double m_param_maximumZNorm = 5;
+    std::vector<double> m_param_maximumXYDistance {10, 10, 10, 10};
+    std::vector<double> m_param_maximumDistance {20, 20, 20, 20};
+    std::vector<double> m_param_maximumChi2Difference {100, 100, 100, 100};
   };
 }

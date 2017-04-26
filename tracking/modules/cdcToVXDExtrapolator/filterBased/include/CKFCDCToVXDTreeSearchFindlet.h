2e765bf604b69971af661aa4455741c7fc17db9a
Nils Braun <nils.braun@desy.de>
2017-04-26 09:48:10 +0200

    Use the cache

--- tracking/modules/cdcToVXDExtrapolator/filterBased/include/CKFCDCToVXDTreeSearchFindlet.h
+++ tracking/modules/cdcToVXDExtrapolator/filterBased/include/CKFCDCToVXDTreeSearchFindlet.h
@@ -34,121 +34,146 @@ namespace Belle2 {
     LayerToggleCDCToVXDExtrapolationFilter<CDCTrackSpacePointCombinationFilterFactory>> {
   public:
     using Super = TrackFindingCDC::TreeSearchFindlet<CKFCDCToVXDStateObject,
-          CDCTrackSpacePointCombinationFilterFactory>;
+          LayerToggleCDCToVXDExtrapolationFilter<CDCTrackSpacePointCombinationFilterFactory>>;
 
+    void exposeParameters(ModuleParamList* moduleParamList, const std::string& prefix) override
+    {
+      Super::exposeParameters(moduleParamList, prefix);
+
+      moduleParamList->addParameter("useCaching", m_param_useCaching, "Use caching or not", m_param_useCaching);
+    }
   private:
     TrackFindingCDC::SortedVectorRange<const SpacePoint*> getMatchingHits(Super::StateIterator currentState) final {
-      const unsigned int nextLayer = currentState->getLastLayer() - 1;
+      const unsigned int nextLayer = currentState->getLayer() - 1;
       return m_cachedHitMap[nextLayer];
     }
 
     bool fit(Super::StateIterator currentState) final {
+      B2ASSERT("Encountered invalid state", not currentState->isFitted() and currentState->isAdvanced());
+
       const SpacePoint* spacePoint = currentState->getSpacePoint();
 
-      if (spacePoint)
+      if (not spacePoint)
       {
-        genfit::MeasuredStateOnPlane& measuredStateOnPlane = currentState->getMeasuredStateOnPlane();
-        double& chi2 = currentState->getChi2();
-
-        // We will change the state x_k, the covariance C_k and the chi2
-        Eigen::Vector5d x_k_old(measuredStateOnPlane.getState().GetMatrixArray());
-        Eigen::Matrix5d C_k_old(measuredStateOnPlane.getCov().GetMatrixArray());
-
-        // Loop over the two clusters and extract the change for x_k and C_k.
-        for (const SVDCluster& relatedCluster : spacePoint->getRelationsTo<SVDCluster>()) {
-          SVDRecoHit clusterMeasurement(&relatedCluster);
-          const std::vector<genfit::MeasurementOnPlane*> measurementsOnPlane = clusterMeasurement.constructMeasurementsOnPlane(
-            measuredStateOnPlane);
-
-          B2ASSERT("There should be exactly one measurement on plane", measurementsOnPlane.size() == 1);
-          const genfit::MeasurementOnPlane& measurementOnPlane = *(measurementsOnPlane.front());
-
-          Eigen::Vector1d m_k(measurementOnPlane.getState().GetMatrixArray());
-          Eigen::RowVector5d H_k(measurementOnPlane.getHMatrix()->getMatrix().GetMatrixArray());
-          Eigen::Vector5d H_k_t = H_k.transpose();
-          Eigen::Vector1d V_k(measurementOnPlane.getCov().GetMatrixArray());
-
-          const Eigen::Vector5d& K_k = C_k_old * H_k_t * (V_k + H_k * C_k_old * H_k_t).inverse();
-
-          B2DEBUG(200, "C_k_old " << C_k_old);
-          B2DEBUG(200, "H_k " << H_k);
-          B2DEBUG(200, "m_k " << m_k);
-          B2DEBUG(200, "V_k " << V_k);
-          B2DEBUG(200, "x_k_old " << x_k_old);
-          B2DEBUG(200, "K_k " << K_k);
-
-          C_k_old -= K_k * H_k * C_k_old;
-          x_k_old += K_k * (m_k - H_k * x_k_old);
-
-          Eigen::Vector1d residual = m_k - H_k * x_k_old;
-
-          chi2 += residual.transpose() * (V_k - H_k * C_k_old * H_k_t).inverse() * residual;
-
-          B2DEBUG(200, "C_k_old " << C_k_old);
-          B2DEBUG(200, "x_k_old " << x_k_old);
-          B2DEBUG(100, "chi2 " << chi2);
-        }
-
-        measuredStateOnPlane.setState(TVectorD(5, x_k_old.data()));
-        measuredStateOnPlane.setCov(TMatrixDSym(5, C_k_old.data()));
+        currentState->setFitted();
+        return true;
       }
 
-      currentState->setState(2);
+      genfit::MeasuredStateOnPlane& measuredStateOnPlane = currentState->getMeasuredStateOnPlane();
+      double& chi2 = currentState->getChi2();
 
+      // We will change the state x_k, the covariance C_k and the chi2
+      Eigen::Vector5d x_k_old(measuredStateOnPlane.getState().GetMatrixArray());
+      Eigen::Matrix5d C_k_old(measuredStateOnPlane.getCov().GetMatrixArray());
+
+      // Loop over the two clusters and extract the change for x_k and C_k.
+      for (const SVDCluster& relatedCluster : spacePoint->getRelationsTo<SVDCluster>())
+      {
+        SVDRecoHit clusterMeasurement(&relatedCluster);
+        // Important: measuredStateOnPlane must already be extrapolated to the correct plane.
+        // Only the plane and the rep are accessed (but the rep has no meaningful members).
+        const std::vector<genfit::MeasurementOnPlane*> measurementsOnPlane = clusterMeasurement.constructMeasurementsOnPlane(
+          measuredStateOnPlane);
+
+        B2ASSERT("There should be exactly one measurement on plane", measurementsOnPlane.size() == 1);
+        const genfit::MeasurementOnPlane& measurementOnPlane = *(measurementsOnPlane.front());
+
+        Eigen::Vector1d m_k(measurementOnPlane.getState().GetMatrixArray());
+        Eigen::RowVector5d H_k(measurementOnPlane.getHMatrix()->getMatrix().GetMatrixArray());
+        Eigen::Vector5d H_k_t = H_k.transpose();
+        Eigen::Vector1d V_k(measurementOnPlane.getCov().GetMatrixArray());
+
+        const Eigen::Vector5d& K_k = C_k_old * H_k_t * (V_k + H_k * C_k_old * H_k_t).inverse();
+
+        B2DEBUG(200, "C_k_old " << C_k_old);
+        B2DEBUG(200, "H_k " << H_k);
+        B2DEBUG(200, "m_k " << m_k);
+        B2DEBUG(200, "V_k " << V_k);
+        B2DEBUG(200, "x_k_old " << x_k_old);
+        B2DEBUG(200, "K_k " << K_k);
+
+        C_k_old -= K_k * H_k * C_k_old;
+        x_k_old += K_k * (m_k - H_k * x_k_old);
+
+        Eigen::Vector1d residual = m_k - H_k * x_k_old;
+
+        chi2 += residual.transpose() * (V_k - H_k * C_k_old * H_k_t).inverse() * residual;
+
+        B2DEBUG(200, "C_k_old " << C_k_old);
+        B2DEBUG(200, "x_k_old " << x_k_old);
+        B2DEBUG(100, "chi2 " << chi2);
+      }
+
+      measuredStateOnPlane.setState(TVectorD(5, x_k_old.data()));
+      measuredStateOnPlane.setCov(TMatrixDSym(5, C_k_old.data()));
+
+      currentState->setFitted();
       return true;
     }
 
     bool advance(Super::StateIterator currentState) final {
+      B2ASSERT("Encountered invalid state", not currentState->isFitted() and not currentState->isAdvanced());
+
       const SpacePoint* spacePoint = currentState->getSpacePoint();
+
+      if (not spacePoint)
+      {
+        currentState->setAdvanced();
+        return true;
+      }
+
+      // We always use the first cluster here to create the plane. Should not make much difference?
+      SVDRecoHit recoHit(spacePoint->getRelated<SVDCluster>());
+
+      // This is the mSoP we will edit.
       genfit::MeasuredStateOnPlane& measuredStateOnPlane = currentState->getMeasuredStateOnPlane();
 
-      if (spacePoint)
+      // This mSoP may help us for extrapolation
+      genfit::MeasuredStateOnPlane& parentsCachedMSoP = currentState->getParentsCachedMeasuredStateOnPlane();
+
+      // Possibility 1: extrapolate onto a "common ground", e.g. the average radius and store this as a cache.
+      // In possibility 2 (below), we start with the former
+      if (m_param_useCaching and m_param_useCachingOne)
       {
-        SVDRecoHit recoHit(spacePoint->getRelated<SVDCluster>());
-        const VxdID& vxdID = recoHit.getSensorID();
-        const short id = vxdID.getID();
-
-        // TODO: Test if it is possible to not use the ID, but something larger here
-
-        // Extrapolate from k-1 -> k. The mSoP is copied from the state before, so it is k-1.
-        // By extrapolating, we will will go to k.
-        const auto& cachedMSOP = m_cachedMeasuredStates.find(id);
-        if (cachedMSOP == m_cachedMeasuredStates.end()) {
-          const genfit::SharedPlanePtr& plane = recoHit.constructPlane(measuredStateOnPlane);
-
-          B2DEBUG(100, "New Plane: " << id << " " << vxdID.getLayerNumber() << " " << vxdID.getLadderNumber() << " " <<
-          vxdID.getSensorNumber());
-          B2DEBUG(100, plane->getO().X() << " " << plane->getO().Y() << " " << plane->getO().Z());
-          B2DEBUG(100, plane->getU().X() << " " << plane->getU().Y() << " " << plane->getU().Z());
-          B2DEBUG(100, plane->getV().X() << " " << plane->getV().Y() << " " << plane->getV().Z());
-
-          try {
-            measuredStateOnPlane.extrapolateToPlane(plane);
-
-            B2DEBUG(100, "Result:");
-            B2DEBUG(100, measuredStateOnPlane.getPos().X() << " " << measuredStateOnPlane.getPos().Y() << " " <<
-            measuredStateOnPlane.getPos().Z());
-            B2DEBUG(100, measuredStateOnPlane.getMom().X() << " " << measuredStateOnPlane.getMom().Y() << " " <<
-            measuredStateOnPlane.getMom().Z());
-
-            m_cachedMeasuredStates[id] = measuredStateOnPlane;
-          } catch (genfit::Exception e) {
-            B2WARNING(e.what());
-            m_cachedMeasuredStates[id] = boost::none;
-            return false;
-          }
-        } else {
-          if (cachedMSOP->second) {
-            measuredStateOnPlane.setStateCovPlane(cachedMSOP->second->getState(), cachedMSOP->second->getCov(), cachedMSOP->second->getPlane());
-            measuredStateOnPlane.setAuxInfo(cachedMSOP->second->getAuxInfo());
-          } else {
-            return false;
-          }
+        // Test if we have already calculated an extrapolated state in former calculations. The parent
+        // knows about this.
+        if (not currentState->parentHasCache()) {
+          // If not, use the parents mSoP (which is copied into the cache before) as a starting point and extrapolate this.
+          // parentsCachedMSoP.extrapolateToCone();
+          B2FATAL("Extrapolation not implemented in the moment");
+          currentState->setParentHasCache();
         }
       }
 
-      currentState->setState(1);
+      // only use the cache if it is there.
+      // Otherwise, just stay with the mSoP, which is equal to the parents (fitted) state.
+      if (m_param_useCaching and currentState->parentHasCache())
+      {
+        measuredStateOnPlane = parentsCachedMSoP;
+      }
 
+      // The mSoP plays no role here (it is unused in the function)
+      const genfit::SharedPlanePtr& plane = recoHit.constructPlane(measuredStateOnPlane);
+
+      try {
+        measuredStateOnPlane.extrapolateToPlane(plane);
+      } catch (genfit::Exception e)
+      {
+        B2WARNING(e.what());
+        return false;
+      }
+
+
+      // Possibility 2: extrapolate this state as normal and store this as a cache (the other then maybe have to
+      //                extrapolate back a bit, but the calculation is much easier)
+      if (m_param_useCaching and not m_param_useCachingOne)
+      {
+        // TODO: Always or only store the first one?
+        parentsCachedMSoP = measuredStateOnPlane;
+        currentState->setParentHasCache();
+      }
+
+      currentState->setAdvanced();
       return true;
     }
 
@@ -179,15 +204,12 @@ namespace Belle2 {
       }
     }
 
-    void initializeSeedCache(RecoTrack*) final {
-      m_cachedMeasuredStates.clear();
-    }
-
 
   private:
     /// Cache for sorted hits
     std::map<unsigned int, TrackFindingCDC::SortedVectorRange<const SpacePoint*>> m_cachedHitMap;
-    /// Cache for extrapolated states
-    std::map<short, boost::optional<genfit::MeasuredStateOnPlane>> m_cachedMeasuredStates;
+    /// Parameter: which caching method to use
+    bool m_param_useCachingOne = false;
+    bool m_param_useCaching = true;
   };
 }

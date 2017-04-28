78a589a5014d52ed8e3266ded1b14e328443426d
Jan Strube <jan.strube@pnnl.gov>
2017-04-29 05:34:15 +0900

    saving the plots to disk rather than showing

--- top/examples/plotTOPPDFCollection.py
+++ top/examples/plotTOPPDFCollection.py
@@ -11,6 +11,16 @@ x = t.TOPPDFCollections[0]
 # we create the image from the channels on X, and time bins on Y
 X, Y = np.meshgrid(range(512), np.linspace(0, 0.3 * 199, 200))
 # x.m_data is a map of hypotheses to a 2D PDF
-Z = np.array(x.m_data[1])
+Z = np.array(x.m_data[1]).T
 plt.pcolor(X, Y, Z)
-plt.show()
+
+ch = []
+times = []
+digits = t.TOPDigits
+for d in digits:
+    ch.append(d.getChannel())
+    times.append(d.getTime())
+
+plt.plot(ch, times, 'ro')
+# plt.show()
+plt.savefig("pdf.png")

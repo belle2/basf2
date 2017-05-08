5885bbd76af3c761d3576504c1772e4707b6d960
igal <igjaegle@ufl.edu>
2017-05-08 14:49:43 -0400

    Adapt CSI digitization and study to a daily threshold change

--- beast/csi/modules/src/CsiStudy_v2Module.cc
+++ beast/csi/modules/src/CsiStudy_v2Module.cc
@@ -62,6 +62,30 @@ CsiStudy_v2Module::~CsiStudy_v2Module()
 //This module is a histomodule. Any histogram created here will be saved by the HistoManager module
 void CsiStudy_v2Module::defineHisto()
 {
+  for (int i = 0; i < 153; i++) {
+    h_csi_drate[i] = new TH1F(TString::Format("csi_rate_%d", i), "count", 18, 0., 18.);
+    h_csi_drate[i]->Sumw2();
+
+    h_csi_rs_drate[i] = new TH2F(TString::Format("csi_rs_drate_%d", i), "count v. ring section", 18, 0., 18., 12, 0., 12.);
+    h_csi_rs_drate[i]->Sumw2();
+
+    for (int j = 0; j < 18; j++) {
+
+      h_csi_dedep[j][i] = new TH1F(TString::Format("csi_dedep_%d_%d", j, i), "Energy deposited [MeV]", 5000, 0., 400.);
+      h_csi_rs_dedep[j][i] = new TH2F(TString::Format("csi_rs_dedep_%d_%d", j, i), "Energy deposited [MeV]", 5000, 0., 400., 12, 0., 12.);
+
+      h_csi_denergy[j][i] = new TH1F(TString::Format("csi_denergy_%d_%d", j, i), "Energy deposited [MeV]", 200, 0.1, 3.);
+      h_csi_rs_denergy[j][i] = new TH2F(TString::Format("csi_rs_denergy_%d_%d", j, i), "Energy deposited [MeV] per section", 200, 0.1, 3.,
+                                        12, 0.,
+                                        12.);
+
+      h_csi_dedep[j][i]->Sumw2();
+      h_csi_rs_dedep[j][i]->Sumw2();
+      h_csi_denergy[j][i]->Sumw2();
+      h_csi_rs_denergy[j][i]->Sumw2();
+    }
+  }
+  /*
   for (int i = 0; i < 2; i++) {
     h_csi_rate[i] = new TH1F(TString::Format("csi_rate_%d", i), "count", 18, 0., 18.);
     h_csi_rate[i]->Sumw2();
@@ -139,7 +163,7 @@ void CsiStudy_v2Module::defineHisto()
     h_csi_rs_edep1Weight[i]->Sumw2();
     h_csi_rs_edep2Weight[i]->Sumw2();
   }
-
+  */
 }
 
 
@@ -171,11 +195,11 @@ void CsiStudy_v2Module::event()
   }
 
   //Look at the meta data to extract IR rate and scattering ring section
-  double rate = 0;
+  //double rate = 0;
   int ring_section = -1;
   int section_ordering[12] = {1, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
   for (const auto& MetaHit : MetaHits) {
-    rate = MetaHit.getrate();
+    //rate = MetaHit.getrate();
     double sad_ssraw = MetaHit.getssraw();
     double ssraw = 0;
     if (sad_ssraw >= 0) ssraw = sad_ssraw / 100.;
@@ -183,7 +207,7 @@ void CsiStudy_v2Module::event()
     ring_section = section_ordering[(int)((ssraw) / 250.)] - 1;
     //ring_section = MetaHit.getring_section() - 1;
   }
-
+  /*
   //Loop over SimHit
   for (const auto& SimHit : SimHits) {
     int detNB = SimHit.getCellId();
@@ -200,13 +224,14 @@ void CsiStudy_v2Module::event()
     h_csi_energyVrs1[detNB]->Fill(log10(Edep), ring_section);
     h_csi_energyVrs1W[detNB]->Fill(log10(Edep), ring_section, rate);
   }
-
+  */
   //Loop over DigiHit
   for (const auto& Hit : Hits) {
     int detNB = Hit.getCellId();
-    double Edep = Hit.getEnergyDep() * 1e3; //GeV -> MeV
+    //double Edep = Hit.getEnergyDep() * 1e3; //GeV -> MeV
     double RecEdep = Hit.getEnergyRecDep() * 1e3; //GeV -> MeV
-    double tof = Hit.getFlightTime(); //ns
+    //double tof = Hit.getFlightTime(); //ns
+    /*
     h_csi_rate[0]->Fill(detNB);
     h_csi_rate[1]->Fill(detNB, rate);
     h_csi_edep1[detNB]->Fill(Edep);
@@ -228,6 +253,20 @@ void CsiStudy_v2Module::event()
     h_csi_energyVrs3[detNB]->Fill(log10(RecEdep), ring_section);
     h_csi_energyVrs2W[detNB]->Fill(log10(Edep), ring_section, rate);
     h_csi_energyVrs3W[detNB]->Fill(log10(RecEdep), ring_section, rate);
+    */
+    for (int i = 0; i < 153; i ++) {
+      if (m_Thres_hitRate[detNB][i] >= RecEdep) {
+        h_csi_drate[i]->Fill(detNB);
+        h_csi_rs_drate[i]->Fill(detNB, ring_section);
+      }
+      if (m_Thres_sumE[detNB][i] >= RecEdep) {
+        h_csi_dedep[detNB][i]->Fill(RecEdep);
+        h_csi_rs_dedep[detNB][i]->Fill(RecEdep, ring_section);
+
+        h_csi_denergy[detNB][i]->Fill(log10(RecEdep));
+        h_csi_rs_denergy[detNB][i]->Fill(log10(RecEdep), ring_section);
+      }
+    }
   }
 }
 //read tube centers, impulse response, and garfield drift data filename from CSI.xml
@@ -235,6 +274,20 @@ void CsiStudy_v2Module::getXMLData()
 {
   GearDir content = GearDir("/Detector/DetectorComponent[@name=\"CSI\"]/Content/");
   m_Ethres = content.getDouble("Ethres");
+
+
+  for (int i = 0; i < 18; i ++) {
+    int iThres_hitRate = 0;
+    for (double Thres : content.getArray(TString::Format("thres_hitRate_det%d", i).Data(), {0})) {
+      m_Thres_hitRate[i][iThres_hitRate] = Thres;
+      iThres_hitRate++;
+    }
+    int iThres_sumE = 0;
+    for (double Thres : content.getArray(TString::Format("thres_sumE_det%d", i).Data(), {0})) {
+      m_Thres_sumE[i][iThres_sumE] = Thres;
+      iThres_sumE++;
+    }
+  }
 
   B2INFO("CsiStudy_v2");
 

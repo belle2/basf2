25bbded8f859b7973e19b6cb8e8b09091fa54d41
Jan Strube <jan.strube@pnnl.gov>
2017-05-10 13:00:20 +0900

    adding more functionality to the qualityPlotter

--- top/examples/TOPFE_qualityPlots.py
+++ top/examples/TOPFE_qualityPlots.py
@@ -98,21 +98,45 @@ def wf_display(waveform, run, event, suffix=""):
     c1.SaveAs(pdfFile + suffix + '.pdf')
 
 
+def peakWidths(data, peaks):
+    """
+    Finds the values at 1/2 the peak value to the left and the right of the peak.
+    Ignores baselines
+    """
+    widths = []
+    for p in peaks:
+        if p < 0:
+            widths.append(-1)
+            continue
+        left = p
+        right = p
+        while -1 < left and data[left] > 0.5 * data[p]:
+            left -= 1
+        while right < len(data) and data[right] > 0.5 * data[p]:
+            right += 1
+        w = -1
+        if left > -1 and right < len(data):
+            w = right - left
+        widths.append(w)
+    return widths
+
+
 class WaveformDumper(Module):
     '''
     Dumps top digit information into hdf5
     '''
-
     def initialize(self):
         ''' Initialize the Module: open the canvas. '''
         print("WaveformDumper init")
-        self.feProps_calPulse = TNtuple(
-            "feProps_calPulse",
-            "feProps_calPulse",
-            "pyPeak0Ht:pyPeak1Ht:fePeakHt:fePeakWd:fePeakIntegral:nPyPeaks")
-        self.feProps_signal = TNtuple("feProps_signal", "feProps_signal", "pyPeak0Ht:fePeakHt:fePeakWd:fePeakIntegral:nPyPeaks")
+        self.feProps = TNtuple(
+            "feProps",
+            "feProps",
+            "pyPeak0Ht:pyPeak0TDC:pyPeak0Wd:pyPeak1Ht:pyPeak1TDC:pyPeak1Wd:fePeakHt:fePeakTDC:fePeakWd:fePeakIntegral:nPyPeaks:ch")
         self.nWaveForms = 0
         self.nWaveFormsOutOfOrder = 0
+        self.f = TFile.Open("ntuple.root", "RECREATE")
+        # this variable counts how many plots were created. Stop after 10
+        self.plotCounter = 0
 
     def event(self):
         '''
@@ -123,10 +147,14 @@ class WaveformDumper(Module):
         run = evtMetaData.obj().getRun()
         waveforms = Belle2.PyStoreArray('TOPRawWaveforms')
         for waveform in waveforms:
-            self.nWaveForms += 1
-            if not waveform.areWindowsInOrder():
-                self.nWaveFormsOutOfOrder += 1
             chan = waveform.getChannel()
+            self.nWaveForms += 1
+            # waveform.areWindowsInOrder is a bit too strict at the moment
+            wins = np.array(waveform.getReferenceWindows())
+            if not np.all(wins[:-1] <= wins[1:]):
+                self.nWaveFormsOutOfOrder += 1
+                wf_display(waveform, run, event, "windowOrder")
+                self.plotCounter += 1
             wf = np.array(waveform.getWaveform())
             indexes = peakutils.indexes(wf, thres=0.5, min_dist=20)
             rawDigits = waveform.getRelationsWith("TOPRawDigits")
@@ -135,31 +163,62 @@ class WaveformDumper(Module):
                 print("Warning: #TOPRawDigits is not 1, but", len(rawDigits))
             raw = rawDigits[0]
             fePeakHt = raw.getValuePeak()
+            fePeakTDC = raw.getSamplePeak()
             fePeakWd = raw.getSampleFall() - raw.getSampleRise()
             fePeakIntegral = raw.getIntegral()
             # we are sorting the peaks. The FE should always point to the highest one
-            idx = sorted(wf[indexes], reverse=True)
+            idx = np.argsort(wf[indexes])
             pyPeak0Ht = -1
+            pyPeak0TDC = -1
             if nPyPeaks > 2 and 0 == 1:
                 wf_display(waveform, run, event, "manyPeaks")
+                self.plotCounter += 1
             elif nPyPeaks > 0:
-                pyPeak0Ht = idx[0]
+                pyPeak0Ht = wf[indexes][idx[-1]]
+                pyPeak0TDC = indexes[idx[-1]]
+                assert pyPeak0Ht == wf[pyPeak0TDC]
             if pyPeak0Ht != fePeakHt and 0 == 1:
                 print(pyPeak0Ht, fePeakHt)
                 wf_display(waveform, run, event, "notHighest")
-            if chan == 0:
-                pyPeak1Ht = -1
-                if nPyPeaks > 1:
-                    pyPeak1Ht = idx[1]
-                self.feProps_calPulse.Fill(pyPeak0Ht, pyPeak1Ht, fePeakHt, fePeakWd, fePeakIntegral, nPyPeaks)
-            else:
-                self.feProps_signal.Fill(pyPeak0Ht, fePeakHt, fePeakWd, fePeakIntegral, nPyPeaks)
+                self.plotCounter += 1
+            if 0 < pyPeak0TDC < 65 and chan % 8 == 0 and 1 == 0:
+                wf_display(waveform, run, event, "calPuls_firstWin")
+                self.plotCounter += 1
+
+            pyPeak1Ht = -1
+            pyPeak1TDC = -1
+            # the channel we get from the waveform encodes the asic, carrier, and the actual channel
+            if nPyPeaks > 1:
+                pyPeak1Ht = wf[indexes][idx[-2]]
+                pyPeak1TDC = indexes[idx[-2]]
+                assert pyPeak1Ht == wf[pyPeak1TDC]
+            pyPeak0Wd, pyPeak1Wd = peakWidths(wf, [pyPeak0TDC, pyPeak1TDC])
+            if 1 == 0 and (pyPeak0Wd < 5 or nPyPeaks > 1 and pyPeak1Wd < 5):
+                wf_display(waveform, run, event, "thinpeak")
+                self.plotCounter += 1
+            self.feProps.Fill(pyPeak0Ht,
+                              pyPeak0TDC,
+                              pyPeak0Wd,
+                              pyPeak1Ht,
+                              pyPeak1TDC,
+                              pyPeak1Wd,
+                              fePeakHt,
+                              fePeakTDC,
+                              fePeakWd,
+                              fePeakIntegral,
+                              nPyPeaks,
+                              chan
+                              )
+            if 0 == 0 and (145 < pyPeak1Ht < 155 and chan % 8 == 0):
+                wf_display(waveform, run, event, "thinpeak")
+                self.plotCounter += 1
+            # only plot the first 10 figures.
+            if self.plotCounter >= 100:
+                evtMetaData.obj().setEndOfData()
 
     def terminate(self):
-        f = TFile.Open("ntuple.root", "RECREATE")
-        f.WriteTObject(self.feProps_calPulse)
-        f.WriteTObject(self.feProps_signal)
-        f.Close()
+        self.f.WriteTObject(self.feProps)
+        self.f.Close()
         print("# Waveforms:", self.nWaveForms)
         print("out of order:", self.nWaveFormsOutOfOrder)
 

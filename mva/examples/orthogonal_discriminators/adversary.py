9da2a53d75a9816a4256237d105759dfde643ca4
Thomas Keck <thomas.keck2@kit.edu>
2017-05-11 12:06:12 +0200

    Started to implemented examples for othogonal_discriminators

--- mva/examples/orthogonal_discriminators/adversary.py
+++ mva/examples/orthogonal_discriminators/adversary.py
@@ -136,11 +136,7 @@ def partial_fit(state, X, S, y, w, epoch):
 
 
 if __name__ == "__main__":
-
-    general_options = basf2_mva.GeneralOptions()
-    general_options.m_datafiles = basf2_mva.vector("train.root")
-    general_options.m_treename = "tree"
-    variables = ['p', 'pt', 'pz',
+    variables = ['M', 'p', 'pt', 'pz',
                  'daughter(0, p)', 'daughter(0, pz)', 'daughter(0, pt)',
                  'daughter(1, p)', 'daughter(1, pz)', 'daughter(1, pt)',
                  'daughter(2, p)', 'daughter(2, pz)', 'daughter(2, pt)',
@@ -148,19 +144,20 @@ if __name__ == "__main__":
                  'daughter(0, dr)', 'daughter(1, dr)',
                  'daughter(0, dz)', 'daughter(1, dz)',
                  'daughter(0, chiProb)', 'daughter(1, chiProb)', 'daughter(2, chiProb)',
-                 'daughter(0, Kid)', 'daughter(0, piid)',
-                 'daughterInvariantMass(0, 1)', 'daughterInvariantMass(0, 2)', 'daughterInvariantMass(1, 2)']
+                 'daughter(0, Kid)', 'daughter(0, piid)']
+
+    general_options = basf2_mva.GeneralOptions()
+    general_options.m_datafiles = basf2_mva.vector("train.root")
+    general_options.m_treename = "tree"
     general_options.m_variables = basf2_mva.vector(*variables)
-    general_options.m_spectators = basf2_mva.vector('M')
+    general_options.m_spectators = basf2_mva.vector('daughterInvariantMass(0, 1)', 'daughterInvariantMass(0, 2)')
     general_options.m_target_variable = "isSignal"
+    general_options.m_identifier = "adversary"
 
     specific_options = basf2_mva.PythonOptions()
     specific_options.m_framework = "tensorflow"
-    specific_options.m_steering_file = 'mva/examples/tensorflow/adversary.py'
+    specific_options.m_steering_file = 'mva/examples/orthogonal_discriminators/adversary.py'
     specific_options.m_nIterations = 400
     specific_options.m_mini_batch_size = 100
-
-    for i, l in enumerate([1, 2, 5, 10, 20, 50, 100]):
-        general_options.m_identifier = "TensorflowAdversary_{}".format(i)
-        specific_options.m_config = '{"adversary_steps": 20, "learning_rate": 0.001, "lambda": ' + str(i) + '}'
-        basf2_mva.teacher(general_options, specific_options)
+    specific_options.m_config = '{"adversary_steps": 20, "learning_rate": 0.001, "lambda": 10}'
+    basf2_mva.teacher(general_options, specific_options)

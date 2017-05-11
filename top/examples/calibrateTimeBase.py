a97b4158561c614aee93f919536bea2582115a46
staric <marko.staric@ijs.si>
2017-05-11 10:22:40 +0000

    updates of TOPDoublePulseGenerator and TOPTimeBaseCalibrator modules + examples

--- top/examples/calibrateTimeBase.py
+++ top/examples/calibrateTimeBase.py
@@ -5,7 +5,7 @@ from basf2 import *
 import math
 
 # ----------------------------------------------------------------------------
-# Example of generating double calibration pulses with TOPDoublePulseGenerator
+# Example of running time base calibration using simulated double pulses
 # ----------------------------------------------------------------------------
 
 # local database with Kichimi-san TBC and T0 constants
@@ -13,12 +13,18 @@ reset_database()
 pathTo = '/ghi/fs01/belle2/bdata/group/detector/TOP/DB_TBT0_20170421/'  # on KEKCC
 use_local_database(pathTo + "localDB/localDB.txt", pathTo + "localDB")
 
+# slot number to calibrate
+moduleID = 5
+
+# Suppress messages and warnings during processing:
+set_log_level(LogLevel.ERROR)
+
 # Create path
 main = create_path()
 
 # Set number of events to generate
 eventinfosetter = register_module('EventInfoSetter')
-eventinfosetter.param({'evtNumList': [100], 'runList': [1]})
+eventinfosetter.param({'evtNumList': [10000], 'runList': [1]})
 main.add_module(eventinfosetter)
 
 # Gearbox: access to database (xml files)
@@ -33,16 +39,20 @@ main.add_module(geometry)
 # pulse generator
 calpulse = register_module('TOPDoublePulseGenerator')
 calpulse.param('asicChannels', [0])
-calpulse.param('moduleIDs', [1])
-# intervals = [math.exp(-float(i)/99.0)+1 for i in range(128)]
-# intervals = intervals + intervals
-# calpulse.param('sampleTimeIntervals', intervals)
+calpulse.param('moduleIDs', [moduleID])
 calpulse.param('useDatabase', True)
 calpulse.param('outputFileName', 'usedSampleTimes.root')
 main.add_module(calpulse)
 
-# output
-main.add_module('RootOutput')
+# TB calibrator
+calib = register_module('TOPTimeBaseCalibrator')
+calib.param('moduleID', moduleID)
+calib.param('minTimeDiff', 40)
+calib.param('maxTimeDiff', 80)
+calib.param('directoryName', 'tbc')
+calib.param('method', 1)
+calib.logging.log_level = LogLevel.INFO
+main.add_module(calib)
 
 # Show progress of processing
 progress = register_module('Progress')
@@ -53,3 +63,4 @@ process(main)
 
 # Print call statistics
 print(statistics)
+print(statistics(statistics.TERM))

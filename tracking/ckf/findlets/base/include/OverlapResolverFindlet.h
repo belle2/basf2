f7164701a8ef234c76f42b416987a8f93e89c880
Nils Braun <nils.braun@desy.de>
2017-05-11 09:34:08 +0200

    Change the way the overlapresolver handles NAN weights to not run into segfaults again

--- tracking/ckf/findlets/base/include/OverlapResolverFindlet.h
+++ tracking/ckf/findlets/base/include/OverlapResolverFindlet.h
@@ -12,6 +12,7 @@
 #include <tracking/trackFindingCDC/findlets/base/Findlet.h>
 #include <tracking/trackFindingCDC/utilities/StringManipulation.h>
 #include <tracking/trackFindingCDC/utilities/Algorithms.h>
+#include <tracking/trackFindingCDC/numerics/WithWeight.h>
 
 #include <tracking/trackFindingVXD/trackSetEvaluator/OverlapResolverNodeInfo.h>
 #include <tracking/trackFindingVXD/trackSetEvaluator/HopfieldNetwork.h>
@@ -29,6 +30,7 @@ namespace Belle2 {
 
       m_overlapResolverInfos.reserve(100);
       m_temporaryResults.reserve(100);
+      m_resultsWithWeight.reserve(100);
     }
 
     /// Expose the parameters of the filter
@@ -43,13 +45,6 @@ namespace Belle2 {
                                     "Enable the overlap resolving.", m_param_enableOverlapResolving);
     }
 
-    void beginEvent() final {
-      Super::beginEvent();
-
-      m_overlapResolverInfos.clear();
-      m_temporaryResults.clear();
-    }
-
     /// Main function of this findlet: traverse a tree starting from a given seed object.
     void apply(std::vector<ResultPair>& resultElements) final {
       if (not m_param_enableOverlapResolving or resultElements.empty())
@@ -58,11 +53,10 @@ namespace Belle2 {
       }
 
       TrackFindingCDC::Weight maximalWeight = 0;
+      m_resultsWithWeight.clear();
 
-      for (unsigned int resultIndex = 0; resultIndex < resultElements.size(); resultIndex++)
+      for (ResultPair& resultPair : resultElements)
       {
-        ResultPair& resultPair = resultElements[resultIndex];
-
         TrackFindingCDC::Weight weight = m_qualityFilter(resultPair);
         if (std::isnan(weight)) {
           continue;
@@ -72,20 +66,26 @@ namespace Belle2 {
           maximalWeight = weight;
         }
 
+        m_resultsWithWeight.emplace_back(&resultPair, weight);
+      }
+
+      m_overlapResolverInfos.clear();
+
+      for (unsigned int resultIndex = 0; resultIndex < m_resultsWithWeight.size(); resultIndex++)
+      {
         // activity state has no meaning here -> set to 0.
         // the overlap will be set later on.
-        // TODO: why is emplace_back not working here?
-        m_overlapResolverInfos.emplace_back(OverlapResolverNodeInfo(weight, resultIndex, {}, 0));
+        m_overlapResolverInfos.push_back(OverlapResolverNodeInfo(m_resultsWithWeight[resultIndex].getWeight(), resultIndex, {}, 0));
       }
 
       for (OverlapResolverNodeInfo& resolverInfo : m_overlapResolverInfos)
       {
-        const ResultPair& resultPair = resultElements[resolverInfo.trackIndex];
+        const ResultPair* resultPair = m_resultsWithWeight[resolverInfo.trackIndex];
 
         // Normalize the weight
         resolverInfo.qualityIndex /= maximalWeight;
 
-        // Find overlaps. TODO: Can this be done better? Maybe with a list of pointers?
+        // Find overlaps.
         auto& overlaps = resolverInfo.overlaps;
 
         for (const OverlapResolverNodeInfo& loopResolverInfo : m_overlapResolverInfos) {
@@ -94,15 +94,15 @@ namespace Belle2 {
             continue;
           }
 
-          const ResultPair& loopResultPair = resultElements[loopResolverInfo.trackIndex];
+          const ResultPair* loopResultPair = m_resultsWithWeight[loopResolverInfo.trackIndex];
 
-          if (loopResultPair.first == resultPair.first) {
+          if (loopResultPair->first == resultPair->first) {
             overlaps.push_back(loopResolverInfo.trackIndex);
             continue;
           }
 
-          for (const auto& hit : resultPair.second) {
-            if (TrackFindingCDC::is_in(hit, loopResultPair.second)) {
+          for (const auto& hit : resultPair->second) {
+            if (TrackFindingCDC::is_in(hit, loopResultPair->second)) {
               overlaps.push_back(loopResolverInfo.trackIndex);
               break;
             }
@@ -114,10 +114,11 @@ namespace Belle2 {
       m_hopfieldNetwork.doHopfield(m_overlapResolverInfos);
 
       // copy results
+      m_temporaryResults.clear();
       for (OverlapResolverNodeInfo& node : m_overlapResolverInfos)
       {
         if (node.activityState > m_param_minimalActivityState) {
-          m_temporaryResults.push_back(resultElements[node.trackIndex]);
+          m_temporaryResults.push_back(*(m_resultsWithWeight[node.trackIndex]));
         }
       }
 
@@ -142,5 +143,7 @@ namespace Belle2 {
     std::vector<OverlapResolverNodeInfo> m_overlapResolverInfos;
     /// temporary results vector, that will be swapped with the real results vector.
     std::vector<ResultPair> m_temporaryResults;
+    /// temporary results vector with weights, out of which the overlaps will be build.
+    std::vector<TrackFindingCDC::WithWeight<ResultPair*>> m_resultsWithWeight;
   };
 }

2d6ca364bea54d5a3fc12d70b9c28338a9d827ef
Nils Braun <nils.braun@desy.de>
2017-05-18 10:06:13 +0200

    Removed unused object members

--- tracking/ckf/filters/cdcTrackSpacePointCombination/include/Chi2CDCVXDTrackCombinationFilter.h
+++ tracking/ckf/filters/cdcTrackSpacePointCombination/include/Chi2CDCVXDTrackCombinationFilter.h
@@ -10,32 +10,12 @@
 #pragma once
 
 #include <tracking/ckf/filters/cdcTrackSpacePointCombination/BaseCDCTrackSpacePointCombinationFilter.h>
-#include <tracking/ckf/findlets/cdcToSpacePoint/SpacePointAdvanceAlgorithm.h>
-#include <tracking/ckf/findlets/cdcToSpacePoint/SpacePointKalmanUpdateFitter.h>
 
 #include <tracking/trackFindingCDC/numerics/Weight.h>
 
 namespace Belle2 {
   class Chi2CDCVXDTrackCombinationFilter : public BaseCDCVXDTrackCombinationFilter {
   public:
-    Chi2CDCVXDTrackCombinationFilter()
-    {
-      addProcessingSignalListener(&m_advanceAlgorithm);
-      addProcessingSignalListener(&m_kalmanAlgorithm);
-    }
-
-    void exposeParameters(ModuleParamList* moduleParamList, const std::string& prefix)
-    {
-      BaseCDCVXDTrackCombinationFilter::exposeParameters(moduleParamList, prefix);
-
-      m_advanceAlgorithm.exposeParameters(moduleParamList, prefix);
-      m_kalmanAlgorithm.exposeParameters(moduleParamList, prefix);
-    }
-
     TrackFindingCDC::Weight operator()(const BaseCDCVXDTrackCombinationFilter::Object& currentState) final;
-
-  private:
-    SpacePointAdvanceAlgorithm m_advanceAlgorithm;
-    SpacePointKalmanUpdateFitter m_kalmanAlgorithm;
   };
 }

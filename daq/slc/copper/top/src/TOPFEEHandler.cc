38d24647b4727a241b8b2079f83db7b66713f1c0
Tobias Weber <webert@hawaii.edu>
2017-05-25 13:46:31 -1000

    Add information which scrod was updated

--- daq/slc/copper/top/src/TOPFEEHandler.cc
+++ daq/slc/copper/top/src/TOPFEEHandler.cc
@@ -3,7 +3,7 @@
 #include "daq/slc/copper/top/B2L_defs.h"
 
 #include <daq/slc/runcontrol/RCCallback.h>
-
+#include <daq/slc/copper/HSLB.h>
 
 using namespace Belle2;
 
@@ -15,7 +15,7 @@ bool TOPHandlerLookback::handleGetInt(int& value)
 bool TOPHandlerLookback::handleSetInt(const int val)
 {
   PrepBoardstackData::SetLookback(m_hslb, val);
-  m_callback.log(LogFile::DEBUG, StringUtil::form("lookback value changed to %d", val));
+  m_callback.log(LogFile::DEBUG, StringUtil::form("lookback value changed to %d on scrod %d", val, m_hslb.get_finid()));
   return NSMVHandlerInt::handleSetInt(val);
 }
 
@@ -30,6 +30,7 @@ bool TOPHandlerFEMode::handleSetInt(const int val)
   PrepBoardstackData::SetFEMode(m_hslb, val);
   std::string femodeString = "UNDEFINED";
   if (val > -1 && val < 4) femodeString = featureExtModeList[val];
-  m_callback.log(LogFile::DEBUG, "feature extraction mode changed to " + femodeString);
+  m_callback.log(LogFile::DEBUG, StringUtil::form("feature extraction mode changed to " + femodeString + " on scrod %d",
+                                                  m_hslb.get_finid()));
   return NSMVHandlerInt::handleSetInt(val);
 }

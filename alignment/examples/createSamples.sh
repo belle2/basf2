c86baf59362dfb06549ce34afe0ed54c7786bbb4
Tadeas Bilka <tadeas.bilka@desy.de>
2017-05-27 19:32:12 +0200

    Handy bash script (in dev)

---
+++ alignment/examples/createSamples.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+n=10
+p=2
+
+basf2 ${BELLE2_LOCAL_DIR}/alignment/examples/sampleCosmicsB0.py --experiment 1 --run 1 -n $n -o DST_cosmicsB0_exp1run1.root -p $p
+basf2 ${BELLE2_LOCAL_DIR}/alignment/examples/sampleCosmics.py --experiment 2 --run 1 -n $n -o DST_cosmics_exp2run1.root -p $p
+basf2 ${BELLE2_LOCAL_DIR}/alignment/examples/sampleMuMu.py --experiment 3 --run 1 -n $n -o DST_mumu_exp3run1.root -p $p
+basf2 ${BELLE2_LOCAL_DIR}/alignment/examples/sampleBMuons.py --experiment 4 --run 1 -n $n -o DST_bMuons_exp4run1.root -p $p
+
+wait
+
+basf2 ${BELLE2_LOCAL_DIR}/alignment/tools/millepede_calibration.py collect VXDAlignment -i "DST_cosmicsB0_*.root" -o cosmicsB0.root -p $p -- --magnet-off &
+basf2 ${BELLE2_LOCAL_DIR}/alignment/tools/millepede_calibration.py collect VXDAlignment -i "DST_cosmics_*.root" -o cosmics.root -p $p &
+basf2 ${BELLE2_LOCAL_DIR}/alignment/tools/millepede_calibration.py collect VXDAlignment -i "DST_mumu_*.root" -o mumu.root -p $p &
+basf2 ${BELLE2_LOCAL_DIR}/alignment/tools/millepede_calibration.py collect VXDAlignment -i "DST_bMuons_*.root" -o bMuons.root -p $p
+
+wait
+
+basf2 ${BELLE2_LOCAL_DIR}/alignment/examples/4_merge.py -i cosmicsB0.root -i cosmics.root -i mumu.root -i bMuons.root -o merged.root
+
+basf2 ${BELLE2_LOCAL_DIR}/alignment/tools/millepede_calibration.py calibrate VXDAlignment -i merged.root

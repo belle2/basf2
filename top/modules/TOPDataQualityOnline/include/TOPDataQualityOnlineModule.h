a5c41fd1e64d6c4f1eb5147a5db86bcdbaed09cf
Wang, Boqun <boqunwg@ucmail.uc.edu>
2017-05-29 15:21:40 +0900

    Add new histograms to TOPDataQualityOnline

--- top/modules/TOPDataQualityOnline/include/TOPDataQualityOnlineModule.h
+++ top/modules/TOPDataQualityOnline/include/TOPDataQualityOnlineModule.h
@@ -11,7 +11,7 @@
 #pragma once
 
 #include <framework/core/Module.h>
-#include <top/dataobjects/TOPRawWaveform.h>
+#include <top/dataobjects/TOPDigit.h>
 #include <framework/datastore/StoreObjPtr.h>
 #include <framework/datastore/StoreArray.h>
 #include "TH1F.h"
@@ -29,7 +29,7 @@ namespace Belle2 {
   /**
    * Module for online data quality monitoring
    */
-  class TOPDataQualityOnlineModule : public Module {
+  class TOPDataQualityOnlineModule : public HistoModule {
     /**
      * Define the channel id type
      */
@@ -76,33 +76,59 @@ namespace Belle2 {
     void terminate();
 
     /**
-     * Clear the graphs when necessary
+     * Flag hit quality
+     * @return 0 for good hit
      */
-    void clear_graph();
-
-    /**
-     * Update the graphs when new histograms are available
-     */
-    void update_graph();
-
-    /**
-     * Draw wavefroms for each event (deprecated)
-    * @param wave The pointer to the TOPRawWaveform
-     */
-    void drawWaveforms(TOPRawWaveform* wave);
+    int flag_hit(const TOPDigit& digit);
 
   private:
-    int m_iFrame = -1; /**< Number of frames within one event */
+    std::string m_histogramDirectoryName; /**< histogram directory in ROOT file */
     int m_iEvent = -1; /**< Number of events that has been processed */
-    int m_refreshCount = -1; /**< For how many events should the graphs be updated */
-    int m_framesPerEvent = -1; /**< Number of frames within one event */
-    int m_nhits = -1; /**< Number of hits within one event */
+    int m_ADC_cut_low = -1, m_ADC_cut_high = -1; /**< hit cut for ADC */
+    int m_PulseWidth_cut_low = -1, m_PulseWidth_cut_high = -1; /**< hit cut for PulseWidth */
+    int m_TDC_particle_low = -999, m_TDC_particle_high = -999;
+    int m_TDC_cal_low = -999, m_TDC_cal_high = -999;
+    int m_TDC_laser_low = -999, m_TDC_laser_high = -999;
 
-    std::map<int, std::map<int, TMultiGraph*>> m_channels; /**< map: scrod_id --> (asic_id --> TMultiGraph) */
-    std::map<int, std::map<int, std::set<int>>> m_channelLabels; /**< map: scrod_id --> (asic_id --> channels within one asic) */
-    std::map<int, TCanvas*> m_canvas; /**< map: scrod_id --> TCanvas */
-    TCanvas* m_canvas_nhits = nullptr; /**< TCanvas for displaying waveform */
-    TH1F* m_h_nhits = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_all_hits = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_good_hits = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_bad_hits = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_particle_hits = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_laser_hits = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_cal_hits = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_other_hits = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_good_hits_mean = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_bad_hits_mean = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_good_hits_rms = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_bad_hits_rms = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_particle_hits_mean = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_laser_hits_mean = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_cal_hits_mean = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_other_hits_mean = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_particle_hits_rms = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_laser_hits_rms = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_cal_hits_rms = nullptr; /**< Histogram for number of hits within on event */
+    TH1F* m_other_hits_rms = nullptr; /**< Histogram for number of hits within on event */
+    std::vector<TH1F*> m_slot_all_hits; /**< Histogram for number of hits for one module within on event */
+    std::vector<TH1F*> m_slot_good_hits; /**< Histogram for number of hits for one module within on event */
+    std::vector<TH1F*> m_slot_bad_hits; /**< Histogram for number of hits for one module within on event */
+    std::vector<TH1F*> m_slot_particle_hits; /**< Histogram for number of hits for one module within on event */
+    std::vector<TH1F*> m_slot_laser_hits; /**< Histogram for number of hits for one module within on event */
+    std::vector<TH1F*> m_slot_cal_hits; /**< Histogram for number of hits for one module within on event */
+    std::vector<TH1F*> m_slot_other_hits; /**< Histogram for number of hits for one module within on event */
+    std::vector<TH1F*> m_channel_all_hits; /**< Histogram for number of hits for one module within on event */
+    std::vector<TH1F*> m_channel_good_hits; /**< Histogram for number of hits for one module within on event */
+    std::vector<TH1F*> m_channel_bad_hits; /**< Histogram for number of hits for one module within on event */
+    std::vector<TH1F*> m_channel_particle_hits; /**< Histogram for number of hits for one module within on event */
+    std::vector<TH1F*> m_hit_quality; /**< hit quality for each module */
+    std::vector<TH2F*> m_all_hits_xy;
+    std::vector<TH2F*> m_laser_hits_xy;
+    std::vector<TH1F*> m_all_ADC;
+    std::vector<TH2F*> m_all_ADC_mean;
+    std::vector<TH2F*> m_all_ADC_RMS;
+    std::vector<TH1F*> m_all_TDC;
+    std::vector<TH2F*> m_all_TDC_mean;
+    std::vector<TH2F*> m_all_TDC_RMS;
   };
 
 } //namespace Belle2
--- top/modules/TOPDataQualityOnline/src/TOPDataQualityOnlineModule.cc
+++ top/modules/TOPDataQualityOnline/src/TOPDataQualityOnlineModule.cc
@@ -19,139 +19,273 @@
 #include "TDirectory.h"
 #include "TCanvas.h"
 #include "TGraph.h"
+#include <boost/format.hpp>
 
 using namespace std;
+using boost::format;
 
 namespace Belle2 {
   REG_MODULE(TOPDataQualityOnline)
 
-  TOPDataQualityOnlineModule::TOPDataQualityOnlineModule() : Module(), m_iFrame(0), m_iEvent(0), m_nhits(0)
+  TOPDataQualityOnlineModule::TOPDataQualityOnlineModule() : HistoModule(), m_iEvent(0)
   {
-    setDescription("TOPCAF online monitoring module");
-    addParam("refreshCount", m_refreshCount, "refresh count", 1);
-    addParam("framesPerEvent", m_framesPerEvent, "frames per event", 16);
+    setDescription("TOP online monitoring module");
+    addParam("histogramDirectoryName", m_histogramDirectoryName,
+             "histogram directory in ROOT file", string("TOP"));
+    addParam("ADCCutLow", m_ADC_cut_low, "lower bound of ADC cut", 100);
+    addParam("ADCCutHigh", m_ADC_cut_high, "higher bound of ADC cut", 2048);
+    addParam("PulseWidthCutLow",  m_PulseWidth_cut_low, "lower bound of PulseWidth cut", 3);
+    addParam("PulseWidthCutHigh", m_PulseWidth_cut_high, "higher bound of PulseWidth cut", 10);
+    addParam("TDCParticleLow", m_TDC_particle_low, "lower bound of particle TDC", -300);
+    addParam("TDCParticleHigh", m_TDC_particle_high, "Higher bound of particle TDC", -250);
+    addParam("TDCCalLow", m_TDC_cal_low, "lower bound of cal TDC", -50);
+    addParam("TDCCalHigh", m_TDC_cal_high, "Higher bound of cal TDC", 50);
+    addParam("TDCLaserLow", m_TDC_laser_low, "lower bound of laser TDC", 50);
+    addParam("TDCLaserHigh", m_TDC_laser_high, "Higher bound of laser TDC", 100);
   }
 
 
   void TOPDataQualityOnlineModule::defineHisto()
   {
+    // Create a separate histogram directory and cd into it.
+    TDirectory* oldDir = gDirectory;
+    oldDir->mkdir(m_histogramDirectoryName.c_str())->cd();
+
+    m_all_hits = new TH1F("all_hits", "Number of all hits per bar", 16, 0.5, 16.5);
+    m_good_hits = new TH1F("good_hits", "Number of good hits per bar", 16, 0.5, 16.5);
+    m_bad_hits = new TH1F("bad_hits", "Number of bad hits per bar", 16, 0.5, 16.5);
+    m_good_hits_mean = new TH1F("good_hits_mean", "Mean of number of good hits per bar", 16, 0.5, 16.5);
+    m_bad_hits_mean = new TH1F("bad_hits_mean", "Mean of number of bad hits per bar", 16, 0.5, 16.5);
+    m_good_hits_rms = new TH1F("good_hits_rms", "RMS of number of good hits per bar", 16, 0.5, 16.5);
+    m_bad_hits_rms = new TH1F("bad_hits_rms", "RMS of number of bad hits per bar", 16, 0.5, 16.5);
+
+    m_particle_hits = new TH1F("particle_hits", "Number of particle hits per bar", 16, 0.5, 16.5);
+    m_laser_hits = new TH1F("laser_hits", "Number of laser hits per bar", 16, 0.5, 16.5);
+    m_cal_hits = new TH1F("cal_hits", "Number of cal hits per bar", 16, 0.5, 16.5);
+    m_other_hits = new TH1F("other_hits", "Number of other hits per bar", 16, 0.5, 16.5);
+
+    m_particle_hits_mean = new TH1F("particle_hits_mean", "Mean of number of particle hits per bar", 16, 0.5, 16.5);
+    m_laser_hits_mean = new TH1F("laser_hits_mean", "Mean of number of laser hits per bar", 16, 0.5, 16.5);
+    m_cal_hits_mean = new TH1F("cal_hits_mean", "Mean of number of cal hits per bar", 16, 0.5, 16.5);
+    m_other_hits_mean = new TH1F("other_hits_mean", "Mean of number of other hits per bar", 16, 0.5, 16.5);
+
+    m_particle_hits_rms = new TH1F("particle_hits_rms", "RMS of number of particle hits per bar", 16, 0.5, 16.5);
+    m_laser_hits_rms = new TH1F("laser_hits_rms", "RMS of number of laser hits per bar", 16, 0.5, 16.5);
+    m_cal_hits_rms = new TH1F("cal_hits_rms", "RMS of number of cal hits per bar", 16, 0.5, 16.5);
+    m_other_hits_rms = new TH1F("other_hits_rms", "RMS of number of other hits per bar", 16, 0.5, 16.5);
+
+    for (int i = 0; i < 16; i++) {
+      int module = i + 1;
+      string name = str(format("all_hits_%1%") % (module));
+      string title = str(format("Number of all hits per event of module #%1%") % (module));
+      string name1 = str(format("good_hits_%1%") % (module));
+      string title1 = str(format("Number of good hits per event of module #%1%") % (module));
+      string name2 = str(format("bad_hits_%1%") % (module));
+      string title2 = str(format("Number of bad hits per event of module #%1%") % (module));
+      TH1F* h = new TH1F(name.c_str(), title.c_str(), 200, 0, 400);
+      TH1F* h1 = new TH1F(name1.c_str(), title1.c_str(), 200, 0, 400);
+      TH1F* h2 = new TH1F(name2.c_str(), title2.c_str(), 200, 0, 400);
+      m_slot_all_hits.push_back(h);
+      m_slot_good_hits.push_back(h1);
+      m_slot_bad_hits.push_back(h2);
+    }
+    for (int i = 0; i < 16; i++) {
+      int module = i + 1;
+      string name = str(format("particle_hits_%1%") % (module));
+      string title = str(format("Number of particle hits per event of module #%1%") % (module));
+      string name1 = str(format("laser_hits_%1%") % (module));
+      string title1 = str(format("Number of laser hits per event of module #%1%") % (module));
+      string name2 = str(format("cal_hits_%1%") % (module));
+      string title2 = str(format("Number of cal hits per event of module #%1%") % (module));
+      string name3 = str(format("other_hits_%1%") % (module));
+      string title3 = str(format("Number of other hits per event of module #%1%") % (module));
+      TH1F* h = new TH1F(name.c_str(), title.c_str(), 200, 0, 400);
+      TH1F* h1 = new TH1F(name1.c_str(), title1.c_str(), 200, 0, 400);
+      TH1F* h2 = new TH1F(name2.c_str(), title2.c_str(), 200, 0, 400);
+      TH1F* h3 = new TH1F(name3.c_str(), title3.c_str(), 200, 0, 400);
+      m_slot_particle_hits.push_back(h);
+      m_slot_laser_hits.push_back(h1);
+      m_slot_cal_hits.push_back(h2);
+      m_slot_other_hits.push_back(h3);
+    }
+    for (int i = 0; i < 16; i++) {
+      int module = i + 1;
+      string name = str(format("all_channel_hits_%1%") % (module));
+      string title = str(format("Number of all hits by channel of module #%1%") % (module));
+      string name1 = str(format("good_channel_hits_%1%") % (module));
+      string title1 = str(format("Number of good hits by channel of module #%1%") % (module));
+      string name2 = str(format("bad_channel_hits_%1%") % (module));
+      string title2 = str(format("Number of bad hits by channel of module #%1%") % (module));
+      string name3 = str(format("particle_channel_hits_%1%") % (module));
+      string title3 = str(format("Number of particle hits by channel of module #%1%") % (module));
+      TH1F* h = new TH1F(name.c_str(), title.c_str(), 512, 0.5, 512.5);
+      TH1F* h1 = new TH1F(name1.c_str(), title1.c_str(), 512, 0.5, 512.5);
+      TH1F* h2 = new TH1F(name2.c_str(), title2.c_str(), 512, 0.5, 512.5);
+      TH1F* h3 = new TH1F(name3.c_str(), title2.c_str(), 512, 0.5, 512.5);
+      m_channel_all_hits.push_back(h);
+      m_channel_good_hits.push_back(h1);
+      m_channel_bad_hits.push_back(h2);
+      m_channel_particle_hits.push_back(h3);
+    }
+    for (int i = 0; i < 16; i++) {
+      int module = i + 1;
+      string name = str(format("hit_quality_%1%") % (module));
+      string title = str(format("Hit quality of module #%1%") % (module));
+      TH1F* h = new TH1F(name.c_str(), title.c_str(), 8, 0.5, 8.5);
+      m_hit_quality.push_back(h);
+    }
+    for (int i = 0; i < 16; i++) {
+      int module = i + 1;
+      string name = str(format("all_hits_xy_%1%") % (module));
+      string title = str(format("Number of hits in x-y for module #%1%") % (module));
+      string name1 = str(format("laser_hits_xy_%1%") % (module));
+      string title1 = str(format("Number of laser hits in x-y for module #%1%") % (module));
+      TH2F* h = new TH2F(name.c_str(), title.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
+      TH2F* h1 = new TH2F(name1.c_str(), title1.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
+      m_all_hits_xy.push_back(h);
+      m_laser_hits_xy.push_back(h1);
+    }
+    for (int i = 0; i < 16; i++) {
+      int module = i + 1;
+      string name = str(format("all_ADC_%1%") % (module));
+      string title = str(format("ADC distribution for module #%1%") % (module));
+      string name1 = str(format("all_ADC_mean_%1%") % (module));
+      string title1 = str(format("ADC mean distribution for module #%1%") % (module));
+      string name2 = str(format("all_ADC_RMS_%1%") % (module));
+      string title2 = str(format("ADC RMS distribution for module #%1%") % (module));
+      TH1F* h = new TH1F(name.c_str(), title.c_str(), 1000, 0.5, 1000.5);
+      TH2F* h1 = new TH2F(name1.c_str(), title1.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
+      TH2F* h2 = new TH2F(name2.c_str(), title2.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
+      m_all_ADC.push_back(h);
+      m_all_ADC_mean.push_back(h1);
+      m_all_ADC_RMS.push_back(h2);
+    }
+    for (int i = 0; i < 16; i++) {
+      int module = i + 1;
+      string name = str(format("all_TDC_%1%") % (module));
+      string title = str(format("TDC distribution for module #%1%") % (module));
+      string name1 = str(format("all_TDC_mean_%1%") % (module));
+      string title1 = str(format("TDC mean distribution for module #%1%") % (module));
+      string name2 = str(format("all_TDC_RMS_%1%") % (module));
+      string title2 = str(format("TDC RMS distribution for module #%1%") % (module));
+      TH1F* h = new TH1F(name.c_str(), title.c_str(), 1000, -499.5, 500.5);
+      TH2F* h1 = new TH2F(name1.c_str(), title1.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
+      TH2F* h2 = new TH2F(name2.c_str(), title2.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
+      m_all_TDC.push_back(h);
+      m_all_TDC_mean.push_back(h1);
+      m_all_TDC_RMS.push_back(h2);
+    }
   }
 
   void TOPDataQualityOnlineModule::initialize()
   {
-    m_canvas_nhits = new TCanvas("canvas_nhits", "canvas_nhits", 800, 600);
-    m_h_nhits = new TH1F("h_nhits", "h_nhits", 100, 0, 100);
-    m_h_nhits->Draw();
-    //waves_ptr.isRequired();
+    REG_HISTOGRAM;
+    StoreArray<TOPDigit>::required();
   }
 
   void TOPDataQualityOnlineModule::beginRun()
   {
   }
 
-  void TOPDataQualityOnlineModule::clear_graph()
+  int TOPDataQualityOnlineModule::flag_hit(const TOPDigit& digit)
   {
-    for (auto scrod_it : m_channels) {
-      int scrod_id = scrod_it.first;
-      for (auto graph_it : scrod_it.second) {
-        TMultiGraph* mg = graph_it.second;
-        delete mg;
-      }
-      m_channels[scrod_id].clear();
-      m_channelLabels[scrod_id].clear();
-    }
-  }
-
-  void TOPDataQualityOnlineModule::update_graph()
-  {
-    //m_canvas_nhits->cd();
-    //m_h_nhits->Draw();
-    m_canvas_nhits->GetPad(0)->Modified();
-    m_canvas_nhits->Update();
-    for (auto scrod_it : m_channels) {
-      int scrod_id = scrod_it.first;
-      if (m_canvas.find(scrod_id) == m_canvas.end()) {
-        string cname = string("scrod_id_") + to_string(scrod_id);
-        m_canvas[scrod_id] = new TCanvas(cname.c_str(), cname.c_str(), 800, 800);
-        m_canvas[scrod_id]->Divide(4, 4);
-        m_canvas[scrod_id]->Show();
-      }
-      for (auto graph_it : scrod_it.second) {
-        int asic_id = graph_it.first;
-        TMultiGraph* mg = graph_it.second;
-        m_canvas[scrod_id]->cd(asic_id + 1);
-        mg->Draw("ALP");
-        m_canvas[scrod_id]->GetPad(asic_id + 1)->Modified();
-      }
-      m_canvas[scrod_id]->Update();
-    }
-  }
-
-  void TOPDataQualityOnlineModule::drawWaveforms(TOPRawWaveform* wave)
-  {
-    const TOPRawWaveform& v = *wave;
-    vector<short> waveform = v.getWaveform();
-    if (waveform.empty()) {
-      return;
-    }
-    int scrodid = v.getScrodID();
-    if (scrodid == 0) {
-      return;
-    }
-    int asicid = v.getASICNumber() + 4 * v.getCarrierNumber();
-    if (m_channelLabels[scrodid].find(asicid) == m_channelLabels[scrodid].end()) {
-      string gname = string("channels") + to_string(scrodid) + string("_") + to_string(asicid);
-      m_channels[scrodid].insert(make_pair(asicid, new TMultiGraph(gname.c_str(), gname.c_str())));
-    }
-    int iChannel = v.getASICChannel();
-    if (m_channelLabels[scrodid][asicid].find(iChannel) != m_channelLabels[scrodid][asicid].end()) {
-      return;
-    }
-    m_channelLabels[scrodid][asicid].insert(iChannel);
-    TMultiGraph* mg = m_channels[scrodid][asicid];
-
-    vector<double> x, y;
-    for (size_t i = 0; i < waveform.size(); ++i) {
-      y.push_back(waveform[i] + iChannel * 1000);
-      x.push_back(i);
-    }
-    TGraph* g = new TGraph(y.size(), &x[0], &y[0]);
-    g->SetMarkerStyle(7);
-    mg->Add(g);
+    int flag = 0;
+    if (digit.getPulseHeight() < m_ADC_cut_low || digit.getPulseHeight() > m_ADC_cut_high) flag += 1;
+    if (digit.getPulseWidth() < m_PulseWidth_cut_low || digit.getPulseWidth() > m_PulseWidth_cut_high) flag += 2;
+    return flag;
   }
 
   void TOPDataQualityOnlineModule::event()
   {
-    StoreArray<TOPRawWaveform> waves_ptr;
-    StoreArray<TOPDigit> digits_ptr;
-    if (not waves_ptr) {
-      return;
-    }
-    for (int c = 0; c < waves_ptr.getEntries(); c++) {
-      TOPRawWaveform* wave_ptr = waves_ptr[c];
-      //cout << "drawWaveforms: iFrame = " << m_iFrame << endl;
-      drawWaveforms(wave_ptr);
-    }
-    for (int c = 0; c < digits_ptr.getEntries(); c++) {
-      TOPDigit* digit = digits_ptr[c];
-      double width = digit->getPulseWidth();
-      double height = digit->getPulseHeight();
-      //int flag=digit->getFlag();
-      //double corr_time=digit->getCorrTime();
-      if (width > 3 && width < 10 && height > 100 && height < 2048) {
-        m_nhits += 1;
+    StoreArray<TOPDigit> digits;
+    vector<int> all_hit(16, 0), good_hit(16, 0), bad_hit(16, 0);
+    vector<int> particle_hit(16, 0), laser_hit(16, 0), cal_hit(16, 0), other_hit(16, 0);
+    for (const auto& digit : digits) {
+      m_all_hits->Fill(digit.getModuleID());
+      int i = digit.getModuleID() - 1;
+      int col = digit.getPixelCol();
+      int row = digit.getPixelRow();
+      if (i < 0 || i >= 16) {
+        B2ERROR("Invalid module ID found in TOPDigits: ID = " << i);
+        continue;
+      }
+      all_hit[i]++;
+      m_channel_all_hits[i]->Fill(digit.getPixelID());
+      m_all_ADC[i]->Fill(digit.getPulseHeight());
+      m_all_TDC[i]->Fill(digit.getRawTime());
+
+      double nhits = m_all_hits_xy[i]->GetBinContent(col, row);
+      double adc_mean = m_all_ADC_mean[i]->GetBinContent(col, row);
+      double adc_rms = m_all_ADC_RMS[i]->GetBinContent(col, row);
+      double adc_mean2 = adc_rms * adc_rms + adc_mean * adc_mean;
+      double tdc_mean = m_all_TDC_mean[i]->GetBinContent(col, row);
+      double tdc_rms = m_all_TDC_RMS[i]->GetBinContent(col, row);
+      double tdc_mean2 = tdc_rms * tdc_rms + tdc_mean * tdc_mean;
+
+      adc_mean = (nhits * adc_mean + digit.getPulseHeight()) / (nhits + 1);
+      adc_mean2 = (nhits * adc_mean2 + digit.getPulseHeight() * digit.getPulseHeight()) / (nhits + 1);
+      tdc_mean = (nhits * tdc_mean + digit.getRawTime()) / (nhits + 1);
+      tdc_mean2 = (nhits * tdc_mean2 + digit.getRawTime() * digit.getRawTime()) / (nhits + 1);
+
+      m_all_ADC_mean[i]->SetBinContent(col, row, adc_mean);
+      m_all_ADC_RMS[i]->SetBinContent(col, row, sqrt(adc_mean2 - adc_mean * adc_mean));
+      m_all_TDC_mean[i]->SetBinContent(col, row, tdc_mean);
+      m_all_TDC_RMS[i]->SetBinContent(col, row, sqrt(tdc_mean2 - tdc_mean * tdc_mean));
+      m_all_hits_xy[i]->Fill(col, row);
+
+      int flag = flag_hit(digit);
+      if (flag == 0) { // good hit
+        m_good_hits->Fill(digit.getModuleID());
+        good_hit[i]++;
+        m_channel_good_hits[i]->Fill(digit.getPixelID());
+      } else { // bad hit
+        m_bad_hits->Fill(digit.getModuleID());
+        bad_hit[i]++;
+        m_channel_bad_hits[i]->Fill(digit.getPixelID());
+        m_hit_quality[i]->Fill(flag);
+      }
+      double digit_tdc = digit.getRawTime();
+      if (digit_tdc > m_TDC_particle_low && digit_tdc < m_TDC_particle_high) { // particle hits
+        m_particle_hits->Fill(digit.getModuleID());
+        particle_hit[i]++;
+        m_channel_particle_hits[i]->Fill(digit.getPixelID());
+      } else if (digit_tdc > m_TDC_laser_low && digit_tdc < m_TDC_laser_high) { // laser hits
+        m_laser_hits->Fill(digit.getModuleID());
+        laser_hit[i]++;
+        m_laser_hits_xy[i]->Fill(col, row);
+      } else if (digit_tdc > m_TDC_cal_low && digit_tdc < m_TDC_cal_high) { // cal hits
+        m_cal_hits->Fill(digit.getModuleID());
+        cal_hit[i]++;
+      } else {
+        m_other_hits->Fill(digit.getModuleID());
+        other_hit[i]++;
       }
     }
-    m_iFrame += 1;
-    if (m_iFrame % m_framesPerEvent == 0) {
-      m_iEvent += 1;
-      //cout << "nhits: " << m_nhits << endl;
-      m_h_nhits->Fill(m_nhits);
-      m_nhits = 0;
-      if (m_iEvent % m_refreshCount == 0) {
-        update_graph();
-      }
-      clear_graph();
+    m_iEvent++;
+    for (int i = 0; i < 16; i++) {
+      if (all_hit[i] > 0) m_slot_all_hits[i]->Fill(all_hit[i]);
+      if (good_hit[i] > 0) m_slot_good_hits[i]->Fill(good_hit[i]);
+      if (bad_hit[i] > 0) m_slot_bad_hits[i]->Fill(bad_hit[i]);
+      if (particle_hit[i] > 0) m_slot_particle_hits[i]->Fill(particle_hit[i]);
+      if (laser_hit[i] > 0) m_slot_laser_hits[i]->Fill(laser_hit[i]);
+      if (cal_hit[i] > 0) m_slot_cal_hits[i]->Fill(cal_hit[i]);
+      if (other_hit[i] > 0) m_slot_other_hits[i]->Fill(other_hit[i]);
+
+      m_good_hits_mean->SetBinContent(i + 1, m_slot_good_hits[i]->GetMean());
+      m_bad_hits_mean->SetBinContent(i + 1, m_slot_bad_hits[i]->GetMean());
+
+      m_good_hits_rms->SetBinContent(i + 1, m_slot_good_hits[i]->GetRMS());
+      m_bad_hits_rms->SetBinContent(i + 1, m_slot_bad_hits[i]->GetRMS());
+
+      m_particle_hits_mean->SetBinContent(i + 1, m_slot_particle_hits[i]->GetMean());
+      m_laser_hits_mean->SetBinContent(i + 1, m_slot_laser_hits[i]->GetMean());
+      m_cal_hits_mean->SetBinContent(i + 1, m_slot_cal_hits[i]->GetMean());
+      m_other_hits_mean->SetBinContent(i + 1, m_slot_other_hits[i]->GetMean());
+
+      m_particle_hits_rms->SetBinContent(i + 1, m_slot_particle_hits[i]->GetRMS());
+      m_laser_hits_rms->SetBinContent(i + 1, m_slot_laser_hits[i]->GetRMS());
+      m_cal_hits_rms->SetBinContent(i + 1, m_slot_cal_hits[i]->GetRMS());
+      m_other_hits_rms->SetBinContent(i + 1, m_slot_other_hits[i]->GetRMS());
     }
     return;
   }
@@ -165,4 +299,4 @@ namespace Belle2 {
   void TOPDataQualityOnlineModule::terminate()
   {
   }
-}
+} // end Belle2 namespace

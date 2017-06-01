0762eae9bd8de8954ef5bcdb78fee705c0074382
Jake Bennett <jvbennett@cmu.edu>
2017-06-02 06:06:12 +0900

    Fixing charm semileptonic skims

--- analysis/skim/CharmSemileptonic_Skim_Standalone.py
+++ analysis/skim/CharmSemileptonic_Skim_Standalone.py
@@ -10,26 +10,25 @@
 
 from basf2 import *
 from modularAnalysis import *
-from stdCharged import *
-from stdPi0s import *
-from stdV0s import *
-from stdCharm import *
-
-set_log_level(LogLevel.INFO)
+from stdFSParticles import *
 
 filelist = [
-    '/ghi/fs01/belle2/bdata/MC/fab/sim/release-00-05-03/DBxxxxxxxx/MC5/prod00000018/s00/e0001/4S/r00001/ccbar/sub00/' +
-    'mdst_000001_prod00000018_task00000001.root'
+    '/ghi/fs01/belle2/bdata/MC/release-00-08-00/DB00000208/MC8/' +
+    'prod00000972/s00/e0000/4S/r00000/ccbar/sub00/mdst_001678_prod00000972_task00001680.root'
 ]
 
-inputMdstList('MC5', filelist)
+inputMdstList('default', filelist)
 
-loadStdCharged()
-loadStdPi0()
-loadStdKS()
+stdPi()
+stdK()
+stdPi0s('veryLooseFit')
 
-loadStdD0()
-loadStdDplus()
+reconstructDecay('K_S0:all -> pi-:95eff pi+:95eff', '0.4 < M < 0.6', 1, True, analysis_main)
+vertexKFit('K_S0:all', 0.0)
+applyCuts('K_S0:all', '0.477614 < M < 0.517614')
+
+fillParticleList('e+:std', 'eid > 0.1 and chiProb > 0.001 and p > 0.25', True, analysis_main)
+fillParticleList('mu+:std', 'muid > 0.1 and chiProb > 0.001 and p > 0.25', True, analysis_main)
 
 # CSL Skim
 from CharmSemileptonic_List import *

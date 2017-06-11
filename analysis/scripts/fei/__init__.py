5185ffd13ef962a8d58ff15ccf1243b01ed1696b
Thomas Keck <thomas.keck2@kit.edu>
2017-06-11 05:56:19 +0200

    Added FeiConfiguration to exports in __init__.py of fei package

--- analysis/scripts/fei/__init__.py
+++ analysis/scripts/fei/__init__.py
@@ -11,9 +11,9 @@ Detailed usage examples can be found in analysis/examples/FEI/
 # @cond internal
 # only public interfaces, to make package doc nicer. Also the only things imported by 'from fei import *'
 __all__ = ['FeiState', 'get_path', 'get_default_channels', 'get_unittest_channels', 'do_trainings',
-           'Particle', 'MVAConfiguration', 'PreCutConfiguration', 'PostCutConfiguration', 'DecayChannel']
+           'Particle', 'MVAConfiguration', 'PreCutConfiguration', 'PostCutConfiguration', 'FeiConfiguration', 'DecayChannel']
 # @endcond
 
-from fei.config import Particle, MVAConfiguration, PreCutConfiguration, PostCutConfiguration, DecayChannel
+from fei.config import Particle, MVAConfiguration, PreCutConfiguration, PostCutConfiguration, FeiConfiguration, DecayChannel
 from fei.default_channels import get_default_channels, get_unittest_channels
 from fei.core import get_path, FeiState, do_trainings

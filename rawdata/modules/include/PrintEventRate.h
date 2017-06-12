c43a764f6e544689ef0e9756047bed89b8a6a5aa
Satoru YAMADA <satoru.yamada@kek.jp>
2017-06-12 18:50:15 +0900

    Add the number of COPPERs in the output.

--- rawdata/modules/include/PrintEventRate.h
+++ rawdata/modules/include/PrintEventRate.h
@@ -8,15 +8,38 @@
 
 #ifndef PRINTEVENTRATE_H
 #define PRINTEVENTRATE_H
-#include <rawdata/modules/PrintDataTemplate.h>
+#include <stdlib.h>
+#include <string>
+
+#include <framework/core/Module.h>
+#include <framework/pcore/EvtMessage.h>
+#include <framework/pcore/MsgHandler.h>
+
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/dataobjects/EventMetaData.h>
+
+
+#include <rawdata/dataobjects/RawDataBlock.h>
+#include <rawdata/dataobjects/RawFTSW.h>
+#include <rawdata/dataobjects/RawTLU.h>
+#include <rawdata/dataobjects/RawCOPPER.h>
+#include <rawdata/dataobjects/RawSVD.h>
+#include <rawdata/dataobjects/RawCDC.h>
+#include <rawdata/dataobjects/RawTOP.h>
+#include <rawdata/dataobjects/RawARICH.h>
+#include <rawdata/dataobjects/RawECL.h>
+#include <rawdata/dataobjects/RawKLM.h>
+#include <rawdata/dataobjects/RawPXD.h>
+#include <rawdata/dataobjects/RawTRG.h>
+
 
 namespace Belle2 {
 
-
-
   /*! A class definition of an input module for Sequential ROOT I/O */
 
-  class PrintEventRateModule : public PrintDataTemplateModule {
+  class PrintEventRateModule : public Module {
 
     // Public functions
   public:
@@ -27,37 +50,67 @@ namespace Belle2 {
 
     //! Module functions to be called from event process
     virtual void event();
-
-    //! print the contents of a RawCOPPER event
-    virtual void printCOPPEREvent(RawCOPPER* raw_array, int i);
-
+    virtual void initialize();
+    virtual void printCOPPEREvent(RawCOPPER* raw_array, int i, int blksize, int evtcnt);
     virtual void endRun();
 
   protected:
-
-    //! Run #
     int m_run;
-
-    //! current UNIX time
+    int m_subrun;
     unsigned int m_cur_utime;
-
-    //! current event number
     unsigned int m_cur_event;
-
-    //! UNIX time of the run start
-    unsigned int start_utime;
-
-    //! the initial event number
-    unsigned int start_event;
-
-    //! UNIX time of the prev. event
-    unsigned int prev_utime;
-
-    //! # of the prev. event
-    unsigned int prev_event;
-
-    //! Size
+    unsigned int m_start_utime;
+    unsigned int m_start_event;
+    unsigned int m_prev_utime;
+    unsigned int m_prev_event;
     int m_size;
+
+    int m_cpr;
+    int m_cpr_svd;
+    int m_cpr_cdc;
+    int m_cpr_top;
+    int m_cpr_arich;
+    int m_cpr_ecl;
+    int m_cpr_klm;
+    int m_cpr_trg;
+    int m_cpr_others;
+
+    unsigned int m_n_basf2evt;
+    double m_tot_bytes;
+    double m_prev_tot_bytes;
+
+    // For distribution over COPPERs
+
+    int m_datablk_blksize;
+    int m_cpr_blksize;
+    int m_tlu_blksize;
+    int m_ftsw_blksize;
+    int m_pxd_blksize;
+    int m_svd_blksize;
+    int m_cdc_blksize;
+    int m_top_blksize;
+    int m_arich_blksize;
+    int m_ecl_blksize;
+    int m_klm_blksize;
+    int m_trg_blksize;
+
+    int m_datablk_evecnt;
+    int m_cpr_evecnt;
+    int m_tlu_evecnt;
+    int m_ftsw_evecnt;
+    int m_pxd_evecnt;
+    int m_svd_evecnt;
+    int m_cdc_evecnt;
+    int m_top_evecnt;
+    int m_arich_evecnt;
+    int m_ecl_evecnt;
+    int m_klm_evecnt;
+    int m_trg_evecnt;
+    int m_evecnt;
+
+    int m_print_interval;
+
+    int m_first_evt;
 
   };
 

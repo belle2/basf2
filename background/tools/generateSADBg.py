91e41df16c1a773e4b9a600fd86218c5600cff19
staric <marko.staric@ijs.si>
2017-06-17 20:36:05 +0900

    updated steering files for BG production

--- background/tools/generateSADBg.py
+++ background/tools/generateSADBg.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# -------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
 # BG simulation using SAD input files
 #
 # make a link to the SAD files before running it (at KEKCC):
-#    ln -s /home/belle/staric/public/basf2/SADfiles/phase3-15th input
-# -------------------------------------------------------------------------
+#    ln -s /home/belle/staric/public/basf2/SADfiles input
+#
+# usage:
+#    basf2 generateSADBg.py bgType accRing equivTime_us num [sampleType phase outdir]
+# arguments:
+#    bgType         Coulomb or Touschek
+#    accRing        LER or HER
+#    equivTime_us   equivalent SuperKEKB running time in micro-seconds
+#    num            output file number
+#    sampleType     one of: study, usual, PXD, ECL
+#    phase          2 or 3
+#    outdir         output directory path
+# -------------------------------------------------------------------------------------
 
 from basf2 import *
 import sys
+import os
 from background import add_output
 
 # read parameters
@@ -23,7 +35,8 @@ if argc == 5:
     equivTime = argvs[3]  # equivalent SuperKEKB running time in micro-seconds
     num = argvs[4]        # output file number
     sampleType = 'usual'  # study, usual, PXD, ECL
-    phase = 3             # 1, 2 or 3
+    phase = 3             # 2 or 3
+    outdir = 'output'     # output directory path
 elif argc == 6:
     bgType = argvs[1]
     accRing = argvs[2]
@@ -31,24 +44,58 @@ elif argc == 6:
     num = argvs[4]
     sampleType = argvs[5]
     phase = 3
+    outdir = 'output'
 elif argc == 7:
     bgType = argvs[1]
     accRing = argvs[2]
     equivTime = argvs[3]
     num = argvs[4]
     sampleType = argvs[5]
-    phase = argvs[6]
+    phase = int(argvs[6])
+    outdir = 'output'
+elif argc == 8:
+    bgType = argvs[1]
+    accRing = argvs[2]
+    equivTime = argvs[3]
+    num = argvs[4]
+    sampleType = argvs[5]
+    phase = int(argvs[6])
+    outdir = argvs[7]
 else:
     print('usage:')
     print('basf2', argvs[0],
-          '(Touschek,Coulomb) (HER,LER) equivTime_us num [(study,usual,ECL,PXD) phase]')
+          '(Touschek,Coulomb) (HER,LER) equivTime_us num [(study,usual,ECL,PXD) phase outdir]')
+    sys.exit()
+
+# set parameters
+
+if phase == 3:
+    subdir = 'phase3-15th/'
+elif phase == 2:
+    subdir = 'phase2/'
+else:
+    print('phase ', phase, 'not supported')
     sys.exit()
 
 bgType = bgType + '_' + accRing
-sadFile = 'input/' + bgType + '.root'
+sadFile = 'input/' + subdir + bgType + '.root'
 realTime = float(equivTime) * 1000
-fname = bgType + '_' + sampleType + '-' + num
-outputFile = 'output/' + fname + '.root'
+fname = bgType + '_' + sampleType + '-phase' + str(phase) + '-' + num
+outputFile = outdir + '/' + fname + '.root'
+
+# check for the existance of a SAD file
+
+if not os.path.exists(sadFile):
+    B2ERROR('SAD file ' + sadFile + ' not found')
+    sys.exit()
+
+# make output directory if it doesn't exist
+
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+
+# log message
+B2RESULT('Output file: ' + outputFile)
 
 # Suppress messages and warnings during processing:
 set_log_level(LogLevel.RESULT)
@@ -59,11 +106,13 @@ main = create_path()
 # Event info setter
 # Set some large number of events - processing will be stopped by BG generator
 eventinfosetter = register_module('EventInfoSetter')
-eventinfosetter.param('evtNumList', [1000000])
+eventinfosetter.param('evtNumList', [10000000])
 main.add_module(eventinfosetter)
 
 # Gearbox
 gearbox = register_module('Gearbox')
+if phase == 2:
+    gearbox.param('fileName', 'geometry/Beast2_phase2.xml')
 if sampleType == 'study':
     gearbox.param('override', [
         ("/DetectorComponent[@name='PXD']//ActiveChips", 'true', ''),

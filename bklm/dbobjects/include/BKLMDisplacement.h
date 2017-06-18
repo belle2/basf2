9689f259c2bbbe9886321ed0df8c4caf90188b93
Yinghui GUAN <guanyh@post.kek.jp>
2017-06-18 14:37:22 +0900

    fix missing documents

--- bklm/dbobjects/include/BKLMDisplacement.h
+++ bklm/dbobjects/include/BKLMDisplacement.h
@@ -27,69 +27,71 @@ namespace Belle2 {
   class BKLMDisplacement: public TObject {
 
   public:
-    // Constructor
+    //! Constructor
     BKLMDisplacement() : m_elementID(65535), m_uShift(0), m_vShift(0), m_wShift(0), m_alphaRotation(0), m_betaRotation(0),
       m_gammaRotation(0) {}
 
-    // Constructor
+    //! Constructor
     BKLMDisplacement(const unsigned short elementID, float dU, float dV, float dW, float dAlpha,  float dBeta,  float dGamma) :
       m_elementID(elementID), m_uShift(dU), m_vShift(dV), m_wShift(dW), m_alphaRotation(dAlpha), m_betaRotation(dBeta),
       m_gammaRotation(dGamma)
     {
     }
 
-    // Destructor
+    //! Destructor
     ~BKLMDisplacement() {}
 
-    //get the F/B info of current module, 0: backward, 1: forward
-    //int getIsForward();
+    //!get the F/B info of current module, 0: backward, 1: forward
+    // int getIsForward();
 
-    //get the identifier id
+    //! get the identifier id
     float getElementID() const {return m_elementID;}
 
-    //get the movement along u
+    //! get the movement along u
     float getUShift() const {return m_uShift;}
 
-    //get the movement along v
+    //! get the movement along v
     float getVShift() const {return m_vShift;}
 
-    //get the movement along w
+    //! get the movement along w
     float getWShift() const {return m_wShift;}
 
-    //get the rotation around u
+    //! get the rotation around u
     float getAlphaRotation() const {return m_alphaRotation;}
 
-    //get the rotation around v
+    //! get the rotation around v
     float getBetaRotation() const {return m_betaRotation;}
 
-    //get the rotation around w
+    //! get the rotation around w
     float getGammaRotation() const {return m_gammaRotation;}
 
 
   private:
 
-    //unique identifier for a BKLM module/supperlayer
+    //! unique identifier for a BKLM module/supperlayer
     unsigned short m_elementID;
 
-    //same with six alignment parameters
-    //let's define them in local system, which should be more convenient for geometry constructor
-    //movement along u, v, w and rotation around u v w
-    //movement along u, u direction is along z-measuring strips
+    /* same with six alignment parameters
+     *let's define them in local system, which should be more convenient for geometry constructor
+     *movement along u, v, w and rotation around u v w
+     */
+
+    //! movement along u, u direction is along z-measuring strips
     float m_uShift;
 
-    //movement along v, v direction is along phi-measuring strips
+    //! movement along v, v direction is along phi-measuring strips
     float m_vShift;
 
-    //movement along w, w direction is normal the a module plane
+    //! movement along w, w direction is normal the a module plane
     float m_wShift;
 
-    //rotation around u, hardly be non-zero
+    //! rotation around u, hardly be non-zero
     float m_alphaRotation;
 
-    //rotation around v, hardly be non-zero
+    //! rotation around v, hardly be non-zero
     float m_betaRotation;
 
-    //rotation around w, hardly be non-zero
+    //! rotation around w, hardly be non-zero
     float m_gammaRotation;
 
     ClassDef(BKLMDisplacement, 1); /**< BKLM displaced-geometry*/

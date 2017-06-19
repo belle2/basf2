c5d21f5d5d7b649eee04ad8a6a81fb1f4afb69f9
Nils Braun <nils.braun@desy.de>
2017-06-19 18:27:43 +0900

    Added geometry and gearbox and more comments

--- generators/scripts/generators.py
+++ generators/scripts/generators.py
@@ -1,18 +1,44 @@
 '''
 File for summarizing all default generator settings.
 '''
+from ROOT import Belle2
 
 
-def add_cosmics_generator(path, accept_box=None, keep_box=None,
+def add_cosmics_generator(path, components=None, global_box_size=None, accept_box=None,
+                          keep_box=None,
                           cosmics_data_dir='data/generators/modules/cryinput/',
                           setup_file='simulation/scripts/cry.setup'):
     """
     Add the cosmics generator CRY with the default parameters to the path.
+    :param path: Add the modules to this path.
+    :param components: list of geometry components to add in the geometry module,
+           or None for all components.
+    :param global_box_size: sets global length, width and height.
+    :param accept_box: sets the size of the accept box. As a default it is
+           set to 8.0 m as Belle2 detector size.
+    :param keep_box: sets the size of the keep box (keep box >= accept box).
+    :param cosmics_data_dir: parameter CosmicDataDir for the cry module (absolute or relative to the basf2 repo).
+    :param setup_file: location of the cry.setup file (absolute or relative to the basf2 repo)
     """
+    if global_box_size is None:
+        global_box_size = [20, 20, 9]
     if accept_box is None:
         accept_box = [8, 8, 8]
     if keep_box is None:
         keep_box = [8, 8, 8]
+
+    if 'Gearbox' not in path:
+        path.add_module('Gearbox', override=[
+            ("/Global/length", str(global_box_size[0]), "m"),
+            ("/Global/width", str(global_box_size[1]), "m"),
+            ("/Global/height", str(global_box_size[2]), "m")]
+        )
+
+    # detector geometry
+    if 'Geometry' not in path:
+        geometry = path.add_module('Geometry')
+        if components:
+            geometry.param('components', components)
 
     cry = path.add_module('CRYInput')
 

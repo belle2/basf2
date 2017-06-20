80ad157d5cbbb1b0b0aeb03833e2360a21f36901
Martin Ritter <martin.ritter@desy.de>
2017-06-20 16:05:54 +0900

    Make sure the ParticleCombiner does not use infinite memory (BII-2357)
    Without a proper limit the `ParticleCombiner` will happily combine as many
    candidates as it can find and this is an factorial combinatorial
    problem this can result in infinite memory consumption. (recently double
    charm skimming jobs were killed with 15-20GB ram consumption)
    
    On the other hand lists with that many candidates seem questionable
    so it seems reasonable to introduce a default limit on the number of
    candidates to make sure memory consumption does not grow without limit.
    
    This commit adds a default limit of 2000 candidates and print a
    warning if this limit is exceeded. It will also exposes the limit in the
    `modularAnalysis.py` so that users can easily increase the limit for
    their special use case.

--- analysis/scripts/modularAnalysis.py
+++ analysis/scripts/modularAnalysis.py
@@ -736,7 +736,8 @@ def reconstructDecay(
     cut,
     dmID=0,
     writeOut=False,
-    path=analysis_main
+    path=analysis_main,
+    candidate_limit=None,
 ):
     """
     Creates new Particles by making combinations of existing Particles - it reconstructs unstable particles via
@@ -752,6 +753,10 @@ def reconstructDecay(
     @param dmID        user specified decay mode identifier
     @param writeOut    wether RootOutput module should save the created ParticleList
     @param path        modules are added to this path
+    @param candidate_limit Maximum amount of candidates to be reconstructed. If
+                       the number of candidates is exceeded no candidate will be
+                       reconstructed for that event and a Warning will be
+                       printed.
     """
 
     pmake = register_module('ParticleCombiner')
@@ -760,6 +765,8 @@ def reconstructDecay(
     pmake.param('cut', cut)
     pmake.param('decayMode', dmID)
     pmake.param('writeOut', writeOut)
+    if candidate_limit is not None:
+        pmake.param("maximumNumberOfCandidates", candidate_limit)
     path.add_module(pmake)
 
 
@@ -792,6 +799,7 @@ def reconstructRecoil(
     dmID=0,
     writeOut=False,
     path=analysis_main,
+    candidate_limit=None,
 ):
     """
     Creates new Particles that recoil against the input particles.
@@ -809,6 +817,10 @@ def reconstructRecoil(
     @param dmID        user specified decay mode identifier
     @param writeOut    wether RootOutput module should save the created ParticleList
     @param path        modules are added to this path
+    @param candidate_limit Maximum amount of candidates to be reconstructed. If
+                       the number of candidates is exceeded no candidate will be
+                       reconstructed for that event and a Warning will be
+                       printed.
     """
 
     pmake = register_module('ParticleCombiner')
@@ -818,6 +830,8 @@ def reconstructRecoil(
     pmake.param('decayMode', dmID)
     pmake.param('writeOut', writeOut)
     pmake.param('recoilParticleType', 1)
+    if candidate_limit is not None:
+        pmake.param("maximumNumberOfCandidates", candidate_limit)
     path.add_module(pmake)
 
 
@@ -827,6 +841,7 @@ def reconstructRecoilDaughter(
     dmID=0,
     writeOut=False,
     path=analysis_main,
+    candidate_limit=None,
 ):
     """
     Creates new Particles that are daughters of the particle reconstructed in the recoil (always assumed to be the first daughter).
@@ -844,6 +859,10 @@ def reconstructRecoilDaughter(
     @param dmID        user specified decay mode identifier
     @param writeOut    wether RootOutput module should save the created ParticleList
     @param path        modules are added to this path
+    @param candidate_limit Maximum amount of candidates to be reconstructed. If
+                       the number of candidates is exceeded no candidate will be
+                       reconstructed for that event and a Warning will be
+                       printed.
     """
 
     pmake = register_module('ParticleCombiner')
@@ -853,6 +872,8 @@ def reconstructRecoilDaughter(
     pmake.param('decayMode', dmID)
     pmake.param('writeOut', writeOut)
     pmake.param('recoilParticleType', 2)
+    if candidate_limit is not None:
+        pmake.param("maximumNumberOfCandidates", candidate_limit)
     path.add_module(pmake)
 
 

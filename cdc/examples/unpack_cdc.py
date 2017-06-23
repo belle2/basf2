e77dfa1ee69e36405ce80ecb205451fd2e782c25
Makoto Uchida <uchida@phys.titech.ac.jp>
2017-06-23 16:10:02 +0900

    Update unpack_cdc.py to identify file type (.sroot or .root) automatically.

--- cdc/examples/unpack_cdc.py
+++ cdc/examples/unpack_cdc.py
@@ -17,10 +17,14 @@ set_log_level(LogLevel.INFO)
 use_database_chain()
 use_local_database(Belle2.FileSystem.findFile("data/framework/database.txt"))
 
-# Input (ROOT file).
-input = register_module('RootInput')
-# Input (Seq. ROOT file).
-# input = register_module('SeqRootInput')
+# Input file
+# Get type of input file to decide, which input module we want to use
+input_files = Belle2.Environment.Instance().getInputFilesOverride()
+if not input_files.empty() and input_files.front().endswith(".sroot"):
+    root_input = register_module('SeqRootInput')
+else:
+    root_input = register_module('RootInput')
+
 unpacker = register_module('CDCUnpacker')
 output = register_module('RootOutput')
 output.param('outputFileName', 'UnpackerOutput.root')
@@ -30,7 +34,7 @@ output.param('branchNames', ['CDCHits', 'CDCRawHits'])
 main = create_path()
 
 # Add modules to main path
-main.add_module(input)
+main.add_module(root_input)
 main.add_module(unpacker)
 main.add_module(output)
 

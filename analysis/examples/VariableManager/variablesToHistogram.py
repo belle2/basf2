b6bcc0017f3815b99552a6eda78d1a5da7b0443a
Thomas Keck <thomas.keck2@kit.edu>
2017-06-26 13:30:39 +0200

    Examples work now on KEKCC, kit, and otherwise throw an error message telling the user to copy a mdst file from somewhere to his local machine

--- analysis/examples/VariableManager/variablesToHistogram.py
+++ analysis/examples/VariableManager/variablesToHistogram.py
@@ -6,7 +6,19 @@
 from basf2 import *
 from modularAnalysis import *
 
-inputMdstList('MC7', ['/storage/jbod/tkeck/MC7/evtgen-charged/sub00/mdst_000240_prod00000788_task00000685.root'])
+import os
+if os.path.isfile('mdst.root'):
+    filename = 'mdst.root'
+elif os.path.isfile('/storage/jbod/tkeck/MC7/evtgen-charged/sub00/mdst_000240_prod00000788_task00000685.root'):
+    filename = '/storage/jbod/tkeck/MC7/evtgen-charged/sub00/mdst_000240_prod00000788_task00000685.root'
+elif os.path.isfile('/ghi/fs01/belle2/bdata/MC/release-00-07-02/DBxxxxxxxx/MC7/prod00000788/s00/e0000/4S/r00000/'
+                    'charged/sub00/mdst_000240_prod00000788_task00000685.root'):
+    filename = '/ghi/fs01/belle2/bdata/MC/release-00-07-02/DBxxxxxxxx/MC7/prod00000788/s00/e0000/4S/r00000/'\
+               'charged/sub00/mdst_000240_prod00000788_task00000685.root'
+else:
+    raise RuntimeError("Please copy an mdst file from KEKCC into this directory named mdst.root")
+
+inputMdstList('MC7', [filename])
 
 fillParticleLists([('K-', 'Kid > 0.2'), ('pi+', 'piid > 0.2')])
 reconstructDecay('D0 -> K- pi+', '1.750 < M < 1.95')

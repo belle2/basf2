ddfe9d512e958fe7410a68291d8fd56676930886
Nils Braun <nils.braun@desy.de>
2017-06-27 11:53:01 +0200

    Added convenience functions for managing the trigger menu in the DB

--- hlt/softwaretrigger/scripts/softwaretrigger/db_access.py
+++ hlt/softwaretrigger/scripts/softwaretrigger/db_access.py
@@ -43,6 +43,22 @@ def upload_cut_to_db(base_identifier, cut_identifier, cut_string, prescale_facto
     db_handler.upload(software_trigger_cut, base_identifier, cut_identifier, iov)
 
 
+def upload_trigger_menu_to_db(base_identifier, cut_identifiers, accept_mode=False, iov=None):
+    """
+    Python function to upload the given software trigger enu to the database.
+    Additional to the software trigger menu, the base identifier
+    has to be given. Optionally, the interval of validity can be defined
+    (default is always valid).
+    """
+    if not iov:
+        iov = Belle2.IntervalOfValidity(0, 0, -1, -1)
+
+    cut_identifiers = list_to_vector(cut_identifiers)
+
+    db_handler = Belle2.SoftwareTrigger.SoftwareTriggerDBHandler
+    db_handler.uploadTriggerMenu(base_identifier, cut_identifiers, accept_mode, iov)
+
+
 def download_cut_from_db(base_name, cut_name, not_set_event_number=False):
     """
     Python function to download a cut from the database. As each cut is uniquely identified by a
@@ -55,13 +71,39 @@ def download_cut_from_db(base_name, cut_name, not_set_event_number=False):
     :param not_set_event_number: it is important to always have a proper event number set for the database to work.
         This is why this functions sets the event number in all cases to (1, 0, 0). If you want to prevent this
         (because you maybe want to use another event number), set this flag to True.
-    :return: the downloaded cut or throw an error, if the name can not be found.
+    :return: the downloaded cut or None, if the name can not be found.
     """
     if not not_set_event_number:
         set_event_number(1, 0, 0)
 
     db_handler = Belle2.SoftwareTrigger.SoftwareTriggerDBHandler
-    return db_handler.download(base_name, cut_name)
+    result = db_handler.download(base_name, cut_name)
+    if not result:
+        result = None
+    return result
+
+
+def download_trigger_menu_from_db(base_name, not_set_event_number=False):
+    """
+    Python function to download a trigger menu from the database. As each trigger menu is uniquely identified by a
+    base name, you have to give here.
+    Please remember that the database access depends on the current event number. If you do not call
+    this function in a basf2 module environment, you can use the set_event_number function in this
+    python file to set the event number correctly nevertheless.
+    :param base_name: the base name of the menu
+    :param not_set_event_number: it is important to always have a proper event number set for the database to work.
+        This is why this functions sets the event number in all cases to (1, 0, 0). If you want to prevent this
+        (because you maybe want to use another event number), set this flag to True.
+    :return: the downloaded cut or None, if the name can not be found.
+    """
+    if not not_set_event_number:
+        set_event_number(1, 0, 0)
+
+    db_handler = Belle2.SoftwareTrigger.SoftwareTriggerDBHandler
+    result = db_handler.downloadTriggerMenu(base_name)
+    if not result:
+        result = None
+    return result
 
 
 def set_event_number(evt_number, run_number, exp_number):
@@ -83,14 +125,16 @@ def set_event_number(evt_number, run_number, exp_number):
     Belle2.DataStore.Instance().setInitializeActive(False)
 
 
-def get_all_cuts_in_database(base_identifier=None, software_trigger_global_tag_name=SOFTWARE_TRIGGER_GLOBAL_TAG_NAME):
+def _get_all_objects_in_database(base_identifier,
+                                 number_of_splits,
+                                 software_trigger_global_tag_name=SOFTWARE_TRIGGER_GLOBAL_TAG_NAME):
     """
-    Helper function to download a last of all cuts from the condition database in the given tag (and for
+    Helper function to download a last of all items from the condition database in the given tag (and for
     the given base identifier, if not None).
     :param base_identifier: The base identifier for which the cut list should be downloaded. Leave empty to get a list
         of all cuts for all base identifiers.
     :param software_trigger_global_tag_name: The global tag in the database to look at.
-    :return: A list of tuples (base_identifier, cut_identifier) for each cut in the database.
+    :return: A list of tuples (base_identifier, cut_identifier) for each item in the database.
     """
     from conditions_db import ConditionsDB
     from ROOT import Belle2
@@ -99,12 +143,40 @@ def get_all_cuts_in_database(base_identifier=None, software_trigger_global_tag_n
     db = ConditionsDB()
     payloads = db.get_payloads(software_trigger_global_tag_name)
     cuts_in_database = [tuple(cut_name.split("&")[1:]) for cut_name, checksum in payloads
-                        if "&" in cut_name and len(cut_name.split("&")) == 3 and cut_name.startswith(identifier)]
+                        if "&" in cut_name and len(cut_name.split("&")) == number_of_splits and cut_name.startswith(identifier)]
 
     if base_identifier:
         cuts_in_database = list(filter(lambda x: x[0] == base_identifier, cuts_in_database))
 
     return sorted(cuts_in_database)
+
+
+def get_all_cuts_in_database(base_identifier=None,
+                             software_trigger_global_tag_name=SOFTWARE_TRIGGER_GLOBAL_TAG_NAME):
+    """
+    Helper function to download a last of all cuts from the condition database in the given tag (and for
+    the given base identifier, if not None).
+    :param base_identifier: The base identifier for which the cut list should be downloaded. Leave empty to get a list
+        of all cuts for all base identifiers.
+    :param software_trigger_global_tag_name: The global tag in the database to look at.
+    :return: A list of tuples (base_identifier, cut_identifier) for each cut in the database.
+    """
+    return _get_all_objects_in_database(base_identifier=base_identifier, number_of_splits=3,
+                                        software_trigger_global_tag_name=software_trigger_global_tag_name)
+
+
+def get_all_menus_in_database(base_identifier=None,
+                              software_trigger_global_tag_name=SOFTWARE_TRIGGER_GLOBAL_TAG_NAME):
+    """
+    Helper function to download a last of all menus from the condition database in the given tag (and for
+    the given base identifier, if not None).
+    :param base_identifier: The base identifier for which the cut list should be downloaded. Leave empty to get a list
+        of all cuts for all base identifiers.
+    :param software_trigger_global_tag_name: The global tag in the database to look at.
+    :return: A list of tuples (base_identifier,) for each menu in the database.
+    """
+    return _get_all_objects_in_database(base_identifier=base_identifier, number_of_splits=2,
+                                        software_trigger_global_tag_name=software_trigger_global_tag_name)
 
 
 if __name__ == '__main__':

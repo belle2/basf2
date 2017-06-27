96af891d6bc25a6419fbe3168d2f19c1a1b72b93
Nils Braun <nils.braun@desy.de>
2017-06-27 11:52:27 +0200

    Removed the trigger menu from the steering file as this is now handled via the DB

--- hlt/softwaretrigger/scripts/softwaretrigger/__init__.py
+++ hlt/softwaretrigger/scripts/softwaretrigger/__init__.py
@@ -4,16 +4,6 @@ import vertex
 
 SOFTWARE_TRIGGER_GLOBAL_TAG_NAME = "production"
 
-FAST_RECO_CUTS = ["reject_ee", "accept_ee", "reject_bkg"]
-
-HLT_CUTS = ["accept_hadron", "accept_tau_tau", "accept_2_tracks", "accept_1_track1_cluster",
-            "accept_mumu_2trk", "accept_mumu_1trk", "accept_single_photon"]
-
-CALIB_CUTS = ["accept_ee", "accept_gee", "accept_mumu", "accept_gmumu", "accept_gg_ee", "accept_gg_4pi",
-              "accept_D0_Kpi", "accept_Dstar", "accept_Xi_piLambda", "accept_test",
-              "accept_dqm_D0", "accept_dqm_Dplus", "accept_dqm_Dstar",
-              "accept_dqm_Jpsiee", "accept_dqm_Jpsimumu"]
-
 
 def add_fast_reco_software_trigger(path, store_array_debug_prescale=0):
     """

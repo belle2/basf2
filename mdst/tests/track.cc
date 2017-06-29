d44d81f8e152c44392169c7b56553d7d07b0f58a
Thomas Hauth <Thomas.Hauth@kit.edu>
2017-06-29 15:40:49 +0200

    adapted to changed behavior to return nullptr if no requested fit does not exist and extended unit tests for Track class.

--- mdst/tests/track.cc
+++ mdst/tests/track.cc
@@ -5,6 +5,8 @@
 #include <framework/datastore/StoreArray.h>
 #include <gtest/gtest.h>
 
+#include <algorithm>
+
 using namespace std;
 
 namespace Belle2 {
@@ -33,7 +35,8 @@ namespace Belle2 {
     const float pValue = 1.;
     const float bField = 1.5;
 
-    const auto myPion = myResults.appendNew(dummyVector3, dummyVector3, dummyMatrix, charge, Const::pion,     pValue, bField, 0, 0);
+    const auto myPion = myResults.appendNew(dummyVector3, dummyVector3, dummyMatrix, charge, Const::pion,     pValue,
+                                            bField, 0, 0);
 
     const auto myKaon = myResults.appendNew(dummyVector3, dummyVector3, dummyMatrix, charge, Const::kaon,     pValue, bField, 0, 0);
 
@@ -56,14 +59,37 @@ namespace Belle2 {
     EXPECT_EQ(mytrack1.getTrackFitResult(Const::pion)->getParticleType(), Const::pion);
     EXPECT_EQ(mytrack1.getTrackFitResult(Const::kaon)->getParticleType(), Const::kaon);
     EXPECT_EQ(mytrack1.getTrackFitResult(Const::muon)->getParticleType(), Const::muon);
-    // If the index of the corresponding particle is *not* set, the pion hypothesis should be returned.
-    EXPECT_EQ(mytrack1.getTrackFitResult(Const::electron)->getParticleType(), Const::pion);
+    // If the index of the corresponding particle is *not* set, a nullptr should be returned.
+    EXPECT_EQ(mytrack1.getTrackFitResult(Const::electron), nullptr);
 
     // If the index of the corresponding particle is set, the correct particle should be returned.
     EXPECT_EQ(mytrack1.getTrackFitResult(Const::pion)->getArrayIndex(), myPion->getArrayIndex());
     EXPECT_EQ(mytrack1.getTrackFitResult(Const::kaon)->getArrayIndex(), myKaon->getArrayIndex());
     EXPECT_EQ(mytrack1.getTrackFitResult(Const::muon)->getArrayIndex(), myMuon->getArrayIndex());
-    // If the index of the corresponding particle is *not* set, the pion hypothesis should be returned.
-    EXPECT_EQ(mytrack1.getTrackFitResult(Const::electron)->getArrayIndex(), myPion->getArrayIndex());
+    // If the index of the corresponding particle is *not* set, a nullptr should be returned.
+    EXPECT_EQ(mytrack1.getTrackFitResult(Const::electron), nullptr);
+
+    const auto allResults = mytrack1.getTrackFitResults();
+    // should return all hypothesis which were added before
+    EXPECT_EQ(allResults.size(), 3);
+
+    // check that all correct hypothesis are returned and the electron is not
+    auto countPion = std::count_if(allResults.begin(), allResults.end(),
+                                   [myPion](std::pair<Const::ChargedStable,  const TrackFitResult*> fitPair)
+    {return (fitPair.first == Const::pion) && (fitPair.second->getArrayIndex() == myPion->getArrayIndex());});
+    auto countMuon = std::count_if(allResults.begin(), allResults.end(),
+                                   [myMuon](std::pair<Const::ChargedStable,  const TrackFitResult*> fitPair)
+    {return (fitPair.first == Const::muon) && (fitPair.second->getArrayIndex() == myMuon->getArrayIndex());});
+    auto countElectron = std::count_if(allResults.begin(), allResults.end(),
+                                       [](std::pair<Const::ChargedStable,  const TrackFitResult*> fitPair)
+    {return fitPair.first == Const::electron;});
+    auto countKaon = std::count_if(allResults.begin(), allResults.end(),
+                                   [myKaon](std::pair<Const::ChargedStable,  const TrackFitResult*> fitPair)
+    {return (fitPair.first == Const::kaon) && (fitPair.second->getArrayIndex() == myKaon->getArrayIndex());});
+
+    EXPECT_EQ(countPion, 1);
+    EXPECT_EQ(countMuon, 1);
+    EXPECT_EQ(countElectron, 0);
+    EXPECT_EQ(countKaon, 1);
   }
 }  // namespace

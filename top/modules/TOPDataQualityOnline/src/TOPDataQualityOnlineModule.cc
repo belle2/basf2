69ad0d7b86241f80e367adc948a14c3a2d8893ae
Wang, Boqun <boqunwg@ucmail.uc.edu>
2017-07-05 20:06:29 +0900

    Move unnecessary histograms to verbose mode

--- top/modules/TOPDataQualityOnline/src/TOPDataQualityOnlineModule.cc
+++ top/modules/TOPDataQualityOnline/src/TOPDataQualityOnlineModule.cc
@@ -45,6 +45,7 @@ namespace Belle2 {
     addParam("TDCCalHigh", m_TDC_cal_high, "Higher bound of cal TDC", 0);
     addParam("TDCLaserLow", m_TDC_laser_low, "lower bound of laser TDC", -200);
     addParam("TDCLaserHigh", m_TDC_laser_high, "Higher bound of laser TDC", -50);
+    addParam("VerboseMode", m_verbose, "use more histograms", 0);
   }
 
 
@@ -59,13 +60,55 @@ namespace Belle2 {
     const auto* geo = TOPGeometryPar::Instance()->getGeometry();
     m_numModules = geo->getNumModules();
 
-    m_all_hits = new TH1F("all_hits", "Number of all hits per bar", m_numModules, 0.5, m_numModules + 0.5);
-    m_good_hits = new TH1F("good_hits", "Number of good hits per bar", m_numModules, 0.5, m_numModules + 0.5);
-    m_bad_hits = new TH1F("bad_hits", "Number of bad hits per bar", m_numModules, 0.5, m_numModules + 0.5);
-    m_good_hits_mean = new TH1F("good_hits_mean", "Mean of number of good hits per bar", m_numModules, 0.5, m_numModules + 0.5);
-    m_bad_hits_mean = new TH1F("bad_hits_mean", "Mean of number of bad hits per bar", m_numModules, 0.5, m_numModules + 0.5);
-    m_good_hits_rms = new TH1F("good_hits_rms", "RMS of number of good hits per bar", m_numModules, 0.5, m_numModules + 0.5);
-    m_bad_hits_rms = new TH1F("bad_hits_rms", "RMS of number of bad hits per bar", m_numModules, 0.5, m_numModules + 0.5);
+    if (m_verbose == 1) { // in verbose mode
+      m_all_hits = new TH1F("all_hits", "Number of all hits per bar", m_numModules, 0.5, m_numModules + 0.5);
+      m_good_hits = new TH1F("good_hits", "Number of good hits per bar", m_numModules, 0.5, m_numModules + 0.5);
+      m_bad_hits = new TH1F("bad_hits", "Number of bad hits per bar", m_numModules, 0.5, m_numModules + 0.5);
+      m_good_hits_mean = new TH1F("good_hits_mean", "Mean of number of good hits per bar", m_numModules, 0.5, m_numModules + 0.5);
+      m_bad_hits_mean = new TH1F("bad_hits_mean", "Mean of number of bad hits per bar", m_numModules, 0.5, m_numModules + 0.5);
+      m_good_hits_rms = new TH1F("good_hits_rms", "RMS of number of good hits per bar", m_numModules, 0.5, m_numModules + 0.5);
+      m_bad_hits_rms = new TH1F("bad_hits_rms", "RMS of number of bad hits per bar", m_numModules, 0.5, m_numModules + 0.5);
+
+      for (int i = 0; i < m_numModules; i++) {
+        int module = i + 1;
+        string name = str(format("all_hits_%1%") % (module));
+        string title = str(format("Number of all hits per event of module #%1%") % (module));
+        string name1 = str(format("good_hits_%1%") % (module));
+        string title1 = str(format("Number of good hits per event of module #%1%") % (module));
+        string name2 = str(format("bad_hits_%1%") % (module));
+        string title2 = str(format("Number of bad hits per event of module #%1%") % (module));
+        TH1F* h = new TH1F(name.c_str(), title.c_str(), 200, 0, 400);
+        TH1F* h1 = new TH1F(name1.c_str(), title1.c_str(), 200, 0, 400);
+        TH1F* h2 = new TH1F(name2.c_str(), title2.c_str(), 200, 0, 400);
+        m_slot_all_hits.push_back(h);
+        m_slot_good_hits.push_back(h1);
+        m_slot_bad_hits.push_back(h2);
+      }
+
+      for (int i = 0; i < m_numModules; i++) {
+        int module = i + 1;
+        string name = str(format("hit_quality_%1%") % (module));
+        string title = str(format("Hit quality of module #%1%") % (module));
+        TH1F* h = new TH1F(name.c_str(), title.c_str(), 8, 0.5, 8.5);
+        m_hit_quality.push_back(h);
+      }
+
+      for (int i = 0; i < m_numModules; i++) {
+        int module = i + 1;
+        string name = str(format("all_ADC_%1%") % (module));
+        string title = str(format("ADC distribution for module #%1%") % (module));
+        string name1 = str(format("all_ADC_mean_%1%") % (module));
+        string title1 = str(format("ADC mean distribution for module #%1%") % (module));
+        string name2 = str(format("all_ADC_RMS_%1%") % (module));
+        string title2 = str(format("ADC RMS distribution for module #%1%") % (module));
+        TH1F* h = new TH1F(name.c_str(), title.c_str(), 1000, 0.5, 1000.5);
+        TH2F* h1 = new TH2F(name1.c_str(), title1.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
+        TH2F* h2 = new TH2F(name2.c_str(), title2.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
+        m_all_ADC.push_back(h);
+        m_all_ADC_mean.push_back(h1);
+        m_all_ADC_RMS.push_back(h2);
+      }
+    }
 
     m_particle_hits = new TH1F("particle_hits", "Number of particle hits per bar", m_numModules, 0.5, m_numModules + 0.5);
     m_laser_hits = new TH1F("laser_hits", "Number of laser hits per bar", m_numModules, 0.5, m_numModules + 0.5);
@@ -98,21 +141,6 @@ namespace Belle2 {
 
     for (int i = 0; i < m_numModules; i++) {
       int module = i + 1;
-      string name = str(format("all_hits_%1%") % (module));
-      string title = str(format("Number of all hits per event of module #%1%") % (module));
-      string name1 = str(format("good_hits_%1%") % (module));
-      string title1 = str(format("Number of good hits per event of module #%1%") % (module));
-      string name2 = str(format("bad_hits_%1%") % (module));
-      string title2 = str(format("Number of bad hits per event of module #%1%") % (module));
-      TH1F* h = new TH1F(name.c_str(), title.c_str(), 200, 0, 400);
-      TH1F* h1 = new TH1F(name1.c_str(), title1.c_str(), 200, 0, 400);
-      TH1F* h2 = new TH1F(name2.c_str(), title2.c_str(), 200, 0, 400);
-      m_slot_all_hits.push_back(h);
-      m_slot_good_hits.push_back(h1);
-      m_slot_bad_hits.push_back(h2);
-    }
-    for (int i = 0; i < m_numModules; i++) {
-      int module = i + 1;
       string name = str(format("particle_hits_%1%") % (module));
       string title = str(format("Number of particle hits per event of module #%1%") % (module));
       string name1 = str(format("laser_hits_%1%") % (module));
@@ -140,22 +168,17 @@ namespace Belle2 {
       string title2 = str(format("Number of bad hits by channel of module #%1%") % (module));
       string name3 = str(format("particle_channel_hits_%1%") % (module));
       string title3 = str(format("Number of particle hits by channel of module #%1%") % (module));
-      TH1F* h = new TH1F(name.c_str(), title.c_str(), 512, 0.5, 512.5);
-      TH1F* h1 = new TH1F(name1.c_str(), title1.c_str(), 512, 0.5, 512.5);
-      TH1F* h2 = new TH1F(name2.c_str(), title2.c_str(), 512, 0.5, 512.5);
       TH1F* h3 = new TH1F(name3.c_str(), title2.c_str(), 512, 0.5, 512.5);
       h3->SetOption("LIVE");
-      m_channel_all_hits.push_back(h);
-      m_channel_good_hits.push_back(h1);
-      m_channel_bad_hits.push_back(h2);
       m_channel_particle_hits.push_back(h3);
-    }
-    for (int i = 0; i < m_numModules; i++) {
-      int module = i + 1;
-      string name = str(format("hit_quality_%1%") % (module));
-      string title = str(format("Hit quality of module #%1%") % (module));
-      TH1F* h = new TH1F(name.c_str(), title.c_str(), 8, 0.5, 8.5);
-      m_hit_quality.push_back(h);
+      if (m_verbose == 1) { // in verbose mode
+        TH1F* h = new TH1F(name.c_str(), title.c_str(), 512, 0.5, 512.5);
+        TH1F* h1 = new TH1F(name1.c_str(), title1.c_str(), 512, 0.5, 512.5);
+        TH1F* h2 = new TH1F(name2.c_str(), title2.c_str(), 512, 0.5, 512.5);
+        m_channel_all_hits.push_back(h);
+        m_channel_good_hits.push_back(h1);
+        m_channel_bad_hits.push_back(h2);
+      }
     }
     for (int i = 0; i < m_numModules; i++) {
       int module = i + 1;
@@ -163,26 +186,13 @@ namespace Belle2 {
       string title = str(format("Number of hits in x-y for module #%1%") % (module));
       string name1 = str(format("laser_hits_xy_%1%") % (module));
       string title1 = str(format("Number of laser hits in x-y for module #%1%") % (module));
-      TH2F* h = new TH2F(name.c_str(), title.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
       TH2F* h1 = new TH2F(name1.c_str(), title1.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
       h1->SetOption("LIVE");
-      m_all_hits_xy.push_back(h);
       m_laser_hits_xy.push_back(h1);
-    }
-    for (int i = 0; i < m_numModules; i++) {
-      int module = i + 1;
-      string name = str(format("all_ADC_%1%") % (module));
-      string title = str(format("ADC distribution for module #%1%") % (module));
-      string name1 = str(format("all_ADC_mean_%1%") % (module));
-      string title1 = str(format("ADC mean distribution for module #%1%") % (module));
-      string name2 = str(format("all_ADC_RMS_%1%") % (module));
-      string title2 = str(format("ADC RMS distribution for module #%1%") % (module));
-      TH1F* h = new TH1F(name.c_str(), title.c_str(), 1000, 0.5, 1000.5);
-      TH2F* h1 = new TH2F(name1.c_str(), title1.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
-      TH2F* h2 = new TH2F(name2.c_str(), title2.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
-      m_all_ADC.push_back(h);
-      m_all_ADC_mean.push_back(h1);
-      m_all_ADC_RMS.push_back(h2);
+      if (m_verbose == 1) { // in verbose mode
+        TH2F* h = new TH2F(name.c_str(), title.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
+        m_all_hits_xy.push_back(h);
+      }
     }
     for (int i = 0; i < m_numModules; i++) {
       int module = i + 1;
@@ -193,12 +203,14 @@ namespace Belle2 {
       string name2 = str(format("all_TDC_RMS_%1%") % (module));
       string title2 = str(format("TDC RMS distribution for module #%1%") % (module));
       TH1F* h = new TH1F(name.c_str(), title.c_str(), 500, -250, 250);
-      TH2F* h1 = new TH2F(name1.c_str(), title1.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
-      TH2F* h2 = new TH2F(name2.c_str(), title2.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
       h->SetOption("LIVE");
       m_all_TDC.push_back(h);
-      m_all_TDC_mean.push_back(h1);
-      m_all_TDC_RMS.push_back(h2);
+      if (m_verbose == 1) { // in verbose mode
+        TH2F* h1 = new TH2F(name1.c_str(), title1.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
+        TH2F* h2 = new TH2F(name2.c_str(), title2.c_str(), 64, 0.5, 64.5, 8, 0.5, 8.5);
+        m_all_TDC_mean.push_back(h1);
+        m_all_TDC_RMS.push_back(h2);
+      }
     }
     oldDir->cd();
   }
@@ -235,7 +247,6 @@ namespace Belle2 {
     vector<double> rawTimeHit;
     vector<int> isCal;
     for (const auto& digit : digits) {
-      m_all_hits->Fill(digit.getModuleID());
       int i = digit.getModuleID() - 1;
       int col = digit.getPixelCol();
       int row = digit.getPixelRow();
@@ -244,8 +255,6 @@ namespace Belle2 {
         continue;
       }
       all_hit[i]++;
-      m_channel_all_hits[i]->Fill(digit.getPixelID());
-      m_all_ADC[i]->Fill(digit.getPulseHeight());
 
       if (digit.getASICChannel() == 0) {
         isCal.push_back(1);
@@ -262,36 +271,42 @@ namespace Belle2 {
       rowHit.push_back(digit.getPixelRow());
       m_nhits++;
 
-      double nhits = m_all_hits_xy[i]->GetBinContent(col, row);
-      double adc_mean = m_all_ADC_mean[i]->GetBinContent(col, row);
-      double adc_rms = m_all_ADC_RMS[i]->GetBinContent(col, row);
-      double adc_mean2 = adc_rms * adc_rms + adc_mean * adc_mean;
-      double tdc_mean = m_all_TDC_mean[i]->GetBinContent(col, row);
-      double tdc_rms = m_all_TDC_RMS[i]->GetBinContent(col, row);
-      double tdc_mean2 = tdc_rms * tdc_rms + tdc_mean * tdc_mean;
+      if (m_verbose == 1) { // in verbose mode
+        m_all_hits->Fill(digit.getModuleID());
+        m_channel_all_hits[i]->Fill(digit.getPixelID());
+        m_all_ADC[i]->Fill(digit.getPulseHeight());
+        double nhits = m_all_hits_xy[i]->GetBinContent(col, row);
+        double adc_mean = m_all_ADC_mean[i]->GetBinContent(col, row);
+        double adc_rms = m_all_ADC_RMS[i]->GetBinContent(col, row);
+        double adc_mean2 = adc_rms * adc_rms + adc_mean * adc_mean;
+        double tdc_mean = m_all_TDC_mean[i]->GetBinContent(col, row);
+        double tdc_rms = m_all_TDC_RMS[i]->GetBinContent(col, row);
+        double tdc_mean2 = tdc_rms * tdc_rms + tdc_mean * tdc_mean;
 
-      adc_mean = (nhits * adc_mean + digit.getPulseHeight()) / (nhits + 1);
-      adc_mean2 = (nhits * adc_mean2 + digit.getPulseHeight() * digit.getPulseHeight()) / (nhits + 1);
-      tdc_mean = (nhits * tdc_mean + digit.getRawTime()) / (nhits + 1);
-      tdc_mean2 = (nhits * tdc_mean2 + digit.getRawTime() * digit.getRawTime()) / (nhits + 1);
+        adc_mean = (nhits * adc_mean + digit.getPulseHeight()) / (nhits + 1);
+        adc_mean2 = (nhits * adc_mean2 + digit.getPulseHeight() * digit.getPulseHeight()) / (nhits + 1);
+        tdc_mean = (nhits * tdc_mean + digit.getRawTime()) / (nhits + 1);
+        tdc_mean2 = (nhits * tdc_mean2 + digit.getRawTime() * digit.getRawTime()) / (nhits + 1);
 
-      m_all_ADC_mean[i]->SetBinContent(col, row, adc_mean);
-      m_all_ADC_RMS[i]->SetBinContent(col, row, sqrt(adc_mean2 - adc_mean * adc_mean));
-      m_all_TDC_mean[i]->SetBinContent(col, row, tdc_mean);
-      m_all_TDC_RMS[i]->SetBinContent(col, row, sqrt(tdc_mean2 - tdc_mean * tdc_mean));
-      m_all_hits_xy[i]->Fill(col, row);
+        m_all_ADC_mean[i]->SetBinContent(col, row, adc_mean);
+        m_all_ADC_RMS[i]->SetBinContent(col, row, sqrt(adc_mean2 - adc_mean * adc_mean));
+        m_all_TDC_mean[i]->SetBinContent(col, row, tdc_mean);
+        m_all_TDC_RMS[i]->SetBinContent(col, row, sqrt(tdc_mean2 - tdc_mean * tdc_mean));
+        m_all_hits_xy[i]->Fill(col, row);
+        int flag = flag_hit(digit);
+        if (flag == 0) { // good hit
+          m_good_hits->Fill(digit.getModuleID());
+          m_channel_good_hits[i]->Fill(digit.getPixelID());
+        } else { // bad hit
+          m_bad_hits->Fill(digit.getModuleID());
+          m_channel_bad_hits[i]->Fill(digit.getPixelID());
+          m_hit_quality[i]->Fill(flag);
+        }
+      }
 
       int flag = flag_hit(digit);
-      if (flag == 0) { // good hit
-        m_good_hits->Fill(digit.getModuleID());
-        good_hit[i]++;
-        m_channel_good_hits[i]->Fill(digit.getPixelID());
-      } else { // bad hit
-        m_bad_hits->Fill(digit.getModuleID());
-        bad_hit[i]++;
-        m_channel_bad_hits[i]->Fill(digit.getPixelID());
-        m_hit_quality[i]->Fill(flag);
-      }
+      if (flag == 0) good_hit[i]++;
+      else bad_hit[i]++;
     }
     for (int i = 0; i < m_nhits; i++) {
       int reducedPixelId = (pixelHit[i] - 1) / 8 + moduleHit[i] * 100;
@@ -322,19 +337,20 @@ namespace Belle2 {
     }
     m_iEvent++;
     for (int i = 0; i < m_numModules; i++) {
-      if (all_hit[i] > 0) m_slot_all_hits[i]->Fill(all_hit[i]);
-      if (good_hit[i] > 0) m_slot_good_hits[i]->Fill(good_hit[i]);
-      if (bad_hit[i] > 0) m_slot_bad_hits[i]->Fill(bad_hit[i]);
+      if (m_verbose == 1) { // in verbose mode
+        if (all_hit[i] > 0) m_slot_all_hits[i]->Fill(all_hit[i]);
+        if (good_hit[i] > 0) m_slot_good_hits[i]->Fill(good_hit[i]);
+        if (bad_hit[i] > 0) m_slot_bad_hits[i]->Fill(bad_hit[i]);
+        m_good_hits_mean->SetBinContent(i + 1, m_slot_good_hits[i]->GetMean());
+        m_bad_hits_mean->SetBinContent(i + 1, m_slot_bad_hits[i]->GetMean());
+
+        m_good_hits_rms->SetBinContent(i + 1, m_slot_good_hits[i]->GetRMS());
+        m_bad_hits_rms->SetBinContent(i + 1, m_slot_bad_hits[i]->GetRMS());
+      }
       if (particle_hit[i] > 0) m_slot_particle_hits[i]->Fill(particle_hit[i]);
       if (laser_hit[i] > 0) m_slot_laser_hits[i]->Fill(laser_hit[i]);
       if (cal_hit[i] > 0) m_slot_cal_hits[i]->Fill(cal_hit[i]);
       if (other_hit[i] > 0) m_slot_other_hits[i]->Fill(other_hit[i]);
-
-      m_good_hits_mean->SetBinContent(i + 1, m_slot_good_hits[i]->GetMean());
-      m_bad_hits_mean->SetBinContent(i + 1, m_slot_bad_hits[i]->GetMean());
-
-      m_good_hits_rms->SetBinContent(i + 1, m_slot_good_hits[i]->GetRMS());
-      m_bad_hits_rms->SetBinContent(i + 1, m_slot_bad_hits[i]->GetRMS());
 
       m_particle_hits_mean->SetBinContent(i + 1, m_slot_particle_hits[i]->GetMean());
       m_laser_hits_mean->SetBinContent(i + 1, m_slot_laser_hits[i]->GetMean());

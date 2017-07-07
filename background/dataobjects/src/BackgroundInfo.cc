524d4b6de6e3ba5e719eb69e257cf158bfa3ad8c
staric <marko.staric@ijs.si>
2017-07-07 11:17:15 +0000

    Make BackgroundMetaData and BackgroundInfo mergeable classes (unfinished)

--- background/dataobjects/src/BackgroundInfo.cc
+++ background/dataobjects/src/BackgroundInfo.cc
@@ -9,10 +9,89 @@
  **************************************************************************/
 
 #include <background/dataobjects/BackgroundInfo.h>
+#include <framework/logging/Logger.h>
 #include <iostream>
 
 using namespace std;
 using namespace Belle2;
+
+
+bool BackgroundInfo::canBeMerged(const BackgroundInfo* otherObj)
+{
+
+  if (otherObj->getMethod() != m_method) {
+    B2ERROR("BackgroundInfo: objects cannot be merged (different method)");
+    return false;
+  }
+
+  if (otherObj->getComponents() != m_components) { // TODO: sort & compare
+    B2ERROR("BackgroundInfo: objects cannot be merged (different components)");
+    return false;
+  }
+
+  if (otherObj->getMinTime() != m_minTime) {
+    B2ERROR("BackgroundInfo: objects cannot be merged (different minTime)");
+    return false;
+  }
+
+  if (otherObj->getMaxTime() != m_maxTime) {
+    B2ERROR("BackgroundInfo: objects cannot be merged (different maxTime)");
+    return false;
+  }
+
+  if (otherObj->getMinTimeECL() != m_minTimeECL) {
+    B2ERROR("BackgroundInfo: objects cannot be merged (different minTimeECL)");
+    return false;
+  }
+
+  if (otherObj->getMaxTimeECL() != m_maxTimeECL) {
+    B2ERROR("BackgroundInfo: objects cannot be merged (different maxTimeECL)");
+    return false;
+  }
+
+  if (otherObj->getMinTimePXD() != m_minTimePXD) {
+    B2ERROR("BackgroundInfo: objects cannot be merged (different minTimePXD)");
+    return false;
+  }
+
+  if (otherObj->getMaxTimePXD() != m_maxTimePXD) {
+    B2ERROR("BackgroundInfo: objects cannot be merged (different maxTimePXD)");
+    return false;
+  }
+
+  if (otherObj->getWrapAround() != m_wrapAround) {
+    B2ERROR("BackgroundInfo: objects cannot be merged (different wrapAround)");
+    return false;
+  }
+
+  if (otherObj->getMaxEdepECL() != m_maxEdepECL) {
+    B2ERROR("BackgroundInfo: objects cannot be merged (different maxEdepECL)");
+    return false;
+  }
+
+  // TODO   BackgroundDescr
+
+  return true;
+}
+
+
+void BackgroundInfo::merge(const Mergeable* other)
+{
+  auto otherObj = static_cast<const BackgroundInfo*>(other);
+
+  if (!canBeMerged(otherObj)) throw BackgroundInfoNotMergeable();
+
+  // TODO merge
+
+}
+
+void BackgroundInfo::clear()
+{
+
+  // TODO: clear what's merged
+
+}
+
 
 void BackgroundInfo::print() const
 {

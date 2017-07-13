2c8ffe832fb0ff0a47888f7642bebf2efc4f2315
Tomoyuki konno <tomoyuki.konno@desy.de>
2017-07-13 11:00:45 +0900

    Modified to show all histograms by default

--- daq/dqm/analysis/examples/canvasserver.C
+++ daq/dqm/analysis/examples/canvasserver.C
@@ -2,14 +2,13 @@
    // Open a server socket looking for connections on a named service or
    // on a specified port.
    //TServerSocket *ss = new TServerSocket("rootserv", kTRUE);
-   TServerSocket *ss = new TServerSocket(9191, kTRUE);
+   TServerSocket *ss = new TServerSocket(9090, kTRUE);
    THttpServer *m_serv = new THttpServer("http:8081");
    TMonitor *fMon = new TMonitor;
    TList    *fSockets = new TList;   // list of open spy sockets
    
    fMon->Add(ss);
    TCanvas *h = 0;
-   TH1 * hh;
    while (1) {
       TMessage *mess;
       TSocket  *s;
@@ -49,7 +48,7 @@
           if(h) printf("===== recv object %s ====\n",h->GetName());
           gROOT->GetListOfCanvases()->ls();
           if (h) {           
-	    h->Draw();  // draw canvas
+              h->Draw();  // draw canvas
           }
         }
       } else {
--- daq/dqm/analysis/modules/src/DQMHistAnalysisInput.cc
+++ daq/dqm/analysis/modules/src/DQMHistAnalysisInput.cc
@@ -9,7 +9,6 @@
 
 #include <daq/dqm/analysis/modules/DQMHistAnalysisInput.h>
 
-using namespace std;
 using namespace Belle2;
 
 //-----------------------------------------------------------------
@@ -25,7 +24,7 @@ DQMHistAnalysisInputModule::DQMHistAnalysisInputModule()
   : DQMHistAnalysisModule()
 {
   //Parameter definition
-  addParam("HistMemoryPath", m_mempath, "Path to Input Hist memory", string(""));
+  addParam("HistMemoryPath", m_mempath, "Path to Input Hist memory", std::string(""));
   addParam("HistMemorySize", m_memsize, "Size of Input Hist memory", 10000000);
   addParam("RefreshInterval", m_interval, "Refresh interval of histograms", 10);
   B2DEBUG(1, "DQMHistAnalysisInput: Constructor done.");
@@ -52,34 +51,47 @@ void DQMHistAnalysisInputModule::beginRun()
 void DQMHistAnalysisInputModule::event()
 {
   sleep(m_interval);
-  std::vector<TH1*> h;
+  std::vector<TH1*> hs;
   TMemFile* file = m_memory->LoadMemFile();
   file->cd();
   TIter next(file->GetListOfKeys());
   TKey* key = NULL;
   while ((key = (TKey*)next())) {
-    h.push_back((TH1*)key->ReadObj());
+    TH1* h = (TH1*)key->ReadObj();
+    hs.push_back(h);
+    TString a = h->GetName();
+    a.ReplaceAll("/", "_");
+    std::string name = a.Data();
+    if (m_cs.find(name) == m_cs.end()) {
+      TCanvas* c = new TCanvas("c_" + a);
+      m_cs.insert(std::pair<std::string, TCanvas*>(name, c));
+    }
+    TCanvas* c = m_cs[name];
+    c->cd();
+    h->Draw();
+    c->Update();
   }
   resetHist();
-  for (size_t i = 0; i < h.size(); i++) {
-    addHist("", h[i]->GetName(), h[i]);
-    B2INFO("Found : " << h[i]->GetName() << " : " << h[i]->GetEntries());
-    std::string vname = h[i]->GetName();
-    setFloatValue(vname + ".entries", h[i]->GetEntries());
-    if (h[i]->GetDimension() == 1) {
-      setFloatValue(vname + ".rms", h[i]->GetRMS());
-      setFloatValue(vname + ".rmserr", h[i]->GetRMSError());
-      setFloatValue(vname + ".mean", h[i]->GetMean());
-      setFloatValue(vname + ".meanerr", h[i]->GetMeanError());
-    } else if (h[i]->GetDimension() == 2) {
-      setFloatValue(vname + ".xrms", h[i]->GetRMS(1));
-      setFloatValue(vname + ".xrmserr", h[i]->GetRMSError(1));
-      setFloatValue(vname + ".xmean", h[i]->GetMean(1));
-      setFloatValue(vname + ".xmeanerr", h[i]->GetMeanError(1));
-      setFloatValue(vname + ".yrms", h[i]->GetRMS(2));
-      setFloatValue(vname + ".yrmserr", h[i]->GetRMSError(2));
-      setFloatValue(vname + ".ymean", h[i]->GetMean(2));
-      setFloatValue(vname + ".ymeanerr", h[i]->GetMeanError(2));
+  for (size_t i = 0; i < hs.size(); i++) {
+    TH1* h = hs[i];
+    addHist("", h->GetName(), h);
+    B2INFO("Found : " << h->GetName() << " : " << h->GetEntries());
+    std::string vname = h->GetName();
+    setFloatValue(vname + ".entries", h->GetEntries());
+    if (h->GetDimension() == 1) {
+      setFloatValue(vname + ".rms", h->GetRMS());
+      setFloatValue(vname + ".rmserr", h->GetRMSError());
+      setFloatValue(vname + ".mean", h->GetMean());
+      setFloatValue(vname + ".meanerr", h->GetMeanError());
+    } else if (h->GetDimension() == 2) {
+      setFloatValue(vname + ".xrms", h->GetRMS(1));
+      setFloatValue(vname + ".xrmserr", h->GetRMSError(1));
+      setFloatValue(vname + ".xmean", h->GetMean(1));
+      setFloatValue(vname + ".xmeanerr", h->GetMeanError(1));
+      setFloatValue(vname + ".yrms", h->GetRMS(2));
+      setFloatValue(vname + ".yrmserr", h->GetRMSError(2));
+      setFloatValue(vname + ".ymean", h->GetMean(2));
+      setFloatValue(vname + ".ymeanerr", h->GetMeanError(2));
     }
   }
   m_count++;

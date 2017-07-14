c0e88f01d9577da762823f590984b4f18379281d
Nils Braun <nils.braun@desy.de>
2017-07-14 16:10:08 +0000

    Added a second mode to the overlap resolver, to check each seed seperately

--- tracking/ckf/findlets/base/include/OverlapResolverFindlet.h
+++ tracking/ckf/findlets/base/include/OverlapResolverFindlet.h
@@ -32,6 +32,18 @@ namespace Belle2 {
    */
   template<class AFilter>
   class OverlapResolverFindlet : public TrackFindingCDC::Findlet<typename AFilter::Object> {
+    /// Helper Functor to get the Seed of a given result
+    struct SeedGetter {
+      /// Make it a functor
+      operator TrackFindingCDC::FunctorTag();
+
+      template<class T>
+      auto operator()(const T& t) const -> decltype(t.getSeed())
+      {
+        return t.getSeed();
+      }
+    };
+
   public:
     /// The pair of seed and hit vector to check
     using ResultPair = typename AFilter::Object;
@@ -55,12 +67,21 @@ namespace Belle2 {
 
       m_qualityFilter.exposeParameters(moduleParamList, prefix);
 
-      moduleParamList->addParameter("minimalActivityState", m_param_minimalActivityState,
-                                    "Minimal activation state below which the node is not accepted.", m_param_minimalActivityState);
-      moduleParamList->addParameter("useGreedyAlgorithm", m_param_useGreedyAlgorithm, "Use the greedy algorithm"
-                                    "instead of the hopfield algorithm.", m_param_useGreedyAlgorithm);
-      moduleParamList->addParameter("enableOverlapResolving", m_param_enableOverlapResolving,
+      moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "minimalActivityState"),
+                                    m_param_minimalActivityState,
+                                    "Minimal activation state below which the node is not accepted.",
+                                    m_param_minimalActivityState);
+      moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "useGreedyAlgorithm"),
+                                    m_param_useGreedyAlgorithm,
+                                    "Use the greedy algorithm instead of the hopfield algorithm.",
+                                    m_param_useGreedyAlgorithm);
+      moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "enableOverlapResolving"),
+                                    m_param_enableOverlapResolving,
                                     "Enable the overlap resolving.", m_param_enableOverlapResolving);
+      moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "allowOverlapBetweenSeeds"),
+                                    m_param_allowOverlapBetweenSeeds,
+                                    "Allow overlaps between results with the same seed.",
+                                    m_param_allowOverlapBetweenSeeds);
     }
 
     /// Main function of this findlet: find a non overlapping set of results with the best quality.
@@ -70,89 +91,18 @@ namespace Belle2 {
         return;
       }
 
-      TrackFindingCDC::Weight maximalWeight = std::nan("");
-      TrackFindingCDC::Weight minimalWeight = std::nan("");
+      m_temporaryResults.clear();
       m_resultsWithWeight.clear();
-
-      for (ResultPair& resultPair : resultElements)
-      {
-        TrackFindingCDC::Weight weight = m_qualityFilter(resultPair);
-        if (std::isnan(weight)) {
-          continue;
-        }
-
-        if (std::isnan(maximalWeight) or weight > maximalWeight) {
-          maximalWeight = weight;
-        }
-        if (std::isnan(minimalWeight) or weight < minimalWeight) {
-          minimalWeight = weight;
-        }
-
-        m_resultsWithWeight.emplace_back(&resultPair, weight);
-      }
-
       m_overlapResolverInfos.clear();
 
-      for (unsigned int resultIndex = 0; resultIndex < m_resultsWithWeight.size(); resultIndex++)
+      // Sort results by seed, as it makes the next operations faster
+      std::sort(resultElements.begin(), resultElements.end(), TrackFindingCDC::LessOf<SeedGetter>());
+
+      if (not m_param_allowOverlapBetweenSeeds)
       {
-        // normalize the weight
-        double weight = m_resultsWithWeight[resultIndex].getWeight();
-        if (minimalWeight == maximalWeight) {
-          weight = 1;
-        } else {
-          weight = (weight - minimalWeight) / (maximalWeight - minimalWeight);
-        }
-
-        // activity state has no meaning here -> set to 1.
-        // the overlap will be set later on.
-        m_overlapResolverInfos.push_back(OverlapResolverNodeInfo(weight, resultIndex, {}, 1));
-      }
-
-      for (OverlapResolverNodeInfo& resolverInfo : m_overlapResolverInfos)
-      {
-        const ResultPair* resultPair = m_resultsWithWeight[resolverInfo.trackIndex];
-
-        // Find overlaps.
-        auto& overlaps = resolverInfo.overlaps;
-
-        for (const OverlapResolverNodeInfo& loopResolverInfo : m_overlapResolverInfos) {
-          // We do not allow overlap with ourselves
-          if (&loopResolverInfo == &resolverInfo) {
-            continue;
-          }
-
-          const ResultPair* loopResultPair = m_resultsWithWeight[loopResolverInfo.trackIndex];
-
-          if (loopResultPair->getSeed() == resultPair->getSeed()) {
-            overlaps.push_back(loopResolverInfo.trackIndex);
-            continue;
-          }
-
-          for (const auto& hit : resultPair->getHits()) {
-            if (TrackFindingCDC::is_in(hit, loopResultPair->getHits())) {
-              overlaps.push_back(loopResolverInfo.trackIndex);
-              break;
-            }
-          }
-        }
-      }
-
-      if (m_overlapResolverInfos.size() > 1)
-      {
-        if (m_param_useGreedyAlgorithm) {
-          m_greedyAlgorithm.performSelection(m_overlapResolverInfos);
-        } else {
-          m_hopfieldNetwork.doHopfield(m_overlapResolverInfos);
-        }
-      }
-
-      // copy results
-      m_temporaryResults.clear();
-      for (OverlapResolverNodeInfo& node : m_overlapResolverInfos)
-      {
-        if (node.activityState > m_param_minimalActivityState) {
-          m_temporaryResults.push_back(*(m_resultsWithWeight[node.trackIndex]));
-        }
+        groupbySeedsAndMaximize(resultElements, m_temporaryResults);
+      } else {
+        resolveOverlaps(resultElements, m_temporaryResults);
       }
 
       resultElements.swap(m_temporaryResults);
@@ -174,6 +124,8 @@ namespace Belle2 {
     bool m_param_enableOverlapResolving = true;
     /// Parameter: Use Greedy Algorithm
     bool m_param_useGreedyAlgorithm = false;
+    /// Parameter: Allow overlaps between results with the same seed.
+    bool m_param_allowOverlapBetweenSeeds = false;
 
     // Object Pools
     /// Overlap resolver infos as input to the hopfield network.
@@ -182,5 +134,116 @@ namespace Belle2 {
     std::vector<ResultPair> m_temporaryResults;
     /// temporary results vector with weights, out of which the overlaps will be build.
     std::vector<TrackFindingCDC::WithWeight<ResultPair*>> m_resultsWithWeight;
+
+    /// Resolve the overlaps in the given set of results
+    void resolveOverlaps(std::vector<ResultPair>& resultElements, std::vector<ResultPair>& outputResults);
+
+    /// Group the results by their seed and handle each group seperately.
+    void groupbySeedsAndMaximize(std::vector<ResultPair>& resultElements, std::vector<ResultPair>& outputResults);
   };
+
+  template<class AFilter>
+  void OverlapResolverFindlet<AFilter>::groupbySeedsAndMaximize(std::vector<ResultPair>& resultElements,
+      std::vector<ResultPair>& outputResults)
+  {
+    // resolve overlaps in each seed separately
+    const auto& groupedBySeed = TrackFindingCDC::adjacent_groupby(resultElements.begin(), resultElements.end(), SeedGetter());
+    for (const auto& resultElementsWithSameSeed : groupedBySeed) {
+      for (ResultPair& resultPair : resultElementsWithSameSeed) {
+        TrackFindingCDC::Weight weight = m_qualityFilter(resultPair);
+        if (std::isnan(weight)) {
+          continue;
+        }
+        m_resultsWithWeight.emplace_back(&resultPair, weight);
+      }
+
+      if (not m_resultsWithWeight.empty()) {
+        const auto& bestElement = *(std::max_element(m_resultsWithWeight.begin(), m_resultsWithWeight.end(),
+                                                     TrackFindingCDC::LessWeight()));
+        outputResults.push_back(*(bestElement));
+      }
+    }
+  }
+
+  template<class AFilter>
+  void OverlapResolverFindlet<AFilter>::resolveOverlaps(std::vector<ResultPair>& resultElements,
+                                                        std::vector<ResultPair>& outputResults)
+  {
+    TrackFindingCDC::Weight maximalWeight = NAN;
+    TrackFindingCDC::Weight minimalWeight = NAN;
+
+    for (ResultPair& resultPair : resultElements) {
+      TrackFindingCDC::Weight weight = m_qualityFilter(resultPair);
+      if (std::isnan(weight)) {
+        continue;
+      }
+
+      if (std::isnan(maximalWeight) or weight > maximalWeight) {
+        maximalWeight = weight;
+      }
+      if (std::isnan(minimalWeight) or weight < minimalWeight) {
+        minimalWeight = weight;
+      }
+
+      m_resultsWithWeight.emplace_back(&resultPair, weight);
+    }
+
+
+    for (unsigned int resultIndex = 0; resultIndex < m_resultsWithWeight.size(); resultIndex++) {
+      // normalize the weight
+      double weight = m_resultsWithWeight[resultIndex].getWeight();
+      if (minimalWeight == maximalWeight) {
+        weight = 1;
+      } else {
+        weight = (weight - minimalWeight) / (maximalWeight - minimalWeight);
+      }
+
+      // activity state has no meaning here -> set to the minimum.
+      // the overlap will be set later on.
+      m_overlapResolverInfos.push_back(OverlapResolverNodeInfo(weight, resultIndex, {}, m_param_minimalActivityState));
+    }
+
+    for (OverlapResolverNodeInfo& resolverInfo : m_overlapResolverInfos) {
+      const ResultPair* resultPair = m_resultsWithWeight[resolverInfo.trackIndex];
+
+      // Find overlaps.
+      auto& overlaps = resolverInfo.overlaps;
+
+      for (const OverlapResolverNodeInfo& loopResolverInfo : m_overlapResolverInfos) {
+        // We do not allow overlap with ourselves
+        if (&loopResolverInfo == &resolverInfo) {
+          continue;
+        }
+
+        const ResultPair* loopResultPair = m_resultsWithWeight[loopResolverInfo.trackIndex];
+
+        if (loopResultPair->getSeed() == resultPair->getSeed()) {
+          overlaps.push_back(loopResolverInfo.trackIndex);
+          continue;
+        }
+
+        for (const auto& hit : resultPair->getHits()) {
+          if (TrackFindingCDC::is_in(hit, loopResultPair->getHits())) {
+            overlaps.push_back(loopResolverInfo.trackIndex);
+            break;
+          }
+        }
+      }
+    }
+
+    if (m_overlapResolverInfos.size() > 1) {
+      if (m_param_useGreedyAlgorithm) {
+        m_greedyAlgorithm.performSelection(m_overlapResolverInfos);
+      } else {
+        m_hopfieldNetwork.doHopfield(m_overlapResolverInfos);
+      }
+    }
+
+    // copy results to output vector
+    for (OverlapResolverNodeInfo& node : m_overlapResolverInfos) {
+      if (node.activityState >= m_param_minimalActivityState) {
+        outputResults.push_back(*(m_resultsWithWeight[node.trackIndex]));
+      }
+    }
+  }
 }

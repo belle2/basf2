4bd262201d9abf3ed29bfc4fc362357927cf7a72
Peter Kvasnicka <kvasnicka@ipnp.troja.mff.cuni.cz>
2017-07-18 21:44:03 +0200

    Minor fix in SVDDigitMerger, re T. Hauth

--- svd/modules/svdDigitMerging/include/SVDDigitMergerModule.h
+++ svd/modules/svdDigitMerging/include/SVDDigitMergerModule.h
@@ -3,7 +3,7 @@
  * Copyright(C) 2010 - Belle II Collaboration                             *
  *                                                                        *
  * Author: The Belle II Collaboration                                     *
- * Contributors: Peter Kvasnicka                           *
+ * Contributors: Peter Kvasnicka                                          *
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/

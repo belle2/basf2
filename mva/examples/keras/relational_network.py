6132496d252566ad6aa1b1ec8b9c815cee11c1b8
Dennis Weyland <dennis.weyland@desy.de>
2017-08-07 16:45:11 +0200

    improve documentation and implementation of relational networks in keras

--- mva/examples/keras/relational_network.py
+++ mva/examples/keras/relational_network.py
@@ -9,11 +9,13 @@ import h5py
 import tensorflow as tf
 import tensorflow.contrib.keras as keras
 
-from keras.layers import Input, Dense, Concatenate
+from keras.layers import Input, Dense, Concatenate, Flatten, Dropout
+from keras.layers.merge import Average
+from keras.layers.core import Reshape
 from keras.layers import activations
 from keras.models import Model, load_model
 from keras.optimizers import adam
-from keras.losses import binary_crossentropy
+from keras.losses import binary_crossentropy, mean_squared_error
 from keras.activations import sigmoid, tanh
 from keras.engine.topology import Layer
 from keras import backend as K
@@ -27,9 +29,16 @@ class Relations(Layer):
     Relational Layer compares every combination of two feature groups with shared weights.
     Use this class as every other Layer in Keras.
     Relevant Paper: https://arxiv.org/abs/1706.01427
+    RN(O) = f_phi(sum_phi(g_theta(o_i,o_j)))
+    For flexibility reason only the part g(o_i,o_j) is modelled
+    f_phi corresponds to a MLP net
+    To sum over the output see RevelationsAverage
     """
 
-    def __init__(self, number_groups, number_features, hidden_feature_shape=[30, 30], activation=tanh, **kwargs):
+    def __init__(self, number_groups, number_features, hidden_feature_shape=[30, 30, 30, 30], activation=tanh, **kwargs):
+        """
+        Init class.
+        """
         #: Number of groups in input. This layer will divide the input shape by number of groups for comparison.
         self.number_groups = number_groups
         #: Number of features. Number of different shared weights used for comparison for each relation.
@@ -38,12 +47,18 @@ class Relations(Layer):
         self.hidden_feature_shape = hidden_feature_shape
         #: activation used for hidden layer in shared weights. For output sigmoid will always be used.
         self.activation = activations.get(activation)
+        #: how many neurons has one comparable object
+        self.group_len = 0
+        #: saves weights for call
+        self.variables = []
+        #: number of relation combinations
+        self.combinations = np.int32(np.math.factorial(self.number_groups) / (2 * np.math.factorial(self.number_groups - 2)))
 
         super(Relations, self).__init__(**kwargs)
 
     def build(self, input_shape):
         """
-        Build all weights for Relational Layer
+        Build all weights for Relations Layer
         :param input_shape: Input shape of tensor
         :return:  Nothing
         """
@@ -52,13 +67,10 @@ class Relations(Layer):
         # every neuron which goes into the raltional layer has to be used
         assert(input_shape[1] % self.number_groups == 0)
 
-        #: how many neurons has one comparable object
-        self.feature_len = input_shape[1] // self.number_groups
+        self.group_len = input_shape[1] // self.number_groups
 
-        dense_shape = [2 * self.feature_len] + self.hidden_feature_shape + [1]
+        dense_shape = [2 * self.group_len] + self.hidden_feature_shape + [1]
 
-        #: saves weights for call
-        self.variables = []
         for feature_number in range(self.number_features):
             weight_array = []
             for i in range(len(dense_shape[:-1])):
@@ -79,7 +91,7 @@ class Relations(Layer):
         :param inputs: input tensor
         :return: output tensor
         """
-        input_groups = [inputs[:, i * self.feature_len:(i + 1) * self.feature_len] for i in range(self.number_groups)]
+        input_groups = [inputs[:, i * self.group_len:(i + 1) * self.group_len] for i in range(self.number_groups)]
         outputs = []
         for feature in range(self.number_features):
             for index, group1 in enumerate(input_groups[:-1]):
@@ -92,25 +104,25 @@ class Relations(Layer):
                         net = K.bias_add(net, variables[1])
                     outputs.append(sigmoid(net))
 
-        return K.concatenate(outputs)
+        flat_result = K.concatenate(outputs)
+        return Reshape((self.combinations, self.number_features,))(flat_result)
 
     def compute_output_shape(self, input_shape):
         """
         Compute Output shape
         :return: Output shape
         """
-        import numpy as np
-
         # only accept flat layers
         assert(len(input_shape) == 2)
         # every neuron which goes into the raltional layer has to be used
         assert(input_shape[1] % self.number_groups == 0)
 
-        combinations = np.int32(np.math.factorial(self.number_groups) / (2 * np.math.factorial(self.number_groups - 2)))
-
-        return (input_shape[0], combinations * self.number_features)
+        return (input_shape[0], self.combinations, self.number_features)
 
     def get_config(self):
+        """
+        Config required for saving parameters in keras model.
+        """
         config = {
             'number_groups': self.number_groups,
             'number_features': self.number_features,
@@ -121,25 +133,91 @@ class Relations(Layer):
         return dict(list(base_config.items()) + list(config.items()))
 
 
+class RelationsAverage(Layer):
+    """
+    Follow up class for Relations.
+    Relevant paper:
+    Relevant Paper: https://arxiv.org/abs/1706.01427
+    RN(O) = f_phi(sum_phi(g_theta(o_i,o_j)))
+    Here only the sum_phi is implemented.
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Init class.
+        """
+        #: Number of Relations
+        self.number_relations = 0
+        #: Number of features coming from Relation
+        self.number_features = 0
+
+        super(RelationsAverage, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        """
+        Build all weights for Relations Layer
+        :param input_shape: Input shape of tensor
+        :return:  Nothing
+        """
+        # only accept 2D layers
+        assert(len(input_shape) == 3)
+
+        self.number_relations = input_shape[1]
+        self.number_features = input_shape[2]
+
+        super(RelationsAverage, self).build(input_shape)
+
+    def call(self, inputs):
+        """
+        Compute Relational Layer
+        :param inputs: input tensor
+        :return: output tensor
+        """
+        feature_array = []
+        for feature_number in range(self.number_features):
+            feature_array.append(inputs[:, :, feature_number])
+
+        return Average()(feature_array)
+
+    def compute_output_shape(self, input_shape):
+        """
+        Compute Output shape
+        :return: Output shape
+        """
+        # only accept 2D layers
+        assert(len(input_shape) == 3)
+
+        return (input_shape[0], input_shape[1])
+
+    def get_config(self):
+        """
+        Config required for saving parameters in keras model.
+        """
+        config = {}
+        base_config = super(RelationsAverage, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
 def get_model(number_of_features, number_of_spectators, number_of_events, training_fraction, parameters):
-    """Building Graph inside tensorflow"""
+    """
+    Building keras model
+    """
 
     input = Input(shape=(number_of_features,))
     net = input
 
     if parameters['use_relations']:
         net = Relations(number_groups=parameters['number_groups'], number_features=parameters['number_features'])(net)
-
-    if parameters['use_feed_forward']:
-        for i in range(3):
+        net = RelationsAverage()(net)
+    else:
+        for i in range(6):
             net = Dense(units=2 * number_of_features, activation=tanh)(net)
 
     output = Dense(units=1, activation=sigmoid)(net)
 
-    state = State(Model(input, output), custom_objects={'Relations': Relations})
+    state = State(Model(input, output), custom_objects={'Relations': Relations, 'RelationsAverage': RelationsAverage})
 
-    state.model.compile(optimizer=adam(lr=0.01), loss=binary_crossentropy, metrics=['accuracy'])
-
+    state.model.compile(optimizer=adam(lr=0.001), loss=binary_crossentropy, metrics=['accuracy'])
     state.model.summary()
 
     return state
@@ -157,10 +235,19 @@ def begin_fit(state, Xtest, Stest, ytest, wtest):
 
 def partial_fit(state, X, S, y, w, epoch):
     """
-    Pass received data to tensorflow session
+    Do the fit
     """
     class TestCallback(Callback):
+        """
+        Print small summary.
+        Class has to inherit from abstract Callback class
+        """
+
         def on_epoch_end(self, epoch, logs={}):
+            """
+            Print summary at the end of epoch.
+            For other possibilities look at the abstract Callback class.
+            """
             loss, acc = state.model.evaluate(state.Xtest, state.ytest, verbose=0, batch_size=1000)
             loss2, acc2 = state.model.evaluate(X[:10000], y[:10000], verbose=0, batch_size=1000)
             print('\nTesting loss: {}, acc: {}'.format(loss, acc))
@@ -192,7 +279,7 @@ if __name__ == "__main__":
     # Training results differs from the number of total lines.
 
     variables = []
-    # try using 10 lines and see what happens
+    # try using 10 and 20 lines and see what happens
     number_total_lines = 5
     # Names for the training data set
     for i in range(number_total_lines):
@@ -205,7 +292,7 @@ if __name__ == "__main__":
         """Building two lines which are hitting each other"""
         p_vec1, p_vec2 = np.random.normal(size=3), np.random.normal(size=3)
         v_cross = np.random.normal(size=3)
-        epsilon1, epsilon2 = np.random.rand() * 2 - 1, np.random.rand() * 2 - 1
+        epsilon1, epsilon2 = (np.random.rand() * 2 - 1) / 10, (np.random.rand() * 2 - 1) / 10
         v_vec1 = v_cross + (p_vec1 * epsilon1)
         v_vec2 = v_cross + (p_vec2 * epsilon2)
         return np.concatenate([p_vec1, v_vec1]), np.concatenate([p_vec2, v_vec2])
@@ -255,23 +342,22 @@ if __name__ == "__main__":
         # Train relational Net
         print('Train relational net ')
         general_options.m_identifier = os.path.join(path, 'relation.xml')
-        specific_options.m_config = json.dumps({'use_relations': True, 'use_feed_forward': False,
+        specific_options.m_config = json.dumps({'use_relations': True,
                                                 'number_groups': number_total_lines, 'number_features': 3})
         basf2_mva.teacher(general_options, specific_options)
 
         # Train normal feed forward Net:
         print('Train feed forward net')
         general_options.m_identifier = os.path.join(path, 'feed_forward.xml')
-        specific_options.m_config = json.dumps({'use_relations': False, 'use_feed_forward': True,
-                                                'number_groups': number_total_lines, 'number_features': 3})
+        specific_options.m_config = json.dumps({'use_relations': False})
         basf2_mva.teacher(general_options, specific_options)
 
         # ########################Compare Results####################################
-
         method1 = basf2_mva_util.Method(os.path.join(path, 'relation.xml'))
         method2 = basf2_mva_util.Method(os.path.join(path, 'feed_forward.xml'))
 
         test_data = basf2_mva.vector(os.path.join(path, 'test.root'))
+
         print('Apply relational net')
         p1, t1 = method1.apply_expert(test_data, general_options.m_treename)
         print('Apply feed forward net')

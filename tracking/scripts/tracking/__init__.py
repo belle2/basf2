7b5271ef89e1143711c92793b100658074e0fa8e
Sebastian Racs <sebastian.racs@desy.de>
2017-08-11 16:45:40 +0200

    Moving minium SVD SPs cut form QualityEstimator to its own module and adding it to the add_vxd_track_finding_vxdtf2 convenience function

--- tracking/scripts/tracking/__init__.py
+++ tracking/scripts/tracking/__init__.py
@@ -742,6 +742,13 @@ def add_vxd_track_finding_vxdtf2(path, svd_clusters="", reco_tracks="RecoTracks"
     # Analyzer
     #################
 
+    # When using PXD require at least 2 SVD SPs for the SPTCs
+    if(use_pxd):
+        pxdSVDCut = register_module('PXDSVDCut')
+        pxdSVDCut.param('minSVDSPs', 2)
+        pxdSVDCut.param('SpacePointTrackCandsStoreArrayName', nameSPTCs)
+        path.add_module(pxdSVDCut)
+
     # Quality
     qualityEstimator = register_module('QualityEstimatorVXD')
     qualityEstimator.param('EstimationMethod', quality_estimator)

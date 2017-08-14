cda1b538175b2406cf14e0b5b2895ff087693aa6
staric <marko.staric@ijs.si>
2017-08-14 13:25:19 +0000

    replacing: using namespace Belle2  with: namespace Belle2

--- top/dbobjects/src/TOPGeoHoneycombPanel.cc
+++ top/dbobjects/src/TOPGeoHoneycombPanel.cc
@@ -14,62 +14,64 @@
 #include <iostream>
 
 using namespace std;
-using namespace Belle2;
 
-void TOPGeoHoneycombPanel::appendContour(std::vector<std::pair<double, double> >& contour,
-                                         double y0,
-                                         bool fromLeftToRight) const
-{
-  if (m_N <= 0) return;
+namespace Belle2 {
 
-  typedef std::pair<double, double> Pair;
+  void TOPGeoHoneycombPanel::appendContour(std::vector<std::pair<double, double> >& contour,
+                                           double y0,
+                                           bool fromLeftToRight) const
+  {
+    if (m_N <= 0) return;
 
-  double R = getRadius();
-  double yc = getMaxThickness() - getMinThickness() - R;
-  double x0 = sqrt(R * R - yc * yc);
-  if (!fromLeftToRight) x0 = -x0;
-  double dx = 2.0 * x0 / m_N;
-  double x = -x0;
-  contour.push_back(Pair(x, y0));
-  for (int i = 1; i < m_N; i++) {
-    x += dx;
-    double y = y0 + sqrt(R * R - x * x) + yc;
-    contour.push_back(Pair(x, y));
+    typedef std::pair<double, double> Pair;
+
+    double R = getRadius();
+    double yc = getMaxThickness() - getMinThickness() - R;
+    double x0 = sqrt(R * R - yc * yc);
+    if (!fromLeftToRight) x0 = -x0;
+    double dx = 2.0 * x0 / m_N;
+    double x = -x0;
+    contour.push_back(Pair(x, y0));
+    for (int i = 1; i < m_N; i++) {
+      x += dx;
+      double y = y0 + sqrt(R * R - x * x) + yc;
+      contour.push_back(Pair(x, y));
+    }
+    contour.push_back(Pair(x0, y0));
+
   }
-  contour.push_back(Pair(x0, y0));
-
-}
 
 
-bool TOPGeoHoneycombPanel::isConsistent() const
-{
-  if (m_width <= 0) return false;
-  if (m_length <= 0) return false;
-  if (m_minThickness <= 0) return false;
-  if (m_maxThickness < m_minThickness) return false;
-  if (m_radius <= 0) return false;
-  if (m_edgeWidth <= 0) return false;
-  if (m_material.empty()) return false;
-  if (m_edgeMaterial.empty()) return false;
-  return true;
-}
+  bool TOPGeoHoneycombPanel::isConsistent() const
+  {
+    if (m_width <= 0) return false;
+    if (m_length <= 0) return false;
+    if (m_minThickness <= 0) return false;
+    if (m_maxThickness < m_minThickness) return false;
+    if (m_radius <= 0) return false;
+    if (m_edgeWidth <= 0) return false;
+    if (m_material.empty()) return false;
+    if (m_edgeMaterial.empty()) return false;
+    return true;
+  }
 
 
-void TOPGeoHoneycombPanel::print(const std::string& title) const
-{
-  TOPGeoBase::print(title);
+  void TOPGeoHoneycombPanel::print(const std::string& title) const
+  {
+    TOPGeoBase::print(title);
 
-  cout << " width = " << getWidth() << " " << s_unitName;
-  cout << ", length = " << getLength() << " " << s_unitName;
-  cout << ", thickness: min = " << getMinThickness() << " " << s_unitName;
-  cout << ", max = " << getMaxThickness() << " " << s_unitName;
-  cout << ", radius = " << getRadius() << " " << s_unitName << endl;
+    cout << " width = " << getWidth() << " " << s_unitName;
+    cout << ", length = " << getLength() << " " << s_unitName;
+    cout << ", thickness: min = " << getMinThickness() << " " << s_unitName;
+    cout << ", max = " << getMaxThickness() << " " << s_unitName;
+    cout << ", radius = " << getRadius() << " " << s_unitName << endl;
 
-  cout << " edge width = " << getEdgeWidth() << " " << s_unitName;
-  cout << ", y = " << getY() << " " << s_unitName;
-  cout << ", N = " << m_N << endl;
+    cout << " edge width = " << getEdgeWidth() << " " << s_unitName;
+    cout << ", y = " << getY() << " " << s_unitName;
+    cout << ", N = " << m_N << endl;
 
-  cout << " materials: body = " << getMaterial();
-  cout << ", edge = " << getEdgeMaterial() << endl;
-}
+    cout << " materials: body = " << getMaterial();
+    cout << ", edge = " << getEdgeMaterial() << endl;
+  }
 
+} // end Belle2 namespace

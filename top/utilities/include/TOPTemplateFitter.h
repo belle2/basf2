172fc846d7507e58178d42efdd1b63faef89dd1d
Tobias Weber <webert@hawaii.edu>
2017-08-14 17:18:53 +0200

    Implementing Getter/Setters and functions for template generation.

--- top/utilities/include/TOPTemplateFitter.h
+++ top/utilities/include/TOPTemplateFitter.h
@@ -13,21 +13,45 @@
 #include <top/dataobjects/TOPRawWaveform.h>
 #include <top/dbobjects/TOPSampleTimes.h>
 
+#include <vector>
+
 namespace Belle2 {
 
   namespace TOP {
 
     /**
+    @brief Parameters of the template function.
+    We use a crystal ball function
+    */
+    struct TemplateParameters {
+      double amplitude = 100.;
+      double mean = 32.;
+      double sigma = 1.81;
+      double alpha = -0.45;
+      double n = 18.06;
+    };
+
+    /**
     @brief structure holding values from template fit
     */
     struct FitResult {
-      double m_amplitude, m_amplitudeErr; /**< fitted amplitude and error*/
-      double m_backgroundOffset, m_backgroundOffsetError; /**< fitted background offset and error*/
-      double m_risingEdge, m_risingEdgeError;/**< fitted rising edge and error*/
+      double amplitudeScaling, amplitudeScalingErr; /**< fitted amplitude and error*/
+      double backgroundOffset, backgroundOffsetError; /**< fitted background offset and error*/
+      double risingEdge, risingEdgeError;/**< fitted rising edge and error*/
+    };
+
+    /**
+    @brief Variables used during template fit minimization
+    */
+    struct MinimizationSums {
+      double S1;
+      double Sx, Sxx;
+      float Sy, Sxy, Syy;
     };
 
     /**
      *@brief Class to perform template fit on TOP waveform data
+     *Minimzation method is described here http://wwwa1.kph.uni-mainz.de/Vorlesungen/SS11/Statistik/
      *@author Tobias Weber
      */
     class TOPTemplateFitter {
@@ -50,30 +74,93 @@ namespace Belle2 {
 
       /**
       @brief Returns fit chi square
-      @return fit chi square*/
+      @return fit chi square
+      */
       double getChisq() const {return m_chisq;}
 
       /**
       @brief Returns fitted values with errors
-      @return fitted values with errors*/
+      @return fitted values with errors
+      */
       const FitResult& getFitResult() const {return m_result;}
 
       /**
-      @brief Performs the template fit
+      @brief Returns the template parameters
+      @return template parameters
+      */
+      static TemplateParameters& GetTemplateParameters() {return s_templateParameters;}
+
+      /**
+      @brief Returns the total number of template samples
+      @return total number of template samples
+      */
+      static int GetTemplateSamples() {return s_totalTemplateSamples;}
+
+      /**
+      @brief Returns the template resolution
+      @return template resolution
+      */
+      static int GetTemplateResolution() {return s_templateResolution;}
+
+      /**
+      @brief Sets the template parameters
+      @param template Parameters
+      */
+      static void SetTemplateParameters(const TemplateParameters& params);
+
+      /**
+      @brief Set the total number of template samples
+      @param total number of template samples
+      */
+      static void SetTemplateSamples(int nSamples);
+
+      /**
+      @brief Set the template resolution
+      @param template resolution
+      */
+      static void SetTemplateResolution(int resolution);
+
+      /**
+      @brief Performs the template fit in sample space
       @param initial guess for rising edge position from CFD
       @param range of template fit*/
       void PerformTemplateFit(const double risingEdgeStart,
                               const double fitRange);
 
     private:
+
+      /**
+      @brief crystal ball function
+      @param sample position
+      @return value of crystall ball function
+      */
+      static double Crystalball(const double x);
+
+      /**
+      @brief Calculate the value of the Template Function at given sample.
+      @param sample position. Can be fractional number
+      @return value of template function
+      */
+      static double CalculateTemplate(const double x);
+
+      /**
+      @brief Intializes the template fit using default values
+      */
+      static void InitializeTemplateFit();
+
       const TOPRawWaveform m_wf; /** <raw sampled waveforms */
       const TOPSampleTimes m_sampleTimes; /** <provides timing correction */
       double m_averageRMS; /**average RMS of waveform samples, no database for this */
 
+      static int s_totalTemplateSamples;/**< number of samples used for template*/
+      static int s_templateResolution;/**< resolution of template with respect to normal sample spacing*/
+      static TemplateParameters s_templateParameters;/**< parameters used for the template calculation*/
+      static std::vector<double> s_templateSamples;/**<precomputed template samples*/
+
       //fit results
       FitResult m_result;/**< fit result from template fit*/
       double m_chisq;/**< chi square value from template fit */
-      int m_ndf;/**< number of degrees of freedom of template fit*/
+
     };
 
   }
--- top/utilities/src/TOPTemplateFitter.cc
+++ top/utilities/src/TOPTemplateFitter.cc
@@ -9,8 +9,15 @@
  **************************************************************************/
 
 #include <top/utilities/TOPTemplateFitter.h>
+#include <framework/logging/Logger.h>
 
+using namespace Belle2;
 using namespace Belle2::TOP;
+
+TemplateParameters TOPTemplateFitter::s_templateParameters;
+int TOPTemplateFitter::s_totalTemplateSamples = 64;
+int TOPTemplateFitter::s_templateResolution = 4;
+std::vector<double> TOPTemplateFitter::s_templateSamples;
 
 TOPTemplateFitter::TOPTemplateFitter(const TOPRawWaveform& wf,
                                      const TOPSampleTimes& sampleTimes,
@@ -18,10 +25,89 @@ TOPTemplateFitter::TOPTemplateFitter(const TOPRawWaveform& wf,
   : m_wf(wf), m_sampleTimes(sampleTimes), m_averageRMS(averageRMS)
 {
   m_chisq = -1.;
+  InitializeTemplateFit();
+}
+
+void TOPTemplateFitter::SetTemplateParameters(const TemplateParameters& params)
+{
+  s_templateParameters = params;
+  InitializeTemplateFit();
+}
+
+void TOPTemplateFitter::SetTemplateSamples(int nSamples)
+{
+  s_totalTemplateSamples = nSamples;
+  InitializeTemplateFit();
+}
+
+void TOPTemplateFitter::SetTemplateResolution(int resolution)
+{
+  s_templateResolution = resolution;
+  InitializeTemplateFit();
+}
+
+double TOPTemplateFitter::Crystalball(const double x)
+{
+  double value, t, absAlpha;
+  double a, b;
+
+  //crystalball function from roofit
+  t = (x - s_templateParameters.mean) / s_templateParameters.sigma;
+  if (s_templateParameters.alpha < 0) t = -t;
+  if (s_templateParameters.alpha > 0) absAlpha = s_templateParameters.alpha;
+  else absAlpha = -s_templateParameters.alpha;
+
+  if (t >= -absAlpha) {
+    value = exp(-0.5 * t * t) * s_templateParameters.amplitude;
+  } else {
+    a = powf(s_templateParameters.n / absAlpha, s_templateParameters.n) * exp(-0.5 * absAlpha * absAlpha);
+    b = s_templateParameters.n / absAlpha - absAlpha;
+    value = a / powf(b - t, s_templateParameters.n) * s_templateParameters.amplitude;
+  }
+  return value;
+}
+
+double TOPTemplateFitter::CalculateTemplate(const double x)
+{
+  //mimic integration of IRSX ASIC by integrating one bin around x value
+  const int integrationSteps = 32;
+  const double stepSize = 1. / integrationSteps;
+  double value = 0;
+  for (int i = 0; i < integrationSteps; ++i) value += Crystalball(x - 0.5 + stepSize * i);
+  return value * stepSize;
+}
+
+void TOPTemplateFitter::InitializeTemplateFit()
+{
+  s_templateSamples.clear();
+  if (s_templateParameters.mean < 0 || s_templateParameters.mean > s_totalTemplateSamples) {
+    B2FATAL("mean of template function invalid!");
+  }
+  for (double x = 0; x < s_totalTemplateSamples; x += 1. / s_templateResolution) {
+    s_templateSamples.push_back(CalculateTemplate(x));
+  }
 }
 
 void TOPTemplateFitter::PerformTemplateFit(const double risingEdgeStart,
                                            const double fitRange)
 {
+  //access time base correction and calculate correction to waveform sample number
 
+  //perform template fit
+}
+
+/**
+@brief  Compute the minimized parameters and chi square value
+@param minimization sums for chisq calculation
+@param minimized parameters
+@return chi square
+*/
+inline double ComputeMinimizedParametersAndChisq(const MinimizationSums& sums, FitResult& result)
+{
+  const double determinant = sums.S1 * sums.Sxx - sums.Sx * sums.Sx;
+  result.amplitudeScaling = (-sums.Sx * sums.Sy + sums.S1 * sums.Sxy) / determinant;
+  result.backgroundOffset = (sums.Sxx * sums.Sy - sums.Sx * sums.Sxy) / determinant;
+  result.amplitudeScalingErr = sums.S1 / determinant;
+  result.backgroundOffsetError = sums.Sxx / determinant;
+  return sums.Syy - result.backgroundOffset * sums.Sy - result.amplitudeScaling * sums.Sxy;
 }

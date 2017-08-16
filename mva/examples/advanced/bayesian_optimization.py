6bad8e4cc4f48a04cefd75f0d123ff25e41e5a51
Markus Prim <markus.prim@kit.edu>
2017-08-16 09:10:42 +0200

    Update the bayesian optimization example to use scikit-optimize.

--- mva/examples/advanced/bayesian_optimization.py
+++ mva/examples/advanced/bayesian_optimization.py
@@ -1,28 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Thomas Keck 2017
-# Dennis Weyland
+# Markus Prim 2017
+# Thomas Keck
+# Dennis Weland
 
-# A example very similar to grid_search.py except using Bayesian optimization instead of grid search.
-# GPy and GPyOpt has to be installed.
+# A simple example to use bayesian optimiation for the hyperparameters of a FastBDT.
+# The package used in this example is https://github.com/scikit-optimize
+# and can be installed with
+# pip3 install scikit-optimize
+
+# Training and test sample can be downloaded:
+# http://ekpwww.ekp.kit.edu/~tkeck/train.root
+# http://ekpwww.ekp.kit.edu/~tkeck/test.root
+
 
 import basf2_mva
 import basf2_mva_util
-import multiprocessing
-import itertools
-import pickle
-import numpy as np
+import skopt
+import matplotlib.pyplot as plt
 
-import GPy
-import GPyOpt
+
+def f(x):
+    """Returns the figure of merit for the optimization.
+    The functions trains the classifier with the given hyperparamters on the training sample and
+    calculates the AUC on the independet test sample.
+    """
+    nTrees = int(x[0])
+    depth = int(x[1])
+    method = basf2_mva_util.Method(general_options.m_identifier)
+    options = basf2_mva.FastBDTOptions()
+    options.m_nTrees = nTrees
+    options.m_nLevels = depth
+    m = method.train_teacher(training_data, general_options.m_treename, specific_options=options)
+    p, t = m.apply_expert(test_data, general_options.m_treename)
+    return -basf2_mva_util.calculate_roc_auc(p, t)
+
 
 if __name__ == "__main__":
 
     training_data = basf2_mva.vector("train.root")
     test_data = basf2_mva.vector("test.root")
 
-    # Train model with default parameters
     general_options = basf2_mva.GeneralOptions()
     general_options.m_datafiles = training_data
     general_options.m_treename = "tree"
@@ -33,43 +52,19 @@ if __name__ == "__main__":
     fastbdt_options = basf2_mva.FastBDTOptions()
     basf2_mva.teacher(general_options, fastbdt_options)
 
-    # Load the model and train it again searching for the best hyperparameters
-    def grid_search(hyperparameters):
-        nTrees, depth = int(hyperparameters[0, 0]), int(hyperparameters[0, 1])
-        method = basf2_mva_util.Method(general_options.m_identifier)
-        options = basf2_mva.FastBDTOptions()
-        options.m_nTrees = nTrees
-        options.m_nLevels = depth
-        m = method.train_teacher(training_data, general_options.m_treename, specific_options=options)
-        p, t = m.apply_expert(test_data, general_options.m_treename)
-        return -basf2_mva_util.calculate_roc_auc(p, t)
+    # Start optimization
+    res = skopt.gp_minimize(f,  # the function to minimize
+                            [(10, 1000), (2, 6)],  # the bounds on each dimension of x
+                            x0=[10, 2],  # initial guess
+                            n_calls=20)  # number of evaluations of f
+    # Give some results
+    print(res)
+    skopt.plots.plot_convergence(res)
+    plt.savefig('convergence.png')
+    skopt.plots.plot_evaluations(res)
+    plt.savefig('evaluations.png')
+    skopt.plots.plot_objective(res)
+    plt.savefig('objective.png')
 
-    bounds = [{'name': 'nTrees', 'type': 'continuous', 'domain': (10, 1000)},
-              {'name': 'nLevels', 'type': 'discrete', 'domain': (2, 4, 5, 6)}]
-
-    initial_guess = np.array([[200, 3]])
-    initial_guess_results = np.array([[grid_search(initial_guess)]])
-
-    num_parallel_processes = 5
-    num_cycles = 6
-    myBopt = GPyOpt.methods.BayesianOptimization(f=grid_search,
-                                                 domain=bounds,
-                                                 acquisition_type='MPI',
-                                                 exact_feval=False,
-                                                 batch_size=num_parallel_processes,
-                                                 num_cores=num_parallel_processes,
-                                                 evaluator_type='local_penalization',
-                                                 initial_design_numdata=num_parallel_processes)
-
-    # If you want to give the data in the constructor the number of initial guesses has to be >= initial_design_numdata
-    myBopt.X = np.append(myBopt.X, initial_guess, axis=0)
-    myBopt.Y = np.append(myBopt.Y, initial_guess_results, axis=0)
-
-    for cycle in range(num_cycles):
-        # run parallel optimization
-        myBopt.run_optimization(1, max_time=np.inf, verbosity=False)
-        # save some pics
-        myBopt.plot_convergence('convergence_' + str(cycle) + '.png')
-        myBopt.plot_acquisition('acquisition_' + str(cycle) + '.png')  # works only for 1D and 2D problems
-        # save all points for continuing at a later time
-        pickle.dump({'X': myBopt.X, 'Y': myBopt.Y}, open('opt.p', 'wb'))
+    # Store result of optimization
+    skopt.dump(res, 'opt-result.pkl')

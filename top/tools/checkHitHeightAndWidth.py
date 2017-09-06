8d0274ddbc0e4fd5ba7086e72f2c9af02da5e080
Umberto Tamponi <tamponi@to.infn.it>
2017-09-06 23:17:37 +0900

    Basic python module to study the width of the TOPDigits, both as function of the amplitude and as function of the sample in which it occurs

--- top/tools/checkHitHeightAndWidth.py
+++ top/tools/checkHitHeightAndWidth.py
@@ -3,12 +3,17 @@
 
 from basf2 import *
 import sys
+import glob
 from ROOT import Belle2
 from ROOT import TH2F, TFile
 
+#
 # ------------------------------------------------------------------------
-# example of making histograms with the width and amplidute of all the topdigits
-# needs as input the file produced by top/tools/simLaserCalibration.py
+# Simple module to study the width and amplitude of all the topdigits
+# Umberto Tamponi (tamponi@to.infn.it)
+#
+# usage: basf2 checkHitHeightAndWidth.py run_number output_name.root
+#        the run number accepts wildcards
 # ------------------------------------------------------------------------
 
 
@@ -18,7 +23,19 @@ class Histogrammer(Module):
 
     #: 2D histograms
     hist = [TH2F('WidthVSAmplitude_Slot_' + str(k + 1), 'With VS amplidute of the Digits in slot ' + str(k + 1),
-                 2000, 0., 2000, 80, 0., 20) for k in range(15)]
+                 2000, 0., 2000, 80, 0., 20) for k in range(16)]
+    histSampling = [[TH2F('WidthVSample_Slot_' + str(k + 1) + '_Channel_' + str(j),
+                          'With VS amplidute of the Digits in slot ' + str(k + 1) + ' channel ' + str(j),
+                          256,
+                          0.,
+                          256,
+                          100,
+                          0.,
+                          20) for k in range(16)] for j in range(512)]
+    outname = 'calpulseCheck.root'
+
+    def setOutputName(self, outputname):
+        self.outname = outputname
 
     def event(self):
         ''' Event processor: fill histograms '''
@@ -26,34 +43,37 @@ class Histogrammer(Module):
         digits = Belle2.PyStoreArray('TOPDigits')
         for digit in digits:
             quality = digit.getHitQuality()
-            if quality == 4:
-                slotID = digit.getModuleID()
-                hwchan = digit.getChannel()
-                self.hist[slotID - 1].Fill(digit.getPulseHeight(), digit.getPulseWidth())
+            slotID = digit.getModuleID()
+            hwchan = digit.getChannel()
+            self.hist[slotID - 1].Fill(digit.getPulseHeight(), digit.getPulseWidth())
+            self.histSampling[hwchan][slotID - 1].Fill(digit.getModulo256Sample(), digit.getPulseWidth())
 
     def terminate(self):
         ''' Write histograms to file '''
 
-        tfile = TFile('calpulseCheck.root', 'recreate')
-        for k in range(9):
+        tfile = TFile(self.outname, 'recreate')
+        for k in range(16):
+            self.hist[k].GetXaxis().SetTitle("TOPDigit amplitude [ADC counts]")
+            self.hist[k].GetYaxis().SetTitle("TOPDigit width [ns]")
             self.hist[k].Write()
-        self.propTime.Write()
+        for k in range(16):
+            for j in range(512):
+                self.histSampling[j][k].GetXaxis().SetTitle("TOPDigit sample%256")
+                self.histSampling[j][k].GetYaxis().SetTitle("TOPDigit width [ns]")
+                self.histSampling[j][k].Write()
+
         tfile.Close()
 
 
 argvs = sys.argv
-if len(argvs) is not 5:
-    print('usage: basf2', argvs[0],
-          '-i <file_sroot> (pocket|local) <slot> <channel> <output_dir>')
-    sys.exit()
 
-datatype = argvs[1]      # data type (pocket, local)
-slot = int(argvs[2])     # slot number (1-16)
-channel = int(argvs[3])  # calibration channel (0-7)
-outdir = argvs[4]        # output directory path
+print('usage: basf2', argvs[0], 'runNumber outfileName')
 
-print('data type:', datatype, ' slot:', slot, ' calibration channel:', channel,
-      ' output to:', outdir)
+runnumber = argvs[1]   # run number. Wildcards should work
+outfile = argvs[2]     # output name
+
+
+files = [f for f in glob.glob('/ghi/fs01/belle2/bdata/Data/sRaw/e0001/r0' + str(runnumber) + '/sub00/*.sroot')]
 
 # Suppress messages and warnings during processing
 set_log_level(LogLevel.ERROR)
@@ -63,13 +83,17 @@ main = create_path()
 
 # input
 roinput = register_module('SeqRootInput')
+roinput.param('inputFileNames', files)
 main.add_module(roinput)
 
+#
 # conversion from RawCOPPER or RawDataBlock to RawDetector objects
-if datatype == 'pocket':
-    print('pocket DAQ data assumed')
-    converter = register_module('Convert2RawDet')
-    main.add_module(converter)
+# Pocket DAQ data not supported yet
+#
+# if datatype == 'pocket':
+#    print('pocket DAQ data assumed')
+#    converter = register_module('Convert2RawDet')
+#    main.add_module(converter)
 
 # geometry parameters
 gearbox = register_module('Gearbox')
@@ -96,7 +120,7 @@ converter.param('useSampleTimeCalibration', False)
 converter.param('useChannelT0Calibration', False)
 converter.param('useModuleT0Calibration', False)
 converter.param('useCommonT0Calibration', False)
-converter.param('calibrationChannel', channel)  # if set, cal pulses will be flagged
+# converter.param('calibrationChannel', channel)  # if set, cal pulses will be flagged
 converter.param('calpulseHeightMin', 450)  # in [ADC counts]
 converter.param('calpulseHeightMax', 900)  # in [ADC counts]
 converter.param('calpulseWidthMin', 2.0)  # in [ns]
@@ -105,7 +129,9 @@ converter.param('lookBackWindows', 29)  # in number of windows
 main.add_module(converter)
 
 # Histogrammer
-main.add_module(Histogrammer())
+histogramModule = Histogrammer()
+histogramModule.setOutputName(outfile)
+main.add_module(histogramModule)
 
 # Show progress of processing
 progress = register_module('Progress')

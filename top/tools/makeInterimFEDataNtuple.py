f0acf2bdc48dd2c4a1c6682edf2ab8705125cfeb
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2017-09-08 10:19:29 +0200

    Revert "update of TOPInterimFENtuple and relevant tools (makeInterimFENtuple and plotInterimFENtuple)"
    This reverts commit 63709fc73fecc71296ee5449daa307d70db31128.

--- top/tools/makeInterimFEDataNtuple.py
+++ top/tools/makeInterimFEDataNtuple.py
@@ -8,77 +8,25 @@
 
 from basf2 import *
 import sys
-import argparse
 import re
 from plotInterimFEDataNtuple import plotInterimFEDataNtupleSummary
+from plotInterimFEDataNtuple import plotInterimFEDataNtupleAdvanced
 
-parser = argparse.ArgumentParser(description="Create ntuple file and plots from interimFE data")
-parser.add_argument("inputFile", nargs='?', default="NoInputFile",
-                    help="input sroot file name")
-parser.add_argument("--outputFile", default="NoOutputFile",
-                    help="output root file name")
-parser.add_argument("--NoOfflineFE", action="store_true", default=False,
-                    help="Use only online FE hits, and do not use waveform data")
-parser.add_argument("--SaveWaveform", action="store_true", default=False,
-                    help="Save waveform data in the output ntuple file")
-parser.add_argument("--GlobalDAQ", action="store_true", default=False,
-                    help="Force to assume global DAQ data.")
-parser.add_argument("--PocketDAQ", action="store_true", default=False,
-                    help="Force to assume global DAQ data.")
-parser.add_argument("--SkipPlot", action="store_true", default=False,
-                    help="Skip making summary plot.")
-args = parser.parse_args()
-
-if args.inputFile == "NoInputFile":
-    print("Create a flat ntuple file from sroot data file(s) taken with interimFE firmware and plots for quick data quality check.")
+args = sys.argv
+if len(args) < 2:
     print("usage:")
-    print("basf2 makeInterimFEDataNtuple.py (input_filename.sroot) [--arg --outputFile output_ntuple.root]")
-    print("                                  [--arg --NoOfflineFE] [--arg --SaveWaveform]")
-    print("                                  [--arg --GlobalDAQ] [--arg --PocketDAQ]")
-    print("                                  [--arg --SkipPlot]")
-    print("*Output file name is automatically set as folows if it is not specified:")
-    print("  runXXXXXX_slotYY_ntuple.root (in case of local run)")
-    print(" or top.XXXXX.YYYYYY_ntuple.root (in case of global run)")
-    print("*Option \"--NoOfflineFE\"  : disable offline FE from waveform data.")
-    print("                             Calculation of reference timing is based on hit in calibration channel.")
-    print("*       \"--SaveWaveform\" : save waveform data in the output ntuple file")
-    print("*       \"--GlobalDAQ\"    : force to assume global DAQ data as an input file")
-    print("*       \"--PocketDAQ\"    : force to assume pocketDAQ data as an input file")
-    print("*       \"--SkipPlot\"     : only processing sroot data file, do not create summary plot")
+    print(args[0] + " (input_filename.sroot) [output_file.root]")
+    print("*if output filename is not given, output file name is automatically set as folows :")
+    print("  runXXXXXX_slotYY_ana.root")
     sys.exit()
 
-input_file = args.inputFile
-IsGlobalDAQ = False
-IsOfflineFEDisabled = args.NoOfflineFE
-IsGlobalDAQForced = args.GlobalDAQ
-IsPocketDAQForced = args.PocketDAQ
-if re.search(r"run[0-9]*_slot[0-1][0-9]", input_file):
-    output_root = re.search(r"run[0-9]*_slot[0-1][0-9]", input_file).group() + "_ntuple.root"
-elif re.search(r"(top|cosmic|cdc)\.[0-9][0-9][0-9][0-9]\.[0-9][0-9][0-9][0-9][0-9][0-9]", input_file):
-    IsGlobalDAQ = True
-    output_root = re.search(
-        r"(top|cosmic|cdc)\.[0-9][0-9][0-9][0-9]\.[0-9][0-9][0-9][0-9][0-9][0-9]",
-        input_file).group() + "_ntuple.root"
+input_file = args[1]
+matchtest = re.search(r"run[0-9]*_slot[0-1][0-9]", input_file).group()
+if matchtest:
+    output_root = matchtest + "_ana.root"
 else:
-    output_root = input + "_ntuple.root"
-
-if args.outputFile != "NoOutputFile":
-    output_root = args.outputFile
-
-if IsGlobalDAQForced and (not IsPocketDAQForced):
-    IsGlobalDAQ = True
-elif (not IsGlobalDAQForced) and IsPocketDAQForced:
-    IsGlobalDAQ = False
-elif IsGlobalDAQForced and IsPocketDAQForced:
-    print("ERROR : both of --GlobalDAQ or --PocketDAQ can not be given.")
-    sys.exit()
-
+    output_root = input_file + "_ana.root"
 print(input_file + " --> " + output_root)
-print("Is global DAQ? : " + str(IsGlobalDAQ))
-print("OfflineFE : " + str(not IsOfflineFEDisabled))
-print("Save waveform? : " + str(args.SaveWaveform))
-print()
-print("start process...")
 
 # Create path
 main = create_path()
@@ -94,9 +42,8 @@ histoman.param('histoFileName', output_root)
 main.add_module(histoman)
 
 # conversion from RawCOPPER or RawDataBlock to RawDetector objects
-if not IsGlobalDAQ:
-    converter = register_module('Convert2RawDet')
-    main.add_module(converter)
+converter = register_module('Convert2RawDet')
+main.add_module(converter)
 
 # geometry parameters
 gearbox = register_module('Gearbox')
@@ -113,34 +60,16 @@ unpack.param('swapBytes', True)
 unpack.param('dataFormat', 0x0301)
 main.add_module(unpack)
 
-# Add multiple hits by running feature extraction offline
-if not IsOfflineFEDisabled:
-    featureExtractor = register_module('TOPWaveformFeatureExtractor')
-    main.add_module(featureExtractor)
-
 # Convert to TOPDigits
 converter = register_module('TOPRawDigitConverter')
 converter.param('useSampleTimeCalibration', False)
 converter.param('useChannelT0Calibration', False)
 converter.param('useModuleT0Calibration', False)
 converter.param('useCommonT0Calibration', False)
-converter.param('calibrationChannel', 0)  # if set, cal pulses will be flagged
-converter.param('calpulseHeightMin', 450)  # in [ADC counts]
-converter.param('calpulseHeightMax', 900)  # in [ADC counts]
-converter.param('calpulseWidthMin', 2.0)  # in [ns]
-converter.param('calpulseWidthMax', 6.0)  # in [ns]
 main.add_module(converter)
 
-ntuple = register_module('MY_TOPInterimFENtupleDev')
-ntuple.param('saveWaveform', (args.SaveWaveform))
-ntuple.param('useDoublePulse', (not IsOfflineFEDisabled))
-ntuple.param('calibrationChannel', 0)
-ntuple.param('averageSamplingRate', 2.71394)
-ntuple.param('minHeightFirstCalPulse', 600)  # in [ADC counts]
-ntuple.param('minHeightSecondCalPulse', 450)  # in [ADC counts]
-ntuple.param('nominalDeltaT', 21.85)  # in [ns]
-ntuple.param('nominalDeltaTRange', 2)  # in [ns]
-main.add_module(ntuple)
+ana = register_module('TOPInterimFENtuple')
+main.add_module(ana)
 
 # Print progress
 progress = register_module('Progress')
@@ -152,6 +81,5 @@ process(main)
 # Print statistics
 print(statistics)
 
-if not args.SkipPlot:
-    plotInterimFEDataNtupleSummary(output_root, 2, IsOfflineFEDisabled)
-    # plotInterimFEDataNtupleAdvanced(output_root)
+plotInterimFEDataNtupleSummary(output_root)
+plotInterimFEDataNtupleAdvanced(output_root)

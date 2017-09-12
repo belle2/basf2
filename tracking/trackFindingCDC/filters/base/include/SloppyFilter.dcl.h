9ef094a8486a8ef862b02032d349368250875d69
Oliver Frost <oliver.frost@desy.de>
2017-09-12 22:53:09 +0200

    Compile time: Factored out a sloppy filter mixin.

---
+++ tracking/trackFindingCDC/filters/base/include/SloppyFilter.dcl.h
@@ -0,0 +1,42 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2017 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+#include <tracking/trackFindingCDC/numerics/Weight.h>
+
+#include <string>
+namespace Belle2 {
+  namespace TrackFindingCDC {
+    class ParamList;
+
+    template<class AFilter>
+    class Sloppy : public AFilter {
+
+    private:
+      /// The parent class
+      using Super = AFilter;
+
+    public:
+      /// Inherit constructors
+      using Super::Super;
+
+      /// Expose the set of parameters of the filter to the module parameter list.
+      void exposeParams(ParamList* paramList, const std::string& prefix) override;
+
+    public:
+      /// Reject an item if the truth variable is 0 or in some fractions of the events, else accept it.
+      Weight operator()(const typename Super::Object& object) override;
+
+    private:
+      /// Parameter : The prescaling of the false items returning true.
+      unsigned int m_param_sloppinessFactor = 10;
+    };
+  }
+}

6c9ea87f6f02a781b66d08b9e61d93c254bc450b
Oliver Frost <oliver.frost@desy.de>
2017-09-12 22:20:01 +0200

    Compile time: Split RandomFilter into declaration and implementation.

--- tracking/trackFindingCDC/filters/base/include/RandomFilter.icc.h
+++ tracking/trackFindingCDC/filters/base/include/RandomFilter.icc.h
@@ -9,7 +9,17 @@
  **************************************************************************/
 #pragma once
 
-// This header file is deprecated
-// Instead use one of the following headers depending on the *minimal* needs of your use.
 #include <tracking/trackFindingCDC/filters/base/RandomFilter.dcl.h>
-#include <tracking/trackFindingCDC/filters/base/RandomFilter.icc.h>
+
+#include <TRandom.h>
+
+namespace Belle2 {
+  namespace TrackFindingCDC {
+
+    template <class AFilter>
+    Weight RandomFilter<AFilter>::operator()(const Object& obj __attribute__((unused)))
+    {
+      return gRandom->Rndm();
+    }
+  }
+}

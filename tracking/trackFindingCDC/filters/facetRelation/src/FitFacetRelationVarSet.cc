78b1fef01e9709946a33e40b7b558563dc7813fd
Oliver Frost <oliver.frost@desy.de>
2017-09-14 20:08:15 +0200

    Compile time: Fixing includes from fitting/

--- tracking/trackFindingCDC/filters/facetRelation/src/FitFacetRelationVarSet.cc
+++ tracking/trackFindingCDC/filters/facetRelation/src/FitFacetRelationVarSet.cc
@@ -11,7 +11,14 @@
 
 #include <tracking/trackFindingCDC/fitting/FacetFitter.h>
 #include <tracking/trackFindingCDC/fitting/CDCKarimakiFitter.h>
+#include <tracking/trackFindingCDC/fitting/CDCObservations2D.h>
+
+#include <tracking/trackFindingCDC/eventdata/hits/CDCFacet.h>
+
 #include <tracking/trackFindingCDC/geometry/UncertainParameterLine2D.h>
+#include <tracking/trackFindingCDC/geometry/LineParameters.h>
+#include <tracking/trackFindingCDC/geometry/Vector2D.h>
+
 #include <tracking/trackFindingCDC/numerics/Angle.h>
 
 #include <Eigen/Dense>

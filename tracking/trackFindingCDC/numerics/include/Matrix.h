0819f81d92958f0bd1e9837c8735e716a06619c0
Oliver Frost <oliver.frost@desy.de>
2017-09-16 14:19:05 +0200

    Compile time: Adding header file to define the currently used matrix file.

---
+++ tracking/trackFindingCDC/numerics/include/Matrix.h
@@ -0,0 +1,18 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Oliver Frost                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+#include <Eigen/Core>
+namespace Belle2 {
+  namespace TrackFindingCDC {
+    template <class T, int M, int N>
+    using Matrix = Eigen::Matrix<T, M, N>;
+  }
+}

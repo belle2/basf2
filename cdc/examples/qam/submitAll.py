92315bcef090bd30d97458d47881a28c7609f7a6
Makoto Uchida <uchida@phys.titech.ac.jp>
2017-09-19 17:47:45 +0900

    Add first version of QAM scripts for CDC performance.

--- cdc/examples/qam/submitAll.py
+++ cdc/examples/qam/submitAll.py
@@ -4,6 +4,12 @@
 import glob
 import subprocess
 from functools import reduce
+import argparse
+
+parser.add_argument('mode', help='dqm or qam')
+args = parser.parse_args()
+
+mode = args.mode
 
 
 def getRunList(dir):
@@ -26,8 +32,16 @@ exp = 1
 runs = getRunList(dir)
 
 for run in runs:
-    logfile = 'dqm.cosmic.{0:0>4}.{1:0>5}.log'.format(exp, run)
-    command = ' '.join(['basf2 CDCHistMaker.py', str(exp), str(run), '>&', logfile])
+    if mode is 'dqm':
+        logfile = 'dqm.cosmic.{0:0>4}.{1:0>5}.log'.format(exp, run)
+        command = ' '.join(['basf2 CDCHistMaker.py', str(exp), str(run), '>&', logfile])
+    elif mode is 'qam':
+        logfile = 'qam.cosmic.{0:0>4}.{1:0>5}.log'.format(exp, run)
+        command = ' '.join(['basf2 QAMHistMaker.py', str(exp), str(run), '>&', logfile])
+    else:
+        print('Undefined mode')
+        exit(1)
+
     # Check submitting job list
     # print('bsub -q s \"' + command + ' \"')
     # Submit jobs.

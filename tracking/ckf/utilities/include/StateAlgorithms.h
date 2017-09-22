e83288845e8aab7230ac5e68900fd7a606b9be8f
Nils Braun <nils.braun@desy.de>
2017-09-22 14:38:06 +0200

    Better comments

--- tracking/ckf/utilities/include/StateAlgorithms.h
+++ tracking/ckf/utilities/include/StateAlgorithms.h
@@ -17,28 +17,29 @@
 #include <tracking/trackFindingCDC/utilities/EnableIf.h>
 
 namespace Belle2 {
-  /// Shortcut to make compilation dependent on state content for space point-like states
+  /// Shortcut to make compilation dependent on state content for space point-like states (SFINAE)
   template<class AState>
   using EnableIfSpacePoint =
     TrackFindingCDC::EnableIf<std::is_same<decltype(std::declval<AState>().getHit()), const SpacePoint*>::value>;
 
-  /// Shortcut to make compilation dependent on state content for cdc wire hit-like states
+  /// Shortcut to make compilation dependent on state content for cdc wire hit-like states (SFINAE)
   template<class AState>
   using EnableIfCDCWireHit =
     TrackFindingCDC::EnableIf<std::is_same<decltype(std::declval<AState>().getHit()), const TrackFindingCDC::CDCRLWireHit*>::value>;
 
+  /// Functor for extracting the geometrical layer of a state
   struct GeometryLayerExtractor {
     /// Marker function for the isFunctor test
     operator TrackFindingCDC::FunctorTag();
 
-    /// Calculate the layer this state is located on from a space point.
+    /// Calculate the layer this state is located on from a space point. (SFINAE)
     template<class AState>
     unsigned int operator()(const AState& stateObject, EnableIfSpacePoint<AState>* = 0)
     {
       return static_cast<unsigned int>((static_cast<double>(stateObject.getNumber()) / 2) + 1);
     }
 
-    /// Calculate the layer this state is located on for wire hits.
+    /// Calculate the layer this state is located on for wire hits. (SFINAE)
     template<class AState>
     unsigned int operator()(const AState& stateObject, EnableIfCDCWireHit<AState>* = 0)
     {
@@ -46,35 +47,23 @@ namespace Belle2 {
     }
   };
 
+  /// Functor for returning true, if the state is on an overlap layer
   struct OverlapExtractor {
     /// Marker function for the isFunctor test
     operator TrackFindingCDC::FunctorTag();
 
-    /// Check if this state should describe an overlap hit.
+    /// Check if this state should describe an overlap hit. (SFINAE)
     template<class AState>
     bool operator()(const AState& stateObject, EnableIfSpacePoint<AState>* = 0)
     {
       return stateObject.getNumber() % 2 == 0;
     }
 
-    /// Check if this state should describe an overlap hit. Always false for CDC hits
+    /// Check if this state should describe an overlap hit. Always false for CDC hits (SFINAE)
     template<class AState>
     bool operator()(const AState& stateObject __attribute__((unused)), EnableIfCDCWireHit<AState>* = 0)
     {
       return false;
-    }
-  };
-
-  /// Helper functor for extracting the hit
-  struct HitGetter {
-    /// Marker function for the isFunctor test
-    operator TrackFindingCDC::FunctorTag();
-
-    /// from a general state
-    template <class AState>
-    auto operator()(const AState& state) const
-    {
-      return state->getHit();
     }
   };
 
@@ -83,7 +72,7 @@ namespace Belle2 {
     /// Marker function for the isFunctor test
     operator TrackFindingCDC::FunctorTag();
 
-    /// ... from a space point
+    /// ... from a space point (SFINAE)
     template<class AState>
     auto operator()(const AState* state, EnableIfSpacePoint<AState>* = 0) const
     {
@@ -95,7 +84,7 @@ namespace Belle2 {
       }
     }
 
-    /// ... from a wire hit
+    /// ... from a wire hit (SFINAE)
     template<class AState>
     auto operator()(const AState* state, EnableIfCDCWireHit<AState>* = 0) const
     {

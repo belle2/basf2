e8b0fd6debda8e76d48f50554270369828bfbc3d
Oliver Frost <oliver.frost@desy.de>
2017-09-25 18:27:55 +0200

    Dump leftover comment

--- tracking/trackFindingCDC/findlets/generic/tests/TreeTraversal.test.cc
+++ tracking/trackFindingCDC/findlets/generic/tests/TreeTraversal.test.cc
@@ -1,18 +1,12 @@
 /**************************************************************************
  * BASF2 (Belle Analysis Framework 2)                                     *
- * Copyright(C) 2014 - Belle II Collaboration                             *
+ * Copyright(C) 2017 - Belle II Collaboration                             *
  *                                                                        *
  * Author: The Belle II Collaboration                                     *
  * Contributors: Oliver Frost <oliver.frost@desy.de>                      *
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/
-
-/*
- * This file contains test to check the behaviour of the c++ programming language.
- * Its purpose is mainly to asure the programmer that his assumptions about run time behaviour are
- * correct.
- */
 
 #include <gtest/gtest.h>
 #include <tracking/trackFindingCDC/findlets/generic/TreeTraversal.h>

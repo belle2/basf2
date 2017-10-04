fb9db682ea0cd6ea38316454e141b7acf64e1648
Makoto Uchida <uchida@phys.titech.ac.jp>
2017-10-04 15:03:32 +0900

    Change the default global tag for cdc sim/rec to GT277.

--- cdc/examples/performance/submitAll.py
+++ cdc/examples/performance/submitAll.py
@@ -13,14 +13,25 @@ basf2 submitAll.py
 
 # Experimantl number
 exp = 'e0001'
+
 # Data directory
+# GCR 2017
+dir = '/hsm/belle2/bdata/Data/Raw/' + exp
 # pre GCR period
 # dir = '/ghi/fs01/belle2/bdata/group/detector/CDC/unpacked/'
 # files = glob.glob(dir + exp + '/gcr.cdc.*.root')
-# GCR 2017
-dir = '/hsm/belle2/bdata/Data/Raw/' + exp
+
 # run lists
-runs = [3118, 3119, 3120, 3165, 3169, 3170, 3173]
+# runs = [3118, 3119]
+runs = [3944, 3948, 3964, 4011, 3966, 3980, 3996, 4002, 4004, 4005]
+# runs = [3860, 3871, 3883, 3912, 3917, 3927]
+# runs = [3944, 3948, 3964, 4011, 3966,3980,3996,4002,4004,4005,
+#        3860, 3871, 3883, 3912, 3917, 3927, 3965, 3967, 3981]
+# runs = [i for i in range(3112,4118)]
+# print(runs)
+# exit(1)
+
+
 files_nested = [glob.glob(dir + '/*/sub00/cosmic.*' + str(i) + '.HLT1.f*.root') for i in runs]
 # flatting nested list.
 files = reduce(lambda x, y: x + y, files_nested)

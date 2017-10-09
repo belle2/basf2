4460adc4b188520cd0935e6551349dc4d4e6d86a
Nils Braun <nils.braun@desy.de>
2017-10-09 14:45:09 +0200

    Going into the direction of an overlap resolver implementation. Teacher is missing in the moment

--- tracking/ckf/general/findlets/include/OverlapResolver.dcl.h
+++ tracking/ckf/general/findlets/include/OverlapResolver.dcl.h
@@ -10,6 +10,7 @@
 #pragma once
 
 #include <tracking/trackFindingCDC/findlets/base/Findlet.h>
+#include <tracking/trackFindingCDC/numerics/WithWeight.h>
 
 namespace Belle2 {
   class ModuleParamList;
@@ -17,6 +18,9 @@ namespace Belle2 {
   template<class AFilter>
   class OverlapResolver : public TrackFindingCDC::Findlet<typename AFilter::Object> {
   public:
+    /// The object to filter
+    using Object = typename AFilter::Object;
+
     /// The parent class
     using Super = TrackFindingCDC::Findlet<typename AFilter::Object>;
 
@@ -28,10 +32,22 @@ namespace Belle2 {
 
     /**
      */
-    void apply(std::vector<typename AFilter::Object>& results) override;
+    void apply(std::vector<Object>& results) override;
 
   private:
     /// Subfindlet for filtering
     AFilter m_filter;
+
+    // Temporary vectors
+    /// temporary results vector, that will be swapped with the real results vector.
+    std::vector<Object> m_temporaryResults;
+    /// temporary results vector with weights, out of which the overlaps will be build.
+    std::vector<TrackFindingCDC::WithWeight<Object*>> m_resultsWithWeight;
+
+    // Parameters
+    /// Parameter: Enable overlap
+    bool m_param_enableOverlapResolving = true;
+    /// Parameter: In seed mode, use only the best seeds
+    unsigned long m_param_useBestNInSeed = 3;
   };
 }

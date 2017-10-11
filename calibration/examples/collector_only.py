fc159341d2241137ce660582d570277d136377f7
David Dossett <david.dossett@desy.de>
2017-10-11 23:31:56 +1100

    Replacing CaTest with the TestHisto that we've been using. Found a weird segfault that happens at the end of collection. Missed it until now probably because I was calling the collector in a subprocess. I suspect there's some memory allocation/destruction issue in the CalibObjManager. Doesn't actually affect the collection or subsequent python code though.

--- calibration/examples/collector_only.py
+++ calibration/examples/collector_only.py
@@ -1,9 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from basf2 import *
 set_log_level(LogLevel.INFO)
 
-for run in range(1, 5):
-    main = create_path()
-    main.add_module("RootInput", inputFileName="test_data/DST_exp1_run" + str(run) + ".root")
-    main.add_module("CaTest", preScale=0.5, maxEventsPerRun=250)
-    main.add_module('RootOutput', branchNames=['EventMetaData'], outputFileName="CollectorOutput_" + str(run) + ".root")
-    process(main)
+main = create_path()
+main.add_module("RootInput", inputFileNames=["test_data/DST_exp1_run*.root"])
+# HistoManager must be used right after RootInput
+main.add_module('HistoManager', histoFileName="CollectorOutput.root", workDirName=".")
+# Showing off the options usable by all Collector Modules:
+# * preScale is a builtin random prescaling factor. This fraction of events are collected
+# * maxEventsPerRun allows you to stop collecting once this many events have been collected in a run
+# * granularity can be 'run' or 'all'. If it's 'run' the collector objects are separated into different
+#   objects. If it's 'all' then a single object is used for all runs.
+# main.add_module("CaTest", preScale=0.5, maxEventsPerRun=250, granularity="run")
+main.add_module("CaTest")
+# Notice that there is NO RootOutput module required anymore, although you could include it if you wanted
+process(main)
+print(statistics(statistics.TOTAL))
--- calibration/examples/test_newalg.py
+++ calibration/examples/test_newalg.py
@@ -7,7 +7,7 @@ from basf2 import *
 import ROOT
 from ROOT.Belle2 import IntervalOfValidity
 
-set_log_level(LogLevel.DEBUG)
+# set_log_level(LogLevel.DEBUG)
 
 algo = ROOT.Belle2.TestCalibrationAlgorithm()
 # Set the prefix manually if you want to use the algorithm for a specific collector
@@ -17,25 +17,25 @@ algo.setPrefix("TestHisto")
 inputFileNames = ["MyOutputFile_*.root"]
 algo.setInputFileNames(inputFileNames)
 # Python list object of files returned
-d = algo.getInputFileNames()
-for name in d:
-    print("Resolved input file to algorithm:", name)
-
+# d = algo.getInputFileNames()
+# for name in d:
+#    print("Resolved input file to algorithm:", name)
+#
 # Can use python lists/tuples to define the runs whose data you want to use [(exp,run), ...]
 # IoV of the payloads will be calculated from this input list
-print("Result of calibration =", algo.execute([(1, 1), (1, 2)]))
+# print("Result of calibration =", algo.execute([(1, 1), (1, 2)]))
 # localdb isn't updated until you call this
-algo.commit()
+# algo.commit()
 
 # Can run over all collected data and auto define the IoV of the payloads
-print("Result of calibration =", algo.execute())
+# print("Result of calibration =", algo.execute())
 # Can commit multiple times in one process since the payloads are cleared at the start
 # of each execution
-algo.commit()
+# algo.commit()
 
 # Could also define an IoV for your calibrations at the start of execution
-unbound_iov = IntervalOfValidity(0, 0, 1, 1)
-print("Result of calibration =", algo.execute([], 0, unbound_iov))
+iov = IntervalOfValidity.always()
+print("Result of calibration =", algo.execute([], 0, iov))
 # IoV is applied to payloads during execution and gets used during commit
 algo.commit()
 

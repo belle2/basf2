eecca0e322eb2c132a30c63a57e4a022412ebc2a
Nils Braun <nils.braun@desy.de>
2017-10-17 10:22:30 +0200

    Removed the PXD part from the SVD result

--- tracking/ckf/svd/entities/src/CKFToSVDResult.cc
+++ tracking/ckf/svd/entities/src/CKFToSVDResult.cc
@@ -14,7 +14,6 @@
 
 #include <tracking/dataobjects/RecoTrack.h>
 #include <tracking/spacePointCreation/SpacePoint.h>
-#include <pxd/dataobjects/PXDCluster.h>
 #include <svd/dataobjects/SVDCluster.h>
 
 using namespace Belle2;
@@ -27,21 +26,12 @@ CKFToSVDResult::CKFToSVDResult(const std::vector<const CKFToSVDState*>& path) :
 void CKFToSVDResult::addToRecoTrack(RecoTrack& recoTrack) const
 {
   for (const SpacePoint* spacePoint : TrackFindingCDC::reversedRange(getHits())) {
-    const int detID = spacePoint->getType();
     unsigned int sortingParameter = 0;
 
-    if (detID == VXD::SensorInfoBase::PXD) {
-      RelationVector<PXDCluster> relatedClusters = spacePoint->getRelationsTo<PXDCluster>();
-      for (const PXDCluster& cluster : relatedClusters) {
-        recoTrack.addPXDHit(&cluster, sortingParameter, Belle2::RecoHitInformation::c_VXDTrackFinder);
-        sortingParameter++;
-      }
-    } else if (detID == VXD::SensorInfoBase::SVD) {
-      RelationVector<SVDCluster> relatedClusters = spacePoint->getRelationsTo<SVDCluster>();
-      for (const SVDCluster& cluster : relatedClusters) {
-        recoTrack.addSVDHit(&cluster, sortingParameter, Belle2::RecoHitInformation::c_VXDTrackFinder);
-        sortingParameter++;
-      }
+    RelationVector<SVDCluster> relatedClusters = spacePoint->getRelationsTo<SVDCluster>();
+    for (const SVDCluster& cluster : relatedClusters) {
+      recoTrack.addSVDHit(&cluster, sortingParameter, Belle2::RecoHitInformation::c_VXDTrackFinder);
+      sortingParameter++;
     }
   }
 }

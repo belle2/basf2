a2e08ca3fdd9f1f969a8246481879507ca4f9379
Sebastian Racs <sebastian.racs@desy.de>
2017-10-17 18:26:22 +0200

    mc_side_module to inherit from basic module

--- tracking/scripts/tracking/harvesting_validation/mc_side_module.py
+++ tracking/scripts/tracking/harvesting_validation/mc_side_module.py
@@ -4,19 +4,14 @@ import ROOT
 ROOT.gSystem.Load("libtracking")
 from ROOT import Belle2
 
-import math
-import warnings
-
-import numpy as np
+import tracking.harvesting_validation.basic_mc_side_module as basic_mc_side_module
 
 import tracking.validation.utilities as utilities
-
 import tracking.harvest.refiners as refiners
-import tracking.harvest.harvesting as harvesting
 import tracking.harvest.peelers as peelers
 
 
-class MCSideTrackingValidationModule(harvesting.HarvestingModule):
+class MCSideTrackingValidationModule(basic_mc_side_module.BasicMCSideTrackingValidationModule):
 
     """Module to collect matching information about the found particles and to generate
        validation plots and figures of merit on the performance of track finding."""
@@ -30,22 +25,12 @@ class MCSideTrackingValidationModule(harvesting.HarvestingModule):
             mc_reco_tracks_name='MCRecoTracks',
             expert_level=None):
 
-        output_file_name = output_file_name or name + 'TrackingValidation.root'
-
-        super().__init__(foreach=mc_reco_tracks_name,
-                         name=name,
-                         output_file_name=output_file_name,
+        super().__init__(name=name,
                          contact=contact,
+                         output_file_name=output_file_name,
+                         reco_tracks_name=reco_tracks_name,
+                         mc_reco_tracks_name=mc_reco_tracks_name,
                          expert_level=expert_level)
-
-        #: Name of the StoreArray of the tracks from pattern recognition
-        self.reco_tracks_name = reco_tracks_name
-
-        #: Name of the StoreArray of the ideal mc tracks
-        self.mc_reco_tracks_name = mc_reco_tracks_name
-
-        #: Reference to the track match lookup object reading the relation information constructed by the MCMatcherTracksModule
-        self.track_match_look_up = None
 
         #: Set of all detector and hits ids contained in any pr track. Updated each event.
         self.found_det_hit_ids = set()
@@ -58,10 +43,6 @@ class MCSideTrackingValidationModule(harvesting.HarvestingModule):
 
         #: Set of all detector and hits ids contained in background and ghost pr tracks. Updated each event.
         self.fake_det_hit_ids = set()
-
-    def initialize(self):
-        super().initialize()
-        self.track_match_look_up = Belle2.TrackMatchLookUp(self.mc_reco_tracks_name, self.reco_tracks_name)
 
     def prepare(self):
         """Collect some statistics about the pattern recognition tracks used for comparision to the MC tracks
@@ -98,23 +79,13 @@ class MCSideTrackingValidationModule(harvesting.HarvestingModule):
         self.clone_det_hit_ids = clone_det_hit_ids
         self.fake_det_hit_ids = fake_det_hit_ids
 
-    def pick(self, mc_reco_track):
-        return True
-
     def peel(self, mc_reco_track):
-        """Looks at the individual Monte Carlo tracks and store information about them"""
-        track_match_look_up = self.track_match_look_up
-        mc_particles = Belle2.PyStoreArray('MCParticles')
+        crops = super().peel(mc_reco_track=mc_reco_track)
 
-        # Analyse from the Monte Carlo reference side
-        mc_reco_tracks = Belle2.PyStoreArray(self.foreach)
-        multiplicity = mc_reco_tracks.getEntries()
-
-        reco_track = track_match_look_up.getRelatedPRRecoTrack(mc_reco_track)
         mc_particle = track_match_look_up.getRelatedMCParticle(mc_reco_track)
+        reco_track = track_match_look_up.getRelatedPRRecoTrack(mc_reco_track)
         mc_particle_crops = peelers.peel_mc_particle(mc_particle)
         hit_content_crops = peelers.peel_reco_track_hit_content(mc_reco_track)
-        mc_to_pr_match_info_crops = self.peel_mc_to_pr_match_info(mc_reco_track)
 
         # Custom peel function to get single detector hit purities
         subdetector_hit_efficiency_crops = peelers.peel_subdetector_hit_efficiency(mc_reco_track, reco_track)
@@ -127,71 +98,19 @@ class MCSideTrackingValidationModule(harvesting.HarvestingModule):
 
         # Store Array for easier joining
         store_array_crops = peelers.peel_store_array_info(reco_track, key="pr_{part_name}")
-        mc_store_array_crops = peelers.peel_store_array_info(mc_reco_track, key="mc_{part_name}")
 
-        crops = dict(multiplicity=multiplicity,
-                     **mc_to_pr_match_info_crops,
-                     **hit_content_crops,
-                     **mc_particle_crops,
-                     **subdetector_hit_efficiency_crops,
-                     **mc_hit_efficiencies_in_all_pr_tracks_crops,
-                     **event_crops,
-                     **store_array_crops,
-                     **mc_store_array_crops
-                     )
+        crops.update(dict(**hit_content_crops,
+                          **mc_particle_crops,
+                          **subdetector_hit_efficiency_crops,
+                          **mc_hit_efficiencies_in_all_pr_tracks_crops,
+                          **event_crops,
+                          **store_array_crops
+                          ))
 
         return crops
 
-    def peel_mc_to_pr_match_info(self, mc_reco_track):
-        track_match_look_up = self.track_match_look_up
-        return dict(
-            is_matched=track_match_look_up.isMatchedMCRecoTrack(mc_reco_track),
-            is_merged=track_match_look_up.isMergedMCRecoTrack(mc_reco_track),
-            is_missing=track_match_look_up.isMissingMCRecoTrack(mc_reco_track),
-            hit_efficiency=track_match_look_up.getRelatedEfficiency(mc_reco_track),
-        )
-
-    def peel_hit_efficiencies_in_all_pr_tracks(self, mc_reco_track):
-        mc_det_hit_ids = utilities.get_det_hit_ids(mc_reco_track)
-
-        hit_efficiency_in_all_found = utilities.calc_hit_efficiency(self.found_det_hit_ids,
-                                                                    mc_det_hit_ids)
-
-        unfound_hit_efficiency = 1.0 - hit_efficiency_in_all_found
-
-        hit_efficiency_in_all_matched = utilities.calc_hit_efficiency(self.matched_det_hit_ids,
-                                                                      mc_det_hit_ids)
-
-        hit_efficiency_in_all_fake = utilities.calc_hit_efficiency(self.fake_det_hit_ids,
-                                                                   mc_det_hit_ids)
-
-        hit_efficiency_crops = dict(
-            hit_efficiency_in_all_found=hit_efficiency_in_all_found,
-            unfound_hit_efficiency=unfound_hit_efficiency,
-            hit_efficiency_in_all_mached=hit_efficiency_in_all_matched,
-            hit_efficiency_in_all_fake=hit_efficiency_in_all_fake,
-        )
-        return hit_efficiency_crops
-
     # Refiners to be executed on terminate #
     # #################################### #
-
-    # Save a tree of all collected variables in a sub folder
-    save_tree = refiners.save_tree(name="mc_tree", folder_name="mc_tree", above_expert_level=1)
-
-    # Generate the average finding efficiencies and hit efficiencies
-    save_overview_figures_of_merit = refiners.save_fom(
-        name="{module.id}_overview_figures_of_merit",
-        title="Overview figures in {module.title}",
-        aggregation=np.nanmean,
-        key="{part_name}",
-        select={"is_matched": "finding efficiency", "hit_efficiency": "hit efficiency", },
-        filter_on="is_primary",
-        description="""
-finding efficiency - the ratio of matched primary Monte Carlo tracks to all Monte Carlo tracks
-hit efficiency - the ratio of hits picked up by the matched pattern recognition track of primary Monte Carlo tracks
-"""
-    )
 
     # Save a histogram of the hit efficiency
     save_hit_efficiency_histogram = refiners.save_histograms(
@@ -294,15 +213,3 @@ hit efficiency - the ratio of hits picked up by the matched pattern recognition 
         # renaming quantity to name that is more suitable for display
         select=dict(hit_efficiency_in_all_found="total hit efficiency in all reconstructed tracks for missing mc tracks")
     )
-
-
-class ExpertMCSideTrackingValidationModule(MCSideTrackingValidationModule):
-    """Module to collect more matching information about the found particles and to generate
-       validation plots and figures of merit on the performance of track finding. This module
-       gives information on the number of hits etc. """
-
-    def __init__(self, *args, **kwds):
-        """Constructor issuing a deprecation warning"""
-        warnings.warn("ExpertMCSideTrackingValidationModule is depricated for MCSideTrackingValidationModule",
-                      DeprecationWarning)
-        super().__init__(*args, **kwds)

3d1347b9949972f724d133ac42be9a26a86fe670
Rok Pestotnik <Rok.Pestotnik@desy.de>
2017-10-19 12:44:07 +0200

    changed outputUserConfig to external sense regulation. set the background color based on the deviation of set end sense voltage.

--- daq/slc/hvcontrol/arichlv/src/MPOD.cc
+++ daq/slc/hvcontrol/arichlv/src/MPOD.cc
@@ -108,6 +108,7 @@ static SnmpObject outputMeasurementTerminalVoltage[MaxChannelsPerCrate];
 static SnmpObject outputMeasurementCurrent[MaxChannelsPerCrate];
 static SnmpObject outputMeasurementTemperature[MaxChannelsPerCrate];
 static SnmpObject outputSwitch[MaxChannelsPerCrate];
+static SnmpObject outputUserConfig[MaxChannelsPerCrate];
 static SnmpObject outputVoltage[MaxChannelsPerCrate];
 static SnmpObject outputCurrent[MaxChannelsPerCrate];
 static SnmpObject outputVoltageRiseRate[MaxChannelsPerCrate];
@@ -169,13 +170,18 @@ static int getIndexNode(const char* const nodeBase, int index, SnmpObject* objec
 
 
 #ifdef MPOD_MAIN
+float mabs(float a)
+{
+  if (a > 0) return a;
+  else return -a;
+}
 int MPOD_CableTest(int id)
 {
   double ret;
   int iret;
   int min = 0;
   int max = 3;
-//const char  path[100]="arich-mpod3.b2nsm.kek.jp";
+  //const char  path[100]="arich-mpod3.b2nsm.kek.jp";
   const char  path[100] = "f9mpod2.ijs.si";
   MPOD_Start();
   HSNMP crate =  MPOD_Open(path);
@@ -197,19 +203,34 @@ int MPOD_CableTest(int id)
 
   if (id < 2)  for (int ch = 0; ch < 4; ch++) {
       setChannelSwitch(crate, ch , id);
+      if (id) setChannelSwitch(crate, ch , 10);
+      printf("setOutputSwitch ch=%d =>%d", ch, id);
       printf("<br/>\n");
     }
   else {
 
 // set output voltage
+    float vset[4];
     for (int ch = 0; ch < 4; ch++) {
-      setOutputVoltage(crate, ch , ch + 1);
+      vset[ch] = ch + 1;
+      setOutputVoltage(crate, ch , vset[ch]);
+
+      char  name[0xFF], sch[0xFF];
+      sprintf(sch, "%d", ch + 1);
+      sprintf(name, "outputConfigMaxTerminalVoltage.%s", sch);
+      ret = MPOD_SetDouble(crate, name, ch + 1.5);
+
+      iret = getOutputUserConfig(crate, ch + 1);
+      iret &= 0x17;
+      setOutputUserConfig(crate, ch + 1 , iret);
     }
 
 
-    printf("<table class='mytable' ><tr><th>Ch.</th><th>Vset</th><th>Vstatus</th><th>Imon</th><th>Vmon</th>");
-    // printf("<th>Sense1</th><th>Sense2</th><th>Sense3</th><th>Sense4</th>\n");
 
+
+    printf("<table border=1 class='mytable' >");
+    printf("<tr><th>Ch.</th><th>Vset</th><th>Vstatus</th><th>Imon</th><th>Vterminal</th><th>Vsense</th>\n");
+    for (int ch = 0; ch < 4; ch++) printf("<th>Vterm%d</th><th>Sense%d</th>\n", ch + 1, ch + 1);
     for (int ch = 0; ch < 4; ch++) {
       printf("<tr><td>%d</td>\n", ch);
 
@@ -219,30 +240,57 @@ int MPOD_CableTest(int id)
         sprintf(sch, "%d", chid + 1);
         if (slot == min) {
 
-          sprintf(name, "outputVoltage.%s", sch);
-          ret = MPOD_GetDouble(crate, name);
-          printf("<td>%2.5f</td>", ret);
+          ret = getOutputVoltage(crate, chid);
+          printf("<td>%2.2f</td>", ret);
 
           iret = getChannelSwitch(crate, chid);
-          printf("<td>%d</td>", iret);
+          int iret0 = getOutputUserConfig(crate, chid);
+          printf("<td>%d:0x%x</td>", iret,  iret0);
 
           ret = getCurrentMeasurement(crate, chid);
           printf("<td>%f</td>\n", ret);
+
+
         }
 
         if (slot == min) {
+          sprintf(name, "outputMeasurementTerminalVoltage.%s", sch);
+          ret = MPOD_GetDouble(crate, name);
+          printf("<td>%2.2f</td>", ret);
+
           sprintf(name, "outputMeasurementSenseVoltage.%s", sch);
           ret = MPOD_GetDouble(crate, name);
-          printf("<td>%2.5f</td>", ret);
+          if (mabs(vset[ch] - ret) > 0.1)
+            printf("<td bgcolor='red' >%2.2f</td>\n", ret);
+          else
+            printf("<td bgcolor='green'>%2.2f</td>\n", ret);
+
         } else {
-          /*
-                  for (int k=0;k<2;k++){
-                    sprintf(sch,"%d", ch +k*4 + slot*100);
-                    sprintf(name,"outputMeasurementSenseVoltage.%s",sch);
-                    ret = MPOD_GetDouble(crate, name);
-                    printf("<td>%2.5f</td>", ret);
-                  }
-          */
+
+          for (int k = 0; k < 2; k++) {
+            chid = ch + k * 4 + slot * 100;
+            setOutputVoltage(crate, chid  , 0);
+            setChannelSwitch(crate, chid  , 1);
+            iret = getOutputUserConfig(crate, chid);
+            iret &= 0x17;
+            if (slot < 2) setOutputUserConfig(crate, chid  , iret);
+            sprintf(sch, "%d", chid + 1);
+
+
+            sprintf(name, "outputMeasurementTerminalVoltage.%s", sch);
+            ret = MPOD_GetDouble(crate, name);
+            printf("<td>%2.2f</td>", ret);
+
+
+            sprintf(name, "outputMeasurementSenseVoltage.%s", sch);
+            ret = MPOD_GetDouble(crate, name);
+            if (mabs(vset[ch] - ret) > 0.1)
+              printf("<td bgcolor='red' >%2.2f</td>\n", ret);
+            else
+              printf("<td bgcolor='green'>%2.2f</td>\n", ret);
+
+          }
+
 
         }
       }
@@ -697,6 +745,7 @@ int snmpInit(void)
 
     for (int channel = base; channel < base + MaxChannelsPerSlot; ++channel) {
       if (
+        (!getIndexNode("outputUserConfig", channel + 1, &outputUserConfig[channel])) ||
         (!getIndexNode("outputName", channel + 1, &outputName[channel])) ||
         (!getIndexNode("outputIndex", channel + 1, &outputIndex[channel])) ||
         (!getIndexNode("outputGroup", channel + 1, &outputGroup[channel])) ||
@@ -1033,6 +1082,23 @@ int getOutputStatus(HSNMP session, int channel)
   return snmpGetInt(session, &outputStatus[channel]);
 }
 
+
+/**
+ * @brief Returns the channel outputUserConfig register.
+ * @since 1.1
+ * @param session The handle returned by snmpOpen()
+ * @param channel The requested channel in the range of 0...999
+ * @return The channels outputStatus register
+ */
+int getOutputUserConfig(HSNMP session, int channel)
+{
+  if (channel < 0 || channel >= MaxChannelsPerCrate)
+    return 0;
+
+  return snmpGetInt(session, &outputUserConfig[channel]);
+}
+
+
 /**
  * @brief Returns the measured output sense voltage for channel in Volt.
  * @note This is only valid for WIENER LV modules.
@@ -1118,9 +1184,43 @@ int setChannelSwitch(HSNMP session, int channel, int value)
 int setOutputSwitch(HSNMP session, int channel, int value)
 {
   if (channel < 0 || channel >= MaxChannelsPerCrate) return 0;
-  printf("setOutputSwitch ch=%d =>%d", channel, value);
   return snmpSetInt(session, &outputSwitch[channel], value);
 }
+
+/**
+ * @brief Change the outputUserConfig of the channel
+ * @param session The handle returned by snmpOpen()
+ * @param channel The channel in the range of 0...999
+ * @param value bitvalue:A bit field packed into an integer which define the behavior of the output channel.
+             Usable for WIENER LV devices only.
+             The position of the bit fields in the integer value are:
+             Bit 0:    Voltage ramping at switch off:
+                           0: Ramp down at switch off.
+                           1: No ramp at switch off (immediate off)
+             Bit 1, 2: Set different regulation modes, dependent on the
+                           cable inductance:
+                     0: fast:     short cables, up to 1 meter.
+                     1: moderate: cables from 1 to 30 meter.
+                     2: slow:     cables longer than 30 meter.
+                     3: slow (identical to 2, should not be used)
+             Bit 3:    Internal sense line connection to the output (MPOD only):
+                           0: The sense input at the sense connector is used
+                              for regulation.
+                           1: The output voltage is used for regulation.
+                              Any signals at the sense connector are ignored.
+             Bit 4:    External Inhibit input.
+                           0: The external inhibit input is ignored.
+                           1: The external inhibit input must be connected to
+                              a voltage source to allow switch on.
+ * @return
+ */
+int setOutputUserConfig(HSNMP session, int channel, int value)
+{
+  if (channel < 0 || channel >= MaxChannelsPerCrate) return 0;
+  return snmpSetInt(session, &outputUserConfig[channel], value);
+}
+
+
 
 /**
  * @brief Returns the state of the channel.

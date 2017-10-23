0db661533746e2aa887d057d96d0f2e1bbbf7ed8
Tzu-An Sheng <tzu-an.sheng@desy.de>
2017-10-24 01:26:48 +0900

    Clean up old char array style SLV manipulation code

--- trg/cdc/modules/houghtracking/src/CDCTrigger2DFinderFirmware.cc
+++ trg/cdc/modules/houghtracking/src/CDCTrigger2DFinderFirmware.cc
@@ -30,24 +30,6 @@
 namespace Cosim {
   const char* std_logic_literal[] = {"U", "X", "0", "1", "Z", "W", "L", "H", "-"};
 
-  // void vector(const std::string word, char*& vec)
-  // {
-  //   // char vec[word.size()];
-  //   vec = (char*) malloc(word.size() + 1);
-  //   for (unsigned i = 0; i < word.size(); ++i) {
-  //     vec[i] = (word[i] == '0') ? 2 : 3;
-  //   }
-  // }
-
-  // char * toSLV(const std::string word)
-  // {
-  //   static char * vec = (char*) malloc(word.size() + 1);
-  //   for (unsigned i = 0; i < word.size(); ++i) {
-  //     vec[i] = (word[i] == '0') ? 2 : 3;
-  //   }
-  //   return vec;
-  // }
-
   std::string display_value(const char* count, int size)
   {
     std::string res;
@@ -78,7 +60,7 @@ CDCTrigger2DFinderFirmware::CDCTrigger2DFinderFirmware(const CDCTrigger2DFinderM
   m_mod(mod)
 {
 
-};
+}
 
 CDCTrigger2DFinderFirmware::~CDCTrigger2DFinderFirmware()
 {
@@ -100,11 +82,6 @@ void CDCTrigger2DFinderFirmware::initialize()
     Xsi_Instance.trace_all();
 
     clk  = Xsi_Instance.get_port_number_or_exit("Top_clkData_s");
-    // tsf0 = Xsi_Instance.get_port_number_or_exit("TSF0_input_i");
-    // tsf2 = Xsi_Instance.get_port_number_or_exit("TSF2_input_i");
-    // tsf4 = Xsi_Instance.get_port_number_or_exit("TSF4_input_i");
-    // tsf6 = Xsi_Instance.get_port_number_or_exit("TSF6_input_i");
-    // tsf8 = Xsi_Instance.get_port_number_or_exit("TSF8_input_i");
     for (int iSL = 0; iSL < 5; ++iSL) {
       string name = "TSF" + to_string(iSL * 2) + "_input_i";
       tsf[iSL] = Xsi_Instance.get_port_number_or_exit(name);
@@ -113,118 +90,9 @@ void CDCTrigger2DFinderFirmware::initialize()
     }
     out  = Xsi_Instance.get_port_number_or_exit("Main_out");
 
-    // memset(tsf0_in, 2, sizeof(tsf0_in));
-    // memset(tsf2_in, 2, sizeof(tsf2_in));
-    // memset(tsf4_in, 2, sizeof(tsf4_in));
-    // memset(tsf6_in, 2, sizeof(tsf6_in));
-    // memset(tsf8_in, 2, sizeof(tsf8_in));
-
-    // char* i0 = 0;
-    // char* i1 = 0;
-    // char* i2 = 0;
-    // char* i3 = 0;
-
-    // char* p00 = 0;
-    // char* p10 = 0;
-    // char* p01 = 0;
-    // char* p11 = 0;
-
-    // std::vector<char*> ids;
-    // std::vector<char*> pris;
-
-    // vector("00", p00);
-    // vector("11", p11);
-    // vector("01", p01);
-    // vector("10", p10);
-
-    // vector("00010101", i0);
-    // vector("00010110", i1);
-    // vector("00110010", i2);
-    // vector("00110011", i3);
-
-    // pris.insert(pris.end(), {p10, p11, p11, p01});
-    // ids.insert(ids.end(), {i0, i1, i2, i3});
-    // for (unsigned i = 0; i < ids.size(); ++i) {
-    //   memcpy(tsf0_in + 21 * i + 9, ids[i], 8);
-    //   memcpy(tsf0_in + 21 * i + 26, pris[i], 2);
-    //   memcpy(tsf0_in + 21 * i + 28, pris[i], 2);
-    // }
-    // pris.clear();
-    // ids.clear();
-
-    // vector("00010110", i0);
-    // vector("00010111", i1);
-    // vector("00111010", i2);
-    // pris.insert(pris.end(), {p10, p11, p11});
-    // ids.insert(ids.end(), {i0, i1, i2});
-    // for (unsigned i = 0; i < ids.size(); ++i) {
-    //   memcpy(tsf2_in + 21 * i + 9, ids[i], 8);
-    //   memcpy(tsf2_in + 21 * i + 26, pris[i], 2);
-    //   memcpy(tsf2_in + 21 * i + 28, pris[i], 2);
-    // }
-    // pris.clear();
-    // ids.clear();
-
-    // vector("00011000", i0);
-    // vector("00011001", i1);
-    // vector("01001001", i2);
-    // vector("01001010", i3);
-    // pris.insert(pris.end(), {p11, p11, p11, p01});
-    // ids.insert(ids.end(), {i0, i1, i2, i3});
-    // for (unsigned i = 0; i < ids.size(); ++i) {
-    //   memcpy(tsf4_in + 21 * i + 9, ids[i], 8);
-    //   memcpy(tsf4_in + 21 * i + 26, pris[i], 2);
-    //   memcpy(tsf4_in + 21 * i + 28, pris[i], 2);
-    // }
-    // pris.clear();
-    // ids.clear();
-
-    // vector("00010111", i0);
-    // vector("01010111", i1);
-    // pris.insert(pris.end(), {p11, p11});
-    // ids.insert(ids.end(), {i0, i1});
-    // for (unsigned i = 0; i < ids.size(); ++i) {
-    //   memcpy(tsf6_in + 21 * i + 9, ids[i], 8);
-    //   memcpy(tsf6_in + 21 * i + 26, pris[i], 2);
-    //   memcpy(tsf6_in + 21 * i + 28, pris[i], 2);
-    // }
-    // pris.clear();
-    // ids.clear();
-
-    // vector("01110011", i0);
-    // pris.insert(pris.end(), {p11});
-    // ids.insert(ids.end(), {i0});
-    // for (unsigned i = 0; i < ids.size(); ++i) {
-    //   memcpy(tsf8_in + 21 * i + 9, ids[i], 8);
-    //   memcpy(tsf8_in + 21 * i + 26, pris[i], 2);
-    //   memcpy(tsf8_in + 21 * i + 28, pris[i], 2);
-    // }
-    // pris.clear();
-    // ids.clear();
-
-    // std::cout << display_value(tsf0_in, width_in) << "\n";
-    // std::cout << display_value(tsf2_in, width_in) << "\n";
-    // std::cout << display_value(tsf4_in, width_in) << "\n";
-    // std::cout << display_value(tsf6_in, width_in) << "\n";
-    // std::cout << display_value(tsf8_in, width_in) << "\n";
-
-    // Xsi_Instance.put_value(tsf0, &tsf0_in);
-    // Xsi_Instance.put_value(tsf2, &tsf2_in);
-    // Xsi_Instance.put_value(tsf4, &tsf4_in);
-    // Xsi_Instance.put_value(tsf6, &tsf6_in);
-    // Xsi_Instance.put_value(tsf8, &tsf8_in);
-
     // Start low clock
     Xsi_Instance.put_value(clk, &zero_val);
     Xsi_Instance.run(10);
-
-    // Reset to 1 and clock it
-    Xsi_Instance.put_value(clk, &one_val);
-    Xsi_Instance.run(10);
-
-    // Put clk to 0, reset to 0 and enable to 1
-    Xsi_Instance.put_value(clk, &zero_val);
-
   } catch (std::exception& e) {
     std::cerr << "ERROR: An exception occurred: " << e.what() << std::endl;
   } catch (...) {
@@ -232,7 +100,7 @@ void CDCTrigger2DFinderFirmware::initialize()
     // Xsi_Instance.get_error_info();
     throw;
   }
-};
+}
 
 int CDCTrigger2DFinderFirmware::localSegmentID(int globalID)
 {
@@ -393,5 +261,5 @@ void CDCTrigger2DFinderFirmware::event()
     status = 3;
   }
 
-};
+}
 

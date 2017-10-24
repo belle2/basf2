ea2608950784f40951dd933413a5422e9efe9a32
Thomas Hauth <Thomas.Hauth@kit.edu>
2017-10-24 11:20:51 +0200

    removed legacy (commented) code

--- tracking/modules/trackCreator/src/TrackCreatorModule.cc
+++ tracking/modules/trackCreator/src/TrackCreatorModule.cc
@@ -98,30 +98,12 @@ void TrackCreatorModule::event()
   TrackBuilder trackBuilder(m_trackColName, m_trackFitResultColName, m_mcParticleColName,
                             m_beamSpotAsTVector, m_beamAxisAsTVector);
   for (auto& recoTrack : recoTracks) {
-//<<<<<<< HEAD
     for (const auto& pdg : m_pdgCodes) {
       // Does not refit in case the particle hypotheses demanded in this module have already been fitted before.
       // Otherwise fits them with the default fitter.
       B2DEBUG(200, "Trying to fit with PDG = " << pdg);
       trackFitter.fit(recoTrack, Const::ParticleType(abs(pdg)));
     }
-//=======
-    // Require pion fit as a safety measure
-    /*   const bool pionFitWasSuccessful = trackFitter.fit(recoTrack, Const::ParticleType(abs(m_defaultPDGCode)));
-       // Does not refit in case the particle hypotheses demanded in this module have already been fitted before.
-       // Otherwise fits them with the default fitter.
-       if (pionFitWasSuccessful) {
-         for (const auto& pdg : m_additionalPDGCodes) {
-           B2DEBUG(200, "Trying to fit with PDG = " << pdg);
-           trackFitter.fit(recoTrack, Const::ParticleType(abs(pdg)));
-         }
-         trackBuilder.storeTrackFromRecoTrack(recoTrack, Const::ParticleType(abs(m_defaultPDGCode)),
-                                              m_useClosestHitToIP, m_useBFieldAtHit);
-       } else {
-         B2DEBUG(200, "Pion fit failed - not creating a Track out of this RecoTrack.");
-    >>>>>>> master
-       }
-    */
     trackBuilder.storeTrackFromRecoTrack(recoTrack, m_useClosestHitToIP);
   }
 }

fe74e4218b9b30910e3f6311138b27009e82030f
Torben Ferber <ferber@physics.ubc.ca>
2017-10-25 00:35:46 +0200

    Added pi0 particle lists with fixed efficiencies. Added polar angle selection to photons.

--- analysis/scripts/stdPhotons.py
+++ analysis/scripts/stdPhotons.py
@@ -1,23 +1,102 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+########################################################
+#
+# Author(s): Torben Ferber (ferber@physics.ubc.ca)
+#
+# more documentation: https://confluence.desy.de/x/I3I0Aw
+#
+########################################################
+
 from basf2 import *
 from modularAnalysis import *
 
 
 def stdPhotons(listtype='loose', path=analysis_main):
+    """
+    Function to prepare one of several standardized types of photon lists:
 
+    - 'gamma:all' with no cuts
+    - 'gamma:loose' (default) with some loose quality selections
+    - 'gamma:tight' like loose but with higher energy cuts depending on detector regions
+    - 'gamma:pi0eff60' gamma list for 60% pi0 efficiency list (Phase 2)
+    - 'gamma:pi0eff50' gamma list for 50% pi0 efficiency list (Phase 2)
+    - 'gamma:pi0eff40' gamma list for 40% pi0 efficiency list (Phase 2)
+    - 'gamma:pi0eff30' gamma list for 30% pi0 efficiency list (Phase 2)
+    - 'gamma:pi0eff20' gamma list for 20% pi0 efficiency list (Phase 2)
+    - 'gamma:pi0' gamma list for pi0 list
+    - 'gamma:pi0highE' gamma list for pi0 list, high energy selection
+
+    @param listtype name of standard list
+    @param path     modules are added to this path
+    """
+
+    # all photons (reconstructed using the N1 clustering)
     if listtype == 'all':
         fillParticleList('gamma:all', 'clusterHypothesis == 5', True, path)
+    # clusterErrorTiming < 1e6 removes failed waveform fits, this is not an actual timing cut. A 99% efficienct cut
+    # is already applied on mdst level for photons with E < 50 MeV.
     elif listtype == 'loose':
         stdPhotons('all', path)
-        cutAndCopyList('gamma:loose', 'gamma:all', 'clusterErrorTiming < 1e6 and [clusterE1E9 > 0.4 or E > 0.075]', True, path)
+        cutAndCopyList(
+            'gamma:loose',
+            'gamma:all',
+            'Theta > 0.296706 and Theta < 2.61799 and clusterErrorTiming < 1e6 and [clusterE1E9 > 0.4 or E > 0.075]',
+            True,
+            path)
+    # additional region dependent energy cuts
     elif listtype == 'tight':
         stdPhotons('loose', path)
         cutAndCopyList(
             'gamma:tight',
             'gamma:loose',
             '[clusterReg == 1 and E > 0.05] or [clusterReg == 2 and E > 0.05] or [clusterReg == 3 and E > 0.075]',
+            True,
+            path)
+    elif listtype == 'pi0eff20':
+        stdPhotons('all', path)
+        cutAndCopyList(
+            'gamma:pi0eff20',
+            'gamma:all', 'Theta > 0.296706 and Theta < 2.61799 and
+            [[clusterReg == 1 and E > 0.075] or [clusterReg == 2 and E > 0.075] or [clusterReg == 3 and E > 0.075]] and
+            [abs(clusterTiming) < 0.1 * clusterErrorTiming or E > 0.1] and [clusterE1E9 > 0.7 or E > 0.1] ',
+            True,
+            path)
+    elif listtype == 'pi0eff30':
+        stdPhotons('all', path)
+        cutAndCopyList(
+            'gamma:pi0eff30',
+            'gamma:all', 'Theta > 0.296706 and Theta < 2.61799 and
+            [[clusterReg == 1 and E > 0.075] or [clusterReg == 2 and E > 0.05] or [clusterReg == 3 and E > 0.075]] and
+            [abs(clusterTiming) < 0.5 * clusterErrorTiming or E > 0.1] and [clusterE1E9 > 0.7 or E > 0.1] ',
+            True,
+            path)
+    elif listtype == 'pi0eff40':
+        stdPhotons('all', path)
+        cutAndCopyList(
+            'gamma:pi0eff40',
+            'gamma:all', 'Theta > 0.296706 and Theta < 2.61799 and
+            [[clusterReg == 1 and E > 0.075] or [clusterReg == 2 and E > 0.03] or [clusterReg == 3 and E > 0.075]] and
+            [abs(clusterTiming) < 0.5 * clusterErrorTiming or E > 0.1] and [clusterE1E9 > 0.5 or E > 0.1] ',
+            True,
+            path)
+    elif listtype == 'pi0eff50':
+        stdPhotons('all', path)
+        cutAndCopyList(
+            'gamma:pi0eff50',
+            'gamma:all', 'Theta > 0.296706 and Theta < 2.61799 and
+            [[clusterReg == 1 and E > 0.05] or [clusterReg == 2 and E > 0.03] or [clusterReg == 3 and E > 0.05]] and
+            [abs(clusterTiming) < 1.0 * clusterErrorTiming or E > 0.1] and [clusterE1E9 > 0.3 or E > 0.1] ',
+            True,
+            path)
+    elif listtype == 'pi0eff60':
+        stdPhotons('all', path)
+        cutAndCopyList(
+            'gamma:pi0eff60',
+            'gamma:all', 'Theta > 0.296706 and Theta < 2.61799 and
+            [[clusterReg == 1 and E > 0.03] or [clusterReg == 2 and E > 0.02] or [clusterReg == 3 and E > 0.03]] and
+            [abs(clusterTiming) < 1.0 * clusterErrorTiming or E > 0.1] and [clusterE1E9 > 0.3 or E > 0.1] ',
             True,
             path)
     elif listtype == 'pi0':
@@ -36,10 +115,8 @@ def stdPhotons(listtype='loose', path=analysis_main):
             'E > 0.2',
             True,
             path)
-    else:
-        fillParticleList('gamma:all', 'clusterHypothesis == 5', True, path)
 
-# Used in skimming code
+    # Used in skimming code
 
 
 def loadStdSkimPhoton(path=analysis_main):
@@ -50,6 +127,12 @@ def loadStdSkimPhoton(path=analysis_main):
         '',
         True,
         path)
+
+
+# Only used for Belle via b2bii
+def loadStdGoodBellePhoton(path=analysis_main):
+    loadStdAllPhoton(path)
+    cutAndCopyList('gamma:goodBelle', 'gamma:all', '0.5 < goodBelleGamma < 1.5', True, path)
 
 # old, will be removed/replaced
 
@@ -63,15 +146,7 @@ def loadStdPhotonE15(path=analysis_main):
     loadStdAllPhoton(path)
     cutAndCopyList('gamma:E15', 'gamma:all', '1.5 < E < 100', True, path)
 
-# deprecated
-
 
 def loadStdGoodPhoton(path=analysis_main):
     loadStdAllPhoton(path)
     cutAndCopyList('gamma:good', 'gamma:all', '0.5 < goodGamma < 1.5', True, path)
-
-
-# Only used for Belle via b2bii
-def loadStdGoodBellePhoton(path=analysis_main):
-    loadStdAllPhoton(path)
-    cutAndCopyList('gamma:goodBelle', 'gamma:all', '0.5 < goodBelleGamma < 1.5', True, path)

ca3b2e26babe80d6417a9ea85829fdcd55c69076
Sam Cunliffe <samuel.cunliffe@pnnl.gov>
2017-10-27 23:01:45 +0900

    Forgot the B to K* mu mu plotting script

--- analysis/validation/test2_1120240011.py
+++ analysis/validation/test2_1120240011.py
@@ -3,14 +3,14 @@
 
 """
 <header>
-  <input>../1120240010.ntup.root</input>
-  <output>1120240010_Validation.root</output>
+  <input>../1120240011.ntup.root</input>
+  <output>1120240011_Validation.root</output>
   <contact>Sam Cunliffe; sam.cunliffe@desy.de</contact>
 </header>
 """
 
 # Validation plotting script for event type: 112024001
-# i.e. B0 -> K*0 e+ e-
+# i.e. B0 -> K*0 mu+ mu-
 #            |
 #            +-> K+ pi-
 #
@@ -19,11 +19,11 @@
 from ROOT import TFile, TTree, TH1F, TNamed
 
 # TTree of candidates, declare output file
-ifile = TFile("../1120240010.ntup.root", "READ")
+ifile = TFile("../1120240011.ntup.root", "READ")
 tree = ifile.Get("h1")
-ofile = TFile("1120240010_Validation.root", "RECREATE")
+ofile = TFile("1120240011_Validation.root", "RECREATE")
 
-title = "B^{0}#rightarrow K*^{0}ee"
+title = "B^{0}#rightarrow K*^{0}#mu#mu"
 contactperson = "Saurabh Sandilya (saurabhsandilya@gmail.com)"
 
 hmbc = TH1F("hmbc", title, 180, 5.2, 5.29)
@@ -39,14 +39,14 @@ hmkpi.SetXTitle("m_{K#pi} (GeV/c^{2})")
 tree.Project("hmkpi", "B0_KST0_M")
 
 # add meaningful information to the histograms
-dmbc = TNamed("Description", "Reconstructed M_{bc} distribution for B^{0}#to K*(892)^{0}e^{+}e^{-} decays.")
+dmbc = TNamed("Description", "Reconstructed M_{bc} distribution for B^{0}#to K*(892)^{0}#mu^{+}#mu^{-} decays.")
 cmbc = TNamed("Check", "Consistent shape, centred at 0, longer tail on right")
 pmbc = TNamed("Contact", contactperson)
 hmbc.GetListOfFunctions().Add(dmbc)
 hmbc.GetListOfFunctions().Add(cmbc)
 hmbc.GetListOfFunctions().Add(pmbc)
 
-ddeltae = TNamed("Description", "Reconstructed #Delta E distribution for B^{0}#to K*(892)^{0}e^{+}e^{-} decays.")
+ddeltae = TNamed("Description", "Reconstructed #Delta E distribution for B^{0}#to K*(892)^{0}#mu^{+}#mu^{-} decays.")
 cdeltae = TNamed("Check", "Consistent shape, centred at 0, longer tail on right")
 pdeltae = TNamed("Contact", contactperson)
 hdeltae.GetListOfFunctions().Add(ddeltae)
@@ -55,7 +55,7 @@ hdeltae.GetListOfFunctions().Add(pdeltae)
 
 dmkpi = TNamed(
     "Description",
-    "Reconstructed K#pi invariant mass distribution for K*(892)^{0}#to K#pi, from B^{0}#to K*(892)^{0}e^{+}e^{-} decays.")
+    "Reconstructed K#pi invariant mass distribution for K*(892)^{0}#to K#pi, from B^{0}#to K*(892)^{0}#mu^{+}#mu^{-} decays.")
 cmkpi = TNamed("Check", "Consistent Breit-Wigner shape, mean at 0.896 GeV/c^{2}")
 pmkpi = TNamed("Contact", contactperson)
 hmkpi.GetListOfFunctions().Add(dmkpi)

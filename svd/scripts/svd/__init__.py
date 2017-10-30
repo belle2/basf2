1a4952a7863764a2c2e76cac61973a09a010de05
Peter Kvasnicka <kvasnicka@ipnp.troja.mff.cuni.cz>
2017-10-30 05:13:25 +0100

    Added an option to use NN reconstruction in the add_* scripts.

--- svd/scripts/svd/__init__.py
+++ svd/scripts/svd/__init__.py
@@ -12,6 +12,20 @@ def add_svd_reconstruction(path, clusterName=""):
     path.add_module(svd_clusterizer)
 
 
+def add_svd_reconstruction_nn(path, clusterName="", direct=False):
+
+    if direct:
+        svd_clusterizer = register_module('SVDClusterizerDirect')
+        svd_clusterizer.param('Clusters', clusterName)
+        path.add_module(svd_clusterizer)
+    else:
+        svd_recofitter = register_module('SVDNNShapeFitter')
+        svd_clusterizer = register_module('SVDNNClusterizer')
+        svd_clusterizer.param('Clusters', clusterName)
+        path.add_module(svd_recofitter)
+        path.add_module(svd_clusterizer)
+
+
 def add_svd_simulation(path):
 
     svd_digitizer = register_module('SVDDigitizer')
--- svd/scripts/svd/dump_digits.py
+++ svd/scripts/svd/dump_digits.py
@@ -14,73 +14,100 @@ class dump_digits(Module):
 
     """A simple module to check the reconstruction of SVD hits."""
 
-    def __init__(self):
+    def __init__(self, calmap='', oo_map='testbeam/vxd/data/2017_svd_mapping.xml', filename='dumped_digits.txt'):
         """Initialize the module"""
 
         super(dump_digits, self).__init__()
         #: Input file object.
-        self.file = open('dumped_digits.txt', 'w')
+        self.outfile = open(filename, 'w')
         #: Factors for decoding VXDId's
         self.vxdid_factors = (8192, 256, 32)
         # Get a handle on the GeoCache
         self.geoCache = Belle2.VXD.GeoCache.getInstance()
+        # Record filenames
+        self.oo_map = oo_map
+        self.calmap = calmap
 
     def beginRun(self):
-        """ Write legend for file columns """
-
-        self.file.write('VxdID Layer Ladder Sensor Side  Sample0 Sample1 Sample2 Sample3 Sample4 lSample5')
+        """ Tasks at the start of a run """
+        # Write header to output file
+        self.outfile.write(
+            'EventNo Layer Ladder Sensor Side StripNo TimeTrigger ' +
+            'GoodStrip Noise Width TimeShift' +
+            'Sample0 Sample1 Sample2 Sample3 Sample4 Sample5 Charge TimeFit Chi2\n')
+        # Initialize stripmap
+        self.strip_map = Belle2.SVD.StripCalibrationMap(self.oo_map, self.calmap)
 
     def event(self):
-        """Find clusters with a truehit and print some stats."""
+        """Cycle through RecoDigit/ShaperDigit pairs and dump the corresponding data"""
 
-        last_sensorID = 0
-        thresholdU = 0
-        thresholdV = 0
+        evt_info = Belle2.PyStoreObj('EventMetaData')
+        event_number = evt_info.getEvent()
+        reco_digits = Belle2.PyStoreArray('SVDRecoDigits')
 
-        digits = Belle2.PyStoreArray('SVDShaperDigits')
-        nDigits = digits.getEntries()
+        for reco_digit in reco_digits:
 
-        for digit in digits:
-
-            sensorID = digit.getRawSensorID()
-            # Only get new values if we are on a new sensor
-            if sensorID != last_sensorID:
-                last_sensorID = sensorID
-                sensor_info = self.geoCache.get(digit.getSensorID())
-                thresholdU = 3.0 * sensor_info.getElectronicNoiseU() / sensor_info.getAduEquivalentU()
-                thresholdV = 3.0 * sensor_info.getElectronicNoiseV() / sensor_info.getAduEquivalentV()
-
-            uSide = digit.isUStrip()
-            digit_threshold = thresholdU if uSide else thresholdV
-
-            if not self.three_test(digit, digit_threshold):
-                continue
-
-            # Sesnor identification
-            [layer, ladder, sensor] = self.decode(sensorID)
-            side_str = 'u' if uSide else 'v'
+            shaper_digit = reco_digit.getRelatedTo('SVDShaperDigits')
 
             s = ''
 
-            s_id = '{sID} {layer} {ladder} {sensor} {side} '.format(
-                sID=sensorID,
+            # Sesnor/side/strip identification
+            [layer, ladder, sensor] = self.decode(reco_digit.getRawSensorID())
+            s += '{event} {layer} {ladder} {sensor} {side} {strip} '.format(
+                event=event_number,
                 layer=layer,
                 ladder=ladder,
                 sensor=sensor,
-                side=side_str
+                side=('u' if reco_digit.isUStrip() else 'v'),
+                strip=reco_digit.getCellID()
             )
-            s += s_id
+            sensorID = reco_digit.getSensorID()
+            stripNo = reco_digit.getCellID()
+            # Trigger bin from SVDModeByte
+            triggerBin = ord(shaper_digit.getModeByte().getTriggerBin())
+            triggerTime = 0.25 * 31.44 * (-4 + triggerBin + 0.5)
+            s += '{trigger:.3f} '.format(trigger=triggerTime)
+            # Calibrations
+            stripCals = self.strip_map.getStripData(sensorID, reco_digit.isUStrip(), stripNo)
+            s += '{mask} {gain:.3f} {noise:.3f} {width} {delay:.3f} '.format(
+                mask=('y' if stripCals.m_goodStrip else 'n'),
+                gain=stripCals.m_calPeak,
+                noise=stripCals.m_noise,
+                width=stripCals.m_calWidth,
+                delay=stripCals.m_calTimeDelay
+            )
+            # Mask: True if good strip
+            stripMask = stripCals.m_goodStrip
+            # Gain: electrons per ADU
+            stripGain = stripCals.m_calPeak
+            if stripGain == 0:
+                stripGain = 1.0e6
+            # Noise: in electrons, convert to ADU
+            stripNoise = stripCals.m_noise / stripGain
+            # Width: correct beta-prime scale
+            stripWaveWidth = stripCals.m_calWidth
+            # TimeDelay: beta-prime shift,
+            stripTimeDelay = stripCals.m_calTimeDelay
+
             # Digit information
-            samples = digit.getSamples()
+            samples = shaper_digit.getSamples()
             for iSample in range(6):
                 s += '{0} '.format(samples[iSample])
+
+            # Data from time fit
+            s += '{amplitude:.3f} {time:.3f} {chi2:.3f}'.format(
+                amplitude=reco_digit.getCharge(),
+                time=reco_digit.getTime(),
+                chi2=reco_digit.getChi2Ndf()
+            )
+
             s += '\n'
-            self.file.write(s)
+            self.outfile.write(s)
 
     def terminate(self):
         """ Close the output file."""
 
-        self.file.close()
+        self.outfile.close()
 
     def decode(self, vxdid):
         """ Utility to decode sensor IDs """

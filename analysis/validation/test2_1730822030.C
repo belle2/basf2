1ec202d659fc54e15136a6294f1d95c8f4557c8c
lisx <lisuxian@buaa.edu.cn>
2017-10-30 12:54:29 +0900

    include improvement

--- analysis/validation/test2_1730822030.C
+++ analysis/validation/test2_1730822030.C
@@ -1,37 +1,39 @@
 
 /*
-<header>
-<input>../1730822030.ntup.root</input>
-<output>1730822030_Validation.root</output>
-<contact>Suxian Li; 13191004@buaa.edu.cn</contact>
-</header>
-*/
+ * <header>
+ * <input>../1730822030.ntup.root</input>
+ * <output>1730822030_Validation.root</output>
+ * <contact>Suxian Li; lisuxian@buaa.edu.cn</contact>
+ * </header>
+ * */
 
 ////////////////////////////////////////////////////////
 //
-//test2_1730840030.C
-//Check InvM of Y(1S,2S),chi_b(2P) and efficiency.
+// test2_1730822030.C
+// Check InvM of Y(1S,2S),chi_b(2P) and efficiency.
 //
-//Constributor: Suxian Li
-//May 30, 2017
-////////////////////////////////////////////////////////
+// Constributor: Suxian Li
+// May 30, 2017
+//
+/////////////////////////////////////////////////////////////////
+//
+
 
 #include "TFile.h"
 #include "TTree.h"
 #include "TH1F.h"
-#include <TApplication.h>
 #include <TROOT.h>
 
 void plot_1Smumu(TFile* pfile, TTree* ptree, TFile* outputFile){
-   
-   TString pidCut1 ("Upsilon3S_chi_b12P_Upsilon_mu0_PIDmu > 0.1 && Upsilon3S_chi_b12P_Upsilon_mu1_PIDmu >0.1 ");
 
+   TString pidCut1 ("Upsilon3S_chi_b12P_Upsilon_mu0_PIDmu > 0.1 && Upsilon3S_chi_b12P_Upsilon_mu1_PIDmu >0.1 ");
+   
    TH1F *h_M_Y1S_mumu = new TH1F ("h_M_Y1S_mumu","M[Y(1S)->mumu]",20,9.36,9.56);
    ptree->Project("h_M_Y1S_mumu","Upsilon3S_chi_b12P_Upsilon_M",pidCut1);
    h_M_Y1S_mumu->GetXaxis()->SetTitle("M_{#mu^{+}#mu^{-}} (GeV/c^{2})");
    h_M_Y1S_mumu->GetYaxis()->SetTitle("Event /0.01GeV/c^{2}");
    h_M_Y1S_mumu->GetListOfFunctions()->Add(new TNamed("Description","Y(3S)->gamma [chi_b2P->gamma [Y(1S)->mu+ mu-]]"));
-   h_M_Y1S_mumu->GetListOfFunctions()->Add(new TNamed("Contact","lisuxian@buaa.edu.cn"));
+   h_M_Y1S_mumu->GetListOfFunctions()->Add(new TNamed("Contact","lisuxian@buaa.edu.cn"));   
    h_M_Y1S_mumu->GetListOfFunctions()->Add(new TNamed("Check","check the InvM of Y(1S) reconstructed by dipion"));
 
    TH1F *h_M_chi_b12P_1Smumu = new TH1F("h_M_chi_b12P_1Smumu","M[chi_b(2P)->1Smumu]",14,10.18,10.32);
@@ -41,6 +43,7 @@ void plot_1Smumu(TFile* pfile, TTree* ptree, TFile* outputFile){
    h_M_chi_b12P_1Smumu->GetListOfFunctions()->Add(new TNamed("Description","Y(3S)->gamma [chi_b2P->gamma [Y(1S)->mu+ mu-]]"));
    h_M_chi_b12P_1Smumu->GetListOfFunctions()->Add(new TNamed("Contact","lisuxian@buaa.edu.cn"));
    h_M_chi_b12P_1Smumu->GetListOfFunctions()->Add(new TNamed("Check","check the InvM of chi_b2P(->gammma Y(1S)) and efficiency "));
+
 
    outputFile->cd();
    
@@ -52,7 +55,7 @@ void plot_1Smumu(TFile* pfile, TTree* ptree, TFile* outputFile){
 void plot_1See(TFile* pfile, TTree* ptree, TFile* outputFile){
    
    TString pidCut2 ("Upsilon3S_chi_b12P_Upsilon_e0_PIDe >0.1 && Upsilon3S_chi_b12P_Upsilon_e1_PIDe > 0.1");
-
+  
    TH1F* h_M_Y1S_ee = new TH1F("h_M_Y1S_ee","M[Y(1S)->ee]",20,9.36,9.56);
    ptree->Project("h_M_Y1S_ee","Upsilon3S_chi_b12P_Upsilon_M",pidCut2);
    h_M_Y1S_ee->GetXaxis()->SetTitle("M_{e^{+}e^{-}} (Gev/c^{2})");
@@ -60,7 +63,7 @@ void plot_1See(TFile* pfile, TTree* ptree, TFile* outputFile){
    h_M_Y1S_ee->GetListOfFunctions()->Add(new TNamed("Description","Y(3S)->gamma [chi_b2P->gamma [Y(1S)->e+ e-]]"));
    h_M_Y1S_ee->GetListOfFunctions()->Add(new TNamed("Contact","lisuxian@buaa.edu.cn"));
    h_M_Y1S_ee->GetListOfFunctions()->Add(new TNamed("Check","check the InvM of Y(1S)[-> e+ e-]"));
-
+   
    TH1F* h_M_chi_b12P_1See = new TH1F("h_M_chi_b12P_1See","M[chi_b(2P)->1See]",14,10.18,10.32);
    ptree->Project("h_M_chi_b12P_1See","Upsilon3S_chi_b12P_M-Upsilon3S_chi_b12P_Upsilon_M+9.46",pidCut2);
    h_M_chi_b12P_1See->GetXaxis()->SetTitle("M_{#gamma#Upsilon(1S)} (Gev/c^{2})");
@@ -68,8 +71,7 @@ void plot_1See(TFile* pfile, TTree* ptree, TFile* outputFile){
    h_M_chi_b12P_1See->GetListOfFunctions()->Add(new TNamed("Description","Y(3S)->gamma [chi_b2P->gamma [Y(1S)->e+ e-]]"));
    h_M_chi_b12P_1See->GetListOfFunctions()->Add(new TNamed("Contact","lisuxian@buaa.edu.cn"));
    h_M_chi_b12P_1See->GetListOfFunctions()->Add(new TNamed("Check","check the InvM of chi_b2P(->gammma Y(1S)) and efficiency"));
-                                                                                                                    59,4            
-
+   
    outputFile->cd();
     
    h_M_Y1S_ee->Write();
@@ -80,7 +82,7 @@ void plot_1See(TFile* pfile, TTree* ptree, TFile* outputFile){
 void plot_2Smumu(TFile* pfile,TTree *ptree, TFile* outputFile){
 
    TString pidCut3 ("Upsilon3S_chi_b12P_Upsilon2S_mu0_PIDmu > 0.1 && Upsilon3S_chi_b12P_Upsilon2S_mu1_PIDmu >0.1");
-
+  
    TH1F* h_M_Y2S_mumu = new TH1F("h_M_Y2S_mumu","M[Y(2S)->mumu]",20,9.9232,10.1232);
    ptree->Project("h_M_Y2S_mumu","Upsilon3S_chi_b12P_Upsilon2S_M",pidCut3);
    h_M_Y2S_mumu->GetXaxis()->SetTitle("M_{#mu^{+}#mu^{-}} (GeV/c^{2})");
@@ -97,6 +99,7 @@ void plot_2Smumu(TFile* pfile,TTree *ptree, TFile* outputFile){
    h_M_chi_b12P_2Smumu->GetListOfFunctions()->Add(new TNamed("Contact","lisuxian@buaa.edu.cn"));
    h_M_chi_b12P_2Smumu->GetListOfFunctions()->Add(new TNamed("Check","check the InvM of chi_b2P(->gammma mu+ mu-) and efficiency"));
 
+
    outputFile->cd();
 
    h_M_Y2S_mumu->Write();
@@ -107,7 +110,7 @@ void plot_2Smumu(TFile* pfile,TTree *ptree, TFile* outputFile){
 void plot_2See(TFile* pfile,TTree *ptree, TFile* outputFile){
 
    TString pidCut4 ("Upsilon3S_chi_b12P_Upsilon2S_e0_PIDe > 0.1 && Upsilon3S_chi_b12P_Upsilon2S_e1_PIDe > 0.1");
-
+   
    TH1F* h_M_Y2S_ee = new TH1F("h_M_Y2S_ee","M[Y(2S)->ee]",20,9.9232,10.1232);
    ptree->Project("h_M_Y2S_ee","Upsilon3S_chi_b12P_Upsilon2S_M",pidCut4);
    h_M_Y2S_ee->GetXaxis()->SetTitle("M_{e^{+}e^{-}} (Gev/c^{2})");
@@ -152,3 +155,4 @@ void test2_1730822030(){
 
 
 }
+

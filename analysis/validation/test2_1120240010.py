3a7241eb37b5524e4f517870007d2fa19a6a5ba8
Sam Cunliffe <samuel.cunliffe@pnnl.gov>
2017-10-31 12:51:12 +0900

    Adding q2 histograms.

--- analysis/validation/test2_1120240010.py
+++ analysis/validation/test2_1120240010.py
@@ -18,6 +18,28 @@
 
 from ROOT import TFile, TTree, TH1F, TNamed
 
+
+def sq_p_component(particle_a, particle_b, component):
+    """A variable string for a squared component of a particle's 3-momentum"""
+    p = '({A}_P4[{i}] + {B}_P4[{i}])'.format(
+        A=particle_a, B=particle_b, i=str(component))
+    return '(' + p + '*' + p + ')'
+
+
+def sumsq_p_string(particle_a, particle_b):
+    """Make a variable string for the squared sum of a particle's 3-momentum"""
+    xsq = sq_p_component(particle_a, particle_b, 0)
+    ysq = sq_p_component(particle_a, particle_b, 1)
+    zsq = sq_p_component(particle_a, particle_b, 2)
+    return xsq + '+' + ysq + '+' + zsq
+
+
+def inv_mass_sq(particle_a, particle_b):
+    """A variable string or the squared invariant mass of two particles"""
+    tsq = sq_p_component(particle_a, particle_b, 3)
+    return tsq + '-' + sumsq_p_string(particle_a, particle_b)
+
+
 # TTree of candidates, declare output file
 ifile = TFile("../1120240010.ntup.root", "READ")
 tree = ifile.Get("h1")
@@ -37,6 +59,10 @@ tree.Project("hdeltae", "B0_deltae")
 hmkpi = TH1F("hmkpi", title, 70, 0.795, 0.995)
 hmkpi.SetXTitle("m_{K#pi} (GeV/c^{2})")
 tree.Project("hmkpi", "B0_KST0_M")
+
+hq2 = TH1F("hq2", title, 50, 0., 25.0)
+hq2.SetXTitle("q^{2} (GeV^{2}/c^{4})")
+tree.Project("hq2", inv_mass_sq('B0_e0', 'B0_e1'))
 
 # add meaningful information to the histograms
 dmbc = TNamed("Description", "Reconstructed M_{bc} distribution for B^{0}#to K*(892)^{0}e^{+}e^{-} decays.")
@@ -61,6 +87,15 @@ pmkpi = TNamed("Contact", contactperson)
 hmkpi.GetListOfFunctions().Add(dmkpi)
 hmkpi.GetListOfFunctions().Add(cmkpi)
 hmkpi.GetListOfFunctions().Add(pmkpi)
+
+dq2 = TNamed(
+    "Description",
+    "Squared invariant mass of the pair of electrons (q^{2}) from B^{0}#to K*(892)^{0}e^{+}e^{-} decays.")
+cq2 = TNamed("Check", "Consistent curving shape turns and ends before 25")
+pq2 = TNamed("Contact", contactperson)
+hq2.GetListOfFunctions().Add(dq2)
+hq2.GetListOfFunctions().Add(cq2)
+hq2.GetListOfFunctions().Add(pq2)
 
 # save
 ofile.cd()

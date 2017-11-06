bf7fa3474f38d33d564a5b0a94c50425fd38d219
kodys <peter.kodys@mff.cuni.cz>
2017-11-06 20:15:24 +0100

    QDM for Tracking

---
+++ tracking/examples/DQMTracking_BelleII.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#############################################################
+# Simple steering file to demonstrate how to run Track DQM on BelleII geometry
+#############################################################
+
+from basf2 import *
+from simulation import add_simulation
+from pxd import add_pxd_reconstruction
+from svd import add_svd_reconstruction
+from tracking import add_tracking_reconstruction
+from reconstruction import add_reconstruction, add_mdst_output
+from L1trigger import add_tsim
+from ROOT import Belle2
+import glob
+
+
+# background (collision) files
+# put here the directory for phase2 bkg MC
+# bg = glob.glob('/group/belle2/users/jbennett/BG15th/phase2/set0/*.root') # if you run at KEKCC
+# bg = glob.glob('./BG/*.root')
+bg = None
+
+# number of events to generate, can be overriden with -n
+num_events = 100
+# output filename, can be overriden with -o
+output_filename = "RootOutput_BelleII.root"
+
+# create path
+main = create_path()
+
+# the sectormaps for Phase2 are currently only in the development tag
+# Also note that these SectorMaps are preliminary and may change
+# use_central_database("development", loglevel=LogLevel.INFO)
+
+# specify number of events to be generated
+# the experiment number has to be 1002, otherwise the wrong payloads (for VXDTF2 the SectorMap) are loaded
+# main.add_module("EventInfoSetter", expList=1002, runList=1, evtNumList=num_events)
+main.add_module('EventInfoSetter', evtNumList=num_events)
+
+# in case you need to fix seed of random numbers
+# set_random_seed(1111123)
+
+# generate BBbar events
+evtgeninput = register_module('EvtGenInput')
+main.add_module(evtgeninput)
+
+# Gearbox to override the default geometry with the one for Phase 2
+gearbox = register_module('Gearbox')
+# gearbox.param('fileName', 'geometry/Beast2_phase2.xml')
+main.add_module(gearbox)
+
+
+# detector simulation
+add_simulation(main, bkgfiles=bg)
+
+# remove the cache for background files to reduce memory
+if bg is not None:
+    set_module_parameters(main, "BeamBkgMixer", cacheSize=0)
+
+# trigger simulation
+# add_tsim(main)
+
+# reconstruction
+# add_reconstruction(main)
+add_pxd_reconstruction(main)
+add_svd_reconstruction(main)
+add_tracking_reconstruction(main, use_vxdtf2=True)
+
+# histomanager: use DqmHistoManager for in-line monitoring, or HistoManager for offline training
+# histomanager = register_module('DqmHistoManager', Port=7777)
+histomanager = register_module('HistoManager', histoFileName='Histos_DQMTracks_BelleII.root')
+main.add_module(histomanager)
+
+# DQM of tracking
+trackDQM = register_module('TrackDQM')
+# In case to see more details:
+trackDQM.logging.log_level = LogLevel.DEBUG
+trackDQM.logging.debug_level = 250
+main.add_module(trackDQM)
+
+# Finally add output, if you need
+# main.add_module("RootOutput", outputFileName=output_filename)
+
+# process events and print call statistics
+process(main)
+print(statistics)
---
+++ tracking/modules/DQM/src/TrackDQMModule.cc
@@ -0,0 +1,271 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2017 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Peter Kodys                                              *
+ *                                                                        *
+ * Prepared for cluster shape correction quality check                    *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <vxd/geometry/GeoCache.h>
+#include <pxd/geometry/SensorInfo.h>
+#include <genfit/MeasurementOnPlane.h>
+#include <tracking/modules/DQM/TrackDQMModule.h>
+#include <pxd/reconstruction/PXDClusterShape.h>
+#include <pxd/reconstruction/PXDRecoHit.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/RelationArray.h>
+#include <pxd/dataobjects/PXDFrame.h>
+#include <tracking/dataobjects/RecoTrack.h>
+#include <tracking/dataobjects/RecoHitInformation.h>
+#include <pxd/dataobjects/PXDTrueHit.h>
+#include <TFile.h>
+#include <pxd/reconstruction/HitCorrector.h>
+
+#include <framework/database/DBObjPtr.h>
+
+#include <TDirectory.h>
+#include <TVectorD.h>
+
+using namespace Belle2;
+using namespace Belle2::PXD;
+using namespace std;
+using boost::format;
+
+//class PXDClusterShapeCalibrationAlgorithm;
+
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(TrackDQM)
+
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+TrackDQMModule::TrackDQMModule() : HistoModule()
+{
+  //Set module properties
+  setDescription("DQM of finding tracks, their momentum, "
+                 "Number of hits in tracks, "
+                 "Number of tracks. "
+                );
+  setPropertyFlags(c_ParallelProcessingCertified);
+
+}
+
+
+TrackDQMModule::~TrackDQMModule()
+{
+}
+
+//------------------------------------------------------------------
+// Function to define histograms
+//-----------------------------------------------------------------
+
+void TrackDQMModule::initialize()
+{
+  // Register histograms (calls back defineHisto)
+  REG_HISTOGRAM
+
+  StoreArray<RecoTrack> recotracks(m_storeRecoTrackName);
+  m_storeRecoTrackName = recotracks.getName();
+}
+
+void TrackDQMModule::defineHisto()
+{
+  // Create a separate histogram directories and cd into it.
+  TDirectory* oldDir = gDirectory;
+  TDirectory* DirTracks = NULL;
+  DirTracks = oldDir->mkdir("TracksDQM");
+  DirTracks->cd();
+
+  m_MomX = NULL;
+  m_MomY = NULL;
+  m_MomZ = NULL;
+  m_Mom = NULL;
+  m_HitsPXD = NULL;
+  m_HitsSVD = NULL;
+  m_HitsCDC = NULL;
+  m_Hits = NULL;
+  m_TracksVXD = NULL;
+  m_TracksCDC = NULL;
+  m_TracksVXDCDC = NULL;
+  m_Tracks = NULL;
+
+  int iHitsInPXD = 10;
+  int iHitsInSVD = 20;
+  int iHitsInCDC = 200;
+  int iHits = 200;
+  int iTracks = 30;
+  int iMomRange = 600;
+  float fMomRange = 3.0;
+  string name = str(format("TrackMomentumX"));
+  string title = str(format("Track Momentum X"));
+  m_MomX = new TH1F(name.c_str(), title.c_str(), 2 * iMomRange, -fMomRange, fMomRange);
+  m_MomX->GetXaxis()->SetTitle("Momentum");
+  m_MomX->GetYaxis()->SetTitle("counts");
+  name = str(format("TrackMomentumY"));
+  title = str(format("Track Momentum Y"));
+  m_MomY = new TH1F(name.c_str(), title.c_str(), 2 * iMomRange, -fMomRange, fMomRange);
+  m_MomY->GetXaxis()->SetTitle("Momentum");
+  m_MomY->GetYaxis()->SetTitle("counts");
+  name = str(format("TrackMomentumZ"));
+  title = str(format("Track Momentum Z"));
+  m_MomZ = new TH1F(name.c_str(), title.c_str(), 2 * iMomRange, -fMomRange, fMomRange);
+  m_MomZ->GetXaxis()->SetTitle("Momentum");
+  m_MomZ->GetYaxis()->SetTitle("counts");
+  name = str(format("TrackMomentum"));
+  title = str(format("Track Momentum"));
+  m_Mom = new TH1F(name.c_str(), title.c_str(), 2 * iMomRange, 0.0, fMomRange);
+  m_Mom->GetXaxis()->SetTitle("Momentum");
+  m_Mom->GetYaxis()->SetTitle("counts");
+
+  name = str(format("NoOfHitsInTrack_PXD"));
+  title = str(format("No Of Hits In Track - PXD"));
+  m_HitsPXD = new TH1F(name.c_str(), title.c_str(), iHitsInPXD, 0, iHitsInPXD);
+  m_HitsPXD->GetXaxis()->SetTitle("# hits");
+  m_HitsPXD->GetYaxis()->SetTitle("counts");
+  name = str(format("NoOfHitsInTrack_SVD"));
+  title = str(format("No Of Hits In Track - SVD"));
+  m_HitsSVD = new TH1F(name.c_str(), title.c_str(), iHitsInSVD, 0, iHitsInSVD);
+  m_HitsSVD->GetXaxis()->SetTitle("# hits");
+  m_HitsSVD->GetYaxis()->SetTitle("counts");
+  name = str(format("NoOfHitsInTrack_CDC"));
+  title = str(format("No Of Hits In Track - CDC"));
+  m_HitsCDC = new TH1F(name.c_str(), title.c_str(), iHitsInCDC, 0, iHitsInCDC);
+  m_HitsCDC->GetXaxis()->SetTitle("# hits");
+  m_HitsCDC->GetYaxis()->SetTitle("counts");
+  name = str(format("NoOfHitsInTrack"));
+  title = str(format("No Of Hits In Track"));
+  m_Hits = new TH1F(name.c_str(), title.c_str(), iHits, 0, iHits);
+  m_Hits->GetXaxis()->SetTitle("# hits");
+  m_Hits->GetYaxis()->SetTitle("counts");
+
+  name = str(format("NoOfTracksInVXDOnly"));
+  title = str(format("No Of Tracks Per Event, Only In VXD"));
+  m_TracksVXD = new TH1F(name.c_str(), title.c_str(), iTracks, 0, iTracks);
+  m_TracksVXD->GetXaxis()->SetTitle("# tracks");
+  m_TracksVXD->GetYaxis()->SetTitle("counts");
+  name = str(format("NoOfTracksInCDCOnly"));
+  title = str(format("No Of Tracks Per Event, Only In CDC"));
+  m_TracksCDC = new TH1F(name.c_str(), title.c_str(), iTracks, 0, iTracks);
+  m_TracksCDC->GetXaxis()->SetTitle("# tracks");
+  m_TracksCDC->GetYaxis()->SetTitle("counts");
+  name = str(format("NoOfTracksInVXDCDC"));
+  title = str(format("No Of Tracks Per Event, In VXD+CDC"));
+  m_TracksVXDCDC = new TH1F(name.c_str(), title.c_str(), iTracks, 0, iTracks);
+  m_TracksVXDCDC->GetXaxis()->SetTitle("# tracks");
+  m_TracksVXDCDC->GetYaxis()->SetTitle("counts");
+  name = str(format("NoOfTracks"));
+  title = str(format("No Of All Tracks Per Event"));
+  m_Tracks = new TH1F(name.c_str(), title.c_str(), iTracks, 0, iTracks);
+  m_Tracks->GetXaxis()->SetTitle("# tracks");
+  m_Tracks->GetYaxis()->SetTitle("counts");
+
+  oldDir->cd();
+
+}
+
+void TrackDQMModule::beginRun()
+{
+  if (m_MomX != NULL) m_MomX->Reset();
+  if (m_MomY != NULL) m_MomY->Reset();
+  if (m_MomZ != NULL) m_MomZ->Reset();
+  if (m_Mom != NULL) m_Mom->Reset();
+  if (m_HitsPXD != NULL) m_HitsPXD->Reset();
+  if (m_HitsSVD != NULL) m_HitsSVD->Reset();
+  if (m_HitsCDC != NULL) m_HitsCDC->Reset();
+  if (m_Hits != NULL) m_Hits->Reset();
+  if (m_TracksVXD != NULL) m_TracksVXD->Reset();
+  if (m_TracksCDC != NULL) m_TracksCDC->Reset();
+  if (m_TracksVXDCDC != NULL) m_TracksVXDCDC->Reset();
+  if (m_Tracks != NULL) m_Tracks->Reset();
+}
+
+
+void TrackDQMModule::event()
+{
+  const StoreArray<PXDFrame> storeFrames(m_storeFramesName);
+  StoreArray<RecoTrack> recotracks(m_storeRecoTrackName);
+  int iTrack = 0;
+  int iTrackVXD = 0;
+  int iTrackCDC = 0;
+  int iTrackVXDCDC = 0;
+  for (auto& recoTrack : recotracks) {  // over recotracks
+    if (!recoTrack.wasFitSuccessful())
+      continue;
+    if (!recoTrack.getTrackFitStatus())
+      continue;
+    auto& genfitTrack = RecoTrackGenfitAccess::getGenfitTrack(recoTrack);
+    TString message = Form("TrackDQM: track %3i, Mom: %f, %f, %f, Pt: %f +- %f, Hits: ",
+                           iTrack,
+                           genfitTrack.getFittedState().getMom().X(),
+                           genfitTrack.getFittedState().getMom().Y(),
+                           genfitTrack.getFittedState().getMom().Z(),
+                           genfitTrack.getFittedState().getMom().Perp(),
+                           genfitTrack.getFittedState().getMomVar()
+                          );
+    int valAll = 0;
+    int val = 0;
+    if (recoTrack.getNumberOfPXDHits() > 0) {
+      val = recoTrack.getNumberOfPXDHits();
+    }
+    message = Form("%sPXD %i ", message.Data(), val);
+    valAll += val;
+    val = 0;
+    if (recoTrack.getNumberOfSVDHits() > 0) {
+      val = recoTrack.getNumberOfSVDHits();
+    }
+    message = Form("%sSVD %i ", message.Data(), val);
+    valAll += val;
+    val = 0;
+    if (recoTrack.getNumberOfCDCHits() > 0) {
+      val = recoTrack.getNumberOfCDCHits();
+    }
+    message = Form("%sCDC %i ", message.Data(), val);
+    valAll += val;
+    message = Form("%sSuma %i (%i) ", message.Data(), valAll, genfitTrack.getNumPoints());
+    B2DEBUG(230, message.Data());
+
+    iTrack++;
+    if (((recoTrack.getNumberOfPXDHits() > 0) || (recoTrack.getNumberOfSVDHits() > 0)) &&
+        (recoTrack.getNumberOfCDCHits() > 0)) iTrackVXDCDC++;
+    if (((recoTrack.getNumberOfPXDHits() > 0) || (recoTrack.getNumberOfSVDHits() > 0)) &&
+        (recoTrack.getNumberOfCDCHits() == 0)) iTrackVXD++;
+    if (((recoTrack.getNumberOfPXDHits() == 0) && (recoTrack.getNumberOfSVDHits() == 0)) &&
+        (recoTrack.getNumberOfCDCHits() > 0)) iTrackCDC++;
+    // if ((recoTrack.getNumberOfPXDHits() > 0) || (recoTrack.getNumberOfSVDHits() > 0)) iTrackVXD++;
+    // if (recoTrack.getNumberOfCDCHits() > 0) iTrackCDC++;
+    if (m_MomX != NULL) m_MomX->Fill(genfitTrack.getFittedState().getMom().X());
+    if (m_MomY != NULL) m_MomY->Fill(genfitTrack.getFittedState().getMom().Y());
+    if (m_MomZ != NULL) m_MomZ->Fill(genfitTrack.getFittedState().getMom().Z());
+    if (m_Mom != NULL) m_Mom->Fill(genfitTrack.getFittedState().getMom().Perp());
+    if (m_HitsPXD != NULL) m_HitsPXD->Fill(recoTrack.getNumberOfPXDHits());
+    if (m_HitsSVD != NULL) m_HitsSVD->Fill(recoTrack.getNumberOfSVDHits());
+    if (m_HitsCDC != NULL) m_HitsCDC->Fill(recoTrack.getNumberOfCDCHits());
+    // if (m_Hits != NULL) m_Hits->Fill(genfitTrack.getNumPoints()); // not works for Phase2 geometry!!!
+    if (m_Hits != NULL) m_Hits->Fill(recoTrack.getNumberOfPXDHits() +
+                                       recoTrack.getNumberOfSVDHits() + recoTrack.getNumberOfCDCHits()
+                                      );
+  }
+  if (m_TracksVXD != NULL) m_TracksVXD->Fill(iTrackVXD);
+  if (m_TracksCDC != NULL) m_TracksCDC->Fill(iTrackCDC);
+  if (m_TracksVXDCDC != NULL) m_TracksVXDCDC->Fill(iTrackVXDCDC);
+  if (m_Tracks != NULL) m_Tracks->Fill(iTrack);
+}
+
+
+void TrackDQMModule::endRun()
+{
+}
+
+
+void TrackDQMModule::terminate()
+{
+}

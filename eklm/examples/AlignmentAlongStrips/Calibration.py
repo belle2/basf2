9637a15b204917923fb4535f291d72289a5b85d9
David Dossett <david.dossett@desy.de>
2017-11-08 16:17:23 +1100

    Updating EKLM Strip alignment

--- eklm/examples/AlignmentAlongStrips/Calibration.py
+++ eklm/examples/AlignmentAlongStrips/Calibration.py
@@ -11,13 +11,13 @@ import sys
 # Set the global log level
 set_log_level(LogLevel.INFO)
 
-root_input = register_module('RootInput')
-root_input.param('inputFileName', sys.argv[1])
-root_input.initialize()
+inputFileNames = [sys.argv[1]]
 
 gearbox = register_module('Gearbox')
 gearbox.initialize()
 
 algo = Belle2.EKLMAlignmentAlongStripsAlgorithm()
+algo.setInputFileNames(inputFileNames)
 algo.setOutputFile('alignment_along_strips.root')
 algo.execute()
+algo.commit()

52d528116b0d6d23d4b129e4454497915b53ce2f
Makoto Uchida <uchida@phys.titech.ac.jp>
2017-11-25 18:14:52 +0900

    Add time walk calibration algorithm Add script for time walk calibration algorithm

--- cdc/calibration/include/linkdef.h
+++ cdc/calibration/include/linkdef.h
@@ -13,4 +13,5 @@
 #pragma link C++ class Belle2::CDC::T0CalibrationAlgorithm-;
 #pragma link C++ class Belle2::CDC::XTCalibrationAlgorithm-;
 #pragma link C++ class Belle2::CDC::SpaceResolutionCalibrationAlgorithm-;
+#pragma link C++ class Belle2::CDC::TimeWalkCalibrationAlgorithm-;
 #endif

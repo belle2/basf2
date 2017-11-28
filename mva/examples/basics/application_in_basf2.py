6912b4dd92814b8568862785a0265835bcac56e6
Thomas Keck <thomas.keck2@kit.edu>
2017-11-28 10:44:24 +0100

    Added missing documentation to adversary example

--- mva/examples/basics/application_in_basf2.py
+++ mva/examples/basics/application_in_basf2.py
@@ -10,7 +10,7 @@ from modularAnalysis import *
 import create_data_sample
 
 if __name__ == "__main__":
-    path = create_data_sample.reconstruction_path(['/storage/jbod/tkeck/MC6/evtgen-charged/sub00/mdst_0001*.root'])
+    path = create_data_sample.reconstruction_path(['mdst_002001_prod00000789_task00004203.root'])
     path.add_module('MVAExpert', listNames=['D0'], extraInfoName='Test', identifier='weightfile.root')
     variablesToNTuple('D0', ['isSignal', 'extraInfo(Test)'], path=path)
     process(path)

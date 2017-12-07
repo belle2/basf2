belle2/basf2

commit 513d3c9fc4f9bd71009cba342b92f6492beea63b

Updates in readout PCs

daq/slc/copper/ecl/src/ECLFEE.cc
@@ -15,7 +15,7 @@ const int HSLB_HARDWARE_VERSION = 0xA;
 
 using namespace Belle2;
 
-ECLFEE::ECLFEE() : FEE("ecl")
+ECLFEE::ECLFEE()
 {
 }
 
@@ -39,63 +39,53 @@ void ECLFEE::init(RCCallback& callback, HSLB& hslb, const DBObject& /*obj*/)
   callback.add(new FEE8Handler(vname + "calib_event_per_step", callback, hslb, *this, 0x48));
 }
 
-void ECLFEE::rio_sh_wreg(RCCallback& callback, HSLB& hslb, unsigned int sh_num, unsigned int reg_num, unsigned int reg_wdata)
-{
-  unsigned int value = (reg_num << 16) + reg_wdata;
-  callback.log(LogFile::INFO, "write 0x00B0<<0x%x", value);
-  callback.log(LogFile::INFO, "write 0x00B2<<0x%x", sh_num);
-  hslb.writefee32(0x00B0, value);
-  hslb.writefee32(0x00B2, sh_num);
-  hslb.writefee32(0x00B8, 0x4);
-  callback.log(LogFile::INFO, "read 0x00B8>>0x%x", hslb.readfee32(0x00B8));
-  callback.log(LogFile::INFO, "read 0x00B9>>0x%x", hslb.readfee32(0x00B9));
-  callback.log(LogFile::INFO, "read 0x00B8>>0x%x", hslb.readfee32(0x00B8));
-}
-
 void ECLFEE::boot(RCCallback& callback, HSLB& hslb, const DBObject& obj)
 {
+  /*
+  int ver;
+  if ((ver = hslb.readfee8(HSREG_HWVER)) != HSLB_HARDWARE_VERSION) {
+    throw (IOException("Inconsitent HWVER (%d!=%d)",
+                       ver, HSLB_HARDWARE_VERSION));
+  }
+  if ((ver = hslb.readfee8(HSREG_FWVER)) != HSLB_FIRMWARE_VERSION) {
+    throw (IOException("Inconsitent FWVER (%d!=%d)",
+                       ver, HSLB_FIRMWARE_VERSION));
+  }
+  hslb.writefee8(0x30, 0x00);
+  */
 }
 
 void ECLFEE::load(RCCallback& callback, HSLB& hslb, const DBObject& obj)
 {
-  callback.log(LogFile::INFO, "Load ECL config");
-  const DBObjectList o_sh_datas(obj.getObjects("sh_data"));
-  for (size_t i = 0; i < o_sh_datas.size(); i++) {
-    const DBObject& o_sh_data(o_sh_datas[i]);
-    unsigned int sh_num  = o_sh_data.getInt("sh_num");//0xFFF;
-    unsigned int reg_num = o_sh_data.getInt("reg_num");
-    unsigned int reg_wdata = o_sh_data.getInt("reg_wdata");
-    rio_sh_wreg(callback, hslb, sh_num, reg_num, reg_wdata);
+  hslb.writefee32(0x00B0, (obj.getInt("reg_num") << 16) & obj.getInt("reg_wdata"));
+  hslb.writefee32(0x00B2, obj.getInt("sh_mask"));
+  hslb.writefee32(0x00B8, 0x4);
+  int er_code = 1;
+  for (int i = 0; i < 1000; i++) {
+    er_code = hslb.readfee32(0x00B8);
+    if (er_code == 00000000) break;
+    usleep(10);
   }
+  if (er_code != 00000000) {
+    LogFile::error("Failed to set sh_wreg");
+  }
+
   hslb.writefee8(0x20, obj.getInt("shaper_mask_low"));
   hslb.writefee8(0x21, obj.getInt("shaper_mask_high"));
-  callback.log(LogFile::INFO, "write fee-8 %x << %x", 0x38, obj.getInt("ttd_trg_rare_factor"));
   hslb.writefee8(0x38, obj.getInt("ttd_trg_rare_factor"));
-  callback.log(LogFile::INFO, "read fee-8 %x >> %x", 0x38, hslb.readfee8(0x38));
-  callback.log(LogFile::INFO, "write fee-8 %x << %x", 0x39, obj.getInt("ttd_trg_type"));
   hslb.writefee8(0x39, obj.getInt("ttd_trg_type"));
-  callback.log(LogFile::INFO, "read fee-8 %x >> %x", 0x39, hslb.readfee8(0x39));
   hslb.writefee8(0x40, obj.getInt("calib_ampl0_low"));
   hslb.writefee8(0x41, obj.getInt("calib_ampl0_high"));
-  hslb.writefee8(0x42, obj.getInt("calib_ampl_step_low"));
+  hslb.writefee8(0x42, obj.getInt("calib_ampl_step_high"));
   hslb.writefee8(0x43, obj.getInt("calib_ampl_step_high"));
 
-  hslb.writefee8(0x45, obj.getInt("calib_delay0_low"));
   hslb.writefee8(0x45, obj.getInt("calib_delay0_high"));
   hslb.writefee8(0x46, obj.getInt("calib_delay_step_low"));
   hslb.writefee8(0x47, obj.getInt("calib_delay_step_high"));
-
   hslb.writefee8(0x48, obj.getInt("calib_events_per_step"));
 
-  const DBObjectList o_reg30s(obj.getObjects("reg30"));
-  for (size_t i = 0; i < o_reg30s.size(); i++) {
-    const DBObject& o_reg30(o_reg30s[i]);
-    unsigned int val = o_reg30.getInt("val");
-    hslb.writefee8(0x30, val);
-    callback.log(LogFile::INFO, "write fee-8 %x >> %x", 0x30, val);
-  }
-  //hslb.writefee8(0x30, 0x03D);
-  //hslb.writefee8(0x30, 0x039);
+  hslb.writefee8(0x30, 0x0D);
+  hslb.writefee8(0x30, 0x09);
 }
 
 extern "C" {

commit 6b7771a2fcafec9fe1baf39c97971c19f7dbddda

Quickfix of errors in slc/.../ECLFEE.cc introduced by commit 513d3c9fc4f

daq/slc/copper/ecl/src/ECLFEE.cc
@@ -15,7 +15,7 @@ const int HSLB_HARDWARE_VERSION = 0xA;
 
 using namespace Belle2;
 
-ECLFEE::ECLFEE()
+ECLFEE::ECLFEE() : FEE("ecl")
 {
 }
 
@@ -39,53 +39,63 @@ void ECLFEE::init(RCCallback& callback, HSLB& hslb, const DBObject& /*obj*/)
   callback.add(new FEE8Handler(vname + "calib_event_per_step", callback, hslb, *this, 0x48));
 }
 
+void ECLFEE::rio_sh_wreg(RCCallback& callback, HSLB& hslb, unsigned int sh_num, unsigned int reg_num, unsigned int reg_wdata)
+{
+  unsigned int value = (reg_num << 16) + reg_wdata;
+  callback.log(LogFile::INFO, "write 0x00B0<<0x%x", value);
+  callback.log(LogFile::INFO, "write 0x00B2<<0x%x", sh_num);
+  hslb.writefee32(0x00B0, value);
+  hslb.writefee32(0x00B2, sh_num);
+  hslb.writefee32(0x00B8, 0x4);
+  callback.log(LogFile::INFO, "read 0x00B8>>0x%x", hslb.readfee32(0x00B8));
+  callback.log(LogFile::INFO, "read 0x00B9>>0x%x", hslb.readfee32(0x00B9));
+  callback.log(LogFile::INFO, "read 0x00B8>>0x%x", hslb.readfee32(0x00B8));
+}
+
 void ECLFEE::boot(RCCallback& callback, HSLB& hslb, const DBObject& obj)
 {
-  /*
-  int ver;
-  if ((ver = hslb.readfee8(HSREG_HWVER)) != HSLB_HARDWARE_VERSION) {
-    throw (IOException("Inconsitent HWVER (%d!=%d)",
-                       ver, HSLB_HARDWARE_VERSION));
-  }
-  if ((ver = hslb.readfee8(HSREG_FWVER)) != HSLB_FIRMWARE_VERSION) {
-    throw (IOException("Inconsitent FWVER (%d!=%d)",
-                       ver, HSLB_FIRMWARE_VERSION));
-  }
-  hslb.writefee8(0x30, 0x00);
-  */
 }
 
 void ECLFEE::load(RCCallback& callback, HSLB& hslb, const DBObject& obj)
 {
-  hslb.writefee32(0x00B0, (obj.getInt("reg_num") << 16) & obj.getInt("reg_wdata"));
-  hslb.writefee32(0x00B2, obj.getInt("sh_mask"));
-  hslb.writefee32(0x00B8, 0x4);
-  int er_code = 1;
-  for (int i = 0; i < 1000; i++) {
-    er_code = hslb.readfee32(0x00B8);
-    if (er_code == 00000000) break;
-    usleep(10);
+  callback.log(LogFile::INFO, "Load ECL config");
+  const DBObjectList o_sh_datas(obj.getObjects("sh_data"));
+  for (size_t i = 0; i < o_sh_datas.size(); i++) {
+    const DBObject& o_sh_data(o_sh_datas[i]);
+    unsigned int sh_num  = o_sh_data.getInt("sh_num");//0xFFF;
+    unsigned int reg_num = o_sh_data.getInt("reg_num");
+    unsigned int reg_wdata = o_sh_data.getInt("reg_wdata");
+    rio_sh_wreg(callback, hslb, sh_num, reg_num, reg_wdata);
   }
-  if (er_code != 00000000) {
-    LogFile::error("Failed to set sh_wreg");
-  }
-
   hslb.writefee8(0x20, obj.getInt("shaper_mask_low"));
   hslb.writefee8(0x21, obj.getInt("shaper_mask_high"));
+  callback.log(LogFile::INFO, "write fee-8 %x << %x", 0x38, obj.getInt("ttd_trg_rare_factor"));
   hslb.writefee8(0x38, obj.getInt("ttd_trg_rare_factor"));
+  callback.log(LogFile::INFO, "read fee-8 %x >> %x", 0x38, hslb.readfee8(0x38));
+  callback.log(LogFile::INFO, "write fee-8 %x << %x", 0x39, obj.getInt("ttd_trg_type"));
   hslb.writefee8(0x39, obj.getInt("ttd_trg_type"));
+  callback.log(LogFile::INFO, "read fee-8 %x >> %x", 0x39, hslb.readfee8(0x39));
   hslb.writefee8(0x40, obj.getInt("calib_ampl0_low"));
   hslb.writefee8(0x41, obj.getInt("calib_ampl0_high"));
-  hslb.writefee8(0x42, obj.getInt("calib_ampl_step_high"));
+  hslb.writefee8(0x42, obj.getInt("calib_ampl_step_low"));
   hslb.writefee8(0x43, obj.getInt("calib_ampl_step_high"));
 
+  hslb.writefee8(0x45, obj.getInt("calib_delay0_low"));
   hslb.writefee8(0x45, obj.getInt("calib_delay0_high"));
   hslb.writefee8(0x46, obj.getInt("calib_delay_step_low"));
   hslb.writefee8(0x47, obj.getInt("calib_delay_step_high"));
+
   hslb.writefee8(0x48, obj.getInt("calib_events_per_step"));
 
-  hslb.writefee8(0x30, 0x0D);
-  hslb.writefee8(0x30, 0x09);
+  const DBObjectList o_reg30s(obj.getObjects("reg30"));
+  for (size_t i = 0; i < o_reg30s.size(); i++) {
+    const DBObject& o_reg30(o_reg30s[i]);
+    unsigned int val = o_reg30.getInt("val");
+    hslb.writefee8(0x30, val);
+    callback.log(LogFile::INFO, "write fee-8 %x >> %x", 0x30, val);
+  }
+  //hslb.writefee8(0x30, 0x03D);
+  //hslb.writefee8(0x30, 0x039);
 }
 
 extern "C" {
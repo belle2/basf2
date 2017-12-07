f62a7ac8be0daf661b7d75c63e54dd52795e2dd5
Jo-Frederik Krohn <jo-frederik.krohn@desy.de>
2017-12-07 09:49:40 +0000

    forgot the most important part ...

---
+++ analysis/tests/test_TreeFitter.cc
@@ -0,0 +1,10 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2017 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors:  Jo-Frederik Krohn                                       *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#include "TreeFitter/test_Fitparams.h"

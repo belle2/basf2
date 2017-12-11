5b7590d641222e5f7e2588e612796098347c3a6f
Nils Braun <nils.braun@desy.de>
2017-12-11 09:00:01 +0100

    Set the cdc tracks name in the steering file

--- tracking/ckf/scripts/ckf/path_functions.py
+++ tracking/ckf/scripts/ckf/path_functions.py
@@ -34,6 +34,7 @@ def add_ckf_based_merger(path, cdc_reco_tracks, svd_reco_tracks, use_mc_truth, r
 
                     inputRecoTrackStoreArrayName=cdc_reco_tracks,
                     outputRecoTrackStoreArrayName=svd_reco_tracks,
+                    cdcTracksStoreArrayName=cdc_reco_tracks,
                     vxdTracksStoreArrayName=svd_reco_tracks,
 
                     firstHighFilterParameters={"direction": direction},

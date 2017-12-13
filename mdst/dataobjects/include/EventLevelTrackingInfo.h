b4a8d89827c6211dc2855b1642fb05ba95d4270d
Martin Heck <martin.heck@desy.de>
2017-12-13 13:11:16 +0100

    Adding number of clusters per VXD layer.

--- mdst/dataobjects/include/EventLevelTrackingInfo.h
+++ mdst/dataobjects/include/EventLevelTrackingInfo.h
@@ -12,6 +12,7 @@
 #include <mdst/dataobjects/HitPatternCDC.h>
 
 #include <framework/datastore/RelationsObject.h>
+#include <framework/logging/Logger.h>
 
 namespace Belle2 {
   /** Tracking-related info on event-level, for example number of unassigned measurements.
@@ -23,14 +24,15 @@ namespace Belle2 {
    */
   class EventLevelTrackingInfo : public RelationsObject {
   public:
+    //--- CDC related Stuff -------------------------------------------------------------------------------------------
     /** Getter for number of CDC measurements, that are not assigned to any Track. */
-    unsigned short getNCDCHitsNotAssigned() const
+    unsigned short getNHitsNotAssigned() const
     {
       return m_nCDCHitsNotAssigned;
     }
 
     /** Setter for number of CDC measurements, that are not assigned to any Track. */
-    void setNCDCHitsNotAssigned(unsigned short nCDCHitsNotAssigned)
+    void setNHitsNotAssigned(unsigned short nCDCHitsNotAssigned)
     {
       m_nCDCHitsNotAssigned = nCDCHitsNotAssigned;
     }
@@ -44,13 +46,13 @@ namespace Belle2 {
       *  is performed. Here we want to get only the number of those CDC hits, that survive that cut,
       *  but are then not used for an actual Track.
       */
-    unsigned short getNCDCHitsNotAssignedPostCleaning() const
+    unsigned short getNHitsNotAssignedPostCleaning() const
     {
       return m_nCDCHitsNotAssignedPostCleaning;
     }
 
     /** Setter for number of CDC measurements, that are not assigned to any Track nor very likely beam-background. */
-    void setNCDCHitsNotAssignedPostCleaning(unsigned short nCDCHitsNotAssignedPostCleaning)
+    void setNHitsNotAssignedPostCleaning(unsigned short nCDCHitsNotAssignedPostCleaning)
     {
       m_nCDCHitsNotAssignedPostCleaning = nCDCHitsNotAssignedPostCleaning;
     }
@@ -61,13 +63,13 @@ namespace Belle2 {
      *  @param  cdcLayer  Specification, which layer in the CDC shall be tested for a hit.
      *  @return true, if a non-assigned hit exists in the specified layer.
      */
-    bool hasCDCLayer(unsigned short cdcLayer) const
+    bool hasLayer(unsigned short cdcLayer) const
     {
       return HitPatternCDC(m_hitPatternCDCInitializer).hasLayer(cdcLayer);
     }
 
     /** Setter for presence of hit in specific CDC Layer. */
-    void setCDCLayer(unsigned short cdcLayer)
+    void setLayer(unsigned short cdcLayer)
     {
       HitPatternCDC hitPatternCDC(m_hitPatternCDCInitializer);
       hitPatternCDC.setLayer(cdcLayer);
@@ -75,7 +77,7 @@ namespace Belle2 {
     }
 
     /** Getter for the presence of hit in a SuperLayer. */
-    bool hasCDCSLayer(unsigned short cdcSLayer) const
+    bool hasSLayer(unsigned short cdcSLayer) const
     {
       return HitPatternCDC(m_hitPatternCDCInitializer).hasSLayer(cdcSLayer);
     }
@@ -86,17 +88,61 @@ namespace Belle2 {
      *  We want to store the number of segments, that we couldn't attach to any Track.
      *  The maximum number of unstored segments is 255.
      */
-    unsigned short getCDCNSegments() const
+    unsigned short getNSegments() const
     {
       return HitPatternCDC(m_hitPatternCDCInitializer).getNHits();
     }
 
     /** Setter for number of Segments not used in Tracks. */
-    void setCDCNSegments(unsigned short nHits)
+    void setNSegments(unsigned short nHits)
     {
       HitPatternCDC hitPatternCDC(m_hitPatternCDCInitializer);
       hitPatternCDC.setNHits(nHits);
       m_hitPatternCDCInitializer = hitPatternCDC.getInteger();
+    }
+
+    //--- VXD related stuff -------------------------------------------------------------------------------------------
+    /** Getter for number of clusters in a specific VXD layer, SVD separated by direction.
+     *
+     *  All clusters, that have been part of a RecoTrack, that was converted into a Track, are removed.
+     *  @param layer  1 to 6 for respective VXD layer for which you want to have the remaining clusters.
+     *  @param isU    only used for layers 3 to 6, set true for u direction, false for v direction.
+     */
+    unsigned short getNClustersInLayer(unsigned short const layer, bool const isU)
+    {
+      if (layer == 1 or layer == 2) {
+        return m_nPXDClusters[layer - 1];
+      }
+      if (layer > 2 and layer < 7) {
+        return m_nSVDClusters[layer - 3 + 4 * isU];
+      }
+      B2FATAL("The VXD only has layer 1 to 6, but you asked for " << layer);
+    }
+
+    /** Getter for number of clsuters in specific VXD layer, SVD directions are accumulated.
+     *
+     *  @param layer  1 to 6 for respective VXD layer, for which you want to have the remaining clusters.
+     */
+    unsigned short getNClustersInLayer(unsigned short const layer)
+    {
+      if (layer == 1 or layer == 2) {
+        return getNClustersInLayer(layer, true);
+      }
+      return getNClustersInLayer(layer, true) + getNClustersInLayer(layer, false);
+    }
+
+    /** Setter for number of clusters in specific VXD layer, SVD directions are separated. */
+    void setNClustersInLayer(unsigned short const layer, bool const isU, unsigned short const nClusters)
+    {
+      if (layer == 1 or layer == 2) {
+        m_nPXDClusters[layer - 1] = nClusters;
+        return;
+      }
+      if (layer > 2 and layer < 7) {
+        m_nSVDClusters[layer - 3 + 4 * isU] = nClusters;
+        return;
+      }
+      B2FATAL("The VXD only has layer 1 to 6, but you asked for " << layer);
     }
 
   private:
@@ -121,10 +167,24 @@ namespace Belle2 {
       *  supposed to be written out, we only store an initalizer integer.
       *  As we are expecting a larger number of hits, some functionality can't be used and
       *  the corresponding information is handled with speparate members.
+      *  Instead we store the number of segments into the bits reserved for number of hits in the Track.
       *
       *  @sa HitPatternCDC
       */
     uint64_t m_hitPatternCDCInitializer {0};
+
+    /** Storage for number of clusters in the PXD.
+     *
+     *  We can store up to 65'535 cluster per layer here. This is slightly above the hardware readout limit,
+     *  and therefore sufficient.
+     */
+    uint16_t m_nPXDClusters[2] {0, 0};
+
+    /** Storage for number of clusters in the SVD
+     *
+     *
+     */
+    uint8_t m_nSVDClusters[8] {0, 0, 0, 0, 0, 0, 0, 0};
 
     ClassDef(EventLevelTrackingInfo, 1); /**< ROOTification. */
   };
--- mdst/tests/eventLevelTrackingInfo.cc
+++ mdst/tests/eventLevelTrackingInfo.cc
@@ -9,40 +9,54 @@ namespace Belle2 {
   protected:
   };
 
-  /** Test simple Setters, Getters, and Hassers. */
-  TEST_F(EventLevelTrackingInfoTest, settersNGetters)
+  /** Test simple Setters, Getters, and Hassers for the CDC related part. */
+  TEST_F(EventLevelTrackingInfoTest, settersNGettersCDC)
   {
     // Test proper initialization for an empty afterimage.
     EventLevelTrackingInfo eventLevelTrackingInfo;
-    EXPECT_EQ(eventLevelTrackingInfo.getNCDCHitsNotAssigned(), 0);
-    EXPECT_EQ(eventLevelTrackingInfo.getNCDCHitsNotAssignedPostCleaning(), 0);
+    EXPECT_EQ(eventLevelTrackingInfo.getNHitsNotAssigned(), 0);
+    EXPECT_EQ(eventLevelTrackingInfo.getNHitsNotAssignedPostCleaning(), 0);
 
     //Let's set the total number CDC hits in the afterimage
-    eventLevelTrackingInfo.setNCDCHitsNotAssigned(324);
-    eventLevelTrackingInfo.setNCDCHitsNotAssignedPostCleaning(257);
+    eventLevelTrackingInfo.setNHitsNotAssigned(324);
+    eventLevelTrackingInfo.setNHitsNotAssignedPostCleaning(257);
 
-    EXPECT_EQ(eventLevelTrackingInfo.getNCDCHitsNotAssigned(), 324);
-    EXPECT_EQ(eventLevelTrackingInfo.getNCDCHitsNotAssignedPostCleaning(), 257);
+    EXPECT_EQ(eventLevelTrackingInfo.getNHitsNotAssigned(), 324);
+    EXPECT_EQ(eventLevelTrackingInfo.getNHitsNotAssignedPostCleaning(), 257);
 
     //Let's set and get information for the various layers:
-    eventLevelTrackingInfo.setCDCLayer(9); // SL1
-    EXPECT_TRUE(eventLevelTrackingInfo.hasCDCLayer(9));
-    EXPECT_FALSE(eventLevelTrackingInfo.hasCDCLayer(10));
-    eventLevelTrackingInfo.setCDCLayer(23);
-    EXPECT_TRUE(eventLevelTrackingInfo.hasCDCLayer(23));
-    EXPECT_FALSE(eventLevelTrackingInfo.hasCDCLayer(55));
+    eventLevelTrackingInfo.setLayer(9); // SL1
+    EXPECT_TRUE(eventLevelTrackingInfo.hasLayer(9));
+    EXPECT_FALSE(eventLevelTrackingInfo.hasLayer(10));
+    eventLevelTrackingInfo.setLayer(23);
+    EXPECT_TRUE(eventLevelTrackingInfo.hasLayer(23));
+    EXPECT_FALSE(eventLevelTrackingInfo.hasLayer(55));
 
-    EXPECT_B2FATAL(eventLevelTrackingInfo.setCDCLayer(56));
-    EXPECT_B2FATAL(eventLevelTrackingInfo.hasCDCLayer(56));
+    EXPECT_B2FATAL(eventLevelTrackingInfo.setLayer(56));
+    EXPECT_B2FATAL(eventLevelTrackingInfo.hasLayer(56));
 
     //Let's have a look at the SuperLayers ...
-    EXPECT_TRUE(eventLevelTrackingInfo.hasCDCSLayer(1));
-    EXPECT_FALSE(eventLevelTrackingInfo.hasCDCSLayer(5));
+    EXPECT_TRUE(eventLevelTrackingInfo.hasSLayer(1));
+    EXPECT_FALSE(eventLevelTrackingInfo.hasSLayer(5));
 
     //Let's set and get the information on unused segments:
-    eventLevelTrackingInfo.setCDCNSegments(21);
-    EXPECT_EQ(eventLevelTrackingInfo.getCDCNSegments(), 21);
-    eventLevelTrackingInfo.setCDCNSegments(300);
-    EXPECT_EQ(eventLevelTrackingInfo.getCDCNSegments(), 255);
+    eventLevelTrackingInfo.setNSegments(21);
+    EXPECT_EQ(eventLevelTrackingInfo.getNSegments(), 21);
+    eventLevelTrackingInfo.setNSegments(300);
+    EXPECT_EQ(eventLevelTrackingInfo.getNSegments(), 255);
   }
+  /** Test simple Setters and Getters for the VXD related part. */
+  TEST_F(EventLevelTrackingInfoTest, settersNGettersVXD)
+  {
+    EventLevelTrackingInfo eventLevelTrackingInfo;
+    EXPECT_EQ(eventLevelTrackingInfo.getNClustersInLayer(3), 0);
+    eventLevelTrackingInfo.setNClustersInLayer(3, true, 81);
+    EXPECT_EQ(eventLevelTrackingInfo.getNClustersInLayer(3), 81);
+    eventLevelTrackingInfo.setNClustersInLayer(2, true, 3456);
+    EXPECT_EQ(eventLevelTrackingInfo.getNClustersInLayer(2), 3456);
+    EXPECT_EQ(eventLevelTrackingInfo.getNClustersInLayer(3, true), 81);
+    EXPECT_EQ(eventLevelTrackingInfo.getNClustersInLayer(3, false), 0);
+    EXPECT_B2FATAL(eventLevelTrackingInfo.getNClustersInLayer(56));
+  }
+
 } // namespace Belle2

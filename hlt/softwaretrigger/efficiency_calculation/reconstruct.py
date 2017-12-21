f238102a6b6cb3765183cb66f107dc260af653f8
Nils Braun <nils.braun@desy.de>
2017-12-21 16:28:05 +0100

    Added a proper reconstruct script for the HLT efficiency testing

--- hlt/softwaretrigger/efficiency_calculation/reconstruct.py
+++ hlt/softwaretrigger/efficiency_calculation/reconstruct.py
@@ -1,23 +1,43 @@
 import basf2
 import os
 
+from softwaretrigger.path_functions import add_softwaretrigger_reconstruction, DEFAULT_HLT_COMPONENTS, \
+    RAW_SAVE_STORE_ARRAYS
+
+from rawdata import add_unpackers
+from simulation import add_roiFinder
+
 
 def main():
     # Get all parameters for this calculation
     input_file = os.environ.get("input_file")
-    output_file = input_file.replace("/generated/", "/reconstructed/")
+    output_file = os.environ.get("output_file")
 
-    # Create output directory
-    output_dir = os.path.dirname(output_file)
+    raw_output_file = output_file.replace(".root", "_raw.root")
 
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    log_file = output_file.replace(".root", ".log")
 
-    # Do the real calculation
-    # TODO: Fake
-    with open(output_file, "w") as output_f:
-        with open(input_file, "r") as input_f:
-            output_f.write(input_f.read())
+    # Now start the real basf2 calculation
+    path = basf2.create_path()
+    path.add_module("RootInput", inputFileName=input_file)
+
+    add_unpackers(path, components=DEFAULT_HLT_COMPONENTS)
+
+    # Add the ST and also write out all variables connected to it. Also, do not cut, but just write out the variables
+    add_softwaretrigger_reconstruction(path, store_array_debug_prescale=1, softwaretrigger_mode="monitoring")
+
+    # TODO: until the ROI finding HLT setup is handled properly, we have to do this "manually" here
+    add_roiFinder(path, reco_tracks="RecoTracks")
+
+    path.add_module("RootOutput", outputFileName=output_file,
+                    branchNames=["EventMetaData", "SoftwareTriggerResult", "SoftwareTriggerVariables"])
+
+    path.add_module("RootOutput", outputFileName=raw_output_file,
+                    branchNames=["EventMetaData", "SoftwareTriggerResult"] + RAW_SAVE_STORE_ARRAYS)
+
+    basf2.log_to_file(log_file)
+    basf2.print_path(path)
+    basf2.process(path)
 
 
 if __name__ == "__main__":

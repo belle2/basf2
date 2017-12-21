e90f03bea033e0490b37d7873795151937649535
Felix Metzner <felix.metzner@kit.edu>
2017-12-21 17:07:07 +0100

    Removing directory displayInterfaceTF which contained only vxdtf1 code.

--- tracking/trackFindingVXD/displayInterfaceTF/include/linkdef.h
+++ tracking/trackFindingVXD/displayInterfaceTF/include/linkdef.h
@@ -5,12 +5,3 @@
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
-#pragma link C++ class Belle2::BaseTFInfo+;
-#pragma link C++ class Belle2::CellTFInfo+;
-#pragma link C++ class Belle2::SectorTFInfo+;
-#pragma link C++ class Belle2::HitTFInfo+;
-#pragma link C++ class Belle2::CollectorTFInfo+;
-#pragma link C++ class Belle2::AnalizerCollectorTFInfo+;
-#pragma link C++ class Belle2::TrackCandidateTFInfo+;
-#pragma link C++ class Belle2::ClusterTFInfo+;
-

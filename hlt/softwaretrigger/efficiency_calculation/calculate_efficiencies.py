cc4790b9fca1998b9cc4d733ff2024f754a725df
Nils Braun <nils.braun@desy.de>
2017-12-21 17:39:46 +0100

    Put everything together in the main script

--- hlt/softwaretrigger/efficiency_calculation/calculate_efficiencies.py
+++ hlt/softwaretrigger/efficiency_calculation/calculate_efficiencies.py
@@ -2,6 +2,7 @@ from glob import glob
 import os
 import argparse
 
+from extract import extract_efficiencies, extract_file_sizes
 from gridcontrol_helper import write_gridcontrol_file, call_gridcontrol
 
 
@@ -10,15 +11,18 @@ def generate_events(channels, n_events, n_jobs, storage_location):
 
     for channel in channels:
         for number in range(n_jobs):
-            parameter = {}
-
-            parameter["channel"] = channel
-            parameter["random_seed"] = number + 1111
-
             channel_path = os.path.join(storage_location, channel)
             generated_path = os.path.join(channel_path, "generated")
             output_file = os.path.join(generated_path, f"{number}.root")
-            parameter["output_file"] = output_file
+
+            parameter = {"channel": channel, "random_seed": number + 1111, "output_file": output_file,
+                         "n_events": n_events}
+
+            # Create output directory
+            output_dir = os.path.dirname(output_file)
+
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
 
             parameters.append(parameter)
 
@@ -30,17 +34,21 @@ def run_reconstruction(channels, storage_location):
     parameters = []
 
     for channel in channels:
-        parameter = {}
-
         channel_path = os.path.join(storage_location, channel)
         generated_path = os.path.join(channel_path, "generated")
-        for filename in glob(os.path.join(generated_path, "*.root")):
-            parameter = {"input_file": filename}
+        for input_file in glob(os.path.join(generated_path, "*.root")):
+            # Create output directory
+            output_file = input_file.replace("/generated/", "/reconstructed/")
+            output_dir = os.path.dirname(output_file)
+
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+
+            parameter = {"input_file": input_file, "output_file": output_file}
 
             parameters.append(parameter)
 
-    gridcontrol_file = write_gridcontrol_file(steering_file="reconstruct.py",
-                                              parameters=parameters)
+    gridcontrol_file = write_gridcontrol_file(steering_file="reconstruct.py", parameters=parameters)
     call_gridcontrol(gridcontrol_file=gridcontrol_file, retries=2)
 
 
@@ -51,21 +59,59 @@ def calculate_efficiencies(channels, storage_location):
 
         channel_path = os.path.join(storage_location, channel)
         reconstructed_path = os.path.join(channel_path, "reconstructed")
-        for filename in glob(os.path.join(reconstructed_path, "*.root")):
-            parameter = {"input_file": filename}
+        for input_file in glob(os.path.join(reconstructed_path, "[0-9].root")):
+            # Create output directory
+            output_file = input_file.replace("/reconstructed/", "/analysed/")
+            output_dir = os.path.dirname(output_file)
+
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+
+            parameter = {"input_file": input_file, "output_file": output_file}
 
             parameters.append(parameter)
 
-    gridcontrol_file = write_gridcontrol_file(steering_file="analyse.py",
-                                              parameters=parameters)
+    gridcontrol_file = write_gridcontrol_file(steering_file="analyse.py", parameters=parameters)
     call_gridcontrol(gridcontrol_file=gridcontrol_file, retries=2)
+
+    extract_efficiencies(channels=channels, storage_location=storage_location)
+    extract_file_sizes(channels=channels, storage_location=storage_location)
 
 
 if __name__ == "__main__":
     channels_to_study = [
-        "BB_mixed",
+        "background_only",
+
+        "ee",
+        "eemumu",
+
+        "tau",
+        # "tau_to_1_prong_1_prong",
+        # "tau_to_e_gamma",
+        # "tau_to_mu_gamma",
+        # "tau_to_e_nu",
+        # "tau_to_mu_nu",
+
+        "mumu",
+        "mumu_kkgen",
+
+        "ee",
+        "gg",
+
+        "continuum_ccbar",
+        "continuum_uubar",
+        "continuum_ddbar",
+        "continuum_ssbar",
+
         "BB_charged",
-        # etc.
+        "BB_mixed",
+        # "B_to_nu_nu",
+        # "B_to_pi0_pi0",
+        # "B_to_rho0_gamma",
+        # "B_to_JpsiKS_e_e",
+
+        "pipi",
+        "pipipi",
     ]
 
     parser = argparse.ArgumentParser()
@@ -73,14 +119,12 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    storage_location = args.storage_location
-
     # Produce 5*1000 events in each channel
     generate_events(channels=channels_to_study, n_events=1000,
-                    n_jobs=5, storage_location=storage_location)
+                    n_jobs=5, storage_location=args.storage_location)
 
     # Reconstruct each channel
-    run_reconstruction(channels=channels_to_study, storage_location=storage_location)
+    run_reconstruction(channels=channels_to_study, storage_location=args.storage_location)
 
     # Calculate file size and efficiencies for each channel
-    calculate_efficiencies(channels=channels_to_study, storage_location=storage_location)
+    calculate_efficiencies(channels=channels_to_study, storage_location=args.storage_location)
---
+++ hlt/softwaretrigger/efficiency_calculation/extract.py
@@ -0,0 +1,68 @@
+import os
+from glob import glob
+
+import ROOT
+import pandas as pd
+
+
+def extract_efficiencies(channels, storage_location):
+    efficiency_list = []
+
+    for channel in channels:
+        result_list = []
+        channel_path = os.path.join(storage_location, channel)
+        analysed_path = os.path.join(channel_path, "analysed")
+
+        for filename in glob(os.path.join(analysed_path, "*_results.pkl")):
+            result_list += pd.read_pickle(filename)
+
+        results = pd.DataFrame(result_list)
+
+        efficiencies = (results[["final_decision", "software_trigger_cut&calib&total_result",
+                                 "software_trigger_cut&fast_reco&total_result",
+                                 "software_trigger_cut&hlt&total_result"]] == 1).mean()
+        efficiencies.index = ["final decision", "calibration", "fast reco", "hlt"]
+        efficiencies.name = channel
+        efficiency_list.append(efficiencies)
+
+    all_efficiencies = pd.DataFrame(efficiency_list)
+    all_efficiencies.to_pickle("all_efficiencies.pkl")
+
+    print("\n### Final Efficiencies per channel ###\n")
+    print(all_efficiencies)
+
+
+def extract_file_sizes(channels, storage_location):
+    # Is this really needed?
+    ROOT.gErrorIgnoreLevel = ROOT.kWarning + 1
+
+    filesize_list = []
+
+    for channel in channels:
+        result_list = []
+        channel_path = os.path.join(storage_location, channel)
+        reconstructed_path = os.path.join(channel_path, "reconstructed")
+
+        for filename in glob(os.path.join(reconstructed_path, "*_raw.root")):
+            result = {}
+
+            root_file = ROOT.TFile(filename)
+            tree = root_file.Get('tree')
+            root_events = tree.GetEntriesFast() * 1.0
+            for branch in tree.GetListOfBranches():
+                result[branch.GetName()] = branch.GetZipBytes('*') / root_events
+            result["root_total_size"] = tree.GetZipBytes() / root_events
+            result["total_size_on_disk"] = os.path.getsize(filename) / root_events
+            result_list.append(result)
+
+        results = pd.DataFrame(result_list)
+
+        filesizes = results.mean()
+        filesizes.name = channel
+        filesize_list.append(filesizes)
+
+    all_filesizes = pd.DataFrame(filesize_list)
+    all_filesizes.to_pickle("all_filesizes.pkl")
+
+    print("\n### Final File Sizes per channel in bytes ###\n")
+    print(all_filesizes)

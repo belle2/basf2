ae304764adef7afcea0fa05674dbf91f3e9ddbc5
Kirill Chilikin <chilikin@lebedev.ru>
2017-12-22 14:26:07 +0300

    Fix annotation.

--- eklm/modules/EKLMDataChecker/include/EKLMDataCheckerModule.h
+++ eklm/modules/EKLMDataChecker/include/EKLMDataCheckerModule.h
@@ -1,9 +1,9 @@
 /**************************************************************************
  * BASF2 (Belle Analysis Framework 2)                                     *
- * Copyright(C) 2010  Belle II Collaboration                              *
+ * Copyright(C) 2017  Belle II Collaboration                              *
  *                                                                        *
  * Author: The Belle II Collaboration                                     *
- * Contributors: Timofey Uglov, Kirill Chilikin                           *
+ * Contributors: Kirill Chilikin                                          *
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/

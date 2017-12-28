aa2105a3f4fd7efb2ec7a825c975f6e2839d3305
Satoru YAMADA <satoru.yamada@kek.jp>
2017-12-28 18:18:24 +0900

    Change the file path of the input xml file.

--- daq/slc/apps/errdiagd/tools/errdiagd.cc
+++ daq/slc/apps/errdiagd/tools/errdiagd.cc
@@ -10,14 +10,24 @@ using namespace Belle2;
 
 int main(int argc, char** argv)
 {
-  if (Daemon::start(argv[1], argc, argv, 2, "<config>")) {
+
+  if (argc != 5) {
+    printf("Usage : %s <slc config file> <timeout(s)> <max entries> <offset entries>\n", argv[0]);
+    exit(1);
+  }
+  if (Daemon::start(argv[1], argc, argv, 1, "<config>")) {
     ConfigFile config("slowcontrol", argv[1]);
     std::string host = config.get("nsm.host");
     int port = config.getInt("nsm.port");
     std::string name = config.get("nsm.nodename");
-    int timeout = config.getInt("timeout");
+    //     int timeout = config.getInt("timeout");
+    int timeout = atoi(argv[2]);
     if (timeout <= 0) timeout = 5;
-    NSMNodeDaemon(new ErrdiagCallback(name), host, port).run();
+    const char* path = getenv("BELLE2_LOCAL_DIR");
+    std::string xmlpath = path;
+    xmlpath = xmlpath + "/daq/slc/data/errdiag/CPRROPC_Diagnosis.xml";
+
+    NSMNodeDaemon(new ErrdiagCallback(name, xmlpath, timeout, atoi(argv[3]), atoi(argv[4])), host, port).run();
   }
   return 0;
 }

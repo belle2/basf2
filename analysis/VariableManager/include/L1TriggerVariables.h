ba38daf7f8ba8df09144fc4e60975935a7ce0b15
Torben Ferber <torben.ferber@desy.de>
2018-01-04 20:46:19 +0100

    Add L1 variables (accessed via bit, not optimal).

---
+++ analysis/VariableManager/include/L1TriggerVariables.h
@@ -0,0 +1,32 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2018 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Torben Ferber                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace Belle2 {
+  class Particle;
+
+  namespace Variable {
+
+    /**
+     * returns trigger status for a given bit trigger bit
+     */
+    double L1TriggerBit(const Particle*, const std::vector<double>& bit);
+
+    /**
+     * returns trigger prescale for a given trigger bit
+     */
+    double L1TriggerBitPrescale(const Particle*, const std::vector<double>& bit);
+
+  }
+}

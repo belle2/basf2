3e2bc68a820e4b05d57a553a02f6f5f73fd6495e
Benjamin Schwenker <benjamin.schwenker@phys.uni-goettingen.de>
2018-01-19 18:37:40 +0100

    python based database importer

---
+++ pxd/examples/PXDDataBaseImporter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+##############################################################################
+#
+# Imports PXD calibration payloads
+# Some PXD calibration constants are currently stored in numpy files
+# or binary files with pickled python objects.
+#
+# This example reads these constants, fills dbobjects and imports them
+# into the conditions db.
+#
+##############################################################################
+
+from basf2 import *
+from ROOT import Belle2
+
+
+offset = Belle2.PXDClusterOffsetPar()
+
+offset.setU(-1.0)
+
+print("offset ", offset)
+
+iov = Belle2.IntervalOfValidity(0, 0, -1, -1)
+Belle2.Database.Instance().storeData('PXDClusterOffsetPar', offset, iov)

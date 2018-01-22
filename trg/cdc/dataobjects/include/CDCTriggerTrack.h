17189b54bc2bff6d6656c73f51e4df81e3dd7add
Tzu-An Sheng <tzu-an.sheng@desy.de>
2018-01-23 03:14:46 +0800

    Add found time to the CDCTriggerTrack (for firmware track)

--- trg/cdc/dataobjects/include/CDCTriggerTrack.h
+++ trg/cdc/dataobjects/include/CDCTriggerTrack.h
@@ -10,15 +10,15 @@ namespace Belle2 {
   class CDCTriggerTrack : public Helix {
   public:
     /** default constructor, initializing everything to 0. */
-    CDCTriggerTrack(): Helix(), m_chi2D(0.), m_chi3D(0.) { }
+    CDCTriggerTrack(): Helix(), m_chi2D(0.), m_chi3D(0.), m_time(0) { }
 
     /** 2D constructor, initializing 3D values to 0.
      *  @param phi0   The angle between the transverse momentum and the x axis and in [-pi, pi].
      *  @param omega  The signed curvature of the track where the sign is given by the charge of the particle.
      *  @param chi2   Chi2 value of the 2D fit.
      */
-    CDCTriggerTrack(double phi0, double omega, double chi2):
-      Helix(0., phi0, omega, 0., 0.), m_chi2D(chi2), m_chi3D(0.) { }
+    CDCTriggerTrack(double phi0, double omega, double chi2, short time = 0):
+      Helix(0., phi0, omega, 0., 0.), m_chi2D(chi2), m_chi3D(0.), m_time(time) { }
 
     /** 3D constructor
      *  @param phi0      The angle between the transverse momentum and the x axis and in [-pi, pi].
@@ -30,7 +30,7 @@ namespace Belle2 {
      */
     CDCTriggerTrack(double phi0, double omega, double chi2D,
                     double z0, double cotTheta, double chi3D):
-      Helix(0., phi0, omega, z0, cotTheta), m_chi2D(chi2D), m_chi3D(chi3D) { }
+      Helix(0., phi0, omega, z0, cotTheta), m_chi2D(chi2D), m_chi3D(chi3D), m_time(0) { }
 
     /** destructor, empty because we don't allocate memory anywhere. */
     ~CDCTriggerTrack() { }
@@ -40,15 +40,19 @@ namespace Belle2 {
     float getChi2D() const { return m_chi2D; }
     /** get chi2 value of 3D fitter */
     float getChi3D() const { return m_chi3D; }
+    /** get the track found time */
+    float getTime() const { return m_time; }
 
   protected:
     /** chi2 value from 2D fitter */
     float m_chi2D;
     /** chi2 value from 3D fitter */
     float m_chi3D;
+    /** number of trigger clocks of (the track output - L1 trigger)*/
+    short m_time;
 
     //! Needed to make the ROOT object storable
-    ClassDef(CDCTriggerTrack, 4);
+    ClassDef(CDCTriggerTrack, 5);
   };
 }
 #endif

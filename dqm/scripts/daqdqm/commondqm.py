935807a5c1c3ba44169b95741bd301d7739a75c9
Thomas Hauth <Thomas.Hauth@kit.edu>
2018-01-23 16:40:50 +0900

    added collision dqm utility method and a common dqm method where modules can go which will be used during collision & cosmics run.

---
+++ dqm/scripts/daqdqm/commondqm.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from basf2 import *
+
+
+def add_common_dqm(path, components=None):
+    """
+    This function adds DQMs which are common for Cosmic runs and Collion runs
+    """
+    # CDC
+    if components is None or 'CDC' in components:
+        cdcdqm = register_module('cdcDQM7')
+        path.add_module(cdcdqm)
+    # ECL
+    if components is None or 'ECL' in components:
+        ecldqm = register_module('ECLDQM')
+        path.add_module(ecldqm)
+    # TOP
+    if components is None or 'TOP' in components:
+        topdqm = register_module('TOPDataQualityOnline')
+        path.add_module(topdqm)
+    # BKLM
+    if components is None or 'BKLM' in components:
+        bklmdqm = register_module("bklmDQM")
+        path.add_module(bklmdqm)
+    # ECLTRG
+    if components is None or 'TRG' in components:
+        trgecldqm = register_module('TRGECLDQM')
+        path.add_module(trgecldqm)

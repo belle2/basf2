0501a26a18ff7ca455cb49559a21bbc1c6ef2630
Kirill Chilikin <chilikin@lebedev.ru>
2018-01-26 14:58:16 +0300

    Add CTIME to packer.

--- eklm/modules/EKLMRawPacker/include/EKLMRawPackerModule.h
+++ eklm/modules/EKLMRawPacker/include/EKLMRawPackerModule.h
@@ -76,14 +76,16 @@ namespace Belle2 {
      * @param[in]  plane  Plane number.
      * @param[in]  strip  Strip number.
      * @param[in]  charge Charge.
-     * @param[in]  ctime  Time.
+     * @param[in]  ctime  CTine.
+     * @param[in]  time   Time.
      * @param[out] bword1 First word.
      * @param[out] bword2 Second word.
      * @param[out] bword3 Third word.
      * @param[out] bword4 Fourth word.
      */
     void formatData(const EKLMDataConcentratorLane* lane,
-                    int plane, int strip, int charge, float time,
+                    int plane, int strip, int charge,
+                    uint16_t ctime, float time,
                     uint16_t& bword1, uint16_t& bword2, uint16_t& bword3,
                     uint16_t& bword4);
 

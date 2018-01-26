adb2dec12c5f3467bf03236ac66cc829b64c4d1c
Thomas Hauth <Thomas.Hauth@kit.edu>
2018-01-26 22:59:03 +0900

    added the dqm_environment option allow modules to be added only for hlt or expressreco running.

--- dqm/scripts/daqdqm/commondqm.py
+++ dqm/scripts/daqdqm/commondqm.py
@@ -4,9 +4,17 @@
 from basf2 import *
 
 
-def add_common_dqm(path, components=None):
+def add_common_dqm(path, components=None, dqm_environment="expressreco"):
     """
     This function adds DQMs which are common for Cosmic runs and Collion runs
+
+    @param components: A list of the detector components which are available in this
+                       run of basf2
+    @param dqm_environment: The environment the DQM modules are running in
+                            "expressreco" (default) if running on the ExpressReco system
+                            "hlt" if running on the HLT online reconstructon nodes
+                            If running on the hlt, you may want to output less or other DQM plots
+                            due to the limited bandwith of the HLT nodes.
     """
     # CDC
     if components is None or 'CDC' in components:

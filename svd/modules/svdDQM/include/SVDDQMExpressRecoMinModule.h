425e3fd197e4dbc48a7ee7e284d0c84fb96b1339
kodys <peter.kodys@mff.cuni.cz>
2018-01-28 07:23:45 +0100

    DQM for VXD and SVD - add minimal version for on-line express reco

---
+++ svd/modules/svdDQM/include/SVDDQMExpressRecoMinModule.h
@@ -0,0 +1,185 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2017 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Peter Kodys                                              *
+ *                                                                        *
+ * Prepared for Belle II geometry                                         *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+#include <framework/core/HistoModule.h>
+#include <vxd/dataobjects/VxdID.h>
+#include <svd/geometry/SensorInfo.h>
+#include <vxd/geometry/GeoCache.h>
+#include <vector>
+#include "TH1I.h"
+#include "TH1F.h"
+#include "TH2F.h"
+
+namespace Belle2 {
+
+  /** SVD DQM Module */
+  class SVDDQMExpressRecoMinModule : public HistoModule {  // <- derived from HistoModule class
+
+  public:
+
+    /** Constructor */
+    SVDDQMExpressRecoMinModule();
+    /* Destructor */
+    virtual ~SVDDQMExpressRecoMinModule();
+
+    /** Module function initialize */
+    virtual void initialize();
+    /** Module function beginRun */
+    virtual void beginRun();
+    /** Module function event */
+    virtual void event();
+    /** Module function endRun */
+    virtual void endRun();
+    /** Module function terminate */
+    virtual void terminate();
+
+    /**
+     * Histogram definitions such as TH1(), TH2(), TNtuple(), TTree().... are supposed
+     * to be placed in this function.
+    */
+    virtual void defineHisto();
+
+  private:
+
+    /** cut for accepting to hitmap histogram, using strips only, default = 22 */
+    float m_CutSVDCharge = 22.0;
+
+    /** SVDShaperDigits StoreArray name */
+    std::string m_storeSVDShaperDigitsName;
+    /** SVDClusters StoreArray name */
+    std::string m_storeSVDClustersName;
+    /** SVD diagnostics module name */
+    std::string m_svdDAQDiagnosticsListName;
+
+
+    /** Basic Directory in output file */
+    TDirectory* m_oldDir;
+
+    /** Name of file contain reference histograms, default=VXD-ReferenceHistos */
+    std::string m_RefHistFileName = "vxd/data/VXD-DQMReferenceHistos.root";
+    /** Number of events */
+    int m_NoOfEvents;
+    /** Number of events in reference histogram */
+    int m_NoOfEventsRef;
+
+    /** Using local files instead of DataBase for reference histogram, default=0 */
+    int m_NotUseDB = 0;
+    /** Create and fill reference histograms in DataBase, default=0 */
+    int m_CreateDB = 0;
+
+    /** Flags of u Hitmaps of Digits */
+    TH1I* m_fHitMapCountsUFlag;
+    /** Flags of v Hitmaps of Digits */
+    TH1I* m_fHitMapCountsVFlag;
+    /** Flags of u Hitmaps of Clusters*/
+    TH1I* m_fHitMapClCountsUFlag;
+    /** Flags of v Hitmaps of Clusters*/
+    TH1I* m_fHitMapClCountsVFlag;
+    /** Flags of Hitmaps in U of Digits */
+    TH1I* m_fFiredUFlag;
+    /** Flags of Hitmaps in V of Digits */
+    TH1I* m_fFiredVFlag;
+    /** Flags of u Clusters per event */
+    TH1I* m_fClustersUFlag;
+    /** Flags of v Clusters per event */
+    TH1I* m_fClustersVFlag;
+    /** Flags of u Charge of clusters */
+    TH1I* m_fClusterChargeUFlag;
+    /** Flags of v Charge of clusters */
+    TH1I* m_fClusterChargeVFlag;
+    /** Flags of u Charge of strips */
+    TH1I* m_fStripSignalUFlag;
+    /** Flags of v Charge of strips */
+    TH1I* m_fStripSignalVFlag;
+    /** Flags of u cluster size */
+    TH1I* m_fClusterSizeUFlag;
+    /** Flags of v cluster size */
+    TH1I* m_fClusterSizeVFlag;
+    /** Flags of u time */
+    TH1I* m_fClusterTimeUFlag;
+    /** Flags of v time */
+    TH1I* m_fClusterTimeVFlag;
+
+    /** Hitmaps u of Digits */
+    TH1I* m_hitMapCountsU;
+    /** Hitmaps v of Digits */
+    TH1I* m_hitMapCountsV;
+    /** Hitmaps u of Clusters*/
+    TH1I* m_hitMapClCountsU;
+    /** Hitmaps v of Clusters*/
+    TH1I* m_hitMapClCountsV;
+    /** Fired u strips per event */
+    TH1F** m_firedU;
+    /** Fired v strips per event */
+    TH1F** m_firedV;
+    /** u clusters per event */
+    TH1F** m_clustersU;
+    /** v clusters per event */
+    TH1F** m_clustersV;
+    /** u charge of clusters */
+    TH1F** m_clusterChargeU;
+    /** v charge of clusters */
+    TH1F** m_clusterChargeV;
+    /** u charge of strips */
+    TH1F** m_stripSignalU;
+    /** v charge of strips */
+    TH1F** m_stripSignalV;
+    /** u size */
+    TH1F** m_clusterSizeU;
+    /** v size */
+    TH1F** m_clusterSizeV;
+    /** u time */
+    TH1F** m_clusterTimeU;
+    /** v time */
+    TH1F** m_clusterTimeV;
+
+    /** Number of VXD layers on Belle II */
+    int c_nVXDLayers;
+    /** Number of PXD layers on Belle II */
+    int c_nPXDLayers;
+    /** Number of SVD layers on Belle II */
+    int c_nSVDLayers;
+    /** First VXD layer on Belle II */
+    int c_firstVXDLayer;
+    /** Last VXD layer on Belle II */
+    int c_lastVXDLayer;
+    /** First PXD layer on Belle II */
+    int c_firstPXDLayer;
+    /** Last PXD layer on Belle II */
+    int c_lastPXDLayer;
+    /** First SVD layer on Belle II */
+    int c_firstSVDLayer;
+    /** Last SVD layer on Belle II */
+    int c_lastSVDLayer;
+    /** Number of SVD sensors on Belle II */
+    int c_nSVDSensors;
+
+    /** Function return index of sensor in plots.
+       * @param Layer Layer position of sensor
+       * @param Ladder Ladder position of sensor
+       * @param Sensor Sensor position of sensor
+       * @return Index of sensor in plots.
+       */
+    int getSensorIndex(int Layer, int Ladder, int Sensor);
+    /** Function return index of sensor in plots.
+       * @param Index Index of sensor in plots.
+       * @param Layer return Layer position of sensor
+       * @param Ladder return Ladder position of sensor
+       * @param Sensor return Sensor position of sensor
+       */
+    void getIDsFromIndex(int Index, int* Layer, int* Ladder, int* Sensor);
+
+  };
+
+}
---
+++ vxd/examples/DQMVXD_Phase2Min.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#############################################################
+# Simple steering file to demonstrate how to run:
+#   PXD, SVD, VXD and Track DQM on BelleII or Phase2 geometry
+#   for ExpressReco on-line use Min modules without analysis
+#   Base on module from Martin Ritter:
+#               tracking/examples/DQMTracking_Phase2.py
+# Contributors: Peter Kodys                                              *
+#############################################################
+
+from basf2 import *
+from simulation import add_simulation
+from reconstruction import add_reconstruction
+from L1trigger import add_tsim
+import glob
+
+# background (collision) files
+# bg = glob.glob('./BG/*.root')
+# on KEKCC: (choose one of the sets)
+# bg = /group/belle2/BGFile/OfficialBKG/15thCampaign/phase2/set*/*.root
+# bg = /group/belle2/BGFile/OfficialBKG/15thCampaign/phase3/set*/*.root
+bg = None
+
+# number of events to generate, can be overriden with -n
+num_events = 100
+# output filename, can be overriden with -o
+output_filename = "RootOutput_Phase2.root"
+
+# create path
+main = create_path()
+
+# specify number of events to be generated
+# main.add_module('EventInfoSetter', evtNumList=num_events)
+# the experiment number for phase2 MC has to be 1002, otherwise the wrong payloads (for VXDTF2 the SectorMap) are loaded
+main.add_module("EventInfoSetter", expList=1002, runList=1, evtNumList=num_events)
+
+# in case you need to fix seed of random numbers
+# set_random_seed('some fixed value')
+# set_random_seed('d33fa68eab781f3dcb069fb23425885fcd92d3432e6433a14894e5d7bba34272')
+
+# generate BBbar events
+main.add_module('EvtGenInput')
+
+# detector simulation
+add_simulation(main, bkgfiles=bg)
+
+# trigger simulation
+add_tsim(main)
+
+# reconstruction
+add_reconstruction(main)
+
+# histomanager: use DqmHistoManager for in-line monitoring, or HistoManager for offline training
+# main.add_module('DqmHistoManager', Port=7777)
+main.add_module('HistoManager', histoFileName='Histos_DQMTracks_Phase2.root')
+# main.add_module('HistoManager', histoFileName='Histos_DQMTracks_BelleII.root')
+
+pxddqmExpRecoMin = register_module('PXDDQMExpressRecoMin')
+pxddqmExpReco = register_module('PXDDQMExpressReco')
+svddqmExpRecoMin = register_module('SVDDQMExpressRecoMin')
+svddqmExpReco = register_module('SVDDQMExpressReco')
+vxddqmExpRecoMin = register_module('VXDDQMExpressRecoMin')
+vxddqmExpReco = register_module('VXDDQMExpressReco')
+# main.add_module(pxddqmExpReco)
+main.add_module(pxddqmExpRecoMin)
+main.add_module(svddqmExpRecoMin)
+main.add_module(vxddqmExpRecoMin)
+
+# DQM of tracking
+main.add_module('TrackDQM')
+# In case to see more details:
+# trackDQM = main.add_module('TrackDQM')
+# trackDQM = main.add_module('TrackDQM', debugLevel=250)
+# trackDQM.logging.log_level = LogLevel.DEBUG
+
+# Finally add output, if you need
+# main.add_module("RootOutput", outputFileName=output_filename)
+
+# process events and print call statistics
+process(main)
+print(statistics)

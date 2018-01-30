a4f4f307f3ba47d0910e508d473346a10fd4ea3d
OBERHOF Benjamin Jerome Andreas <benjamin.oberhof@lnf.infn.it>
2018-01-31 00:44:18 +0900

    new module for reconstruction on decays with KLs in the final state

---
+++ analysis/modules/KLDecayReconstructor/include/KLDecayReconstructorModule.h
@@ -0,0 +1,83 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Benjamin Oberhof                                         *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+#include <framework/core/Module.h>
+
+#include <analysis/VariableManager/Utility.h>
+#include <analysis/dataobjects/ParticleList.h>
+#include <analysis/ParticleCombiner/ParticleCombiner.h>
+
+#include <analysis/DecayDescriptor/DecayDescriptor.h>
+
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/StoreObjPtr.h>
+
+#include <string>
+
+namespace Belle2 {
+  class Particle;
+
+  /**
+   * reco missing module
+   */
+  class KLDecayReconstructorModule : public Module {
+
+  public:
+
+    /**
+     * Constructor
+     */
+    KLDecayReconstructorModule();
+
+    /**
+     * Initialize the Module.
+     * This method is called at the beginning of data processing.
+     */
+    virtual void initialize() override;
+
+    /**
+     * Event processor.
+     */
+    virtual void event() override;
+
+
+  private:
+
+    int m_pdgCode;                /**< PDG code of the combined mother particle */
+    int m_kpdgCode;                /**< PDG code for the output K_L0 list */
+
+    std::string m_decayString;   /**< Input DecayString specifying the decay being reconstructed */
+    std::string m_finalDecayString;   /**< Final DecayString specifying the decay being reconstructed with recalculated KL momenta*/
+    DecayDescriptor m_decaydescriptor; /**< Decay descriptor of the decay being reconstructed */
+    std::unique_ptr<ParticleGenerator> m_generator; /**< Generates the combinations */
+
+    std::string m_listName;   /**< output particle list name */
+    std::string m_antiListName;   /**< output anti-particle list name */
+
+    std::string m_recoList;   /**< suffix for input K_L0 list name */
+
+    int m_decayModeID; /**< user specified decay mode identifier */
+
+    bool m_isSelfConjugatedParticle; /**< flag that indicates whether an anti-particle mother does not exist and should not be reconstructed as well*/
+
+    std::string m_cutParameter;  /**< selection criteria */
+    std::unique_ptr<Variable::Cut> m_cut; /**< cut object which performs the cuts */
+
+    bool m_writeOut;  /**< toggle output particle list btw. transient/writeOut */
+
+    int m_maximumNumberOfCandidates; /**< drop all candidates if more candidates than this parameter are produced */
+
+  };
+
+} // Belle2 namespace
+
+//#endif
--- analysis/scripts/modularAnalysis.py
+++ analysis/scripts/modularAnalysis.py
@@ -882,6 +882,45 @@ def reconstructDecay(
     path.add_module(pmake)
 
 
+def reconstructMissingKLDecay(
+    decayString,
+    cut,
+    dmID=0,
+    writeOut=False,
+    path=analysis_main,
+    recoList="_reco",
+):
+    """
+    Creates mother particle accounting for missing momentum.
+
+    @param decayString DecayString specifying what kind of the decay should be reconstructed
+                       (from the DecayString the mother and daughter ParticleLists are determined)
+    @param cut         created (mother) Particles are added to the mother ParticleList if they
+                       pass give cuts (in VariableManager style) and rejected otherwise
+    @param dmID        user specified decay mode identifier
+    @param writeOut    wether RootOutput module should save the created ParticleList
+    @param path        modules are added to this path
+    """
+
+    pcalc = register_module('KLMomentumCalculator')
+    pcalc.set_name('KLMomentumCalculator_' + decayString)
+    pcalc.param('decayString', decayString)
+    pcalc.param('cut', cut)
+    pcalc.param('decayMode', dmID)
+    pcalc.param('writeOut', writeOut)
+    pcalc.param('recoList', "_reco")
+    analysis_main.add_module(pcalc)
+
+    rmake = register_module('KLDecayReconstructor')
+    rmake.set_name('KLDecayReconstrucotr_' + decayString)
+    rmake.param('decayString', decayString)
+    rmake.param('cut', cut)
+    rmake.param('decayMode', dmID)
+    rmake.param('writeOut', writeOut)
+    rmake.param('recoList', "_reco")
+    analysis_main.add_module(rmake)
+
+
 def replaceMass(
     replacerName,
     particleLists=[],

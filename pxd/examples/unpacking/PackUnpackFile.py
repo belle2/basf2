5490dc67275d7975252ffd19f94c59fb5c455ed4
Björn Spruck <bspruck@uni-mainz.de>
2018-01-31 16:39:30 +0100

    BS->New

--- pxd/examples/unpacking/PackUnpackFile.py
+++ pxd/examples/unpacking/PackUnpackFile.py
@@ -44,7 +44,7 @@ packer.param('dhe_to_dhc', [
 #    [    5, 35    ]
 # ])
 
-unpacker = register_module('PXDUnpackerBS')
+unpacker = register_module('PXDUnpackerNew')
 unpacker.param('HeaderEndianSwap', True)
 
 simpleoutput = register_module('RootOutput')

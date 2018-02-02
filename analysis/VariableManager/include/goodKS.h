1f3d30bd637e20871ad2d72923835d2a3c10b362
Minakshi Nayak <minakshi.nayak@desy.de>
2018-02-02 14:49:29 +0900

    goodKS.h

---
+++ analysis/VariableManager/include/goodKS.h
@@ -0,0 +1,18 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Author: The Belle II Collaboration                                     *
+ * Contributor: Minakshi Nayak                                              *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+
+
+namespace Belle2 {
+  class Particle;
+
+  namespace Variable {
+
+    double goodKs(const Particle* KS);
+
+  }
+}

076b357a7079a30b0cffc787a2d43f63f2ac87d1
longos <longos@uvic.ca>
2018-02-02 21:03:07 +0900

    final step added.

--- ecl/tools/eclComputePulseTemplates_Step0.cc
+++ ecl/tools/eclComputePulseTemplates_Step0.cc
@@ -32,37 +32,60 @@ eclComputePulseTemplates_Step4.cc - organizes photon, hadron and diode templates
 
 To run calibration :
 
-execute eclComputePulseTemplates_Step0.cc
+execute eclComputePulseTemplates_Step0 0
 for i=0 i<873 i++
-  execute eclComputePulseTemplates_Step1.cc i*10 (i+1)*10
+  execute eclComputePulseTemplates_Step1 i*10 (i+1)*10
   basf2 eclComputePulseTemplates_Step2.py i*10 (i+1)*10
-  execute eclComputePulseTemplates_Step3.cc i*10 (i+1)*10
-execute eclComputePulseTemplates_Step4.py
+  execute eclComputePulseTemplates_Step3 i*10 (i+1)*10
+execute eclComputePulseTemplates_Step0 1
 //
 */
 //
 
 struct crystalInfo {
   vector<double> PhotonWaveformPars;
+  vector<double> HadronWaveformPars;
+  vector<double> DiodeWaveformPars;
+  double MaxResHadron;
+  double MaxValHadron;
+  double MaxResDiode;
+  double MaxValDiode;
 };
 
-int main()
+int main(int argc, char* argv[])
 {
   //
+  int Flag = atoi(argv[1]);
+  //
   TString ParameterTreeOutputName = "PhotonWaveformParameters.root";
+  if (Flag == 1) ParameterTreeOutputName = "DigitWaveformParameters.root";
   TFile* ParameterTreeOutputFile = new TFile(ParameterTreeOutputName, "RECREATE");
 
   TTree* ParameterTree = new TTree("ParTree", "");
   double PhotonWaveformPar[11];
-  for (int k = 0; k < 11; k++)  PhotonWaveformPar[k] = 0;
-  //
+  double HadronWaveformPar[11];
+  double DiodeWaveformPar[11];
+  for (int k = 0; k < 11; k++) {
+    PhotonWaveformPar[k] = 0;
+    HadronWaveformPar[k] = 0;
+    DiodeWaveformPar[k] = 0;
+  }
+  double mHadronRes = 0;
+  double mDiodeRes = 0;
+  double mHadronMax = 0;
+  double mDiodeMax = 0;
   ParameterTree->Branch("PhotonPar", &PhotonWaveformPar, "PhotonWaveformPar[11]/D");
+  ParameterTree->Branch("HadronPar", &HadronWaveformPar, "HadronWaveformPar[11]/D");
+  ParameterTree->Branch("DiodePar", &DiodeWaveformPar, "DiodeWaveformPar[11]/D");
+  ParameterTree->Branch("mHadronRes", &mHadronRes, "mHadronRes/D");
+  ParameterTree->Branch("mDiodeRes", &mDiodeRes, "mDiodeRes/D");
+  ParameterTree->Branch("mHadronMax", &mHadronMax, "mHadronMax/D");
+  ParameterTree->Branch("mDiodeMax", &mDiodeMax, "mDiodeMax/D");
   //
-  vector<crystalInfo> cellIDcheck(8736);
-  //
+  std::vector<crystalInfo> cellIDcheck(8736);
   ifstream PhotonFile("/home/belle2/longos/WaveformFitting/ecl/tools/params_gamma_shape.dat");
   if (PhotonFile.is_open()) {
-    vector<double> templine(12);
+    std::vector<double> templine(12);
     for (int k = 0; k < 8736; k++) {
       for (unsigned int j = 0; j < templine.size(); j++)  PhotonFile >> templine[j];
       cellIDcheck[int(templine[0]) - 1].PhotonWaveformPars.resize(11);
@@ -71,16 +94,70 @@ int main()
       std::cout << int(templine[0]) << " " << templine[1] << endl;
     }
     PhotonFile.close();
+  } else {
+    std::cout << "ERROR cannont open photon param file." << endl;
+    return -1;
+  }
+  if (Flag == 0) {
+    for (unsigned int f = 0; f < cellIDcheck.size(); f++) {
+      for (int k = 0; k < 11; k++)  PhotonWaveformPar[k] = cellIDcheck[f].PhotonWaveformPars[k];
+      ParameterTree->Fill();
+    }
+  } else {
+    int batch = 10;
+    for (int k = 0; k < 874; k++) {
+      int low = k * batch;
+      int high = (k + 1) * batch;
+      TFile* TempFile = new TFile(Form("HadronPars_Low%d_High%d.root", low, high), "READ");
+      TTree* TempTree = (TTree*)  TempFile->Get("HadronWaveformInfo");
+      double tHadronShapePars_A[11];
+      double tDiodeShapePars_A[11];
+      double tMaxResidualHadron_A;
+      double tMaxResidualDiode_A;
+      double tMaxValDiode_A;
+      double tMaxValHadron_A;
+      TempTree->SetBranchAddress("TempHadronPar11_A", &tHadronShapePars_A);
+      TempTree->SetBranchAddress("TempDiodePar11_A", &tDiodeShapePars_A);
+      TempTree->SetBranchAddress("MaxResHadron_A", &tMaxResidualHadron_A);
+      TempTree->SetBranchAddress("MaxResDiode_A", &tMaxResidualDiode_A);
+      TempTree->SetBranchAddress("MaxValDiode_A", &tMaxValDiode_A);
+      TempTree->SetBranchAddress("MaxValHadron_A", &tMaxValHadron_A);
+      //
+      for (int j = 0; j < batch; j++) {
+        TempTree->GetEntry(j);
+        //
+        int tCellID = (k * batch) + j;
+        cellIDcheck[tCellID].HadronWaveformPars.resize(11);
+        cellIDcheck[tCellID].DiodeWaveformPars.resize(11);
+        for (int p = 0; p < 11; p++)  cellIDcheck[tCellID].HadronWaveformPars[p] = tHadronShapePars_A[p];
+        cellIDcheck[tCellID].MaxResHadron = tMaxResidualHadron_A;
+        cellIDcheck[tCellID].MaxValHadron = tMaxValHadron_A;
+        for (int p = 0; p < 11; p++)  cellIDcheck[tCellID].DiodeWaveformPars[p] = tDiodeShapePars_A[p];
+        cellIDcheck[tCellID].MaxResDiode = tMaxResidualDiode_A;
+        cellIDcheck[tCellID].MaxValDiode = tMaxValDiode_A;
+      }
+      TempFile->Close();
+    }
+    //
+    for (unsigned int f = 0; f < 8736; f++) {
+      for (int k = 0; k < 11; k++) {
+        PhotonWaveformPar[k] = cellIDcheck[f].PhotonWaveformPars[k];
+        HadronWaveformPar[k] = cellIDcheck[f].HadronWaveformPars[k];
+        DiodeWaveformPar[k] = cellIDcheck[f].DiodeWaveformPars[k];
+      }
+      mHadronRes = cellIDcheck[f].MaxResHadron;
+      mDiodeRes = cellIDcheck[f].MaxResDiode;
+      mHadronMax = cellIDcheck[f].MaxValHadron;
+      mDiodeMax = cellIDcheck[f].MaxValDiode;
+      ParameterTree->Fill();
+    }
   }
   //
-  for (unsigned int f = 0; f < cellIDcheck.size(); f++) {
-    for (int k = 0; k < 11; k++)  PhotonWaveformPar[k] = cellIDcheck[f].PhotonWaveformPars[k];
-    ParameterTree->Fill();
-  }
   ParameterTreeOutputFile->cd();
   ParameterTree->Write();
   ParameterTreeOutputFile->Write();
   ParameterTreeOutputFile->Close();
+  //
   //
   return 0;
 }

7ccb44e6878f6d604db1d9f06593f352c29c0373
Björn Spruck <bspruck@uni-mainz.de>
2018-02-03 15:25:24 +0900

    eanup c+++

--- dqm/analysis/modules/include/DQMHistAnalysisPXDEff.h
+++ dqm/analysis/modules/include/DQMHistAnalysisPXDEff.h
@@ -31,19 +31,19 @@ namespace Belle2 {
 
     //! Constructor / Destructor
     DQMHistAnalysisPXDEffModule();
-    virtual ~DQMHistAnalysisPXDEffModule();
+  private:
+    ~DQMHistAnalysisPXDEffModule() override final;
 
     //! Module functions to be called from main process
-    virtual void initialize();
+    void initialize() override final;
 
     //! Module functions to be called from event process
-    virtual void beginRun();
-    virtual void event();
-    virtual void endRun();
-    virtual void terminate();
+    void beginRun() override final;
+    void event() override final;
+    void endRun() override final;
+    void terminate() override final;
 
     // Data members
-  private:
     int m_u_bins;
     int m_v_bins;
     std::string m_histogramDirectoryName;

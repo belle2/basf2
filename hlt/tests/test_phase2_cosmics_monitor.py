f8d918b01ca8a11d820f19bf18825a426e8591f6
Thomas Hauth <Thomas.Hauth@kit.edu>
2018-02-07 12:10:39 +0900

    updated test scripts for cosmics

--- hlt/tests/test_phase2_cosmics_monitor.py
+++ hlt/tests/test_phase2_cosmics_monitor.py
@@ -1,6 +1,8 @@
 import basf2
 import generators
-from softwaretrigger.path_functions import add_hlt_processing
+from softwaretrigger.path_functions import add_hlt_processing, get_store_only_rawdata_path
+from rawdata import add_packers
+from simulation import add_simulation
 
 
 path = basf2.create_path()
@@ -8,16 +10,19 @@ path = basf2.create_path()
 basf2.set_random_seed(12345)
 
 # specify number of events to be generated
-path.add_module('EventInfoSetter', evtNumList=[1], runList=[1], expList=[1])
+path.add_module('EventInfoSetter', evtNumList=[1])
 path.add_module("HistoManager", histoFileName="RemoveMePlease.root")
 
-# generate BBbar events
-# path.add_module('EvtGenInput')
-generators.add_continuum_generator(path, finalstate="ccbar")
+generators.add_cosmics_generator(path)
+add_simulation(path, usePXDDataReduction=False)
+add_packers(path)
 
+# remove everything but HLT input raw objects
+path.add_path(get_store_only_rawdata_path())
+
+path.add_module("PrintCollections", printForEvent=0)
 # no reconstruction or software trigger added at all
 add_hlt_processing(path, run_type="cosmics", softwaretrigger_mode="monitor")
 
-finalize_hlt_path(path)
 basf2.print_path(path)
 basf2.process(path)

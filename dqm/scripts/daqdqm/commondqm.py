c2e7bc56113fe5ed965e0009df9017352c90d037
Wang, Boqun <boqunwg@ucmail.uc.edu>
2018-02-08 22:37:55 +0900

    Change dqm module for top

--- dqm/scripts/daqdqm/commondqm.py
+++ dqm/scripts/daqdqm/commondqm.py
@@ -41,7 +41,7 @@ def add_common_dqm(path, components=None, dqm_environment="expressreco"):
         path.add_module(ecldqm)
     # TOP
     if components is None or 'TOP' in components:
-        topdqm = register_module('TOPDataQualityOnline')
+        topdqm = register_module('TOPDQM')
         path.add_module(topdqm)
     # BKLM
     if components is None or 'BKLM' in components:

9c85a5d7a4c2933096138b0d5bf42774a2c84818
Thomas Hauth <Thomas.Hauth@kit.edu>
2018-02-08 15:16:17 +0900

    removed DQM adding script

--- hlt/operation/phase2/global/evp_scripts/phase2_cosmics_passthrough.py
+++ hlt/operation/phase2/global/evp_scripts/phase2_cosmics_passthrough.py
@@ -8,9 +8,6 @@ setup_basf2_and_db()
 path = create_hlt_path()
 
 # no reconstruction or software trigger added at all
-# just add dqm
-add_hlt_dqm(path, run_type="cosmics")
-
 
 finalize_hlt_path(path)
 basf2.print_path(path)

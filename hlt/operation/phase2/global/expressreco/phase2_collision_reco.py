b7a242aba82089bb52130e8b96daf9478ccc2bda
Thomas Hauth <Thomas.Hauth@kit.edu>
2018-02-08 15:20:03 +0900

    added explicit parameter that the reconstruction will be done

--- hlt/operation/phase2/global/expressreco/phase2_collision_reco.py
+++ hlt/operation/phase2/global/expressreco/phase2_collision_reco.py
@@ -7,7 +7,7 @@ from softwaretrigger.path_functions import setup_basf2_and_db, create_hlt_path, 
 setup_basf2_and_db()
 path = create_hlt_path()
 
-add_expressreco_processing(path, run_type="collison")
+add_expressreco_processing(path, run_type="collison", do_reconstruction=True)
 
 finalize_hlt_path(path)
 basf2.print_path(path)

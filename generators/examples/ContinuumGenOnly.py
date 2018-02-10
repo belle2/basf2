951ce13154789773a005dcdb0a5c3e86204ed86b
Torben Ferber <torben.ferber@desy.de>
2018-02-10 05:37:42 +0100

    Re-added examples and remove two more that are not useful anymore.

--- generators/examples/ContinuumGenOnly.py
+++ generators/examples/ContinuumGenOnly.py
@@ -2,35 +2,37 @@
 # -*- coding: utf-8 -*-
 
 ########################################################
-# 10 generic BBbar events using EvtGen incl. FullSim
+# 100 continuum events using EvtGen (using EvtGen, i.e. without ISR)
+# using the custom BELLE2_DECAY file.
 #
 # Example steering file
 ########################################################
 
 from basf2 import *
+from generators import *
 
 # suppress messages and warnings during processing:
-set_log_level(LogLevel.ERROR)
+set_log_level(LogLevel.INFO)
 
 main = create_path()
 
 # event info setter
 main.add_module("EventInfoSetter", expList=1, runList=1, evtNumList=100)
 
-# to run the framework the used modules need to be registered
-evtgen = register_module('EvtGenInput')
-evtgen.set_log_level(LogLevel.INFO)
-
 # run
 main.add_module("Progress")
 main.add_module("Gearbox")
-main.add_module("Geometry")
-main.add_module(evtgen)
-main.add_module("FullSim")
-main.add_module("PrintMCParticles", logLevel=LogLevel.DEBUG, onlyPrimaries=False)
-main.add_module("RootOutput", outputFileName="evtgen_upsilon4s_fullsim.root")
 
-# Process the events
+# use default continuum production
+add_continuum_generator(main, finalstate='ccbar')
+
+# add full root output
+main.add_module("RootOutput", outputFileName="continuum.root")
+
+# print MC particles (for debugging)
+main.add_module("PrintMCParticles", logLevel=LogLevel.DEBUG, onlyPrimaries=False)
+
+# generate events
 process(main)
 
 # show call statistics

813a09f465590392cfd1844103bd09de7f079264
Thomas Hauth <Thomas.Hauth@kit.edu>
2018-02-13 22:26:01 +0900

    removed merge conflict strings

--- daq/slc/nsm2/daemon/nsm2.h
+++ daq/slc/nsm2/daemon/nsm2.h
@@ -65,15 +65,12 @@ extern "C" {
   20170102 1950 nsmd2 ackdaemon more fix
   20171002 1951 nsmd2 destroyconn/ackdaemon fix
   20171002 1952 nsmd2 recv buffer to global
-<<<<<<< HEAD:daq/slc/extra/nsm2/daemon/nsm2.h
-=======
   20180111 1953 debug orphan node
   20180118 1954 nsmlib fix on select timeout, conid fix, message cleanup
   20180118 1955 put more priority to less frequent messages
   20180120 1956 b2nsm_wait to accept usec precision time
   20180121 1957 b2nsm_nodename added
   20180124 1958 smarter nsmd2 debug messages
->>>>>>> 35ac2479fb... moved nsm2 to daq/slc folder, updated to version 1958 and added SConscript file for compilation:daq/slc/nsm2/daemon/nsm2.h
  */
 
 /* -- DATA TYPES ----------------------------------------------------- */

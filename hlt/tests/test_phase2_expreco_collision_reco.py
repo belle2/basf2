12db99723c1e8ca2d909d9d364f8cfe90e7bdb4a
Thomas Hauth <Thomas.Hauth@kit.edu>
2018-02-13 18:23:07 +0900

    setting correct experiment number for phase 2 test now

--- hlt/tests/test_phase2_expreco_collision_reco.py
+++ hlt/tests/test_phase2_expreco_collision_reco.py
@@ -3,7 +3,7 @@
 from softwaretrigger.path_functions import add_expressreco_processing
 from softwaretrigger.test_support import create_test_path, finalize_test_path
 
-path, tempfolder = create_test_path(runtype="collision", with_pxd=True)
+path, tempfolder = create_test_path(runtype="collision", expNum=1002, location="expressreco")
 
 # no reconstruction or software trigger added at all
 add_expressreco_processing(path, run_type="collision")

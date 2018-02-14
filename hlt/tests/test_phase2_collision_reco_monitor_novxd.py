27ed43e82120e9ec054964eb404e85e7e0fada6d
Thomas Hauth <Thomas.Hauth@kit.edu>
2018-02-14 21:48:44 +0900

    added phase 2 experiment number to hlt unit tests

--- hlt/tests/test_phase2_collision_reco_monitor_novxd.py
+++ hlt/tests/test_phase2_collision_reco_monitor_novxd.py
@@ -3,7 +3,7 @@
 from softwaretrigger.path_functions import add_hlt_processing, DEFAULT_HLT_COMPONENTS
 from softwaretrigger.test_support import create_test_path, finalize_test_path
 
-path, tempfolder = create_test_path(runtype="collision")
+path, tempfolder = create_test_path(runtype="collision", expNum=1002)
 
 reco_components = DEFAULT_HLT_COMPONENTS
 reco_components.remove("SVD")

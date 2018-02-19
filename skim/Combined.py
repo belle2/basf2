ff9ce1e217d3b7071f18e17abd89caa7dcbc04df
Racha Cheaib <racha.cheaib@desy.de>
2018-02-20 02:50:35 +0900

    update combined skim

--- skim/Combined.py
+++ skim/Combined.py
@@ -15,26 +15,47 @@ from stdPi0s import *
 from stdV0s import *
 from stdCharm import *
 from stdLightMesons import *
-
+from stdDiLeptons import *
 set_log_level(LogLevel.INFO)
 
-fileList = [
-    '/ghi/fs01/belle2/bdata/MC/release-00-09-01/DB00000276/MC9/prod00002288/e0000/4S/r00000/mixed/sub00/' +
-    'mdst_000001_prod00002288_task00000001.root'
-]
-inputMdstList(filelist)
+from skimExpertFunctions import *
+
+if len(sys.argv) > 1:
+    bkgType = sys.argv[1]
+    f = open('inputFiles/' + bkgType + '.txt', 'r')
+    fileList = f.read()
+    f.close()
+    if not os.path.isfile(fileList[:-1]):
+        sys.exit('Could not find root file : ' + fileList[:-1])
+    print('Running over file ' + fileList[:-1])
+elif len(sys.argv) == 1:
+    fileList = \
+        ['/ghi/fs01/belle2/bdata/MC/fab/sim/release-00-05-03/DBxxxxxxxx/MC5/prod00000001/s00/e0001/4S/r00001/mixed/sub00/' +
+         'mdst_000001_prod00000001_task00000001.root'
+
+         ]
+    bkgType = 'old'
+
+
+if len(sys.argv) > 1:
+    inputMdstList('default', fileList[:-1])
+elif len(sys.argv) == 1:
+    inputMdstList('default', fileList)
+
 
 loadStdCharged()
-loadStdPi0()
+stdPi0s('loose')
+stdPhotons('loose')
 loadStdKS()
 loadStdLightMesons()
-loadStdPhoton()
-loadStdPhotonE15()
+loadStdSkimPi0()
+loadStdSkimPhoton()
 
 loadStdD0()
 loadStdDplus()
 loadStdDstar0()
 loadStdDstarPlus()
+loadStdDiLeptons(True)
 
 
 def add_skim(label, lists):
@@ -42,7 +63,7 @@ def add_skim(label, lists):
     create uDST skim for given lists, saving into $label.udst.root
     Particles not necessary for the given particle lists are not saved.
     """
-    skimOutputUdst(label, lists)
+    skimOutputUdst('outputCombinedFiles/' + label + '_' + bkgType, lists)
     summaryOfLists(lists)
 
 
@@ -55,9 +76,9 @@ from Tau_List import *
 add_skim('TauLFV', TauLFVList())
 
 # EWP Skim
-from EWP_List import *
+from BtoXgamma_List import *
 add_skim('BtoXgamma', B2XgammaList())
-
+from BtoXll_List import *
 add_skim('BtoXll', B2XllList())
 
 # Had Skim
@@ -69,13 +90,13 @@ from TCPV_List import *
 add_skim('TCPV', TCPVList())
 
 # SL Skim
-from Semileptonic_List import *
+from SLUntagged_List import *
 add_skim('SLUntagged', SemileptonicList())
-
+from LeptonicUntagged_List import *
 add_skim('LeptonicUntagged', LeptonicList())
 
 # Charm Skim
-from Charm_List import *
+from CharmRare_List import *
 add_skim('CharmRare', CharmRareList())
 
 # FEI Skim

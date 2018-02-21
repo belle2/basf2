249497fbae72f33162c18753b21a1d5d8a23c1c4
Yinghui GUAN <guanyh@post.kek.jp>
2018-02-21 15:27:09 +0900

    comment out sending slow control data and add window mintors

--- daq/slc/copper/klm/src/KLMFEE.cc
+++ daq/slc/copper/klm/src/KLMFEE.cc
@@ -17,7 +17,16 @@ KLMFEE::KLMFEE() : FEE("klm")
 void KLMFEE::init(RCCallback& callback, HSLB& hslb, const DBObject& obj)
 {
   std::string vname = StringUtil::form("klm[%d].", hslb.get_finid());
-  callback.add(new FEE32Handler(vname + "test", callback, hslb, *this, 0x32));
+  //callback.add(new FEE32Handler(vname + "test", callback, hslb, *this, 0x32));
+  //callback.add(new KLMWindowHandler(vname + "coarse_start", callback, hslb, *this, 0x30));
+  //callback.add(new KLMWindowHandler(vname + "coarse_stop", callback, hslb, *this, 0x31));
+  //callback.add(new KLMWindowHandler(vname + "coarse_stop", callback, hslb, *this, 0x32));
+  //callback.add(new KLMWindowHandler(vname + "coarse_stop", callback, hslb, *this, 0x33));
+
+  callback.add(new NSMVHandlerInt(vname + ".coarse_start", true, false, 0));
+  callback.add(new NSMVHandlerInt(vname + ".coarse_stop", true, false, 0));
+  callback.add(new NSMVHandlerInt(vname + ".fine_start", true, false, 0));
+  callback.add(new NSMVHandlerInt(vname + ".fine_stop", true, false, 0));
 }
 
 void KLMFEE::boot(RCCallback& callback, HSLB& hslb, const DBObject& obj)
@@ -27,7 +36,7 @@ void KLMFEE::boot(RCCallback& callback, HSLB& hslb, const DBObject& obj)
 void KLMFEE::load(RCCallback& callback, HSLB& hslb, const DBObject& obj)
 {
   // write access to register "reg0"
-  LogFile::info("0x38>>0x%x", hslb.readfee32(0x38));
+  /*LogFile::info("0x38>>0x%x", hslb.readfee32(0x38));
   hslb.writefee32(0x38, 10);
   int ret = hslb.readfee32(0x38);
   if (ret != 10) {
@@ -45,6 +54,7 @@ void KLMFEE::load(RCCallback& callback, HSLB& hslb, const DBObject& obj)
   }
 
   // writing stream file to FEE
+  // no need to send slow control every run. It will overide the calibraiton for scin., Let's comment out for a while
   const std::string bitfile = obj.getText("bitfile");
   if (File::exist(bitfile)) {
     hslb.writestream(bitfile.c_str());
@@ -53,7 +63,8 @@ void KLMFEE::load(RCCallback& callback, HSLB& hslb, const DBObject& obj)
   }
   sleep(2);
 
-  // Write window values only to slot-a
+
+  // Write look back window values
   if (obj.hasObject("window")) {
     DBObject o_window(obj("window"));
     LogFile::debug("Write window values...");
@@ -64,6 +75,38 @@ void KLMFEE::load(RCCallback& callback, HSLB& hslb, const DBObject& obj)
     hslb.writefee32(0x33, o_window.getInt("val3"));
   }
   sleep(30);
+  */
+}
+
+void KLMFEE::monitor(RCCallback& callback, HSLB& hslb)
+{
+
+  std::string vname = StringUtil::form("klm[%d].", hslb.get_finid());
+
+  //rpc look back window coarse start
+  callback.wait(5);
+  int wins = hslb.readfee32(0X30);
+  int win_start = wins & 0xffff;
+  callback.set(vname + ".coarse_start", win_start);
+
+  //rpc look back window coarse stop
+  callback.wait(5);
+  int winstop = hslb.readfee32(0X31);
+  int win_stop = winstop & 0xffff;
+  callback.set(vname + ".coarse_stop", win_stop);
+
+  //rpc look back window fine start
+  callback.wait(5);
+  int fwins = hslb.readfee32(0X32);
+  int fwin_start = fwins & 0xffff;
+  callback.set(vname + ".fine_start", fwin_start);
+
+  //rpc look back window fine stop
+  callback.wait(5);
+  int fwinstop = hslb.readfee32(0X33);
+  int fwin_stop = fwinstop & 0xffff;
+  callback.set(vname + ".fine_stop", fwin_stop);
+
 }
 
 extern "C" {

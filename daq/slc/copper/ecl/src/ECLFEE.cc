e3716b6036d25ff6d9f1470be6a243ecdcb60a52
Mikhail Remnev <RemnevMA@gmail.com>
2018-02-25 22:23:13 +0900

    1. Added PV variables for all important ECL registers. 2. Disabled BOOT operations for now. 3. Removed configuration parameters that can easily be set with sh_data_{before,after} and col_data.

--- daq/slc/copper/ecl/src/ECLFEE.cc
+++ daq/slc/copper/ecl/src/ECLFEE.cc
@@ -17,8 +17,8 @@ using namespace Belle2;
 // TODO: Reduce number of calls to getShapersInCollector
 //  (Might just set the variables in init(...) method.
 
-// TODO: Add custom operations before AND after routine operations.
 // TODO: Optimize writeToShaper via intelligent masking
+// (Note: probably won't be necessary)
 
 ECLFEE::ECLFEE() : FEE("ecl")
 {
@@ -26,15 +26,71 @@ ECLFEE::ECLFEE() : FEE("ecl")
 
 void ECLFEE::init(RCCallback& callback, HSLB& hslb, const DBObject& /*obj*/)
 {
+  // See detailed description of ECL registers @ https://confluence.desy.de/display/~remnev/ECL+Registers
+
   std::string vname = StringUtil::form("ecl[%d].", hslb.get_finid());
-  callback.add(new FEE32Handler(vname + "reg_num_wdata",         callback, hslb, *this, 0xB0));
-  callback.add(new FEE32Handler(vname + "sh_mask",               callback, hslb, *this, 0xB2));
+
+  //== Shaper data packet
+  // (In case register writing operation)
+  // high 2 bytes -- shaper register address
+  // low  2 bytes -- shaper register value
+  callback.add(new FEE32Handler(vname + "sh_packet",            callback, hslb, *this, 0xB0));
+  callback.add(new FEE32Handler(vname + "sh_mask",              callback, hslb, *this, 0xB2));
+  //== Threshold type
+  // 0-hit, 1-skip ampl, 2-low ampl, 3-ADC always
+  callback.add(new FEE32Handler(vname + "thr_type",             callback, hslb, *this, 0xB4));
+  //== Shaper control register
+  // !!! Sets its content to 0 when operation is finished.
+  //
+  // 0x1     - boot shapers with firmware stored in flash memory of ECL Collector
+  //           at 4-byte address set in 0x00B0.
+  // 0x2     - write DSP coefficients from flash memory of ECL Collector
+  //           using 4-byte address set in 0x00B0.
+  // 0x4     - write to shaper register, for all shapers in `0x00B2`.
+  //           2 high bytes of 0x00B0 contain register address, 2 low bytes
+  //           contain value to be written.
+  //           If shaper mask at 0x00B2 contains non-existent shapers (max
+  //           shaper mask is 0xFFF for barrel (cpr50__:a,b),
+  //           0x3FF for forward (cpr600_:a), 0x0FF for backward (cpr600_:b)),
+  //           operation will fail without writing anything at all!!!
+  // 0x40    - init write thresholds operations. Uses 0x00B4.
+  //           See threshold registers per channel below.
+  // 0x200   - read from shaper register with address set at `0x00B0`,
+  //           for all shapers in `0x00B2`.
+  //           Results are saved in registers 0xC0, 0xC2... 0xD6
+  callback.add(new FEE32Handler(vname + "sh_ctrl",              callback, hslb, *this, 0xB8));
+  //== Operation status, 0 == success.
+  callback.add(new FEE32Handler(vname + "sh_retval",            callback, hslb, *this, 0xB9));
   callback.add(new FEE8Handler(vname + "shaper_mask_low",       callback, hslb, *this, 0x20));
   callback.add(new FEE8Handler(vname + "shaper_mask_high",      callback, hslb, *this, 0x21));
+  //== Register #30
+  // bit 0 - output to rio
+  // bit 1 - round-robin rare event mode
+  // bit 2 - reset of calibration counter
+  // bit 3 - ttd busy
+  // bit 4 - compressing
+  // bit 5 - packing before send
+  // bit 6 - disable random trigger
+  callback.add(new FEE8Handler(vname + "reg30",                 callback, hslb, *this, 0x30));
+  //== Generator relay.
+  // Register to control two generator relays.
+  //  - 0 - relays off, 1 - first relay on, 2 - second relay on, 3 - both relays on
+  //  One relay decreases the signal by a factor of 8.
+  callback.add(new FEE8Handler(vname + "relay_g",               callback, hslb, *this, 0x31));
+  //== Rare factor
+  // Write waveform every n * 10^P event (Binary format: PPPn nnnn)
   callback.add(new FEE8Handler(vname + "ttd_trg_rare_factor",   callback, hslb, *this, 0x38));
+  //== Trigger type
+  // data to write for each
+  // normal event low byte
+  // wave form event second byte
+  //  1-dsp data 2-wave form data
   callback.add(new FEE8Handler(vname + "ttd_trg_type",          callback, hslb, *this, 0x39));
+  //== Pulse height
+  // Set in DAQ units, 1 DAQ unit ~= 10 ADC energy units ~= 0.5 MeV
   callback.add(new FEE8Handler(vname + "calib_ampl0_low",       callback, hslb, *this, 0x40));
   callback.add(new FEE8Handler(vname + "calib_ampl0_high",      callback, hslb, *this, 0x41));
+  //TODO: more documentation.
   callback.add(new FEE8Handler(vname + "calib_ampl_step_low",   callback, hslb, *this, 0x42));
   callback.add(new FEE8Handler(vname + "calib_ampl_step_high",  callback, hslb, *this, 0x43));
   callback.add(new FEE8Handler(vname + "calib_delay0_low",      callback, hslb, *this, 0x44));
@@ -42,6 +98,17 @@ void ECLFEE::init(RCCallback& callback, HSLB& hslb, const DBObject& /*obj*/)
   callback.add(new FEE8Handler(vname + "calib_delay_step_low",  callback, hslb, *this, 0x46));
   callback.add(new FEE8Handler(vname + "calib_delay_step_high", callback, hslb, *this, 0x47));
   callback.add(new FEE8Handler(vname + "calib_event_per_step",  callback, hslb, *this, 0x48));
+  //== Waveform saving mode
+  // 0x00 -- normal run, don't save waveforms
+  // 0x20 -- save waveforms using ttd_trg_rare_factor
+  callback.add(new FEE32Handler(vname + "calib_mode_num",       callback, hslb, *this, 0x80));
+  //== Data values (16 channels), used in threshold setting and in shaper register readout.
+  int addr;
+  for (int i = 0; i < 16; i++) {
+    std::string thrname = StringUtil::form("data%d", i + 1);
+    addr = 0xC0 + i;
+    callback.add(new FEE32Handler(vname + thrname,              callback, hslb, *this, addr));
+  }
 }
 
 void ECLFEE::boot(RCCallback& callback, HSLB& hslb, const DBObject& obj)
@@ -49,18 +116,22 @@ void ECLFEE::boot(RCCallback& callback, HSLB& hslb, const DBObject& obj)
   callback.log(LogFile::INFO, "Boot ecl[%d] with config %s",
                hslb.get_finid(), obj.getName().c_str());
 
-  callback.log(LogFile::INFO, "Loading shaper firmware");
-  loadShFw(callback, hslb, obj);
-  callback.log(LogFile::INFO, "Loading shaper coefficients");
-  loadShCoefs(callback, hslb, obj);
-  callback.log(LogFile::INFO, "Loading shaper parameters");
-  loadShPars(callback, hslb, obj);
+  // Disabled for now.
+  //
+  // callback.log(LogFile::INFO, "Loading shaper firmware");
+  // loadShFw(callback, hslb, obj);
+  // callback.log(LogFile::INFO, "Loading shaper coefficients");
+  // loadShCoefs(callback, hslb, obj);
+  // callback.log(LogFile::INFO, "Loading shaper parameters");
+  // loadShPars(callback, hslb, obj);
 }
 
 void ECLFEE::load(RCCallback& callback, HSLB& hslb, const DBObject& obj)
 {
-  // NOTE: sh_data became obsolete in this version. TODO: Remove it after new version
-  // is proven to work stably.
+  int slot = hslb.get_finid();
+
+  callback.log(LogFile::INFO, "Load ecl[%d] with config %s",
+               slot, obj.getName().c_str());
 
   if (obj.hasObject("sh_data_before")) {
     const DBObjectList o_sh_datas(obj.getObjects("sh_data_before"));
@@ -70,35 +141,22 @@ void ECLFEE::load(RCCallback& callback, HSLB& hslb, const DBObject& obj)
       unsigned int reg_num   = o_sh_data.getInt("addr");
       unsigned int reg_wdata = o_sh_data.getInt("data");
       // Print description for custom register into log
-      callback.log(LogFile::INFO, "ecl[%d]: %s", hslb.get_finid(),
-                   o_sh_data.getText("desc"));
+      callback.log(LogFile::INFO, "ecl[%d]: write shaper reg0x%x val0x%x (%s)",
+                   slot, reg_num, reg_wdata,
+                   o_sh_data.getText("info"));
       rio_sh_wreg(callback, hslb, sh_num, reg_num, reg_wdata);
     }
   }
 
-  callback.log(LogFile::INFO, "Load ecl[%d] with config %s",
-               hslb.get_finid(), obj.getName().c_str());
-
   hslb.writefee8(0x20, obj.getInt("shaper_mask_low"));
   hslb.writefee8(0x21, obj.getInt("shaper_mask_high"));
 
-  // Generator relay.
-  // Register to control two generator relays.
-  //  - 0 - relays off, 1 - first relay on, 2 - second relay on, 3 - both relays on
-  //  One relay decreases the signal by a factor of 8.
-  hslb.writefee8(0x31, obj.getInt("relay_g"));
-  // Waveform saving mode
-  // 0x00 -- normal run, don't save waveforms
-  // 0x20 -- save waveforms using ttd_trg_rare_factor
-  hslb.writefee32(0x80, obj.getInt("calib_mode_num"));
-  callback.log(LogFile::INFO, "write fee-32 %x << %x", 0x80, obj.getInt("calib_mode_num"));
-
   callback.log(LogFile::INFO, "write fee-8 %x << %x", 0x38, obj.getInt("ttd_trg_rare_factor"));
   hslb.writefee8(0x38, obj.getInt("ttd_trg_rare_factor"));
-  callback.log(LogFile::INFO, "read fee-8 %x >> %x", 0x38, hslb.readfee8(0x38));
+  callback.log(LogFile::INFO, "read  fee-8 %x >> %x", 0x38, hslb.readfee8(0x38));
   callback.log(LogFile::INFO, "write fee-8 %x << %x", 0x39, obj.getInt("ttd_trg_type"));
   hslb.writefee8(0x39, obj.getInt("ttd_trg_type"));
-  callback.log(LogFile::INFO, "read fee-8 %x >> %x", 0x39, hslb.readfee8(0x39));
+  callback.log(LogFile::INFO, "read  fee-8 %x >> %x", 0x39, hslb.readfee8(0x39));
 
   hslb.writefee8(0x40, obj.getInt("calib_ampl0_low"));
   hslb.writefee8(0x41, obj.getInt("calib_ampl0_high"));
@@ -110,13 +168,13 @@ void ECLFEE::load(RCCallback& callback, HSLB& hslb, const DBObject& obj)
   hslb.writefee8(0x47, obj.getInt("calib_delay_step_high"));
   hslb.writefee8(0x48, obj.getInt("calib_events_per_step"));
 
-  rio_sh_wreg(callback, hslb, 0xFFF, 0x220, obj.getInt("trg2adc_data_end"));
-  // 0 for normal run, 4 for testpulse
-  rio_sh_wreg(callback, hslb, 0xFFF, 0x221, obj.getInt("run_type"));
-  rio_sh_wreg(callback, hslb, 0xFFF, 0x223, obj.getInt("adc_data_len"));
-
-  callback.log(LogFile::INFO, "Loading shaper parameters: relays");
-  writeRelays(callback, hslb, obj);
+  const DBObjectList o_reg30s(obj.getObjects("reg30"));
+  for (size_t i = 0; i < o_reg30s.size(); i++) {
+    const DBObject& o_reg30(o_reg30s[i]);
+    unsigned int val = o_reg30.getInt("val");
+    hslb.writefee8(0x30, val);
+    callback.log(LogFile::INFO, "write fee-8 %x >> %x", 0x30, val);
+  }
 
   if (obj.hasObject("col_data")) {
     const DBObjectList o_col_datas(obj.getObjects("col_data"));
@@ -127,8 +185,8 @@ void ECLFEE::load(RCCallback& callback, HSLB& hslb, const DBObject& obj)
       std::string type       = o_col_data.getText("type");
       // Print description for custom register into log
       callback.log(LogFile::INFO, "ecl[%d]: write%s reg0x%x val0x%x (%s)",
-                   hslb.get_finid(), type, reg_num, reg_wdata,
-                   o_col_data.getText("desc"));
+                   slot, type, reg_num, reg_wdata,
+                   o_col_data.getText("info"));
       if (type == "fee8") {
         hslb.writefee8(reg_num, reg_wdata);
       } else if (type == "fee32") {
@@ -146,18 +204,10 @@ void ECLFEE::load(RCCallback& callback, HSLB& hslb, const DBObject& obj)
       unsigned int reg_wdata = o_sh_data.getInt("data");
       // Print description for custom register into log
       callback.log(LogFile::INFO, "ecl[%d]: write shaper reg0x%x val0x%x (%s)",
-                   hslb.get_finid(), reg_num, reg_wdata,
-                   o_sh_data.getText("desc"));
+                   slot, reg_num, reg_wdata,
+                   o_sh_data.getText("info"));
       rio_sh_wreg(callback, hslb, sh_num, reg_num, reg_wdata);
     }
-  }
-
-  const DBObjectList o_reg30s(obj.getObjects("reg30"));
-  for (size_t i = 0; i < o_reg30s.size(); i++) {
-    const DBObject& o_reg30(o_reg30s[i]);
-    unsigned int val = o_reg30.getInt("val");
-    hslb.writefee8(0x30, val);
-    callback.log(LogFile::INFO, "write fee-8 %x >> %x", 0x30, val);
   }
 }
 

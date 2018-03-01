36dfa38127a0a3e3a99983986bfc95ebde948ea1
Tzu-An Sheng <tzu-an.sheng@desy.de>
2018-03-01 22:54:06 +0900

    Amend file for f694d13282

---
+++ trg/cdc/dataobjects/include/CDCTriggerFinderClone.h
@@ -0,0 +1,37 @@
+#ifndef CDCTRIGGERFINDERCLONE_H
+#define CDCTRIGGERFINDERCLONE_H
+
+#include <framework/datastore/RelationsObject.h>
+
+namespace Belle2 {
+
+  /** Additional information from the 2D finder unpacker of the CDC trigger.
+
+      Contains information for the clone study
+   */
+  class CDCTriggerFinderClone : public RelationsObject {
+  public:
+    /** default constructor */
+    CDCTriggerFinderClone():
+      oldTrack(true) { }
+
+    /** constructor with arguments */
+    CDCTriggerFinderClone(bool inOldTrack):
+      oldTrack(inOldTrack) { }
+
+    /** destructor, empty because we don't allocate memory anywhere. */
+    ~CDCTriggerFinderClone() { }
+
+    // accessors
+    /** get size of bounding rectangle of cluster */
+    bool isOldTrack() const { return oldTrack; }
+
+  protected:
+    /** whether this is an old track */
+    bool oldTrack;
+
+    //! Needed to make the ROOT object storable
+    ClassDef(CDCTriggerFinderClone, 1);
+  };
+}
+#endif

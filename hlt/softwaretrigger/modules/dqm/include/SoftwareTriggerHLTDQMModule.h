d35193dcbd4797921738e0e8f703f0a84b95f0ba
Nils Braun <nils.braun@desy.de>
2018-03-14 19:58:49 +0100

    Refactored the STM DQM plot a lot to include everything that is needed

--- hlt/softwaretrigger/modules/dqm/include/SoftwareTriggerHLTDQMModule.h
+++ hlt/softwaretrigger/modules/dqm/include/SoftwareTriggerHLTDQMModule.h
@@ -1,75 +1,67 @@
-#ifndef SOFTWARETRIGGERHLTDQMMODULE_H
-#define SOFTWARETRIGGERHLTDQMMODULE_H
-//+
-// File : SoftwareTriggerHLTDQMModule.h
-// Description : Module to monitor raw data accumulating histos
-//
-// Author : Chunhua LI, the University of Melbourne
-//          Thomas Hauth
-// Date :  4 - March - 2015
-//-
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2018 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Chunhua Li, Thomas Hauth, Nils Braun                     *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+#include <framework/core/HistoModule.h>
+
+#include <TH1F.h>
+
+#include <mdst/dataobjects/SoftwareTriggerResult.h>
+#include <hlt/softwaretrigger/dataobjects/SoftwareTriggerVariables.h>
+
+#include <framework/datastore/StoreObjPtr.h>
 
 #include <string>
 #include <vector>
 #include <map>
 
-#include <TH1F.h>
-
-#include <framework/core/HistoModule.h>
-
-
 namespace Belle2 {
   namespace SoftwareTrigger {
-
-    /*! A class definition of an input module for Sequential ROOT I/O */
-
+    /// Module defining the STM histograms
     class SoftwareTriggerHLTDQMModule : public HistoModule {
-
-      // Public functions
     public:
-
       //! Constructor / Destructor
       SoftwareTriggerHLTDQMModule();
-      virtual ~SoftwareTriggerHLTDQMModule() = default;
 
       //! Module functions to be called from main process
-      virtual void initialize() override;
+      void initialize() override;
 
       //! Module functions to be called from event process
-      virtual void event() override;
+      void event() override;
 
       //! Histogram definition
-      virtual void defineHisto() override;
-
-      // Data members
+      void defineHisto() override;
 
     private:
+      // Parameters
+      /// Which cuts should be reported? Please remember to include the total_result also, if wanted.
+      std::map<std::string, std::vector<std::string>> m_param_cutResultIdentifiers;
 
-      /// contains a list of sw trigger variables that should be plotted
-      std::vector<std::string> m_param_triggerVariables;
-
-      /// Base identifier for all variables and cut results reported
-      std::string m_param_baseIdentifier = "hlt";
-
-      /// List of cut results to include in the final dqm plots
-      std::vector<std::string> m_param_cutIdentifiers;
+      /// Which variables should be reported?
+      std::vector<std::string> m_param_variableIdentifiers;
 
       /// Directory to put the generated histograms
       std::string m_param_histogramDirectoryName = "softwaretrigger";
 
-      /// contains the histogram holding the final sw trigger decision when
-      /// all individual cuts are combined
-      TH1F* m_totalResultHistogram;
+      // Histograms
+      /// histograms for the final sw trigger decisions for each base identifier
+      std::map<std::string, TH1F> m_cutResultHistograms;
 
-      /// contains the histograms holding the sw trigger variables plots
-      std::map<std::string, TH1F*> m_triggerVariablesHistogram;
+      /// histograms for the software trigger variables in all calculators (although maybe not filled)
+      std::map<std::string, TH1F> m_triggerVariablesHistograms;
 
-      /// contains the histograms holnding the results of the cuts
-      std::map<std::string, TH1F*> m_cutResultsHistogram;
-
+      // Datastore members
+      /// STM cut results
+      StoreObjPtr<SoftwareTriggerResult> m_triggerResult;
+      /// STM cut variables
+      StoreObjPtr<SoftwareTriggerVariables> m_variables;
     };
-
-  } // end namespace SoftwareTrigger
-} // end namespace Belle2
-
-#endif // SOFTWARETRIGGERDQMMODULE_H
+  }
+}
--- hlt/softwaretrigger/modules/dqm/src/SoftwareTriggerHLTDQMModule.cc
+++ hlt/softwaretrigger/modules/dqm/src/SoftwareTriggerHLTDQMModule.cc
@@ -1,88 +1,92 @@
-//+
-// File : SoftwareTriggerHLTDQMModule.cc
-// Description : Module to monitor physics trigger on HLT
-//
-// Author : Chunhua LI, Thomas Hauth
-// Date : 13 - Oct - 2016
-//-
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2018 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Chunhua Li, Thomas Hauth, Nils Braun                     *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
 #include <hlt/softwaretrigger/modules/dqm/SoftwareTriggerHLTDQMModule.h>
-#include <mdst/dataobjects/SoftwareTriggerResult.h>
-#include <hlt/softwaretrigger/dataobjects/SoftwareTriggerVariables.h>
-#include <framework/datastore/StoreObjPtr.h>
 
 #include <TDirectory.h>
 
-#include <map>
-#include <string>
-#include <iostream>
+#include <hlt/softwaretrigger/core/SoftwareTriggerDBHandler.h>
 #include <hlt/softwaretrigger/core/FinalTriggerDecisionCalculator.h>
+
+#include <framework/core/ModuleParam.templateDetails.h>
 
 using namespace Belle2;
 using namespace SoftwareTrigger;
 
-
-//-----------------------------------------------------------------
-//                 Register the Module
-//-----------------------------------------------------------------
 REG_MODULE(SoftwareTriggerHLTDQM)
 
-//-----------------------------------------------------------------
-//                 Implementation
-//-----------------------------------------------------------------
 
 SoftwareTriggerHLTDQMModule::SoftwareTriggerHLTDQMModule() : HistoModule()
 {
-  //Set module properties
-
   setDescription("Monitor Physics Trigger");
   setPropertyFlags(c_ParallelProcessingCertified);
 
-  addParam("triggerVariables", m_param_triggerVariables, "List of trigger variables"
-           "that should be written into histograms. The option preScaleStoreDebugOutputToDataStore of the "
-           "SoftwareTriggerModule needs to be enabled for this to work.", m_param_triggerVariables);
+  // Fill in the default values of the module parameters
+  m_param_variableIdentifiers = {"fast_reco_visible_energy"};
 
-  addParam("baseIdentifier", m_param_baseIdentifier, "Base identifier for all variables and cut results reported",
-           m_param_baseIdentifier);
+  m_param_cutResultIdentifiers["fast_reco"] = {"total_result"};
+  m_param_cutResultIdentifiers["hlt"] = {"total_result"};
 
-  addParam("cutIdentifiers", m_param_cutIdentifiers, "List of identifiers for the different cuts to include in the plotting",
-           m_param_cutIdentifiers);
+  addParam("cutResultIdentifiers", m_param_cutResultIdentifiers,
+           "Which cuts should be reported? Please remember to include the total_result also, if wanted.",
+           m_param_cutResultIdentifiers);
+
+  addParam("variableIdentifiers", m_param_variableIdentifiers,
+           "Which variables should be reported?",
+           m_param_variableIdentifiers);
 
   addParam("histogramDirectoryName", m_param_histogramDirectoryName,
            "SoftwareTrigger DQM histograms will be put into this directory", m_param_histogramDirectoryName);
-
 }
 
 void SoftwareTriggerHLTDQMModule::defineHisto()
 {
-  // move to the specified folder
-  TDirectory* oldDir = nullptr;
+  TDirectory* oldDirectory = nullptr;
 
   if (!m_param_histogramDirectoryName.empty()) {
-    oldDir = gDirectory;
-    TDirectory* histDir = oldDir->mkdir(m_param_histogramDirectoryName.c_str());
+    oldDirectory = gDirectory;
+    TDirectory* histDir = oldDirectory->mkdir(m_param_histogramDirectoryName.c_str());
     histDir->cd();
   }
 
-  for (auto const& s : m_param_triggerVariables) {
+  for (auto const& variable : m_param_variableIdentifiers) {
     // todo: make correct range
-    auto pVarName = new TH1F(s.c_str(), s.c_str(), 50, 0, 50);
-
-    pVarName->SetXTitle(("SoftwareTriggerVariable " + s).c_str());
-    m_triggerVariablesHistogram[s] = pVarName;
+    const unsigned int numberOfBins = 50;
+    const double lowerX = 0;
+    const double upperX = 50;
+    m_triggerVariablesHistograms.emplace(variable, TH1F(variable.c_str(), variable.c_str(), numberOfBins, lowerX, upperX));
+    m_triggerVariablesHistograms[variable].SetXTitle(("SoftwareTriggerVariable " + variable).c_str());
   }
 
-  for (auto const& s : m_param_cutIdentifiers) {
-    // can hold the values -1, 0, 1
-    auto pCutResultHistogram = new TH1F(s.c_str(), s.c_str(), 3, -1.5 , 1.5);
-    pCutResultHistogram->SetXTitle(("Cut Result for " + s).c_str());
-    m_cutResultsHistogram[s] = pCutResultHistogram;
+  for (const auto& cutIdentifier : m_param_cutResultIdentifiers) {
+    const std::string& baseIdentifier = cutIdentifier.first;
+    const auto& cuts = cutIdentifier.second;
+
+    const unsigned int numberOfBins = cuts.size();
+    const double lowerX = 0;
+    const double upperX = numberOfBins;
+    m_cutResultHistograms.emplace(baseIdentifier,
+                                  TH1F(baseIdentifier.c_str(), baseIdentifier.c_str(), numberOfBins, lowerX, upperX));
+    m_cutResultHistograms[baseIdentifier].SetXTitle(("Cut Result for " + baseIdentifier).c_str());
   }
 
-  m_totalResultHistogram = new TH1F("Total Cut Result", "Total Cut Result", 3, -1.5 , 1.5);
-  m_totalResultHistogram->SetXTitle("Total Cut Result");
+  // We add one for the total result
+  const unsigned int numberOfBins = m_param_cutResultIdentifiers.size() + 1;
+  const double lowerX = 0;
+  const double upperX = numberOfBins;
+  m_cutResultHistograms.emplace("total_result",
+                                TH1F("total_result", "total_result", numberOfBins, lowerX, upperX));
+  m_cutResultHistograms["total_result"].SetXTitle("Total Cut Result");
 
-  if (oldDir)
-    oldDir->cd();
+  if (oldDirectory) {
+    oldDirectory->cd();
+  }
 }
 
 
@@ -94,91 +98,42 @@ void SoftwareTriggerHLTDQMModule::initialize()
 
 void SoftwareTriggerHLTDQMModule::event()
 {
-  // load and plot the output of the variable computation
-  StoreObjPtr<SoftwareTriggerVariables> variables;
-
   // this might be pre-scaled for performance reasons in the final configuration, therefore this structure
   // might not be filled in every event
-  if (variables.isValid()) {
-    for (auto& varHistPair : m_triggerVariablesHistogram) {
-
-      const auto fullName = m_param_baseIdentifier + "_" + varHistPair.first;
+  if (m_variables.isValid()) {
+    for (auto& variableNameAndTH1F : m_triggerVariablesHistograms) {
+      const std::string& variable = variableNameAndTH1F.first;
+      TH1F& histogram = variableNameAndTH1F.second;
 
       // try to load this variable from the computed trigger variables
-      if (!variables->has(fullName)) {
-        B2FATAL("Variable " << fullName << " configured for SoftwareTriggerDQM plotting is not available");
+      if (!m_variables->has(variable)) {
+        B2FATAL("Variable " << variable << " configured for SoftwareTriggerDQM plotting is not available");
       } else {
-        const auto value = variables->getVariable(fullName);
-        varHistPair.second->Fill(value);
+        const double value = m_variables->getVariable(variable);
+        histogram.Fill(value);
       }
     }
   }
 
-  // load and plot the Cut results
-  StoreObjPtr<SoftwareTriggerResult> triggerResult;
+  if (m_triggerResult.isValid()) {
+    for (auto const& cutIdentifier : m_param_cutResultIdentifiers) {
+      const std::string& baseIdentifier = cutIdentifier.first;
+      const auto& cuts = cutIdentifier.second;
 
-  if (triggerResult.isValid()) {
-    for (auto const& cutIdentifier : m_param_cutIdentifiers) {
-      auto fullCutIndenfitier = "software_trigger_cut&" + m_param_baseIdentifier + "&" + cutIdentifier;
-      auto cutResult = triggerResult->getResult(fullCutIndenfitier);
+      for (const std::string& cutName : cuts) {
+        const std::string& fullCutIdentifier = SoftwareTriggerDBHandler::makeFullCutName(baseIdentifier, cutName);
+        const int cutResult = static_cast<int>(m_triggerResult->getResult(fullCutIdentifier));
 
-      int cutResultInt = 0;
-      if (cutResult == SoftwareTriggerCutResult::c_accept) {
-        cutResultInt = 1;
-      } else if (cutResult == SoftwareTriggerCutResult::c_noResult) {
-        cutResultInt = 0;
-      } else if (cutResult == SoftwareTriggerCutResult::c_reject) {
-        cutResultInt = -1;
-      } else {
-        B2FATAL("Unsupported SoftwareTriggerCutResult value");
+        m_cutResultHistograms[baseIdentifier].Fill(fullCutIdentifier.c_str(), cutResult);
       }
 
-      m_cutResultsHistogram.at(cutIdentifier)->Fill(cutResultInt);
+      const std::string& totalCutIdentifier = SoftwareTriggerDBHandler::makeTotalCutName(baseIdentifier);
+      const int cutResult = static_cast<int>(m_triggerResult->getResult(totalCutIdentifier));
+
+      m_cutResultHistograms[baseIdentifier].Fill(totalCutIdentifier.c_str(), cutResult);
     }
 
-    // todo: support override rejects option
-    m_totalResultHistogram->Fill(FinalTriggerDecisionCalculator::getFinalTriggerDecision(*triggerResult));
+    const bool totalResult = FinalTriggerDecisionCalculator::getFinalTriggerDecision(*m_triggerResult);
+    m_cutResultHistograms["total_result"].Fill("total_result", totalResult);
   }
-
-  /*
-   * Legacy code which needs to be replaced by adding the proper
-   * triggerVariables to the configuration
-  const auto& variableMap = variables->get();
-  double numberofTracks = variableMap.at("hlt_nTracksLE");
-  double visibleEnergy = variableMap.at("hlt_VisibleEnergyLE");
-  double totalEnergy = variableMap.at("hlt_EtotLE");
-  h_NTrack->Fill(numberofTracks);
-  h_ESum->Fill(totalEnergy);
-  h_EVis->Fill(visibleEnergy);
-
-
-  StoreObjPtr<SoftwareTriggerResult> result;
-  //Fill h_hlt_map and h_hlt_unique_map
-  std::string hlt_trigger[] = {"hadron", "bhabha", "tau_tau", "2_tracks", "1_track1_cluster", "mu_mu", "gamma_gamma"};
-  int numberelement = sizeof(hlt_trigger) / sizeof(hlt_trigger[0]);
-  int record_unique[2] = {0, -1};
-  for (int i = 1; i <= numberelement; i++) {
-    std::string cutname = "software_trigger_cut&hlt&accept_" + hlt_trigger[i - 1];
-    int hltres = (int)result->getResult(cutname);
-    if (hltres) {
-      h_hlt_map->Fill(i);
-      record_unique[0]++;
-      record_unique[1] = i;
-    }
-  }
-  if (record_unique[0] == 1) h_hlt_unique_map->Fill(record_unique[1]);
-
-  //Fill histgrom of intermediate states
-  std::string dqm_part[] = {"D0", "Dplus", "Dstar", "Jpsiee", "Jpsimumu"};
-  int dqmsize = sizeof(dqm_part) / sizeof(dqm_part[0]);
-  for (int i = 0; i < dqmsize; i++) {
-    std::string dqmcutname = "software_trigger_cut&calib&accept_dqm_" + dqm_part[i];
-    int dqmres = (int)result->getResult(dqmcutname);
-    if (dqmres) {
-      std::string varname = "calib_dqm_" + dqm_part[i] + "_M";
-      double mass = variableMap.at(varname);
-      h_dqm[i]->Fill(mass);
-    }
-  }
-  */
 }

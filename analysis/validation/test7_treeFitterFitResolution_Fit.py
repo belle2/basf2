1ce6828b5ce972f061e222a9d15452f47127cc9e
Jo-Frederik Krohn <jo-frederik.krohn@desy.de>
2018-03-15 02:44:51 +0000

    validation scripts for treefitter

--- analysis/validation/test7_treeFitterFitResolution_Fit.py
+++ analysis/validation/test7_treeFitterFitResolution_Fit.py
@@ -11,19 +11,14 @@
 
 from basf2 import *
 from modularAnalysis import *
-from modularAnalysis import analysis_main
-from modularAnalysis import fillParticleList
-from modularAnalysis import reconstructDecay
 
 from vertex import *
-from vertex import vertexTree
 
 from variables import variables
 import sys
 
-# Get Special GT for the flavor tagger weight files
 use_central_database("development")
-
+set_random_seed('#BAADF00D')
 
 if 'BELLE2_VALIDATION_DATA_DIR' not in os.environ:
     sys.exit(0)
@@ -31,13 +26,12 @@ inputFile = os.path.join(os.environ['BELLE2_VALIDATION_DATA_DIR'], 'analysis/mds
 inputMdst('default', inputFile)
 
 
-# Reconstruction of signal side and MC match
 fillParticleList('pi+:all', '')
 fillParticleList('mu+:all', '')
 
 reconstructDecay('K_S0:pipi -> pi+:all pi-:all', 'dM<0.25')
 reconstructDecay('J/psi:mumu -> mu+:all mu-:all', 'dM<0.11')
-reconstructDecay('B0:jpsiks -> J/psi:mumu K_S0:pipi', 'Mbc > 5.2 and abs(deltaE)<0.2')
+reconstructDecay('B0:jpsiks -> J/psi:mumu K_S0:pipi', 'Mbc > 5.27 and abs(deltaE)<0.2')
 
 matchMCTruth('B0:jpsiks')
 
@@ -72,44 +66,44 @@ variables = [
     'mcDX',
     'mcDY',
     'mcDZ',
-
-    'daughter(0,x)',  # J/Psi
-    'daughter(0,y)',
-    'daughter(0,z)',
-    'daughter(0,z_uncertainty)',
-    'daughter(0,y_uncertainty)',
-    'daughter(0,x_uncertainty)',
-    'daughter(0,mcDX)',
-    'daughter(0,mcDY)',
-    'daughter(0,mcDX)',
-
-    'daughter(0,p)',
-    'daughter(0,mcP)',
-    'daughter(0,pErr)',
-    'daughter(0,E)',
-    'daughter(0,mcE)',
-    'daughter(0,E_uncertainty)',
-    'daughter(0,M)',
-    'daughter(0,InvM)',
-
-    'daughter(1,x)',  # Ks
-    'daughter(1,y)',
-    'daughter(1,z)',
-    'daughter(1,z_uncertainty)',
-    'daughter(1,y_uncertainty)',
-    'daughter(1,x_uncertainty)',
-    'daughter(1,mcDX)',
-    'daughter(1,mcDY)',
-    'daughter(1,mcDX)',
-
-    'daughter(1,p)',
-    'daughter(1,mcP)',
-    'daughter(1,pErr)',
-    'daughter(1,E)',
-    'daughter(1,mcE)',
-    'daughter(1,E_uncertainty)',
-    'daughter(1,M)',
-    'daughter(1,InvM)',
+    # not really needed
+    #    'daughter(0,x)',  # J/Psi
+    #    'daughter(0,y)',
+    #    'daughter(0,z)',
+    #    'daughter(0,z_uncertainty)',
+    #    'daughter(0,y_uncertainty)',
+    #    'daughter(0,x_uncertainty)',
+    #    'daughter(0,mcDX)',
+    #    'daughter(0,mcDY)',
+    #    'daughter(0,mcDX)',
+    #
+    #    'daughter(0,p)',
+    #    'daughter(0,mcP)',
+    #    'daughter(0,pErr)',
+    #    'daughter(0,E)',
+    #    'daughter(0,mcE)',
+    #    'daughter(0,E_uncertainty)',
+    #    'daughter(0,M)',
+    #    'daughter(0,InvM)',
+    #
+    #    'daughter(1,x)',  # Ks
+    #    'daughter(1,y)',
+    #    'daughter(1,z)',
+    #    'daughter(1,z_uncertainty)',
+    #    'daughter(1,y_uncertainty)',
+    #    'daughter(1,x_uncertainty)',
+    #    'daughter(1,mcDX)',
+    #    'daughter(1,mcDY)',
+    #    'daughter(1,mcDX)',
+    #
+    #    'daughter(1,p)',
+    #    'daughter(1,mcP)',
+    #    'daughter(1,pErr)',
+    #    'daughter(1,E)',
+    #    'daughter(1,mcE)',
+    #    'daughter(1,E_uncertainty)',
+    #    'daughter(1,M)',
+    #    'daughter(1,InvM)',
 ]
 
 analysis_main.add_module('VariablesToNtuple',

6d976be8b7d8160961e38007d527beed16d908b1
Jake Bennett <jake.bennett@desy.de>
2018-03-16 17:31:19 -0400

    Make it easier to keep mdst and cdst output consistent

--- mdst/scripts/cdst.py
+++ mdst/scripts/cdst.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from basf2 import *
+from mdst import add_mdst_output
 
 
 def add_cdst_output(
@@ -12,7 +13,8 @@ def add_cdst_output(
     dataDescription=None,
 ):
     """
-    This function adds the MDST output modules to a path, saving only objects defined as part of the MDST data format.
+    This function adds the cDST output modules (mDST + calibration objects) to a path,
+    saving only objects defined as part of the cDST data format.
 
     @param path Path to add modules to
     @param mc Save Monte Carlo quantities? (MCParticles and corresponding relations)
@@ -22,27 +24,6 @@ def add_cdst_output(
            fields to the output FileMetaData
     """
 
-    output = register_module('RootOutput')
-    output.param('outputFileName', filename)
-    branches = [
-        'Tracks',
-        'V0s',
-        'TrackFitResults',
-        'PIDLikelihoods',
-        'TracksToPIDLikelihoods',
-        'ECLClusters',
-        'TracksToECLClusters',
-        'KLMClusters',
-        'KlIds',
-        'KLMClustersToKlIds',
-        'TRGSummary',
-        'SoftwareTriggerResult',
-    ]
-    persistentBranches = ['FileMetaData']
-    if mc:
-        branches += ['MCParticles', 'TracksToMCParticles',
-                     'ECLClustersToMCParticles', 'KLMClustersToMCParticles']
-        persistentBranches += ['BackgroundInfo']
     calibrationBranches = [
         'RecoTracks',
         'EventT0',
@@ -65,16 +46,7 @@ def add_cdst_output(
         'EKLMDigits',
         'ExtHits',
     ]
-    branches += calibrationBranches
-    branches += additionalBranches
-    output.param('branchNames', branches)
-    output.param('branchNamesPersistent', persistentBranches)
-    # set dataDescription correctly
     if dataDescription is None:
         dataDescription = {}
-    # set dataLevel to cdst if it's not already set to something else (which
-    # might happen for udst output since that calls this function)
     dataDescription.setdefault("dataLevel", "cdst")
-    output.param("additionalDataDescription", dataDescription)
-    path.add_module(output)
-    return output
+    mdst.add_mdst_output(path, mc, filename, additionalBranches + calibrationBranches, dataDescription)

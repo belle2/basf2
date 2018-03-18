ff33b941507e38da754304d59ea99ac7f4cdef2f
Dmitry Matvienko <dmitry.matvienko@desy.de>
2018-03-18 20:14:16 +0900

    ECL namespace is removed in header file

--- ecl/modules/eclDQM/include/eclDQM.h
+++ ecl/modules/eclDQM/include/eclDQM.h
@@ -32,102 +32,101 @@
 
 
 namespace Belle2 {
-  namespace ECL {
 
-    /**
-     * This module is for ECL Data Quality Monitor.
-     */
-    class ECLDQMModule : public HistoModule {  /**< derived from HistoModule class. */
+  /**
+   * This module is for ECL Data Quality Monitor.
+   */
 
-    public:
+  class ECLDQMModule : public HistoModule {  /**< derived from HistoModule class. */
 
-      /** Constructor. */
-      ECLDQMModule();
+  public:
 
-      /** Destructor. */
-      virtual ~ECLDQMModule();
+    /** Constructor. */
+    ECLDQMModule();
 
-      /** Initialize the module. */
-      virtual void initialize();
-      /** Call when a run begins. */
-      virtual void beginRun();
-      /** Event processor. */
-      virtual void event();
-      /** Call when a run ends. */
-      virtual void endRun();
-      /** Terminate. */
-      virtual void terminate();
+    /** Destructor. */
+    virtual ~ECLDQMModule();
 
-      /** Function to define histograms. */
-      virtual void defineHisto();
+    /** Initialize the module. */
+    virtual void initialize();
+    /** Call when a run begins. */
+    virtual void beginRun();
+    /** Event processor. */
+    virtual void event();
+    /** Call when a run ends. */
+    virtual void endRun();
+    /** Terminate. */
+    virtual void terminate();
 
-    private:
-      /** Global event number. */
-      int m_iEvent;
-      /** Histogram directory in ROOT file. */
-      std::string m_histogramDirectoryName;
-      /** Upper threshold of number of hits in event. */
-      int m_NHitsUpperThr1;
-      /** Upper threshold of number of hits in event (w/ Thr = 10 MeV). */
-      int m_NHitsUpperThr2;
-      /** Upper threshold of energy deposition in event, [GeV]. */
-      double m_EnergyUpperThr;
-      /** Lower threshold of pedestal distribution. */
-      int m_PedestalMeanLowerThr;
-      /** Upper threshold of pedestal distribution. */
-      int m_PedestalMeanUpperThr;
-      /** Upper threshold of pedestal rms error distribution. */
-      double m_PedestalRmsUpperThr;
-      /** WF sampling points for digit array.   */
-      int m_DspArray[8736][31];
-      /** Pedestal average values.   */
-      int m_PedestalMean[8736];
-      /** Pedestal rms error values.    */
-      int m_PedestalRms[8736];
+    /** Function to define histograms. */
+    virtual void defineHisto();
 
-      /** Histogram: Crystal Cell IDs w/o software threshold.  */
-      TH1F* h_cid;
-      /** Histogram: Crystal Cell IDs above threshold = 5 MeV.  */
-      TH1F* h_cid_Thr5MeV;
-      /** Histogram: Crystal Cell IDs above threshold = 10 MeV. */
-      TH1F* h_cid_Thr10MeV;
-      /** Histogram: Crystal Cell IDs above threshold = 50 MeV.  */
-      TH1F* h_cid_Thr50MeV;
-      /** Histogram: Energy deposition in event. */
-      TH1F* h_edep;
-      /** Histogram: Reconstructed signal time for the barrel calorimeter above the threshold = 5 MeV.  */
-      TH1F* h_time_barrel_Thr5MeV;
-      /** Histogram: Reconstructed signal time for the endcap calorimeter above the threshold = 5 MeV.  */
-      TH1F* h_time_endcaps_Thr5MeV;
-      /** Histogram: Reconstructed signal time for the barrel calorimeter above the threshold = 10 MeV. */
-      TH1F* h_time_barrel_Thr10MeV;
-      /** Histogram: Reconstructed signal time for the endcap calorimeter above the threshold = 10 MeV. */
-      TH1F* h_time_endcaps_Thr10MeV;
-      /** Histogram: Reconstructed signal time for the barrel calorimeter above the threshold = 50 MeV. */
-      TH1F* h_time_barrel_Thr50MeV;
-      /** Histogram: Reconstructed signal time for the endcap calorimeter above the threshold = 50 MeV. */
-      TH1F* h_time_endcaps_Thr50MeV;
-      /** Histogram: Fit quality flag (0 - good, 1 - large amplitude, 3 - bad chi2). */
-      TH1F* h_quality;
-      /** Histogram: Number of hits in each event w/o software threshold.  */
-      TH1F* h_ncev;
-      /** Histogram: Number of hits in each event above the treshold = 10 MeV.  */
-      TH1F* h_ncev_Thr10MeV;
-      /** Histogram: Trigger tag flag #1. */
-      TH1F* h_trigtag1;
-      /** Histogram: Flag of ADC samples. */
-      TH1F* h_adc_flag;
-      /** Histogram: Fraction of ADC samples in event (w/o 8736 ADC samples). */
-      TH1F* h_adc_hits;
-      /** Histogram: Trigger time vs. Trig Cell ID.  */
-      TH2F* h_trigtime_trigid;
-      /** Histogram: Trigger tag flag #2 vs. Trig Cell ID.   */
-      TH2F* h_trigtag2_trigid;
-      /** Histogram: Pedestal Average vs. Cell ID.   */
-      TH2F* h_pedmean_cellid;
-      /** Histogram: Pedestal rms error vs. Cell ID.   */
-      TH2F* h_pedrms_cellid;
-    };
-  }; // end ECL namespace
+  private:
+    /** Global event number. */
+    int m_iEvent;
+    /** Histogram directory in ROOT file. */
+    std::string m_histogramDirectoryName;
+    /** Upper threshold of number of hits in event. */
+    int m_NHitsUpperThr1;
+    /** Upper threshold of number of hits in event (w/ Thr = 10 MeV). */
+    int m_NHitsUpperThr2;
+    /** Upper threshold of energy deposition in event, [GeV]. */
+    double m_EnergyUpperThr;
+    /** Lower threshold of pedestal distribution. */
+    int m_PedestalMeanLowerThr;
+    /** Upper threshold of pedestal distribution. */
+    int m_PedestalMeanUpperThr;
+    /** Upper threshold of pedestal rms error distribution. */
+    double m_PedestalRmsUpperThr;
+    /** WF sampling points for digit array.   */
+    int m_DspArray[8736][31];
+    /** Pedestal average values.   */
+    int m_PedestalMean[8736];
+    /** Pedestal rms error values.    */
+    int m_PedestalRms[8736];
+
+    /** Histogram: Crystal Cell IDs w/o software threshold.  */
+    TH1F* h_cid;
+    /** Histogram: Crystal Cell IDs above threshold = 5 MeV.  */
+    TH1F* h_cid_Thr5MeV;
+    /** Histogram: Crystal Cell IDs above threshold = 10 MeV. */
+    TH1F* h_cid_Thr10MeV;
+    /** Histogram: Crystal Cell IDs above threshold = 50 MeV.  */
+    TH1F* h_cid_Thr50MeV;
+    /** Histogram: Energy deposition in event. */
+    TH1F* h_edep;
+    /** Histogram: Reconstructed signal time for the barrel calorimeter above the threshold = 5 MeV.  */
+    TH1F* h_time_barrel_Thr5MeV;
+    /** Histogram: Reconstructed signal time for the endcap calorimeter above the threshold = 5 MeV.  */
+    TH1F* h_time_endcaps_Thr5MeV;
+    /** Histogram: Reconstructed signal time for the barrel calorimeter above the threshold = 10 MeV. */
+    TH1F* h_time_barrel_Thr10MeV;
+    /** Histogram: Reconstructed signal time for the endcap calorimeter above the threshold = 10 MeV. */
+    TH1F* h_time_endcaps_Thr10MeV;
+    /** Histogram: Reconstructed signal time for the barrel calorimeter above the threshold = 50 MeV. */
+    TH1F* h_time_barrel_Thr50MeV;
+    /** Histogram: Reconstructed signal time for the endcap calorimeter above the threshold = 50 MeV. */
+    TH1F* h_time_endcaps_Thr50MeV;
+    /** Histogram: Fit quality flag (0 - good, 1 - large amplitude, 3 - bad chi2). */
+    TH1F* h_quality;
+    /** Histogram: Number of hits in each event w/o software threshold.  */
+    TH1F* h_ncev;
+    /** Histogram: Number of hits in each event above the treshold = 10 MeV.  */
+    TH1F* h_ncev_Thr10MeV;
+    /** Histogram: Trigger tag flag #1. */
+    TH1F* h_trigtag1;
+    /** Histogram: Flag of ADC samples. */
+    TH1F* h_adc_flag;
+    /** Histogram: Fraction of ADC samples in event (w/o 8736 ADC samples). */
+    TH1F* h_adc_hits;
+    /** Histogram: Trigger time vs. Trig Cell ID.  */
+    TH2F* h_trigtime_trigid;
+    /** Histogram: Trigger tag flag #2 vs. Trig Cell ID.   */
+    TH2F* h_trigtag2_trigid;
+    /** Histogram: Pedestal Average vs. Cell ID.   */
+    TH2F* h_pedmean_cellid;
+    /** Histogram: Pedestal rms error vs. Cell ID.   */
+    TH2F* h_pedrms_cellid;
+  };
 }; // end Belle2 namespace
 #endif

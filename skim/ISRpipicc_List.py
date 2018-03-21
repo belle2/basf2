a74e113bbd4ca7e713937edc9f8513c554e83f11
SEN Jia <jiasen@buaa.edu.cn>
2018-03-21 23:47:52 +0900

    add new files: ISRpipicc_List.py and ISRpipicc_Skim_Standalone.py for Charmonium

---
+++ skim/ISRpipicc_List.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#######################################################
+#
+# Charmonium skims
+# Sen Jia, 21/Mar/2018
+#
+######################################################
+
+from basf2 import *
+from modularAnalysis import *
+
+
+def ISRpipiccList():
+    fillParticleList('gamma:tight', '')
+    fillParticleList('e+:loose', '')
+    fillParticleList('mu+:loose', '')
+    fillParticleList('pi+:loose', '')
+    reconstructDecay('J/psi:ee -> e+:loose e-:loose', 'M>2.7 and M<3.0')
+    reconstructDecay('J/psi:mumu -> mu+:loose mu-:loose', 'M>2.7 and M<3.0')
+    reconstructDecay('psi(2S):ee -> pi+:loose pi-:loose e+:loose e-:loose', 'M>3.64 and M<3.74')
+    reconstructDecay('psi(2S):mumu -> pi+:loose pi-:loose mu+:loose mu-:loose', 'M>3.64 and M<3.74')
+    vpho_Channels = [
+                   'gamma:tight pi+:loose pi-:loose J/psi:ee',
+                   'gamma:tight pi+:loose pi-:loose J/psi:mumu',
+                   'gamma:tight pi+:loose pi-:loose psi(2S):ee',
+                   'gamma:tight pi+:loose pi-:loose psi(2S):mumu'
+                   ]
+    vphoList = []
+    for chID, channel in enumerate(vpho_Channels):
+        reconstructDecay('vpho:myCombination' + str(chID) + ' -> ' + channel, '', chID)
+        vphoList.append('vpho:myCombination' + str(chID))
+
+    Lists = vphoList
+    return Lists

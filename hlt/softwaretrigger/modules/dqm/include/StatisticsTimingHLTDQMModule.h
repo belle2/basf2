9995402cdd4a1714206bcb874f067f2f9961ee09
Nils Braun <nils.braun@desy.de>
2018-04-06 17:09:01 +0200

    Refactor the statistics module to show nicer timing stats

--- hlt/softwaretrigger/modules/dqm/include/StatisticsTimingHLTDQMModule.h
+++ hlt/softwaretrigger/modules/dqm/include/StatisticsTimingHLTDQMModule.h
@@ -1,64 +1,53 @@
-#ifndef STATISTICSTIMINGHLTDQMMODULE_H
-#define STATISTICSTIMINGHLTDQMMODULE_H
-
-#include <string>
-#include <vector>
-#include <map>
-
-#include <TH1F.h>
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2018 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Chunhua Li, Thomas Hauth, Nils Braun                     *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
 
 #include <framework/core/HistoModule.h>
+#include <vector>
+#include <string>
+
+class TH1F;
 
 namespace Belle2 {
   namespace SoftwareTrigger {
-
-    /*! A class definition of an input module for Sequential ROOT I/O */
-
+    /// Module for Event Time Statistics DQM plots.
     class StatisticsTimingHLTDQMModule : public HistoModule {
-
-      // Public functions
     public:
-
-      //! Constructor / Destructor
+      /// Constructor for module parameters
       StatisticsTimingHLTDQMModule();
-      virtual ~StatisticsTimingHLTDQMModule();
 
-      //! Module functions to be called from main process
-      virtual void initialize();
+      /// Define the histograms
+      void initialize();
 
-      //! Module functions to be called from event process
-      virtual void event();
+      /// Full the histograms
+      void event();
 
-      //! Histogram definition
-      virtual void defineHisto();
-
+      /// Histogram definition
+      void defineHisto();
 
     private:
-      /// Directory to put the generated histograms
-      std::string m_param_histogramDirectoryName;
-      /**mean process time of event*/
-      TH1F* h_MeanTime;
-      /**mean memory of event*/
-      TH1F* h_MeanMem;
-      /**process time of event*/
-      TH1F* h_EvtTime;
-      /**mean process time of modules with large time consumption*/
-      TH1F* h_MeanTime_TopModule;
-      /**process time of module per event*/
-      std::vector<TH1F*> h_ModuleTime;
-      /**total process time of modules*/
-      std::map<std::string, double> m_sumtime_module;
-      /**modules in fastreco*/
-      std::vector<std::string> m_fastreco_modules;
-      /**modules in physicstrigger*/
-      std::vector<std::string> m_physfilter_modules;
-      /**modules with large time consumption*/
-      std::vector<std::string> m_name_topmodule;
-      /**four subgroups: unpacker, fast_reco, physcistrigger, flag samples*/
-      const int m_nsubhist = 4;
+      /// Mean time of certain modules
+      TH1F* m_meanTimeHistogram;
+      //// Mean memory of certain modules
+      TH1F* m_meanMemoryHistogram;
+      /// Time of the full event
+      TH1F* m_fullTimeHistogram;
+
+      /// Storage for the last full time sum
+      double m_lastFullTimeSum = 0;
+
+      /// Parameter: Directory to put the generated histograms
+      std::string m_param_histogramDirectoryName = "timing_statistics";
+      /// Parameter: which modules should be shown in the overview list
+      std::vector<std::string> m_param_overviewModuleList = {"Sum_Tracking", "Sum_PID", "Sum_Clustering"};
     };
 
-  } // end namespace SoftwareTrigger
-} // end namespace Belle2
-
-#endif // SOFTWARETRIGGERCOSMICRAYDQMMODULE_H
+  }
+}

95547a11f09fed723cca668210edbc78947f2351
neverov <dmitrii.neverov@desy.de>
2018-04-12 21:24:39 +0900

    Added Kinematics to ntuples for electrons and muons

--- analysis/validation/test1_Validation_StandardMuons.py
+++ analysis/validation/test1_Validation_StandardMuons.py
@@ -65,6 +65,7 @@ tools += ['InvMass', '^B0 -> [^J/psi -> mu+:all mu-:all] [^K_S0 -> pi+ pi-]']
 
 # PID VARIABLES AND TRACK RECONSTRUCTION
 tools += ['PID', 'B0 -> [J/psi -> ^mu+ ^mu-] [K_S0 -> ^pi+ ^pi-]']
+tools += ['Kinematics', 'B0 -> [J/psi -> ^mu+ ^mu-] [K_S0 -> ^pi+ ^pi-]']
 tools += ['CustomFloats[piExpertPID_ALL:muExpertPID_ALL:eExpertPID_ALL:KExpertPID_ALL:pExpertPID_ALL:cosTheta]',
           "B0 -> [J/psi -> ^mu+ ^mu-] [K_S0 -> ^pi+ ^pi-]"]
 

a20b57ff1402f94113ce69b2f77483cbd7031908
Racha Cheaib <racha.cheaib@desy.de>
2018-04-14 03:09:48 +0900

    to reduce log size output

--- skim/Resonance_Skim_Standalone.py
+++ skim/Resonance_Skim_Standalone.py
@@ -21,11 +21,28 @@ import sys
 import os
 import glob
 
-fileList = [
-    '/ghi/fs01/belle2/bdata/MC/release-00-09-01/DB00000276/MC9/prod00002288/e0000/4S/r00000/mixed/sub00/' +
-    'mdst_000001_prod00002288_task00000001.root'
-]
-inputMdstList('default', fileList)
+if len(sys.argv) > 1:
+    bkgType = sys.argv[1]
+    f = open('inputFiles/' + bkgType + '.txt', 'r')
+    fileList = f.read()
+    f.close()
+    if not os.path.isfile(fileList[:-1]):
+        sys.exit('Could not find root file : ' + fileList[:-1])
+    print('Running over file ' + fileList[:-1])
+elif len(sys.argv) == 1:
+    fileList = \
+        ['/ghi/fs01/belle2/bdata/MC/fab/sim/release-00-05-03/DBxxxxxxxx/MC5/prod00000001/s00/e0001/4S/r00001/mixed/sub00/' +
+         'mdst_000001_prod00000001_task00000001.root'
+
+         ]
+    bkgType = 'old'
+
+
+if len(sys.argv) > 1:
+    inputMdstList('default', fileList[:-1])
+elif len(sys.argv) == 1:
+    inputMdstList('default', fileList)
+
 
 argvs = sys.argv
 argc = len(argvs)
@@ -37,7 +54,7 @@ stdPi0s('looseFit')
 
 from Resonance_List import *
 ResonanceList = ResonanceList()
-skimOutputUdst('Resonance', ResonanceList)
+skimOutputUdst('outputFiles/Resonance_' + bkgType, ResonanceList)
 summaryOfLists(ResonanceList)
 
 if 'Validation' in argvs:
@@ -100,6 +117,9 @@ if 'Validation' in argvs:
     toolsv += ['MCTruth', '^vpho -> ^mu+ ^mu-']
     ntupleTree('vpho', 'vpho:resonance0', toolsv)
 
+for module in analysis_main.modules():
+    if module.type() == "ParticleLoader":
+        module.set_log_level(LogLevel.ERROR)
 process(analysis_main)
 
 print(statistics)

92f47f368dcaa0b35790b08772154afaaa280539
Benjamin Schwenker <benjamin.schwenker@phys.uni-goettingen.de>
2018-04-16 18:32:58 +0200

    added usePXDClusterShapes flag to add_pxd_reconstruction

--- pxd/scripts/pxd/__init__.py
+++ pxd/scripts/pxd/__init__.py
@@ -5,15 +5,17 @@ from basf2 import *
 from ROOT import Belle2
 
 
-def add_pxd_reconstruction(path, clusterName=None, digitsName=None):
+def add_pxd_reconstruction(path, clusterName=None, digitsName=None, usePXDClusterShapes=False):
 
-    # path.add_module('ActivatePXDClusterPositionEstimator')
+    if usePXDClusterShapes:
+        if 'ActivatePXDClusterPositionEstimator' not in [e.name() for e in path.modules()]:
+            shape_activator = register_module('ActivatePXDClusterPositionEstimator')
+            shape_activator.set_name('ActivatePXDClusterPositionEstimator')
+            path.add_module(shape_activator)
 
-    clusterizerName = 'PXDClusterizer'
-
-    if clusterizerName not in [e.name() for e in path.modules()]:
+    if 'PXDClusterizer' not in [e.name() for e in path.modules()]:
         clusterizer = register_module('PXDClusterizer')
-        clusterizer.set_name(clusterizerName)
+        clusterizer.set_name('PXDClusterizer')
         if clusterName:
             clusterizer.param('Clusters', clusterName)
         if digitsName:

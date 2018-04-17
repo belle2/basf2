7cce0a4103d3ff206633b3c8e9e15f9d51e10ac2
Bryan Fulsom <bryan.fulsom@desy.de>
2018-04-18 05:38:54 +0900

    Changed Phase 2/3 Y(6S) validation scripts to Y(4S) generic

---
+++ analysis/validation/test0_BBgeneric_Phase3_GENSIMRECtoDST.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+<header>
+  <output>../BBgeneric_Phase2.dst.root</output>
+  <contact>Cate MacQueen, cmq.centaurus@gmail.com, Bryan Fulsom, bryan.fulsom@pnnl.gov</contact>
+</header>
+"""
+
+from basf2 import *
+from simulation import add_simulation
+from reconstruction import add_reconstruction
+from reconstruction import add_mdst_output
+from beamparameters import add_beamparameters
+from ROOT import Belle2
+
+set_random_seed(10000)
+
+main = create_path()
+
+# specify number of events to be generated
+eventinfosetter = register_module('EventInfoSetter')
+eventinfosetter.param('evtNumList', [100])
+eventinfosetter.param('runList', [1])
+eventinfosetter.param('expList', [1])
+main.add_module(eventinfosetter)
+
+evtgen = register_module('EvtGenInput')
+evtgen.param('ParentParticle', "Upsilon(4S)")
+evtgen.param('userDECFile', Belle2.FileSystem.findFile('generators/evtgen/decayfiles/DECAY_BELLE2.DEC'))
+main.add_module(evtgen)
+print_params(evtgen)
+
+# simulation
+add_simulation(main)
+
+# reconstruction
+add_reconstruction(main)
+
+# dst and mdst output
+output = register_module('RootOutput')
+output.param('outputFileName', '../BBgeneric_Phase3.dst.root')
+main.add_module(output)
+
+# Go!
+process(main)
+
+# Print call statistics
+print(statistics)

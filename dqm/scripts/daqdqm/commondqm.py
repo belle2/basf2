d17962c9e57703267ac483fee1c455c14cce8269
Nils Braun <nils.braun@desy.de>
2018-04-18 11:06:12 +0200

    Reenabled the rest of the DQM modules for HLT

--- dqm/scripts/daqdqm/commondqm.py
+++ dqm/scripts/daqdqm/commondqm.py
@@ -31,37 +31,38 @@ def add_common_dqm(path, components=None, dqm_environment="expressreco"):
         if components is None or 'PXD' in components or 'SVD' in components:
             vxddqm = register_module('VXDDQMExpressReco')
             path.add_module(vxddqm)
-        # CDC
-        if components is None or 'CDC' in components:
-            cdcdqm = register_module('cdcDQM7')
-            path.add_module(cdcdqm)
-        # ECL
-        if components is None or 'ECL' in components:
-            ecldqm = register_module('ECLDQM')
-            path.add_module(ecldqm)
-        # TOP
-        if components is None or 'TOP' in components:
-            topdqm = register_module('TOPDQM')
-            path.add_module(topdqm)
-        # BKLM
-        if components is None or 'BKLM' in components:
-            bklmdqm = register_module("BKLMDQM")
-            path.add_module(bklmdqm)
-        # EKLM
-        if components is None or 'EKLM' in components:
-            eklmdqm = register_module('EKLMDQM')
-            path.add_module(eklmdqm)
-        # ECLTRG
-        if components is None or 'TRG' in components:
-            trgecldqm = register_module('TRGECLDQM')
-            path.add_module(trgecldqm)
-        # TrackDQM, needs at least one VXD components to be present or will crash otherwise
-        if components is None or 'SVD' in components or 'PXD' in components:
-            trackDqm = register_module('TrackDQM')
-            path.add_module(trackDqm)
-        # ARICH
-        if components is None or 'ARICH' in components:
-            path.add_module('ARICHDQM')
 
-    # We always add the hlt DQMs
+    # CDC
+    if components is None or 'CDC' in components:
+        cdcdqm = register_module('cdcDQM7')
+        path.add_module(cdcdqm)
+    # ECL
+    if components is None or 'ECL' in components:
+        ecldqm = register_module('ECLDQM')
+        path.add_module(ecldqm)
+    # TOP
+    if components is None or 'TOP' in components:
+        topdqm = register_module('TOPDQM')
+        path.add_module(topdqm)
+    # BKLM
+    if components is None or 'BKLM' in components:
+        bklmdqm = register_module("BKLMDQM")
+        path.add_module(bklmdqm)
+    # EKLM
+    if components is None or 'EKLM' in components:
+        eklmdqm = register_module('EKLMDQM')
+        path.add_module(eklmdqm)
+    # ECLTRG
+    if components is None or 'TRG' in components:
+        trgecldqm = register_module('TRGECLDQM')
+        path.add_module(trgecldqm)
+    # TrackDQM, needs at least one VXD components to be present or will crash otherwise
+    if components is None or 'SVD' in components or 'PXD' in components:
+        trackDqm = register_module('TrackDQM')
+        path.add_module(trackDqm)
+    # ARICH
+    if components is None or 'ARICH' in components:
+        path.add_module('ARICHDQM')
+
+    # HLT
     standard_hltdqm(path)

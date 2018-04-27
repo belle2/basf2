885ea8b4ac7a807b5af318f49a163129e242a196
Felix Metzner <felix.metzner@kit.edu>
2018-04-27 13:55:55 +0200

    Updated recorded data harvesting module.

--- tracking/scripts/tracking/harvesting_validation/recorded_data_module.py
+++ tracking/scripts/tracking/harvesting_validation/recorded_data_module.py
@@ -16,60 +16,67 @@ class EventInfoHarvester(harvesting.HarvestingModule):
     """
         Harvester retrieving event level information of reconstructed recorded data.
     """
-    def __init__(self, output_file_name):
+
+    def __init__(self,
+                 output_file_name,
+                 pxd_clusters_name='PXDClusters',
+                 pxd_spacepoints_name="PXDSpacePoints",
+                 svd_clusters_name="SVDClusters",
+                 svd_spacepoints_name="SVDSpacePoints",
+                 cdc_hits_name="CDCHits",
+                 reco_tracks_name="RecoTracks",
+                 cdc_reco_tracks_name="CDCRecoTracks",
+                 svd_cdc_reco_tracks_name="SVDCDCRecoTracks",
+                 svd_reco_tracks_name="SVDRecoTracks",
+                 pxd_reco_tracks_name="PXDRecoTracks"
+                 ):
         """Expecting a name for the output file"""
-        super(EventInfoHarvester, self).__init__(foreach='EventMetaData', output_file_name=output_file_name)
+        super(EventInfoHarvester, self).__init__(foreach='EventMetaData',
+                                                 output_file_name=output_file_name,
+                                                 )
+
+        self.pxd_clusters_name = pxd_clusters_name
+        self.pxd_spacepoints_name = pxd_spacepoints_name
+        self.svd_clusters_name = svd_clusters_name
+        self.svd_spacepoints_name = svd_spacepoints_name
+        self.cdc_hits_name = cdc_hits_name
+
+        self.reco_track_name = reco_tracks_name
+        self.cdc_reco_tracks_name = cdc_reco_tracks_name
+        self.svd_cdc_reco_tracks_name = svd_cdc_reco_tracks_name
+        self.svd_reco_tracks_name = svd_reco_tracks_name
+        self.pxd_reco_tracks_name = pxd_reco_tracks_name
 
     def peel(self, event_meta_data):
-        # Event Info
         event_crops = peelers.peel_event_info(event_meta_data)
 
-        # Retrieving flag indicating if the vxdtf2 execution was aborted
         event_level_tracking_info = Belle2.PyStoreObj("EventLevelTrackingInfo")
-        has_vxdtf2_failure_flag = event_level_tracking_info.hasVXDTF2AbortionFlag()
-
-        # Number of PXD and SVD Clusters/SpacePoints as well as CDC hits
-        pxd_clusters = Belle2.PyStoreArray("PXDClusters")
-        pxd_sps = Belle2.PyStoreArray("PXDSpacePoints")
-        svd_clusters = Belle2.PyStoreArray("SVDClusters")
-        svd_sps = Belle2.PyStoreArray("SVDSpacePoints")
-        cdc_hits = Belle2.PyStoreArray("CDCHits")
+        event_level_tracking_info_crops = peelers.peel_event_level_tracking_info(event_level_tracking_info)
 
         number_of_hits = dict(
-            pxd_clusters_size=pxd_clusters.getEntries() if pxd_clusters else 0,
-            pxd_spacepoints_size=pxd_sps.getEntries() if pxd_sps else 0,
-            svd_clusters_size=svd_clusters.getEntries() if svd_clusters else 0,
-            svd_spacepoints_size=svd_sps.getEntries() if svd_sps else 0,
-            cdc_hits_size=cdc_hits.getEntries() if cdc_hits else 0,
+            pxd_clusters_size=peelers.peel_store_array_size(Belle2.PyStoreArray(self.pxd_clusters_name)),
+            pxd_spacepoints_size=peelers.peel_store_array_size(Belle2.PyStoreArray(self.pxd_spacepoints_name)),
+            svd_clusters_size=peelers.peel_store_array_size(Belle2.PyStoreArray(self.svd_clusters_name)),
+            svd_spacepoints_size=peelers.peel_store_array_size(Belle2.PyStoreArray(self.svd_spacepoints_name)),
+            cdc_hits_size=peelers.peel_store_array_size(Belle2.PyStoreArray(self.cdc_hits_name)),
         )
 
-        # Number of tracks (reco, cdc, cdcsvd, svd, pxd)
-        tracks = Belle2.PyStoreArray("RecoTracks")
-        cdc_tracks = Belle2.PyStoreArray("CDCRecoTracks")
-        svd_cdc_tracks = Belle2.PyStoreArray("SVDCDCRecoTracks")
-        svd_tracks = Belle2.PyStoreArray("SVDRecoTracks")
-        pxd_tracks = Belle2.PyStoreArray("PXDRecoTracks")
         number_of_tracks = dict(
-            tracks_size=tracks.getEntries() if tracks else 0,
-            cdc_tracks_size=cdc_tracks.getEntries() if cdc_tracks else 0,
-            svd_cdc_tracks_size=svd_cdc_tracks.getEntries() if svd_cdc_tracks else 0,
-            svd_tracks_size=svd_tracks.getEntries() if svd_tracks else 0,
-            pxd_tracks_size=pxd_tracks.getEntries() if pxd_tracks else 0,
+            tracks_size=peelers.peel_store_array_size(Belle2.PyStoreArray(self.reco_tracks_name)),
+            cdc_tracks_size=peelers.peel_store_array_size(Belle2.PyStoreArray(self.cdc_reco_tracks_name)),
+            svd_cdc_tracks_size=peelers.peel_store_array_size(Belle2.PyStoreArray(self.svd_cdc_reco_tracks_name)),
+            svd_tracks_size=peelers.peel_store_array_size(Belle2.PyStoreArray(self.svd_reco_tracks_name)),
+            pxd_tracks_size=peelers.peel_store_array_size(Belle2.PyStoreArray(self.pxd_reco_tracks_name)),
         )
 
-        # Information about the run time and memory consumption of selected VXDTF2 modules
-        stats = {}
-        module_stats = basf2.statistics.modules
-        for module in module_stats:
-            if module.name in ["SegmentNetworkProducer", "TrackFinderVXDCellOMat"]:
-                stats[str(module.name) + "_mem"] = module.memory_sum(basf2.statistics.EVENT)
-                stats[str(module.name) + "_time"] = module.time_sum(basf2.statistics.EVENT)
+        module_list = ["SegmentNetworkProducer", "TrackFinderVXDCellOMat"]
+        module_stats = peelers.peel_module_statistics(module_list)
 
-        return dict(has_vxdtf2_failure_flag=has_vxdtf2_failure_flag,
+        return dict(**event_crops,
+                    **event_level_tracking_info_crops,
                     **number_of_hits,
                     **number_of_tracks,
-                    **event_crops,
-                    **stats,
+                    **module_stats,
                     )
 
     save_tree = refiners.SaveTreeRefiner()
@@ -79,16 +86,28 @@ class TrackInfoHarvester(harvesting.HarvestingModule):
     """
         Harvester retrieving track level information of reconstructed recorded data.
     """
-    def __init__(self, output_file_name, reco_tracks_name="RecoTracks"):
+
+    def __init__(self, output_file_name,
+                 reco_tracks_name="RecoTracks",
+                 svd_cdc_reco_tracks_name="SVDCDCRecoTracks",
+                 svd_reco_tracks_name="SVDRecoTracks",
+                 sp_track_cands_name="SPTrackCands"
+                 ):
         """Expecting a name for the output file and the name of the RecoTracks StoreArray
            to operate on. The latter dafaults to 'RecoTracks'"""
         super(TrackInfoHarvester, self).__init__(foreach=reco_tracks_name,
                                                  output_file_name=output_file_name)
 
+        self.svd_cdc_reco_tracks_name = svd_cdc_reco_tracks_name
+        self.svd_reco_tracks_name = svd_reco_tracks_name
+        self.sp_tracks_cands_name = sp_track_cands_name
+
     def peel(self, reco_track):
-        # Event Info
         event_meta_data = Belle2.PyStoreObj("EventMetaData")
         event_crops = peelers.peel_event_info(event_meta_data)
+
+        # Information on the store array
+        store_array_info = peelers.peel_store_array_info(reco_track)
 
         # General information on the track
         reco_track_hit_content = peelers.peel_reco_track_hit_content(reco_track)
@@ -103,15 +122,12 @@ class TrackInfoHarvester(harvesting.HarvestingModule):
         else:
             track_fit_status = peelers.peel_track_fit_result(None)
 
-        # Information on the store array
-        store_array_info = peelers.peel_store_array_info(reco_track)
-
         vxdtf2_was_involved = False
-        svd_cdc_reco_track = reco_track.getRelated("SVDCDCRecoTracks")
+        svd_cdc_reco_track = reco_track.getRelated(self.svd_cdc_reco_tracks_name)
         if svd_cdc_reco_track:
-            svd_reco_track = svd_cdc_reco_track.getRelated("SVDRecoTracks")
+            svd_reco_track = svd_cdc_reco_track.getRelated(self.svd_reco_tracks_name)
             if svd_reco_track:
-                svd_sptcs = svd_reco_track.getRelated("SPTrackCands")
+                svd_sptcs = svd_reco_track.getRelated(self.sp_track_cands_name)
                 if svd_sptcs:
                     vxdtf2_was_involved = True
 
@@ -162,5 +178,8 @@ class HitInfoHarvester(harvesting.HarvestingModule):
                                    )
                     except BaseException:
                         pass
+            else:
+                # One could add here more information about unused hits if wanted.
+                pass
 
     save_tree = refiners.SaveTreeRefiner()

62c47f992d774fe40f9e60df94c5ab56a56ca689
Thomas Hauth <Thomas.Hauth@kit.edu>
2018-05-02 17:16:35 +0900

    updated hlt steering files from releaes/01-02 branch

--- hlt/operation/phase2/global/hlt/evp_scripts/beam_reco_filter.py
+++ hlt/operation/phase2/global/hlt/evp_scripts/beam_reco_filter.py
@@ -4,7 +4,8 @@ import basf2
 from softwaretrigger.path_functions import setup_basf2_and_db, create_hlt_path, add_hlt_processing, finalize_hlt_path
 
 # set basf2 settings and configure local DB access
-args = setup_basf2_and_db()
+args = setup_basf2_and_db(dbfile="/dev/shm/LocalDB.rel0101/database.txt")
+
 # create the HLT path and add the Ring Buffer input and DQM modules
 path = create_hlt_path(args)
 

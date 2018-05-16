daa91636946f2f9683cfe845024612a757197de9
Torben Ferber <torben.ferber@desy.de>
2018-05-16 11:06:02 +0200

    Add documentation.

--- ecl/digitization/include/WrapArray2D.h
+++ ecl/digitization/include/WrapArray2D.h
@@ -7,10 +7,12 @@ namespace Belle2 {
     template <typename T>
     class WrapArray2D {
     public:
+      /** replace POD 2D array  */
       WrapArray2D(int rows,  int cols) :
         m_data(new T[rows * cols]), m_ncols(cols)
       {}
 
+      /** copy */
       WrapArray2D(const WrapArray2D& obj)
       {
         m_data = new T;
@@ -18,25 +20,29 @@ namespace Belle2 {
         m_ncols = obj.m_ncols;
       }
 
+      /** destructor */
       ~WrapArray2D()
       {  delete [] m_data;  }
 
+      /** return row*/
       T* operator[](int irow)
       {
         return m_data + irow * m_ncols;
       }
 
+      /** = assign operator */
       T& operator=(const T& x)
       {
         *m_data = x.m_data;
         return *this;
       }
 
+      /** return */
       operator T* () { return m_data; }
     private:
 
-      T* m_data;
-      int m_ncols;
+      T* m_data; /**< content of the 2d array */
+      int m_ncols; /**< number of columns */
     };
   }
 }

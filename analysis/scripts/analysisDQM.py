aebda866d3784101566a635d81c097a5d95a52b6
Wang, Boqun <boqunwg@ucmail.uc.edu>
2018-05-22 16:58:50 +0900

    Fix analysisDQM.py to pass the tests

--- analysis/scripts/analysisDQM.py
+++ analysis/scripts/analysisDQM.py
@@ -11,9 +11,13 @@ import stdPhotons
 
 
 def add_analysis_dqm(path):
-    stdPhotons.stdPhotons('all', path)
-    modularAnalysis.reconstructDecay('pi0:all -> gamma:all gamma:all', '0.10 < M < 0.15', 1, True, path)
-    modularAnalysis.reconstructDecay('K_S0:all -> pi-:HLT pi+:HLT', '0.48 < M < 0.52', 1, True, path)
+    module_names = [m.name() for m in path.modules()]
+    if ('ParticleLoader_pi+:HLT' in module_names and
+            'ParticleLoader_gamma:HLT' in module_names and
+            'SoftwareTrigger' in module_names):
+        stdPhotons.stdPhotons('all', path)
+        modularAnalysis.reconstructDecay('pi0:all -> gamma:all gamma:all', '0.10 < M < 0.15', 1, True, path)
+        modularAnalysis.reconstructDecay('K_S0:all -> pi-:HLT pi+:HLT', '0.48 < M < 0.52', 1, True, path)
 
-    dqm = register_module('PhysicsObjectsDQM')
-    path.add_module(dqm)
+        dqm = register_module('PhysicsObjectsDQM')
+        path.add_module(dqm)

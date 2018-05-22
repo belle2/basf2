4c12fbe436412bc63ca90483bba5f0b374068206
Sam Cunliffe <sam.cunliffe@desy.de>
2018-05-22 08:31:21 +0200

    Ultraminor title tidying

--- analysis/validation/test2_1163350000.C
+++ analysis/validation/test2_1163350000.C
@@ -80,8 +80,8 @@ void plotUpsHad( TTree* ptree, TFile *outputFile){
   h_missM2->GetListOfFunctions()->Add(new TNamed("Contact", contact));
   TH1F* h_missP = new TH1F("h_had_missP",title,40,0,4);
   ptree->Project("h_had_missP", "Upsilon4S_pRecoil");
-  h_missP->GetXaxis()->SetTitle("missing momentum p_{miss} (GeV/c)");
-  h_missP->GetListOfFunctions()->Add(new TNamed("Description", "The missing momentum"));
+  h_missP->GetXaxis()->SetTitle("recoil momentum p_{recoil} (GeV/c)");
+  h_missP->GetListOfFunctions()->Add(new TNamed("Description", "The recoil momentum"));
   h_missP->GetListOfFunctions()->Add(new TNamed("Check", "Consistent shape"));
   h_missP->GetListOfFunctions()->Add(new TNamed("Contact", contact));
   TH1F* h_DstarMomentum = new TH1F("h_DstarMomentum",title,40,0,4);

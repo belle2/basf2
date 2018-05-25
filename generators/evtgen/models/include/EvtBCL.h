6a8be3b02a4cdc07f2b0a426268869d3911056c8
Markus Prim <markus.prim@kit.edu>
2018-05-25 17:36:06 +0200

    implementation

--- generators/evtgen/models/include/EvtBCL.h
+++ generators/evtgen/models/include/EvtBCL.h
@@ -1,8 +1,57 @@
-//
-// Created by mapr on 15.05.18.
-//
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2012 Belle II Collaboration                               *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Markus Prim                                              *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
 
-#ifndef BASF2_EVTBCL_H
-#define BASF2_EVTBCL_H
+#include "EvtGenBase/EvtDecayAmp.hh"
+#include "EvtGenBase/EvtSemiLeptonicFF.hh"
+#include "EvtGenBase/EvtSemiLeptonicAmp.hh"
 
-#endif //BASF2_EVTBCL_H
+class EvtParticle;
+
+namespace Belle2 {
+
+  /** The class provides the form factors for orbitally excited semileptonic decays
+   */
+  class EvtBCL : public EvtDecayAmp {
+
+  public:
+
+    /** Default constructor */
+    EvtBCL();
+
+    /** virtual destructor */
+    virtual ~EvtBCL();
+
+    /** Returns name of module */
+    std::string getName();
+
+    /** Clones module */
+    EvtDecayBase* clone();
+
+    /** Creates a decay */
+    void decay(EvtParticle* p);
+
+    /** Sets maximal probab. */
+    void initProbMax();
+
+    /** Initializes module */
+    void init();
+
+  private:
+
+    /** Pointers needed for FFs */
+    EvtSemiLeptonicFF* bclmodel;
+
+    /** Pointers needed to calculate amplitude */
+    EvtSemiLeptonicAmp* calcamp;
+
+  };
+
+}
--- generators/evtgen/models/src/EvtBCL.cc
+++ generators/evtgen/models/src/EvtBCL.cc
@@ -1,4 +1,103 @@
-//
-// Created by mapr on 15.05.18.
-//
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2016 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Florian Bernlochner                                      *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include "EvtGenBase/EvtPatches.hh"
+#include <stdlib.h>
+#include <assert.h>
+#include "EvtGenBase/EvtParticle.hh"
+#include "EvtGenBase/EvtGenKine.hh"
+#include "EvtGenBase/EvtPDL.hh"
+#include "EvtGenBase/EvtReport.hh"
+#include "EvtGenBase/EvtSemiLeptonicVectorAmp.hh"
+#include "EvtGenBase/EvtSemiLeptonicScalarAmp.hh"
+#include <string>
+
+#include "generators/evtgen/EvtGenModelRegister.h"
+#include "generators/evtgen/models/EvtBCL.h"
+#include "generators/evtgen/models/EvtBCLFF.h"
+
+
+namespace Belle2 {
+
+  B2_EVTGEN_REGISTER_MODEL(EvtBCL);
+
+  EvtBCL::EvtBCL() : bclmodel(nullptr), calcamp(nullptr) {}
+
+  EvtBCL::~EvtBCL()
+  {
+    delete bclmodel;
+    bclmodel = nullptr;
+    delete calcamp;
+    calcamp = nullptr;
+  }
+
+  std::string EvtBCL::getName()
+  {
+    return "BCL";
+  }
+
+  EvtDecayBase* EvtBCL::clone()
+  {
+    return new EvtBCL;
+  }
+
+  void EvtBCL::decay(EvtParticle* p)
+  {
+    p->initializePhaseSpace(getNDaug(), getDaugs());
+    calcamp->CalcAmp(p, _amp2, bclmodel);
+  }
+
+
+  void EvtBCL::initProbMax()
+  {
+
+    EvtId parnum, mesnum, lnum, nunum;
+
+    parnum = getParentId();
+    mesnum = getDaug(0);
+    lnum = getDaug(1);
+    nunum = getDaug(2);
+
+    double mymaxprob = calcamp->CalcMaxProb(parnum, mesnum, lnum, nunum, bclmodel);
+
+    setProbMax(mymaxprob);
+  }
+
+
+  void EvtBCL::init()
+  {
+
+    checkNDaug(3);
+
+    //We expect the parent to be a scalar
+    //and the daughters to be X lepton neutrino
+
+    checkSpinParent(EvtSpinType::SCALAR);
+    checkSpinDaughter(1, EvtSpinType::DIRAC);
+    checkSpinDaughter(2, EvtSpinType::NEUTRINO);
+
+    EvtSpinType::spintype mesontype = EvtPDL::getSpinType(getDaug(0));
+
+    bclmodel = new EvtBCLFF(getNArg(), getArgs());
+
+    if (mesontype == EvtSpinType::SCALAR) {
+      calcamp = new EvtSemiLeptonicScalarAmp;
+    }
+    if (mesontype == EvtSpinType::VECTOR) {
+      calcamp = new EvtSemiLeptonicVectorAmp;
+    }
+//    if (mesontype == EvtSpinType::TENSOR) {
+//      calcamp = new EvtSemiLeptonicTensorAmp;
+//    }
+
+  }
+
+}
 

48e89098452266e8daa3c5ea06133c7a3a93b36e
Björn Spruck <bspruck@uni-mainz.de>
2018-05-25 16:04:23 +0900

    applied suggestions, added TOP

--- rawdata/modules/scripts/SetMetaTimeModule.py
+++ rawdata/modules/scripts/SetMetaTimeModule.py
@@ -14,25 +14,27 @@ class SetMetaTimeModule(Module):
     Author: bjoern.spruck@belle2.org
     """
 
+    #: pointer to event meta data
     meta = None
-    svd = None
-    cdc = None
-    ecl = None
+    #: array of pointers to the detector raw data arrays
+    raws = []
 
     def initialize(self):
+        """initialize"""
         self.meta = Belle2.PyStoreObj('EventMetaData')
         if not self.meta:
             B2FATAL("no EventMetaData object")
             return
-        self.svd = Belle2.PyStoreArray('RawSVDs')
-        self.cdc = Belle2.PyStoreArray('RawCDCs')
-        self.ecl = Belle2.PyStoreArray('RawECLs')
+
+        self.raws = [Belle2.PyStoreArray(f"Raw{e}s") for e in "SVD CDC ECL TOP".split()]
 
     def calc_time(self, it):
+        """Calculate the time in ns since epoch from UnixTime and Accelerator Clock count"""
         # t = it.GetTTTimeNs(0)
         return it.GetTTUtime(0) * 1000000000 + int(it.GetTTCtime(0) / 0.127216)
 
     def event(self):
+        """event"""
         if not self.meta.isValid():
             B2FATAL("no valid EventMetaData object in this event")
             return
@@ -41,30 +43,11 @@ class SetMetaTimeModule(Module):
         if t != 0:
             return  # No need to update
 
-        if t == 0:
-            if self.svd:
-                # Take the first SVD packet
-                for it in self.svd:
+        for det in self.raws:
+            if det:
+                for it in det:
                     t = self.calc_time(it)
-                    break
+                    self.meta.setTime(t)
+                    return
 
-        if t == 0:
-            if self.cdc:
-                # Take the first CDC packet
-                for it in self.cdc:
-                    t = self.calc_time(it)
-                    break
-
-        if t == 0:
-            if self.ecl:
-                # Take the first CDC packet
-                for it in self.ecl:
-                    t = self.calc_time(it)
-                    break
-
-        if t == 0:
-            # could check for other detector raws
-            B2DEBUG(1, "No Time for EventMetaData extracted as no raw SVD, CDC, ECL entries available")
-            return
-
-        self.meta.setTime(t)
+        B2DEBUG(1, "No Time for EventMetaData extracted as no raw SVD, CDC, ECL entries available")

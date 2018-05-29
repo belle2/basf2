d158b553d12bdb682c873c9bcbe37235fa978f0b
Marco Milesi <marco.milesi@unimelb.edu.au>
2018-05-29 01:16:26 +0000

    Add linkdef for ecl/chargedPID

---
+++ ecl/chargedPID/include/linkdef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class RooRealVar-;
+#pragma link C++ class RooAddPdf-;
+#pragma link C++ class RooCBShape-;
+#pragma link C++ class RooGaussian-;
+
+#endif

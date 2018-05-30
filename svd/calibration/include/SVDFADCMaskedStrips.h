6fb8fe092ea972ed6065b83fd80190f830850479
Laura Zani <laura.zani@pi.infn.it>
2018-05-30 23:59:21 +0900

    Removed from the wrapper class SVDFADCMaskedStrips the method isGood which was just for the importer implementation, depending on the old structure of the xml file from which the payloads would be generated. Added the documentation of the class and the isMasked() method. Added the method to get the string for the uniqueID.

--- svd/calibration/include/SVDFADCMaskedStrips.h
+++ svd/calibration/include/SVDFADCMaskedStrips.h
@@ -24,10 +24,8 @@
 
 namespace Belle2 {
 
-  /** This class defines the dbobject and the method to access SVD
-   * calibrations from the noise local runs. It provides the strip noise
-   * in ADC units.
-   *
+  /** This class defines the dbobject and the method to access strips which
+   * are masked at FADC level.
    *
    */
   class SVDFADCMaskedStrips {
@@ -40,28 +38,33 @@ namespace Belle2 {
     {}
 
 
-    /** This is the method for getting the offline list of bad strips to be masked.
+    /** This is the method for getting the comprehensive list of masked strips
+     * at FADC level. The list includes both strips masked accordingly to
+     * the calibration results and strips permanently masked regardless the
+     * outcome of the calibration runs.
      * Input:
-     * @param sensor ID: identitiy of the sensor for which the
+     * @param sensor ID: identity of the sensor for which the
      * calibration is required
      * @param isU: sensor side, true for p (u) side, false for n (v) side
      * @param strip: strip number
      *
-     * Output: boolean value, 1 is the strip has to be masked, 0 otherwise.
+     * Output: boolean value, 1 is masked, 0 otherwise.
      * it throws std::out_of_range if the strip is unknown
      */
 
-    inline float isGood(const VxdID& sensorID, const bool& isU , const unsigned short& strip) const
+    inline float isMasked(const VxdID& sensorID, const bool& isU , const unsigned short& strip) const
     {
       return m_aDBObjPtr->get(sensorID.getLayerNumber(), sensorID.getLadderNumber(),
                               sensorID.getSensorNumber(), m_aDBObjPtr->sideIndex(isU),
                               strip);
     }
 
-    inline float isMasked(const VxdID& sensorID, const bool& isU , const unsigned short& strip) const
-    {
-      return !isGood(sensorID, isU, strip);
-    }
+
+    /** returns the unique ID of the payload */
+    TString getUniqueID() { return m_aDBObjPtr->get_uniqueID(); }
+
+    /** returns true if the m_aDBObtPtr is valid in the requested IoV */
+    bool isValid() { return m_aDBObjPtr.isValid(); }
 
   private:
     DBObjPtr< t_payload > m_aDBObjPtr;

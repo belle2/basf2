398a613daf608e543143b5cce9ee1003908f7927
Ilya Komarov <ilya.komarov@desy.de>
2018-06-01 22:44:06 +0900

    Better skim for visible

--- skim/LFVZpVisible_List.py
+++ skim/LFVZpVisible_List.py
@@ -19,14 +19,28 @@ def LFVZpVisibleList():
     track_cuts = 'abs(dz) < 2.0 and abs(dr) < 0.5'
     Event_cuts_vis = 'nCleanedTracks(abs(dz) < 2.0 and abs(dr) < 0.5) == 4'
 
-    cutAndCopyList('mu+:lfvzp', 'mu+:all', track_cuts)
     cutAndCopyList('e+:lfvzp', 'e+:all', track_cuts)
 
-    # Z' to lfv
-    LFVZpVisChannel = 'mu+:lfvzp mu+:lfvzp e-:lfvzp e-:lfvzp'
+    # Z' to lfv: fully reconstructed
+    LFVZpVisChannel = 'e+:lfvzp e+:lfvzp e-:lfvzp e-:lfvzp'
 
     reconstructDecay('vpho:vislfvzp -> ' + LFVZpVisChannel, Event_cuts_vis)
 
     lfvzp_list.append('vpho:vislfvzp')
+
+    # Z' to lfv: part reco
+    LFVZpVisChannel = 'e+:lfvzp e+:lfvzp e-:lfvzp'
+    Event_cuts_vis = 'nCleanedTracks(abs(dz) < 2.0 and abs(dr) < 0.5) == 3'
+
+    reconstructDecay('vpho:3tr_vislfvzp -> ' + LFVZpVisChannel, Event_cuts_vis)
+
+    lfvzp_list.append('vpho:3tr_vislfvzp')
+
+    # Z' to lfv: two same-sign tracks
+    LFVZpVisChannel = 'e+:lfvzp e+:lfvzp'
+    Event_cuts_vis = 'nCleanedTracks(abs(dz) < 2.0 and abs(dr) < 0.5) == 2'
+    reconstructDecay('vpho:2tr_vislfvzp -> ' + LFVZpVisChannel, Event_cuts_vis)
+
+    lfvzp_list.append('vpho:2tr_vislfvzp')
 
     return lfvzp_list

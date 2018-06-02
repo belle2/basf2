3a6536b1a1f60e8944bfd650f1a8209117b137c7
Nils Braun <nils.braun@desy.de>
2018-06-02 09:24:37 +0200

    Fixed warning message

--- hlt/softwaretrigger/scripts/softwaretrigger/path_functions.py
+++ hlt/softwaretrigger/scripts/softwaretrigger/path_functions.py
@@ -546,7 +546,8 @@ def add_hlt_dqm(path, run_type, standalone=False, components=DEFAULT_HLT_COMPONE
     Add all the DQM modules for HLT to the path
     """
     if "HistoManager" not in path.modules() or "DqmHistoManager" not in path.modules():
-        basf2.B2WARNING("I am not adding the DQM modules as there is no histo manager present")
+        basf2.B2WARNING("I am not adding the DQM modules " +
+                        "as there is neither a HistoManager nor the DqmHistoManager module present.")
         return
 
     if standalone:

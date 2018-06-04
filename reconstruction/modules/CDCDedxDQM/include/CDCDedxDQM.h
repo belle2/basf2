6b64fdb03b8e04167eb191f3fca0da885e23412e
Jake Bennett <jake.bennett@desy.de>
2018-06-04 15:21:01 -0400

    Adding dE/dx DQM plots

--- reconstruction/modules/CDCDedxDQM/include/CDCDedxDQM.h
+++ reconstruction/modules/CDCDedxDQM/include/CDCDedxDQM.h
@@ -14,10 +14,28 @@
 
 #include <framework/core/HistoModule.h>
 #include <framework/datastore/StoreArray.h>
+#include <mdst/dataobjects/Track.h>
+#include <mdst/dataobjects/TrackFitResult.h>
+#include <framework/dataobjects/EventMetaData.h>
+#include <framework/database/DBObjPtr.h>
 
 #include "TH1F.h"
 #include "TH2F.h"
 #include "TF1.h"
+#include "TList.h"
+#include "TFile.h"
+#include "TString.h"
+#include "TPaveText.h"
+#include "TDirectory.h"
+#include "TVector.h"
+#include "TLatex.h"
+
+
+//import some useful namespace
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
 
 namespace Belle2 {
 
@@ -49,15 +67,42 @@ namespace Belle2 {
     /** End of the event processing. */
     virtual void terminate();
 
-    /** Function to define histograms. */
-    virtual void defineHisto();
+
 
   private:
 
     StoreArray<CDCDedxTrack> m_cdcDedxTracks; /**< Store array for CDCDedxTrack */
 
-    TH1F* m_h_dedxmeans = nullptr; /**< Histogram for dE/dx electron means */
-    TH2F* m_h_dedxbands = nullptr; /**< Histogram for dE/dx band plot */
+    TFile* fFile = nullptr;     //! Write final objects
+
+    TList* fOutput = nullptr;   //! List of all objects
+    TDirectory* fDir1 = nullptr;
+    TDirectory* fDir2 = nullptr;
+
+    Int_t fCurrentEventNum;
+    TPaveText* tempText;
+
+    Bool_t isHadronfile;
+    string fOutFileName; //name of output ROOT file
+    TString fCollType; //Tag the file collision type
+
+    vector<Double_t> TotMean;   //Mean of dedx distrbution by Fit
+    vector<Double_t> TotMeanE;  //Mean Error of dedx distrbution by Fit
+    vector<Double_t> TotSigma;  //Sigma of dedx distrbution by Fit
+    vector<Double_t> TotSigmaE; //Sigma Error of dedx distrbution by Fit
+    vector<Int_t> TotRunN;    //Number corresponds to Run
+
+    vector<TH1F*> i1DHistoV;  //Vector of dedx histograms
+    vector<TH2F*> i2DHistoV;  //vector of dedx vs P histograms
+
+    Int_t    nBinsdedx; //nbin of dedx range
+    Double_t nBinsdedxLE; //lowedge of dedx
+    Double_t nBinsdedxUE; //upedge of dedx
+
+    Int_t    nBinsP; //nbins of P range
+    Double_t nBinsPLE; //lowedge of P range
+    Double_t nBinsPUE; //upedge of P range
 
   };
+
 } // Belle2 namespace
--- reconstruction/modules/CDCDedxDQM/src/CDCDedxDQM.cc
+++ reconstruction/modules/CDCDedxDQM/src/CDCDedxDQM.cc
@@ -9,62 +9,202 @@
  **************************************************************************/
 
 #include <reconstruction/modules/CDCDedxDQM/CDCDedxDQM.h>
+#include <framework/core/HistoModule.h>
+#include <TH1F.h>
 
 using namespace Belle2;
 
+
 REG_MODULE(CDCDedxDQM)
 
-CDCDedxDQMModule::CDCDedxDQMModule() : HistoModule()
+
+//---------------------------------
+CDCDedxDQMModule::CDCDedxDQMModule(): HistoModule()
 {
 
   setDescription("Make data quality monitoring plots for dE/dx: means and resolutions for bhabha samples, band plots for lepton/hadron samples.");
+  addParam("outputFileName", fOutFileName, "Name for output file", string("DefaultdEdxTrendsFile.root"));
+  addParam("UsingHadronfiles", isHadronfile, "Switch to hadron (false) vs bhabha files", kFALSE);
+
 }
 
+//---------------------------------
 CDCDedxDQMModule::~CDCDedxDQMModule() { }
 
-void CDCDedxDQMModule::defineHisto()
-{
-  B2INFO("Creating a ROOT file for CDC dE/dx DQM...");
 
-  m_h_dedxmeans = new TH1F("h_dedxmeans", "CDC dE/dx electron truncated mean", 100, 0.0, 2.0);
-  m_h_dedxbands = new TH2F("h_dedxbands", "CDC dE/dx band plots", 100, 0.0, 3.0, 100, 0.0, 10.0);
-}
-
+//---------------------------------
 void CDCDedxDQMModule::initialize()
 {
-  // register in datastore
+
+  nBinsdedx = 200; nBinsdedxLE = 0.; nBinsdedxUE = 4.;
+  nBinsP = 500; nBinsPLE = 0.; nBinsPUE = 10.;
+  fCollType = "bhabhaCand";
+
+  if (isHadronfile) {
+    nBinsdedx = 500; nBinsdedxLE = 0.; nBinsdedxUE = 10.;
+    nBinsP = 150; nBinsPLE = 0.; nBinsPUE = 3.;
+    fCollType = "charged";
+  }
+
+
+  fFile = new TFile(Form("%s", fOutFileName.data()), "RECREATE");
+  fDir1 = fFile->mkdir("TrendingHisto");
+  fDir2 = fFile->mkdir("RunLevelHisto");
+
+  fOutput = new TList();
+  fOutput->SetOwner();
+  fOutput->SetName("dEdxHistosPlusTrends");
+
+  TH1F* hdEdx_allRun = new TH1F("hdEdx_allRun", "dE/dx Distribution all Runs", nBinsdedx, nBinsdedxLE, nBinsdedxUE);
+  hdEdx_allRun->GetXaxis()->SetTitle(Form("dEdx trucMean of %s tracks", fCollType.Data()));
+  hdEdx_allRun->GetYaxis()->SetTitle("Entries");
+  fOutput->Add(hdEdx_allRun);
+
+
+  TH2F* hdEdxVsP_allRun = new TH2F("hdEdxVsP_allRun", "dE/dxvs P Distribution all Runs", nBinsP, nBinsPLE, nBinsPUE, nBinsdedx,
+                                   nBinsdedxLE, nBinsdedxUE);
+  hdEdxVsP_allRun->GetXaxis()->SetTitle(Form("Momentum (P) of %s tracks", fCollType.Data()));
+  hdEdxVsP_allRun->GetYaxis()->SetTitle("dEdx");
+  fOutput->Add(hdEdxVsP_allRun);
   m_cdcDedxTracks.isRequired();
 
-  REG_HISTOGRAM   // required to register histograms to HistoManager
+  REG_HISTOGRAM
+
 }
 
+
+
+//---------------------------------
 void CDCDedxDQMModule::beginRun()
 {
+
+  StoreObjPtr<EventMetaData> eventMetaDataPtr;
+  fCurrentEventNum = eventMetaDataPtr->getRun();
+  //cout << " ----> fCurrentEventNum  = " << Form("Run Number = %d", fCurrentEventNum) << endl;
+
+  TH1F* temp1D = new TH1F(Form("hdEdx_Run%d", fCurrentEventNum), Form("Run Number = %d", fCurrentEventNum), nBinsdedx, nBinsdedxLE,
+                          nBinsdedxUE);
+  temp1D->GetXaxis()->SetTitle(Form("dEdx trucMean of %s tracks", fCollType.Data()));
+  temp1D->GetYaxis()->SetTitle("Entries");
+  fOutput->Add(temp1D);
+
+
+  TH2F* temp2D = new TH2F(Form("hdEdxVsP_Run%d", fCurrentEventNum), Form("Run Number = %d", fCurrentEventNum), nBinsP, nBinsPLE,
+                          nBinsPUE, nBinsdedx, nBinsdedxLE, nBinsdedxUE);
+  temp2D->GetXaxis()->SetTitle(Form("Momentum (P) of %s tracks", fCollType.Data()));
+  temp2D->GetYaxis()->SetTitle("dEdx");
+  fOutput->Add(temp2D);
+
+  i1DHistoV.push_back(temp1D);
+  i2DHistoV.push_back(temp2D);
+
 }
 
+
+//---------------------------------
 void CDCDedxDQMModule::event()
 {
-  // **************************************************
-  //
-  //  LOOP OVER EACH DEDX MEASUREMENT (TRACK LEVEL)
-  //
-  // **************************************************
 
-  for (auto& dedxTrack : m_cdcDedxTracks) {
-    if (dedxTrack.size() == 0) {
-      B2WARNING("No good hits on this track...");
-      continue;
-    }
+  for (Int_t idedx = 0; idedx < m_cdcDedxTracks.getEntries(); idedx++) {
 
-    m_h_dedxmeans->Fill(dedxTrack.getDedx());
-    m_h_dedxbands->Fill(dedxTrack.getMomentum(), dedxTrack.getDedx());
+    CDCDedxTrack* dedxTrack = m_cdcDedxTracks[idedx];
+    //per run
+    ((TH1F*)(i1DHistoV.at(0)))->Fill(float(dedxTrack->getDedx()));
+    ((TH2F*)(i2DHistoV.at(0)))->Fill(float(dedxTrack->getMomentum()), float(dedxTrack->getDedx()));
+    //all runs
+    ((TH1F*)fOutput->FindObject(Form("hdEdx_allRun")))->Fill(float(dedxTrack->getDedx()));
+    ((TH1F*)fOutput->FindObject(Form("hdEdxVsP_allRun")))->Fill(float(dedxTrack->getMomentum()), float(dedxTrack->getDedx()));
+
   }
+
+
 }
 
+//---------------------------------
 void CDCDedxDQMModule::endRun()
 {
+
+
+  ((TH1F*)(i1DHistoV.at(0)))->Fit("gaus", "Q"); //Q = No printing
+  TF1* fit = (TF1*)((i1DHistoV.at(0)))->GetFunction("gaus");
+  Double_t mean = fit->GetParameter(1);
+  Double_t meanError = fit->GetParError(1);
+
+  Double_t sigma = fit->GetParameter(2);
+  Double_t sigmaError = fit->GetParError(2);
+
+  TotRunN.push_back(fCurrentEventNum);
+  TotMean.push_back(mean);
+  TotMeanE.push_back(meanError);
+  TotSigma.push_back(sigma);
+  TotSigmaE.push_back(sigmaError);
+
+  i1DHistoV.pop_back();
+  i2DHistoV.pop_back();
+
 }
 
+
+
+//---------------------------------
 void CDCDedxDQMModule::terminate()
 {
+
+
+  const Int_t allNRun = TotMean.size();
+
+  TH1F* hRunVsdEdxMean = new TH1F("hRunVsdEdxMean", "dE/dx mean vs. RunNumber", allNRun, 0, allNRun);
+  hRunVsdEdxMean->SetMarkerStyle(21);
+  hRunVsdEdxMean->SetMarkerColor(kRed);
+  hRunVsdEdxMean->SetMarkerSize(1);
+  hRunVsdEdxMean->GetXaxis()->SetTitle("Run Numbers");
+  hRunVsdEdxMean->GetYaxis()->SetTitle(Form("dEdx mean of %s", fCollType.Data()));
+  hRunVsdEdxMean->Sumw2();
+
+  TH1F* hRunVsdEdxSigma = new TH1F("hRunVsdEdxSigma", "dE/dx sigma vs. RunNumber", allNRun, 0, allNRun);
+  hRunVsdEdxSigma->SetMarkerStyle(21);
+  hRunVsdEdxSigma->SetMarkerColor(kBlue);
+  hRunVsdEdxSigma->SetMarkerSize(1);
+  hRunVsdEdxSigma->GetXaxis()->SetTitle("Run Numbers");
+  hRunVsdEdxSigma->GetYaxis()->SetTitle(Form("dEdx sigma of %s", fCollType.Data()));
+  hRunVsdEdxSigma->Sumw2();
+
+  vector<Double_t> TempTotMean = TotMean;   //Mean of dedx distrbution by Fit
+  sort(TempTotMean.begin(), TempTotMean.end());
+
+  for (Int_t i = 0; i < allNRun; i++) {
+
+    hRunVsdEdxMean->GetXaxis()->SetBinLabel(i + 1, Form("%d", TotRunN.at(i)));
+    hRunVsdEdxSigma->GetXaxis()->SetBinLabel(i + 1, Form("%d", TotRunN.at(i)));
+
+    if (TotMean.at(i) > 1.15 * TempTotMean.at(int(allNRun / 2)) || TotMean.at(i) < 0.85 * TempTotMean.at(int(allNRun / 2))) {
+      hRunVsdEdxMean->GetXaxis()->SetBinLabel(i + 1, Form("#font[22]{#color[2]{%d}}", TotRunN.at(i)));
+      hRunVsdEdxSigma->GetXaxis()->SetBinLabel(i + 1, Form("#font[22]{#color[2]{%d}}", TotRunN.at(i)));
+    }
+
+    hRunVsdEdxMean->SetBinContent(i + 1, TotMean.at(i));
+    hRunVsdEdxMean->SetBinError(i + 1, TotMeanE.at(i));
+
+    hRunVsdEdxSigma->SetBinContent(i + 1, TotSigma.at(i));
+    hRunVsdEdxSigma->SetBinError(i + 1, TotSigmaE.at(i));
+  }
+
+  //Double_t maxM = hRunVsdEdxMean->GetMaximum();
+  hRunVsdEdxMean->SetMinimum(0);
+  hRunVsdEdxMean->SetMaximum(TempTotMean.at(int(allNRun / 2)) * 2.0);
+
+  //Double_t maxS = hRunVsdEdxSigma->GetMaximum();
+  hRunVsdEdxSigma->SetMinimum(0);
+  //hRunVsdEdxSigma->SetMaximum(1);
+
+  fDir1->cd();
+  hRunVsdEdxMean->Write();
+  hRunVsdEdxSigma->Write();
+
+  fFile->cd();
+  fDir2->cd();
+  fOutput->Write();
+  fFile->Close();
+  cout << "--------------Sucessfull-------------- " << endl;
+
 }

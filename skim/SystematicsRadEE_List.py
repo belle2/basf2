6d5052a5912011bf9da7caffa8f36ffaedc59658
Sam Cunliffe <sam.cunliffe@desy.de>
2018-06-06 11:07:18 +0200

    Prescales and prescale logic for ee --> ee(g) skim
    Prescale settings currently at 2% (20%) for the forward e-'s (full event).
    By construction this gets us under the 1.9 nb rule for the ee --> ee(g) sample.

--- skim/SystematicsRadEE_List.py
+++ skim/SystematicsRadEE_List.py
@@ -13,7 +13,7 @@ from basf2 import *
 from modularAnalysis import *
 
 
-def SystematicsRadEEList(prescaleEndcaps='0.1', prescaleBarrel='1.0'):
+def SystematicsRadEEList(prescale_all='0.2', prescale_fwd_electron='0.02'):
     """
     Build the list of radiative electron pairs for photon systematics. In
     particular this is for the endcaps where we have no track triggers, we
@@ -31,21 +31,46 @@ def SystematicsRadEEList(prescaleEndcaps='0.1', prescaleBarrel='1.0'):
         list name of the skim candidates
     """
     # require a pair of good electrons one of which must be cluster-matched
-    # with 3 GeV of energy (also alloww for region-dependent prescales for this event)
+    # with 3 GeV of energy
     goodtrack = 'abs(dz) < 2.0 and abs(dr) < 0.5'
-    prescale = '[clusterReg == 2 and eventRandom <= %s]' % prescaleBarrel
-    prescale += ' or [clusterReg == 1 and eventRandom <= %s]' % prescaleEndcaps
-    prescale += ' or [clusterReg == 3 and eventRandom <= %s]' % prescaleEndcaps
-    goodtrackwithcluster = '%s and clusterE > 3.0 and [ %s ]' % (goodtrack, prescale)
+    goodtrackwithcluster = '%s and clusterE > 3.0' % goodtrack
     cutAndCopyList('e+:skimtight', 'e+:all', goodtrackwithcluster)
     cutAndCopyList('e+:skimloose', 'e+:all', goodtrack)
 
-    # a recoil momentum of the pair and that the recoil is within the CDC acceptance
-    recoil = 'pRecoil > 0.075 and pRecoilTheta > 0.296706 and pRecoilTheta < 2.61799'
+    # a minimum momentum of 75 MeV/c recoiling against the pair,
+    # and require that the recoil is within the CDC acceptance
+    recoil = 'pRecoil > 0.075 and 0.296706 < pRecoilTheta < 2.61799'  # GeV/c, rad
     reconstructDecay('vpho:radee -> e+:skimtight e-:skimloose', recoil)
 
-    # apply event cuts (exactly two clean tracks in the event)
-    eventCuts = '[nCleanedTracks(abs(dz) < 2.0 and abs(dr) < 0.5) == 2]'
-    applyCuts('vpho:radee', eventCuts)
+    # apply event cuts (exactly two clean tracks in the event, and prescale
+    # the whole event regardless of where the electron went)
+    event_cuts = '[nCleanedTracks(abs(dz) < 2.0 and abs(dr) < 0.5) == 2]'  # cm, cm
+    event_cuts += ' and [eventRandom <= %s]' % prescale_all
 
+    # now prescale the *electron* (e-) in the forward endcap (for bhabhas)
+    # note this is all done with cut strings to circumnavigate BII-3607
+    fwd_encap_border = '0.5480334'  # rad (31.4 deg)
+    electron_is_first = 'daughter(0, charge) < 0'
+    first_in_fwd_endcap = 'daughter(0, Theta) < %s' % fwd_encap_border
+    first_not_in_fwd_endcap = 'daughter(0, Theta) > %s' % fwd_encap_border
+    electron_is_second = 'daughter(1, charge) < 0'
+    second_in_fwd_endcap = 'daughter(1, Theta) < %s' % fwd_encap_border
+    second_not_in_fwd_endcap = 'daughter(1, Theta) > %s' % fwd_encap_border
+    passes_prescale = 'eventRandom <= %s' % prescale_fwd_electron
+    #
+    # four possible scenarios:
+    # 1) electron first in the decaystring and in fwd endcap: prescale these
+    prescale_logic = '[%s and %s and %s]' \
+        % (electron_is_first, first_in_fwd_endcap, passes_prescale)
+    # 2) electron second in string and in fwd endcap: prescale these
+    prescale_logic += ' or [%s and %s and %s]' \
+        % (electron_is_second, second_in_fwd_endcap, passes_prescale)
+    # 3) electron first in string and not in fwd endcap (no prescale)
+    prescale_logic += ' or [%s and %s]' % (electron_is_first, first_not_in_fwd_endcap)
+    # 4) electron second in string and not in fwd endcap (no prescale)
+    prescale_logic += ' or [%s and %s]' % (electron_is_second, second_not_in_fwd_endcap)
+
+    # final candidate building with cuts and prescales
+    prescale_logic = '[%s]' % prescale_logic
+    applyCuts('vpho:radee', event_cuts + ' and ' + prescale_logic)
     return ['vpho:radee']

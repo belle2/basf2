1a24665889f4666ebf856503123b5c21ffa6dbdc
Kirill Chilikin <chilikin@lebedev.ru>
2018-06-07 12:35:06 +0300

    Remove alignment restoring functions that are no longer necessary.

--- eklm/geometry/include/AlignmentChecker.h
+++ eklm/geometry/include/AlignmentChecker.h
@@ -86,43 +86,6 @@ namespace Belle2 {
        */
       bool checkAlignment(EKLMAlignment* alignment) const;
 
-      /**
-       * Restore sector alignment.
-       * @param[in] endcap             Endcap.
-       * @param[in] layer              Layer.
-       * @param[in] sector             Sector.
-       * @param[in] sectorAlignment    Sector alignment.
-       * @param[in] oldSectorAlignment Old sector alignment.
-       */
-      void restoreSectorAlignment(int endcap, int layer, int sector,
-                                  EKLMAlignmentData* sectorAlignment,
-                                  EKLMAlignmentData* oldSectorAlignment);
-
-      /**
-       * Restore segment alignment.
-       * @param[in] endcap              Endcap.
-       * @param[in] layer               Layer.
-       * @param[in] sector              Sector.
-       * @param[in] plane               Plane.
-       * @param[in] segment             Segment.
-       * @param[in] sectorAlignment     Sector alignment.
-       * @param[in] segmentAlignment    Segment alignment.
-       * @param[in] oldSegmentAlignment Old segment alignment.
-       */
-      void restoreSegmentAlignment(
-        int endcap, int layer, int sector, int plane, int segment,
-        EKLMAlignmentData* sectorAlignment,
-        EKLMAlignmentData* segmentAlignment,
-        EKLMAlignmentData* oldSegmentAlignment);
-
-      /**
-       * Restore alignment (check and move elements if there are overlaps).
-       * @param[in] alignment    Alignment to be checked.
-       * @param[in] oldAlignment Old alignment (if NULL, restores to zeros).
-       */
-      void restoreAlignment(EKLMAlignment* alignment,
-                            EKLMAlignment* oldAlignment);
-
     private:
 
       /** Print information about overlaps or not. */

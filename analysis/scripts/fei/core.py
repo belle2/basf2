51c6c2016fdde6a21c0af854321cee7a1b4fd889
Sam Cunliffe <sam.cunliffe@desy.de>
2018-06-08 09:30:58 +0200

    Silence doxygen's false positives

--- analysis/scripts/fei/core.py
+++ analysis/scripts/fei/core.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
+
+# @cond
+
 # Thomas Keck 2017
 
 """
--- analysis/scripts/fei/monitoring.py
+++ analysis/scripts/fei/monitoring.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
+
+# @cond
+
 # Thoms Keck 2017
 
 """

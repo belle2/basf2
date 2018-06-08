e68aab3b3dfe745f7f64c27ab4c39294c9345df0
Martin Ritter <martin.ritter@desy.de>
2018-06-08 10:53:22 +0200

    Fix small bug: iov not updated correctly.
    We ignore the iov for almost everything at the moment so this does not
    affect anything except debug messages for now.

--- framework/database/src/ConditionsDatabase.cc
+++ framework/database/src/ConditionsDatabase.cc
@@ -97,6 +97,7 @@ bool ConditionsDatabase::getData(const EventMetaData& event, DBQuery& query)
   query.filename = info.filename;
   query.revision = info.revision;
   query.checksum = info.digest;
+  query.iov = info.iov;
 
   if (m_invertLogging)
     B2LOG(m_logLevel, 35, "Payload " << std::quoted(query.name) << " found in the conditions database for global tag "

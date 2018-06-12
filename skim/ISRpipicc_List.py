476d7f80714548aa2d91cd64cadc411d30c791f1
SEN Jia <jiasen@buaa.edu.cn>
2018-06-12 23:39:27 +0900

    (1) add sphinx docuemntation as your suggestions (2) correct the selections with ISR characteristic

--- skim/ISRpipicc_List.py
+++ skim/ISRpipicc_List.py
@@ -5,11 +5,6 @@
 #
 # Charmonium skim
 # Sen Jia, 11/Jun/2018
-# This skim script reconstructs four ISR processes
-# (1) e+e- -> gamma_{ISR} pi+ pi- J/psi -> e+e-
-# (2) e+e- -> gamma_{ISR} pi+ pi- J/psi -> mu+mu-
-# (3) e+e- -> gamma_{ISR} pi+ pi- psi(2S) -> pi+ pi- J/psi -> e+e-
-# (4) e+e- -> gamma_{ISR} pi+ pi- psi(2S) -> pi+ pi- J/psi -> mu+mu-
 #
 ######################################################
 
@@ -18,15 +13,22 @@ from modularAnalysis import *
 
 
 def ISRpipiccList():
+    """
+    Skim selection of the following channels:
+    - e+e- -> pi+ pi- J/psi -> e+e-
+    - e+e- -> pi+ pi- J/psi -> mu+mu-
+    - e+e- -> pi+ pi- psi(2S) -> pi+ pi- J/psi -> e+e-
+    - e+e- -> pi+ pi- psi(2S) -> pi+ pi- J/psi -> mu+mu-
+    selection criteria are listed below
+    (1) standard e/mu/pi ParticleLists
+    (2) mass window cut for J/psi and psi(2S) candidates
+    (3) Apply -4 < the recoil mass square of hadrons < 4 GeV^{2}/c^{4} to extract ISR signal events
+    """
 
-    # create and fill standard gamma/e/mu/pi ParticleLists
-    fillParticleList('gamma:tight', '')
+    # create and fill standard e/mu/pi ParticleLists
     fillParticleList('e+:loose', '')
     fillParticleList('mu+:loose', '')
     fillParticleList('pi+:loose', '')
-
-    # fill the maximum energy photon as ISR photon
-    rankByHighest('gamma:tight', 'E', 1)
 
     # intermediate state J/psi and psi(2S) are reconstructed
     # add mass window cut for J/psi and psi(2S) candidates
@@ -35,16 +37,19 @@ def ISRpipiccList():
     reconstructDecay('psi(2S):ee -> pi+:loose pi-:loose e+:loose e-:loose', 'M>3.64 and M<3.74')
     reconstructDecay('psi(2S):mumu -> pi+:loose pi-:loose mu+:loose mu-:loose', 'M>3.64 and M<3.74')
 
+    # the requirement of recoil mass square of hadrons
+    MMScuts = '-4 < m2Recoil < 4'
+
     # four ISR modes are reconstructed
-    # e+e- -> gamma_{ISR} pi+ pi- J/psi -> e+e-
-    # e+e- -> gamma_{ISR} pi+ pi- J/psi -> mu+mu-
-    # e+e- -> gamma_{ISR} pi+ pi- psi(2S) -> pi+ pi- J/psi -> e+e-
-    # e+e- -> gamma_{ISR} pi+ pi- psi(2S) -> pi+ pi- J/psi -> mu+mu-
+    # e+e- -> pi+ pi- J/psi -> e+e- via ISR
+    # e+e- -> pi+ pi- J/psi -> mu+mu- via ISR
+    # e+e- -> pi+ pi- psi(2S) -> pi+ pi- J/psi -> e+e- via ISR
+    # e+e- -> pi+ pi- psi(2S) -> pi+ pi- J/psi -> mu+mu- via ISR
     vpho_Channels = [
-        'gamma:tight pi+:loose pi-:loose J/psi:ee',
-        'gamma:tight pi+:loose pi-:loose J/psi:mumu',
-        'gamma:tight pi+:loose pi-:loose psi(2S):ee',
-        'gamma:tight pi+:loose pi-:loose psi(2S):mumu'
+        'pi+:loose pi-:loose J/psi:ee',
+        'pi+:loose pi-:loose J/psi:mumu',
+        'pi+:loose pi-:loose psi(2S):ee',
+        'pi+:loose pi-:loose psi(2S):mumu'
     ]
 
     # define the ISR process list
@@ -52,7 +57,7 @@ def ISRpipiccList():
 
     # reconstruct the different ISR channels and append to the virtual photon
     for chID, channel in enumerate(vpho_Channels):
-        reconstructDecay('vpho:myCombination' + str(chID) + ' -> ' + channel, '', chID)
+        reconstructDecay('vpho:myCombination' + str(chID) + ' -> ' + channel, MMScuts, chID)
         vphoList.append('vpho:myCombination' + str(chID))
 
     # return the ISR process list

8c8c9b4d83896e73e0ef7b4d8faa2c83c7e60044
Jo-Frederik Krohn <jo-frederik.krohn@desy.de>
2018-06-12 05:43:00 +0000

    added missing semicolon

--- analysis/VariableManager/include/ECLVariables.h
+++ analysis/VariableManager/include/ECLVariables.h
@@ -291,6 +291,6 @@ namespace Belle2 {
     double nRejectedECLShowers(const Particle*);
 
     /** energy over momentum can be used to separate electrons from muons*/
-    double eclClusterEoP(const Particle* part)
+    double eclClusterEoP(const Particle* part);
   }
 } // Belle2 namespace

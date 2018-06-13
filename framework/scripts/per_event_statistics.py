c1138b4ccc75bf0cf9e29d4ab3b41c48426fad1f
Martin Ritter <martin.ritter@desy.de>
2018-06-13 11:13:41 +0200

    Simplify PerEventStatistics Module
    * don't use a mergable, the statistics are already merged by the
      framework so as long as we are in the output process this should work
      just fine with a simple tree
    * use simple numpy arrays and references to single elements as branches
    * replace string concatenations by formatting

--- framework/scripts/per_event_statistics.py
+++ framework/scripts/per_event_statistics.py
@@ -3,6 +3,9 @@ import ROOT
 from ROOT import Belle2
 import numpy as np
 
+# circumvent BII-1264
+ROOT.gROOT.ProcessLine("#include <framework/utilities/MakeROOTCompatible.h>")
+
 
 class PerEventStatisticsGetterModule(basf2.Module):
     """
@@ -15,21 +18,21 @@ class PerEventStatisticsGetterModule(basf2.Module):
         Create a new PerEventStatisticsGetterModule. You have to give the name of the
         file, where the TTree with the full event statistics will be saved.
         """
+        super().__init__()
+
         #: Name of the output file.
         self.output_file_name = output_file_name
-
-        #: Will host the Store Object Pointer to the ROOTMergeable later.
+        #: Will host the pointer to the opened TFile later.
+        self.tfile = None
+        #: Will host the TTree later.
         self.statistics = None
 
-        #: Will host the pointer to the opened TFile later.
-        self.file = None
-
+        #: The columns to store the event number.
+        self.event_number = np.zeros(3, dtype=float)
         #: The columns for the statistics TTree (they will be filled in the event function).
         self.ttree_inputs = None
-        #: The column to store the proc ID (when doing multiprocessing).
-        self.proc_id_input = np.zeros(1, dtype=float)
-        #: The column to store the event number.
-        self.event_number_input = np.zeros(1, dtype=float)
+        #: Last recored sum of event calls for all modules
+        self.last_time_sum = None
 
         #: A flag to indicate that we have already added the Branches to the TTree (which we will do in the first event)
         self.branches_added = False
@@ -37,63 +40,61 @@ class PerEventStatisticsGetterModule(basf2.Module):
         #: The event meta data
         self.event_meta_data = Belle2.PyStoreObj("EventMetaData")
 
-        basf2.Module.__init__(self)
-
     def initialize(self):
         """
         Create the needed store object pointer in the DataStore and the TFile with the TTree.
         """
-        # Create the object in the DataStore
-        self.statistics = Belle2.PyStoreObj(Belle2.RootMergeable(ROOT.TTree).Class(),
-                                            Belle2.DataStore.c_Persistent)
-        self.statistics.registerInDataStore("PerEventStatistics")
-
-        # Before creating the TTree, open the output file.
-        self.file = ROOT.TFile.Open(self.output_file_name, "RECREATE")
-        self.statistics.create()
-
-        # Create the TTree.
-        statistics_obj = self.statistics.obj()
-        statistics_obj.assign(ROOT.TTree("statistics", "statistics"))
-
-        # Add two branches we already know about.
-        ttree = statistics_obj.get()
-        ttree.Branch("proc_id", self.proc_id_input, "proc_id/D")
-        ttree.Branch("event_number", self.event_number_input, "event_number/D")
-
         self.event_meta_data.isRequired()
+
+        # try to avoid side effects ...
+        old = ROOT.gDirectory
+        # Before creating the TTree, open the output file.
+        self.tfile = ROOT.TFile.Open(self.output_file_name, "RECREATE")
+        # Create the TTree.
+        self.statistics = ROOT.TTree("statistics", "Per event execution Statistics for all modules")
+        # and reset stupid global directory pointer
+        ROOT.gDirectory = old
+
+        # Add branches identifying the event
+        self.statistics.Branch("expNo", self.event_number[0:], "expNo/D")
+        self.statistics.Branch("runNo", self.event_number[1:], "runNo/D")
+        self.statistics.Branch("evtNo", self.event_number[2:], "evtNo/D")
 
     def event(self):
         """
         The event loop: Store the statistics as a new row in the TTree.
         """
-        # Stored TTree in the DataStore.
-        ttree = self.statistics.obj().get()
+        # TTree reference (for shorter code)
+        ttree = self.statistics
 
-        # Get the statistics and the process ID.
+        # Get the statistics
         module_stats = basf2.statistics.modules
-        proc_id = Belle2.ProcHandler.EvtProcID()
+
+        # Fill in the event information
+        self.event_number[0] = self.event_meta_data.getExperiment()
+        self.event_number[1] = self.event_meta_data.getRun()
+        self.event_number[2] = self.event_meta_data.getEvent()
 
         if not self.branches_added:
+            # make sure all this is only done in the output process
+            if Belle2.ProcHandler.parallelProcessingUsed() and not Belle2.ProcHandler.isOutputProcess():
+                basf2.B2FATAL("PerEventStatisticsGetterModule can only be used in single processing mode or in the output process")
 
-            self.ttree_inputs = [np.zeros(1, dtype=float) for module in module_stats]
+            self.ttree_inputs = np.zeros(len(module_stats), dtype=float)
+            self.last_time_sum = np.zeros(len(module_stats), dtype=float)
 
             for i, stat in enumerate(module_stats):
                 # escape the module names in ROOT-safe manner. Otherwise weird stuff happens like
                 # sub-branches get created or the branch cannot be opened in the TBrowser
-                module_name = Belle2.makeROOTCompatible(stat.name)
-
-                ttree.Branch(module_name + "_" + str(i), self.ttree_inputs[i], module_name + "_" + str(i) + "/D")
+                module_name = "{name}_{i}".format(name=Belle2.makeROOTCompatible(stat.name), i=i)
+                ttree.Branch(module_name, self.ttree_inputs[i:], f"{module_name}/D")
 
             self.branches_added = True
 
         # Fill in the module statistics into the branches
-        for i, module_stat in enumerate(module_stats):
-            self.ttree_inputs[i][0] = module_stat.time_sum(basf2.statistics.EVENT) - self.ttree_inputs[i][0]
-
-        # Fill in the other two variables into the branches
-        self.proc_id_input[0] = proc_id
-        self.event_number_input[0] = self.event_meta_data.getEvent()
+        time_sum = np.array([m.time_sum(basf2.statistics.EVENT) for m in module_stats], dtype=float)
+        self.ttree_inputs[:] = time_sum - self.last_time_sum
+        self.last_time_sum[:] = time_sum
 
         # Send the branches to the TTree.
         ttree.Fill()
@@ -103,5 +104,5 @@ class PerEventStatisticsGetterModule(basf2.Module):
         Write out the merged statistics to the ROOT file.
         This should only be called once, as we would end up with different versions otherwise.
         """
-        self.statistics.obj().write(self.file)
-        self.file.Close()
+        self.statistics.Write()
+        self.tfile.Close()

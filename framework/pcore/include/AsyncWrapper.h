a0e74be3f0c7521f43b783c41630fb4f8e568e55
Nils Braun <nils.braun@desy.de>
2018-06-25 11:11:28 +0200

    Readded the AsyncWrapper, which is however not working in the moment

---
+++ framework/pcore/include/AsyncWrapper.h
@@ -0,0 +1,84 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2013 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Christian Pulvermacher                                   *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+#include <framework/core/Module.h>
+
+namespace Belle2 {
+  class RxModule;
+  class TxModule;
+  class RingBuffer;
+  class ProcHandler;
+
+  /** Wraps a given Module to execute it asynchronously.
+   *
+   * The module will be executed in its own process and will receive all
+   * event data that would normally be available to it, once available.
+   * Anything written to the DataStore by the wrapped module will be lost.
+   *
+   * To use it, inherit from AsyncWrapper and call it's constructor within
+   * your own constructor, passing it the name of the wrapped module.
+   *
+   * \sa See AsyncDisplayModule for an example that wraps DisplayModule.
+   *
+   */
+  class AsyncWrapper : public Module {
+  public:
+    /**
+     * Wrap am module of given type.
+     */
+    AsyncWrapper(const std::string& moduleType);
+
+    ~AsyncWrapper();
+
+    /** Access wrapped module (owned by AsyncWrapper). */
+    ModulePtr getWrapped() { return m_wrappedModule; }
+
+    /** Call this from initialize().
+     *
+     * This function will fork() to create the wrapping environment, everything done previously will be available in the wrapped module.
+     */
+    void initialize();
+    /** Call this from event().  */
+    void event();
+    /** Call this from terminate().  */
+    void terminate();
+
+    /** returns true if the current process is on the receiving (async) side of an AsyncWrapper. */
+    static bool isAsync() { return s_isAsync; }
+
+    /** Retun number of events available in the RingBuffer. */
+    static int numAvailableEvents();
+
+
+  private:
+    /** The wrapped module. */
+    ModulePtr m_wrappedModule;
+
+    /** shared memory buffer */
+    RingBuffer* m_ringBuffer;
+
+    /** receiving module. */
+    RxModule* m_rx;
+
+    /** transmitting module. */
+    TxModule* m_tx;
+
+    /** Discard old events when buffer is full. If false, the main process will wait until there is enough space in the buffer. (i.e. synchronous operation) */
+    bool m_discardOldEvents;
+
+    /** true if the current process is on the receiving (async) side of an AsyncWrapper. */
+    static bool s_isAsync;
+
+    /** if s_isAsync is true, this contains the corresponding RingBuffer, see numAvailableEvents(). */
+    static RingBuffer* s_currentRingBuffer;
+  };
+}
---
+++ framework/pcore/src/AsyncWrapper.cc
@@ -0,0 +1,137 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2013 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Christian Pulvermacher                                   *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <framework/pcore/AsyncWrapper.h>
+#include <framework/core/Path.h>
+
+#include <framework/core/EventProcessor.h>
+#include <framework/core/ModuleManager.h>
+#include <framework/pcore/ProcHandler.h>
+#include <framework/pcore/RingBuffer.h>
+#include <framework/pcore/RxModule.h>
+#include <framework/pcore/TxModule.h>
+#include <framework/logging/LogSystem.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/dataobjects/EventMetaData.h>
+
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <cstdlib>
+
+using namespace Belle2;
+
+bool AsyncWrapper::s_isAsync = false;
+RingBuffer* AsyncWrapper::s_currentRingBuffer = NULL;
+namespace {
+  static std::vector<RingBuffer*> rbList;
+  void cleanupIPC()
+  {
+    if (!AsyncWrapper::isAsync()) {
+      for (RingBuffer* rb : rbList)
+        delete rb;
+      rbList.clear();
+    }
+  }
+}
+
+int AsyncWrapper::numAvailableEvents()
+{
+  if (!s_isAsync) {
+    B2ERROR("AsyncWrapper::numAvailableEvents() used in synchronous thread??");
+    return true;
+  }
+
+  return s_currentRingBuffer->numq();
+}
+
+AsyncWrapper::AsyncWrapper(const std::string& moduleType): Module(), m_ringBuffer(0), m_rx(0), m_tx(0)
+{
+  m_wrappedModule = ModuleManager::Instance().registerModule(moduleType);
+  setParamList(m_wrappedModule->getParamList()); //inherit parameters from wrapped module
+
+  addParam("discardOldEvents", m_discardOldEvents,
+           "Discard old events when buffer is full. If false, the main process will wait until there is enough space in the buffer. (i.e. synchronous operation)",
+           true);
+}
+
+AsyncWrapper::~AsyncWrapper()
+{
+}
+
+void AsyncWrapper::initialize()
+{
+  B2INFO("Initializing AsyncWrapper...");
+
+  ProcHandler::initialize(1);
+  const int bufferSizeInts = 8000000; //~32M, within Ubuntu's shmmax limit
+  m_ringBuffer = new RingBuffer(bufferSizeInts);
+  rbList.push_back(m_ringBuffer);
+  m_rx = new RxModule(m_ringBuffer);
+  m_rx->disableMergeableHandling();
+  m_tx = new TxModule(m_ringBuffer);
+  m_tx->disableMergeableHandling();
+  m_tx->setBlockingInsert(!m_discardOldEvents); //actually decouple this process
+
+  //fork out one extra process
+  if (ProcHandler::startWorkerProcesses(1)) {
+    //forked thread:
+    //allow access to async parts
+    s_isAsync = true;
+    s_currentRingBuffer = m_ringBuffer;
+
+    PathPtr path(new Path);
+    path->addModule(ModulePtr(m_rx));
+    path->addModule(m_wrappedModule);
+
+    //LogSystem::Instance().resetMessageCounter(); //for testing parallel processing
+
+    EventProcessor eventProc;
+    eventProc.process(path);
+    B2INFO("Asynchronous process done!");
+    exit(0);
+  } else {
+    atexit(cleanupIPC);
+
+    //main thread: chain tx and return
+    m_tx->initialize();
+  }
+}
+
+void AsyncWrapper::event()
+{
+  if (!ProcHandler::isWorkerProcess()) {
+    if (waitpid(-1, NULL, WNOHANG) != 0) {
+      StoreObjPtr<EventMetaData> eventMetaData;
+      eventMetaData->setEndOfData();
+    }
+
+    m_tx->event();
+  }
+}
+
+void AsyncWrapper::terminate()
+{
+  if (!ProcHandler::isWorkerProcess()) {
+    m_tx->terminate();
+
+    m_ringBuffer->kill();
+    B2INFO("Waiting for asynchronous process...");
+    // TODO:
+    //m_procHandler->waitForAllProcesses();
+    B2INFO("Done, cleaning up...");
+    delete m_tx;
+    delete m_rx;
+    delete m_ringBuffer;
+    for (RingBuffer*& rb : rbList)
+      if (rb == m_ringBuffer)
+        rb = nullptr;
+  }
+}

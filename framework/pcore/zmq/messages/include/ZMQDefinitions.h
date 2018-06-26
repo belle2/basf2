9ddd014f38a74d6ea95df86bcb2d5b588e14a51e
Nils Braun <nils.braun@desy.de>
2018-06-26 13:36:17 +0200

    Updated comments

--- framework/pcore/zmq/messages/include/ZMQDefinitions.h
+++ framework/pcore/zmq/messages/include/ZMQDefinitions.h
@@ -1,19 +1,17 @@
 #pragma once
 
 namespace Belle2 {
+  /// Type the messages can have
   enum class c_MessageTypes {
     c_helloMessage = 'h', // is sent by all processes on startup
     c_terminateMessage = 't', // is sent by all processed on termination
-    c_stopMessage = '2', // Ask all processes to stop
-    c_deleteMessage = '3', // TODO where is this used??
+    c_stopMessage = 's', // Ask all processes to (gently) stop
+    c_deleteMessage = 'd', // Delete the events of a given worker
     c_eventMessage = 'e', // a normal message with event data
-    c_endMessage = 'q', // indicates the last event reached the input process for confirmation
+    c_endMessage = 'q', // indicates the last event was processed by the input process (sent to worker and output)
     c_readyMessage = 'r', // is sent from the worker to the input to show, that it can process an event
-    c_whelloMessage = 'w', // is sent from the workers to the input
+    c_whelloMessage = 'w', // is sent from the workers to the input for initialisation
     c_confirmMessage = 'c', // is sent when an event is confirmed to be at the output process
-    c_deathMessage = 'd', // is sent when a worker should be killed (because it needed too much time)
+    c_deathMessage = 'k', // is sent when a worker should be killed (because it needed too much time)
   };
-
-  const bool c_sendMore = true;
-
 }

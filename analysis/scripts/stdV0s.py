ded87092181f33e49624aa0855c5ce1707cf3ddb
Francesco Tenchini <francesco.tenchini@desy.de>
2018-07-12 07:21:11 +0900

    removed chiProb cut once again

--- analysis/scripts/stdV0s.py
+++ analysis/scripts/stdV0s.py
@@ -37,8 +37,7 @@ def mergedKshorts(prioritiseV0=True, path=analysis_main):
         path (basf2.Path) the path to load the modules
     """
     fillParticleList('K_S0:V0', '0.3 < M < 0.7', True, path=path)
-    stdPi('all')
-    applyCuts('pi+:all', 'chiProb>0.001')
+    stdPi('all')  # no quality cuts
     reconstructDecay('K_S0:RD -> pi-:all pi+:all', '0.3 < M < 0.7', 1, True, path)
     V0ListMerger('K_S0:V0', 'K_S0:RD', prioritiseV0)  # outputs K_S0:merged
     vertexKFit('K_S0:merged', 0.0, '', '', path)

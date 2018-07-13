a8a24679bbfb1ac30967712a478540764fca584e
Yun-Tsung Lai <ytlai@post.kek.jp>
2018-07-13 15:21:15 +0900

    fix the warnings in trg package

--- trg/cdc/src/EventTime.cc
+++ trg/cdc/src/EventTime.cc
@@ -199,7 +199,7 @@ namespace Belle2 {
       for (int iSL = 0; iSL < 9; iSL++) {
         int hmbw = 160 + 32 * (iSL - 1); //set bit width of hitmap part
         if (iSL == 0) hmbw = 160;
-        string h[10];
+        string hs[10];
         string t[10];
         char file[30];
         sprintf(file, "SL%d.coe", iSL);
@@ -208,9 +208,9 @@ namespace Belle2 {
         string clk = bitset<9>(iClk + 64 * (m_eventN - 1)).to_string();
         string clk2 = bitset<5>(iClk).to_string();
         for (int i = 0; i < 10; i++) {
-          h[i] = bitset<4>(ft[iSL][iClk][i]).to_string();
-          if (cnt[iSL][iClk] > i) t[i] = clk2 + h[i];
-          else t[i] = "00000" + h[i];
+          hs[i] = bitset<4>(ft[iSL][iClk][i]).to_string();
+          if (cnt[iSL][iClk] > i) t[i] = clk2 + hs[i];
+          else t[i] = "00000" + hs[i];
         }
         ofstream fout;
         fout.open(file, ios::app);

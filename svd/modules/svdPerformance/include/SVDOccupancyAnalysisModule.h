d52cb703ffdb986ff24aacbb47dec35c5b896e6c
Giulia Casarosa <giulia.casarosa@desy.de>
2018-07-17 02:37:04 +0900

    add SVDHistograms to SVDOccupancyAnalysis module

--- svd/modules/svdPerformance/include/SVDOccupancyAnalysisModule.h
+++ svd/modules/svdPerformance/include/SVDOccupancyAnalysisModule.h
@@ -16,6 +16,7 @@
 #include <framework/datastore/StoreArray.h>
 #include <framework/datastore/StoreObjPtr.h>
 #include <svd/dataobjects/SVDShaperDigit.h>
+#include <svd/dataobjects/SVDHistograms.h>
 #include <framework/dataobjects/EventMetaData.h>
 
 #include <svd/calibration/SVDNoiseCalibrations.h>
@@ -75,34 +76,12 @@ namespace Belle2 {
 
     SVDNoiseCalibrations m_NoiseCal;
 
-    static const int m_nLayers = 4;
-    static const int m_nSensors = 5;
-    static const int m_nSides = 2;
-
-    unsigned int sensorsOnLayer[4];
-
-    TList* m_histoList_shaper[m_nLayers];
 
     //SHAPER
-    TH1F* h_occ[m_nLayers][m_nSensors][m_nSides]; //number per event
-    TH1F* h_zsOcc[m_nLayers][m_nSensors][m_nSides]; //number per event
-    TH1F* h_zsOccSQ[m_nLayers][m_nSensors][m_nSides]; //number per event
-
-    TH2F* h_occtdep[m_nLayers][m_nSensors][m_nSides]; //number per event
-    int getSensor(int sensor)
-    {
-      return sensor - 1;;
-    }
-
-    //list of functions to create histograms:
-    TH1F* createHistogram1D(const char* name, const char* title,
-                            Int_t nbins, Double_t min, Double_t max,
-                            const char* xtitle, TList* histoList = NULL);  /**< thf */
-    TH2F* createHistogram2D(const char* name, const char* title,
-                            Int_t nbinsX, Double_t minX, Double_t maxX, const char* titleX,
-                            Int_t nbinsY, Double_t minY, Double_t maxY, const char* titleY,
-                            TList* histoList = NULL);  /**< thf */
-
+    SVDHistograms<TH1F>* m_histo_occ;
+    SVDHistograms<TH1F>* m_histo_zsOcc;
+    SVDHistograms<TH1F>* m_histo_zsOccSQ;
+    SVDHistograms<TH2F>* m_histo_occtdep;
 
   };
 }

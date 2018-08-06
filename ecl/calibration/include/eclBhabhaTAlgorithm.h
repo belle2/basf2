fa1a7dc598fc6f01bdf8b3e71ae99938ad2f9c73
Mikhail Remnev <mikhail.remnev@desy.de>
2018-08-06 13:17:07 +0900

    Initial version of time calibration algorithm for bhabha events

---
+++ ecl/calibration/include/eclBhabhaTAlgorithm.h
@@ -0,0 +1,55 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2018 - Belle II Collaboration                             *
+ *                                                                        *
+ * Calculate time offsets from bhabha events.                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Mikhail Remnev                                           *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+#include <ecl/calibration/eclBhabhaTAlgorithm.h>
+#include <calibration/CalibrationAlgorithm.h>
+
+class TH2F;
+
+namespace Belle2 {
+  namespace ECL {
+
+    /** Calibrate ecl crystals using bhabha events */
+    class eclBhabhaTAlgorithm : public CalibrationAlgorithm {
+    public:
+
+      /**..Constructor */
+      eclBhabhaTAlgorithm();
+
+      /**..Destructor */
+      virtual ~eclBhabhaTAlgorithm() {}
+
+      /*** Parameters ***/
+
+      int cellIDLo; /**< Fit crystals with cellID0 in the inclusive range [cellIDLo,cellIDHi] */
+      int cellIDHi; /**< */
+      int maxIterations; /**< Fit is always stopped after maxIterations */
+
+      bool debugOutput; /**< Save every histogram and fitted function to debugFilename */
+      /** Name of file with debug output, eclBhabhaTAlgorithm.root by default */
+      std::string debugFilename;
+
+    protected:
+
+      /**..Run algorithm on events */
+      virtual EResult calibrate();
+
+    private:
+      /** Sum of all TimevsCrys histograms. */
+      TH2F* m_TimevsCrys_tot;
+      /** Number of processed runs */
+      unsigned int m_run_count;
+    };
+  }
+} // namespace Belle2
+

9e0e9284fa0401430c9180e5c4b6bec117c958b0
ikomarov <ilya.komarov@desy.de>
2018-08-08 17:46:24 +0200

    B2A421-OrcaKinFit_1CFit.py

--- analysis/examples/tutorials/B2A421-OrcaKinFit_1CFit.py
+++ analysis/examples/tutorials/B2A421-OrcaKinFit_1CFit.py
@@ -29,7 +29,7 @@ print_params(beamparameters)
 
 # Input file(s).
 filelistSIG = ['/gpfs/group/belle2/tutorial/orcakinfit/out-1.root']
-inputMdstList('default', filelistSIG)
+inputMdstList('MC9', filelistSIG)
 
 # use standard final state particle lists for muons
 fillParticleList('mu-:z0', 'chiProb > 0.001 and p > 1.0')
@@ -46,36 +46,28 @@ matchMCTruth('Z0:mm_kinfit')
 # kinematic 1C hard fit
 UnmeasuredfitKinematic1C('Z0:mm_kinfit')
 
-# create and fill flat Ntuple with MCTruth and kinematic information for z0 without no kinfit
-from variableCollections import event_variables, kinematic_variables, cluster_variables,\
-    track_variables, mc_variables, pid_variables, \
-    convert_to_daughter_vars, convert_to_gd_vars, \
-    flight_info, mc_flight_info, vertex, mc_vertex, tag_vertex, mc_tag_vertex,\
-    make_mc, momentum_uncertainty
+# Select variables that we want to store to ntuple
+from variableCollections import *
 
-from modularAnalysis import variablesToNTuple
-rootOutputFile = 'B2A421-Orcakinfit_1CFit.root'
+muvars = kinematics + mc_truth + mc_kinematics + momentum_uncetainty
+z0vars = event_meta_data + inv_mass + kinematics + mc_kinematics + mc_truth + \
+    convert_to_all_selected_vars(muvars, 'Z0 -> ^mu+ ^mu-')
 
-variablesToNTuple(filename=rootOutputFile,
-                  decayString='Z0:mm_rec',
-                  treename='Z0_mm_rec',
-                  event_variables + kinematic_variables + make_mc(kinematic_variables) + mc_variables +
-                  convert_to_daughter_vars(kinematic_variables + mc_variables +
-                                           momentum_uncertainty + make_mc(kinematic_variables), 0) +
-                  convert_to_daughter_vars(kinematic_variables + mc_variables +
-                                           momentum_uncertainty + make_mc(kinematic_variables), 1))
+z0uvars = z0vars + \
+    wrap_list(['OrcaKinFitProb',
+               'OrcaKinFitChi2',
+               'OrcaKinFitErrorCode',
+               'OrcaKinFitUnmeasuredTheta',
+               'OrcaKinFitUnmeasuredPhi',
+               'OrcaKinFitUnmeasuredE'], 'extraInfo(variable)', "")
 
-variablesToNTuple(filename=rootOutputFile,
-                  decayString='Z0:mm_kinfit',
-                  treename='Z0_mm_kinfit',
-                  ['extraInfo(OrcaKinFitProb)', 'extraInfo(OrcaKinFitChi2)',
-                   'extraInfo(OrcaKinFitErrorCode)', 'extraInfo(OrcaKinFitUnmeasuredTheta)',
-                   'extraInfo(OrcaKinFitUnmeasuredPhi)', 'extraInfo(OrcaKinFitUnmeasuredE)'] +
-                  event_variables + kinematic_variables + make_mc(kinematic_variables) + mc_variables +
-                  convert_to_daughter_vars(kinematic_variables + mc_variables +
-                                           momentum_uncertainty + make_mc(kinematic_variables), 0) +
-                  convert_to_daughter_vars(kinematic_variables + mc_variables +
-                                           momentum_uncertainty + make_mc(kinematic_variables), 1))
+# Saving variables to ntuple
+from modularAnalysis import variablesToNtuple
+output_file = 'B2A421-Orcakinfit_1CFit.root'
+variablesToNtuple('Z0:mm_rec', z0vars,
+                  filename=output_file, treename='Z0_mm_rec')
+variablesToNtuple('Z0:mm_kinfit', z0uvars,
+                  filename=output_file, treename='Z0_mm_kinfit')
 
 
 # Process the events

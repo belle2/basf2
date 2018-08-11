b3de087bfb9a66c11e49230dd801a2d995bcac9a
Benjamin Schwenker <benjamin.schwenker@phys.uni-goettingen.de>
2018-08-11 22:06:27 +0900

    updated some calibration scritps

--- pxd/examples/calibration/create_mcrun.py
+++ pxd/examples/calibration/create_mcrun.py
@@ -4,28 +4,39 @@
 # Simple basf2 steering file that creates mc file with PXDSimHits and
 # EventMetaData needed for PXD gain calibration.
 #
-# basf2 create_mcrun.py -- --expNo=3 --runNo=3360 --setNo=0
+# Note that this script should only be used for runs in phase2 geometry
+# wiht beams and magnetic field.
+#
+# Note that only SimHits from bg will be produced. A path to the mixer
+# files for phase 2 must be specified. Only use for  runs with beam
+# where most clusters are from bg.
+#
+# The command below will create reference mc file for gain calibration
+# for run=3360 in exp=3 being part of the phase2 campaign.
+#
+# basf2 create_mcrun.py -- --tag='Calibration_Offline_Development'  --expNo=3 --runNo=3360 --setNo=0
 #
 # author: benjamin.schwenker@pyhs.uni-goettingen.de
 
 
 from basf2 import *
 
-# Default GT for phase2 data production GT
-reset_database()
-use_central_database("Calibration_Offline_Development")
-
 import argparse
 parser = argparse.ArgumentParser(description="Create SimHits for a run with user specified ExpRun")
+parser.add_argument('--tag', default='Calibration_Offline_Development', type=str, help='Set name of GT')
 parser.add_argument('--expNo', default=3, type=int, help='Set experiment number')
 parser.add_argument('--runNo', default=3360, type=int, help='Set run number')
 parser.add_argument('--setNo', default=0, type=int, help='setnumber for bg simulation')
-parser.add_argument('--bg', default='/home/benjamin/BeamRun18/phase_bg_campaign15', type=str, help='Path to mixer sets')
+parser.add_argument('--bg', default='/group/belle2/BGFile/OfficialBKG/15thCampaign/phase2', type=str, help='Path to mixer sets')
 parser.add_argument('--scaleFactor', default=1.0, type=float, help='Scale factor for mixer')
 args = parser.parse_args()
 
 import glob
 bg = glob.glob(args.bg + '/set' + str(args.setNo) + '/*.root')
+
+
+reset_database()
+use_central_database(args.tag)
 
 main = create_path()
 main.add_module("EventInfoSetter", expList=[args.expNo], runList=[args.runNo], evtNumList=[1000])

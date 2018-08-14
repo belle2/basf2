8af152b1bbdba116d73690dc1da69477df8293b9
Sam Cunliffe <sam.cunliffe@desy.de>
2018-08-14 16:52:19 +0200

    Resolves BII-3926

--- analysis/scripts/stdV0s.py
+++ analysis/scripts/stdV0s.py
@@ -39,7 +39,7 @@ def mergedKshorts(prioritiseV0=True, path=analysis_main):
     fillParticleList('K_S0:V0', '0.3 < M < 0.7', True, path=path)
     stdPi('all')  # no quality cuts
     reconstructDecay('K_S0:RD -> pi-:all pi+:all', '0.3 < M < 0.7', 1, True, path)
-    V0ListMerger('K_S0:V0', 'K_S0:RD', prioritiseV0)  # outputs K_S0:merged
+    V0ListMerger('K_S0:V0', 'K_S0:RD', prioritiseV0, path)  # outputs K_S0:merged
     vertexKFit('K_S0:merged', 0.0, '', '', path)
     applyCuts('K_S0:merged', '0.450 < M < 0.550', path)
 

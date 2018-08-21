7452050692b8f8e5265d98596791f105d16c2bd0
Racha Cheaib <racha.cheaib@desy.de>
2018-08-22 04:38:56 +0900

    added validation scripts

--- skim/scripts/skim/fei.py
+++ skim/scripts/skim/fei.py
@@ -4,8 +4,22 @@
 """ Skim list building functions for analyses with FEI """
 
 __authors__ = [
-    ""
+    "Sophie Hollit & R. Cheaib"
 ]
 
 from basf2 import *
 from modularAnalysis import *
+
+
+from variables import *
+variables.addAlias('sigProb', 'extraInfo(SignalProbability)')
+
+
+def B0hadronic():
+    """FEI Hadronic B0 skim list"""
+    # Reconstruct tag side
+    # Apply cuts
+    applyCuts('B0:generic', 'Mbc>5.24 and abs(deltaE)<0.200 and sigProb>0.001')
+
+    B0hadronicList = ['B0:generic']
+    return B0hadronicList

22785bc2b27295be6fae5c7dbeb4d7f2ec1a6768
Mikhail Remnev <mikhail.remnev@desy.de>
2018-09-03 19:25:00 +0900

    Changed steering file for eclBhabhaTAlgorithm to merge input files

--- ecl/calibration/scripts/run_eclBhabhaT_algorithm.py
+++ ecl/calibration/scripts/run_eclBhabhaT_algorithm.py
@@ -17,9 +17,11 @@ import os
 import sys
 from basf2 import *
 from ROOT import Belle2
+from glob import glob
 
 algo = Belle2.ECL.eclBhabhaTAlgorithm()
 algo.setInputFileNames(['eclBhabhaTCollector.root'])
+
 algo.cellIDLo = 0
 algo.cellIDHi = 8735
 algo.maxIterations = 10
@@ -27,11 +29,6 @@ algo.debugOutput = True
 algo.debugFilename = "eclBhabhaTAlgorithm.root"
 
 exprun_vector = algo.getRunListFromAllData()
-for exprun in exprun_vector:
-    iov_to_execute = [(exprun.first, exprun.second)]
-    alg_result = algo.execute(iov_to_execute, 0)
-    print("result was", alg_result)
-
-    if (alg_result == 0):
-        # Commits a successful list of dbobjects to localdb/
-        algo.commit()
+alg_result = algo.execute()
+print("result was", alg_result)
+algo.commit()

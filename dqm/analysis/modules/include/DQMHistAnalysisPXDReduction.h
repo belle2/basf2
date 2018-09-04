a06342de3e444bb193f71f08e53b3e0c4ad5ac7a
Björn Spruck <bspruck@uni-mainz.de>
2018-09-04 18:26:38 +0200

    PXD data reduction, mean values for all modules

---
+++ dqm/analysis/modules/include/DQMHistAnalysisPXDReduction.h
@@ -0,0 +1,57 @@
+//+
+// File : DQMHistAnalysisPXDReduction.h
+// Description : DAQM Analysis for PXD Common Modes
+//
+// Author : Bjoern Spruck, University Mainz
+// Date : 2018
+//-
+
+#pragma once
+
+#include <framework/core/Module.h>
+
+#include <dqm/analysis/modules/DQMHistAnalysis.h>
+
+#include <vxd/geometry/SensorInfoBase.h>
+
+#include <TF1.h>
+#include <TH2F.h>
+#include <TCanvas.h>
+#include <TLine.h>
+
+namespace Belle2 {
+  /*! DQM Histogram Analysis for PXD Reduction */
+
+  class DQMHistAnalysisPXDReductionModule : public DQMHistAnalysisModule {
+
+    // Public functions
+  public:
+
+    //! Constructor / Destructor
+    DQMHistAnalysisPXDReductionModule();
+  private:
+
+    //! Module functions to be called from main process
+    void initialize(void) override final;
+
+    //! Module functions to be called from event process
+    void beginRun(void) override final;
+    void event(void) override final;
+    void endRun(void) override final;
+    void terminate(void) override final;
+
+    // Data members
+    std::string m_histodir;
+
+    //IDs of all PXD Modules to iterate over
+    std::vector<VxdID> m_PXDModules;
+
+    TH1F* m_hReduction;
+    TCanvas* m_cReduction;
+//     TLine* m_line1, *m_line2, *m_line3;
+
+    TH1* findHistLocal(TString& a);
+
+  };
+} // end namespace Belle2
+

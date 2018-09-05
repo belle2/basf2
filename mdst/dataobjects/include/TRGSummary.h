531bfa92db040d102fde1261f2354e8e31c2fcac
Thomas Hauth <Thomas.Hauth@kit.edu>
2018-09-05 16:31:37 +0900

    moved constructor and two big methods to the .cc file

--- mdst/dataobjects/include/TRGSummary.h
+++ mdst/dataobjects/include/TRGSummary.h
@@ -84,15 +84,7 @@ namespace Belle2 {
     TRGSummary(unsigned int inputBits[10],
                unsigned int ftdlBits[10],
                unsigned int psnmBits[10],
-               ETimingType timType)
-    {
-      for (int i = 0; i < 10; i++) {
-        m_inputBits[i] = inputBits[i];
-        m_ftdlBits[i] = ftdlBits[i];
-        m_psnmBits[i] = psnmBits[i];
-      }
-      m_timType = timType;
-    }
+               ETimingType timType);
 
     /** Destructor.
      */
@@ -160,53 +152,12 @@ namespace Belle2 {
     }
 
     /** Return a short summary of this object's contents in HTML format. */
-    std::string getInfoHTML() const override
-    {
-      std::stringstream htmlOutput;
-
-      htmlOutput << "<table>";
-      htmlOutput << "<tr><td></td><td bgcolor='#cccccc'>GDL Input</td><td bgcolor='#cccccc' colspan='2'>GDL Output</td></tr>";
-      htmlOutput << "<tr><td>Bit</td><td>Input Bits</td><td>Final Trg DL</td><td>Prescaled Trg and Mask</td></tr>";
-
-      for (unsigned int currentBit = 0; currentBit < (c_ntrgWords * c_trgWordSize);
-           currentBit++) {
-        htmlOutput << "<tr>";
-
-        const auto currentWord = currentBit / c_trgWordSize;
-        const auto currentBitInWord = currentBit % c_trgWordSize;
-
-        const auto ftdlBit = (getFtdlBits(currentWord)
-                              & (1 << currentBitInWord)) > 0;
-        const auto psnmBit = (getPsnmBits(currentWord)
-                              & (1 << currentBitInWord)) > 0;
-        const auto inputBit = (getInputBits(currentWord)
-                               & (1 << currentBitInWord)) > 0;
-
-        htmlOutput << "<td>" << currentBit << "(word " << currentWord
-                   << " bit " << currentBitInWord << ")</td>";
-        htmlOutput << outputBitWithColor(inputBit);
-        htmlOutput << outputBitWithColor(ftdlBit);
-        htmlOutput << outputBitWithColor(psnmBit);
-        htmlOutput << "</tr>";
-      }
-      htmlOutput << "</table>";
-
-      return htmlOutput.str();
-    }
+    std::string getInfoHTML() const override;
 
   private:
 
     /** return the td part of an HTML table with green of the bit is > 0 */
-    std::string outputBitWithColor(bool bit) const
-    {
-      const std::string colorNeutral = gROOT->GetColor(kWhite)->AsHexString();
-      const std::string colorAccept = gROOT->GetColor(kGreen)->AsHexString();
-
-      std::string color = bit > 0 ? colorAccept : colorNeutral;
-      std::stringstream outStream;
-      outStream << "<td bgcolor=\"" << color << "\">" << bit << "</td>";
-      return outStream.str();
-    }
+    std::string outputBitWithColor(bool bit) const;
 
     /**
      * version of this code
---
+++ mdst/dataobjects/src/TRGSummary.cc
@@ -0,0 +1,65 @@
+#include <mdst/dataobjects/TRGSummary.h>
+
+using namespace Belle2;
+
+TRGSummary::TRGSummary(unsigned int inputBits[10],
+                       unsigned int ftdlBits[10],
+                       unsigned int psnmBits[10],
+                       ETimingType timType)
+{
+  for (int i = 0; i < 10; i++) {
+    m_inputBits[i] = inputBits[i];
+    m_ftdlBits[i] = ftdlBits[i];
+    m_psnmBits[i] = psnmBits[i];
+  }
+  m_timType = timType;
+}
+
+std::string TRGSummary::getInfoHTML() const
+{
+  std::stringstream htmlOutput;
+
+  htmlOutput << "<table>";
+  htmlOutput
+      << "<tr><td></td><td bgcolor='#cccccc'>GDL Input</td><td bgcolor='#cccccc' colspan='2'>GDL Output</td></tr>";
+  htmlOutput
+      << "<tr><td>Bit</td><td>Input Bits</td><td>Final Trg DL</td><td>Prescaled Trg and Mask</td></tr>";
+
+  for (unsigned int currentBit = 0;
+       currentBit < (c_ntrgWords * c_trgWordSize); currentBit++) {
+    htmlOutput << "<tr>";
+
+    const auto currentWord = currentBit / c_trgWordSize;
+    const auto currentBitInWord = currentBit % c_trgWordSize;
+
+    const auto ftdlBit =
+      (getFtdlBits(currentWord) & (1 << currentBitInWord)) > 0;
+    const auto psnmBit =
+      (getPsnmBits(currentWord) & (1 << currentBitInWord)) > 0;
+    const auto inputBit = (getInputBits(currentWord)
+                           & (1 << currentBitInWord)) > 0;
+
+    htmlOutput << "<td>" << currentBit << "(word " << currentWord << " bit "
+               << currentBitInWord << ")</td>";
+    htmlOutput << outputBitWithColor(inputBit);
+    htmlOutput << outputBitWithColor(ftdlBit);
+    htmlOutput << outputBitWithColor(psnmBit);
+    htmlOutput << "</tr>";
+  }
+  htmlOutput << "</table>";
+
+  return htmlOutput.str();
+}
+
+
+/** return the td part of an HTML table with green of the bit is > 0 */
+std::string TRGSummary::outputBitWithColor(bool bit) const
+{
+  const std::string colorNeutral = gROOT->GetColor(kWhite)->AsHexString();
+  const std::string colorAccept = gROOT->GetColor(kGreen)->AsHexString();
+
+  std::string color = bit > 0 ? colorAccept : colorNeutral;
+  std::stringstream outStream;
+  outStream << "<td bgcolor=\"" << color << "\">" << bit << "</td>";
+  return outStream.str();
+}

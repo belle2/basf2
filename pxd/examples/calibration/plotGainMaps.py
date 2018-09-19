825bd89050f2015e6bb7511a6181c08d9ceb3187
Benjamin Schwenker <benjamin.schwenker@phys.uni-goettingen.de>
2018-09-19 19:05:30 +0200

    better scripts for plotting pxd calibrations

--- pxd/examples/calibration/plotGainMaps.py
+++ pxd/examples/calibration/plotGainMaps.py
@@ -5,11 +5,13 @@
 #
 # At first, you can extract the gain calibration payloads from a localdb/centraldb using the tool
 #
-# b2conditionsdb-extract --exp 3 --runs 126-6522 --tag Calibration_Offline_Development --output gain_payloads.root  PXDGainMapPar
+# b2conditionsdb-extract --exp 3 --runs 0-5614 --tag Calibration_Offline_Development --output gain_payloads.root  PXDGainMapPar
 #
 # Secondly, execute the script as
 #
-# basf2 gain_plot.py
+# basf2 plotGainMaps.py
+#
+# basf2 plotGainMaps.py -- --maps
 #
 # author: benjamin.schwenker@phys.uni-goettingen.de
 
@@ -20,6 +22,12 @@ import ROOT
 from ROOT import Belle2
 import numpy as np
 from array import array
+
+import argparse
+parser = argparse.ArgumentParser(description="Plot gain maps")
+parser.add_argument('--maps', dest='maps', action="store_true", help='Create maps from payloads. This can be slow!')
+args = parser.parse_args()
+
 
 sensor_list = [Belle2.VxdID("1.1.1"), Belle2.VxdID("1.1.2"), Belle2.VxdID("2.1.1"), Belle2.VxdID("2.1.2")]
 
@@ -49,26 +57,30 @@ for condition in conditions:
             ladder = sensorID.getLadderNumber()
             sensor = sensorID.getSensorNumber()
 
-            name = "Gains_{:d}_{:d}_{:d}_run_{:d}".format(layer, ladder, sensor, condition.run)
-            title = "Relative energy calibration Sensor={:d}.{:d}.{:d} run={:d}".format(layer, ladder, sensor, condition.run)
-            gain_map = ROOT.TH2F(name, title, nBinsU, 0, nBinsU, nBinsV, 0, nBinsV)
-            gain_map.GetXaxis().SetTitle("uBin")
-            gain_map.GetYaxis().SetTitle("vBin")
-            gain_map.GetZaxis().SetTitle("rel. gain factor")
-            gain_map.SetStats(0)
-
-            gain_list = []
+            mean_gain = 0.0
             for guID in range(nBinsU):
                 for gvID in range(nBinsV):
-                    gain = condition.PXDGainMapPar.getContent(sensorID.getID(), guID, gvID)
-                    gain_list.append(gain)
-                    gain_map.SetBinContent(int(guID + 1), int(gvID + 1), gain)
+                    mean_gain += condition.PXDGainMapPar.getContent(sensorID.getID(), guID, gvID)
 
-            mean_gain = np.mean(np.asarray(gain_list))
+            mean_gain /= (nBinsU * nBinsV)
             gain_table[sensorID.getID()].append(mean_gain)
 
-            histofile.cd("maps")
-            gain_map.Write()
+            if args.maps:
+                name = "Gains_{:d}_{:d}_{:d}_run_{:d}".format(layer, ladder, sensor, condition.run)
+                title = "Relative energy calibration Sensor={:d}.{:d}.{:d} run={:d}".format(layer, ladder, sensor, condition.run)
+                gain_map = ROOT.TH2F(name, title, nBinsU, 0, nBinsU, nBinsV, 0, nBinsV)
+                gain_map.GetXaxis().SetTitle("uBin")
+                gain_map.GetYaxis().SetTitle("vBin")
+                gain_map.GetZaxis().SetTitle("rel. gain factor")
+                gain_map.SetStats(0)
+
+                for guID in range(nBinsU):
+                    for gvID in range(nBinsV):
+                        gain = condition.PXDGainMapPar.getContent(sensorID.getID(), guID, gvID)
+                        gain_map.SetBinContent(int(guID + 1), int(gvID + 1), gain)
+
+                histofile.cd("maps")
+                gain_map.Write()
 
 histofile.cd()
 c = ROOT.TCanvas('gain_vs_runno', 'Gain evolution vs. run number', 200, 10, 700, 500)

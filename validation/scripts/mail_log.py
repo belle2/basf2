ab33d689bb472af106f96dfb2cac10b26f888f70
David Koch <david.koch@desy.de>
2018-09-20 14:50:23 +0200

    parse mail addresses from contacts and compose message for users

--- validation/scripts/mail_log.py
+++ validation/scripts/mail_log.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import re
 import json
-
-# # load comparison json file
-# with open("../../html/plots/current_reference/comparison.json") as f:
-#     comparison = json.load(f)
 
 
 def create_mail_log(comparison):
@@ -15,7 +12,7 @@ def create_mail_log(comparison):
     {
          "email@address.test" : [
              {
-                 "package": ... "plot": ....
+                 "package": ... "description": ....
              },
              {...}
          ],
@@ -29,14 +26,43 @@ def create_mail_log(comparison):
             for plotfile in package["plotfiles"]:
                 if plotfile["comparison_error"] > 0:
                     for plot in plotfile["plots"]:
-                        if plot["comparison_result"] == "error":
+                        if plot["comparison_result"] == "error" or plot["comparison_result"] == "not_compared":
                             # save all the information that's needed for an informative email
                             error_data = {}
                             error_data["package"] = plotfile["package"]
-                            error_data["plot"] = plot
-                            # check if this contact already gets mail
-                            if plot["contact"] not in mail_log:
-                                # create new key for this contact
-                                mail_log[plot["contact"]] = []
-                            mail_log[plot["contact"]].append(error_data)
+                            error_data["rootfile"] = plotfile["rootfile"]
+                            error_data["title"] = plot["title"]
+                            error_data["comparison_text"] = plot["comparison_text"]
+                            error_data["description"] = plot["description"]
+                            # every contact gets an email
+                            for contact in parse_mail_address(plot["contact"]):
+                                # check if this contact already gets mail
+                                if contact not in mail_log:
+                                    # create new key for this contact
+                                    mail_log[contact] = []
+                                mail_log[contact].append(error_data)
     return mail_log
+
+
+def parse_mail_address(string):
+    """
+    take a string and return list of email addresses that appear in it
+    """
+    return re.findall(r'[\w\.-]+@[\w\.-]+', string)
+
+
+def compose_message(plotlist):
+    """
+    takes a list of dicts (like in create_mail_log) and composes a mail body
+    """
+
+    body = "There were problem(s) with the validation of the following plots:\n\n"
+    for plot in plotlist:
+        body += "Package: "+plot["package"]+"\n"
+        body += "Title: "+plot["title"]+"\n"
+        body += "Rootfile: "+plot["rootfile"]+".root\n"
+        body += "Description: "+plot["description"]+"\n"
+        body += "Comparison: "+plot["comparison_text"]+"\n\n"
+    body += "You can take a look on the plots in more detail at <link to validation thing>."
+
+    return body

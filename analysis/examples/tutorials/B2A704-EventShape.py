dd3b565dc15dba6e41c4a44c5f8679115513a148
Umberto Tamponi <tamponi@to.infn.it>
2018-10-05 16:15:05 +0200

    Adding a variable collection with the most basic things, and improving a bit the example with more comments

--- analysis/examples/tutorials/B2A704-EventShape.py
+++ analysis/examples/tutorials/B2A704-EventShape.py
@@ -6,6 +6,7 @@
 # Stuck? Ask for help at questions.belle2.org
 #
 # This tutorial demonstrates how to use the Event Shape framework
+# usage : basf2 -i myMDSTFile.root B2A704-EventShape.py
 #
 # Contributors: Umberto Tamponi (October 2018)
 # tamponi@to.infn.it
@@ -13,84 +14,44 @@
 ####################################################################
 
 
-#
-# Import and mdst loading
-#
-
 from basf2 import *
 from modularAnalysis import *
+import variableCollections
 
-# load input ROOT file
-filelistSIG = '/group/belle2/tutorial/release_01-00-00/mdst-Y4setaY.root'
+# load input ROOT file. Please set it using the -i option
+filelistSIG = ''
+inputMdst('default', '')
 
-inputMdst('default', filelistSIG)
-
-# Creates a list of good pions and good gammas with very minimal cuts
+# Creates a list of all the good tracks (using the pion mass hypothesys)
+# and good gammas with very minimal cuts
 fillParticleList('pi+:all', 'pt> 0.1')
 fillParticleList('gamma:all', 'E > 0.1')
 
-# Builds the evt shape enabling ALL the variables
-buildEventShape(inputListNames=['pi+:all', 'gamma:all'], allMoments=True)
+# Builds the event shape enabling explicitly ALL the variables.
+# Most of the are actually enabled by defoult, but here we prefer
+# to list explicitly all the flags
+buildEventShape(inputListNames=['pi+:all', 'gamma:all'],
+                allMoments=True,
+                foxWolfram=True,
+                harmonicMoments=True,
+                cleoCones=True,
+                thrust=True,
+                collisionAxis=True,
+                jets=True,
+                sphericity=True,
+                checkForDuplicates=False)
 
-
-variables = ['foxWolframR1',
-             'foxWolframR2',
-             'foxWolframR3',
-             'foxWolframR4',
-             'cleoCone(0,thrust)',
-             'cleoCone(1,thrust)',
-             'cleoCone(2,thrust)',
-             'cleoCone(3,thrust)',
-             'cleoCone(4,thrust)',
-             'cleoCone(5,thrust)',
-             'cleoCone(6,thrust)',
-             'cleoCone(7,thrust)',
-             'cleoCone(8,thrust)',
-             'multipoleMoment(0,thrust)',
-             'multipoleMoment(1,thrust)',
-             'multipoleMoment(2,thrust)',
-             'multipoleMoment(3,thrust)',
-             'multipoleMoment(4,thrust)',
-             'multipoleMoment(5,thrust)',
-             'multipoleMoment(6,thrust)',
-             'multipoleMoment(7,thrust)',
-             'multipoleMoment(8,thrust)',
-             'multipoleMoment(0,collision)',
-             'multipoleMoment(1,collision)',
-             'multipoleMoment(2,collision)',
-             'multipoleMoment(3,collision)',
-             'multipoleMoment(4,collision)',
-             'multipoleMoment(5,collision)',
-             'multipoleMoment(6,collision)',
-             'multipoleMoment(7,collision)',
-             'multipoleMoment(8,collision)',
-             'foxWolframR(0)',
-             'foxWolframR(1)',
-             'foxWolframR(2)',
-             'foxWolframR(3)',
-             'foxWolframR(4)',
-             'foxWolframR(5)',
-             'foxWolframR(6)',
-             'foxWolframR(7)',
-             'foxWolframR(8)',
-             'foxWolframH(0)',
-             'foxWolframH(1)',
-             'foxWolframH(2)',
-             'foxWolframH(3)',
-             'foxWolframH(4)',
-             'foxWolframH(5)',
-             'foxWolframH(6)',
-             'foxWolframH(7)',
-             'foxWolframH(8)',
-             'backwardEmisphereMass',
-             'forwardEmisphereMass',
-             'sphericity',
-             'aplanarity',
-             'thrust',
-             'thrustAxisCosTheta',
-             'R2EventLevel']
-
-variablesToNtuple('', variables, filename='B2A704-EventShape.root')
+# Here we use the pre-defined collection 'EventShape', that contains
+# thrust, sphericity, aplanarity, FW ratios up to 4, harmonic moments w/respect to
+# the thrust axis up to 4 and all the cleo cones w/respect to the thrust axis.
+# In addition, we will save also the forward and backward hemisphere (or "jet") energies,
+# and the 2nd order harmonic moment calculate respect to the collision axis (i.e. the z axis)
+variablesToNtuple('',
+                  variables=['EventShape',
+                             'backwardHemisphereEnergy',
+                             'forwardHemisphereEnergy',
+                             'harmonicMoment(2, collision)'],
+                  filename='B2A704-EventShape.root')
 
 
 # Process the events

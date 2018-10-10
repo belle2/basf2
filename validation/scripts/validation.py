1280da742da638771c5c41914c1d6362aec3d52c
David Koch <david.koch@desy.de>
2018-10-10 16:52:21 +0200

    dont send mails if dry run

--- validation/scripts/validation.py
+++ validation/scripts/validation.py
@@ -1204,18 +1204,17 @@ def execute(tag=None, isTest=None):
             validation.log.note('Start creating plots...')
             validation.create_plots()
             validation.log.note('Plots have been created...')
+            # send mails
+            if cmd_arguments.send_mails:
+                mails = mail_log.Mails(validation)
+                validation.log.note('Send mails...')
+                # send mails to all users with failed scripts/comparison
+                mails.send_all_mails()
+                validation.log.note('Save mail data to {}'.format(validation.get_log_folder()))
+                # save json with data about outgoing mails
+                mails.write_log()
         else:
             validation.log.note('Skipping plot creation (dry run)...')
-
-        # send mails
-        if cmd_arguments.send_mails:
-            mails = mail_log.Mails(validation)
-            validation.log.note('Send mails...')
-            # send mails to all users with failed scripts/comparison
-            mails.send_all_mails()
-            validation.log.note('Save mail data to {}'.format(validation.get_log_folder()))
-            # save json with data about outgoing mails
-            mails.write_log()
 
         # Log that everything is finished
         validation.log.note('Validation finished! Total runtime: {0}s'

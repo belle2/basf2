eb84d3a761337721fb50d1d486b78595d6a852bb
Martin Ritter <martin.ritter@desy.de>
2018-10-12 13:13:49 +0200

    Create LogConnection which prints json log messages to console

---
+++ framework/logging/src/LogConnectionJSON.cc
@@ -0,0 +1,31 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2010 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Andreas Moll                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <framework/logging/LogConnectionJSON.h>
+#include <framework/logging/LogMessage.h>
+#include <boost/property_tree/json_parser.hpp>
+#include <fstream>
+#include <iostream>
+#include <cstdio>
+
+namespace Belle2 {
+
+  LogConnectionJSON::LogConnectionJSON(bool complete): m_console{STDOUT_FILENO, false}, m_complete{complete}
+  {
+  }
+
+  bool LogConnectionJSON::sendMessage(const LogMessage& message)
+  {
+    if (!isConnected()) return false;
+    m_console.write(message.toJSON(m_complete));
+    return true;
+  }
+
+}

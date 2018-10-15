45c336b17c6207fc8de91f7bdc6cc7df281a9907
ikomarov <ilya.komarov@desy.de>
2018-10-15 15:54:57 +0900

    Fixed typo

--- analysis/examples/tutorials/B2A101-Y4SEventGeneration.py
+++ analysis/examples/tutorials/B2A101-Y4SEventGeneration.py
@@ -43,7 +43,7 @@ ma.setupEventInfo(noEvents=100, path=my_path)
 ge.add_evtgen_generator(path=my_path,
                         finalstate='signal',
                         signaldecfile=Belle2.FileSystem.findFile(
-                            'analysis/examples/tutorials/dec/B2A101-Y4SEventGeneration.dec'))
+                            'analysis/examples/tutorials/B2A101-Y4SEventGeneration.dec'))
 
 # If the simulation and reconstruction is not performed in the sam job,
 # then the Gearbox needs to be loaded with the loadGearbox() function.

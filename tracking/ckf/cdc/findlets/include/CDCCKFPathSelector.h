534b3554153556596eb06ddb6dee03fc12742aec
Sasha Glazov <glazov@mail.desy.de>
2018-10-17 02:23:39 +0200

    Add selection of paths based on smallest sum dist**2

--- tracking/ckf/cdc/findlets/include/CDCCKFPathSelector.h
+++ tracking/ckf/cdc/findlets/include/CDCCKFPathSelector.h
@@ -29,16 +29,38 @@ namespace Belle2 {
 
     void apply(std::vector<CDCCKFPath>& newPaths) override
     {
+
       const auto pathComparison = [](const CDCCKFPath & lhs, const CDCCKFPath & rhs) {
+
+
+        auto sumOfDistances = [](const CDCCKFPath & path) {
+          // compute sum of distances for the two pats:
+          double sum = 0;
+          for (auto const& state : path) {
+            double dist = (state.getHitDistance());
+            sum += dist * dist;
+          }
+          return sum;
+        };
+
+
         const auto& lhsLastState = lhs.back();
         const auto& rhsLastState = rhs.back();
+
+
+        const auto lhsDistSum = sumOfDistances(lhs);
+        const auto rhsDistSum = sumOfDistances(rhs);
 
         const auto lhsArcLength = lhsLastState.getArcLength();
         const auto rhsArcLength = rhsLastState.getArcLength();
 
+        // priority is most hits
         if (lhs.size() != rhs.size()) {
           return lhs.size() > rhs.size();
         }
+
+        // Otherwise order by bset sum of dist^2
+        return lhsDistSum < rhsDistSum;
 
         return lhsArcLength < rhsArcLength;
       };

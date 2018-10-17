381b3118d87f4e16e1c2d658bb2c96e6851888ec
Nils Braun <nils.braun@kit.edu>
2018-10-17 23:42:21 +0200

    Replace the final selector with a chooseable filter

--- tracking/ckf/cdc/findlets/include/CDCCKFResultFinalizer.h
+++ tracking/ckf/cdc/findlets/include/CDCCKFResultFinalizer.h
@@ -11,7 +11,11 @@
 
 #include <tracking/trackFindingCDC/findlets/base/Findlet.h>
 
+#include <tracking/trackFindingCDC/filters/base/ChooseableFilter.h>
+#include <tracking/ckf/cdc/filters/paths/CDCPathFilterFactory.h>
+
 #include <tracking/trackFindingCDC/utilities/Functional.h>
+#include <tracking/trackFindingCDC/numerics/Weight.h>
 
 #include <tracking/ckf/cdc/entities/CDCCKFPath.h>
 #include <tracking/ckf/cdc/entities/CDCCKFResult.h>
@@ -25,17 +29,40 @@ namespace Belle2 {
 
   class CDCCKFResultFinalizer : public TrackFindingCDC::Findlet<const CDCCKFPath, CDCCKFResult> {
   public:
+    CDCCKFResultFinalizer()
+    {
+      addProcessingSignalListener(&m_filter);
+    }
+
+    void exposeParameters(ModuleParamList* moduleParamList, const std::string& prefix) override
+    {
+      m_filter.exposeParameters(moduleParamList, prefix);
+    }
+
     void apply(const std::vector<CDCCKFPath>& paths, std::vector<CDCCKFResult>& results) override
     {
       if (paths.empty()) {
         return;
       }
 
-      const auto& largestElementIt = std::max_element(paths.begin(), paths.end(),
-                                                      TrackFindingCDC::LessOf<TrackFindingCDC::Size>());
-      if (largestElementIt != paths.end()) {
-        results.push_back(*largestElementIt);
+      const CDCCKFPath* bestElement = nullptr;
+      TrackFindingCDC::Weight bestWeight = -NAN;
+
+      for (const CDCCKFPath& path : paths) {
+        const TrackFindingCDC::Weight weight = m_filter(path);
+        if (weight <= bestWeight) {
+          continue;
+        }
+        bestWeight = weight;
+        bestElement = &path;
+      }
+
+      if (bestElement and not std::isnan(bestWeight)) {
+        results.push_back(*bestElement);
       }
     }
+
+  private:
+    TrackFindingCDC::ChooseableFilter<CDCPathFilterFactory> m_filter;
   };
 }

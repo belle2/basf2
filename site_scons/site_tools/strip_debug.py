f22d169deddb2c55b33abc87cbd37af5d5149d04
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2018-10-18 17:22:27 +0200

    move debug files to .debug directory

--- site_scons/site_tools/strip_debug.py
+++ site_scons/site_tools/strip_debug.py
@@ -8,7 +8,7 @@ strip_debug = Builder(
     action='objcopy --only-keep-debug $SOURCE $TARGET && '
     'strip --strip-debug --strip-unneeded $SOURCE && '
     'objcopy --add-gnu-debuglink=$TARGET $SOURCE',
-    suffix='.debug')
+    suffix='.debug', prefix=".debug/")
 strip_debug.action.cmdstr = '${STRIPCOMSTR}'
 
 

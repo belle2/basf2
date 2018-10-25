2412bdeb6b0bfbe6c7a2017301931c27e997ed92
Michael Eliachevitch <michael.eliachevitch@kit.edu>
2018-10-25 10:58:06 +0200

    Change nested namespace syntax to C++17 version

--- tracking/trackFindingCDC/filters/track/include/TrackQualityVarSet.h
+++ tracking/trackFindingCDC/filters/track/include/TrackQualityVarSet.h
@@ -16,62 +16,60 @@
 /// TODO: Reuse code of `BasicTrackVarSet` instead of copying
 /// TODO: Add eventwise features, for distinguishing clones
 
-namespace Belle2 {
-  namespace TrackFindingCDC {
-    class CDCTrack;
+namespace Belle2::TrackFindingCDC {
+  class CDCTrack;
 
-    /// Names of the variables to be generated
-    /// Mostly copied from `BasicTrackVarSet`
-    constexpr
-    static char const* const trackQualityVarNames[] = {
-      "size",
-      "pt",
-      "sz_slope",
+  /// Names of the variables to be generated
+  /// Mostly copied from `BasicTrackVarSet`
+  constexpr
+  static char const* const trackQualityVarNames[] = {
+    "size",
+    "pt",
+    "sz_slope",
 
-      "drift_length_mean",
-      "drift_length_variance",
-      "drift_length_max",
-      "drift_length_min",
-      "drift_length_sum",
+    "drift_length_mean",
+    "drift_length_variance",
+    "drift_length_max",
+    "drift_length_min",
+    "drift_length_sum",
 
-      "adc_mean",
-      "adc_variance",
-      "adc_max",
-      "adc_min",
-      "adc_sum",
+    "adc_mean",
+    "adc_variance",
+    "adc_max",
+    "adc_min",
+    "adc_sum",
 
-      "empty_s_mean",
-      "empty_s_variance",
-      "empty_s_max",
-      "empty_s_min", // is only non-zero for CA tracks (why?)
-      "empty_s_sum",
-      "s_range"
-    };
+    "empty_s_mean",
+    "empty_s_variance",
+    "empty_s_max",
+    "empty_s_min", // is only non-zero for CA tracks (why?)
+    "empty_s_sum",
+    "s_range"
+  };
 
 
-    /// Vehicle class to transport the variable names
-    struct TrackQualityVarSetNames : public VarNames<CDCTrack> {
+  /// Vehicle class to transport the variable names
+  struct TrackQualityVarSetNames : public VarNames<CDCTrack> {
 
-      /// Number of variables to be generated
-      static const size_t nVars = size(trackQualityVarNames);
+    /// Number of variables to be generated
+    static const size_t nVars = size(trackQualityVarNames);
 
-      /// Getter for the name at the given index
-      static constexpr char const* getName(int iName)
-      {
-        return trackQualityVarNames[iName];
-      }
-    };
+    /// Getter for the name at the given index
+    static constexpr char const* getName(int iName)
+    {
+      return trackQualityVarNames[iName];
+    }
+  };
 
-    /**
-     *  Copy of `BasicTrackVarSet`, adapted for track quality indicator.
-     *  Class to compute floating point variables from a track
-     *  which can be recorded as a flat TNtuple or serve as input to a MVA method
-     */
-    class TrackQualityVarSet : public VarSet<TrackQualityVarSetNames> {
+  /**
+   *  Copy of `BasicTrackVarSet`, adapted for track quality indicator.
+   *  Class to compute floating point variables from a track
+   *  which can be recorded as a flat TNtuple or serve as input to a MVA method
+   */
+  class TrackQualityVarSet : public VarSet<TrackQualityVarSetNames> {
 
-    public:
-      /// Generate and assign the contained variables
-      bool extract(const CDCTrack* track) override;
-    };
-  }
+  public:
+    /// Generate and assign the contained variables
+    bool extract(const CDCTrack* track) override;
+  };
 }

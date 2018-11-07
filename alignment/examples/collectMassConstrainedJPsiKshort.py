5ed59d5606f2078c50a2d934185e4e8f5616c5c7
Tadeas Bilka <tadeas.bilka@desy.de>
2018-11-07 03:17:00 +0100

    fix include

--- alignment/examples/collectMassConstrainedJPsiKshort.py
+++ alignment/examples/collectMassConstrainedJPsiKshort.py
@@ -36,6 +36,9 @@ ana.fillParticleList('pi+:K_S0', 'pionID > 0.1', writeOut=True, path=main)
 ana.reconstructDecay('J/psi:mumu -> mu-:Jpsi mu+:Jpsi', 'InvM > 2.9969 and InvM <  3.1969', writeOut=True, path=main)
 ana.reconstructDecay('K_S0:pipi -> pi-:K_S0 pi+:K_S0', 'InvM > 0.44761 and InvM <  0.54761', writeOut=True, path=main)
 
+ana.vertexRaveDaughtersUpdate('J/psi:mumu', 0.0, path=main)
+ana.vertexRaveDaughtersUpdate('K_S0:pipi', 0.0, path=main)
+
 # ana.fitVertex(list_name='Z0:mumu', conf_level=0.0, fitter='kfitter', fit_type='fourC', daughtersUpdate=True, path=main)
 # ana.vertexRaveDaughtersUpdate('Z0:mumu', 0.0, path=main)#, constraint='ipprofile')
 # ana.vertexRaveDaughtersUpdate('K_S0:pipi', 0.0, path=main)
--- alignment/modules/MillepedeCollector/src/MillepedeCollectorModule.cc
+++ alignment/modules/MillepedeCollector/src/MillepedeCollectorModule.cc
@@ -20,7 +20,7 @@
 #include <genfit/GblFitter.h>
 
 #include <analysis/dataobjects/ParticleList.h>
-#include <analysis/utility/include/ReferenceFrame.h>
+#include <analysis/utility/ReferenceFrame.h>
 #include <framework/datastore/StoreObjPtr.h>
 #include <framework/datastore/RelationArray.h>
 #include <framework/database/DBObjPtr.h>

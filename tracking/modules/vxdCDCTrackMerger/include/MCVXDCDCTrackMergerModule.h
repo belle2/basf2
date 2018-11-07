25500b94f30e3c98343418b76d27489ebf30075d
Nils Braun <nils.braun@desy.de>
2018-11-07 15:07:36 +0100

    Added a MC VXD CDC Merger

---
+++ tracking/modules/vxdCDCTrackMerger/include/MCVXDCDCTrackMergerModule.h
@@ -0,0 +1,60 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2018 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors:  Nils Braun                                              *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+#include <tracking/trackFindingCDC/findlets/base/FindletModule.h>
+#include <tracking/trackFindingCDC/findlets/base/Findlet.h>
+#include <tracking/trackFindingCDC/collectors/selectors/BestMatchSelector.h>
+#include <tracking/trackFindingCDC/collectors/adders/RelationAdder.h>
+
+#include <tracking/modules/vxdCDCTrackMerger/StoreArrayMerger.h>
+
+#include <framework/core/ModuleParamList.h>
+#include <framework/core/ModuleParam.h>
+
+namespace Belle2 {
+  class ModuleParamList;
+  class RecoTrack;
+
+  class MCVXDCDCTrackMergerFindlet : public TrackFindingCDC::Findlet<> {
+  public:
+    /// Constructor, for setting module description and parameters.
+    MCVXDCDCTrackMergerFindlet();
+
+    /// Expose the parameters of the sub findlets.
+    void exposeParameters(ModuleParamList* moduleParamList, const std::string& prefix) override;
+
+    /// Do the track merging.
+    void apply() override;
+
+  private:
+    // Findlets
+    /// Get and write back the relations to the store array.
+    StoreArrayMerger m_storeArrayMerger;
+    /// Make a best candidate selection
+    TrackFindingCDC::BestMatchSelector<RecoTrack*, RecoTrack*> m_bestMatchSelector;
+    /// Use the weighted relations to turn them into real DataStore relations.
+    TrackFindingCDC::RelationAdder<RecoTrack*, RecoTrack*> m_relationAdder;
+
+    // Parameters
+    /// Only use fitted CDC tracks, as otherwise the comparison with the CKF is unfair.
+    bool m_param_onlyFittedCDCTracks = true;
+  };
+
+  /// This module merges tracks which are reconstructed, separately, in the silicon (PXD+VXD) and in the CDC using MC
+  class MCVXDCDCTrackMergerModule : public TrackFindingCDC::FindletModule<MCVXDCDCTrackMergerFindlet> {
+  public:
+    MCVXDCDCTrackMergerModule()
+    {
+      setDescription(
+        "This module merges tracks which are reconstructed, separately, in the silicon (PXD+VXD) and in the CDC using MC");
+    }
+  };
+}

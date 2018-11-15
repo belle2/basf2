122c3f59119e6db7383f146a6f75465faf1a39b0
Kenji Inami <kenji.inami@desy.de>
2018-11-15 15:49:11 +0900

    debug thrust variable names to follow official module updates

--- skim/scripts/skim/taupair.py
+++ skim/scripts/skim/taupair.py
@@ -50,10 +50,10 @@ def SetTauGenericSkimVariables():
     buildEventKinematics(['pi+:all', 'gamma:all'])
 
     # Split in signal and tag
-    cutAndCopyList('pi+:S1', 'pi+:all', 'cosToEvtThrust > 0')
-    cutAndCopyList('pi+:S2', 'pi+:all', 'cosToEvtThrust < 0')
-    cutAndCopyList('gamma:S1', 'gamma:all', 'cosToEvtThrust > 0')
-    cutAndCopyList('gamma:S2', 'gamma:all', 'cosToEvtThrust < 0')
+    cutAndCopyList('pi+:S1', 'pi+:all', 'cosToThrustOfEvent > 0')
+    cutAndCopyList('pi+:S2', 'pi+:all', 'cosToThrustOfEvent < 0')
+    cutAndCopyList('gamma:S1', 'gamma:all', 'cosToThrustOfEvent > 0')
+    cutAndCopyList('gamma:S2', 'gamma:all', 'cosToThrustOfEvent < 0')
 
     variables.addAlias('nGoodTracks', 'nParticlesInList(pi+:all)')
     variables.addAlias('netCharge', 'formula(countInList(pi+:all, charge == 1) - countInList(pi+:all, charge == -1))')

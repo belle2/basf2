e709e61bd68e8d7ef8e5d2b68cf6611a1d860ad6
Racha Cheaib <racha.cheaib@desy.de>
2018-11-20 07:47:40 +0900

    fixed dark skim

--- skim/standalone/Dark_Skim_Standalone.py
+++ skim/standalone/Dark_Skim_Standalone.py
@@ -26,48 +26,54 @@ fileList = [
     'mdst_000001_prod00002288_task00000001.root'
 ]
 
-inputMdstList('MC9', fileList)
+darkskimpath = Path()
+inputMdstList('MC9', fileList, path=darkskimpath)
 
 
-loadStdCharged()
-stdPi0s('loose')
-stdPhotons('loose')
-loadStdKS()
-loadStdLightMesons()
-loadStdSkimPi0()
-loadStdSkimPhoton()
+loadStdCharged(path=darkskimpath)
+stdPi0s('loose', path=darkskimpath)
+stdPhotons('loose', path=darkskimpath)
+loadStdKS(path=darkskimpath)
+loadStdLightMesons(path=darkskimpath)
+loadStdSkimPi0(path=darkskimpath)
+loadStdSkimPhoton(path=darkskimpath)
 
-loadStdD0()
-loadStdDplus()
-loadStdDstar0()
-loadStdDstarPlus()
-loadStdDiLeptons(True)
+loadStdD0(path=darkskimpath)
+loadStdDplus(path=darkskimpath)
+loadStdDstar0(path=darkskimpath)
+loadStdDstarPlus(path=darkskimpath)
+loadStdDiLeptons(True, path=darkskimpath)
 
-cutAndCopyList('gamma:E15', 'gamma:loose', '1.4<E<4')
+cutAndCopyList('gamma:E15', 'gamma:loose', '1.4<E<4', path=darkskimpath)
 
 
-def add_skim(label, lists):
+def add_skim(label, lists, path=darkskimpath):
     """
     create uDST skim for given lists, saving into $label.udst.root
     Particles not necessary for the given particle lists are not saved.
     """
     skimCode = encodeSkimName(label)
-    skimOutputUdst(skimCode, lists)
+    skimOutputUdst(skimCode, lists, path=darkskimpath)
     summaryOfLists(lists)
 
 
-from skim.dark import SinglePhotonDarkList
-darklist = SinglePhotonDarkList()
-add_skim('SinglePhotonDark', darklist)
+from skim.dark import ALP3GammaList
+add_skim('ALP3Gamma', ALP3GammaList(path=darkskimpath))
 
 
 from skim.dark import LFVZpInvisibleList
-add_skim('LFVZpInvisible', LFVZpInvisibleList())
+add_skim('LFVZpInvisible', LFVZpInvisibleList(path=darkskimpath))
 
 from skim.dark import LFVZpVisibleList
-add_skim('LFVZpVisible', LFVZpVisibleList())
-setSkimLogging()
-process(analysis_main)
+add_skim('LFVZpVisible', LFVZpVisibleList(path=darkskimpath))
+
+
+from skim.dark import SinglePhotonDarkList
+darklist = SinglePhotonDarkList(path=darkskimpath)
+add_skim('SinglePhotonDark', darklist, path=darkskimpath)
+
+setSkimLogging(skim_path=darkskimpath)
+process(darkskimpath)
 
 # print out the summary
 print(statistics)

31e68e3b2d53a0c7d874c4d800ac6c52404dab3c
Jan Strube <jan.strube@pnnl.gov>
2018-11-30 05:34:02 +0900

    fixing name space scope

--- top/variables/include/TOPDigitVariables.h
+++ top/variables/include/TOPDigitVariables.h
@@ -75,84 +75,85 @@ namespace Belle2 {
       // returns the expected number of photons for a given hypothesis
       double getExpectedPhotonCount(const Particle* particle, int pdg = 0);
 
+      //! @returns the number of digits in the same module as the particle
+      double topDigitCount(const Particle* particle);
+
+      //! @returns the number of digits in all other module as the particle
+      double topBackgroundDigitCount(const Particle* particle);
+
+      //! @returns the number of digits in all other module as the particle
+      double topBackgroundDigitCountRaw(const Particle* particle);
+
+      //! @returns the number of all digits regardless of hit quality in the same module as the particle
+      double topRawDigitCount(const Particle* particle);
+
+      //! @returns the largest time between to subsequent digits in the same module as the particle
+      double topDigitGapSize(const Particle* particle);
+
+      // The number of reflected digits is defined as the number of digits after the gap
+      // This method is a helper function to count the top digits after the largest gap
+      // between subsequent hits, under the constraints gap > minGap and gap < maxGap
+      double topCountPhotonsAfterLargesGapWithin(const Particle* particle, double min, double max);
+
+      //! @returns the number of reflected digits in the same module as the particle
+      double topReflectedDigitCount(const Particle* particle);
+
+      //! @returns the X coordinate of the particle entry point to the TOP in the local frame
+      double getTOPLocalX(const Particle* particle);
+
+      //! @returns the Y coordinate of the particle entry point to the TOP in the local frame
+      double getTOPLocalY(const Particle* particle);
+
+      //! @returns the Z coordinate of the particle entry point to the TOP in the local frame
+      double getTOPLocalZ(const Particle* particle);
+
+      //! @returns the local phi component of the particle's momentum in the TOP
+      double getTOPLocalPhi(const Particle* particle);
+
+      //! @returns the local theta component of the particle's momentum in the TOP
+      double getTOPLocalTheta(const Particle* particle);
+
+      //! @returns the number of photons associated with the particle likelihood
+      double getTOPPhotonCount(const Particle* particle);
+
+      //! @returns the expected number of photons for the assigned hypothesis
+      double getExpectedTOPPhotonCount(const Particle* particle, const vector<double>& vars);
+
+      //! @returns the slot ID of the TOP for the particle
+      double getTOPSlotID(const Particle* particle);
+
+      //! @returns the number of TOP photons in the given time interval
+      double countTOPHitsInInterval(const Particle* particle, const vector<double>& vars);
+
+      //! @returns the number of TOP photons in the first 20 ns
+      double countTOPHitsInFirst20ns(const Particle* particle);
+
+      //! @returns the number of raw TOP photons in the given time interval
+      double countRawTOPHitsInInterval(const Particle* particle, const vector<double>& vars);
+
+      //---------------- TOPRecBunch related --------------------
+      //! @returns whether the rec bunch is reconstructed
+      double isTOPRecBunchReconstructed([[maybe_unused]] const Particle* particle);
+
+      //! returns the bunch number. Use -9999 to indicate error
+      double TOPRecBunchNumber([[maybe_unused]] const Particle* particle);
+
+      //! returns the current offset
+      double TOPRecBunchCurrentOffset([[maybe_unused]] const Particle* particle);
+
+      //! returns the number of tracks in the TOP acceptance
+      double TOPRecBunchTrackCount([[maybe_unused]] const Particle* particle);
+
+      //! returns the number of tracks used in the bunch reconstruction
+      double TOPRecBunchUsedTrackCount([[maybe_unused]] const Particle* particle);
+
+      //-------------- Event based -----------------------------------
+      //! returns the number of photons in a given slot without cleaning
+      double TOPRawPhotonsInSlot([[maybe_unused]] const Particle* particle, const vector<double>& vars);
+
+      //! returns the number of good photons in a given slot
+      double TOPGoodPhotonsInSlot([[maybe_unused]] const Particle* particle, const vector<double>& vars);
+
     }
-    //! @returns the number of digits in the same module as the particle
-    double topDigitCount(const Particle* particle);
-
-    //! @returns the number of digits in all other module as the particle
-    double topBackgroundDigitCount(const Particle* particle);
-
-    //! @returns the number of digits in all other module as the particle
-    double topBackgroundDigitCountRaw(const Particle* particle);
-
-    //! @returns the number of all digits regardless of hit quality in the same module as the particle
-    double topRawDigitCount(const Particle* particle);
-
-    //! @returns the largest time between to subsequent digits in the same module as the particle
-    double topDigitGapSize(const Particle* particle);
-
-    // The number of reflected digits is defined as the number of digits after the gap
-    // This method is a helper function to count the top digits after the largest gap
-    // between subsequent hits, under the constraints gap > minGap and gap < maxGap
-    double topCountPhotonsAfterLargesGapWithin(const Particle* particle, double min, double max);
-
-    //! @returns the number of reflected digits in the same module as the particle
-    double topReflectedDigitCount(const Particle* particle);
-
-    //! @returns the X coordinate of the particle entry point to the TOP in the local frame
-    double getTOPLocalX(const Particle* particle);
-
-    //! @returns the Y coordinate of the particle entry point to the TOP in the local frame
-    double getTOPLocalY(const Particle* particle);
-
-    //! @returns the Z coordinate of the particle entry point to the TOP in the local frame
-    double getTOPLocalZ(const Particle* particle);
-
-    //! @returns the local phi component of the particle's momentum in the TOP
-    double getTOPLocalPhi(const Particle* particle);
-
-    //! @returns the local theta component of the particle's momentum in the TOP
-    double getTOPLocalTheta(const Particle* particle);
-
-    //! @returns the number of photons associated with the particle likelihood
-    double getTOPPhotonCount(const Particle* particle);
-
-    //! @returns the expected number of photons for the assigned hypothesis
-    double getExpectedTOPPhotonCount(const Particle* particle, const vector<double>& vars);
-
-    //! @returns the slot ID of the TOP for the particle
-    double getTOPSlotID(const Particle* particle);
-
-    //! @returns the number of TOP photons in the given time interval
-    double countTOPHitsInInterval(const Particle* particle, const vector<double>& vars);
-
-    //! @returns the number of TOP photons in the first 20 ns
-    double countTOPHitsInFirst20ns(const Particle* particle);
-
-    //! @returns the number of raw TOP photons in the given time interval
-    double countRawTOPHitsInInterval(const Particle* particle, const vector<double>& vars);
-
-    //---------------- TOPRecBunch related --------------------
-    //! @returns whether the rec bunch is reconstructed
-    double isTOPRecBunchReconstructed([[maybe_unused]] const Particle* particle);
-
-    //! returns the bunch number. Use -9999 to indicate error
-    double TOPRecBunchNumber([[maybe_unused]] const Particle* particle);
-
-    //! returns the current offset
-    double TOPRecBunchCurrentOffset([[maybe_unused]] const Particle* particle);
-
-    //! returns the number of tracks in the TOP acceptance
-    double TOPRecBunchTrackCount([[maybe_unused]] const Particle* particle);
-
-    //! returns the number of tracks used in the bunch reconstruction
-    double TOPRecBunchUsedTrackCount([[maybe_unused]] const Particle* particle);
-
-    //-------------- Event based -----------------------------------
-    //! returns the number of photons in a given slot without cleaning
-    double TOPRawPhotonsInSlot([[maybe_unused]] const Particle* particle, const vector<double>& vars);
-
-    //! returns the number of good photons in a given slot
-    double TOPGoodPhotonsInSlot([[maybe_unused]] const Particle* particle, const vector<double>& vars);
   }
 }

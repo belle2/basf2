620c64135a43b8b25a92019bac860a56e9d7634f
Giacomo De Pietro <giacomo.depietro@roma3.infn.it>
2018-12-07 02:28:51 +0900

    Added new Muid variables; added an example

---
+++ tracking/examples/MuidVariables.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+########################################################
+#
+#  This steering file shows how to correctly "load"
+#  the Muid variables and use them at analysis level.
+#
+#  In the first part of the script we generate a sample
+#  of muons and we simulate and reconstuct them.
+#  In the second part we run a (very) simple analysis
+#  and print out some variables.
+#
+#  Contact: Giacomo De Pietro (2018)
+#           giacomo.depietro@roma3.infn.it
+#
+########################################################
+
+import basf2 as b2
+import simulation as sim
+import reconstruction as rec
+import modularAnalysis as ma
+from ROOT import Belle2
+
+# Load the tracking libraries
+# Fundamental to use the Muid variables!
+from ROOT import gSystem
+gSystem.Load('libtracking.so')
+
+# Create the main path
+main = b2.create_path()
+
+#############################################
+# GENERATION, SIMULATION AND RECONSTRUCTION #
+#############################################
+
+# Set EventInfoSetter and add a progress bar
+main.add_module('EventInfoSetter',
+                expList=0,
+                runList=1,
+                evtNumList=100)
+main.add_module('Progress')
+main.add_module('ProgressBar')
+
+use_KKMC = False
+if use_KKMC:  # Use KKMC to generate generic mu+mu- events
+    main.add_module('KKGenInput',
+                    tauinputFile=Belle2.FileSystem.findFile('data/generators/kkmc/mu.input.dat'),
+                    KKdefaultFile=Belle2.FileSystem.findFile('data/generators/kkmc/KK2f_defaults.dat'),
+                    taudecaytableFile='',
+                    kkmcoutputfilename='output.kkmc.dat')
+else:  # Use ParticleGun to generate 4GeV mu+ and mu-
+    main.add_module('ParticleGun',
+                    nTracks=1,
+                    pdgCodes=[13, -13],
+                    momentumGeneration='fixed',
+                    momentumParams=[4],
+                    thetaGeneration='uniform',
+                    thetaParams=[37, 130])
+
+# Add simulation and reconstruction
+# Note that we skip the trigger simulation in this example
+sim.add_simulation(path=main)
+rec.add_reconstruction(path=main)
+
+############
+# ANALYSIS #
+############
+
+# Reconstruct the muon-candidates in the event with ECL-based cuts
+ma.fillParticleList(
+    'mu+:basic',
+    'nCDCHits > 20 and abs(dz) < 2.0 and abs(dr) < 0.5 and 0.15 < clusterE < 0.4 and formula(clusterE/p) < 0.4',
+    path=main)
+
+# Select muons requiring at least 4 hits in the KLM
+ma.cutAndCopyList('mu+:klm', 'mu+:basic', 'muidHitLayer > 3', path=main)
+
+# Saving variables to a flat ntuple
+listOfVariables = ['p',
+                   'theta',
+                   'phi',
+                   'muonID',
+                   'muonPdf',
+                   'muidOutcomeExtTrack',
+                   'muidHitLayer',
+                   'muidExtLayer',
+                   'klmClusterInnermostLayer',
+                   'klmClusterLayers']
+ma.variablesToNtuple('mu+:klm',
+                     listOfVariables,
+                     filename='MuidVariables.root',
+                     treename='muonKLM',
+                     path=main)
+
+# Process the path
+b2.process(main)
+print(b2.statistics)
--- tracking/variables/MuidVariables.cc
+++ tracking/variables/MuidVariables.cc
@@ -16,16 +16,13 @@
 #include <mdst/dataobjects/Track.h>
 #include <tracking/dataobjects/Muid.h>
 
-// ROOT include
-#include <TLorentzVector.h>
-
 using namespace std;
 
 namespace Belle2 {
   namespace Variable {
 
-//! @returns the muon pdf stored in the Muid dataobject
-    double muidPdf(const Particle* particle)
+    //! @returns the muon pdf stored in the Muid dataobject (normalized with all others)
+    double muonPdf(const Particle* particle)
     {
       const Track* track = particle->getTrack();
       if (!track)
@@ -40,9 +37,87 @@ namespace Belle2 {
       }
     }
 
+    //! @returns the pion pdf stored in the Muid dataobject (normalized with all others)
+    double pionPdf(const Particle* particle)
+    {
+      const Track* track = particle->getTrack();
+      if (!track)
+        return std::numeric_limits<float>::quiet_NaN();
+      else {
+        Muid* muid = track->getRelatedTo<Muid>();
+        if (!muid)
+          return std::numeric_limits<float>::quiet_NaN();
+        else {
+          return muid->getPionPDFValue();
+        }
+      }
+    }
 
-//! @return outcome of the extrapolation: 0=missed KLM, 1=barrel stop, 2=endcap stop, 3=barrel exit, 4=endcap exit
+    //! @returns the kaon pdf stored in the Muid dataobject (normalized with all others)
+    double kaonPdf(const Particle* particle)
+    {
+      const Track* track = particle->getTrack();
+      if (!track)
+        return std::numeric_limits<float>::quiet_NaN();
+      else {
+        Muid* muid = track->getRelatedTo<Muid>();
+        if (!muid)
+          return std::numeric_limits<float>::quiet_NaN();
+        else {
+          return muid->getKaonPDFValue();
+        }
+      }
+    }
 
+    //! @returns the electron pdf stored in the Muid dataobject (normalized with all others)
+    double electronPdf(const Particle* particle)
+    {
+      const Track* track = particle->getTrack();
+      if (!track)
+        return std::numeric_limits<float>::quiet_NaN();
+      else {
+        Muid* muid = track->getRelatedTo<Muid>();
+        if (!muid)
+          return std::numeric_limits<float>::quiet_NaN();
+        else {
+          return muid->getElectronPDFValue();
+        }
+      }
+    }
+
+    //! @returns the proton pdf stored in the Muid dataobject (normalized with all others)
+    double protonPdf(const Particle* particle)
+    {
+      const Track* track = particle->getTrack();
+      if (!track)
+        return std::numeric_limits<float>::quiet_NaN();
+      else {
+        Muid* muid = track->getRelatedTo<Muid>();
+        if (!muid)
+          return std::numeric_limits<float>::quiet_NaN();
+        else {
+          return muid->getProtonPDFValue();
+        }
+      }
+    }
+
+    //! @returns the deuteron pdf stored in the Muid dataobject (normalized with all others)
+    double deuteronPdf(const Particle* particle)
+    {
+      const Track* track = particle->getTrack();
+      if (!track)
+        return std::numeric_limits<float>::quiet_NaN();
+      else {
+        Muid* muid = track->getRelatedTo<Muid>();
+        if (!muid)
+          return std::numeric_limits<float>::quiet_NaN();
+        else {
+          return muid->getDeuteronPDFValue();
+        }
+      }
+    }
+
+    //! @return outcome of the extrapolation: 0=missed KLM, 1=barrel stop, 2=endcap stop, 3=barrel exit, 4=endcap exit
     double muidOutcomeExtTrack(const Particle* particle)
     {
       const Track* track = particle->getTrack();
@@ -58,9 +133,23 @@ namespace Belle2 {
       }
     }
 
+    //! @return outermost EKLM layer crossed by track during extrapolation
+    double muidEndcapExtLayer(const Particle* particle)
+    {
+      const Track* track = particle->getTrack();
+      if (!track)
+        return std::numeric_limits<float>::quiet_NaN();
+      else {
+        Muid* muid = track->getRelatedTo<Muid>();
+        if (!muid)
+          return std::numeric_limits<float>::quiet_NaN();
+        else {
+          return muid->getEndcapExtLayer();
+        }
+      }
+    }
 
-//! @return outermost BKLM layer crossed by track during extrapolation
-
+    //! @return outermost BKLM layer crossed by track during extrapolation
     double muidBarrelExtLayer(const Particle* particle)
     {
       const Track* track = particle->getTrack();
@@ -92,7 +181,23 @@ namespace Belle2 {
       }
     }
 
-//! @return outermost BKLM layer with a matching hit
+    //! @return outermost EKLM layer with a matching hit
+    double muidEndcapHitLayer(const Particle* particle)
+    {
+      const Track* track = particle->getTrack();
+      if (!track)
+        return std::numeric_limits<float>::quiet_NaN();
+      else {
+        Muid* muid = track->getRelatedTo<Muid>();
+        if (!muid)
+          return std::numeric_limits<float>::quiet_NaN();
+        else {
+          return muid->getEndcapHitLayer();
+        }
+      }
+    }
+
+    //! @return outermost BKLM layer with a matching hit
     double muidBarrelHitLayer(const Particle* particle)
     {
       const Track* track = particle->getTrack();
@@ -108,7 +213,7 @@ namespace Belle2 {
       }
     }
 
-//! @return outermost BKLM or EKLM layer with a matching hit
+    //! @return outermost BKLM or EKLM layer with a matching hit
     double muidHitLayer(const Particle* particle)
     {
       const Track* track = particle->getTrack();
@@ -124,7 +229,7 @@ namespace Belle2 {
       }
     }
 
-//! @return layer-crossing bit pattern during extrapolation
+    //! @return layer-crossing bit pattern during extrapolation
     double muidExtLayerPattern(const Particle* particle)
     {
       const Track* track = particle->getTrack();
@@ -140,7 +245,7 @@ namespace Belle2 {
       }
     }
 
-//! @return matching-hit bit pattern
+    //! @return matching-hit bit pattern
     double muidHitLayerPattern(const Particle* particle)
     {
       const Track* track = particle->getTrack();
@@ -156,25 +261,32 @@ namespace Belle2 {
       }
     }
 
-
-    // REGISTER_VARIABLE("basf2Name", variableNameAsAbove, "Description");
     VARIABLE_GROUP("KLM Calibration | PID");
-    REGISTER_VARIABLE("muidPdf", muidPdf , "[calibration] Returns the muon pdf stored in the Muid dataobject");
+    REGISTER_VARIABLE("muonPdf", muonPdf ,
+                      "[calibration] Returns the muon pdf stored in the Muid dataobject (normalized with all others)");
+    REGISTER_VARIABLE("pionPdf", pionPdf ,
+                      "[calibration] Returns the pion pdf stored in the Muid dataobject (normalized with all others)");
+    REGISTER_VARIABLE("kaonPdf", kaonPdf ,
+                      "[calibration] Returns the kaon pdf stored in the Muid dataobject (normalized with all others)");
+    REGISTER_VARIABLE("electronPdf", electronPdf ,
+                      "[calibration] Returns the electron pdf stored in the Muid dataobject (normalized with all others)");
+    REGISTER_VARIABLE("protonPdf", protonPdf ,
+                      "[calibration] Returns the proton pdf stored in the Muid dataobject (normalized with all others)");
+    REGISTER_VARIABLE("deuteronPdf", deuteronPdf ,
+                      "[calibration] Returns the deuteron pdf stored in the Muid dataobject (normalized with all others)");
     REGISTER_VARIABLE("muidOutcomeExtTrack", muidOutcomeExtTrack ,
                       "[calibration] Return outcome of muon extrapolation: 0=missed KLM, 1=barrel stop, 2=endcap stop, 3=barrel exit, 4=endcap exit");
+    REGISTER_VARIABLE("muidEndcapExtLayer", muidEndcapExtLayer ,
+                      "[calibration] Return outermost EKLM layer crossed by track during extrapolation");
     REGISTER_VARIABLE("muidBarrelExtLayer", muidBarrelExtLayer ,
                       "[calibration] Return outermost BKLM layer crossed by track during extrapolation");
     REGISTER_VARIABLE("muidExtLayer", muidExtLayer ,
                       "[calibration] Return outermost BKLM or EKLM layer crossed by track during extrapolation");
+    REGISTER_VARIABLE("muidEndcapHitLayer", muidEndcapHitLayer , "[calibration] Return outermost EKLM layer with a matching hit");
     REGISTER_VARIABLE("muidBarrelHitLayer", muidBarrelHitLayer , "[calibration] Return outermost BKLM layer with a matching hit");
     REGISTER_VARIABLE("muidHitLayer", muidHitLayer , "[calibration] Return outermost BKLM or EKLM layer with a matching hit");
     REGISTER_VARIABLE("muidExtLayerPattern", muidExtLayerPattern ,
                       "[calibration] Return layer-crossing bit pattern during extrapolation");
     REGISTER_VARIABLE("muidHitLayerPattern", muidHitLayerPattern , "[calibration] Return matching-hit bit pattern");
-
   }
-
-  // Create an empty module which allows basf2 to easily find the library and load it from the steering file
-  class EnableMuidVariablesModule: public Module {}; // Register this module to create a .map lookup file.
-  REG_MODULE(EnableMuidVariables);
 }

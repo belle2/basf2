5b71bef81a4a37fe0039a281a83aa6974cf4b467
Martin Ritter <martin.ritter@desy.de>
2018-12-20 15:15:14 +0100

    Variables to HDF5 prototype
    also a small utility function to convert mcErrors to named columns

---
+++ analysis/scripts/b2pandas_utils.py
@@ -0,0 +1,142 @@
+import basf2
+from ROOT import Belle2
+import tables
+import numpy as np
+import warnings
+from variables import variables as variable_manager
+from variables import std_vector
+
+"""
+Python uilities to help create or manage ntuples and their work in pandas
+"""
+
+
+class VariablesToHDF5(basf2.Module):
+    """
+    Dump variables directly to HDF5
+
+    This Module is the equivalent of VariablesToNtuple but creates an hdf5 file
+    instead of a root file. It is slower at is implemented in pure python and
+    should currently be considered a proof of concept but creates an hdf5 file
+    instead of a root file. It is slower at is implemented in pure python and
+    should currently be considered a proof of concept.
+    """
+    def __init__(self, filename, listname, variables):
+        """Constructor to initialize the internal state
+
+        Arguments:
+            filename(str): name of the hdf5 file to be created
+            listname(str): name of the particle list
+            variables(list(str)): list of variables to save for each particle
+        """
+        super().__init__()
+        #: Output filename
+        self._filename = filename
+        #: Particle list name
+        self._listname = listname
+        #: List of variables
+        self._variables = variables
+
+    def initialize(self):
+        # get variables from manager
+        varnames = variable_manager.resolveCollections(std_vector(*self._variables))
+        #: variable objects for each variable
+        self._var_objects = [variable_manager.getVariable(n) for n in varnames]
+
+        #: Event metadata
+        self._evtmeta = Belle2.PyStoreObj("EventMetaData")
+        self._evtmeta.isRequired()
+        #: Pointer to the particle list
+        self._plist = Belle2.PyStoreObj(self._listname)
+        self._plist.isRequired()
+
+        # create hdf5 file
+        self._hdf5file = tables.open_file(self._filename, mode="w", title="Belle2 Variables to HDF5")
+        if not self._hdf5file:
+            basf2.B2ERROR("Cannot create output file")
+            return
+
+        dtype = [("exp", np.int32), ("run", np.int32), ("evt", np.uint32), ("icand", np.uint32), ("ncand", np.uint32)]
+        for v in self._var_objects:
+            # only float variables for now
+            dtype.append((v.name, np.float64))
+
+        self._dtype = dtype
+        filters = tables.Filters(complevel=1, complib='blosc:lz4', fletcher32=False)
+        # some variable names are not just A-Za-z0-9 so pytables complains but
+        # seems to work. Ignore warning
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self._table = self._hdf5file.create_table("/", self._listname, obj=np.zeros(0, dtype), filters=filters)
+
+    def event(self):
+        buf = np.empty(self._plist.getListSize(), dtype=self._dtype)
+        # add some extra columns for bookkeeping
+        buf["exp"] = self._evtmeta.getExperiment()
+        buf["run"] = self._evtmeta.getRun()
+        buf["evt"] = self._evtmeta.getEvent()
+        buf["_ncand_"] = len(buf)
+        buf["_icand_"] = np.arange(len(buf))
+
+        for row, p in zip(buf, self._plist):
+            for v in self._var_objects:
+                # pyroot proxy not working with callables, we should fix this.
+                # For now we need to go back by name and call it.
+                # should be `row[v.name] = v.func(p)`
+                row[v.name] = variable_manager.evaluate(v.name, p)
+
+        self._table.append(buf)
+
+    def terminate(self):
+        self._table.flush()
+        self._hdf5file.close()
+
+
+def make_mcerrors_readable(dataframe, column="mcErrors"):
+    """
+    Take a dataframe containing an column with the output of the :b2:var:`mcErrors`
+    variable from :b2:mod:`VariablesToNTuple` and convert it to a readable set
+    of columns of the form ``{column}_{name}`` where column is the value of the
+    ``column`` argument and ``name`` is one of one of the :ref:`mcmatching`
+    error flags (without the leading 'c_').
+
+    Arguments:
+        dataframe(pandas.DataFrame): the pandas dataframe containing an ntuple
+                with column containing the output of the  mcErrors variable
+        column(str): the name containing the values from the mcErrors variable
+    """
+
+    if column not in dataframe:
+        raise KeyError(f"Cannot find coulumn '{column}'")
+
+    # convert mcErrors to int to be able to logical operate on it
+    mcErrors = dataframe[column].astype(int)
+
+    # and loop over all the c_ constants in the Belle2.MCMatching class
+    for flag in (e for e in dir(Belle2.MCMatching) if e.startswith("c_")):
+        try:
+            value = int(getattr(Belle2.MCMatching, flag))
+        except ValueError:
+            # probably the extraInfo column name, ignore
+            continue
+
+        # and set the column
+        name = column + flag[1:]
+        if value == 0:
+            dataframe[name] = mcErrors == 0
+        else:
+            dataframe[name] = (mcErrors & value) == value
+
+
+if __name__ == "__main__":
+    import modularAnalysis as ma
+
+    p = basf2.create_path()
+    p.add_module("EventInfoSetter", evtNumList=100)
+    p.add_module("EvtGenInput")
+    ma.fillParticleListsFromMC([("pi-:gen", "")], path=p)
+    a = VariablesToHDF5("test.hdf5", "pi-:gen", ["M", "E", "px", "py", "pz"])
+    p.add_module(a)
+    # Process the events
+    basf2.process(p)
+    print(basf2.statistics)

a7fd87ad76df632b8dc2452196cbee5c88d35161
Sam Cunliffe <sam.cunliffe@desy.de>
2019-01-12 13:16:24 +0100

    Skip the test on buildbot

--- analysis/tests/code-quality-cppcheck.py
+++ analysis/tests/code-quality-cppcheck.py
@@ -7,9 +7,14 @@ Eventually these checks can be included as git hooks.
 """
 
 import re
-from b2test_utils import check_error_free
+from b2test_utils import skip_test, check_error_free
 
 if __name__ == "__main__":
+
+    # skip this test if being run on the buildbot
+    if os.getlogin() is 'b2soft':
+        skip_test('no need to run cppcheck test on the buildbot')
+
     # ignore the nofile .. [missingInclude] that is always at the end of cppcheck
     ignoreme = 'Cppcheck cannot find all the include files'
     check_error_free("b2code-cppcheck", "cppcheck", "analysis",

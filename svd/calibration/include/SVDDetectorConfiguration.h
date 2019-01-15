8bb8725efd161468b71458ae7debe15bd90c47ec
Laura Zani <laura.zani@pi.infn.it>
2019-01-15 20:59:03 +0900

    created the basic wrapper class for importing the global and local SVD configuration parameters as payloads, and also the global configuration xml file. Currently ONLY methods for getting the local config parameters from the wrapper have been implemented.

---
+++ svd/calibration/include/SVDDetectorConfiguration.h
@@ -0,0 +1,146 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2017 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Laura Zani                                               *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ * WARNING: Do not try to fry it with water. Use only olive oil.          *
+ **************************************************************************/
+
+#pragma once
+
+#include <vxd/dataobjects/VxdID.h>
+#include <svd/dbobjects/SVDCalibrationsBase.h>
+#include <svd/dbobjects/SVDCalibrationsVector.h>
+#include <svd/dbobjects/SVDCalibrationsScalar.h>
+#include <svd/dbobjects/SVDLocalConfigParameters.h>
+#include <svd/dbobjects/SVDGlocalConfigParameters.h>
+#include <framework/database/DBObjPtr.h>
+#include <framework/logging/Logger.h>
+#include <string>
+
+
+#include <math.h>
+
+namespace Belle2 {
+
+  /** This class defines the dbobject and the methods to access the
+   * parameters to configure SVD for data taking with:
+   * - a std string storing the complete xml file configuring SVD for global runs
+   * - float zero suppression value (global run)
+   * - int latency (global run)
+   * - string: date and time time of the calibration run
+   * - float injected charge (local cal run)
+   * - time units (local cal run)
+   * - masking bitmap
+   *
+   */
+  class SVDDetectorConfiguration {
+  public:
+
+    static std::string svdLocalConfig_name;
+    static std::string svdGlobalConfig_name;
+    static std::string xml_name;
+    typedef SVDCalibrationsBase< SVDLocalConfigParameters > t_svdLocalConfig_payload;
+    typedef SVDCalibrationsBase< SVDGlobalConfigParameters > t_svdGlobalConfig_payload;
+
+    typedef SVDCalibrationsBase< std::string> > t_xml_payload;
+
+
+    /** Constructor, no input argument is required */
+    SVDDetectorConfiguration()
+      : m_svdLocalConfig_aDBObjPtr(svdLocalConfig_name)
+      , m_svdGlobalConfig_aDBObjPtr(svdGlobalConfig_name)
+      , m_xml_aDBObjPtr(xml_name)
+      , m_xml_aDBObjPtr(date_name)
+    {
+      m_svdGlobalConfig_aDBObjPtr.addCallback([ this ](const std::string&) -> void {
+        B2INFO("SVDDetectorConfiguration, global run parameters: from now on we are using " <<
+        this->m_svdGlobalConfig_aDBObjPtr -> get_uniqueID()); });
+
+      m_svdLobalConfig_aDBObjPtr.addCallback([ this ](const std::string&) -> void {
+        B2INFO("SVDDetectorConfiguration, local run parameters: from now on we are using " <<
+        this->m_svdLocalConfig_aDBObjPtr -> get_uniqueID()); });
+    }
+
+    /** Return the charge injected on each strip for the pulse
+     *  shape calibration during the calibration local runs
+     *
+     * Input:
+     * no input parameters are required since it is a detector based payload
+     *
+     * Output: float corresponding to the injected charge [e]
+     */
+    float getInjectedCharged()
+    {
+      return m_svdLocalConfig_aDBObjPtr->get(0, 0, 0, 0, 0).injectedCharge;
+    }
+
+    /** Return the time units retireved form the accelerator RF
+     *
+     * Input:
+     * no input parameters are required since it is a detector based payload
+     *
+     * Output: float corresponding to the time units [ns]
+     */
+    float getTimeUnits()
+    {
+      return m_svdLocalConfig_aDBObjPtr->get(0, 0, 0, 0, 0).timeUnits;
+    }
+
+    /** Return the masking bitmap applied to mask strips at FADC level (only taken into account for cm-section)
+     *
+     * Input:
+     * no input parameters are required since it is a detector based payload
+     *
+     * Output: int corresponding to the filtering mask applied to the field <masks>
+     */
+    float getMaskFilter()
+    {
+      return m_svdLocalConfig_aDBObjPtr->get(0, 0, 0, 0, 0).maskFilter;
+    }
+
+
+    /** Return the time stamp (date, hour) of the calibration
+     *
+     * Input:
+     * no input parameters are required since it is a detector based payload
+     *
+     * Output: string corresponding to the date_hour of when the calibration has been taken
+     */
+    float getCalibDate()
+    {
+      return m_svdLocalConfig_aDBObjPtr->get(0, 0, 0, 0, 0).calibDate;
+    }
+
+
+    /** returns the unique ID of the payload */
+    TString getUniqueID() { return m_svdLocalConfig_aDBObjPtr->get_uniqueID(); }
+
+    /** returns true if the m_aDBObtPtr is valid in the requested IoV */
+    bool isValid()
+    {
+      return m_svdLocalConfig_aDBObjPtr.isValid();
+    }
+
+    /** returns the unique ID of the payload */
+    TString getUniqueID() { return m_svdGlobalConfig_aDBObjPtr->get_uniqueID(); }
+
+    /** returns true if the m_aDBObtPtr is valid in the requested IoV */
+    bool isValid()
+    {
+      return m_svdGlobalConfig_aDBObjPtr.isValid();
+    }
+
+
+  private:
+    DBObjPtr< t_svdLocalConfig_payload > m_svdLocalConfig_aDBObjPtr;
+    DBObjPtr< t_svdGlobalConfig_payload > m_svdGlobalConfig_aDBObjPtr;
+    DBObjPtr< t_xml_payload > m_xml_aDBObjPtr;
+
+
+  };
+}
+
---
+++ svd/calibration/src/SVDDetectorConfiguration.cc
@@ -0,0 +1,3 @@
+#include <svd/calibration/SVDDetectorConfiguration.h>
+
+std::string Belle2::SVDDetectorConfiguration::calAmp_name = std::string("SVDDetectorConfiguration");

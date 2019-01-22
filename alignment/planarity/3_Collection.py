5b19afa9e3a3166acc1f74adbd423f855c388955
Jakub Kandra <jakub.kandra@desy.de>
2019-01-22 21:18:11 +0900

    Extension for determination planar deformation

---
+++ alignment/planarity/3_Collection.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from basf2 import *
+import ROOT
+from ROOT import Belle2
+
+
+class MomentumSetup(Module):
+
+    def __init__(self):
+        """ init """
+        super(MomentumSetup, self).__init__()
+
+    def event(self):
+        """ Setup momentum before fitting """
+
+        recoTracks = Belle2.PyStoreArray('RecoTracks')
+
+        for track in recoTracks:
+
+            definedMagMomentum = 10.0
+            momentum = track.getMomentumSeed()
+            magnitudeMomentum = momentum.Mag()
+            newMomentumX = (momentum.Px() * definedMagMomentum) / magnitudeMomentum
+            newMomentumY = (momentum.Py() * definedMagMomentum) / magnitudeMomentum
+            newMomentumZ = (momentum.Pz() * definedMagMomentum) / magnitudeMomentum
+            newMomentum = ROOT.TVector3(newMomentumX, newMomentumY, newMomentumZ)
+            track.setPositionAndMomentum(track.getPositionSeed(), newMomentum)
+
+
+class MomentumPrint(Module):
+
+    def __init__(self):
+        """ init """
+        super(MomentumPrint, self).__init__()
+
+    def event(self):
+        """ Setup momentum before fitting """
+
+        recoTracks = Belle2.PyStoreArray('RecoTracks')
+
+        for track in recoTracks:
+
+            momentum = track.getMomentumSeed().Mag()
+            print('Momentum of RecoTrack is', momentum)
+
+
+main = create_path()
+# main.add_module('SeqRootInput')
+main.add_module('RootInput')
+main.add_module('Gearbox')
+
+main.add_module('Geometry', components=['PXD', 'SVD'], excludedComponents=['MagneticField'],
+                # useDB=False
+                )
+
+MomentumSetup = MomentumSetup()
+main.add_module(MomentumSetup)
+
+# MomentumPrint = MomentumPrint()
+# main.add_module(MomentumPrint)
+
+main.add_module('SetupGenfitExtrapolation', noiseBetheBloch=False, noiseCoulomb=False, noiseBrems=False)
+main.add_module('HistoManager', histoFileName=Belle2.Environment.Instance().getOutputFileOverride())
+main.add_module('MillepedeCollector',
+                minPValue=-1.0,
+                components=['VXDAlignment'],
+                tracks=['RecoTracks'],
+                particles=[],
+                vertices=[],
+                primaryVertices=[],
+                calibrateVertex=False,
+                useGblTree=True)
+
+main.add_module('ProgressBar')
+# main.add_module('Progress')
+print_path(main)
+
+process(main)
+print(statistics)
--- alignment/reconstruction/src/AlignableSVDRecoHit2D.cc
+++ alignment/reconstruction/src/AlignableSVDRecoHit2D.cc
@@ -16,6 +16,8 @@
 
 #include <alignment/GlobalDerivatives.h>
 
+#include <svd/geometry/SensorInfo.h>
+#include <vxd/geometry/GeoCache.h>
 
 using namespace std;
 using namespace Belle2;
@@ -23,7 +25,45 @@ using namespace alignment;
 
 std::pair<std::vector<int>, TMatrixD> AlignableSVDRecoHit2D::globalDerivatives(const genfit::StateOnPlane* sop)
 {
-  auto globals = GlobalCalibrationManager::getInstance().getAlignmentHierarchy().getGlobalDerivatives<VXDAlignment>(getPlaneId(),
-                 sop);
-  return GlobalDerivatives(globals);
+  auto alignment = GlobalCalibrationManager::getInstance().getAlignmentHierarchy().getGlobalDerivatives<VXDAlignment>(getPlaneId(),
+                   sop);
+
+  auto globals = GlobalDerivatives(alignment);
+
+  bool applyDeformation(true); // To determine planar deformation
+
+  if (applyDeformation and getSensorID() == VxdID("4.3.2")) { // Choose testing SVD sensor
+
+    const SVD::SensorInfo& geometry = dynamic_cast<const SVD::SensorInfo&>(VXD::GeoCache::get(getSensorID()));
+
+    auto L1 = [](double x) {return x;};
+    auto L2 = [](double x) {return (3 * pow(x, 2) - 1) / 2;};
+    auto L3 = [](double x) {return (5 * pow(x, 3) - 3 * x) / 2;};
+    auto L4 = [](double x) {return (35 * pow(x, 4) - 30 * pow(x, 2) + 3) / 8;};
+
+    double du_dw = sop->getState()[1];       // slope in U direction
+    double dv_dw = sop->getState()[2];       // slope in V direction
+    double u = getU();                       // U coordinate of hit
+    double v = getV();                       // V coordinate of hit
+    double width = geometry.getWidth(v);     // Width of sensor (U side)
+    double length = geometry.getLength();    // Length of sensor (V side)
+    u = u * 2 / width;                       // Legendre parametrization required U in (-1, 1)
+    v = v * 2 / length;                      // Legendre parametrization required V in (-1, 1)
+
+    globals.add(31, std::vector<double> {L2(u)*du_dw,       L2(u)*dv_dw});
+    globals.add(32, std::vector<double> {L1(u)*L1(v)*du_dw, L1(u)*L1(v)*dv_dw});
+    globals.add(33, std::vector<double> {L2(v)*du_dw,       L2(v)*dv_dw});
+
+    globals.add(41, std::vector<double> {L3(u)*du_dw,       L3(u)*dv_dw});
+    globals.add(42, std::vector<double> {L2(u)*L1(v)*du_dw, L2(u)*L1(v)*dv_dw});
+    globals.add(43, std::vector<double> {L1(u)*L2(v)*du_dw, L1(u)*L2(v)*dv_dw});
+    globals.add(44, std::vector<double> {L3(v)*du_dw,       L3(v)*dv_dw});
+
+    globals.add(51, std::vector<double> {L4(u)*du_dw,       L4(u)*dv_dw});
+    globals.add(52, std::vector<double> {L3(u)*L1(v)*du_dw, L3(u)*L1(v)*dv_dw});
+    globals.add(53, std::vector<double> {L2(u)*L2(v)*du_dw, L2(u)*L2(v)*dv_dw});
+    globals.add(54, std::vector<double> {L1(u)*L3(v)*du_dw, L1(u)*L3(v)*dv_dw});
+    globals.add(55, std::vector<double> {L4(v)*du_dw,       L4(v)*dv_dw});
+  }
+  return globals;
 }

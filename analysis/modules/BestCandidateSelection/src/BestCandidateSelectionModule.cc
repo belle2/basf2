64821d2183e9ed8d494b436796c2c264f0e6f63b
Martin Ritter <martin.ritter@desy.de>
2019-01-22 14:05:04 +0100

    Update the documentation of BestCandidateSelection
    That was a bit outdated. Hopefully it will be more clear now

--- analysis/modules/BestCandidateSelection/src/BestCandidateSelectionModule.cc
+++ analysis/modules/BestCandidateSelection/src/BestCandidateSelectionModule.cc
@@ -30,17 +30,43 @@ BestCandidateSelectionModule::BestCandidateSelectionModule():
   m_variable(nullptr)
 {
   //the "undefined order" bit is not strictly true in the current implementation, but details (with anti-particle lists) are tricky
-  setDescription("Selects Particles with the highest values of 'variable' in the input list and removes all other particles from the list. Particles will receive an extra-info field '${variable}_rank' containing their rank as an integer starting at 1 (best). The ranking also takes antiparticles into account, so there will only be one B+- candidate with rank=1. Candidates with same value of 'variable' will have different ranks, with undefined order. The remaining list is sorted from best to worst candidate (each charge, e.g. B+/B-, separately).");
+  setDescription(R"DOC(Sort particles by the value of a given ``variable``
+in the input list and optionally remove particles after the nth position
+
+Particles will receive an extra-info field containing their rank as an integer
+starting at 1 (best). The name of this extra-info field defaults to
+``${variable}_rank`` but can be chosen freely using the ``outputVariable``
+parameter.
+
+The ranking also takes antiparticles into account, so there will only be one
+B+- candidate with `rank=1`.  The remaining list is sorted from best to worst
+candidate (each charge, e.g. B+/B-, separately). The sorting is guaranteed
+to be stable between particle and anti particle list: particles with the same
+value for ``variable`` will keep their relative order. That is, a particle "A"
+which was before another particle "B" in the same list and has the same value
+for ``variable`` will also stay before "B" after sorting.
+
+If ``allowMultiRank=False`` (the default) candidates with same value of
+``variable`` will have different ranks. If ``allowMultiRank=True`` they will
+share the same rank.
+
+IF ``numBest>0`` only candidates with this rank or better will remain in the
+output list. If ``allowMultiRank=True`` that means that there can be more than
+``numBest`` candidates in the output list if they share ranks.
+)DOC");
+
   setPropertyFlags(c_ParallelProcessingCertified);
 
-  addParam("particleList", m_inputListName, "Name of the ParticleList to reduce to the best candidates");
-  addParam("variable", m_variableName, "Variable which defines the candidate ranking (see selectLowest for ordering)");
-  addParam("selectLowest", m_selectLowest, "Candidate with lower value are better (default: higher is better))", false);
-  addParam("allowMultiRank", m_allowMultiRank, "Candidates with identical values get identical rank", false);
-  addParam("numBest", m_numBest, "Keep this many of the best candidates (0: keep all)", 0);
+  addParam("particleList", m_inputListName, "Name of the ParticleList to rank for best candidate");
+  addParam("variable", m_variableName, "Variable which defines the candidate ranking (see ``selectLowest`` for ordering)");
+  addParam("selectLowest", m_selectLowest, "If true, candidate with lower values of ``variable`` are better, otherwise higher is better", false);
+  addParam("allowMultiRank", m_allowMultiRank, "If true, candidates with identical values get identical rank", false);
+  addParam("numBest", m_numBest, "Keep only particles with this rank or better. If ``allowMultiRank=False`` this is "
+           "identical to the maximum amount of candidates left in the list. Otherwise there may be more candidates if "
+           "some share the same rank (0: keep all)", 0);
   addParam("cut", m_cutParameter, "Only candidates passing the cut will be ranked. The others will have rank -1.", std::string(""));
   addParam("outputVariable", m_outputVariableName,
-           "Name for created variable, which contains the rank for the particle. If not provided, the standard name '${variable}_rank' is used.");
+           "Name for created variable, which contains the rank for the particle. If not provided, the standard name ``${variable}_rank`` is used.");
 
 }
 

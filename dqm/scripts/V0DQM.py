92f523ca6dc9d08843ec244fd4b697320a6e75ad
Bryan Fulsom <bryan.fulsom@desy.de>
2019-01-25 12:27:27 +0900

    Removed pT cut

--- dqm/scripts/V0DQM.py
+++ dqm/scripts/V0DQM.py
@@ -10,7 +10,7 @@ from modularAnalysis import *
 
 
 def add_V0_dqm(path):
-    fillParticleList("pi+:V0DQM", 'pt>0.05', False, path)
+    fillParticleList("pi+:V0DQM", '', False, path)
     reconstructDecay('K_S0:V0DQM -> pi-:V0DQM pi+:V0DQM', '', 1, True, path)
     vertexKFit('K_S0:V0DQM', 0.001, '', '', path, True)
 

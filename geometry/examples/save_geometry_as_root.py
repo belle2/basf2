2525d1e08351df11af69135f2c0dbf3ac02f7dd9
Martin Ritter <martin.ritter@desy.de>
2019-01-28 10:16:20 +0100

    geometry examples: document problems with ECL

--- geometry/examples/save_geometry_as_root.py
+++ geometry/examples/save_geometry_as_root.py
@@ -5,6 +5,10 @@
 # This steering file creates the Belle II detector
 # geometry, and saves it as a Root file.
 #
+# This currently doesn't work for the ECL as the ECL uses custom volume types
+# which cannot be converted to ROOT so for this to work we need to create a
+# geometry without the ECL.
+#
 # Example steering file - 2011 Belle II Collaboration
 ######################################################
 
@@ -15,8 +19,10 @@ main = Path()
 main.add_module("EventInfoSetter", expList=[0], runList=[0], evtNumList=[1])
 # Geometry parameter loader
 main.add_module('Gearbox')
-# Geometry builder
-main.add_module('Geometry')
+# Geometry builder, ignore database and exclude the ECL as we cannot exclude
+# things when loading Geometry from the database. Alternatively one could
+# create a database geometry configuration without the ecl.
+main.add_module('Geometry', useDB=False, excludedComponents=['ECL', 'Cryostat'])
 # Saves the geometry as a Root file
 main.add_module('ExportGeometry', Filename='Belle2Geo.root')
 # Process one event

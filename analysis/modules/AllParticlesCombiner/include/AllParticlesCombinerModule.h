d792b7b1f2630c5d00d6c718ef5888d5f7d9d9dd
Frank Meier <frank.meier@sydney.edu.au>
2019-01-30 14:12:35 +0900

    first version of new module combining all particles of a given input ParticleList

---
+++ analysis/modules/AllParticlesCombiner/include/AllParticlesCombinerModule.h
@@ -0,0 +1,60 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2019 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Frank Meier                                              *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+#include <framework/core/Module.h>
+
+#include <analysis/dataobjects/ParticleList.h>
+#include <analysis/DecayDescriptor/DecayDescriptor.h>
+#include <analysis/VariableManager/Utility.h>
+
+#include <framework/datastore/StoreObjPtr.h>
+
+namespace Belle2 {
+
+  /**
+   * This module combines all particles of the provided list to one mother particle.
+   *
+   */
+  class AllParticlesCombinerModule : public Module {
+
+  public:
+
+    /**
+    * Constructor: Sets the description, the properties and the parameters of the module.
+    */
+    AllParticlesCombinerModule();
+
+    /** Register input and output data */
+    virtual void initialize() override;
+
+    /** process event */
+    virtual void event() override;
+
+
+  private:
+
+    std::string m_inputListName;  /**< List of particles which are supposed to be combined */
+    StoreObjPtr<ParticleList> m_inputList;  /**< input particle list */
+    std::vector<int> m_indices;  /**< Indices of the particles that are combined */
+
+    bool m_isSelfConjugatedParticle;  /**< flag that indicates whether an anti-particle mother does not exist and should not be reconstructed as well*/
+
+    std::string m_cutString;  /**< Selection criteria */
+    std::unique_ptr<Variable::Cut> m_cut;  /**< cut object which performs the cuts */
+    bool m_writeOut;  /**< If true, the output ParticleList will be saved by RootOutput. If false, it will be ignored when writing the file. */
+
+    std::string m_outputListName;  /**< Name of the output list created by the combination of all particles in the input list. */
+    std::string m_antiListName;  /**< output anti-particle list name */
+    DecayDescriptor m_decaydescriptor;  /**< Decay descriptor of the decay being reconstructed */
+    int m_pdgCode;  /**< PDG code of the combined mother particle */
+  };
+}
---
+++ analysis/modules/AllParticlesCombiner/src/AllParticlesCombinerModule.cc
@@ -0,0 +1,111 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2019 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Frank Meier                                              *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <analysis/modules/AllParticlesCombiner/AllParticlesCombinerModule.h>
+#include <analysis/dataobjects/Particle.h>
+#include <analysis/DecayDescriptor/ParticleListName.h>
+#include <framework/datastore/StoreArray.h>
+
+using namespace Belle2;
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(AllParticlesCombiner)
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+AllParticlesCombinerModule::AllParticlesCombinerModule() : Module()
+{
+  // Set module properties
+  setDescription(R"DOC("This module combines all particles of the provided list to one mother particle.
+  )DOC");
+
+  // Parameter definitions
+  addParam("inputListName", m_inputListName, "List of particles which are supposed to be combined", std::string(""));
+  addParam("cut", m_cutString, "Selection criteria", std::string(""));
+  addParam("writeOut", m_writeOut,
+           "If true, the output ParticleList will be saved by RootOutput. If false, it will be ignored when writing the file.", false);
+  addParam("outputListName", m_outputListName,
+           "Name of the output list created by the combination of all particles in the input list.", std::string(""));
+
+}
+
+void AllParticlesCombinerModule::initialize()
+{
+  StoreArray<Particle>().isRequired();
+  m_inputList.isRequired(m_inputListName);
+
+  bool valid = m_decaydescriptor.init(m_outputListName);
+  if (!valid)
+    B2ERROR("Invalid output ListName: " << m_outputListName);
+
+  // Mother particle
+  const DecayDescriptorParticle* mother = m_decaydescriptor.getMother();
+
+  m_pdgCode = mother->getPDGCode();
+  m_antiListName = ParticleListName::antiParticleListName(mother->getFullName());
+  m_isSelfConjugatedParticle = (m_outputListName == m_antiListName);
+
+  StoreObjPtr<ParticleList> particleList(m_outputListName);
+  DataStore::EStoreFlags flags = m_writeOut ? DataStore::c_WriteOut : DataStore::c_DontWriteOut;
+  particleList.registerInDataStore(flags);
+  if (!m_isSelfConjugatedParticle) {
+    StoreObjPtr<ParticleList> antiParticleList(m_antiListName);
+    antiParticleList.registerInDataStore(flags);
+  }
+
+  m_cut = Variable::Cut::compile(m_cutString);
+}
+
+void AllParticlesCombinerModule::event()
+{
+  StoreArray<Particle> particles;
+
+  StoreObjPtr<ParticleList> outputList(m_outputListName);
+  outputList.create();
+  outputList->initialize(m_pdgCode, m_outputListName);
+
+  if (!m_isSelfConjugatedParticle) {
+    StoreObjPtr<ParticleList> outputAntiList(m_antiListName);
+    outputAntiList.create();
+    outputAntiList->initialize(-1 * m_pdgCode, m_antiListName);
+
+    outputList->bindAntiParticleList(*(outputAntiList));
+  }
+
+  StoreObjPtr<ParticleList> plist(m_inputListName);
+
+  double px = 0;
+  double py = 0;
+  double pz = 0;
+  double E = 0;
+  for (unsigned int i = 0; i < plist->getListSize(); ++i) {
+    Particle* particle = plist->getParticle(i, true);
+    m_indices[i] = particle->getArrayIndex();
+    px += particle->getPx();
+    py += particle->getPy();
+    pz += particle->getPz();
+    E += particle->getEnergy();
+  }
+  const TLorentzVector vec(px, py, pz, E);
+
+  Particle combinedParticle = Particle(vec, m_pdgCode, m_isSelfConjugatedParticle ? Particle::c_Unflavored : Particle::c_Flavored,
+                                       m_indices);//, m_particleArray.getPtr());
+
+  Particle* newParticle = particles.appendNew(combinedParticle);
+  if (m_cut->check(newParticle)) {
+    outputList->addParticle(newParticle);
+  }
+  // int iparticle = particles.getEntries() - 1;
+  // outputList->addParticle(iparticle, combinedParticle.getPDGCode(), combinedParticle.getFlavorType());
+}

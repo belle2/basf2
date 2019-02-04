fb8691aa584a62e09610431d33c2147d481ad6df
Peter Kodys <peter.kodys@mff.cuni.cz>
2019-02-04 05:10:08 +0100

    PXDDQM-BII-4498-fix-dqm-for-phase-3-early-geometry

--- vxd/examples/DQMVXD_Phase3Early.py
+++ vxd/examples/DQMVXD_Phase3Early.py
@@ -14,6 +14,13 @@ from basf2 import *
 from simulation import add_simulation
 from reconstruction import add_reconstruction
 from L1trigger import add_tsim
+from daqdqm.commondqm import add_common_dqm
+# import PXDDAQDQM
+
+import PXDROIUnpackerModule
+# import PXDHardwareClusterDQM
+# import PXDHardwareClusterDQMModule
+
 import glob
 
 # background (collision) files
@@ -54,6 +61,32 @@ add_simulation(main, bkgfiles=bg)
 # trigger simulation
 add_tsim(main)
 
+# PXD digitization module
+# main.add_module('PXDDigitizer')
+# Convert digits to raw pxd data
+PXDPACKER = register_module('PXDPacker')
+# ============================================================================
+# [[dhhc1, dhh1, dhh2, dhh3, dhh4, dhh5] [ ... ]]
+# -1 is disable port
+PXDPACKER.param('dhe_to_dhc', [
+    [0, 2, 4, 34, 36, 38],
+    [1, 6, 8, 40, 42, 44],
+    [2, 10, 12, 46, 48, 50],
+    [3, 14, 16, 52, 54, 56],
+    [4, 3, 5, 35, 37, 39],
+    [5, 7, 9, 41, 43, 45],
+    [6, 11, 13, 47, 49, 51],
+    [7, 15, 17, 53, 55, 57],
+])
+main.add_module(PXDPACKER)
+# Convert digits from raw pxd data
+main.add_module('PXDUnpacker')
+'''Unpack ROIs from ONSEN output'''
+main.add_module(PXDROIUnpackerModule.PXDRawROIUnpackerModule())
+'''Unpack ROIs from HLT Payload (depends if there are in the sroot file)'''
+# main.add_module(PXDROIUnpackerModule.PXDPayloadROIUnpackerModule())
+
+
 # reconstruction
 add_reconstruction(main)
 
@@ -61,6 +94,16 @@ add_reconstruction(main)
 # main.add_module('DqmHistoManager', Port=7777)
 main.add_module('HistoManager', histoFileName='Histos_DQMVXD_Phase3Early.root')
 # main.add_module('HistoManager', histoFileName='Histos_DQMTracks_BelleII.root')
+
+main.add_module('PXDDAQDQM', histogramDirectoryName='PXDDAQ')
+main.add_module('PXDDQMClusters', histogramDirectoryName='PXDCls')
+main.add_module('PXDDQMCorr', histgramDirectoryName='PXDCor')
+main.add_module('PXDDQMEfficiency', histogramDirectoryName='PXDEff')
+# main.add_module('PXDHardwareClusterDQM', histgramDirectoryName='PXDHCl')
+main.add_module('PXDRawDQMChips', histogramDirectoryName='PXDRCh')
+main.add_module('PXDRawDQMCorr', histgramDirectoryName='PXDRCo')
+main.add_module('PXDRawDQM', histgramDirectoryName='PXDRaw')
+main.add_module('PXDROIDQM', histgramDirectoryName='PXDROI')
 
 pxddqmExpReco = register_module('PXDDQMExpressReco')
 svddqmExpReco = register_module('SVDDQMExpressReco')

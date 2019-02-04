edd9bc645902230d2fa55acae082bd90ccad088f
Peter Kodys <peter.kodys@mff.cuni.cz>
2019-02-04 17:33:22 +0100

    DQM-BII-4498-fix-dqm-for-phase-3-early-geometry_3

--- vxd/examples/DQMVXD_BelleII.py
+++ vxd/examples/DQMVXD_BelleII.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 #############################################################
-# Simple steering file to demonstrate how to run:
-#   PXD, SVD, VXD and Track DQM on BelleII or Phase2 geometry
+# Simple steering file to demonstrate how to run DQM on BelleII geometry
+# Valid for Phase 2, Phase 3 Early and Phase 3 regular as well as for testbeams
+#   PXD, SVD, VXD and Track DQM
 #   for ExpressReco on-line use Min modules without analysis
 #   Base on module from Martin Ritter:
 #               tracking/examples/DQMTracking_Phase2.py
@@ -15,45 +16,56 @@ from simulation import add_simulation
 from reconstruction import add_reconstruction
 from L1trigger import add_tsim
 from daqdqm.commondqm import add_common_dqm
-# import PXDDAQDQM
 
 import PXDROIUnpackerModule
-# import PXDHardwareClusterDQM
-# import PXDHardwareClusterDQMModule
 
 import glob
 
+import argparse
+parser = argparse.ArgumentParser(
+    description="VXD DQM Belle II for Phase 2 (Exp=1), Phase 3 Early (Exp=2) and Phase 3 regular (Exp=3), show all possible histos")
+parser.add_argument('--experiment-type', dest='ExperimentType', action='store',
+                    default=2, type=int,
+                    help='Set which experiment you want: 1 (Phase 2), 2 (Phase 3 Early) or 3 (Phase 3 regular), default = 2')
+
+args = parser.parse_args()
+
+print("Final setting of arguments: ")
+print("                 ExperimentType: ", args.ExperimentType)
+
 # background (collision) files
 # bg = glob.glob('./BG/*.root')
-# on KEKCC: (choose one of the sets)
-# bg = /group/belle2/BGFile/OfficialBKG/15thCampaign/phase2/set*/*.root
-# bg = /group/belle2/BGFile/OfficialBKG/15thCampaign/phase3/set*/*.root
 bg = None
 
 # number of events to generate, can be overriden with -n
 num_events = 100
 # output filename, can be overriden with -o
-output_filename = "RootOutput_Phase3Early.root"
+output_filename = "RootOutput.root"
+if (args.ExperimentType is 1):
+    output_filename = "RootOutput_Phase2.root"
+if (args.ExperimentType is 2):
+    output_filename = "RootOutput_Phase3Early.root"
+if (args.ExperimentType is 3):
+    output_filename = "RootOutput_Phase3.root"
 
 # create path
 main = create_path()
 
-# specify number of events to be generated
-# main.add_module('EventInfoSetter', evtNumList=num_events)
-# the experiment number for phase2 MC has to be 1002, otherwise the wrong payloads (for VXDTF2 the SectorMap) are loaded
-# main.add_module("EventInfoSetter", expList=1002, runList=1, evtNumList=num_events)
-main.add_module("EventInfoSetter", evtNumList=num_events)
+if (args.ExperimentType is 1):
+    # the experiment number for phase2 MC has to be 1002, otherwise the wrong payloads (for VXDTF2 the SectorMap) are loaded
+    main.add_module("EventInfoSetter", expList=1002, runList=1, evtNumList=num_events)
+if (args.ExperimentType is 2):
+    # the experiment number for early phase3 MC has to be 1003, otherwise the wrong payloads for this faze are loaded
+    main.add_module("EventInfoSetter", expList=1003, runList=1, evtNumList=num_events)
+if (args.ExperimentType is 3):
+    # the experiment number for regular phase3 MC has no need to set, it is default
+    main.add_module("EventInfoSetter", evtNumList=num_events)
 
 # in case you need to fix seed of random numbers
-# set_random_seed('some fixed value')
 # set_random_seed('d33fa68eab781f3dcb069fb23425885fcd92d3432e6433a14894e5d7bba34272')
 
 # generate BBbar events
 main.add_module('EvtGenInput')
-
-main.add_module("Gearbox", fileName='/geometry/Belle2_earlyPhase3.xml')
-main.add_module("Geometry", useDB=False)
-
 
 # detector simulation
 add_simulation(main, bkgfiles=bg)
@@ -86,14 +98,19 @@ main.add_module(PXDROIUnpackerModule.PXDRawROIUnpackerModule())
 '''Unpack ROIs from HLT Payload (depends if there are in the sroot file)'''
 # main.add_module(PXDROIUnpackerModule.PXDPayloadROIUnpackerModule())
 
-
 # reconstruction
 add_reconstruction(main)
 
 # histomanager: use DqmHistoManager for in-line monitoring, or HistoManager for offline training
 # main.add_module('DqmHistoManager', Port=7777)
-main.add_module('HistoManager', histoFileName='Histos_DQMVXD_Phase3Early.root')
-# main.add_module('HistoManager', histoFileName='Histos_DQMTracks_BelleII.root')
+Histos_filename = "Histos_DQMVXD.root"
+if (args.ExperimentType is 1):
+    Histos_filename = "Histos_DQMVXD_Phase2.root"
+if (args.ExperimentType is 2):
+    Histos_filename = "Histos_DQMVXD_Phase3Early.root"
+if (args.ExperimentType is 3):
+    Histos_filename = "Histos_DQMVXD_Phase3.root"
+main.add_module('HistoManager', histoFileName=Histos_filename)
 
 main.add_module('PXDDAQDQM', histogramDirectoryName='PXDDAQ')
 main.add_module('PXDDQMClusters', histogramDirectoryName='PXDCls')

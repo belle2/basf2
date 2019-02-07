29d34e4cb65bb7b7c88973c7cec12be2b6fd4f04
luka <luka.santelj@desy.de>
2019-02-07 18:45:44 +0900

    Update of old email address in validation script.

--- arich/validation/ARICHValidate.C
+++ arich/validation/ARICHValidate.C
@@ -26,7 +26,7 @@ void ARICHValidate(){
   ch->Draw("(logL.pi-logL.K)>>hall","(abs(PDG)==321 || abs(PDG)==211) && sqrt((mcHit.x - recHit.x)**2 +(mcHit.y - recHit.y)**2)<1 && mcHit.p>3 && status>1000");
 
   hall->GetListOfFunctions()->Add(new TNamed("Description", "Difference of ARICHLikelihood value for K and #pi hypothesis, for K and #pi tracks with 3.0 - 3.5 GeV (particle gun from the IP). TrackFinderMCTruth is used for track matching."));
-  hall->GetListOfFunctions()->Add(new TNamed("Contact","luka.santelj@kek.jp"));
+  hall->GetListOfFunctions()->Add(new TNamed("Contact","luka.santelj@ijs.si"));
   hall->GetListOfFunctions()->Add(new TNamed("Check", "Well separated K and #pi peaks."));
 
   hall->Write();
@@ -34,7 +34,7 @@ void ARICHValidate(){
   TH1F* heff = new TH1F("heff","K id. efficiency vs. #pi missid. probability;fake;efficiency",100,0.005,0.05);
   
   heff->GetListOfFunctions()->Add(new TNamed("Description", "K identification efficiency vs. #pi missidentification probability, for K and #pi tracks with 3.0 - 3.5 GeV. TrackFinderMCTruth is used for track matching."));
-  heff->GetListOfFunctions()->Add(new TNamed("Contact","luka.santelj@kek.jp"));
+  heff->GetListOfFunctions()->Add(new TNamed("Contact","luka.santelj@ijs.si"));
   heff->GetListOfFunctions()->Add(new TNamed("Check", "Continuous, >0.98 eff. for > 0.02 fake. NOTE: as this is not a usual histogram p-value calculation is not proper so it will often apear RED. If other two plots are OK please don't mind."));
   
   int startbin = hpi->FindFirstBinAbove(0.);
@@ -65,7 +65,7 @@ void ARICHValidate(){
   ch->Draw("detPhot.pi>>hnphot","abs(PDG)==211 && sqrt((mcHit.x - recHit.x)**2 +(mcHit.y - recHit.y)**2)<1 && mcHit.p>3 && status>1000");
   
   hnphot->GetListOfFunctions()->Add(new TNamed("Description", "Number of detected photons in a 3#sigma band around the expected Cherenkov angle, for #pi with momenta 3.0-3.5 GeV."));
-  hnphot->GetListOfFunctions()->Add(new TNamed("Contact","luka.santelj@kek.jp"));
+  hnphot->GetListOfFunctions()->Add(new TNamed("Contact","luka.santelj@ijs.si"));
   hnphot->GetListOfFunctions()->Add(new TNamed("Check", "Poissonian peak with average at ~11.5."));
   
   hnphot->Write();

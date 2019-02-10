2b08cd816f87fe7d89560d9beb6fff4507b3e172
Nils Braun <nils.braun@kit.edu>
2019-02-10 20:13:28 +0900

    Did the same with the tests

---
+++ hlt/tests/test_phase3_hlt_cosmics_dqm_only.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from softwaretrigger.path_functions import add_hlt_dqm
+from softwaretrigger.test_support import create_test_path, finalize_test_path
+
+path, tempfolder = create_test_path(runtype="cosmics", expNum=1003, location="hlt")
+
+add_hlt_dqm(path, run_type="cosmics", standalone=True)
+
+finalize_test_path(path, tempfolder, has_softwaretriggerresult=False)
--- hlt/tests/test_phase3_hlt_cosmics_reco_monitor.py
+++ hlt/tests/test_phase3_hlt_cosmics_reco_monitor.py
@@ -3,12 +3,9 @@
 from softwaretrigger.path_functions import add_hlt_processing
 from softwaretrigger.test_support import create_test_path, finalize_test_path
 
-path, tempfolder = create_test_path(runtype="cosmics", expNum=1003)
+path, tempfolder = create_test_path(runtype="cosmics", expNum=1003, location="hlt")
 
-add_hlt_processing(
-    path,
-    run_type="cosmics",
-    softwaretrigger_mode="monitoring",
-    data_taking_period="phase2")
+add_hlt_processing(path, run_type="cosmics", softwaretrigger_mode="monitoring",
+                   data_taking_period="phase3")
 
 finalize_test_path(path, tempfolder, has_softwaretriggerresult=False)

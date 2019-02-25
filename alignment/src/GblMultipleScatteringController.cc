b60d49969f2369c187d7fb77718601fb4db077b9
bilkat <tadeas.bilka@desy.de>
2019-02-25 18:56:44 +0900

    Remove commented code

--- alignment/src/GblMultipleScatteringController.cc
+++ alignment/src/GblMultipleScatteringController.cc
@@ -11,16 +11,12 @@
 #include <alignment/GblMultipleScatteringController.h>
 #include <genfit/GblFitter.h>
 
-//#include <framework/logging/Logger.h>
-
 using namespace std;
 using namespace Belle2;
 
 void GblMultipleScatteringController::controlTrackSegment(TVector3, TVector3, double scatTheta, genfit::GblFitter* fitter)
 {
   bool enableScattering = true;
-  //TVector3 pos = 0.5 * (entry + exit);
-  //B2INFO("scatTheta = " << scatTheta << " [rad] @ [x,y,z] = [ " << pos[0] << ", " << pos[1] << ", " << pos[2] << "] [cm]");
 
   if (scatTheta < 0.0001) enableScattering = false;
 

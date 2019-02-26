64fd4590fb89f0c7f741e68ff5177160ed021367
Marcel Hohmann <mhohmann@student.unimelb.edu.au>
2019-02-26 19:13:28 +0900

    adding training script

---
+++ analysis/scripts/trainCurlTaggerClassifier.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*-  coding: utf-8 -*-
+# Author: Marcel Hohmann (marcel.hohmann@desy.de)
+
+import basf2.core
+from modularAnalysis import inputMdst, tagCurlTracks, process, statistics, register_module
+from stdCharged import stdPi
+import sys
+import os
+
+try:
+    input_file_name = str(sys.argv[1])
+except BaseException:
+    input_file_name = '/hsm/belle2/bdata/MC/release-02-00-01/DB00000411/MC11/prod00005678/'\
+        's00/e0000/4S/r00000/mixed/mdst/sub00/mdst_00000*_prod00005678_task0000000*.root'
+
+upload = False  # upload to conditions database
+remove_local_files = False  # delete local db and training data
+tag_name = 'development'
+
+# names used by the CurlTagger module:
+training_file_name = 'CurlTagger_TrainingData_BelleII.root'
+identifier = 'CurlTagger_FastBDT_BelleII'
+
+# --- create training data set and train the classifier ---
+training_path = basf2.core.Path()
+
+inputMdst('default', input_file_name, path=training_path)
+stdPi('all', path=training_path)
+tagCurlTracks('pi+:all', train=True, selectorType='mva', path=training_path)
+
+progress = register_module('ProgressBar')
+training_path.add_module(progress)
+
+process(training_path, int(2e5))
+print(statistics)
+
+
+here = os.getcwd()
+data_base_file = here + "/localdb/database.txt"
+
+# upload to global database
+if upload:
+    os.system("conditionsdb upload {TAGNAME} {DATABASEFILE}".format(TAGNAME=tag_name, DATABASEFILE=data_base_file))
+
+if remove_local_files:
+    os.system('rm -r {}'.format(here + '/localdb/'))
+    os.system('rm {}/{}'.format(here, training_file_name))

5525baa915b6ea98bdd5639d6a896d05118614c3
Ryosuke Itoh <ryosuke.itoh@kek.jp>
2019-03-01 08:58:12 +0900

    mfile2rb added. : Read event SROOT file and put first n events in memory, and then place them in Ringbuffer in a random order.

---
+++ daq/rfarm/event/tools/mfile2rb.cc
@@ -0,0 +1,84 @@
+//+
+// File : file2rb.cc
+// Description : Get an event from a SeqRoot file and place it in Rbuf
+//
+// Author : Ryosuke Itoh, IPNS, KEK
+// Date : 28 - Apr - 2012
+//-
+#include <string>
+#include <vector>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "framework/pcore/SeqFile.h"
+#include "framework/pcore/RingBuffer.h"
+
+#define RBUFSIZE 100000000
+#define MAXEVTSIZE  400000000
+#define MAXEVT 1000
+
+using namespace Belle2;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  if (argc < 3) {
+    printf("file2rb : rbufname filename nevt\n");
+    exit(-1);
+  }
+
+  SeqFile* file = new SeqFile(argv[2], "r");
+  if (file->status() <= 0) {
+    perror("file open");
+    exit(-1);
+  }
+
+  int nevt = atoi(argv[3]);
+
+  //  RingBuffer* rbuf = new RingBuffer(argv[1], RBUFSIZE);
+  RingBuffer* rbuf = new RingBuffer(argv[1]);
+  rbuf->dump_db();
+
+  // Skip the first record (StreamerInfo)
+  char* dumbuf = new char[MAXEVTSIZE];
+  int is = file->read(dumbuf, MAXEVTSIZE);
+  if (is <= 0) exit(-1);
+  delete dumbuf;
+
+  char* evbuf[MAXEVT];
+  // Create event buffers
+  for (int i = 0; i < nevt; i++) {
+    evbuf[i] = new char[MAXEVTSIZE];
+    int is = file->read(evbuf[i], MAXEVTSIZE);
+    if (is <= 0) {
+      perror("read file");
+      exit(-1);
+    }
+    if (is > MAXEVTSIZE) {
+      printf("Event size too large : %d\n", is);
+      exit(-1);
+    }
+  }
+
+  int totevt = 0;
+  for (;;) {
+    int bufno = std::rand() % nevt;
+
+    // Put the message in ring buffer
+    int irb = 0;
+    for (;;) {
+      irb = rbuf->insq((int*)evbuf[bufno], (is - 1) / 4 + 1);
+      if (irb >= 0) break;
+      //      usleep(100);
+      usleep(20);
+    }
+    totevt++;
+    if (totevt % 1000 == 0)
+      printf("mfile2rb : event = %d (bufno = %d)\n", totevt, bufno);
+  }
+}
+
+
+

5ddc551ba8e26077aa418e2d6bf3ddc460853339
Nils Braun <nils.braun@kit.edu>
2019-03-03 21:58:35 +0900

    Use the new executor in the hlt and expressreco scripts

---
+++ hlt/operation/phase3/global/expressreco/evp_scripts/run.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+import subprocess
+
+from hlt.clean_execution import CleanBasf2Execution
+from argparse import ArgumentParser
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('script_name', type=str,
+                        help='Which script to execute')
+
+    args, remaining = parser.parse_known_args()
+    execution = CleanBasf2Execution(["basf2", "--no-stats", args.script_name, "--"] + remaining)
+    try:
+        execution.run()
+    finally:
+        subprocess.check_call("clear_basf2_ipc")

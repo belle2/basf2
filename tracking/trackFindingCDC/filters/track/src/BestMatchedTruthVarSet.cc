7e5a89bb2013ac448ebcf95fcbafe11c3d4d5907
Michael Eliachevitch <michael.eliachevitch@kit.edu>
2019-03-05 12:48:10 +0100

    Set the copyright boxes in files created by me correctly

--- tracking/trackFindingCDC/filters/track/src/BestMatchedTruthVarSet.cc
+++ tracking/trackFindingCDC/filters/track/src/BestMatchedTruthVarSet.cc
@@ -1,9 +1,9 @@
 /**************************************************************************
  * BASF2 (Belle Analysis Framework 2)                                     *
- * Copyright(C) 2015 - Belle II Collaboration                             *
+ * Copyright(C) 2019 - Belle II Collaboration                             *
  *                                                                        *
  * Author: The Belle II Collaboration                                     *
- * Contributors: Nils Braun, Michael Eliachevitch                         *
+ * Contributors: Michael Eliachevitch, Nils Braun                         *
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/

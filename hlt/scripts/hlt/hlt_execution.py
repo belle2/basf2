6d3f683e84a7fb4175c72ac641e1ecc692b47349
Nils Braun <nils.braun@desy.de>
2019-03-05 11:09:04 +0900

    Removed the single run scripts in favor of one-script-per-python-file as suggested by @itoh -san

---
+++ hlt/scripts/hlt/hlt_execution.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+import subprocess
+import sys
+
+from hlt.clean_execution import CleanBasf2Execution
+
+
+def main(script_name):
+    """Shortcut method to do the basf2 execution on expressreco and hlt"""
+    execution = CleanBasf2Execution(["basf2", "--no-stats", script_name, "--"] + sys.argv)
+    try:
+        execution.run()
+    finally:
+        subprocess.run("framework-pcore-clear_ipcs")

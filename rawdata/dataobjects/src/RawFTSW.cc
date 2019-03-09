42803e93ec7582533e992f81ffcb626c02c73080
Satoru YAMADA <satoru.yamada@kek.jp>
2019-03-09 15:58:19 +0900

    Doubly allocating m_access in SetVersion() of RawFTSW.cc is fixed.

--- rawdata/dataobjects/src/RawFTSW.cc
+++ rawdata/dataobjects/src/RawFTSW.cc
@@ -33,7 +33,8 @@ void RawFTSW::SetVersion()
     delete m_access;
   }
 
-
+  //
+  // Assign unpacker
   //
   // Special treatment is required because Nakao-san did not set verision number in ver.0,1, and 2 header.
   // I'm using the size of header which is stored the 1st word (0,1,..)
@@ -48,7 +49,7 @@ void RawFTSW::SetVersion()
   } else if (m_buffer[ POS_HEADER_SIZE ] == VER_0_HEADER_SIZE) {
     char err_buf[500];
     sprintf(err_buf,
-            "[FATAL] Ver.0 of RawFTSW( so-called early DESYtest format is detected but not supported. (header size = 0x%.8x ) Exiting...\n %s %s %d\n",
+            "[FATAL] Ver.0 of RawFTSW( so-called early DESYtest format ) is detected but not supported. (header size = 0x%.8x ) Exiting...\n %s %s %d\n",
             m_buffer[ POS_HEADER_SIZE ], __FILE__, __PRETTY_FUNCTION__, __LINE__);
     printf("%s", err_buf); fflush(stdout);
     string err_str = err_buf; throw (err_str);
@@ -69,26 +70,6 @@ void RawFTSW::SetVersion()
     string err_str = err_buf; throw (err_str);
   }
 
-  //
-  // Assign unpacker
-  //
-  switch (m_version) {
-    case LATEST_FTSW_FORMAT_VER :
-      m_access = new RawFTSWFormat_latest;
-      break;
-    case 1 : // it is 0x20 as of 2019/02/18
-      m_access = new RawFTSWFormat_v1;
-      break;
-    default : {
-      char err_buf[500];
-      sprintf(err_buf,
-              "[FATAL] ERROR_EVENT : Invalid version of FTSW data format (= data says it is 0x%.8x now). Exiting...\n %s %s %d\n",
-              m_version, __FILE__, __PRETTY_FUNCTION__, __LINE__);
-      printf("%s", err_buf); fflush(stdout);
-      string err_str = err_buf; throw (err_str);
-    }
-    exit(1);
-  }
   m_access->SetBuffer(m_buffer, m_nwords, 0, m_num_events, m_num_nodes);
 }
 

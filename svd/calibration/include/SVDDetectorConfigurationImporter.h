e6f66efbd10c276fd03859e3577d8ee8399a970f
Laura Zani <laura.zani@pi.infn.it>
2019-03-14 00:33:59 +0900

    Added the SVDDetectorConfigurationImporter class. Function to import Global and Local configuration payloads from xml have been defined but still empty.

---
+++ svd/calibration/include/SVDDetectorConfigurationImporter.h
@@ -0,0 +1,103 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2017 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Laura Zani                                               *
+ *                                                                       *
+ * This software is provided "as is" without any warranty.                *
+ * WARNING: Do not try to fry it with water. Use only olive oil.          *
+ **************************************************************************/
+
+#pragma once
+
+#include <TObject.h>
+
+namespace Belle2 {
+
+  /**
+   * This class import to the database the dbobjects
+   * SVDGlobalConfigParameters and SVDLocalConfigParameters.
+   *
+   *
+   */
+  class SVDDetectorConfigurationImporter {
+
+  public:
+
+    /**
+     * Default constructor
+     */
+    SVDDetectorConfigurationImporter():
+      m_firstExperiment(0), m_firstRun(0),
+      m_lastExperiment(-1), m_lastRun(-1)
+    {
+    }
+
+    /**
+     * A constructor accepting as arguments the interval of
+     * validity coordinates.
+     */
+    SVDDetectorConfigurationImporter(int fexp, int frun, int lexp, int lrun):
+      m_firstExperiment(fexp), m_firstRun(frun),
+      m_lastExperiment(lexp), m_lastRun(lrun)
+    {
+    }
+
+
+    /**
+     * Destructor
+     */
+    virtual ~SVDDetectorConfigurationImporter()
+    {}
+
+    /**
+     * This method import to the database the global configuration parameters
+     * used during data taking
+     * @param xmlFileName is the file name of the xml file
+     * @param errorTollerant if true bypasses sanity checks
+     */
+
+    void importSVDGlobalConfigParametersFromXML(const std::string& xmlfileName, bool errorTollerant = false);
+
+    /**
+     * This method import to the database the local configuration parameters
+     * used during data taking
+     * @param xmlFileName is the file name of the xml file
+     * @param errorTollerant if true bypasses sanity checks
+     */
+
+    void importSVDLocalConfigParametersFromXML(const std::string& xmlfileName, bool errorTollerant = false);
+
+    /**
+     * This method import to the database the strip
+     * noise from the noise local runs.
+     * @param condDbname is the name of the payload in the condition database
+     * @param xmlFileName is the file name of the xml file
+     * @param xmlTag is the tag in the xml file that contains the actual calibrations
+     * @param default is the value assigned to the calibration constant by default
+     * @param errorTollerant if true bypasses sanity checks
+     */
+
+    template< class SVDcalibration >
+    void importSVDCalibrationsFromXML(const std::string& condDbname,
+                                      const std::string& xmlFileName,
+                                      const std::string& xmlTag,
+                                      typename SVDcalibration::t_perSideContainer::calibrationType defaultValue,
+                                      bool errorTollerant);
+
+
+  private:
+
+    /**
+     * The interval of validity coordinates are defined as
+     * private members.
+     */
+    int m_firstExperiment; /**< First experiment. */
+    int m_firstRun; /**< First run. */
+    int m_lastExperiment; /**< Last experiment */
+    int m_lastRun; /**< Last run. */
+    ClassDef(SVDDetectorConfigurationImporter, 1);
+  };
+
+} // Belle2 namespace

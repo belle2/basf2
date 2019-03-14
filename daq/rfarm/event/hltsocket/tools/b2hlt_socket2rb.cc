b57883597636783957dc5076e2f17f8dbe4f9725
Nils Braun <nils.braun@desy.de>
2019-03-15 04:13:24 +0900

    Implemented the suggestions by @ritter

--- daq/rfarm/event/hltsocket/tools/b2hlt_socket2rb.cc
+++ daq/rfarm/event/hltsocket/tools/b2hlt_socket2rb.cc
@@ -97,8 +97,6 @@ int main(int argc, char* argv[])
       size = socket.get_wordbuf(buffer, MAXEVTSIZE);
     } else {
       size = socket.get(reinterpret_cast<char*>(buffer), MAXEVTSIZE);
-      // We want to have it in words, not bytes
-      size = ((size - 1) / sizeof(int) + 1);
     }
     // Error checking socket
     if (size == 0) {
@@ -114,8 +112,11 @@ int main(int argc, char* argv[])
     }
     B2ASSERT("Size is negative! This should be handled above. Not good!", size > 0);
 
-    // Terminate messages make us terminate
     if (not raw) {
+      // We want to have it in words, not bytes
+      size = ((size - 1) / sizeof(int) + 1);
+
+      // Terminate messages make us terminate
       EvtMessage message(reinterpret_cast<char*>(buffer));
       if (message.type() == MSG_TERMINATE) {
         B2RESULT("Having received terminate message");
@@ -124,7 +125,7 @@ int main(int argc, char* argv[])
     }
 
     // Monitoring
-    flow.log(size * 4);
+    flow.log(size * sizeof(int));
 
     // Write to ring buffer
     const int returnValue = mainLoop.writeToRingBufferWaiting(ringBuffer, buffer, size);

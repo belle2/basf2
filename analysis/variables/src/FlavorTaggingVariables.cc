c8ef5e03ff25994a984b2ef8a871c7cff1d44b81
ABUDINEN Fernando <fernando.abudinen@ts.infn.it>
2019-03-18 03:58:42 +0900

    Removed code repetition in qrCombined and isRelatedRestOfEventB0Flavor, and in the majority MC flavor variables. This potentialle fully resolves Jira BII-4712.

--- analysis/variables/src/FlavorTaggingVariables.cc
+++ analysis/variables/src/FlavorTaggingVariables.cc
@@ -377,14 +377,7 @@ namespace Belle2 {
         const Particle* Bcp = roe->getRelated<Particle>();
         const MCParticle* BcpMC = roe->getRelated<Particle>()->getRelatedTo<MCParticle>();
 
-        int MCMatchingError = MCMatching::getMCErrors(Bcp, BcpMC);
-
-        MCMatchingError &= (~MCMatching::c_MissFSR);
-        MCMatchingError &= (~MCMatching::c_MissPHOTOS);
-        MCMatchingError &= (~MCMatching::c_MissingResonance);
-        MCMatchingError &= (~MCMatching::c_MissGamma);
-
-        if (MCMatchingError == MCMatching::c_Correct) {
+        if (Variable::isSignal(Bcp) > 0) {
           const MCParticle* Y4S = BcpMC->getMother();
           if (Y4S != nullptr) {
             for (auto& iTrack : roe->getTracks()) {
@@ -416,46 +409,11 @@ namespace Belle2 {
     {
       StoreObjPtr<RestOfEvent> roe("RestOfEvent");
 
-      float BtagFlavor = 0;
-      float BcpFlavor = 0;
-
       if (roe.isValid()) {
         const Particle* Bcp = roe->getRelated<Particle>();
-        const MCParticle* BcpMC = roe->getRelated<Particle>()->getRelatedTo<MCParticle>();
+        return Variable::isRelatedRestOfEventB0Flavor(Bcp);
+      } else return 0;
 
-        int MCMatchingError = MCMatching::getMCErrors(Bcp, BcpMC);
-
-        MCMatchingError &= (~MCMatching::c_MissFSR);
-        MCMatchingError &= (~MCMatching::c_MissPHOTOS);
-        MCMatchingError &= (~MCMatching::c_MissingResonance);
-        MCMatchingError &= (~MCMatching::c_MissGamma);
-
-        if (MCMatchingError == MCMatching::c_Correct) {
-          const MCParticle* Y4S = BcpMC->getMother();
-          if (Y4S != nullptr) {
-            for (auto& iTrack : roe->getTracks()) {
-              const MCParticle* mcParticle = iTrack->getRelated<MCParticle>();
-              while (mcParticle != nullptr) {
-                if (mcParticle->getMother() == Y4S) {
-                  if (mcParticle == BcpMC) {
-                    if (mcParticle -> getPDG() > 0) BcpFlavor = 2;
-                    else BcpFlavor = -2;
-                  } else if (BtagFlavor == 0) {
-                    if (TMath::Abs(mcParticle -> getPDG()) == 511 || TMath::Abs(mcParticle -> getPDG()) == 521) {
-                      if (mcParticle -> getPDG() > 0) BtagFlavor = 1;
-                      else BtagFlavor = -1;
-                    } else BtagFlavor = 5;
-                  }
-                  break;
-                }
-                mcParticle = mcParticle->getMother();
-              }
-              if (BcpFlavor != 0 || BtagFlavor == 5) break;
-            }
-          }
-        }
-      }
-      return (BcpFlavor != 0) ? BcpFlavor : BtagFlavor;
     }
 
     double ancestorHasWhichFlavor(const Particle* particle)
@@ -567,59 +525,10 @@ namespace Belle2 {
     double isRestOfEventMajorityB0Flavor(const Particle*)
     {
       StoreObjPtr<RestOfEvent> roe("RestOfEvent");
-      float q_MC = 0; //Flavor of B
+
       if (roe.isValid()) {
-        if (roe-> getNTracks() != 0) {
-          for (auto& track : roe->getTracks()) {
-            const MCParticle* mcParticle = track->getRelated<MCParticle>();
-            while (mcParticle != nullptr) {
-              if (mcParticle->getPDG() == 511) {
-                q_MC++;
-                break;
-              }
-              if (mcParticle->getPDG() == -511) {
-                q_MC--;
-                break;
-              }
-              mcParticle = mcParticle->getMother();
-            }
-          }
-        } else if (roe-> getNECLClusters() != 0) {
-          for (auto& cluster : roe-> getECLClusters()) {
-            const MCParticle* mcParticle = cluster->getRelated<MCParticle>();
-            while (mcParticle != nullptr) {
-              if (mcParticle->getPDG() == 511) {
-                q_MC++;
-                break;
-              }
-              if (mcParticle->getPDG() == -511) {
-                q_MC--;
-                break;
-              }
-              mcParticle = mcParticle->getMother();
-            }
-          }
-        } else if (roe-> getNKLMClusters() != 0) {
-          for (auto& klmcluster : roe-> getKLMClusters()) {
-            const MCParticle* mcParticle = klmcluster->getRelated<MCParticle>();
-            while (mcParticle != nullptr) {
-              if (mcParticle->getPDG() == 511) {
-                q_MC++;
-                break;
-              }
-              if (mcParticle->getPDG() == -511) {
-                q_MC--;
-                break;
-              }
-              mcParticle = mcParticle->getMother();
-            }
-          }
-        }
-      }
-      if (q_MC > 0) {
-        return 1;
-      } else if (q_MC < 0) {
-        return 0;
+        const Particle* Bcp = roe->getRelated<Particle>();
+        return Variable::isRelatedRestOfEventMajorityB0Flavor(Bcp);
       } else return -2;//gRandom->Uniform(0, 1);
     }
 

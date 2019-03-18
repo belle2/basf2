3ce15004972160b6f604dc5087edd5b72e4ea185
Sviatoslav Bilokin <sviatoslav.bilokin@desy.de>
2019-03-18 14:38:53 +0100

    Correcting the mess

--- analysis/tests/utilities/TestParticleFactory.h
+++ analysis/tests/utilities/TestParticleFactory.h
@@ -1,3 +1,13 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2019 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors:  Sviatoslav Bilokin                                      *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
 #pragma once
 #include <framework/datastore/StoreArray.h>
 #include <analysis/dataobjects/Particle.h>
@@ -13,13 +23,11 @@
 
 
 #include <string>
-using namespace std;
-using namespace Belle2;
 namespace TestUtilities {
   /**
    * This is a class, which generates DataStore particles, according to
    * the provided decay string e.g. "^K_S0 -> ^pi+ ^pi-".
-   * All particles are added to StoreArray<Particle>
+   * All particles are added to Belle2::StoreArray<Belle2::Particle>
    * It is primarily used in the ROE tests.
    */
   class TestParticleFactory {
@@ -36,16 +44,16 @@ namespace TestUtilities {
      * The decay string can have any complexity and all PDG codes of allowed
      * and charges  will be respected.
      */
-    const Particle* produceParticle(const string& decayString, const TLorentzVector& momentum, const TVector3& vertex)
+    const Belle2::Particle* produceParticle(const std::string& decayString, const TLorentzVector& momentum, const TVector3& vertex)
     {
-      StoreArray<Particle> myParticles;
-      DecayDescriptor* decaydescriptor = new DecayDescriptor();
+      Belle2::StoreArray<Belle2::Particle> myParticles;
+      Belle2::DecayDescriptor* decaydescriptor = new Belle2::DecayDescriptor();
       bool isString = decaydescriptor->init(decayString);
       if (!isString) {
         B2INFO("Decay string is not defined: " << decayString);
         return nullptr;
       }
-      vector<string> strNames = decaydescriptor->getSelectionNames();
+      std::vector<std::string> strNames = decaydescriptor->getSelectionNames();
       for (auto& name : strNames) {
         B2INFO("Creation of particle: " << name);
       }
@@ -58,38 +66,39 @@ namespace TestUtilities {
     /**
      * Helper method to get EParticleType from PDG code
      */
-    Particle::EParticleType getType(const DecayDescriptorParticle* particleDescription)
+    Belle2::Particle::EParticleType getType(const Belle2::DecayDescriptorParticle* particleDescription)
     {
       int pdg = abs(particleDescription->getPDGCode());
       // Emmm, I still do not know how to make it in an elegant way
       if (pdg == 211 || pdg == 11 || pdg == 321 || pdg == 13 || pdg == 2212) {
-        return Particle::EParticleType::c_Track;
+        return Belle2::Particle::EParticleType::c_Track;
       }
       if (pdg == 22) {
-        return Particle::EParticleType::c_ECLCluster;
+        return Belle2::Particle::EParticleType::c_ECLCluster;
       }
-      return Particle::EParticleType::c_Composite;
+      return Belle2::Particle::EParticleType::c_Composite;
     }
 
     /**
      * This method is used for recursion.
      */
-    const Particle* createParticle(const DecayDescriptor* particleDescriptor, const TLorentzVector& momentum, const TVector3& vertex)
+    const Belle2::Particle* createParticle(const Belle2::DecayDescriptor* particleDescriptor, const TLorentzVector& momentum,
+                                           const TVector3& vertex)
     {
-      Particle::EParticleType type = getType(particleDescriptor->getMother());
-      if (type == Particle::EParticleType::c_Track) {
+      Belle2::Particle::EParticleType type = getType(particleDescriptor->getMother());
+      if (type == Belle2::Particle::EParticleType::c_Track) {
         return createCharged(particleDescriptor, momentum, vertex);
       }
-      if (type == Particle::EParticleType::c_ECLCluster) {
+      if (type == Belle2::Particle::EParticleType::c_ECLCluster) {
         return createPhoton(momentum);
       }
-      StoreArray<Particle> myParticles;
+      Belle2::StoreArray<Belle2::Particle> myParticles;
       //Create composite particle:
       auto* motherDescriptor = particleDescriptor->getMother();
       B2INFO("Mother PDG: " << motherDescriptor->getPDGCode() << " selected: " << motherDescriptor->isSelected() << " name: " <<
              motherDescriptor->getNameSimple());
       unsigned int nDaughters = particleDescriptor->getNDaughters();
-      Particle mother(momentum, motherDescriptor->getPDGCode());
+      Belle2::Particle mother(momentum, motherDescriptor->getPDGCode());
       for (unsigned int i = 0; i < nDaughters; i++) {
         auto* daughter = particleDescriptor->getDaughter(i)->getMother();
         B2INFO("\tDaughter PDG: " << daughter->getPDGCode() << " selected: " << daughter->isSelected() << " name: " <<
@@ -104,23 +113,23 @@ namespace TestUtilities {
     /**
      * Creates different photons for tests
      * */
-    const Particle* createPhoton(const TLorentzVector& momentum)
+    const Belle2::Particle* createPhoton(const TLorentzVector& momentum)
     {
-      StoreArray<ECLCluster> myECLClusters;
-      StoreArray<Particle> myParticles;
-      ECLCluster myECL;
+      Belle2::StoreArray<Belle2::ECLCluster> myECLClusters;
+      Belle2::StoreArray<Belle2::Particle> myParticles;
+      Belle2::ECLCluster myECL;
       myECL.setIsTrack(false);
       //TRandom3 generator;
       float eclREC = momentum[3];
       myECL.setConnectedRegionId(m_photonIndex++);
       myECL.setEnergy(eclREC);
-      myECL.setHypothesis(ECLCluster::EHypothesisBit::c_nPhotons);
-      //This is necessary to avoid isCopyOf == true for ECLClusters:
+      myECL.setHypothesis(Belle2::ECLCluster::EHypothesisBit::c_nPhotons);
+      //This is necessary to avoid isCopyOf == true for Belle2::ECLClusters:
       myECL.setClusterId(m_photonIndex++);
-      ECLCluster* savedECL = myECLClusters.appendNew(myECL);
+      Belle2::ECLCluster* savedECL = myECLClusters.appendNew(myECL);
 
-      Particle p(savedECL);
-      Particle* part = myParticles.appendNew(p);
+      Belle2::Particle p(savedECL);
+      Belle2::Particle* part = myParticles.appendNew(p);
       B2INFO("\tParticle PDG: " << part->getPDGCode() << " charge: " << part->getCharge()  << " momentum: " <<
              part->getMomentumMagnitude() << " index: " << part->getArrayIndex() << " eclindex: " << part->getECLCluster()->getArrayIndex()
              << " theta: " << part->getECLCluster()->getTheta());
@@ -131,7 +140,8 @@ namespace TestUtilities {
     /**
      * Creates different charged particles for tests
      * */
-    const Particle* createCharged(const DecayDescriptor* particleDescriptor,  const TLorentzVector& momentum, const TVector3& vertex)
+    const Belle2::Particle* createCharged(const Belle2::DecayDescriptor* particleDescriptor,  const TLorentzVector& momentum,
+                                          const TVector3& vertex)
     {
       auto* particleDescription = particleDescriptor->getMother();
       TVector3 tmomentum(momentum[0], momentum[1], momentum[2]);
@@ -141,22 +151,23 @@ namespace TestUtilities {
       int chargefactor = (particleDescription->getPDGCode() == 11 || particleDescription->getPDGCode() == 13) ? -1 : 1;
       const int charge = (particleDescription->getPDGCode()) / abs(particleDescription->getPDGCode()) * chargefactor;
       unsigned long long int CDCValue = static_cast<unsigned long long int>(0x300000000000000);
-      StoreArray<TrackFitResult> myTrackFits;
-      StoreArray<Track> myTracks;
-      myTrackFits.appendNew(vertex, tmomentum, cov6, charge, Const::ChargedStable(abs(particleDescription->getPDGCode())), pValue, bField,
+      Belle2::StoreArray<Belle2::TrackFitResult> myTrackFits;
+      Belle2::StoreArray<Belle2::Track> myTracks;
+      myTrackFits.appendNew(vertex, tmomentum, cov6, charge, Belle2::Const::ChargedStable(abs(particleDescription->getPDGCode())), pValue,
+                            bField,
                             CDCValue, 16777215);
-      Track mytrack;
-      StoreArray<Particle> myParticles;
-      mytrack.setTrackFitResultIndex(Const::ChargedStable(abs(particleDescription->getPDGCode())), myTrackFits.getEntries() - 1);
-      Track* savedTrack = myTracks.appendNew(mytrack);
-      Particle* part = myParticles.appendNew(savedTrack, Const::ChargedStable(abs(particleDescription->getPDGCode())));
+      Belle2::Track mytrack;
+      Belle2::StoreArray<Belle2::Particle> myParticles;
+      mytrack.setTrackFitResultIndex(Belle2::Const::ChargedStable(abs(particleDescription->getPDGCode())), myTrackFits.getEntries() - 1);
+      Belle2::Track* savedTrack = myTracks.appendNew(mytrack);
+      Belle2::Particle* part = myParticles.appendNew(savedTrack, Belle2::Const::ChargedStable(abs(particleDescription->getPDGCode())));
       B2INFO("\tParticle PDG: " << part->getPDGCode() << " charge: " << part->getCharge()  << " charge: " << charge << " momentum: " <<
              part->getMomentumMagnitude() << " index: " << part->getArrayIndex());
       return part;
     }
 
   private:
-    // Used to differentiate photons for one another
+    // Used to differentiate photons from one another
     int m_photonIndex;
   };
 };

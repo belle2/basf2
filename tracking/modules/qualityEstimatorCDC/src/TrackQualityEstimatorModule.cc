0b80b19152aa9f84b28795d014b2d66a5c285028
Michael Eliachevitch <michael.eliachevitch@kit.edu>
2019-03-19 10:06:42 +0100

    Fix wrong include after renaming dir in prev commit 73fd1739968

--- tracking/modules/qualityEstimatorCDC/src/TrackQualityEstimatorModule.cc
+++ tracking/modules/qualityEstimatorCDC/src/TrackQualityEstimatorModule.cc
@@ -7,7 +7,7 @@
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/
-#include <tracking/modules/TrackQualityEstimator/TrackQualityEstimatorModule.h>
+#include <tracking/modules/qualityEstimatorCDC/TrackQualityEstimatorModule.h>
 #include <tracking/trackFindingCDC/eventdata/tracks/CDCTrack.h>
 
 using namespace Belle2;

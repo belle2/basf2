cb65971c89387991c2a12d7a3668eedc9227bf9e
Nils Braun <nils.braun@desy.de>
2019-03-20 10:54:49 +0900

    Do not change the current dir

--- hlt/scripts/hlt/hlt_execution.py
+++ hlt/scripts/hlt/hlt_execution.py
@@ -8,7 +8,7 @@ from hlt.clean_execution import CleanBasf2Execution
 
 def main(script_name):
     """Shortcut method to do the basf2 execution on expressreco and hlt"""
-    os.chdir(sys.path[0])
+    # os.chdir(sys.path[0])
     execution = CleanBasf2Execution()
     try:
         execution.start(["basf2", "--no-stats", script_name, "--"] + sys.argv[1:])

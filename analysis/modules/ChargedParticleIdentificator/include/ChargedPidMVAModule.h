9d8d9f76a7b80fed45cc121f769a1d866ebb3ef0
Marco Milesi <marco.milesi@unimelb.edu.au>
2019-03-27 07:35:45 +0000

    Implementation of module to apply MVA for charged pid.

--- analysis/modules/ChargedParticleIdentificator/include/ChargedPidMVAModule.h
+++ analysis/modules/ChargedParticleIdentificator/include/ChargedPidMVAModule.h
@@ -10,53 +10,172 @@
 
 #pragma once
 
+// FRAMEWORK
 #include <framework/core/Module.h>
+#include <framework/gearbox/Unit.h>
+#include <framework/gearbox/Const.h>
+#include <framework/logging/Logger.h>
+#include <framework/dataobjects/EventMetaData.h>
+#include <framework/database/DBObjPtr.h>
+
+// MVA
+#include <mva/interface/Expert.h>
+#include <mva/interface/Interface.h>
+#include <mva/interface/Weightfile.h>
+#include <mva/interface/Dataset.h>
+
+// ANALYSIS
+#include <analysis/VariableManager/Manager.h>
+#include <analysis/dataobjects/Particle.h>
+#include <analysis/dataobjects/ParticleList.h>
+#include <analysis/dataobjects/ParticleExtraInfoMap.h>
+#include <analysis/dataobjects/EventExtraInfo.h>
+#include <analysis/dbobjects/ChargedPidMVAWeights.h>
+
+//C++
+#include <unordered_map>
+
 
 namespace Belle2 {
-  /** A module template.
+
+  /**
    *
-   *  A detailed description of your module.
+   * This module evaluates the response of an MVA trained for charged particle identification between two hypotheses, S and B.
+   *
+   * It takes a set of Particle objects in a standard charged particle's ParticleList, calculates the MVA score
+   * using the appropriate xml weight file for a given input set of (S,B) mass hypotheses,
+   * and adds it as ExtraInfo to the Particle objects.
+   *
    */
   class ChargedPidMVAModule : public Module {
+
+    typedef std::vector<std::unique_ptr<MVA::Expert> > ExpertsList; /**< Typedef */
+    typedef std::vector<std::unique_ptr<MVA::SingleDataset> > DatasetsList; /**< Typedef */
+    typedef std::vector< std::vector<const Variable::Manager::Var*> > VariablesList; /**< Typedef */
+
+    typedef std::unordered_map<int, ExpertsList > ExpertsByParticle; /**< Typedef */
+    typedef std::unordered_map<int, DatasetsList > DatasetsByParticle; /**< Typedef */
+    typedef std::unordered_map<int, VariablesList > VariablesListsByParticle; /**< Typedef */
+
   public:
-    /** Constructor, for setting module description and parameters. */
+
+    /**
+     * Constructor, for setting module description and parameters.
+     */
     ChargedPidMVAModule();
 
-    /** Use to clean up anything you created in the constructor. */
+    /**
+     * Destructor, use this to clean up anything you created in the constructor.
+     */
     virtual ~ChargedPidMVAModule();
 
-    /** Use this to initialize resources or memory your module needs.
+    /**
+     * Use this to initialize resources or memory your module needs.
      *
-     *  Also register any outputs of your module (StoreArrays, StoreObjPtrs, relations) here,
-     *  see the respective class documentation for details.
+     * Also register any outputs of your module (StoreArrays, StoreObjPtrs, relations) here,
+     * see the respective class documentation for details.
      */
     virtual void initialize() override;
 
-    /** Called once before a new run begins.
+    /**
+     * Called once before a new run begins.
      *
      * This method gives you the chance to change run dependent constants like alignment parameters, etc.
      */
     virtual void beginRun() override;
 
-    /** Called once for each event.
+    /**
+     * Called once for each event.
      *
      * This is most likely where your module will actually do anything.
      */
     virtual void event() override;
 
-    /** Called once when a run ends.
+    /**
+     * Called once when a run ends.
      *
-     *  Use this method to save run information, which you aggregated over the last run.
+     * Use this method to save run information, which you aggregated over the last run.
      */
     virtual void endRun() override;
 
-    /** Clean up anything you created in initialize(). */
+    /**
+     * Clean up anything you created in initialize().
+     */
     virtual void terminate() override;
-  private:
-    //define your own data members here
 
-  protected:
-    //definition of input parameters
+  private:
+
+    /**
+     * 1. Check if a payload is found in the database.
+     * 2. Check the MVA weights for consistency every time they change in the database.
+     * 3. Load MVA weight files, and set MVA::Expert and MVA::SingleDataset objects for each file.
+    */
+    void initializeMVA();
+
+  private:
+
+    /**
+     * The signal mass hypothesis (unsigned) pdgId.
+     */
+    int m_sig_pdg;
+
+    /**
+     * The background mass hypothesis (unsigned) pdgId.
+     */
+    int m_bkg_pdg;
+
+    /**
+     * The lookup name of the MVA score variable, given the input S, B mass hypotheses for the algorithm.
+     */
+    std::string m_score_varname;
+
+    /**
+     * The pdgIds and names of the standard charged particle lists.
+     * This is not supposed to change at any time (?).
+     */
+    const std::unordered_map<int, std::string> m_charged_particle_lists = { { 11, "e" },
+      { 13, "mu" },
+      { 211, "pi" },
+      { 321, "K" },
+      { 2212, "p" },
+      { 1000010020, "d" }
+    };
+
+    /**
+     * The event information. Used for debugging purposes.
+     */
+    StoreObjPtr<EventMetaData> m_event_metadata;
+
+    /**
+     * Interface to get the database payload w/ the MVA weight files etc.
+     * The payload class has a method to retrieve the correct weightfile representation
+     * given a particle (pdgId, clusterTheta, p).
+     */
+    DBObjPtr<ChargedPidMVAWeights> m_weightfiles_representation;
+
+    /**
+     * This map contains - for each charged particle mass hypothesis' pdgId - a list of MVA::Expert objects.
+     * One Expert to be stored for each xml file found in the database.
+     */
+    ExpertsByParticle m_experts;
+
+    /**
+     * This map contains - for each charged particle mass hypothesis' pdgId - a list of MVA::SingleDataset objects.
+     * One DS to be stored for each xml file found in the database.
+     */
+    DatasetsByParticle m_datasets;
+
+    /**
+     * This map contains - for each charged particle mass hypothesis' pdgId - a list of lists of feature variables.
+     * One list of lists to be stored for each xml file found in the database.
+     */
+    VariablesListsByParticle m_variables;
+
+    /**
+     * This map contains - for each charged particle mass hypothesis' pdgId - a list of lists of spectator variables.
+     * One list of lists to be stored for each xml file found in the database.
+     */
+    VariablesListsByParticle m_spectators;
 
   };
 }
--- analysis/modules/ChargedParticleIdentificator/src/ChargedPidMVAModule.cc
+++ analysis/modules/ChargedParticleIdentificator/src/ChargedPidMVAModule.cc
@@ -1,4 +1,8 @@
+//THIS MODULE
 #include <analysis/modules/ChargedParticleIdentificator/ChargedPidMVAModule.h>
+
+//C++
+#include <algorithm>
 
 using namespace Belle2;
 
@@ -6,33 +10,160 @@ REG_MODULE(ChargedPidMVA)
 
 ChargedPidMVAModule::ChargedPidMVAModule() : Module()
 {
-  setDescription("This is an example module. Please make sure to provide a meaningful description of what your module actually does. Make it as long as you need to.");
 
-  //add module parameters here [see one of the following tutorials]
+  setDescription("This module evaluates the response of an MVA trained for charged particle identification between two hypotheses, S and B. It takes a set of Particle objects in a standard charged particle's ParticleList, calculates the MVA score using the appropriate xml weight file for a given input set of (S,B) mass hypotheses, and adds it as ExtraInfo to the Particle objects.");
+
+  setPropertyFlags(c_ParallelProcessingCertified);
+
+  addParam("sigPdgId",
+           m_sig_pdg,
+           "The pdgId of the signal mass hypothesis.");
+  addParam("bkgPdgId",
+           m_bkg_pdg,
+           "The pdgId of the background mass hypothesis.");
+
 }
 
 
-ChargedPidMVAModule::~ChargedPidMVAModule()
-{
-}
+ChargedPidMVAModule::~ChargedPidMVAModule() {}
 
 
 void ChargedPidMVAModule::initialize()
 {
+  m_event_metadata.isRequired();
+
+  m_score_varname = "chargedPidBDTScore_" + std::to_string(m_sig_pdg) + "_VS_" + std::to_string(m_bkg_pdg);
 }
+
 
 void ChargedPidMVAModule::beginRun()
 {
+  m_weightfiles_representation.addCallback([this]() { initializeMVA(); });
+  initializeMVA();
 }
+
 
 void ChargedPidMVAModule::event()
 {
+
+  auto sigPart = Const::ChargedStable(m_sig_pdg);
+  auto bkgPart = Const::ChargedStable(m_bkg_pdg);
+
+  for (const auto& [pdg, name] : m_charged_particle_lists) {
+
+    StoreObjPtr<ParticleList> pList(name);
+
+    // Skip if this particle list does not match any of the input (S, B) hypotheses.
+    if (pdg != sigPart.getPDGCode() && pdg != bkgPart.getPDGCode()) {
+      continue;
+    }
+
+    for (unsigned int ipart(0); ipart < pList->getListSize(); ++ipart) {
+
+      Particle* particle = pList->getParticle(ipart);
+
+      // Retrieve the index for the correct MVA expert and dataset, given (signal hypo, clusterTheta, p)
+      auto theta   = particle->getECLCluster()->getTheta();
+      auto p       = particle->getP();
+      auto index   = m_weightfiles_representation->getMVAWeightIdx(sigPart, theta, p);
+
+      auto nvars = m_variables.at(pdg).at(index).size();
+      for (unsigned int ivar(0); ivar < nvars; ++ivar) {
+        m_datasets.at(pdg).at(index)->m_input[ivar] = m_variables.at(pdg).at(index).at(ivar)->function(particle);
+      }
+      // NB: the MVA::Expert used to calculate the score must be the one trained to discriminate THIS signal hypothesis!
+      float score = m_experts.at(sigPart.getPDGCode()).at(index)->apply(*m_datasets.at(pdg).at(index))[0];
+
+      // Store the MVA score as a new particle object property.
+      particle->writeExtraInfo(m_score_varname, score);
+
+    }
+
+  }
 }
+
 
 void ChargedPidMVAModule::endRun()
 {
 }
 
+
 void ChargedPidMVAModule::terminate()
 {
 }
+
+
+void ChargedPidMVAModule::initializeMVA()
+{
+
+  if (!m_weightfiles_representation) { B2FATAL("No MVA weightfile representations payload found in database! Abort."); }
+
+  B2INFO("Loading weightfiles from the payload...");
+
+  // THis only if need to de-serialize xml from string
+  // std::vector<MVA::Weightfile> weightfiles;
+  // for (const auto& representation : *m_weightfiles_representation) {
+  //  // The actual weightfile is stored in the m_data field of the representation as a serialized string.
+  //  std::stringstream ss(representation.m_data);
+  //  weightfiles.push_back(MVA::Weightfile::loadFromStream(ss));
+  // }
+
+  B2INFO("Load supported MVA interfaces...");
+
+  // The supported methods have to be initialized once (calling it more than once is safe).
+  MVA::AbstractInterface::initSupportedInterfaces();
+  auto supported_interfaces = MVA::AbstractInterface::getSupportedInterfaces();
+
+  // Iterate over standard charged particles.
+  for (const auto& [pdg, name] : m_charged_particle_lists) {
+
+    B2INFO("\tParticle hypothesis: (" << pdg << "," << name << ")");
+
+    auto weightfiles = m_weightfiles_representation->getMVAWeights(pdg);
+
+    B2INFO("\tConstruct the MVA experts and datasets from the weightfiles...");
+
+    // Initialise list of experts/ds for this pdgId.
+    ExpertsList experts;
+    DatasetsList datasets;
+
+    // Initialise list of lists of variables/spectators for this pdgId.
+    VariablesList variables, spectators;
+
+    for (unsigned int idx(0); idx < weightfiles.size(); idx++) {
+
+      MVA::GeneralOptions general_options;
+      weightfiles.at(idx).getOptions(general_options);
+
+      // Store the list of pointers to the relevant variables for this xml file.
+      Variable::Manager& manager = Variable::Manager::Instance();
+      variables.push_back(manager.getVariables(general_options.m_variables));
+      spectators.push_back(manager.getVariables(general_options.m_spectators));
+
+      // Store an MVA::Expert object.
+      experts.push_back(supported_interfaces[general_options.m_method]->getExpert());
+
+      // Store an MVA::SingleDataset object, in which we will save our features later...
+      std::vector<float> v(general_options.m_variables.size(), 0.0);
+      std::vector<float> s(general_options.m_spectators.size(), 0.0);
+      //datasets.push_back( std::unique_ptr<MVA::SingleDataset>(new MVA::SingleDataset(general_options, v, 1.0, s)) );
+      datasets.push_back(std::make_unique<MVA::SingleDataset>(general_options, v, 1.0));
+
+    }
+
+    // Load the weightfiles into the MVA::Expert objects.
+    for (unsigned int idx(0); idx < weightfiles.size(); idx++) {
+      experts.at(idx)->load(weightfiles.at(idx));
+    }
+
+    // Update maps w/ values for this pdgId.
+    m_experts.emplace(pdg, std::move(experts)); // https://stackoverflow.com/questions/28921250/c-nested-map-with-unique-ptr/32749132
+    m_datasets.emplace(pdg, std::move(datasets));
+    m_variables.emplace(pdg, variables);
+    m_spectators.emplace(pdg, spectators);
+
+  }
+
+}
+
+

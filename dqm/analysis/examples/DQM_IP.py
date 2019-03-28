ba1b9ce6ad026029737b841f6a31418befcdcea8
Bjoern Spruck <bspruck@uni-mainz.de>
2019-03-28 15:06:35 +0100

    add plain gaus fit for vertex histo

--- dqm/analysis/examples/DQM_IP.py
+++ dqm/analysis/examples/DQM_IP.py
@@ -27,11 +27,42 @@ main.add_module(inroot)
 # input.param('AutoCanvasFolders', [])
 # main.add_module(input)
 
+$  # !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from basf2 import *
+import sys
+argv = sys.argv
+
+# Set the log level to show only error and fatal messages
+set_log_level(LogLevel.ERROR)
+
+# Create main path
+main = create_path()
+
+ip_x = register_module('DQMHistAnalysisEpicsExampleModule')
+ip_x.param("HistoName", "IPMonitoring/Y4S_Vertex.X")
+# ip_x.param("Function", "gaus(0)")
+ip_y.param('PVName', 'DQM:Beam:IP:X:')
+main.add_module(ip_x)
+
+ip_y = register_module('DQMHistAnalysisEpicsExampleModule')
+ip_y.param("HistoName", "IPMonitoring/Y4S_Vertex.Y")
+# ip_y.param("Function", "gaus(0)")
+ip_y.param('PVName', 'DQM:Beam:IP:Y:')
+main.add_module(ip_y)
+
+ip_z = register_module('DQMHistAnalysisEpicsExampleModule')
+ip_z.param("HistoName", "IPMonitoring/Y4S_Vertex.Z")
+# ip_z.param("Function", "gaus(0)")
+ip_z.param('PVName', 'DQM:Beam:IP:Z:')
+main.add_module(ip_z)
+
 
 epicsarray = register_module('DQMHistOutputToEPICS')
 epicsarray.param('HistoList', [
     ['IPMonitoring/Y4S_Vertex.X', 'DQM:Beam:IP:X:Proj', 'DQM:Beam:IP:X:Proj:Last'],
-    ['IPMonitoring/Y4S_Vertex.Y', 'DQM:Beam:IP:Y:Proj:Last'],
+    ['IPMonitoring/Y4S_Vertex.Y', 'DQM:Beam:IP:Y:Proj', 'DQM:Beam:IP:Y:Proj:Last'],
     ['IPMonitoring/Y4S_Vertex.Z', 'DQM:Beam:IP:Z:Proj', 'DQM:Beam:IP:Z:Proj:Last'],
 ])
 

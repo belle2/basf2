5ce4fbb47f89454c68b67eaa59d37f8cf894394b
Dmitrii Neverov <dmitrii.neverov@desy.de>
2019-04-03 17:25:50 +0900

    Made RKPropagate(..) virtual in RKTrackRep for an inhereted prototype of monopole track representation

---
+++ tracking/dataobjects/monopoles/src/MplTrackRep.cc
@@ -0,0 +1,37 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2019 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Dmitrii Neverov                                          *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <tracking/dataobjects/monopoles/MplTrackRep.h>
+
+#include <framework/logging/Logger.h>
+
+using namespace genfit;
+
+MplTrackRep::MplTrackRep(int pdgCode, float magCharge, char propDir) :
+  RKTrackRep(pdgCode, propDir),
+  m_magCharge(magCharge)
+{
+  B2RESULT("Monopole representation is created");
+}
+
+MplTrackRep::~MplTrackRep()
+{
+}
+
+double MplTrackRep::RKPropagate(M1x7& state7,
+                                M7x7* jacobian,
+                                M1x3& SA,
+                                double S,
+                                bool varField,
+                                bool calcOnlyLastRowOfJ) const
+{
+  B2RESULT("Propagation with monopole representation is called");
+  return 0.0;
+}

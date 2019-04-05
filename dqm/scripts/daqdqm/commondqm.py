58e8aa48ce1c094056faf2d0be5e171b1f91f7c4
DE Marino Gaetano <gaetano.de.marino@desy.de>
2019-04-05 22:21:42 +0900

    Removing the import of IPDQM script since it is already present in collisiondqm.py

--- dqm/scripts/daqdqm/commondqm.py
+++ dqm/scripts/daqdqm/commondqm.py
@@ -3,7 +3,6 @@
 
 from basf2 import *
 from analysisDQM import add_analysis_dqm
-from IPDQM import add_IP_dqm
 
 
 def add_common_dqm(path, components=None, dqm_environment="expressreco"):
@@ -44,7 +43,6 @@ def add_common_dqm(path, components=None, dqm_environment="expressreco"):
         if components is None or 'PXD' in components or 'SVD' in components:
             vxddqm = register_module('VXDDQMExpressReco')
             path.add_module(vxddqm)
-            add_IP_dqm(path)
 
     if dqm_environment == "hlt":
         # HLT

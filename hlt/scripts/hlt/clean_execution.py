7572c8bdd472b414585685477e7d85a4c2af0389
Nils Braun <nils.braun@desy.de>
2019-04-05 13:35:08 +0900

    Increase the timeout only for the worker processes

--- hlt/scripts/hlt/clean_execution.py
+++ hlt/scripts/hlt/clean_execution.py
@@ -41,7 +41,7 @@ class CleanBasf2Execution:
 
     """
 
-    def __init__(self):
+    def __init__(self, timeout=10):
         """
         Create a new execution with the given parameters (list of arguments)
         """
@@ -49,6 +49,8 @@ class CleanBasf2Execution:
         self._handled_processes = []
         # The commands related to the processes
         self._handled_commands = []
+
+        self.timeout = timeout
 
     def start(self, command):
         """
@@ -111,7 +113,7 @@ class CleanBasf2Execution:
                     pass
 
             # Give the process some time to react
-            if not self.wait_for_process(timeout=10):
+            if not self.wait_for_process(timeout=self.timeout):
                 basf2.B2WARNING("Process did not react in time. Sending a SIGKILL.")
         finally:
             # In any case: kill the process
@@ -133,13 +135,16 @@ class CleanBasf2Execution:
             # And reinstall the signal handlers
             self.install_signal_handler()
 
-    def wait_for_process(self, process_list=None, timeout=10, minimum_delay=0.0005):
+    def wait_for_process(self, process_list=None, timeout=None, minimum_delay=1):
         """
         Wait maximum "timeout" for the process to stop.
         If it did not end in this period, returns False.
         """
         if process_list is None:
             process_list = self._handled_processes
+
+        if timeout is None:
+            timeout = self.timeout
 
         endtime = time() + timeout
         while True:
@@ -151,8 +156,7 @@ class CleanBasf2Execution:
             if remaining <= 0:
                 return False
 
-            delay = min(minimum_delay, remaining)
-            sleep(delay)
+            sleep(minimum_delay)
 
     def install_signal_handler(self):
         """
--- hlt/scripts/hlt/hlt_execution.py
+++ hlt/scripts/hlt/hlt_execution.py
@@ -9,7 +9,7 @@ from hlt.clean_execution import CleanBasf2Execution
 def main(script_name):
     """Shortcut method to do the basf2 execution on expressreco and hlt"""
     # os.chdir(sys.path[0])
-    execution = CleanBasf2Execution()
+    execution = CleanBasf2Execution(timeout=5*60)
     try:
         execution.start(["basf2", "--no-stats", script_name, "--"] + sys.argv[1:])
         return_code = execution.wait()

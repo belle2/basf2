belle2/basf2

commit 339d4b99191e42361d31b0c9b8cfb0285ffaf215

Remove the trigger mode as it is not needed anymore

tracking/scripts/tracking/__init__.py
@@ -6,7 +6,7 @@ from tracking.path_utils import *
 
 
 def add_tracking_reconstruction(path, components=None, pruneTracks=False, skipGeometryAdding=False,
-                                mcTrackFinding=False, trigger_mode="all", trackFitHypotheses=None,
+                                mcTrackFinding=False, trackFitHypotheses=None,
                                 reco_tracks="RecoTracks", prune_temporary_tracks=True, fit_tracks=True,
                                 use_second_cdc_hits=False, skipHitPreparerAdding=False):
     """
@@ -23,7 +23,6 @@ def add_tracking_reconstruction(path, components=None, pruneTracks=False, skipGe
     :param skipHitPreparerAdding: Advanced flag: do not add the hit preparation (esp. VXD cluster creation
         modules. This is useful if they have been added before already.
     :param mcTrackFinding: Use the MC track finders instead of the realistic ones.
-    :param trigger_mode: For a description of the available trigger modes see add_reconstruction.
     :param reco_tracks: Name of the StoreArray where the reco tracks should be stored
     :param prune_temporary_tracks: If false, store all information of the single CDC and VXD tracks before merging.
         If true, prune them.
@@ -36,39 +35,35 @@ def add_tracking_reconstruction(path, components=None, pruneTracks=False, skipGe
         return
 
     if not skipGeometryAdding:
-        # Add the geometry in all trigger modes if not already in the path
         add_geometry_modules(path, components=components)
 
-    if not skipHitPreparerAdding and trigger_mode in ["all", "hlt"]:
+    if not skipHitPreparerAdding:
         add_hit_preparation_modules(path, components=components)
 
     # Material effects for all track extrapolations
-    if trigger_mode in ["all", "hlt"] and 'SetupGenfitExtrapolation' not in path:
+    if 'SetupGenfitExtrapolation' not in path:
         path.add_module('SetupGenfitExtrapolation',
                         energyLossBrems=False, noiseBrems=False)
 
     if mcTrackFinding:
-        # Always add the MC finder in all trigger modes.
         add_mc_track_finding(path, components=components, reco_tracks=reco_tracks,
                              use_second_cdc_hits=use_second_cdc_hits)
     else:
-        add_track_finding(path, components=components, trigger_mode=trigger_mode, reco_tracks=reco_tracks,
+        add_track_finding(path, components=components, reco_tracks=reco_tracks,
                           prune_temporary_tracks=prune_temporary_tracks,
                           use_second_cdc_hits=use_second_cdc_hits)
 
     # Only run the track time extraction on the full reconstruction chain for now. Later, we may
     # consider to do the CDC-hit based method already during the fast reconstruction stage
-    if trigger_mode in ["hlt", "all"]:
-        add_time_extraction(path, components=components)
+    add_time_extraction(path, components=components)
 
-    if trigger_mode in ["hlt", "all"]:
-        add_mc_matcher(path, components=components, reco_tracks=reco_tracks,
-                       use_second_cdc_hits=use_second_cdc_hits)
+    add_mc_matcher(path, components=components, reco_tracks=reco_tracks,
+                   use_second_cdc_hits=use_second_cdc_hits)
 
-        if fit_tracks:
-            add_track_fit_and_track_creator(path, components=components, pruneTracks=pruneTracks,
-                                            trackFitHypotheses=trackFitHypotheses,
-                                            reco_tracks=reco_tracks)
+    if fit_tracks:
+        add_track_fit_and_track_creator(path, components=components, pruneTracks=pruneTracks,
+                                        trackFitHypotheses=trackFitHypotheses,
+                                        reco_tracks=reco_tracks)
 
 
 def add_time_extraction(path, components=None):
@@ -110,7 +105,6 @@ def add_cr_tracking_reconstruction(path, components=None, prune_tracks=False,
         return
 
     if not skip_geometry_adding:
-        # Add the geometry in all trigger modes if not already in the path
         add_geometry_modules(path, components)
 
     add_hit_preparation_modules(path, components=components)
@@ -156,14 +150,13 @@ def add_mc_tracking_reconstruction(path, components=None, pruneTracks=False, use
                                 use_second_cdc_hits=use_second_cdc_hits)
 
 
-def add_track_finding(path, components=None, trigger_mode="all", reco_tracks="RecoTracks",
+def add_track_finding(path, components=None, reco_tracks="RecoTracks",
                       prune_temporary_tracks=True, use_second_cdc_hits=False,
                       use_mc_truth=False, svd_ckf_mode="VXDTF2_after", add_both_directions=True):
     """
     Add the CKF to the path with all the track finding related to and needed for it.
     :param path: The path to add the tracking reconstruction modules to
     :param reco_tracks: The store array name where to output all tracks
-    :param trigger_mode: For a description of the available trigger modes see add_reconstruction.
     :param use_mc_truth: Use the truth information in the CKF modules
     :param svd_ckf_mode: how to apply the CKF (with VXDTF2 or without). Defaults to "VXDTF2_after".
     :param add_both_directions: Curlers may be found in the wrong orientation by the CDC track finder, so try to
@@ -191,15 +184,12 @@ def add_track_finding(path, components=None, trigger_mode="all", reco_tracks="Re
 
     latest_reco_tracks = None
 
-    if trigger_mode in ["fast_reco", "all"] and is_cdc_used(components):
+    if is_cdc_used(components):
         add_cdc_track_finding(path, use_second_hits=use_second_cdc_hits, output_reco_tracks=cdc_reco_tracks)
         latest_reco_tracks = cdc_reco_tracks
 
-    if trigger_mode in ["hlt", "all"] and is_svd_used(components):
-        # in case the lastest_reco_tracks is not set and we are in hlt mode, a previous call to
-        # this method using trigger_mode = "fast_reco" was done and we have the CDC-only RecoTrack
-        # already
-        if trigger_mode == "hlt" and latest_reco_tracks is None:
+    if is_svd_used(components):
+        if latest_reco_tracks is None:
             latest_reco_tracks = cdc_reco_tracks
 
         add_svd_track_finding(path, components=components, input_reco_tracks=latest_reco_tracks,
@@ -208,13 +198,13 @@ def add_track_finding(path, components=None, trigger_mode="all", reco_tracks="Re
                               svd_ckf_mode=svd_ckf_mode, add_both_directions=add_both_directions)
         latest_reco_tracks = svd_cdc_reco_tracks
 
-    if trigger_mode in ["all"] and is_pxd_used(components):
+    if is_pxd_used(components):
         add_pxd_track_finding(path, components=components, input_reco_tracks=latest_reco_tracks,
                               use_mc_truth=use_mc_truth, output_reco_tracks=full_reco_tracks,
                               temporary_reco_tracks=pxd_reco_tracks,
                               add_both_directions=add_both_directions)
 
-    if trigger_mode in ["all"] and prune_temporary_tracks:
+    if prune_temporary_tracks:
         for temporary_reco_track_name in [pxd_reco_tracks, svd_reco_tracks, cdc_reco_tracks, svd_cdc_reco_tracks]:
             if temporary_reco_track_name != reco_tracks:
                 path.add_module('PruneRecoTracks', storeArrayName=temporary_reco_track_name)

hlt/softwaretrigger/scripts/softwaretrigger/path_utils.py
@@ -30,7 +30,7 @@ def add_hlt_dqm(path, run_type, components=constants.DEFAULT_HLT_COMPONENTS, sta
         add_geometry_if_not_present(path)
         add_unpackers(path, components=components)
 
-    add_online_dqm(path, run_type=run_type, dqm_environment=constants.Location.hlt, components=components)
+    add_online_dqm(path, run_type=run_type, dqm_environment=str(constants.Location.hlt), components=components)
 
 
 def add_expressreco_dqm(path, run_type, components=constants.DEFAULT_EXPRESSRECO_COMPONENTS, standalone=False):
@@ -41,7 +41,7 @@ def add_expressreco_dqm(path, run_type, components=constants.DEFAULT_EXPRESSRECO
         add_geometry_if_not_present(path)
         add_unpackers(path, components=components)
 
-    add_online_dqm(path, run_type=run_type, dqm_environment=constants.Location.expressreco, components=components)
+    add_online_dqm(path, run_type=run_type, dqm_environment=str(constants.Location.expressreco), components=components)
 
 
 def add_geometry_if_not_present(path):

hlt/softwaretrigger/scripts/softwaretrigger/processing.py
@@ -185,7 +185,7 @@ def add_expressreco_processing(path,
     if do_reconstruction:
         if run_type == constants.RunTypes.beam:
             add_reconstruction(path, components=reco_components, pruneTracks=False,
-                               skipGeometryAdding=True, **kwargs)
+                               skipGeometryAdding=True, add_trigger_calculation=False, **kwargs)
         elif run_type == constants.RunTypes.cosmic:
             add_cosmics_reconstruction(path, components=reco_components, pruneTracks=False,
                                        skipGeometryAdding=True, **kwargs)

hlt/softwaretrigger/scripts/softwaretrigger/softwaretrigger_reconstruction.py
@@ -51,7 +51,8 @@ def add_softwaretrigger_reconstruction(
     add_skim_software_trigger(skim_reconstruction_path, store_array_debug_prescale)
 
     # Add the default reconstruction for now
-    reconstruction.add_reconstruction(path, skipGeometryAdding=True, pruneTracks=False, components=components, **kwargs)
+    reconstruction.add_reconstruction(path, skipGeometryAdding=True, pruneTracks=False,
+                                      add_trigger_calculation=False, components=components, **kwargs)
 
     # Add the module doing the filter decision
     hlt_filter_module = add_filter_software_trigger(path, store_array_debug_prescale)

reconstruction/scripts/reconstruction.py
@@ -23,7 +23,7 @@ from softwaretrigger.path_utils import (
 import mdst
 
 
-def add_reconstruction(path, components=None, pruneTracks=True, trigger_mode="all", skipGeometryAdding=False,
+def add_reconstruction(path, components=None, pruneTracks=True, add_trigger_calculation=True, skipGeometryAdding=False,
                        trackFitHypotheses=None, addClusterExpertModules=True,
                        use_second_cdc_hits=False, add_muid_hits=False):
     """
@@ -34,17 +34,6 @@ def add_reconstruction(path, components=None, pruneTracks=True, trigger_mode="al
     :param path: Add the modules to this path.
     :param components: list of geometry components to include reconstruction for, or None for all components.
     :param pruneTracks: Delete all hits except the first and last of the tracks after the dEdX modules.
-    :param trigger_mode: Trigger mode to emulate. Possible values are: "all", "hlt", "fast_reco".
-
-        * "all": Normal mode. Do add all the modules in the standard reconstruction. This is the mode for the typical
-          user.
-        * "fast_reco": Only add those modules that are needed for classifying an event as background or not in the
-          fast reco part of the software trigger.
-        * "hlt": Only add those modules thar are needed for classifying an event as background or not in the
-          hlt part of the software trigger. Please note that the fast reco part is also needed for this to work.
-
-        The trigger_mode does just steer, which modules in the standard reconstruction are added to the path. It does
-        not make any trigger decisions itself.
     :param skipGeometryAdding: Advances flag: The tracking modules need the geometry module and will add it,
         if it is not already present in the path. In a setup with multiple (conditional) paths however, it can not
         determine, if the geometry is already loaded. This flag can be used to just turn off the geometry adding at
@@ -55,6 +44,7 @@ def add_reconstruction(path, components=None, pruneTracks=True, trigger_mode="al
         execution time.
     :param use_second_cdc_hits: If true, the second hit information will be used in the CDC track finding.
     :param add_muid_hits: Add the found KLM hits to the RecoTrack. Make sure to refit the track afterwards.
+    :param add_trigger_calculation: add the software trigger modules for monitoring (do not make any cut)
     """
 
     # Add tracking reconstruction modules
@@ -62,7 +52,6 @@ def add_reconstruction(path, components=None, pruneTracks=True, trigger_mode="al
                                 components=components,
                                 pruneTracks=False,
                                 mcTrackFinding=False,
-                                trigger_mode=trigger_mode,
                                 skipGeometryAdding=skipGeometryAdding,
                                 trackFitHypotheses=trackFitHypotheses,
                                 use_second_cdc_hits=use_second_cdc_hits)
@@ -75,11 +64,10 @@ def add_reconstruction(path, components=None, pruneTracks=True, trigger_mode="al
                                     components=components,
                                     pruneTracks=pruneTracks,
                                     add_muid_hits=add_muid_hits,
-                                    addClusterExpertModules=addClusterExpertModules,
-                                    trigger_mode=trigger_mode)
+                                    addClusterExpertModules=addClusterExpertModules)
 
     # Add the modules calculating the software trigger cuts (but not performing them)
-    if trigger_mode == "all" and (not components or (
+    if add_trigger_calculation and (not components or (
             "CDC" in components and "ECL" in components and "EKLM" in components and "BKLM" in components)):
         add_filter_software_trigger(path)
         add_skim_software_trigger(path)
@@ -145,7 +133,6 @@ def add_cosmics_reconstruction(
                                     components=components,
                                     pruneTracks=pruneTracks,
                                     addClusterExpertModules=addClusterExpertModules,
-                                    trigger_mode="all",
                                     add_muid_hits=add_muid_hits,
                                     cosmics=True)
 
@@ -179,8 +166,7 @@ def add_mc_reconstruction(path, components=None, pruneTracks=True, addClusterExp
 
 
 def add_posttracking_reconstruction(path, components=None, pruneTracks=True, addClusterExpertModules=True,
-                                    add_muid_hits=False,
-                                    trigger_mode="all", cosmics=False):
+                                    add_muid_hits=False, cosmics=False):
     """
     This function adds the standard reconstruction modules after tracking
     to a path.
@@ -188,53 +174,46 @@ def add_posttracking_reconstruction(path, components=None, pruneTracks=True, add
     :param path: The path to add the modules to.
     :param components: list of geometry components to include reconstruction for, or None for all components.
     :param pruneTracks: Delete all hits except the first and last after the post-tracking modules.
-    :param trigger_mode: Please see add_reconstruction for a description of all trigger modes.
     :param addClusterExpertModules: Add the cluster expert modules in the KLM and ECL. Turn this off to reduce
         execution time.
     :param add_muid_hits: Add the found KLM hits to the RecoTrack. Make sure to refit the track afterwards.
     :param cosmics: if True, steer TOP for cosmic reconstruction
     """
 
-    if trigger_mode in ["hlt", "all"]:
-        add_dedx_modules(path, components)
-        add_ext_module(path, components)
-        add_top_modules(path, components, trigger_mode=trigger_mode, cosmics=cosmics)
-        add_arich_modules(path, components)
+    add_dedx_modules(path, components)
+    add_ext_module(path, components)
+    add_top_modules(path, components, cosmics=cosmics)
+    add_arich_modules(path, components)
 
     path.add_module('StatisticsSummary').set_name('Sum_PID')
 
-    if trigger_mode in ["fast_reco", "all"]:
-        add_ecl_modules(path, components)
+    add_ecl_modules(path, components)
 
-    if trigger_mode in ["hlt", "all"]:
-        path.add_module("EventT0Combiner")
+    path.add_module("EventT0Combiner")
 
-    if trigger_mode in ["fast_reco", "all"]:
-        add_ecl_finalizer_module(path, components)
+    add_ecl_finalizer_module(path, components)
 
-    if trigger_mode in ["hlt", "all"]:
-        add_ecl_mc_matcher_module(path, components)
+    add_ecl_mc_matcher_module(path, components)
 
-        add_klm_modules(path, components)
+    add_klm_modules(path, components)
 
-        add_klm_mc_matcher_module(path, components)
+    add_klm_mc_matcher_module(path, components)
 
-        add_muid_module(path, add_hits_to_reco_track=add_muid_hits, components=components)
-        add_ecl_track_cluster_modules(path, components)
-        add_ecl_cluster_properties_modules(path, components)
-        add_ecl_eip_module(path, components)
-        add_pid_module(path, components)
+    add_muid_module(path, add_hits_to_reco_track=add_muid_hits, components=components)
+    add_ecl_track_cluster_modules(path, components)
+    add_ecl_cluster_properties_modules(path, components)
+    add_ecl_eip_module(path, components)
+    add_pid_module(path, components)
 
-    if trigger_mode in ["all"] and addClusterExpertModules:
+    if addClusterExpertModules:
         # FIXME: Disabled for HLT until execution time bug is fixed
         add_cluster_expert_modules(path, components)
 
-    if trigger_mode in ["hlt", "all"]:
-        add_ecl_track_brem_finder(path, components)
+    add_ecl_track_brem_finder(path, components)
 
-        # Prune tracks as soon as the post-tracking steps are complete
-        if pruneTracks:
-            add_prune_tracks(path, components)
+    # Prune tracks as soon as the post-tracking steps are complete
+    if pruneTracks:
+        add_prune_tracks(path, components)
 
     path.add_module('StatisticsSummary').set_name('Sum_Clustering')
 
@@ -335,13 +314,12 @@ def add_arich_modules(path, components=None):
         path.add_module(arich_rec)
 
 
-def add_top_modules(path, components=None, trigger_mode="all", cosmics=False):
+def add_top_modules(path, components=None, cosmics=False):
     """
     Add the TOP reconstruction to the path.
 
     :param path: The path to add the modules to.
     :param components: The components to use or None to use all standard components.
-    :param trigger_mode: Please see add_reconstruction for a description of all trigger modes.
     :param cosmics: if True, steer TOP for cosmic reconstruction
     """
     # TOP reconstruction
@@ -354,8 +332,7 @@ def add_top_modules(path, components=None, trigger_mode="all", cosmics=False):
         else:
             top_finder = register_module('TOPBunchFinder')
             path.add_module(top_finder)
-            if trigger_mode in ["hlt"]:
-                top_finder.param('addOffset', True)
+            top_finder.param('addOffset', True)
         top_rec = register_module('TOPReconstructor')
         path.add_module(top_rec)
 

commit 8b77d3c948df0ce1bd23f3a833eda27cafe3c96b

Removed a wrongly introduced line from 339d4b99191, which prevents SVD only tracking

tracking/scripts/tracking/__init__.py
@@ -189,9 +189,6 @@ def add_track_finding(path, components=None, reco_tracks="RecoTracks",
         latest_reco_tracks = cdc_reco_tracks
 
     if is_svd_used(components):
-        if latest_reco_tracks is None:
-            latest_reco_tracks = cdc_reco_tracks
-
         add_svd_track_finding(path, components=components, input_reco_tracks=latest_reco_tracks,
                               output_reco_tracks=svd_cdc_reco_tracks, use_mc_truth=use_mc_truth,
                               temporary_reco_tracks=svd_reco_tracks,
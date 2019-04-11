57d3d32289b83dfee19264626b89039cf21f1f03
ABUDINEN Fernando <fernando.abudinen@ts.infn.it>
2019-04-11 16:40:43 +0900

    Added aditional contributor name to list.

--- analysis/variables/include/TimeDependentVariables.h
+++ analysis/variables/include/TimeDependentVariables.h
@@ -3,7 +3,7 @@
  * Copyright(C) 2010 - Belle II Collaboration                             *
  *                                                                        *
  * Author: The Belle II Collaboration                                     *
- * Contributors: Luigi Li Gioi                                            *
+ * Contributors: Luigi Li Gioi, Fernando Abudinen                         *
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/

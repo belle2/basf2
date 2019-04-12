087b3e146ee22bc4ae5b5488bd3e6fab89b2542c
ABUDINEN Fernando <fernando.abudinen@ts.infn.it>
2019-04-13 03:09:00 +0900

    Added phill as author as I ported code from the NtupleHelicityTool. This pr takes care of Jira BII-4713.

--- analysis/variables/src/HelicityVariables.cc
+++ analysis/variables/src/HelicityVariables.cc
@@ -3,7 +3,7 @@
 * Copyright(C) 2010 - Belle II Collaboration                             *
 *                                                                        *
 * Author: The Belle II Collaboration                                     *
-* Contributors: Fernando Abudinen                                        *
+* Contributors: Phillip Urquijo, Fernando Abudinen                       *
 *                                                                        *
 * This software is provided "as is" without any warranty.                *
 **************************************************************************/

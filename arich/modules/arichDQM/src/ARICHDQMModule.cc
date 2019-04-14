cd99be4eeedf46ac668d1c9faff64c66b316605f
luka <luka.santelj@desy.de>
2019-04-14 14:12:28 +0900

    small fix

--- arich/modules/arichDQM/src/ARICHDQMModule.cc
+++ arich/modules/arichDQM/src/ARICHDQMModule.cc
@@ -167,6 +167,7 @@ namespace Belle2 {
     h_theta->SetOption("LIVE");
     h_hitsPerTrack->SetOption("LIVE");
     h_trackPerEvent->SetOption("LIVE");
+    h_flashPerAPD->SetOption("LivE");
 
     for (int i = 0; i < 6; i++) {
       h_secTheta[i]->SetOption("LIVE");
@@ -185,7 +186,7 @@ namespace Belle2 {
     h_bits->SetMinimum(0);
     h_hitsPerTrack2D->SetMinimum(0);
     h_tracks2D->SetMinimum(0);
-
+    h_flashPerAPD->SetMinumum(0);
     h_hitsPerEvent->SetMinimum(0);
     h_theta->SetMinimum(0);
     h_hitsPerTrack->SetMinimum(0);

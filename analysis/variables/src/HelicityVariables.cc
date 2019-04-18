7cac76891b1070a34dddb914bf473f107a1ad3d8
ABUDINEN Fernando <fernando.abudinen@ts.infn.it>
2019-04-18 20:43:05 +0900

    Added Michael De Nuccio as author and updated copyright date.

--- analysis/variables/src/HelicityVariables.cc
+++ analysis/variables/src/HelicityVariables.cc
@@ -1,9 +1,9 @@
 /**************************************************************************
 * BASF2 (Belle Analysis Framework 2)                                     *
-* Copyright(C) 2010 - Belle II Collaboration                             *
+* Copyright(C) 2019 - Belle II Collaboration                             *
 *                                                                        *
 * Author: The Belle II Collaboration                                     *
-* Contributors: Phillip Urquijo, Fernando Abudinen                       *
+* Contributors: Phillip Urquijo, Fernando Abudinen, Michael de Nuccio    *
 *                                                                        *
 * This software is provided "as is" without any warranty.                *
 **************************************************************************/

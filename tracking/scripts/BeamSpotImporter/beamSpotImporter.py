4e6cd8242aba593a773699c49c45b645b77a50cf
Giulia Casarosa <giulia.casarosa@desy.de>
2019-04-20 12:38:38 +0900

    added verbose parameter

--- tracking/scripts/BeamSpotImporter/beamSpotImporter.py
+++ tracking/scripts/BeamSpotImporter/beamSpotImporter.py
@@ -17,7 +17,7 @@
 import basf2
 from basf2 import *
 import ROOT
-from ROOT import TVector3, TMatrixDSym, TFile, TH1F
+from ROOT import TVector3, TMatrixDSym, TFile, TH1F, TMath
 from ROOT import Belle2
 from ROOT.Belle2 import BeamSpot
 from array import array
@@ -30,6 +30,8 @@ parser = argparse.ArgumentParser(description="BeamSpot Importer from DQM root fi
 parser.add_argument('--exp', metavar='experiment', dest='exp', type=int, nargs=1, help='Experiment Number')
 parser.add_argument('--run', metavar='run', dest='run', type=int, nargs=1, help='Run Number')
 parser.add_argument('--dqm', metavar='dqm', dest='dqm', type=str, nargs=1, help='DQM ROOT file')
+parser.add_argument('--verbose', dest='verbose', action='store_const', const=True,
+                    default=False, help='print BeamSpot to screen.')
 
 print('')
 
@@ -71,6 +73,7 @@ class beamSpotImporter(basf2.Module):
 
         entries = hVertexX.GetEntries()
 
+        # do not create DBObject if too few vertices
         if entries < minVertices:
             print('')
             RED = "\033[1;31m"
@@ -97,7 +100,6 @@ class beamSpotImporter(basf2.Module):
         # vertex position
         # Computed as the medians of the vertex position histograms
         vertexPos = ROOT.TVector3(medianX[0], medianY[0], medianZ[0])
-        vertexPos.Print()
 
         # vertex position error
         # Computed as the squared RMS of the vertex position histograms, divided by the number of entries
@@ -112,7 +114,6 @@ class beamSpotImporter(basf2.Module):
         vertexCov[1][0] = 0
         vertexCov[2][0] = 0
         vertexCov[2][1] = 0
-        vertexCov.Print()
 
         # beam spot size
         vertexSize = ROOT.TMatrixDSym(3)
@@ -125,7 +126,72 @@ class beamSpotImporter(basf2.Module):
         vertexSize[0][0] = 0.1
         vertexSize[1][1] = 0.2
         vertexSize[2][2] = 0.3
-        vertexSize.Print()
+
+        if args.verbose:
+            bBLUE = "\033[1;34m"
+            BLUE = "\033[0;34m"
+            sys.stdout.write(bBLUE)
+            print()
+            print('~~ BeamSpot for Experiment = ' + str(experiment) + ', Run = ' + str(run) + ' ~~')
+            print()
+            sys.stdout.write(bBLUE)
+            print('-> the vertex position (cm):')
+            sys.stdout.write(RESET)
+
+            sys.stdout.write(BLUE)
+            vertexPos.Print()
+            print(' Position X = {} mm '.format(round(vertexPos[0] * 10, 4)))
+            print(' Position Y = {} mm '.format(round(vertexPos[1] * 10, 4)))
+            print(' Position Z = {} mm '.format(round(vertexPos[2] * 10, 4)))
+            print()
+            sys.stdout.write(bBLUE)
+            print('-> the vertex position error matrix (cm2):')
+            sys.stdout.write(RESET)
+
+            sys.stdout.write(BLUE)
+            vertexCov.Print()
+            print(
+                ' Error X = {} um VS bin width = {} um'.format(
+                    round(
+                        TMath.Sqrt(
+                            vertexCov[0][0]) *
+                        10000,
+                        2),
+                    round(
+                        hVertexX.GetBinWidth(1) *
+                        10000),
+                    1))
+            print(
+                ' Error Y = {} um VS bin width = {} um'.format(
+                    round(
+                        TMath.Sqrt(
+                            vertexCov[1][1]) *
+                        10000,
+                        2),
+                    round(
+                        hVertexX.GetBinWidth(1) *
+                        10000),
+                    1))
+            print(
+                ' Error Z = {} um VS bin width = {} um'.format(
+                    round(
+                        TMath.Sqrt(
+                            vertexCov[2][2]) *
+                        10000,
+                        2),
+                    round(
+                        hVertexX.GetBinWidth(1) *
+                        10000),
+                    1))
+            print()
+            sys.stdout.write(bBLUE)
+            print('-> the luminous region size matrix (cm2):')
+            sys.stdout.write(RESET)
+            sys.stdout.write(BLUE)
+            vertexSize.Print()
+            print()
+
+            sys.stdout.write(RESET)
 
         # create and fill the payload
         payload = Belle2.BeamSpot()

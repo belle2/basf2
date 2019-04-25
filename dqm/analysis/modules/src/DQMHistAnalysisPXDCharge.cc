bb22461869b24bc09ef07195b11a4219c9e1e6e8
Björn Spruck <bjoern.spruck@desy.de>
2019-04-25 16:25:14 +0900

    remove leftover print-to-file

--- dqm/analysis/modules/src/DQMHistAnalysisPXDCharge.cc
+++ dqm/analysis/modules/src/DQMHistAnalysisPXDCharge.cc
@@ -157,7 +157,7 @@ void DQMHistAnalysisPXDChargeModule::event()
       m_cCharge->cd();
       hh1->Draw();
       m_fLandau->Draw("same");
-      m_cCharge->Print(str(format("cc_%d.pdf") % i).data());
+//      m_cCharge->Print(str(format("cc_%d.pdf") % i).data());
 
       if (hh1->GetEntries() > 1000) enough = true;
     }

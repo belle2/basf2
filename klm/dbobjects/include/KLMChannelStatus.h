0c8380dd1823433f5bba3732c25a732a4f01b030
Kirill Chilikin <chilikin@lebedev.ru>
2019-04-27 16:07:47 +0900

    New database object for KLM channel status.

---
+++ klm/dbobjects/include/KLMChannelStatus.h
@@ -0,0 +1,87 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2019  Belle II Collaboration                              *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Kirill Chilikin                                          *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+/* C++ headers. */
+#include <cstdint>
+#include <map>
+
+/* External headers. */
+#include <TObject.h>
+
+namespace Belle2 {
+
+  /**
+   * KLM channel status.
+   */
+  class KLMChannelStatus : public TObject {
+
+  public:
+
+    /**
+     * Channel status.
+     */
+    enum ChannelStatus {
+
+      /** Unknown status. */
+      c_Unknown,
+
+      /** Normally operating channel. */
+      c_Normal,
+
+      /** Dead channel (no signal). */
+      c_Dead,
+
+      /** Hot channel (large background). */
+      c_Hot,
+
+    };
+
+    /**
+     * Constructor.
+     */
+    KLMChannelStatus();
+
+    /**
+     * Destructor.
+     */
+    ~KLMChannelStatus();
+
+    /**
+     * Get channel status.
+     * @param[in] channel Channel number.
+     */
+    enum ChannelStatus getChannelStatus(uint16_t channel) const;
+
+    /**
+     * Set channel status.
+     * @param[in] channel Channel number.
+     * @param[in] status  Status.
+     */
+    void setChannelStatus(uint16_t channel, enum ChannelStatus status);
+
+    /**
+     * Get number of active strips in the specified EKLM sector.
+     * @param[in] sectorGlobal Sector global number.
+     */
+    int getActiveStripsEKLMSector(int sectorGlobal) const;
+
+  private:
+
+    /** Channel data. */
+    std::map<uint16_t, enum ChannelStatus> m_ChannelStatus;
+
+    /** Class version. */
+    ClassDef(Belle2::KLMChannelStatus, 1);
+
+  };
+
+}
---
+++ klm/dbobjects/src/KLMChannelStatus.cc
@@ -0,0 +1,75 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2019  Belle II Collaboration                              *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Kirill Chilikin                                          *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+/* Belle2 headers. */
+#include <eklm/dataobjects/ElementNumbersSingleton.h>
+#include <klm/dataobjects/KLMElementNumbers.h>
+#include <klm/dbobjects/KLMChannelStatus.h>
+#include <framework/logging/Logger.h>
+
+using namespace Belle2;
+
+KLMChannelStatus::KLMChannelStatus()
+{
+}
+
+KLMChannelStatus::~KLMChannelStatus()
+{
+}
+
+enum  KLMChannelStatus::ChannelStatus
+KLMChannelStatus::getChannelStatus(uint16_t channel) const {
+  std::map<uint16_t, enum ChannelStatus>::const_iterator it;
+  it = m_ChannelStatus.find(channel);
+  if (it == m_ChannelStatus.end())
+    return c_Unknown;
+  return it->second;
+}
+
+void KLMChannelStatus::setChannelStatus(uint16_t channel,
+                                        enum ChannelStatus status)
+{
+  std::map<uint16_t, enum ChannelStatus>::iterator it;
+  it = m_ChannelStatus.find(channel);
+  if (it == m_ChannelStatus.end()) {
+    m_ChannelStatus.insert(
+      std::pair<uint16_t, enum ChannelStatus>(channel, status));
+  } else {
+    it->second = status;
+  }
+}
+
+int KLMChannelStatus::getActiveStripsEKLMSector(int sectorGlobal) const
+{
+  int active;
+  int nPlanes, nStrips;
+  int endcap, layer, sector, plane, strip;
+  const EKLM::ElementNumbersSingleton* eklmElementNumbers =
+    &(EKLM::ElementNumbersSingleton::Instance());
+  const KLMElementNumbers* elementNumbers =
+    &(KLMElementNumbers::Instance());
+  nPlanes = eklmElementNumbers->getMaximalPlaneNumber();
+  nStrips = eklmElementNumbers->getMaximalStripNumber();
+  eklmElementNumbers->sectorNumberToElementNumbers(
+    sectorGlobal, &endcap, &layer, &sector);
+  active = 0;
+  for (plane = 1; plane <= nPlanes; ++plane) {
+    for (strip = 1; strip <= nStrips; ++strip) {
+      int channel = elementNumbers->channelNumberEKLM(
+                      endcap, layer, sector, plane, strip);
+      enum ChannelStatus status = getChannelStatus(channel);
+      if (status == c_Unknown)
+        B2FATAL("Incomplete KLM channel data.");
+      if (status != c_Dead)
+        active++;
+    }
+  }
+  return active;
+}

09722fa32c1addecdee29458ee4a452a60814e09
Sam Cunliffe <sam.cunliffe@desy.de>
2019-05-06 11:09:58 +0200

    Remove unnecessary include.

--- analysis/variables/src/FlavorTaggingVariables.cc
+++ analysis/variables/src/FlavorTaggingVariables.cc
@@ -29,7 +29,6 @@
 #include <analysis/dataobjects/ParticleList.h>
 #include <analysis/dataobjects/FlavorTaggerInfo.h>
 #include <analysis/ContinuumSuppression/Thrust.h>
-#include <analysis/dataobjects/TagVertex.h>
 
 #include <mdst/dataobjects/MCParticle.h>
 #include <mdst/dataobjects/Track.h>

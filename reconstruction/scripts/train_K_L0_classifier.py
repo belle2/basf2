81baccfcc66d3fde0f3f5e641065892cc1850c08
OBERHOF Benjamin Jerome Andreas <benjamin.oberhof@lnf.infn.it>
2019-05-10 04:07:21 +0900

    fix Kl classifier (for now, KLM only)

--- reconstruction/scripts/train_K_L0_classifier.py
+++ reconstruction/scripts/train_K_L0_classifier.py
@@ -1,24 +1,23 @@
-#!/bin/zsh
+# !/bin/zsh
 # -*- coding: utf-8 -*-
 # Jo-Frederik.krohn@desy.de
+
 import sys
 import os
 
-
 # USE THIS TO TRAIN AND UPLOAD A NEW CLASSIFIER FOR THE KLONG ID
 # USE THE OTHER SCRIPT TO GENERATE DATA. THEN TRAIN AND UPLOAD WITH &THIS.
-
 # only thing to set is the path were you have the data to train the classifier on.
 
 try:
     data_path = sys.argv[1]
 except BaseException:
-    data_path = "/home/belle2/jkrohn/root_files/training_R1/generic/*GENERIC*.root"
+    data_path = "root_files/training/*.root"
 try:
     identifier = sys.argv[2]
 except BaseException:
     # the default name used in KLMExpert
-    identifier = "KLM_fBDT_10xbkg100k.xml"
+    identifier = "KLM_fBDT_50xbkg1.xml"
 
 ex_b = 0   # experiment begin, 0 for all of them
 ex_e = -1   # experiment end, -1 for all of them
@@ -28,21 +27,20 @@ upload = True  # upload to conditions database
 remove_db = True  # delete local db
 tag_name = "development"  # global tag name
 
-training_string = 'basf2_mva_teacher --datafiles {data_path} --treename KLMdata --identifier {identifier} --variables\
-    "KLMnLayer" "KLMnInnermostlayer" "KLMglobalZ" "KLMtime" "KLMdistToNextCl" "KLMenergy" "KLMTrackSepDist"\ 
-    "KLMInitialtrackSepAngle" "KLMTrackRotationAngle" "KLMTrackSepAngle" "KLMhitDepth" "KLMECLenergy" "KLMECLE9oE25" \
-    "KLMECLtiming" "KLMECLmintrackDist" "KLMECLZMVA" "KLMECLZ40" "KLMECLZ51"\
-    - -target_variable isSignal - -method FastBDT - -nTrees 400 - -nCutLevels 8 \
-    - -nLevels 4'.format(
-        data_path=data_path,
-        identifier=identifier)
+training_string = 'basf2_mva_teacher - -datafiles {data_path} - -treename KLMdata - -identifier {identifier}
+--variable "KLMnLayer" "KLMnInnermostlayer" "KLMglobalZ" "KLMtime" "KLMdistToNextCl" "KLMenergy" "KLMTrackSepDist"
+"KLMInitialtrackSepAngle" "KLMTrackRotationAngle" "KLMTrackSepAngle" "KLMhitDepth" "KLMECLenergy" "KLMECLE9oE25"
+"KLMECLtiming" "KLMECLmintrackDist" "KLMECLZMVA" "KLMECLZ40" "KLMECLZ51"
+--target_variable isSignal - -method FastBDT - -nTrees 400 - -nCutLevels 8 - -nLevels 4'.format(
+    data_path=data_path,
+    identifier=identifier)
 
 os.system(training_string)
 
 # "upload" to localdb
 os.system(
-    "basf2_mva_upload --identifier {identifier} --db_identifier {identifier_db} \
-        --begin_experiment {ex_b} --end_experiment {ex_e} --begin_run {run_b} --end_run {run_e} ".format(
+    'basf2_mva_upload --identifier {identifier} --db_identifier {identifier_db} --begin_experiment {ex_b}'
+    '--end_experiment {ex_e} --begin_run {run_b} --end_run {run_e}'.format(
         identifier=identifier,
         identifier_db=identifier.split(".xml")[0],
         ex_b=ex_b,

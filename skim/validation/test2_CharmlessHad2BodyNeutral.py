0032b9c3f11eef2a66abab42ce78df2c3e8da2db
Kim Smith <khsmith@student.unimelb.edu.au>
2019-05-15 16:02:32 +0900

    bugfixes. Corrected output directory, MC9 to default, corrected input particle lists.

--- skim/validation/test2_CharmlessHad2BodyNeutral.py
+++ skim/validation/test2_CharmlessHad2BodyNeutral.py
@@ -3,7 +3,7 @@
 
 """
 <header>
-    <input>CharmlessHad2BodyNeutral.udst.root</input>
+    <input>../CharmlessHad2BodyNeutral.udst.root</input>
     <output>CharmlessHad2BodyNeutral_Validation.root</output>
     <contact>khsmith@student.unimelb.edu.au</contact>
 </header>
@@ -15,7 +15,7 @@ from modularAnalysis import *
 charmless2neutralpath = Path()
 
 # the variables that are printed out are: Mbc, deltaE and the daughter particle invariant masses.
-inputMdst('MC9', 'CharmlessHad2BodyNeutral.udst.root', path=charmless2neutralpath)
+inputMdst('default', '../CharmlessHad2BodyNeutral.udst.root', path=charmless2neutralpath)
 
 from variables import variables
 variablesToHistogram(
@@ -27,7 +27,7 @@ variablesToHistogram(
         ('daughter(0, InvM)', 100, 0.3, 1.1),  # K_S0 invariant mass
         ('daughter(1, InvM)', 100, 0.06, 0.18)],  # pi0 invariant mass
     variables_2d=[
-        ('Mbc', 50, 5.23, 5.31, 'deltaE', 50, -0.7, 0.7)])
+        ('Mbc', 50, 5.23, 5.31, 'deltaE', 50, -0.7, 0.7)], path=charmless2neutralpath)
 
 process(charmless2neutralpath)
 print(statistics)

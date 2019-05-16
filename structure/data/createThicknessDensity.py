1d83270425fc92983f365dd2a89cd7c2d2931bcf
Yu Hu <yu.hu@desy.de>
2019-05-16 20:06:20 +0200

    Add some instructions and commet

--- structure/data/createThicknessDensity.py
+++ structure/data/createThicknessDensity.py
@@ -1,49 +1,81 @@
-# Script of creating the xml file for tickness and density of each cell volume.
+#!/usr/bin/env python3
 import xml.dom
 import numpy as np
+
+"""
+Creating the xml file for tickness and density of each cell volume
+for the service matirial.
+
+Two kinds of material can be selected:
+
+1. Defalut value:
+    The thickness and density calculated from the photo of actual
+    cable arrangement and density of each cable.
+
+2. vacuum:
+    No service matirial in the gap.
+"""
 
 dom1 = xml.dom.getDOMImplementation()
 doc = dom1.createDocument(None, "TicknessDensity", None)
 top_element = doc.documentElement
 
-thickness_CDCback = (np.ones((16, 144)) * 1.0e-5).ravel()  # 1.0e-5
-thickness_CDCfor = (np.ones((16, 144)) * 1.0e-5).ravel()
-thickness_ARICH = (np.ones((3, 144)) * 1.0e-5).ravel()
-thickness_TOPback = (np.ones(144) * 1.0e-5).ravel()
-thickness_TOPfor = (np.ones(144) * 1.0e-5).ravel()
-thickness_ECLback = (np.ones((3, 5, 144)) * 1.0e-5).ravel()
-thickness_ECLfor = (np.ones((3, 5, 144)) * 1.0e-5).ravel()
+# Select which kind of material you want to input. Defalut value: 1, vacuum: 0
+DefalutValue = 0
 
-# Input the thickness of the matirial in the backward gap between  CDC and ECL
-# thickness_CDCback = (np.loadtxt(open("thickness_CDCback.csv", "rb"), delimiter=",")).ravel()
-# thickness_CDCback[thickness_CDCback < 1.0e-5] = 1.0e-5
+if DefalutValue == 0:
+    # Input the thickness of the matirial in the backward gap between CDC and ECL, unit is 'mm'
+    thickness_CDCback = (np.ones((16, 144)) * 1.0e-5).ravel()
 
-# You can change the thickness of backward cell(i,j) by:
-# thickness_CDCback[i,j] = thickness
+    # Input the thickness of the matirial in the forward gap between CDC and ARICH
+    thickness_CDCfor = (np.ones((16, 144)) * 1.0e-5).ravel()
 
-# Input the thickness of the matirial in the forward gap between  CDC and ARICH
-# thickness_CDCfor = (np.loadtxt(open("thickness_CDCfor.csv", "rb"), delimiter=",")).ravel()
-# thickness_CDCfor[thickness_CDCfor < 1.0e-5] = 1.0e-5
+    # Input the thickness of the matirial in the backward gap between TOP and ECL
+    thickness_TOPback = (np.ones(144) * 1.0e-5).ravel()
 
-# Input the thickness of the matirial in the forward gap between  TOP and ECL
-# thickness_TOPback = (np.loadtxt(open("thickness_TOP_back.csv", "rb"), delimiter=",")).ravel()
-# thickness_TOPback[thickness_TOPback < 1.0e-5] = 1.0e-5
+    # Input the thickness of the matirial in the forward gap between TOP and ECL
+    thickness_TOPfor = (np.ones(144) * 1.0e-5).ravel()
 
-# Input the thickness of the matirial in the forward gap between  TOP and ECL
-# thickness_TOPfor = (np.loadtxt(open("thickness_TOP_for.csv", "rb"), delimiter=",")).ravel()
-# thickness_TOPfor[thickness_TOPfor < 1.0e-5] = 1.0e-5
+    # Input the thickness of the matirial in the forward gap between ARICH and TOP
+    thickness_ARICH = (np.ones((3, 144)) * 1.0e-5).ravel()
 
-# Input the thickness of the matirial in the forward gap between  ARICH and TOP
-# thickness_ARICH = (np.loadtxt(open("thickness_ARICH.csv", "rb"), delimiter=",")).ravel()
-# thickness_ARICH[thickness_ARICH < 1.0e-5] = 1.0e-5
+    # Input the thickness of the matirial in the backward gap between barrel and endcap of ECL
+    thickness_ECLback = (np.ones((3, 5, 144)) * 1.0e-5).ravel()
 
-# Input the thickness of the matirial in the backward gap between barrel and endcap of ECL
-# thickness_ECLback = (np.loadtxt(open("thickness_ECLback.csv", "rb"), delimiter=",")).ravel()
-# thickness_ECLback[thickness_ECLback < 1.0e-5] = 1.0e-5
+    # Input the thickness of the matirial in the forward gap between barrel and endcap of ECL
+    thickness_ECLfor = (np.ones((3, 5, 144)) * 1.0e-5).ravel()
 
-# Input the thickness of the matirial in the forward gap between barrel and endcap of ECL
-# thickness_ECLfor = (np.loadtxt(open("thickness_ECLfor.csv", "rb"), delimiter=",")).ravel()
-# thickness_ECLfor[thickness_ECLfor < 1.0e-5] = 1.0e-5
+else:
+    # Input the thickness of the matirial in the backward gap between  CDC and ECL
+    thickness_CDCback = (np.loadtxt(open("thickness_CDCback.csv", "rb"), delimiter=",")).ravel()
+    thickness_CDCback[thickness_CDCback < 1.0e-5] = 1.0e-5
+
+    # You can change the thickness of backward cell(i,j) by:
+    # thickness_CDCback[i,j] = thickness
+
+    # Input the thickness of the matirial in the forward gap between CDC and ARICH
+    thickness_CDCfor = (np.loadtxt(open("thickness_CDCfor.csv", "rb"), delimiter=",")).ravel()
+    thickness_CDCfor[thickness_CDCfor < 1.0e-5] = 1.0e-5
+
+    # Input the thickness of the matirial in the backward gap between TOP and ECL
+    thickness_TOPback = (np.loadtxt(open("thickness_TOP_back.csv", "rb"), delimiter=",")).ravel()
+    thickness_TOPback[thickness_TOPback < 1.0e-5] = 1.0e-5
+
+    # Input the thickness of the matirial in the forward gap between TOP and ECL
+    thickness_TOPfor = (np.loadtxt(open("thickness_TOP_for.csv", "rb"), delimiter=",")).ravel()
+    thickness_TOPfor[thickness_TOPfor < 1.0e-5] = 1.0e-5
+
+    # Input the thickness of the matirial in the forward gap between ARICH and TOP
+    thickness_ARICH = (np.loadtxt(open("thickness_ARICH.csv", "rb"), delimiter=",")).ravel()
+    thickness_ARICH[thickness_ARICH < 1.0e-5] = 1.0e-5
+
+    # Input the thickness of the matirial in the backward gap between barrel and endcap of ECL
+    thickness_ECLback = (np.loadtxt(open("thickness_ECLback.csv", "rb"), delimiter=",")).ravel()
+    thickness_ECLback[thickness_ECLback < 1.0e-5] = 1.0e-5
+
+    # Input the thickness of the matirial in the forward gap between barrel and endcap of ECL
+    thickness_ECLfor = (np.loadtxt(open("thickness_ECLfor.csv", "rb"), delimiter=",")).ravel()
+    thickness_ECLfor[thickness_ECLfor < 1.0e-5] = 1.0e-5
 
 thickness_list = list(map(str, thickness_CDCback.tolist() +
                           thickness_CDCfor.tolist() +
@@ -54,11 +86,19 @@ thickness_list = list(map(str, thickness_CDCback.tolist() +
                           thickness_ECLfor.tolist()))
 
 
-density_ARICH = (np.ones(3) * 1.29e-10).ravel()  # 1.29e-10
-density_TOPback = (np.ones(1) * 1.29e-10).ravel()
-density_TOPfor = (np.ones(1) * 1.29e-10).ravel()
-density_ECLback = (np.ones(5) * 1.29e-10).ravel()
-density_ECLfor = (np.ones(5) * 1.29e-10).ravel()
+# Density of the servece material in each gap. Unity is 'g/cm3'
+if DefalutValue == 0:
+    density_ARICH = (np.ones(3) * 1.29e-10).ravel()
+    density_TOPback = (np.ones(1) * 1.29e-10).ravel()
+    density_TOPfor = (np.ones(1) * 1.29e-10).ravel()
+    density_ECLback = (np.ones(5) * 1.29e-10).ravel()
+    density_ECLfor = (np.ones(5) * 1.29e-10).ravel()
+else:
+    density_ARICH = (np.ones(3) * 8.96).ravel()
+    density_TOPback = (np.ones(1) * 8.96).ravel()
+    density_TOPfor = (np.ones(1) * 8.96).ravel()
+    density_ECLback = (np.ones(5) * 8.96).ravel()
+    density_ECLfor = (np.ones(5) * 8.96).ravel()
 
 
 density_list = list(map(str, density_ARICH.tolist() + density_TOPback.tolist() +
@@ -126,7 +166,6 @@ for element in elements:
     sNode.setAttribute('desc', desc[element])
     if element in unit.keys():
         sNode.setAttribute('unit', unit[element])
-# print(element)
     if isinstance(value[element], list):
         textNode = doc.createTextNode("  ".join(value[element]))
     else:

c103270526aead3a9f2f6019f7b0d699c6565557
Hannah Marie Wakeling <hannah.marie.wakeling@desy.de>
2019-05-23 07:13:07 +0900

    First commit

--- skim/standalone/feiHadronicBplus_Skim_Standalone.py
+++ skim/standalone/feiHadronicBplus_Skim_Standalone.py
@@ -4,7 +4,7 @@
     FEI Hadronic B+ tag skim standalone for generic analysis in the
     (Semi-)Leptonic and Missing Energy Working Group
     Skim LFN code: 11180200
-    fei training: MC11 based, release-03-00-03 'FEIv4_2018_MC11_release_03_00_00'
+    fei training: MC12 based, release-03-01-01 'FEIv4_2019_MC12_release_03_01_01'
     """
 
 __authors__ = ["Racha Cheaib", "Sophie Hollitt", "Hannah Wakeling"]
@@ -16,7 +16,7 @@ from analysisPath import analysis_main
 from beamparameters import add_beamparameters
 from skimExpertFunctions import *
 
-gb2_setuprel = 'release-03-00-03'
+gb2_setuprel = 'release-03-01-01'
 
 import sys
 import os

11052f9029edd801b6ae24fae09ee20f40f907b6
Kirill Chilikin <chilikin@lebedev.ru>
2019-05-24 19:44:02 +0900

    Channel indices with levels.

--- klm/dataobjects/src/BKLMChannelIndex.cc
+++ klm/dataobjects/src/BKLMChannelIndex.cc
@@ -14,7 +14,8 @@
 
 using namespace Belle2;
 
-BKLMChannelIndex::BKLMChannelIndex() :
+BKLMChannelIndex::BKLMChannelIndex(enum IndexLevel indexLevel) :
+  m_IndexLevel(indexLevel),
   m_Forward(0),
   m_Sector(1),
   m_Layer(1),
@@ -27,7 +28,9 @@ BKLMChannelIndex::BKLMChannelIndex() :
 }
 
 BKLMChannelIndex::BKLMChannelIndex(
-  int forward, int sector, int layer, int plane, int strip) :
+  int forward, int sector, int layer, int plane, int strip,
+  enum IndexLevel indexLevel) :
+  m_IndexLevel(indexLevel),
   m_Forward(forward),
   m_Sector(sector),
   m_Layer(layer),
@@ -51,7 +54,7 @@ uint16_t BKLMChannelIndex::getKLMChannelNumber()
 
 BKLMChannelIndex& BKLMChannelIndex::begin()
 {
-  static BKLMChannelIndex index(0, 1, 1, 0, 1);
+  static BKLMChannelIndex index(0, 1, 1, 0, 1, m_IndexLevel);
   return index;
 }
 
@@ -66,63 +69,107 @@ BKLMChannelIndex& BKLMChannelIndex::end()
       BKLMElementNumbers::getMaximalForwardNumber(),
       BKLMElementNumbers::getMaximalSectorNumber(),
       BKLMElementNumbers::getMaximalLayerNumber(),
-      BKLMElementNumbers::getMaximalPlaneNumber()));
+      BKLMElementNumbers::getMaximalPlaneNumber()),
+    m_IndexLevel);
   return index;
+}
+
+void BKLMChannelIndex::increment(enum IndexLevel indexLevel)
+{
+  switch (indexLevel) {
+    case c_IndexLevelStrip:
+      m_Strip++;
+      if (m_Strip > m_NStripsPlane) {
+        m_Strip = 1;
+        increment(c_IndexLevelPlane);
+        m_NStripsPlane = BKLMElementNumbers::getNStrips(
+                           m_Forward, m_Sector, m_Layer, m_Plane);
+      }
+      break;
+    case c_IndexLevelPlane:
+      m_Plane++;
+      if (m_Plane > BKLMElementNumbers::getMaximalPlaneNumber()) {
+        m_Plane = 0;
+        increment(c_IndexLevelLayer);
+      }
+      break;
+    case c_IndexLevelLayer:
+      m_Layer++;
+      if (m_Layer > BKLMElementNumbers::getMaximalLayerNumber()) {
+        m_Layer = 1;
+        increment(c_IndexLevelSector);
+      }
+      break;
+    case c_IndexLevelSector:
+      m_Sector++;
+      if (m_Sector > BKLMElementNumbers::getMaximalSectorNumber()) {
+        m_Sector = 1;
+        increment(c_IndexLevelForward);
+      }
+      break;
+    case c_IndexLevelForward:
+      m_Forward++;
+      if (m_Forward > BKLMElementNumbers::getMaximalForwardNumber())
+        m_Forward = 0;
+      break;
+  }
 }
 
 BKLMChannelIndex& BKLMChannelIndex::operator++()
 {
-  m_Strip++;
-  if (m_Strip > m_NStripsPlane) {
-    m_Strip = 1;
-    m_Plane++;
-    if (m_Plane > BKLMElementNumbers::getMaximalPlaneNumber()) {
-      m_Plane = 0;
-      m_Layer++;
-      if (m_Layer > BKLMElementNumbers::getMaximalLayerNumber()) {
-        m_Layer = 1;
-        m_Sector++;
-        if (m_Sector > BKLMElementNumbers::getMaximalSectorNumber()) {
-          m_Sector = 1;
-          m_Forward++;
-          if (m_Forward > BKLMElementNumbers::getMaximalForwardNumber())
-            m_Forward = 0;
-        }
-      }
-    }
-    m_NStripsPlane = BKLMElementNumbers::getNStrips(
-                       m_Forward, m_Sector, m_Layer, m_Plane);
-  }
+  increment(m_IndexLevel);
   return *this;
 }
 
 bool BKLMChannelIndex::operator==(BKLMChannelIndex& index)
 {
-  if (m_Forward != index.getForward())
-    return false;
-  if (m_Sector != index.getSector())
-    return false;
-  if (m_Layer != index.getLayer())
-    return false;
-  if (m_Plane != index.getPlane())
-    return false;
-  if (m_Strip != index.getStrip())
-    return false;
+  switch (m_IndexLevel) {
+    case c_IndexLevelStrip:
+      if (m_Strip != index.getStrip())
+        return false;
+      [[fallthrough]];
+    case c_IndexLevelPlane:
+      if (m_Plane != index.getPlane())
+        return false;
+      [[fallthrough]];
+    case c_IndexLevelLayer:
+      if (m_Layer != index.getLayer())
+        return false;
+      [[fallthrough]];
+    case c_IndexLevelSector:
+      if (m_Sector != index.getSector())
+        return false;
+      [[fallthrough]];
+    case c_IndexLevelForward:
+      if (m_Forward != index.getForward())
+        return false;
+  }
   return true;
 }
 
 bool BKLMChannelIndex::operator!=(BKLMChannelIndex& index)
 {
-  if (m_Forward != index.getForward())
-    return true;
-  if (m_Sector != index.getSector())
-    return true;
-  if (m_Layer != index.getLayer())
-    return true;
-  if (m_Plane != index.getPlane())
-    return true;
-  if (m_Strip != index.getStrip())
-    return true;
+  switch (m_IndexLevel) {
+    case c_IndexLevelStrip:
+      if (m_Strip != index.getStrip())
+        return true;
+      [[fallthrough]];
+    case c_IndexLevelPlane:
+      if (m_Plane != index.getPlane())
+        return true;
+      [[fallthrough]];
+    case c_IndexLevelLayer:
+      if (m_Layer != index.getLayer())
+        return true;
+      [[fallthrough]];
+    case c_IndexLevelSector:
+      if (m_Sector != index.getSector())
+        return true;
+      [[fallthrough]];
+    case c_IndexLevelForward:
+      if (m_Forward != index.getForward())
+        return true;
+  }
   return false;
 }
 
--- klm/dataobjects/src/EKLMChannelIndex.cc
+++ klm/dataobjects/src/EKLMChannelIndex.cc
@@ -14,7 +14,8 @@
 
 using namespace Belle2;
 
-EKLMChannelIndex::EKLMChannelIndex() :
+EKLMChannelIndex::EKLMChannelIndex(enum IndexLevel indexLevel) :
+  m_IndexLevel(indexLevel),
   m_Endcap(1),
   m_Sector(1),
   m_Layer(1),
@@ -25,7 +26,9 @@ EKLMChannelIndex::EKLMChannelIndex() :
 }
 
 EKLMChannelIndex::EKLMChannelIndex(
-  int endcap, int sector, int layer, int plane, int strip) :
+  int endcap, int sector, int layer, int plane, int strip,
+  enum IndexLevel indexLevel) :
+  m_IndexLevel(indexLevel),
   m_Endcap(endcap),
   m_Sector(sector),
   m_Layer(layer),
@@ -48,7 +51,7 @@ uint16_t EKLMChannelIndex::getKLMChannelNumber()
 
 EKLMChannelIndex& EKLMChannelIndex::begin()
 {
-  static EKLMChannelIndex index(1, 1, 1, 1, 1);
+  static EKLMChannelIndex index(1, 1, 1, 1, 1, m_IndexLevel);
   return index;
 }
 
@@ -59,61 +62,105 @@ EKLMChannelIndex& EKLMChannelIndex::end()
     EKLMElementNumbers::getMaximalSectorNumber(),
     EKLMElementNumbers::getMaximalLayerNumber(),
     EKLMElementNumbers::getMaximalPlaneNumber(),
-    EKLMElementNumbers::getMaximalStripNumber());
+    EKLMElementNumbers::getMaximalStripNumber(),
+    m_IndexLevel);
   return index;
+}
+
+void EKLMChannelIndex::increment(enum IndexLevel indexLevel)
+{
+  switch (indexLevel) {
+    case c_IndexLevelStrip:
+      m_Strip++;
+      if (m_Strip > EKLMElementNumbers::getMaximalStripNumber()) {
+        m_Strip = 1;
+        increment(c_IndexLevelPlane);
+      }
+      break;
+    case c_IndexLevelPlane:
+      m_Plane++;
+      if (m_Plane > EKLMElementNumbers::getMaximalPlaneNumber()) {
+        m_Plane = 1;
+        increment(c_IndexLevelLayer);
+      }
+      break;
+    case c_IndexLevelLayer:
+      m_Layer++;
+      if (m_Layer > EKLMElementNumbers::getMaximalLayerNumber()) {
+        m_Layer = 1;
+        increment(c_IndexLevelSector);
+      }
+      break;
+    case c_IndexLevelSector:
+      m_Sector++;
+      if (m_Sector > EKLMElementNumbers::getMaximalSectorNumber()) {
+        m_Sector = 1;
+        increment(c_IndexLevelEndcap);
+      }
+      break;
+    case c_IndexLevelEndcap:
+      m_Endcap++;
+      if (m_Endcap > EKLMElementNumbers::getMaximalEndcapNumber())
+        m_Endcap = 1;
+      break;
+  }
 }
 
 EKLMChannelIndex& EKLMChannelIndex::operator++()
 {
-  m_Strip++;
-  if (m_Strip > EKLMElementNumbers::getMaximalStripNumber()) {
-    m_Strip = 1;
-    m_Plane++;
-    if (m_Plane > EKLMElementNumbers::getMaximalPlaneNumber()) {
-      m_Plane = 1;
-      m_Sector++;
-      if (m_Sector > EKLMElementNumbers::getMaximalSectorNumber()) {
-        m_Sector = 1;
-        m_Layer++;
-        if (m_Layer > m_ElementNumbersEKLM->getMaximalDetectorLayerNumber(m_Endcap)) {
-          m_Layer = 1;
-          m_Endcap++;
-          if (m_Endcap > EKLMElementNumbers::getMaximalEndcapNumber())
-            m_Endcap = 1;
-        }
-      }
-    }
-  }
+  increment(m_IndexLevel);
   return *this;
 }
 
 bool EKLMChannelIndex::operator==(EKLMChannelIndex& index)
 {
-  if (m_Endcap != index.getEndcap())
-    return false;
-  if (m_Sector != index.getSector())
-    return false;
-  if (m_Layer != index.getLayer())
-    return false;
-  if (m_Plane != index.getPlane())
-    return false;
-  if (m_Strip != index.getStrip())
-    return false;
+  switch (m_IndexLevel) {
+    case c_IndexLevelStrip:
+      if (m_Strip != index.getStrip())
+        return false;
+      [[fallthrough]];
+    case c_IndexLevelPlane:
+      if (m_Plane != index.getPlane())
+        return false;
+      [[fallthrough]];
+    case c_IndexLevelLayer:
+      if (m_Layer != index.getLayer())
+        return false;
+      [[fallthrough]];
+    case c_IndexLevelSector:
+      if (m_Sector != index.getSector())
+        return false;
+      [[fallthrough]];
+    case c_IndexLevelEndcap:
+      if (m_Endcap != index.getEndcap())
+        return false;
+  }
   return true;
 }
 
 bool EKLMChannelIndex::operator!=(EKLMChannelIndex& index)
 {
-  if (m_Endcap != index.getEndcap())
-    return true;
-  if (m_Sector != index.getSector())
-    return true;
-  if (m_Layer != index.getLayer())
-    return true;
-  if (m_Plane != index.getPlane())
-    return true;
-  if (m_Strip != index.getStrip())
-    return true;
+  switch (m_IndexLevel) {
+    case c_IndexLevelStrip:
+      if (m_Strip != index.getStrip())
+        return true;
+      [[fallthrough]];
+    case c_IndexLevelPlane:
+      if (m_Plane != index.getPlane())
+        return true;
+      [[fallthrough]];
+    case c_IndexLevelLayer:
+      if (m_Layer != index.getLayer())
+        return true;
+      [[fallthrough]];
+    case c_IndexLevelSector:
+      if (m_Sector != index.getSector())
+        return true;
+      [[fallthrough]];
+    case c_IndexLevelEndcap:
+      if (m_Endcap != index.getEndcap())
+        return true;
+  }
   return false;
 }
 

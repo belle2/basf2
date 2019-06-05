de0b1115dcae2078f61b3f58a594da7bdac51823
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2019-06-06 07:14:52 +0900

    use add_cosmics_reconstruction instead of add_reconstruction for cosmics

--- reconstruction/tests/reco_raw.py
+++ reconstruction/tests/reco_raw.py
@@ -13,4 +13,5 @@ if 'BELLE2_VALIDATION_DATA_DIR' not in os.environ:
 steering = Belle2.FileSystem.findFile('reconstruction/tests/reco_raw.py_noexec')
 
 for data_file in glob.glob(os.environ['BELLE2_VALIDATION_DATA_DIR'] + '/rawdata/*HLT?.*.root'):
-    assert(0 == os.system("basf2 -n 10 -i " + data_file + ' ' + steering))
+    data_type = os.path.basename(data_file).split('.')[0]
+    assert(0 == os.system("basf2 -n 10 -i " + data_file + ' ' + steering + ' ' + data_type))

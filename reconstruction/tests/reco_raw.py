202b6f3cd2f4a7599ade2e88f74d49e6b9cbe417
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2019-06-05 11:33:48 +0200

    tests of reconstruction of raw data with validation GT added

---
+++ reconstruction/tests/reco_raw.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import glob
+from ROOT import Belle2
+
+if 'BELLE2_VALIDATION_DATA_DIR' not in os.environ:
+    print("TEST SKIPPED: BELLE2_VALIDATION_DATA_DIR environment variable not set.", file=sys.stderr)
+    sys.exit(1)
+
+steering = Belle2.FileSystem.findFile('reconstruction/tests/reco_raw.py_noexec')
+
+for data_file in glob.glob(os.environ['BELLE2_VALIDATION_DATA_DIR'] + '/rawdata/*HLT?.*.root'):
+    assert(0 == os.system("basf2 -n 10 -i " + data_file + ' ' + steering))

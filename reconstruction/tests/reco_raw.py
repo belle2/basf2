bac30a5de5d03a77b8c68e3e8d35dbaa052045c9
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2019-06-06 22:36:50 +0200

    use subprocess and b2test_utils

--- reconstruction/tests/reco_raw.py
+++ reconstruction/tests/reco_raw.py
@@ -4,14 +4,15 @@
 import os
 import sys
 import glob
+import subprocess
+import b2test_utils
 from ROOT import Belle2
 
 if 'BELLE2_VALIDATION_DATA_DIR' not in os.environ:
-    print("TEST SKIPPED: BELLE2_VALIDATION_DATA_DIR environment variable not set.", file=sys.stderr)
-    sys.exit(1)
+    b2test_utils.skip_test("BELLE2_VALIDATION_DATA_DIR environment variable not set.")
 
 steering = Belle2.FileSystem.findFile('reconstruction/tests/reco_raw.py_noexec')
 
 for data_file in glob.glob(os.environ['BELLE2_VALIDATION_DATA_DIR'] + '/rawdata/*HLT?.*.root'):
     data_type = os.path.basename(data_file).split('.')[0]
-    assert(0 == os.system("basf2 -n 10 -i " + data_file + ' ' + steering + ' ' + data_type))
+    assert(0 == subprocess.check_call(['basf2', '-n', '10', '-i', data_file, steering, data_type]))

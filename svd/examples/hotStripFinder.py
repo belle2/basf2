5e93be15a0ba69ffbd88cb32a34836c0b5dbef5c
Laura Zani <laura.zani@pi.infn.it>
2019-06-12 19:24:20 +0900

    Corrected bug in the steering python, before unpacking the RootInput branchname should be RawSVDs

--- svd/examples/hotStripFinder.py
+++ svd/examples/hotStripFinder.py
@@ -13,13 +13,18 @@ import glob
 #
 #######
 
+# set here the correct exp and run number you like to process
+exp = 8
+run = 2265
+
 # set this string to identify the output rootfiles
-outputfile = "SVDHotStripFinderZS5_exp8run2265.root"
+outputfile = "SVDHotStripFinderZS5_exp" + str(exp) + "run" + str(run) + "_V2.root"
 
 use_database_chain()
-use_central_database("data_reprocessing_prompt_bucket6")
+# use_central_database("data_reprocessing_prompt_bucket6")
+use_central_database("data_reprocessing_prompt")
 use_central_database("svd_basic")
-use_central_database("svd_onlySVDinGeoConfiguration")
+# use_central_database("svd_onlySVDinGeoConfiguration")
 
 main = create_path()
 
@@ -28,11 +33,11 @@ set_random_seed(1)
 main.add_module(
     'RootInput',
     #    inputFileName="~/svd/BII-3221-svd-hot-strip-masking/buc6/r03844/all/cdst/sub00/cdst.physics.0007.03844.HLT*",
-    inputFileName="/ghi/fs01/belle2/bdata/Data/Raw/e0008/r02265/sub00/physics.0008.02265.HLT*",
-    # luminosity run 2265, exp8, raw data:add unpacker
-    # inputFileName="/ghi/fs01/belle2/bdata/Data/Raw/e0008/r02308/sub00/cosmic.0008.02308.HLT*",
-    # # cosmic run 2308, exp8, raw data:add unpacker
-    branchNames=['SVDShaperDigits'])
+    # luminosity run 2265, exp8, raw data
+    # cosmic run 2308, exp8, raw data
+    inputFileName="/ghi/fs01/belle2/bdata/Data/Raw/e000" + \
+    str(exp) + "/r0" + str(run) + "/sub00/physics.000" + str(exp) + ".0" + str(run) + ".HLT*",
+    branchNames=['RawSVDs'])
 
 main.add_module('Gearbox')
 main.add_module('Geometry')
@@ -48,7 +53,7 @@ main.add_module(
 
 # default parameters
 main.add_module('SVDHotStripFinder', ShaperDigits='SVDShaperDigitsZS5', outputFileName=outputfile, searchBase=0,
-                threshold=1.0, absOccThreshold=0.20, relOccPrec=5, firstExp=8, firstRun=2265, useHSFinderV1=True)
+                threshold=1.0, absOccThreshold=0.20, relOccPrec=5, firstExp=8, firstRun=2265, useHSFinderV1=False)
 
 main.add_module('Progress')
 

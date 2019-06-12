3d6bd3bd5e2f5669abdc158b2f3ba4e36947934d
Giulia Casarosa <giulia.casarosa@desy.de>
2019-06-12 22:02:33 +0900

    occupancy distribution histos + histogram colors

--- svd/examples/hotStripFinder.py
+++ svd/examples/hotStripFinder.py
@@ -18,7 +18,8 @@ outputfile = "SVDHotStripFinderZS5_exp8run2265.root"
 
 use_database_chain()
 use_central_database("data_reprocessing_prompt_bucket6")
-use_central_database("svd_basic")
+# use_central_database("svd_basic")
+use_local_database("centraldb/database_ok.txt", "centraldb")
 use_central_database("svd_onlySVDinGeoConfiguration")
 
 main = create_path()
@@ -28,11 +29,11 @@ set_random_seed(1)
 main.add_module(
     'RootInput',
     #    inputFileName="~/svd/BII-3221-svd-hot-strip-masking/buc6/r03844/all/cdst/sub00/cdst.physics.0007.03844.HLT*",
-    inputFileName="/ghi/fs01/belle2/bdata/Data/Raw/e0008/r02265/sub00/physics.0008.02265.HLT*",
+    inputFileName="/ghi/fs01/belle2/bdata/Data/Raw/e0008/r02265/sub00/physics.0008.02265.HLT1.f00000.root",
     # luminosity run 2265, exp8, raw data:add unpacker
     # inputFileName="/ghi/fs01/belle2/bdata/Data/Raw/e0008/r02308/sub00/cosmic.0008.02308.HLT*",
     # # cosmic run 2308, exp8, raw data:add unpacker
-    branchNames=['SVDShaperDigits'])
+    branchNames=['RawSVDs'])
 
 main.add_module('Gearbox')
 main.add_module('Geometry')
@@ -48,7 +49,7 @@ main.add_module(
 
 # default parameters
 main.add_module('SVDHotStripFinder', ShaperDigits='SVDShaperDigitsZS5', outputFileName=outputfile, searchBase=0,
-                threshold=1.0, absOccThreshold=0.20, relOccPrec=5, firstExp=8, firstRun=2265, useHSFinderV1=True)
+                threshold=1.0, absOccThreshold=0.20, relOccPrec=5, firstExp=8, firstRun=2265, useHSFinderV1=False)
 
 main.add_module('Progress')
 

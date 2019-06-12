54aaf914482561a2970c1274473e2873e2a95428
Giulia Casarosa <giulia.casarosa@desy.de>
2019-06-13 06:57:48 +0900

    different rootfile names for the two algorithms + bool param in the script to choose algorithm

--- svd/examples/hotStripFinder.py
+++ svd/examples/hotStripFinder.py
@@ -15,8 +15,10 @@ import glob
 
 # set here the correct exp and run number you like to process
 exp = 8
-run = 2308  # 2265
+run = 2265  # 2308  # 2265
 runTpe = ""
+
+useLauraAlg = True
 
 if run == 2308:
     runType = "cosmic"
@@ -26,13 +28,15 @@ else:
     print("Check the run number!!")
 
 # set this string to identify the output rootfiles
-outputfile = "SVDHotStripFinderZS5_exp" + str(exp) + "run" + str(run) + "_V2.root"
+outputfile = "SVDHotStripFinderZS5_exp" + str(exp) + "run" + str(run) + "_V1.root"
+if useLauraAlg:
+    outputfile = "SVDHotStripFinderZS5_exp" + str(exp) + "run" + str(run) + "_V2.root"
 
 use_database_chain()
 use_central_database("data_reprocessing_prompt_bucket6")
 # use_central_database("svd_basic")
-use_local_database("/home/belle2/casarosa/master/hotStrips/centraldb/database_ok.txt",
-                   "/home/belle2/casarosa/master/hotStrips/centraldb")
+# use_local_database("/home/belle2/casarosa/master/hotStrips/centraldb/database_ok.txt",
+#                   "/home/belle2/casarosa/master/hotStrips/centraldb")
 use_central_database("svd_onlySVDinGeoConfiguration")
 
 use_local_database("localDB_HSF/database.txt", "localDB_HSF")
@@ -47,7 +51,7 @@ main.add_module(
     # luminosity run 2265, exp8, raw data
     # cosmic run 2308, exp8, raw data
     inputFileName="/ghi/fs01/belle2/bdata/Data/Raw/e000" + \
-    str(exp) + "/r0" + str(run) + "/sub00/" + str(runType) + ".000" + str(exp) + ".0" + str(run) + ".HLT1.f00000.root",
+    str(exp) + "/r0" + str(run) + "/sub00/" + str(runType) + ".000" + str(exp) + ".0" + str(run) + ".HLT*.root",
     branchNames=['RawSVDs'])
 
 main.add_module('Gearbox')
@@ -64,7 +68,7 @@ main.add_module(
 
 # default parameters
 main.add_module('SVDHotStripFinder', ShaperDigits='SVDShaperDigitsZS5', outputFileName=outputfile, searchBase=0,
-                threshold=1.0, absOccThreshold=0.20, relOccPrec=5, useHSFinderV1=False)
+                threshold=1, absOccThreshold=0.20, relOccPrec=5, useHSFinderV1=not useLauraAlg)
 
 main.add_module('Progress')
 

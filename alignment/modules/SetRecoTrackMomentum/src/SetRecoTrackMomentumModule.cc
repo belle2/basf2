989839540ee63bb35ba75e02804b8afacb271a19
Tadeas Bilka <tadeas.bilka@desy.de>
2019-06-24 05:27:54 +0900

    Implementation done. Needs a test now.

--- alignment/modules/SetRecoTrackMomentum/src/SetRecoTrackMomentumModule.cc
+++ alignment/modules/SetRecoTrackMomentum/src/SetRecoTrackMomentumModule.cc
@@ -9,6 +9,7 @@
 **************************************************************************/
 
 #include <alignment/modules/SetRecoTrackMomentum/SetRecoTrackMomentumModule.h>
+#include <framework/geometry/BFieldManager.h>
 
 using namespace Belle2;
 
@@ -24,15 +25,15 @@ REG_MODULE(SetRecoTrackMomentum)
 SetRecoTrackMomentumModule::SetRecoTrackMomentumModule() : Module()
 {
   // Set module properties
-  setDescription(R"DOC("Set momentum magnitude for RecoTracks to given value (for runs without magnetic field)
+  setDescription(R"DOC("Set momentum magnitude for RecoTracks seed to given value (for runs without magnetic field)
 
-  Take the momentum direction and update its magnitude to artificial value for all RecoTracks - needed for tracks without magnetic field.
+  Take the momentum direction from seed and update its magnitude to artificial value for all RecoTracks - needed for tracks without magnetic field.
   By default activated automatically, when zero B-field is detected at the origin
   )DOC");
 
   // Parameter definitions
   addParam("automatic", m_automatic, "Detect the B-field at origin automatically - disable module if non-zero", true);
-  addParam("momentum", m_momentum, "Default momentum magnitude (GeV/c) to set for RecoTracks", 10.);
+  addParam("momentum", m_momentum, "Default momentum magnitude (GeV/c) to set for seed of RecoTracks", 10.);
 
 }
 
@@ -43,6 +44,17 @@ void SetRecoTrackMomentumModule::initialize()
 
 void SetRecoTrackMomentumModule::event()
 {
+  // In automatic mode, do nothing if B-field > 0 at origin
+  if (m_automatic && BFieldManager::getInstance().getField(TVector3(0., 0., 0.)).Mag() > 1.e-14) {
+    return;
+  }
+
+  for (auto& track : m_tracks) {
+    TVector3 mom = track.getMomentumSeed();
+    mom = 1. / mom.Mag() * m_momentum * mom;
+
+    track.setPositionAndMomentum(track.getPositionSeed(), mom);
+  }
 }
 
 

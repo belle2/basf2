b7b5668def4962504a9cf4e2c3e7418d19e7d6c7
Michael Eliachevitch <michael.eliachevitch@kit.edu>
2019-07-03 15:49:17 +0200

    Fix & improve catching errors when modules not found

--- tracking/scripts/tracking/train/combined_quality_estimator_teacher.py
+++ tracking/scripts/tracking/train/combined_quality_estimator_teacher.py
@@ -6,20 +6,17 @@ import basf2_mva
 import simulation
 import tracking
 import tracking.root_utils as root_utils
-from tracking.harvesting_validation.combined_module import (
-    CombinedTrackingValidationModule,
-)
+from tracking.harvesting_validation.combined_module import CombinedTrackingValidationModule
 
+# wrap python modules that are used here but not in the externals into a try except block
+install_helpstring_formatter = ("\nCould not find {module} python module.Try installing it via\n"
+                                "  python3 -m pip install [--user] {module}\n")
 try:
     import b2luigi as luigi
     from b2luigi.basf2_helper import Basf2PathTask, Basf2Task
-except ImportError as e:
-    print(
-        "Could not import ``b2luigi`` python package. Try running\n"
-        "  python3 -m pip3 install [--user] b2luigi\n"
-        "to install it from PiPI."
-    )
-    raise e
+except ModuleNotFoundError:
+    print(install_helpstring_formatter.format(module="b2luigi"))
+    raise
 
 
 def my_basf2_mva_teacher(

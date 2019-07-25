c6b5a2f007b95f3162b8beb4fe44a954b6eed760
Dmitrii Neverov <dmitrii.neverov@desy.de>
2019-07-25 16:37:21 +0900

    calculate deltaZ -> calculate Z

--- tracking/trackFindingCDC/hough/algorithms/include/HitInHyperBox.h
+++ tracking/trackFindingCDC/hough/algorithms/include/HitInHyperBox.h
@@ -23,7 +23,7 @@ namespace Belle2 {
     /** An algorithm to check if a hit is contained in a hyperbolic cosine hough box
     *
     * The parameterization is
-    * z(R) = 1/rho * sqrt(1 + mu**2 * rho**2) * (cosh(rho(R - alpha) - cosh(rho*alpha))
+    * z(R) = 1/rho * sqrt(1 + mu**2 * rho**2) * (cosh(rho(R - alpha)) - cosh(rho*alpha))
     *
     * rho is g*c*B / p_t
     * mu is m / g*c*B
@@ -53,14 +53,14 @@ namespace Belle2 {
         const double perpS = recoHit.getArcLength2D();
         const double recoZ = recoHit.getRecoZ();
 
-        const bool sameSign = SameSignChecker::sameSign(deltaZ(lowerAlpha, lowerMu, lowerRho, perpS, recoZ),
-                                                        deltaZ(lowerAlpha, lowerMu, upperRho, perpS, recoZ),
-                                                        deltaZ(lowerAlpha, upperMu, lowerRho, perpS, recoZ),
-                                                        deltaZ(lowerAlpha, upperMu, upperRho, perpS, recoZ),
-                                                        deltaZ(upperAlpha, lowerMu, lowerRho, perpS, recoZ),
-                                                        deltaZ(upperAlpha, lowerMu, upperRho, perpS, recoZ),
-                                                        deltaZ(upperAlpha, upperMu, lowerRho, perpS, recoZ),
-                                                        deltaZ(upperAlpha, upperMu, upperRho, perpS, recoZ));
+        const bool sameSign = SameSignChecker::sameSign(catZ(lowerAlpha, lowerMu, lowerRho, perpS) - recoZ,
+                                                        catZ(lowerAlpha, lowerMu, upperRho, perpS) - recoZ,
+                                                        catZ(lowerAlpha, upperMu, lowerRho, perpS) - recoZ,
+                                                        catZ(lowerAlpha, upperMu, upperRho, perpS) - recoZ,
+                                                        catZ(upperAlpha, lowerMu, lowerRho, perpS) - recoZ,
+                                                        catZ(upperAlpha, lowerMu, upperRho, perpS) - recoZ,
+                                                        catZ(upperAlpha, upperMu, lowerRho, perpS) - recoZ,
+                                                        catZ(upperAlpha, upperMu, upperRho, perpS) - recoZ);
         if (not sameSign) {
           return 1.0;
         } else {
@@ -84,20 +84,19 @@ namespace Belle2 {
         const double lhsS = lhsRecoHit.getArcLength2D();
         const double rhsS = rhsRecoHit.getArcLength2D();
 
-        const double lhsZDistance = deltaZ(centerAlpha, centerMu, centerRho, lhsS, lhsZ);
-        const double rhsZDistance = deltaZ(centerAlpha, centerMu, centerRho, rhsS, rhsZ);
+        const double lhsZDistance = catZ(centerAlpha, centerMu, centerRho, lhsS) - lhsZ;
+        const double rhsZDistance = catZ(centerAlpha, centerMu, centerRho, rhsS) - rhsZ;
 
         return lhsZDistance < rhsZDistance;
       }
 
     private:
-      static double deltaZ(const double alpha, const double mu, const double rho, const double R, const double Z)
+      static double catZ(const double alpha, const double mu, const double rho, const double R)
       {
         //100 here is a reference  - size of CDC in cm
-        return 100.0 * (1.0 / rho) * std::sqrt(1 + mu * mu * rho * rho) *
-               (std::cosh(rho * (R / 100.0 - alpha)) -
-                std::cosh(alpha * rho)
-               ) - Z;
+        return 100.0 / rho * std::sqrt(1.0 + mu * mu * rho * rho) *
+               (std::cosh((R / 100.0 - alpha) * rho) -
+                std::cosh(alpha * rho));
       }
     };
   }

c445ae1b086e1b495960fe4b64f26c01b5343594
Nils Braun <nils.braun@kit.edu>
2019-07-27 17:29:44 +0900

    Add the ZMQ HLT modules building on top of the implemented connections

---
+++ daq/hbasf2/modules/include/HLTZMQ2DsDirect.h
@@ -0,0 +1,57 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2018 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Nils Braun, Anselm Baur                                  *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+#include <framework/core/Module.h>
+
+#include <daq/hbasf2/utils/HLTStreamHelper.h>
+#include <framework/pcore/zmq/connections/ZMQLoadBalancedConnection.h>
+
+#include <framework/pcore/zmq/utils/ZMQParent.h>
+
+#include <memory>
+#include <string>
+
+namespace Belle2 {
+  /**
+   * Special ZMQ2Ds module without the HLT-specific handling of initialization and begin/end run.
+   * Basically just receives and event from ZMQ and stores it to the datastore without further processing.
+   * Similar to the former Rx module.
+   */
+  class HLTZMQ2DsDirectModule : public Module {
+  public:
+    /// Register the module parameters
+    HLTZMQ2DsDirectModule();
+
+    /// Receive the first event and initialize the data store with it
+    void initialize() override;
+    /// If not in the first event, receive an event and store in the DS
+    void event() override;
+
+  private:
+    /// ZMQ Parent needed for the connections
+    std::shared_ptr<ZMQParent> m_parent;
+    /// Load balanced connection to the previous ZMQ application
+    std::unique_ptr<ZMQLoadBalancedInput> m_input;
+
+    /// Utility class for deserialization
+    HLTStreamHelper m_streamHelper;
+
+    /// Are we still in the first real event?
+    bool m_firstEvent = true;
+
+    /// Module parameter: ZMQ address of the input ZMQ application
+    std::string m_param_input;
+    /// Module parameter: how many events should be kept in flight. Has an impact on the stopping time as well as the rate stability
+    unsigned int m_param_bufferSize = 20;
+
+    void readEvent();
+  };
+}

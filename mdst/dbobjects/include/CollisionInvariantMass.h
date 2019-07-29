98d4bd159ac217e716d6311dbe744c73323f86f0
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2019-07-29 18:35:12 +0200

    mass spread added to CollisionInvariantMass

--- mdst/dbobjects/include/CollisionInvariantMass.h
+++ mdst/dbobjects/include/CollisionInvariantMass.h
@@ -27,10 +27,11 @@ namespace Belle2 {
      * @param mass invariant mass of the colliding beams
      * @param error uncertainty of invariant mass
      */
-    void setMass(double mass, double error)
+    void setMass(double mass, double error, double spread)
     {
       m_mass = mass;
       m_massError = error;
+      m_massSpread = spread;
     }
 
     /** Get the measured average CMS energy */
@@ -45,6 +46,12 @@ namespace Belle2 {
       return m_massError;
     }
 
+    /** Get the spread of the measured CMS energy */
+    double getMassSpread() const
+    {
+      return m_massError;
+    }
+
   private:
 
     /** Average center-of-mass energy = invariant mass of the colliding beams */
@@ -52,6 +59,9 @@ namespace Belle2 {
 
     /** Uncertainty of the average center-of-mass energy / invariant mass of the colliding beams */
     Double32_t m_massError{0};
+
+    /** Width of the distribution of the center-of-mass energy / invariant mass of the colliding beams */
+    Double32_t m_massSpread{0};
 
     ClassDef(CollisionInvariantMass, 1); /**< Measured average invariant mass of the colliding beams and its uncertainty **/
   };

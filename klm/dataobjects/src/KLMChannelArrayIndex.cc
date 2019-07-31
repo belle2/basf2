495b682b6e466f2604ccccd5dbec903adb74890f
Kirill Chilikin <chilikin@lebedev.ru>
2019-07-31 09:49:47 +0300

    Fix condition in KLMChannelArrayIndex.

--- klm/dataobjects/src/KLMChannelArrayIndex.cc
+++ klm/dataobjects/src/KLMChannelArrayIndex.cc
@@ -52,7 +52,7 @@ uint16_t KLMChannelArrayIndex::getNumber(uint16_t index) const
 {
   std::map<uint16_t, uint16_t>::const_iterator it =
     m_MapIndexNumber.find(index);
-  if (it == m_MapNumberIndex.end())
+  if (it == m_MapIndexNumber.end())
     B2FATAL("Incorrect KLM channel index.");
   return it->second;
 }

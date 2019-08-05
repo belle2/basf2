bdd6aebde07407d5b45aa734dde8018b68f4c0aa
Sasha Glazov <glazov@mail.desy.de>
2019-08-05 21:05:50 +0200

    Initial implementation of the CDC cross talk filter which targets ASIC cross talk

---
+++ tracking/trackFindingCDC/findlets/minimal/include/AsicBackgroundDetector.h
@@ -0,0 +1,72 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2012 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Sasha Glazov, Tracking group                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+#include <vector>
+#include <string>
+#include <map>
+#include <tracking/trackFindingCDC/findlets/base/Findlet.h>
+#include <framework/database/DBArray.h>
+#include <cdc/dbobjects/CDCChannelMap.h>
+
+namespace Belle2 {
+
+  namespace TrackFindingCDC {
+    class CDCWireHit;
+
+    /// Marks hits as background based on the result of a filter
+    class AsicBackgroundDetector : public Findlet<CDCWireHit&> {
+    private:
+      /// Type of the base class
+      using Super = Findlet<CDCWireHit&>;
+
+    public:
+      /// Default constructor
+      AsicBackgroundDetector() {}
+
+      /// Access database here:
+      void initialize() final;
+
+      /// Terimante
+      void terminate() final;
+
+      /// Reload channel map if needed
+      void beginRun() final;
+
+      /// Short description of the findlet
+      std::string getDescription() final;
+
+      /// Expose the parameters to a module
+      virtual void exposeParameters(ModuleParamList* moduleParamList, const std::string& prefix)  final;
+
+      /// Main algorithm marking hit as background
+      virtual void apply(std::vector<CDCWireHit>& wireHits) final;
+
+      /// Algorithm marking hit as background for each CDC ASIC
+      virtual void applyAsicFilter(std::vector<CDCWireHit*>& wireHits) final;
+
+    private:
+      /**
+       * Channel map retrieved from DB.
+       */
+      DBArray<CDCChannelMap>* m_channelMapFromDB;
+      /// map from ewire to board/channel ID
+      std::map<int, std::pair<int, int> > m_map;
+
+      /// min. number of hits in ASIC for background check
+      size_t m_minimal_hit_number{5};
+
+      /// distance from median TDC, to be considered as bg.
+      double m_deviation_from_median{3.5};
+
+      /// max. number of signal-like hits in ASIC for background check
+      size_t m_nsignal_max{2};
+    };
+  }
+}

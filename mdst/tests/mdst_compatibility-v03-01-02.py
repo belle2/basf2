1f6b55fe7d3137ae1adf90f23154e08a4c3e9ae6
Ewan Chin Hill <ewan.chin.hill@desy.de>
2019-08-13 00:01:40 +0900

    undoing comment edit

--- mdst/tests/mdst_compatibility-v03-01-02.py
+++ mdst/tests/mdst_compatibility-v03-01-02.py
@@ -7,6 +7,7 @@ Test backwards compatibility for an mdst file produced with release-03-01-02.
 See https://confluence.desy.de/display/BI/Backward+Compatibility if this test fails.
 
 CHANGES since release-03-01-02:
+- ECLCluster:
   + hasFailedFitTime
   + hasFailedTimeResolution
 """

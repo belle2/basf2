0134b1dcdcf867a6e8907d6eb121be62604c83e6
Sviatoslav Bilokin <sviatoslav.bilokin@desy.de>
2019-08-13 13:24:35 +0200

    Resolving issues

--- analysis/tests/roemodules_clone.py
+++ analysis/tests/roemodules_clone.py
@@ -10,10 +10,10 @@ wherever the validation-data are visible  (it's also a bit slower).
 import b2test_utils
 from basf2 import set_random_seed, create_path, process
 
+inputFile = b2test_utils.require_file('mdst12.root', 'validation')
 # make logging more reproducible by replacing some strings
 b2test_utils.configure_logging_for_tests()
 set_random_seed("1337")
-inputFile = b2test_utils.require_file('mdst12.root', 'validation')
 fsps = ['K-', 'pi-', 'gamma', 'K_L0']
 
 ###############################################################################

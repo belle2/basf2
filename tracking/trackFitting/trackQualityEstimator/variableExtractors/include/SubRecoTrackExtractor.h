c69a6f0cf63a6d82d041c4c9cd711278dabb12fe
Michael Eliachevitch <michael.eliachevitch@kit.edu>
2019-09-13 17:07:23 +0200

    Refactor: One statement per line, remove abbrevations, add consts

--- tracking/trackFitting/trackQualityEstimator/variableExtractors/include/SubRecoTrackExtractor.h
+++ tracking/trackFitting/trackQualityEstimator/variableExtractors/include/SubRecoTrackExtractor.h
@@ -76,91 +76,92 @@ namespace Belle2 {
 
       if (SVDRecoTrack and CDCRecoTrack and SVDRecoTrack->wasFitSuccessful() and CDCRecoTrack->wasFitSuccessful()) {
 
-        CDCwall_extrapolation(CDCRecoTrack, SVDRecoTrack);
-        POCA_extrapolation(CDCRecoTrack, SVDRecoTrack);
+        cdcWallExtrapolation(CDCRecoTrack, SVDRecoTrack);
+        pocaExtrapolation(CDCRecoTrack, SVDRecoTrack);
 
       } else {
-        setDiffs("SVD_CDC_CDCwall_Pos", nullptr, nullptr);
-        setDiffs("SVD_CDC_CDCwall_Mom", nullptr, nullptr);
+        setCDCSVDTrackDifferenceVariables("SVD_CDC_CDCwall_Pos", nullptr, nullptr);
+        setCDCSVDTrackDifferenceVariables("SVD_CDC_CDCwall_Mom", nullptr, nullptr);
         m_variables.at("SVD_CDC_CDCwall_Chi2") = -1.;
 
-        setDiffs("SVD_CDC_POCA_Pos", nullptr, nullptr);
-        setDiffs("SVD_CDC_POCA_Mom", nullptr, nullptr);
+        setCDCSVDTrackDifferenceVariables("SVD_CDC_POCA_Pos", nullptr, nullptr);
+        setCDCSVDTrackDifferenceVariables("SVD_CDC_POCA_Mom", nullptr, nullptr);
       }
 
     }
 
   protected:
     /// initialize statistics subsets of variables from clusters that get combined for SPTC
-    void initializeStats(std::string identifier, std::vector<Named<float*>>& variables)
+    void initializeStats(const std::string prefix, std::vector<Named<float*>>& variables)
     {
-      addVariable(identifier + "_diff_Z", variables);
-      addVariable(identifier + "_diff_Pt", variables);
-      addVariable(identifier + "_diff_Theta", variables);
-      addVariable(identifier + "_diff_Phi", variables);
-      addVariable(identifier + "_diff_Mag", variables);
-      addVariable(identifier + "_diff_Eta", variables);
+      addVariable(prefix + "_diff_Z", variables);
+      addVariable(prefix + "_diff_Pt", variables);
+      addVariable(prefix + "_diff_Theta", variables);
+      addVariable(prefix + "_diff_Phi", variables);
+      addVariable(prefix + "_diff_Mag", variables);
+      addVariable(prefix + "_diff_Eta", variables);
     }
 
     /// calculated differences and saves them in variable set
-    void setDiffs(std::string identifier, const TVector3* svd_vec, const TVector3* cdc_vec)
+    void setCDCSVDTrackDifferenceVariables(const std::string prefix,
+                                           const TVector3* svdTrackVector,
+                                           const TVector3* cdcTrackVector)
     {
-      if (not(svd_vec and cdc_vec)) {
-        m_variables.at(identifier + "_diff_Z") = -1.;
-        m_variables.at(identifier + "_diff_Pt") = -1.;
-        m_variables.at(identifier + "_diff_Theta") = -1.;
-        m_variables.at(identifier + "_diff_Phi") = -1.;
-        m_variables.at(identifier + "_diff_Mag") = -1.;
-        m_variables.at(identifier + "_diff_Eta") = -1.;
+      if (not(svdTrackVector and cdcTrackVector)) {
+        m_variables.at(prefix + "_diff_Z") = -1.;
+        m_variables.at(prefix + "_diff_Pt") = -1.;
+        m_variables.at(prefix + "_diff_Theta") = -1.;
+        m_variables.at(prefix + "_diff_Phi") = -1.;
+        m_variables.at(prefix + "_diff_Mag") = -1.;
+        m_variables.at(prefix + "_diff_Eta") = -1.;
         return;
       }
-
-      m_variables.at(identifier + "_diff_Z") = fabs(cdc_vec->Z() - svd_vec->Z());
-      m_variables.at(identifier + "_diff_Pt") = fabs(cdc_vec->Pt() - svd_vec->Pt());
-      m_variables.at(identifier + "_diff_Theta") = fabs(cdc_vec->Theta() - svd_vec->Theta());
-      m_variables.at(identifier + "_diff_Phi") = fabs(cdc_vec->Phi() - svd_vec->Phi());
-      m_variables.at(identifier + "_diff_Mag") = fabs(cdc_vec->Mag() - svd_vec->Mag());
-      m_variables.at(identifier + "_diff_Eta") = fabs(cdc_vec->Eta() - svd_vec->Eta());
-
+      m_variables.at(prefix + "_diff_Z") = fabs(cdcTrackVector->Z() - svdTrackVector->Z());
+      m_variables.at(prefix + "_diff_Pt") = fabs(cdcTrackVector->Pt() - svdTrackVector->Pt());
+      m_variables.at(prefix + "_diff_Theta") = fabs(cdcTrackVector->Theta() - svdTrackVector->Theta());
+      m_variables.at(prefix + "_diff_Phi") = fabs(cdcTrackVector->Phi() - svdTrackVector->Phi());
+      m_variables.at(prefix + "_diff_Mag") = fabs(cdcTrackVector->Mag() - svdTrackVector->Mag());
+      m_variables.at(prefix + "_diff_Eta") = fabs(cdcTrackVector->Eta() - svdTrackVector->Eta());
     }
 
-    void CDCwall_extrapolation(RecoTrack const* CDCRecoTrack, RecoTrack const* SVDRecoTrack)
+    void cdcWallExtrapolation(RecoTrack const* CDCRecoTrack, RecoTrack const* SVDRecoTrack)
     {
       // position and momentum used for extrapolations to the CDC Wall
       TVector3 center(0., 0., 0.);
       TVector3 direction(0., 0., 1.);
 
-      genfit::MeasuredStateOnPlane svd_sop; genfit::MeasuredStateOnPlane cdc_sop;
+      genfit::MeasuredStateOnPlane svdTrackStateOnPlane;
+      genfit::MeasuredStateOnPlane cdcTrackStateOnPlane;
       try {
         // do extrapolation of SVD and CDC onto CDCwall
-        svd_sop = SVDRecoTrack->getMeasuredStateOnPlaneFromLastHit();
-        cdc_sop = CDCRecoTrack->getMeasuredStateOnPlaneFromFirstHit();
-        cdc_sop.extrapolateToCylinder(m_CDC_wall_radius, center, direction);
-        svd_sop.extrapolateToPlane(cdc_sop.getPlane());
+        svdTrackStateOnPlane = SVDRecoTrack->getMeasuredStateOnPlaneFromLastHit();
+        cdcTrackStateOnPlane = CDCRecoTrack->getMeasuredStateOnPlaneFromFirstHit();
+        cdcTrackStateOnPlane.extrapolateToCylinder(m_CDC_wall_radius, center, direction);
+        svdTrackStateOnPlane.extrapolateToPlane(cdcTrackStateOnPlane.getPlane());
       } catch (genfit::Exception const& e) {
         // extrapolation not possible, skip this track
         B2WARNING("SubRecoTrackExtractor: SVDRecoTrack and/or CDCRecoTrack extrapolation to the CDCwall failed!\n"
                   << "-->" << e.what());
-        setDiffs("SVD_CDC_CDCwall_Pos", nullptr, nullptr);
-        setDiffs("SVD_CDC_CDCwall_Mom", nullptr, nullptr);
+        setCDCSVDTrackDifferenceVariables("SVD_CDC_CDCwall_Pos", nullptr, nullptr);
+        setCDCSVDTrackDifferenceVariables("SVD_CDC_CDCwall_Mom", nullptr, nullptr);
         m_variables.at("SVD_CDC_CDCwall_Chi2") = -1.;
         return;
       }
 
       // set differences in Position and Momentum
-      TVector3 svd_pos = svd_sop.getPos(); TVector3 svd_mom = svd_sop.getMom();
-      TVector3 cdc_pos = cdc_sop.getPos(); TVector3 cdc_mom = cdc_sop.getMom();
-      setDiffs("SVD_CDC_CDCwall_Pos", &svd_pos, &cdc_pos);
-      setDiffs("SVD_CDC_CDCwall_Mom", &svd_mom, &cdc_mom);
-
+      const TVector3 svdTrackPositionOnPlaneAtCDCWall = svdTrackStateOnPlane.getPos();
+      const TVector3 svdTrackMomentumOnPlaneAtCDCWall = svdTrackStateOnPlane.getMom();
+      const TVector3 cdcTrackPositionOnPlaneAtCDCWall = cdcTrackStateOnPlane.getPos();
+      const TVector3 cdcTrackMomentumOnPlaneAtCDCWall = cdcTrackStateOnPlane.getMom();
+      setCDCSVDTrackDifferenceVariables("SVD_CDC_CDCwall_Pos", &svdTrackPositionOnPlaneAtCDCWall, &cdcTrackPositionOnPlaneAtCDCWall);
+      setCDCSVDTrackDifferenceVariables("SVD_CDC_CDCwall_Mom", &svdTrackMomentumOnPlaneAtCDCWall, &cdcTrackMomentumOnPlaneAtCDCWall);
       try {
         // calculate chi2 between SVD and CDC (like in VXDCDCTrackMerger)
-        TMatrixDSym inv_covmtrx = (svd_sop.getCov() + cdc_sop.getCov()).Invert();
-        TVectorD state_diff = cdc_sop.getState() - svd_sop.getState();
-        state_diff *= inv_covmtrx;
-        float chi_2 = state_diff * (cdc_sop.getState() - svd_sop.getState());
-
-        m_variables.at("SVD_CDC_CDCwall_Chi2") = chi_2;
+        const TMatrixDSym invCovariance = (svdTrackStateOnPlane.getCov() + cdcTrackStateOnPlane.getCov()).Invert();
+        TVectorD stateDifference = cdcTrackStateOnPlane.getState() - svdTrackStateOnPlane.getState();
+        stateDifference *= invCovariance;
+        const float chi2 = stateDifference * (cdcTrackStateOnPlane.getState() - svdTrackStateOnPlane.getState());
+        m_variables.at("SVD_CDC_CDCwall_Chi2") = chi2;
 
       } catch (genfit::Exception const& e) {
         // extrapolation not possible, skip this track
@@ -171,38 +172,40 @@ namespace Belle2 {
       }
     }
 
-    void POCA_extrapolation(RecoTrack const* CDCRecoTrack, RecoTrack const* SVDRecoTrack)
+    void pocaExtrapolation(RecoTrack const* CDCRecoTrack, RecoTrack const* SVDRecoTrack)
     {
       // position and momentum used for extrapolations to the CDC Wall
-      TVector3 linePoint(0., 0., 0.);
-      TVector3 lineDirection(0., 0., 1.);
+      const TVector3 linePoint(0., 0., 0.);
+      const TVector3 lineDirection(0., 0., 1.);
 
-      genfit::MeasuredStateOnPlane svd_sop; genfit::MeasuredStateOnPlane cdc_sop;
+      genfit::MeasuredStateOnPlane svdTrackStateOnPlane;
+      genfit::MeasuredStateOnPlane cdcTrackStateOnPlane;
       try {
         // do extrapolation of SVD and CDC onto CDCwall
-        svd_sop = SVDRecoTrack->getMeasuredStateOnPlaneFromFirstHit();
-        cdc_sop = CDCRecoTrack->getMeasuredStateOnPlaneFromFirstHit();
-        cdc_sop.extrapolateToLine(linePoint, lineDirection);
-        svd_sop.extrapolateToLine(linePoint, lineDirection);
+        svdTrackStateOnPlane = SVDRecoTrack->getMeasuredStateOnPlaneFromFirstHit();
+        cdcTrackStateOnPlane = CDCRecoTrack->getMeasuredStateOnPlaneFromFirstHit();
+        cdcTrackStateOnPlane.extrapolateToLine(linePoint, lineDirection);
+        svdTrackStateOnPlane.extrapolateToLine(linePoint, lineDirection);
       } catch (genfit::Exception const& e) {
         // extrapolation not possible, skip this track
         B2WARNING("SubRecoTrackExtractor: SVDRecoTrack and/or CDCRecoTrack extrapolation to POCA failed!\n"
                   << "-->" << e.what());
-        setDiffs("SVD_CDC_POCA_Pos", nullptr, nullptr);
-        setDiffs("SVD_CDC_POCA_Mom", nullptr, nullptr);
+        setCDCSVDTrackDifferenceVariables("SVD_CDC_POCA_Pos", nullptr, nullptr);
+        setCDCSVDTrackDifferenceVariables("SVD_CDC_POCA_Mom", nullptr, nullptr);
         return;
       }
 
       // set differences in Position and Momentum
-      TVector3 svd_pos = svd_sop.getPos(); TVector3 svd_mom = svd_sop.getMom();
-      TVector3 cdc_pos = cdc_sop.getPos(); TVector3 cdc_mom = cdc_sop.getMom();
-      setDiffs("SVD_CDC_POCA_Pos", &svd_pos, &cdc_pos);
-      setDiffs("SVD_CDC_POCA_Mom", &svd_mom, &cdc_mom);
+      const TVector3 svdTrackPositionOnPlaneAtPOCA = svdTrackStateOnPlane.getPos();
+      const TVector3 svdTrackMomentumOnPlaneAtPOCA = svdTrackStateOnPlane.getMom();
+      const TVector3 cdcTrackPositionOnPlaneAtPOCA = cdcTrackStateOnPlane.getPos();
+      const TVector3 cdcTrackMomentumOnPlaneAtPOCA = cdcTrackStateOnPlane.getMom();
+      setCDCSVDTrackDifferenceVariables("SVD_CDC_POCA_Pos", &svdTrackPositionOnPlaneAtPOCA, &cdcTrackPositionOnPlaneAtPOCA);
+      setCDCSVDTrackDifferenceVariables("SVD_CDC_POCA_Mom", &svdTrackMomentumOnPlaneAtPOCA, &cdcTrackMomentumOnPlaneAtPOCA);
     }
 
   private:
     /** Radius of the inner CDC wall in centimeters */
-    double m_CDC_wall_radius = 16.25;
-
+    const double m_CDC_wall_radius = 16.25;
   };
 }

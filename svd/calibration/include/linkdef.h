e87a97c9fef92d1a958616c7e0170df9e97b5eba
Laura Zani <laura.zani@pi.infn.it>
2019-09-13 16:37:42 +0900

    Added SVDHotStrips algortihm class to the linkdef.h

--- svd/calibration/include/linkdef.h
+++ svd/calibration/include/linkdef.h
@@ -21,5 +21,6 @@
 #pragma link C++ class Belle2::SVDCrossTalkStripsCalibrations;
 #pragma link C++ class Belle2::SVDOccupancyCalibrations;
 #pragma link C++ class Belle2::SVDOccupancyCalibrationsAlgorithm;
+#pragma link C++ class Belle2::SVDHotStripsCalibrationsAlgorithm;
 
 #endif

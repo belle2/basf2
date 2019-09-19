39cc99e9e56350ab90e22729026485ca6b000122
jkrohn <jo-frederik.krohn@desy.de>
2019-09-19 14:39:05 +0900

    do not except fits with very small covariance

--- analysis/VertexFitting/TreeFitter/src/FitParams.cc
+++ analysis/VertexFitting/TreeFitter/src/FitParams.cc
@@ -50,7 +50,7 @@ namespace TreeFitter {
   {
     bool ok = true;
     for (int row = 0; row < m_dim; ++row) {
-      ok = (m_globalCovariance(row, row) >= 0);
+      ok = (m_globalCovariance(row, row) > 0);
       if (!ok) break;
     }
     return ok;

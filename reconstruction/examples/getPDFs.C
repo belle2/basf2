61a1e15ba626c664716e21778ff7c58c06c5cfe2
Jitendra Kumar <jitendra.kumar@desy.de>
2019-09-20 18:38:02 -0400

    improved version

--- reconstruction/examples/getPDFs.C
+++ reconstruction/examples/getPDFs.C
@@ -1,23 +1,44 @@
+/*
+  Example macro to draw PDFs histogram from payload
+  Run Instruction: with basf2 environment
+  root -l -b >> .L getPDFs.C++g >> getPDFs()
+ */
+
 #include <iostream>
 #include <vector>
-#include <cmath>
-
 #include "TFile.h"
-#include "TH1F.h"
-#include "TGraph.h"
+#include "TH2F.h"
+#include "TStyle.h"
 #include "TCanvas.h"
+#include <include/reconstruction/dbobjects/DedxPDFs.h>
 
+void getPDFs() {
 
-void getPDFs(){
-
-  TFile* runfile = new TFile("/cvmfs/belle.cern.ch/conditions/dbstore_DedxPDFs_rev_1.root");
+  TFile* runfile = new TFile("~/Belle2Work/GT_Play/centraldb/dbstore_DedxPDFs_rev_12.root");
   Belle2::DedxPDFs* pdfs = (Belle2::DedxPDFs*)runfile->Get("DedxPDFs");
-  const TH2F* cdcpi = pdfs->getCDCPDF(2,1); const  TH2F* cdck = pdfs->getCDCPDF(3,1);
-  TCanvas* can = new TCanvas("can","",1200,600);
-  can->Divide(2,1);
-  can->cd(1);
-  cdcpi->DrawCopy("colz");
-  can->cd(2);
-  cdck->DrawCopy("colz");
-  can->SaveAs("cdc.pdf");
+  std::vector<TH2F*> hDedxPDFs(6);
+
+  std::array<std::string, 6> part = {"Electron", "Muon", "Pion", "Kaon", "Proton", "Deuteron"};
+  std::array<std::string, 3> det = {"CDC", "PXD", "VXD"};
+
+  TCanvas* can = new TCanvas("can", "blah-blah", 1200, 700);
+  can->Divide(3, 2);
+  gStyle->SetOptStat(11);
+
+  for (const auto& idet : det) {
+    for (bool trunmean : { false, true }) {
+      std::stringstream check;
+      check << std::boolalpha << trunmean;
+      for (int iPart = 0; iPart < 6; iPart++) {
+        if (idet.compare("CDC") == 0)hDedxPDFs[iPart] = (TH2F*)pdfs->getCDCPDF(iPart, trunmean);
+        else if (idet.compare("SVD") == 0)hDedxPDFs[iPart] = (TH2F*)pdfs->getSVDPDF(iPart, trunmean);
+        else if (idet.compare("VXD") == 0)hDedxPDFs[iPart] = (TH2F*)pdfs->getPXDPDF(iPart, trunmean);
+        can->cd(iPart + 1);
+        hDedxPDFs[iPart]->SetTitle(Form("%s; p(GeV/c) of %s; dE/dx", hDedxPDFs[iPart]->GetTitle(), part[iPart].data()));
+        hDedxPDFs[iPart]->DrawCopy("colz");
+      }
+      can->SetTitle(Form("Likehood dist. of charged particles from %s, trunmean = %s", idet.data(), check.str().data()));
+      can->SaveAs(Form("Plots_%sDedxPDFs_wTrucMean_%s.pdf", idet.data(), check.str().data()));
+    }
+  }
 }

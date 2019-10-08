8034f1ffd96b85ac5c5bb1d89119876aa9ce7350
staric <marko.staric@ijs.si>
2019-10-08 13:40:16 +0200

    algorithm completed

--- top/calibration/include/TOPPulseHeightAlgorithm.h
+++ top/calibration/include/TOPPulseHeightAlgorithm.h
@@ -11,7 +11,12 @@
 
 #pragma once
 #include <calibration/CalibrationAlgorithm.h>
-#include <TH1F.h>
+#include <TH2F.h>
+#include <TH1D.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <top/dbobjects/TOPCalChannelPulseHeight.h>
+#include <top/dbobjects/TOPCalChannelThresholdEff.h>
 
 namespace Belle2 {
   namespace TOP {
@@ -39,6 +44,12 @@ namespace Belle2 {
        */
       void setXmin(double xmin) {m_xmin = xmin;}
 
+      /**
+       * Sets minimal fraction of calibrated channels to declare this calibration as c_OK
+       * @param minFract minimal fraction of calibrated channels
+       */
+      void setMinFracCalibrated(double minFract) {m_minCalibrated = minFract;}
+
     private:
 
       /**
@@ -47,15 +58,32 @@ namespace Belle2 {
       virtual EResult calibrate() final;
 
       /**
-       * Fit pulse-height distribution with P(x) = (x/x0)^p1 * exp(-(x/x0)^p2)
+       * Fit pulse-height distributions of a single module
+       * @param h 2D histogram of pulse-height vs channel
+       * @return number of successfully fitted channels
+       */
+      int fitChannels(std::shared_ptr<TH2F> h);
+
+      /**
+       * Fit pulse-height distribution with P(x) = (x/x0)^p1 * exp(-(x/x0)^p2) and p1 = 1.
+       * Fitting with floating p1 found to be unstabe.
        * @param h histogram
        * @return fit status
        */
-      int fitPulseHeight(std::shared_ptr<TH1F> h);
+      int fitPulseHeight(TH1D* h);
+
+      /**
+       * Calculate and return threshold efficiency
+       * @param k histogram
+       * @param func fitted function
+       * @return threshold efficiency
+       */
+      double getEfficiency(TH1D* h, TF1* func);
 
       // algorithm parameters
       int m_minEntries = 100; /**< minimal number of histogram entries to perform fit */
       double m_xmin = 100.0; /**< fit range lower limit [ADC counts] */
+      double m_minCalibrated = 0.90; /**< min. fracton of calibrated channels for c_OK */
 
       // ntuple variables
       int m_slot = 0; /**< module ID */
@@ -68,9 +96,18 @@ namespace Belle2 {
       float m_p1err = 0; /**< error on p1 */
       float m_p2err = 0; /**< error on p2 */
       float m_effi = 0; /**< threshold efficiency */
+      float m_mean = 0; /**< fitted distribution mean value */
       float m_chi2 = 0;  /**< chi^2 */
       int m_ndf = 0; /**< NDF */
       int m_fitStatus = -1; /**< fit status */
+
+      // output file and tree
+      TFile* m_file = 0; /**< root file */
+      TTree* m_tree = 0; /**< output ntuple */
+
+      // output DB objects
+      TOPCalChannelPulseHeight* m_pulseHeight = 0; /**< parametrized PH distributions */
+      TOPCalChannelThresholdEff* m_thresholdEffi = 0; /**< threshold efficiencies */
 
     };
 
--- top/calibration/src/TOPPulseHeightAlgorithm.cc
+++ top/calibration/src/TOPPulseHeightAlgorithm.cc
@@ -10,6 +10,14 @@
  **************************************************************************/
 
 #include <top/calibration/TOPPulseHeightAlgorithm.h>
+#include "TROOT.h"
+#include <TDirectory.h>
+#include <TF1.h>
+#include <TMath.h>
+#include <string>
+#include <algorithm>
+
+using namespace std;
 
 namespace Belle2 {
   namespace TOP {
@@ -22,14 +30,164 @@ namespace Belle2 {
 
     CalibrationAlgorithm::EResult TOPPulseHeightAlgorithm::calibrate()
     {
+      gROOT->SetBatch();
+
+      // construct file name, open output root file and book output tree
+
+      const auto& expRun = getRunList();
+      string expNo = to_string(expRun[0].first);
+      while (expNo.length() < 4) expNo.insert(0, "0");
+      string runNo = to_string(expRun[0].second);
+      while (runNo.length() < 5) runNo.insert(0, "0");
+      string outputFileName = "pulseHeight-e" + expNo + "-r" + runNo + ".root";
+      m_file = TFile::Open(outputFileName.c_str(), "recreate");
+
+      m_tree = new TTree("tree", "pulse-height calibration results");
+      m_tree->Branch<int>("slot", &m_slot);
+      m_tree->Branch<unsigned>("channel", &m_channel);
+      m_tree->Branch<int>("numPhot", &m_numPhot);
+      m_tree->Branch<float>("x0", &m_x0);
+      m_tree->Branch<float>("p1", &m_p1);
+      m_tree->Branch<float>("p2", &m_p2);
+      m_tree->Branch<float>("x0err", &m_x0err);
+      m_tree->Branch<float>("p1err", &m_p1err);
+      m_tree->Branch<float>("p2err", &m_p2err);
+      m_tree->Branch<float>("effi", &m_effi);
+      m_tree->Branch<float>("mean", &m_mean);
+      m_tree->Branch<float>("chi2", &m_chi2);
+      m_tree->Branch<int>("ndf", &m_ndf);
+      m_tree->Branch<int>("fitStatus", &m_fitStatus);
+
+      // create payloads for storing results
+
+      m_pulseHeight = new TOPCalChannelPulseHeight();
+      m_thresholdEffi = new TOPCalChannelThresholdEff();
+
+      // fit histograms
+
+      TDirectory* oldDir = gDirectory;
+      int nsucc = 0;
+      for (int slot = 1; slot <= 16; slot++) {
+        m_slot = slot;
+        string name = "ph_slot_" + to_string(slot);
+        auto h = getObjectPtr<TH2F>(name);
+        if (not h) continue;
+
+        name = "slot_" + to_string(slot);
+        oldDir->mkdir(name.c_str())->cd();
+        h->Write();
+
+        int n = fitChannels(h);
+        B2INFO("slot " << slot << ": " << n << "/512 channels successfully fitted");
+        nsucc += n;
+      }
+
+      // write the results and close the file
+
+      m_file->Write();
+      m_file->Close();
+
+      // check the results and return if fraction of successfully fitted too small
+
+      if (nsucc / 8192.0 < m_minCalibrated) {
+        delete m_pulseHeight;
+        delete m_thresholdEffi;
+        return c_NotEnoughData;
+      }
+
+      // otherwise import payloads to DB
+
+      saveCalibration(m_pulseHeight);
+      saveCalibration(m_thresholdEffi);
 
       return c_OK;
     }
 
 
-    int TOPPulseHeightAlgorithm::fitPulseHeight(std::shared_ptr<TH1F> h)
+    int TOPPulseHeightAlgorithm::fitChannels(std::shared_ptr<TH2F> h2d)
     {
-      return -1;
+
+      int n = 0;
+      for (int ch = 0; ch < h2d->GetNbinsX(); ch++) {
+        m_channel =  ch;
+        string chan = to_string(ch);
+        while (chan.length() < 3) chan.insert(0, "0");
+        string name = "chan_" + chan;
+        auto* h = h2d->ProjectionY(name.c_str(), ch + 1, ch + 1);
+        string title = "slot " + to_string(m_slot) + " channel " + to_string(ch);
+        h->SetTitle(title.c_str());
+        auto status = fitPulseHeight(h);
+        if (status == 0) {
+          m_pulseHeight->setParameters(m_slot, m_channel, m_x0, m_p1, m_p2);
+          m_thresholdEffi->setThrEff(m_slot, m_channel, m_effi, m_xmin);
+          n++;
+        }
+      }
+      return n;
+    }
+
+
+    int TOPPulseHeightAlgorithm::fitPulseHeight(TH1D* h)
+    {
+      m_numPhot = h->GetSumOfWeights();
+      if (m_numPhot < std::max(m_minEntries, 5)) return -1;
+
+      double xmin = h->GetXaxis()->GetXmin();
+      double xmax = h->GetXaxis()->GetXmax();
+
+      auto* func = new TF1("func", "[0]*x/[1]*exp(-pow(x/[1],[2]))", xmin, xmax);
+
+      double p2 = 1.0;
+      double x0 = h->GetMean() / 3;
+      func->SetParameter(0, 1);
+      func->SetParameter(1, x0);
+      func->SetParameter(2, p2);
+      double maxPosition = h->GetBinCenter(h->GetMaximumBin());
+      double fmax = h->GetMaximum() / func->Eval(maxPosition);
+      func->SetParameter(0, fmax);
+
+      int status = h->Fit(func, "LRSQ", "", m_xmin, xmax);
+
+      if (status == 4) {
+        func->SetParameter(1, x0 / 2);
+        status = h->Fit(func, "LRSQ", "", m_xmin, xmax);
+      }
+
+      m_x0 = func->GetParameter(1);
+      m_p1 = 1.0;
+      m_p2 = func->GetParameter(2);
+      m_x0err = func->GetParError(1);
+      m_p1err = 0;
+      m_p2err = func->GetParError(2);
+      m_chi2 = func->GetChisquare();
+      m_effi = getEfficiency(h, func);
+      m_mean = TMath::Gamma((m_p1 + 2) / m_p2) / TMath::Gamma((m_p1 + 1) / m_p2) * m_x0;
+      m_fitStatus = status;
+      m_tree->Fill();
+
+      return status;
+    }
+
+
+    double TOPPulseHeightAlgorithm::getEfficiency(TH1D* h, TF1* func)
+    {
+
+      double fbelow = 0;
+      double fabove = 0;
+      double count = 0;
+      m_ndf = -func->GetNumberFreeParameters();
+      for (int i = 0; i < h->GetNbinsX(); i++) {
+        double x = h->GetBinCenter(i + 1);
+        if (x > m_xmin) {
+          fabove += func->Eval(x);
+          count += h->GetBinContent(i);
+          if (h->GetBinContent(i) > 0) m_ndf++;
+        } else {
+          fbelow += func->Eval(x);
+        }
+      }
+      double integral = (fbelow + fabove) * count / fabove;
+      return h->GetSumOfWeights() / integral;
     }
 
 

4edd28493b4f223376eab77e3e83a13418066075
Michael Eliachevitch <michael.eliachevitch@kit.edu>
2019-10-09 17:00:22 +0200

    Rename param for temporary quality indicators in add_track_finding
    The previous `add_mva_quality_indicator` would suggest that the quality
    indicator is added to the exported `reco_tracks` store array, but this is not
    true. The final quality indicator can only be added in
    `add_tracking_reconstruction`, as it requires the track fit to be performed.

--- tracking/scripts/tracking/__init__.py
+++ tracking/scripts/tracking/__init__.py
@@ -61,7 +61,7 @@ def add_tracking_reconstruction(path, components=None, pruneTracks=False, skipGe
                           use_second_cdc_hits=use_second_cdc_hits,
                           use_svd_to_cdc_ckf=use_svd_to_cdc_ckf,
                           use_ecl_to_cdc_ckf=use_ecl_to_cdc_ckf,
-                          add_mva_quality_indicator=add_mva_quality_indicator)
+                          add_subtrack_mva_quality_indicators=add_mva_quality_indicator)
 
     # Only run the track time extraction on the full reconstruction chain for now. Later, we may
     # consider to do the CDC-hit based method already during the fast reconstruction stage
@@ -172,7 +172,7 @@ def add_track_finding(path, components=None, reco_tracks="RecoTracks",
                       prune_temporary_tracks=True, use_second_cdc_hits=False,
                       use_mc_truth=False, svd_ckf_mode="VXDTF2_after", add_both_directions=True,
                       use_svd_to_cdc_ckf=True, use_ecl_to_cdc_ckf=False,
-                      add_mva_quality_indicator=True):
+                      add_subtrack_mva_quality_indicators=True):
     """
     Add the CKF to the path with all the track finding related to and needed for it.
     :param path: The path to add the tracking reconstruction modules to
@@ -187,8 +187,11 @@ def add_track_finding(path, components=None, reco_tracks="RecoTracks",
         If true, prune them.
     :param use_svd_to_cdc_ckf: if true, add SVD to CDC CKF module.
     :param use_ecl_to_cdc_ckf: if true, add ECL to CDC CKF module.
-    :param add_mva_quality_indicator: If true, add the MVA track quality estimation
-        to the path that sets the quality indicator property of the found tracks.
+    :param add_subtrack_mva_quality_indicators: If true, add intermediate
+        subtrack MVA quality indicators to the temporary reco tracks from CDC
+        and SVD standalone tracking. Does not add the final MVA quality
+        indicator to the produced ``reco_tracks`` store array, as it requires
+        the output of the track fit.
     """
     if not is_svd_used(components) and not is_cdc_used(components):
         return
@@ -227,7 +230,7 @@ def add_track_finding(path, components=None, reco_tracks="RecoTracks",
 
     if is_cdc_used(components):
         add_cdc_track_finding(path, use_second_hits=use_second_cdc_hits, output_reco_tracks=cdc_reco_tracks,
-                              add_mva_quality_indicator=add_mva_quality_indicator)
+                              add_mva_quality_indicator=add_subtrack_mva_quality_indicators)
         temporary_reco_track_list.append(cdc_reco_tracks)
         latest_reco_tracks = cdc_reco_tracks
 
@@ -237,7 +240,7 @@ def add_track_finding(path, components=None, reco_tracks="RecoTracks",
                               temporary_reco_tracks=svd_reco_tracks,
                               svd_ckf_mode=svd_ckf_mode, add_both_directions=add_both_directions,
                               use_svd_to_cdc_ckf=use_svd_to_cdc_ckf, prune_temporary_tracks=prune_temporary_tracks,
-                              add_mva_quality_indicator=add_mva_quality_indicator)
+                              add_mva_quality_indicator=add_subtrack_mva_quality_indicators)
         temporary_reco_track_list.append(svd_reco_tracks)
         temporary_reco_track_list.append(svd_cdc_reco_tracks)
         latest_reco_tracks = svd_cdc_reco_tracks

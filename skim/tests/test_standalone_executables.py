9c61a658be1f2ed87fa19bd79944ad5977b5bfd6
Phil Grace <philip.grace@adelaide.edu.au>
2019-10-30 16:31:07 +1030

    Test all skims in registry, and print filtered output

--- skim/tests/test_standalone_executables.py
+++ skim/tests/test_standalone_executables.py
@@ -8,11 +8,14 @@ events, and tests whether the executable run successfully.
 __author__ = 'Phil Grace'
 __email__ = 'philip.grace@adelaide.edu.au'
 
-from os import getenv
-from pathlib import Path
+import re
 import subprocess
 import sys
 from termcolor import colored, cprint
+from tempfile import NamedTemporaryFile
+
+from basf2 import find_file
+from skim.registry import skim_registry, combined_skims
 
 
 def getSkimStandaloneScript(skimName, combined=False):
@@ -26,46 +29,80 @@ def getSkimStandaloneScript(skimName, combined=False):
         skimScript (str): The absolute path to the standalone script in the currently set up basf2
             installation.
     """
-    basf2Directory = getenv('BELLE2_RELEASE', getenv('BELLE2_LOCAL_DIR'))
-
     if combined:
         scriptDirectory = 'combined'
     else:
         scriptDirectory = 'standalone'
 
-    skimScript = str(Path(basf2Directory, 'skim', scriptDirectory, f'{skimName}_Skim_Standalone.py'))
+    try:
+        skimScript = find_file(f'skim/{scriptDirectory}/{skimName}_Skim_Standalone.py')
+        return skimScript
+    except FileNotFoundError:
+        cprint(f'WARNING! Skim {skimName} registered in skim registry, but no executable found!', 'red')
 
-    return skimScript
 
-
-def testExecutable(skimName):
-    """Runs the standalone script for a given skim. If it fails, the STDOUT of the script is printed
-    to the screen. Otherwise, it is suppressed.
+def filterErrorMessages(output):
+    """Filter the output of basf2 to only print WARNING, ERROR, and FATAL messages.
 
     Args:
-        skimName (str): The name of the skim to be tested.
+        output (str): Output to be filted.
     Returns:
-        executableWorks (bool): True if skim standalone script exited with exit code 0, and False otherwise.
+        filtedOutput (str): Output filtered to only problematic lines.
     """
-    skimScript = getSkimStandaloneScript(skimName)
-    print(f'Testing script {skimScript}...')
+    matchingLines = re.findall(r'.WARNING.*|.ERROR.*|.FATAL.*', output)
+    return '\n'.join(matchingLines)
 
-    process = subprocess.run(['basf2', skimScript, '-n', '10'], universal_newlines=True,
+
+def testExecutable(skimScript):
+    """Runs the standalone script for a given skim. If it fails, the error messages from basf2 are
+    printed to the screen.
+
+    Args:
+        skimScript (str): The path to the standalone skim script to be tested.
+    Returns:
+        executableWorks (bool): True if basf2 exited normally, and False otherwise.
+    """
+    tempFile = NamedTemporaryFile(suffix='.udst.root')
+    process = subprocess.run(['basf2', skimScript, '-n', '10', '-o', tempFile.name],
+                             universal_newlines=True,
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     executableWorks = (process.returncode == 0)
 
     if not executableWorks:
-        cprint(f'Error: Script {skimScript} failed! See output below.', 'red')
-        print(subprocess.stdout)
+        cprint(f'ERROR! Script {skimScript} failed! See output below.', 'red')
+
+        filteredStdout = filterErrorMessages(process.stdout)
+        print(process.stderr, filteredStdout)
     else:
-        cprint(f'Script {skimScript} worked.', 'green')
+        cprint(f'Script {skimScript} ran successfully.', 'green')
 
     return executableWorks
 
 
-def main():
-    executableWorks = testExecutable('SLUntagged')
+def testScripts(standaloneSkims, combinedSkims):
+    """Tests executable scripts for given lists of skims, and prints a report about which failed to
+    run. If any failed, exits with return code 1.
+
+    Args:
+        standaloneSkims (list): List of names of individual skims to test.
+        combinedSkims (list): List of names of combined skims to test.
+    """
+    standaloneScripts = [getSkimStandaloneScript(skim) for skim in standaloneSkims]
+    combinedScripts = [getSkimStandaloneScript(skim, combined=True) for skim in combinedSkims]
+    allScripts = filter(None, standaloneScripts + combinedScripts)
+
+    failedScripts = [script for script in allScripts if not testExecutable(script)]
+
+    if failedScripts:
+        cprint('The following standalone executables failed:', 'red', attrs=['bold'])
+        cprint('\n'.join(failedScripts), 'red', attrs=['bold'])
+
+        sys.exit(1)
 
 if __name__ == '__main__':
-    main()
+    standaloneSkims = [name for _, name in skim_registry]
+    combinedSkims = combined_skims.keys()
+
+    testScripts(['SLUntagged', 'PRsemileptonicUntagged'], [])
+    # testScripts(standaloneSkims, combinedSkims)

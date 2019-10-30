d62298069eb70f4b87165ed7b938cf160483d0c9
Phil Grace <philip.grace@adelaide.edu.au>
2019-10-30 16:38:40 +1030

    Changed 'executable' to 'steering file'

--- skim/tests/test_standalone_executables.py
+++ skim/tests/test_standalone_executables.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This script runs each of the standalone executables in the skim package with a small number of
-events, and tests whether the executable run successfully.
+This script runs each of the standalone steering files in the skim package with a small number of
+events, and tests whether the steering files run successfully.
 """
 
 __author__ = 'Phil Grace'
@@ -38,7 +38,7 @@ def getSkimStandaloneScript(skimName, combined=False):
         skimScript = find_file(f'skim/{scriptDirectory}/{skimName}_Skim_Standalone.py')
         return skimScript
     except FileNotFoundError:
-        cprint(f'WARNING! Skim {skimName} registered in skim registry, but no executable found!', 'red')
+        cprint(f'WARNING! Skim {skimName} registered in skim registry, but no standalone steering file found!', 'red')
 
 
 def filterErrorMessages(output):
@@ -53,36 +53,36 @@ def filterErrorMessages(output):
     return '\n'.join(matchingLines)
 
 
-def testExecutable(skimScript):
-    """Runs the standalone script for a given skim. If it fails, the error messages from basf2 are
-    printed to the screen.
+def testSteeringFile(skimScript):
+    """Runs the standalone script for a given skim. If it fails, STDERR and the error messages from
+    basf2 are printed to the screen.
 
     Args:
         skimScript (str): The path to the standalone skim script to be tested.
     Returns:
-        executableWorks (bool): True if basf2 exited normally, and False otherwise.
+        steeringFileWorks (bool): True if basf2 exited normally, and False otherwise.
     """
     tempFile = NamedTemporaryFile(suffix='.udst.root')
     process = subprocess.run(['basf2', skimScript, '-n', '10', '-o', tempFile.name],
                              universal_newlines=True,
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    executableWorks = (process.returncode == 0)
+    steeringFileWorks = (process.returncode == 0)
 
-    if not executableWorks:
+    if steeringFileWorks:
+        cprint(f'Script {skimScript} ran successfully.', 'green')
+    else:
         cprint(f'ERROR! Script {skimScript} failed! See output below.', 'red')
 
         filteredStdout = filterErrorMessages(process.stdout)
         print(process.stderr, filteredStdout)
-    else:
-        cprint(f'Script {skimScript} ran successfully.', 'green')
 
-    return executableWorks
+    return steeringFileWorks
 
 
-def testScripts(standaloneSkims, combinedSkims):
-    """Tests executable scripts for given lists of skims, and prints a report about which failed to
-    run. If any failed, exits with return code 1.
+def testAllSkims(standaloneSkims, combinedSkims):
+    """Tests steering files for given lists of skims, and prints a report about which failed to run.
+    If any failed, exits with return code 1.
 
     Args:
         standaloneSkims (list): List of names of individual skims to test.
@@ -90,12 +90,13 @@ def testScripts(standaloneSkims, combinedSkims):
     """
     standaloneScripts = [getSkimStandaloneScript(skim) for skim in standaloneSkims]
     combinedScripts = [getSkimStandaloneScript(skim, combined=True) for skim in combinedSkims]
+
     allScripts = filter(None, standaloneScripts + combinedScripts)
 
-    failedScripts = [script for script in allScripts if not testExecutable(script)]
+    failedScripts = [script for script in allScripts if not testSteeringFile(script)]
 
     if failedScripts:
-        cprint('The following standalone executables failed:', 'red', attrs=['bold'])
+        cprint('The following standalone steering files failed:', 'red', attrs=['bold'])
         cprint('\n'.join(failedScripts), 'red', attrs=['bold'])
 
         sys.exit(1)
@@ -104,5 +105,4 @@ if __name__ == '__main__':
     standaloneSkims = [name for _, name in skim_registry]
     combinedSkims = combined_skims.keys()
 
-    testScripts(['SLUntagged', 'PRsemileptonicUntagged'], [])
-    # testScripts(standaloneSkims, combinedSkims)
+    testScripts(standaloneSkims, combinedSkims)

3823608e00a23164589d394bb0b3c31263c2a419
Kirill Chilikin <chilikin@lebedev.ru>
2019-11-02 12:57:54 +0300

    Update the quarks and framework test (it compares numeric values now).

--- framework/tests/evtpdl.py
+++ framework/tests/evtpdl.py
@@ -37,22 +37,54 @@ pdg.add_particle("foo\tbar", 10001, 0, 0, 0, 0)
 # default evt.pdl filename
 default_evtpdl = Belle2.FileSystem.findFile(os.path.join("decfiles", "dec", "evt.pdl"))
 # create a temporary one and compare
+retcode = 0
 with NamedTemporaryFile() as tempfile:
     # write a evt.pdl from the EventGenDatabasePDG
     db.WriteEvtGenTable(tempfile.name)
     # and make sure that it's the same as the original one except for
     # whitespace, empty lines and lines starting with a "*"
-    retcode = subprocess.call(["diff", "-w", "-B", "-I^\*.*$", default_evtpdl, tempfile.name])
-
+    file1 = open(default_evtpdl, 'r')
+    lines1 = list(filter(lambda x: x[0] != '*' and x != 'end', file1.readlines()))
+    file2 = open(tempfile.name, 'r')
+    lines2 = list(filter(lambda x: x[0] != '*' and x != 'end', file2.readlines()))
+    if len(lines1) != len(lines2):
+        retcode = 1
+    else:
+        for i in range(len(lines1)):
+            fields1 = lines1[i].split(None)
+            fields2 = lines2[i].split(None)
+            if (len(fields1) != len(fields2)):
+                retcode = 1
+            else:
+                if (fields1[0] == 'end' and fields2[0] == 'end'):
+                    continue
+                if (fields1[0] != fields2[0] or  # add
+                    fields1[1] != fields2[1] or  # p
+                    fields1[2] != fields2[2] or  # Particle
+                    fields1[3] != fields2[3] or  # name
+                    int(fields1[4]) != int(fields2[4]) or  # code
+                    float(fields1[5]) != float(fields2[5]) or  # mass
+                    float(fields1[6]) != float(fields2[6]) or  # width
+                    float(fields1[7]) != float(fields2[7]) or  # Delta m
+                    int(fields1[8]) != int(fields2[8]) or  # charge
+                    int(fields1[9]) != int(fields2[9]) or  # spin
+                    float(fields1[10]) != float(fields2[10]) or  # c * tau
+                        int(fields1[11]) != int(fields2[11])):  # Pythia code
+                    print("Particles differ: %s %s" % (lines1[i], lines2[i]))
+                    retcode = 1
+    file1.close()
+    file2.close()
     # create a new particle
     db.AddParticle("testparticle", "testparticle", 1, False, 2, 3, "custom", 10001, 4, 5, 6, 7, 8, 9)
 
     # also test creating particle with the python functions
     pdg.add_particle("test2", 10002, 5.28, 0.1, -1, 0.5)
     # write the table again
-    db.WriteEvtGenTable(tempfile.name)
+    tempfile2 = NamedTemporaryFile()
+    db.WriteEvtGenTable(tempfile2.name)
     # and make sure that it has the additional particle
-    subprocess.call(["diff", "-w", "-B", "-I^\*.*$", default_evtpdl, tempfile.name])
+    subprocess.call(["diff", tempfile.name, tempfile2.name])
+    tempfile2.close()
 
 db.ReadEvtGenTable("/no/such/file/(I hope)")
 # and also test the python function

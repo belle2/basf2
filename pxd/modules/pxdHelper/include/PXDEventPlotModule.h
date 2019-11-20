c0912abeef020c89d83399cdc11cc51ad5e317d7
SPRUCK Bjoern <bjoern.spruck@desy.de>
2019-11-20 20:32:30 +0900

    Event Plotter for special events

---
+++ pxd/modules/pxdHelper/include/PXDEventPlotModule.h
@@ -0,0 +1,74 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2018 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Bjoern Spruck                                            *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+#include <framework/core/Module.h>
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <vxd/geometry/GeoCache.h>
+#include <vxd/geometry/SensorInfoBase.h>
+#include <framework/dataobjects/EventMetaData.h>
+#include <pxd/dataobjects/PXDRawHit.h>
+#include <pxd/dataobjects/PXDDAQStatus.h>
+#include <rawdata/dataobjects/RawFTSW.h>
+#include <TCanvas.h>
+#include <TH2F.h>
+#include <TLine.h>
+#include <string>
+
+
+namespace Belle2 {
+
+  namespace PXD {
+    /**
+     * Plot each event with ROI and Pixels
+     */
+    class PXDEventPlotModule : public Module {
+
+    public:
+
+      /** Constructor defining the parameters */
+      PXDEventPlotModule();
+
+    private:
+      void initialize() override final;
+
+      void event() override final;
+
+    private:
+      //the geometry
+      VXD::GeoCache& m_vxdGeometry;
+
+      std::string m_storeRawHitsName;             /**< PXDRawHits StoreArray name */
+
+      StoreObjPtr<EventMetaData> m_eventMetaData;/**< Event Metadata StorePtr */
+
+      /** Input array for DAQ Status. */
+      StoreObjPtr<PXDDAQStatus> m_storeDAQEvtStats;
+
+      /** Storearray for raw pixels   */
+      StoreArray<PXDRawHit> m_storeRawHits;
+
+      /** Input array for DAQ Status. */
+      StoreArray<RawFTSW> m_rawTTD;
+
+      TCanvas* m_c{};
+      std::map <VxdID, TH2F*> m_histos;
+
+      TLine* m_l1{}, *m_l2{}, *m_l3{};
+
+    };//end class declaration
+
+
+  } //end PXD namespace;
+} // end namespace Belle2
+

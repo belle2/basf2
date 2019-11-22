f1709865f70416a74818d3a39cf839639054ae30
Jitendra Kumar <jitendra.kumar@desy.de>
2019-11-22 12:29:35 +0900

    update to CDC dedx for skims events histo and cleanup

--- reconstruction/modules/CDCDedxDQM/include/CDCDedxDQM.h
+++ reconstruction/modules/CDCDedxDQM/include/CDCDedxDQM.h
@@ -75,20 +75,8 @@ namespace Belle2 {
     Int_t fCurrentEventNum; /**< variable to get run number */
     std::string m_triggerIdentifier = ""; /**< variable to get specific trigger event */
 
-    Bool_t isHadronfile; /**< Parameter-1 to switch binning */
-    TString fCollType = ""; /**< Parameter-2 to switch binning */
-
     TH1D* temp1D{nullptr}; /**< Dedx histogram per run */
     TH2D* temp2D{nullptr}; /**< Dedx vs P histogram per run */
-
-    Int_t    nBinsdedx; /**< nbin of dedx range */
-    Double_t nBinsdedxLE; /**< Lowedge of dedx */
-    Double_t nBinsdedxUE; /**< Upedge of dedx */
-
-    Int_t    nBinsP; /**< nbins of P range */
-    Double_t nBinsPLE; /**< Lowedge of P range */
-    Double_t nBinsPUE; /**< Upedge of P range */
-
   };
 
 } // Belle2 namespace

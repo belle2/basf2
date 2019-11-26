be1eda9d3980f3a6bd958d1de94258092c7bce06
Simon Kurz <Simon.Kurz@desy.de>
2019-11-26 18:01:01 +0100

    added DB object for ROI parameters

--- tracking/modules/pxdDataReduction/include/PXDdigiFilterModule.h
+++ tracking/modules/pxdDataReduction/include/PXDdigiFilterModule.h
@@ -13,8 +13,6 @@
 #include <framework/core/Module.h>
 #include <framework/datastore/SelectSubset.h>
 #include <pxd/dataobjects/PXDDigit.h>
-#include <framework/database/DBObjPtr.h>
-#include <simulation/dbobjects/ROIParameters.h>
 
 namespace Belle2 {
 
@@ -37,8 +35,6 @@ namespace Belle2 {
     /**  */
     void initialize() override final;
 
-    /**  */
-    void beginRun() override;
 
     /**  */
     void event() override final;
@@ -49,10 +45,6 @@ namespace Belle2 {
     std::string m_PXDDigitsInsideROIName;  /**< The name of the StoreArray of Filtered PXDDigits */
     std::string m_PXDDigitsOutsideROIName;  /**< The name of the StoreArray of Filtered PXDDigits */
     std::string m_ROIidsName;  /**< The name of the StoreArray of ROIs */
-
-    int m_countNthEvent = 0;  /**< Event counter to be able to disable data reduction for every Nth event */
-    DBObjPtr<ROIParameters> m_roiParameters;  /**< Configuration parameters for ROIs */
-    int m_skipEveryNth = -1;  /**< Parameter from DB for how many events to skip data reduction */
 
     SelectSubset< PXDDigit > m_selectorIN; /**< selector of the subset of PXDDigits contained in the ROIs*/
     SelectSubset< PXDDigit > m_selectorOUT; /**< selector of the subset of PXDDigits NOT contained in the ROIs*/

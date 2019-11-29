d8a24efafccd162b5311780c5f7d6a31bf78e84a
HEARTY Christopher <christopher.hearty@desy.de>
2019-11-30 07:31:48 +0900

    script to run 3 ECL energy calibrations in airflow

--- ecl/calibration/include/linkdef.h
+++ ecl/calibration/include/linkdef.h
@@ -9,5 +9,6 @@
 #pragma link C++ class Belle2::ECL::eclCosmicEAlgorithm-;
 #pragma link C++ class Belle2::ECL::eclGammaGammaEAlgorithm-;
 #pragma link C++ class Belle2::ECL::eclBhabhaTAlgorithm-;
+#pragma link C++ class Belle2::ECL::eclee5x5Algorithm-;
 
 #endif

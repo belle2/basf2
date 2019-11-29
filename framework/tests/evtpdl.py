00178a2dd6da4572bdf22d812c1f2b0a6e25bfca
Martin Ritter <martin.ritter@desy.de>
2019-11-29 10:26:59 +0100

    Pythonify the comparison of the evt pdl.
    And remove the diff header so we are not dependent on the amount of
    particles in evt.pdl anymore

--- framework/tests/evtpdl.py
+++ framework/tests/evtpdl.py
@@ -17,8 +17,6 @@ This script tests that
 from tempfile import NamedTemporaryFile
 import basf2
 import ROOT
-from ROOT import Belle2
-import re
 import os
 import sys
 import subprocess
@@ -35,7 +33,7 @@ pdg.add_particle("test2", 1, 5.28, 0.1, 0, 0.5)
 pdg.add_particle("foo\tbar", 10001, 0, 0, 0, 0)
 
 # default evt.pdl filename
-default_evtpdl = Belle2.FileSystem.findFile(os.path.join("decfiles", "dec", "evt.pdl"))
+default_evtpdl = basf2.find_file(os.path.join("decfiles", "dec", "evt.pdl"))
 # create a temporary one and compare
 retcode = 0
 with NamedTemporaryFile() as tempfile:
@@ -43,48 +41,45 @@ with NamedTemporaryFile() as tempfile:
     db.WriteEvtGenTable(tempfile.name)
     # and make sure that it's the same as the original one except for
     # whitespace, empty lines and lines starting with a "*"
-    file1 = open(default_evtpdl, 'r')
-    lines1 = list(filter(lambda x: x[0] != '*' and x != 'end', file1.readlines()))
-    file2 = open(tempfile.name, 'r')
-    lines2 = list(filter(lambda x: x[0] != '*' and x != 'end', file2.readlines()))
-    if len(lines1) != len(lines2):
+    with open(default_evtpdl) as default:
+        old_lines = [x.split() for x in default if x[0] != '*' and not x.startswith("end")]
+    with open(tempfile.name) as changed:
+        new_lines = [x.split() for x in changed if x[0] != '*' and not x.startswith("end")]
+
+    if len(old_lines) != len(new_lines):
+        basf2.B2ERROR("Different number of lines when writing pdl file")
         retcode = 1
     else:
-        for i in range(len(lines1)):
-            fields1 = lines1[i].split(None)
-            fields2 = lines2[i].split(None)
-            if (len(fields1) != len(fields2)):
+        # field types for comparison
+        types = [str]*4 + [int, float, float, float, int, int, float, int]
+        # check all lines for equality but don't care about formatting
+        for old, new in zip(old_lines, new_lines):
+            # make sure the lines have the same number of fields
+            if len(old) != len(new):
+                basf2.B2ERROR("Number of field different between old and new:",
+                              old=old, new=new)
                 retcode = 1
+            # and that they have enough fields for comparison
+            elif len(new) < len(types):
+                basf2.B2ERROR("Not enough fields", old=old, new=new)
+                retcode = 1
+            # and then make sure they're the same compariny the correct types
             else:
-                if (fields1[0] == 'end' and fields2[0] == 'end'):
-                    continue
-                if (fields1[0] != fields2[0] or  # add
-                    fields1[1] != fields2[1] or  # p
-                    fields1[2] != fields2[2] or  # Particle
-                    fields1[3] != fields2[3] or  # name
-                    int(fields1[4]) != int(fields2[4]) or  # code
-                    float(fields1[5]) != float(fields2[5]) or  # mass
-                    float(fields1[6]) != float(fields2[6]) or  # width
-                    float(fields1[7]) != float(fields2[7]) or  # Delta m
-                    int(fields1[8]) != int(fields2[8]) or  # charge
-                    int(fields1[9]) != int(fields2[9]) or  # spin
-                    float(fields1[10]) != float(fields2[10]) or  # c * tau
-                        int(fields1[11]) != int(fields2[11])):  # Pythia code
-                    print("Particles differ: %s %s" % (lines1[i], lines2[i]))
+                if not all(t(o) == t(n) for t, o, n in zip(types, old, new)):
+                    basf2.B2ERROR("Particles differ", old=old, new=new)
                     retcode = 1
-    file1.close()
-    file2.close()
-    # create a new particle
-    db.AddParticle("testparticle", "testparticle", 1, False, 2, 3, "custom", 10001, 4, 5, 6, 7, 8, 9)
 
-    # also test creating particle with the python functions
-    pdg.add_particle("test2", 10002, 5.28, 0.1, -1, 0.5)
-    # write the table again
-    tempfile2 = NamedTemporaryFile()
-    db.WriteEvtGenTable(tempfile2.name)
-    # and make sure that it has the additional particle
-    subprocess.call(["diff", tempfile.name, tempfile2.name])
-    tempfile2.close()
+    with NamedTemporaryFile() as tempfile2:
+        # create a new particle
+        db.AddParticle("testparticle", "testparticle", 1, False, 2, 3, "custom", 10001, 4, 5, 6, 7, 8, 9)
+
+        # also test creating particle with the python functions
+        pdg.add_particle("test2", 10002, 5.28, 0.1, -1, 0.5)
+        # write the table again
+        db.WriteEvtGenTable(tempfile2.name)
+        # and make sure that it has the additional particle
+        subprocess.call(["diff", "--old-line-format=- %L", "--new-line-format=+ %L",
+                         "--unchanged-line-format=", tempfile.name, tempfile2.name])
 
 db.ReadEvtGenTable("/no/such/file/(I hope)")
 # and also test the python function

95a70cbeed0a6f3f8ae45aec131bb8bcdbfb1f33
LA Licata Chiara <chiara.lalicata@ts.infn.it>
2019-12-03 22:33:04 +0900

    skimming scripts for neutral B to charm decays

---
+++ skim/standalone/B0toDpi_kpipi_Skim_Standalone.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#######################################################
+#
+# neutral B to charm mode:
+# B0 -> D-(k+ pi- pi-)pi+
+#
+# Chiara La Licata 2019/Oct/18
+#
+######################################################
+
+from ROOT import Belle2
+from basf2 import *
+import modularAnalysis as ma
+from stdCharged import stdPi, stdK
+from stdPi0s import *
+from skimExpertFunctions import encodeSkimName, setSkimLogging, get_test_file
+
+set_log_level(LogLevel.INFO)
+gb2_setuprel = 'release-04-00-04'
+
+mypath = Path()
+
+skimCode = encodeSkimName('B0toDpi_kpipi')
+
+fileList = get_test_file("mixedBGx1", "MC12")
+
+inputMdstList('default', fileList, path=mypath)
+
+# create and fill pion and kaon ParticleLists
+stdPi('all', path=mypath)
+stdK('all', path=mypath)
+stdPi0s('skim', path=mypath)
+
+ma.applyCuts(list_name='pi+:all', cut='abs(dr) < 2 and abs(dz) < 5',
+             path=mypath)
+ma.applyCuts(list_name='K+:all', cut='abs(dr) < 2 and abs(dz) < 5',
+             path=mypath)
+
+
+# B0 to D-(k+ pi- pi-)pi+ Skim
+from skim.b0toDh_TDCPVControlSample_List import loadB0toDpi, loadD
+loadD(path=mypath)
+B0toDpiList = loadB0toDpi(path=mypath)
+
+skimOutputUdst(skimCode, B0toDpiList, path=mypath)
+summaryOfLists(B0toDpiList, path=mypath)
+
+setSkimLogging(mypath)
+process(mypath)
+
+# print out the summary
+print(statistics)

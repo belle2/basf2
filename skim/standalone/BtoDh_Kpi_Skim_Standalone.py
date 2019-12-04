8904139a6537605bf1be1de677aff6b3b4273996
niharika <niharikarout@physics.iitm.ac.in>
2019-12-04 20:06:36 +0900

    modified scripts

--- skim/standalone/BtoDh_Kpi_Skim_Standalone.py
+++ skim/standalone/BtoDh_Kpi_Skim_Standalone.py
@@ -8,8 +8,8 @@
 ######################################################
 
 from ROOT import Belle2
-from basf2 import *
-from modularAnalysis import *
+import basf2 as b2
+import modularAnalysis as ma
 from stdCharged import stdPi, stdK
 from stdPi0s import *
 from stdV0s import *
@@ -18,31 +18,30 @@ from skimExpertFunctions import encodeSkimName, setSkimLogging, get_test_file
 set_log_level(LogLevel.INFO)
 gb2_setuprel = 'release-04-00-04'
 
-path = Path()
+path = b2.create_path()
 
 skimCode = encodeSkimName('BtoDh_Kpi')
 
 fileList = get_test_file("mixedBGx1", "MC12")
 
-inputMdstList('default', fileList, path=path)
-
+ma.inputMdstList('default', fileList, path=path)
 
 # create and fill pion and kaon ParticleLists
 stdPi('all', path=path)
 stdK('all', path=path)
 
-applyCuts(list_name='pi+:all', cut='abs(dr) < 2 and abs(dz) < 5', path=path)
-applyCuts(list_name='K+:all', cut='abs(dr) < 2 and abs(dz) < 5', path=path)
+ma.applyCuts(list_name='pi+:all', cut='abs(dr) < 2 and abs(dz) < 5', path=path)
+ma.applyCuts(list_name='K+:all', cut='abs(dr) < 2 and abs(dz) < 5', path=path)
 
 # B+ to D(->h+h-)h+ Skim
-from skim.HadronicBValidation import loadDKpi, BsigToDhToKpiList
-loadDKpi(path=path)
+from skim.btohadron_validation import loadD0_Kpi, BsigToDhToKpiList
+loadD0_Kpi(path=path)
 BtoDhList = BsigToDhToKpiList(path=path)
 skimOutputUdst(skimCode, BtoDhList, path=path)
 summaryOfLists(BtoDhList, path=path)
 
 setSkimLogging(path)
-process(path)
+b2.process(path)
 
 # print out the summary
-print(statistics)
+print(b2.statistics)

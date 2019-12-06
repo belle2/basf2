3efb99ef07d1d764b06eff8839efaaac0453d549
ABUDINEN Fernando <fernando.abudinen@ts.infn.it>
2019-12-07 03:23:33 +0900

    Minor correction in comment.

--- analysis/examples/tutorials/B2A801-FlavorTagger.py
+++ analysis/examples/tutorials/B2A801-FlavorTagger.py
@@ -99,7 +99,7 @@ ma.matchMCTruth(list_name='B0:sig', path=cp_val_path)
 ma.buildRestOfEvent(target_list_name='B0:sig',
                     path=cp_val_path)
 
-# Temporary we use the analysis GT with the flavor tagger payloads for release-03-00-02.
+# Temporary we use the analysis GT with the flavor tagger payloads for release-03-02-00.
 # This will NOT be needed aonce we retrain with MC13!
 
 b2.conditions.append_globaltag("analysis_tools_release-03-02-00")

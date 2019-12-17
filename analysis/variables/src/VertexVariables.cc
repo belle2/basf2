e47ee13290d0c93eefd66f4090867aea36c0050a
Dazhi Wang <dazhi.wang@desy.de>
2019-12-17 12:11:18 +0900

    Calculate track parameters with respect to IP when needed

--- analysis/variables/src/VertexVariables.cc
+++ analysis/variables/src/VertexVariables.cc
@@ -9,17 +9,24 @@
 **************************************************************************/
 
 #include <analysis/variables/VertexVariables.h>
-#include <framework/database/DBObjPtr.h>
-#include <mdst/dbobjects/BeamSpot.h>
-#include <mdst/dataobjects/MCParticle.h>
+#include <analysis/variables/TrackVariables.h>
 #include <analysis/utility/ReferenceFrame.h>
+
+#include <framework/database/DBObjPtr.h>
 #include <framework/logging/Logger.h>
 #include <framework/utilities/Conversion.h>
+
 #include <boost/lexical_cast.hpp>
 #include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
+
 #include <TMatrixFSym.h>
 #include <TVector3.h>
-#include <boost/format.hpp>
+
+#include <mdst/dbobjects/BeamSpot.h>
+#include <mdst/dataobjects/MCParticle.h>
+#include <mdst/dataobjects/Track.h>
+#include <mdst/dataobjects/TrackFitResult.h>
 
 namespace Belle2 {
   class Particle;
@@ -231,49 +238,98 @@ namespace Belle2 {
     {
       static DBObjPtr<BeamSpot> beamSpotDB;
       const auto& frame = ReferenceFrame::GetCurrent();
-      return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).X();
+      auto trackFit = getTrackFitResultFromParticle(part);
+      if (!trackFit) {
+        return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).X();
+      } else {
+        UncertainHelix helix = trackFit->getUncertainHelix();
+        helix.passiveMoveBy(beamSpotDB->getIPPosition());
+        return frame.getVertex(helix.getPerigee()).X();
+      }
     }
 
     double particleDY(const Particle* part)
     {
       static DBObjPtr<BeamSpot> beamSpotDB;
       const auto& frame = ReferenceFrame::GetCurrent();
-      return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).Y();
+      auto trackFit = getTrackFitResultFromParticle(part);
+      if (!trackFit) {
+        return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).Y();
+      } else {
+        UncertainHelix helix = trackFit->getUncertainHelix();
+        helix.passiveMoveBy(beamSpotDB->getIPPosition());
+        return frame.getVertex(helix.getPerigee()).Y();
+      }
     }
 
     double particleDZ(const Particle* part)
     {
       static DBObjPtr<BeamSpot> beamSpotDB;
       const auto& frame = ReferenceFrame::GetCurrent();
-      return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).Z();
+      auto trackFit = getTrackFitResultFromParticle(part);
+      if (!trackFit) {
+        return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).Z();
+      } else {
+        UncertainHelix helix = trackFit->getUncertainHelix();
+        helix.passiveMoveBy(beamSpotDB->getIPPosition());
+        return frame.getVertex(helix.getPerigee()).Z();
+      }
     }
 
     double particleDRho(const Particle* part)
     {
       static DBObjPtr<BeamSpot> beamSpotDB;
       const auto& frame = ReferenceFrame::GetCurrent();
-      return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).Perp();
+      auto trackFit = getTrackFitResultFromParticle(part);
+      if (!trackFit) {
+        return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).Perp();
+      } else {
+        UncertainHelix helix = trackFit->getUncertainHelix();
+        helix.passiveMoveBy(beamSpotDB->getIPPosition());
+        return frame.getVertex(helix.getPerigee()).Perp();
+      }
     }
 
     double particleDPhi(const Particle* part)
     {
       static DBObjPtr<BeamSpot> beamSpotDB;
       const auto& frame = ReferenceFrame::GetCurrent();
-      return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).Phi();
+      auto trackFit = getTrackFitResultFromParticle(part);
+      if (!trackFit) {
+        return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).Phi();
+      } else {
+        UncertainHelix helix = trackFit->getUncertainHelix();
+        helix.passiveMoveBy(beamSpotDB->getIPPosition());
+        return frame.getVertex(helix.getPerigee()).Phi();
+      }
     }
 
     double particleDCosTheta(const Particle* part)
     {
       static DBObjPtr<BeamSpot> beamSpotDB;
       const auto& frame = ReferenceFrame::GetCurrent();
-      return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).CosTheta();
+      auto trackFit = getTrackFitResultFromParticle(part);
+      if (!trackFit) {
+        return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).CosTheta();
+      } else {
+        UncertainHelix helix = trackFit->getUncertainHelix();
+        helix.passiveMoveBy(beamSpotDB->getIPPosition());
+        return frame.getVertex(helix.getPerigee()).CosTheta();
+      }
     }
 
     double particleDistance(const Particle* part)
     {
       static DBObjPtr<BeamSpot> beamSpotDB;
       const auto& frame = ReferenceFrame::GetCurrent();
-      return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).Mag();
+      auto trackFit = getTrackFitResultFromParticle(part);
+      if (!trackFit) {
+        return frame.getVertex(part->getVertex() - beamSpotDB->getIPPosition()).Mag();
+      } else {
+        UncertainHelix helix = trackFit->getUncertainHelix();
+        helix.passiveMoveBy(beamSpotDB->getIPPosition());
+        return frame.getVertex(helix.getPerigee()).Mag();
+      }
     }
 
     double particleDistanceSignificance(const Particle* part)

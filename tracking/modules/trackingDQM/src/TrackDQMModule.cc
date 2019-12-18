3d6c9895326627f289f16f9606417a21eae61f78
THOMAS Lueck <thomas.lueck@desy.de>
2019-12-18 22:22:11 +0900

    adjusted title of Histogram

--- tracking/modules/trackingDQM/src/TrackDQMModule.cc
+++ tracking/modules/trackingDQM/src/TrackDQMModule.cc
@@ -354,7 +354,7 @@ void TrackDQMModule::defineHisto()
   }
 
   // only monitor if any flag was set so only 2 bins needed
-  m_trackingErrorFlags = new TH1F("NumberTrackingErrorFlags", "Number tracking Erros."
+  m_trackingErrorFlags = new TH1F("NumberTrackingErrorFlags", "Tracking error summary."
                                   " Mean = errors/event (should be 0 or very close to 0); Error occured yes or no; Number of events", 2, -0.5, 1.5);
   m_trackingErrorFlags->GetXaxis()->SetBinLabel(1, "No Error");
   m_trackingErrorFlags->GetXaxis()->SetBinLabel(2, "Error occured");

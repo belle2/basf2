c7687191733e291fda41b6a38b5990cda5a7fd26
Racha Cheaib <rachac@mail.ubc.ca>
2020-01-12 13:14:11 +0900

    added test files

--- skim/scripts/skimTestFilesInfo.py
+++ skim/scripts/skimTestFilesInfo.py
@@ -57,6 +57,14 @@ nEventsPerFile = {
     'proc9_exp8': 7846,
     'bucket7_exp8': 11242,
 
+    'MC13_mixedBGx1': 200000,
+    'MC13_chargedBGx1': 200000,
+    'MC13_uubarBGx1': 197609,
+    'MC13_ddbarBGx1':  197673,
+    'MC13_ssbarBGx1': 189320,
+    'MC13_ccbarBGx1': 195621,
+    'MC13_taupairBGx1': 200000,
+
     'MC12_mixedBGx1': 200000,
     'MC12_chargedBGx1': 200000,
     'MC12_uubarBGx1': 200000,
@@ -75,6 +83,28 @@ nEventsPerFile = {
 
 #: location of test files on kekcc
 kekcc_locations = [
+
+    ('proc10_exp7', '/group/belle2/dataprod/Data/release-04-01-00/DB00000748/proc10/e0007/4S/r01232/all/' +
+     'mdst/sub00/mdst.physics.0007.01232.HLT1.f00000.root'),
+    ('proc10_exp8', '/group/belle2/dataprod/Data/release-04-01-00/DB00000748/proc10/e0008/4S/r01026/all/' +
+     'mdst/sub00/mdst.physics.0008.01026.HLT1.f00001.root'),
+
+    ('MC13_mixedBGx1', '/group/belle2/dataprod/MC/MC13a/prod00009434/s00/e1003/4S/r00000/mixed/' +
+     'mdst/sub00/mdst_000001_prod00009434_task10020000001.root')
+    ('MC13_chargedBGx1', '/group/belle2/dataprod/MC/MC13a/prod00009435/s00/e1003/4S/r00000/charged/' +
+     'mdst/sub00/mdst_000001_prod00009435_task10020000001.root'),
+    ('MC13_uubarBGx1', '/ghi/fs01/belle2/bdata/MC/release-04-00-03/DB00000757/MC13a/prod00009436/s00/e1003/4S/r00000/uubar/' +
+     'mdst/sub00/mdst_000001_prod00009436_task10020000001.root'),
+    ('MC13_ccbarBGx1', '/ghi/fs01/belle2/bdata/MC/release-04-00-03/DB00000757/MC13a/prod00009437/s00/e1003/4S/r00000/ccbar/' +
+     'mdst/sub00/mdst_000001_prod00009437_task10020000001.root'),
+    ('MC13_ddbarBGx1', '/ghi/fs01/belle2/bdata/MC/release-04-00-03/DB00000757/MC13a/prod00009438/s00/e1003/4S/r00000/ddbar/' +
+     'mdst/sub00/mdst_000001_prod00009438_task10020000001.root'),
+    ('MC13_ssbarBGx1', '/ghi/fs01/belle2/bdata/MC/release-04-00-03/DB00000757/MC13a/prod00009439/s00/e1003/4S/r00000/ssbar/' +
+     'mdst/sub00/mdst_000001_prod00009439_task10020000001.root'),
+    ('MC13_taupairBGx1', '/ghi/fs01/belle2/bdata/MC/release-04-00-03/DB00000757/MC13a/prod00009441/s00/e1003/4S/r00000/taupair/' +
+     'mdst/sub00/mdst_000001_prod00009441_task10020000001.root'),
+
+
     ('proc9_exp3', '/group/belle2/dataprod/Data/release-03-02-02/DB00000654/proc9/e0003/4S/r05241/skim/hlt_hadron/' +
      'mdst/sub00/mdst.physics.0003.05241.HLT0.hlt_hadron.f00000.root'),
     ('proc9_exp7', '/group/belle2/dataprod/Data/release-03-02-02/DB00000654/proc9/e0007/4S/GoodRuns/r03822/skim/hlt_hadron/' +

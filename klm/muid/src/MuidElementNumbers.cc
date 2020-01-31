352600e09dbefcc2b758d1dc794dc3bf807a3124
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2020-01-31 22:29:04 +0900

    Added the class MuidElementNumbers to handle all the Muid constants and numbers

---
+++ klm/muid/src/MuidElementNumbers.cc
@@ -0,0 +1,22 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2020  Belle II Collaboration                              *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Giacomo De Pietro                                        *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+/* Own header. */
+#include <klm/muid/MuidElementNumbers.h>
+
+using namespace Belle2;
+
+MuidElementNumbers::MuidElementNumbers()
+{
+}
+
+MuidElementNumbers::~MuidElementNumbers()
+{
+}

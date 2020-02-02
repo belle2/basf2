8e6b61e6071bb7eb02d8548ec618167ab1814c5a
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2020-02-02 10:44:42 +0900

    Better coding and commenting style.

--- klm/muid/src/MuidBuilder.cc
+++ klm/muid/src/MuidBuilder.cc
@@ -28,9 +28,10 @@ namespace Belle2 {
   {
     for (int outcome = 1; outcome <= MuidElementNumbers::getMaximalOutcome(); ++outcome) {
       for (int lastLayer = 0; lastLayer <= MuidElementNumbers::getMaximalBarrelLayer(); ++lastLayer) {
+        /* MuidElementNumbers::getMaximalBarrelLayer() and MuidElementNumbers::getMaximalEndcapForwardLayer() are 0 index based thus an addition of 2 is needed. */
         for (unsigned int layer = 0;
              layer < MuidElementNumbers::getMaximalBarrelLayer() + MuidElementNumbers::getMaximalEndcapForwardLayer() + 2;
-             ++layer) { // MuidElementNumbers::getMaximalBarrelLayer() and MuidElementNumbers::getMaximalEndcapForwardLayer() are 0 index based thus an addition of 2 is needed
+             ++layer) {
           m_LayerPDF[outcome][lastLayer][layer] = 0.0;
         }
       }
@@ -52,22 +53,28 @@ namespace Belle2 {
 
   MuidBuilder::MuidBuilder(int expNo, const char hypothesisName[]) : m_ReducedChiSquaredDx(0.0)
   {
-    //fill PDFs by reading database
+    /* Fill PDFs by reading database. */
     fillPDFs(hypothesisName);
-    if (m_ReducedChiSquaredDx == 0.0) { B2FATAL("Failed to read " << hypothesisName << " PDFs for experiment " << expNo); }
+    if (m_ReducedChiSquaredDx == 0.0)
+      B2FATAL("Failed to read " << hypothesisName << " PDFs for experiment " << expNo);
   }
 
   MuidBuilder::~MuidBuilder()
   {
   }
 
-
   void MuidBuilder::fillPDFs(const char hypothesisName[])
   {
     vector<string> const hypotheses = {"Positron", "Electron" , "Deuteron", "Antideuteron", "Proton", "Antiproton", "PionPlus", "PionMinus", "KaonPlus", "KaonMinus", "MuonPlus", "MuonMinus" };
     int hypothesis = -1;
-    for (unsigned int ii = 0; ii < hypotheses.size(); ii++) { if (hypothesisName == hypotheses[ii]) {hypothesis = ii; break;}}
-    if (hypothesis == -1) B2FATAL("MuidBuilder::fillPDFs(): hypothesisName " << hypothesisName << "is not expected. ");
+    for (unsigned int ii = 0; ii < hypotheses.size(); ii++) {
+      if (hypothesisName == hypotheses[ii]) {
+        hypothesis = ii;
+        break;
+      }
+    }
+    if (hypothesis == -1)
+      B2FATAL("MuidBuilder::fillPDFs(): hypothesisName " << hypothesisName << "is not expected. ");
 
     for (int outcome = 1; outcome <= MuidElementNumbers::getMaximalOutcome(); ++outcome) {
       for (int lastLayer = 0; lastLayer <= MuidElementNumbers::getMaximalBarrelLayer(); ++lastLayer) {
@@ -92,7 +99,7 @@ namespace Belle2 {
         if (reducedChiSquaredPDF.size() != MuidElementNumbers::getSizeReducedChiSquared()) {
           B2ERROR("TransversePDF vector for hypothesis " << hypothesisName << "  detector " << detector
                   << " has " << reducedChiSquaredPDF.size() << " entries; should be " << MuidElementNumbers::getSizeReducedChiSquared());
-          m_ReducedChiSquaredDx = 0.0; // invalidate the PDFs for this hypothesis
+          m_ReducedChiSquaredDx = 0.0; /* Invalidate the PDFs for this hypothesis. */
         } else {
           for (int i = 0; i < MuidElementNumbers::getSizeReducedChiSquared(); ++i) {
             m_ReducedChiSquaredPDF[detector][halfNdof][i] = reducedChiSquaredPDF[i];
@@ -108,26 +115,23 @@ namespace Belle2 {
         }
       }
     }
-
   }
 
   void MuidBuilder::spline(int n, double dx, double Y[], double B[], double C[], double D[])
   {
-
-    // Generate the spline interpolation coefficients B, C, D to smooth out a
-    // binned histogram. Restrictions:  equal-size bins. more than 3 bins.
-
+    /* Generate the spline interpolation coefficients B, C, D to smooth out
+     * a binned histogram. Restrictions: equal-size bins; more than 3 bins. */
     D[0] = dx;
     C[1] = (Y[1] - Y[0]) / dx;
     for (int i = 1; i < n - 1; i++) {
-      D[i]   = dx;
-      B[i]   = dx * 4.0;
+      D[i]     = dx;
+      B[i]     = dx * 4.0;
       C[i + 1] = (Y[i + 1] - Y[i]) / dx;
-      C[i]   = C[i + 1] - C[i];
+      C[i]     = C[i + 1] - C[i];
     }
-    B[0]   = -dx;
+    B[0]     = -dx;
     B[n - 1] = -dx;
-    C[0]   = (C[2]   - C[1]) / 6.0;
+    C[0]     = (C[2]   - C[1]) / 6.0;
     C[n - 1] = -(C[n - 2] - C[n - 3]) / 6.0;
     for (int i = 1; i < n; i++) {
       double temp = dx / B[i - 1];
@@ -148,23 +152,22 @@ namespace Belle2 {
     D[n - 1] = D[n - 2];
   }
 
-
   double MuidBuilder::getPDF(const KLMMuidLikelihood* muid, bool isForward) const
   {
     return getPDFLayer(muid, isForward) * getPDFRchisq(muid);
   }
 
-
   double MuidBuilder::getPDFLayer(const KLMMuidLikelihood* muid, bool isForward) const
   {
-
     int outcome = muid->getOutcome();
-    if ((outcome <= 0) || (outcome > MuidElementNumbers::c_ExitForwardEndcap)) return 0.0;
-
+    if ((outcome <= MuidElementNumbers::c_NotReached) || (outcome > MuidElementNumbers::c_ExitForwardEndcap))
+      return 0.0;
     int barrelExtLayer = muid->getBarrelExtLayer();
+    if (barrelExtLayer > MuidElementNumbers::getMaximalBarrelLayer())
+      return 0.0;
     int endcapExtLayer = muid->getEndcapExtLayer();
-    if (barrelExtLayer > MuidElementNumbers::getMaximalBarrelLayer()) return 0.0;
-    if (endcapExtLayer > MuidElementNumbers::getMaximalEndcapForwardLayer()) return 0.0;
+    if (endcapExtLayer > MuidElementNumbers::getMaximalEndcapForwardLayer())
+      return 0.0;
     unsigned int extLayerPattern = muid->getExtLayerPattern();
     unsigned int hitLayerPattern = muid->getHitLayerPattern();
 
@@ -189,14 +192,12 @@ namespace Belle2 {
       }
     }
 
-
-    //Pdf treatment used to avoid layer inefficiency problems
+    /* Longitudinal PDF computation for barrel. */
     double pdf = 1.0;
     unsigned int testBit = 1;
-
     for (int layer = 0; layer <= barrelExtLayer; ++layer) {
       if ((testBit & extLayerPattern) != 0) {
-        if ((testBit & hitLayerPattern) != 0) {//checking the presence of a hit in the layer
+        if ((testBit & hitLayerPattern) != 0) { /* Checking the presence of a hit in the layer. */
           pdf *= m_LayerPDF[outcome][lastLayer][layer];
         } else {
           if (((layer == 0) && (outcome < MuidElementNumbers::c_CrossBarrelStopInForwardMin))
@@ -205,14 +206,14 @@ namespace Belle2 {
           }
         }
       }
-      testBit <<= 1; // move to next bit
+      testBit <<= 1; /* Move to next bit. */
     }
-
+    /* Longitudinal PDF computation for endcap. */
     int maxLayer = isForward ? MuidElementNumbers::getMaximalEndcapForwardLayer() : MuidElementNumbers::getMaximalEndcapBackwardLayer();
     testBit = 1 << (MuidElementNumbers::getMaximalBarrelLayer() + 1);
     for (int layer = 0; layer <= endcapExtLayer; ++layer) {
       if ((testBit & extLayerPattern) != 0) {
-        if ((testBit & hitLayerPattern) != 0) {//checking the presence of a hit in the layer
+        if ((testBit & hitLayerPattern) != 0) { /* Checking the presence of a hit in the layer. */
           pdf *= m_LayerPDF[outcome][lastLayer][layer + MuidElementNumbers::getMaximalBarrelLayer() + 1];
         } else {
           if ((layer == 0) || (layer == maxLayer) || (layer < endcapExtLayer)) {
@@ -221,43 +222,39 @@ namespace Belle2 {
           }
         }
       }
-      testBit <<= 1; // move to next bit
+      testBit <<= 1; /* move to next bit. */
     }
-
     return pdf;
-
   }
-
 
   double MuidBuilder::getPDFRchisq(const KLMMuidLikelihood* muid) const
   {
-
-    // Evaluate the transverse-coordinate PDF for this particleID hypothesis.
-
+    /* Evaluate the transverse-coordinate PDF for this particleID hypothesis. */
     int ndof = muid->getDegreesOfFreedom();
-    if (ndof <= 0) return 1.0;
+    if (ndof <= 0)
+      return 1.0;
     double chiSquared = muid->getChiSquared();
-    if (chiSquared < 0.0) return 0.0;
+    if (chiSquared < 0.0)
+      return 0.0;
     int halfNdof = (ndof >> 1);
     double x = chiSquared / ndof;
 
-    // Assume that the track crossed both barrel and endcap
+    /* Assume that the track crossed both barrel and endcap. */
     int detector = 0;
     if (muid->getEndcapExtLayer() < 0) {
-      detector = 1; // crossed barrel only
+      detector = 1; /* Crossed barrel only. */
     } else if (muid->getBarrelExtLayer() < 0) {
-      detector = 2;  // crossed endcap only
+      detector = 2;  /* Crossed endcap only. */
     }
 
-    // Use spline interpolation of the logarithms of the PDF to avoid binning artifacts.
-    // Use fitted tail function for reduced-chiSquared values beyond the tabulated threshold.
-
+    /* Use spline interpolation of the logarithms of the PDF to avoid binning artifacts.
+     * Use fitted tail function for reduced-chiSquared values beyond the tabulated threshold. */
     double pdf = 0.0;
-    if (halfNdof > MuidElementNumbers::getMaximalHalfNdof()) { // extremely rare
+    if (halfNdof > MuidElementNumbers::getMaximalHalfNdof()) { /* Extremely rare. */
       x *= m_ReducedChiSquaredScaleX[detector][MuidElementNumbers::getMaximalHalfNdof()] * halfNdof;
       pdf = m_ReducedChiSquaredScaleY[detector][MuidElementNumbers::getMaximalHalfNdof()] * std::pow(x, halfNdof - 1.0) * std::exp(-x);
     } else {
-      if (x > m_ReducedChiSquaredThreshold[detector][halfNdof]) { // tail function for large x
+      if (x > m_ReducedChiSquaredThreshold[detector][halfNdof]) { /* Tail function for large x. */
         x *= m_ReducedChiSquaredScaleX[detector][halfNdof] * halfNdof;
         if (halfNdof == 1) {
           pdf = m_ReducedChiSquaredScaleY[detector][halfNdof] * std::exp(-x);
@@ -268,7 +265,7 @@ namespace Belle2 {
         } else {
           pdf = m_ReducedChiSquaredScaleY[detector][halfNdof] * std::pow(x, halfNdof - 1.0) * std::exp(-x);
         }
-      } else { // spline-interpolated histogram for small x
+      } else { /* Spline-interpolated histogram for small x. */
         x -= 0.5 * m_ReducedChiSquaredDx;
         int i  = (int)(x / m_ReducedChiSquaredDx);
         double logPdf = m_ReducedChiSquaredPDF[detector][halfNdof][i];

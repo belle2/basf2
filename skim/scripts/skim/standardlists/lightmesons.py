e668b37f5b01504cecc285d216e987f5c3058cee
ABUDINEN Fernando <fernando.abudinen@ts.infn.it>
2020-02-06 15:17:59 +0900

    Introduced new 'pi0:bth_skim' list for BTohadron decays with pi0. The new list is based on recommendations provided by the neutrals group.

--- skim/scripts/skim/standardlists/lightmesons.py
+++ skim/scripts/skim/standardlists/lightmesons.py
@@ -2,6 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import modularAnalysis as ma
+from vertex import KFit
+from stdPi0s import stdPi0s
+
+# Creates list of neutral pions for hadronic B skims following the recommendations of the neutral group
+# for winter 2020.
+
+
+def loadStdPi0ForBToHadrons(persistent=True, path=None):
+    """
+    Creates a list 'pi0:bth_skim' for :math:`B\\to {\\rm hadrons}` skims, based on recommendations of
+    the neutral group (avoiding ECL timing cuts) for winter 2020. We require the energy of the photons
+    to be larger than :math:`80~{\\rm MeV}` in the forward end cap, :math:`30~{\\rm MeV}` in the barrel,
+    and :math:`60~{\\rm MeV}` in the backward end cap. For the :math:`\\pi^{0}`, we require the mass to be
+    :math:`105 < M < 150~{\\rm MeV}/c^2` and a massKFit to converge.
+    """
+    stdPi0s('all', path)
+    ma.cutAndCopyList(outputListName='pi0:bth_skim', inputListName='pi0:all',
+                      cut='[[daughter(0, clusterReg) == 1 and daughter(0, E) > 0.080] or ' +
+                      '[daughter(0, clusterReg) == 2 and daughter(0, E) > 0.030] or ' +
+                      '[daughter(0, clusterReg) == 3 and daughter(0, E) > 0.060]] and ' +
+                      '[[daughter(1, clusterReg) == 1 and daughter(1, E) > 0.080] or ' +
+                      '[daughter(1, clusterReg) == 2 and daughter(1, E) > 0.030] or ' +
+                      '[daughter(1, clusterReg) == 3 and daughter(1, E) > 0.060]] and ' +
+                      'M > 0.105 and M < 0.150', path=path)
+    KFit('pi0:bth_skim', 0.0, 'mass', path=path)
+
 
 # Call to build all light mesons. Not recommended to use this general function as it creates many candidates
 
@@ -71,12 +97,15 @@ def loadStdLooseRhoPlus(persistent=True, path=None):
 
 def loadStdAllRhoPlus(persistent=True, path=None):
     """
-    Create a list of 'rho+:all' list from 'pi0:loose pi+:all' with :math:`0.47 < M < 1.07~GeV`
+    Create a list of 'rho+:all' list from 'pi0:bth_skim' pi+:all' with :math:`0.47 < M < 1.07~GeV`.
+    We apply few sanity cuts on the pi+: thetaInCDCAcceptance, abs(dr) < 0.5, and abs(dz) < 3.
 
     @param persistent   whether RootOutput module should save the created ParticleLists (default True)
     @param path         modules are added to this path
     """
-    ma.reconstructDecay('rho+:all -> pi0:loose pi+:all', '0.47 < M < 1.07', 1, persistent, path)
+    ma.reconstructDecay('rho+:all -> pi0:bth_skim pi+:all', '0.47 < M < 1.07 and ' +
+                        'daughter(1,thetaInCDCAcceptance) > 0 and abs(daughter(1,dr)) < 0.5 and ' +
+                        'abs(daughter(1,dz)) < 3', 1, persistent, path)
     return 'rho+:all'
 
 

941ca6db80dd952348f9562a377256d346d15cff
Karol Adamczyk <karol.mateusz.adamczyk@desy.de>
2020-02-25 09:29:46 +0100

    Added plots for cluster validation

---
+++ svd/validation/SVDValidationClusterPerformance.py
@@ -0,0 +1,238 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+<header>
+    <input>SVDValidationTTree.root</input>
+    <output>ClusterPerformance.root</output>
+    <description>
+    Validation plots related to cluster performance.
+    </description>
+    <contact>
+    SVD Software Group, svd-software@belle2.org
+    </contact>
+</header>
+"""
+
+from plotUtils import *
+
+# open the files with simulated and reconstructed events data
+input = R.TFile.Open("../SVDValidationTTree.root")
+
+# loads ttrees
+tree = input.Get("tree")
+
+# ClusterPerformance
+histsCP = R.TFile.Open("ClusterPerformance.root", "recreate")
+
+ploter(
+    name='UPositionResolution_size1',
+    title='Cluster position resolution for one strip on U side',
+    nbins=100,
+    xmin=-0.01,
+    xmax=0.01,
+    x_label='Cluster position resolution (cm)',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='cluster_position - truehit_position',
+    cut='(cluster_size==1)&&(strip_dir==0)',
+    descr='position resolution = (reconstructed position of the Cluster) - (position of the Truehit); for one strip on U side',
+    check='peak around 0',
+    isShifter=False)
+
+ploter(
+    name='VPositionResolution_size1',
+    title='Cluster position resolution for one strip in V side',
+    nbins=100,
+    xmin=-0.01,
+    xmax=0.01,
+    x_label='Cluster position resolution (cm)',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='cluster_position - truehit_position',
+    cut='(cluster_size==1)&&(strip_dir==1)',
+    descr='position resolution = (reconstructed position of the Cluster) - (position of the Truehit); for one strip on V side',
+    check='peak around 0',
+    isShifter=False)
+
+ploter(
+    name='UPositionResolution_size2',
+    title='Cluster position resolution for two strips on U side',
+    nbins=100,
+    xmin=-0.01,
+    xmax=0.01,
+    x_label='Cluster position resolution (cm)',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='cluster_position - truehit_position',
+    cut='(cluster_size==2)&&(strip_dir==0)',
+    descr='position resolution = (reconstructed position of the Cluster) - (position of the Truehit); for two strips on U side',
+    check='peak around 0',
+    isShifter=False)
+
+ploter(
+    name='VPositionResolution_size2',
+    title='Cluster position resolution for two strips on V side',
+    nbins=100,
+    xmin=-0.01,
+    xmax=0.01,
+    x_label='Cluster position resolution (cm)',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='cluster_position - truehit_position',
+    cut='(cluster_size==2)&&(strip_dir==1)',
+    descr='position resolution = (reconstructed position of the Cluster) - (position of the Truehit); for two strips on V side',
+    check='peak around 0',
+    isShifter=False)
+
+ploter(
+    name='UPositionResolution_size3plus',
+    title='Cluster position resolution for 3 or more strips on U side',
+    nbins=100,
+    xmin=-0.01,
+    xmax=0.01,
+    x_label='Cluster position resolution (cm)',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='cluster_position - truehit_position',
+    cut='(cluster_size>2)&&(strip_dir==0)',
+    descr='position resolution = (reconstructed position of the Cluster) - (position of the Truehit); for >2 strips on U side',
+    check='peak around 0',
+    isShifter=False)
+
+ploter(
+    name='VPositionResolution_size3plus',
+    title='Cluster position resolution for 3 or more strips on U side',
+    nbins=100,
+    xmin=-0.01,
+    xmax=0.01,
+    x_label='Cluster position resolution (cm)',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='cluster_position - truehit_position',
+    cut='(cluster_size>2)&&(strip_dir==1)',
+    descr='position resolution = (reconstructed position of the Cluster) - (position of the Truehit); for >2 strips on V side',
+    check='peak around 0',
+    isShifter=False)
+
+ploter(
+    name='UPositionPull_size1',
+    title='Cluster position pull for one strip on U side',
+    nbins=100,
+    xmin=-5,
+    xmax=5,
+    x_label='Cluster position pull',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='(cluster_position - truehit_position)/cluster_positionSigma',
+    cut='(cluster_size==1)&&(strip_dir==0)',
+    descr='Pull: (cluster_position - truehit_position)/cluster_positionSigma; for one strip on U side',
+    check='peak around 0 with RMS less than 2',
+    isShifter=False)
+
+ploter(
+    name='VPositionPull_size1',
+    title='Cluster position pull for one strip on V side',
+    nbins=100,
+    xmin=-5,
+    xmax=5,
+    x_label='Cluster position pull',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='(cluster_position - truehit_position)/cluster_positionSigma',
+    cut='(cluster_size==1)&&(strip_dir==1)',
+    descr='Pull: (cluster_position - truehit_position)/cluster_positionSigma; for one strip on V side',
+    check='peak around 0 with RMS less than 2',
+    isShifter=False)
+
+
+ploter(
+    name='UPositionPull_size2',
+    title='Cluster position pull for two strip on U side',
+    nbins=100,
+    xmin=-5,
+    xmax=5,
+    x_label='Cluster position pull',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='(cluster_position - truehit_position)/cluster_positionSigma',
+    cut='(cluster_size==2)&&(strip_dir==0)',
+    descr='Pull: (cluster_position - truehit_position)/cluster_positionSigma; for two strips on U side',
+    check='peak around 0 with RMS less than 2',
+    isShifter=False)
+
+ploter(
+    name='VPositionPull_size2',
+    title='Cluster position pull for two strip on V side',
+    nbins=100,
+    xmin=-5,
+    xmax=5,
+    x_label='Cluster position pull',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='(cluster_position - truehit_position)/cluster_positionSigma',
+    cut='(cluster_size==2)&&(strip_dir==1)',
+    descr='Pull: (cluster_position - truehit_position)/cluster_positionSigma; for two strips on V side',
+    check='peak around 0 with RMS less than 2',
+    isShifter=False)
+
+
+ploter(
+    name='UPositionPull_size3plus',
+    title='Cluster position pull for 3 or more strips on U side',
+    nbins=100,
+    xmin=-5,
+    xmax=5,
+    x_label='Cluster position pull',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='(cluster_position - truehit_position)/cluster_positionSigma',
+    cut='(cluster_size>2)&&(strip_dir==0)',
+    descr='Pull: (cluster_position - truehit_position)/cluster_positionSigma; for 3 or more strips on U side',
+    check='peak around 0 with RMS less than 2',
+    isShifter=False)
+
+
+ploter(
+    name='VPositionPull_size3plus',
+    title='Cluster position pull for 3 or more strips on V side',
+    nbins=100,
+    xmin=-5,
+    xmax=5,
+    x_label='Cluster position pull',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='(cluster_position - truehit_position)/cluster_positionSigma',
+    cut='(cluster_size>2)&&(strip_dir==1)',
+    descr='Pull: (cluster_position - truehit_position)/cluster_positionSigma; for 3 or more strips on V side',
+    check='peak around 0 with RMS less than 2',
+    isShifter=False)
+
+
+ploter(
+    name='timeResolution',
+    title='Cluster time resolution',
+    nbins=200,
+    xmin=-100,
+    xmax=100,
+    x_label='Cluster time resolution (ns)',
+    y_label='counts',
+    granules=granulesD,
+    tree=tree,
+    expr='cluster_clsTime - truehit_time',
+    cut='',
+    descr='(reconstructed time of the Cluster) - (time of the Truehit) for all Clusters related to TrueHits',
+    check='peak around 0',
+    isShifter=True)

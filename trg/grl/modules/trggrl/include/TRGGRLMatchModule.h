95ed18346730da18049bea2ccb86ae97b25ddbd3
Yun-Tsung Lai <ytlai@post.kek.jp>
2020-03-02 21:42:12 +0900

    Short tracking in TSIM

--- trg/grl/modules/trggrl/include/TRGGRLMatchModule.h
+++ trg/grl/modules/trggrl/include/TRGGRLMatchModule.h
@@ -16,8 +16,17 @@
 #include <trg/cdc/dataobjects/CDCTriggerTrack.h>
 #include <trg/ecl/dataobjects/TRGECLCluster.h>
 #include <trg/klm/dataobjects/KLMTriggerTrack.h>
+#include <trg/cdc/dataobjects/CDCTriggerSegmentHit.h>
+#include <trg/grl/dataobjects/TRGGRLInfo.h>
 #include <string>
 #include <memory>
+#include <vector>
+
+#include <framework/datastore/DataStore.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/datastore/RelationArray.h>
+
 
 namespace Belle2 {
   /**
@@ -56,7 +65,8 @@ namespace Belle2 {
     void calculationdistance(CDCTriggerTrack* track, TRGECLCluster* cluster, double* ds, int _match3D);
 
     /**calculate dphi_d between track and cluster*/
-    void calculationphiangle(CDCTriggerTrack* track, TRGECLCluster* cluster, int& dphi_d, std::vector<bool>& track_phimap);
+    void calculationphiangle(CDCTriggerTrack* track, TRGECLCluster* cluster, int& dphi_d, std::vector<bool>& track_phimap,
+                             std::vector<bool>& track_phimap_i);
 
     /**calculate dphi between 2D track and KLM track*/
     void sectormatching_klm(CDCTriggerTrack* track, KLMTriggerTrack* klmtrack, double& dphi);
@@ -64,7 +74,27 @@ namespace Belle2 {
     /**determine photon from isolated cluster*/
     bool photon_cluster(TRGECLCluster* cluster, std::vector<bool> track_phimap, double e_threshold);
 
+    /** Force an int to be witnin 0 to 63*/
+    int N64(int x);
+
+    /** Force an int to be witnin 0 to 35*/
+    int N36(int x);
+
+    /** Fill the patterns in short tracking logic*/
+    void fill_pattern_base2(std::vector< std::vector<int> >& patt);
+
+    /** Make the full track phi veto map for short tracking*/
+    void make_veto_map(StoreArray<CDCTriggerTrack> track2Dlist, std::vector<bool>&  map_veto);
+
+    /** Short tracking logic*/
+    void short_tracking(StoreArray<CDCTriggerSegmentHit> tslist, std::vector<bool>  map_veto, std::vector<bool>  phimap_i,
+                        std::vector< std::vector<int> >& pattern_base0, std::vector< std::vector<int> >& pattern_base2, StoreObjPtr<TRGGRLInfo> trgInfo);
+
+    /** Short track extrapolation (to endcap) function*/
+    void extrapolation(int pattern, int& l, int& r, int& ec);
+
   private:
+    StoreObjPtr<TRGGRLInfo> m_TRGGRLInfo; /**< output for TRGGRLInfo */
 
     /// Mode for TRGGRL simulation. 0th bit : fast simulation switch,
     /// 1st bit : firmware simulation switch.
@@ -89,6 +119,8 @@ namespace Belle2 {
     double m_dphi_klm_threshold;
     /**36 bits phi map of all 2D tracks */
     std::vector<bool> track_phimap;
+    /**36 bits phi map of all 2D tracks */
+    std::vector<bool> track_phimap_i;
     /**the 2D finder track list*/
     std::string m_2d_tracklist;
     /**the 3D NN track list*/
@@ -111,5 +143,13 @@ namespace Belle2 {
     std::string m_klmmatch_tracklist;
     /**Non-matched cluster list at GRL*/
     std::string m_grlphotonlist;
+    /**Track Segment list*/
+    std::string m_hitCollectionName;
+    /** Name of the StoreArray holding projects information from grl */
+    std::string m_TrgGrlInformationName;
+    /** Short tracking patterns based on SL0*/
+    std::vector< std::vector<int> > patterns_base0;
+    /** Short tracking patterns based on SL2*/
+    std::vector< std::vector<int> > patterns_base2;
   };
 }

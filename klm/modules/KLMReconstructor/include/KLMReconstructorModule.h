ce9a935c3bbca0b3473fe9d3e8e2fef3cded4d54
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2020-03-20 04:39:21 +0900

    Fixed year and contributor names

--- klm/modules/KLMReconstructor/include/KLMReconstructorModule.h
+++ klm/modules/KLMReconstructor/include/KLMReconstructorModule.h
@@ -1,9 +1,10 @@
 /**************************************************************************
  * BASF2 (Belle Analysis Framework 2)                                     *
- * Copyright(C) 2019 - Belle II Collaboration                             *
+ * Copyright(C) 2020 - Belle II Collaboration                             *
  *                                                                        *
  * Author: The Belle II Collaboration                                     *
- * Contributors: Leo Piilonen                                             *
+ * Contributors: Kirill Kirill Chilikin, Giacomo De Pietro,               *
+ *               Leo Piilonen, Timofey Uglov                              *
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/

02029c31a6fa31972d0daab34cee077f64424154
Giulia Casarosa <giulia.casarosa@desy.de>
2020-03-21 23:50:09 +0900

    fixed documentation in svd/scripts/svd (except for SVDsimBase.py becuase of lack of knowledge on the script)

--- svd/scripts/svd/executionTime_utils.py
+++ svd/scripts/svd/executionTime_utils.py
@@ -1,11 +1,11 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-##
+#
 # contributors: G. Casarosa, T. Lueck
-##
+#
 
-######
+#
 # utils to measure the per-event execution time of all modules
 # appended to the path, SVD observables are also stored in the tree
 #
@@ -16,7 +16,7 @@
 #
 # examples are in svd/examples/executionTime.py
 #
-######
+#
 
 from basf2 import *
 from ROOT import Belle2
@@ -25,26 +25,49 @@ from per_event_statistics import PerEventStatisticsGetterModule
 
 
 class SVDExtraEventStatisticsModule(PerEventStatisticsGetterModule):
+    """
+    a basf2 python module to export all module time statistics (PerEventStatisticsGetterModule) +
+    number of SVDSpacePoints, SVDClusters, SVDShaperDigits and SVDSHaperDigitsZS5
+    into a ROOT TTree written to a file.
+    """
 
     def __init__(self, filename):
+        """
+        creates the module
+        @param filename: name of the rootfile where the TTree is written.
+        """
         super().__init__(filename)
+        #: StoreArray of SVDSpacePoints
         self.svdSPs = Belle2.PyStoreArray("SVDSpacePoints")
+        #: StoreArray of SVDClusters
         self.svdclusters = Belle2.PyStoreArray("SVDClusters")
+        #: StoreArray of SVDShaperDigits
         self.svdstrips = Belle2.PyStoreArray("SVDShaperDigits")
+        #: StoreArray of ZS5  SVDShaperDigits
         self.svdZS5strips = Belle2.PyStoreArray("SVDShaperDigitsZS5")
 
     def initialize(self):
+        """
+        Create the needed store object pointer in the DataStore and the TFile with the TTree.
+        """
+
         super().initialize()
-        # an array needed to assign the actual values, can be extended with further information
+        #: array storing the number of SVDSpacePoints
         self.svd_sps = np.zeros(1, dtype=np.int32)
+        #: array storing the number of SVDClusters
         self.svd_clusters = np.zeros(1, dtype=np.int32)
+        #: array storing the number of SVDShaperDigits
         self.svd_strips = np.zeros(1, dtype=np.int32)
+        #: array storing the number of ZS5  SVDShaperDigits
         self.svd_ZS5strips = np.zeros(1, dtype=np.int32)
 
-        # assign the branch address to the first field of the array
+        #: branch address assignment for SVDSpacePoints
         self.statistics.Branch('svdSPs', self.svd_clusters[0:], "svdSPs/I")
+        #: branch address assignment for SVDClusters
         self.statistics.Branch('svdClusters', self.svd_clusters[0:], "svdClusters/I")
+        #: branch address assignment for SVDShaperDigits
         self.statistics.Branch('svdStrips', self.svd_strips[0:], "svdStrips/I")
+        #: branch address assignment for ZS5 SVDShaperDigits
         self.statistics.Branch('svdZS5strips', self.svd_ZS5strips[0:], "svdZS5strips/I")
 
         # register the StoreArray for the SVD clusters
@@ -59,5 +82,4 @@ class SVDExtraEventStatisticsModule(PerEventStatisticsGetterModule):
         self.svd_clusters[0] = self.svdclusters.getEntries()
         self.svd_strips[0] = self.svdstrips.getEntries()
         self.svd_ZS5strips[0] = self.svdZS5strips.getEntries()
-        print("number of SP = "+str(self.svd_sps[0])+" and clusters = "+str(self.svd_clusters[0]))
         super().event()

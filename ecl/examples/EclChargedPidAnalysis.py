539e0bcfa55e721f897af62535e4de83dfa311cf
Abtin Narimani Charan <abtin.narimani.charan@desy.de>
2020-03-24 20:00:35 +0100

    proper docstring and better handling input file

--- ecl/examples/EclChargedPidAnalysis.py
+++ ecl/examples/EclChargedPidAnalysis.py
@@ -1,40 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-###############################################################
-# This steering file shows how to use ECLChargedPIDDataAnalysis
-# module to dump ECL related quantities in an ntuple
-# starting from dst root file.
-#
-# Author: The Belle II Collaboration
-# Contributor: Cate MacQueen
-# Contact: cmq.centaurus@gmail.com
-#
-###############################################################
+"""This steering file shows how to use 'ECLChargedPIDDataAnalysis'
+   module to dump ECL-related quantities in an ntuple.
 
-import sys
+Input:
+    File with DST format
+
+Output:
+    Ntuple with ECL-related quantities
+
+Usage:
+    $ basf2 -i <path_to_input_file> -n <number_of_events>
+            EclChargedPidAnalysis.py
+"""
+
 import basf2 as b2
 
-particle_type = sys.argv[1]
-file_num = sys.argv[2]
+__authors__ = ['Caitlin MacQueen', 'Marco Milesi']
+__copyright__ = 'Copyright 2018 - Belle II Collaboration'
+__maintainer__ = 'Abtin Narimani Charan'
+__email__ = 'abtin.narimani.charan@desy.de'
 
-# Input File
+# Create path. Register necessary modules to this path.
+mainPath = b2.create_path()
+
+# Register and add 'RootInput' module
 inputFile = b2.register_module('RootInput')
-inputFile.param('inputFileName',
-                './MDST_pdg{}_BGx1_{}.root'.format(particle_type, file_num))
+mainPath.add_module(inputFile)
 
-# Create paths
-main = b2.create_path()
+# Register and add 'ECLChargedPIDDataAnalysis' module
+eclChargedPIDDataAnalysis = b2.register_module('ECLChargedPIDDataAnalysis')
+eclChargedPIDDataAnalysis.param('rootFileName',
+                                'ECLChargedPIDDataAnalysis_Test.root')
+mainPath.add_module(eclChargedPIDDataAnalysis)
 
-main.add_module(inputFile)
-
-# ECLChargedPIDDataAnalysis module
-eclchargedpid = b2.register_module('ECLChargedPIDDataAnalysis')
-eclchargedpid.param(
-    'rootFileName',
-    './RootOut_pdg{}_BGx1_{}.root'.format(particle_type, file_num))
-
-main.add_module(eclchargedpid)
-
-b2.process(main)
+# Process the events and print call statistics
+mainPath.add_module('Progress')
+b2.process(mainPath)
 print(b2.statistics)

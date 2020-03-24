56a979725d6620a32de9e464fa550601790e0c1e
Abtin Narimani Charan <abtin.narimani.charan@desy.de>
2020-03-24 20:07:05 +0100

    proper docstring

--- ecl/examples/EclDQM.py
+++ ecl/examples/EclDQM.py
@@ -1,52 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-########################################################
-# This steering file creates ECL DQM histograms from
-# input SeqRoot file
-#
-# The user should provide input and output filenames and
-# control EclDQM module parameters
-#
-# Example steering file - 2018 Belle II Collaboration
-#
-# Dmitry Matvienko (d.v.matvienko@inp.nsk.su)
-#
-########################################################
+
+"""This steering file creates ECL DQM histograms from
+   input SeqRoot file
+
+The user should provide input and output file names and
+control 'ECLDQM' module parameters.
+
+Usage:
+    $ basf2 -i <path_to_input_files> -o <path_to_output_files>
+            EclDQM.py
+"""
 
 import basf2 as b2
 
+__author__ = 'Dmitry Matvienko'
+__copyright__ = 'Copyright 2019 - Belle II Collaboration'
+__maintainer__ = 'Dmitry Matvienko'
+__email__ = 'd.v.matvienko@inp.nsk.su'
+
+# Create path. Register necessary modules to this path.
+mainPath = b2.create_path()
+
 b2.set_log_level(b2.LogLevel.ERROR)
 
-# Register necessary modules
-input = b2.register_module('SeqRootInput')
+# Register and add 'SeqRootInput' module
+seqRootInput = b2.register_module('SeqRootInput')
+mainPath.add_module(seqRootInput)
 
-unpacker = b2.register_module("ECLUnpacker")
-unpacker.param("storeTrigTime", True)
+# Register and add 'ECLUnpacker' module
+eclUnpacker = b2.register_module('ECLUnpacker')
+eclUnpacker.param('storeTrigTime', True)
+mainPath.add_module(eclUnpacker)
 
-calib = b2.register_module('ECLDigitCalibrator')
+# Register and add 'ECLDigitCalibrator' module
+eclDigitCalibrator = b2.register_module('ECLDigitCalibrator')
+mainPath.add_module(eclDigitCalibrator)
 
-histo = b2.register_module('HistoManager')
+# Register and add 'HistoManager' module
+histoManager = b2.register_module('HistoManager')
+mainPath.add_module(histoManager)
 
-trigger_skim_module = b2.register_module("TriggerSkim")
-trigger_skim_module.param('triggerLines', ["software_trigger_cut&all&total_result"])
-trigger_skim_module.if_value("==0", Path(), AfterConditionPath.END)
+# Register and add 'TriggerSkim' module and settings
+triggerSkim = b2.register_module('TriggerSkim')
+triggerSkim.param('triggerLines',
+                  ['software_trigger_cut&all&total_result'])
+triggerSkim.if_value('==0', Path(), AfterConditionPath.END)
+mainPath.add_module(triggerSkim)
 
-ecldqm = b2.register_module('ECLDQM')
-ecldqm.param("histogramDirectoryName", "ECL")
-ecldqm.param("EnergyUpperThr", 1.5)
-ecldqm.param("PedestalMeanUpperThr", 7000)
-ecldqm.param("PedestalMeanLowerThr", -1000)
-ecldqm.param("PedestalRmsUpperThr", 100.)
+# Register and add 'ECLDQM' module and settings
+eclDQM = b2.register_module('ECLDQM')
+eclDQM.param('histogramDirectoryName', 'ECL')
+eclDQM.param('EnergyUpperThr', 1.5)
+eclDQM.param('PedestalMeanUpperThr', 7000)
+eclDQM.param('PedestalMeanLowerThr', -1000)
+eclDQM.param('PedestalRmsUpperThr', 100.)
+mainPath.add_module(eclDQM)
 
-# Create paths
-main = b2.create_path()
-main.add_module(input)
-main.add_module(unpacker)
-main.add_module(calib)
-main.add_module(histo)
-main.add_module(trigger_skim_module)
-main.add_module(ecldqm)
-
-# Process all events
-b2.process(main)
+# Process the events and print call statistics
+mainPath.add_module('Progress')
+b2.process(mainPath)
 print(b2.statistics)

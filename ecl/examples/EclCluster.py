187aa43085c5da6224a2eb7c53e8be8e9e62b857
Abtin Narimani Charan <abtin.narimani.charan@desy.de>
2020-03-24 20:02:04 +0100

    proper docstring and better handling input file

--- ecl/examples/EclCluster.py
+++ ecl/examples/EclCluster.py
@@ -1,34 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-########################################################
-# This steering file shows how to use EclDataAnalysis
-# module to dump ECL related quantities in an ntuple
-# starting from dst root file.
-#
-# The user should provide input and output root filnames
-# as first and second argument respectively.
-#
-# Example steering file - 2014 Belle II Collaboration
-########################################################
+"""This steering file shows how to use 'ECLClusterAnalysis'
+   module to dump ECL-related quantities in an ntuple.
+
+Input:
+    File with mdst format
+
+Output:
+    Ntuple with ECL cluster quantities
+
+Usage:
+    $ basf2 -i <path_to_input_file> -n <number_of_events>
+            EclCluster.py
+"""
 
 import basf2 as b2
 
-# Create paths
-main = b2.create_path()
+__authors__ = ['Benjamin Oberhof', 'Frank Meier']
+__copyright__ = 'Copyright 2018 - Belle II Collaboration'
+__maintainer__ = 'Abtin Narimani Charan'
+__email__ = 'abtin.narimani.charan@desy.de'
 
-# Input file
-inputs = 'mdst_000544_prod00000001_task00000544.root'
+# Create path. Register necessary modules to this path.
+mainPath = b2.create_path()
 
-simpleinput = b2.register_module('RootInput')
-simpleinput.param('inputFileNames', inputs)
-main.add_module(simpleinput)
+# Register and add 'RootInput' module
+inputFile = b2.register_module('RootInput')
+mainPath.add_module(inputFile)
 
-# eclDataAnalysis module
-eclanalysis = b2.register_module('ECLClusterAnalysis')
-eclanalysis.param('rootFileName', 'EclClusterAnalysis.root')
-eclanalysis.param('doTracking', 1)
-main.add_module(eclanalysis)
+# Register and add 'ECLClusterAnalysis' module and settings
+eclClusterAnalysis = b2.register_module('ECLClusterAnalysis')
+eclClusterAnalysis.param('rootFileName',
+                         'EclClusterAnalysis_Test.root')
+eclClusterAnalysis.param('doTracking', 1)
+mainPath.add_module(eclClusterAnalysis)
 
-b2.process(main)
+# Process the events and print call statistics
+mainPath.add_module('Progress')
+b2.process(mainPath)
 print(b2.statistics)

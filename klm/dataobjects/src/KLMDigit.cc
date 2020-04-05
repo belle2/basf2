bd87492f4bb3378b334468ac8e20a7f60ebf917a
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2020-04-05 14:26:39 +0200

    Added date, author and copyright info in KLMDigit.cc

--- klm/dataobjects/src/KLMDigit.cc
+++ klm/dataobjects/src/KLMDigit.cc
@@ -1,3 +1,12 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2020 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Kirill Chilikin                                          *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
 
 /* Own header. */
 #include <klm/dataobjects/KLMDigit.h>

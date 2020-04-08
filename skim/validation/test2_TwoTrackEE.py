4fdfe77f2fd659fc22934df3872ed672dd7f5983
Xing-Yu Zhou <xing-yu.zhou@desy.de>
2020-04-09 06:51:52 +0900

    Update the ranges of some validation plots

--- skim/validation/test2_TwoTrackEE.py
+++ skim/validation/test2_TwoTrackEE.py
@@ -30,11 +30,11 @@ variables.addAlias('delta_theta_cms', 'abs(formula('
                    + 'daughter(1, useCMSFrame(theta))*180/3.1415927 - 180))')
 
 variablesHist = [
-                  ('ep_p_cms', 100, 1, 6),
-                  ('em_p_cms', 100, 1, 6),
+                  ('ep_p_cms', 100, 4, 6),
+                  ('em_p_cms', 100, 4, 6),
                   ('ep_theta_lab', 90, 0, 180),
                   ('em_theta_lab', 90, 0, 180),
-                  ('delta_theta_cms', 100, 0, 10)
+                  ('delta_theta_cms', 100, 0, 2)
                 ]
 
 # Output the variables to a ntuple

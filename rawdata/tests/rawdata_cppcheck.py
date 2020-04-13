a9f1fda1379805305c8bc33c5f55ff06b308ae29
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2020-04-13 20:50:25 +0200

    Fixed the cppcheck test

--- rawdata/tests/rawdata_cppcheck.py
+++ rawdata/tests/rawdata_cppcheck.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """
-Perform code doxygen checks for every commit to the rawdata package.
+Perform code quality cppchecks for every commit to the rawdata package.
 """
 
 import re
 from b2test_utils import check_error_free
 
 if __name__ == "__main__":
-    ignoreme = ''
-    check_error_free("b2code-doxygen-warnings", "doxygen", "rawdata",
+    # Ignore the nofile .. [missingInclude] that is always at the end of cppcheck
+    ignoreme = 'Cppcheck cannot find all the include files'
+    check_error_free("b2code-cppcheck", "cppcheck", "rawdata",
                      lambda x: re.findall(ignoreme, x) or x is "'")

ce91d5468d4e9847e269d69003ad7ccf6147ae63
Karol Adamczyk <karol.mateusz.adamczyk@desy.de>
2020-04-14 10:57:34 +0200

    Added SP time and reviewed of labels

--- svd/validation/SVDValidationTrackingPerformance.py
+++ svd/validation/SVDValidationTrackingPerformance.py
@@ -4,6 +4,7 @@
 """
 <header>
     <input>SVDValidationTTreeRecoTrack.root</input>
+    <input>SVDValidationTTreeSpacePoint.root</input>
     <output>SVDTrackingPerformance.root</output>
     <description>
     Validation plots related to tracking performance.
@@ -16,12 +17,45 @@
 
 from plotUtils import *
 
-input = R.TFile.Open("../SVDValidationTTreeRecoTrack.root")
+inputRT = R.TFile.Open("../SVDValidationTTreeRecoTrack.root")
+inputSP = R.TFile.Open("../SVDValidationTTreeSpacePoint.root")
 
-tree = input.Get("tree")
+treeRT = inputRT.Get("tree")
+treeSP = inputSP.Get("tree")
 
 histsTP = R.TFile.Open("SVDTrackingPerformance.root", "recreate")
 
+ploter(
+    name='SpacePointTime_U',
+    title='SpacePoint time in U side',
+    nbins=200,
+    xmin=-100,
+    xmax=100,
+    x_label='SP time (ns)',
+    y_label='counts',
+    granules=gD2,
+    tree=treeSP,
+    expr='time_u',
+    cut='',
+    descr='Time of the U cluster which belong to SP.',
+    check='Distributions between -20 and 20 ns.',
+    isShifter=True)
+
+ploter(
+    name='SpacePointTime_V',
+    title='SpacePoint time in V side',
+    nbins=200,
+    xmin=-100,
+    xmax=100,
+    x_label='SP time (ns)',
+    y_label='counts',
+    granules=gD2,
+    tree=treeSP,
+    expr='time_v',
+    cut='',
+    descr='Time of the V cluster which belong to SP.',
+    check='Distributions between -20 and 20 ns.',
+    isShifter=True)
 
 ploter(
     name='cluster_UVTimeDiff',
@@ -31,14 +65,13 @@ ploter(
     xmax=20,
     x_label='Cluster time difference (ns)',
     y_label='counts',
-    granules=granulesD,
-    tree=tree,
+    granules=gD,
+    tree=treeRT,
     expr='cluster_UVTimeDiff',
     cut=cut_oneTH,
-    descr='time difference between opposite sides of clusters belonging to the same layer',
-    check='peak around 0',
+    descr='Time difference between opposite sides of clusters belonging to the same layer.',
+    check='Distributions peak around 0.',
     isShifter=True)
-
 
 ploter(
     name='cluster_UUTimeDiff',
@@ -49,11 +82,11 @@ ploter(
     x_label='Cluster time difference (ns)',
     y_label='counts',
     granules=granulesTD,
-    tree=tree,
+    tree=treeRT,
     expr='cluster_UUTimeDiff',
     cut=cut_U+cut_oneTH,
-    descr='time difference between clusters belonging to the neighbour layers',
-    check='peak around 0',
+    descr='Time difference between clusters belonging to the neighbour layers.',
+    check='Distributions peak around 0.',
     isShifter=True)
 
 ploter(
@@ -65,13 +98,12 @@ ploter(
     x_label='Cluster time difference (ns)',
     y_label='counts',
     granules=granulesTD,
-    tree=tree,
+    tree=treeRT,
     expr='cluster_VVTimeDiff',
     cut=cut_V+cut_oneTH,
-    descr='time difference between clusters belonging to the neighbour layers',
-    check='peak around 0',
+    descr='Time difference between clusters belonging to the neighbour layers.',
+    check='Distributions peak around 0.',
     isShifter=True)
-
 
 plotRegions(
     name='ClusterizationPurity_U',
@@ -79,15 +111,14 @@ plotRegions(
     x_label='SVD regions',
     y_label='Purity',
     granules=granulesLayersTypes,
-    tree=tree,
+    tree=treeRT,
     expr='strip_dir',
     cutALL=cut_noV,
     cut=cut_oneTH,
-    descr='(number of clusters related to one TrueHit) / (number of clusters).\
+    descr='Definition: (number of clusters related to one TrueHit) / (number of clusters).\
     Evaluates the fraction of signal cluster over the total number of clusters.',
     check='Should be close to 1 in all bins',
     isShifter=True)
-
 
 plotRegions(
     name='ClusterizationPurity_V',
@@ -95,13 +126,13 @@ plotRegions(
     x_label='SVD regions',
     y_label='Purity',
     granules=granulesLayersTypes,
-    tree=tree,
+    tree=treeRT,
     expr='strip_dir',
     cutALL=cut_noU,
     cut=cut_oneTH,
-    descr='(number of clusters related to one TrueHit) / (number of clusters).\
+    descr='Definition: (number of clusters related to one TrueHit) / (number of clusters).\
     Evaluates the fraction of signal cluster over the total number of clusters.',
-    check='Should be close to 1 in all bins',
+    check='Should be close to 1 in all bins.',
     isShifter=True)
 
 ploter(
@@ -113,42 +144,10 @@ ploter(
     x_label='Number of clusters in one track',
     y_label='counts',
     granules=granulesL3456,
-    tree=tree,
+    tree=treeRT,
     expr='clusters_number',
     cut=cut_U,
     descr='Number of clusters in one track.',
     check='Maximum is expected for 8, i.e. each cluster for one of 4 layers,\
     separately for U and V side',
     isShifter=True)
-
-ploter(
-    name='cluster_truehits_number_U',
-    title='cluster_truehits_number for U side',
-    nbins=12,
-    xmin=0,
-    xmax=13,
-    x_label='Number of truehit for one clusters',
-    y_label='counts',
-    granules=granulesLayersTypes,
-    tree=tree,
-    expr='cluster_truehits_number',
-    cut=cut_noV,
-    descr='Multiplicity of TrueHits related to one cluster.',
-    check='Signal cluster is associated to one TrueHit.',
-    isShifter=False)
-
-ploter(
-    name='cluster_truehits_number_V',
-    title='cluster_truehits_number for V side',
-    nbins=12,
-    xmin=0,
-    xmax=13,
-    x_label='Number of truehit for one clusters',
-    y_label='counts',
-    granules=granulesLayersTypes,
-    tree=tree,
-    expr='cluster_truehits_number',
-    cut=cut_noU,
-    descr='Multiplicity of TrueHits related to one cluster.',
-    check='Signal cluster is associated to one TrueHit.',
-    isShifter=False)

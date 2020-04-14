438f552bd25248985b19501582cf862662d86bdc
Phil Grace <philip.grace@adelaide.edu.au>
2020-04-14 14:25:56 +0930

    Implemented validation_histograms for btocharm skims

--- skim/scripts/skim/btocharm.py
+++ skim/scripts/skim/btocharm.py
@@ -936,6 +936,21 @@ class BtoD0h_Kspi0(BaseSkim):
 
         self.SkimLists = BsigList
 
+    def validation_histograms(self, path):
+        ma.reconstructDecay('D0 -> K_S0:merged pi0:veryLooseFit', '1.84 < M < 1.89', path=path)
+        ma.reconstructDecay('B-:ch3 ->D0 K-:all', '5.24 < Mbc < 5.3 and abs(deltaE) < 0.15', path=path)
+
+        # the variables that are printed out are: Mbc, deltaE and the daughter particle invariant masses.
+        ma.variablesToHistogram(
+            filename='BtoDh_Kspi0_Validation.root',
+            decayString='B-:ch3',
+            variables=[
+                ('Mbc', 100, 5.2, 5.3),
+                ('deltaE', 100, -1, 1),
+                ('daughter(0, InvM)', 100, 1.8, 1.9)],  # D0 invariant mass
+            variables_2d=[
+                ('Mbc', 50, 5.23, 5.31, 'deltaE', 50, -0.7, 0.7)], path=path)
+
 
 @fancy_skim_header
 class BtoD0h_Kspipipi0(BaseSkim):
@@ -975,6 +990,9 @@ class BtoD0h_Kspipipi0(BaseSkim):
             "stdK": ["all"],
             "stdPi": ["all"],
         },
+        "stdPi0s": {
+            "stdPi0": ["looseFit"]
+        }
     }
 
     def build_lists(self, path):
@@ -989,6 +1007,21 @@ class BtoD0h_Kspipipi0(BaseSkim):
             BsigList.append("B+:BtoD0h_Kspipipi0" + str(chID))
 
         self.SkimLists = BsigList
+
+    def validation_histograms(self, path):
+        ma.reconstructDecay('D0 -> K_S0:merged pi-:all pi+:all pi0:looseFit', '1.84 < M < 1.89', path=path)
+        ma.reconstructDecay('B-:ch3 ->D0 K-:all', '5.24 < Mbc < 5.3 and abs(deltaE) < 0.15', path=path)
+
+        # the variables that are printed out are: Mbc, deltaE and the daughter particle invariant masses.
+        ma.variablesToHistogram(
+            filename='BtoDh_Kspipipi0_Validation.root',
+            decayString='B-:ch3',
+            variables=[
+                ('Mbc', 100, 5.2, 5.3),
+                ('deltaE', 100, -1, 1),
+                ('daughter(0, InvM)', 100, 1.8, 1.9)],  # D0 invariant mass
+            variables_2d=[
+                ('Mbc', 50, 5.23, 5.31, 'deltaE', 50, -0.7, 0.7)], path=path)
 
 
 @fancy_skim_header
@@ -1470,6 +1503,22 @@ class BtoD0h_hh(BaseSkim):
 
         self.SkimLists = BsigList
 
+    def validation_histograms(self, path):
+        ma.reconstructDecay('D0 -> K-:all pi+:all', '1.84 < M < 1.89', path=path)
+        ma.reconstructDecay('B-:ch3 ->D0 K-:all', '5.24 < Mbc < 5.3 and abs(deltaE) < 0.15', path=path)
+
+        # the variables that are printed out are: Mbc, deltaE and the daughter particle invariant masses.
+
+        ma.variablesToHistogram(
+            filename='BtoDh_hh_Validation.root',
+            decayString='B-:ch3',
+            variables=[
+                ('Mbc', 100, 5.2, 5.3),
+                ('deltaE', 100, -1, 1),
+                ('daughter(0, InvM)', 100, 1.8, 1.9)],  # D0 invariant mass
+            variables_2d=[
+                ('Mbc', 50, 5.23, 5.31, 'deltaE', 50, -0.7, 0.7)], path=path)
+
 
 @fancy_skim_header
 class BtoD0h_Kpi(BaseSkim):
@@ -1663,6 +1712,21 @@ class BtoD0h_Kshh(BaseSkim):
             BsigList.append("B+:BtoD0h_Kshh" + str(chID))
 
         self.SkimLists = BsigList
+
+    def validation_histograms(self, path):
+        ma.reconstructDecay('D0 -> K_S0:merged pi+:all pi-:all', '1.84 < M < 1.89', path=path)
+        ma.reconstructDecay('B-:ch3 ->D0 K-:all', '5.24 < Mbc < 5.3 and abs(deltaE) < 0.15', path=path)
+
+        # the variables that are printed out are: Mbc, deltaE and the daughter particle invariant masses.
+        ma.variablesToHistogram(
+            filename='BtoDh_Kshh_Validation.root',
+            decayString='B-:ch3',
+            variables=[
+                ('Mbc', 100, 5.2, 5.3),
+                ('deltaE', 100, -1, 1),
+                ('daughter(0, InvM)', 100, 1.8, 1.9)],  # D0 invariant mass
+            variables_2d=[
+                ('Mbc', 50, 5.23, 5.31, 'deltaE', 50, -0.7, 0.7)], path=path)
 
 
 @fancy_skim_header

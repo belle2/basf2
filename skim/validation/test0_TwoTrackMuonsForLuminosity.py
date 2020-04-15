cbef8c463dde565e4603e71539b7f2a435ac8fc9
Xing-Yu Zhou <xing-yu.zhou@desy.de>
2020-04-15 11:30:28 +0900

    Update skim/validation/test0_TwoTrackMuonsForLuminosity.py with the generator updated from BabaYaga@NLO to KKMC

--- skim/validation/test0_TwoTrackMuonsForLuminosity.py
+++ skim/validation/test0_TwoTrackMuonsForLuminosity.py
@@ -10,6 +10,7 @@
 """
 
 import basf2
+from ROOT import Belle2
 from simulation import add_simulation
 from reconstruction import add_reconstruction, add_mdst_output
 
@@ -20,39 +21,12 @@ main = basf2.create_path()
 
 main.add_module("EventInfoSetter", expList=0, runList=1, evtNumList=10000)
 
-# Register the BABAYAGA.NLO module
-babayaganlo = basf2.register_module('BabayagaNLOInput')
-
-# Set the logging level for the BABAYAGA.NLO module to INFO
-babayaganlo.set_log_level(basf2.LogLevel.INFO)
-
-babayaganlo.param('FinalState', 'mm')  # ee, gg, mm
-babayaganlo.param('DebugEnergySpread', 5.e-3)
-babayaganlo.param('ScatteringAngleRange', [35.0, 145.0])
-babayaganlo.param('MinEnergy', 0.01)
-babayaganlo.param('MaxAcollinearity', 180.)
-
-babayaganlo.param('Mode', 'unweighted')  # weighted, unweighted
-babayaganlo.param('Order', 'exp')  # born, alpha, exp
-babayaganlo.param('Model', 'matched')  # matched
-babayaganlo.param('VacuumPolarization', 'hlmnt')
-babayaganlo.param('VPUncertainty', True)
-babayaganlo.param('NPhotons', -1)
-babayaganlo.param('Epsilon', 1.e-7)
-babayaganlo.param('SearchMax', 1000000)
-# babayaganlo.param('FMax', 1.e+6) # for unweighted generation
-
-babayaganlo.param('UserMode', 'NONE')
-# babayaganlo.param('EEMIN', 1.);
-# babayaganlo.param('TEMIN', 17.5);
-# babayaganlo.param('EEVETO', 0.0);
-# babayaganlo.param('TEVETO', 12.5);
-# babayaganlo.param('EGMIN', 1.0);
-# babayaganlo.param('TGMIN', 1.0);
-# babayaganlo.param('EGVETO', 0.0);
-# babayaganlo.param('TGVETO', 5.0);
-
-main.add_module(babayaganlo)
+# generate events
+main.add_module('KKGenInput',
+                tauinputFile=Belle2.FileSystem.findFile('data/generators/kkmc/mu.input.dat'),
+                KKdefaultFile=Belle2.FileSystem.findFile('data/generators/kkmc/KK2f_defaults.dat'),
+                taudecaytableFile='',
+                kkmcoutputfilename='kkmc_mumu.dat')
 
 # detector simulation
 add_simulation(main)

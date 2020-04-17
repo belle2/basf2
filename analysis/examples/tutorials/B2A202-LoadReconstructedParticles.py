c49c881a199c59e01cad1f990fff7c5220af9a06
Savino Longo <savino.longo@desy.de>
2020-04-17 12:19:52 +0200

    changed pi0:looseFit to pi0:eff40_Jan2020Fit in examples

--- analysis/examples/tutorials/B2A202-LoadReconstructedParticles.py
+++ analysis/examples/tutorials/B2A202-LoadReconstructedParticles.py
@@ -67,7 +67,7 @@ ma.fillParticleList(decayString='p+:good', cut='protonID > 0.1', path=my_path)
 # (-> for more details about V0s have a look at B2A203-LoadV0s.py)
 # or for example stdPi0s() from stdPi0s.py:
 stdKshorts(prioritiseV0=True, path=my_path)
-stdPi0s(listtype='looseFit', path=my_path)
+stdPi0s(listtype='eff40_Jan2020Fit', path=my_path)
 stdKlongs(listtype='allklm', path=my_path)  # only 'allklm' us recommended at the moment
 
 # print contents of the DataStore after loading Particles
@@ -87,7 +87,7 @@ ma.printList('K-:good', False, path=my_path)
 ma.printList('anti-p-:all', False, path=my_path)
 ma.printList('anti-p-:good', False, path=my_path)
 ma.printList('K_S0:merged', False, path=my_path)
-ma.printList('pi0:looseFit', False, path=my_path)
+ma.printList('pi0:eff40_Jan2020Fit', False, path=my_path)
 ma.printList('K_L0:allklm', False, path=my_path)
 
 
@@ -159,7 +159,7 @@ ma.variablesToNtuple(decayString='K_L0:allklm',
 # Note here, that since we want to get info about gammas from pi0,
 # we convert names of te variables from the gamma list in the way that they will
 # correspond to given gammas.
-ma.variablesToNtuple(decayString='pi0:looseFit',
+ma.variablesToNtuple(decayString='pi0:eff40_Jan2020Fit',
                      variables=pi0_variables + vu.create_aliases_for_selected(gamma_variables, 'pi0 -> ^gamma ^gamma'),
                      filename=output_file,
                      treename='pi0',

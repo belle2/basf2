f8b1b55bae8d8410a26e9a96205a5ffd6514bee3
Xing-Yu Zhou <xing-yu.zhou@desy.de>
2020-04-21 17:50:08 +0900

    Update TwoTrackLeptonsForLuminosityList to include event candidates reconstructed from one track plus one standalone cluster

--- skim/validation/test2_TwoTrackMuonsForLuminosity.py
+++ skim/validation/test2_TwoTrackMuonsForLuminosity.py
@@ -25,22 +25,43 @@ variables.addAlias('mup_p_cms', 'daughter(0, useCMSFrame(p))')
 variables.addAlias('mum_p_cms', 'daughter(1, useCMSFrame(p))')
 variables.addAlias('mup_theta_lab', 'formula(daughter(0, theta)*180/3.1415927)')
 variables.addAlias('mum_theta_lab', 'formula(daughter(1, theta)*180/3.1415927)')
-variables.addAlias('delta_theta_cms', 'abs(formula('
+variables.addAlias('mup_mum_delta_theta_cms', 'abs(formula('
                    + 'daughter(0, useCMSFrame(theta))*180/3.1415927 +'
                    + 'daughter(1, useCMSFrame(theta))*180/3.1415927 - 180))')
 
-variablesHist = [
-                  ('mup_p_cms', 100, 4, 6),
-                  ('mum_p_cms', 100, 4, 6),
-                  ('mup_theta_lab', 90, 0, 180),
-                  ('mum_theta_lab', 90, 0, 180),
-                  ('delta_theta_cms', 100, 0, 2)
-                ]
+variables.addAlias('mu_p_cms', 'daughter(0, useCMSFrame(p))')
+variables.addAlias('gamma_p_cms', 'daughter(1, useCMSFrame(p))')
+variables.addAlias('mu_theta_lab', 'formula(daughter(0, theta)*180/3.1415927)')
+variables.addAlias('gamma_theta_lab', 'formula(daughter(1, theta)*180/3.1415927)')
+variables.addAlias('mu_gamma_delta_theta_cms', 'abs(formula('
+                   + 'daughter(0, useCMSFrame(theta))*180/3.1415927 +'
+                   + 'daughter(1, useCMSFrame(theta))*180/3.1415927 - 180))')
+
+variablesHist2 = [
+                   ('mup_p_cms', 100, 4, 6),
+                   ('mum_p_cms', 100, 4, 6),
+                   ('mup_theta_lab', 90, 0, 180),
+                   ('mum_theta_lab', 90, 0, 180),
+                   ('mup_mum_delta_theta_cms', 100, 0, 2)
+                 ]
+
+variablesHist1 = [
+                   ('mu_p_cms', 100, 4, 6),
+                   ('gamma_p_cms', 100, 1, 6),
+                   ('mu_theta_lab', 90, 0, 180),
+                   ('gamma_theta_lab', 90, 0, 180),
+                   ('mu_gamma_delta_theta_cms', 100, 0, 2)
+                 ]
 
 # Output the variables to a ntuple
 variablesToHistogram(
-    'vpho:TwoTrackLeptonsForLuminosity',
-    variablesHist,
+    'vpho:TwoTrackLeptonsForLuminosity2',
+    variablesHist2,
+    filename='../TwoTrackMuonsForLuminosity_Validation.root',
+    path=TwoTrackLeptonsForLuminosity_path)
+variablesToHistogram(
+    'vpho:TwoTrackLeptonsForLuminosity1',
+    variablesHist1,
     filename='../TwoTrackMuonsForLuminosity_Validation.root',
     path=TwoTrackLeptonsForLuminosity_path)
 

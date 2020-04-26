864536794e6ab887beda58756da65d26cd1c9a24
Jitendra Kumar <jitendra.kumar@desy.de>
2020-04-27 02:10:24 +0900

    updating script to test all exp file

--- reconstruction/tests/reco_raw.py
+++ reconstruction/tests/reco_raw.py
@@ -16,6 +16,6 @@ steering = Belle2.FileSystem.findFile('reconstruction/tests/reco_raw.py_noexec')
 for data_file in glob.glob(os.environ['BELLE2_VALIDATION_DATA_DIR'] + '/rawdata/*HLT?.*.root'):
     data_type = os.path.basename(data_file).split('.')[0]
     exp_number = os.path.basename(data_file).split('.')[1]
-    if(exp_number == '0008' or exp_number == '0010' or exp_number == '0005'):
-        print("--> Testing reconstruction on: ", data_type, exp_number)
-        assert(0 == subprocess.check_call(['basf2', '-n', '10', '-i', data_file, steering, data_type]))
+    run_number = os.path.basename(data_file).split('.')[2]
+    print("--> Testing reconstruction on: ", data_type, exp_number, run_number)
+    assert(0 == subprocess.check_call(['basf2', '-n', '10', '-i', data_file, steering, data_type]))

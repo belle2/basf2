56ec924a2cea6ec5673bac9698cb481f14a99a77
Christian Wessel <christian.wessel@desy.de>
2020-04-28 21:34:55 +0200

    Changed variable names of the LoosXXDPairFilter variables.

--- tracking/ckf/pxd/filters/relations/include/LoosePXDPairFilter.h
+++ tracking/ckf/pxd/filters/relations/include/LoosePXDPairFilter.h
@@ -23,12 +23,12 @@ namespace Belle2 {
 
   private:
     /// Pre-filter relations in theta for overlay
-    double m_param_theta_overlay_region_precut = 0.4;
+    double m_param_ThetaOverlayRegionPrecut = 0.4;
     /// Pre-filter relations in phi between seed states and hit states
-    double m_param_phi_seed_hit_precut = M_PI / 3.;
+    double m_param_PhiSeedHitPrecut = M_PI / 3.;
     /// Pre-filter relations in phi between hit states
-    double m_param_phi_hit_hit_precut = M_PI / 3.;
+    double m_param_PhiHitHitPrecut = M_PI / 3.;
     /// Pre-filter relations in theta between hit states
-    double m_param_theta_hit_hit_precut = M_PI / 3.;
+    double m_param_ThetaHitHitPrecut = M_PI / 3.;
   };
 }

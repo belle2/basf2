302650eaf035178d2ff9edc300588b4b93a82040
Gian Luca Pinna Angioni <gian.luca.pinna.angioni@desy.de>
2020-04-29 00:14:42 +0900

    change author

--- framework/dbobjects/include/Clocks.h
+++ framework/dbobjects/include/Clocks.h
@@ -1,9 +1,9 @@
 /**************************************************************************
  * BASF2 (Belle Analysis Framework 2)                                     *
- * Copyright(C) 2016 - Belle II Collaboration                             *
+ * Copyright(C) 2020 - Belle II Collaboration                             *
  *                                                                        *
  * Author: The Belle II Collaboration                                     *
- * Contributors: Nils Braun                                               *
+ * Contributors: Gian Luca Pinna Angioni                                  *
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/

6b67f3f423905ce1c09ce47d02e1abee42ffe94f
CORONA Luigi <luigi.corona@desy.de>
2020-05-01 03:28:40 +0900

    Documentation in svdCoGCalibrationAnalysisToolModule.py

--- svd/scripts/svd/svdCoGCalibrationAnalysisToolModule.py
+++ svd/scripts/svd/svdCoGCalibrationAnalysisToolModule.py
@@ -32,68 +32,129 @@ gROOT.SetBatch(True)
 
 
 class svdCoGCalibrationAnalysisTool(basf2.Module):
+    """
+    Class used to check the goodness of the CoG time calibration
+    """
 
     def initialize(self):
+        """
+        Initialize object (histograms, lists, ...) used by the class
+        """
+        #: name of the output file provided by the user
         self.outputFileName = outputFile
+        #: layer array for the tree
         self.layer = np.zeros(1, dtype=int)
+        #: ladder array for the tree
         self.ladder = np.zeros(1, dtype=int)
+        #: sensor array for the tree
         self.sensor = np.zeros(1, dtype=int)
+        #: side array for the tree
         self.side = np.zeros(1, dtype=int)
+        #: TB array for the tree
         self.TB = np.zeros(1, dtype=int)
+        #: T0 array for the tree
         self.evtTime = np.zeros(1, dtype=float)
+        #: T0Sync array for the tree
         self.evtTimeSync = np.zeros(1, dtype=float)
+        #: cluster time array for the tree
         self.clsTime = np.zeros(1, dtype=float)
+        #: cluster size array for the tree
         self.clsSize = np.zeros(1, dtype=float)
+        #: cluster charge array for the tree
         self.clsCharge = np.zeros(1, dtype=float)
+        #: cluster SNR array for the tree
         self.clsSNR = np.zeros(1, dtype=float)
+        #: run number array for the tree
         self.run = np.zeros(1, dtype=int)
+        #: experiment number array for the tree
         self.exp = np.zeros(1, dtype=int)
 
+        #: layer array for the tree for clusters associated to tracks
         self.layerTracks = np.zeros(1, dtype=int)
+        #: ladder array for the tree for clusters associated to tracks
         self.ladderTracks = np.zeros(1, dtype=int)
+        #: sensor array for the tree for clusters associated to tracks
         self.sensorTracks = np.zeros(1, dtype=int)
+        #: side array for the tree for clusters associated to tracks
         self.sideTracks = np.zeros(1, dtype=int)
+        #: T0 array for the tree for clusters associated to tracks
         self.evtTimeTracks = np.zeros(1, dtype=float)
+        #: T0Sync array for the tree for clusters associated to tracks
         self.evtTimeSyncTracks = np.zeros(1, dtype=float)
+        #: cluster time array for the tree for clusters associated to tracks
         self.clsTimeTracks = np.zeros(1, dtype=float)
+        #: cluster size array for the tree for clusters associated to tracks
         self.clsSizeTracks = np.zeros(1, dtype=float)
+        #: cluster charge array for the tree for clusters associated to tracks
         self.clsChargeTracks = np.zeros(1, dtype=float)
+        #: cluster SNR array for the tree for clusters associated to tracks
         self.clsSNRTracks = np.zeros(1, dtype=float)
 
         # self.tFile = TFile(self.outputFileName, 'recreate')
         # self.tFile.cd()
 
+        #: tree for clusters
         self.tree = TTree('cls', 'tree')
+        #: tree for clusters, branch for layer number
         self.tree.Branch('layer', self.layer, 'layer/I')
+        #: tree for clusters, branch for ladder number
         self.tree.Branch('ladder', self.ladder, 'ladder/I')
+        #: tree for clusters, branch for sensor number
         self.tree.Branch('sensor', self.sensor, 'sensor/I')
+        #: tree for clusters, branch for side
         self.tree.Branch('side', self.side, 'side/I')
+        #: tree for clusters, branch for trigger bin
         self.tree.Branch('tb', self.TB, 'tb/I')
+        #: tree for clusters, branch for T0
         self.tree.Branch('evtT0', self.evtTime, 'evtT0/D')
+        #: tree for clusters, branch for T0Sync
         self.tree.Branch('evtT0Sync', self.evtTimeSync, 'evtT0Sync/D')
+        #: tree for clusters, branch for cluster time
         self.tree.Branch('clsTime', self.clsTime, 'clsTime/D')
+        #: tree for clusters, branch for cluster size
         self.tree.Branch('clsSize', self.clsSize, 'clsSize/D')
+        #: tree for clusters, branch for cluster charge
         self.tree.Branch('clsCharge', self.clsCharge, 'clsCharge/D')
+        #: tree for clusters, branch for cluster SNR
         self.tree.Branch('clsSNR', self.clsSNR, 'clsSNR/D')
+        #: tree for clusters, branch for run number
         self.tree.Branch('run', self.run, 'run/I')
+        #: tree for clusters, branch for exp number
         self.tree.Branch('exp', self.exp, 'exp/I')
 
+        #: tree for clusters associated to tracks
         self.treeTracks = TTree('clsTracks', 'treeTracks')
+        #: tree for clusters associated to tracks, branch for layer number
         self.treeTracks.Branch('layer', self.layerTracks, 'layer/I')
+        #: tree for clusters associated to tracks, branch for ladder number
         self.treeTracks.Branch('ladder', self.ladderTracks, 'ladder/I')
+        #: tree for clusters associated to tracks, branch for sensor number
         self.treeTracks.Branch('sensor', self.sensorTracks, 'sensor/I')
+        #: tree for clusters associated to tracks, branch for side
         self.treeTracks.Branch('side', self.sideTracks, 'side/I')
+        #: tree for clusters associated to tracks, branch for trigger bin
         self.treeTracks.Branch('tb', self.TB, 'tb/I')
+        #: tree for clusters associated to tracks, branch for T0
         self.treeTracks.Branch('evtT0', self.evtTimeTracks, 'evtT0/D')
+        #: tree for clusters associated to tracks, branch for T0Sync
         self.treeTracks.Branch('evtT0Sync', self.evtTimeSyncTracks, 'evtT0Sync/D')
+        #: tree for clusters associated to tracks, branch for cluster time
         self.treeTracks.Branch('clsTime', self.clsTimeTracks, 'clsTime/D')
+        #: tree for clusters associated to tracks, branch for cluster size
         self.treeTracks.Branch('clsSize', self.clsSizeTracks, 'clsSize/D')
+        #: tree for clusters associated to tracks, branch for cluster charge
         self.treeTracks.Branch('clsCharge', self.clsChargeTracks, 'clsCharge/D')
+        #: tree for clusters associated to tracks, branch for cluster SNR
         self.treeTracks.Branch('clsSNR', self.clsSNRTracks, 'clsSNR/D')
+        #: tree for clusters associated to tracks, branch for run number
         self.treeTracks.Branch('run', self.run, 'run/I')
+        #: tree for clusters associated to tracks, branch for exp number
         self.treeTracks.Branch('exp', self.exp, 'exp/I')
 
     def event(self):
+        """
+        Function that allows to cicle on the events
+        """
         svd_cls = Belle2.PyStoreArray(svd_Clusters)
         svd_clsFromTrks = Belle2.PyStoreArray(svd_ClustersFromTracks)
         et0 = Belle2.PyStoreObj(time0)
@@ -113,6 +174,7 @@ class svdCoGCalibrationAnalysisTool(basf2.Module):
             self.ladder[0] = svdCluster.getSensorID().getLadderNumber()
             self.sensor[0] = svdCluster.getSensorID().getSensorNumber()
             self.side[0] = svdCluster.isUCluster()
+            #: correction to pass to the FTSW reference system
             self.ftswShift = svd_evtInfo.getSVD2FTSWTimeShift(svdCluster.getFirstFrame())
             hasT0 = et0.hasEventT0()
             if hasT0:
@@ -132,6 +194,7 @@ class svdCoGCalibrationAnalysisTool(basf2.Module):
             self.ladderTracks[0] = svdClusterTracks.getSensorID().getLadderNumber()
             self.sensorTracks[0] = svdClusterTracks.getSensorID().getSensorNumber()
             self.sideTracks[0] = svdClusterTracks.isUCluster()
+            #: correction to pass to the FTSW reference system
             self.ftswShiftTracks = svd_evtInfo.getSVD2FTSWTimeShift(svdCluster.getFirstFrame())
             hasT0 = et0.hasEventT0()
             if hasT0:
@@ -143,6 +206,10 @@ class svdCoGCalibrationAnalysisTool(basf2.Module):
             self.treeTracks.Fill()
 
     def terminate(self):
+        """
+        Terminates te class and produces the output rootfile
+        """
+        #: creation of TFile
         self.tFile = TFile(self.outputFileName, 'recreate')
         self.tFile.cd()
         self.tree.Write()

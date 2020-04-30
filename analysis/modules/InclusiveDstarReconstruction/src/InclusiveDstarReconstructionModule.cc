7025fd3156b7e4b7842d357d383c0b3879d09581
Pascal Schmolz <p.schmolz@physik.uni-muenchen.de>
2020-04-30 14:57:10 +0200

    InclusiveDstarReconstruction module now creates antiparticles and mc-matching is applicable

--- analysis/modules/InclusiveDstarReconstruction/src/InclusiveDstarReconstructionModule.cc
+++ analysis/modules/InclusiveDstarReconstruction/src/InclusiveDstarReconstructionModule.cc
@@ -1,13 +1,12 @@
 /**************************************************************************
- * BASF2 (Belle Analysis Framework 2)                                     *
- * Copyright(C) 2020 - Belle II Collaboration                             *
- *                                                                        *
- * Author: The Belle II Collaboration                                     *
- * Contributors: Maximilian Welsch                                        *
- *                                                                        *
- * This software is provided "as is" without any warranty.                *
- **************************************************************************/
-
+  * BASF2 (Belle Analysis Framework 2)                                     *
+  * Copyright(C) 2020 - Belle II Collaboration                             *
+  *                                                                        *
+  * Author: The Belle II Collaboration                                     *
+  * Contributors: Maximilian Welsch, Pascal Schmolz                        *
+  *                                                                        *
+  * This software is provided "as is" without any warranty.                *
+  **************************************************************************/
 
 #include <analysis/modules/InclusiveDstarReconstruction/InclusiveDstarReconstructionModule.h>
 #include <analysis/utility/PCmsLabTransform.h>
@@ -17,6 +16,8 @@
 
 #include <analysis/dataobjects/ParticleList.h>
 #include <analysis/DecayDescriptor/ParticleListName.h>
+
+#include <mdst/dataobjects/MCParticle.h>
 
 #include <TVector3.h>
 #include <TDatabasePDG.h>
@@ -37,105 +38,131 @@ InclusiveDstarReconstructionModule::InclusiveDstarReconstructionModule() : Modul
   setDescription("Inclusive Dstar reconstruction by estimating the four vector using slow pions");
 
   // Parameter definitions
-  addParam("pionListName", m_inputPionListName, "Name of the input pion ParticleList", std::string(""));
-  addParam("DstarListName", m_outputDstarListName, "Name of the output D* ParticleList", std::string(""));
+  addParam("decayString", m_decayString, "Input DecayDescriptor string", std::string(""));
   addParam("slowPionCut", m_slowPionCut, "Cut for selecting slow pions", std::string("useCMSFrame(p) < 0.2"));
 
   m_dstar_pdg_code = 0;
   m_dstar_pdg_mass = 0;
   m_d_pdg_mass = 0;
-
 }
 
 InclusiveDstarReconstructionModule::~InclusiveDstarReconstructionModule() = default;
 
 void InclusiveDstarReconstructionModule::initialize()
 {
+  m_outputListName = "";
+  m_pionListName = "";
 
-  // check the validity of output ParticleList name
-  bool valid = m_decaydescriptor.init(m_outputDstarListName);
+  bool valid = m_decaydescriptor.init(m_decayString);
   if (!valid)
-    B2ERROR("InclusiveDstarReconstructionModule::initialize Invalid Output ParticleList Name: " << m_outputDstarListName);
+    B2ERROR("Invalid input DecayString: " << m_decayString);
 
-  // get output particle pdg code
+  // mother particle: D*
   const DecayDescriptorParticle* mother = m_decaydescriptor.getMother();
-  m_dstar_pdg_code  = mother->getPDGCode();
+  m_dstar_pdg_code = mother->getPDGCode();
 
-  valid = m_decaydescriptor.init(m_inputPionListName);
-  if (!valid)
-    B2ERROR("InclusiveDstarReconstructionModule::initialize Invalid Input ParticleList Name: " << m_outputDstarListName);
+  // daughter particle: pion (is the only daughter)
+  int pion_pdg_code = 0;
+  const DecayDescriptorParticle* daughter = m_decaydescriptor.getDaughter(0)->getMother();
+  m_pionListName = daughter->getFullName();
+  StoreObjPtr<ParticleList>().isRequired(m_pionListName);
+  pion_pdg_code = daughter->getPDGCode();
 
-  // get input particle pdg code
-  mother = m_decaydescriptor.getMother();
-  int pion_pdg_code  = mother->getPDGCode();
-
-  // check compatibility of particle lists like (D*+ needs either pi+ or pi0, D*0 only compatible with pi0)
   if (!pionCompatibleWithDstar(pion_pdg_code))
-    B2ERROR("Pion PDG code " << pion_pdg_code << " not compatible with D* PDG code " << m_dstar_pdg_code);
+    B2ERROR("Pion PDG code " << pion_pdg_code << " not compatible with D* PDG code " <<  m_dstar_pdg_code);
 
-  // ensure that input pion list has been registered in data store previously
-  StoreObjPtr<ParticleList> inputPionList(m_inputPionListName);
-  inputPionList.isRequired();
+  // decay property: ignore missing massive particle
+  m_properties = m_decaydescriptor.getProperty();
 
   // create and register output particle list
-  StoreObjPtr<ParticleList> outputDstarList(m_outputDstarListName);
+  m_outputListName = mother->getFullName();
+  StoreObjPtr<ParticleList> outputDstarList(m_outputListName);
   outputDstarList.registerInDataStore();
-  StoreObjPtr<ParticleList> outputAntiDstarList(ParticleListName::antiParticleListName(m_outputDstarListName));
+  // create and register output antiparticle list
+  m_outputAntiListName = ParticleListName::antiParticleListName(m_outputListName);
+  StoreObjPtr<ParticleList> outputAntiDstarList(m_outputAntiListName);
   outputAntiDstarList.registerInDataStore();
 
   m_cut = Variable::Cut::compile(m_slowPionCut);
 
   m_dstar_pdg_mass = TDatabasePDG::Instance()->GetParticle(m_dstar_pdg_code)->Mass();
 
+  // get the mass of daughter-D:
+  /*
+  decay 1. D*+ {413} -> pi+ {211} D0 {421}
+  decay 2. D*+ {413} -> pi0 {111} D+ {411}
+  decay 3. D*0 {423} -> pi0 {111} D0 {421}
+  */
+  int d_pdg_code = 0;
   if (abs(m_dstar_pdg_code) == 413) {
-    m_d_pdg_mass = (pion_pdg_code == 111) ? TDatabasePDG::Instance()->GetParticle(421)->Mass() : TDatabasePDG::Instance()->GetParticle(
-                     411)->Mass();
+    d_pdg_code = (pion_pdg_code == 111) ? 411 : 421;
   } else {
-    m_d_pdg_mass = TDatabasePDG::Instance()->GetParticle(421)->Mass();
+    d_pdg_code = 421;
   }
-
+  m_d_pdg_mass = TDatabasePDG::Instance()->GetParticle(d_pdg_code)->Mass();
 }
 
 void InclusiveDstarReconstructionModule::event()
 {
   StoreArray<Particle> particles;
 
-  // create output particle lists and bind anti-particle list
-  StoreObjPtr<ParticleList> outputDstarList(m_outputDstarListName);
+  StoreObjPtr<ParticleList> outputDstarList(m_outputListName);
   outputDstarList.create();
   outputDstarList->initialize(m_dstar_pdg_code, outputDstarList.getName());
 
-  StoreObjPtr<ParticleList> outputAntiDstarList(ParticleListName::antiParticleListName(m_outputDstarListName));
+  StoreObjPtr<ParticleList> outputAntiDstarList(ParticleListName::antiParticleListName(m_outputListName));
   outputAntiDstarList.create();
-  outputAntiDstarList->initialize(-m_dstar_pdg_code, ParticleListName::antiParticleListName(m_outputDstarListName));
+  outputAntiDstarList->initialize(-m_dstar_pdg_code, ParticleListName::antiParticleListName(m_outputListName));
   outputDstarList->bindAntiParticleList(*outputAntiDstarList);
 
-  // loop over all pions and cut on slow pion cut
-  StoreObjPtr<ParticleList> inputPionList(m_inputPionListName);
+  StoreObjPtr<ParticleList> inputPionList(m_pionListName);
   unsigned int num_pions = inputPionList->getListSize();
 
   for (unsigned int pion_index = 0; pion_index < num_pions; pion_index++) {
     const Particle* pion = inputPionList->getParticle(pion_index);
 
-    if (!m_cut->check(pion)) {
-      continue;
-    };
+    if (!m_cut->check(pion)) continue;
 
     TLorentzVector dstar_four_vector = estimateDstarFourMomentum(pion);
 
     if (isnan(dstar_four_vector.P())) continue;
 
-    Particle dstar = Particle(dstar_four_vector, m_dstar_pdg_code, Particle::EFlavorType::c_Flavored, {pion->getArrayIndex()},
-                              pion->getArrayPointer());
+    /*
+    decay 1:
+    - pion-charge > 0: m_dstar_pdg_code
+    - pion-charge < 0: -m_dstar_pdg_code
+    decay 2 and 3:
+    - pion-charge = 0: m_dstar_pdg_code as well as -m_dstar_pdg_code
+    - for these cases we need to store the particleList and antiParticleList
+      with the same candidates
+    */
+    // for decay 1 and decay 2/3 with positive flavor
+    Particle dstar = Particle(dstar_four_vector,
+                              (pion->getCharge() >= 0) ? m_dstar_pdg_code : -m_dstar_pdg_code,
+                              Particle::EFlavorType::c_Flavored,
+    {pion->getArrayIndex()},
+    m_properties,
+    pion->getArrayPointer());
     Particle* new_dstar = particles.appendNew(dstar);
     outputDstarList->addParticle(new_dstar);
 
+    // for decay 2/3 with negative flavor
+    if (pion->getCharge() == 0) {
+      Particle antidstar = Particle(dstar_four_vector,
+                                    -m_dstar_pdg_code,
+                                    Particle::EFlavorType::c_Flavored,
+      {pion->getArrayIndex()},
+      m_properties,
+      pion->getArrayPointer());
+      Particle* new_antidstar = particles.appendNew(antidstar);
+      outputAntiDstarList->addParticle(new_antidstar);
+    }
   }
 }
 
 TLorentzVector InclusiveDstarReconstructionModule::estimateDstarFourMomentum(const Particle* pion)
 {
-//  // estimate D* energy and absolute momentum using the slow pion energy
+  // estimate D* energy and absolute momentum using the slow pion energy
   double energy_dstar = pion->getEnergy() * m_dstar_pdg_mass / (m_dstar_pdg_mass - m_d_pdg_mass);
   double abs_momentum_dstar = sqrt(energy_dstar * energy_dstar - m_dstar_pdg_mass * m_dstar_pdg_mass);
 
@@ -156,6 +183,5 @@ bool InclusiveDstarReconstructionModule::pionCompatibleWithDstar(int pion_pdg_co
   } else if (pion_pdg_code == -211) {
     is_compatible = (m_dstar_pdg_code == -413);
   }
-
   return is_compatible;
 }

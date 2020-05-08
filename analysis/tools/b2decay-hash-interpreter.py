fb477cb8ece5377759d26c4964947f96ca5f259c
Michael Eliachevitch <meliache@uni-bonn.de>
2020-05-08 14:17:14 +0200

    Lazy import of decayHash to prevent __name__ being root in the usage string

--- analysis/tools/b2decay-hash-interpreter.py
+++ analysis/tools/b2decay-hash-interpreter.py
@@ -2,12 +2,13 @@
 
 import os
 import sys
-import decayHash
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
         print(f'Usage {os.path.basename(__file__)} hashmap.root decayHash decayHashExtended')
 
+    # lazy import of decayHash for lazy import of ROOT, otherwise it seems that __file__ is root in the main
+    import decayHash
     rootfile = sys.argv[1]
     hashmap = decayHash.DecayHashMap(rootfile)
     hashmap.print_hash(float(sys.argv[2]), float(sys.argv[3]))

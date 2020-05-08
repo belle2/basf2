fa817a6628ed9be9ef2de7210c0cb2bb17ee5c61
Sviatoslav Bilokin <sviatoslav.bilokin@desy.de>
2020-05-08 21:30:42 +0200

    intermediate commit of tests (will fail)

---
+++ analysis/tests/sk_variables.cc
@@ -0,0 +1,74 @@
+#include <gtest/gtest.h>
+#include "utilities/TestParticleFactory.h"
+
+#include <analysis/dataobjects/Particle.h>
+#include <mdst/dataobjects/Track.h>
+#include <mdst/dataobjects/ECLCluster.h>
+#include <mdst/dataobjects/KLMCluster.h>
+#include <framework/gearbox/Gearbox.h>
+#include <framework/datastore/StoreArray.h>
+
+#include <analysis/variables/SpecificKinematicVariables.h>
+
+#include <analysis/VariableManager/Manager.h>
+
+#include <TLorentzVector.h>
+
+using namespace std;
+using namespace Belle2;
+using namespace Belle2::Variable;
+namespace {
+  class SpecificKinematicVariablesTest : public ::testing::Test {
+  protected:
+    /** register Particle array + ParticleExtraInfoMap object. */
+    void SetUp() override
+    {
+
+      DataStore::Instance().setInitializeActive(true);
+      StoreArray<ECLCluster> myECLClusters;
+      StoreArray<KLMCluster> myKLMClusters;
+      StoreArray<TrackFitResult> myTFRs;
+      StoreArray<Track> myTracks;
+      StoreArray<Particle> myParticles;
+      myECLClusters.registerInDataStore();
+      myKLMClusters.registerInDataStore();
+      myTFRs.registerInDataStore();
+      myTracks.registerInDataStore();
+      myParticles.registerInDataStore();
+      DataStore::Instance().setInitializeActive(false);
+
+      TestUtilities::TestParticleFactory factory;
+      TLorentzVector b0momentum(0.065314, -0.082151, 1.706387, 5.548032);
+      TLorentzVector pimomentum(0.1, 0, 2.5, sqrt(0.139 * 0.139 + 2.5 * 2.5));
+      TLorentzVector emomentum(0., 0, 1., 1.);
+      TVector3 ipposition(0, 0, 0);
+      factory.produceParticle(string("^B0 -> pi- e+"), b0momentum, ipposition);
+      myParticles[0]->set4Vector(pimomentum); //pion
+      myParticles[1]->set4Vector(emomentum);  //electron
+    }
+
+    /** clear datastore */
+    void TearDown() override
+    {
+      DataStore::Instance().reset();
+    }
+  };
+
+  TEST_F(SpecificKinematicVariablesTest, REC_q2BhSimple)
+  {
+    StoreArray<Particle> myParticles;
+    B2INFO("HERE!");
+    myParticles[0]->print();
+    myParticles[1]->print();
+    EXPECT_FLOAT_EQ(7.9078646, REC_q2BhSimple(myParticles[2])); // B-meson
+  }
+  TEST_F(SpecificKinematicVariablesTest, REC_q2Bh)
+  {
+    StoreArray<Particle> myParticles;
+    B2INFO("HERE!");
+    myParticles[0]->print();
+    myParticles[1]->print();
+    EXPECT_FLOAT_EQ(7.9078646, REC_q2Bh(myParticles[2])); // B-meson
+  }
+
+}

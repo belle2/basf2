ad8b5df64af62b546782516dc075ffb32d38fd40
NA Ijeong <iijeong26@gmail.com>
2020-05-11 15:51:11 +0900

    Fixed missing documents in G4LongLivedNeutralDecay.h

--- simulation/longlivedneutral/include/G4LongLivedNeutralDecay.h
+++ simulation/longlivedneutral/include/G4LongLivedNeutralDecay.h
@@ -12,16 +12,18 @@ namespace Belle2 {
 
 
   class G4LongLivedNeutral;
-
+  /**
+   * This class is a decay process
+   */
   class G4LongLivedNeutralDecay : public G4VRestDiscreteProcess {
-// Class Description
-    //  This class is a decay process
 
   public:
-    //  Constructors
+    /**  Constructors
+     */
     G4LongLivedNeutralDecay(const G4String& processName = "LongLivedNeutralDecay");
 
-    //  Destructor
+    /**  Destructor
+     */
     virtual ~G4LongLivedNeutralDecay();
 
   private:
@@ -32,9 +34,14 @@ namespace Belle2 {
     G4LongLivedNeutralDecay& operator=(const G4LongLivedNeutralDecay& right);
 
   public: //With Description
-    // G4Decay Process has both
-    // PostStepDoIt (for decay in flight)
-    //   and
+    /**
+     * G4VProcess::PostStepDoIt() implemention
+     * @param aTrack
+     * @param astep
+     *
+     * for decay in flight
+     * G4Decay Process has both */
+
     // AtRestDoIt (for decay at rest)
 
     virtual G4VParticleChange* PostStepDoIt(
@@ -42,35 +49,67 @@ namespace Belle2 {
       const G4Step& aStep
     ) override;
 
+    /**
+     * G4VProess::AtRestDoIt() implemention,
+     * @param aTrack
+     * @param aStep
+     */
+
     virtual G4VParticleChange* AtRestDoIt(
       const G4Track& aTrack,
       const G4Step&  aStep
     ) override;
 
     virtual void BuildPhysicsTable(const G4ParticleDefinition&) override;
-    // In G4Decay, thePhysicsTable stores values of
-    //    beta * std::sqrt( 1 - beta*beta)
-    //  as a function of normalized kinetic enregy (=Ekin/mass),
-    //  becasuse this table is universal for all particle types,
+    /**< In G4Decay, thePhysicsTable stores values of
+         beta * std::sqrt( 1 - beta*beta)
+         as a function of normalized kinetic enregy (=Ekin/mass),
+         becasuse this table is universal for all particle types*/
 
 
     virtual G4bool IsApplicable(const G4ParticleDefinition&) override;
-    // returns "true" if the decay process can be applied to
-    // the particle type.
+    /**< returns "true" if the decay process can be applied to
+         the particle type. */
 
   protected: // With Description
+
+    /**
+     * G4Decay::DecayIt() implemention
+     * @param aTrack
+     * @param aStep
+     *
+     * The DecayIt() method returns by pointer a particle-change object,
+     * which has information of daughter particles.
+     */
     virtual G4VParticleChange* DecayIt(
       const G4Track& aTrack,
       const G4Step&  aStep
     );
-    // The DecayIt() method returns by pointer a particle-change object,
-    // which has information of daughter particles.
 
   public:
+
+    /**
+     * G4VProcess::AtRestGetPhysicalInteractionLength() implemention
+     * @param track
+     * @param condition
+     *
+     * No operation in AtRestDoIt.
+     */
+
     virtual G4double AtRestGetPhysicalInteractionLength(
       const G4Track& track,
       G4ForceCondition* condition
     ) override;
+
+    /**
+     * G4VProcess::PostStepGetPhysicalInteractionLength() implemention
+     * @param track This argument of base function is ignored
+     * @param previousStepSize This argument of base function is ignored
+     * @param condition
+     *
+     * Forces the PostStepDoIt action to be called, so that it can do the relocation if it is needed,
+     * but does not limit the step.
+     */
 
     virtual G4double PostStepGetPhysicalInteractionLength(
       const G4Track& track,
@@ -79,51 +118,57 @@ namespace Belle2 {
     ) override;
 
   protected: // With Description
-    // GetMeanFreePath returns ctau*beta*gamma for decay in flight
-    // GetMeanLifeTime returns ctau for decay at rest
+
+
     virtual G4double GetMeanFreePath(const G4Track& aTrack,
                                      G4double   previousStepSize,
                                      G4ForceCondition* condition
                                     ) override;
+    /**< GetMeanFreePath returns ctau*beta*gamma for decay in flight*/
 
     virtual G4double GetMeanLifeTime(const G4Track& aTrack,
                                      G4ForceCondition* condition
-                                    ) override;
+                                    ) override
+    /**<  GetMeanLifeTime returns ctau for decay at rest*/
 
-  public: //With Description
-    virtual void StartTracking(G4Track*) override;
-    virtual void EndTracking() override;
-    // inform Start/End of tracking for each track to the physics process
 
-  public: //With Description
 
-    G4double GetRemainderLifeTime() const;
-    //Get Remainder of life time at rest decay
+    public: //With Description
 
-  protected:
-    G4int verboseLevel;
-    // controle flag for output message
-    //  0: Silent
-    //  1: Warning message
-    //  2: More
+      virtual void StartTracking(G4Track*) override;
+      /**< inform Start of tracking for each track to the physics process*/
+      virtual void EndTracking() override;
+      /**< inform End of tracking for each track to the physics process */
 
-  protected:
-    // HighestValue.
-    const G4double HighestValue;
 
-    // Remainder of life time at rest
-    G4double fRemainderLifeTime;
+    public: //With Description
 
-    // ParticleChange for decay process
-    G4ParticleChangeForDecay fParticleChangeForDecay;
+      G4double GetRemainderLifeTime() const;
+      /**< Get Remainder of life time at rest decay*/
 
-  };
+    protected:
+      G4int verboseLevel;
+      /**< controle flag for output message
+       *   0: Silent
+       *   1: Warning message
+       *   2: More */
 
-  inline
-  G4VParticleChange* G4LongLivedNeutralDecay::AtRestDoIt(
-    const G4Track& aTrack,
-    const G4Step&  aStep
-  )
+    protected:
+      // HighestValue.
+
+      const G4double HighestValue; /**< Remainder of life time at rest */
+
+      G4double fRemainderLifeTime; /**< ParticleChange for decay process */
+
+      G4ParticleChangeForDecay fParticleChangeForDecay; /**< ParticleChange for decay process */
+
+    };
+
+    inline
+    G4VParticleChange * G4LongLivedNeutralDecay::AtRestDoIt(
+      const G4Track & aTrack,
+      const G4Step &  aStep
+    )
   {
     return DecayIt(aTrack, aStep);
   }

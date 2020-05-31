156fd73b123c492a32de97fffae220c55506de7b
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2020-05-31 20:48:26 +0900

    Wrong name...

--- dqm/analysis/modules/include/DQMHistAnalysisKLM.h
+++ dqm/analysis/modules/include/DQMHistAnalysisKLM.h
@@ -3,7 +3,7 @@
  * Copyright(C) 2018  Belle II Collaboration                              *
  *                                                                        *
  * Author: The Belle II Collaboration                                     *
- * Contributors: Kirill Chilikin, Leo Piilonen, Viping Gaur,              *
+ * Contributors: Kirill Chilikin, Leo Piilonen, Vipin Gaur,               *
  *               Giacomo De Pietro                                        *
  *                                                                        *
  * This software is provided "as is" without any warranty.                *

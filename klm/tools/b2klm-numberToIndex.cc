fac107db28cc4a334cd98145b4696b467806af71
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2020-06-05 06:08:40 +0900

    Added two more tools to translate channel number to index

---
+++ klm/tools/b2klm-numberToIndex.cc
@@ -0,0 +1,36 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2020  Belle II Collaboration                              *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Giacomo De Pietro                                        *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+/* KLM headers. */
+#include <klm/dataobjects/KLMChannelArrayIndex.h>
+
+/* Belle 2 headers. */
+#include <framework/logging/Logger.h>
+
+/* C++ headers. */
+#include <cstdlib>
+
+using namespace Belle2;
+
+int main(int argc, char* argv[])
+{
+  int nChannels = argc - 1;
+  if (nChannels == 0) {
+    B2ERROR("There are no channels to convert.");
+    return 0;
+  }
+  const KLMChannelArrayIndex* channelArrayIndex = &(KLMChannelArrayIndex::Instance());
+  for (int i = 1; i <= nChannels; ++i) {
+    uint16_t number = std::atoi(argv[i]);
+    uint16_t index = channelArrayIndex->getIndex(number);
+    B2INFO("Channel number: " << number << "  ==> Channel index: " << index);
+  }
+  return 0;
+}

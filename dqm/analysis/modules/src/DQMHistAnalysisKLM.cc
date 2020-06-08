6f6ec080d5c21cb47eed5723a23d7a49e2ea3f9d
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2020-06-09 05:38:26 +0900

    Ugly solution: hard-code masking of EB3, layer 6

--- dqm/analysis/modules/src/DQMHistAnalysisKLM.cc
+++ dqm/analysis/modules/src/DQMHistAnalysisKLM.cc
@@ -160,8 +160,12 @@ void DQMHistAnalysisKLMModule::analyseChannelHitHistogram(
       std::vector<uint16_t>::iterator ite = std::find(m_DeadEndcapModules.begin(),
                                                       m_DeadEndcapModules.end(),
                                                       moduleNumber);
-      if (ite == m_DeadEndcapModules.end())
-        m_DeadEndcapModules.push_back(moduleNumber);
+      if (ite == m_DeadEndcapModules.end()) {
+        /* FIXME: remove this hard-coded selection for EB3, layer 6. */
+        if (moduleNumber != 24) {
+          m_DeadEndcapModules.push_back(moduleNumber);
+        }
+      }
     }
   }
   if (activeModuleChannels == 0)

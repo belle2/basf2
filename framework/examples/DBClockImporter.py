f095a0a50b7e601a2dbdffc9821cdb1d1913c1f1
Gian Luca Pinna Angioni <gian.luca.pinna.angioni@desy.de>
2020-06-09 00:58:00 +0900

    fix error in payload

--- framework/examples/DBClockImporter.py
+++ framework/examples/DBClockImporter.py
@@ -11,7 +11,7 @@ clocks = Belle2.HardwareClockSettings()
 clocks.setAcceleratorRF(508.887)
 clocks.setClockPrescale(Belle2.Const.EDetector.TOP, "sampling", 6)
 clocks.setClockPrescale(Belle2.Const.EDetector.SVD, "sampling", 4)
-clocks.setClockPrescale(Belle2.Const.EDetector.SVD, "sampling", 3 * 24)
+clocks.setClockPrescale(Belle2.Const.EDetector.ECL, "sampling", 3 * 24)
 
 iov = Belle2.IntervalOfValidity.always()
 db = Belle2.Database.Instance()

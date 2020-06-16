e0cbf6cd78d92dde3efa1ff6222b9b96852f83c0
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2020-06-16 10:23:53 +0200

    Remvoed dead code

--- analysis/variables/include/KLMClusterVariables.h
+++ analysis/variables/include/KLMClusterVariables.h
@@ -107,11 +107,6 @@ namespace Belle2 {
     double maximumKLMAngleCMS(const Particle* particle);
 
     /**
-     * returns the minimum angle of a KLMCluster to this Particle in the CMS frame
-     */
-    double minimumKLMAngleCMS(const Particle* particle);
-
-    /**
      * returns the number of Tracks matched to the KLMCluster associated to this Particle
      * (>0 for K_L0 and matched Tracks, NaN for not-matched Tracks).
      */

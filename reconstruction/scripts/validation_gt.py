0a407d34c660581dce2f1696d3f5409bbc5ae397
Jitendra Kumar <jitendra.kumar@desy.de>
2020-07-07 01:32:29 +0900

    adding a function to get GTs used in validaton test

---
+++ reconstruction/scripts/validation_gt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import glob
+
+
+def get_validation_globaltags():
+    globaltags = ['validation_2020-05-02', 'online_proc11', 'Legacy_IP_Information']
+    return globaltags

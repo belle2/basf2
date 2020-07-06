ee8823a20f27165914cdff60abed70186dc572fa
Thibaud Humair <thibaud.humair@desy.de>
2020-07-07 01:38:31 +0900

    increment TagVertex class version number

--- analysis/dataobjects/include/TagVertex.h
+++ analysis/dataobjects/include/TagVertex.h
@@ -436,7 +436,8 @@ namespace Belle2 {
      */
     void resetConstraintCov();
 
-    ClassDef(TagVertex, 4) /**<
+    ClassDef(TagVertex, 5) /**<
+           5. Renamed variables related to DeltaZ and DeltaT
            4. Add info related to fit tracks, rave weights
            3. Add NDF, Chi2, Chi2IP
            2. Name to contain "Tag"

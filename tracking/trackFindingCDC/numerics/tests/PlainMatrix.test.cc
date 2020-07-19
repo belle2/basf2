57fc741dd1e9335d35e0641a5f4357527b0c2a48
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2020-07-19 23:05:25 +0900

    Reverted a modification, since it was a false positive from cppcheck

--- tracking/trackFindingCDC/numerics/tests/PlainMatrix.test.cc
+++ tracking/trackFindingCDC/numerics/tests/PlainMatrix.test.cc
@@ -282,6 +282,9 @@ TEST(TrackFindingCDCTest, PlainMatrixEigenMap_mapToEigen)
   EXPECT_EQ(0, eigenMap(0, 0)) << "write back test " << 0 << ", " << 0;
   EXPECT_EQ(0, result(0, 0)) << "write back test " << 0 << ", " << 0;
 
+  // cppcheck-suppress unreadVariable
+  eigenMap = Eigen::Matrix<double, 5, 2>::Zero();
+
   for (int i = 0; i < result.rows(); ++i) {
     for (int j = 0; j < result.cols(); ++j) {
       EXPECT_EQ(0, result(i, j)) << "write back test " << i << ", " << j;

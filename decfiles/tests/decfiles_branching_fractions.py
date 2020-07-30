5db11256b9741abaeef917fdbd39156f8333bc05
Stephan Duell <s.duell@physik.uni-bonn.de>
2020-07-30 16:02:33 +0200

    forgot to exit in test

--- decfiles/tests/decfiles_branching_fractions.py
+++ decfiles/tests/decfiles_branching_fractions.py
@@ -87,7 +87,9 @@ for particle in database.ParticleList():
             if((abs(code) == 511 or abs(code) == 521) and bfsum > 0 and abs(1.0-bfsum) > 1e-7):
                 print(f'Sum of decay mode branching fractions '
                       f'for {name} is not compatible with 1 ({bfsum}).')
+                exit(1)
             # This should be done for each particle, not only B mesons, but the other particle's decays have not yet been fixed
             if((abs(code) == 511 or abs(code) == 521) and antibfsum > 0 and abs(1.0-antibfsum) > 1e-7):
                 print(f'Sum of decay mode branching fractions '
                       f'for {antiname} is not compatible with 1 ({antibfsum}).')
+                exit(1)

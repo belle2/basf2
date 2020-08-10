f57996fabfdd125dc699096ca115363c38398ac7
Sviatoslav Bilokin <sviatoslav.bilokin@desy.de>
2020-08-10 20:04:41 +0200

    scripts

--- analysis/scripts/stdPhotons.py
+++ analysis/scripts/stdPhotons.py
@@ -77,7 +77,7 @@ def stdPhotons(listtype='loose', path=None):
             True,
             path)
     elif listtype == 'pi0eff30_Jan2020' or listtype == 'pi0eff40_Jan2020':
-            fillParticleList(
+        fillParticleList(
                 f'gamma:{listtype}',
                 '[clusterReg==1 and E>0.080] or [clusterReg==2 and E>0.030] or [clusterReg==3 and E>0.060 ]',
                 True,

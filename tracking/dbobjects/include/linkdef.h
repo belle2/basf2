fa3bfe982226cc5b273e390113dfd626ff24e714
JUNKERKALEFELD Henrik <junkerkalefeld@physik.uni-bonn.de>
2020-09-03 18:18:51 +0900

    added checksum and version to linkdef file

--- tracking/dbobjects/include/linkdef.h
+++ tracking/dbobjects/include/linkdef.h
@@ -5,6 +5,6 @@
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
-#pragma link C++ class Belle2::CDCQICutParameter+;
+#pragma link C++ class Belle2::CDCQICutParameter+; // checksum=0x83ab19e1, version=1
 
 #endif

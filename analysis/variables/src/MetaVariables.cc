5d7252955535a2f2c59fc74fa7cfb482d00bd5b5
Sato Yo <sato.yo@desy.de>
2020-09-08 17:59:50 +0900

    aligned indents again

--- analysis/variables/src/MetaVariables.cc
+++ analysis/variables/src/MetaVariables.cc
@@ -2831,7 +2831,7 @@ Returns 1.0 if the particle's matched MC particle is also matched to a particle 
                       E.g. ``mcDaughter(0, PDG)`` will return the PDG code of the first MC daughter of the matched MC
                       particle of the reconstructed particle the function is applied to.\n
                       The meta variable can also be nested: ``mcDaughter(0, mcDaughter(1, PDG))``.
-                      
+
                       .. warning::
                         MC variables cannot be used in this function, i.e. ``mcDaughter(0, mcPDG)`` returns NaN, for example.
                         This function gives 'variable of MC daughter' which are always MC truth.
@@ -2847,7 +2847,7 @@ Returns 1.0 if the particle's matched MC particle is also matched to a particle 
 
                       .. warning::
                         MC variables cannot be used in this function, i.e. ``mcMother(mcPDG)`` returns NaN, for example.
-		        This function gives 'variable of MC mother' which are always MC truth.
+                        This function gives 'variable of MC mother' which are always MC truth.
 
                       )DOC");
     REGISTER_VARIABLE("genParticle(index, variable)", genParticle,  R"DOC(

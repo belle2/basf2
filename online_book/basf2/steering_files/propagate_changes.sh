77bca8e122258274e1351616cb003c13c8fd7825
Kilian <kilian.adriano.lieret@desy.de>
2020-09-16 13:07:52 +0200

    Add usage instructions for propagate_changes.sh

--- online_book/basf2/steering_files/propagate_changes.sh
+++ online_book/basf2/steering_files/propagate_changes.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# Example use case:
+# You change something in 019_first_stering_file.py
+# For consistency, this change needs to be applied to
+# 029, ..., 059_vertex_fitting.py as well.
+# Then run
+# ./propagate_changes 019_first_steering_file 029_roe.py 039_various_additions.py <etc>
+# To apply these changes there as well.
+
+# Contributors: Kilian Lieret
+
 set -e
 
 changed_file=$1

707441f877837e8982a679ffafebbe1d7a9b2a1d
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2020-09-18 23:46:50 +0900

    Added a test to check the counters functionality

--- hlt/softwaretrigger/core/src/utilities.cc
+++ hlt/softwaretrigger/core/src/utilities.cc
@@ -7,6 +7,7 @@
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/
+
 #include <hlt/softwaretrigger/core/utilities.h>
 #include <TRandom.h>
 

b053627c9fea82e62827fe7d881b276bd59f5dc3
Hideyuki Nakazawa <hideyuki.nakazawa@desy.de>
2020-09-20 09:30:46 +0900

    setTimQuality removed from mdst.py.

--- mdst/scripts/mdst.py
+++ mdst/scripts/mdst.py
@@ -137,7 +137,7 @@ def add_mdst_dump(path, print_untested=False):
                              "getRelationsWith": ["KlIds", "MCParticles"],
                              }),
         DataStorePrinter("KlId", ["isKLM", "isECL", "getKlId"]),
-        DataStorePrinter("TRGSummary", ["getTimType", "getTimQuality", "setTimQuality"], {
+        DataStorePrinter("TRGSummary", ["getTimType", "getTimQuality"], {
             "getTRGSummary": range(10),
             "getPreScale": [[int(i / 32), i % 32] for i in list(range(320))],
             "getInputBits": range(10),

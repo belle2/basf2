e742816ab37a86316083e76bb585cc4cf1d0dc1b
Martin Ritter <martin.ritter@desy.de>
2020-09-22 12:20:18 +0200

    TUPPR: Update default globaltag to '{args.tagname}'

--- framework/database/src/Configuration.cc
+++ framework/database/src/Configuration.cc
@@ -23,7 +23,7 @@
 #include <TPython.h>
 
 // Current default globaltag when generating events.
-#define CURRENT_DEFAULT_TAG "master_2020-08-06"
+#define CURRENT_DEFAULT_TAG "master_2020-09-26"
 
 namespace py = boost::python;
 

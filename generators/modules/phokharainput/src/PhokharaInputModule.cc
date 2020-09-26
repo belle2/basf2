50114d3d11e26675291831faabd00e8f852b6e4c
Martin Ritter <martin.ritter@desy.de>
2020-09-26 16:01:41 +0200

    Use the environment variable utilities
    To avoid the rare case the externals dir is unset and thus we would try
    to convert a nullptr to a string.

--- generators/modules/phokharainput/src/PhokharaInputModule.cc
+++ generators/modules/phokharainput/src/PhokharaInputModule.cc
@@ -11,6 +11,7 @@
 #include <generators/modules/phokharainput/PhokharaInputModule.h>
 
 #include <framework/utilities/FileSystem.h>
+#include <framework/utilities/EnvironmentVariables.h>
 
 #include <framework/datastore/StoreArray.h>
 
@@ -71,8 +72,8 @@ PhokharaInputModule::PhokharaInputModule() : Module(), m_initial(BeamParameters:
   addParam("MinEnergyGamma", m_MinEnergyGamma, "Minimal photon energy/missing energy, must be greater than 0.0098 * CMS energy [GeV]",
            0.15);
 
-  std::string defaultParameterFile = std::getenv("BELLE2_EXTERNALS_DIR");
-  defaultParameterFile += "/share/phokhara/const_and_model_paramall10.0.dat";
+  const std::string defaultParameterFile =
+    EnvironmentVariables::expand("${BELLE2_EXTERNALS_DIR}/share/phokhara/const_and_model_paramall10.0.dat");
   addParam("ParameterFile", m_ParameterFile,
            "File that contains particle properties.",
            defaultParameterFile);

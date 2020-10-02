03f6adaff6a79ff7eee878da34d3135659362cd9
Giulia Casarosa <giulia.casarosa@desy.de>
2020-10-02 20:36:37 +0900

    SVD TriggerBin in simulation taken from SimClockState class

--- svd/modules/svdSimulation/include/SVDEventInfoSetterModule.h
+++ svd/modules/svdSimulation/include/SVDEventInfoSetterModule.h
@@ -17,6 +17,7 @@
 #include <svd/dataobjects/SVDEventInfo.h>
 #include <svd/dataobjects/SVDModeByte.h>
 #include <svd/dataobjects/SVDTriggerType.h>
+#include <simulation/dataobjects/SimClockState.h>
 
 namespace Belle2 {
   /**
@@ -51,6 +52,7 @@ namespace Belle2 {
 
   private:
     StoreObjPtr<SVDEventInfo> m_svdEventInfoPtr; /**< Output object. */
+    StoreObjPtr<SimClockState> m_simClockState; /**< generated hardware clock state */
 
     // SVDModeByte content */
     int m_runType; /**< run type */

028ad77800629abd18716b960657d24ee39b359b
Sato Yo <sato.yo@desy.de>
2020-10-05 18:31:58 +0900

    changed to use the common tutorial style

--- analysis/examples/pi0EtaVeto_usage.py
+++ analysis/examples/pi0EtaVeto_usage.py
@@ -10,20 +10,19 @@ and eta probability.
 
 __authors__ = "Yo Sato"
 
-import basf2
+import basf2 as b2
 from ROOT import Belle2
-from modularAnalysis import inputMdst, reconstructDecay, matchMCTruth, \
-    writePi0EtaVeto, buildRestOfEvent, variablesToNtuple
+import modularAnalysis as ma
 from variables import variables as vm  # shorthand for the variable manager instance
 from stdCharged import stdK, stdPi
 from stdPhotons import stdPhotons
 
 # writePi0EtaVeto uses a payload in analysis global tag.
-basf2.conditions.prepend_globaltag('analysis_tools_release-04-02')
+b2.conditions.prepend_globaltag('analysis_tools_release-04-02')
 
-mypath = basf2.create_path()
+mypath = b2.create_path()
 testinput = Belle2.FileSystem.findFile('analysis/tests/mdst.root')
-inputMdst("default", testinput, path=mypath)
+ma.inputMdst("default", testinput, path=mypath)
 
 stdK("loose", path=mypath)
 stdPi("loose", path=mypath)
@@ -31,36 +30,36 @@ stdPhotons("loose", path=mypath)
 
 # Example 1 : Calculate pi0/eta probability using gamma
 # Reconstruct B0 -> K*0 gamma
-reconstructDecay("K*0:Kpi     -> K+:loose pi-:loose", "", path=mypath)
-reconstructDecay("B0:Kpigamma -> K*0:Kpi gamma:loose", "useCMSFrame(daughter(1,E))>1.4", path=mypath)
-matchMCTruth("B0:Kpigamma", path=mypath)
+ma.reconstructDecay("K*0:Kpi     -> K+:loose pi-:loose", "", path=mypath)
+ma.reconstructDecay("B0:Kpigamma -> K*0:Kpi gamma:loose", "useCMSFrame(daughter(1,E))>1.4", path=mypath)
+ma.matchMCTruth("B0:Kpigamma", path=mypath)
 
 # writePi0EtaVeto requires to build ROE
-buildRestOfEvent("B0:Kpigamma", path=mypath)
+ma.buildRestOfEvent("B0:Kpigamma", path=mypath)
 
 # Call writePi0EtaVeto
 # particleList : Signal side particle's particleList
 # decayString : DecayString specifing a particle which is used to calculate the pi0/eta probability
-writePi0EtaVeto(particleList='B0:Kpigamma',
-                decayString='B0 -> [K*0 -> K+ pi-] ^gamma',
-                path=mypath)
+ma.writePi0EtaVeto(particleList='B0:Kpigamma',
+                   decayString='B0 -> [K*0 -> K+ pi-] ^gamma',
+                   path=mypath)
 
 # Example 2 : Calculate pi0/eta probability using non-gamma particle.
 # Reconstruct B+ -> anti-D0 pi+. This is one of the mode to validate the pi0/eta veto tool.
-reconstructDecay("D0:Kpi -> K-:loose pi+:loose", "", path=mypath)
-reconstructDecay("B+:Dpi -> anti-D0:Kpi pi+:loose", "useCMSFrame(daughter(1,E))>1.4", path=mypath)
-matchMCTruth("B+:Dpi", path=mypath)
+ma.reconstructDecay("D0:Kpi -> K-:loose pi+:loose", "", path=mypath)
+ma.reconstructDecay("B+:Dpi -> anti-D0:Kpi pi+:loose", "useCMSFrame(daughter(1,E))>1.4", path=mypath)
+ma.matchMCTruth("B+:Dpi", path=mypath)
 
 # writePi0EtaVeto requires to build ROE
-buildRestOfEvent("B+:Dpi", path=mypath)
+ma.buildRestOfEvent("B+:Dpi", path=mypath)
 
 # Call writePi0EtaVeto
 # hardParticle : If you want to use non-gamma particle to calcuate the pi0/eta probability,
 #                you have to tell the particle name with an argument hardPhoton. (default: gammma)
-writePi0EtaVeto('B+:Dpi',
-                'B+ -> [anti-D0 -> K+ pi-] ^pi+',
-                hardParticle='pi+',
-                path=mypath)
+ma.writePi0EtaVeto('B+:Dpi',
+                   'B+ -> [anti-D0 -> K+ pi-] ^pi+',
+                   hardParticle='pi+',
+                   path=mypath)
 
 vm.addAlias("Pi0ProbOrigin", "extraInfo(Pi0ProbOrigin)")
 vm.addAlias("EtaProbOrigin", "extraInfo(EtaProbOrigin)")
@@ -69,10 +68,10 @@ interesting_variables = ['isSignal', "Mbc", "deltaE", "Pi0ProbOrigin", "EtaProbO
 
 vm.printAliases()
 
-variablesToNtuple("B0:Kpigamma", interesting_variables, treename="Kstgamma", path=mypath)
-variablesToNtuple("B+:Dpi",      interesting_variables, treename="Dpi", path=mypath)
+ma.variablesToNtuple("B0:Kpigamma", interesting_variables, treename="Kstgamma", path=mypath)
+ma.variablesToNtuple("B+:Dpi",      interesting_variables, treename="Dpi", path=mypath)
 
-basf2.process(mypath)
-print(basf2.statistics)
+b2.process(mypath)
+print(b2.statistics)
 
 # @endcond

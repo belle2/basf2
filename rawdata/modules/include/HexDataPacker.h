e269631ae05cae41a05bab6eaed9098090e7900e
Satoru Yamada <satoru.yamada@kek.jp>
2020-10-23 12:16:46 +0900

    Add HexDataPacker module to make a sroot file from hex-dumped data in the Ehut KLM test with PCIe40 in October, 2020.

---
+++ rawdata/modules/include/HexDataPacker.h
@@ -0,0 +1,107 @@
+//+
+// File : HexDataPacker.h
+// Description : Module to store dummy data in RawCOPPER object
+//
+// Author : Satoru Yamada, IPNS, KEK
+// Date : 14 - Jul - 2014
+//-
+
+#ifndef HEXDATAPACKER_H
+#define HEXDATAPACKER_H
+
+#include <fstream>
+
+#include <framework/core/Module.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <framework/datastore/StoreArray.h>
+#include <framework/dataobjects/EventMetaData.h>
+
+#include <rawdata/dataobjects/RawCOPPER.h>
+#include <rawdata/dataobjects/RawSVD.h>
+#include <rawdata/dataobjects/RawCDC.h>
+#include <rawdata/dataobjects/RawTOP.h>
+#include <rawdata/dataobjects/RawARICH.h>
+#include <rawdata/dataobjects/RawECL.h>
+#include <rawdata/dataobjects/RawKLM.h>
+#include <rawdata/dataobjects/RawTRG.h>
+
+using namespace std;
+
+#define MAX_CPRBUF_WORDS 5000
+
+namespace Belle2 {
+
+  /*! Module to store dummy data in RawCOPPER object */
+
+  class HexDataPackerModule : public Module {
+
+    // Public functions
+  public:
+
+    //! Constructor / Destructor
+    HexDataPackerModule();
+    virtual ~HexDataPackerModule();
+
+    //! initialization
+    virtual void initialize() override;
+
+    //! event module
+    virtual void event() override;
+
+
+  protected :
+    //! check data contents
+    //    virtual void checkData(RawDataBlock* raw_datablk, unsigned int* eve_copper_0);
+
+    //    StoreArray<RawFTSW> raw_ftswarray;
+
+    //! # of events in a run
+    int max_nevt;
+
+    //! Node ID
+    //    int m_nodeid;
+
+    //! event counter
+    int n_basf2evt;
+
+    //! Messaage handler
+    //    MsgHandler* m_msghandler;
+
+    /*     //! Compression Level */
+    /*     int m_compressionLevel; */
+
+    //! Event Meta Data
+    StoreObjPtr<EventMetaData> m_eventMetaDataPtr;
+
+    //! RawCOPPER array
+    StoreArray<RawCOPPER> raw_cprarray;
+
+    //! RawSVD array
+    StoreArray<RawSVD> raw_svdarray;
+
+    //! RawCDC array
+    StoreArray<RawCDC> raw_cdcarray;
+
+    //! RawTOP array
+    StoreArray<RawTOP> raw_bpidarray;
+
+    //! RawARICH array
+    StoreArray<RawARICH> raw_epidarray;
+
+    //! RawECL array
+    StoreArray<RawECL> raw_eclarray;
+
+    //! RawKLM array
+    StoreArray<RawKLM> raw_klmarray;
+
+    //! RawARICH array
+    StoreArray<RawTRG> raw_trgarray;
+
+  private:
+    ifstream m_ifs; //! No record
+
+  };
+
+} // end namespace Belle2
+
+#endif // MODULEHELLO_H
---
+++ rawdata/modules/src/HexDataPacker.cc
@@ -0,0 +1,212 @@
+//+
+// File : DummyDataPacker.cc
+// Description : Module to store dummy data in RawSVD(and others) object
+//
+// Author : Satoru Yamada, IPNS, KEK
+// Date : 14 - Jul - 2014
+//-
+
+
+// The format of readable hex dump
+// ************************************************
+// Program to read events by DMA
+// Data mode
+// SUCCESS: Device opened for ECS 0
+// SUCCESS: Device opened for ECS 2
+// SUCCESS: Device opened for DMA
+// Header  : eeee000c aaaaeeee 0180aaaa 00000000 00010000 00000000 00000000 00000000
+//  data   0 : eeee000c aaaaeeee 0180aaaa 00000000 00010000 00000000 00000000 00000000
+//  data   7 : 0000004e 7f7f0438 0343cd00 00000000 21ed8e47 5f7e6a02 00000000 00000001
+//  data  15 : 00000038 00000038 00000038 00000038 00000038 00000038 00000038 00000038
+//  data  23 : 00000038 00000038 00000038 00000038 0000004a 0000004a 0000004a 0000004a
+//  data  31 : 0000004a 0000004a 0000004a 0000004a 0000004a 0000004a 0000004a 0000004a
+//  data  39 : 0000004a 0000004a 0000004a 0000004a 0000004a 0000004a 0000004a 0000004a
+//  data  47 : 0000004a 0000004a 0000004a 0000004a 0000004a 0000004a 0000004a 0000004a
+//  data  55 : 0000004a 0000004a 0000004a 0000004a 0000004a 0000004a 0000004a 0000004a
+//  data  63 : ffaa0b00 21ed8e47 00000000 5f7e6a02 0343cd00 21ed9c30 4898121d 06a40000
+//  data  71 : 4cb01218 06840000 540110de 04980000 5401120f 060c0000 12ee0000 21ed8e47
+//  data  79 : 00006408 ff550000 7fff1775 7fff0006 7fff0007 7fff0008 7fff0009 7fff000a
+//  data  87 : Trailer :        0       0       0       0       0       0       0       C
+// Bad event number prev 0 cur 0
+// Header  : eeee000c aaaaeeee 0180aaaa 00000000 00010000 00000001 00000000 00000000
+//  data   0 : eeee000c aaaaeeee 0180aaaa 00000000 00010000 00000001 00000000 00000000
+//  data   7 : 0000004c 7f7f0438 0343cd00 00000001 21fc9f57 5f7e6a02 00000000 00000001
+// ************************************************
+#include <iostream>
+#include <rawdata/modules/HexDataPacker.h>
+
+
+using namespace std;
+using namespace Belle2;
+
+#define USE_PCIE40
+
+//-----------------------------------------------------------------
+//                 Register the Module
+//-----------------------------------------------------------------
+REG_MODULE(HexDataPacker)
+
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
+
+HexDataPackerModule::HexDataPackerModule() : Module()
+{
+  ///Set module properties
+  setDescription("an Example to pack data to a RawCOPPER object");
+
+  ///  maximum # of events to produce( -1 : inifinite)
+  addParam("MaxEventNum", max_nevt, "Maximum event number to make", -1);
+
+  ///  maximum # of events to produce( -1 : inifinite)
+  //  addParam("NodeID", m_nodeid, "Node ID", 0);
+
+  B2INFO("HexDataPacker: Constructor done.");
+
+  // initialize event #
+  n_basf2evt = 0;
+
+
+}
+
+
+
+HexDataPackerModule::~HexDataPackerModule()
+{
+
+}
+
+
+void HexDataPackerModule::initialize()
+{
+  B2INFO("HexDataPacker: initialize() started.");
+
+  // Open message handler
+  //  m_msghandler = new MsgHandler(m_compressionLevel);
+
+  /// Initialize EvtMetaData
+  m_eventMetaDataPtr.registerInDataStore();
+
+  raw_cprarray.registerInDataStore();
+  raw_svdarray.registerInDataStore();
+  raw_cdcarray.registerInDataStore();
+  raw_bpidarray.registerInDataStore();
+  raw_epidarray.registerInDataStore();
+  raw_eclarray.registerInDataStore();
+  raw_klmarray.registerInDataStore();
+
+  B2INFO("HexDataPacker: initialize() done.");
+
+
+  // Read the channel map from the local text.
+  std::string fileName = "/home/usr/yamadas/hex.txt";
+  std::cout << fileName << std::endl;
+
+  if (fileName == "") {
+    B2FATAL("HexDataPacker can't fine a filename: ");
+  }
+
+  m_ifs.open(fileName.c_str());
+  if (!m_ifs) {
+    B2FATAL("HexDataPacker can't open a file " << fileName.c_str());
+  }
+}
+
+
+
+
+void HexDataPackerModule::event()
+{
+  StoreArray<RawKLM> ary;
+
+  int* evtbuf = new int[MAX_CPRBUF_WORDS];
+  char char1[50], char2[50], char3[50], char4[50], char5[50], char6[50];
+  char char11[50], char12[50], char13[50], char14[50], char15[50], char16[50];
+
+
+  unsigned int val[10];
+  int size = 0;
+  int run_end = 0;
+  int word_count = 0;
+  int event_end = 0;
+  while (true) {
+
+    if (m_ifs.eof()) {
+      run_end = 1;
+      break;
+    }
+    string strin;
+    getline(m_ifs, strin);
+    printf("%s\n", strin.c_str());
+    sscanf(strin.c_str(), "%s %s %s %s",
+           char1, char2, char3, char4);
+    printf("Printing %s %s %s %s\n",
+           char1, char2, char3, char4);
+    if (strcmp(char1, "data") == 0) {
+      printf("pos 1\n"); fflush(stdout);
+      if (strcmp(char4, "Trailer") == 0) {
+        printf("pos 2\n"); fflush(stdout);
+        continue;
+      }
+      if (strcmp(char2, "0") == 0) {
+        printf("pos 3\n"); fflush(stdout);
+        continue;
+      } else if (strcmp(char2, "7") == 0) {
+        printf("pos 4\n"); fflush(stdout);
+        sscanf(strin.c_str(), "%s %d %s %x %x %x %x %x %x %x %x",
+               char1, &(val[0]), char2, &(val[1]), &(val[2]), &(val[3]), &(val[4]), &(val[5]), &(val[6]), &(val[7]), &(val[8]));
+        size = val[1];
+        if (size < 0 || size > MAX_CPRBUF_WORDS) {
+          B2FATAL("The size of an event =(" << size << ") is too large. Exiting...");
+        }
+      }
+      if (size <= 0) {
+        B2FATAL("The size of an event (=" << size << ") is too large. Exiting...");
+      } else {
+        printf("pos 5\n"); fflush(stdout);
+        for (int i = 1; i <= 8 ; i++) {
+          if (word_count >= MAX_CPRBUF_WORDS) {
+            B2FATAL("The size of an event (=" << size << ") is too large. Exiting...");
+          }
+          evtbuf[word_count] = val[i];
+          printf("word %d size %d\n", word_count, size); fflush(stdout);
+          word_count++;
+          if (word_count == size) {
+            event_end = 1;
+            break;
+          }
+        }
+        if (event_end == 1) {break;}
+      }
+    }
+  }
+
+  StoreObjPtr<EventMetaData> evtmetadata;
+  if (run_end == 0) {
+    (ary.appendNew())->SetBuffer(evtbuf, size, 1, 1, 1);
+
+    //
+    // Update EventMetaData :
+    //
+    RawCOPPER tempcpr;
+    tempcpr.SetBuffer(evtbuf, size, false, 1, 1);
+
+
+    evtmetadata.create();
+    evtmetadata->setExperiment(tempcpr.GetExpNo(0));
+    evtmetadata->setRun(tempcpr.GetRunNo(0));
+    evtmetadata->setSubrun(tempcpr.GetSubRunNo(0));
+    evtmetadata->setEvent(tempcpr.GetEveNo(0));
+    //  evtmetadata->setTime(mtime);  //time(NULL));
+
+  } else {
+    delete[] evtbuf;
+    evtmetadata->setEndOfData();
+  }
+
+  printf("Event counter %8d\n", n_basf2evt); fflush(stdout);
+  n_basf2evt++;
+
+
+  return;
+}

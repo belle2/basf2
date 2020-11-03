c94ea3c16eaee0389ca71c965903ffe765436c5b
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2020-11-03 16:43:34 +0100

    Added file statistics to the test; Checking also the log

--- framework/tests/include_exclude_branches.py
+++ framework/tests/include_exclude_branches.py
@@ -4,9 +4,12 @@ import ROOT
 
 
 def filter_input_output(input_file_name, output_file_name, include_branches=None, exclude_branches=None):
+
+    b2u.configure_logging_for_tests()
     child_path = b2.Path()
     child_path.add_module('RootInput',
-                          inputFileName=input_file_name)
+                          inputFileName=input_file_name,
+                          collectStatistics=True)
     if include_branches is not None:
         b2.set_module_parameters(child_path,
                                  'RootInput',
@@ -21,26 +24,25 @@ def filter_input_output(input_file_name, output_file_name, include_branches=None
 
 
 def check_file_content(file_name, input_branches):
-    file = ROOT.TFile.Open(file_name, 'READ')
-    if file.IsZombie():
+
+    f = ROOT.TFile.Open(file_name, 'READ')
+    if f.IsZombie():
         b2.B2FATAL(f'Cannot open the input file {file_name}.')
-    tree = file.Get('tree')
+    tree = f.Get('tree')
     file_branches = [branch.GetName() for branch in tree.GetListOfBranches()]
-    file.Close()
+    f.Close()
     return set(input_branches) <= set(file_branches)
 
 
 if __name__ == '__main__':
+
     with b2u.clean_working_directory():
-
+        # Let's check the input file
         input_file_name = b2.find_file('framework/tests/mdst_with_muid.root')
-
-        # Let's check if some branches are present.
-        assert (
-            1 == check_file_content(
-                input_file_name, [
-                    'Tracks', 'ECLClusters', 'TracksToECLClusters', 'Muids', 'TracksToMuids']))
-
+        assert (0 == b2u.run_in_subprocess(input_file_name, 'mdst_all_branches.root', None, None,
+                                           target=filter_input_output))
+        all_branches = ['Tracks', 'ECLClusters', 'TracksToECLClusters', 'Muids', 'TracksToMuids']
+        assert (1 == check_file_content(input_file_name, all_branches))
         # Let's exclude Muids.
         file_to_check = 'mdst_without_muid.root'
         assert (0 == b2u.run_in_subprocess(input_file_name, file_to_check, None, ['Muids'],
@@ -48,14 +50,12 @@ if __name__ == '__main__':
         assert (0 == check_file_content(file_to_check, ['Muids']))
         assert (0 == check_file_content(file_to_check, ['TrakcsToMuids']))
         assert (1 == check_file_content(file_to_check, ['Tracks', 'ECLClusters']))
-
         # Let's include only Muids.
         file_to_check = 'mdst_only_muid.root'
         assert (0 == b2u.run_in_subprocess(input_file_name, file_to_check, ['Muids'], None,
                                            target=filter_input_output))
         # The dictionary is not found, so it can't be stored.
         assert (0 == check_file_content(file_to_check, ['Muids']))
-
         # Let's include only Tracks and ECLClusters.
         assert (0 == b2u.run_in_subprocess(input_file_name, file_to_check, ['Tracks', 'ECLClusters'], None,
                                            target=filter_input_output))

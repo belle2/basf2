35b0eeb3fd21674e23c2345d52bc53b45ab8f4a6
staric <marko.staric@ijs.si>
2020-11-12 15:46:29 +0100

    Track at TOP

---
+++ top/reconstruction_cpp/include/HelixSwimmer.h
@@ -0,0 +1,19 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2020 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Marko Staric                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#pragma once
+
+namespace Belle2 {
+  namespace TOP {
+
+
+  } // namespace TOP
+} // namespace Belle2
+
---
+++ top/reconstruction_cpp/src/HelixSwimmer.cc
@@ -0,0 +1,21 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2020 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Marko Staric                                             *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+
+#include <top/reconstruction_cpp/HelixSwimmer.h>
+#include <framework/logging/Logger.h>
+#include <cmath>
+#include <algorithm>
+
+namespace Belle2 {
+  namespace TOP {
+
+  } //TOP
+} //Belle2
+

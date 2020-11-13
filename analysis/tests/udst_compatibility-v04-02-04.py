dc97553ca9206ea827a51748186ed5f77a6746f8
Sam Cunliffe <sam.cunliffe@desy.de>
2020-11-13 21:10:04 +0100

    Add udst backward-compatibility test for release-04-01-04.
    This commit *must* fail. At the very least, because of different untested methods.

---
+++ analysis/tests/udst_compatibility-v04-02-04.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Test backwards compatibility for a udst file produced with release-04-01-04.
+
+This is not always guaranteed. Analysis objects may break backwards
+compatibility for older files. Some niche corner cases might be problematic for
+file produced with release older than release-05-00-00.
+
+See https://confluence.desy.de/display/BI/Backward+Compatibility for more
+"""
+
+import basf2
+import b2test_utils
+import udst
+
+if __name__ == "__main__":
+
+    # clear out stray env variables and set seeding for reproducible results
+    b2test_utils.configure_logging_for_tests()
+    basf2.set_random_seed(1)
+
+    # configure processing path - input file is the first event from the Phil's
+    # favourite test udst file from https://questions.belle2.org/question/9758/
+    main = basf2.create_path()
+    main.add_module(
+        "RootInput", inputFileName=basf2.find_file("analysis/tests/udst-v04-01-04.root")
+    )
+    main.add_module("EventInfoPrinter")
+
+    # this function does the hard work
+    udst.add_udst_dump(main, True)
+
+    basf2.process(main, 1)

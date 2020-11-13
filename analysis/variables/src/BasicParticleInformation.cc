524a954d036db00cc7bee98a3ee7d57674fb0cf1
Frank Meier <frank.meier@duke.edu>
2020-11-13 23:43:37 +0900

    added new variable that returns particle source

--- analysis/variables/src/BasicParticleInformation.cc
+++ analysis/variables/src/BasicParticleInformation.cc
@@ -35,6 +35,11 @@ namespace Belle2 {
       return (part->getParticleSource() == Particle::EParticleSourceObject::c_V0);
     }
 
+    double particleSource(const Particle* part)
+    {
+      return part->getParticleSource();
+    }
+
     double particleMdstArrayIndex(const Particle* part)
     {
       return part->getMdstArrayIndex();
@@ -76,6 +81,7 @@ namespace Belle2 {
     REGISTER_VARIABLE("isFromKLM", particleIsFromKLM, "Returns 1.0 if this particle was created from a KLMCluster, 0 otherwise.");
     REGISTER_VARIABLE("isFromTrack", particleIsFromTrack, "Returns 1.0 if this particle was created from a track, 0 otherwise.");
     REGISTER_VARIABLE("isFromV0", particleIsFromV0, "Returns 1.0 if this particle was created from a V0, 0 otherwise.");
+    REGISTER_VARIABLE("particleSource", particleSource, "Returns mdst source used to create the particle.");
     REGISTER_VARIABLE("mdstIndex", particleMdstArrayIndex, R"DOC(
 Store array index (0 - based) of the MDST object from which the Particle was created. 
 It's 0 for composite particles.

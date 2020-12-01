e4e37887167bad2d5033cdd7ba4ac6f3e172642d
JUNKERKALEFELD Henrik <junkerkalefeld@physik.uni-bonn.de>
2020-12-01 22:21:41 +0900

    changed the validation scripts back to default for the coming PR

--- tracking/validation/do_validation_chain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-i=0
-imax=2
-while [ "$i" -le "$imax" ]; do
-    python3 raster_validation_helpertask.py $i
-    python3 fullTrackingValidation.py
-    ((i++))
-done
--- tracking/validation/fullTrackingValidation.py
+++ tracking/validation/fullTrackingValidation.py
@@ -10,6 +10,10 @@
 </header>
 """
 
+VALIDATION_OUTPUT_FILE = 'FullTrackingValidation.root'
+N_EVENTS = 1000
+ACTIVE = True
+
 import basf2
 basf2.set_random_seed(1337)
 
@@ -17,28 +21,6 @@ import logging
 import tracking
 
 from tracking.validation.run import TrackingValidationRun
-
-test_cuts = [0.0, 0.7, 0.75]  # 0.25, 0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
-processes = ['BBBAR']
-with open("fullVal_cut_iterator.txt", "r") as cut_file:
-    iterator = int(cut_file.read())
-process_it, cut_it = divmod(iterator, 3)
-cut = test_cuts[cut_it]
-process = processes[process_it]
-print("The cut value of the CDC QI is:", cut)
-print("Test the process:", process)
-print(str(int(cut*100)).zfill(3))
-N_EVENTS = 2000
-# elif process =='BBBAR':
-#    N_EVENTS=1000
-VALIDATION_OUTPUT_FILE = 'ftv_reco_qi_N' + str(N_EVENTS) + '_' + process + \
-                                               '_novxd_cdcQi' + str(int(cut*100)).zfill(3) + '_exp0.root'
-
-print(VALIDATION_OUTPUT_FILE)
-# cut = 0.0
-# VALIDATION_OUTPUT_FILE = 'ftv_cdc_qi_N1000_cdcQi' + str(int(cut*100)).zfill(3) + '.root'
-# N_EVENTS = 1000
-ACTIVE = True
 
 
 class Full(TrackingValidationRun):
@@ -48,39 +30,9 @@ class Full(TrackingValidationRun):
     #: Generator to be used in the simulation (-so)
     generator_module = 'generic'
     #: no background overlay
-    # root_input_file = '../EvtGenSim_1k_exp1003_run2_new.root'
-    root_input_file = 'generated_mc_N' + str(N_EVENTS) + '_' + process + '_test_exp0.root'
+    root_input_file = '../EvtGenSimNoBkg.root'
     #: use the complete track-reconstruction chain
-    #: finder_module = staticmethod(tracking.add_tracking_reconstruction)
-
-    def finder_module(self, path):
-        tracking.add_tracking_reconstruction(path, add_cdcTrack_QI=True, add_vxdTrack_QI=False, add_recoTrack_QI=True)
-
-        # Replace weightfile identifiers from defaults (CDB payloads) to new
-        # weightfiles created by the b2luigi script
-        cdc_qe_mva_filter_parameters = {
-            # "identifier": 'trackfindingcdc_TrackQualityIndicator_nTrees350_nCuts6_nLevels5_shrin10_skimmedVar.weights.xml',
-            # "identifier": 'cdc_mva_qe_nTrees350_nCuts6_nLevels5_shrin10.weights.xml',
-            "cut": cut}
-        basf2.set_module_parameters(
-            path,
-            name="TFCDC_TrackQualityEstimator",
-            filterParameters=cdc_qe_mva_filter_parameters,
-            deleteTracks=True,
-            resetTakenFlag=True
-            )
-        # basf2.set_module_parameters(
-        #    path,
-        #    name="VXDQualityEstimatorMVA",
-        #    WeightFileIdentifier='vxdtf2_mva_qe_nTrees350_nCuts6_nLevels5_shrin10.weights.xml'
-        #    )
-        basf2.set_module_parameters(
-            path,
-            name="TrackQualityEstimatorMVA",
-            WeightFileIdentifier="recotrack_mva_qe_nTrees350_nCuts6_nLevels5_shrin10_deleteCDCQI"
-            + str(int(cut*100)).zfill(3) + "_noVXD_useCDC.weights.xml",
-            )
-
+    finder_module = staticmethod(tracking.add_tracking_reconstruction)
     #: Define the user parameters for the track-finding module
     tracking_coverage = {
         'WhichParticles': [],  # Include all particles, also secondaries
@@ -101,9 +53,6 @@ class Full(TrackingValidationRun):
     pulls = True
     #: Include resolution information in the validation output
     resolution = True
-    # save trees such that manual studies are possible
-    extended = True
-    saveFullTrees = True
     #: name of the output ROOT file
     output_file_name = VALIDATION_OUTPUT_FILE
 

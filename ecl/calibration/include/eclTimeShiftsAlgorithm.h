8c2d5891985ea29cb8c4b2fd463c19ac45e7f44b
Ewan Chin Hill <ewan.chin.hill@desy.de>
2020-12-02 04:34:47 +0900

    Adding tree

--- ecl/calibration/include/eclTimeShiftsAlgorithm.h
+++ ecl/calibration/include/eclTimeShiftsAlgorithm.h
@@ -45,6 +45,10 @@ namespace Belle2 {
       double crysCrateShift_min;
       double crysCrateShift_max;
 
+      /** Whether or not to have the algorithm code to loop
+          over all the runs and read the payloads itself.  */
+      bool algorithmReadPayloads;
+
       /** Whether or not to veto runs where an older payload is used.
           Force the payload revision number to increase as the
           run number increases. */
@@ -71,6 +75,29 @@ namespace Belle2 {
 
       /** Number of Crates expected */
       const int m_numCrates = 52;
+
+      // Variables to be read in from the per-crystal tree
+      Int_t m_run_perCrystal;
+      Int_t m_exp_perCrystal;
+      Int_t m_crystalID;
+      Double_t m_crateTimeConst;
+      Double_t m_crystalTimeConst;
+      Double_t m_crateTimeUnc;
+      Double_t m_crystalTimeUnc;
+      Int_t m_crateID;
+
+      // Variables to be read in from the per-crate tree
+      //Int_t m_run_perCrate;
+      //Int_t m_exp_perCrate;
+      Int_t m_refCrystalID;
+
+      // Cuts for runs to plot
+      double m_tcrate_min_cut = -150;
+      double m_tcrate_max_cut = 150;
+      double m_tcrate_unc_min_cut = 0.0001 ;
+      double m_tcrate_unc_max_cut = 0.4 ;
+
+
     };
   }
 } // namespace Belle2

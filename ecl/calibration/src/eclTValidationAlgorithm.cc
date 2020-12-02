0934c04d416a2e45e2b8ece6e152d4d89fbf1e53
Ewan Chin Hill <ewan.chin.hill@desy.de>
2020-12-03 02:50:24 +0900

    major improvements

--- ecl/calibration/src/eclTValidationAlgorithm.cc
+++ ecl/calibration/src/eclTValidationAlgorithm.cc
@@ -89,20 +89,13 @@ CalibrationAlgorithm::EResult eclTValidationAlgorithm::calibrate()
   // Collect other plots just for reference - combines all the runs for these plots.
   auto cutflow = getObjectPtr<TH1F>("cutflow");
 
+  vector <int> binProjectionLeft_Time_vs_E_runDep ;
+  vector <int> binProjectionRight_Time_vs_E_runDep ;
 
-  /* Define some of the parameters required for fitting the histograms */
-  //vector <int> binProjectionLeft_dt99_vs_E_runDep  = {1,2,3,4,5,6,7,8,9,10,11} ;
-  //vector <int> binProjectionRight_dt99_vs_E_runDep = {1,2,3,4,5,6,7,8,9,10,100} ;
-
-  /*
-    vector <int> binProjectionLeft_Time_vs_E_runDep  = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,40,45,50,60,70,80,90} ;
-    vector <int> binProjectionRight_Time_vs_E_runDep = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,39,44,49,59,69,79,89,100} ;
-  */
-  vector <int> binProjectionLeft_Time_vs_E_runDep  = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 41, 46, 51, 61, 71, 81, 91} ;
-  vector <int> binProjectionRight_Time_vs_E_runDep = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 40, 45, 50, 60, 70, 80, 90, 100} ;
-
-
-
+  for (int binCounter = 1; binCounter <= clusterTimeClusterE->GetNbinsX(); binCounter++) {
+    binProjectionLeft_Time_vs_E_runDep.push_back(binCounter);
+    binProjectionRight_Time_vs_E_runDep.push_back(binCounter);
+  }
 
   if (!clusterTime_cid) return c_Failure;
 
@@ -110,9 +103,18 @@ CalibrationAlgorithm::EResult eclTValidationAlgorithm::calibrate()
 
   TFile* histfile = 0;
 
+  /* 1/(4fRF) = 0.4913 ns/clock tick, where fRF is the accelerator RF frequency, fRF=508.889 MHz.
+     Same for all crystals.  Proper accurate value*/
+  const double TICKS_TO_NS = 1.0 / (4.0 * EclConfiguration::m_rf) * 1e3;
+
   // Vector of time offsets to track how far from nominal the cluster times are.
   vector<float> t_offsets(8736, 0.0);
   vector<float> t_offsets_unc(8736, 0.0);
+  vector<long> numClusterPerCrys(8736, 0);
+  vector<bool> crysHasGoodFitandStats(8736, false);
+  vector<bool> crysHasGoodFit(8736, false);
+  int numCrysWithNonZeroEntries = 0 ;
+  int numCrysWithGoodFit = 0 ;
 
   int minNumEntries = 40;
 
@@ -165,6 +167,57 @@ CalibrationAlgorithm::EResult eclTValidationAlgorithm::calibrate()
   // * crystal payload updating for iterating crystal and crate fits
   int eventNumberForCrates = 1;
 
+  //-------------------------------------------------------------------
+  /* Uploading ts payload for the older set of runs,
+     hopefully a previous "bucket" worth of runs. */
+  int olderBucketRunNum = 0;
+  int olderBucketExpNum = 0;
+  if (minRunNum > 0) {
+    olderBucketRunNum = minRunNum - 1 ;
+    olderBucketExpNum = minExpNum ;
+  } else if (minExpNum > 0) {
+    olderBucketExpNum = minExpNum - 1 ;
+    olderBucketRunNum = 5000;
+  }
+
+
+  StoreObjPtr<EventMetaData> evtPtrOlderRuns;
+  // simulate the initialize() phase where we can register objects in the DataStore
+  DataStore::Instance().setInitializeActive(true);
+  evtPtrOlderRuns.registerInDataStore();
+  DataStore::Instance().setInitializeActive(false);
+  // now construct the event metadata
+  evtPtrOlderRuns.construct(eventNumberForCrates, olderBucketRunNum, olderBucketExpNum);
+  // and update the database contents
+  DBStore& dbstoreOlderRuns = DBStore::Instance();
+  dbstoreOlderRuns.update();
+  // this is only needed it the payload might be intra-run dependent,
+  // that is if it might change during one run as well
+  dbstoreOlderRuns.updateEvent();
+
+
+  B2INFO("For previous bucket comparisons, uploading payload for exp " << olderBucketExpNum << ", run " << olderBucketRunNum <<
+         ", event " << eventNumberForCrates);
+  updateDBObjPtrs(eventNumberForCrates, olderBucketRunNum, olderBucketExpNum);
+
+  //------------------------------------------------------------------------
+  //..Read payloads from database
+  DBObjPtr<Belle2::ECLCrystalCalib> crystalTimeOlderRunsObject("ECLCrystalTimeOffset");
+  B2INFO("Dumping payload");
+
+  //..Get vectors of values from the payloads
+  std::vector<float> olderRunsCrys = crystalTimeOlderRunsObject->getCalibVector();
+  std::vector<float> olderRunsUncCrys = crystalTimeOlderRunsObject->getCalibUncVector();
+
+  //..Print out a few values for quality control
+  B2INFO("Values read from database for the older runs.");
+  for (int ic = 0; ic < 8736; ic += 500) {
+    B2INFO("ts: cellID " << ic + 1 << " " << olderRunsCrys[ic] << " +/- " << olderRunsUncCrys[ic]);
+  }
+
+
+  //-------------------------------------------------------------------
+  /* Uploading older payloads for the current set of runs */
 
   StoreObjPtr<EventMetaData> evtPtr;
   // simulate the initialize() phase where we can register objects in the DataStore
@@ -185,6 +238,22 @@ CalibrationAlgorithm::EResult eclTValidationAlgorithm::calibrate()
   updateDBObjPtrs(eventNumberForCrates, minRunNum, minExpNum);
   unique_ptr<ECLChannelMapper> crystalMapper(new ECL::ECLChannelMapper());
   crystalMapper->initFromDB();
+
+
+  //------------------------------------------------------------------------
+  //..Read payloads from database
+  DBObjPtr<Belle2::ECLCrystalCalib> crystalTimeObject("ECLCrystalTimeOffset");
+  B2INFO("Dumping payload");
+
+  //..Get vectors of values from the payloads
+  std::vector<float> currentValuesCrys = crystalTimeObject->getCalibVector();
+  std::vector<float> currentUncCrys = crystalTimeObject->getCalibUncVector();
+
+  //..Print out a few values for quality control
+  B2INFO("Values read from database.  Write out for their values for comparison against those from tcol");
+  for (int ic = 0; ic < 8736; ic += 500) {
+    B2INFO("ts: cellID " << ic + 1 << " " << currentValuesCrys[ic] << " +/- " << currentUncCrys[ic]);
+  }
 
 
   //------------------------------------------------------------------------
@@ -219,6 +288,17 @@ CalibrationAlgorithm::EResult eclTValidationAlgorithm::calibrate()
   auto peakClusterTime_cid = new TH1F("peakClusterTime_cid", ";cell id;Peak cluster time [ns]", 8736, 1, 8736 + 1);
   auto peakClusterTimes = new TH1F("peakClusterTimes",
                                    "-For crystals with at least one hit-;Peak cluster time [ns];Number of crystals", hist_nTbins, hist_tmin, hist_tmax);
+
+
+  /* The new crystal calibration contant minus the values from a previous set of runs,
+     that should have a different set of payloads. */
+  auto tsNew_MINUS_tsOlderRuns__cid = new TH1F("tsNew_MINUS_tsOlderRuns__cid", ";cell id; ts(new) - ts(older runs)  [ns]", 8736, 1,
+                                               8736 + 1);
+  auto tsNew_MINUS_tsOlderRuns = new TH1F("tsNew_MINUS_tsOlderRuns", ";ts(new) - ts(older runs)  [ns];Number of crystals", 201,
+                                          -10.05, 10.05);
+
+
+
 
   /* CRYSTAL BY CRYSTAL VALIDATION */
 
@@ -370,6 +450,8 @@ CalibrationAlgorithm::EResult eclTValidationAlgorithm::calibrate()
 
     } else {
       good_fit = true;
+      numCrysWithGoodFit++;
+      crysHasGoodFit[crys_id - 1] = true ;
     }
 
 
@@ -379,6 +461,7 @@ CalibrationAlgorithm::EResult eclTValidationAlgorithm::calibrate()
     if ((numEntries >= minNumEntries)  &&  good_fit) {
       clusterTime_mean = fit_mean;
       clusterTime_mean_unc = fit_mean_unc;
+      crysHasGoodFitandStats[crys_id - 1] = true ;
     } else {
       clusterTime_mean = default_mean;
       clusterTime_mean_unc = default_mean_unc;
@@ -388,9 +471,9 @@ CalibrationAlgorithm::EResult eclTValidationAlgorithm::calibrate()
     if (numEntries == 0)   B2INFO("Number of entries == 0");
 
 
-    // For the database, convert back from ns to ADC ticks.
     t_offsets[crys_id - 1] = clusterTime_mean ;
     t_offsets_unc[crys_id - 1] = clusterTime_mean_unc ;
+    numClusterPerCrys[crys_id - 1] = numEntries ;
 
     histfile->WriteTObject(h_time, (std::string("h_time_psi") + std::to_string(crys_id)).c_str());
     histfile->WriteTObject(h_timeMasked, (std::string("h_time_psi_masked") + std::to_string(crys_id)).c_str());
@@ -399,10 +482,11 @@ CalibrationAlgorithm::EResult eclTValidationAlgorithm::calibrate()
     peakClusterTime_cid->SetBinContent(crys_id, t_offsets[crys_id - 1]);
     peakClusterTime_cid->SetBinError(crys_id, t_offsets_unc[crys_id - 1]);
 
-    /* store mean cluster time info in a separate histogram but only if there is at
+    /* Store mean cluster time info in a separate histogram but only if there is at
        least one entry for that crystal.  */
     if (numEntries > 0) {
       peakClusterTimes->Fill(t_offsets[crys_id - 1]);
+      numCrysWithNonZeroEntries++ ;
     }
 
 
@@ -413,8 +497,6 @@ CalibrationAlgorithm::EResult eclTValidationAlgorithm::calibrate()
 
   histfile->WriteTObject(peakClusterTime_cid, "peakClusterTime_cid");
   histfile->WriteTObject(peakClusterTimes, "peakClusterTimes");
-
-
 
 
 
@@ -609,8 +691,9 @@ CalibrationAlgorithm::EResult eclTValidationAlgorithm::calibrate()
 
 
     int numEntries = h_time->GetEntries();
-    // If number of entries in histogram is greater than X then use the statistical information from the data otherwise leave crystal uncalibrated.  Histograms are still shown though.
-    //  ALSO require the that fits are good.
+    /* If number of entries in histogram is greater than X then use the statistical information
+       from the data otherwise leave crystal uncalibrated.  Histograms are still shown though.
+       ALSO require the that fits are good. */
     if ((numEntries >= minNumEntries)  &&  good_fit) {
       clusterTime_mean = fit_mean;
       clusterTime_mean_unc = fit_mean_unc;
@@ -636,15 +719,111 @@ CalibrationAlgorithm::EResult eclTValidationAlgorithm::calibrate()
 
     Ebin_counter++;
 
-
     delete gaus;
   }
+
+
+
+  /***************************************************************************
+  For the user, print out some information about the peak cluster times.
+  It is sorted by the absolute value of the peak cluster time so that the
+  worst times are at the end.
+  ***************************************************************************/
+
+  // Vector to store element with respective present index
+  vector< pair<double, int> > fitClusterTime__crystalIDBase0__pairs;
+
+  // Prepare a vector of pairs containing the fitted cluster time and cell ID (base 0)
+  for (int cid = 0; cid < 8736; cid++) {
+    fitClusterTime__crystalIDBase0__pairs.push_back(make_pair(0.0, cid));
+  }
+
+  // Inserting element in pair vector to keep track of crystal id.
+  for (int crys_id = cellIDLo; crys_id <= cellIDHi; crys_id++) {
+    fitClusterTime__crystalIDBase0__pairs[crys_id - 1] = make_pair(fabs(t_offsets[crys_id - 1]), crys_id - 1) ;
+  }
+
+  // Sorting by the absolute value of the fitted cluster time for the crystal
+  sort(fitClusterTime__crystalIDBase0__pairs.begin(), fitClusterTime__crystalIDBase0__pairs.end());
+
+
+  // Print out the fitted peak cluster time values sorted by their absolute value
+  B2INFO("-------- List of the (fitted) peak cluster times sorted by their absolute value ----------");
+  B2INFO("------------------------------------------------------------------------------------------");
+  B2INFO("------------------------------------------------------------------------------------------");
+  B2INFO("Quoted # of clusters is before the cutting off of the distribution tails, crysID=1..8736");
+
+  bool hasHitThresholdBadTimes = false ;
+  for (int iSortedTimes = 0; iSortedTimes < 8736; iSortedTimes++) {
+    int cid = fitClusterTime__crystalIDBase0__pairs[iSortedTimes].second ;
+    if (!hasHitThresholdBadTimes && fitClusterTime__crystalIDBase0__pairs[iSortedTimes].first > 2) {
+      B2INFO("======== |t_fit| > Xns threshold ======");
+      hasHitThresholdBadTimes = true;
+    }
+    //B2INFO("crystal ID = " << cid << ", peak clust t = " << t_offsets[cid] << " +- " << t_offsets_unc[cid] << ", # clusters = " << numClusterPerCrys[cid] << ", fabs(t) = " << fitClusterTime__crystalIDBase0__pairs[iSortedTimes].first );
+    B2INFO("crysID = " << cid << ", peak clust t = " << t_offsets[cid] << " +- " << t_offsets_unc[cid] << " ns, # clust = " <<
+           numClusterPerCrys[cid] << ", good fit = " << crysHasGoodFit[cid] << ", good fit & stats = " << crysHasGoodFitandStats[cid]);
+  }
+
+
+
+
+
+  // Print out just a subset that definitely don't look good even though they have good stats.
+  B2INFO("######## List of poor (fitted) peak cluster times sorted by their absolute value #########");
+  B2INFO("##########################################################################################");
+  B2INFO("##########################################################################################");
+
+  for (int iSortedTimes = 0; iSortedTimes < 8736; iSortedTimes++) {
+    int cid = fitClusterTime__crystalIDBase0__pairs[iSortedTimes].second ;
+    if (fitClusterTime__crystalIDBase0__pairs[iSortedTimes].first > 2  && crysHasGoodFitandStats[cid]) {
+      B2INFO("WARNING: crysID = " << cid << ", peak clust t = " << t_offsets[cid] << " +- " << t_offsets_unc[cid] << " ns, # clust = " <<
+             numClusterPerCrys[cid] << ", good fit = " << crysHasGoodFit[cid] << ", good fit & stats = " << crysHasGoodFitandStats[cid]);
+    }
+  }
+
+
+
+
+  B2INFO("~~~~~~~~");
+  B2INFO("Number of crystals with non-zero number of hits = " << numCrysWithNonZeroEntries);
+  B2INFO("Number of crystals with good quality fits = " << numCrysWithGoodFit);
+
 
   clusterTimePeak_ClusterEnergy_varBin->ResetStats();
   clusterTimePeakWidth_ClusterEnergy_varBin->ResetStats();
 
   histfile->WriteTObject(clusterTimePeak_ClusterEnergy_varBin, "clusterTimePeak_ClusterEnergy_varBin");
   histfile->WriteTObject(clusterTimePeakWidth_ClusterEnergy_varBin, "clusterTimePeakWidth_ClusterEnergy_varBin");
+
+
+  /* Make plots of how much the crystal calibration constants changed
+     beteen the current values and those of a previous run. */
+
+  for (int crys_id = 1; crys_id <= 8736; crys_id++) {
+
+    /* Fill histograms with the difference in the ts values between this
+       calibration of tho ts values of some previous runs. */
+    double tsDiffPreRuns_ns = (currentValuesCrys[crys_id - 1] - olderRunsCrys[crys_id - 1]) * TICKS_TO_NS;
+    B2INFO("Crystal " << crys_id << ": ts new - older runs = " << tsDiffPreRuns_ns << " ns");
+    tsNew_MINUS_tsOlderRuns__cid->SetBinContent(crys_id, tsDiffPreRuns_ns);
+    tsNew_MINUS_tsOlderRuns__cid->SetBinError(crys_id, 0);
+
+    tsNew_MINUS_tsOlderRuns->Fill(tsDiffPreRuns_ns);
+  }
+  tsNew_MINUS_tsOlderRuns__cid->ResetStats();
+  tsNew_MINUS_tsOlderRuns->ResetStats();
+
+
+  string expRunDiff = "__e" + to_string(minExpNum) +
+                      "r" + to_string(minRunNum) + "-"
+                      "e" + to_string(olderBucketExpNum) +
+                      "r" + to_string(olderBucketRunNum) ;
+
+  // Save the histograms to the output root file
+  histfile->WriteTObject(tsNew_MINUS_tsOlderRuns__cid, (std::string("tsNew_MINUS_tsOlderRuns__cid") + expRunDiff).c_str());
+  histfile->WriteTObject(tsNew_MINUS_tsOlderRuns, (std::string("tsNew_MINUS_tsOlderRuns") + expRunDiff).c_str());
+
 
   histfile->Close();
 

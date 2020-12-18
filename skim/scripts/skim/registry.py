e6c3705baf113b04b435ef0f8874acad7f169722
lalicata <chiara.lalicata@ipmu.jp>
2020-12-18 16:26:31 +0900

    new dilepton skim added

--- skim/scripts/skim/registry.py
+++ skim/scripts/skim/registry.py
@@ -26,6 +26,7 @@ _RegisteredSkims = [
     # --- WG1: SL + missing energy ---
     ("11110100", "semileptonic", "PRsemileptonicUntagged"),
     ("11130300", "leptonic", "LeptonicUntagged"),
+    ("11130301", "leptonic", "dilepton"),
     ("11160200", "semileptonic", "SLUntagged"),
     ("11180100", "fei", "feiHadronicB0"),
     ("11180200", "fei", "feiHadronicBplus"),

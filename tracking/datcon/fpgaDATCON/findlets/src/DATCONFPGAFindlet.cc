3f4339afd8db683ac18e2deadd7ebbb08c6d92c9
Christian Wessel <christian.wessel@desy.de>
2021-01-19 22:49:23 +0100

    Added the new findlets to the main findlet.

--- tracking/datcon/fpgaDATCON/findlets/src/DATCONFPGAFindlet.cc
+++ tracking/datcon/fpgaDATCON/findlets/src/DATCONFPGAFindlet.cc
@@ -21,7 +21,13 @@ DATCONFPGAFindlet::~DATCONFPGAFindlet() = default;
 
 DATCONFPGAFindlet::DATCONFPGAFindlet()
 {
-  addProcessingSignalListener(&m_clusterLoader);
+  addProcessingSignalListener(&m_digitConverter);
+  addProcessingSignalListener(&m_uClusterizer);
+  addProcessingSignalListener(&m_vClusterizer);
+
+  addProcessingSignalListener(&m_clusterLoaderAndPreparer);
+  addProcessingSignalListener(&m_clusterLoaderAndPreparer2);
+
   addProcessingSignalListener(&m_uInterceptFinder);
   addProcessingSignalListener(&m_vInterceptFinder);
 }
@@ -30,7 +36,13 @@ void DATCONFPGAFindlet::exposeParameters(ModuleParamList* moduleParamList, const
 {
   Super::exposeParameters(moduleParamList, prefix);
 
-  m_clusterLoader.exposeParameters(moduleParamList, prefix);
+  m_digitConverter.exposeParameters(moduleParamList, prefix);
+  m_uClusterizer.exposeParameters(moduleParamList, TrackFindingCDC::prefixed(prefix, "uSide"));
+  m_vClusterizer.exposeParameters(moduleParamList, TrackFindingCDC::prefixed(prefix, "vSide"));
+
+  m_clusterLoaderAndPreparer.exposeParameters(moduleParamList, prefix);
+  m_clusterLoaderAndPreparer2.exposeParameters(moduleParamList, prefix);
+
   m_uInterceptFinder.exposeParameters(moduleParamList, TrackFindingCDC::prefixed(prefix, "uSide"));
   m_vInterceptFinder.exposeParameters(moduleParamList, TrackFindingCDC::prefixed(prefix, "vSide"));
 }
@@ -39,6 +51,10 @@ void DATCONFPGAFindlet::beginEvent()
 {
   Super::beginEvent();
 
+  m_uDigits.clear();
+  m_vDigits.clear();
+  m_uClusters.clear();
+  m_vClusters.clear();
   m_uHits.clear();
   m_vHits.clear();
   m_uTracks.clear();
@@ -48,8 +64,17 @@ void DATCONFPGAFindlet::beginEvent()
 
 void DATCONFPGAFindlet::apply()
 {
-  m_clusterLoader.apply(m_uHits, m_vHits);
-//   B2INFO("m_uHits.size(): " << m_uHits.size() << " m_vHits.size(): " << m_vHits.size());
+  m_digitConverter.apply(m_uDigits, m_vDigits);
+  B2INFO("m_uDigits.size(): " << m_uDigits.size() << " m_vDigits.size(): " << m_vDigits.size());
+
+  m_uClusterizer.apply(m_uDigits, m_uClusters);
+  m_vClusterizer.apply(m_vDigits, m_vClusters);
+  B2INFO("m_uClusters.size(): " << m_uClusters.size() << " m_vClusters.size(): " << m_vClusters.size());
+
+  m_clusterLoaderAndPreparer.apply(m_uHits, m_vHits);
+  B2INFO("1: m_uHits.size(): " << m_uHits.size() << " m_vHits.size(): " << m_vHits.size());
+  m_clusterLoaderAndPreparer2.apply(m_uClusters, m_vClusters, m_uHits, m_vHits);
+  B2INFO("2: m_uHits.size(): " << m_uHits.size() << " m_vHits.size(): " << m_vHits.size());
 
   // hit vectors are empty in case of high occupancy, a warning is created in m_clusterLoader
   if (m_uHits.empty() or m_vHits.empty()) {

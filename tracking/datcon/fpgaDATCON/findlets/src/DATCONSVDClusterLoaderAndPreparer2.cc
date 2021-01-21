c9bfb11a3ada43881d2bf08e595fbe917fd4f1d2
Christian Wessel <christian.wessel@desy.de>
2021-01-21 19:44:54 +0100

    Aslo here, plus added B2DEBUG.

--- tracking/datcon/fpgaDATCON/findlets/src/DATCONSVDClusterLoaderAndPreparer2.cc
+++ tracking/datcon/fpgaDATCON/findlets/src/DATCONSVDClusterLoaderAndPreparer2.cc
@@ -66,9 +66,10 @@ void DATCONSVDClusterLoaderAndPreparer2::apply(std::vector<SVDCluster>& uCluster
     const long y = round(sensorRadius * sinRotAngle + ytmp * cosRotAngle); // y is in µm
     const long radiusSquared = x * x + y * y; // therefore radius is in µm as well, like all other length values
     // x or y divided by r^2 can create values very close to 0. To cope for this and still
-    // have non-zero numbers after conversion and rounding, the conversion to int is made with 10^7
+    // have non-zero numbers after conversion and rounding, the conversion to int is made with 10^10
     std::pair<long, long> pos2D = std::make_pair(convertToInt(2. * (double)x / (double)radiusSquared, 10),
                                                  convertToInt(2. * (double)y / (double)radiusSquared, 10));
+    B2DEBUG(29, "x, y: " << x << " " << y << " Hough-values: " << pos2D.first << " " << pos2D.second);
     uHits.emplace_back(std::make_pair(sensorID, pos2D));
   }
 

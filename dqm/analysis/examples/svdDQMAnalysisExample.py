253a9502b6f1d5528a3c8449280847bf19922f1a
Karol Adamczyk <karol.mateusz.adamczyk@desy.de>
2021-02-01 19:55:25 +0100

    Add number of events as parameter

--- dqm/analysis/examples/svdDQMAnalysisExample.py
+++ dqm/analysis/examples/svdDQMAnalysisExample.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # illustrative run:
-# basf2 svdDQMAnalysisExample.py dqm_e0014r000921.root
+# basf2 svdDQMAnalysisExample.py dqm_e0014r000921.root 1
 
 from basf2 import *
 import sys
@@ -12,12 +12,14 @@ mypath = Path()
 inputFile = sys.argv[1]
 exp_nr = int(re.findall(r'\d+', inputFile)[0])
 run_nr = int(re.findall(r'\d+', inputFile)[1])
+nevt = int(sys.argv[2])  # number of events
 
 inroot = register_module('DQMHistAnalysisInputRootFile')
 inroot.param('FileList', inputFile)
 inroot.param('SelectHistograms', ['SVD*/*'])
 inroot.param('Experiment', exp_nr)
 inroot.param('RunList', [run_nr])
+inroot.param('EventsList', [nevt])
 mypath.add_module(inroot)
 
 dqmSVD = register_module('DQMHistAnalysisSVDOnMiraBelle')
@@ -28,6 +30,7 @@ outroot = register_module('DQMHistAnalysisOutputMonObj')
 outroot.param('ProcID', 'online')  # set processing ID
 outroot.param('exp', exp_nr)
 outroot.param('run', run_nr)
+outroot.param('nevt', nevt)
 mypath.add_module(outroot)
 
 # Process the events

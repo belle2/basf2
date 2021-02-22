e07f1e05f3a04d36a5211c2dd39be698bd08126a
Kirill Chilikin <chilikin@lebedev.ru>
2021-02-22 16:40:14 +0300

    Exclude directories for the intermediate ones, too.

--- site_scons/site_tools/process_dir.py
+++ site_scons/site_tools/process_dir.py
@@ -195,7 +195,15 @@ def process_dir(
 
     # loop over subdirs
     entries = os.listdir(dir_name)
+    exclude_dirs = set()
+    if (os.path.exists('.excluded_directories')):
+        f = open('.excluded_directories', 'r')
+        for line in f.readlines():
+            exclude_dirs.add(line.rstrip('\n'))
+        f.close()
     for entry in entries:
+        if entry in exclude_dirs:
+            continue
         if entry.find('.') == -1 \
             and not os.path.isfile(os.path.join(dir_name, entry)) and entry not in [
             'include',

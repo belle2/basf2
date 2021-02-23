a716ae7bf25663532c180bb476edbdd5cc3a21d2
Christian Wessel <christian.wessel@desy.de>
2021-02-23 19:45:32 +0100

    Added first basic implementation of multi-hit filters.

---
+++ tracking/datcon/optimizedDATCON/filters/hitCombinations/include/ThreeHitFilterFactory.h
@@ -0,0 +1,44 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2021 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Christian Wessel                                         *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#pragma once
+
+// #include <tracking/datcon/optimizedDATCON/filters/hitCombinations/BaseThreeHitFilter.h>
+#include <tracking/datcon/optimizedDATCON/filters/hitCombinations/BaseHitCombinationFilter.h>
+
+#include <tracking/trackFindingCDC/filters/base/FilterFactory.dcl.h>
+
+namespace Belle2 {
+  /// Factory that can create appropriate cluster filters from associated names.
+  class ThreeHitFilterFactory : public TrackFindingCDC::FilterFactory<BaseThreeHitFilter> {
+
+  private:
+    /// Type of the base class
+    using Super = TrackFindingCDC::FilterFactory<BaseThreeHitFilter>;
+
+  public:
+    /// Constructor forwarding the default filter name
+    explicit ThreeHitFilterFactory(const std::string& defaultFilterName = "all");
+
+    /// Default destructor
+    ~ThreeHitFilterFactory();
+
+    /// Getter for a short identifier for the factory
+    std::string getIdentifier() const override;
+
+    /// Getter for a descriptive purpose of the constructed filters
+    std::string getFilterPurpose() const override;
+
+    /// Getter for valid filter names and a description for each
+    std::map<std::string, std::string> getValidFilterNamesAndDescriptions() const override;
+
+    /// Create a filter with the given name.
+    std::unique_ptr<BaseThreeHitFilter> create(const std::string& filterName) const override;
+  };
+}
---
+++ tracking/datcon/optimizedDATCON/filters/hitCombinations/src/ThreeHitFilterFactory.cc
@@ -0,0 +1,58 @@
+/**************************************************************************
+ * BASF2 (Belle Analysis Framework 2)                                     *
+ * Copyright(C) 2021 - Belle II Collaboration                             *
+ *                                                                        *
+ * Author: The Belle II Collaboration                                     *
+ * Contributors: Christian Wessel                                         *
+ *                                                                        *
+ * This software is provided "as is" without any warranty.                *
+ **************************************************************************/
+#include <tracking/datcon/optimizedDATCON/filters/hitCombinations/ThreeHitFilterFactory.h>
+
+#include <tracking/trackFindingCDC/filters/base/Filter.icc.h>
+#include <tracking/trackFindingCDC/filters/base/FilterFactory.icc.h>
+#include <tracking/trackFindingCDC/filters/base/NoneFilter.icc.h>
+#include <tracking/trackFindingCDC/filters/base/AllFilter.icc.h>
+
+using namespace Belle2;
+using namespace TrackFindingCDC;
+
+
+ThreeHitFilterFactory::ThreeHitFilterFactory(const std::string& defaultFilterName)
+  : Super(defaultFilterName)
+{
+}
+
+ThreeHitFilterFactory::~ThreeHitFilterFactory() = default;
+
+std::string ThreeHitFilterFactory::getIdentifier() const
+{
+  return "SVD Hit Pair";
+}
+
+std::string ThreeHitFilterFactory::getFilterPurpose() const
+{
+  return "Rejects SVD hit pairs. ";
+}
+
+std::map<std::string, std::string> ThreeHitFilterFactory::getValidFilterNamesAndDescriptions() const
+{
+  return {
+    {"all", "all combinations are valid"},
+    {"none", "no combination is valid"},
+  };
+}
+
+std::unique_ptr<BaseThreeHitFilter>
+ThreeHitFilterFactory::create(const std::string& filterName) const
+{
+  if (filterName == "all") {
+    return std::make_unique<TrackFindingCDC::AllFilter<BaseThreeHitFilter>>();
+  }
+
+  if (filterName == "none") {
+    return std::make_unique<TrackFindingCDC::NoneFilter<BaseThreeHitFilter>>();
+  }
+
+  return Super::create(filterName);
+}

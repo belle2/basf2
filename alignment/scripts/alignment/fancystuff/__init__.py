042fa5560954a898b129480ec2627736d1698e38
Phil Grace <philip.grace@adelaide.edu.au>
2021-02-24 10:13:19 +1030

    Replaced relative imports with module imports

--- alignment/scripts/alignment/fancystuff/__init__.py
+++ alignment/scripts/alignment/fancystuff/__init__.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from ._version import get_versions
-from .tools import draw_fancy_correlation_matrix, draw_flat_corr_matrix, draw_flat_correlation
-
-__version__ = get_versions()['version']
-del get_versions
+from alignment.fancystuff.tools import draw_fancy_correlation_matrix, draw_flat_corr_matrix, draw_flat_correlation  # noqa

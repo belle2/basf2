b030411cecbe8054c70b7e449ebf5d2cc6c1eb56
Giacomo De Pietro <giacomo.depietro@roma3.infn.it>
2021-02-24 15:01:30 +0100

    Merge pull request #8074 in B2/software from feature/BII-7460_starterkit_cs to release/05-01
    * commit '7a4bf016752ea2043f04ab1abd77171ca8ce85eb':
      Kilians suggestions and two more plots
      Frank's suggestions. Fixed file locations
      Forgot figures
      Initial commit CS lesson

---
+++ online_book/basf2/steering_files/090_cs.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import basf2 as b2
+import modularAnalysis as ma
+import stdPi0s
+
+# Perform analysis.
+main = b2.create_path()
+
+ma.inputMdstList(
+    environmentType="default",
+    filelist=[
+        b2.find_file("starterkit/2021/B02ks0pi0_sample.root",
+                     data_type="examples"),
+        b2.find_file("starterkit/2021/uubar_sample.root",
+                     data_type="examples"),
+    ],
+    path=main,
+)
+
+stdPi0s.stdPi0s(path=main, listtype="eff60_Jan2020")
+ma.fillParticleList(
+    decayString="pi+:good", cut="chiProb > 0.001 and pionID > 0.5", path=main
+)
+
+ma.reconstructDecay(
+    decayString="K_S0 -> pi+:good pi-:good", cut="0.480<=M<=0.516", path=main
+)
+
+ma.reconstructDecay(
+    decayString="B0  -> K_S0 pi0:eff60_Jan2020",
+    cut="5.1 < Mbc < 5.3 and abs(deltaE) < 2",
+    path=main,
+)
+
+ma.buildRestOfEvent(target_list_name="B0", path=main)
+cleanMask = (
+    "cleanMask",
+    "nCDCHits > 0 and useCMSFrame(p)<=3.2",
+    "p >= 0.05 and useCMSFrame(p)<=3.2",
+)
+ma.appendROEMasks(list_name="B0", mask_tuples=[cleanMask], path=main)
+
+ma.buildContinuumSuppression(list_name="B0", roe_mask="cleanMask", path=main)
+
+simpleCSVariables = [
+    "R2",
+    "thrustBm",
+    "thrustOm",
+    "cosTBTO",
+    "cosTBz",
+]
+
+ma.variablesToNtuple(
+    decayString="B0",
+    variables=simpleCSVariables + ["Mbc", "isContinuumEvent"],
+    filename="ContinuumSuppression.root",
+    treename="tree",
+    path=main,
+)
+
+b2.process(main)

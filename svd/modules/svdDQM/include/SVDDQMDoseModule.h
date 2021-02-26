1608770401d2e22bb9a092508e13fb8469494bd5
Ludovico Massaccesi <l.massaccesi@studenti.unipi.it>
2021-02-26 23:50:41 +0900

    Added instantaneous occupancy over multiple sensors plots.

--- svd/modules/svdDQM/include/SVDDQMDoseModule.h
+++ svd/modules/svdDQM/include/SVDDQMDoseModule.h
@@ -16,9 +16,11 @@
 #include <svd/dataobjects/SVDShaperDigit.h>
 #include <svd/calibration/SVDFADCMaskedStrips.h>
 #include <svd/dataobjects/SVDHistograms.h>
+#include <vxd/dataobjects/VxdID.h>
 #include <TH1.h>
 #include <TH2.h>
 #include <string>
+#include <vector>
 
 namespace Belle2 {
   namespace SVD {
@@ -57,6 +59,83 @@ namespace Belle2 {
       SVDHistograms<TH2F>* m_nHitsVsTime = nullptr;
       /** Hist of the total evts in each time bin (time since inj. and time in cycle). */
       TH2F* h_nEvtsVsTime = nullptr;
+
+      /** Hists of the instantaneous occupancy distribution for L3 U side.
+       *  - `h_occupancyL3[0]`: averaged on all layer 3
+       *  - `h_occupancyL3[1]`: L3.X.1 averaged on ladders
+       *  - `h_occupancyL3[2]`: L3.X.2 averaged on ladders
+       */
+      TH1F* h_occupancyL3U[3] = {0}; // Init to all zeros
+      /** Hists of the instantaneous occupancy distribution for L4 U side.
+       *  - `h_occupancyL4[0]`: averaged on all layer 4
+       *  - `h_occupancyL4[1]`: L4.X.1 averaged on ladders
+       *  - ...
+       *  - `h_occupancyL4[3]`: L4.X.3 averaged on ladders
+       */
+      TH1F* h_occupancyL4U[4] = {0}; // Init to all zeros
+      /** Hists of the instantaneous occupancy distribution for L5 U side.
+       *  - `h_occupancyL4[0]`: averaged on all layer 5
+       *  - `h_occupancyL5[1]`: L5.X.1 averaged on ladders
+       *  - ...
+       *  - `h_occupancyL5[4]`: L5.X.4 averaged on ladders
+       */
+      TH1F* h_occupancyL5U[5] = {0}; // Init to all zeros
+      /** Hists of the instantaneous occupancy distribution for L6 U side.
+       *  - `h_occupancyL6[0]`: averaged on all layer 6
+       *  - `h_occupancyL6[1]`: L6.X.1 averaged on ladders
+       *  - ...
+       *  - `h_occupancyL6[5]`: L6.X.5 averaged on ladders
+       */
+      TH1F* h_occupancyL6U[6] = {0}; // Init to all zeros
+      /** Hists of the instantaneous occupancy distribution for L3 V side.
+       *  - `h_occupancyL3[0]`: averaged on all layer 3
+       *  - `h_occupancyL3[1]`: L3.X.1 averaged on ladders
+       *  - `h_occupancyL3[2]`: L3.X.2 averaged on ladders
+       */
+      TH1F* h_occupancyL3V[3] = {0}; // Init to all zeros
+      /** Hists of the instantaneous occupancy distribution for L4 V side.
+       *  - `h_occupancyL4[0]`: averaged on all layer 4
+       *  - `h_occupancyL4[1]`: L4.X.1 averaged on ladders
+       *  - ...
+       *  - `h_occupancyL4[3]`: L4.X.3 averaged on ladders
+       */
+      TH1F* h_occupancyL4V[4] = {0}; // Init to all zeros
+      /** Hists of the instantaneous occupancy distribution for L5 V side.
+       *  - `h_occupancyL4[0]`: averaged on all layer 5
+       *  - `h_occupancyL5[1]`: L5.X.1 averaged on ladders
+       *  - ...
+       *  - `h_occupancyL5[4]`: L5.X.4 averaged on ladders
+       */
+      TH1F* h_occupancyL5V[5] = {0}; // Init to all zeros
+      /** Hists of the instantaneous occupancy distribution for L6 V side.
+       *  - `h_occupancyL6[0]`: averaged on all layer 6
+       *  - `h_occupancyL6[1]`: L6.X.1 averaged on ladders
+       *  - ...
+       *  - `h_occupancyL6[5]`: L6.X.5 averaged on ladders
+       */
+      TH1F* h_occupancyL6V[6] = {0}; // Init to all zeros
+
+      /** L3 mid plane U-side instant. occupancy average (L3.1.X and L3.2.X). */
+      TH1F* h_occupancyL3Umid = nullptr;
+      /** L4 mid plane U-side instant. occupancy average (L4.6.1 and L4.6.2). */
+      TH1F* h_occupancyL4Umid = nullptr;
+      /** L5 mid plane U-side instant. occupancy average (L5.8.1 and L5.8.2). */
+      TH1F* h_occupancyL5Umid = nullptr;
+      /** L6 mid plane U-side instant. occupancy average (L6.10.1 and L6.10.2). */
+      TH1F* h_occupancyL6Umid = nullptr;
+      /** L3 mid plane V-side instant. occupancy average (L3.1.X and L3.2.X). */
+      TH1F* h_occupancyL3Vmid = nullptr;
+      /** L4 mid plane V-side instant. occupancy average (L4.6.1 and L4.6.2). */
+      TH1F* h_occupancyL4Vmid = nullptr;
+      /** L5 mid plane V-side instant. occupancy average (L5.8.1 and L5.8.2). */
+      TH1F* h_occupancyL5Vmid = nullptr;
+      /** L6 mid plane V-side instant. occupancy average (L6.10.1 and L6.10.2). */
+      TH1F* h_occupancyL6Vmid = nullptr;
+
+      const std::vector<VxdID> c_L3midSensors {VxdID(3, 1, 1), VxdID(3, 1, 2), VxdID(3, 2, 1), VxdID(3, 2, 2)};
+      const std::vector<VxdID> c_L4midSensors {VxdID(4, 6, 1), VxdID(4, 6, 2)};
+      const std::vector<VxdID> c_L5midSensors {VxdID(5, 8, 1), VxdID(5, 8, 2)};
+      const std::vector<VxdID> c_L6midSensors {VxdID(6, 10, 1), VxdID(6, 10, 2)};
     };
   }
 }
--- svd/modules/svdDQM/src/SVDDQMDoseModule.cc
+++ svd/modules/svdDQM/src/SVDDQMDoseModule.cc
@@ -1,6 +1,5 @@
 #include <svd/modules/svdDQM/SVDDQMDoseModule.h>
 #include <vxd/geometry/GeoCache.h>
-#include <vxd/dataobjects/VxdID.h>
 #include <svd/geometry/SensorInfo.h>
 #include "TDirectory.h"
 #include <map>
@@ -49,6 +48,185 @@ void SVDDQMDoseModule::defineHisto()
     "SVDEvtsVsInjTime2",
     "SVD Events;Time since last injection [#mus];Time in beam cycle [#mus];Events / bin",
     500, 0, 30e3, 100, 0, m_revolutionTime);
+
+  h_occupancyL3U[0] = new TH1F(
+    "SVDInstOccupancy_L3P",
+    "SVD Instantaneous Occupancy L3 (all) P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL3U[1] = new TH1F(
+    "SVDInstOccupancy_L3X1P",
+    "SVD Instantaneous Occupancy L3.X.1 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL3U[2] = new TH1F(
+    "SVDInstOccupancy_L3X2P",
+    "SVD Instantaneous Occupancy L3.X.2 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL4U[0] = new TH1F(
+    "SVDInstOccupancy_L4P",
+    "SVD Instantaneous Occupancy L4 (all) P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL4U[1] = new TH1F(
+    "SVDInstOccupancy_L4X1P",
+    "SVD Instantaneous Occupancy L4.X.1 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL4U[2] = new TH1F(
+    "SVDInstOccupancy_L4X2P",
+    "SVD Instantaneous Occupancy L4.X.2 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL4U[3] = new TH1F(
+    "SVDInstOccupancy_L4X3P",
+    "SVD Instantaneous Occupancy L4.X.3 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL5U[0] = new TH1F(
+    "SVDInstOccupancy_L5P",
+    "SVD Instantaneous Occupancy L5 (all) P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL5U[1] = new TH1F(
+    "SVDInstOccupancy_L5X1P",
+    "SVD Instantaneous Occupancy L5.X.1 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL5U[2] = new TH1F(
+    "SVDInstOccupancy_L5X2P",
+    "SVD Instantaneous Occupancy L5.X.2 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL5U[3] = new TH1F(
+    "SVDInstOccupancy_L5X3P",
+    "SVD Instantaneous Occupancy L5.X.3 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL5U[4] = new TH1F(
+    "SVDInstOccupancy_L5X4P",
+    "SVD Instantaneous Occupancy L5.X.4 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6U[0] = new TH1F(
+    "SVDInstOccupancy_L6P",
+    "SVD Instantaneous Occupancy L6 (all) P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6U[1] = new TH1F(
+    "SVDInstOccupancy_L6X1P",
+    "SVD Instantaneous Occupancy L6.X.1 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6U[2] = new TH1F(
+    "SVDInstOccupancy_L6X2P",
+    "SVD Instantaneous Occupancy L6.X.2 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6U[3] = new TH1F(
+    "SVDInstOccupancy_L6X3P",
+    "SVD Instantaneous Occupancy L6.X.3 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6U[4] = new TH1F(
+    "SVDInstOccupancy_L6X4P",
+    "SVD Instantaneous Occupancy L6.X.4 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6U[5] = new TH1F(
+    "SVDInstOccupancy_L6X5P",
+    "SVD Instantaneous Occupancy L6.X.5 P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+
+  h_occupancyL3V[0] = new TH1F(
+    "SVDInstOccupancy_L3N",
+    "SVD Instantaneous Occupancy L3 (all) N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL3V[1] = new TH1F(
+    "SVDInstOccupancy_L3X1N",
+    "SVD Instantaneous Occupancy L3.X.1 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL3V[2] = new TH1F(
+    "SVDInstOccupancy_L3X2N",
+    "SVD Instantaneous Occupancy L3.X.2 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL4V[0] = new TH1F(
+    "SVDInstOccupancy_L4N",
+    "SVD Instantaneous Occupancy L4 (all) N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL4V[1] = new TH1F(
+    "SVDInstOccupancy_L4X1N",
+    "SVD Instantaneous Occupancy L4.X.1 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL4V[2] = new TH1F(
+    "SVDInstOccupancy_L4X2N",
+    "SVD Instantaneous Occupancy L4.X.2 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL4V[3] = new TH1F(
+    "SVDInstOccupancy_L4X3N",
+    "SVD Instantaneous Occupancy L4.X.3 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL5V[0] = new TH1F(
+    "SVDInstOccupancy_L5N",
+    "SVD Instantaneous Occupancy L5 (all) N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL5V[1] = new TH1F(
+    "SVDInstOccupancy_L5X1N",
+    "SVD Instantaneous Occupancy L5.X.1 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL5V[2] = new TH1F(
+    "SVDInstOccupancy_L5X2N",
+    "SVD Instantaneous Occupancy L5.X.2 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL5V[3] = new TH1F(
+    "SVDInstOccupancy_L5X3N",
+    "SVD Instantaneous Occupancy L5.X.3 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL5V[4] = new TH1F(
+    "SVDInstOccupancy_L5X4N",
+    "SVD Instantaneous Occupancy L5.X.4 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6V[0] = new TH1F(
+    "SVDInstOccupancy_L6N",
+    "SVD Instantaneous Occupancy L6 (all) N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6V[1] = new TH1F(
+    "SVDInstOccupancy_L6X1N",
+    "SVD Instantaneous Occupancy L6.X.1 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6V[2] = new TH1F(
+    "SVDInstOccupancy_L6X2N",
+    "SVD Instantaneous Occupancy L6.X.2 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6V[3] = new TH1F(
+    "SVDInstOccupancy_L6X3N",
+    "SVD Instantaneous Occupancy L6.X.3 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6V[4] = new TH1F(
+    "SVDInstOccupancy_L6X4N",
+    "SVD Instantaneous Occupancy L6.X.4 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6V[5] = new TH1F(
+    "SVDInstOccupancy_L6X5N",
+    "SVD Instantaneous Occupancy L6.X.5 N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+
+  h_occupancyL3Umid = new TH1F(
+    "SVDInstOccupancy_L3Pmid",
+    "SVD Instantaneous Occupancy L3 mid plane P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL4Umid = new TH1F(
+    "SVDInstOccupancy_L4Pmid",
+    "SVD Instantaneous Occupancy L4 mid plane P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL5Umid = new TH1F(
+    "SVDInstOccupancy_L5Pmid",
+    "SVD Instantaneous Occupancy L5 mid plane P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6Umid = new TH1F(
+    "SVDInstOccupancy_L6Pmid",
+    "SVD Instantaneous Occupancy L6 mid plane P-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL3Vmid = new TH1F(
+    "SVDInstOccupancy_L3Nmid",
+    "SVD Instantaneous Occupancy L3 mid plane N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL4Vmid = new TH1F(
+    "SVDInstOccupancy_L4Nmid",
+    "SVD Instantaneous Occupancy L4 mid plane N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL5Vmid = new TH1F(
+    "SVDInstOccupancy_L5Nmid",
+    "SVD Instantaneous Occupancy L5 mid plane N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
+  h_occupancyL6Vmid = new TH1F(
+    "SVDInstOccupancy_L6Nmid",
+    "SVD Instantaneous Occupancy L6 mid plane N-side;Occupancy [%];Count / (0.05%)",
+    100, 0, 5);
 
   oldDir->cd();
 }
@@ -106,13 +284,88 @@ void SVDDQMDoseModule::event()
   // Compute instantaneous occupancy
   VXD::GeoCache& geo = VXD::GeoCache::getInstance();
   for (const auto& layer : geo.getLayers(VXD::SensorInfoBase::SVD)) {
+    // Total hits [0] in the layer and [i] in LL.X.i (see h_occupancyL*)
+    int layerHitsU[layer.getLayerNumber()] = {0};
+    int layerHitsV[layer.getLayerNumber()] = {0};
     for (const auto& ladder : geo.getLadders(layer)) {
       for (const auto& sensor : geo.getSensors(ladder)) {
-        // const auto& sInfo = dynamic_cast<const SVD::SensorInfo&>(VXD::GeoCache::get(sensor));
         const auto& sInfo = VXD::GeoCache::get(sensor);
-        m_occupancy->fill(sensor, true, hitsU[sensor] * 100.0 / sInfo.getUCells());
-        m_occupancy->fill(sensor, false, hitsV[sensor] * 100.0 / sInfo.getVCells());
+        int hU = hitsU[sensor];
+        int hV = hitsV[sensor];
+        m_occupancy->fill(sensor, true, hU * 100.0 / sInfo.getUCells());
+        m_occupancy->fill(sensor, false, hV * 100.0 / sInfo.getVCells());
+        layerHitsU[0] += hU;
+        layerHitsV[0] += hV;
+        layerHitsU[sensor.getSensorNumber()] += hU;
+        layerHitsV[sensor.getSensorNumber()] += hV;
       }
     }
+    switch (layer.getLayerNumber()) {
+      case 3:
+        h_occupancyL3U[0]->Fill(layerHitsU[0] * 100.0 / 14.0 / 768.0);
+        h_occupancyL3V[0]->Fill(layerHitsV[0] * 100.0 / 14.0 / 768.0);
+        for (int i = 1; i <= 2; i++) {
+          h_occupancyL3U[i]->Fill(layerHitsU[i] * 100.0 / 7.0 / 768.0);
+          h_occupancyL3V[i]->Fill(layerHitsV[i] * 100.0 / 7.0 / 768.0);
+        }
+        break;
+      case 4:
+        h_occupancyL4U[0]->Fill(layerHitsU[0] * 100.0 / 30.0 / 768.0);
+        h_occupancyL4V[0]->Fill(layerHitsV[0] * 100.0 / 30.0 / 512.0);
+        for (int i = 1; i <= 3; i++) {
+          h_occupancyL4U[i]->Fill(layerHitsU[i] * 100.0 / 10.0 / 768.0);
+          h_occupancyL4V[i]->Fill(layerHitsV[i] * 100.0 / 10.0 / 512.0);
+        }
+        break;
+      case 5:
+        h_occupancyL5U[0]->Fill(layerHitsU[0] * 100.0 / 48.0 / 768.0);
+        h_occupancyL5V[0]->Fill(layerHitsV[0] * 100.0 / 48.0 / 512.0);
+        for (int i = 1; i <= 4; i++) {
+          h_occupancyL5U[i]->Fill(layerHitsU[i] * 100.0 / 12.0 / 768.0);
+          h_occupancyL5V[i]->Fill(layerHitsV[i] * 100.0 / 12.0 / 512.0);
+        }
+        break;
+      case 6:
+        h_occupancyL6U[0]->Fill(layerHitsU[0] * 100.0 / 80.0 / 768.0);
+        h_occupancyL6V[0]->Fill(layerHitsV[0] * 100.0 / 80.0 / 512.0);
+        for (int i = 1; i <= 5; i++) {
+          h_occupancyL6U[i]->Fill(layerHitsU[i] * 100.0 / 16.0 / 768.0);
+          h_occupancyL6V[i]->Fill(layerHitsV[i] * 100.0 / 16.0 / 512.0);
+        }
+        break;
+    }
   }
+
+  // Mid plane occupancies
+  int midHitsU = 0, midHitsV = 0;
+  for (const VxdID& id : c_L3midSensors) {
+    midHitsU += hitsU[id];
+    midHitsV += hitsV[id];
+  }
+  h_occupancyL3Umid->Fill(midHitsU * 100.0 / 768.0 / c_L3midSensors.size());
+  h_occupancyL3Vmid->Fill(midHitsV * 100.0 / 768.0 / c_L3midSensors.size());
+
+  midHitsU = midHitsV = 0;
+  for (const VxdID& id : c_L4midSensors) {
+    midHitsU += hitsU[id];
+    midHitsV += hitsV[id];
+  }
+  h_occupancyL4Umid->Fill(midHitsU * 100.0 / 768.0 / c_L4midSensors.size());
+  h_occupancyL4Vmid->Fill(midHitsV * 100.0 / 512.0 / c_L4midSensors.size());
+
+  midHitsU = midHitsV = 0;
+  for (const VxdID& id : c_L5midSensors) {
+    midHitsU += hitsU[id];
+    midHitsV += hitsV[id];
+  }
+  h_occupancyL5Umid->Fill(midHitsU * 100.0 / 768.0 / c_L5midSensors.size());
+  h_occupancyL5Vmid->Fill(midHitsV * 100.0 / 512.0 / c_L5midSensors.size());
+
+  midHitsU = midHitsV = 0;
+  for (const VxdID& id : c_L6midSensors) {
+    midHitsU += hitsU[id];
+    midHitsV += hitsV[id];
+  }
+  h_occupancyL6Umid->Fill(midHitsU * 100.0 / 768.0 / c_L6midSensors.size());
+  h_occupancyL6Vmid->Fill(midHitsV * 100.0 / 512.0 / c_L6midSensors.size());
 }

2314076bda54d1977d045a09801974092aa8c35f
Seokhee Park <seokhee.park@kek.jp>
2021-03-05 16:36:15 +0900

    For test runtype

--- daq/slc/database/src/RunNumberTable.cc
+++ daq/slc/database/src/RunNumberTable.cc
@@ -26,11 +26,10 @@ RunNumber RunNumberTable::add(const std::string& node,
       return RunNumber();
     }
   } else if (node == "global") {
-    if (runtype != "physics" && runtype != "cosmic" &&
-        runtype != "beam" && runtype != "null" && runtype != "debug") {
-      LogFile::fatal("wrong node name or runtype : node='%s' runtype='%s'!=physics/cosmic/beam/debug/null", node.c_str(),
+    if (runtype != "physics" && runtype != "cosmic" && runtype != "beam" &&
+        runtype != "null" && runtype != "debug" && runtype != "test") {
+      LogFile::fatal("wrong node name or runtype : node='%s' runtype='%s'!=physics/cosmic/beam/debug/test/null", node.c_str(),
                      runtype.c_str());
-      return RunNumber();
     }
   } else {
     LogFile::fatal("wrong node name : node='%s'!=global/pxd/svd/cdc/top/arich/ecl/klm/trg/b4test runtype='%s'", node.c_str(),

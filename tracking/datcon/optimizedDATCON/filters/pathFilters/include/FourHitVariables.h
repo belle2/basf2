91acb88a5378e2daa4e1c4c85801c5207a492d1e
Christian Wessel <christian.wessel@desy.de>
2021-03-22 18:02:31 +0100

    Deleted some unused code and added option to set the BField for the variable calcualtors.

--- tracking/datcon/optimizedDATCON/filters/pathFilters/include/FourHitVariables.h
+++ tracking/datcon/optimizedDATCON/filters/pathFilters/include/FourHitVariables.h
@@ -102,12 +102,13 @@ namespace Belle2 {
       double outerCircleRadius = m_outerThreeHitVariables.calcAvgDistanceXY(outerCircleCenter);
       double innerCircleRadius = m_innerThreeHitVariables.calcAvgDistanceXY(innerCircleCenter);
 
-      // 0.3 * B * R, but with R in cm instead of m -> (0.3 -> 0.003)
-      // 0.00299792458 * 1.5 = 0.00449688687
-      return fabs(0.00449688687 * (outerCircleRadius - innerCircleRadius));
+      return fabs(0.00299792458 * m_BFieldZ * (outerCircleRadius - innerCircleRadius));
     } // return unit: GeV/c
 
+    void setBFieldZ(const double bfieldZ = 1.5) { m_BFieldZ = bfieldZ; }
+
   private:
+    double m_BFieldZ = 1.5;
     B2Vector3D m_oHit;
     B2Vector3D m_ocHit;
     B2Vector3D m_icHit;
@@ -118,66 +119,3 @@ namespace Belle2 {
   };
 
 }
-
-// namespace Belle2 {
-//
-//   /** calculates dpt-value (dpt= difference in transverse momentum of 2 subsets of the hits), returning unit: cm */
-//   /** TODO: a straight line exception is thrown and not catched (at least here) in calcCircleCenter */
-//   static double getCircleRadiusDifference(const B2Vector3D& outerHit, const B2Vector3D& outerCenterHit,
-//                                    const B2Vector3D& innerCenterHit, const B2Vector3D& innerHit)
-//   {
-//     B2Vector3D outerCircleCenter = getCircleCenterXY(outerHit, outerCenterHit, innerCenterHit);
-//     if (outerCircleCenter.Perp2() > 1e30) {
-//       return NAN;
-//     }
-//     double outerCircleRadius = calcAvgDistanceXY(outerHit, outerCenterHit, innerCenterHit, outerCircleCenter);
-//
-//     B2Vector3D innerCircleCenter = getCircleCenterXY(outerCenterHit, innerCenterHit, innerHit);
-//     if (innerCircleCenter.Perp2() > 1e30) {
-//       return NAN;
-//     }
-//     double innerCircleRadius = calcAvgDistanceXY(outerCenterHit, innerCenterHit, innerHit, innerCircleCenter);
-//
-//     return outerCircleRadius - innerCircleRadius;
-//   } // return unit: cm
-//
-//
-//   /** calculates the distance between the estimated circle centers (using 2 subsets of given hits) in the xy-plane, returning unit: cm */
-//   static double getCircleCenterPositionDifference(const B2Vector3D& outerHit, const B2Vector3D& outerCenterHit,
-//                                            const B2Vector3D& innerCenterHit, const B2Vector3D& innerHit)
-//   {
-//     B2Vector3D outerCircleCenter = getCircleCenterXY(outerHit, outerCenterHit, innerCenterHit);
-//     if (outerCircleCenter.Perp2() > 1e30) {
-//       return NAN;
-//     }
-//     B2Vector3D innerCircleCenter = getCircleCenterXY(outerCenterHit, innerCenterHit, innerHit);
-//     if (innerCircleCenter.Perp2() > 1e30) {
-//       return NAN;
-//     }
-//
-//     return fabs(outerCircleCenter.Perp() - innerCircleCenter.Perp());
-//   } // return unit: GeV/c
-//
-//
-//   /** calculates dpt-value (dpt= difference in transverse momentum of 2 subsets of the hits), returning unit: GeV/c */
-//   static double getDeltaPT(const B2Vector3D& outerHit, const B2Vector3D& outerCenterHit,
-//                     const B2Vector3D& innerCenterHit, const B2Vector3D& innerHit)
-//   {
-//     B2Vector3D outerCircleCenter = getCircleCenterXY(outerHit, outerCenterHit, innerCenterHit);
-//     if (outerCircleCenter.Perp2() > 1e30) {
-//       return NAN;
-//     }
-//     double outerCircleRadius = calcAvgDistanceXY(outerHit, outerCenterHit, innerCenterHit, outerCircleCenter);
-//
-//     B2Vector3D innerCircleCenter = getCircleCenterXY(outerCenterHit, innerCenterHit, innerHit);
-//     if (innerCircleCenter.Perp2() > 1e30) {
-//       return NAN;
-//     }
-//     double innerCircleRadius = calcAvgDistanceXY(outerCenterHit, innerCenterHit, innerHit, innerCircleCenter);
-//
-//     // 0.3 * B * R, but with R in cm instead of m -> (0.3 -> 0.003)
-//     // 0.00299792458 * 1.5 = 0.00449688687
-//     return fabs(0.00449688687 * (outerCircleRadius - innerCircleRadius));
-//   } // return unit: GeV/c
-//
-// }
--- tracking/datcon/optimizedDATCON/filters/pathFilters/include/TwoHitVariables.h
+++ tracking/datcon/optimizedDATCON/filters/pathFilters/include/TwoHitVariables.h
@@ -74,70 +74,12 @@ namespace Belle2 {
       return outerSpacePoint.TimeV() - innerSpacePoint.TimeV();
     }
 
+    void setBFieldZ(const double bfieldZ = 1.5) { m_BFieldZ = bfieldZ; }
+
   private:
+    double m_BFieldZ = 1.5;
     B2Vector3D m_oHit;
     B2Vector3D m_iHit;
 
   };
 }
-
-
-// namespace Belle2 {
-//
-//   static double getCosXY(const B2Vector3D& oHit, const B2Vector3D& iHit) {
-//     return oHit.Unit() * iHit.Unit();
-//   }
-//
-//   static double get1DZDistance(const B2Vector3D& oHit, const B2Vector3D& iHit) {
-//     return oHit.Z() - iHit.Z();
-//   }
-//
-//   static double get1DZDistanceSquared(const B2Vector3D& oHit, const B2Vector3D& iHit) {
-//     return (oHit.Z() - iHit.Z()) * (oHit.Z() - iHit.Z());
-//   }
-//
-//   static double get2DXYDistanceSquared(const B2Vector3D& oHit, const B2Vector3D& iHit) {
-// //     return (oHit.X() - iHit.X()) * (oHit.X() - iHit.X()) + (oHit.Y() - iHit.Y()) * (oHit.Y() - iHit.Y());
-//     return (oHit - iHit).Perp2();
-//   }
-//
-// // // //   double get3DNormedXYDistance(const B2Vector3D& oHit, const B2Vector3D& iHit) {
-// // // //     double result =   (oHit - iHit).Perp2()
-// // // //       /
-// // // //       (std::pow(oHit.X() - iHit.X() , 2)
-// // // //         + std::pow(oHit.Y() - iHit.Y() , 2)
-// // // //         + std::pow(oHit.Z() - iHit.Z() , 2));
-// // // //
-// // // //     return
-// // // //       (std::isnan(result) || std::isinf(result)) ? 0 : result;
-// // // //   }
-//
-//   static double get3DDistanceSquared(const B2Vector3D& oHit, const B2Vector3D& iHit) {
-// //     return  ((oHit.X() - iHit.X()) * (oHit.X() - iHit.X())) +
-// //             ((oHit.Y() - iHit.Y()) * (oHit.Y() - iHit.Y())) +
-// //             ((oHit.Z() - iHit.Z()) * (oHit.Z() - iHit.Z()));
-//     return (oHit - iHit).Mag2();
-//   }
-//
-//   static double getRZSlope(const B2Vector3D& oHit, const B2Vector3D& iHit) {
-//     double result = atan(
-//                       sqrt(std::pow(double(oHit.X() - iHit.X()), 2)
-//                            + std::pow(double(oHit.Y() - iHit.Y()), 2)
-//                           ) / double(oHit.Z() - iHit.Z())
-//                     );
-//     // atan also returns negative angles, so map back to [0,Pi] otherwise one get two peaks at +/-Pi/2 for 90 degree angles
-//     if (result < 0.0) result += M_PI;
-//
-//     // TODO: check if 0 is a good default return value in the case z_i==z_o!
-//     return (std::isnan(result) || std::isinf(result)) ? double(0) : result;
-//   }
-//
-//
-//   static double getDistanceInTimeU(const SpacePoint& oHit, const SpacePoint& iHit) {
-//     return oHit.TimeU() - iHit.TimeU();
-//   }
-//
-//   static double getDistanceInTimeV(const SpacePoint& oHit, const SpacePoint& iHit) {
-//     return oHit.TimeV() - iHit.TimeV();
-//   }
-// }

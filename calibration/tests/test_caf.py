53c2e1edd37f8332434812797004547518073501
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-04-02 18:42:37 +0900

    Updated the test by adding the new files from the validation directory

--- calibration/tests/test_caf.py
+++ calibration/tests/test_caf.py
@@ -6,32 +6,30 @@ import b2test_utils as b2u
 
 
 with b2u.clean_working_directory():
-    globaltags = []
-    for globaltag in reversed(vgt.get_validation_globaltags()):
-        globaltags.append(globaltag)
+
+    # Prepare the configuration dictionary for CAF and dump it into a JSON file.
     config = {
         "caf_script": "caf_klm_channel_status.py",
-        "database_chain": globaltags,
+        "database_chain": [globaltag for globaltag in reversed(vgt.get_validation_globaltags())],
         "requested_iov": [0, 0, -1, -1]
     }
-
     with open("config.json", "w") as config_json:
         json.dump(config, config_json)
 
+    # Prepare the input files for the calibration and dump them into a JSON file.
     input_files = {
         "raw_physics": [
-            # [f"{os.environ['BELLE2_VALIDATION_DATA_DIR']}/calibration/cdst_e0008_r3121_hadronSkim/", [8, 3121]]
-            ["/home/belle2/giacomo/paolo/calibration/cdst_e0008_r3121_hadronSkim/", [8, 3121]],
-            ["/home/belle2/giacomo/paolo/calibration/cdst_e0010_r5095_hadronSkim/", [10, 5095]]
+            [f"{os.environ['BELLE2_VALIDATION_DATA_DIR']}/calibration/cdst_e0008_r3121_hadronSkim/", [8, 3121]],
+            [f"{os.environ['BELLE2_VALIDATION_DATA_DIR']}/calibration/cdst_e0010_r5095_hadronSkim/", [10, 5095]]
         ],
         "raw_cosmic": [
-            ["/home/belle2/giacomo/paolo/calibration/cdst_e0008_r3121_cosmicSkim/", [8, 3121]],
-            ["/home/belle2/giacomo/paolo/calibration/cdst_e0010_r5095_hadronSkim/", [10, 5095]]
+            [f"{os.environ['BELLE2_VALIDATION_DATA_DIR']}/calibration/cdst_e0008_r3121_cosmicSkim/", [8, 3121]],
+            [f"{os.environ['BELLE2_VALIDATION_DATA_DIR']}/calibration/cdst_e0010_r5095_hadronSkim/", [10, 5095]]
         ],
         "raw_beam": []
     }
-
     with open("input_files.json", "w") as input_files_json:
         json.dump(input_files, input_files_json)
 
+    # Now simply run the calibration locally.
     subprocess.check_call(['b2caf-prompt-run', 'Local', 'config.json', 'input_files.json', '--heartbeat', '10'])

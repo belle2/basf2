613d7bce78001a12db189354ae4627c49baf9b0b
Christian Wessel <christian.wessel@desy.de>
2021-04-12 20:15:51 +0200

    Added actual ROI finding, several additional parameters, and did some debugging. Final tests of ROI finding performance still pending.

--- tracking/datcon/optimizedDATCON/findlets/include/ROIFinder.h
+++ tracking/datcon/optimizedDATCON/findlets/include/ROIFinder.h
@@ -27,16 +27,16 @@ namespace Belle2 {
   /**
    * Findlet for performing the DATCON ROI calculation.
    */
-  class DATCONROIFinder : public TrackFindingCDC::Findlet<const SpacePointTrackCand> {
+  class ROIFinder : public TrackFindingCDC::Findlet<const SpacePointTrackCand> {
     /// Parent class
     using Super = TrackFindingCDC::Findlet<const SpacePointTrackCand>;
 
   public:
     /// Constructor for adding the subfindlets
-    DATCONROIFinder();
+    ROIFinder();
 
     /// Default desctructor
-    ~DATCONROIFinder();
+    ~ROIFinder();
 
     /// Expose the parameters of the sub findlets.
     void exposeParameters(ModuleParamList* moduleParamList, const std::string& prefix) override;
@@ -46,6 +46,9 @@ namespace Belle2 {
 
     /// Initialize the StoreArrays
     void initialize() override;
+
+    /// Initialize the BField
+    void beginRun() override;
 
     /// Clear the object pools
     void beginEvent() override;
@@ -61,15 +64,53 @@ namespace Belle2 {
     /// ROIs StoreArray
     StoreArray<ROIid> m_storeROIs;
 
-    const double m_const_centerZShiftLayer1[2] = {3.68255, -0.88255};    // for use of mhp_z > (lengh/-2)+shiftZ &&  mhp_z < (lengh/2)+shiftZ      // ATTENTION: hard coded values taken and derived from pxd/data/PXD-Components.xml
-    const double m_const_centerZShiftLayer2[2] = {5.01455, -1.21455};    // for use of mhp_z > (lengh/-2)+shiftZ &&  mhp_z < (lengh/2)+shiftZ      // ATTENTION: hard coded values taken and derived from pxd/data/PXD-Components.xml
-    const double m_const_sensorMinY =
-      -0.36;              // ATTENTION: hard coded values taken and derived from pxd/data/PXD-Components.xml
-    const double m_const_sensorMaxY =
-      0.89;                 // ATTENTION: hard coded values taken and derived from pxd/data/PXD-Components.xml
+
+    /// Minimum size of ROI in u-direction on L1
+    unsigned short m_param_minimumROISizeUL1 = 40;
+    /// Minimum size of ROI in v-direction on L1
+    unsigned short m_param_minimumROISizeVL1 = 40;
+    /// Minimum size of ROI in u-direction on L2
+    unsigned short m_param_minimumROISizeUL2 = 40;
+    /// Minimum size of ROI in v-direction on L2
+    unsigned short m_param_minimumROISizeVL2 = 40;
+
+    /// Multiplier term in ROI size estimation
+    /// size = multiplier * 1/R + minimumROISize
+    /// Multiplier term for u-direction on L1
+    double m_param_multiplierUL1 = 100;
+    /// Multiplier term for v-direction on L1
+    double m_param_multiplierVL1 = 100;
+    /// Multiplier term for u-direction on L2
+    double m_param_multiplierUL2 = 100;
+    /// Multiplier term for v-direction on L2
+    double m_param_multiplierVL2 = 100;
+
+    /// allowed overlap (in cm) in u (=r-phi)
+    double m_param_overlapU = 0.2;
+    /// allowed overlap (in cm) in v (= z / theta)
+    double m_param_overlapV = 0.2;
+
+    // ATTENTION: hard coded values taken and derived from pxd/data/PXD-Components.xml
+    /// Shift of the center of the active area of each sensor in a ladder of layer 1
+    /// For use of mhp_z > (lengh/-2)+shiftZ &&  mhp_z < (lengh/2)+shiftZ
+    const double m_const_centerZShiftLayer1[2] = {3.68255, -0.88255};
+    /// Shift of the center of the active area of each sensor in a ladder of layer 2
+    /// For use of mhp_z > (lengh/-2)+shiftZ &&  mhp_z < (lengh/2)+shiftZ
+    const double m_const_centerZShiftLayer2[2] = {5.01455, -1.21455};
+    /// PXD is shifted to create the windmill structure.
+    /// Minimum y coordinate if x-axis is perpendicular to the sensor:
+    const double m_const_sensorMinY = -0.36;
+    /// Maximum y coordinate if x-axis is perpendicular to the sensor:
+    const double m_const_sensorMaxY =  0.89;
+    /// Shift of the center position in y if the x-axis is perpendicular to the sensor:
     const double m_const_shiftY = (m_const_sensorMaxY + m_const_sensorMinY) / 2.0;
-    const double m_const_layerRadius[2] = {1.42854, 2.21218};      // ATTENTION: hard coded values from pxd/data/PXD-Components.xml
-    const double m_const_sensorLength[2] = {4.48, 6.144};
+    /// Radius of the two layers
+    const double m_const_layerRadius[2] = {1.42854, 2.21218};
+    /// Length of the active region for L1 and L2
+    const double m_const_activeSensorLength[2] = {4.48, 6.144};
+
+    /// BField in Tesla
+    double m_bFieldZ = 1.5;
 
   };
 }
--- tracking/datcon/optimizedDATCON/findlets/src/ROIFinder.cc
+++ tracking/datcon/optimizedDATCON/findlets/src/ROIFinder.cc
@@ -7,26 +7,28 @@
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/
-#include <tracking/datcon/optimizedDATCON/findlets/DATCONROIFinder.h>
+#include <tracking/datcon/optimizedDATCON/findlets/ROIFinder.h>
 #include <tracking/datcon/optimizedDATCON/findlets/RawTrackCandCleaner.icc.h>
 
 #include <tracking/trackFindingCDC/utilities/StringManipulation.h>
-#include <framework/logging/Logger.h>
 
+#include <framework/logging/Logger.h>
 #include <framework/core/ModuleParamList.h>
+#include <framework/geometry/BFieldManager.h>
+
+#include <pxd/geometry/SensorInfo.h>
+#include <vxd/geometry/GeoCache.h>
 
 using namespace Belle2;
 using namespace TrackFindingCDC;
 
-DATCONROIFinder::~DATCONROIFinder() = default;
+ROIFinder::~ROIFinder() = default;
 
-DATCONROIFinder::DATCONROIFinder()
+ROIFinder::ROIFinder()
 {
-//   addProcessingSignalListener(&m_spacePointLoaderAndPreparer);
-
 }
 
-void DATCONROIFinder::exposeParameters(ModuleParamList* moduleParamList, const std::string& prefix)
+void ROIFinder::exposeParameters(ModuleParamList* moduleParamList, const std::string& prefix)
 {
   Super::exposeParameters(moduleParamList, prefix);
 
@@ -40,37 +42,92 @@ void DATCONROIFinder::exposeParameters(ModuleParamList* moduleParamList, const s
                                 "Name of the ROIs StoreArray produced by DATCON using a simple circle extrapolation in r-phi and a straight line extrapolation in theta.",
                                 m_param_storeROIsName);
 
-//   m_spacePointLoaderAndPreparer.exposeParameters(moduleParamList, prefix);
+  moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "minimumROISizeUL1"), m_param_minimumROISizeUL1,
+                                "Minimum ROI size (in pixel) in u direction on L1.",
+                                m_param_minimumROISizeUL1);
+  moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "minimumROISizeVL1"), m_param_minimumROISizeVL1,
+                                "Minimum ROI size (in pixel) in v direction on L1.",
+                                m_param_minimumROISizeVL1);
+  moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "minimumROISizeUL2"), m_param_minimumROISizeUL2,
+                                "Minimum ROI size (in pixel) in u direction on L2.",
+                                m_param_minimumROISizeUL2);
+  moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "minimumROISizeVL2"), m_param_minimumROISizeVL2,
+                                "Minimum ROI size (in pixel) in v direction on L2.",
+                                m_param_minimumROISizeVL2);
+  moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "multiplierUL1"), m_param_multiplierUL1,
+                                "Multiplier term for ROI size estimation for L1 u direction. Usage: multiplierUL1 * 1/R + minimumROISizeUL1, with R in cm.",
+                                m_param_multiplierUL1);
+  moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "multiplierVL1"), m_param_multiplierVL1,
+                                "Multiplier term for ROI size estimation for L1 v direction. Usage: multiplierVL1 * 1/R + minimumROISizeVL1, with R in cm.",
+                                m_param_multiplierVL1);
+  moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "multiplierUL2"), m_param_multiplierUL2,
+                                "Multiplier term for ROI size estimation for L2 u direction. Usage: multiplierUL2 * 1/R + minimumROISizeUL2, with R in cm.",
+                                m_param_multiplierUL2);
+  moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "multiplierVL2"), m_param_multiplierVL2,
+                                "Multiplier term for ROI size estimation for L2 v direction. Usage: multiplierVL2 * 1/R + minimumROISizeVL2, with R in cm.",
+                                m_param_multiplierVL2);
+
+  moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "overlapU"), m_param_overlapU,
+                                "Allowed overlap along u. If the intercept is within this range of the active region, still create a ROI, since part of the ROI might hit the active area.",
+                                m_param_overlapU);
+  moduleParamList->addParameter(TrackFindingCDC::prefixed(prefix, "overlapV"), m_param_overlapV,
+                                "Allowed overlap along v. If the intercept is within this range of the active region, still create a ROI, since part of the ROI might hit the active area.",
+                                m_param_overlapV);
+
 }
 
 /// Create the store arrays
-void DATCONROIFinder::initialize()
+void ROIFinder::initialize()
 {
   Super::initialize();
   m_storePXDIntercepts.registerInDataStore(m_param_storePXDInterceptsName);
   m_storeROIs.registerInDataStore(m_param_storeROIsName);
+
+  if (m_param_overlapU < 0) {
+    B2ERROR("overlapU < 0 is not permitted, please choose a value >= 0.");
+  }
+  if (m_param_overlapV < 0) {
+    B2ERROR("overlapV < 0 is not permitted, please choose a value >= 0.");
+  }
 };
 
+void ROIFinder::beginRun()
+{
+  Super::beginRun();
 
-void DATCONROIFinder::beginEvent()
+  m_bFieldZ = BFieldManager::getField(0, 0, 0).Z() / Unit::T;
+}
+
+
+void ROIFinder::beginEvent()
 {
   Super::beginEvent();
 }
 
-void DATCONROIFinder::apply(const std::vector<SpacePointTrackCand>& finalTracks)
+void ROIFinder::apply(const std::vector<SpacePointTrackCand>& finalTracks)
 {
   for (auto& track : finalTracks) {
+    // do nothing if the SpacePointTrackCand is not active
+    if (!track.hasRefereeStatus(SpacePointTrackCand::c_isActive)) {
+      continue;
+    }
+
+    std::vector<PXDIntercept> thisTracksIntercepts;
+    thisTracksIntercepts.reserve(8);
+
     const B2Vector3D& momentumEstimate = track.getMomSeed();
 
-    const double trackPhi     = momentumEstimate.Phi();
-    const double trackTheta   = momentumEstimate.Theta();
-    const double trackRadius  = momentumEstimate.Perp();
-    const double trackCharge  = track.getChargeSeed();
+    const double trackPhi       = momentumEstimate.Phi();
+    const double trackTheta     = momentumEstimate.Theta();
+    const double tanTrackLambda = tan(M_PI_2 - trackTheta);
+    const double trackRadius    = momentumEstimate.Perp() / (0.00299792458 * m_bFieldZ) ;
+    const double trackCharge    = track.getChargeSeed();
 
     /** Loop over both PXD layer */
     for (int layer = 1; layer <= 2; layer++) {
       const double sensorPerpRadius = m_const_layerRadius[layer - 1];
       const double x = sensorPerpRadius;
+
       /** Loop over all ladders of layer */
       for (int ladder = 1; ladder <= (layer == 1 ? 8 : 12); ladder++) {
         const double sensorPhi = (layer == 1) ? M_PI / 4. * (ladder - 1) : M_PI / 6. * (ladder - 1);
@@ -93,6 +150,9 @@ void DATCONROIFinder::apply(const std::vector<SpacePointTrackCand>& finalTracks)
         }
         const double xCenter  = trackRadius * cos(relTrackCenterPhi);
         const double yCenter  = trackRadius * sin(relTrackCenterPhi);
+        if ((trackRadius * trackRadius - (x - xCenter) * (x - xCenter)) < 0) {
+          continue;
+        }
         const double ytmp     = sqrt(trackRadius * trackRadius - (x - xCenter) * (x - xCenter));
         const double yplus    = yCenter + ytmp;
         const double yminus   = yCenter - ytmp;
@@ -100,46 +160,111 @@ void DATCONROIFinder::apply(const std::vector<SpacePointTrackCand>& finalTracks)
         const double localUPositionMinus  = yminus - m_const_shiftY;
 
         // if the hit for sure is out of reach of the current ladder, continue
-        if (not(yplus  >= m_const_sensorMinY - 0.5 and yplus  <= m_const_sensorMaxY + 0.5) and
-            not(yminus >= m_const_sensorMinY - 0.5 and yminus <= m_const_sensorMaxY + 0.5)) {
+        if (not(yplus  >= m_const_sensorMinY - m_param_overlapU and yplus  <= m_const_sensorMaxY + m_param_overlapU) and
+            not(yminus >= m_const_sensorMinY - m_param_overlapU and yminus <= m_const_sensorMaxY + m_param_overlapU)) {
           continue;
         }
+
+        // estimate the z coordinate of the extrapolation on this layer
+        const double z = sensorPerpRadius * tanTrackLambda; // tan(trackLambda);
 
         /** Loop over both modules of a ladder */
         for (int sensor = 1; sensor <= 2; sensor++) {
 
           const double shiftZ = (layer == 1) ? m_const_centerZShiftLayer1[sensor - 1] : m_const_centerZShiftLayer2[sensor - 1];
-          const double z = (fabs(trackTheta - M_PI_2) > 1e-3) ? sensorPerpRadius / tan(trackTheta) : 0.0;
 
+          double localVPosition = z - shiftZ;
           // check whether z intersection possibly is on sensor to be checked, only continue with the rest of calculations if that's the case
-          if (z >= ((m_const_sensorLength[layer - 1] / -2.0) - 1.0 + shiftZ)
-              && z <= ((m_const_sensorLength[layer - 1] / 2.0) + 1.0 + shiftZ)) {
-
+          if (localVPosition >= ((m_const_activeSensorLength[layer - 1] / -2.0) - m_param_overlapV) and
+              localVPosition <= ((m_const_activeSensorLength[layer - 1] /  2.0) + m_param_overlapV)) {
+            // // check whether z intersection possibly is on sensor to be checked, only continue with the rest of calculations if that's the case
+            // if (z >= ((m_const_activeSensorLength[layer - 1] / -2.0) - m_param_overlapV + shiftZ) and
+            //     z <= ((m_const_activeSensorLength[layer - 1] /  2.0) + m_param_overlapV + shiftZ)) {
 
             const VxdID sensorID = VxdID(layer, ladder, sensor);
 
-            double localVPosition = z - shiftZ;
+            // double localVPosition = z - shiftZ;
             // check for first option of the intersection
-            if (yplus >= m_const_sensorMinY - 0.5 && yplus <= m_const_sensorMaxY + 0.5) {
+            if (yplus >= m_const_sensorMinY - m_param_overlapU and yplus <= m_const_sensorMaxY + m_param_overlapU) {
               PXDIntercept intercept;
               intercept.setCoorU(localUPositionPlus);
               intercept.setCoorV(localVPosition);
               intercept.setVxdID(sensorID);
               m_storePXDIntercepts.appendNew(intercept);
-              //break;
+              thisTracksIntercepts.push_back(intercept);
             }
             // check for second option of the intersection
-            if (yminus >= m_const_sensorMinY - 0.5 && yminus <= m_const_sensorMaxY + 0.5) {
+            if (yminus >= m_const_sensorMinY - m_param_overlapU and yminus <= m_const_sensorMaxY + m_param_overlapU) {
               PXDIntercept intercept;
               intercept.setCoorU(localUPositionMinus);
               intercept.setCoorV(localVPosition);
               intercept.setVxdID(sensorID);
               m_storePXDIntercepts.appendNew(intercept);
-              //break;
+              thisTracksIntercepts.push_back(intercept);
             }
           }
         }
       }
+    }
+
+    const double omega = 1. / trackRadius;
+    // const double tanLambdaFactor = 1. + fabs(tan(trackLambda));
+    const double tanLambdaFactor = 1. + fabs(tanTrackLambda);
+    const double radiusFactor = omega * tanLambdaFactor;
+    unsigned short uSizeL1 = (unsigned short)(m_param_multiplierUL1 * radiusFactor + m_param_minimumROISizeUL1);
+    unsigned short uSizeL2 = (unsigned short)(m_param_multiplierUL2 * radiusFactor + m_param_minimumROISizeUL2);
+    unsigned short vSizeL1 = (unsigned short)(m_param_multiplierVL1 * radiusFactor + m_param_minimumROISizeVL1);
+    unsigned short vSizeL2 = (unsigned short)(m_param_multiplierVL2 * radiusFactor + m_param_minimumROISizeVL2);
+    for (auto& intercept : thisTracksIntercepts) {
+      const VxdID& interceptSensorID = intercept.getSensorID();
+
+      double uCoordinate = intercept.getCoorU();
+      double vCoordinate = intercept.getCoorV();
+
+      const PXD::SensorInfo* currentSensor = dynamic_cast<const PXD::SensorInfo*>(&VXD::GeoCache::get(interceptSensorID));
+
+      int interceptUCell = currentSensor->getUCellID(uCoordinate, vCoordinate, false);
+      int interceptVCell = currentSensor->getVCellID(vCoordinate, false);
+      int nUCells = currentSensor->getUCells();
+      int nVCells = currentSensor->getVCells();
+
+      unsigned short uSize = (interceptSensorID.getLayerNumber() == 1 ? uSizeL1 : uSizeL2);
+      unsigned short vSize = (interceptSensorID.getLayerNumber() == 1 ? vSizeL1 : vSizeL2);
+      if (uSize > nUCells) {
+        uSize = nUCells;
+      }
+      if (vSize > nVCells) {
+        vSize = nVCells;
+      }
+
+      /** Lower left corner */
+      short uCellDownLeft = interceptUCell - uSize / 2;
+      short vCellDownLeft = interceptVCell - vSize / 2;
+
+      /** Upper right corner */
+      short uCellUpRight = interceptUCell + uSize / 2;
+      short vCellUpRight = interceptVCell + vSize / 2;
+
+      if (uCellDownLeft >= nUCells or vCellDownLeft >= nVCells or uCellUpRight < 0 or vCellUpRight < 0) {
+        continue;
+      }
+
+      if (uCellDownLeft < 0) {
+        uCellDownLeft = 0;
+      }
+      if (vCellDownLeft < 0) {
+        vCellDownLeft = 0;
+      }
+
+      // minimum cell id is 0, maximum is nCells - 1
+      if (uCellUpRight >= nUCells) {
+        uCellUpRight = nUCells - 1;
+      }
+      if (vCellUpRight >= nVCells) {
+        vCellUpRight = nVCells - 1;
+      }
+
+      m_storeROIs.appendNew(ROIid(uCellDownLeft, uCellUpRight, vCellDownLeft, vCellUpRight, interceptSensorID));
     }
 
   }

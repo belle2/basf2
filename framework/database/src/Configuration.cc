ddb4edf47cbf8455ad644854fea33d9c130db5b8
Mateusz Kaleta <mateusz.kaleta@desy.de>
2021-04-14 17:00:56 +0200

    Revert "temporary commit for testing purposes waiting for a TUPPR"
    This reverts commit b6be2fd1a3c7fe5a6f018b26c1bd58bbbaeabcc8.

--- framework/database/src/Configuration.cc
+++ framework/database/src/Configuration.cc
@@ -23,7 +23,7 @@
 #include <TPython.h>
 
 // Current default globaltag when generating events.
-#define CURRENT_DEFAULT_TAG "master_2021-03-25 svd_testChargeSimulation"
+#define CURRENT_DEFAULT_TAG "master_2021-03-25"
 
 namespace py = boost::python;
 

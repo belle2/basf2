4c79d0c33061c4a6d9f9561a872aa31c7c3416db
Sviatoslav Bilokin <sviatoslav.bilokin@desy.de>
2021-04-26 11:33:57 +0200

    Acceptance and Basic Info variable documentation

--- analysis/variables/src/AcceptanceVariables.cc
+++ analysis/variables/src/AcceptanceVariables.cc
@@ -164,29 +164,45 @@ namespace Belle2 {
 
     VARIABLE_GROUP("Acceptance");
 
-    REGISTER_VARIABLE("thetaInCDCAcceptance",   thetaInCDCAcceptance, "Particle is within CDC angular acceptance.");
-    REGISTER_VARIABLE("thetaInTOPAcceptance",   thetaInTOPAcceptance, "Particle is within TOP angular acceptance.");
-    REGISTER_VARIABLE("thetaInARICHAcceptance", thetaInARICHAcceptance, "Particle is within ARICH angular acceptance.");
-    REGISTER_VARIABLE("thetaInECLAcceptance",   thetaInECLAcceptance,
-                      "Particle is within ECL angular acceptance. 1: Forward; 2: Barrel; 3: Backwards.");
-    REGISTER_VARIABLE("thetaInBECLAcceptance",  thetaInBECLAcceptance, "Particle is within Barrel ECL angular acceptance.");
-    REGISTER_VARIABLE("thetaInEECLAcceptance",  thetaInEECLAcceptance, "Particle is within Endcap ECL angular acceptance.");
-    REGISTER_VARIABLE("thetaInKLMAcceptance",   thetaInKLMAcceptance,
-                      "Particle is within KLM angular acceptance. 1: Forward endcap; 2: Forward overalp; 3: Barrel; 4: Backward overlap; 5: Backward endcap.");
-    REGISTER_VARIABLE("thetaInBKLMAcceptance",  thetaInBKLMAcceptance, "Particle is within Barrel KLM angular acceptance.");
-    REGISTER_VARIABLE("thetaInEKLMAcceptance",  thetaInEKLMAcceptance, "Particle is within Endcap KLM angular acceptance.");
+    REGISTER_VARIABLE("thetaInCDCAcceptance",   thetaInCDCAcceptance,
+                      "Returns 1.0 if particle is within CDC angular acceptance, 0.0 otherwise.");
+    REGISTER_VARIABLE("thetaInTOPAcceptance",   thetaInTOPAcceptance,
+                      "Returns 1.0 if particle is within TOP angular acceptance, 0.0 otherwise.");
+    REGISTER_VARIABLE("thetaInARICHAcceptance", thetaInARICHAcceptance,
+                      "Returns 1.0 if particle is within ARICH angular acceptance, 0.0 otherwise.");
+    REGISTER_VARIABLE("thetaInECLAcceptance",   thetaInECLAcceptance, R"DOC(
+Checks if particle is within ECL angular acceptance. Return values are the following:
+
+* 1: Forward; 
+* 2: Barrel; 
+* 3: Backwards.
+)DOC");
+    REGISTER_VARIABLE("thetaInBECLAcceptance",  thetaInBECLAcceptance, "Returns 1.0 if particle is within Barrel ECL angular acceptance, 0.0 otherwise.");
+    REGISTER_VARIABLE("thetaInEECLAcceptance",  thetaInEECLAcceptance, "Returns 1.0 if particle is within Endcap ECL angular acceptance, 0.0 otherwise.");
+    REGISTER_VARIABLE("thetaInKLMAcceptance",   thetaInKLMAcceptance,  R"DOC(
+Checks if particle is within KLM angular acceptance. Return values are the following:
+
+* 1: Forward endcap; 
+* 2: Forward overlap; 
+* 3: Barrel; 
+* 4: Backward overlap; 
+* 5: Backward endcap.
+)DOC");
+
+    REGISTER_VARIABLE("thetaInBKLMAcceptance",  thetaInBKLMAcceptance, "Returns 1.0 if particle is within Barrel KLM angular acceptance, 0.0 otherwise.");
+    REGISTER_VARIABLE("thetaInEKLMAcceptance",  thetaInEKLMAcceptance, "Returns 1.0 if particle is within Endcap KLM angular acceptance, 0.0 otherwise.");
     REGISTER_VARIABLE("thetaInKLMOverlapAcceptance",  thetaInKLMOverlapAcceptance,
-                      "Particle is within the angular region where KLM barrel and endcaps overlap.");
+                      "Returns 1.0 if particle is within the angular region where KLM barrel and endcaps overlap, 0.0 otherwise.");
 
-    REGISTER_VARIABLE("ptInTOPAcceptance",   ptInTOPAcceptance,  "Particle is within TOP transverse momentum acceptance.");
-    REGISTER_VARIABLE("ptInBECLAcceptance",  ptInBECLAcceptance, "Particle is within Barrel ECL transverse momentum acceptance.");
-    REGISTER_VARIABLE("ptInBKLMAcceptance",  ptInBKLMAcceptance, "Particle is within Barrel KLM transverse momentum acceptance.");
+    REGISTER_VARIABLE("ptInTOPAcceptance",   ptInTOPAcceptance,  "Returns 1.0 if particle is within TOP transverse momentum acceptance, 0.0 otherwise.");
+    REGISTER_VARIABLE("ptInBECLAcceptance",  ptInBECLAcceptance, "Returns 1.0 if particle is within Barrel ECL transverse momentum acceptance, 0.0 otherwise.");
+    REGISTER_VARIABLE("ptInBKLMAcceptance",  ptInBKLMAcceptance, "Returns 1.0 if particle is within Barrel KLM transverse momentum acceptance, 0.0 otherwise.");
 
-    REGISTER_VARIABLE("inCDCAcceptance",   inCDCAcceptance,   "Particle is within CDC geometrical acceptance.");
-    REGISTER_VARIABLE("inTOPAcceptance",   inTOPAcceptance,   "Particle is within TOP geometrical acceptance.");
-    REGISTER_VARIABLE("inARICHAcceptance", inARICHAcceptance, "Particle is within ARICH geometrical acceptance.");
-    REGISTER_VARIABLE("inECLAcceptance",   inECLAcceptance,   "Particle is within ECL geometrical acceptance.");
-    REGISTER_VARIABLE("inKLMAcceptance",   inKLMAcceptance,   "Particle is within KLM geometrical acceptance.");
+    REGISTER_VARIABLE("inCDCAcceptance",   inCDCAcceptance,   "Returns 1.0 if particle is within CDC geometrical and kinematical acceptance, 0.0 otherwise.");
+    REGISTER_VARIABLE("inTOPAcceptance",   inTOPAcceptance,   "Returns 1.0 if particle is within TOP geometrical and kinematical acceptance, 0.0 otherwise.");
+    REGISTER_VARIABLE("inARICHAcceptance", inARICHAcceptance, "Returns 1.0 if particle is within ARICH geometrical and kinematical acceptance, 0.0 otherwise.");
+    REGISTER_VARIABLE("inECLAcceptance",   inECLAcceptance,   "Returns 1.0 if particle is within ECL geometrical and kinematical acceptance, 0.0 otherwise.");
+    REGISTER_VARIABLE("inKLMAcceptance",   inKLMAcceptance,   "Returns 1.0 if particle is within KLM geometrical and kinematical acceptance, 0.0 otherwise.");
 
   }
 }

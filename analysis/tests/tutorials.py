179b7c771c166eaa8f6ec8a3ae937477df4d9b7a
Frank Meier <frank.meier@duke.edu>
2021-04-30 10:10:15 +0900

    reactivate test of read decay hash example

--- analysis/tests/tutorials.py
+++ analysis/tests/tutorials.py
@@ -16,7 +16,6 @@ class TutorialsTest(unittest.TestCase):
     #: list of the broken tutorials (to be removed when they are individually fixed)
     broken_tutorials = ['B2A702-ContinuumSuppression_MVATrain.py',  # BII-4246
                         'B2A703-ContinuumSuppression_MVAExpert.py',  # BII-4246
-                        'B2A503-ReadDecayHash.py',  # BII-4254
                         ]
 
     @unittest.skipIf(not os.getenv('BELLE2_EXAMPLES_DATA_DIR'),

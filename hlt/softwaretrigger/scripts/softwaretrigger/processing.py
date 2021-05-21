363d3dd7ab25329ba098bbbd2a82b72f9f0e27a4
Björn Spruck <bjoern.spruck@desy.de>
2021-05-21 12:37:56 +0200

    use conv function to set parameter

--- hlt/softwaretrigger/scripts/softwaretrigger/processing.py
+++ hlt/softwaretrigger/scripts/softwaretrigger/processing.py
@@ -260,10 +260,8 @@ def add_expressreco_processing(path,
     path_utils.add_geometry_if_not_present(path)
     add_unpackers(path, components=unpacker_components, writeKLMDigitRaws=True)
 
-    # dont filter prune pxd for partly broken events, as we loose diagnostics in DQM
-    for m in path.modules():
-        if m.name() == "PXDPostErrorChecker":
-            m.param('CriticalErrorMask', 0)
+    # dont filter/prune pxd for partly broken events, as we loose diagnostics in DQM
+    set_module_parameters(path, "PXDPostErrorChecker", CriticalErrorMask=0)
 
     if do_reconstruction:
         if run_type == constants.RunTypes.beam:

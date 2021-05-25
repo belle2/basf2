6b7cf54430fad21e06e5da6521160a6912036545
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-05-25 11:06:31 +0200

    Added the input_data_filters; Addressed some flake8 warnings

--- calibration/scripts/prompt/calibrations/caf_klm_time.py
+++ calibration/scripts/prompt/calibrations/caf_klm_time.py
@@ -6,7 +6,7 @@ and KLMTimeConstants database objects.
 """
 
 import basf2
-from prompt import CalibrationSettings
+from prompt import CalibrationSettings, input_data_filters
 
 ##############################
 # REQUIRED VARIABLE #
@@ -22,6 +22,11 @@ settings = CalibrationSettings(name='KLM time',
                                description=__doc__,
                                input_data_formats=['cdst'],
                                input_data_names=['hlt_mumu'],
+                               input_data_filters={
+                                   'hlt_mumu': [input_data_filters['Run Type']['physics'],
+                                                input_data_filters['Data Tag']['mumutight_calib'],
+                                                input_data_filters['Data Quality Tag']['Good Or Recoverable']]
+                               },
                                depends_on=[])
 
 ##############################
@@ -57,26 +62,8 @@ def get_calibrations(input_data, **kwargs):
     # Set up config options
 
     # In this script we want to use one sources of input data.
-    # Get the input files  from the input_data variable
+    # Get the input files from the input_data variable
     file_to_iov_cdst = input_data['hlt_mumu']
-
-    # We might have requested an enormous amount of data across a run range.
-    # There's a LOT more files than runs!
-    # Lets set some limits because this calibration doesn't need that much to run.
-    max_files_per_run = 2
-
-    # If you are using Raw data there's a chance that input files could have zero events.
-    # This causes a B2FATAL in basf2 RootInput so the collector job will fail.
-    # Currently we don't have a good way of filtering this on the automated side, so we can check here.
-    min_events_per_file = 1
-
-    # We filter out any more than 2 files per run. The input data files are sorted alphabetically by b2caf-prompt-run
-    # already. This procedure respects that ordering
-    from prompt.utils import filter_by_max_files_per_run
-
-    # For testing
-    # reduced_file_to_iov_cdst = filter_by_max_files_per_run(file_to_iov_cdst, max_files_per_run, min_events_per_file)
-    # input_files_cdst = sorted(list(reduced_file_to_iov_cdst.keys()))
     input_files_cdst = sorted(list(file_to_iov_cdst.keys()))
     basf2.B2INFO(f'Total number of \'hlt_mumu\' files actually used as input = {len(input_files_cdst)}')
 
@@ -94,7 +81,6 @@ def get_calibrations(input_data, **kwargs):
     ###################################################
     # Algorithm setup
 
-    import ROOT
     from ROOT.Belle2 import KLMTimeAlgorithm
 
     alg = KLMTimeAlgorithm()

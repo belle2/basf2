bbad09a976ca79935c5653f3a7d2f3078d7ea42f
Giulia Casarosa <giulia.casarosa@desy.de>
2021-05-29 00:41:31 +0900

    removed SVDDetectorConfiguration wrapper, fixed all scripts and modules

--- svd/modules/svdUnpacker/src/SVDUnpackerModule.cc
+++ svd/modules/svdUnpacker/src/SVDUnpackerModule.cc
@@ -11,7 +11,7 @@
 ******************************************************************************/
 
 #include <svd/modules/svdUnpacker/SVDUnpackerModule.h>
-#include <svd/calibration/SVDDetectorConfiguration.h>
+
 
 #include <framework/datastore/DataStore.h>
 #include <framework/datastore/StoreObjPtr.h>
@@ -128,11 +128,10 @@ void SVDUnpackerModule::beginRun()
   seenHeadersAndTrailers = 0;
 
   //get the relative time shift
-  SVDDetectorConfiguration detectorConfig;
-  if (detectorConfig.isValid())
-    m_relativeTimeShift = detectorConfig.getRelativeTimeShift();
+  if (m_svdGlobalConfig.isValid())
+    m_relativeTimeShift = m_svdGlobalConfig->getRelativeTimeShift();
   else {
-    B2ERROR("SVDDetectorConfiguration not valid!! Setting relativeTimeShift to 0 for this reconstruction.");
+    B2ERROR("SVDGlobalConfigParamters not valid!! Setting relativeTimeShift to 0 for this reconstruction.");
     m_relativeTimeShift = 0;
   }
 }

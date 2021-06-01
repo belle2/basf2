59cbfc89dcb89a5f1aefbc2b1b919681f4500d83
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2021-06-01 20:19:45 +0200

    Also set VO_BELLE2_SW_DIR in caf jobs if it exists

--- calibration/scripts/caf/backends.py
+++ calibration/scripts/caf/backends.py
@@ -617,6 +617,8 @@ class Job:
             raise BackendError("No BELLE2_TOOLS found in environment")
         if "BELLE2_CONFIG_DIR" in os.environ:
             self.setup_cmds.append(f"export BELLE2_CONFIG_DIR={os.environ['BELLE2_CONFIG_DIR']}")
+        if "VO_BELLE2_SW_DIR" in os.environ:
+            self.setup_cmds.append(f"export VO_BELLE2_SW_DIR={os.environ['VO_BELLE2_SW_DIR']}")
         if "BELLE2_RELEASE" in os.environ:
             self.setup_cmds.append(f"source {os.environ['BELLE2_TOOLS']}/b2setup {os.environ['BELLE2_RELEASE']}")
         elif 'BELLE2_LOCAL_DIR' in os.environ:

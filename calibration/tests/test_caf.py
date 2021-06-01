2e30f4fc82c1a519b609469b67c3ece443805524
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-06-02 02:01:20 +0900

    Add a new convenient function for printing all the BELLE2 environment variables; Use it in test_caf.py for debugging

--- calibration/tests/test_caf.py
+++ calibration/tests/test_caf.py
@@ -40,7 +40,9 @@ with b2test_utils.clean_working_directory():
         # validationtools.update_env()
         subprocess.check_call(['b2caf-prompt-run', 'Local', 'config.json', 'input_files.json', '--heartbeat', '20'])
     except subprocess.CalledProcessError as e:
-        # In case of failure, prints the submit.sh...
+        # In case of failure, prints all the BELLE2 environment variables...
+        b2test_utils.print_belle2_environment()
+        # ... and the submit.sh...
         sub_name = basf2.find_file('calibration_results/KLMChannelStatus/0/collector_output/raw/0/submit.sh', '', True)
         if sub_name:
             with open(sub_name) as sub_file:

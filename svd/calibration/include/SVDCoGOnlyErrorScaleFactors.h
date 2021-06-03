c1a84565c042565580383df221ca731c826957dc
Giulia Casarosa <giulia.casarosa@desy.de>
2021-06-03 23:24:48 +0900

    updated year

--- svd/calibration/include/SVDCoGOnlyErrorScaleFactors.h
+++ svd/calibration/include/SVDCoGOnlyErrorScaleFactors.h
@@ -1,9 +1,9 @@
 /**************************************************************************
  * BASF2 (Belle Analysis Framework 2)                                     *
- * Copyright(C) 2017 - Belle II Collaboration                             *
+ * Copyright(C) 2021 - Belle II Collaboration                             *
  *                                                                        *
  * Author: The Belle II Collaboration                                     *
- * Contributors: Giulia Casarosa                                           *
+ * Contributors: Giulia Casarosa                                          *
  *                                                                        *
  * This software is provided "as is" without any warranty.                *
  **************************************************************************/

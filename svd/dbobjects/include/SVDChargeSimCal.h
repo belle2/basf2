81b28085d15f8727ec98b3454d33c11de894759c
Mateusz Kaleta <mateusz.kaleta@desy.de>
2021-06-04 01:15:43 +0900

    APV coupling from SVDChargeSimulationCalibrations. Maybe no need to change any actual code?

--- svd/dbobjects/include/SVDChargeSimCal.h
+++ svd/dbobjects/include/SVDChargeSimCal.h
@@ -20,10 +20,11 @@ namespace Belle2 {
 
     /** Coupling constant; used to parametrise signal coupling among strips in simulation.
      * Currently we have 4 couplings implemented:
-     * couplingConstant["C0"]  |  Coupling from readout implant to its readout channel
-     * couplingConstant["C1"]  |  Coupling from first neighbour implant (floating) to readout channel
-     * couplingConstant["C2"]  |  Coupling from second neighbour implant (readout) to readout channel
-     * couplingConstant["C3"]  |  Coupling from third neighbour implant (floating) to readout channel
+     * couplingConstant["C0"]           |  Coupling from readout implant to its readout channel
+     * couplingConstant["C1"]           |  Coupling from first neighbour implant (floating) to readout channel
+     * couplingConstant["C2"]           |  Coupling from second neighbour implant (readout) to readout channel
+     * couplingConstant["C3"]           |  Coupling from third neighbour implant (floating) to readout channel
+     * couplingConstant["APVCoupling"]  |  Coupling between two adjacent channels done at APV level after charge sharing
      */
     std::map<std::string, float> couplingConstant;
 

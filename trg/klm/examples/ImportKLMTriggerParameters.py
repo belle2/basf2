0bf87dcba2f3010ea707aa6f60f1582e46349a23
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-06-18 22:53:44 +0900

    Fix the importer example

--- trg/klm/examples/ImportKLMTriggerParameters.py
+++ trg/klm/examples/ImportKLMTriggerParameters.py
@@ -6,14 +6,14 @@
 from ROOT.Belle2 import KLMTriggerParameters, Database, IntervalOfValidity
 
 parameters = KLMTriggerParameters()
-parameters.setNLayers(1)
-parameters.setWhichLayers("2:3")
+parameters.setNLayers(5)
+parameters.setWhichLayers("2:16")
 
 database = Database.Instance()
 
 iov = IntervalOfValidity(0, 0, 0, -1)
-database.storeData('EventsOfDoomParameters', parameters, iov)
+database.storeData('KLMTriggerParameters', parameters, iov)
 iov = IntervalOfValidity(1002, 0, 1002, -1)
-database.storeData('EventsOfDoomParameters', parameters, iov)
+database.storeData('KLMTriggerParameters', parameters, iov)
 iov = IntervalOfValidity(1003, 0, 1003, -1)
-database.storeData('EventsOfDoomParameters', parameters, iov)
+database.storeData('KLMTriggerParameters', parameters, iov)

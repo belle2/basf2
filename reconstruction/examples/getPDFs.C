cecb9e004dc23d2578cc851ab6dfa6814573b586
Filippo Dattola <filippo.dattola@desy.de>
2021-06-19 17:48:55 +0200

    Fix some of the cppcheck warnings in the reconstruction package.

--- reconstruction/examples/getPDFs.C
+++ reconstruction/examples/getPDFs.C
@@ -17,31 +17,33 @@ void getPDFs(std::string inputfile = "temp.root") {
     TFile* runfile = new TFile(Form("%s", inputfile.data()));
     if (!runfile)std::cout << "Input payload file not found" << std::endl;
 
-    Belle2::DedxPDFs* pdfs = (Belle2::DedxPDFs*)runfile->Get("DedxPDFs");
-    std::vector<TH2F*> hDedxPDFs(6);
+    else{
+      Belle2::DedxPDFs* pdfs = (Belle2::DedxPDFs*)runfile->Get("DedxPDFs");
+      std::vector<TH2F*> hDedxPDFs(6);
 
-    std::array<std::string, 6> part = {"Electron", "Muon", "Pion", "Kaon", "Proton", "Deuteron"};
-    std::array<std::string, 3> det = {"CDC", "PXD", "VXD"};
+      std::array<std::string, 6> part = {"Electron", "Muon", "Pion", "Kaon", "Proton", "Deuteron"};
+      std::array<std::string, 3> det = {"CDC", "PXD", "VXD"};
 
-    TCanvas* can = new TCanvas("can", "blah-blah", 1200, 700);
-    can->Divide(3, 2);
-    gStyle->SetOptStat(11);
+      TCanvas* can = new TCanvas("can", "blah-blah", 1200, 700);
+      can->Divide(3, 2);
+      gStyle->SetOptStat(11);
 
-    for (const auto& idet : det) {
+      for (const auto& idet : det) {
         for (bool trunmean : { false, true }) {
-            std::stringstream check;
-            check << std::boolalpha << trunmean;
-            for (int iPart = 0; iPart < 6; iPart++) {
-                if (idet.compare("CDC") == 0)hDedxPDFs[iPart] = (TH2F*)pdfs->getCDCPDF(iPart, trunmean);
-                else if (idet.compare("SVD") == 0)hDedxPDFs[iPart] = (TH2F*)pdfs->getSVDPDF(iPart, trunmean);
-                else if (idet.compare("VXD") == 0)hDedxPDFs[iPart] = (TH2F*)pdfs->getPXDPDF(iPart, trunmean);
-                can->cd(iPart + 1);
-                hDedxPDFs[iPart]->SetTitle(Form("%s; p(GeV/c) of %s; dE/dx", hDedxPDFs[iPart]->GetTitle(), part[iPart].data()));
-                hDedxPDFs[iPart]->DrawCopy("colz");
-            }
-            can->SetTitle(Form("Likehood dist. of charged particles from %s, trunmean = %s", idet.data(), check.str().data()));
-            can->SaveAs(Form("Plots_%sDedxPDFs_wTrucMean_%s.pdf", idet.data(), check.str().data()));
+	  std::stringstream check;
+	  check << std::boolalpha << trunmean;
+	  for (int iPart = 0; iPart < 6; iPart++) {
+	    if (idet.compare("CDC") == 0)hDedxPDFs[iPart] = (TH2F*)pdfs->getCDCPDF(iPart, trunmean);
+	    else if (idet.compare("SVD") == 0)hDedxPDFs[iPart] = (TH2F*)pdfs->getSVDPDF(iPart, trunmean);
+	    else if (idet.compare("VXD") == 0)hDedxPDFs[iPart] = (TH2F*)pdfs->getPXDPDF(iPart, trunmean);
+	    can->cd(iPart + 1);
+	    hDedxPDFs[iPart]->SetTitle(Form("%s; p(GeV/c) of %s; dE/dx", hDedxPDFs[iPart]->GetTitle(), part[iPart].data()));
+	    hDedxPDFs[iPart]->DrawCopy("colz");
+	  }
+	  can->SetTitle(Form("Likehood dist. of charged particles from %s, trunmean = %s", idet.data(), check.str().data()));
+	  can->SaveAs(Form("Plots_%sDedxPDFs_wTrucMean_%s.pdf", idet.data(), check.str().data()));
         }
+      }
+      delete can;
     }
-    delete can;
 }

e2b9a7611d06ae9892276a0b10a1f8712f2c4b53
Björn Spruck <bjoern.spruck@desy.de>
2021-06-22 10:31:19 +0200

    add TTD unpacker to unpackers

--- rawdata/scripts/rawdata.py
+++ rawdata/scripts/rawdata.py
@@ -76,6 +76,9 @@ def add_unpackers(path, components=None, writeKLMDigitRaws=False):
     if "Geometry" not in path:
         path.add_module("Geometry")
 
+    # TTD
+    path.add_module('TTDUnpacker')
+
     # PXD
     if components is None or 'PXD' in components:
         add_pxd_unpacker(path)

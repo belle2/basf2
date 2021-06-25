9a6ad3aaabcdcb9a1abf5467cafdddaa8752a3b2
Christian Wessel <christian.wessel@desy.de>
2021-06-25 14:25:48 +0200

    Changed argument in add_datcon and removed all module parameters that were equal to the default ones in the cpp code itself.

--- tracking/datcon/scripts/datcon/datcon_functions.py
+++ tracking/datcon/scripts/datcon/datcon_functions.py
@@ -9,8 +9,8 @@ def add_datcon(path,
                svd_space_points='SVDSpacePoints',
                svd_clusters='SVDClusters',
                datcon_reco_tracks='DATCONRecoTracks',
-               use_simple_roi_calculation=True,
-               use_advanced_roi_calculation=False,
+               use_simple_roi_calculation=False,
+               usePXDROIFinderModule=True,
                datcon_pxd_intercepts_name='DATCONPXDIntercepts',
                datcon_rois_name='DATCONROIs'):
     """
@@ -19,7 +19,7 @@ def add_datcon(path,
     : param datcon_reco_tracks: Name of the StoreArray containing the RecoTracks found by DATCON
     : param use_simple_roi_calculation: Use a simple ROI calculation with a circle extrapolation in r-phi
         and a straight line extrapolation in theta
-    : param use_advanced_roi_calculation: Calculate ROI using the PXDROIFinderModule
+    : param usePXDROIFinderModule: Calculate ROI using the PXDROIFinderModule
     : param datcon_pxd_intercepts_name: Name of the StoreArray containing the PXDIntercepts calculated by DATCON.
         If both simple and advanced ROI finding are selected, the StoreArray for the advanced ROI finding will get
         the prefix PXDROIFinder,
@@ -33,7 +33,7 @@ def add_datcon(path,
     # If both simple DATCON ROI finding and regular ROI finding using the PXDROIFinder with the RecoTracks found by
     # DATCON shall be performed, set a different name for the PXDInterecepts and the ROIs from the PXDROIFinder
     # (advancedPXDInterceptsName and advancedROINames) to be able to distinguish them.
-    if use_simple_roi_calculation and use_advanced_roi_calculation:
+    if use_simple_roi_calculation and usePXDROIFinderModule:
         advancedPXDInterceptsName = 'PXDROIFinder' + advancedPXDInterceptsName
         advancedROIName = 'PXDROIFinder' + advancedROIName
 
@@ -43,52 +43,15 @@ def add_datcon(path,
                     finalOverlapResolverNameSVDClusters=svd_clusters,
                     refinerOverlapResolverNameSVDClusters=svd_clusters,
 
-                    minimumUClusterTime=-25,
-                    minimumVClusterTime=-25,
-                    maximumUClusterTime=40,
-                    maximumVClusterTime=40,
-
-                    simpleVerticalHoughSpaceSize=0.25,
-                    simpleNAngleSectors=256,
-                    simpleNVerticalSectors=256,
-                    simpleMinimumHSClusterSize=2,
-                    simpleMaximumHSClusterSize=10,
-
                     relationFilter='angleAndTime',
                     relationFilterParameters={'AngleAndTimeThetaCutDeltaL0': 0.03,
                                               'AngleAndTimeThetaCutDeltaL1': 0.10,
                                               'AngleAndTimeThetaCutDeltaL2': 0.20,
                                               'AngleAndTimeDeltaUTime': 15.,
                                               'AngleAndTimeDeltaVTime': 15., },
-
-                    maxRelations=2000,
-                    twoHitFilterLimit=50,
-                    twoHitFilter='twoHitVirtualIPQI',
-                    twoHitUseNBestHits=5,
-                    threeHitUseNBestHits=5,
-                    fourHitUseNBestHits=3,
-                    fiveHitUseNBestHits=2,
-
-                    minQualitiyIndicatorSize3=0.001,
-                    minQualitiyIndicatorSize4=0.001,
-                    minQualitiyIndicatorSize5=0.001,
-                    maxNumberOfEachPathLength=10,
-
-                    RecoTracksStoreArrayName=datcon_reco_tracks,
-
-                    calculateROI=use_simple_roi_calculation,
-                    # tolerancePhi=0,
-                    # toleranceZ=0,
-                    multiplierUL1=600,
-                    multiplierUL2=800,
-                    multiplierVL1=0.8,
-                    multiplierVL2=1.2,
-                    radiusCorrectionFactor=4.0,
-                    sinPhiCorrectionFactor=0.0,
-                    cosPhiCorrectionFactor=0.0,
                     )
 
-    if use_advanced_roi_calculation:
+    if usePXDROIFinderModule:
 
         if 'SetupGenfitExtrapolation' not in path:
             path.add_module('SetupGenfitExtrapolation').set_name('SetupGenfitExtrapolationForDATCON')

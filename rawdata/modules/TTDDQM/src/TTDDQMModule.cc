88048140a9dca36c38659d97c5b979e054a244f1
Björn Spruck <bjoern.spruck@desy.de>
2021-06-29 10:08:00 +0200

    include/TTDDQMModule.h
    Documentation, renaming of variables, axis etc

--- rawdata/modules/TTDDQM/src/TTDDQMModule.cc
+++ rawdata/modules/TTDDQM/src/TTDDQMModule.cc
@@ -27,6 +27,10 @@ TTDDQMModule::TTDDQMModule() : HistoModule(),  m_EventLevelTriggerTimeInfo()
 {
   //Set module properties
   setDescription("Monitor TTD Trigger and Injection");
+  // his DQM module can be run offline or online
+  // if included in online DQM, binning should be reviewed (size!) and
+  // adapted to the binning of the corresponding sub detector (or GDL) histograms
+  // it may replace some of the histograms currently filled by sub-detectors
   setPropertyFlags(c_ParallelProcessingCertified);
   addParam("histogramDirectoryName", m_histogramDirectoryName, "Name of the directory where histograms will be placed",
            std::string("TTD"));
@@ -48,15 +52,19 @@ void TTDDQMModule::defineHisto()
                               "Triggers for HER veto tuning;Time since last injection in #mus;Time within beam cycle in #mus", 500, 0, 30000, 100, 0,
                               5120 / 508.);
 
-  hTriggersAfterTrigger = new TH1I("hTTDTriggersAfterLast",
-                                   "Trigger after Last Trigger;Time diff in #mus;Count/Time (0.5 #mus bins)", 100000, 0, 50000);
-  hTriggersPerBunch = new TH1I("hTTDTriggerBunch", "Trigger per Bunch;Bunch/4;Triggers", 1280, 0, 1280);
+  hTriggersDeltaT = new TH1I("hTTDTriggersDeltaT",
+                             "#delta Trigger Time since previous trigger;#delta t in #mus;Triggers/Time (0.5 #mus bins)", 100000, 0, 50000);
+  hTriggersPerBunch = new TH1I("hTTDTriggerBunch", "Triggers per Bunch;Bunch(rel);Triggers per 4 Bunches)", 1280, 0, 1280 * 4);
 
-  hBunchInjHER = new TH1I("hTTDBunchInjHER", "Last Inj Bunch HER;Counts;BunchNr/4", 1280, 0, 1280);
-  hBunchInjLER = new TH1I("hTTDBunchInjLER", "Last Inj Bunch LER;Counts;BunchNr/4", 1280, 0, 1280);
+  hBunchInjHER = new TH1I("hTTDBunchInjHER", "Last Injected Bunch HER;Bunch(rel);Counts per 4 Bunches", 1280, 0, 1280 * 4);
+  hBunchInjLER = new TH1I("hTTDBunchInjLER", "Last Injected Bunch LER;Bunch(rel);Counts per 4 Bunches", 1280, 0, 1280 * 4);
 
-  hTrigBunchInjLER =  new TH2I("hTTDTrigBunchInjLER", "TrigBunchInjLER", 256, 0, 1280, 256, 0, 1280);
-  hTrigBunchInjHER =  new TH2I("hTTDTrigBunchInjHer", "TrigBunchInjHer", 256, 0, 1280, 256, 0, 1280);
+  hTrigBunchInjLER =  new TH2I("hTTDTrigBunchInjLER",
+                               "Correlation between triggered bunch and injected bunch in LER;Injected Bunch(rel);Triggered Bunch(rel)", 256, 0, 1280 * 4, 256, 0,
+                               1280 * 4);
+  hTrigBunchInjHER =  new TH2I("hTTDTrigBunchInjHer",
+                               "Correlation between triggered bunch and injected bunch in HER;Injected Bunch(rel);Triggered Bunch(rel)", 256, 0, 1280 * 4, 256, 0,
+                               1280 * 4);
 
   // cd back to root directory
   oldDir->cd();
@@ -70,10 +78,10 @@ void TTDDQMModule::initialize()
 
 void TTDDQMModule::beginRun()
 {
-  // Do not assume that everthing is non-zero, e.g. Max might be nullptr
+  // all ptr are set in defineHisto, thus no need for nullptr check
   hTrigAfterInjLER->Reset();
   hTrigAfterInjHER->Reset();
-  hTriggersAfterTrigger->Reset();
+  hTriggersDeltaT->Reset();
   hTriggersPerBunch->Reset();
   hBunchInjHER->Reset();
   hBunchInjLER->Reset();
@@ -85,24 +93,33 @@ void TTDDQMModule::event()
 {
 
   if (m_EventLevelTriggerTimeInfo->isValid()) {
-    hTriggersAfterTrigger->Fill(m_EventLevelTriggerTimeInfo->getTimeSincePrevTrigger() / 127.);
-    int bunch_trg = m_EventLevelTriggerTimeInfo->getBunchNumber();
-    hTriggersPerBunch->Fill(bunch_trg);
+    // TODO conversion of clock ticks to time not yet done in EventLevelTriggerTimeInfo
+    // all values are given in clock ticks of RF clock/4, thus one tick correspond to 4 bunches
+    // ~ 508 MHz RF -> 127 = 508/4 (clock ticks)
+    // 5120 possible bunch positions from RF and ring circumference -> 1280 = 5120/4 (in ticks)
+    // use an int and not the real fraction number to avoid binning effects for getting inexact time
+    // any bunch number is currently relative and not matching the SKB number (offset differs for LER, HER)
+    // time after injection is time after prekick signal, real injection happens ~90us afterwards (offset differs for LER, HER)
+    hTriggersDeltaT->Fill(m_EventLevelTriggerTimeInfo->getTimeSincePrevTrigger() / 127.);
+    int triggered_bunch_in_ticks = m_EventLevelTriggerTimeInfo->getBunchNumber();
+    hTriggersPerBunch->Fill(triggered_bunch_in_ticks * 4);
 
 
     if (m_EventLevelTriggerTimeInfo->hasInjection()) {
-      auto difference = m_EventLevelTriggerTimeInfo->getTimeSinceLastInjection();
-      int bunch_inj = ((difference - bunch_trg + 1280) % 1280); // swapped? 1280-1-bunch_inj?
+      auto time_since_inj_in_ticks = m_EventLevelTriggerTimeInfo->getTimeSinceLastInjection();// in clock ticks
+      //  127MHz clock ticks to us, inexact rounding, use integer to avoid binning effects
+      float time_since_inj_in_us = time_since_inj_in_ticks / 127.;
+      // swapped? 1280-1-injected_bunch_in_ticks?
+      int injected_bunch_in_ticks = ((time_since_inj_in_ticks - triggered_bunch_in_ticks + 1280) % 1280);
 
-      float diff2 = difference / 127.; //  127MHz clock ticks to us, inexact rounding
       if (m_EventLevelTriggerTimeInfo->isHER()) {
-        hTrigAfterInjHER->Fill(diff2, diff2 - int(diff2 / (5120 / 508.)) * (5120 / 508.));
-        hBunchInjHER->Fill(bunch_inj);
-        hTrigBunchInjHER->Fill(bunch_inj, bunch_trg);
+        hTrigAfterInjHER->Fill(time_since_inj_in_us, time_since_inj_in_us - int(time_since_inj_in_us / (5120 / 508.)) * (5120 / 508.));
+        hBunchInjHER->Fill(injected_bunch_in_ticks * 4);
+        hTrigBunchInjHER->Fill(injected_bunch_in_ticks * 4, triggered_bunch_in_ticks * 4);
       } else {
-        hTrigAfterInjLER->Fill(diff2, diff2 - int(diff2 / (5120 / 508.)) * (5120 / 508.));
-        hBunchInjLER->Fill(bunch_inj);
-        hTrigBunchInjLER->Fill(bunch_inj, bunch_trg);
+        hTrigAfterInjLER->Fill(time_since_inj_in_us, time_since_inj_in_us - int(time_since_inj_in_us / (5120 / 508.)) * (5120 / 508.));
+        hBunchInjLER->Fill(injected_bunch_in_ticks * 4);
+        hTrigBunchInjLER->Fill(injected_bunch_in_ticks * 4, triggered_bunch_in_ticks * 4);
       }
     }
   }

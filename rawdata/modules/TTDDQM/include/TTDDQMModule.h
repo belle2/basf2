d3c9c04ce3ff6eedaa366d9741c79f437fc77b09
Björn Spruck <bjoern.spruck@desy.de>
2021-06-29 10:21:49 +0200

    Documentation, renaming of variables, axis etc

--- rawdata/modules/TTDDQM/include/TTDDQMModule.h
+++ rawdata/modules/TTDDQM/include/TTDDQMModule.h
@@ -39,17 +39,17 @@ namespace Belle2 {
     StoreObjPtr<EventLevelTriggerTimeInfo> m_EventLevelTriggerTimeInfo;
 
 
-    TH2I* hTrigAfterInjLER{};         /**< Histogram Veto tuning triggers after LER injection */
-    TH2I* hTrigAfterInjHER{};         /**< Histogram Veto tuning triggers after HER injection */
+    TH2I* hTrigAfterInjLER{};  /**< Histogram Veto tuning triggers after LER injection */
+    TH2I* hTrigAfterInjHER{};  /**< Histogram Veto tuning triggers after HER injection */
 
-    TH2I* hTrigBunchInjLER{};         /**< Histogram Veto tuning triggers after LER injection */
-    TH2I* hTrigBunchInjHER{};         /**< Histogram Veto tuning triggers after HER injection */
+    TH2I* hTrigBunchInjLER{};  /**< Histogram correlation triggered and injected bunch LER */
+    TH2I* hTrigBunchInjHER{};  /**< Histogram correlation triggered and injected bunch HER */
 
-    TH1I* hTriggersAfterTrigger{};          /**< Histogram for Nr Entries (=Triggers after Last Trigger */
-    TH1I* hTriggersPerBunch{};          /**< Histogram for Trigger per Bunch  */
+    TH1I* hTriggersDeltaT{};   /**< Histogram for Delta T to previous Trigger */
+    TH1I* hTriggersPerBunch{}; /**< Histogram for Triggers per Bunch  */
 
-    TH1I* hBunchInjLER{}; /**< Histogram injected HER bunch (triggers) */
-    TH1I* hBunchInjHER{}; /**< Histogram injected LER bunch (triggers) */
+    TH1I* hBunchInjLER{};      /**< Histogram injected HER bunch (triggers) */
+    TH1I* hBunchInjHER{};      /**< Histogram injected LER bunch (triggers) */
 
     void initialize() override final; /**< initialize function */
 

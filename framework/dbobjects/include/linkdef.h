9744908c4f3ef540e9e15bf4a25d575899713f30
Kirill Chilikin <chilikin@lebedev.ru>
2021-06-29 20:19:32 +0300

    Update class version.

--- framework/dbobjects/include/linkdef.h
+++ framework/dbobjects/include/linkdef.h
@@ -10,7 +10,7 @@
 #pragma link C++ class Belle2::MagneticFieldComponent+; // checksum=0x8f4b7a53, version=1
 #pragma link C++ class Belle2::MagneticFieldComponentConstant+; // checksum=0xb387820a, version=1
 #pragma link C++ class Belle2::MagneticField+; // checksum=0x1f3e9954, version=1
-#pragma link C++ class Belle2::HardwareClockSettings+; // checksum=0xf93fb086, version=2
+#pragma link C++ class Belle2::HardwareClockSettings+; // checksum=0x727e9fa3, version=3
 #pragma link C++ class Belle2::BunchStructure+; // checksum=0x811b7f71, version=1
 
 

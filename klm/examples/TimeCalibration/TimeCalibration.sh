86a7a02f1de188077f52b8f887e1db7be2802f94
Kirill Chilikin <chilikin@lebedev.ru>
2021-07-01 22:57:57 +0900

    Use parallel batch jobs in KLM time calibration example.

--- klm/examples/TimeCalibration/TimeCalibration.sh
+++ klm/examples/TimeCalibration/TimeCalibration.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # This file provides an example of how to call KLM time calibration examples.
 DIR=${BELLE2_LOCAL_DIR}/klm/examples/TimeCalibration
+NEVENTS=1000
+NFILES=100
+SUBMISSION="bsub -qs"
 mkdir time_calibration
-basf2 -n 100000 ${DIR}/Generation.py time_calibration/gen.root
-basf2 ${DIR}/Simulation.py time_calibration/gen.root time_calibration/sim.root
-basf2 ${DIR}/Calibration.py ${PWD}/time_calibration/sim.root
+for ((i = 0; i < ${NFILES}; i++)); do
+  ${SUBMISSION} basf2 -n ${NEVENTS} ${DIR}/Generation.py time_calibration/gen_${i}.root
+done
+for ((i = 0; i < ${NFILES}; i++)); do
+  ${SUBMISSION} basf2 ${DIR}/Simulation.py time_calibration/gen_${i}.root time_calibration/sim_${i}.root
+done
+basf2 ${DIR}/Calibration.py ${PWD}/time_calibration/sim_*.root

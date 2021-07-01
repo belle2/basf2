692856e4aa78b5b45dba0a9408b2f98bd64d6351
Frank Meier <frank.meier@duke.edu>
2021-07-02 08:35:38 +0900

    simplify printing of license

--- framework/scripts/basf2_cli/print_license.py
+++ framework/scripts/basf2_cli/print_license.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 
-from basf2.core import basf2label
 from basf2 import find_file
 
-short_license = ""
+print('')
 
 with open(find_file("LICENSE.md")) as license_file:
-    lines = [line for line in license_file.readlines()]
-    short_license = "".join(lines[:20])
-    # magic number to only grab the short version of the license
+    print(license_file.read())
 
-short_license += "\nFor the full text of the LGPL-3.0 License see $BELLE2_RELEASE_DIR/LICENSE.md"
-
-print('')
-print(short_license)
+print("For the full text of the LGPL-3.0 License see $BELLE2_RELEASE_DIR/COPYING")

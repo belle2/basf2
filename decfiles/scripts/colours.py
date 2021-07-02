811c8b457b0cffb4e8d75bcd9a424c4a8752e3a3
Frank Meier <frank.meier@duke.edu>
2021-07-02 23:57:46 +0900

    update copyright statement for decfiles package

--- decfiles/scripts/colours.py
+++ decfiles/scripts/colours.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 # --------------------------------------
 # Class holding colours for printing coloured text to terminal and printing functions, also handles the general log
 # Written by Phillip Urquijo
--- decfiles/scripts/test_dec_table.py
+++ decfiles/scripts/test_dec_table.py
@@ -2,6 +2,14 @@
 # -*- coding: utf-8 -*-
 # Descriptor: script to test decay tables
 
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 """
 <header>
   <output>../test.dst.root</output>

26b5d150e6f2fefb87f26bac5860dc0275d61df1
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-07-02 18:00:57 +0200

    Update copyright in online_book package

--- online_book/basf2/steering_files/090_cs.py
+++ online_book/basf2/steering_files/090_cs.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
 import basf2 as b2
 import modularAnalysis as ma
 import stdPi0s

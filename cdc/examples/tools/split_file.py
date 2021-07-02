c1ac7cabe3c191a8b91a9a2b93a36e73eb33e6a4
Frank Meier <frank.meier@duke.edu>
2021-07-02 23:06:12 +0900

    update copyright statement for cdc package

--- cdc/examples/tools/split_file.py
+++ cdc/examples/tools/split_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 ###############################################################################
 # Split one big data file to smaller files.
 # command: basf2 split_file.py filename entries_per_file nfile
--- cdc/modules/cdcDQM/include/cdcDQM7.h
+++ cdc/modules/cdcDQM/include/cdcDQM7.h
@@ -1,3 +1,10 @@
+/**************************************************************************
+ * basf2 (Belle II Analysis Software Framework)                           *
+ * Author: The Belle II Collaboration                                     *
+ *                                                                        *
+ * See git log for contributors and copyright holders.                    *
+ * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
+ **************************************************************************/
 /* Nanae Taniguchi 2017.07.12 */
 /* Nanae Taniguchi 2018.02.06 */
 /* Nanae Taniguchi 2019.02.17 */

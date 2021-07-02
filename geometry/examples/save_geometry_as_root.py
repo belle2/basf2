54b8ada5c0aac66c8f225a61ebd93e877a16cc78
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-07-02 15:05:58 +0200

    Update copyright in geometry package

--- geometry/examples/save_geometry_as_root.py
+++ geometry/examples/save_geometry_as_root.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 ######################################################
 # This steering file creates the Belle II detector
 # geometry, and saves it as a Root file.

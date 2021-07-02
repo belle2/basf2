625ec15dbae9067c28c07df4349efee424449d51
Frank Meier <frank.meier@duke.edu>
2021-07-02 22:37:08 +0900

    update copyright statement for background package

--- background/steering_tools/makeBGOverlayMCFile.py
+++ background/steering_tools/makeBGOverlayMCFile.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 from basf2 import create_path, set_log_level, B2ERROR, B2INFO, LogLevel, process, statistics
 import os
 from svd import add_svd_simulation

af3196ca5df9954c28c774fe94b5a9acda0b9b64
Frank Meier <frank.meier@duke.edu>
2021-07-02 22:41:32 +0900

    update copyright statement for beast package

--- beast/examples/collApertOverride.py
+++ beast/examples/collApertOverride.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 ###########################################################################
 # This steering file demonstrates how to override collimator displacements,
 # create the Belle II detector geometry and check for overlaps
--- beast/scripts/phase2/getHists.C
+++ beast/scripts/phase2/getHists.C
@@ -1,3 +1,10 @@
+/**************************************************************************
+ * basf2 (Belle II Analysis Software Framework)                           *
+ * Author: The Belle II Collaboration                                     *
+ *                                                                        *
+ * See git log for contributors and copyright holders.                    *
+ * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
+ **************************************************************************/
 {
   TString sample = "phase2.1.3_june_Touschek/EM_2/";
   TString outfile = "phase2.1.3_june_Touschek_EM_2.root";

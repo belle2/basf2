6db99a2db87d8770572b7a70864bb1b37eafd36b
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-07-02 12:48:32 +0200

    Update copyright in mdst package

--- mdst/tests/mdst_compatibility-v00-09-03.py
+++ mdst/tests/mdst_compatibility-v00-09-03.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 """
 Test backwards compatibility for an mdst file produced with release-00-09-03.
 

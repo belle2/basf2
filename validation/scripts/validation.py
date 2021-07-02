0fd18fc1a532c32f6fa552ddb21aedb907a1a7a4
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-07-02 19:40:54 +0200

    Update copyright in validation package

--- validation/scripts/validation.py
+++ validation/scripts/validation.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 # basf2 specific imports
 from basf2 import statistics
 from ROOT import PyConfig
--- validation/tests/test_folder_creation.py
+++ validation/tests/test_folder_creation.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 import os
 import shutil
 import tempfile

b99792fcf0329c01f87bf77981850ee19fd9d4fa
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-07-05 09:05:37 +0200

    Remove copyright statements in online_book/basf2/steering_files files

--- online_book/basf2/steering_files/067_generate_mc.py
+++ online_book/basf2/steering_files/067_generate_mc.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
 
-##########################################################################
-# basf2 (Belle II Analysis Software Framework)                           #
-# Author: The Belle II Collaboration                                     #
-#                                                                        #
-# See git log for contributors and copyright holders.                    #
-# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
-##########################################################################
-
 import basf2 as b2
 import generators as ge
 import simulation as si

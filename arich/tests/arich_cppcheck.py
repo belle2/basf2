15e028a6c8037d6836cb2af4086ea4f71cde2f32
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-07-07 11:09:59 +0200

    Remove my name from some files

--- arich/tests/arich_cppcheck.py
+++ arich/tests/arich_cppcheck.py
@@ -17,7 +17,7 @@ import re
 from b2test_utils import check_error_free
 
 if __name__ == "__main__":
-    # Comment from Giacomo: this is a temporary workaround, since this package is affected by cppcheck warnings
+    # Comment: this is a temporary workaround, since this package is affected by cppcheck warnings
     # from other packages. check_error_free skips all the lines identified by findall, so here we skip all the lines
     # that do no not contain the name of the tested package. The output will be incomplete and a bit obscure,
     # but at least the test will correctly check if there are cppcheck warnings affecting this package.

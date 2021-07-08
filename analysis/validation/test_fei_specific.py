3ec3ad7b507f8160ae6cd27cd5ad425ddc09df42
Artur Gottmann <artur.gottmann@desy.de>
2021-07-08 17:03:23 +0200

    analysis/validation/test_fei_specific.py: correct and update specific FEI validation

--- analysis/validation/test_fei_specific.py
+++ analysis/validation/test_fei_specific.py
@@ -15,17 +15,18 @@
 </header>
 """
 
-import fei
-from fei.default_channels import get_unittest_channels
 import os
+import subprocess
 
 import tempfile
 import shutil
 import glob
 
+import fei
 import basf2 as b2
 import modularAnalysis as ma
 import basf2_mva
+import ROOT
 
 basf2_mva.loadRootDictionary()
 
@@ -37,8 +38,9 @@ b2.conditions.append_testing_payloads('localdb/database.txt')
 fei.core.Teacher.MaximumNumberOfMVASamples = int(1e7)
 fei.core.Teacher.MinimumNumberOfMVASamples = int(10)
 
-particles = fei.get_unittest_channels()
+particles = fei.get_unittest_channels(specific=True)
 
+# Construct path for reconstruction of signal side B-meson
 sig_path = b2.create_path()
 ma.inputMdst(environmentType='default',
              filename=b2.find_file('mdst14.root', 'validation', False),
@@ -50,10 +52,10 @@ ma.reconstructDecay('B+:sig -> tau+:sig', '', writeOut=True, path=sig_path)
 ma.matchMCTruth('B+:sig', path=sig_path)
 ma.buildRestOfEvent('B+:sig', path=sig_path)
 
-particles = get_unittest_channels(specific=True)
-configuration = fei.config.FeiConfiguration(prefix='FEI_VALIDATION', training=True, cache=-1)
+configuration = fei.config.FeiConfiguration(prefix='FEI_VALIDATION', cache=-1, training=True, monitor=False)
 feistate = fei.get_path(particles, configuration)
 
+# Construct path for production of mcParticlesCount.root at stage -1
 path = b2.create_path()
 roe_path = b2.create_path()
 cond_module = b2.register_module('SignalSideParticleFilter')
@@ -63,14 +65,14 @@ roe_path.add_module(cond_module)
 path.add_path(sig_path)
 path.for_each('RestOfEvent', 'RestOfEvents', roe_path)
 
-assert feistate.stage == 0
+assert feistate.stage == 0  # corresponds to stage -1, since increased by 1 after creating path
 print(path)
 b2.process(path, max_event=10000)
 assert len(glob.glob('mcParticlesCount.root')) == 1
 
-configuration = fei.config.FeiConfiguration(prefix='FEI_VALIDATION', training=True, cache=0)
-fei.do_trainings(particles, configuration)
+# Construct path for production of stage 0 training data
 path = b2.create_path()
+configuration = fei.config.FeiConfiguration(prefix='FEI_VALIDATION', cache=0, training=True, monitor=False)
 feistate = fei.get_path(particles, configuration)
 roe_path = b2.create_path()
 cond_module = b2.register_module('SignalSideParticleFilter')
@@ -80,22 +82,32 @@ roe_path.add_module(cond_module)
 path.add_path(sig_path)
 path.for_each('RestOfEvent', 'RestOfEvents', roe_path)
 
-assert feistate.stage == 1, feistate.stage
+assert feistate.stage == 1  # corresponds to stage 0, since increased by 1 after creating path
 print(path)
 b2.process(path, max_event=10000)
-assert len(glob.glob('gamma*')) == 2
-assert len(glob.glob('mu+*')) == 1
-assert len(glob.glob('pi+*')) == 1
-assert len(glob.glob('K+*')) == 1
 
+# Check avaliability of training input and cache file for stage 0
+assert len(glob.glob('training_input.root')) == 1
+f = ROOT.TFile.Open("training_input.root", "read")
+assert sum(['gamma' in key.GetName()[:5] for key in f.GetListOfKeys()]) == 2
+assert sum(['mu+' in key.GetName()[:4] for key in f.GetListOfKeys()]) == 1
+assert sum(['pi+' in key.GetName()[:4] for key in f.GetListOfKeys()]) == 1
+assert sum(['K+' in key.GetName()[:3] for key in f.GetListOfKeys()]) == 1
+f.Close()
+
+# Perform stage 0 training
 fei.do_trainings(particles, configuration)
 
-assert len(glob.glob('gamma*')) == 6
-assert len(glob.glob('mu+*')) == 3
-assert len(glob.glob('pi+*')) == 3
-assert len(glob.glob('K+*')) == 3
+# Moving training_input.root to training_input_stage0.root for later usage
+shutil.move("training_input.root", f"training_input_stage{feistate.stage-1}.root")
 
-fei.do_trainings(particles, configuration)
+# Check availability of stage 0 *.xml training files
+assert len(glob.glob('gamma*.xml')) == 2
+assert len(glob.glob('mu+*.xml')) == 1
+assert len(glob.glob('pi+*.xml')) == 1
+assert len(glob.glob('K+*.xml')) == 1
+
+# Construct path for production of stage 1 training data
 path = b2.create_path()
 feistate = fei.get_path(particles, configuration)
 roe_path = b2.create_path()
@@ -106,15 +118,26 @@ roe_path.add_module(cond_module)
 path.add_path(sig_path)
 path.for_each('RestOfEvent', 'RestOfEvents', roe_path)
 
-assert feistate.stage == 2
+assert feistate.stage == 2  # corresponds to stage 1, since increased by 1 after creating path
 print(path)
 b2.process(path, max_event=10000)
-assert len(glob.glob('pi0*')) == 1
 
-fei.do_trainings(particles, configuration)
-assert len(glob.glob('pi0*')) == 3
+# Check avaliability of training input and cache file for stage 1
+assert len(glob.glob('training_input.root')) == 1
+f = ROOT.TFile.Open("training_input.root", "read")
+assert sum(['pi0' in key.GetName()[:4] for key in f.GetListOfKeys()]) == 1
+f.Close()
 
+# Perform stage 1 training
 fei.do_trainings(particles, configuration)
+
+# Moving training_input.root to training_input_stage1.root for later usage
+shutil.move("training_input.root", f"training_input_stage{feistate.stage-1}.root")
+
+# Check availability of stage 1 *.xml training files
+assert len(glob.glob('pi0*.xml')) == 1
+
+# Construct path for production of stage 3 training data (stage 2 is skipped)
 path = b2.create_path()
 feistate = fei.get_path(particles, configuration)
 roe_path = b2.create_path()
@@ -125,16 +148,34 @@ roe_path.add_module(cond_module)
 path.add_path(sig_path)
 path.for_each('RestOfEvent', 'RestOfEvents', roe_path)
 
-assert feistate.stage == 4
+assert feistate.stage == 4  # corresponds to stage 3, since increased by 1 after creating path
 print(path)
 b2.process(path, max_event=10000)
-assert len(glob.glob('D*')) == 5
 
-# One training will fail D -> pi pi due to low statistic
-fei.do_trainings(particles, configuration)
-assert len(glob.glob('D*')) == 15
+# Check avaliability of training input and cache file for stage 3
+assert len(glob.glob('training_input.root')) == 1
+f = ROOT.TFile.Open("training_input.root", "read")
+assert sum(['D' in key.GetName()[:2] for key in f.GetListOfKeys()]) == 5
+f.Close()
 
+# Perform stage 3 training
 fei.do_trainings(particles, configuration)
+
+# Moving training_input.root to training_input_stage3.root for later usage
+shutil.move("training_input.root", f"training_input_stage{feistate.stage-1}.root")
+
+# Check availability of stage 3 *.xml training files
+assert len(glob.glob('D*.xml')) == 5
+
+# Merge training input files for validation
+subprocess.call(["analysis-fei-mergefiles", "training_input.root"] + glob.glob("training_input_stage*.root"))
+
+# Expect 4 different training_input*.root files now
+assert len(glob.glob('training_input*.root')) == 4
+
+
+# Construct path for stage 6 preparing evaluation (stages 4 and 5 skipped, input evaluated from stage 0 on)
+configuration = fei.config.FeiConfiguration(prefix='FEI_VALIDATION', cache=0, monitor=True)
 path = b2.create_path()
 feistate = fei.get_path(particles, configuration)
 roe_path = b2.create_path()
@@ -145,35 +186,44 @@ roe_path.add_module(cond_module)
 path.add_path(sig_path)
 path.for_each('RestOfEvent', 'RestOfEvents', roe_path)
 
-assert feistate.stage == 7
-
-configuration = fei.config.FeiConfiguration(prefix='FEI_VALIDATION', monitor=True, training=False)
-feistate = fei.get_path(particles, configuration)
-
-fei.do_trainings(particles, configuration)
-path = b2.create_path()
-feistate = fei.get_path(particles, configuration)
-roe_path = b2.create_path()
-cond_module = b2.register_module('SignalSideParticleFilter')
-cond_module.param('particleLists', ['B+:sig'])
-cond_module.if_true(feistate.path, b2.AfterConditionPath.END)
-roe_path.add_module(cond_module)
-path.add_path(sig_path)
-path.for_each('RestOfEvent', 'RestOfEvents', roe_path)
-
-assert feistate.stage == 7
+assert feistate.stage == 7  # corresponds to stage 6, since increased by 1 after creating path
 print(path)
 b2.process(path, max_event=10000)
+
+
+# Check avaliability of monitoring files constructed at stage 6 for validation
 assert len(glob.glob('Monitor_FSPLoader.root')) == 1
-assert len(glob.glob('Monitor_TrainingData_*')) == 11
-assert len(glob.glob('Monitor_PreReconstruction_BeforeRanking_*')) == 11
-assert len(glob.glob('Monitor_PreReconstruction_AfterRanking_*')) == 11
-assert len(glob.glob('Monitor_PreReconstruction_AfterVertex_*')) == 11
-assert len(glob.glob('Monitor_PostReconstruction_AfterMVA_*')) == 6
-assert len(glob.glob('Monitor_PostReconstruction_BeforePostCut_*')) == 5
-assert len(glob.glob('Monitor_PostReconstruction_BeforeRanking_*')) == 5
-assert len(glob.glob('Monitor_PostReconstruction_AfterRanking_*')) == 5
-assert len(glob.glob('Monitor_Final_*')) == 5
+
 assert len(glob.glob('Monitor_ModuleStatistics.root')) == 1
+
+assert len(glob.glob('Monitor_Final.root')) == 1
+f = ROOT.TFile.Open("Monitor_Final.root", "read")
+assert len([key.GetName() for key in f.GetListOfKeys()]) == 7
+f.Close()
+
+prereconstruction_files = [
+    'Monitor_PreReconstruction_BeforeRanking.root',
+    'Monitor_PreReconstruction_AfterRanking.root',
+    'Monitor_PreReconstruction_AfterVertex.root',
+    'Monitor_PostReconstruction_AfterMVA.root'
+]
+
+for fname in prereconstruction_files:
+    assert len(glob.glob(fname)) == 1
+    f = ROOT.TFile.Open(fname, "read")
+    assert len([key.GetName() for key in f.GetListOfKeys()]) == 11
+    f.Close()
+
+postreconstruction_files = [
+    'Monitor_PostReconstruction_BeforePostCut.root',
+    'Monitor_PostReconstruction_BeforeRanking.root',
+    'Monitor_PostReconstruction_AfterRanking.root'
+]
+
+for fname in postreconstruction_files:
+    assert len(glob.glob(fname)) == 1
+    f = ROOT.TFile.Open(fname, "read")
+    assert len([key.GetName() for key in f.GetListOfKeys()]) == 7
+    f.Close()
 
 shutil.rmtree(tempdir)

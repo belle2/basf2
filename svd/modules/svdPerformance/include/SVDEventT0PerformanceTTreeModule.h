89681cf21706d3906145cc9a52d6019e63883ee4
Giulia Casarosa <giulia.casarosa@desy.de>
2021-07-13 02:15:05 +0900

    remove extra line in copyright statement

--- svd/modules/svdPerformance/include/SVDEventT0PerformanceTTreeModule.h
+++ svd/modules/svdPerformance/include/SVDEventT0PerformanceTTreeModule.h
@@ -4,7 +4,6 @@
  *                                                                        *
  * See git log for contributors and copyright holders.                    *
  * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
- *                                                                        *
  **************************************************************************/
 
 #pragma once

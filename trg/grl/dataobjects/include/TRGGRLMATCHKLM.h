dd089d85c0a1d40d5a608f419d8ff178e47c1d34
Frank Meier <frank.meier@duke.edu>
2021-07-27 02:24:24 +0900

    set initial value for members

--- trg/grl/dataobjects/include/TRGGRLMATCHKLM.h
+++ trg/grl/dataobjects/include/TRGGRLMATCHKLM.h
@@ -42,7 +42,7 @@ namespace Belle2 {
     double m_dphi;
 
     /**KLM sector id used for the matching*/
-    double m_sector;
+    double m_sector{ -1};
 
     /**! The Class title*/
     ClassDef(TRGGRLMATCHKLM, 3); /*< the class title */

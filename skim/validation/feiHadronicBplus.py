53470d68f2f482adfd7445cd6c3a819c6f6acb51
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-08-17 19:10:32 +0200

    Add an argument to b2skim-generate-validation for adding the default analysis globaltag to the skim + Fix the FEI skim validation scripts

--- skim/validation/feiHadronicBplus.py
+++ skim/validation/feiHadronicBplus.py
@@ -26,7 +26,11 @@ import modularAnalysis as ma
 from skim.WGs.fei import feiHadronicBplus
 
 path = b2.Path()
-skim = feiHadronicBplus(validation=True, udstOutput=False)
+skim = feiHadronicBplus(
+    validation=True,
+    udstOutput=False,
+    analysis_globaltag=ma.getAnalysisGlobaltag(),
+)
 
 ma.inputMdstList(
     b2.find_file(skim.validation_sample, data_type="validation"),

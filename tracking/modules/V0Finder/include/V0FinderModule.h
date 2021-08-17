1e8ba312bd1d75fbaaaa59cd2027952444aed16d
UEMATSU Yuma <uematsu@hep.phys.s.u-tokyo.ac.jp>
2021-08-17 09:47:10 +0900

    fix doxygen warnings

--- tracking/modules/V0Finder/include/V0FinderModule.h
+++ tracking/modules/V0Finder/include/V0FinderModule.h
@@ -101,7 +101,7 @@ namespace Belle2 {
     double m_mLambdaMax2 = 0; ///< pre-calculated maximum Lambda mass squared
 
     // counter for V0Finder statistics
-    int m_nForceStored = 0;
-    int m_nHitRemoved  = 0;
+    int m_nForceStored = 0; ///< counter for saved V0s failing to remove the inner hits
+    int m_nHitRemoved  = 0; ///< counter for saved V0s successfully removing the inner hits
   };
 }

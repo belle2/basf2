638614753cc82b113a984606b16b310a6325d42c
fpham <fpham@student.unimelb.edu.au>
2021-09-17 22:29:38 +0900

    Correct skimID

--- skim/scripts/skim/registry.py
+++ skim/scripts/skim/registry.py
@@ -85,7 +85,7 @@ _RegisteredSkims = [
     ("14141001", "btocharm", "BtoD0rho_Kpipipi_Kpipi0"),
     ("14141002", "btocharm", "B0toDD_Kpipi_Kspi"),
     ("14141003", "btocharm", "B0toDstarD"),
-    ("14140104", "btocharm", "B0toD0Kpipi0_pi0"),
+    ("14121300", "btocharm", "B0toD0Kpipi0_pi0"),
 
 
     # --- WG5: Quarkonium ---

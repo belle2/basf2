dea34f60a3c2f799357fb4c6e3c833eb0f5a7356
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-09-24 18:19:52 +0900

    TUPPR: update default globaltag to prerelease-06-00-00c

--- framework/database/src/Configuration.cc
+++ framework/database/src/Configuration.cc
@@ -21,7 +21,7 @@
 #include <TPython.h>
 
 // Current default globaltag when generating events.
-#define CURRENT_DEFAULT_TAG "prerelease-06-00-00b"
+#define CURRENT_DEFAULT_TAG "prerelease-06-00-00c"
 
 namespace py = boost::python;
 

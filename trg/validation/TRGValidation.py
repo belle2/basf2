c9ef28a297129a6782883f7505c1477e0aee87ef
YIN Junhao <junhao.yin@desy.de>
2021-10-09 10:41:40 +0900

    format the style of validation script

--- trg/validation/TRGValidation.py
+++ trg/validation/TRGValidation.py
@@ -20,7 +20,7 @@
 
 import basf2
 import ROOT
-from ROOT import Belle2, gROOT, TH1F, TFile, TNamed
+from ROOT import Belle2, TH1F, TFile, TNamed
 from math import pi as PI
 
 Fac = 180.0 / PI
@@ -38,11 +38,11 @@ class MakePlots(basf2.Module):
         :param Descr description text
         '''
 
-        descr = TNamed('Description', description)
+        descr = TNamed("Description", description)
         histogram.GetListOfFunctions().Add(descr)
-        Check = TNamed('Check', check)
+        Check = TNamed("Check", check)
         histogram.GetListOfFunctions().Add(Check)
-        contact = TNamed('Contact', 'yinjh2012@korea.ac.kr')
+        contact = TNamed("Contact", "yinjh2012@korea.ac.kr")
         histogram.GetListOfFunctions().Add(contact)
         Meta = TNamed("MetaOptions", "shifter")
         histogram.GetListOfFunctions().Add(Meta)
@@ -63,44 +63,45 @@ class MakePlots(basf2.Module):
 
     def initialize(self):
 
-        self.tfile = TFile('TRGValidation.root', 'recreate')
+        self.tfile = TFile("TRGValidation.root", "recreate")
 
-        m_dbinput = Belle2.PyDBObj('TRGGDLDBInputBits')
-        m_dbftdl = Belle2.PyDBObj('TRGGDLDBFTDLBits')
+        m_dbinput = Belle2.PyDBObj("TRGGDLDBInputBits")
+        m_dbftdl = Belle2.PyDBObj("TRGGDLDBFTDLBits")
         n_inbit = m_dbinput.getninbit()
         n_outbit = m_dbftdl.getnoutbit()
 
-        mc_px = 'MCParticles.m_momentum_x'
-        mc_py = 'MCParticles.m_momentum_y'
-        mc_pz = 'MCParticles.m_momentum_z'
-        trk2d_omega = 'TRGCDC2DFinderTracks.m_omega'
-        trk2d_phi = 'TRGCDC2DFinderTracks.m_phi0'
+        mc_px = "MCParticles.m_momentum_x"
+        mc_py = "MCParticles.m_momentum_y"
+        trk2d_omega = "TRGCDC2DFinderTracks.m_omega"
+        trk2d_phi = "TRGCDC2DFinderTracks.m_phi0"
 
         ROOT.gStyle.SetOptStat(ROOT.kFALSE)
 
         #: validation histogram
-        self.hist_inbit = TH1F('hin', 'trigger input bits', 320, 0, 320)
+        self.hist_inbit = TH1F("hin", "trigger input bits", 320, 0, 320)
         self.hist_inbit.GetXaxis().SetRangeUser(0, n_inbit)
         self.hist_inbit.GetXaxis().SetLabelSize(0.02)
 
         #: validation histogram
-        self.hist_outbit = TH1F('hout', 'trigger output bits', 320, 0, 320)
+        self.hist_outbit = TH1F("hout", "trigger output bits", 320, 0, 320)
         self.hist_outbit.GetXaxis().SetRangeUser(0, n_outbit)
         self.hist_outbit.GetXaxis().SetLabelSize(0.02)
 
         for i in range(320):
             inbitname = m_dbinput.getinbitname(i)
             outbitname = m_dbftdl.getoutbitname(i)
-            self.hist_inbit.GetXaxis().SetBinLabel(self.hist_inbit.GetXaxis().FindBin(i + 0.5), inbitname)
-            self.hist_outbit.GetXaxis().SetBinLabel(self.hist_outbit.GetXaxis().FindBin(i + 0.5), outbitname)
+            self.hist_inbit.GetXaxis().SetBinLabel(
+                 self.hist_inbit.GetXaxis().FindBin(i + 0.5), inbitname)
+            self.hist_outbit.GetXaxis().SetBinLabel(
+                 self.hist_outbit.GetXaxis().FindBin(i + 0.5), outbitname)
 
         #: validation histogram
-        self.h_E_ECL = TH1F("h_E_ECL", "ECL cluster energy [5 MeV]", 200, 0, 10)
-        self.set_descr(self.h_E_ECL, 'TRG ECL cluster energy', '')
+        self.h_E_ECL = TH1F("h_E_ECL", "ECL cluster energy [50 MeV]", 200, 0, 10)
+        self.set_descr(self.h_E_ECL, "TRG ECL cluster energy", "")
         self.set_style(self.h_E_ECL, "ECL cluster energy (GeV)", "Events/(50 MeV)")
 
         #: validation histogram
-        self.h_Esum_ECL = TH1F("h_Esum_ECL", "sum of ECL cluster energy [5 MeV]", 200, 0, 10)
+        self.h_Esum_ECL = TH1F("h_Esum_ECL", "sum of ECL cluster energy [50 MeV]", 200, 0, 10)
         self.set_descr(self.h_Esum_ECL, "sum of TRG ECL cluster energy", "")
         self.set_style(self.h_Esum_ECL, "sum of ECL cluster energy (GeV)", "Events/(50 MeV)")
 
@@ -111,27 +112,27 @@ class MakePlots(basf2.Module):
 
         #: validation histogram
         self.h_thetaID_ECL = TH1F("h_thetaID_ECL", "ECL cluster TC ID", 610, 0, 610)
-        self.set_descr(self.h_thetaID_ECL, 'TRG ECL cluster theta ID', 'unform in barrel')
-        self.set_style(self.h_thetaID_ECL, 'ECL cluster TC ID', 'Events/(1)')
+        self.set_descr(self.h_thetaID_ECL, "TRG ECL cluster theta ID", "unform in barrel")
+        self.set_style(self.h_thetaID_ECL, "ECL cluster TC ID", "Events/(1)")
 
         #: validation histogram
         self.h_phi_ECL = TH1F("h_phi_ECL", "TRG ECL cluster phi [2.0 degrees]", 180, -180, 180)
-        self.set_descr(self.h_phi_ECL, 'TRG ECL cluster phi distribution', 'distribute unformly')
+        self.set_descr(self.h_phi_ECL, "TRG ECL cluster phi distribution", "distribute unformly")
         self.set_style(self.h_phi_ECL, "ECL cluster #phi (degree) ", "Events/(2.0 degrees)")
 
         #: validation histogram
         self.h_sector_BKLM = TH1F("h_sector_BKLM", "BKLM TRG hit sector", 10, 0, 10)
-        self.set_descr(self.h_sector_BKLM, '# of BKLM TRG sector', 'peak at 0')
-        self.set_style(self.h_sector_BKLM, "# of BKLM TRG sector", 'Events/(1)')
+        self.set_descr(self.h_sector_BKLM, "# of BKLM TRG sector", "peak at 0")
+        self.set_style(self.h_sector_BKLM, "# of BKLM TRG sector", "Events/(1)")
 
         #: validation histogram
         self.h_sector_EKLM = TH1F("h_sector_EKLM", "EKLM TRG hit sector", 10, 0, 10)
-        self.set_descr(self.h_sector_EKLM, "# of EKLM TRG sector", 'peak at 0')
-        self.set_style(self.h_sector_EKLM, "# of EKLM TRG sector", 'Events/(1)')
+        self.set_descr(self.h_sector_EKLM, "# of EKLM TRG sector", "peak at 0")
+        self.set_style(self.h_sector_EKLM, "# of EKLM TRG sector", "Events/(1)")
 
         mc = "abs(MCParticles.m_pdg)==11&&MCParticles.m_status==11"
-        tree = ROOT.TChain('tree')
-        tree.Add('../TRGValidationGen.root')
+        tree = ROOT.TChain("tree")
+        tree.Add("../TRGValidationGen.root")
 
         #: validation histogram
         self.d_w = TH1F("d_w", "#Deltaw of CDC 2D finder, w = 0.00449/p_{t}", 50, -0.02, 0.02)
@@ -141,25 +142,25 @@ class MakePlots(basf2.Module):
         tree.Draw("({0} + 0.00449)/sqrt({1}*{1} + {2}*{2}) >> d_w_2".format(trk2d_omega, mc_px, mc_py),
                   "MCParticles.m_pdg>0 && " + mc)
         self.d_w.Add(self.d_w_2)
-        self.set_descr(self.d_w, 'Comparison on w (=0.00449/pt) of a track between CDC 2D finder output and MC.',
-                                 'A clear peak at 0 with tail.')
+        self.set_descr(self.d_w, "Comparison on w (=0.00449/pt) of a track between CDC 2D finder output and MC.",
+                                 "A clear peak at 0 with tail.")
         self.set_style(self.d_w, "#Deltaw", "Events/(0.08)")
 
         #: validation histogram
         self.d_phi = TH1F("d_phi", "#Delta#phi of CDC 2D finder", 50, -0.5, 0.5)
-        tree.Draw("TRGCDC2DFinderTracks.m_phi0-atan(MCParticles.m_momentum_y/MCParticles.m_momentum_x)>>d_phi",
+        tree.Draw("{0}-atan({1}/{2})>>d_phi".format(trk2d_phi, mc_py, mc_px),
                   "MCParticles.m_status==11&&abs(MCParticles.m_pdg)==11",
-                  "fabs(TRGCDC2DFinderTracks.m_phi0-atan(MCParticles.m_momentum_y/MCParticles.m_momentum_x))<3.1415936&&" + mc)
+                  "fabs({0}-atan({1}/{2}))<{3}&&".format(trk2d_phi, mc_py, mc_px, PI) + mc)
 
         self.d_phi_2 = TH1F("d_phi_2", "d_phi_2", 50, -0.5, 0.5)
-        tree.Draw("TRGCDC2DFinderTracks.m_phi0-atan(MCParticles.m_momentum_y/MCParticles.m_momentum_x)-3.1415936>>d_phi_2",
+        tree.Draw("{0}-atan({1}/{2})-{3}>>d_phi_2".format(trk2d_phi, mc_py, mc_px, PI),
                   "MCParticles.m_status==11&&abs(MCParticles.m_pdg)==11",
-                  "TRGCDC2DFinderTracks.m_phi0-atan(MCParticles.m_momentum_y/MCParticles.m_momentum_x)>=3.1415936 &&" + mc)
+                  "{0}-atan({1}/{2})>={3} &&".format(trk2d_phi, mc_py, mc_px, PI) + mc)
 
         self.d_phi_3 = TH1F("d_phi_3", "d_phi_3", 50, -0.5, 0.5)
-        tree.Draw("TRGCDC2DFinderTracks.m_phi0-atan(MCParticles.m_momentum_y/MCParticles.m_momentum_x)+3.1415936>>d_phi_2",
+        tree.Draw("{0}-atan({1}/{2})+{3}>>d_phi_2".format(trk2d_phi, mc_py, mc_px, PI),
                   "MCParticles.m_status==11&&abs(MCParticles.m_pdg)==11",
-                  "TRGCDC2DFinderTracks.m_phi0-atan(MCParticles.m_momentum_y/MCParticles.m_momentum_x)<=-3.1415936&&" + mc)
+                  "{0}-atan({1}/{2})<=-{3}&&".format(trk2d_phi, mc_py, mc_px, PI) + mc)
 
         self.d_phi.Add(self.d_phi_2)
         self.d_phi.Add(self.d_phi_3)
@@ -199,15 +200,14 @@ class MakePlots(basf2.Module):
 
     def event(self):
 
-        event_meta = Belle2.PyStoreObj('EventMetaData')
-        trg_summary = Belle2.PyStoreObj('TRGSummary')
-        clusters = Belle2.PyStoreArray('TRGECLClusters')
-        klmSummary = Belle2.PyStoreObj('KLMTrgSummary')
+        trg_summary = Belle2.PyStoreObj("TRGSummary")
+        clusters = Belle2.PyStoreArray("TRGECLClusters")
+        klmSummary = Belle2.PyStoreObj("KLMTrgSummary")
 
         for i in range(320):
-            if(trg_summary.testInput(i)):
+            if trg_summary.testInput(i):
                 self.hist_inbit.Fill(i + 0.5)
-            if(trg_summary.testFtdl(i)):
+            if trg_summary.testFtdl(i):
                 self.hist_outbit.Fill(i + 0.5)
 
         etot = 0
@@ -230,7 +230,7 @@ class MakePlots(basf2.Module):
         self.h_sector_EKLM.Fill(klmSummary.getEKLM_n_trg_sectors())
 
     def endRun(self):
-        print('end')
+        print("end")
 
     def terminate(self):
 
@@ -255,9 +255,8 @@ class MakePlots(basf2.Module):
 main = basf2.create_path()
 
 # INput
-root_input = basf2.register_module('RootInput')
-root_input.param('inputFileName', '../TRGValidationGen.root')
-# root_input.param('branchNames', ['EventMetaData', 'TRGECLClusters', 'TRGSummary', 'KLMTrgSummary'])
+root_input = basf2.register_module("RootInput")
+root_input.param("inputFileName", "../TRGValidationGen.root")
 main.add_module(root_input)
 
 # Make plots

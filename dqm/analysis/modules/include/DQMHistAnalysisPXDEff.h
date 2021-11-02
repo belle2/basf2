0e3a992c1bafed40a6e80c2352589cd049b993a4
Bjoern Spruck <bjoern.spruck@desy.de>
2021-11-02 09:05:04 +0100

    fix typo

--- dqm/analysis/modules/include/DQMHistAnalysisPXDEff.h
+++ dqm/analysis/modules/include/DQMHistAnalysisPXDEff.h
@@ -113,7 +113,7 @@ namespace Belle2 {
     //! one EPICS PV
     std::vector <chid>  mychid_status;
     std::map <VxdID, chid> mychid_eff;
-    std::map <VxdID, chid> mychid_lo;
+    std::map <VxdID, chid> mychid_low;
     std::map <VxdID, chid> mychid_lolo;
 #endif
   };

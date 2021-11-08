2874001bcf3d12853c080581255a8b51cfbc7128
Frank Meier <frank.meier@duke.edu>
2021-11-09 08:41:06 +0900

    fix flake8 warnings in framework package

--- framework/scripts/basf2/__init__.py
+++ framework/scripts/basf2/__init__.py
@@ -15,7 +15,6 @@ The main module of the Belle II Analysis Software Framework
 # import most things to be backwards compatible
 from basf2.core import *  # noqa
 from basf2.utils import print_params, print_path  # noqa
-from pybasf2 import logging
 import sys as _sys
 
 #: bool indicating whether we're running in an ipython instance
@@ -24,5 +23,5 @@ if _is_ipython:
     from IPython import get_ipython as _get_ipython
     if hasattr(_get_ipython(), "kernel"):
         # we're in a notebook
-        from basf2._notebooksupport import enable_notebooksupport, process
+        from basf2._notebooksupport import enable_notebooksupport, process  # noqa
         enable_notebooksupport()

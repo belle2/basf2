bc66605941b8d37aff77ba90d7d2946c3aeae417
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-11-09 22:16:22 +0100

    Add useful message showing the number of failed tests; Remove hard-coded values and define them as constants

--- daq/hbasf2/tests/test_hlt.py
+++ daq/hbasf2/tests/test_hlt.py
@@ -12,7 +12,7 @@ import basf2
 
 import zmq
 
-from zmq_daq.test_support import HLTZMQTestCase
+from zmq_daq.test_support import HLTZMQTestCase, ZMQ_TEST_FOR_LOOPS, ZMQ_TEST_MAX_FAILURES
 
 
 class HLTTestCase(HLTZMQTestCase):
@@ -449,10 +449,18 @@ class DyingHLTTestCase(HLTZMQTestCase):
 
 
 if __name__ == '__main__':
-    number_of_fails = 0
-    for i in range(5):
+    #: Number of failed for loops
+    number_of_failures = 0
+
+    for i in range(ZMQ_TEST_FOR_LOOPS):
         try:
             main(exit=False)
         except AssertionError:
-            number_of_fails += 1
-    assert(number_of_fails < 3)
+            number_of_failures += 1
+
+    #: Exit message
+    message = f'Number of failed for loops: {number_of_failures}/{ZMQ_TEST_FOR_LOOPS}'
+    if number_of_failures < ZMQ_TEST_MAX_FAILURES:
+        basf2.B2INFO(message)
+    else:
+        basf2.B2FATAL(message)

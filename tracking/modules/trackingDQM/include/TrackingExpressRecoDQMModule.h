a99e390c4eb1b026d5617375c77a11f2d5ae388f
Ludovico Massaccesi <ludovico.massaccesi@pi.infn.it>
2021-11-15 20:32:16 +0900

    Added histograms for number of events with tracking error and for abort reason to ER DQM module.

--- tracking/modules/trackingDQM/include/TrackingExpressRecoDQMModule.h
+++ tracking/modules/trackingDQM/include/TrackingExpressRecoDQMModule.h
@@ -9,6 +9,9 @@
 #pragma once
 
 #include <tracking/dqmUtils/DQMHistoModuleBase.h>
+#include <framework/datastore/StoreObjPtr.h>
+#include <mdst/dataobjects/EventLevelTrackingInfo.h>
+#include <TH1.h>
 
 namespace Belle2 {
 
@@ -29,6 +32,23 @@ namespace Belle2 {
     /** Histogram definitions such as TH1(), TH2(), TNtuple(), TTree().... are supposed to be placed in this function.
      * Also at the end function all m_histogramParameterChanges should be processed via the ProcessHistogramParameterChange function. */
     void defineHisto() override;
+
+  protected:
+    /** All the following Define- functions should be used in the defineHisto() function to define histograms. The convention is that every Define- function is responsible for creating its
+     * own TDirectory (if it's needed). In any case the function must then return to the original gDirectory.
+     * For the creation of histograms the THFFactory or the Create- functions should be used. */
+    virtual void DefineFlags();
+
+    /** Acccess to the EventLevelTrackingInfo object in the datastore. */
+    StoreObjPtr<EventLevelTrackingInfo> m_eventLevelTrackingInfo;
+
+    /** Monitors the Error flags set by the tracking code.
+     * The histogram records if any flag was set. */
+    TH1F* m_trackingErrorFlags = nullptr;
+
+    /** Monitors the Error flags set by the tracking code.
+     * The histogram has a bin for each flag. */
+    TH1F* m_trackingErrorFlagsReasons = nullptr;
 
   private:
     /** if True, the module produces the 1D Track Residual plot for each VXD sensor*/

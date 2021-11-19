43d31ae726366f1a657c9fc6ca63ccdc153c97f1
Madhav Lalwala <ph18b001@smail.iitm.ac.in>
2021-11-19 21:07:44 +0100

    Improved code quality and documentation and other changes
    Types added to function's outputs and arguments and some other changes
    to improve code quality along with added google style docstrings to
    document the code better.
    
    Improved documentation by using sphinx argparse as well as changes made
    to argument parser in the training code for better argument processing.
    And added 'Priors.rst' to 'index-01-analysis.rst'.
    
    Moved the TCCE loss to the mva package.
    
    Fixed a bug in both evaluation as well as training process to make it
    work properly when not using cuda.
    
    Fixed tests by changing inputs in evaluation process to numpy arrays as
    required.

--- analysis/doc/Priors.rst
+++ analysis/doc/Priors.rst
@@ -1,9 +1,9 @@
-.. _priorProbabilities:
+.. _PidPriorProbabilities:
 
-Prior Probabilities
-===================
+PID Prior Probabilities
+=======================
 
-This program uses the momentum and cos(theta) of particles to train a
+This tool uses the momentum and cos(:math:`\theta`) of particles to train a
 machine learning model to calculate prior probabilities which are
 particle identification probabilities before taking into account the
 detector signals. Combining this with PID's from detector signals gives
@@ -14,75 +14,40 @@ Training
 --------
 
 Training requires as input an ntuple to train the model with, which must
-contain data on particle momentum and cos(theta) as well as set of
+contain data on particle momentum and cos(:math:`\theta`) as well as 
 particle PDG values which are to be considered for training. It also
-requires path (and filename) to store model and scaling file (optional).
+requires the path (and filename) to store the trained model and scaling file (optional).
 There are some options to modify other paramters for training as well.
-The use flags for training are as follows: 
+The usage for this tool is as follows: 
 
-
-
-.. list-table:: **Required Flags**
-   :widths: 1 1
-   :header-rows: 1
-
-   * - Flag
-     - Usage
-   * - -i, --input_path
-     - Path to the ntuple for training
-
-   * - -k, --key
-     - Key of the tree to be used for training in the root file
-   * - -o, --output_path
-     - Output model file name (with path) end with ".pth"
-   * - -p, --particle_list
-     - List of particle mcPDG to be used (comma separated)
-
-.. list-table:: **Optional Flags**
-   :widths: 1 1 
-   :header-rows: 1
-   
-   * - Flag
-     - Usage
-   * - -h, --help 
-     - show help message and exit
-   * - -r, --retrain_model
-     - Path to the model for retraining
-   * - -v, --variable_list
-     - List of variable names in order cos(theta), momentum and pdg (default: cosTheta,p,mcPDG)
-   * - -lr, --learning_rate
-     - Learning rate for training (default = 1e-5)
-   * - -e, --epochs
-     - Number of epochs to be trained for (default = 64)
-   * - -t, --taylor_terms
-     - Number of terms of Taylor series of Cross Entropy Loss to be used during training. 
-       Zero (0) denotes the use of direct log function which is the default
-   * - -s, --scaling_file
-     - Path to the root file to write data for scaling for calibration purpose
-   
-Usage
-~~~~~
-
-.. code-block ::
-
-    TrainPriors.py -i ntuple.root -k data -o newmodel.pth -p 11,13,211,321 -r oldmodel.pth -v cosTheta,p,mcPDG -lr 1e-6 -e 100 -t 5 -s scale.root
+.. argparse::
+    :filename: analysis/tools/TrainPriors.py
+    :func: get_parser
+    :prog: TrainPriors.py
+    :nodefault:
 
 .. note::
          - The output during the training process will show a 
            smaller value for validation loss but this is just because the loss on training
-           set
-           includes regularization.
+           set includes regularization.
          - In case you notice overfitting or want to stop the training, 
            Ctrl+C will stop the training and create the required output files so in
            case it takes a bit of time to exit out, it may be because it is writing
-           those files. 
+           those files.
+
+.. warning::
+         - In case your ``particle_list`` contains only two particles, kindly avoid creation
+           of scaling file because ``TemperatureScaling`` which is used for calibration
+           of priors will give out only the output for the higher PDG value and so further 
+           functions like posterior calculation (as well as getting priors for a specific PDG 
+           value) will not work.
 
 Evaluation
 ----------
 
 For evaluation we have a class called Priors within EvalPriors. The user
-needs to first initialize it using the trained model using an array of
-particlelist, model and a scaling file if created. Then using relevant
+needs to first initialize it using the trained model, the particlelist for 
+which the model was trained and a scaling file (if created). Then using relevant
 input, we can get priors as well as posteriors.
 
 Import
@@ -98,38 +63,44 @@ Initialize
 
     prior = Priors(particle_list,model,scalefile)
 
-Here, ``particle_list`` is an array of the form ``[11,13,211,321]`` and
-``model`` and ``scalefile`` are paths to the model and scaling data file respectively which are of the form ``'/path/model_name.pth'`` and ``'/path/scalefile.root'`` respectively. However, the use of scaling file for calibration is optional.
+Here, ``particle_list`` is a list of the form ``[11,13,211,321]`` and
+``model`` and ``scalefile`` are paths to the model and scaling data file 
+respectively which are of the form ``'/path/model_name.pth'`` and 
+``'/path/scalefile.root'`` respectively. However, the use of scaling file 
+for calibration is optional.
 
-.. note ::
-          To use the scaling file for calibration, you are required to additionally install the netcal module using pip.
+.. note::
+          To use the scaling file for calibration, you are required to 
+          additionally install the netcal module using pip.
 
 Prior Calculation
 ~~~~~~~~~~~~~~~~~
 
 .. code-block:: python
 
-    prior.calculatePriors(momentum,cosTheta)
+    prior.calculate_priors(momentum,cosTheta)
 
-Here, ``momentum`` and ``cosTheta`` are arrays.
+Here, ``momentum`` and ``cosTheta`` are numpy arrays.
 
-Getting the priors
+Getting the Priors
 ~~~~~~~~~~~~~~~~~~
+
 .. code-block:: python
 
-    prior.getPriors(pdg)
+    prior.get_priors(pdg)
 
-This returns a 1D array of calculated priors for the given PDG value.
+This returns a 1D array of calculated priors for the given ``pdg`` value.
 However, this is an optional argument and specifying nothing returns a
 2D array of priors arranged in ascending order of PDG values.
 
-Getting posteriors
-~~~~~~~~~~~~~~~~~~
+Getting the Posteriors
+~~~~~~~~~~~~~~~~~~~~~~
+
 .. code-block:: python
 
-    prior.getPosterior(pid,pdg)
+    prior.get_posterior(pid,pdg)
 
-Again PDG is an optional argument but PID likelihoods must be provided
+Again ``pdg`` is an optional argument but ``pid`` likelihoods must be provided
 as 2D array containing likelihoods for the particles in
 ``particle_list`` with PDG values taken in **ascending order**.
 	
--- analysis/tools/TrainPriors.py
+++ analysis/tools/TrainPriors.py
@@ -2,33 +2,80 @@
 
 # torch imports
 import torch
-from torch.nn import CrossEntropyLoss
-from torch.nn import Softmax
 from torch.optim import Adam
 from torch.utils.data import DataLoader
-import torch.nn as nn
-from torch.nn import functional as F
 
 # custom taylor cross entropy function
-from TCCE import TCCE
+from basf2_mva_extensions.torch_TCCE import TCCE
 
 # other imports
-import uproot as ur
+import uproot3 as ur
 import numpy as np
 from sklearn.metrics import accuracy_score
-import pandas as pd
 import argparse
-import re
 import sys
 
 # Model and DataLoader
+
 from PriorDataLoaderAndModel import PriorModel
 from PriorDataLoaderAndModel import PriorDataLoader
+
+# description
+
+"""
+This tool can be used to train a pytorch model to calculate prior probabilities for particle identification.
+
+This tool uses a root file containing momentum and cos(theta) (theta is the angle made by momentum vector with the beam axis)
+and trains a pytorch model (imported as PriorModel) by taking a second order combination of these variables along with the
+transverse momentum (thus giving 9 total features) which is done by the PriorDataLoader.
+The program then outputs a '.pth' model file along with a scaling file (if the path is provided and if not this step is skipped).
+The required input is taken as flags when using the program and these inputs are listed below.
+
+Parameters:
+    input_path (str): The path to the data file containing the data to train the model with.
+    output_path (str): The path to the output model file that is generated after training.
+    key (str): The key to the tree containing data within the root file.
+    particle_list (int): List of mcPDG values to train the model with.
+    variable_list (optional) (str): List of variables in the order cos(theta), momentum and PDG.
+    taylor_terms (optional) (int): Number of Taylor series terms of categorical cross entropy loss to be used for training.
+    learn_rate (optional) (float): The learning rate to be used for training the model.
+    retrain_model (optional) (str): The path to an already trained model to be used for further training.
+    scale_file (optional) (str): The path where a root file for temperature scaling must be stored for calibration purposes.
+    epochs (optional) (int): The number of epochs for training the model.
+
+Note:
+    The parameters are taken as flags through the arguments parser.
+
+Example:
+    TrainPriors.py -i /path/to/datafile.root -o /path/to/model.pth -p 11 13 211 321 -k tree -v cosTheta p mcPDG
+    -t 20 -lr 1e-6 -r /path/to/alreadyTrainedModel.pth -s /path/to/scalefile.root -e 10
+"""
 
 # model training
 
 
-def mod_train(train_dl, model, test_dl, learn_rate, ep, terms):
+def mod_train(
+    train_dl: DataLoader,
+    model: PriorModel,
+    test_dl: DataLoader,
+    learn_rate: float,
+    ep: int,
+    terms: int,
+):
+    """
+    Trains pytorch model for calculating PID priors.
+
+    Parameters:
+        train_dl (DataLoader): The training data loader.
+        model (PriorModel): The torch model that is trained on the training set.
+        test_dl (DataLoader): Data loader for validation set.
+        learn_rate (float): The learning rate for the model.
+        ep (int): Number of epochs to train the model for.
+        terms (int): Number of terms for the taylor cross entropy loss with 0 denoting log loss.
+
+    Returns:
+        None.
+    """
     optimizer = Adam(model.parameters(), lr=learn_rate)
     l_train = len(train_dl)
     l_test = len(test_dl)
@@ -42,8 +89,8 @@ def mod_train(train_dl, model, test_dl, learn_rate, ep, terms):
         for i, (inputs, targets) in enumerate(train_dl):
             optimizer.zero_grad()
             if torch.cuda.is_available():
-                inputs = inputs.to('cuda')
-                targets = targets.to('cuda')
+                inputs = inputs.to("cuda")
+                targets = targets.to("cuda")
             y_out = model(inputs)
             y = torch.hstack((y_out, targets.reshape(-1, 1)))
             tcce = TCCE(terms)
@@ -53,15 +100,19 @@ def mod_train(train_dl, model, test_dl, learn_rate, ep, terms):
             all_lin2_param = torch.cat([x.view(-1) for x in model.hidden3.parameters()])
             all_lin3_param = torch.cat([x.view(-1) for x in model.hidden5.parameters()])
             all_lin4_param = torch.cat([x.view(-1) for x in model.hidden7.parameters()])
-            l1_regularization = lam1 * (torch.norm(all_lin1_param, 1) + torch.norm(all_lin2_param, 1) +
-                                        torch.norm(all_lin3_param, 1) + torch.norm(all_lin4_param, 1))
+            l1_regularization = lam1 * (
+                torch.norm(all_lin1_param, 1)
+                + torch.norm(all_lin2_param, 1)
+                + torch.norm(all_lin3_param, 1)
+                + torch.norm(all_lin4_param, 1)
+            )
             loss = loss + l1_regularization
             loss.backward()
             optimizer.step()
             if i != l_train - 1:
-                print("The loss is: %0.3f" % loss.item(), end='\r')
+                print("The loss is: %0.3f" % loss.item(), end="\r")
             else:
-                print("The loss is: %0.3f" % loss.item(), end='\n')
+                print("The loss is: %0.3f" % loss.item(), end="\n")
         model = model.eval()
 
         with torch.no_grad():
@@ -69,9 +120,9 @@ def mod_train(train_dl, model, test_dl, learn_rate, ep, terms):
             testloss = 0
             for i, (inputs, targets) in enumerate(test_dl):
                 if torch.cuda.is_available():
-                    inputs = inputs.to('cuda')
+                    inputs = inputs.to("cuda")
                 y_out = model(inputs)
-                y_max = [np.argmax(e) for e in y_out.to('cpu').detach().numpy()]
+                y_max = [np.argmax(e) for e in y_out.to("cpu").detach().numpy()]
                 acc += accuracy_score(y_max, targets.detach().numpy())
                 if i != l_test - 1:
                     testloss += tcce(y_out, targets) / (l_test - 1)
@@ -88,144 +139,179 @@ def mod_train(train_dl, model, test_dl, learn_rate, ep, terms):
             t = 4
             backup_model = model
 
+
 # preparing data for training
 
 
-def prep_dat(path, key, particlelist, labels):
+def prep_dat(path: int, key: int, particlelist: list, labels: list) -> DataLoader:
+    """
+    Prepares dataloaders for training and validation set.
+
+    Parameters:
+        path (str): The path to the data file to train the model with.
+        key (str): The key of the tree in the root file with the data required for training.
+        particlelist (list(int)): List of particles to be considered for training purposes.
+        labels (listi(str)): The list of labels for variables in the order cos(theta), momentum, and mcPDG.
+
+    Returns:
+        The data loaders for training and validation set to be used for training.
+    """
     data = PriorDataLoader(path, key, particlelist, labels)
     train, test = data.get_split()
     train_dl = DataLoader(train, batch_size=512, shuffle=True)
     test_dl = DataLoader(test, batch_size=1024, shuffle=False)
     return train_dl, test_dl
 
+
 # for evaluation to write the scaling file
 
 
-def evalmodel(model, datload, n):
+def eval_model(model: PriorModel, datload: DataLoader) -> np.array:
+    """
+    Evaluates the model output for the model with the given dataset.
+
+    Parameters:
+        model (PriorModel): The trained torch model.
+        datload (DataLoader): Dataloader containing labels as well as features of the data set.
+
+    Returns:
+        Numpy arrays containing probabilities as well as truth values to be stored in a scaling file
+        for calibration purposes when calculating priors.
+    """
+
     prob = []
     true = []
     model = model.eval()
     for i, (inputs, test) in enumerate(datload):
-        y = model(inputs.to('cuda')).to('cpu').detach().numpy()
+        if torch.cuda.is_available():
+            y = model(inputs.to("cuda")).to("cpu").detach().numpy()
+        else:
+            y = model(inputs).detach().numpy()
         true.append(test)
         prob.append(y)
     return np.concatenate(prob), np.concatenate(true)
 
 
-def main():
+def get_parser() -> argparse.ArgumentParser:
+    """
+    Argument parser.
+
+    Parameters:
+        None.
+
+    Returns:
+        Argument parser.
+    """
     ap = argparse.ArgumentParser()
-    required_args = ap.add_argument_group('required arguements')
+    required_args = ap.add_argument_group("Required Arguments")
     required_args.add_argument(
-        '-i',
-        '--input_path',
-        help='Path to ntuples',
-        required=True
-    )
-    ap.add_argument(
-        '-r',
-        '--retrain_model',
-        help='Path to the model for retraining',
-        default=""
-    )
-    ap.add_argument(
-        '-v',
-        '--variable_list',
-        help='List of variable names in order cos(theta), momentum and pdg (default: cosTheta,p,mcPDG)',
-        default="cosTheta,p,mcPDG"
+        "-i", "--input_path", help="Path to ntuples conataining data for training.", required=True
     )
     required_args.add_argument(
-        '-p',
-        '--particle_list',
-        help='List of particle mcPDG to be used (comma separated)',
-        required=True
+        "-p",
+        "--particle_list",
+        help="List of particle mcPDG for which priors are to be calculated.",
+        type=int,
+        required=True,
+        nargs="+",
     )
     required_args.add_argument(
-        '-o',
-        '--output_path',
-        help='Output model file name (with path)',
-        required=True
+        "-o", "--output_path", help="Output model file name (with path).", required=True
     )
     required_args.add_argument(
-        '-k',
-        '--key',
-        help='Key of the tree to be used for training in the root file',
-        required=True
-    )
-    ap.add_argument(
-        '-lr',
-        '--learning_rate',
-        help='Learning rate for training (default = 1e-5)',
-        default=1e-5
-    )
-    ap.add_argument(
-        '-e',
-        '--epochs',
-        help='Number of epochs to be trained for (default = 64)',
-        default=64
-    )
-    ap.add_argument(
-        '-t',
-        '--taylor_terms',
-        help='Number of terms for Taylor series of cross entropy loss (default = 0 (takes log loss instead of taylor series))',
-        default=0
-    )
-    ap.add_argument(
-        '-s',
-        '--scaling_file',
-        help='Path to the root file to write data for scaling',
-        default=""
+        "-k",
+        "--key",
+        help="Key of the tree to be used for training in the root file.",
+        required=True,
     )
 
-    args = ap.parse_args()
+    ap.add_argument("-r", "--retrain_model", help="Path to the model for retraining.")
+    ap.add_argument(
+        "-v",
+        "--variable_list",
+        help="List of variable names in order cos(theta), momentum and pdg (Default: cosTheta p mcPDG).",
+        default=["cosTheta", "p", "mcPDG"],
+        nargs=3,
+    )
+    ap.add_argument(
+        "-lr",
+        "--learning_rate",
+        help="Learning rate for training (Default: 1e-5).",
+        default=1e-5,
+        type=float,
+    )
+    ap.add_argument(
+        "-e",
+        "--epochs",
+        help="Number of epochs to be trained for (Default: 64).",
+        default=64,
+        type=int,
+    )
+    ap.add_argument(
+        "-t",
+        "--taylor_terms",
+        help="Number of terms for Taylor series of cross entropy loss (Default: 0 (takes log loss instead of taylor series)).",
+        default=0,
+        type=int,
+    )
+    ap.add_argument(
+        "-s", "--scaling_file", help="Path to the root file to write data for scaling."
+    )
+    return ap
+
+
+def main():
+    description = __file__.__doc__
+    args = get_parser().parse_args()
     path = args.input_path
-    labels = re.split(',', args.variable_list)
-    particlelist = np.array(re.split(',', args.particle_list)).astype(int)
+    labels = args.variable_list
+    particlelist = np.array(args.particle_list)
     output = args.output_path
     key = args.key
     old_model = args.retrain_model
-    lr = float(args.learning_rate)
-    tt = int(args.taylor_terms)
-    epoch = int(args.epochs)
+    lr = args.learning_rate
+    tt = args.taylor_terms
+    epoch = args.epochs
     scalefile = args.scaling_file
 
     # preparing the data and model
     train_dl, test_dl = prep_dat(path, key, particlelist, labels)
     model = PriorModel(len(particlelist))
     if torch.cuda.is_available():
-        model.to('cuda')
-    if old_model != "":
+        model.to("cuda")
+    if old_model is not None:
         model.load_state_dict(torch.load(old_model))
 
     # to allow the user to exit out of the training if earlystopping does not stop overfitting
     try:
         mod_train(train_dl, model, test_dl, lr, epoch, tt)
         torch.save(model.state_dict(), output)
-        if scalefile != "":
-            y_prob, y_true = evalmodel(model, train_dl, len(particlelist))
+        if scalefile is not None:
+            y_prob, y_true = eval_model(model, train_dl)
             bdict = {}
             for p in particlelist:
-                bdict[str(p) + '_pdg'] = "float64"
+                bdict[f"{p}_pdg"] = "float64"
                 bdict["truth"] = "int32"
             with ur.recreate(scalefile) as f:
                 f["scale"] = ur.newtree(bdict)
                 datadict = {"truth": y_true}
                 for p in range(len(particlelist)):
-                    datadict[str(np.sort(particlelist)[p]) + '_pdg'] = y_prob[:, p]
+                    datadict[str(np.sort(particlelist)[p]) + "_pdg"] = y_prob[:, p]
                 f["scale"].extend(datadict)
 
     except KeyboardInterrupt:
         torch.save(model.state_dict(), output)
-        if scalefile != "":
-            y_prob, y_true = evalmodel(model, train_dl, len(particlelist))
+        if scalefile is not None:
+            y_prob, y_true = eval_model(model, train_dl)
             bdict = {}
             for p in particlelist:
-                bdict[str(p) + '_pdg'] = "float64"
+                bdict[f"{p}_pdg"] = "float64"
             bdict["truth"] = "int32"
             with ur.recreate(scalefile) as f:
                 f["scale"] = ur.newtree(bdict)
                 datadict = {"truth": y_true}
                 for p in range(len(particlelist)):
-                    datadict[str(np.sort(particlelist)[p]) + '_pdg'] = y_prob[:, p]
+                    datadict[str(np.sort(particlelist)[p]) + "_pdg"] = y_prob[:, p]
                 f["scale"].extend(datadict)
         sys.exit()
 

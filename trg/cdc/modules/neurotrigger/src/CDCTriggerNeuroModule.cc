99551f7977935ffe4f9b5cc99ac99ab890b9d1fc
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2021-12-14 22:18:47 +0900

    Fix a -Wunused-variable warning in CDCTriggerNeuro module

--- trg/cdc/modules/neurotrigger/src/CDCTriggerNeuroModule.cc
+++ trg/cdc/modules/neurotrigger/src/CDCTriggerNeuroModule.cc
@@ -147,9 +147,6 @@ CDCTriggerNeuroModule::event()
     // get the hit pattern (depends on phase space sector)
     unsigned long hitPattern =
       m_NeuroTrigger.getInputPattern(geoSectors[0], *m_tracks2D[itrack], m_neuroTrackInputMode);
-    // get the complete hit pattern for debug purposes
-    unsigned long chitPattern =
-      m_NeuroTrigger.getCompleteHitPattern(geoSectors[0], *m_tracks2D[itrack], m_neuroTrackInputMode);
     // get the pure driftthreshold vector
     unsigned long puredriftth =
       m_NeuroTrigger.getPureDriftThreshold(geoSectors[0], *m_tracks2D[itrack], m_neuroTrackInputMode);

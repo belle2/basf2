dd755a97bcace65e6efffe60619c7cb0a9e5e521
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2022-01-14 01:31:44 +0900

    Fix the test by adding configure_logging_for_tests; fix the documentation

--- generators/tests/check_rho_mass_shift.py
+++ generators/tests/check_rho_mass_shift.py
@@ -17,8 +17,19 @@ import b2test_utils
 
 if __name__ == '__main__':
 
+    b2test_utils.configure_logging_for_tests()
+
     steering_file = basf2.find_file('generators/tests/check_rho_mass_shift.py_noexec')
 
     for i in [1, 8, 9, 11, 13]:
         with b2test_utils.clean_working_directory():
-            subprocess.check_call(['basf2', steering_file, str(i)])
+
+            # This may look strange, but configure_logging_for_tests() does not act on the path of the steering file,
+            # that is the first line printed by basf2.
+            # So, we capture the log of the subprocess and we print it: in this way, the configure_loggging_etc
+            # in this process will fix the log of the subprocess, otherwise this test fails on our CI systems.
+            process = subprocess.run(['basf2', steering_file, str(i)],
+                                     capture_output=True,
+                                     text=True,
+                                     check=True)
+            print(process.stdout)

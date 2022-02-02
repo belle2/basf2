4ecb3a856890bc401e4b5334949846fa8179c241
Frank Meier <frank.meier@duke.edu>
2022-02-02 01:09:06 +0100

    replace daughterInvariantMass with daughterInvM in mva examples

--- mva/examples/python/sklearn_mlpclassifier.py
+++ mva/examples/python/sklearn_mlpclassifier.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
                  'daughter(0, dz)', 'daughter(1, dz)',
                  'daughter(0, chiProb)', 'daughter(1, chiProb)', 'daughter(2, chiProb)',
                  'daughter(0, kaonID)', 'daughter(0, pionID)',
-                 'daughterInvariantMass(0, 1)', 'daughterInvariantMass(0, 2)', 'daughterInvariantMass(1, 2)']
+                 'daughterInvM(0, 1)', 'daughterInvM(0, 2)', 'daughterInvM(1, 2)']
 
     # Train a MVA method and directly upload it to the database
     general_options = basf2_mva.GeneralOptions()

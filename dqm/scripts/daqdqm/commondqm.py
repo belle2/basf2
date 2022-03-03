f7d2c3712391f513e4c4e2297bc3719a4b7f36c9
Bjoern Spruck <bjoern.spruck@desy.de>
2022-03-03 18:53:10 +0100

    enable TTD histograms in HLT and ERECO

--- dqm/scripts/daqdqm/commondqm.py
+++ dqm/scripts/daqdqm/commondqm.py
@@ -42,6 +42,9 @@ def add_common_dqm(path, components=None, dqm_environment="expressreco", dqm_mod
     # Check components.
     check_components(components)
 
+    # TTD trigger and bunch injection monitoring
+    path.add_module('TTDDQM')
+
     if dqm_environment == "expressreco" and (dqm_mode in ["dont_care"]):
         # PXD (not useful on HLT)
         if components is None or 'PXD' in components:

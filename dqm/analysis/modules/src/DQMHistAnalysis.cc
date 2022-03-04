8a1d8b713637dfee34819d78abd196e4c982be61
Bjoern Spruck <bjoern.spruck@desy.de>
2022-03-04 10:57:40 +0100

    removed unused variables and functions

--- dqm/analysis/modules/src/DQMHistAnalysis.cc
+++ dqm/analysis/modules/src/DQMHistAnalysis.cc
@@ -7,7 +7,7 @@
  **************************************************************************/
 //+
 // File : DQMHistAnalysisModule.cc
-// Description : DQM histgram analysis module
+// Description : Baseclass for DQM histogram analysis module
 //-
 
 #include <dqm/analysis/modules/DQMHistAnalysis.h>
@@ -28,10 +28,6 @@ REG_MODULE(DQMHistAnalysis)
 //                 Implementation
 //-----------------------------------------------------------------
 
-DQMHistAnalysisModule::ParamTypeList DQMHistAnalysisModule::g_parname;
-DQMHistAnalysisModule::IntValueList DQMHistAnalysisModule::g_vint;
-DQMHistAnalysisModule::FloatValueList DQMHistAnalysisModule::g_vfloat;
-DQMHistAnalysisModule::TextList DQMHistAnalysisModule::g_text;
 DQMHistAnalysisModule::HistList DQMHistAnalysisModule::g_hist;
 DQMHistAnalysisModule::MonObjList DQMHistAnalysisModule::g_monObj;
 
@@ -178,40 +174,3 @@ MonitoringObject* DQMHistAnalysisModule::findMonitoringObject(const std::string&
   B2INFO("MonitoringObject " << objName << " not in memfile.");
   return NULL;
 }
-
-void DQMHistAnalysisModule::setIntValue(const std::string& parname, int vint)
-{
-  if (g_parname.find(parname) == g_parname.end() && g_vint.find(parname) == g_vint.end()) {
-    g_parname.insert(ParamTypeList::value_type(parname, c_ParamINT));
-    g_vint.insert(IntValueList::value_type(parname, vint));
-  } else if (g_vint.find(parname) == g_vint.end()) {
-    B2ERROR(parname + " is already registered as non-int data type");
-  } else {
-    g_vint[parname] = vint;
-  }
-}
-
-void DQMHistAnalysisModule::setFloatValue(const std::string& parname, float vfloat)
-{
-  if (g_parname.find(parname) == g_parname.end() && g_vfloat.find(parname) == g_vfloat.end()) {
-    g_parname.insert(ParamTypeList::value_type(parname, c_ParamFLOAT));
-    g_vfloat.insert(FloatValueList::value_type(parname, vfloat));
-  } else if (g_vfloat.find(parname) == g_vfloat.end()) {
-    B2ERROR(parname + " is already registered as non-float data type");
-  } else {
-    g_vfloat[parname] = vfloat;
-  }
-}
-
-void DQMHistAnalysisModule::setText(const std::string& parname, const std::string& text)
-{
-  if (g_parname.find(parname) == g_parname.end() && g_text.find(parname) == g_text.end()) {
-    g_parname.insert(ParamTypeList::value_type(parname, c_ParamTEXT));
-    g_text.insert(TextList::value_type(parname, text));
-  } else if (g_text.find(parname) == g_text.end()) {
-    B2ERROR(parname + " is already registered as non-text data type");
-  } else {
-    g_text[parname] = text;
-  }
-}
-

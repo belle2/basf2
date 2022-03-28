d5c6a8037708ff95940c98eebafaca5f3c9f6f50
Giulia Casarosa <giulia.casarosa@pi.infn.it>
2022-03-29 01:17:59 +0900

    SVD " from now on we are using " is now B2DEBUG instead of B2INFO

--- svd/calibration/include/SVDChargeSimulationCalibrations.h
+++ svd/calibration/include/SVDChargeSimulationCalibrations.h
@@ -29,7 +29,7 @@ namespace Belle2 {
     SVDChargeSimulationCalibrations(): m_aDBObjPtr(name)
     {
       m_aDBObjPtr.addCallback([ this ](const std::string&) -> void {
-        B2INFO("SVDChargeSimulationCalibrations: from now on we are using " <<
+        B2DEBUG(20, "SVDChargeSimulationCalibrations: from now on we are using " <<
         this->m_aDBObjPtr -> get_uniqueID()); });
 
     }

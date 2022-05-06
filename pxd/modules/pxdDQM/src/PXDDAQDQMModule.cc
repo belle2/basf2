85078774badfb987e95d6403f438c2e8075d396b
Yubo Han <yubo.han@desy.de>
2022-05-06 10:21:55 +0200

    change the name and use '1U'

--- pxd/modules/pxdDQM/src/PXDDAQDQMModule.cc
+++ pxd/modules/pxdDQM/src/PXDDAQDQMModule.cc
@@ -211,8 +211,7 @@ void PXDDAQDQMModule::event()
       }
       unsigned int cmask = dhc.getEndErrorInfo();
       for (int i = 0; i < 32; i++) {
-        unsigned int mask_pre = 1;
-        unsigned int mask = (mask_pre << i);
+        unsigned int mask = (1U << i);
         if ((cmask & mask) == mask) hDAQEndErrorDHC->Fill(dhc.getDHCID(), i);
       }
       if (hDAQDHCReduction[dhc.getDHCID()]) {

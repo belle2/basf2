6ddf17fdc19f6bdbc7d67ec5cd464762e3d1ce00
longos <savino.longo@desy.de>
2022-05-10 09:35:41 +0200

    fix cpp check warning

--- ecl/modules/eclWaveformCalibCollector/src/eclWaveformCalibCollectorModule.cc
+++ ecl/modules/eclWaveformCalibCollector/src/eclWaveformCalibCollectorModule.cc
@@ -26,7 +26,7 @@ using namespace Belle2;
 //-----------------------------------------------------------------
 //                 Register the Module
 //-----------------------------------------------------------------
-REG_MODULE(eclWaveformCalibCollector)
+REG_MODULE(eclWaveformCalibCollector);
 
 //-----------------------------------------------------------------
 //                 Implementation

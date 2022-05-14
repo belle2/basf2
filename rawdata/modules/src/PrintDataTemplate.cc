47ec1000f42c5ed3b3e002e06a6b1ffdc1adc9cb
Frank Meier <frank.meier@duke.edu>
2022-05-15 04:23:03 +0900

    fix unknownMacro cppcheck warnings in rawdata package

--- rawdata/modules/src/PrintDataTemplate.cc
+++ rawdata/modules/src/PrintDataTemplate.cc
@@ -19,7 +19,7 @@ using namespace Belle2;
 //-----------------------------------------------------------------
 //                 Register the Module
 //-----------------------------------------------------------------
-REG_MODULE(PrintDataTemplate)
+REG_MODULE(PrintDataTemplate);
 
 //-----------------------------------------------------------------
 //                 Implementation

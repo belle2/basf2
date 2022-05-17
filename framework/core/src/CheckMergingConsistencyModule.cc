0a7bb9f6ceccf2cf0b3f082fcb76af9db631a8c3
Sasha Glazov <glazov@mail.desy.de>
2022-05-17 21:54:39 +0200

    fixing doxygen error

--- framework/core/src/CheckMergingConsistencyModule.cc
+++ framework/core/src/CheckMergingConsistencyModule.cc
@@ -8,71 +8,69 @@
 
 #include <framework/core/CheckMergingConsistencyModule.h>
 
-namespace Belle2 {
+using namespace Belle2;
 
-  //-----------------------------------------------------------------
-  //                 Register module
-  //-----------------------------------------------------------------
+//-----------------------------------------------------------------
+//                 Register module
+//-----------------------------------------------------------------
+REG_MODULE(CheckMergingConsistency);
 
-  REG_MODULE(CheckMergingConsistency);
+//-----------------------------------------------------------------
+//                 Implementation
+//-----------------------------------------------------------------
 
-  //-----------------------------------------------------------------
-  //                 Implementation
-  //-----------------------------------------------------------------
+CheckMergingConsistencyModule::CheckMergingConsistencyModule() : Module()
+{
+  setDescription("This module checks if events that will be merged are consistent (otherwise events will be skipped later).");
 
-  CheckMergingConsistencyModule::CheckMergingConsistencyModule() : Module()
-  {
-    setDescription("This module checks if events that will be merged are consistent (otherwise events will be skipped later).");
+  setPropertyFlags(c_ParallelProcessingCertified);
+}
 
-    setPropertyFlags(c_ParallelProcessingCertified);
+CheckMergingConsistencyModule::~CheckMergingConsistencyModule() = default;
+
+void CheckMergingConsistencyModule::init(const std::string& option, bool eventMixing)
+{
+  m_option = option;
+  m_eventMixing = eventMixing;
+}
+
+void CheckMergingConsistencyModule::initialize()
+{
+  m_mergedEventConsistency.registerInDataStore(DataStore::c_DontWriteOut);
+  if (!m_EventExtraInfo_main.isOptional() or !m_EventExtraInfo_indep.isOptional()) {
+    B2WARNING("EventExtraInfo does not exist in both paths. Cannot check events for consistency.");
   }
 
-  CheckMergingConsistencyModule::~CheckMergingConsistencyModule() = default;
-
-  void CheckMergingConsistencyModule::init(const std::string& option, bool eventMixing)
-  {
-    m_option = option;
-    m_eventMixing = eventMixing;
+  if (!m_option.empty() && m_option != "charge") {
+    B2ERROR("Only available options for consistency check are 'charge' or no check at all.");
   }
+}
 
-  void CheckMergingConsistencyModule::initialize()
-  {
-    m_mergedEventConsistency.registerInDataStore(DataStore::c_DontWriteOut);
-    if (!m_EventExtraInfo_main.isOptional() or !m_EventExtraInfo_indep.isOptional()) {
-      B2WARNING("EventExtraInfo does not exist in both paths. Cannot check events for consistency.");
-    }
+void CheckMergingConsistencyModule::event()
+{
+  m_mergedEventConsistency.create();
+  m_mergedEventConsistency->setConsistent(true);
 
-    if (!m_option.empty() && m_option != "charge") {
-      B2ERROR("Only available options for consistency check are 'charge' or no check at all.");
-    }
-  }
-
-  void CheckMergingConsistencyModule::event()
-  {
-    m_mergedEventConsistency.create();
-    m_mergedEventConsistency->setConsistent(true);
-
-    if (m_option == "charge") {
-      if (m_EventExtraInfo_main.isValid() && m_EventExtraInfo_indep.isValid()) {
-        if (!m_EventExtraInfo_main->hasExtraInfo("charge") || !m_EventExtraInfo_indep->hasExtraInfo("charge")) {
-          B2ERROR("No EventExtraInfo 'charge' found.");
-          return;
+  if (m_option == "charge") {
+    if (m_EventExtraInfo_main.isValid() && m_EventExtraInfo_indep.isValid()) {
+      if (!m_EventExtraInfo_main->hasExtraInfo("charge") || !m_EventExtraInfo_indep->hasExtraInfo("charge")) {
+        B2ERROR("No EventExtraInfo 'charge' found.");
+        return;
+      }
+      int charge_main = m_EventExtraInfo_main->getExtraInfo("charge");
+      int charge_indep = m_EventExtraInfo_indep->getExtraInfo("charge");
+      if (m_eventMixing) {
+        // charge of both removed tags has to be opposite
+        if (charge_main == charge_indep) {
+          m_mergedEventConsistency->setConsistent(false);
         }
-        int charge_main = m_EventExtraInfo_main->getExtraInfo("charge");
-        int charge_indep = m_EventExtraInfo_indep->getExtraInfo("charge");
-        if (m_eventMixing) {
-          // charge of both removed tags has to be opposite
-          if (charge_main == charge_indep) {
-            m_mergedEventConsistency->setConsistent(false);
-          }
-        } else {
-          // charge of embedded object has to correspond to charge of removed tag
-          if (charge_main != charge_indep) {
-            m_mergedEventConsistency->setConsistent(false);
-          }
+      } else {
+        // charge of embedded object has to correspond to charge of removed tag
+        if (charge_main != charge_indep) {
+          m_mergedEventConsistency->setConsistent(false);
         }
       }
     }
   }
-} // end Belle2 namespace
+}
 

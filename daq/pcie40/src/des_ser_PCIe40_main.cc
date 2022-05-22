a9dc49f497a9c829fa1567383570cd5a96550b5c
Yamadas <satoru.yamada@kek.jp>
2022-05-22 11:43:20 +0900

    Add include/des_ser_PCIe40_main.h .

--- daq/pcie40/src/des_ser_PCIe40_main.cc
+++ daq/pcie40/src/des_ser_PCIe40_main.cc
@@ -1,113 +1,7 @@
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <time.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <limits.h>
-#include <signal.h>
-#include <poll.h>
-#include <netdb.h>
-#include <vector>
-#include <set>
-#include <map>
-#include <cstring>
-#include <omp.h>
-#include <mqueue.h>
-#include <sys/wait.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/types.h>
-#include <fstream>
-#include <errno.h>
-#include "pcie40_b2dma.h"
-#include "dma_driverlib.h"
-#include "pcie40_s_pages.h"
-#include <rawdata/dataobjects/RawHeader_latest.h>
-#include <rawdata/dataobjects/RawTrailer_latest.h>
-#include <rawdata/dataobjects/PreRawCOPPERFormat_latest.h>
-#include <rawdata/dataobjects/PostRawCOPPERFormat_latest.h>
-
-#define USE_ZMQ
-#ifdef USE_ZMQ
-#include "zmq.hpp"
-#endif
-
-/////////////////////////////////////////
-// Parameter for operation
-////////////////////////////////////////
-//#define NOT_SEND
-#define NUM_CLIENTS_PER_THREAD 1
-#define NUM_SENDER_THREADS 5
-//#define NUM_SENDER_THREADS 1
-//#define NO_ERROR_STOP
-
-#define NODE_ID_TEST_BENCH 0x99000001
-/////////////////////////////////////////
-// Parameter for dummy-data
-////////////////////////////////////////
-//#define DUMMY_REDUCED_DATA
-
-/////////////////////////////////////////
-// Parameter for split ECL and ECLTRG data on recl3
-////////////////////////////////////////
-//#define SPLIT_ECL_ECLTRG
-#ifdef SPLIT_ECL_ECLTRG
-#define NUM_SUB_EVE 2
-#else
-#define NUM_SUB_EVE 1
-#endif // #ifdef SPLIT_ECL_ECLTRG
-
-/////////////////////////////////////////
-// Parameter for data-contents
-////////////////////////////////////////
-
-//
-// Format (PCIe40)
-//
-#define NW_RAW_HEADER 56
-#define NW_RAW_TRAILER 4
-#define NW_REDUCED_B2L_HEADER 2
-#define NW_REDUCED_B2L_TRAILER 2
-#define NW_NONREDUCED_B2L_HEADER 6
-#define NW_NONREDUCED_B2L_TRAILER 3
-
-//
-// Sender format
-//
-#define NW_SEND_HEADER 6
-#define NW_SEND_TRAILER 2
-
-
-//
-// return value of checkEventData()
-//
-#define DATACHECK_OK 0
-#define DATACHECK_NG 1
-#define DATACHECK_OK_BUT_ERRFLAG_IN_HDR 2
-
-//
-// Dummy data contents
-//
-#define CTIME_VAL 0x12345601
-#ifdef DUMMY_REDUCED_DATA
-#define NW_B2L_HEADER 3
-#define NW_B2L_TRAILER 2
-#else
-#define NW_B2L_HEADER 7
-#define NW_B2L_TRAILER 3
-#endif
-
+#include "des_ser_PCIe40_main.h"
 using namespace std;
 
 #ifdef SPLIT_ECL_ECLTRG
-#define ECLTRG_NODE_ID 0x13000001
 const std::vector<int> splitted_ch {16}; // recl3: cpr6001-6008,cpr13001 (0-15,16ch)
 #endif
 
@@ -125,16 +19,6 @@ pthread_mutex_t mtx_sender_log;
 int buffer_filled[NUM_SENDER_THREADS][2];
 int copy_nwords[NUM_SENDER_THREADS][2];
 #endif
-
-struct sender_argv {
-  int sender_id;
-  int run_no;  // For dummy data
-  int nwords_per_fee; // For dummy data
-  int ncpr; // For dummy data
-  int nhslb; // For dummy data
-  unsigned int node_id;
-  std::vector< int > valid_ch;
-};
 
 #ifdef USE_ZMQ
 /////////////////////////////////////////////////////////
@@ -182,40 +66,6 @@ unsigned int err_bad_linksize[NUM_SENDER_THREADS] = {0};
 unsigned int err_link_eve_jump[NUM_SENDER_THREADS] = {0};
 unsigned int crc_err_ch[NUM_SENDER_THREADS][ MAX_PCIE40_CH];
 
-// format of ROB header
-#define OFFSET_HDR 0
-#define EVENT_LEN_POS 0+OFFSET_HDR
-#define MAGIC_7F7F_POS 1+OFFSET_HDR
-#define RUNNO_POS 2+OFFSET_HDR
-#define EVENUM_POS 3+OFFSET_HDR
-#define CTIME_POS 4+OFFSET_HDR
-#define UTIME_POS 5+OFFSET_HDR
-#define NODEID_POS 6+OFFSET_HDR
-#define ERR_POS 7+OFFSET_HDR
-#define POS_TABLE_POS 8+OFFSET_HDR
-#define LEN_ROB_HEADER 56
-#define LEN_ROB_TRAILER 4
-
-#define CRC_START_POS 1
-#define NON_CRC_COUNTS_REDUCED 3
-#define NON_CRC_COUNTS_NOTREDUCED 4
-
-// format b2link header
-//#define FFAA_POS 0
-#define LINK_EVE_POS 2
-#define NO_PROC
-#define CRC_CHECK
-
-#define MAX_EVENT_WORDS 500000 // 2MB
-#define DMA_HDR_WORDS 8
-
-// DMA header format
-#define DMA_WORDS_OF_256BITS 0
-#define DMA_HDR_MAGIC 1
-#define DMA_SIZE_IN_BYTES 2
-#define MAX_DMA_WORDS_OF_256BITS 0xFF
-
-
 /////////////////////////////////////////////////////////
 // hostname
 /////////////////////////////////////////////////////////
@@ -242,22 +92,6 @@ unsigned int n_messages[17] = {0};
 //   { 14 , 0 } , // bad data size
 //   { 15 , 0 } , // Bad CRC
 //   { 16 , 0 }   // missing links
-// };
-
-extern "C" int  ecs_open(int dev, int bar);
-extern "C" void ecs_close(int dev, int bar);
-extern "C" unsigned ecs_read(int dev, int bar, unsigned add) ;
-extern "C" int ecs_write(int dev, int bar, unsigned add, int val);
-
-// struct shm_crc {
-//   int cnt ;
-//   int complete ;
-//   int first_crc ;
-//   int crc_data ;
-//   //  unsigned int data[ 10000 ] ;
-//   unsigned int data[ 9998 ] ;
-//   unsigned int eve;
-//   int ch;
 // };
 
 const int CRC16_XMODEM_TABLE[] = {
@@ -1868,64 +1702,6 @@ int fillDataContents(int* buf, int nwords_per_fee, unsigned int node_id, int ncp
   return offset;
 }
 
-
-
-inline void addEvent(int* buf, int nwords_per_fee, unsigned int event, int ncpr, int nhslb)
-//inline void addEvent(int* buf, int nwords, unsigned int event)
-{
-  int offset = 0;
-  buf[ offset + 4 ] = event;
-  offset += NW_SEND_HEADER;
-
-  for (int k = 0; k < ncpr; k++) {
-    int nwords = buf[ offset ];
-    int posback_xorchksum = 2;
-    int pos_xorchksum = offset + nwords - posback_xorchksum;
-    if (buf[ offset + 4 ] != CTIME_VAL) {
-      pthread_mutex_lock(&(mtx_sender_log));
-      printf("[FATAL] data-production error 2 0x%.x", buf[ offset + 4 ]);
-      fflush(stdout);
-      pthread_mutex_unlock(&(mtx_sender_log));
-      exit(1);
-    }
-    // RawHeader
-    buf[ pos_xorchksum ] ^= buf[ offset + 3];
-    buf[ offset + 3] = event;
-    buf[ pos_xorchksum ] ^= buf[ offset + 3];
-
-    // COPPER header
-    offset += NW_RAW_HEADER;
-    for (int i = 0; i < nhslb ; i++) {
-      if ((buf[ offset + 1 ] & 0xffff0000) != 0xffaa0000) {
-        pthread_mutex_lock(&(mtx_sender_log));
-        printf("[FATAL] data-production error 3 : 0x%.x hslb %d cpr %d\n", buf[ offset ], i, k);
-        fflush(stdout);
-        pthread_mutex_unlock(&(mtx_sender_log));
-        exit(1);
-      }
-      buf[ offset +  1 ] = 0xffaa0000 + (event & 0xffff);
-      buf[ offset +  3 ] = event;
-
-      //Adding CRC part
-      int* crc_buf = buf + offset + 2; // 1 => size of HSLB B2L header
-      int crc_nwords = nwords_per_fee + 5; // 5 => size of FEE B2L header
-      unsigned short temp_crc16 = CalcCRC16LittleEndian(0xffff, crc_buf, crc_nwords);
-      buf[ offset + NW_B2L_HEADER + nwords_per_fee + 1 ] = ((event & 0x0000ffff) <<  16) | temp_crc16;
-
-#ifdef DUMMY_REDUCED_DATA
-      offset += NW_B2L_HEADER + nwords_per_fee + NW_B2L_TRAILER;
-#else
-      offset += NW_B2L_HEADER + nwords_per_fee + NW_B2L_TRAILER;
-#endif
-    }
-    offset += NW_RAW_TRAILER;
-    // unsigned int xor_chksum = 0;
-    // unsigned int xor_chksum2 = 0;
-  }
-
-}
-
-
 void split_Ecltrg(int sender_id, unsigned int* data, std::vector< int > valid_ch,
                   unsigned int* data_main, unsigned int* data_splitted,
                   int& event_nwords_main, int& event_nwords_splitted,
@@ -2485,8 +2261,6 @@ void* sender(void* arg)
     //
     // For TOP feature extraction function
     //
-    unsigned int* det_buf =  ;
-
 
 
 

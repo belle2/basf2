cd079888e53539b73954ee3366ede4c508dff984
S.Bilokin <sviatoslav.bilokin@desy.de>
2022-05-25 20:41:00 +0200

    Modifying the unit-test

--- analysis/tests/test_trackIsolationVariables.py
+++ analysis/tests/test_trackIsolationVariables.py
@@ -8,7 +8,9 @@
 import basf2 as b2
 import modularAnalysis as ma
 import stdCharged as stdc
+import stdV0s as stdv0
 import b2test_utils
+import variables.utils as vu
 
 path = b2.create_path()
 
@@ -20,9 +22,9 @@ ma.inputMdstList(filelist=[b2test_utils.require_file("mdst14.root", "validation"
                  path=path)
 
 ntup_vars = []
-for det in ("CDC", "PID", "ECL", "KLM"):
+detector_tuple = ("CDC", "PID", "ECL", "KLM")
+for det in detector_tuple:
     ntup_vars.extend([f"dist3DToClosestTrkAtSurface{det}", f"dist2DRhoPhiToClosestTrkAtSurface{det}"])
-
 
 for ptype in stdc._chargednames:
 
@@ -40,11 +42,11 @@ for ptype in stdc._chargednames:
 
         ma.calculateTrackIsolation(pname,
                                    path,
-                                   "CDC", "PID", "ECL", "KLM",
+                                   *detector_tuple,
                                    alias="dist3DToClosestTrkAtSurface")
         ma.calculateTrackIsolation(pname,
                                    path,
-                                   "CDC", "PID", "ECL", "KLM",
+                                   *detector_tuple,
                                    use2DRhoPhiDist=True,
                                    alias="dist2DRhoPhiToClosestTrkAtSurface")
 
@@ -55,7 +57,29 @@ for ptype in stdc._chargednames:
                              ntup_vars,
                              treename=f"{ptype}{ch}",
                              filename="TrackIsolationVariables.root", path=path)
+v0_types = ['Lambda0:merged', 'K_S0:merged']
+v0_functions = [stdv0.stdLambdas, stdv0.stdKshorts]
+v0_decays = [' -> ^p+ ^pi-', ' -> ^pi+ ^pi-']
 
+for v0_type, v0_function, v0_decays in zip(v0_types, v0_functions, v0_decays):
+    v0_function(path=path)
+    track_vars = vu.create_aliases_for_selected(list_of_variables=ntup_vars,
+                                                decay_string=v0_type+v0_decays)
+    ma.calculateTrackIsolation(v0_type+v0_decays,
+                               path,
+                               *detector_tuple,
+                               alias="dist3DToClosestTrkAtSurface")
+    ma.calculateTrackIsolation(v0_type+v0_decays,
+                               path,
+                               *detector_tuple,
+                               use2DRhoPhiDist=True,
+                               alias="dist2DRhoPhiToClosestTrkAtSurface")
+    ma.printList(v0_type, full=True, path=path)
+    ma.printVariableValues(v0_type, track_vars, path=path)
+    ma.variablesToNtuple(v0_type,
+                         track_vars,
+                         treename=f"{v0_type.split(':')[0]}",
+                         filename="TrackIsolationVariables.root", path=path)
 with b2test_utils.clean_working_directory():
     with b2test_utils.set_loglevel(b2.LogLevel.INFO):
         b2.process(path)

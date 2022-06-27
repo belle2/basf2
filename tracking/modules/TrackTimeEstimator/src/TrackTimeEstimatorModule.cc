0f8af558ce3bb1781c3e913832249fb602d70bd0
THALLER Arthur <thaller@cppm.in2p3.fr>
2022-06-27 17:08:16 +0900

    Removed commented lines

--- tracking/modules/TrackTimeEstimator/src/TrackTimeEstimatorModule.cc
+++ tracking/modules/TrackTimeEstimator/src/TrackTimeEstimatorModule.cc
@@ -35,10 +35,7 @@ void TrackTimeEstimatorModule::event()
     if (svdHypos.size() > 0) { // if SVD eventT0 exists then loop over tracks, don't otherwise and leave their averageTime set at NaN
       // get the latest SVD hypothesis information, this is also the most accurate t0 value the SVD can provide (see SVDEventT0 doc)
       const auto svdBestT0 = svdHypos.back();
-      //for (int i = 0; i < m_tracks.getEntries(); i++) {
       for (auto& track : m_tracks) {
-        // Access Track
-        // const auto& track = m_tracks.operator[](i);
         // Access related recoTrack
         const auto& recoTrack = track.getRelatedTo<RecoTrack>();
         // Get SVD hits

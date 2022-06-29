b6c5e07082f26f4e26e9c79eac03d01fe0da0576
Christian Wessel <christian.wessel@desy.de>
2022-06-29 21:24:33 +0200

    Fixed cpp check errors.

--- tracking/modules/trackSetEvaluatorVXD/src/VTXOverlapResolverModule.cc
+++ tracking/modules/trackSetEvaluatorVXD/src/VTXOverlapResolverModule.cc
@@ -25,7 +25,7 @@
 using namespace std;
 using namespace Belle2;
 
-REG_MODULE(VTXOverlapResolver)
+REG_MODULE(VTXOverlapResolver);
 
 VTXOverlapResolverModule::VTXOverlapResolverModule() : Module()
 {
@@ -126,7 +126,7 @@ void VTXOverlapResolverModule::event()
     }
   }
 
-  for (auto && track : qiTrackOverlap) {
+  for (auto&& track : qiTrackOverlap) {
     if (track.activityState < m_minActivityState) {
       activeCandidates[track.trackIndex]->removeRefereeStatus(SpacePointTrackCand::c_isActive);
     }

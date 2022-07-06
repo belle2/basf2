d88c3667270f037c5d56416ca3cc3bdcf7bd875c
Christian Wessel <christian.wessel@desy.de>
2022-07-06 12:08:26 +0200

    Moved add_flipping_of_recoTracks from init to path_utils.

--- tracking/scripts/tracking/__init__.py
+++ tracking/scripts/tracking/__init__.py
@@ -21,6 +21,7 @@ from tracking.path_utils import (  # noqa
     add_hit_preparation_modules,
     add_mc_matcher,
     add_prune_tracks,
+    add_flipping_of_recoTracks,
     add_pxd_cr_track_finding,
     add_pxd_track_finding,
     add_svd_track_finding,
@@ -229,42 +230,6 @@ def add_prefilter_tracking_reconstruction(path, components=None, skipGeometryAdd
                                                   trackFitHypotheses=trackFitHypotheses,
                                                   reco_tracks=reco_tracks,
                                                   add_mva_quality_indicator=add_recoTrack_QI)
-
-
-def add_flipping_of_recoTracks(path, fit_tracks=True, reco_tracks="RecoTracks", trackFitHypotheses=None):
-    """
-    This function adds the mva based selections and the flipping of the recoTracks
-
-    :param path: The path to add the tracking reconstruction modules to
-    :param fit_tracks: fit the flipped recotracks or not
-    :param reco_tracks: Name of the StoreArray where the reco tracks should be flipped
-    :param trackFitHypotheses: Which pdg hypothesis to fit. Defaults to [211, 321, 2212].
-    """
-
-    path.add_module("FlipQuality", recoTracksStoreArrayName=reco_tracks,
-                    identifier='TRKTrackFlipAndRefit_MVA1_weightfile',
-                    indexOfFlippingMVA=1).set_name("FlipQuality_1stMVA")
-
-    reco_tracks_flipped = "RecoTracks_flipped"
-    path.add_module("RecoTracksReverter", inputStoreArrayName=reco_tracks,
-                    outputStoreArrayName=reco_tracks_flipped)
-    if fit_tracks:
-        path.add_module("DAFRecoFitter", recoTracksStoreArrayName=reco_tracks_flipped).set_name("Combined_DAFRecoFitter_flipped")
-        path.add_module("IPTrackTimeEstimator",
-                        recoTracksStoreArrayName=reco_tracks_flipped, useFittedInformation=False)
-    path.add_module("TrackCreator", trackColName="Tracks_flipped",
-                    trackFitResultColName="TrackFitResults_flipped",
-                    recoTrackColName=reco_tracks_flipped,
-                    pdgCodes=[
-                        211,
-                        321,
-                        2212] if not trackFitHypotheses else trackFitHypotheses).set_name("TrackCreator_flipped")
-    path.add_module("FlipQuality", recoTracksStoreArrayName=reco_tracks,
-                    identifier='TRKTrackFlipAndRefit_MVA2_weightfile',
-                    indexOfFlippingMVA=2).set_name("FlipQuality_2ndMVA")
-    path.add_module("FlippedRecoTracksMerger",
-                    inputStoreArrayName=reco_tracks,
-                    inputStoreArrayNameFlipped=reco_tracks_flipped)
 
 
 def add_postfilter_tracking_reconstruction(path, components=None, pruneTracks=False, fit_tracks=True, reco_tracks="RecoTracks",

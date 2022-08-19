4716912af357e8e3f10d86a8c970552e62fc4699
Marco Milesi <marco.milesi@unimelb.edu.au>
2022-08-19 09:51:29 +0900

    Fix ordering for GT prepending.

--- analysis/examples/PostMdstIdentification/ChargedPidMVAModule.py
+++ analysis/examples/PostMdstIdentification/ChargedPidMVAModule.py
@@ -70,14 +70,16 @@ def argparser():
                         nargs="+",
                         default=[getAnalysisGlobaltag()],
                         help="List of names of conditions DB global tag(s) to append on top of GT replay.\n"
-                        "NB: these GTs will have lowest priority.\n"
+                        "NB: these GTs will have lowest priority over GT replay.\n"
+                        "The order of the sequence passed determines the priority of the GTs, w/ the highest coming first.\n"
                         "Pass a space-separated list of names.")
     parser.add_argument("--global_tag_prepend",
                         type=str,
                         nargs="+",
                         default=None,
-                        help="List of names of conditions DB global tag(s) to prepend.\n"
-                        "NB: these GTs will have highest priority.\n"
+                        help="List of names of conditions DB global tag(s) to prepend to GT replay.\n"
+                        "NB: these GTs will have highest priority over GT replay.\n"
+                        "The order of the sequence passed determines the priority of the GTs, w/ the highest coming first.\n"
                         "Pass a space-separated list of names.")
     parser.add_argument("-d", "--debug",
                         dest="debug",
@@ -104,7 +106,7 @@ if __name__ == '__main__':
     print(f"Appending GTs:\n{args.global_tag_append}")
 
     if args.global_tag_prepend:
-        for tag in args.global_tag_prepend:
+        for tag in reversed(args.global_tag_prepend):
             basf2.conditions.prepend_globaltag(tag)
         print(f"Prepending GTs:\n{args.global_tag_prepend}")
 

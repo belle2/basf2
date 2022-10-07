51b25f5e52151714719c505d7ef020b1a7d58af3
Sato Yo <sato.yo@desy.de>
2022-10-07 10:53:47 +0900

    fixed unit of timeSine variables

--- analysis/variables/src/EventVariables.cc
+++ analysis/variables/src/EventVariables.cc
@@ -894,7 +894,7 @@ Returns NaN for data.
                       "[Eventbased] Time since the previous trigger (127MHz=RF/4 clock).","clock ticks");
 
     REGISTER_VARIABLE("timeSincePrevTriggerMicroSeconds", timeSincePrevTriggerMicroSeconds,
-                      "[Eventbased] Time since the previous trigger.","ms");
+                      "[Eventbased] Time since the previous trigger.",":math:`\\mathrm{\\mu s}`");
 
     REGISTER_VARIABLE("triggeredBunchNumberTTD", triggeredBunchNumberTTD, R"DOC(
 [Eventbased] Number of triggered bunch ranging from 0-1279.
@@ -922,13 +922,13 @@ Returns NaN for data.
 [Eventbased] Time since the last injection pre-kick signal
 
 .. warning:: this returns the time without the delay until the injected bunch reaches the detector (which differs for HER/LER)
-)DOC","ms");
+)DOC",":math:`\\mathrm{\\mu s}`");
 
     REGISTER_VARIABLE("timeSinceLastInjectionClockTicks", timeSinceLastInjectionClockTicks,
       "[Eventbased] Time since the last injected bunch passed by the detector.","clock ticks")
 
     REGISTER_VARIABLE("timeSinceLastInjectionMicroSeconds", timeSinceLastInjectionMicroSeconds,
-      "[Eventbased] Time since the last injected bunch passed by the detector.","ms")
+      "[Eventbased] Time since the last injected bunch passed by the detector.",":math:`\\mathrm{\\mu s}`")
 
     REGISTER_VARIABLE("injectionInHER", injectionInHER,
                   "[Eventbased] Returns 1 if injection was in HER, 0 otherwise.");

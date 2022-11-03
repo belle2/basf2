415b081b4f11304cbe60f544c48a4d0d745655f8
Francesco Tenchini <francesco.tenchini@desy.de>
2022-11-04 01:45:40 +0900

    reset changes to ecl and reconstruction linkdef

--- ecl/dbobjects/include/linkdef.h
+++ ecl/dbobjects/include/linkdef.h
@@ -10,12 +10,12 @@
 #pragma link C++ class Belle2::ECLDigitTimeConstants+; // checksum=0x80f96494, version=2
 #pragma link C++ class Belle2::ECLDigitEnergyConstants+; // checksum=0x8e27b399, version=2
 #pragma link C++ class Belle2::ECLDatabaseImporter-;
-#pragma link C++ class Belle2::ECLShowerShapeSecondMomentCorrection+; // checksum=0xbee9bee0, version=1
-#pragma link C++ class Belle2::ECLShowerCorrectorLeakageCorrection+; // checksum=0x3397034a, version=1
-#pragma link C++ class Belle2::ECLShowerEnergyCorrectionTemporary+; // checksum=0xcaa054cf, version=1
-#pragma link C++ class Belle2::ECLCrystalCalib+; // checksum=0x44484113, version=1
-#pragma link C++ class Belle2::ECLDigitWaveformParameters+; // checksum=0x1b4bedbe, version=2
-#pragma link C++ class Belle2::ECLDigitWaveformParametersForMC+; // checksum=0xc16a3107, version=1
+#pragma link C++ class Belle2::ECLShowerShapeSecondMomentCorrection; // checksum=0xbee9bee0, version=1
+#pragma link C++ class Belle2::ECLShowerCorrectorLeakageCorrection; // checksum=0x3397034a, version=1
+#pragma link C++ class Belle2::ECLShowerEnergyCorrectionTemporary; // checksum=0xcaa054cf, version=1
+#pragma link C++ class Belle2::ECLCrystalCalib; // checksum=0x44484113, version=1
+#pragma link C++ class Belle2::ECLDigitWaveformParameters; // checksum=0x1b4bedbe, version=2
+#pragma link C++ class Belle2::ECLDigitWaveformParametersForMC; // checksum=0xc16a3107, version=1
 #pragma link C++ class Belle2::ECLChargedPidPDFs+; // checksum=0x1bd4d566, version=2
 #pragma link C++ class Belle2::ECLReferenceCrystalPerCrateCalib+; // checksum=0x5c126a47, version=3
 #pragma link C++ enum Belle2::ECLChargedPidPDFs::InputVar+; //implicit
@@ -24,20 +24,20 @@
 #pragma link C++ class Belle2::ECLChargedPIDPhasespaceCategory+; // checksum=0xc3de5acc, implicit, version=1
 #pragma link C++ class Belle2::ECLChargedPIDMVAWeights+; // , checksum=0xcb04e543, version=1
 #pragma link C++ enum Belle2::ECLChargedPIDPhasespaceCategory::MVAResponseTransformMode+; //implicit
-#pragma link C++ class Belle2::PackedAutoCovariance+; // checksum=0xb1a29561, implicit, version=1
-#pragma link C++ class Belle2::ECLAutoCovariance+; // checksum=0x1f75052d, version=1
-#pragma link C++ class Belle2::ECLTrackClusterMatchingThresholds+; // checksum=0xdc6f9453, version=1
+#pragma link C++ class Belle2::PackedAutoCovariance; // checksum=0xb1a29561, implicit, version=1
+#pragma link C++ class Belle2::ECLAutoCovariance; // checksum=0x1f75052d, version=1
+#pragma link C++ class Belle2::ECLTrackClusterMatchingThresholds; // checksum=0xdc6f9453, version=1
 #pragma link C++ class Belle2::ECLTrackClusterMatchingParameterizations+; // checksum=0xcb5d30c4, version=1
-#pragma link C++ class Belle2::ECLHadronComponentEmissionFunction+; // checksum=0x94f5457e, version=1
-#pragma link C++ class Belle2::ECLDspData+; // checksum=0x83a051b9, version=1
+#pragma link C++ class Belle2::ECLHadronComponentEmissionFunction; // checksum=0x94f5457e, version=1
+#pragma link C++ class Belle2::ECLDspData; // checksum=0x83a051b9, version=1
 #pragma link C++ class Belle2::ECLChannelMap+; // checksum=0x31b65fac, version=1
-#pragma link C++ class Belle2::ECLCrystalLocalRunCalib+; // checksum=0xf7c81e44, version=1
-#pragma link C++ class Belle2::ECLLocalRunCalibRef+; // checksum=0x37dc8c50, version=1
+#pragma link C++ class Belle2::ECLCrystalLocalRunCalib; // checksum=0xf7c81e44, version=1
+#pragma link C++ class Belle2::ECLLocalRunCalibRef; // checksum=0x37dc8c50, version=1
 #pragma link C++ class Belle2::ECLWaveformData+; // checksum=0x82c9e7d2, version=1
 #pragma link C++ class Belle2::ECLWFAlgoParams+; // checksum=0x731094cf, implicit, version=1
 #pragma link C++ class Belle2::ECLNoiseData+; // checksum=0xf538d169, implicit, version=1
 #pragma link C++ class Belle2::ECLLookupTable+; // checksum=0xe3749e00, implicit, version=1
-#pragma link C++ class Belle2::ECLLeakageCorrections+; // checksum=0x262d45e4, version=2
+#pragma link C++ class Belle2::ECLLeakageCorrections; // checksum=0x262d45e4, version=2
 
 // workaround for https://github.com/root-project/root/issues/7960
 #pragma link C++ class std::pair<int,std::unordered_map<int,vector<Belle2::ECLChargedPidPDFs::InputVar>>>+; // checksum=0xacd09862, version=-1

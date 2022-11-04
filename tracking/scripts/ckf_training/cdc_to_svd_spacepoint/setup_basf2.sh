9b59ea4f814d10f0ed52b1e2d7652719d12f5a87
Christian Wessel <christian.wessel@desy.de>
2022-11-04 13:28:23 +0100

    Added basf2 setup script for use by b2luigi.

---
+++ tracking/scripts/ckf_training/cdc_to_svd_spacepoint/setup_basf2.sh
@@ -0,0 +1,8 @@
+#cd /afs/desy.de/user/c/cwessel/head
+#source /cvmfs/belle.cern.ch/tools/b2setup
+#source /cvmfs/belle.cern.ch/tools/b2setup prerelease-07-00-00a
+
+olddir="$(pwd)"
+cd /afs/desy.de/user/c/cwessel/head
+source /cvmfs/belle.cern.ch/tools/b2setup
+cd $olddir

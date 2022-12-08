f378f62f08f9c39a8bc021b6af73566ae3e36b87
Suryanarayan Mondal <suryanarayan.mondal@df.unipi.it>
2022-12-08 02:51:22 +0100

    added the module

---
+++ svd/modules/svdTimeGroupComposer/include/SVDTimeGroupComposerModule.h
@@ -0,0 +1,66 @@
+/**************************************************************************
+ * basf2 (Belle II Analysis Software Framework)                           *
+ * Author: The Belle II Collaboration                                     *
+ *                                                                        *
+ * See git log for contributors and copyright holders.                    *
+ * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
+ **************************************************************************/
+
+#pragma once
+
+#include <framework/core/Module.h>
+#include <framework/datastore/StoreArray.h>
+#include <svd/dataobjects/SVDCluster.h>
+
+#include <string>
+
+#include <TFile.h>
+#include <TH1D.h>
+
+namespace Belle2 {
+  /**
+   * Imports Clusters of the SVD detector and converts them to spacePoints.
+   *
+   */
+  class SVDTimeGroupComposerModule : public Module {
+
+  public:
+
+
+
+    /** Constructor */
+    SVDTimeGroupComposerModule();
+
+
+
+    /** Init the module.
+    *
+    * prepares all store- and relationArrays.
+    */
+    virtual void initialize() override;
+
+
+    /** eventWise jobs (e.g. storing spacepoints */
+    virtual void event() override;
+
+
+  protected:
+
+
+    // Data members
+    std::string m_svdClustersName = "SVDClusters"; /**< SVDCluster collection name */
+
+    StoreArray<SVDCluster>
+    m_svdClusters; /**< the storeArray for svdClusters as member, is faster than recreating link for each event */
+
+    std::string m_eventLevelTrackingInfoName = ""; /**< Name of the EventLevelTrackingInfo */
+
+    double m_xRange = 200.; /** Half-x range of time histogram */
+    double m_AverageCountPerBin = 1.; /** This sets the bin width of histogram time scale */
+    double m_removeBaseline = 2.; /** Bin Content bellow this is not considered */
+
+    // modification parameters
+    std::string m_nameOfInstance =
+      ""; /**< allows the user to set an identifier for this module. Usefull if one wants to use several instances of that module */
+  };
+} // end namespace Belle2
---
+++ svd/modules/svdTimeGroupComposer/src/SVDTimeGroupComposerModule.cc
@@ -0,0 +1,138 @@
+/**************************************************************************
+ * basf2 (Belle II Analysis Software Framework)                           *
+ * Author: The Belle II Collaboration                                     *
+ *                                                                        *
+ * See git log for contributors and copyright holders.                    *
+ * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
+ **************************************************************************/
+
+#include <svd/modules/svdTimeGroupComposer/SVDTimeGroupComposerModule.h>
+
+#include <framework/logging/Logger.h>
+#include <framework/utilities/FileSystem.h>
+
+using namespace std;
+using namespace Belle2;
+
+
+REG_MODULE(SVDTimeGroupComposer);
+
+SVDTimeGroupComposerModule::SVDTimeGroupComposerModule() :
+  Module()
+{
+  setDescription("Imports Clusters of the SVD detector and converts them to spacePoints.");
+  setPropertyFlags(c_ParallelProcessingCertified);
+
+  // 1. Collections.
+  addParam("SVDClusters", m_svdClustersName,
+           "SVDCluster collection name", string(""));
+  addParam("EventLevelTrackingInfoName", m_eventLevelTrackingInfoName,
+           "EventLevelTrackingInfo collection name", string(""));
+
+  // 2.Modification parameters:
+  addParam("AverageCountPerBin", m_AverageCountPerBin, "This sets the bin width of histogram.",
+           double(1.));
+  addParam("XRange", m_xRange, "This sets the x-range of histogram.",
+           double(200.));
+  addParam("RemoveBaseline", m_removeBaseline, "Bin Content bellow this is not considered.",
+           double(2.));
+
+}
+
+
+
+void SVDTimeGroupComposerModule::initialize()
+{
+  // prepare all store:
+  m_svdClusters.isRequired(m_svdClustersName);
+
+  B2DEBUG(1, "SVDTimeGroupComposerModule \nsvdClusters: " << m_svdClusters.getName());
+}
+
+
+
+void SVDTimeGroupComposerModule::event()
+{
+  int totClusters = m_svdClusters.getEntries();
+
+  /** declaring histogram */
+  int xbin = totClusters / m_AverageCountPerBin;
+  TH1D h_clsTime("h_clsTime", "h_clsTime", xbin, -m_xRange, m_xRange);
+  for (int ij = 0; ij < totClusters; ij++) {
+    h_clsTime.Fill(m_svdClusters[ij]->getClsTime());
+  }
+
+  /** finalized the groups */
+  int groupBegin = -1; int groupEnd = -1;
+  std::vector<std::tuple<double, double, int>> groupInfo; // start, end, totClusters
+  for (int ij = 2; ij < h_clsTime.GetNbinsX(); ij++) {
+    double sumc = h_clsTime.GetBinContent(ij);
+    double suml = h_clsTime.GetBinContent(ij - 1);
+    double sumr = h_clsTime.GetBinContent(ij + 1);
+    // possible background
+    if (sumc <= 2) sumc = 0;
+    if (suml <= 2) suml = 0;
+    if (sumr <= 2) sumr = 0;
+
+    double sum = suml + sumc + sumr;
+    // finding group
+    if (sum > 0 && groupBegin < 0 && groupEnd < 0) { groupBegin = ij;}
+    if (sum <= 0 && groupBegin > 0 && groupEnd < 0) {
+      groupEnd = ij - 1;
+      int clsInGroup = h_clsTime.Integral(groupBegin, groupEnd);
+      double beginPos = h_clsTime.GetXaxis()->GetBinLowEdge(groupBegin);
+      double endPos   = h_clsTime.GetXaxis()->GetBinLowEdge(groupEnd) +
+                        h_clsTime.GetXaxis()->GetBinWidth(groupEnd);
+      if (clsInGroup > 1) {
+        groupInfo.push_back(std::make_tuple(beginPos, endPos, clsInGroup));
+      }
+      groupBegin = groupEnd = -1; // reset for new group
+    }
+  }
+
+  // sorting groups in descending cluster-counts
+  // this should help speed up the next process
+  std::tuple<double, double, int> key;
+  for (int ij = 1; ij < int(groupInfo.size()); ij++) {
+    key = groupInfo[ij];
+    int kj = ij - 1;
+    while ((kj >= 0) &&
+           ((std::get<2>(groupInfo[kj])) < (std::get<2>(key)))) {
+      groupInfo[kj + 1] = groupInfo[kj];
+      kj--;
+    }
+    groupInfo[kj + 1] = key;
+  }
+
+  std::vector<int> rejectedCls;
+  rejectedCls.assign(totClusters, -1);
+  int totGroups = int(groupInfo.size());
+  for (int ij = 0; ij < totGroups; ij++) {
+    auto beginPos    = std::get<0>(groupInfo[ij]);
+    auto endPos      = std::get<1>(groupInfo[ij]);
+    auto totCls      = std::get<2>(groupInfo[ij]);
+
+    std::cout << " group " << ij << " beginPos " << beginPos << " endPos " << endPos << " totCls " << totCls << std::endl;
+    int rejectedCount = 0;
+    for (int jk = 0; jk < int(rejectedCls.size()); jk++) {
+      int place = rejectedCls[jk] < 0 ? jk : rejectedCls[jk];
+      double clsTime = m_svdClusters[place]->getClsTime();
+      if (clsTime >= beginPos && clsTime <= endPos) {
+        m_svdClusters[place]->setTimeGroupId(ij);
+        std::cout << "    accepted cluster " << place << " clsTime " << clsTime << std::endl;
+      } else {
+        std::cout << "      rejected cluster " << place << " clsTime " << clsTime << std::endl;
+        if (ij == totGroups - 1) {
+          m_svdClusters[place]->setTimeGroupId(-1);
+          std::cout << "        orphan cluster " << place << " clsTime " << clsTime << std::endl;
+        } else {
+          rejectedCls[rejectedCount++] = place;
+        }
+      }
+    } // for(int jk=0;jk<int(rejectedCls.size());jk++) {
+    rejectedCls.resize(rejectedCount);
+  } // for(int ij=0;ij<int(groupInfo.size());ij++) {
+
+
+}
+

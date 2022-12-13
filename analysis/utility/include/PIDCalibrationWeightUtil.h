5e6d86663ef5a70b0ee386db33315f50104e889f
Sato Yo <sato.yo@desy.de>
2022-12-13 20:57:50 +0900

    extend weighted-pid variables for p and theta binning

--- analysis/utility/include/PIDCalibrationWeightUtil.h
+++ analysis/utility/include/PIDCalibrationWeightUtil.h
@@ -9,6 +9,7 @@
 #pragma once
 
 #include <analysis/dbobjects/PIDCalibrationWeight.h>
+#include <analysis/dbobjects/PIDDetectorWeights.h>
 #include <framework/database/DBObjPtr.h>
 #include <framework/logging/Logger.h>
 
@@ -28,9 +29,9 @@ namespace Belle2 {
      */
     PIDCalibrationWeightUtil()
     {
-      m_pidWeightDB = std::make_unique<DBObjPtr<PIDCalibrationWeight>>();
+      m_pidCalibWeightDB = std::make_unique<DBObjPtr<PIDCalibrationWeight>>();
 
-      if (!(*m_pidWeightDB))
+      if (!(*m_pidCalibWeightDB))
         B2FATAL("The dbobject PIDCalibrationWeight could not be found! It is necessary for the weighted PID variables.");
     };
 
@@ -40,27 +41,53 @@ namespace Belle2 {
     PIDCalibrationWeightUtil(std::string matrixName)
     {
       m_matrixName = matrixName;
-      m_pidWeightDB = std::make_unique<DBObjPtr<PIDCalibrationWeight>>(m_matrixName);
 
-      if (!(*m_pidWeightDB))
-        B2FATAL("The dbobject PIDCalibrationWeight, " << m_matrixName <<
-                " could not be found! It is necessary for the weighted PID variables.");
+      if (m_matrixName.find("PIDDetectorWeights") != std::string::npos) {
+        m_pidDetWeightDB = std::make_unique<DBObjPtr<PIDDetectorWeights>>(m_matrixName);
+        if (!(*m_pidDetWeightDB))
+          B2FATAL("The dbobject PIDCalibrationWeight, " << m_matrixName <<
+                  " could not be found! It is necessary for the weighted PID variables.");
+      } else {
+        m_pidCalibWeightDB = std::make_unique<DBObjPtr<PIDCalibrationWeight>>(m_matrixName);
+        if (!(*m_pidCalibWeightDB))
+          B2FATAL("The dbobject PIDCalibrationWeight, " << m_matrixName <<
+                  " could not be found! It is necessary for the weighted PID variables.");
+      }
     };
 
     /**
      * Get the weight for the given combination of the PDG code and the detector name.
      */
-    double getWeight(int pdg, std::string detector) const
+    double getWeight(int pdg, std::string detector, double p, double theta) const
     {
-      return (*m_pidWeightDB)->getWeight(pdg, detector);
+      if (m_matrixName.find("PIDDetectorWeights") != std::string::npos) {
+
+        Const::EDetector det;
+        boost::to_lower(detector);
+        if (detector == "svd") det = Const::SVD;
+        else if (detector == "cdc") det = Const::CDC;
+        else if (detector == "top") det = Const::TOP;
+        else if (detector == "arich") det = Const::ARICH;
+        else if (detector == "ecl") det = Const::ECL;
+        else if (detector == "klm") det = Const::KLM;
+        else B2FATAL("Unknown detector component: " << detector);
+
+        return (*m_pidDetWeightDB)->getWeight(Const::ParticleType(pdg), det, p, theta);
+      } else {
+        return (*m_pidCalibWeightDB)->getWeight(pdg, detector);
+      }
     }
 
     /**
      * Get the weight for the given combination of the PDG code and the detector in Const::EDetector.
      */
-    double getWeight(int pdg, Const::EDetector det) const
+    double getWeight(int pdg, Const::EDetector det, double p, double theta) const
     {
-      return (*m_pidWeightDB)->getWeight(pdg, det);
+      if (m_matrixName.find("PIDDetectorWeights") != std::string::npos) {
+        return (*m_pidDetWeightDB)->getWeight(Const::ParticleType(pdg), det, p, theta);
+      } else {
+        return (*m_pidCalibWeightDB)->getWeight(pdg, det);
+      }
     }
 
     /**
@@ -68,12 +95,15 @@ namespace Belle2 {
      */
     std::vector<double> getWeights(int pdg) const
     {
-      return (*m_pidWeightDB)->getWeights(pdg);
+      return (*m_pidCalibWeightDB)->getWeights(pdg);
     }
 
   private:
     std::string m_matrixName = "PIDCalibrationWeight"; /**< name of the matrix. */
-    std::unique_ptr<DBObjPtr<PIDCalibrationWeight>> m_pidWeightDB; /**< db object for the calibration weight matrix. */
+    std::unique_ptr<DBObjPtr<PIDCalibrationWeight>> m_pidCalibWeightDB; /**< db object for the calibration weight matrix. */
+    std::unique_ptr<DBObjPtr<PIDDetectorWeights>>
+                                               m_pidDetWeightDB; /**< db object for the detector weight matrix for momentum and theta dependent matrix. */
+
 
   };
 

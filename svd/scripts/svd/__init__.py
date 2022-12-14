7e200bc44885f3ca3bfd18f134b00a10d6af4aef
Suryanarayan Mondal <suryanarayan.mondal@df.unipi.it>
2022-12-14 10:18:12 +0100

    changed average bin content to 10 and threshold 4

--- svd/scripts/svd/__init__.py
+++ svd/scripts/svd/__init__.py
@@ -107,8 +107,8 @@ def add_svd_reconstruction(path, isROIsimulation=False, createRecoDigits=False, 
         svdTimeGroupComposer.param('SVDClusters', clustersName)
         svdTimeGroupComposer.param('EventLevelTrackingInfoName', nameEventTrackingInfo)
         svdTimeGroupComposer.param('XRange', 160.)
-        svdTimeGroupComposer.param('AverageCountPerBin', 4.)
-        svdTimeGroupComposer.param('Threshold', 8.)
+        svdTimeGroupComposer.param('AverageCountPerBin', 10.)
+        svdTimeGroupComposer.param('Threshold', 4.)
         path.add_module(svdTimeGroupComposer)
 
     # Add SVDSpacePointCreator
@@ -242,8 +242,8 @@ def add_rel5_svd_reconstruction(path, isROIsimulation=False, applyMasking=False)
         svdTimeGroupComposer.param('SVDClusters', clusterName)
         svdTimeGroupComposer.param('EventLevelTrackingInfoName', nameEventTrackingInfo)
         svdTimeGroupComposer.param('XRange', 160.)
-        svdTimeGroupComposer.param('AverageCountPerBin', 4.)
-        svdTimeGroupComposer.param('Threshold', 8.)
+        svdTimeGroupComposer.param('AverageCountPerBin', 10.)
+        svdTimeGroupComposer.param('Threshold', 4.)
         path.add_module(svdTimeGroupComposer)
 
     # Add SVDSpacePointCreator

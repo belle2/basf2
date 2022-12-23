d8f4ff6938ba59c26976d041b2fbe75e9008f70d
Suryanarayan Mondal <suryanarayan.mondal@df.unipi.it>
2022-12-23 12:15:32 +0100

    changed to OffOn

--- tracking/validation/VXDTF2TrackingValidationBkg.py
+++ tracking/validation/VXDTF2TrackingValidationBkg.py
@@ -30,9 +30,9 @@ ACTIVE = True
 
 basf2.set_random_seed(1337)
 
-# b2conditions.prepend_globaltag("tracking_TEST_SVDTimeSelectionOFFrev1_VXDTF2TimeFiltersONrev27")
+b2conditions.prepend_globaltag("tracking_TEST_SVDTimeSelectionOFFrev1_VXDTF2TimeFiltersONrev27")
 # b2conditions.prepend_globaltag("tracking_TEST_SVDTimeSelectionONrev5_VXDTF2TimeFiltersONrev27")
-b2conditions.prepend_globaltag("tracking_TEST_SVDTimeSelectionOFFrev1_VXDTF2TimeFiltersOFFrev28")
+# b2conditions.prepend_globaltag("tracking_TEST_SVDTimeSelectionOFFrev1_VXDTF2TimeFiltersOFFrev28")
 
 
 def setupFinderModule(path):

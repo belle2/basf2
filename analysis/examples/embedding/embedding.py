713dfed5aa60cb6eac717af61e5dbfeb7ce7c21e
Sato Yo <sato.yo@desy.de>
2022-12-23 17:24:53 +0900

    typo

--- analysis/examples/embedding/embedding.py
+++ analysis/examples/embedding/embedding.py
@@ -44,4 +44,4 @@ if __name__ == "__main__":
                     ' --filePrimary ' + outputDataForRemoveList +
                     ' --fileSecondary ' + outputMCForSignalList +
                     ' --fileOut ' + outputEmbedded,
-                    shel=True)
+                    shell=True)

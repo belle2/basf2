abaac594549b73f0721265a8f88a4fae364bbf9e
Suryanarayan Mondal <suryanarayan.mondal@df.unipi.it>
2022-12-26 15:54:04 +0100

    tested both CoG3 and CoG6. CoG3 is probably is a safe choice

--- svd/modules/svdShaperDigitsGrouping/include/SVDShaperDigitsGroupingModule.h
+++ svd/modules/svdShaperDigitsGrouping/include/SVDShaperDigitsGroupingModule.h
@@ -10,12 +10,19 @@
 
 #include <framework/core/Module.h>
 #include <framework/datastore/StoreArray.h>
+#include <framework/datastore/StoreObjPtr.h>
 #include <framework/dbobjects/HardwareClockSettings.h>
 #include <framework/database/DBObjPtr.h>
 
 #include <svd/dataobjects/SVDShaperDigit.h>
+
 #include <svd/reconstruction/SVDMaxSumAlgorithm.h>
+
+#include <svd/calibration/SVDPulseShapeCalibrations.h>
 #include <svd/calibration/SVD3SampleCoGTimeCalibrations.h>
+#include <svd/calibration/SVDCoGTimeCalibrations.h>
+#include <svd/calibration/SVDNoiseCalibrations.h>
+#include <svd/calibration/SVDClustering.h>
 
 #include <string>
 
@@ -58,6 +65,8 @@ namespace Belle2 {
 
   protected:
 
+    /** Name of the collection to use for the SVDEventInfo */
+    std::string m_svdEventInfoName;
     /** Name of the collection to use for the SVDShaperDigits */
     std::string m_storeShaperDigitsName;
 
@@ -76,8 +85,22 @@ namespace Belle2 {
     /** APV clock period*/
     double m_apvClockPeriod = std::numeric_limits<double>::quiet_NaN();
 
+    /**SVDPulseShaper calibration wrapper*/
+    SVDPulseShapeCalibrations m_PulseShapeCal;
+    /** CoG6 time calibration wrapper*/
+    SVDCoGTimeCalibrations m_CoG6TimeCal;
     /** CoG3 time calibration wrapper*/
     SVD3SampleCoGTimeCalibrations m_CoG3TimeCal;
+
+    /**< SVDNoise calibrations db object*/
+    SVDNoiseCalibrations m_NoiseCal;
+    /**<SVDCluster calibrations db object*/
+    SVDClustering m_ClusterCal;
+
+    /** Adjacent cut in units of noise. DEPRECATED - useDB*/
+    double m_cutAdjacent = 3;
+    /** if true takes the clusterizer cuts and reconstruction configuration from the DB objects*/
+    bool m_useDB = true;
   };
 
 } // end namespace Belle2
--- svd/modules/svdShaperDigitsGrouping/src/SVDShaperDigitsGroupingModule.cc
+++ svd/modules/svdShaperDigitsGrouping/src/SVDShaperDigitsGroupingModule.cc
@@ -11,6 +11,8 @@
 #include <framework/logging/Logger.h>
 #include <framework/utilities/FileSystem.h>
 
+#include <svd/dataobjects/SVDEventInfo.h>
+
 #include <iostream>
 #include <vector>
 
@@ -21,14 +23,24 @@ using namespace Belle2;
 REG_MODULE(SVDShaperDigitsGrouping);
 
 SVDShaperDigitsGroupingModule::SVDShaperDigitsGroupingModule() :
-  Module()
+  Module(),
+  m_cutAdjacent(3.0), m_useDB(true)
 {
   setDescription("Calculate CoG3 time of each strip and grouping them.");
   setPropertyFlags(c_ParallelProcessingCertified);
 
   // 1. Collections.
+  addParam("EventInfo", m_svdEventInfoName,
+           "SVDEventInfo collection name.", string("SVDEventInfo"));
   addParam("ShaperDigits", m_storeShaperDigitsName,
            "SVDShaperDigits collection name.", string(""));
+
+  // 2. Clustering
+  addParam("useDB", m_useDB,
+           "if False, use clustering and reconstruction configuration module parameters", m_useDB);
+  addParam("AdjacentSN", m_cutAdjacent,
+           "minimum SNR for strips to be considered for clustering. Overwritten by the dbobject, unless you set useDB = False.",
+           m_cutAdjacent);
 }
 
 
@@ -59,12 +71,38 @@ void SVDShaperDigitsGroupingModule::event()
   // std::cout<<" strip times ";
   for (SVDShaperDigit& currentDigit : m_storeDigits) {
 
+    if (m_useDB)
+      m_cutAdjacent = m_ClusterCal.getMinAdjSNR(currentDigit.getSensorID(), currentDigit.isUStrip());
+
+    //Ignore digits with insufficient signal
+    float thisNoise = m_NoiseCal.getNoise(currentDigit.getSensorID(), currentDigit.isUStrip(), currentDigit.getCellID());
+    int thisCharge = currentDigit.getMaxADCCounts();
+    B2DEBUG(20, "Noise = " << thisNoise << " ADC, MaxSample = " << thisCharge << " ADC");
+
+    if ((float)thisCharge / thisNoise < m_cutAdjacent)
+      continue;
+
     float stripTime = 0;
     float stripSumAmplitudes = 0;
 
+    Belle2::SVDShaperDigit::APVFloatSamples samples = currentDigit.getSamples();
+
+#define CoG3Alg
+#ifdef CoG6Alg
+
+    int firstFrame = 0;
+    for (int k = 0; k < static_cast<int>(samples.size()); k ++) {
+      stripTime += k * samples.at(k);
+      stripSumAmplitudes += samples.at(k);
+    }
+
+#endif
+#ifdef CoG3Alg
+
     //take the MaxSum 3 samples
-    SVD::SVDMaxSumAlgorithm maxSum = SVD::SVDMaxSumAlgorithm(currentDigit.getSamples());
+    SVD::SVDMaxSumAlgorithm maxSum = SVD::SVDMaxSumAlgorithm(samples);
     std::vector<float> selectedSamples = maxSum.getSelectedSamples();
+    int firstFrame = maxSum.getFirstFrame();
 
     auto begin = selectedSamples.begin();
     const auto end = selectedSamples.end();
@@ -73,6 +111,8 @@ void SVDShaperDigitsGroupingModule::event()
       stripSumAmplitudes += static_cast<float>(*begin);
       stripTime += static_cast<float>(*begin) * step;
     }
+#endif
+
     if (stripSumAmplitudes != 0) {
       stripTime /= (stripSumAmplitudes);
     } else {
@@ -83,8 +123,29 @@ void SVDShaperDigitsGroupingModule::event()
     if (isnan(m_triggerBin))
       B2FATAL("OOPS, we can't continue, you have to set the trigger bin!");
 
+#ifdef CoG6Alg
+
+    stripTime -= m_PulseShapeCal.getPeakTime(currentDigit.getSensorID(), currentDigit.isUStrip(), currentDigit.getCellID());
+    stripTime =  m_CoG6TimeCal.getCorrectedTime(currentDigit.getSensorID(), currentDigit.isUStrip(), currentDigit.getCellID(),
+                                                stripTime, m_triggerBin);
+
+#endif
+#ifdef CoG3Alg
+
     //cellID = 10 not used for calibration
     stripTime = m_CoG3TimeCal.getCorrectedTime(currentDigit.getSensorID(), currentDigit.isUStrip(), 10, stripTime, m_triggerBin);
+
+#endif
+
+    //first take Event Informations:
+    StoreObjPtr<SVDEventInfo> temp_eventinfo(m_svdEventInfoName);
+    if (!temp_eventinfo.isValid())
+      m_svdEventInfoName = "SVDEventInfoSim";
+    StoreObjPtr<SVDEventInfo> eventinfo(m_svdEventInfoName);
+    if (!eventinfo) B2ERROR("No SVDEventInfo!");
+
+    // now go into FTSW time reference frame
+    stripTime = eventinfo->getTimeInFTSWReference(stripTime, firstFrame);
 
     currentDigit.setTime(stripTime);
     // std::cout<<" "<<currentDigit.getTime();

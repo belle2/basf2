b2c0f0bb704ae4b99c4323438c5070246e9ca5cf
Christian Wessel <christian.wessel@desy.de>
2023-01-03 18:21:58 +0100

    Added TrackingMVAFilterPayloads base class.

---
+++ tracking/dbobjects/include/TrackingMVAFilterPayloads.h
@@ -0,0 +1,59 @@
+/**************************************************************************
+ * basf2 (Belle II Analysis Software Framework)                           *
+ * Author: The Belle II Collaboration                                     *
+ *                                                                        *
+ * See git log for contributors and copyright holders.                    *
+ * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
+ **************************************************************************/
+#pragma once
+
+#include <TObject.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+namespace Belle2 {
+  /** Class for the MVA filter payloads. */
+  class TrackingMVAFilterPayloads : public TObject {
+  public:
+    /** Default constructor */
+    TrackingMVAFilterPayloads() : m_cutValue(-1.), m_weightFileIdentifier("") {}
+
+    //    cout << "BaseMVAPayloadParameters.h 0 " << endl;
+
+    /** Destructor */
+    ~TrackingMVAFilterPayloads() {}
+
+    /** Set the MVA filter cut value (-1 if not used) */
+    void setCutValue(const double cutValue)
+    {
+      m_cutValue = cutValue;
+    }
+
+    /** Get the MVA filter cut value (-1 if not used) */
+    double getCutValue() const
+    {
+      return m_cutValue;
+    }
+
+    /** Set the name of the weight file */
+    void setIdentifierName(const std::string identifier)
+    {
+      m_weightFileIdentifier = identifier;
+    }
+
+    /** Get the name of the weight file */
+    std::string getIdentifierName() const
+    {
+      return m_weightFileIdentifier;
+    }
+
+  private:
+    /** MVA cut value */
+    double m_cutValue;
+    /** Name of the MVA weightfile */
+    std::string m_weightFileIdentifier;
+
+    ClassDef(TrackingMVAFilterPayloads, 1);  /**< ClassDef, necessary for ROOT */
+  };
+}

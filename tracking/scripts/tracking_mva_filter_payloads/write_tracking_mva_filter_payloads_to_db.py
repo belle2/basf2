8370c63cf262de38fe286baa2239cb7445927902
Christian Wessel <christian.wessel@desy.de>
2023-01-04 17:18:20 +0100

    Add script to create the tracking MVAFilter payloads and add them to the (local) data base.

---
+++ tracking/scripts/tracking_mva_filter_payloads/write_tracking_mva_filter_payloads_to_db.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
+from ROOT import Belle2
+import basf2
+
+
+def write_tracking_mva_filter_payloads_to_db(dbobj: object = None,
+                                             dbobj_name: str = None,
+                                             iovList=(0, 0, 0, 0),
+                                             weightfile_identifier: str = None,
+                                             cut_value: float = None):
+    """
+    This function creates a DBObject for the TrackQualityEstimatorPayload using
+    :param dbobj DBObject type that should be created
+    :param dbobj_name Name of the DBObject to identify it in the DB
+    :param iovList List of IoVs for which the payload is valid
+    :param weightfile_identifier Name of the weightfile which already is on the DB
+    :param cut_value Cut value
+    """
+
+    # Just a small sanity check for a valid IoV (expLow, runLow, expHigh, runHigh) and valid parameters
+    if (dbobj is None) or \
+       (dbobj_name is None) or \
+       (len(iovList) != 4) or \
+       (weightfile_identifier is None) or \
+       (cut_value is None):
+        basf2.B2ERROR(
+            f"Ooops, something went wrong creating the DB payload for DBObject name {dbobj_name}. \
+              Please check for valid arguments.")
+        return
+
+    # create the iov
+    iov = Belle2.IntervalOfValidity(*iovList)
+    # then set the parameters it contains
+    dbobj.setIdentifierName(weightfile_identifier)
+    dbobj.setCutValue(cut_value)
+
+    # write db object to 'localdb/'
+    Belle2.Database.Instance().storeData(dbobj_name, dbobj, iov)
+
+
+if __name__ == "__main__":
+    dbobj = Belle2.TrackQualityEstimatorPayload()
+    write_tracking_mva_filter_payloads_to_db(dbobj, "TrackQualityEstimatorParameters",
+                                             (0,    0,    0, -1), "trackfindingcdc_TrackQualityIndicator", 0.7)
+    write_tracking_mva_filter_payloads_to_db(dbobj, "TrackQualityEstimatorParameters",
+                                             (1002, 0, 1002, -1), "trackfindingcdc_TrackQualityIndicator", 0.7)
+    write_tracking_mva_filter_payloads_to_db(dbobj, "TrackQualityEstimatorParameters",
+                                             (1003, 0, 1003, -1), "trackfindingcdc_TrackQualityIndicator", 0.7)

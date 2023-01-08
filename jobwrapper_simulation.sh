9807a2b6f74b3c26df3f12e70daeaaa792d6b416
Suryanarayan Mondal <suryanarayan.mondal@df.unipi.it>
2023-01-08 14:15:54 +0100

    added run description in the jobwrapper_simulation

--- jobwrapper_simulation.sh
+++ jobwrapper_simulation.sh
@@ -9,7 +9,10 @@ b2Dir="/nfs/dust/belle2/user/mondal/products/basf2/release_local/"
 cd $b2Dir
 source /cvmfs/belle.cern.ch/tools/b2setup ""
 
+# run 0 : OnOn
+# run 1 : OffOn_frac5p_sig3
+# run 2 : OffOff_frac5p_sig3
 
-exec='basf2 -n '$1' -p 10 svd/examples/svdEventT0PerformanceTTree.py -- --RootOutput --noEventT0Tree --executionTime --isMC --fileTag 90445963f_OffOff_frac5p_sig3'
+exec='basf2 --exp 0 --run 2 -n '$1' -p 10 svd/examples/svdEventT0PerformanceTTree.py -- --RootOutput --noEventT0Tree --executionTime --isMC --fileTag 90445963f_OffOff_frac5p_sig3'
 echo $exec
 echo $exec | sh

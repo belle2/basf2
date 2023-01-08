5bc3873e3663da469399e11c43d0b7a3ef172601
Christian Wessel <christian.wessel@desy.de>
2023-01-08 23:04:02 +0100

    #pragma once 'd ARICHBackgroundModule.h

--- arich/modules/arichBackground/include/ARICHBackgroundModule.h
+++ arich/modules/arichBackground/include/ARICHBackgroundModule.h
@@ -5,9 +5,7 @@
  * See git log for contributors and copyright holders.                    *
  * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
  **************************************************************************/
-
-#ifndef ARICHBACKGROUND_H
-#define ARICHBACKGROUND_H
+#pragma once
 
 #include <framework/core/Module.h>
 #include <framework/datastore/StoreArray.h>
@@ -122,5 +120,3 @@ namespace Belle2 {
 
   } // arich namespace
 } // Belle2 namespace
-
-#endif // ARICHDIGIMODULE_H

4880e9aa5d9cb67591ca8ade1673c60156f6a14e
Christian Wessel <christian.wessel@desy.de>
2023-01-10 15:21:37 +0100

    FIX: decfiles/validation/Validate_B2Vlnu.py now produces output (again, was broken for years) and puts it in the correct place.

--- decfiles/validation/Validate_B2Vlnu.py
+++ decfiles/validation/Validate_B2Vlnu.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 ##########################################################################
 # basf2 (Belle II Analysis Software Framework)                           #
@@ -12,30 +11,33 @@
 """
 <header>
   <contact>P. Urquijo phillip.urquijo@unimelb.edu.au</contact>
-  <output>../Validate_B2Vlnu.root</output>
+  <output>Validate_B2Vlnu.root</output>
 </header>
 """
 
 import basf2
 from generators import add_evtgen_generator
-from modularAnalysis import findMCDecay
+from modularAnalysis import reconstructMCDecay, fillParticleListFromMC
 from variables import variables
 from validation_tools.metadata import create_validation_histograms
 
-basf2.set_random_seed('B+ => [anti-D*0 -> anti-D0 pi0] e+ nu_e')
+basf2.set_random_seed('B+ => anti-D*0 e+ nu_e')
 
 variables.addAlias('q2', 'formula(daughterInvM(1, 2)**2)')
 
 path = basf2.Path()
 path.add_module('EventInfoSetter', evtNumList=[10000])
 add_evtgen_generator(path, 'signal', basf2.find_file('decfiles/dec/Bu_Dst0enu.dec'))
-findMCDecay('B+:sig', 'B+ -> [anti-D*0 -> anti-D0 pi0] e+ nu_e', path=path)
+fillParticleListFromMC('anti-D*0:sig', '', addDaughters=True, path=path)
+fillParticleListFromMC('e+:sig', '', path=path)
+fillParticleListFromMC('nu_e:sig', '', path=path)
+reconstructMCDecay('B+:sig -> anti-D*0:sig e+:sig nu_e:sig', '', path=path)
 create_validation_histograms(
-    path, '../Validate_B2Vlnu.root', 'B+:sig',
+    path, 'Validate_B2Vlnu.root', 'B+:sig',
     [
-        ('q2', 50, -1, 12, '', 'P. Urquijo <phillip.urquijo@unimelb.edu.au>',
+        ('q2', 50, -0.5, 12, '', 'P. Urquijo <phillip.urquijo@unimelb.edu.au>',
          r'B2Vlnu $q^2$ of the $e^+\nu_{e}$ system in $B^+ \to \bar{D^{*0}} e^+ \nu_e$ (truth values)',
-         'should follow the reference', '#q^2'),
+         'should follow the reference', 'q^{2}'),
     ],
     description='B2Vlnu Validation to check generator level distributions',
 )

627da123ef98710b04e1e9025e860474ce37066a
Christian Wessel <christian.wessel@desy.de>
2023-01-12 15:11:05 +0100

    Revert decfiles/validation/Validate_B2Vlnu.py to old version after the function findMCDecay in modular analysis was fixed, but keeping the correct output folder.

--- decfiles/validation/Validate_B2Vlnu.py
+++ decfiles/validation/Validate_B2Vlnu.py
@@ -17,27 +17,24 @@
 
 import basf2
 from generators import add_evtgen_generator
-from modularAnalysis import reconstructMCDecay, fillParticleListFromMC
+from modularAnalysis import findMCDecay
 from variables import variables
 from validation_tools.metadata import create_validation_histograms
 
-basf2.set_random_seed('B+ => anti-D*0 e+ nu_e')
+basf2.set_random_seed('B+ => [anti-D*0 -> anti-D0 pi0] e+ nu_e')
 
 variables.addAlias('q2', 'formula(daughterInvM(1, 2)**2)')
 
 path = basf2.Path()
 path.add_module('EventInfoSetter', evtNumList=[10000])
 add_evtgen_generator(path, 'signal', basf2.find_file('decfiles/dec/Bu_Dst0enu.dec'))
-fillParticleListFromMC('anti-D*0:sig', '', addDaughters=True, path=path)
-fillParticleListFromMC('e+:sig', '', path=path)
-fillParticleListFromMC('nu_e:sig', '', path=path)
-reconstructMCDecay('B+:sig -> anti-D*0:sig e+:sig nu_e:sig', '', path=path)
+findMCDecay('B+:sig', 'B+ -> [anti-D*0 -> anti-D0 pi0] e+ nu_e', path=path)
 create_validation_histograms(
     path, 'Validate_B2Vlnu.root', 'B+:sig',
     [
-        ('q2', 50, -0.5, 12, '', 'P. Urquijo <phillip.urquijo@unimelb.edu.au>',
+        ('q2', 50, -0.5, 12.5, '', 'P. Urquijo <phillip.urquijo@unimelb.edu.au>',
          r'B2Vlnu $q^2$ of the $e^+\nu_{e}$ system in $B^+ \to \bar{D^{*0}} e^+ \nu_e$ (truth values)',
-         'should follow the reference', 'q^{2}'),
+         'should follow the reference', '#q^2'),
     ],
     description='B2Vlnu Validation to check generator level distributions',
 )

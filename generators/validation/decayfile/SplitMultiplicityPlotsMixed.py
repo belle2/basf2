361f5e08cd076f1d300fb8a3307dd4d256436091
Christian Wessel <christian.wessel@desy.de>
2023-01-17 16:05:37 +0100

    Remove now unnecessary code.

--- generators/validation/decayfile/SplitMultiplicityPlotsMixed.py
+++ generators/validation/decayfile/SplitMultiplicityPlotsMixed.py
@@ -43,8 +43,6 @@ if __name__ == '__main__':
     rdf = ROOT.RDataFrame("Split", "MCvalidationMixed.root")
     rdf_fix = rdf.Define("gen_Kn", "-gen_Km").Define("gen_K0bar", "-gen_antiK0")
 
-    B = 'mixed'
-
     axis_dic = {'Kpm': 'K^{+} / K^{#minus} from both B',
                 'K0': 'K^{0} / #bar{K}^{0} from both B'
                 }
@@ -64,35 +62,17 @@ if __name__ == '__main__':
 
     PlottingHistos("Kpm", "gen_Kp", "gen_Kn")
     PlottingHistos("K0", "gen_K0", "gen_K0bar")
-    if B == "charged":
-        axis_dic['Kpm_same'] = 'K^{+} from B^{+} / K^{#minus} from B^{#minus}'
-        axis_dic['Kpm_diff'] = 'K^{+} from B^{#minus} / K^{#minus} from B^{+}'
-        axis_dic['K0_same'] = 'K^{0} from B^{+} / #bar{K}^{0} from B^{#minus}'
-        axis_dic['K0_diff'] = 'K^{0} from B^{#minus} / #bar{K}^{0} from B^{+}'
-
-        rdf_fix = rdf.Define("gen_Kp_Bn", "-gen_Kp_Bm")\
-                     .Define("gen_Km_Bn", "-gen_Km_Bm")\
-                     .Define("gen_K0_Bn", "-gen_K0_Bm")\
-                     .Define("gen_antiK0_Bn", "-gen_antiK0_Bm")
-
-        PlottingHistos("Kpm_same", "gen_Kp_Bp", "gen_Km_Bn")
-        PlottingHistos("Kpm_diff", "gen_Kp_Bn", "gen_Km_Bp")
-        PlottingHistos("K0_same", "gen_K0_Bp", "gen_antiK0_Bn")
-        PlottingHistos("K0_diff", "gen_K0_Bn", "gen_antiK0_Bp")
-    else:
-        axis_dic['Kpm_same'] = 'K^{+} from B^{0} / K^{#minus} from #bar{B}^{0}'
-        axis_dic['Kpm_diff'] = 'K^{+} from #bar{B}^{0} / K^{#minus} from B^{0}'
-        axis_dic['K0_same'] = 'K^{0} from B^{0} / #bar{K}^{0} from #bar{B}^{0}'
-        axis_dic['K0_diff'] = 'K^{0} from #bar{B}^{0} / #bar{K}^{0} from B^{0}'
-
-        rdf_fix = rdf.Define("gen_Kp_B0bar", "-gen_Kp_antiB0")\
-                     .Define("gen_Km_B0bar", "-gen_Km_antiB0")\
-                     .Define("gen_K0_B0bar", "-gen_K0_antiB0")\
-                     .Define("gen_antiK0_B0bar", "-gen_antiK0_antiB0")
-
-        PlottingHistos("Kpm_same", "gen_Kp_B0", "gen_Km_B0bar")
-        PlottingHistos("Kpm_diff", "gen_Kp_B0bar", "gen_Km_B0")
-        PlottingHistos("K0_same", "gen_K0_B0", "gen_antiK0_B0bar")
-        PlottingHistos("K0_diff", "gen_K0_B0bar", "gen_antiK0_B0")
+    axis_dic['Kpm_same'] = 'K^{+} from B^{0} / K^{#minus} from #bar{B}^{0}'
+    axis_dic['Kpm_diff'] = 'K^{+} from #bar{B}^{0} / K^{#minus} from B^{0}'
+    axis_dic['K0_same'] = 'K^{0} from B^{0} / #bar{K}^{0} from #bar{B}^{0}'
+    axis_dic['K0_diff'] = 'K^{0} from #bar{B}^{0} / #bar{K}^{0} from B^{0}'
+    rdf_fix = rdf.Define("gen_Kp_B0bar", "-gen_Kp_antiB0")\
+                 .Define("gen_Km_B0bar", "-gen_Km_antiB0")\
+                 .Define("gen_K0_B0bar", "-gen_K0_antiB0")\
+                 .Define("gen_antiK0_B0bar", "-gen_antiK0_antiB0")
+    PlottingHistos("Kpm_same", "gen_Kp_B0", "gen_Km_B0bar")
+    PlottingHistos("Kpm_diff", "gen_Kp_B0bar", "gen_Km_B0")
+    PlottingHistos("K0_same", "gen_K0_B0", "gen_antiK0_B0bar")
+    PlottingHistos("K0_diff", "gen_K0_B0bar", "gen_antiK0_B0")
 
     outputFile.Close()

0305f9569d66501348e11f95f7a9bdbcca68e0a0
Frank Meier <frank.meier@duke.edu>
2023-01-20 03:12:03 +0900

    fill histograms first before adding meta data

--- analysis/validation/test7_treeFitterFitResolution_Plot.py
+++ analysis/validation/test7_treeFitterFitResolution_Plot.py
@@ -53,29 +53,6 @@ B0_vertex_resolutionz_s = ROOT.TH1F("B0_vertex_resolutionz_s",
 pVal_s = ROOT.TH1F("pVal_s", 'global pValue sig', nBins, -1, 1)
 pVal_b = ROOT.TH1F("pVal_b", 'global pValue bkg', nBins, -1, 1)
 
-histsAndLabels = {
-    B0_E_pull_s: "B^{0} E pull",
-    B0_p_pull_s: "B^{0} momentum pull",
-    B0_vertex_pullx_s: "pull of B^{0} vertex X ",
-    B0_vertex_pully_s: "pull of B^{0} vertex Y ",
-    B0_vertex_pullz_s: "pull of B^{0} vertex Z ",
-    B0_vertex_resolutionx_s: "meas-mc of B^{0} vertex X [#mum]",
-    B0_vertex_resolutiony_s: "meas-mc of B^{0} vertex Y [#mum]",
-    B0_vertex_resolutionz_s: "meas-mc of B^{0} vertex Z [#mum]",
-    pVal_b: "pValue",
-    pVal_s: "pValue",
-}
-
-checkForGausian = "This should be a Gaussian on flat bkg and not changing."
-metaOptions = "pvalue-warn=0.1"
-for hist, xlabel in histsAndLabels.items():
-    hist.GetXaxis().SetTitle(xlabel)
-    hist.GetListOfFunctions().Add(ROOT.TNamed("Contact", "yosato@post.kek.jp"))
-    hist.GetListOfFunctions().Add(ROOT.TNamed("Description", xlabel))
-    hist.GetListOfFunctions().Add(ROOT.TNamed("MetaOptions", metaOptions))
-    if any(string in xlabel for string in ["pull", "meas-mc"]):
-        hist.GetListOfFunctions().Add(ROOT.TNamed("Check", checkForGausian))
-
 signalCut = ROOT.TCut("(isSignal>0)")
 bkgCut = ROOT.TCut("(isSignal<1)")
 
@@ -98,5 +75,28 @@ for histOperation in histOperationStrings:
         drawToNewHist(histOperation, signalCut)
     else:
         drawToNewHist(histOperation, bkgCut)
+
+histsAndLabels = {
+    B0_E_pull_s: "B^{0} E pull",
+    B0_p_pull_s: "B^{0} momentum pull",
+    B0_vertex_pullx_s: "pull of B^{0} vertex X ",
+    B0_vertex_pully_s: "pull of B^{0} vertex Y ",
+    B0_vertex_pullz_s: "pull of B^{0} vertex Z ",
+    B0_vertex_resolutionx_s: "meas-mc of B^{0} vertex X [#mum]",
+    B0_vertex_resolutiony_s: "meas-mc of B^{0} vertex Y [#mum]",
+    B0_vertex_resolutionz_s: "meas-mc of B^{0} vertex Z [#mum]",
+    pVal_b: "pValue",
+    pVal_s: "pValue",
+}
+
+checkForGausian = "This should be a Gaussian on flat bkg and not changing."
+metaOptions = "pvalue-warn=0.1"
+for hist, xlabel in histsAndLabels.items():
+    hist.GetXaxis().SetTitle(xlabel)
+    hist.GetListOfFunctions().Add(ROOT.TNamed("Contact", "yosato@post.kek.jp"))
+    hist.GetListOfFunctions().Add(ROOT.TNamed("Description", xlabel))
+    hist.GetListOfFunctions().Add(ROOT.TNamed("MetaOptions", metaOptions))
+    if any(string in xlabel for string in ["pull", "meas-mc"]):
+        hist.GetListOfFunctions().Add(ROOT.TNamed("Check", checkForGausian))
 
 outputFile.Write()

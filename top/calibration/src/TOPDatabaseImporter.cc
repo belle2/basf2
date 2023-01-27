9148ca904c8b550d45d74c3613aebc220d67d7b6
staric <marko.staric@ijs.si>
2023-01-27 13:44:30 +0100

    first part

--- top/calibration/src/TOPDatabaseImporter.cc
+++ top/calibration/src/TOPDatabaseImporter.cc
@@ -556,7 +556,7 @@ namespace Belle2 {
   }
 
 
-  void TOPDatabaseImporter::getSampleTimeCalibrationInfo()
+  void TOPDatabaseImporter::printSampleTimeCalibrationInfo()
   {
     DBObjPtr<TOPCalTimebase> timeBase;
     if (!timeBase.isValid()) {
@@ -583,11 +583,12 @@ namespace Belle2 {
           if (timeBase->isAvailable(scrodID[bs], channel)) ncal[bs]++;
         }
       }
-      if (ncal[0] + ncal[1] + ncal[2] + ncal[3] == 0) continue;
 
       cout << "Slot " << moduleID << endl;
       for (int bs = 0; bs < 4; bs++) {
-        cout << "  scrodID " << scrodID[bs] << ": " << ncal[bs] << "/128" << endl;
+        cout << "  scrodID " << scrodID[bs] << ": " << ncal[bs] << "/128";
+        if (ncal[bs] < 128) cout << " (" << 128 - ncal[bs] << " missing)";
+        cout << endl;
       }
     }
 
@@ -849,7 +850,7 @@ namespace Belle2 {
   }
 
 
-  void TOPDatabaseImporter::importPmtInstallationData(string fileName, string treeName ,
+  void TOPDatabaseImporter::importPmtInstallationData(string fileName, string treeName,
                                                       int firstExp, int firstRun,
                                                       int lastExp, int lastRun)
   {

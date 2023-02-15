8a9320d69e6615ed721be7234c43896542bd0abb
Christian Wessel <christian.wessel@desy.de>
2023-02-15 05:12:12 +0100

    Move the MC-based slow pion ROI finder from pxd to tracking and adapt the include path.

--- tracking/modules/pxdDataReduction/src/MCSlowPionPXDROICreatorModule.cc
+++ tracking/modules/pxdDataReduction/src/MCSlowPionPXDROICreatorModule.cc
@@ -6,7 +6,7 @@
  * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
  **************************************************************************/
 
-#include <pxd/modules/mcSlowPionPXDROICreator/MCSlowPionPXDROICreatorModule.h>
+#include <tracking/modules/pxdDataReduction/MCSlowPionPXDROICreatorModule.h>
 #include <vxd/geometry/GeoCache.h>
 #include <vxd/geometry/SensorInfoBase.h>
 

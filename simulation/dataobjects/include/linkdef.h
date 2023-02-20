5a43348aa4736e809b0c932fdd36af20d847e5b9
Kirill Chilikin <K.A.Chilikin@inp.nsk.su>
2023-02-20 13:16:55 +0700

    Update class version in linkdef file.

--- simulation/dataobjects/include/linkdef.h
+++ simulation/dataobjects/include/linkdef.h
@@ -6,7 +6,7 @@
 #pragma link C++ nestedclasses;
 
 #pragma link C++ class Belle2::SimHitBase+; // checksum=0xf68f360, version=3
-#pragma link C++ class Belle2::BeamBackHit+; // checksum=0x4ba78b6a, version=1
+#pragma link C++ class Belle2::BeamBackHit+; // checksum=0x3cc4b2fe, version=2
 #pragma link C++ class Belle2::MCParticleTrajectory+; // checksum=0x7957688e, version=1
 #pragma link C++ class Belle2::MCTrajectoryPoint+; // checksum=0xc11b5e2b, version=-1
 #pragma link C++ class Belle2::SimClockState+; // checksum=0x6a8a681e, version=2

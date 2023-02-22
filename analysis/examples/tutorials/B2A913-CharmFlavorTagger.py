30ccd5dd7eff5020820b390f2bca643f2d110c7b
Michel Bertemes <michel.bertemes@desy.de>
2023-02-22 14:40:32 +0900

    Add tutorial and modified script

---
+++ analysis/examples/tutorials/B2A913-CharmFlavorTagger.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
+##########################################################################
+#                                                                        #
+# Stuck? Ask for help at questions.belle2.org                            #
+#                                                                        #
+# This tutorial demonstrates how to include the charm flavor             #
+# tagging user interface into your analysis.                             #
+# The following decay is recontructed:                                   #
+#                                                                        #
+# D0 -> K- pi+                                                           #
+#                                                                        #
+# The qr value, i.e. the flavor*dilution factor                          #
+# is saved as extraInfo to the reconstructed D0.                         #
+#                                                                        #
+##########################################################################
+
+import basf2 as b2
+import modularAnalysis as ma
+import charmFlavorTagger as cft
+import variables.collections as vc
+import variables.utils as vu
+
+
+# create path
+cft_path = b2.Path()
+
+# Environment of the MC or data sample
+environmentType = "default"
+
+# load input ROOT file
+ma.inputMdst(environmentType='default',
+             filename=b2.find_file('Dst2D0pi.root', 'examples', False),
+             path=cft_path)
+
+# create pion and kaon particle lists
+ma.fillParticleList(
+    "pi+:D0", "thetaInCDCAcceptance and dr < 1 and abs(dz) < 3 and pionID>0.1", path=cft_path
+)
+ma.fillParticleList(
+    "K+:D0", "thetaInCDCAcceptance and dr < 1 and abs(dz) < 3 and kaonID>0.1", path=cft_path
+)
+
+# reconstruct D0 signal candidates
+ma.reconstructDecay(
+    decayString="D0:sig -> K-:D0 pi+:D0",
+    cut="1.8 < InvM < 1.9",
+    path=cft_path,
+)
+
+# Does the matching between reconstructed and MC particles
+ma.matchMCTruth(list_name='D0:sig', path=cft_path)
+
+# build the rest of the event associated to the D0
+ma.buildRestOfEvent(target_list_name='D0:sig',
+                    path=cft_path)
+
+# Charm Flavor Tagging Function.
+cft.CharmFlavorTagger(
+    'D0:sig',
+    path=cft_path)
+
+# Select variables that will be stored to ntuple
+d_vars = vc.kinematics+vc.mc_truth+["CFT_qr","CFT_prob"]
+d_vars += vu.create_aliases_for_selected(
+    list_of_variables=vc.kinematics
+    + vc.mc_truth,
+    decay_string="D0 -> ^K- ^pi+",
+)
+
+# Saving variables to ntuple
+output_file = 'B2A913-CharmFlavorTagger.root'
+ma.variablesToNtuple(decayString='D0:sig',
+                     variables=d_vars,
+                     filename=output_file,
+                     treename='D0tree',
+                     path=cft_path)
+
+# Process the events
+b2.process(cft_path)
+
+# print out the summary
+print(b2.statistics)
+
---
+++ analysis/scripts/charmFlavorTagger.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
+import basf2
+import variables as va
+import modularAnalysis as ma
+
+def CharmFlavorTagger(particle_lists,
+                     path=None):
+    """
+    Interfacing for the CharmFlavorTagger.
+
+    This function requires a reconstructed D meson signal particle list with a built RestOfEvent.
+
+    :param particle_lists:  string or list[string], particle list(s) of the reconstructed signal D meson
+    :param path: basf2 path obj
+    :return: None
+    """
+
+    if isinstance(particle_lists, str):
+        particle_lists = [particle_lists]
+
+    # create roe specific paths
+    roe_path = basf2.create_path()
+    dead_end_path = basf2.create_path()
+
+    # define cft specific lists to enable multiple calls, if someone really wants to do that
+    extension = particle_lists[0].replace(':', '_to_')
+    roe_particle_list_cut = 'isInRestOfEvent == 1 and dr < 1 and abs(dz) < 3'
+    roe_particle_list = 'pi+:cft' + '_' + extension
+
+    # filter rest of events only for specific particle list
+    ma.signalSideParticleListsFilter(particle_lists, '', roe_path, dead_end_path)
+
+    # create final state particle lists
+    ma.fillParticleList(roe_particle_list, roe_particle_list_cut, path=roe_path)
+
+    # compute ranking variable and additional CFT input variables, PID_diff=pionID-kaonID, deltaR=sqrt(deltaPhi**2+deltaEta**2)
+    rank_variable = 'opang_shift'
+    va.variables.addAlias(rank_variable,f"abs(formula(angleToClosestInList({particle_lists[0]}) - 3.14159265359/2))")
+    va.variables.addAlias("eta","formula(-1*log(tan(formula(theta/2))))")
+    va.variables.addAlias("phi_sig","particleRelatedToCurrentROE(phi)")
+    va.variables.addAlias("eta_sig","particleRelatedToCurrentROE(eta)")
+    va.variables.addAlias("deltaPhi_temp","abs(formula(phi-phi_sig))")
+    va.variables.addAlias("deltaPhi","conditionalVariableSelector(deltaPhi_temp>3.14159265359,formula(deltaPhi_temp-2*3.14159265359),deltaPhi_temp)")
+    va.variables.addAlias("deltaR","formula(((deltaPhi)**2+(eta-eta_sig)**2)**0.5)")
+    va.variables.addAlias("PID_diff","formula(pionID-kaonID)")
+
+    # split tracks by charge, rank them (keep only the three highest ranking) and write CFT input to extraInfo of signal particle
+    var_list=['mRecoil','PID_diff','deltaR']
+    cft_particle_dict = {'pi+:pos_charge':['charge > 0 and p < infinity', 'p'], 'pi+:neg_charge':['charge < 0 and p < infinity', 'n']}
+
+    for listName, config in cft_particle_dict.items():
+        ma.cutAndCopyList(listName, roe_particle_list, config[0], writeOut=True, path=roe_path)
+        ma.rankByHighest(listName, rank_variable, numBest=3, path=roe_path)
+        roe_dict = {}
+        suffix = config[1]
+        for var in var_list:
+            for i_num in range(1, 3 + 1):
+                roe_dict[f'eventCached(getVariableByRank({listName}, {rank_variable}, {var}, {i_num}))'] = f'pi_{i_num}_{suffix}_{var}'
+                va.variables.addAlias(f'pi_{i_num}_{suffix}_{var}', f'extraInfo(pi_{i_num}_{suffix}_{var})')
+
+        ma.variableToSignalSideExtraInfo(listName, roe_dict, path=roe_path)
+    
+    # apply CFT with MVAExpert module and write output to extraInfo
+    expert_module = basf2.register_module('MVAExpert')
+    expert_module.param('listNames', particle_lists)
+    expert_module.param('identifier', 'CFT.xml')
+
+    expert_module.param('extraInfoName', 'CFT_out')
+
+    roe_path.add_module(expert_module)
+
+    # The CFT output probability should be 0.5 when no track is reconstructed in the ROE
+    va.variables.addAlias('CFT_prob', 'conditionalVariableSelector(isNAN(pi_1_p_deltaR) and isNAN(pi_1_n_deltaR),0.5,formula(1-extraInfo(CFT_out)))')
+    va.variables.addAlias('CFT_qr', 'formula(2*CFT_prob-1)')
+
+    path.for_each('RestOfEvent', 'RestOfEvents', roe_path)

17e7f47d7f235561d39f69c9e211a2c313839805
Giacomo De Pietro <giacomo.de.pietro@desy.de>
2023-02-23 21:12:23 +0900

    Rename scripts and output files accordingly

--- reconstruction/validation/0_dedxPID_ParticleGun.py
+++ reconstruction/validation/0_dedxPID_ParticleGun.py
@@ -12,7 +12,7 @@
 """
 <header>
   <input>PartGunChargedStableGenSim.root</input>
-  <output>EvtGenSimRec_dedx.root</output>
+  <output>ParticleGunSimRec_dedx.root</output>
   <description>Generates dE/dx debug data (DedxTracks) for testing</description>
   <contact>renu2@andrew.cmu.edu</contact>
 </header>
@@ -42,7 +42,7 @@ for m in main.modules():
 
 
 output = basf2.register_module('RootOutput')
-output.param('outputFileName', '../EvtGenSimRec_dedx.root')
+output.param('outputFileName', '../ParticleGunSimRec_dedx.root')
 # let's keep this small
 output.param('branchNames', ['CDCDedxLikelihoods', 'CDCDedxTracks', 'VXDDedxLikelihoods', 'VXDDedxTracks', 'EventMetaData'])
 main.add_module(output)

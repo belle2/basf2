21331d8598e27bbacfd55810b176cf50044c64be
Marcel <mhohmann@student.unimelb.edu.au>
2023-02-24 01:33:04 +0100

    revert unrelated changes

--- ecl/modules/eclFillCellIdMapping/src/eclFillCellIdMappingModule.cc
+++ ecl/modules/eclFillCellIdMapping/src/eclFillCellIdMappingModule.cc
@@ -32,7 +32,6 @@ ECLFillCellIdMappingModule::ECLFillCellIdMappingModule() : Module()
 {
   // Set module properties
   setDescription("Fills a dataobject that provides maping between cell id and store arrays and neighbour maps");
-  setPropertyFlags(c_ParallelProcessingCertified);
 }
 
 void ECLFillCellIdMappingModule::initialize()

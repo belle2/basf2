4e82033ed97fe8efb8c31b63c4dfe828217cf182
Christian Wessel <christian.wessel@desy.de>
2023-04-06 18:07:45 +0200

    Update b2luigi settings.json and setup_basf2.sh scripts to the ones used for the actual training on NAF.

--- tracking/scripts/ckf_training/cdc_to_svd_spacepoint/setup_basf2.sh
+++ tracking/scripts/ckf_training/cdc_to_svd_spacepoint/setup_basf2.sh
@@ -1,8 +1,4 @@
-#cd /afs/desy.de/user/c/cwessel/head
-#source /cvmfs/belle.cern.ch/tools/b2setup
-#source /cvmfs/belle.cern.ch/tools/b2setup prerelease-07-00-00a
-
 olddir="$(pwd)"
-cd /afs/desy.de/user/c/cwessel/head
+cd /nfs/dust/belle2/user/cwessel/head
 source /cvmfs/belle.cern.ch/tools/b2setup
 cd $olddir

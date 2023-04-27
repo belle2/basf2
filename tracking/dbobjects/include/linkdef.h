6c424ae021ad71c2ef07cdbd87f6d8e23a98d447
Christian Wessel <christian.wessel@desy.de>
2023-04-27 10:23:06 +0200

    Fix class version and checksum for ROICalculationParameters class.

--- tracking/dbobjects/include/linkdef.h
+++ tracking/dbobjects/include/linkdef.h
@@ -8,6 +8,6 @@
 #pragma link C++ class Belle2::CKFParameters+; // checksum=0x8dcd40df, version=1
 #pragma link C++ class Belle2::TrackFitMomentumRange+; // checksum=0x8f6de621, version=1
 #pragma link C++ class Belle2::TrackFlippingCuts+; // checksum=0x57cfa387, version=1
-#pragma link C++ class Belle2::ROIParameters+; // checksum=0x4694a7b1, version=2
+#pragma link C++ class Belle2::ROICalculationParameters+; // checksum=0x8691bfeb, version=1
 
 #endif

404f638b42539e6efbb4958887033d44d9249f7a
Björn Spruck <bspruck@uni-mainz.de>
2023-05-15 14:37:35 +0900

    removed unused var

--- dqm/analysis/modules/include/DQMHistAnalysisInputRootFile.h
+++ dqm/analysis/modules/include/DQMHistAnalysisInputRootFile.h
@@ -68,9 +68,6 @@ namespace Belle2 {
     /** Global EventMetaData for run number and event number. */
     StoreObjPtr<EventMetaData> m_eventMetaDataPtr;
 
-    /** The map between canvas name and canvas object. */
-    std::map<std::string, TCanvas*> m_cs;
-
     /** List of histogram name patterns to process. */
     std::vector<std::string> m_histograms;
 

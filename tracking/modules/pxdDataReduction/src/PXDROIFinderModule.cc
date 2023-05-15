64bcccbf402470c03c35b58ebf48ab9d45085734
Christian Wessel <christian.wessel@desy.de>
2023-05-15 13:39:04 +0200

    Re-introducing the ROI calculation parameters as module parameters to PXDROIFinderModule.

--- tracking/modules/pxdDataReduction/src/PXDROIFinderModule.cc
+++ tracking/modules/pxdDataReduction/src/PXDROIFinderModule.cc
@@ -34,6 +34,15 @@ PXDROIFinderModule::PXDROIFinderModule() : Module(), m_ROIinfo()
   addParam("PXDInterceptListName", m_PXDInterceptListName, "name of the list of interceptions", std::string(""));
   addParam("ROIListName", m_ROIListName, "name of the list of ROIs", std::string(""));
 
+
+  addParam("tolerancePhi", m_tolerancePhi, "Tolerance by finding sensor in phi coordinate (radians).", double(0.15));
+  addParam("toleranceZ", m_toleranceZ, "Tolerance by finding sensor in Z coordinate (cm).", double(0.5));
+  addParam("sigmaSystU", m_sigmaSystU, " systematic sigma in the u local coordinate ", double(0.025));
+  addParam("sigmaSystV", m_sigmaSystV, " systematic sigma in the V local coordinate ", double(0.025));
+  addParam("numSigmaTotU", m_numSigmaTotU, " number of sigmas (total) in the U local coordinate ", double(10));
+  addParam("numSigmaTotV", m_numSigmaTotV, " number of sigmas (total) in the V local coordinate ", double(10));
+  addParam("maxWidthU", m_maxWidthU, " upper limit on width of the ROI in the U local coordinate (cm) ", double(0.5));
+  addParam("maxWidthV", m_maxWidthV, " upper limit on width of the ROI in the V local coordinate (cm) ", double(0.5));
 }
 
 

ca9a7c673aa69397e8ee48fdf3d16f34924e002b
Tommy Lam <tommylam@vt.edu>
2023-05-23 03:11:13 +0900

    Adding missing header for doxygen warnings

--- calibration/scripts/prompt/calibrations/caf_vxdcdc_alignment.py
+++ calibration/scripts/prompt/calibrations/caf_vxdcdc_alignment.py
@@ -1,3 +1,10 @@
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
 """
 
 Full Simultaneous Global and Local VXD and CDC alignment with Millepede II

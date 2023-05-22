6b42f2fe791c9da65acd104085a1a1d3964b0eb8
Tommy Lam <tommylam@vt.edu>
2023-05-22 22:57:33 +0900

    Adding copyright header

--- analysis/tests/ecl_variables.py
+++ analysis/tests/ecl_variables.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
 
 """
 A test of the ECL variables using a recently produced mDST file.

5436e26d9274b9075d1928d6f15d9c971a0833e0
LEMETTAIS Clotilde <lemettais@cppm.in2p3.fr>
2023-06-02 01:02:29 +0900

    software modifications in trackFitter to handle DAFparameters payload

--- tracking/trackFitting/fitter/base/src/TrackFitter.cc
+++ tracking/trackFitting/fitter/base/src/TrackFitter.cc
@@ -136,9 +136,18 @@ bool TrackFitter::fit(RecoTrack& recoTrack, genfit::AbsTrackRep* trackRepresenta
 
 void TrackFitter::resetFitterToDefaultSettings()
 {
-  genfit::DAF* dafFitter = new genfit::DAF(true, s_defaultDeltaPValue);
-  dafFitter->setProbCut(s_defaultProbCut);
-  dafFitter->setMaxFailedHits(s_defaultMaxFailedHits);
+  if (!m_DAFparameters.isValid())
+    B2FATAL("DAF parameters are not available.");
+  genfit::DAF* dafFitter = new genfit::DAF(m_DAFparameters->getAnnealingScheme(),
+                                           m_DAFparameters->getMinimumIterations(),
+                                           m_DAFparameters->getMaximumIterations(),
+                                           m_DAFparameters->getMinimumIterationsForPVal(),
+                                           true,
+                                           m_DAFparameters->getDeltaPValue(),
+                                           m_DAFparameters->getDeltaWeight(),
+                                           m_DAFparameters->getProbabilityCut());
+  dafFitter->setProbCut(m_DAFparameters->getProbabilityCut());
+  dafFitter->setMaxFailedHits(m_DAFparameters->getMaximumFailedHits());
 
   m_fitter.reset(dafFitter);
 

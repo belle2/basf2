e94b121d0d262c37a097827de7d6bf6dc12b9bfd
Björn Spruck <bspruck@uni-mainz.de>
2023-06-05 09:34:02 +0900

    enabler and output flusher module

---
+++ dqm/analysis/modules/include/DQMHistAnalysisEpicsOutput.h
@@ -0,0 +1,53 @@
+/**************************************************************************
+ * basf2 (Belle II Analysis Software Framework)                           *
+ * Author: The Belle II Collaboration                                     *
+ *                                                                        *
+ * See git log for contributors and copyright holders.                    *
+ * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
+ **************************************************************************/
+
+#pragma once
+
+//DQM
+#include <dqm/core/DQMHistAnalysis.h>
+#include <TCanvas.h>
+
+namespace Belle2 {
+
+  /**
+   * Example module of how to use MonitoringObject in DQMHistAnalysis module
+   */
+  class DQMHistAnalysisEpicsOutputModule final : public DQMHistAnalysisModule {
+
+  public:
+
+    /**
+     * Constructor
+     */
+    DQMHistAnalysisEpicsOutputModule();
+
+    /**
+     * Destructor
+     */
+    ~DQMHistAnalysisEpicsOutputModule();
+
+    /**
+     * Initialize the Module.
+     */
+    void initialize() override final;
+
+    /**
+     * Event action.
+     */
+    void event() override final;
+
+    /**
+     * Termination action.
+     */
+    void terminate() override final;
+
+  protected:
+
+  };
+
+} // Belle2 namespace

b191ecd3b1388da59a9d624ff8014584c3ce6fae
Giacomo De Pietro <giacomo.pietro@kit.edu>
2023-06-07 07:26:28 +0200

    TUPPR: update default globaltag to main_2023-06-07

--- framework/database/src/Configuration.cc
+++ framework/database/src/Configuration.cc
@@ -21,7 +21,7 @@
 #include <TPython.h>
 
 // Current default globaltag when generating events.
-#define CURRENT_DEFAULT_TAG "main_2023-05-29"
+#define CURRENT_DEFAULT_TAG "main_2023-06-07"
 
 namespace py = boost::python;
 

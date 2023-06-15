a971b317dd3438b734a0cdd547d673cc237ac252
Björn Spruck <bspruck@uni-mainz.de>
2023-06-15 14:50:51 +0900

    move to wrapped EPICS functions, adjust test/example script

--- dqm/analysis/modules/src/DQMHistAnalysisIP.cc
+++ dqm/analysis/modules/src/DQMHistAnalysisIP.cc
@@ -12,8 +12,6 @@
 
 
 #include <dqm/analysis/modules/DQMHistAnalysisIP.h>
-#include <TROOT.h>
-#include <TClass.h>
 
 using namespace std;
 using namespace Belle2;
@@ -35,20 +33,10 @@ DQMHistAnalysisIPModule::DQMHistAnalysisIPModule()
   //Parameter definition
   addParam("HistoName", m_histoName, "Name of Histogram (excl dir)", std::string(""));
   addParam("HistoDirectory", m_histoDirectory, "Name of Histogram dir", std::string(""));
-  addParam("PVName", m_pvPrefix, "PV Prefix", std::string("DQM:TEST:hist:"));
+  addParam("PVName", m_pvPrefix, "PV Prefix and Name", std::string("DQM:TEST:hist:"));
   addParam("MonitorPrefix", m_monPrefix, "Monitor Prefix");// force to be set!
   addParam("minEntries", m_minEntries, "minimum number of new Entries for a fit", 1000);
   B2DEBUG(20, "DQMHistAnalysisIP: Constructor done.");
-}
-
-
-DQMHistAnalysisIPModule::~DQMHistAnalysisIPModule()
-{
-#ifdef _BELLE2_EPICS
-  if (getUseEpics()) {
-    if (ca_current_context()) ca_context_destroy();
-  }
-#endif
 }
 
 void DQMHistAnalysisIPModule::initialize()
@@ -57,12 +45,11 @@ void DQMHistAnalysisIPModule::initialize()
 
   m_monObj = getMonitoringObject("ip");
 
-  if (!hasDeltaPar(m_histoDirectory, m_histoName)) addDeltaPar(m_histoDirectory, m_histoName, HistDelta::c_Entries, m_minEntries,
-        1); // register delta
+  // register delta
+  if (!hasDeltaPar(m_histoDirectory, m_histoName)) addDeltaPar(m_histoDirectory, m_histoName, HistDelta::c_Entries, m_minEntries, 1);
 
-  auto  a = m_histoDirectory + "/" + m_histoName;
-  m_c1 = new TCanvas((a +
-                      "_fit").data()); // prefer to change canvas name to monitorPrefix, but then changes on the web gui are needed :-(
+  // prefer to change canvas name to monitorPrefix, but then changes on the web gui are needed :-(
+  m_c1 = new TCanvas((m_histoDirectory + "/" + m_histoName +  "_fit").data());
 
   m_line = new TLine(0, 10, 0, 0);
   m_line->SetVertical(true);
@@ -75,22 +62,10 @@ void DQMHistAnalysisIPModule::initialize()
 
   m_monObj->addCanvas(m_c1);
 
-  // need the function to get parameter names
-#ifdef _BELLE2_EPICS
-  if (getUseEpics()) {
-    if (!ca_current_context()) SEVCHK(ca_context_create(ca_disable_preemptive_callback), "ca_context_create");
-    std::string aa;
-    aa = m_pvPrefix + "Mean";
-    SEVCHK(ca_create_channel(aa.c_str(), NULL, NULL, 10, &mychid[0]), "ca_create_channel failure");
-    aa = m_pvPrefix + "RMS";
-    SEVCHK(ca_create_channel(aa.c_str(), NULL, NULL, 10, &mychid[1]), "ca_create_channel failure");
-    aa = m_pvPrefix + "Median";
-    SEVCHK(ca_create_channel(aa.c_str(), NULL, NULL, 10, &mychid[2]), "ca_create_channel failure");
-    // Read LO and HI limits from EPICS, seems this needs additional channels?
-    // SEVCHK(ca_get(DBR_DOUBLE,mychid[i],(void*)&data),"ca_get failure"); // data is only valid after ca_pend_io!!
-    SEVCHK(ca_pend_io(5.0), "ca_pend_io failure");
-  }
-#endif
+  registerEpicsPV(m_pvPrefix + "Mean", "Mean");
+  registerEpicsPV(m_pvPrefix + "RMS", "RMS");
+  registerEpicsPV(m_pvPrefix + "Median", "Median");
+  updateEpicsPVs(5.0);
 }
 
 
@@ -138,30 +113,10 @@ void DQMHistAnalysisIPModule::event()
     m_monObj->setVariable(m_monPrefix + "_mean", x);
     m_monObj->setVariable(m_monPrefix + "_width", w);
 
-#ifdef _BELLE2_EPICS
-    if (getUseEpics()) {
-      B2DEBUG(20, "Update EPICS");
-      if (mychid[0]) SEVCHK(ca_put(DBR_DOUBLE, mychid[0], (void*)&x), "ca_set failure");
-      if (mychid[1]) SEVCHK(ca_put(DBR_DOUBLE, mychid[1], (void*)&w), "ca_set failure");
-      if (mychid[2]) SEVCHK(ca_put(DBR_DOUBLE, mychid[2], (void*)&m), "ca_set failure");
-      SEVCHK(ca_pend_io(5.0), "ca_pend_io failure");
-    }
-#endif
-  } else {
-    B2DEBUG(20, "Histo " << m_histoName << " not found in " << m_histoDirectory);
+    B2DEBUG(20, "Now update Epics PVs");
+    setEpicsPV("Mean", x);
+    setEpicsPV("RMS", w);
+    setEpicsPV("Median", m);
+    updateEpicsPVs(5.0);
   }
 }
-
-void DQMHistAnalysisIPModule::terminate()
-{
-#ifdef _BELLE2_EPICS
-  if (getUseEpics()) {
-    for (auto i = 0; i < m_parameters; i++) {
-      if (mychid[i]) SEVCHK(ca_clear_channel(mychid[i]), "ca_clear_channel failure");
-    }
-    SEVCHK(ca_pend_io(5.0), "ca_pend_io failure");
-  }
-#endif
-  B2DEBUG(20, "DQMHistAnalysisIP: terminate called");
-}
-

68f2fa50fe9de8ca6424710ddd715b86b3f79cfe
Suryanarayan Mondal <suryanarayan.mondal@df.unipi.it>
2023-07-17 12:28:13 +0200

    collecting all histograms for all time algorithms in one collector

--- svd/modules/svdClusterTimeShifterCollector/include/SVDClusterTimeShifterCollectorModule.h
+++ svd/modules/svdClusterTimeShifterCollector/include/SVDClusterTimeShifterCollectorModule.h
@@ -20,21 +20,19 @@
 
 #include <svd/dataobjects/SVDCluster.h>
 #include <framework/dataobjects/EventT0.h>
-#include <tracking/dataobjects/RecoTrack.h>
-#include <mdst/dataobjects/Track.h>
 
 namespace Belle2 {
   /**
    * Collector module used to create the histograms needed for the
    * SVD CoG-Time calibration
    */
-  class SVDTimeShifterCollectorModule : public CalibrationCollectorModule {
+  class SVDClusterTimeShifterCollectorModule : public CalibrationCollectorModule {
 
   public:
     /**
      * Constructor
      */
-    SVDTimeShifterCollectorModule();
+    SVDClusterTimeShifterCollectorModule();
 
     /**
      * Initialize the module
@@ -51,23 +49,21 @@ namespace Belle2 {
      */
     void collect() override final;
 
+    /** set prefix of the list of clusters on track */
+    void setSVDClusterOnTracksPrefix(const TString& str) {m_svdClsOnTrksPrefix = str;}
+    /** set list of time algorithm */
+    void setTimeAlgorithm(const std::vector<TString>& lst) {m_timeAlgorithms = lst;}
+
   private:
 
-    /** Maximum size of SVD clusters */
-    int m_maxClusterSize = 6;
+    int m_maxClusterSize = 6; /**< Maximum size of SVD clusters */
+    std::vector<TString> m_timeAlgorithms = {"CoG3", "ELS3", "CoG6"}; /**< List of time algorithms to calibrate */
+    TString m_svdClsOnTrksPrefix = "SVDClustersOnTracks"; /**< SVDClusterOnTracks */
+    std::map<TString, StoreArray<SVDCluster>> m_svdClsOnTrk; /**< SVDClusters store array*/
 
-    /**EventMetaData */
     StoreObjPtr<EventMetaData> m_emdata; /**< EventMetaData store object pointer*/
-
-    /**SVDClusterOnTracks */
-    std::string m_svdClustersOnTracks =
-      "SVDClustersOnTracks"; /**< Name of the SVDClusters store array used as parameter of the module*/
-    StoreArray<SVDCluster> m_svdClsOnTrk; /**< SVDClusters store array*/
-
-    /**EventT0 */
     std::string m_eventTime = "EventT0"; /**< Name of the EventT0 store object pointer used as parameter of the module*/
     StoreObjPtr<EventT0> m_eventT0; /**< EventT0 store object pointer*/
-
   };
 
 } // end namespace Belle2
--- svd/modules/svdClusterTimeShifterCollector/src/SVDClusterTimeShifterCollectorModule.cc
+++ svd/modules/svdClusterTimeShifterCollector/src/SVDClusterTimeShifterCollectorModule.cc
@@ -5,21 +5,20 @@
  * See git log for contributors and copyright holders.                    *
  * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
  **************************************************************************/
-#include <svd/modules/svdTimeShifterCollector/SVDTimeShifterCollectorModule.h>
+#include <svd/modules/svdClusterTimeShifterCollector/SVDClusterTimeShifterCollectorModule.h>
 
-using namespace std;
 using namespace Belle2;
 
 //-----------------------------------------------------------------
 //                 Register the Module
 //-----------------------------------------------------------------
-REG_MODULE(SVDTimeShifterCollector);
+REG_MODULE(SVDClusterTimeShifterCollector);
 
 //-----------------------------------------------------------------
 //                 Implementation
 //-----------------------------------------------------------------
 
-SVDTimeShifterCollectorModule::SVDTimeShifterCollectorModule() : CalibrationCollectorModule()
+SVDClusterTimeShifterCollectorModule::SVDClusterTimeShifterCollectorModule() : CalibrationCollectorModule()
 {
   //Set module properties
 
@@ -27,18 +26,20 @@ SVDTimeShifterCollectorModule::SVDTimeShifterCollectorModule() : CalibrationColl
   setPropertyFlags(c_ParallelProcessingCertified);
 
   addParam("MaxClusterSize", m_maxClusterSize, "Maximum size of SVD clusters", m_maxClusterSize);
-  addParam("SVDClustersOnTracksName", m_svdClustersOnTracks, "Name of the SVDClustersOnTracks list", m_svdClustersOnTracks);
   addParam("EventT0Name", m_eventTime, "Name of the EventT0 list", m_eventTime);
 }
 
-void SVDTimeShifterCollectorModule::prepare()
+void SVDClusterTimeShifterCollectorModule::prepare()
 {
 
-  m_svdClsOnTrk.isRequired(m_svdClustersOnTracks);
+  for (auto alg : m_timeAlgorithms) {
+    m_svdClsOnTrk[alg] = StoreArray<SVDCluster>();
+    m_svdClsOnTrk[alg].isRequired((m_svdClsOnTrksPrefix + '_' + alg).Data());
+  }
   m_eventT0.isRequired(m_eventTime);
 
+  // getting all the svd sensors
   VXD::GeoCache& geoCache = VXD::GeoCache::getInstance();
-
   std::vector<Belle2::VxdID> allSensors;
   for (auto layer : geoCache.getLayers(VXD::SensorInfoBase::SVD))
     for (auto ladder : geoCache.getLadders(layer))
@@ -48,55 +49,56 @@ void SVDTimeShifterCollectorModule::prepare()
   int numberOfSensorBin = 2 * int(allSensors.size());
   B2INFO("Number of SensorBin: " << numberOfSensorBin);
 
-  TH3F* __hClusterSizeVsTimeResidual__  = new TH3F("__hClusterSizeVsTimeResidual__", "ClusterSize vs Time Residual",
-                                                   100, -25., 25., m_maxClusterSize, 0.5, m_maxClusterSize + 0.5,
-                                                   numberOfSensorBin, + 0.5, numberOfSensorBin + 0.5);
-  TH1F* __hBinToSensorMap__             = new TH1F("__hBinToSensorMap__", "__BinToSensorMap__",
-                                                   numberOfSensorBin, + 0.5, numberOfSensorBin + 0.5);
-  __hClusterSizeVsTimeResidual__->GetZaxis()->SetTitle("Sensor");
-  __hClusterSizeVsTimeResidual__->GetYaxis()->SetTitle("Cluster Size");
-  __hClusterSizeVsTimeResidual__->GetXaxis()->SetTitle("Cluster Time - EventT0 (ns)");
+  for (auto alg : m_timeAlgorithms) {
+    TH3F* __hClusterSizeVsTimeResidual__  = new TH3F(("__hClusterSizeVsTimeResidual__" + alg).Data(),
+                                                     ("ClusterSize vs " + alg + " Time Residual").Data(),
+                                                     100, -25., 25., m_maxClusterSize, 0.5, m_maxClusterSize + 0.5,
+                                                     numberOfSensorBin, + 0.5, numberOfSensorBin + 0.5);
+    __hClusterSizeVsTimeResidual__->GetZaxis()->SetTitle("Sensor");
+    __hClusterSizeVsTimeResidual__->GetYaxis()->SetTitle("Cluster Size");
+    __hClusterSizeVsTimeResidual__->GetXaxis()->SetTitle("Cluster Time - EventT0 (ns)");
+    registerObject<TH3F>(__hClusterSizeVsTimeResidual__->GetName(), __hClusterSizeVsTimeResidual__);
+  }
 
+  TH1F* __hBinToSensorMap__ = new TH1F("__hBinToSensorMap__", "__BinToSensorMap__",
+                                       numberOfSensorBin, + 0.5, numberOfSensorBin + 0.5);
   int tmpBinCnt = 0;
   for (auto sensor : allSensors) {
     for (auto view : {'U', 'V'}) {
       tmpBinCnt++;
-      TString binLabel = TString::Format("L%iS%iS%c",
-                                         sensor.getLayerNumber(),
-                                         sensor.getSensorNumber(),
-                                         view);
+      TString binLabel = TString::Format("L%iS%iS%c", sensor.getLayerNumber(), sensor.getSensorNumber(), view);
       __hBinToSensorMap__->GetXaxis()->SetBinLabel(tmpBinCnt, binLabel);
     }
   }
-  registerObject<TH3F>(__hClusterSizeVsTimeResidual__->GetName(), __hClusterSizeVsTimeResidual__);
   registerObject<TH1F>(__hBinToSensorMap__->GetName(), __hBinToSensorMap__);
 }
 
-void SVDTimeShifterCollectorModule::startRun()
+void SVDClusterTimeShifterCollectorModule::startRun()
 {
-  getObjectPtr<TH3F>("__hClusterSizeVsTimeResidual__")->Reset();
+  for (auto alg : m_timeAlgorithms)
+    getObjectPtr<TH3F>(("__hClusterSizeVsTimeResidual__" + alg).Data())->Reset();
 }
 
-void SVDTimeShifterCollectorModule::collect()
+void SVDClusterTimeShifterCollectorModule::collect()
 {
   if (m_eventT0->hasEventT0()) {
     float eventT0 = m_eventT0->getEventT0();
     getObjectPtr<TH1F>("hEventT0")->Fill(eventT0);
 
     // Fill histograms clusters on tracks
-    for (const auto& svdCluster : m_svdClsOnTrk) {
-      // get cluster time
-      float clTime = svdCluster.getClsTime();
-      float clSize = svdCluster.getSize();
+    for (auto alg : m_timeAlgorithms)
+      for (const auto& svdCluster : m_svdClsOnTrk[alg]) {
+        // get cluster time
+        float clTime = svdCluster.getClsTime();
+        float clSize = svdCluster.getSize();
 
-      TString binLabel = TString::Format("L%iS%iS%c",
-                                         svdCluster.getSensorID().getLayerNumber(),
-                                         svdCluster.getSensorID().getSensorNumber(),
-                                         svdCluster.isUCluster() ? 'U' : 'V');
-      int sensorBin = getObjectPtr<TH1F>("__hBinToSensorMap__")->GetXaxis()->FindBin(binLabel.Data());
-      double sensorBinCenter = getObjectPtr<TH1F>("__hBinToSensorMap__")->GetXaxis()->GetBinCenter(sensorBin);
-
-      getObjectPtr<TH3F>("__hClusterSizeVsTimeResidual__")->Fill(clTime - eventT0, clSize, sensorBinCenter);
-    };
+        TString binLabel = TString::Format("L%iS%iS%c",
+                                           svdCluster.getSensorID().getLayerNumber(),
+                                           svdCluster.getSensorID().getSensorNumber(),
+                                           svdCluster.isUCluster() ? 'U' : 'V');
+        int sensorBin = getObjectPtr<TH1F>("__hBinToSensorMap__")->GetXaxis()->FindBin(binLabel.Data());
+        double sensorBinCenter = getObjectPtr<TH1F>("__hBinToSensorMap__")->GetXaxis()->GetBinCenter(sensorBin);
+        getObjectPtr<TH3F>(("__hClusterSizeVsTimeResidual__" + alg).Data())->Fill(clTime - eventT0, clSize, sensorBinCenter);
+      }
   }
 }

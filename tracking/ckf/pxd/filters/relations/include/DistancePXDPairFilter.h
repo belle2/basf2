032e206d00c639ec3ae97f285d505308b58d1a49
Christian Wessel <christian.wessel@desy.de>
2023-08-03 14:45:26 +0200

    Add new module parameters for cut values and the function declaration to set the values to the DistancePXDPairFilter.h.

--- tracking/ckf/pxd/filters/relations/include/DistancePXDPairFilter.h
+++ tracking/ckf/pxd/filters/relations/include/DistancePXDPairFilter.h
@@ -16,5 +16,13 @@ namespace Belle2 {
   public:
     /// Return the weight based on azimuthal-angle separation
     TrackFindingCDC::Weight operator()(const std::pair<const CKFToPXDState*, const CKFToPXDState*>& relation) override;
+    /// Expose the parameters.
+    void exposeParameters(ModuleParamList* moduleParamList, const std::string& prefix) override;
+
+  private:
+    /// Pre-filter relations in phi between seed states and hit states
+    float m_param_PhiRecoTrackToHitCut = 0.8;
+    /// Pre-filter relations in theta between hit states
+    float m_param_PhiHitHitCut = 1.0;
   };
 }

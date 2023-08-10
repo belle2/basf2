60f7fa2570161ede8050d40d950a7d21f0140bae
Christian Wessel <christian.wessel@desy.de>
2023-08-10 17:44:19 +0200

    Replace TVector2 by XYVector in CDCTrigger2DFinderModule.

--- trg/cdc/modules/houghtracking/src/CDCTrigger2DFinderModule.cc
+++ trg/cdc/modules/houghtracking/src/CDCTrigger2DFinderModule.cc
@@ -184,7 +184,7 @@ CDCTrigger2DFinderModule::event()
     phi = phi * 2. * M_PI / (TSoffset[iSL + 1] - TSoffset[iSL]);
     double r = radius[iSL][int(m_usePriority &&
                                m_segmentHits[iHit]->getPriorityPosition() < 3)];
-    TVector2 pos(cos(phi) / r, sin(phi) / r);
+    ROOT::Math::XYVector pos(cos(phi) / r, sin(phi) / r);
     hitMap.insert(std::make_pair(iHit, std::make_pair(iSL, pos)));
   }
 

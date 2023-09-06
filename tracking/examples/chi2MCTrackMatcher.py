eda1464b4fd8b5d8c6d3a7326610e60a67e43e57
Christian Wessel <christian.wessel@desy.de>
2023-09-06 10:52:57 +0200

    Simplify how the chi2 matcher module is added to the path, and remove setting debug level.

--- tracking/examples/chi2MCTrackMatcher.py
+++ tracking/examples/chi2MCTrackMatcher.py
@@ -1,5 +1,5 @@
 # Path building
-from basf2 import create_path, register_module, LogLevel, process, statistics
+from basf2 import create_path, process, statistics
 from simulation import add_simulation
 from tracking import add_prefilter_tracking_reconstruction
 
@@ -17,25 +17,12 @@ add_simulation(path, bkgOverlay=False, forceSetPXDDataReduction=True, usePXDData
 add_prefilter_tracking_reconstruction(path)
 
 # Add the Chi2-matcher to path
-chi2Matcher = register_module("Chi2MCTrackMatcher")
-
-# Define input parameters
-
-# Chi2 Cut Off values.
-# These are the reasonable default Cut Off values:
-CutOffs = [128024, 95, 173, 424, 90, 424]
-chi2Matcher.param("CutOffs", CutOffs)
-# Package used for inversion of the covariance matrix:
+# We need CutOffs values, which are from a preliminary optimisation
+# In addition, a package used for inversion of the covariance matrix is needed, which is set by the linalg parameter
 # ROOT is default since in general it is faster and scales better
 # False: ROOT [default]; True: eigen
-chi2Matcher.param("linalg", False)
-
-# Shows some additional Debug messages
-chi2Matcher.logging.log_level = LogLevel.DEBUG
-chi2Matcher.logging.debug_level = 30
-
 # Add the Chi2-matcher module to the execution path
-path.add_module(chi2Matcher)
+path.add_module("Chi2MCTrackMatcher", CutOffs=[128024, 95, 173, 424, 90, 424], linalg=False)
 # process the path
 process(path)
 # show some module statistics

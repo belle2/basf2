f813b52cbcbe2ca404bb959e919702adcf644119
Christian Wessel <christian.wessel@desy.de>
2023-09-06 10:53:22 +0200

    Add copyright header.

--- tracking/examples/chi2MCTrackMatcher.py
+++ tracking/examples/chi2MCTrackMatcher.py
@@ -1,3 +1,11 @@
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 # Path building
 from basf2 import create_path, process, statistics
 from simulation import add_simulation

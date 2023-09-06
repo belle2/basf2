45fe70aaee5e116df2e74c20cbcf02939df8dad6
Christian Wessel <christian.wessel@desy.de>
2023-09-06 18:27:00 +0200

    Simplification of the pxdDataReduction example script.

--- tracking/examples/pxdDataReduction.py
+++ tracking/examples/pxdDataReduction.py
@@ -12,57 +12,33 @@ import basf2 as b2
 from tracking import add_tracking_reconstruction
 from simulation import add_simulation
 
-numEvents = 2000
-
-# first register the modules
-
 b2.set_random_seed(1)
-
-eventinfosetter = b2.register_module('EventInfoSetter')
-eventinfosetter.param('expList', [0])
-eventinfosetter.param('runList', [1])
-eventinfosetter.param('evtNumList', [numEvents])
-
-eventinfoprinter = b2.register_module('EventInfoPrinter')
-
-evtgeninput = b2.register_module('EvtGenInput')
-evtgeninput.logging.log_level = b2.LogLevel.INFO
-
-pxdROIFinder = b2.register_module('PXDROIFinder')
-pxdROIFinder.logging.log_level = b2.LogLevel.DEBUG
-# pxdROIFinder.logging.debug_level = 2
-param_pxdROIFinder = {
-    'recoTrackListName': 'RecoTracks',
-    'PXDInterceptListName': 'PXDIntercepts',
-    'ROIListName': 'ROIs',
-}
-pxdROIFinder.param(param_pxdROIFinder)
-
-pxdROIFinderAnalysis = b2.register_module('PXDROIFinderAnalysis')
-pxdROIFinderAnalysis.logging.log_level = b2.LogLevel.RESULT
-pxdROIFinderAnalysis.logging.debug_level = 1
-param_pxdROIFinderAnalysis = {
-    'recoTrackListName': 'RecoTracks',
-    'PXDInterceptListName': 'PXDIntercepts',
-    'ROIListName': 'ROIs',
-    'writeToRoot': True,
-    'rootFileName': 'pxdDataRedAnalysis_SVDCDC_MCTF_test',
-}
-pxdROIFinderAnalysis.param(param_pxdROIFinderAnalysis)
 
 # Create paths
 main = b2.create_path()
 
 # Add modules to paths
-main.add_module(eventinfosetter)
-main.add_module(eventinfoprinter)
-main.add_module(evtgeninput)
+main.add_module('EventInfoSetter', expList=[0], runList=[1], evtNumList=[100])
+main.add_module('EvtGenInput', logLevel=b2.LogLevel.INFO)
+
 add_simulation(main, components=['SVD', 'CDC'], forceSetPXDDataReduction=True, usePXDDataReduction=False)
+
 add_tracking_reconstruction(main, ['SVD', 'CDC'], mcTrackFinding=True)
-main.add_module(pxdROIFinder)
-main.add_module(pxdROIFinderAnalysis)
-# display = register_module("Display")
-# main.add_module(display)
+
+main.add_module('PXDROIFinder',
+                recoTrackListName='RecoTracks',
+                PXDInterceptListName='PXDIntercepts',
+                ROIListName='ROIs',
+                logLevel=b2.LogLevel.DEBUG)
+
+main.add_module('PXDROIFinderAnalysis',
+                recoTrackListName='RecoTracks',
+                PXDInterceptListName='PXDIntercepts',
+                ROIListName='ROIs',
+                writeToRoot=True,
+                rootFileName='pxdDataRedAnalysis_SVDCDC_MCTF_test',
+                logLevel=b2.LogLevel.RESULT)
+main.add_module('Progress')
 
 # Process events
 b2.process(main)

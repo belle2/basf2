7056a69cdcf059d0ac72806aa66b1723b8c1bfec
Jitendra Kumar <jkumar@iitj.ac.in>
2023-09-25 10:13:19 +0200

    adding new analysis module for CDC

---
+++ dqm/analysis/modules/include/DQMHistAnalysisCDCEpics.h
@@ -0,0 +1,114 @@
+/**************************************************************************
+ * basf2 (Belle II Analysis Software Framework)                           *
+ * Author: The Belle II Collaboration                                     *
+ *                                                                        *
+ * See git log for contributors and copyright holders.                    *
+ * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
+ **************************************************************************/
+
+#pragma once
+
+#include <framework/database/DBArray.h>
+#include <framework/database/DBObjPtr.h>
+#include <dqm/core/DQMHistAnalysis.h>
+
+#include <TROOT.h>
+#include <TLine.h>
+#include <TH2F.h>
+#include <TH1F.h>
+#include <TStyle.h>
+#include <TBox.h>
+#include <numeric>
+#include <iostream>
+
+namespace Belle2 {
+
+  /**
+   * Make summary of data quality from reconstruction
+   */
+  class DQMHistAnalysisCDCEpicsModule final : public DQMHistAnalysisModule {
+
+  public:
+
+    /**
+     * Constructor
+     */
+    DQMHistAnalysisCDCEpicsModule();
+
+    /**
+     * Destructor
+     */
+    ~DQMHistAnalysisCDCEpicsModule();
+
+    /**
+     * Initialize the Module.
+     */
+    void initialize() override final;
+
+    /**
+     * Called when entering a new run.
+     */
+    void beginRun() override final;
+
+    /**
+     * intra-run actions (EPICC PVs).
+     */
+    void event() override final;
+
+    /**
+     * End-of-run action.
+     */
+    void endRun() override final;
+
+    /**
+     * Termination action.
+     */
+    void terminate() override final;
+
+    /**
+     * get histogram styles
+     */
+    void getHistStyle(TH1F*& htemp, int color, int style) const
+    {
+      gStyle->SetOptStat("ne");
+      htemp->SetMarkerColor(color);
+      htemp->SetMarkerStyle(style);
+      htemp->SetMarkerSize(0.70);
+      htemp->Sumw2(0);
+    };
+
+    /**
+     * Get median of given histogram
+     */
+    float getHistMedian(TH1D* h) const;
+
+
+  protected:
+
+    //Canvas for DQM analysis IR plots
+    TCanvas* m_cBoards = nullptr; /**< main panel */
+    TBox* m_boxadc  = nullptr; /**< box for normal ADC window */
+    TBox* m_boxtdc  = nullptr; /**< box for normal ADC window */
+
+    std::string m_histoDir = ""; /**< histogram dir of CDC DQMs */
+    std::string m_histoADC = ""; /**< ADC histogram names of CDC DQMs */
+    std::string m_histoTDC = ""; /**< TDC histogram names of CDC DQMs */
+    std::string m_pvPrefix = ""; /**< Prefix of PVs */
+
+    int m_minevt;/**< min events for single intra-run point */
+    double m_minadc;/**< min adc median thershold accepted */
+    double m_maxadc;/**< max adc median thershold accepted */
+    double m_mintdc;/**< min tdc median thershold accepted */
+    double m_maxtdc;/**< max tdc median thershold accepted */
+
+    TH2F* delta_adc = nullptr; /**< 2D histograms (Card vs ADC) from track associated hits (Intra run)*/
+    TH1F* hist_adc = nullptr; /**< same as above but for Integrated run*/
+
+    TH2F* delta_tdc = nullptr; /**< 2D histograms (Card vs ADC) from track associated hits (Intra run)*/
+    TH1F* hist_tdc = nullptr; /**< same as above but for Integrated run*/
+
+    TH1D* m_hADCs[300]; /**< ADC histograms with track associated hits for each board (0-299) */
+    TH1D* m_hTDCs[300]; /**< TDC histograms with track associated hits for each board (0-299) */
+
+  };
+} // Belle2 namespace
---
+++ dqm/analysis/modules/src/DQMHistAnalysisCDCEpics.cc
@@ -0,0 +1,176 @@
+/**************************************************************************
+ * basf2 (Belle II Analysis Software Framework)                           *
+ * Author: The Belle II Collaboration                                     *
+ *                                                                        *
+ * See git log for contributors and copyright holders.                    *
+ * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
+ **************************************************************************/
+
+#include <dqm/analysis/modules/DQMHistAnalysisCDCEpics.h>
+
+using namespace std;
+using namespace Belle2;
+
+//-----------------------------------------------------------------
+//                 Register module
+//-----------------------------------------------------------------
+REG_MODULE(DQMHistAnalysisCDCEpics);
+
+DQMHistAnalysisCDCEpicsModule::DQMHistAnalysisCDCEpicsModule()
+  : DQMHistAnalysisModule()
+{
+  addParam("HistDirectory", m_histoDir, "CDC Dir of DQM Histogram", std::string("CDC"));
+  addParam("HistADC", m_histoADC, "ADC Histogram Name", std::string("hADC"));
+  addParam("HistTDC", m_histoTDC, "TDC Histogram Name", std::string("hTDC"));
+  addParam("PvPrefix", m_pvPrefix, "PV Prefix and Name", std::string("CDC:"));
+  addParam("MinEvt", m_minevt, "Min events for intra-run point", 10000);
+  addParam("MinADC", m_minadc, "Min ADC median acceptable", 70.0);
+  addParam("MaxADC", m_maxadc, "max ADC median acceptable", 130.0);
+  addParam("MinTDC", m_mintdc, "min TDC median acceptable", 4600.0);
+  addParam("MaxTDC", m_maxtdc, "max TDC median acceptable", 4900.0);
+
+  for (int i = 0; i < 300; i++) {
+    m_hADCs[i] = nullptr;
+    m_hTDCs[i] = nullptr;
+  }
+
+  B2DEBUG(20, "DQMHistAnalysisCDCEpics: Constructor done.");
+}
+
+DQMHistAnalysisCDCEpicsModule::~DQMHistAnalysisCDCEpicsModule()
+{
+
+}
+
+void DQMHistAnalysisCDCEpicsModule::initialize()
+{
+  hist_adc = new TH1F("hist_adc", "hist_adc", 300, 0, 300);
+  hist_adc->SetTitle("ADC Medians of CDC boards; CDC board index; TDC medians");
+
+  hist_tdc = new TH1F("hist_tdc", "hist_tdc", 300, 0, 300);
+  hist_tdc->SetTitle("TDC Medians of CDC boards; CDC board index; TDC medians");
+
+  m_cBoards = new TCanvas("cdc_boardstatus", "cdc_boardstatus", 1000, 400);
+  m_cBoards->Divide(1, 2);
+
+  if (!hasDeltaPar(m_histoDir, m_histoADC))
+    addDeltaPar(m_histoDir, m_histoADC, HistDelta::c_Entries, m_minevt, 1);
+
+  if (!hasDeltaPar(m_histoDir, m_histoTDC))
+    addDeltaPar(m_histoDir, m_histoTDC, HistDelta::c_Entries, m_minevt, 1);
+
+  registerEpicsPV(m_pvPrefix + "m_fracadcmedian", "m_fracadcmedian");
+  registerEpicsPV(m_pvPrefix + "m_fractdcmedian", "m_fractdcmedian");
+
+  //creating box for normal adc and tdc windows
+  m_boxadc = new TBox(0, m_minadc, 300, m_maxadc);
+  m_boxadc->SetFillColorAlpha(kYellow, 0.40);
+  m_boxadc->SetLineColor(kGray + 3);
+
+  m_boxtdc = new TBox(0, m_mintdc, 300, m_maxtdc);
+  m_boxtdc->SetFillColorAlpha(kYellow, 0.40);
+  m_boxtdc->SetLineColor(kGray + 3);
+
+  B2DEBUG(20, "DQMHistAnalysisCDCEpics: initialized.");
+}
+
+void DQMHistAnalysisCDCEpicsModule::beginRun()
+{
+  B2DEBUG(20, "DQMHistAnalysisCDCEpics: beginRun run called");
+}
+
+void DQMHistAnalysisCDCEpicsModule::event()
+{
+  //get intra-run histogram from CDC DQM module
+  delta_adc = (TH2F*)getDelta(m_histoDir, m_histoADC, 0, true); //true=only if updated
+  delta_tdc = (TH2F*)getDelta(m_histoDir, m_histoTDC, 0, true);
+  if (delta_adc == nullptr || delta_tdc == nullptr) {
+    if (delta_adc == nullptr)B2INFO("Histogram of hADCs not found (intra-run). Exiting");
+    if (delta_tdc == nullptr)B2INFO("Histogram of hTDCs not found (intra-run). Exiting");
+    return;
+  }
+
+  int cadcgood = 0, ctdcgood = 0;
+  double sumadcgood = 0, sumtdcgood = 0;
+  for (int ic = 0; ic < 300; ++ic) {
+    m_hADCs[ic] = delta_adc->ProjectionY(Form("hADC%d", ic + 1), ic + 1, ic + 1, "");
+    m_hADCs[ic]->SetTitle(Form("hADC%d", ic));
+    float md_adc = getHistMedian(m_hADCs[ic]);
+    if (md_adc >= m_minadc && md_adc <= m_maxadc) {
+      sumadcgood = sumadcgood + md_adc;
+      cadcgood++;
+    }
+
+    m_hTDCs[ic] = delta_tdc->ProjectionY(Form("hTDC%d", ic + 1), ic + 1, ic + 1, "");
+    m_hTDCs[ic]->SetTitle(Form("hTDC%d", ic));
+    float md_tdc = getHistMedian(m_hTDCs[ic]);
+    if (md_tdc >= m_mintdc && md_tdc <= m_maxtdc) {
+      sumtdcgood = sumtdcgood + md_tdc;
+      ctdcgood++;
+    }
+  }
+
+  auto adcfrac = cadcgood / 3; // (100.0/300) in %
+  setEpicsPV("m_fracadcmedian", adcfrac);
+
+  auto tdcfrac = ctdcgood / 3;
+  setEpicsPV("m_fractdcmedian", tdcfrac);
+
+  updateEpicsPVs(5.0); // 5 is time in seconds
+  B2DEBUG(20, "DQMHistAnalysisCDCEpics: end event");
+}
+
+void DQMHistAnalysisCDCEpicsModule::endRun()
+{
+  //run integrated histogram from CDC Module
+  auto hrun_adc = (TH2F*)findHist("CDC/hADC");// only if updated
+  auto hrun_tdc = (TH2F*)findHist("CDC/hTDC");
+  if (hrun_adc == nullptr || hrun_tdc == nullptr) {
+    if (hrun_adc == nullptr)B2INFO("Histogram hADC not found (integrated-run)");
+    if (hrun_tdc == nullptr)B2INFO("Histogram hTDC not found (integrated-run)");
+    return;
+  }
+
+  hist_adc->Reset();
+  hist_tdc->Reset();
+  for (int ic = 0; ic < 300; ++ic) {
+    auto hadc_proj = (TH1D*)hrun_adc->ProjectionY(Form("hrunADC%d", ic + 1), ic + 1, ic + 1, "");
+    float md_adc_run = getHistMedian(hadc_proj);
+    hist_adc->SetBinContent(ic + 1, md_adc_run);
+
+    auto htdc_proj = hrun_tdc->ProjectionY(Form("hrunADC%d", ic + 1), ic + 1, ic + 1, "");
+    float md_tdc_run = getHistMedian(htdc_proj);
+    hist_tdc->SetBinContent(ic + 1, md_tdc_run);
+  }
+
+  // Draw canvas
+  m_cBoards->Clear();
+  m_cBoards->Divide(2, 1);
+  m_cBoards->cd(1);// necessary!
+  getHistStyle(hist_adc, 2, 20);
+  m_boxadc->Draw();
+  hist_adc->Draw();
+  m_cBoards->cd(2);// necessary!
+  getHistStyle(hist_tdc, 4, 21);
+  m_boxtdc->Draw();
+  hist_tdc->Draw();
+
+  B2DEBUG(20, "DQMHistAnalysisCDCEpics: end run");
+}
+
+float DQMHistAnalysisCDCEpicsModule::getHistMedian(TH1D* h) const
+{
+  TH1D* hist = (TH1D*)h->Clone();
+  hist->SetBinContent(1, 0.0); // Exclude 0-th bin
+  if (hist->GetMean() == 0) {return 0.0;} // Avoid an error if only TCD/ADC=0 entries
+  double quantiles[1] = {0.0}; // One element to store median
+  double probSums[1] = {0.5}; // Median definition
+  hist->GetQuantiles(1, quantiles, probSums);
+  float median = quantiles[0];
+  return median;
+}
+
+void DQMHistAnalysisCDCEpicsModule::terminate()
+{
+  B2DEBUG(20, "DQMHistAnalysisCDCEpics: terminate called");
+}

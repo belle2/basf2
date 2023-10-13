6829b4c8f00db529a2af398dbc26562f25283f56
DONG Thanh <dongthanh.ht@gmail.com>
2023-10-13 18:38:12 +0900

    refactory code to add vertex fit and components

--- calibration/scripts/prompt/calibrations/caf_cdc_fudgefactor.py
+++ calibration/scripts/prompt/calibrations/caf_cdc_fudgefactor.py
@@ -13,7 +13,8 @@ import basf2
 from ROOT import Belle2
 from caf.framework import Calibration
 from caf import strategies
-
+from modularAnalysis import fillParticleList, cutAndCopyList, reconstructDecay, applyCuts
+from vertex import treeFit
 
 #: Tells the automated system some details of this script
 settings = CalibrationSettings(name="CDC Tracking",
@@ -29,7 +30,9 @@ settings = CalibrationSettings(name="CDC Tracking",
                                expert_config={
                                    "min_events_per_file": 500,
                                    "max_events_per_file": 30000,
-                                   "payload_boundaries": [],
+                                   "components": ["CDC", "ECL", "KLM"],
+                                   "vertex_fit": 0,
+                                    "payload_boundaries": [],
                                    "backend_args": {"request_memory": "4 GB"}
                                })
 
@@ -44,7 +47,9 @@ def get_calibrations(input_data, **kwargs):
     expert_config = kwargs.get("expert_config")
     min_events_per_file = expert_config["min_events_per_file"]
     max_events_per_file = expert_config["max_events_per_file"]
-
+#    file_format = expert_config["file_format"]
+    components = expert_config["components"]
+    vertex_fit = expert_config["vertex_fit"]
     # In this script we want to use one sources of input data.
     # Get the input files  from the input_data variable
     file_to_iov_mumu = input_data["mumu_tight_or_highm_calib"]
@@ -59,14 +64,9 @@ def get_calibrations(input_data, **kwargs):
     payload_boundaries.extend([ExpRun(*boundary) for boundary in expert_config["payload_boundaries"]])
     basf2.B2INFO(f"Payload boundaries from expert_config: {payload_boundaries}")
 
-    # Get the overall IoV we our process should cover. Includes the end values that we may want to ignore since our output
-    # IoV should be open ended. We could also use this as part of the input data selection in some way.
-    requested_iov = kwargs.get("requested_iov", None)
-
-    # Get the expert configurations if you have something you might configure from them. It should always be available
-    # expert_config = kwargs.get("expert_config")
     from caf.utils import IoV
     # The actual value our output IoV payload should have. Notice that we've set it open ended.
+    requested_iov = kwargs.get("requested_iov", None)
     output_iov = IoV(requested_iov.exp_low, requested_iov.run_low, -1, -1)
 
     # for SingleIOV stratrgy, it's better to set the granularity to 'all' so that the collector jobs will run faster
@@ -88,7 +88,9 @@ def get_calibrations(input_data, **kwargs):
                               collector=col,
                               algorithms=algo,
                               input_files=input_files_mumu,
-                              pre_collector_path=pre_collector(max_events_per_file))
+                              pre_collector_path=pre_collector(max_events_per_file,
+                                                               components=components,
+                                                               vertex_fit=vertex_fit))
 #                              backend_args=expert_config["backend_args"])
     #                           collector_granularity=collector_granularity)
     # Do this for the default AlgorithmStrategy to force the output payload IoV
@@ -106,7 +108,7 @@ def get_calibrations(input_data, **kwargs):
 
 
 #################################################
-def pre_collector(max_events=None, is_mdst=None):
+def pre_collector(max_events=None, components=["CDC", "ECL", "KLM"], vertex_fit=0):
     """
     Define pre collection (reconstruction in our purpose).
     Probably, we need only CDC and ECL data.
@@ -120,23 +122,28 @@ def pre_collector(max_events=None, is_mdst=None):
     from softwaretrigger.constants import HLT_INPUT_OBJECTS
     reco_path = create_path()
     if max_events is None:
-        root_input = register_module(
-            'RootInput',
-            branchNames=HLT_INPUT_OBJECTS
-        )
+        root_input = register_module('RootInput', branchNames=HLT_INPUT_OBJECTS)
     else:
-        root_input = register_module(
-            'RootInput',
-            branchNames=HLT_INPUT_OBJECTS,
-            entrySequences=[
-                '0:{}'.format(max_events)])
-
+        root_input = register_module('RootInput', branchNames=HLT_INPUT_OBJECTS,
+                                     entrySequences=['0:{}'.format(max_events)])
     reco_path.add_module(root_input)
     reco_path.add_module('Progress')
     # unpack
     from rawdata import add_unpackers
     from reconstruction import add_reconstruction
-    add_unpackers(reco_path, components=["CDC", "ECL", "KLM"])
+    add_unpackers(reco_path, components=components)
     # reconstruction
-    add_reconstruction(reco_path, components=["CDC", "ECL", "KLM"])
+    add_reconstruction(reco_path,
+                       components=components,
+                       append_full_grid_cdc_eventt0=True)
+
+    fillParticleList('gamma:HLT', 'E>0.1', path=reco_path)
+    goodTrack = 'abs(d0) < 2.0 and abs(z0) < 4.0 and pt > 2.0 and useCMSFrame(p) > 0.5'
+    fillParticleList('mu+:HLT', goodTrack, path=reco_path)
+    cutAndCopyList('mu+:sel', 'mu+:HLT', goodTrack, path=reco_path)
+    reconstructDecay('vpho:mumu -> mu+:sel mu-:sel', '', path=reco_path)  # apply event cuts
+    applyCuts('vpho:mumu', '[nCleanedTracks('+goodTrack+') == 2]', path=reco_path)
+    if vertex_fit == 1:
+        treeFit('vpho:mumu', ipConstraint=False, updateAllDaughters=False, path=reco_path)
+
     return reco_path
--- cdc/modules/cdcCalibrationCollector/src/CDCFudgeFactorCalibrationCollector.cc
+++ cdc/modules/cdcCalibrationCollector/src/CDCFudgeFactorCalibrationCollector.cc
@@ -10,7 +10,8 @@
 #include "analysis/utility/PCmsLabTransform.h"
 #include <framework/dataobjects/Helix.h>
 #include <mdst/dataobjects/ECLCluster.h>
-#include <mdst/dbobjects/BeamSpot.h>
+#include <analysis/dataobjects/Particle.h>
+#include <analysis/dataobjects/ParticleList.h>
 #include <Math/ProbFuncMathCore.h>
 
 #include <TH1F.h>
@@ -30,6 +31,8 @@ CDCFudgeFactorCalibrationCollectorModule::CDCFudgeFactorCalibrationCollectorModu
   addParam("StoreNtuple", m_StoreNtuple, "Store ntuple other studies", true);
   addParam("MinColinearityTheta", m_minCollinearityTheta, "cut on colinear of the two track by theta", 10.);
   addParam("MinColinearityPhi0",  m_minCollinearityPhi0, "cut on colinear of the two track by phi0", 10.);
+  addParam("DiMuonListName",  m_DiMuonListName, "name of the di-muon list", std::string("vpho:mumu"));
+  addParam("GammaListName",  m_GammaListName, "name of the gamma list", std::string("gamma:HLT"));
 
 }
 
@@ -40,8 +43,8 @@ CDCFudgeFactorCalibrationCollectorModule::~CDCFudgeFactorCalibrationCollectorMod
 void CDCFudgeFactorCalibrationCollectorModule::prepare()
 {
   m_Tracks.isRequired(m_trackArrayName);
-  //  m_Vpho_list.isRequired(m_PlistName);
   m_TrackFitResults.isRequired(m_trackFitResultArrayName);
+  m_DiMuonList.isRequired(m_DiMuonListName);
 
   if (m_StoreNtuple) {
     auto m_tree  = new TTree(m_treeName.c_str(), "tree for cdc calibration");
@@ -59,6 +62,8 @@ void CDCFudgeFactorCalibrationCollectorModule::prepare()
     m_tree->Branch<Float_t>("Pval_neg", &Pval_neg);
     m_tree->Branch<Float_t>("ndf_pos", &ndf_pos);
     m_tree->Branch<Float_t>("ndf_neg", &ndf_neg);
+    m_tree->Branch<Float_t>("ncdc_pos", &nCDC_pos);
+    m_tree->Branch<Float_t>("ncdc_neg", &nCDC_neg);
     m_tree->Branch<Float_t>("d0_pos", &D0_pos);
     m_tree->Branch<Float_t>("d0_neg", &D0_neg);
     m_tree->Branch<Float_t>("z0_pos", &Z0_pos);
@@ -88,6 +93,7 @@ void CDCFudgeFactorCalibrationCollectorModule::prepare()
   auto m_hdPt_cm = new TH1F("hdPt_cm", "#DeltaP_{t} in c.m frame ; #DeltaP_{t} (c.m) ; Events ", 200, -0.5, 0.5);
   auto m_hdPtPt_cm = new TH2F("hdPtPt_cm", "#DeltaP_{t}:P_{t} in c.m frame ;P_{t} GeV/c  ; #DeltaP_{t} (c.m) ", 50, 2., 7., 200, -0.5,
                               0.5);
+
   auto m_hdPhi0_cm = new TH1F("hdPhi0_cm", "#DeltaPhi_{0}  in c.m frame ; #Delta#Phi_{0} in c.m ; Events ", 200, -0.7, 0.7);
   auto m_hdTheta_cm = new TH1F("hdTheta_cm", "#Delta#theta in c.m frame ; #Delta#theta in c.m ; Events ", 200, -3.0, 3.0);
 
@@ -113,7 +119,9 @@ void CDCFudgeFactorCalibrationCollectorModule::prepare()
 
 void CDCFudgeFactorCalibrationCollectorModule::collect()
 {
-
+  int nCandidates = m_DiMuonList->getListSize();
+  B2DEBUG(199, "Number of muon canndiate:" << nCandidates);
+  if (nCandidates < 1) return;
 
   //  make evt t0 incase we dont use evt t0
   double evtT0 = 0;
@@ -130,151 +138,131 @@ void CDCFudgeFactorCalibrationCollectorModule::collect()
   int exp = m_EventMetaData->getExperiment();
   exp_run = exp * 1000000 + run;
 
-  /*******************************************/
-  /*            VERTEX information           */
-  /*******************************************/
-  static DBObjPtr<BeamSpot> beamSpotDB;
-  const TVector3 IP = beamSpotDB->getIPPosition();
-
   /************************************/
   /// Calculate total energy of Ecl for neutral
-  StoreArray<Belle2::ECLCluster> eclCluster;
-  int nG = eclCluster.getEntries();
-  //  TLorentzVector gamma_cms;
+  StoreObjPtr<ParticleList> gamma_list(m_GammaListName);
   double Eecl_neutral = 0;
+  int nG = gamma_list->getListSize();
+  B2DEBUG(199, "Number of gamma: " << nG);
   for (int i = 0; i < nG; ++i) {
-    ECLCluster* ecl = eclCluster[i];
-    //    double Eecl->get4Vector().E();
-    if (not ecl->isNeutral()) continue;
-    if (not ecl->hasHypothesis(ECLCluster::EHypothesisBit::c_nPhotons)) continue;
-    double Ecluster = ecl->getEnergy(ECLCluster::EHypothesisBit::c_nPhotons);
-    if (Ecluster > 0.1) {
-      Eecl_neutral += Ecluster;
-    }
+    Particle* gamma = gamma_list->getParticle(i);
+    Eecl_neutral += gamma->getEnergy();
   }
-  //  B2INFO("Sum of neutral ECL "<<Eecl_neutral);
+  B2DEBUG(199, "Sum of neutral ECL " << Eecl_neutral);
   /************************************/
 
-  const int nTr = m_Tracks.getEntries();
+  //  const int nTr = m_Tracks.getEntries();
   PCmsLabTransform T;
   //now start to collect dimuon parameters
   double   theta_pos, theta_neg;
-  int nCDC_pos, nCDC_neg;
-  int nCTracks  = 0;
   int charge_sum = 0;
   double  Eecl_trk;
-  for (int i = 0; i < nTr; ++i) {
-    const Belle2::Track* b2track = m_Tracks[i];
-    const Belle2::TrackFitResult* fitresult = b2track->getTrackFitResultWithClosestMass(Const::muon);
-    if (!fitresult) {
-      B2WARNING("No track fit result found.");
-      continue;
+
+
+  for (int i = 0; i < nCandidates; ++i) {
+    Particle* part = m_DiMuonList->getParticle(i);
+    //vertex from vertex fit
+    ROOT::Math::XYZVector v0Vertex = part->getVertex();
+
+    //loop over its daugter
+    for (int j = 0; j < 2; ++j) {
+      //we have two daugters.
+      const Particle* d0 = part->getDaughter(j);
+      short chg =  d0->getCharge();
+
+      const Belle2::TrackFitResult* fitresult  = d0->getTrackFitResult();
+      if (!fitresult) {
+        B2WARNING("No track fit result found.");
+        break;
+      }
+      //get Cluter Energy
+      Eecl_trk +=  d0->getECLClusterEnergy();
+      if (chg > 0) {
+        ndf_pos = fitresult->getNDF();
+        Pval_pos = fitresult->getPValue();
+        Pt_pos = fitresult->getTransverseMomentum();
+        D0_pos  = fitresult->getD0();
+        Z0_pos  = fitresult->getZ0();
+        theta_pos = fitresult->getMomentum().Theta() * 180 / M_PI;
+        nCDC_pos = fitresult->getHitPatternCDC().getNHits();
+        ROOT::Math::PxPyPzEVector P4_pos = T.rotateLabToCms() * fitresult->get4Momentum();
+        Pt_pos_cm = P4_pos.Pt();
+        Theta_pos_cm = P4_pos.Theta() * 180 / M_PI;
+        Phi0_pos_cm = P4_pos.Phi() * 180 / M_PI;
+        UncertainHelix helix_pos = fitresult->getUncertainHelix();
+        helix_pos.passiveMoveBy(v0Vertex);
+        D0ip_pos = helix_pos.getD0();
+        Z0ip_pos = helix_pos.getZ0();
+      } else if (chg < 0) {
+        ndf_neg = fitresult->getNDF();
+        Pval_neg = fitresult->getPValue();
+        Pt_neg = fitresult->getTransverseMomentum();
+        D0_neg  = fitresult->getD0();
+        Z0_neg  = fitresult->getZ0();
+        theta_neg = fitresult->getMomentum().Theta() * 180 / M_PI;
+        nCDC_neg = fitresult->getHitPatternCDC().getNHits();
+        ROOT::Math::PxPyPzEVector P4_neg = T.rotateLabToCms() * fitresult->get4Momentum();
+        Pt_neg_cm = P4_neg.Pt();
+        Theta_neg_cm = P4_neg.Theta() * 180 / M_PI;
+        Phi0_neg_cm = P4_neg.Phi() * 180 / M_PI;
+        UncertainHelix helix_neg = fitresult->getUncertainHelix();
+        helix_neg.passiveMoveBy(v0Vertex);
+        D0ip_neg = helix_neg.getD0();
+        Z0ip_neg = helix_neg.getZ0();
+      } else {
+        continue;
+      }
+      //count #good charged track and sum of charged
+      charge_sum += chg;
     }
-    // cut on D0 and Z0
-    short chg = fitresult->getChargeSign();
-    double D0  = fitresult->getD0();
-    double  Z0 = fitresult->getZ0();
-    if (fabs(D0) > 2 || fabs(Z0) > 4) continue;
+    // cut good charged track
+    if (charge_sum != 0) {continue;}
+    //cut according to the Line400 in the plotMumu_4ff.C
+    if (Pt_pos < 2.5  || Pt_neg < 2.5
+        || theta_pos < 45 || theta_pos > 125
+        || theta_neg < 45 || theta_neg > 125) {return;}
 
-    ECLCluster* ecl = b2track->getRelatedTo<ECLCluster>();
-    if (ecl) {
-      if (not ecl->hasHypothesis(ECLCluster::EHypothesisBit::c_nPhotons)) continue;
-      double Eecl = ecl->getEnergy(ECLCluster::EHypothesisBit::c_nPhotons);
-      //      B2INFO("ECL found with E: "<< Eecl);
-      Eecl_trk += Eecl;
+    // cut on Energy deposite in ECL
+    double Eecl_tot = Eecl_neutral + Eecl_trk;
+    if (Eecl_tot > 2 || Eecl_trk > 2) return;
+
+    //  B2INFO("Total Eecl_track = "<< Eecl_trk);
+    double dPt = (Pt_pos - Pt_neg) / sqrt(2);
+    double dD0 = (D0_pos + D0_neg) / sqrt(2);
+    double dZ0 = (Z0_pos - Z0_neg) / sqrt(2);
+    double Pt_cm = (Pt_pos_cm + Pt_neg_cm) / 2;
+    double dPt_cm = (Pt_pos_cm - Pt_neg_cm) / sqrt(2);
+    double dPhi0_cm = (180 - fabs(Phi0_pos_cm - Phi0_neg_cm)) / sqrt(2);
+    double dTheta_cm = (180 - fabs(Theta_pos_cm + Theta_neg_cm)) / sqrt(2);
+
+    B2DEBUG(199, "ECL neutral - trk:" << Eecl_neutral << "  -  " << Eecl_trk);
+    B2DEBUG(199, "Pos: theta phi0  :" << Theta_pos_cm << "  - " << Phi0_pos_cm);
+    B2DEBUG(199, "Neg: theta phi0  :" << Theta_neg_cm << "  - " << Phi0_neg_cm);
+    B2DEBUG(199, "DeltaPhi  Theta  :" << dPhi0_cm << "  " << dTheta_cm);
+
+    //require back to back
+    if (fabs(dPhi0_cm) > m_minCollinearityPhi0 || fabs(dTheta_cm) > m_minCollinearityTheta) return;
+
+    getObjectPtr<TH1F>("hPval_pos")->Fill(Pval_pos);
+    getObjectPtr<TH1F>("hPval_neg")->Fill(Pval_neg);
+    getObjectPtr<TH1F>("hNDF_pos")->Fill(ndf_pos);
+    getObjectPtr<TH1F>("hNDF_neg")->Fill(ndf_neg);
+    getObjectPtr<TH1F>("hnCDC_pos")->Fill(nCDC_pos);
+    getObjectPtr<TH1F>("hnCDC_neg")->Fill(nCDC_neg);
+
+    getObjectPtr<TH1F>("hdPt")->Fill(dPt);
+    getObjectPtr<TH1F>("hdD0")->Fill(dD0);
+    getObjectPtr<TH1F>("hdZ0")->Fill(dZ0);
+
+    getObjectPtr<TH1F>("hdPt_cm")->Fill(dPt_cm);
+    getObjectPtr<TH2F>("hdPtPt_cm")->Fill(Pt_cm, dPt_cm);
+    getObjectPtr<TH1F>("hdPhi0_cm")->Fill(dPhi0_cm);
+    getObjectPtr<TH1F>("hdTheta_cm")->Fill(dTheta_cm);
+
+    if (m_StoreNtuple) {
+      getObjectPtr<TTree>(m_treeName.c_str())->Fill();
     }
-
-    if (chg > 0) {
-      ndf_pos = fitresult->getNDF();
-      Pval_pos = fitresult->getPValue();
-      Pt_pos = fitresult->getTransverseMomentum();
-      D0_pos  = fitresult->getD0();
-      Z0_pos  = fitresult->getZ0();
-      theta_pos = fitresult->getMomentum().Theta() * 180 / M_PI;
-      nCDC_pos = fitresult->getHitPatternCDC().getNHits();
-      ROOT::Math::PxPyPzEVector P4_pos = T.rotateLabToCms() * fitresult->get4Momentum();
-      Pt_pos_cm = P4_pos.Pt();
-      Theta_pos_cm = P4_pos.Theta() * 180 / M_PI;
-      Phi0_pos_cm = P4_pos.Phi() * 180 / M_PI;
-      Helix helix  = fitresult->getHelix();
-      helix.passiveMoveBy(IP.X(), IP.Y(), IP.Z());
-      D0ip_pos = helix.getD0();
-      Z0ip_pos = helix.getZ0();
-    } else if (chg < 0) {
-      ndf_neg = fitresult->getNDF();
-      Pval_neg = fitresult->getPValue();
-      Pt_neg = fitresult->getTransverseMomentum();
-      D0_neg  = fitresult->getD0();
-      Z0_neg  = fitresult->getZ0();
-      theta_neg = fitresult->getMomentum().Theta() * 180 / M_PI;
-      nCDC_neg = fitresult->getHitPatternCDC().getNHits();
-      ROOT::Math::PxPyPzEVector P4_neg = T.rotateLabToCms() * fitresult->get4Momentum();
-      Pt_neg_cm = P4_neg.Pt();
-      Theta_neg_cm = P4_neg.Theta() * 180 / M_PI;
-      Phi0_neg_cm = P4_neg.Phi() * 180 / M_PI;
-      Helix helix  = fitresult->getHelix();
-      helix.passiveMoveBy(IP.X(), IP.Y(), IP.Z());
-      D0ip_neg = helix.getD0();
-      Z0ip_neg = helix.getZ0();
-
-    } else {
-      continue;
-    }
-    //count #good charged track and sum of charged
-    nCTracks++;
-    charge_sum += chg;
   }
-  // cut good charged track
-  if (nCTracks != 2 || charge_sum != 0) {
-    return ;
-  }
-
-  //cut according to the Line400 in the plotMumu_4ff.C
-  if (Pt_pos < 2.5  || Pt_neg < 2.5
-      || theta_pos < 45 || theta_pos > 125
-      || theta_neg < 45 || theta_neg > 125) {return;}
-
-  // cut on Energy deposite in ECL
-  double Eecl_tot = Eecl_neutral + Eecl_trk;
-  if (Eecl_tot > 2 || Eecl_trk > 2) return;
-
-  //  B2INFO("Total Eecl_track = "<< Eecl_trk);
-  double dPt = (Pt_pos - Pt_neg) / sqrt(2);
-  double dD0 = (D0_pos + D0_neg) / sqrt(2);
-  double dZ0 = (Z0_pos - Z0_neg) / sqrt(2);
-  double Pt_cm = (Pt_pos_cm + Pt_neg_cm) / 2;
-  double dPt_cm = (Pt_pos_cm - Pt_neg_cm) / sqrt(2);
-  double dPhi0_cm = (180 - fabs(Phi0_pos_cm - Phi0_neg_cm)) / sqrt(2);
-  double dTheta_cm = (180 - fabs(Theta_pos_cm + Theta_neg_cm)) / sqrt(2);
-
-  B2DEBUG(199, "ECL neutral - trk:" << Eecl_neutral << "  -  " << Eecl_trk);
-  B2DEBUG(199, "Pos: theta phi0  :" << Theta_pos_cm << "  - " << Phi0_pos_cm);
-  B2DEBUG(199, "Neg: theta phi0  :" << Theta_neg_cm << "  - " << Phi0_neg_cm);
-  B2DEBUG(199, "DeltaPhi  Theta  :" << dPhi0_cm << "  " << dTheta_cm);
-
-  //require back to back
-  if (fabs(dPhi0_cm) > m_minCollinearityPhi0 || fabs(dTheta_cm) > m_minCollinearityTheta) return;
-
-  getObjectPtr<TH1F>("hPval_pos")->Fill(Pval_pos);
-  getObjectPtr<TH1F>("hPval_neg")->Fill(Pval_neg);
-  getObjectPtr<TH1F>("hNDF_pos")->Fill(ndf_pos);
-  getObjectPtr<TH1F>("hNDF_neg")->Fill(ndf_neg);
-  getObjectPtr<TH1F>("hnCDC_pos")->Fill(nCDC_pos);
-  getObjectPtr<TH1F>("hnCDC_neg")->Fill(nCDC_neg);
-
-  getObjectPtr<TH1F>("hdPt")->Fill(dPt);
-  getObjectPtr<TH1F>("hdD0")->Fill(dD0);
-  getObjectPtr<TH1F>("hdZ0")->Fill(dZ0);
-
-  getObjectPtr<TH1F>("hdPt_cm")->Fill(dPt_cm);
-  getObjectPtr<TH2F>("hdPtPt_cm")->Fill(Pt_cm, dPt_cm);
-  getObjectPtr<TH1F>("hdPhi0_cm")->Fill(dPhi0_cm);
-  getObjectPtr<TH1F>("hdTheta_cm")->Fill(dTheta_cm);
-
-  if (m_StoreNtuple) {
-    getObjectPtr<TTree>(m_treeName.c_str())->Fill();
-  }
-
 }
 
 void CDCFudgeFactorCalibrationCollectorModule::finish()

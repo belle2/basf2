1cdec7c5dd4259f7266f85f6b1b102a702ae2160
Suryanarayan Mondal <suryanarayan.mondal@df.unipi.it>
2023-10-21 12:26:37 +0200

    try c-version first

--- validation/scripts/validationscript.py
+++ validation/scripts/validationscript.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # Import XML Parser. Use C-Version, if available
 try:
-    import xml.etree.ElementTree as XMLTree
+    import xml.etree.cElementTree as XMLTree
 except ImportError:
     import xml.etree.ElementTree as XMLTree
 

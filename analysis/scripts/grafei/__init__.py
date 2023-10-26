3671e79bef45207abc1ac702c0ef1d230e51b9ed
Jacopo Cerasoli <jacopo.cerasoli@iphc.cnrs.fr>
2023-10-26 16:49:00 +0200

    AMAZING graFEI intro

--- analysis/scripts/grafei/__init__.py
+++ analysis/scripts/grafei/__init__.py
@@ -7,3 +7,26 @@ __all__ = ["graFEISaverModule", "FlagBDecayModule"]
 
 from grafei.modules.graFEISaverModule import graFEISaverModule
 from grafei.modules.FlagBDecayModule import FlagBDecayModule
+
+print(r"""
+                                                  ____  ____    _      ____  ____  _
+                                                  |  _  |__|   /_\     |___  |___  |
+                                                  |__|  |  \  /   \    |     |___  |
+
+                                o
+                               / \
+                              /   \                             x-----x
+                             /     \            |-----\         |\   /|        |-----\        0 1 3 3 6
+                            o       \           |----- \        | \ / |        |----- \       1 0 3 3 6
+                           / \       \          |----- /        |  x  |        |----- /       3 3 0 1 6
+                          /   \       \         |-----/         | / \ |        |-----/        3 3 1 0 6
+                         /     \       \                        |/   \|                       6 6 6 6 0
+                        o       o       \                       x-----x
+                       / \     / \       \
+                      x   x   x   x       x
+
+      Authors: Jacopo Cerasoli, Giulio Dujany, Lucas Martel, Corentin Santos 2022 - 2023
+      More information in ---TO BE DEFINED---
+      Code based on the work of Kahn et al 10.1088/2632-2153/ac8de0 (https://github.com/Helmholtz-AI-Energy/BaumBauen)
+      Please consider citing both articles.
+      """)

9bfcfb731954b313a298f441a7c2ed6edc4243d8
Frank Meier <frank.meier@duke.edu>
2023-11-14 17:12:53 -0500

    create temporary directory and file names with temp_directory_path

--- framework/utilities/src/FileSystem.cc
+++ framework/utilities/src/FileSystem.cc
@@ -219,17 +219,18 @@ bool FileSystem::Lock::lock(int timeout, bool ignoreErrors)
 
 FileSystem::TemporaryFile::TemporaryFile(std::ios_base::openmode mode): std::fstream()
 {
-  char temporaryFileName[] = "/tmp/basf2_XXXXXX";
+  char* temporaryFileName = strdup((std::filesystem::temp_directory_path() / "basf2_XXXXXX").c_str());
   int fileDescriptor = mkstemp(temporaryFileName);
   if (fileDescriptor == -1) {
     B2ERROR("Cannot create temporary file: " << strerror(errno));
     return;
   }
-  m_filename = temporaryFileName;
+  m_filename = std::string(temporaryFileName);
   open(temporaryFileName, mode);
   if (!is_open()) {
     B2ERROR("Cannot open temporary file: " << strerror(errno));
   }
+  free(temporaryFileName);
   ::close(fileDescriptor);
 }
 

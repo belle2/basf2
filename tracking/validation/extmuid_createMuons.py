7360b503ba4e28e963275700e943bad4d325b538
Christian Wessel <christian.wessel@desy.de>
2023-12-06 20:36:43 +0100

    Add if __name__ == '__main__' check to several validation scripts and put the actual validation code into a function that is called when executing the scripts.

--- tracking/validation/extmuid_createMuons.py
+++ tracking/validation/extmuid_createMuons.py
@@ -30,51 +30,60 @@ import os
 from simulation import add_simulation
 from reconstruction import add_reconstruction
 
-b2.set_random_seed(123460)
 
-output_filename = '../muon-ExtMuidValidation.root'
+def run():
+    """
+    Create muon sample for the ExtMuid validation.
+    """
+    b2.set_random_seed(123460)
 
-print(output_filename)
+    output_filename = '../muon-ExtMuidValidation.root'
 
-path = b2.create_path()
+    print(output_filename)
 
-eventinfosetter = b2.register_module('EventInfoSetter')
-eventinfosetter.param('evtNumList', [1000])
-path.add_module(eventinfosetter)
+    path = b2.create_path()
 
-pgun = b2.register_module('ParticleGun')
-param_pgun = {
-    'pdgCodes': [-13, 13],
-    'nTracks': 1,
-    'varyNTracks': 0,
-    'momentumGeneration': 'uniform',
-    'momentumParams': [0.5, 5.0],
-    'thetaGeneration': 'uniformCos',
-    'thetaParams': [15., 150.],
-    'phiGeneration': 'uniform',
-    'phiParams': [0.0, 360.0],
-    'vertexGeneration': 'fixed',
-    'xVertexParams': [0.0],
-    'yVertexParams': [0.0],
-    'zVertexParams': [0.0],
-}
-pgun.param(param_pgun)
-path.add_module(pgun)
+    eventinfosetter = b2.register_module('EventInfoSetter')
+    eventinfosetter.param('evtNumList', [1000])
+    path.add_module(eventinfosetter)
 
-# add simulation and reconstruction modules to the path
-if 'BELLE2_BACKGROUND_DIR' in os.environ:
-    background_files = glob.glob(os.environ['BELLE2_BACKGROUND_DIR'] + '/*.root')
-    add_simulation(path, bkgfiles=background_files)
-else:
-    b2.B2FATAL('BELLE2_BACKGROUND_DIR is not set.')
+    pgun = b2.register_module('ParticleGun')
+    param_pgun = {
+        'pdgCodes': [-13, 13],
+        'nTracks': 1,
+        'varyNTracks': 0,
+        'momentumGeneration': 'uniform',
+        'momentumParams': [0.5, 5.0],
+        'thetaGeneration': 'uniformCos',
+        'thetaParams': [15., 150.],
+        'phiGeneration': 'uniform',
+        'phiParams': [0.0, 360.0],
+        'vertexGeneration': 'fixed',
+        'xVertexParams': [0.0],
+        'yVertexParams': [0.0],
+        'zVertexParams': [0.0],
+    }
+    pgun.param(param_pgun)
+    path.add_module(pgun)
 
-add_reconstruction(path)
+    # add simulation and reconstruction modules to the path
+    if 'BELLE2_BACKGROUND_DIR' in os.environ:
+        background_files = glob.glob(os.environ['BELLE2_BACKGROUND_DIR'] + '/*.root')
+        add_simulation(path, bkgfiles=background_files)
+    else:
+        b2.B2FATAL('BELLE2_BACKGROUND_DIR is not set.')
 
-output = b2.register_module('RootOutput')
-output.param('outputFileName', output_filename)
-output.param('branchNames', ['MCParticles', 'ExtHits', 'KLMMuidLikelihoods', 'KLMHit2ds'])
-path.add_module(output)
-path.add_module('Progress')
+    add_reconstruction(path)
 
-b2.process(path)
-print(b2.statistics)
+    output = b2.register_module('RootOutput')
+    output.param('outputFileName', output_filename)
+    output.param('branchNames', ['MCParticles', 'ExtHits', 'KLMMuidLikelihoods', 'KLMHit2ds'])
+    path.add_module(output)
+    path.add_module('Progress')
+
+    b2.process(path)
+    print(b2.statistics)
+
+
+if __name__ == '__main__':
+    run()
--- tracking/validation/extmuid_createPions.py
+++ tracking/validation/extmuid_createPions.py
@@ -30,52 +30,61 @@ import os
 from simulation import add_simulation
 from reconstruction import add_reconstruction
 
-b2.set_random_seed(654321)
 
-output_filename = '../pion-ExtMuidValidation.root'
+def run():
+    """
+    Create pion sample for the ExtMuid validation.
+    """
 
-print(output_filename)
+    b2.set_random_seed(654321)
 
-path = b2.create_path()
+    output_filename = '../pion-ExtMuidValidation.root'
 
-eventinfosetter = b2.register_module('EventInfoSetter')
-eventinfosetter.param('evtNumList', [1000])
-path.add_module(eventinfosetter)
+    print(output_filename)
+
+    path = b2.create_path()
+
+    eventinfosetter = b2.register_module('EventInfoSetter')
+    eventinfosetter.param('evtNumList', [1000])
+    path.add_module(eventinfosetter)
+
+    pgun = b2.register_module('ParticleGun')
+    param_pgun = {
+        'pdgCodes': [-211, 211],
+        'nTracks': 1,
+        'varyNTracks': 0,
+        'momentumGeneration': 'uniform',
+        'momentumParams': [0.5, 5.0],
+        'thetaGeneration': 'uniformCos',
+        'thetaParams': [15., 150.],
+        'phiGeneration': 'uniform',
+        'phiParams': [0.0, 360.0],
+        'vertexGeneration': 'fixed',
+        'xVertexParams': [0.0],
+        'yVertexParams': [0.0],
+        'zVertexParams': [0.0],
+    }
+    pgun.param(param_pgun)
+    path.add_module(pgun)
+
+    # add simulation and reconstruction modules to the path
+    if 'BELLE2_BACKGROUND_DIR' in os.environ:
+        background_files = glob.glob(os.environ['BELLE2_BACKGROUND_DIR'] + '/*.root')
+        add_simulation(path, bkgfiles=background_files)
+    else:
+        b2.B2FATAL('BELLE2_BACKGROUND_DIR is not set.')
+
+    add_reconstruction(path)
+
+    output = b2.register_module('RootOutput')
+    output.param('outputFileName', output_filename)
+    output.param('branchNames', ['MCParticles', 'ExtHits', 'KLMMuidLikelihoods', 'KLMHit2ds'])
+    path.add_module(output)
+    path.add_module('Progress')
+
+    b2.process(path)
+    print(b2.statistics)
 
 
-pgun = b2.register_module('ParticleGun')
-param_pgun = {
-    'pdgCodes': [-211, 211],
-    'nTracks': 1,
-    'varyNTracks': 0,
-    'momentumGeneration': 'uniform',
-    'momentumParams': [0.5, 5.0],
-    'thetaGeneration': 'uniformCos',
-    'thetaParams': [15., 150.],
-    'phiGeneration': 'uniform',
-    'phiParams': [0.0, 360.0],
-    'vertexGeneration': 'fixed',
-    'xVertexParams': [0.0],
-    'yVertexParams': [0.0],
-    'zVertexParams': [0.0],
-}
-pgun.param(param_pgun)
-path.add_module(pgun)
-
-# add simulation and reconstruction modules to the path
-if 'BELLE2_BACKGROUND_DIR' in os.environ:
-    background_files = glob.glob(os.environ['BELLE2_BACKGROUND_DIR'] + '/*.root')
-    add_simulation(path, bkgfiles=background_files)
-else:
-    b2.B2FATAL('BELLE2_BACKGROUND_DIR is not set.')
-
-add_reconstruction(path)
-
-output = b2.register_module('RootOutput')
-output.param('outputFileName', output_filename)
-output.param('branchNames', ['MCParticles', 'ExtHits', 'KLMMuidLikelihoods', 'KLMHit2ds'])
-path.add_module(output)
-path.add_module('Progress')
-
-b2.process(path)
-print(b2.statistics)
+if __name__ == '__main__':
+    run()

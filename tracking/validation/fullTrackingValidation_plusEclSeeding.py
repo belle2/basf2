b3c32f1d005f883414016d8e97ccaac9ab693cef
Christian Wessel <christian.wessel@desy.de>
2023-12-07 13:11:23 +0100

    Be more verbose and add a message that a printed in case a script is skipped.

--- tracking/validation/fullTrackingValidation_plusEclSeeding.py
+++ tracking/validation/fullTrackingValidation_plusEclSeeding.py
@@ -85,3 +85,7 @@ if __name__ == '__main__':
 
     if ACTIVE:
         main()
+    else:
+        print("This validation deactivated and thus basf2 is not executed. \n\
+               If you want to run this validation, please set the 'ACTIVE' flag above to 'True'. \n\
+               Exiting.")

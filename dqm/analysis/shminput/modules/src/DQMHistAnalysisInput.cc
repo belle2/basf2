69e77509eb921af9fbd05084194bed7ffb224a07
SANTELJ Luka <luka.santelj@ijs.si>
2024-01-18 22:38:21 +0900

    minor changes to the dqm analysis input module

--- dqm/analysis/shminput/modules/src/DQMHistAnalysisInput.cc
+++ dqm/analysis/shminput/modules/src/DQMHistAnalysisInput.cc
@@ -119,6 +119,13 @@ void DQMHistAnalysisInputModule::event()
   strftime(mbstr, sizeof(mbstr), "%c", localtime(&now));
   B2INFO("[" << mbstr << "] before input loop");
 
+  // check if histograms were updated since last DQM event (based on number of processed events)
+  if (m_nevent != getEventProcessed()) {
+    m_lastChange = std::string(mbstr);
+  }
+  // update number of processed events
+  m_nevent = getEventProcessed();
+
   while ((key = (TKey*)next())) {
     auto obj = key->ReadObj();
     if (obj == nullptr) continue; // would be strange, but better check
@@ -148,11 +155,12 @@ void DQMHistAnalysisInputModule::event()
   B2INFO("[" << mbstr << "] after input loop");
 
   if (expno == std::string("UNKNOWN") || runno == std::string("UNKNOWN")) {
-    m_expno = 22;
+    m_expno = 122;
     m_runno = 1;
     if (m_c_info != NULL) m_c_info->SetTitle((m_memname + ": Last Updated " + mmt.AsString()).c_str());
   } else {
-    if (m_c_info != NULL) m_c_info->SetTitle((m_memname + ": Exp " + expno + ", Run " + runno + ", RunType " + rtype + ", Last Updated "
+    if (m_c_info != NULL) m_c_info->SetTitle((m_memname + ": Exp " + expno + ", Run " + runno + ", RunType " + rtype + ", Last Changed "
+                                                + m_lastChange + ", Last Updated "
                                                 + mmt.AsString()).c_str());
     m_expno = std::stoi(expno);
     m_runno = std::stoi(runno);

7f44e66b094c6b3942175b8ce4d886a8153ca597
Belle II Software <b2soft@mail.desy.de>
2024-01-19 10:31:21 +0100

    Merge branch 'feature/10209-pipiISR_skim' into 'main'
    Resolve "add/updates in the DS&LM WG skim"
    
    See merge request belle2/software/basf2!2615

--- skim/scripts/skim/registry.py
+++ skim/scripts/skim/registry.py
@@ -156,6 +156,7 @@ _RegisteredSkims = [
     ("18370100", "dark", "AA2uuuu"),
     ("18020600", "dark", "DimuonPlusVisibleDarkHiggs"),
     ("18020700", "dark", "DielectronPlusVisibleDarkHiggs"),
+    ("18530600", "lowMulti", "LowMassOneTrack"),
 
     # --- WG9: Charmless B decays ---
     ("19120100", "btocharmless", "BtoPi0Pi0"),

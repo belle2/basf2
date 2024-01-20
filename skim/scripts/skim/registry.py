93d6b49fdb0a0e3dbba35060357adf5de8ff9edf
Belle II Software <b2soft@mail.desy.de>
2024-01-20 22:01:48 +0100

    Merge branch 'feature/charmed-baryon-skims' into 'main'
    New charmed baryon skims
    
    See merge request belle2/software/basf2!2695

--- skim/scripts/skim/registry.py
+++ skim/scripts/skim/registry.py
@@ -110,8 +110,14 @@ _RegisteredSkims = [
     ("17230300", "charm", "DstToD0Pi_D0ToRare"),  # D0 -> g g/e e/mu mu
     ("17230400", "charm", "XToDp_DpToKsHp"),  # D+ -> Ks h+
     ("17230500", "charm", "XToDp_DpToHpHmJp"),  # D+ -> h+ h- j+
-    ("17230600", "charm", "LambdacTopHpJm"),  # Lambda_c+ -> proton h- j+
     ("17230700", "charm", "DstToD0Pi_D0ToGeneric"),  # D* -> D0 -> generic decays
+    # Charmed baryons
+    ("17230800", "charm", "LambdacTopHpJm"),  # Lambda_c+ -> proton h+ j-
+    ("17230900", "charm", "LambdacToSHpJm"),  # Lambda_c+ -> Sigma+ h+ j-
+    ("17231000", "charm", "XicpTopHpJm"),  # Xi_c+ -> proton h+ j-
+    ("17231100", "charm", "XictoXimpippim"),  # Xi_c+ -> Sigma+/Xi h+ j- (k+)
+    ("17231200", "charm", "Xic0ToLHpJm"),  # Xi_c0 -> Lambda0 h+ j-
+    # D* -> D0
     ("17240100", "charm", "DstToD0Pi_D0ToHpJm"),  # D* -> D0 -> K pi/pi pi/K K
     # D* -> D0 -> K/pi- pi/pi+ pi0 (RS+WS)
     ("17240200", "charm", "DstToD0Pi_D0ToHpJmPi0"),  # D* -> D0 -> h h pi0

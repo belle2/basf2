89d5a67fba391e46bb3d13a013cb5a7d1e607a37
Björn Spruck <bspruck@uni-mainz.de>
2024-01-25 13:38:15 +0900

    changed hardcoded numbers/colors against enums, update only if updated

--- dqm/analysis/modules/src/DQMHistAnalysisRunNr.cc
+++ dqm/analysis/modules/src/DQMHistAnalysisRunNr.cc
@@ -65,12 +65,17 @@ void DQMHistAnalysisRunNrModule::beginRun()
   B2DEBUG(99, "DQMHistAnalysisRunNr: beginRun called.");
 
   m_cRunNr->Clear();
+  m_cRunNr->Pad()->SetFillColor(c_ColorTooFew);// Magenta or Gray
+  auto leg = new TPaveText(0.6, 0.6, 0.95, 0.95, "NDC");
+  leg->SetFillColor(kWhite);
+  leg->AddText("No data yet");
+  leg->Draw();
 
   // make sure we reset at run start to retrigger the alarm
   double mean = 0.0; // must be double, mean of histogram -> runnr
   int status = 0; // must be int, epics alarm status 0 = no data, 2 = o.k., 4 = not o.k.
   // if status & runnr valid
-  setEpicsPV("Alarm", status);
+  setEpicsPV("Alarm", status); // reset alarm status at run start
   setEpicsPV("RunNr", mean);
 }
 
@@ -78,17 +83,16 @@ void DQMHistAnalysisRunNrModule::event()
 {
   if (!m_cRunNr) return;
   double mean = 0.0; // must be double, mean of histogram -> runnr
-  int status = 0; // must be int, epics alarm status 0 = no data, 2 = o.k., 4 = not o.k.
-
-  m_cRunNr->cd();
-  m_cRunNr->Clear();
+  int status = c_StatusTooFew; // must be int, epics alarm status 0 = no data, 2 = o.k., 4 = not o.k.
 
   auto leg = new TPaveText(0.6, 0.6, 0.95, 0.95, "NDC");
   leg->SetFillColor(kWhite);
 
   auto name = "hRunnr";
-  auto hh1 = findHist(m_histogramDirectoryName, name);
+  auto hh1 = findHist(m_histogramDirectoryName, name, true); // only if updated
   if (hh1) {
+    m_cRunNr->Clear();
+    m_cRunNr->cd();
     hh1->SetStats(kFALSE); // get rid of annoying box, we have our own
     hh1->Draw("hist");
     mean = hh1->GetMean();
@@ -108,34 +112,34 @@ void DQMHistAnalysisRunNrModule::event()
       // Check number of bins filled
       if (nfilled > 1) {
         // problem
-        status = 4;
+        status = c_StatusError;
       } else if (nfilled == 1) {
         // ok
-        status = 2;
+        status = c_StatusGood;
       }// else nfilled=0, status stays 0 (no data)
     }
+
+    if (status == c_StatusTooFew) {
+      // no data (empty) or no histogram
+      m_cRunNr->Pad()->SetFillColor(c_ColorTooFew);// Magenta or Gray
+      leg->AddText("No data yet");
+    } else if (status == c_StatusGood) {
+      // only one run
+      m_cRunNr->Pad()->SetFillColor(c_ColorGood);// Green
+    } else { /*if ( status==4 )*/
+      // anything else is bad
+      m_cRunNr->Pad()->SetFillColor(c_ColorError);// Red
+    }
+
+    leg->Draw();
+
+    m_cRunNr->Modified();
+    m_cRunNr->Update();
+    UpdateCanvas(m_cRunNr);
+
+    setEpicsPV("Alarm", status);
+    setEpicsPV("RunNr", mean);
   }
-
-  if (status == 0) {
-    // no data (empty) or no histogram
-    m_cRunNr->Pad()->SetFillColor(kGray);// Magenta or Gray
-    leg->AddText("No data yet");
-  } else if (status == 2) {
-    // only one run
-    m_cRunNr->Pad()->SetFillColor(kGreen);// Green
-  } else { /*if ( status==4 )*/
-    // anything else is bad
-    m_cRunNr->Pad()->SetFillColor(kRed);// Red
-  }
-
-  leg->Draw();
-
-  m_cRunNr->Modified();
-  m_cRunNr->Update();
-  UpdateCanvas(m_cRunNr);
-
-  setEpicsPV("Alarm", status);
-  setEpicsPV("RunNr", mean);
 }
 
 void DQMHistAnalysisRunNrModule::terminate()

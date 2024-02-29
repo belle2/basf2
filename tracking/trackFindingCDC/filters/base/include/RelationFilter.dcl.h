b1e73a7958c5a67d3f8622dddf4538d57d34f352
Sasha Glazov <alexander.glazov@belle2.org>
2024-02-29 13:39:14 +0100

    implement filter for a vector of relations

--- tracking/trackFindingCDC/filters/base/include/RelationFilter.dcl.h
+++ tracking/trackFindingCDC/filters/base/include/RelationFilter.dcl.h
@@ -15,6 +15,8 @@
 
 #include <vector>
 
+#include <cmath>
+
 namespace Belle2 {
   namespace TrackFindingCDC {
 
@@ -39,9 +41,20 @@ namespace Belle2 {
        */
       virtual Weight operator()(const AObject& from, const AObject& to);
 
+      /**
+       * Filter over a vector of relations. We need to override it since derrived relation classes do not implement a call with an pointer to a relation.
+       */
       virtual std::vector<float> operator()(const std::vector <Relation<AObject>*>& objs) override
       {
-        return std::vector<float>(objs.size());
+        std::vector<float> out(objs.size());
+        for (size_t iObj = 0; iObj < objs.size(); iObj += 1) {
+          if (objs[iObj]) {
+            out[iObj] = operator()(*objs[iObj]);
+          } else {
+            out[iObj] = NAN;
+          }
+        }
+        return out;
       }
 
       /**

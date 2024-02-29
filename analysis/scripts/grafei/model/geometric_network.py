ccead392f723863d9f0d2b15462f01a00638d249
Jacopo Cerasoli <jacopo.cerasoli@iphc.cnrs.fr>
2024-02-29 23:23:06 +0900

    Added copyright

--- analysis/scripts/grafei/model/geometric_network.py
+++ analysis/scripts/grafei/model/geometric_network.py
@@ -1,3 +1,11 @@
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 import torch
 from torch_geometric.nn import MetaLayer
 from .geometric_layers import NodeLayer, EdgeLayer, GlobalLayer

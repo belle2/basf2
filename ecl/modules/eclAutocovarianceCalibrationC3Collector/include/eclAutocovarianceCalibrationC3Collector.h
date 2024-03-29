e5cbc7e3a3c18722a91012088fe043636940833c
LONGO Savino <savino.longo@umanitoba.ca>
2024-03-29 11:35:24 +0900

    removed unused member

--- ecl/modules/eclAutocovarianceCalibrationC3Collector/include/eclAutocovarianceCalibrationC3Collector.h
+++ ecl/modules/eclAutocovarianceCalibrationC3Collector/include/eclAutocovarianceCalibrationC3Collector.h
@@ -51,7 +51,6 @@ namespace Belle2 {
     StoreObjPtr<EventMetaData> m_evtMetaData; /**< dataStore EventMetaData */
     DBObjPtr<ECLCrystalCalib> m_ECLAutocovarianceCalibrationC1Threshold;  /**< thresholds obtained from C1 stage */
     std::vector<float> m_PeakToPeakThresholds; /**< vector of thresholds obtained from DB object */
-    DBObjPtr<ECLCrystalCalib> m_ECLAutocovarianceCalibrationC2Baseline; /**< baselines obtained from C2 stage */
     std::vector<float> m_Baselines; /**< vector of thresholds obtained from DB object */
     TH2F* CovarianceMatrixInfoVsCrysID;  /**< result returned by collector that contains the coveriance matrix for each crystal  */
     static constexpr int m_nADCWaveformPoints = 31;  /**< length of ECLDSP waveforms  */

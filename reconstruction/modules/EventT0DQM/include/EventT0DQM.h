bfda76f4888add0ed889b4e26d868a27c41aed45
Christian Wessel <christian.wessel@desy.de>
2024-05-05 12:27:20 +0200

    Change how chi2 is displayed.

--- reconstruction/modules/EventT0DQM/include/EventT0DQM.h
+++ reconstruction/modules/EventT0DQM/include/EventT0DQM.h
@@ -106,7 +106,7 @@ namespace Belle2 {
     TH1F* m_histEventT0_SVD_mumu_L1_TOPTRG{nullptr};   /**< event t0 histogram for SVD, HLT mu mu events, L1 time by TOP trigger */
 
     /// EventT0 algorithms for which to calculate fractions of abundance
-    const char* c_eventT0Algorithms[6] = {"Any", "ECL", "SVD", "CDC hit based", "CDC full grid chi2", "TOP"};
+    const char* c_eventT0Algorithms[6] = {"Any", "ECL", "SVD", "CDC hit based", "CDC full grid #chi^{2}", "TOP"};
     /// Fraction of events with EventT0 from a given algorithm, HLT hadronic events, L1 time by ECL trigger
     TH1D* m_histAlgorithmSourceFractionsHadronL1ECLTRG{nullptr};
     /// Fraction of events with EventT0 from a given algorithm, HLT hadronic events, L1 time by CDC trigger

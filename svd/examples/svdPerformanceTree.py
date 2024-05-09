de03501eb6383e4731251fc8bfd19128a59bcc3a
CORONA Luigi <luigi.corona@pi.infn.it>
2024-05-09 18:10:26 +0900

    adding a line deleted by mistake

--- svd/examples/svdPerformanceTree.py
+++ svd/examples/svdPerformanceTree.py
@@ -81,6 +81,7 @@ else:
 # now do reconstruction:
 trk.add_tracking_reconstruction(
     main,
+    mcTrackFinding=MCTracking,
     trackFitHypotheses=[211],
     append_full_grid_cdc_eventt0=True,
     skip_full_grid_cdc_eventt0_if_svd_time_present=False)

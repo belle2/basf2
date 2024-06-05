7fdd999ff3c164e076382e3cc39e1ad17ea922d8
Christian Finck <christian.finck@iphc.cnrs.fr>
2024-06-05 11:42:21 +0200

    Increase layer1-2 X0 by a factor 2 (0.2%)

--- vtx/examples/GenerateXmlFiles6Layers.py
+++ vtx/examples/GenerateXmlFiles6Layers.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 ##########################################################################
@@ -44,7 +44,7 @@ Xi = 0.001  # 0.002
 sensorL = 30.168
 sensorW = 18.812
 sensorH = 0.400  # str(X0Si*X*1e3)
-sensorHi = 0.095  # str(X0Si*Xi*1e3)
+sensorHi = 0.095*2  # str(X0Si*Xi*1e3)
 offsetU = 3.5
 offsetV = 0.3
 

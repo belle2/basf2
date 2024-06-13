b34732f4aa448176fc97adf69f603f67655a778b
Björn Spruck <bspruck@uni-mainz.de>
2024-06-13 15:12:43 +0900

    add status export to tmp file

--- dqm/analysis/modules/src/DQMHistAnalysisInput2.cc
+++ dqm/analysis/modules/src/DQMHistAnalysisInput2.cc
@@ -35,6 +35,7 @@ DQMHistAnalysisInput2Module::DQMHistAnalysisInput2Module()
 {
   //Parameter definition
   addParam("HistMemoryPath", m_mempath, "Path to Input Hist memory", std::string(""));
+  addParam("AnalysisName", m_name, "Name for status export", std::string(""));
   addParam("RefreshInterval", m_interval, "Refresh interval of histograms", 10);
   addParam("RemoveEmpty", m_remove_empty, "Remove empty histograms", false);
   addParam("EnableRunInfo", m_enable_run_info, "Enable Run Info", false);
@@ -58,10 +59,14 @@ void DQMHistAnalysisInput2Module::beginRun()
   clearHistList();
   resetDeltaList();
   clearCanvases();
+
+  m_last_beginrun = time(0);
 }
 
 void DQMHistAnalysisInput2Module::event()
 {
+  m_last_event = time(0);
+
   TH1::AddDirectory(false);
   initHistListBeforeEvent();
 
@@ -83,6 +88,7 @@ void DQMHistAnalysisInput2Module::event()
     return;
   }
   lasttime = ftime;
+  m_last_file_update = time(0);
 
   char mbstr[100];
   time_t now = time(0);
@@ -209,8 +215,12 @@ void DQMHistAnalysisInput2Module::event()
     B2DEBUG(1, "Found : " << h->GetName() << " : " << h->GetEntries());
   }
 
-  // if there is no update, sleep a moment
-  if (!anyupdate) sleep(m_interval);
+  if (!anyupdate) {
+    m_last_content_update = time(0);
+  } else {
+    // if there is no update, sleep a moment
+    sleep(m_interval);
+  }
 
   // if no histogram was updated, we could stop processing
   setReturnValue(anyupdate);
@@ -227,3 +237,25 @@ void DQMHistAnalysisInput2Module::terminate()
   if (m_c_info) delete m_c_info;
 }
 
+void DQMHistAnalysisInput2Module::write_state(void)
+{
+  if (m_name == "") return;
+  char mbstr[100];
+  std::string name = "/tmp/dqm_analysis_state_" + m_name;
+  FILE* fh = fopen(name.c_str(), "wt+");
+  if (fh) {
+    time_t now = time(0);
+    strftime(mbstr, sizeof(mbstr), "%F %T", localtime(&now));
+    fprintf(fh, "%s,%s,", m_name.c_str(), mbstr);
+    strftime(mbstr, sizeof(mbstr), "%F %T", localtime(&m_last_event));
+    fprintf(fh, "%s,", mbstr);
+    strftime(mbstr, sizeof(mbstr), "%F %T", localtime(&m_last_beginrun));
+    fprintf(fh, "%s,", mbstr);
+    strftime(mbstr, sizeof(mbstr), "%F %T", localtime(&m_last_file_update));
+    fprintf(fh, "%s,", mbstr);
+    strftime(mbstr, sizeof(mbstr), "%F %T", localtime(&m_last_content_update));
+    fprintf(fh, "%s\n", mbstr);
+
+    fclose(fh);
+  }
+}

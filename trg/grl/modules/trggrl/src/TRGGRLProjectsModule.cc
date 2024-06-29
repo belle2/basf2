2ce771dce6de3143adbb3f740b05298c6435c000
yinjh2012@korea.ac.kr <yinjh@nankai.edu.cn>
2024-06-30 08:20:15 +0900

    fix overflow

--- trg/grl/modules/trggrl/src/TRGGRLProjectsModule.cc
+++ trg/grl/modules/trggrl/src/TRGGRLProjectsModule.cc
@@ -652,7 +652,7 @@ void TRGGRLProjectsModule::event()
   bool ehigh1 = (ECLtoGDL[2] & (1 << (90 - 32 * 2))) != 0;
   bool ehigh2 = (ECLtoGDL[2] & (1 << (91 - 32 * 2))) != 0;
   bool ehigh3 = (ECLtoGDL[2] & (1 << (92 - 32 * 2))) != 0;
-  bool ehigh4 = (ECLtoGDL[2] & (1 << (95 - 32 * 2))) != 0;
+  bool ehigh4 = (ECLtoGDL[2] & (1u << (95 - 32 * 2))) != 0;
 
   //---------------------------------------------------------------------
   //..Other input bits

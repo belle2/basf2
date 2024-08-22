cd4ac216eca0f6904abb7c3f24d88dfe556db4d8
Jonas Eppelt <jonas.eppelt@kit.edu>
2024-08-22 10:38:33 +0200

    added checksums for CSV and parquet

--- framework/core/src/MetadataService.cc
+++ framework/core/src/MetadataService.cc
@@ -86,6 +86,38 @@ void MetadataService::addHDF5File(const std::string& fileName)
   writeJson();
 }
 
+
+void MetadataService::addParquetFile(const std::string& fileName)
+{
+  if (m_fileName.empty()) return;
+  if (!FileSystem::isFile(fileName)) return;
+
+  nlohmann::json file_json = {{"type", "parquet"}, {"filename", fileName}};
+
+  file_json["checksums"]["adler32"] = FileSystem::calculateAdler32(fileName);
+
+  m_json["output_files"].push_back(file_json);
+
+  writeJson();
+}
+
+void MetadataService::addCSVFile(const std::string& fileName)
+{
+  if (m_fileName.empty()) return;
+  if (!FileSystem::isFile(fileName)) return;
+
+  nlohmann::json file_json = {{"type", "csv"}, {"filename", fileName}};
+
+  file_json["checksums"]["adler32"] = FileSystem::calculateAdler32(fileName);
+
+  m_json["output_files"].push_back(file_json);
+
+  writeJson();
+}
+
+
+
+
 void MetadataService::addBasf2Status(const std::string& message)
 {
   if (m_fileName.empty()) return;

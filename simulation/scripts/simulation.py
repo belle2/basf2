27fbcf69abc877513c5e14390ff11d18d0dcce81
Giacomo De Pietro <giacomo.pietro@kit.edu>
2024-09-19 14:33:55 +0200

    Run the new module in add_simulation

--- simulation/scripts/simulation.py
+++ simulation/scripts/simulation.py
@@ -234,6 +234,7 @@ def add_simulation(
 
     if components is None or 'TRG' in components:
         add_trigger_simulation(path, simulateT0jitter=simulateT0jitter, FilterEvents=FilterEvents)
+        path.add_module('TRGGDLInjectionVetoFromOverlay')
 
     # SVD digitization, BG Overlay, sorting and zero suppression
     if components is None or 'SVD' in components:

6317c89f5b5a665f1abface60aa5bb7912a20b39
Christian Wessel <christian.wessel@desy.de>
2024-10-04 18:43:43 +0200

    Remove TVector3 fromm TestHelpers.

--- framework/utilities/src/TestHelpers.cc
+++ framework/utilities/src/TestHelpers.cc
@@ -104,11 +104,11 @@ bool Belle2::TestHelpers::allNear<ROOT::Math::XYZVector>(const ROOT::Math::XYZVe
   return xNear and yNear and zNear;
 }
 
-void Belle2::TestHelpers::PrintTo(const TVector3& tVector3, ::std::ostream& output)
+void Belle2::TestHelpers::PrintTo(const ROOT::Math::XYZVector& vector3, ::std::ostream& output)
 {
   output
-      << "TVector3("
-      << tVector3.X() << ", "
-      << tVector3.Y() << ", "
-      << tVector3.Z() << ")";
+      << "ROOT::Math::XYZVector("
+      << vector3.X() << ", "
+      << vector3.Y() << ", "
+      << vector3.Z() << ")";
 }

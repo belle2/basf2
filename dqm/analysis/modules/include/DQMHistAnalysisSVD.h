1c7d295cd654b5aca98a222f0750ac68650437af
FINCK Christian <christian.finck@iphc.cnrs.fr>
2024-10-25 18:08:10 +0900

    Change comment

--- dqm/analysis/modules/include/DQMHistAnalysisSVD.h
+++ dqm/analysis/modules/include/DQMHistAnalysisSVD.h
@@ -6,7 +6,7 @@
  * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
  **************************************************************************/
 //+
-// File : DQMHistAnalysisSVDClustersOnTrack.h
+// File : DQMHistAnalysisSVDModule.h
 // Description : base module for DQM histogram analysis of SVD
 //-
 

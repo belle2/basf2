d82f7769321e6646fce648e33a40d537002fcfc6
LONGO Savino <savino.longo@umanitoba.ca>
2024-11-06 01:02:30 +0900

    fix in file header

--- analysis/modules/ParticleKinematicFitter/include/ParticleKinematicFitterModule.h
+++ analysis/modules/ParticleKinematicFitter/include/ParticleKinematicFitterModule.h
@@ -217,12 +217,12 @@ namespace Belle2 {
 
       /**
        * store fit object information as ExtraInfo
-       * @param prefix can be used to distinguish e.g. "Fitted" and "Measured"
+       * @param fitSuffix can be used to distinguish e.g. "Fitted" and "Measured"
        * @param fitter reference to OrcaKinFit fitter object
        * @param particleChildren list of all particle children
        * @param mother mother particle
        */
-      bool storeOrcaKinFitParticles(const std::string& prefix, BaseFitter& fitter, std::vector<Particle*>& particleChildren,
+      bool storeOrcaKinFitParticles(const std::string& fitSuffix, BaseFitter& fitter, std::vector<Particle*>& particleChildren,
                                     Particle* mother);
 
 

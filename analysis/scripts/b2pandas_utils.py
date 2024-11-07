3a7a97ef8275f869e229640f3636a87f34283e72
Jonas Eppelt <jonas.eppelt@kit.edu>
2024-11-07 15:04:31 +0100

    passing on the hdf5 table name

--- analysis/scripts/b2pandas_utils.py
+++ analysis/scripts/b2pandas_utils.py
@@ -257,12 +257,12 @@ class VariablesToHDF5(VariablesToTable):
     Legacy class to not break existing code
     """
 
-    def __init__(self, listname, variables, filename):
+    def __init__(self, listname, variables, filename, hdf_table_name: Optional[str] = None,):
         assert self._filename.split(".")[-1] in ["h5", "hdf", "hdf5"], (
             "Filename must end with .h5, .hdf or .hdf5 for HDF5 output. "
             f"Got {self._filename}"
         )
-        super().__init__(listname, variables, filename)
+        super().__init__(listname, variables, filename, hdf_table_name)
 
 
 def make_mcerrors_readable(dataframe, column="mcErrors"):

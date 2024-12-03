5c984fc147cfc5edbab0ed576b461aaf86efaf58
Frank Meier <frank.meier@duke.edu>
2024-12-03 08:08:24 -0500

    Revert "register and create EventExtraInfo in B2BIIMdstInput module"
    This reverts commit 94693f5c48b7704b93641f7ce5bdf938528675df.

--- b2bii/modules/B2BIIMdstInput/include/B2BIIMdstInputModule.h
+++ b2bii/modules/B2BIIMdstInput/include/B2BIIMdstInputModule.h
@@ -17,7 +17,6 @@
 #include <framework/datastore/DataStore.h>
 #include <framework/datastore/StoreObjPtr.h>
 #include <framework/dataobjects/EventMetaData.h>
-#include <framework/dataobjects/EventExtraInfo.h>
 #include <framework/dataobjects/FileMetaData.h>
 
 #define BELLE_TARGET_H "belle-x86_64-unknown-linux-gnu-g++.h"
@@ -106,9 +105,6 @@ namespace Belle2 {
 
     //! File metadata to declare if we process real data or simulated data
     StoreObjPtr<FileMetaData> m_fileMetadata{"", DataStore::c_Persistent};
-
-    /** event extra info object pointer */
-    StoreObjPtr<EventExtraInfo> m_eventExtraInfo;
   };
 
 } // end namespace Belle2

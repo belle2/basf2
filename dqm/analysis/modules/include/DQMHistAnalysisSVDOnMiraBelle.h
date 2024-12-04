9bea9a02fc8d98eef2cabc7e67b790f18f6c0528
FINCK Christian <christian.finck@iphc.cnrs.fr>
2024-12-04 19:57:13 +0900

    Using triplet(layer, ladder, sensor) index instead of bin index, fix some bin index errors

--- dqm/analysis/modules/include/DQMHistAnalysisSVDOnMiraBelle.h
+++ dqm/analysis/modules/include/DQMHistAnalysisSVDOnMiraBelle.h
@@ -10,6 +10,7 @@
 
 #include <dqm/core/DQMHistAnalysis.h>
 #include <vxd/dataobjects/VxdID.h>
+#include <vxd/geometry/GeoTools.h>
 
 #include <vector>
 
@@ -58,6 +59,9 @@ namespace Belle2 {
     //! IDs of all SVD Modules to iterate over
     std::vector<VxdID> m_SVDModules;
 
+    //! geometrical tool pointer
+    const VXD::GeoTools* m_gTools = nullptr;
+
     /**
      * Calculate avg offline occupancy for one specific sensor, especially
      * @param iLayer index of layer
@@ -68,20 +72,7 @@ namespace Belle2 {
      * @param nEvents number of events
      * @return vector with values for U and V sides
      */
-    std::vector<float> avgOccupancyUV(TH1F* hU, TH1F* hV,  int layer, int ladder, int sensor, int nEvents) const;
-
-    /**
-    * Calculate avg offline occupancy for specified sensors
-    * @param iLayer index of layer
-    * @param hU, hV pointers to histograms
-    * @param min, max range of ladders
-    * @param offset first bin of relevant sensor
-    * @param step depends on number of sensor in each layer
-    * @param nEvents number of events
-    * @return vector with values for U and V sides
-    */
-    std::vector<float> avgOccupancyUV(int iLayer, TH1F* hU, TH1F* hV, int min, int max, int offset, int step, int nEvents) const;
-
+    std::vector<float> avgOccupancyUV(TH1F* hU, TH1F* hV, int nEvents, int layer = -1, int ladder = -1, int sensor = -1) const;
 
     /**
      * Calculate avg offline occupancy for specified layer for time group id = 0
@@ -91,16 +82,6 @@ namespace Belle2 {
      */
     std::vector<float>  avgOccupancyGrpId0UV(int iLayer, int nEvents) const;
 
-
-    /**
-    * Calculate avg efficiency for specified sensors
-    * @param hMCU, hMCV, hFTU, hFTV pointers to histograms
-    * @param minX, maxX range of ladders
-    * @param minY, maxY range of sensors
-    * @return vector with values for U and V sides
-    */
-    std::vector<float> avgEfficiencyUV(TH2F* hMCU, TH2F* hMCV, TH2F* hFTU, TH2F* hFTV, int minX, int maxX, int minY, int maxY) const;
-
     /**
      * Calculate avg efficiency for specified sensors
      * @param hMCU, hMCV, hFTU, hFTV pointers to histograms
@@ -108,7 +89,8 @@ namespace Belle2 {
      * @param ladder ladder index
      * @param sensor sensor index     * @return vector with values for U and V sides
      */
-    std::vector<float> avgEfficiencyUV(TH2F* hMCU, TH2F* hMCV, TH2F* hFTU, TH2F* hFTV, int layer, int ladder, int sensor) const;
+    std::vector<float> avgEfficiencyUV(TH2F* hMCU, TH2F* hMCV, TH2F* hFTU, TH2F* hFTV, int layer = -1, int ladder = -1,
+                                       int sensor = -1) const;
 
     /**
     * Calculate abscissa of max Y bin
@@ -123,6 +105,13 @@ namespace Belle2 {
     * @return x value for fwhm
     */
     float histFWHM(TH1F* h) const;
+
+    /**
+     * Add variable to object monitoring
+     * @param name name of variable
+     * @param varUV variable vector U/V
+     */
+    void addVariable(std::string name, std::vector<float>& varUV);
 
     /** find the Y bin given the layer and sensor number */
     Int_t findBinY(Int_t layer, Int_t sensor) const
@@ -139,7 +128,44 @@ namespace Belle2 {
       else
         return -1;
     }
+
+    /** get number of ladders per layer*/
+    Int_t getNumberOfLadders(Int_t layer) const
+    {
+      if (layer == 3)
+        return 7;
+      if (layer == 4)
+        return 10;
+      if (layer == 5)
+        return 12;
+      if (layer == 6)
+        return 16;
+      else
+        return -1;
+    }
+
+    /** get number of sensors per layer*/
+    Int_t getNumberOfSesnors(Int_t layer) const
+    {
+      if (layer == 3)
+        return 2;
+      if (layer == 4)
+        return 3;
+      if (layer == 5)
+        return 4;
+      if (layer == 6)
+        return 5;
+      else
+        return -1;
+    }
+
   };
+
+  /** set variable to mirabelle for a given member
+   * @param x name of the variable
+   */
+#define SetVariable(x) addVariable(#x , x)
+
 } // end namespace Belle2
 
 

7f63db1fe582cf1e40d64e5c882ad73116e92e26
Priyanka Cheema <pche3675@uni.sydney.edu.au>
2025-02-13 01:41:23 +0900

    initial commit

--- analysis/variables/src/ECLVariables.cc
+++ analysis/variables/src/ECLVariables.cc
@@ -1496,10 +1496,12 @@ The MVA has been trained using MC and the features used are:
 - `clusterPulseShapeDiscriminationMVA`
 - `clusterE`
 - `clusterTheta`
-- `clusterZernikeMVA`
 
+The variable `clusterZernikeMVA` is also used but only for the MC15 training. 
 Both run-dependent and run-independent weights are available. For more information on this, and for usage recommendations, please see
 the `Neutrals Performance XWiki Page <https://xwiki.desy.de/xwiki/rest/p/e23c8>`_.
+
+Please cite `this <https://inspirehep.net/literature/2785196>`_ if using this tool. 
 )DOC");
     REGISTER_VARIABLE("fakePhotonSuppression", fakePhotonSuppression, R"DOC(
 Returns the output of an MVA classifier that uses shower-related variables to distinguish true photon clusters from fake photon clusters (e.g. split-offs,
@@ -1509,15 +1511,17 @@ The MVA has been trained using MC and the features are:
 
 - `clusterPulseShapeDiscriminationMVA`
 - `minC2TDist`
-- `clusterZernikeMVA`
 - `clusterE`
 - `clusterTiming`
 - `clusterTheta`
 
 This MVA is the same as the one used for `hadronicSplitOffSuppression` but that variable should not be used as it is deprecated and does not use the new weights. 
 
+The variable `clusterZernikeMVA` is also used but only for the MC15 training. 
 Both run-dependent and run-independent weights are available. For more information on this, and for usage recommendations, please see
 the `Neutrals Performance XWiki Page <https://xwiki.desy.de/xwiki/rest/p/e23c8>`_.
+
+Please cite `this <https://inspirehep.net/literature/2785196>`_ if using this tool. 
 )DOC");
     REGISTER_VARIABLE("hadronicSplitOffSuppression", hadronicSplitOffSuppression, R"DOC(
 Returns the output of an MVA classifier that uses shower-related variables to distinguish true photon clusters from hadronic splitoff clusters.

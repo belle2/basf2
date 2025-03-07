a6e6547d5ed74aa9373f8566660d9241f81382aa
manhtt <ttmanh@iop.vast.vn>
2025-03-07 11:58:18 +0900

    update caf

--- cdc/modules/cdcCalibrationCollector/include/CDCBadWireCollector.h
+++ cdc/modules/cdcCalibrationCollector/include/CDCBadWireCollector.h
@@ -66,30 +66,19 @@ namespace Belle2 {
                                                           const Helix& helixFit) const;
 
       StoreArray<Track> m_Tracks;                   /**< Tracks. */
-      StoreArray<RecoTrack> m_RecoTracks;           /**< Tracks. */
       StoreArray<TrackFitResult> m_TrackFitResults; /**< Track fit results. */
       StoreArray<CDCHit> m_CDCHits;                 /**< CDC hits. */
       TrackFindingCDC::StoreWrappedObjPtr<std::vector<TrackFindingCDC::CDCTrack>> m_CDCTracks; /**< CDC tracks. */
 
-      std::string m_trackArrayName;           /**< Belle2::Track StoreArray name. */
-      std::string m_cdcHitArrayName;          /**< Belle2::CDCHit StoreArray name*/
+      std::string m_trackArrayName;                              /**< Belle2::Track StoreArray name. */
+      std::string m_cdcHitArrayName;                             /**< Belle2::CDCHit StoreArray name*/
       std::string m_cdcTrackVectorName = "CDCTrackVector";       /**< Belle2::CDCTrack vectorpointer name*/
-      std::string m_recoTrackArrayName ;      /**< Belle2::RecoTrack StoreArray nam.e */
-      std::string m_trackFitResultArrayName;  /**< Belle2::TrackFitResult StoreArray name. */
-      std::string m_relRecoTrackTrackName;    /**< Relation between RecoTrack and Belle2:Track. */
+      std::string m_trackFitResultArrayName;                     /**< Belle2::TrackFitResult StoreArray name. */
 
-
-      std::string m_treeName;                 /**< Name of tree for the output file. */
       std::string m_effTreeName;              /**< Name of efficiency tree for the output file. */
 
       double m_minimumPt = 0;   /**< minimum pt required for track*/
-      bool m_storeTrackParams = true;          /**< Store Track parameter or not. */
-      bool m_bField = true;                    /**< fit incase no magnetic Field of not, if false, NDF=4 in cal P-value */
       double m_minimumNDF = 0;   /**< minimum NDF required for track*/
-      bool m_effStudy = true;                 /**< When true module collects info only necessary for wire eff study. */
-      Float_t ndf;               /**< degree of freedom. */
-      Float_t Pval;              /**< P-value of fitted track.  */
-
       unsigned short wireID;                   /**< wireID for hit-level wire monitoring */
       unsigned short layerID;                  /**< layerID for hit-level wire monitoring */
       float z;                                 /**< z of hit fot hit-level wire monitoring */

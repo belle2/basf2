5453a83a39fcd09448776b1bc53e4a0e8d861ae7
manhtt <ttmanh@iop.vast.vn>
2025-03-07 13:56:39 +0900

    update caf

--- calibration/scripts/prompt/calibrations/caf_cdc_badwire.py
+++ calibration/scripts/prompt/calibrations/caf_cdc_badwire.py
@@ -15,6 +15,7 @@ from caf.framework import Calibration
 from caf import strategies
 from tracking import add_hit_preparation_modules, add_track_finding, add_track_fit_and_track_creator
 from rawdata import add_unpackers
+
 settings = CalibrationSettings(name="CDC badwire",
                                expert_username="manhtt",
                                description=__doc__,
@@ -34,11 +35,14 @@ settings = CalibrationSettings(name="CDC badwire",
                                })
 
 
+# Main function to get calibrations
 def get_calibrations(input_data, **kwargs):
     expert_config = kwargs.get("expert_config")
     min_events_per_file = expert_config["min_events_per_file"]
     max_events_per_file = expert_config["max_events_per_file"]
     components = expert_config["components"]
+
+    # In this script we want to use one sources of input data.
     # Get the input files  from the input_data variable
     file_to_iov_mumu = input_data["mumu_tight_or_highm_calib"]
     from prompt.utils import filter_by_max_files_per_run
@@ -46,6 +50,7 @@ def get_calibrations(input_data, **kwargs):
     input_files_mumu = list(reduced_file_to_iov_mumu.keys())
     basf2.B2INFO("Complete input data selection.")
     basf2.B2INFO(f"Total number of files actually used as input = {len(input_files_mumu)}")
+
     payload_boundaries = []
     payload_boundaries.extend([ExpRun(*boundary) for boundary in expert_config["payload_boundaries"]])
     basf2.B2INFO(f"Payload boundaries from expert_config: {payload_boundaries}")
@@ -53,11 +58,13 @@ def get_calibrations(input_data, **kwargs):
     # The actual value our output IoV payload should have. Notice that we've set it open ended.
     requested_iov = kwargs.get("requested_iov", None)
     output_iov = IoV(requested_iov.exp_low, requested_iov.run_low, -1, -1)
+
     # for SingleIOV stratrgy, it's better to set the granularity to 'all' so that the collector jobs will run faster
     if payload_boundaries:
         basf2.B2INFO('Found payload_boundaries: set collector granularity to run')
     # call collector module
     col = basf2.register_module("CDCBadWireCollector")
+
     # call algorighm
     algo = Belle2.CDC.WireEfficiencyAlgorithm()
     algo.setInputFileNames("histo_badwire.root")
@@ -78,10 +85,12 @@ def get_calibrations(input_data, **kwargs):
     else:
         for alg in badwire_calib.algorithms:
             alg.params = {"apply_iov": output_iov}
+
     return [badwire_calib]
 
 
 def pre_collector(max_events=None, components=["CDC", "ECL", "KLM"]):
+
     # Create an execution path
     path = basf2.create_path()
     branches = ['EventMetaData', 'RawCDCs', 'RawFTSWs']
@@ -93,11 +102,14 @@ def pre_collector(max_events=None, components=["CDC", "ECL", "KLM"]):
                     energyLossBrems=False, noiseBrems=False)
     add_unpackers(path, components=unpackers)
     add_hit_preparation_modules(path, components=unpackers)
+
     # Print some progress messages
     path.add_module("Progress")
+
     # Execute CDC track finding
     add_track_finding(path, components=unpackers)
     add_track_fit_and_track_creator(path, trackFitHypotheses=[211])  # pion or muon hypothesis
+
     # Making sure CDC Raw Hits are stored
     for module in path.modules():
         if module.name() == 'CDCUnpacker':
@@ -106,6 +118,7 @@ def pre_collector(max_events=None, components=["CDC", "ECL", "KLM"]):
         if module.name() == 'TFCDC_WireHitPreparer':
             print('Enabling bad wires')
             module.param({'useBadWires': True})
+
     # Print a textual representation of what was just put together
     basf2.print_path(path)
     return path

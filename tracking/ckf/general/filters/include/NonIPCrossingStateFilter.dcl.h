f0f23f088a920fc33783c7af38aa11e680ee70e7
Christian Wessel <christian.wessel@desy.de>
2025-03-19 11:17:00 +0100

    Found another one in the ckf.

--- tracking/ckf/general/filters/include/NonIPCrossingStateFilter.dcl.h
+++ tracking/ckf/general/filters/include/NonIPCrossingStateFilter.dcl.h
@@ -7,7 +7,6 @@
  **************************************************************************/
 #pragma once
 
-#include <tracking/ckf/svd/filters/states/AllSVDStateFilter.h>
 #include <tracking/trackFindingCDC/numerics/EForwardBackward.h>
 #include <string>
 

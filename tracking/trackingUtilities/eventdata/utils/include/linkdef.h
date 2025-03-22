eb7cebce6efc6c80297e3b84e50b672e10f5e47b
Christian Wessel <christian.wessel@desy.de>
2025-03-22 20:58:52 +0100

    Add copy of trackFindingCDC/eventdata/utils/include/linkdef.h to corresponding new folder.

---
+++ tracking/trackingUtilities/eventdata/utils/include/linkdef.h
@@ -0,0 +1,8 @@
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class Belle2::TrackFindingCDC::DriftTimeUtil-;
+#pragma link C++ class Belle2::TrackFindingCDC::DriftLengthEstimator-;
+#pragma link C++ class Belle2::TrackFindingCDC::EnergyLossEstimator-;

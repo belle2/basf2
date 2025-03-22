5ba83ce8331335b88d772de4f09a7b5e0c51d0b6
Christian Wessel <christian.wessel@desy.de>
2025-03-22 21:49:49 +0100

    Make required modifications in trackFindingCDC/hough/quadratic.

--- tracking/trackFindingCDC/hough/quadratic/include/HitQuadraticLegendre.h
+++ tracking/trackFindingCDC/hough/quadratic/include/HitQuadraticLegendre.h
@@ -8,7 +8,7 @@
 #pragma once
 #include <tracking/trackFindingCDC/hough/quadratic/QuadraticLegendre.h>
 #include <tracking/trackFindingCDC/hough/algorithms/FirstOfPairInBox.h>
-#include <tracking/trackFindingCDC/eventdata/hits/CDCRecoHit3D.h>
+#include <tracking/trackingUtilities/eventdata/hits/CDCRecoHit3D.h>
 #include <tracking/trackFindingCDC/eventdata/hits/CDCRLWireHit.h>
 #include <tracking/trackFindingCDC/hough/algorithms/HitInQuadraticBox.h>
 
@@ -21,6 +21,6 @@ namespace Belle2 {
      * This class is only an alias. The real algorithm can be found in SimpleBoxDivisionHoughTree.
      */
     using HitQuadraticLegendre =
-      QuadraticLegendre<std::pair<CDCRecoHit3D, const CDCRLWireHit*>, FirstOfPairInBox<HitInQuadraticBox>, 2, 2>;
+      QuadraticLegendre<std::pair<TrackingUtilities::CDCRecoHit3D, const CDCRLWireHit*>, FirstOfPairInBox<HitInQuadraticBox>, 2, 2>;
   }
 }

4ac7f4f6569906d56ce9246dc4b1f7364c01dca4
Christian Wessel <christian.wessel@desy.de>
2025-03-23 13:36:22 +0100

    Replace the trackFindingCDC library with trackingUtilities in the Python module.

--- tracking/trackingUtilities/scripts/trackingutilities/__init__.py
+++ tracking/trackingUtilities/scripts/trackingutilities/__init__.py
@@ -13,5 +13,5 @@ import basf2  # noqa
 from ROOT import gSystem
 gSystem.Load('libcdc')
 gSystem.Load('libtracking')
-gSystem.Load('libtracking_trackFindingCDC')
+gSystem.Load('libtracking_trackingUtilities')
 gSystem.Load('libgenfit2')

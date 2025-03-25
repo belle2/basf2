7f69d4504dd2fb5a648a4a41dcc4829c272f33d5
manhtt <ttmanh@iop.vast.vn>
2025-03-25 11:21:59 +0900

    update caf

--- cdc/modules/cdcCalibrationCollector/src/CDCBadWireCollector.cc
+++ cdc/modules/cdcCalibrationCollector/src/CDCBadWireCollector.cc
@@ -15,7 +15,7 @@
 #include <genfit/KalmanFitterInfo.h>
 #include <genfit/MeasurementOnPlane.h>
 #include <genfit/MeasuredStateOnPlane.h>
-#include <Math/ProbFuncMathCore.h>
+//#include <Math/ProbFuncMathCore.h>
 #include <cdc/dataobjects/WireID.h>
 #include <cdc/geometry/CDCGeometryPar.h>
 #include <TH1F.h>
@@ -34,13 +34,8 @@ CDCBadWireCollectorModule::CDCBadWireCollectorModule() : CalibrationCollectorMod
 {
   setDescription("Collector module for cdc calibration");
   setPropertyFlags(c_ParallelProcessingCertified);  // specify this flag if you need parallel processing
-  addParam("recoTracksColName", m_recoTrackArrayName, "Name of collection hold genfit::Track", std::string(""));
-  addParam("bField", m_bField, "If true -> #Params ==5 else #params ==4 for calculate P-Val", false);
-  addParam("storeTrackParams", m_storeTrackParams, "Store Track Parameter or not, it will be multicount for each hit", false);
   addParam("minimumPt", m_minimumPt, "Tracks with tranverse momentum smaller than this value will not used", 1.0);
   addParam("minimumNDF", m_minimumNDF, "Discard tracks whose degree-of-freedom below this value", 5.);
-  //  addParam("isCosmic", m_isCosmic, "True when we process cosmic events, else False (collision)", m_isCosmic);
-  addParam("effStudy", m_effStudy, "When true module collects info only  necessary for wire eff study", true);
 }
 
 CDCBadWireCollectorModule::~CDCBadWireCollectorModule()
@@ -50,39 +45,26 @@ CDCBadWireCollectorModule::~CDCBadWireCollectorModule()
 void CDCBadWireCollectorModule::prepare()
 {
   m_Tracks.isRequired(m_trackArrayName);
-  m_RecoTracks.isRequired(m_recoTrackArrayName);
   m_TrackFitResults.isRequired(m_trackFitResultArrayName);
   m_CDCHits.isRequired(m_cdcHitArrayName);
   m_CDCTracks.isRequired(m_cdcTrackVectorName);
-  RelationArray relRecoTrackTrack(m_RecoTracks, m_Tracks, m_relRecoTrackTrackName);
-  //Store names to speed up creation later
-  m_relRecoTrackTrackName = relRecoTrackTrack.getName();
 
-  if (m_effStudy) { //if m_effStudy is changed to true prepares to only run wire efficiency study
-    auto m_efftree  = new TTree(m_effTreeName.c_str(), "tree for wire efficiency");
-    m_efftree->Branch<unsigned short>("layerID", &layerID);
-    m_efftree->Branch<unsigned short>("wireID", &wireID);
-    m_efftree->Branch<float>("z", &z);
-    m_efftree->Branch<bool>("isFound", &isFound);
-
-    registerObject<TTree>("efftree", m_efftree);
-  }
+  auto m_efftree  = new TTree(m_effTreeName.c_str(), "tree for wire efficiency");
+  m_efftree->Branch<unsigned short>("layerID", &layerID);
+  m_efftree->Branch<unsigned short>("wireID", &wireID);
+  m_efftree->Branch<float>("z", &z);
+  m_efftree->Branch<bool>("isFound", &isFound);
+  registerObject<TTree>("efftree", m_efftree);
 
   auto m_hNDF = new TH1F("hNDF", "NDF of fitted track;NDF;Tracks", 71, -1, 70);
   auto m_hPval = new TH1F("hPval", "p-values of tracks;pVal;Tracks", 1000, 0, 1);
-  auto m_hNTracks = new TH1F("hNTracks", "Number of tracks", 50, 0, 10);
-  auto m_hOccupancy = new TH1F("hOccupancy", "occupancy", 100, 0, 1.0);
 
   registerObject<TH1F>("hNDF", m_hNDF);
   registerObject<TH1F>("hPval", m_hPval);
-  registerObject<TH1F>("hNTracks", m_hNTracks);
-  registerObject<TH1F>("hOccupancy", m_hOccupancy);
 }
 
 void CDCBadWireCollectorModule::collect()
 {
-  const RelationArray relTrackTrack(m_RecoTracks, m_Tracks, m_relRecoTrackTrackName);
-
   // Collects the WireID and Layer of every hit in this event
   // Used in wire efficiency building
   std::vector<unsigned short> wiresInCDCTrack;
@@ -94,14 +76,7 @@ void CDCBadWireCollectorModule::collect()
     }
   }
   // WireID collection finished
-
   const int nTr = m_Tracks.getEntries();
-  // Skip events which have number of charged tracks <= 1.
-  for (int i = 0; i < nTr; ++i) {
-    const Belle2::Track* b2track = m_Tracks[i];
-    const Belle2::TrackFitResult* fitresult = b2track->getTrackFitResultWithClosestMass(Const::muon);
-    if (!fitresult) continue;
-  }
   for (int i = 0; i < nTr; ++i) {
     const Belle2::Track* b2track = m_Tracks[i];
     const Belle2::TrackFitResult* fitresult = b2track->getTrackFitResultWithClosestMass(Const::muon);
@@ -110,45 +85,27 @@ void CDCBadWireCollectorModule::collect()
       continue;
     }
 
-    Belle2::RecoTrack* recoTrack = b2track->getRelatedTo<Belle2::RecoTrack>(m_recoTrackArrayName);
-    if (!recoTrack) {
-      B2WARNING("Can not access RecoTrack of this Belle2::Track");
-      continue;
-    }
-    const genfit::FitStatus* fs = recoTrack->getTrackFitStatus();
-    if (!fs) continue;
-    ndf = fs->getNdf();
-    if (!m_bField) {
-      ndf += 1;
-    }
-
-    getObjectPtr<TH1F>("hPval")->Fill(Pval);
+    double ndf = fitresult->getNDF();
+    double pval = fitresult->getPValue();
+    double pt = fitresult->getTransverseMomentum();
+    double d0 = fitresult->getD0();
+    double z0 = fitresult->getZ0();
+    getObjectPtr<TH1F>("hPval")->Fill(pval);
     getObjectPtr<TH1F>("hNDF")->Fill(ndf);
-    B2DEBUG(99, "ndf = " << ndf);
-    B2DEBUG(99, "Pval = " << Pval);
 
+    // Cut on NDF, Pt
     if (ndf < m_minimumNDF) continue;
-    double Chi2 = fs->getChi2();
-    Pval = std::max(0., ROOT::Math::chisquared_cdf_c(Chi2, ndf));
+    if (pt < m_minimumPt) continue;
 
-    //cut at Pt
-    if (fitresult->getTransverseMomentum() < m_minimumPt) continue;
-    if (!m_effStudy) {
-      return;
-    }
-    if (m_effStudy) { // call buildEfficiencies for efficiency study
-      // Request tracks coming from IP
-      if (fitresult->getD0() > 2 || fitresult->getZ0() > 5) continue;
-      const Helix helixFit = fitresult->getHelix();
-      buildEfficiencies(wiresInCDCTrack, helixFit);
-    }
+    // Request tracks coming from IP
+    if (d0 > 2 || z0 > 5) continue;
+
+    const Helix helixFit = fitresult->getHelix();
+    buildEfficiencies(wiresInCDCTrack, helixFit);
   }
-
 }
 
-void CDCBadWireCollectorModule::finish()
-{
-}
+void CDCBadWireCollectorModule::finish() {}
 const CDCWire& CDCBadWireCollectorModule::getIntersectingWire(const TVector3& xyz, const CDCWireLayer& layer,
     const Helix& helixFit) const
 {
@@ -162,9 +119,7 @@ const CDCWire& CDCBadWireCollectorModule::getIntersectingWire(const TVector3& xy
     TVector3 xyzOnWire = B2Vector3D(helixFit.getPositionAtArcLength2D(arcLength));
     crosspoint = Vector3D(xyzOnWire);
   }
-
   const CDCWire& wire = layer.getClosestWire(crosspoint);
-
   return wire;
 }
 

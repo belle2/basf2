cee3bac50869f3d04f4c60de641635749f4658c3
Giacomo De Pietro <giacomo.pietro@kit.edu>
2025-04-09 16:55:25 +0200

    Fix -Wabsolute-value warning with clang and C++20 standard

--- cdc/modules/CDCDedxElectronCollector/src/CDCDedxElectronCollectorModule.cc
+++ cdc/modules/CDCDedxElectronCollector/src/CDCDedxElectronCollectorModule.cc
@@ -8,11 +8,12 @@
 
 #include <cdc/modules/CDCDedxElectronCollector/CDCDedxElectronCollectorModule.h>
 
-
 #include <TTree.h>
 #include <TH1D.h>
 #include <TH1I.h>
 #include <TMath.h>
+
+#include <cmath>
 
 using namespace Belle2;
 //-----------------------------------------------------------------
@@ -217,8 +218,8 @@ void CDCDedxElectronCollectorModule::collect()
 
     if (m_cuts) {
       // apply cleanup cuts
-      if (fabs(fitResult->getD0()) >= 1.0)continue;
-      if (fabs(fitResult->getZ0()) >= 1.0) continue;
+      if (fstd::abs(fitResult->getD0()) >= 1.0)continue;
+      if (fstd::abs(fitResult->getZ0()) >= 1.0) continue;
       htstats->Fill(1);
 
       //if outside CDC
@@ -244,7 +245,7 @@ void CDCDedxElectronCollectorModule::collect()
       const ECLCluster* eclCluster = track->getRelated<ECLCluster>();
       if (eclCluster and eclCluster->hasHypothesis(ECLCluster::EHypothesisBit::c_nPhotons)) {
         double TrkEoverP = (eclCluster->getEnergy(ECLCluster::EHypothesisBit::c_nPhotons)) / (fitResult->getMomentum().R());
-        if (abs(TrkEoverP - 1.0) > m_setEoP)continue;
+        if (std::abs(TrkEoverP - 1.0) > m_setEoP)continue;
       }
       htstats->Fill(4);
     }
@@ -256,9 +257,9 @@ void CDCDedxElectronCollectorModule::collect()
       //checking if dedx of other track is restricted
       //will not do too much as radee is clean enough
       for (int jdedx = 0; jdedx < nTracks; jdedx++) {
-        CDCDedxTrack* dedxOtherTrack = m_dedxTracks[abs(jdedx - 1)];
+        CDCDedxTrack* dedxOtherTrack = m_dedxTracks[std::abs(jdedx - 1)];
         if (!dedxOtherTrack)continue;
-        if (abs(dedxOtherTrack->getDedxNoSat() - 1.0) > 0.25)continue; //loose for uncalibrated
+        if (std::abs(dedxOtherTrack->getDedxNoSat() - 1.0) > 0.25)continue; //loose for uncalibrated
         goodradee = true;
         break;
       }
--- hlt/softwaretrigger/calculations/src/FilterCalculator.cc
+++ hlt/softwaretrigger/calculations/src/FilterCalculator.cc
@@ -19,6 +19,7 @@
 
 #include <framework/logging/Logger.h>
 
+#include <cmath>
 #include <numeric>
 #include <stdexcept>
 #include <optional>
@@ -885,8 +886,8 @@ void FilterCalculator::doCalculation(SoftwareTriggerObject& calculationResult)
       const double& d0Pos = posTrack->track->getTrackFitResultWithClosestMass(Const::pion)->getD0();
 
       // Select cosmic using these tracks
-      const bool goodMagneticRegion = (z0Neg<m_goodMagneticRegionZ0 or abs(d0Neg)>m_goodMagneticRegionD0)
-                                      and (z0Pos<m_goodMagneticRegionZ0 or abs(d0Pos)>m_goodMagneticRegionD0);
+      const bool goodMagneticRegion = (z0Neg<m_goodMagneticRegionZ0 or std::abs(d0Neg)>m_goodMagneticRegionD0)
+                                      and (z0Pos<m_goodMagneticRegionZ0 or std::abs(d0Pos)>m_goodMagneticRegionD0);
       if (maxNegpT > m_cosmicMinPt and maxPospT > m_cosmicMinPt and maxClusterENeg < m_cosmicMaxClusterEnergy
           and maxClusterEPos < m_cosmicMaxClusterEnergy and goodMagneticRegion) {
         double dphiLab = std::abs(momentumLabNeg.Phi() - momentumLabPos.Phi()) * TMath::RadToDeg();

68212ff14f9929bd222bad53fd3d15d2cabc978f
Frank Meier <frank.meier@duke.edu>
2025-04-24 17:29:37 -0400

    update flavor tagging documentation

--- analysis/examples/tutorials/B2A801-FlavorTagger.py
+++ analysis/examples/tutorials/B2A801-FlavorTagger.py
@@ -92,7 +92,7 @@ b2.conditions.append_globaltag(ma.getAnalysisGlobaltag())
 
 # The default working directory is '.'
 # Note that if you also train by yourself the weights of the trained Methods are saved therein.
-# To save CPU time the weight files should be saved in the same server were you run.
+# To save CPU time the weight files should be saved in the same server where you run.
 #
 # NEVER set uploadToDatabaseAfterTraining to True if you are not a librarian!!!
 #
@@ -107,11 +107,6 @@ ft.flavorTagger(
     useGNN=False,
     path=cp_val_path)
 
-# By default the flavorTagger trains and applies two methods, 'TMVA-FBDT' and 'FANN-MLP', for the combiner.
-# If you want to train or test the Flavor Tagger only for one of them you have to specify it like:
-#
-# combinerMethods=['TMVA-FBDT']
-#
 # All available categories are:
 # [
 # 'Electron',
@@ -128,9 +123,10 @@ ft.flavorTagger(
 # 'MaximumPstar',
 # 'KaonPion']
 #
-# If you want to train yourself, have a look at the scripts under analysis/release-validation/CPVTools/
-# in principle you need only to run CPVToolsValidatorInParalell.sh
-# If you train the  event and combiner levels, you need two different samples of at least 500k events (one for each sampler).
+# If you want to train yourself, have a look at the scripts at
+# https://gitlab.desy.de/belle2/performance/releasevalidation/-/tree/main/CPVTools
+# In principle, you only need to run CPVToolsValidatorInParalell.sh
+# If you train the event and combiner levels, you need two different samples of at least 500k events (one for each sampler).
 # The different samples are needed to avoid biases between levels.
 # We mean 500k of correctly corrected and MC matched neutral Bs. (isSignal > 0)
 # You can also train the event level for all categories and then train the combiner
@@ -207,9 +203,3 @@ b2.process(cp_val_path)
 
 # print out the summary
 print(b2.statistics)
-
-# If you want to calculate the efficiency of the FlavorTagger on your own
-# File use the script analysis/release-validation/CPVTools/flavorTaggerEfficiency.py giving
-# your file and the treename as arguments:
-
-# basf2 flavorTaggerEfficiency.py 'YourFilesWithWildCards.root' 'B0tree'

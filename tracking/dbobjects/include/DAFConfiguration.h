1dfb4d6abe945ee7de23c4294dd5f4b255e22679
stefanospataro <stefano.spataro@to.infn.it>
2025-05-02 13:53:03 +0200

    Added DAFConfiguration class to store different DAFparameters configurations

---
+++ tracking/dbobjects/include/DAFConfiguration.h
@@ -0,0 +1,68 @@
+/**************************************************************************
+ * basf2 (Belle II Analysis Software Framework)                           *
+ * Author: The Belle II Collaboration                                     *
+ *                                                                        *
+ * See git log for contributors and copyright holders.                    *
+ * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
+ **************************************************************************/
+#pragma once
+
+#include <TObject.h>
+#include <map>
+
+#include "DAFparameters.h"
+
+namespace Belle2 {
+
+  /** The payload containing all the DAFparameters configuration,
+   * one for each different track fit option
+   */
+  class DAFConfiguration: public TObject {
+
+  public:
+
+    /**
+     * Enum for identifying the type of track fit algorythm
+     * ( or cosmic)
+     */
+    enum ETrackFitType {
+      c_Default,
+      c_CDConly,
+      c_Cosmics,
+      c_Custom1,
+      c_Custom2,
+      c_Custom3
+    };
+
+    /** Default constructor */
+    DAFConfiguration() {}
+    /** Destructor */
+    ~DAFConfiguration() {}
+
+    // /** Set the probability cut for the weight calculation for the hits
+    // * @param probabilitycut probability cut used in setProbCut and addProbCut method from DAF
+    // */
+    // void setProbabilityCut(const float probabilitycut)
+    // {
+    //   m_ProbabilityCut = probabilitycut;
+    // }
+
+    /** Get the DAFparameters for the specific track fit type
+     */
+    DAFparameters getDAFparameters(DAFConfiguration::ETrackFitType trackFitType) const
+    {
+      std::map<DAFConfiguration::ETrackFitType, DAFparameters>::const_iterator it = m_DAFparameters.find(trackFitType);
+      if (it != m_DAFparameters.end()) {
+        return it->second;
+      } else {
+        B2FATAL("Track Fit option " << trackFitType << " not found");
+      }
+    }
+
+  private:
+    /** The minimum allowed pValue for the convergence criterion */
+    std::map<DAFConfiguration::ETrackFitType, DAFparameters> m_DAFparameters;
+
+    ClassDef(DAFConfiguration, 1);  /**< ClassDef, necessary for ROOT */
+  };
+}
--- tracking/dbobjects/include/linkdef.h
+++ tracking/dbobjects/include/linkdef.h
@@ -9,7 +9,8 @@
 #pragma link C++ class Belle2::TrackFitMomentumRange+; // checksum=0x8f6de621, version=1
 #pragma link C++ class Belle2::TrackFlippingCuts+; // checksum=0x8e010941, version=4
 #pragma link C++ class Belle2::DAFparameters+; // checksum=0xe9a67894, version=2
-#pragma link C++ class Belle2::ROICalculationParameters+; // checksum=0x8691bfeb, version=1
+#pragma link C++ class Belle2::DAFConfiguration+;
+#pragma link C++ class Belle2::ROICalculationParameters+;
 #pragma link C++ class Belle2::SVDEventT0Configuration+; // checksum=0x3782a1d9, version=1
 #pragma link C++ class Belle2::CkfToPXDFiltersSetting+; // checksum=0x4f402468, version=1
 #pragma link C++ class Belle2::KinkFinderParameters+; // checksum=0x6ba64774, version=1

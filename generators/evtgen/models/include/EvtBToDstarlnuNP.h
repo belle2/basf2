aa8a166dd388009b6ed417d4c59c8ac18de62c69
Alexei Sibidanov <sibid@uvic.ca>
2025-05-16 08:31:30 +0900

    New physics generator for the B->D*lnu decay based on Phys.Rev.D 107 (2023) 1, 015011; e-Print:2206.11283 [hep-ph]. The hadronic form factors are based on the BGL parameterization with updated coefficients from the fit to the most recent experimental data.

---
+++ generators/evtgen/models/include/EvtBToDstarlnuNP.h
@@ -0,0 +1,68 @@
+/**************************************************************************
+ * basf2 (Belle II Analysis Software Framework)                           *
+ * Author: The Belle II Collaboration                                     *
+ *                                                                        *
+ * See git log for contributors and copyright holders.                    *
+ * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
+ **************************************************************************/
+
+#pragma once
+
+#include "EvtGenBase/EvtDecayAmp.hh"
+#include "EvtGenBase/EvtComplex.hh"
+
+class EvtParticle;
+
+/**
+ * Implementation of the B -> D^* \ell \bar{\nu}_\ell with new physics
+ * contributions and the BGL hadronic form factor parameterization.
+ * Based on the publication:
+ * Phys.Rev.D 107 (2023) 1, 015011; e-Print:2206.11283 [hep-ph]
+ */
+class EvtBToDstarlnuNP : public EvtDecayAmp {
+public:
+  /**
+   * The function which returns the name of the model.
+   */
+  std::string getName() override;
+
+  /**
+   * The function which makes a copy of the model.
+   */
+  EvtDecayBase* clone() override;
+
+  /**
+   * The method to calculate a decay amplitude.
+   */
+  void decay(EvtParticle* p) override;
+
+  /**
+   * Initialization method
+   */
+  void init() override;
+
+  /**
+   * The method to evaluate the maximum amplitude.
+   */
+  void initProbMax() override;
+
+private:
+  /** Strength of the left handed part of the vector hadronic current */
+  EvtComplex _Cvl;
+
+  /** Strength of the Right handed part of the vector hadronic current */
+  EvtComplex _Cvr;
+
+  /** (g_S - g_P)  strength of pseudoscalar current */
+  EvtComplex _Csl;
+
+  /** (g_S + g_P)  strength of pseudoscalar current with constraint _Csl = -_Csr */
+  EvtComplex _Csr;
+
+  /** C_T -- tensor current */
+  EvtComplex _cT;
+
+  double CalcMaxProb();
+
+  void CalcAmp(EvtParticle*, EvtAmp&);
+};

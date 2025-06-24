2a1b65f6f3db6bbe564966ce51e7681b490918dc
Taichiro Koga <taichiro@post.kek.jp>
2025-06-24 18:06:11 +0900

    add comment out to explain only CDCTRG BGoverlay is needed

--- simulation/scripts/simulation.py
+++ simulation/scripts/simulation.py
@@ -248,6 +248,8 @@ def add_simulation(
 
     if components is None or 'TRG' in components:
         if bkgfiles is not None and bkgOverlay:
+            # BG Overlay for CDCTRG. That for KLMTRG is already covered by BG Overlay
+            # for KLM. ECL and ECLTRG are already covered independently.
             m = path.add_module('BGOverlayExecutor', components=['CDC'], CDCHitsName='CDCHits4Trg')
             m.set_name('BGOverlayExecutor_TRGCDC')
         add_trigger_simulation(path, simulateT0jitter=simulateT0jitter, FilterEvents=FilterEvents)

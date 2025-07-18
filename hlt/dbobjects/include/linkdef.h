2fd89c5cf72b09a14ce6a900fc0ea89c3afac573
Rahul <rahul.tiwary@tyl.in2p3.fr>
2025-07-18 19:50:36 +0900

    Updated checksum for HLTprefilterParameters class

--- hlt/dbobjects/include/linkdef.h
+++ hlt/dbobjects/include/linkdef.h
@@ -5,6 +5,6 @@
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
-#pragma link C++ class Belle2::HLTprefilterParameters+; // checksum=0x19886D67, version=1
+#pragma link C++ class Belle2::HLTprefilterParameters+; // checksum=0x9472247, version=1
 
 #endif

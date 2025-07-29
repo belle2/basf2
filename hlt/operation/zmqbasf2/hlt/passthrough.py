52964e2ba2be0d93e23572f286c685cdf19bd4cf
Seokhee Park <seokhee.park@kek.jp>
2025-07-29 16:40:00 +0900

    zmqbasf2 hlt passthrough update

--- hlt/operation/zmqbasf2/hlt/passthrough.py
+++ hlt/operation/zmqbasf2/hlt/passthrough.py
@@ -5,19 +5,17 @@
 # See git log for contributors and copyright holders.                    #
 # This file is licensed under LGPL-3.0, see LICENSE.md.                  #
 ##########################################################################
+
 import basf2
 from softwaretrigger import constants
-from softwaretrigger.processing import finalize_zmq_path, setup_basf2_and_db, start_zmq_path
-from pxd import add_pxd_percentframe
-from tracking import add_roi_payload_assembler
+from softwaretrigger.processing import finalize_zmq_path, setup_basf2_and_db, start_zmq_path, add_hlt_passthrough
 
 
 args = setup_basf2_and_db(event_distribution_mode=constants.EventDistributionModes.zmqbasf2)
 
 path, _ = start_zmq_path(args, location=constants.Location.hlt,
                          event_distribution_mode=constants.EventDistributionModes.zmqbasf2)
-add_pxd_percentframe(path, fraction=0.1, random_position=True)
-add_roi_payload_assembler(path, ignore_hlt_decision=True)
+add_hlt_passthrough(path)
 finalize_zmq_path(path, args, location=constants.Location.hlt)
 
 basf2.print_path(path)

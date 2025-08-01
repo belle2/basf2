7c6457c58fa3af44ac42769d4e70d9ddc5556d46
Giacomo De Pietro <giacomo.pietro@kit.edu>
2025-08-01 03:07:16 +0200

    Merge branch 'feature/hlt-script-2' into 'main'
    Feature/hlt script 2
    
    See merge request belle2/software/basf2!4475

--- hlt/operation/zmq/hlt/beam_reco_monitor.py
+++ hlt/operation/zmq/hlt/beam_reco_monitor.py
@@ -10,25 +10,15 @@
 import basf2
 import hbasf2
 from softwaretrigger import constants
-from softwaretrigger.processing import finalize_zmq_path, setup_basf2_and_db, start_zmq_path
-from pxd import add_roi_payload_assembler
+from softwaretrigger.processing import setup_basf2_and_db, start_zmq_path, finalize_zmq_path, add_hlt_processing
 
 
-args = setup_basf2_and_db(zmq=True)
+args = setup_basf2_and_db(event_distribution_mode=constants.EventDistributionModes.zmq)
 
-path, reco_path = start_zmq_path(args, location=constants.Location.hlt)
-
-# Monitor module
-reco_path.add_module('MonitorData')
-
-# ROI Payload related
-reco_path.add_module('ROIGenerator', ROIListName='ROIs', nROIs=8, Layer=1, Ladder=1, Sensor=1)
-add_roi_payload_assembler(reco_path, ignore_hlt_decision=True)
-# reco_path.add_module('ROIReadTest', outfileName='ROIoutHLT.txt',ROIpayloadName='ROIpayload')
-
-# Debug modules
-reco_path.add_module('ElapsedTime', EventInterval=10000)
-
+path, reco_path = start_zmq_path(args, location=constants.Location.hlt,
+                                 event_distribution_mode=constants.EventDistributionModes.zmq)
+add_hlt_processing(reco_path, run_type=constants.RunTypes.beam,
+                   softwaretrigger_mode=constants.SoftwareTriggerModes.monitor)
 finalize_zmq_path(path, args, location=constants.Location.hlt)
 
 basf2.print_path(path)
--- hlt/operation/zmqbasf2/hlt/passthrough.py
+++ hlt/operation/zmqbasf2/hlt/passthrough.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 ##########################################################################
 # basf2 (Belle II Analysis Software Framework)                           #
 # Author: The Belle II Collaboration                                     #
@@ -7,21 +5,18 @@
 # See git log for contributors and copyright holders.                    #
 # This file is licensed under LGPL-3.0, see LICENSE.md.                  #
 ##########################################################################
+
 import basf2
-import hbasf2
 from softwaretrigger import constants
-from softwaretrigger.processing import finalize_zmq_path, setup_basf2_and_db, start_zmq_path
+from softwaretrigger.processing import finalize_zmq_path, setup_basf2_and_db, start_zmq_path, add_hlt_passthrough
 
 
-args = setup_basf2_and_db(zmq=True)
+args = setup_basf2_and_db(event_distribution_mode=constants.EventDistributionModes.zmqbasf2)
 
-path, reco_path = start_zmq_path(args, location=constants.Location.expressreco)
-
-# Monitor/Debug modules
-reco_path.add_module('MonitorData')
-reco_path.add_module('ElapsedTime', EventInterval=10000)
-
-finalize_zmq_path(path, args, location=constants.Location.expressreco)
+path, _ = start_zmq_path(args, location=constants.Location.hlt,
+                         event_distribution_mode=constants.EventDistributionModes.zmqbasf2)
+add_hlt_passthrough(path)
+finalize_zmq_path(path, args, location=constants.Location.hlt)
 
 basf2.print_path(path)
-hbasf2.process(path, [args.dqm, args.output], True)
+basf2.process(path)

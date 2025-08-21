8d4a87e67a9db82476dc2fd20840e12035e3f70a
Christian Wessel <christian.wessel@desy.de>
2025-08-21 21:31:05 +0200

    Work on comments for framework VectorUtil and remove unnecessary function orthogonalComp.

--- framework/geometry/include/VectorUtil.h
+++ framework/geometry/include/VectorUtil.h
@@ -93,44 +93,45 @@ namespace Belle2 {
     }
 
     /**
-     * Find CosTheta Angle between two generic 3D vectors
+     * Calculate CosTheta between two vectors
      * @param[in] v1  Vector v1
      * @param[in] v2  Vector v2
      * \return   cosine of Angle between the two vectors
      * \f[ \cos \theta = \frac { \vec{v1} \cdot \vec{v2} }{ | \vec{v1} | | \vec{v2} | } \f]
      *
-     * Similar to the implementation in the ROOT documentation in Math::VectorUtil:
-     * https://root.cern/doc/master/GenVector_2VectorUtil_8h_source.html#l00142
-     * but that does not use the Mag2 method nor the Dot method to calculate the two properties
-     * as it expects two different vector types and would only work for 3-vectors
+     * There is a function with the same name in ROOT::Math::VectorUtil. However, that function only works for 3-vectors, and does not
+     * use the Mag2 method nor the Dot method to calculate the two properties as it is allowed to provide two different vector types
+     * (like v1 being cartesian and v2 being polar) and would only work for 3-vectors.
+     * However, we also need a 2D version, thus, this custom implementation.
      */
     template <class Vector>
     double CosTheta(const Vector&  v1, const Vector& v2)
     {
-      double arg = 0.;
-      double v1_r2 = v1.Mag2();
-      double v2_r2 = v2.Mag2();
-      double ptot2 = v1_r2 * v2_r2;
+      const double v1_r2 = v1.Mag2();
+      const double v2_r2 = v2.Mag2();
+      const double ptot2 = v1_r2 * v2_r2;
       if (ptot2 == 0) {
         return 0.0;
       }
-      double pdot = v1.Dot(v2);
-      arg = pdot / std::sqrt(ptot2);
-      if (arg >  1.0) arg =  1.0;
-      if (arg < -1.0) arg = -1.0;
+      const double pdot = v1.Dot(v2);
+      double arg = pdot / std::sqrt(ptot2);
+      if (arg >  1.0)
+        arg =  1.0;
+      if (arg < -1.0)
+        arg = -1.0;
       return arg;
     }
 
 
     /**
-     * Find CosTheta Angle between two generic 3D vectors
+     * Calculate the angle (theta in the formula below) between two vectors
      * @param[in] v1  Vector v1
      * @param[in] v2  Vector v2
-     * \return   Angle between the two vectors
+     * \return    Angle between the two vectors
      * \f[ \theta = \cos ^{-1} \frac { \vec{v1} \cdot \vec{v2} }{ | \vec{v1} | | \vec{v2} | } \f]
      *
-     * Same implementation as in the ROOT documentation in Math::VectorUtil:
-     * https://root.cern/doc/master/GenVector_2VectorUtil_8h_source.html#l00169
+     * There is a function with the same name in ROOT::Math::VectorUtil. However, that function only works for 3-vectors as it calls the
+     * ROOT version of CosTheta (see above). Thus, we need a custom version to be able to use it for 2D vectors, too.
      */
     template <class Vector>
     double Angle(const Vector&  v1, const Vector& v2)
@@ -138,20 +139,19 @@ namespace Belle2 {
       return std::acos(CosTheta(v1, v2));
     }
 
-    /** Calculates the part of this vector that is parallel to the given vector
-     *  Adapted from tracking/trackingUtilities/geometry/Vector2D:
-     *  https://gitlab.desy.de/belle2/software/basf2/-/blob/main/tracking/trackingUtilities/geometry/include/Vector2D.h?ref_type=heads#L445
+    /**
+     * Calculates the part of vector v1 that is orthogonal to the vector v2
+     * @param[in] v1  Vector v1
+     * @param[in] v2  Vector v2
+     * \return    Part of v1 that is orthogonal to v2
+     * Adapted from tracking/trackingUtilities/geometry/Vector2D:
+     * https://gitlab.desy.de/belle2/software/basf2/-/blob/main/tracking/trackingUtilities/geometry/include/Vector2D.h?ref_type=heads#L445
      *
-     *  Vector2D orthogonalVector(const Vector2D& relativTo) const
+     * Vector2D orthogonalVector(const Vector2D& relativTo) const
      * {
      *   return relativTo.scaled(relativTo.cross(*this) / relativTo.normSquared()).orthogonal();
      * }
      * with v2 = relativTo and v1 = *this.
-     *
-     * TODO: check whether this can be replaced by ROOT::Math::VectorUtil::PerpVector()
-     * https://root.cern/doc/v628/namespaceROOT_1_1Math_1_1VectorUtil.html#a7503c235c5e71f762d44d253e5f6e9a3
-     * though likely not for the 2D case as we use here.
-     * If we can use it, obviously replace the function and where it's used.
      */
     inline ROOT::Math::XYVector orthogonalVector(const ROOT::Math::XYVector& v1, const ROOT::Math::XYVector& v2)
     {
@@ -160,49 +160,30 @@ namespace Belle2 {
       return ROOT::Math::XYVector(-tmp.Y(), tmp.X());             // = .orthogonal()
     }
 
-    /** Calculates the part of this vector that is parallel to the given vector
-     *  Adapted from tracking/trackingUtilities/geometry/Vector3D:
-     *  https://gitlab.desy.de/belle2/software/basf2/-/blob/main/tracking/trackingUtilities/geometry/include/Vector3D.h?ref_type=heads#L442
+    /**
+     * Calculates the part of this vector that is parallel to the given vector
      *
-     * Vector3D parallelVector(const Vector3D& relativTo) const
+     * Functions with the same name exist in both the Vector2D and Vector3D classes in
+     * tracking/trackingUtilities/geometry, and the implementation for both is
+     * VectorXD parallelVector(const VectorXD& relativTo) const
      * {
      *   return relativTo.scaled(relativTo.dot(*this) / relativTo.normSquared());
      * }
-     * with v2 = relativTo and v1 = *this.
      *
-     * TODO: check whether this can be replaced by ROOT::Math::VectorUtil::ProjVector()
-     * https://root.cern/doc/v628/namespaceROOT_1_1Math_1_1VectorUtil.html#ad067df491cad5594499d9a1860a00f4c
-     * and if so, obviously replace where it is used.
+     * There exists a similar function called ProjVector in ROOT::Math::VectorUtil, but that only works for 3D vectors,
+     * while this implementation also covers 2D vectors.
+     * The existence of this generalised version is mentioned in both the trackingUtilities Vector2D and Vector3D classes in the
+     * documentation of the according parallelVector method.
      */
-    inline ROOT::Math::XYZVector parallelVector(const ROOT::Math::XYZVector& v1, const ROOT::Math::XYZVector& v2)
+    template<class aVector>
+    inline aVector parallelVector(const aVector& v1, const aVector& v2)
     {
-      const double dotp = v1.Dot(v2);                           // = relativTo.dot(*this)
-      const ROOT::Math::XYZVector tmp = v2 * (dotp / v2.Mag2());  // = relativTo.scaled(dotp / relativTo.normSquared())
+      const double v2Mag2 = v2.Mag2();
+      if (v2Mag2 == 0)
+        return aVector(0, 0, 0);
+      const double dotp = v1.Dot(v2);           // = relativTo.dot(*this)
+      const aVector tmp = v2 * (dotp / v2Mag2); // = relativTo.scaled(dotp / relativTo.normSquared())
       return tmp;
-    }
-
-    /// Calculates the component orthogonal to the given vector
-    /** The orthogonal component is the rest of the vector not parallel to \n
-     *  relative to. Since the three dimensional space does not have a unique \n
-     *  orientation given by the vector relative to, the sign of the orthogonal \n
-     *  component is meaningless and is always set to positive
-     *
-     *  Adapted from tracking/trackingUtilities/geometry/Vector3D:
-     *  https://gitlab.desy.de/belle2/software/basf2/-/blob/main/tracking/trackingUtilities/geometry/include/Vector3D.h?ref_type=heads#L436
-     *
-     * Vector3D orthogonalComp(const Vector3D& relativTo) const
-     * {
-     *   return relativTo.cross(*this).norm() / relativTo.norm();
-     * }
-     * with v2 = relativTo and v1 = *this.
-     *
-     * TODO: check whether this can be replaced by ROOT::Math::VectorUtil::Perp()
-     * https://root.cern/doc/v628/namespaceROOT_1_1Math_1_1VectorUtil.html#a1dc4268cc95f8121f1833e24175b0ea2
-     * and if so, obviously replace where it is used.
-     */
-    inline double orthogonalComp(const ROOT::Math::XYZVector& v1, const ROOT::Math::XYZVector& v2)
-    {
-      return v2.Cross(v1).R() / v2.R();
     }
 
   }

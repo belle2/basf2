b4690e48948935f0940a0da5397e96a2e8f1d6ef
Christian Wessel <christian.wessel@desy.de>
2025-08-26 21:06:18 +0200

    Forward declarations in pxd ckf.

--- tracking/ckf/pxd/filters/results/src/PXDResultTruthVarSet.cc
+++ tracking/ckf/pxd/filters/results/src/PXDResultTruthVarSet.cc
@@ -6,6 +6,7 @@
  * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
  **************************************************************************/
 #include <tracking/ckf/pxd/filters/results/PXDResultTruthVarSet.h>
+#include <tracking/ckf/pxd/entities/CKFToPXDResult.h>
 
 #include <tracking/mcMatcher/TrackMatchLookUp.h>
 #include <tracking/dataobjects/RecoTrack.h>

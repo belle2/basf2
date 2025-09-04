bbb69810f9cf9ab0fec19d020f60de05979a6fab
Vidya Sagar Vobbilisetti <vidya.vobbilisetti@pi.infn.it>
2025-09-04 18:20:27 +0900

    Update checksums

--- vtx/dataobjects/include/linkdef.h
+++ vtx/dataobjects/include/linkdef.h
@@ -5,8 +5,8 @@
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
-#pragma link C++ class Belle2::VTXSimHit+; // checksum=0x1d1e99da, version=3
-#pragma link C++ class Belle2::VTXTrueHit+; // checksum=0xbd1c2a4f, version=6
+#pragma link C++ class Belle2::VTXSimHit+; // checksum=0xe2071f54, version=3
+#pragma link C++ class Belle2::VTXTrueHit+; // checksum=0x5fabf2c7, version=6
 #pragma link C++ class Belle2::VTXDigit+; // checksum=0x403cda2f, version=5
 #pragma link C++ class Belle2::VTXCluster+; // checksum=0x6faf1b6b, version=5
 

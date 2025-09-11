39daee3b7b632f3703fac697dee12c6d08ed14cd
Giulio Dujany <giulio.dujany@iphc.cnrs.fr>
2025-09-11 12:04:46 +0200

    Merge branch 'feature/caf_option_update' into 'main'
    add option to change batch sizes of waveform calibration
    
    See merge request belle2/software/basf2!4683
    
    (cherry picked from commit e04ba85e3dc037836f9c21125bad387069ad887d)
    
    d45cd802 add options to change btch sizes, set default to one
    886e85e1 simplified code
    bfcc7c69 fix typo
    
    Co-authored-by: Belle II Software <b2soft@mail.desy.de>

--- calibration/scripts/prompt/calibrations/caf_ecl_WaveformTemplates.py
+++ calibration/scripts/prompt/calibrations/caf_ecl_WaveformTemplates.py
@@ -30,7 +30,8 @@ settings = CalibrationSettings(
     depends_on=[],
     expert_config={
         "C2_MinEnergyThreshold": 2.0,
-        "nFilesCollector": 50
+        "nFilesCollector": 50,
+        "nParallelAlgos": 1
     },
     produced_payloads=["ECLWaveformTemplate"])
 
@@ -82,18 +83,21 @@ def get_calibrations(input_data, **kwargs):
     algos_C3 = []
     collectors_C2 = []
 
-    batchsize = 100
-    nbatches = 88
+    batches = expert_config["nParallelAlgos"]
+    nCrystals = 8736
+    batchsize = int(nCrystals / batches)
+    if (batches * batchsize < nCrystals):
+        batchsize = batchsize + 1
 
     # keep option to run in parallel
-    for i in range(0, nbatches):
+    for i in range(0, batches):
 
         lowLimit = (batchsize*i)+1
 
         highLimit = (batchsize*(i+1))
 
-        if (highLimit > 8736):
-            highLimit = 8736
+        if (highLimit > nCrystals):
+            highLimit = nCrystals
 
         print("lowLimit,highLimit", lowLimit, highLimit)
 

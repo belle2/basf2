d3312584a500cd5949a0c8e8b6798a50e66b0b69
Frank Meier <frank.meier@duke.edu>
2025-10-07 13:42:22 -0400

    add copyright header and remove doxygen suppression, which isn't necessary

--- generators/validation/PhokharaEvtgenAnalyze.py
+++ generators/validation/PhokharaEvtgenAnalyze.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 """
 <header>
     <input>PhokharaEvtgenData.root</input>
@@ -100,8 +108,6 @@ class PhokharaEvtgenAnalysisModule(b2.Module):
         self.output_file.Close()
 
 
-#: \cond Doxygen_suppress
-
 # Input.
 root_input = b2.register_module('RootInput')
 root_input.param('inputFileName', 'PhokharaEvtgenData.root')
@@ -111,8 +117,6 @@ phokhara_evtgen = PhokharaEvtgenAnalysisModule()
 
 # Create main path.
 main = b2.create_path()
-
-#: \endcond
 
 # Add modules to main path
 main.add_module(root_input)
--- generators/validation/PhokharaEvtgenGenerate.py
+++ generators/validation/PhokharaEvtgenGenerate.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+##########################################################################
+# basf2 (Belle II Analysis Software Framework)                           #
+# Author: The Belle II Collaboration                                     #
+#                                                                        #
+# See git log for contributors and copyright holders.                    #
+# This file is licensed under LGPL-3.0, see LICENSE.md.                  #
+##########################################################################
+
 """
 <header>
     <output>PhokharaEvtgenData.root</output>
@@ -18,8 +26,6 @@ basf2.set_log_level(basf2.LogLevel.INFO)
 # Random seed.
 basf2.set_random_seed(12345)
 
-#: \cond Doxygen_suppress
-
 # Create the main path.
 main = basf2.create_path()
 
@@ -36,8 +42,6 @@ generators.add_phokhara_evtgen_combination(
 output = basf2.register_module('RootOutput')
 output.param('outputFileName', 'PhokharaEvtgenData.root')
 main.add_module(output)
-
-#: \endcond
 
 # Progress.
 main.add_module('Progress')

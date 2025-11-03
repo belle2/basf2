fb99d7cfa10f4c8dc0d5f294d57faaafa05348a6
Vidya Sagar Vobbilisetti <vidya.vobbilisetti@pi.infn.it>
2025-11-03 09:01:20 +0900

    Use dedicated add_hlt_passthrough function

--- hlt/operation/ringbuffer/hlt/passthrough.py
+++ hlt/operation/ringbuffer/hlt/passthrough.py
@@ -9,14 +9,12 @@
 ##########################################################################
 import basf2
 from softwaretrigger import constants
-from softwaretrigger.processing import setup_basf2_and_db, start_path, finalize_path
-from pxd import add_pxd_percentframe_phase3_early, add_roi_payload_assembler
+from softwaretrigger.processing import setup_basf2_and_db, start_path, finalize_path, add_hlt_passthrough
 
 args = setup_basf2_and_db()
 
 path = start_path(args, location=constants.Location.hlt)
-add_pxd_percentframe_phase3_early(path, fraction=0.1, random_position=True)
-add_roi_payload_assembler(path, ignore_hlt_decision=True)
+add_hlt_passthrough(path)
 finalize_path(path, args, location=constants.Location.hlt)
 
 basf2.print_path(path)

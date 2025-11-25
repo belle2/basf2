0e828c26daff7ef44d1bcc5f5999a5b7cbe55ee9
Benjamin Schwenker <benjamin.schwenker@phys.uni-goettingen.de>
2025-11-25 17:47:40 +0100

    renamed tensorflow_tflat_model.py to model.py

--- analysis/scripts/tflat/__init__.py
+++ analysis/scripts/tflat/__init__.py
@@ -12,4 +12,4 @@
 Transformer FlavorTagger (TFlat) scripts for Belle II
 """
 
-__all__ = ['FlavorTagger', 'tensorflow_tflat_model', 'utils', 'config']
+__all__ = ['FlavorTagger', 'model', 'utils', 'config']

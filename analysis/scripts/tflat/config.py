978a14c99733bd3af4ef96d7f135e7f329dc8db2
Benjamin Schwenker <benjamin.schwenker@phys.uni-goettingen.de>
2025-11-25 17:52:57 +0100

    re-add isPXDOn variable

--- analysis/scripts/tflat/config.py
+++ analysis/scripts/tflat/config.py
@@ -65,7 +65,7 @@ config.update({  # Input lists
         'countInList(gamma:tflat)/8',
         'countInList(pi+:tflat)/6',
         'NumberOfKShortsInRoe',
-        # 'isPXDOn',
+        'isPXDOn',
         'ptTracksRoe('+maskName+')',
     ],
 })

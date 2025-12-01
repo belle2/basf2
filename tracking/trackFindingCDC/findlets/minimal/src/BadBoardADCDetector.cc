de2130b5943cc7d8dbfa6ea7686b850cfef7d1d2
Sasha Glazov <alexander.glazov@desy.de>
2025-12-01 18:28:17 +0100

    Initial detector

---
+++ tracking/trackFindingCDC/findlets/minimal/src/BadBoardADCDetector.cc
@@ -0,0 +1,63 @@
+/**************************************************************************
+ * basf2 (Belle II Analysis Software Framework)                           *
+ * Author: The Belle II Collaboration                                     *
+ *                                                                        *
+ * See git log for contributors and copyright holders.                    *
+ * This file is licensed under LGPL-3.0, see LICENSE.md.                  *
+ **************************************************************************/
+#include <tracking/trackFindingCDC/findlets/minimal/BadBoardADCDetector.h>
+
+#include <tracking/trackFindingCDC/eventdata/hits/CDCWireHit.h>
+#include <cdc/dataobjects/CDCHit.h>
+
+#include <tracking/trackFindingCDC/utilities/StringManipulation.h>
+#include <framework/core/ModuleParamList.templateDetails.h>
+#include <cdc/geometry/CDCGeometryPar.h>
+
+#include <map>
+
+using namespace Belle2;
+using namespace TrackFindingCDC;
+
+BadBoardADCDetector::BadBoardADCDetector()
+{
+}
+
+std::string BadBoardADCDetector::getDescription()
+{
+  return "Detect boards with bad ADC values and mark hits accordingly";
+}
+
+void BadBoardADCDetector::exposeParameters(ModuleParamList* moduleParamList,
+                                           const std::string& prefix)
+{
+  moduleParamList->addParameter(prefixed(prefix, "badADCaverageMin"),
+                                m_badADCaverageMin,
+                                "Minimal value of average ADC to consider board bad",
+                                m_badADCaverageMin);
+}
+
+void BadBoardADCDetector::apply(std::vector<CDCWireHit>& wireHits)
+{
+  CDC::CDCGeometryPar& geometryPar = CDC::CDCGeometryPar::Instance();
+  /// first loop: average ADC per board
+  std::map <int, double> BoardADC;
+  std::map <int, int> BoardCount;
+  for (auto& wireHit : wireHits) {
+    auto board = geometryPar.getBoardID(wireHit.getWireID());
+    BoardCount[board] += 1;
+    BoardADC[board] += (*wireHit.getHit()).getADCCount();
+  };
+  /// now compute the average:
+  for (auto& pair  : BoardADC) {
+    int board = pair.first;
+    BoardADC[board] /= BoardCount[board];
+  }
+
+  /// second loop, set flag it board is problematic:
+  for (auto& wireHit : wireHits) {
+    auto board = geometryPar.getBoardID(wireHit.getWireID());
+    if (BoardADC[board] > m_badADCaverageMin)
+      wireHit->setBoardWithBadADCFlag();
+  }
+}

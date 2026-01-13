fca2253e83b70bbb8e84593ce2e932192777f0b6
LEJON William <william.lejon@physics.uu.se>
2026-01-13 22:03:49 +0900

    I have made lots of changes in order to get the script to run.

--- tracking/scripts/tracking_mva_filter_payloads/write_tracking_mva_filter_payloads_to_db.py
+++ tracking/scripts/tracking_mva_filter_payloads/write_tracking_mva_filter_payloads_to_db.py
@@ -10,14 +10,19 @@
 
 from ROOT import Belle2
 import basf2
+import os
+# from hashlib import sha256
 
 
-def write_tracking_mva_filter_payloads_to_db(dbobj_name: str = None,
-                                             iovList=(0, 0, 0, 0),
-                                             weightfile_identifier: str = None,
-                                             cut_value: float = None):
+def write_tracking_mva_filter_payloads_to_db(dbobj_name: str,
+                                             iovList: tuple[int, int, int, int],
+                                             weightfile_identifier: str,
+                                             cut_value: float) -> str | None:
     """
     This function creates a database entry for a DBObject for MVA weightfiles with a given set of IoVs etc.
+    *** WARNING: This function will append a hashed number of the weightfile_identifier and cut_value to the DBObject name
+    to avoid overwriting existing different entries in the local DB. ***
+    :returns the name of the created DBObject, or None if something went wrong.
     :param dbobj_name Name of the DBObject to identify it in the DB
     :param iovList List of IoVs for which the payload is valid
     :param weightfile_identifier Name of the weightfile which already is on the DB
@@ -42,8 +47,68 @@ def write_tracking_mva_filter_payloads_to_db(dbobj_name: str = None,
     dbobj.setIdentifierName(weightfile_identifier)
     dbobj.setCutValue(cut_value)
 
+    if weightfile_identifier.endswith(".xml"):
+        with open(weightfile_identifier) as f:
+            weight_file_content = f.read()
+        weightfile_name = write_mva_weightfile_content_to_db(dbobj_name + "_weightfile", weight_file_content, iovList)
+        dbobj.setIdentifierName(weightfile_name)
     # write db object to 'localdb/'
-    Belle2.Database.Instance().storeData(dbobj_name, dbobj, iov)
+    dbobj_new_name = dbobj_name  # + "_" + sha256((weightfile_identifier + str(cut_value)).encode()).hexdigest()[:8]
+    Belle2.Database.Instance().storeData(dbobj_new_name, dbobj, iov)
+    return dbobj_new_name
+
+
+def write_mva_weightfile_content_to_db(dbobj_name: str, content: str, iovList=(0, 0, 0, 0)):
+    """Write a weightfile to database, e.g. for use in the tracking MVA filters
+    *** WARNING: This function will append a hashed number of the content to the DBObject name
+    to avoid overwriting existing different entries in the local DB. ***
+    :param dbobj_name Name of the DBObject to identify it in the DB
+    :param content Content of the weightfile to be written to the DB, in the format of xml
+    :param iovList List of IoVs for which the payload is valid
+    """
+    # create the iov
+    iov = Belle2.IntervalOfValidity(*iovList)
+
+    # create the DBObject and set the content
+    db_obj = Belle2.DatabaseRepresentationOfWeightfile()
+    db_obj.m_data = content
+
+    # write db object to 'localdb/'
+    dbobj_new_name = dbobj_name  # + "_" + sha256(content.encode()).hexdigest()[:8]
+    Belle2.Database.Instance().storeData(dbobj_new_name, db_obj, iov)
+    return dbobj_new_name
+
+
+def get_unique_dbobj_name(dbobj_name: str) -> str:
+    """
+    Check if dbobj_name exists in localdb/database.txt and append a number if needed.
+    :param dbobj_name Name of the DBObject to check
+    :return Modified name that is unique in the database
+    """
+
+    db_file = "localdb/database.txt"
+    if not os.path.exists(db_file):
+        return dbobj_name
+
+    existing_names = set()
+    try:
+        with open(db_file, 'r') as f:
+            for line in f:
+                parts = line.strip().split()
+                if parts:
+                    existing_names.add(parts[0])
+    except Exception as e:
+        print(e)
+        return dbobj_name
+
+    if dbobj_name not in existing_names:
+        return dbobj_name
+
+    counter = 1
+    while f"{dbobj_name}_{counter}" in existing_names:
+        counter += 1
+
+    return f"{dbobj_name}_{counter}"
 
 
 if __name__ == "__main__":

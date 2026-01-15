a367be9ccfde99decf4776bdafbff143dd8b8188
BAGHEL Kumar Naveen <naveen.baghel@louisville.edu>
2026-01-15 14:25:14 +0900

    intial commit for setting Revo9DCArrivalTime in unpacker

--- klm/modules/KLMUnpacker/src/KLMUnpackerModule.cc
+++ klm/modules/KLMUnpacker/src/KLMUnpackerModule.cc
@@ -108,6 +108,7 @@ void KLMUnpackerModule::createDigit(
     std::pair<int, double> rpcTimes =
       m_Time->getRPCTimes(raw->getCTime(), raw->getTDC(), triggerTime);
     klmDigit->setTime(rpcTimes.second);
+    klmDigit->setRevo9DCArrivalTime(rpcTimes.first);
   } else {
     /*
      * For scintillator hits, store the ctime relative to the event header's

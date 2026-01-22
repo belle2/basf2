65d9a21f9c8284be9bd7fc89099a62491966ff97
Vidya Sagar VOBBILISETTI <vidya.vobbilisetti@pi.infn.it>
2026-01-22 21:25:29 +0900

    Pin pandas docs to v2.2

--- site_scons/sphinx/conf.py
+++ site_scons/sphinx/conf.py
@@ -360,7 +360,7 @@ texinfo_documents = [
 intersphinx_mapping = {'python': ('https://docs.python.org/3.11/', None),
                        'numpy': ('https://numpy.org/doc/stable/', None),
                        'scipy': ('https://docs.scipy.org/doc/scipy/', None),
-                       'pandas': ('https://pandas.pydata.org/docs/', None),
+                       'pandas': ('https://pandas.pydata.org/pandas-docs/version/2.2/', None),
                        'matplotlib': ('https://matplotlib.org/stable/', None),
                        'uproot': ('https://uproot.readthedocs.io/en/stable/', None)}
 

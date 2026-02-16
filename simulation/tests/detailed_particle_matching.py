37bd9e04fdd7867410acb12ca9e4187d6495d69f
Jonas Eppelt <jonas.eppelt@kit.edu>
2026-02-16 13:10:38 +0100

    using add_module over regsiter_module

--- simulation/tests/detailed_particle_matching.py
+++ simulation/tests/detailed_particle_matching.py
@@ -20,35 +20,26 @@ import basf2 as b2
 b2.logging.log_level = b2.LogLevel.WARNING
 
 
-# Register modules
-eventinfosetter = b2.register_module('EventInfoSetter')
-eventinfosetter.param('evtNumList', [5])  # Run 5 events
-
-gearbox = b2.register_module('Gearbox')
-geometry = b2.register_module('Geometry')
-pgun = b2.register_module('ParticleGun')
-pgun.param('pdgCodes', [11])
-pgun.param('momentumParams', [1.0, 2.0])
-
-# Configure FullSim with the new parameters
-full_sim = b2.register_module('FullSim')
-full_sim.param('DetailedParticleMatching', True)
-full_sim.param('RegionRho', 125.0)
-full_sim.param('RegionZForward', 196.0)
-full_sim.param('RegionZBackward', -102.0)
-full_sim.param('KineticEnergyThreshold', 0.5)
-full_sim.param('DistanceThreshold', 1.0)
-full_sim.param('UseIsEM', True)
-full_sim.param('UseIsNuclei', False)
-full_sim.param('UseSeenInECL', False)
-
 # Create path
 main = b2.create_path()
-main.add_module(eventinfosetter)
-main.add_module(gearbox)
-main.add_module(geometry)
-main.add_module(pgun)
-main.add_module(full_sim)
+
+# Add modules
+main.add_module('EventInfoSetter', evtNumList=[5])  # Run 5 events
+main.add_module('Gearbox')
+main.add_module('Geometry')
+main.add_module('ParticleGun', pdgCodes=[11], momentumParams=[1.0, 2.0])
+
+# Configure FullSim with the new parameters
+main.add_module('FullSim',
+                DetailedParticleMatching=True,
+                RegionRho=125.0,
+                RegionZForward=196.0,
+                RegionZBackward=-102.0,
+                KineticEnergyThreshold=0.5,
+                DistanceThreshold=1.0,
+                UseIsEM=True,
+                UseIsNuclei=False,
+                UseSeenInECL=False)
 
 # Process
 b2.process(main)

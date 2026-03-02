e8aa944b7d4ecf7fa94b614b1ad781086f0474fc
Vidya Sagar Vobbilisetti <vidya.vobbilisetti@pi.infn.it>
2026-03-02 23:59:30 +0100

    Adjust alignment

--- analysis/variables/src/ECLVariables.cc
+++ analysis/variables/src/ECLVariables.cc
@@ -1963,15 +1963,14 @@ during the calculation of the `distanceToMcKl` variable.
 .. warning::
     This requires the `getNeutralHadronGeomMatches` function to be used.
 )DOC");
-  REGISTER_VARIABLE("clusterTimeNorm90", eclClusterTimeNorm90,
-  R"DOC(
-  Returns cluster's timing normalized such that :math:`90\%` of real photons will 
-  have :math:`|\text{clusterTimeNorm90}| < 1`. Normalization depends on energy, background
-  level, and cellID, and differs for data and MC. Calculated only for crystals within the CDC acceptance, :math:`161 <= |\text{clusterCellID}| <= 8608`; outside this region, :math:`1.5*\text{clusterTiming}/\text{clusterErrorTiming}` is returned. Typical requirement is :math:`|\text{clusterTimeNorm90}|<3`. 
+  REGISTER_VARIABLE("clusterTimeNorm90", eclClusterTimeNorm90,R"DOC(
+Returns cluster's timing normalized such that :math:`90\%` of real photons will 
+have :math:`|\text{clusterTimeNorm90}| < 1`. Normalization depends on energy, background
+level, and cellID, and differs for data and MC. Calculated only for crystals within the CDC acceptance, :math:`161 <= |\text{clusterCellID}| <= 8608`; outside this region, :math:`1.5*\text{clusterTiming}/\text{clusterErrorTiming}` is returned. Typical requirement is :math:`|\text{clusterTimeNorm90}|<3`. 
 
 Note: the required payloads are stored in the neutrals global tag. Please find the latest recommendation using :ref:`b2help-recommendation`.
-)DOC",
-                      "dimensionless");
+
+)DOC", "dimensionless");
 
   }
 }

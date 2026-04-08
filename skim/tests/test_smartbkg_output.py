a4a223ded70cce756524336cabaec116f9697209
D.Giesegh <d.giesegh@campus.lmu.de>
2026-04-08 11:14:49 +0200

    Removed extra root file and use validation data instead

--- skim/tests/test_smartbkg_output.py
+++ skim/tests/test_smartbkg_output.py
@@ -13,7 +13,7 @@ Test that the SmartBackground module runs without error and produces expected re
 """
 
 import unittest
-from basf2 import create_path, find_file
+import basf2 as b2
 import modularAnalysis as ma
 from variables import variables as var
 from skim.WGs.fei import feiHadronic
@@ -30,8 +30,8 @@ def get_smartbkg_path_with_params(file_path, skim, event_type, variable_name):
     test root file.
     """
 
-    path = create_path()
-    ma.inputMdstList([file_path], path=path)
+    path = b2.create_path()
+    ma.inputMdstList([file_path], path=path, entrySequences=["0:9"])
 
     sbg.add_smartbkg_filtering(
         skim=skim,
@@ -58,8 +58,9 @@ class TestSmartbkgOutput(unittest.TestCase):
         Check that SmartBackground fails if provided unknown skim or no event type, succeds
         if passed everything correctly and produces expected predictions on a tiny dataset.
         """
+        b2.conditions.disable_globaltag_replay()
 
-        file_path = b2test_utils.require_file("skim/tests/smartbkg_test_events.mdst.root", py_case=self)
+        file_path = b2.find_file("mdst16.root", "validation", False)
 
         skim1 = feiHadronic()
         skim2 = LowMassOneTrack()
@@ -74,7 +75,10 @@ class TestSmartbkgOutput(unittest.TestCase):
         ]
 
         results_expected = [1, 1, 0]
-        values_expected = [0.06365067, 1., 0.01, 0.5620032, 0.0962249, 0.14172044, 0.01308167, 1., 0.69878286, 1.]
+        values_expected = [
+            0.06416246, 0.57730651, 0.19336239, 0.6606617, 0.428417,
+            1., 0.06399452, 0.07929435, 0.03229678, 0.07054444
+        ]
 
         with b2test_utils.clean_working_directory():
             results = [
@@ -88,7 +92,7 @@ class TestSmartbkgOutput(unittest.TestCase):
                 f"but got {results}."
             )
 
-            output_file_path = find_file("test_predictions.root", silent=True)
+            output_file_path = b2.find_file("test_predictions.root", silent=True)
             self.assertNotEqual(output_file_path, "", msg="Could not find Smart Background module output file.")
 
             file = TFile(output_file_path)

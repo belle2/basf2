4a918fdedd22ea0231473b78a877d82b72784077
staric <marko.staric@ijs.si>
2026-04-09 15:25:49 +0200

    fix cppcheck warnings: part 1

--- background/modules/BeamBkgMixer/include/BeamBkgMixerModule.h
+++ background/modules/BeamBkgMixer/include/BeamBkgMixerModule.h
@@ -15,8 +15,8 @@
 #include <string>
 #include <map>
 
-#include "TChain.h"
-#include "TClonesArray.h"
+#include <TChain.h>
+#include <TClonesArray.h>
 
 
 namespace Belle2 {
@@ -34,32 +34,15 @@ namespace Belle2 {
     BeamBkgMixerModule();
 
     /**
-     * Destructor
-     */
-    virtual ~BeamBkgMixerModule();
-
-    /**
      * Initialize the Module.
      * This method is called at the beginning of data processing.
      */
     virtual void initialize() override;
 
     /**
-     * Called when entering a new run.
-     * Set run dependent things like run header parameters, alignment, etc.
-     */
-    virtual void beginRun() override;
-
-    /**
      * Event processor.
      */
     virtual void event() override;
-
-    /**
-     * End-of-run action.
-     * Save run-related stuff, such as statistics.
-     */
-    virtual void endRun() override;
 
     /**
      * Termination action.

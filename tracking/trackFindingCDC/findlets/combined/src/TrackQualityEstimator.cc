117da68c06f84d5272508cd48eaa296454873aa1
THOMAS Lueck <thomas.lueck@lmu.de>
2026-04-10 23:30:51 +0900

    working prototype

--- tracking/trackFindingCDC/findlets/combined/src/TrackQualityEstimator.cc
+++ tracking/trackFindingCDC/findlets/combined/src/TrackQualityEstimator.cc
@@ -16,6 +16,12 @@
 
 #include <framework/core/ModuleParamList.templateDetails.h>
 
+#include <tracking/trackingUtilities/eventdata/hits/CDCRecoHit3D.h>
+#include <cdc/dataobjects/CDCHit.h>
+#include <cdc/geometry/CDCGeometryPar.h>
+#include <cdc/topology/CDCWire.h>
+
+
 using namespace Belle2;
 using namespace TrackFindingCDC;
 using namespace TrackingUtilities;
@@ -64,6 +70,28 @@ void TrackQualityEstimator::apply(std::vector<CDCTrack>& tracks)
   for (CDCTrack& track : tracks) {
     const double qualityIndicator = m_trackQualityFilter(track);
     track.setQualityIndicator(qualityIndicator);
+
+    if (std::isnan(qualityIndicator)) {
+      // debug track would be deleted
+      auto list = getListOfBadBoardCands(track, 5);
+
+      // debug: dummy vector with bad boards. Simulation did use 62 and 63 as bad boards
+      std::vector<unsigned int> badBoardList = {62, 63};
+
+      std::cout << "list of layer jumps" << std::endl;
+      for (auto& li : list) {
+        std::cout << li.first << " " << li.second << std::endl;
+        if (std::find(badBoardList.begin(), badBoardList.end(), li.first) != badBoardList.end() ||
+            std::find(badBoardList.begin(), badBoardList.end(), li.second) != badBoardList.end()) {
+
+          std::cout << "kept" << std::endl;
+          track.setQualityIndicator(123); // value doesnt matter as all NaN are rejected
+        } else { std::cout << "rejected" << std::endl;}
+      }
+
+
+
+    }
   }
 
   if (m_param_deleteTracks) { // delete track with QI below cut threshold
@@ -76,4 +104,84 @@ void TrackQualityEstimator::apply(std::vector<CDCTrack>& tracks)
     };
     erase_remove_if(tracks, reject);
   }
+}
+
+
+std::vector< std::pair<unsigned short, unsigned short> >
+TrackQualityEstimator::getListOfBadBoardCands(const TrackingUtilities::CDCTrack& aCDCTrack, int minJump)
+{
+
+  CDC::CDCGeometryPar& geometryPar = CDC::CDCGeometryPar::Instance();
+
+  std::cout << "new track" << std::endl;
+  std::vector< std::pair<unsigned short, unsigned short> > list;
+
+  double arcLengthShift = 4.5; //unit cm, correcponding to around 2.25 drift cells (if passing pendicular)
+
+  auto trajectory = aCDCTrack.getStartTrajectory3D();
+
+  Helix localHelix = trajectory.getLocalHelix().helix();
+  auto localOrigin = trajectory.getLocalOrigin();
+
+
+  // detect layer jumps jumping at least minJump layers
+  // Assumes hits are ordered! Need check if that is always true, or sorting without check
+  const CDCRecoHit3D* prevHitPtr = nullptr;
+  for (const CDCRecoHit3D& thisHit : aCDCTrack) {
+    if (not prevHitPtr) {
+      prevHitPtr = &thisHit;
+      continue;
+    }
+
+    int iclayerPrev = ((const CDCHit*)*prevHitPtr)->getICLayer();
+    int iclayerThis = ((const CDCHit*)thisHit)->getICLayer();
+
+    if (abs(iclayerPrev - iclayerThis) >= minJump) {
+      double sPrevious = prevHitPtr->getArcLength2D();
+      double sCurrent = thisHit.getArcLength2D();
+
+      // may want to filter for too high arclength??? observed up to 45cm arclength differences
+      Vector3D pos3DPrev = localHelix.atArcLength2D(sPrevious + arcLengthShift) + localOrigin;
+      Vector3D pos3DThis = localHelix.atArcLength2D(sCurrent - arcLengthShift) + localOrigin;
+
+      // direction in case of backcurling tracks
+      int dir = (iclayerThis - iclayerPrev) / abs(iclayerThis - iclayerPrev);
+      int newlayerThis = iclayerThis - dir * 2;
+      int newlayerPrev = iclayerPrev + dir * 2;
+
+      Belle2::CDC::IWire iWireThis = geometryPar.cellId(newlayerThis, pos3DThis);
+      Belle2::CDC::IWire iWirePrev = geometryPar.cellId(newlayerPrev, pos3DPrev);
+
+      auto boardThis = geometryPar.getBoardID(WireID(newlayerThis, iWireThis));
+      auto boardPrev = geometryPar.getBoardID(WireID(newlayerPrev, iWirePrev));
+
+      if (boardThis != boardPrev) list.push_back({boardThis, boardPrev});
+
+      /*
+      std::cout << "bad boards ? " << boardThis << " " << boardPrev << std::endl;
+      std::cout << "arc length "  << sPrevious << " b " << sCurrent << std::endl;
+      std::cout << "point a " << prevHitPtr->getRecoPos3D() << " point b " << thisHit.getRecoPos3D() << std::endl;
+      std::cout << "extrapolated a " << (localHelix.atArcLength2D(sPrevious) + localOrigin) << " b " << (localHelix.atArcLength2D(sCurrent) + localOrigin)<< std::endl;
+      std::cout << "new hit" << std::endl;
+      std::cout << thisHit.getWire().getISuperLayer() << std::endl;
+      std::cout << thisHit.getWire().getILayer() << std::endl;
+      std::cout << thisHit.getWire().getICLayer() << std::endl;
+      std::cout << thisHit.isAxial() << std::endl;
+      std::cout << "old hit" << std::endl;
+      std::cout << prevHitPtr->getWire().getISuperLayer() << std::endl;
+      std::cout << prevHitPtr->getWire().getILayer() << std::endl;
+      std::cout << prevHitPtr->getWire().getICLayer() << std::endl;
+      std::cout << prevHitPtr->isAxial() << std::endl;
+
+      int boardnumber =  geometryPar.getBoardID( WireID( ((const CDCHit*)thisHit)->getID()) );
+      std::cout << "Board number :  " << boardnumber  << " old board " << geometryPar.getBoardID( WireID( ((const CDCHit*)*prevHitPtr)->getID() ) ) <<  std::endl;
+      */
+
+    }
+
+    prevHitPtr = &thisHit;
+  }
+
+  return list;
+
 }

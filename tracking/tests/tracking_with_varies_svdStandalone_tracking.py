3a38c591428f44417c74dc824d7f727c272a2b5d
HAN Yubo <yubohan@hawaii.edu>
2026-04-10 18:06:53 +0900

    update the test file

--- tracking/tests/tracking_with_varies_svdStandalone_tracking.py
+++ tracking/tests/tracking_with_varies_svdStandalone_tracking.py
@@ -8,21 +8,20 @@
 # This file is licensed under LGPL-3.0, see LICENSE.md.                  #
 ##########################################################################
 
-
+import b2test_utils as b2tu
 from basf2 import set_random_seed, create_path, process
 from simulation import add_simulation
 from tracking import add_tracking_reconstruction
-import logging
 
 
-def main():
+def run_simulation(output_file):
     """Main function to be executed if this script"""
     set_random_seed(12345)
-
     main = create_path()
 
     # specify number of events to be generated
     main.add_module('EventInfoSetter', expList=[0], evtNumList=[5], runList=[1])
+    main.add_module('Progress')
     main.add_module('ParticleGun',
                     pdgCodes=[211],
                     nTracks=1,
@@ -33,15 +32,25 @@ def main():
                     thetaGeneration='fixed',
                     thetaParams=[62.83185])
     add_simulation(main, bkgfiles=None)
-    add_tracking_reconstruction(main, pruneTracks=False, svd_standalone_mode='VXDTF2_and_SVDHough')
-    # add_tracking_reconstruction(main, pruneTracks=False, svd_standalone_mode='SVDHough_and_VXDTF2')
-    # add_tracking_reconstruction(main, pruneTracks=False, svd_standalone_mode='VXDTF2')
-    # add_tracking_reconstruction(main, pruneTracks=False, svd_standalone_mode='SVDHough')
+    main.add_module('RootOutput', outputFileName=output_file)
+    process(main)
 
-    main.add_module('Progress')
+
+def run_tracking(input_file, mode):
+    set_random_seed(12345)
+    main = create_path()
+    main.add_module('RootInput', inputFileName=input_file)
+
+    main.add_module('Gearbox')
+    main.add_module('Geometry')
+    add_tracking_reconstruction(main, pruneTracks=False, svd_standalone_mode=mode)
     process(main)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    main()
+
+    with b2tu.clean_working_directory():
+        dst_file = 'dst.root'
+        b2tu.run_in_subprocess(target=run_simulation, output_file=dst_file)
+        for mode in ['VXDTF2', 'SVDHough', 'VXDTF2_and_SVDHough', 'SVDHough_and_VXDTF2']:
+            b2tu.run_in_subprocess(target=run_tracking, input_file=dst_file, mode=mode)

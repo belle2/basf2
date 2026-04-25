5ed0678c522eac5a939e178c84b77d0a3585ddfb
Rahul <rahul.tiwary@tyl.in2p3.fr>
2026-04-26 01:04:54 +0900

    Fixed import for prefilter

--- hlt/scripts/hlt/__init__.py
+++ hlt/scripts/hlt/__init__.py
@@ -5,3 +5,22 @@
 # See git log for contributors and copyright holders.                    #
 # This file is licensed under LGPL-3.0, see LICENSE.md.                  #
 ##########################################################################
+from softwaretrigger.path_utils import hlt_event_abort
+
+
+def add_prefilter_module(path):
+    """
+    Add the SoftwareTrigger for the HLT prefilter cuts to the given path.
+    Only the calculation of the cuts is implemented here - the cut logic has to be done
+    using the module return value.
+    Save the result of HLTPrefilter lines to HLTDQM.
+    Discard events tagged by HLTPrefilter as injection background or high occupancy.
+    """
+
+    # Always avoid the top-level 'import ROOT'.
+    import ROOT  # noqa
+
+    path.add_module("SoftwareTrigger", baseIdentifier="prefilter")
+    hlt_prefilter_module = path.add_module("TriggerSkim", triggerLines=["software_trigger_cut&prefilter&total_result"])
+    hlt_event_abort(hlt_prefilter_module, "<1", ROOT.Belle2.EventMetaData.c_HLTPrefilterDiscard)
+    path.add_module('StatisticsSummary').set_name('Sum_HLTPrefilter')
--- hlt/softwaretrigger/scripts/softwaretrigger/path_utils.py
+++ hlt/softwaretrigger/scripts/softwaretrigger/path_utils.py
@@ -259,21 +259,3 @@ def hlt_event_abort(module, condition, error_flag):
         p.add_module('StatisticsSummary').set_name('Sum_HLT_Discard')
     elif error_flag == ROOT.Belle2.EventMetaData.c_HLTPrefilterDiscard:
         p.add_module('StatisticsSummary').set_name('Sum_HLTPrefilter_Discard')
-
-
-def add_prefilter_module(path):
-    """
-    Add the SoftwareTrigger for the HLT prefilter cuts to the given path.
-    Only the calculation of the cuts is implemented here - the cut logic has to be done
-    using the module return value.
-    Save the result of HLTPrefilter lines to HLTDQM.
-    Discard events tagged by HLTPrefilter as injection background or high occupancy.
-    """
-
-    # Always avoid the top-level 'import ROOT'.
-    import ROOT  # noqa
-
-    path.add_module("SoftwareTrigger", baseIdentifier="prefilter")
-    hlt_prefilter_module = path.add_module("TriggerSkim", triggerLines=["software_trigger_cut&prefilter&total_result"])
-    hlt_event_abort(hlt_prefilter_module, "<1", ROOT.Belle2.EventMetaData.c_HLTPrefilterDiscard)
-    path.add_module('StatisticsSummary').set_name('Sum_HLTPrefilter')

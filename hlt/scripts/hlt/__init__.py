d3988b5473a4d3e4e2759997985ce856e3347167
Rahul <rahul.tiwary@tyl.in2p3.fr>
2026-04-27 17:07:42 +0900

    Added additional DQM for prefilter

--- hlt/scripts/hlt/__init__.py
+++ hlt/scripts/hlt/__init__.py
@@ -20,7 +20,36 @@ def add_prefilter_module(path):
     # Always avoid the top-level 'import ROOT'.
     import ROOT  # noqa
 
+    # Execute SoftwareTrigger module for prefilter
     path.add_module("SoftwareTrigger", baseIdentifier="prefilter")
+
+    # DQM for prefilter to save decision of individual prefilter lines
+    cutResultIdentifiers = {}
+    hlt_prefilter_lines_in_plot = []
+    from softwaretrigger import prefilter_categories
+    prefilter_cat = [method for method in dir(prefilter_categories) if method.startswith('__') is False if method != 'RESULTS']
+
+    def read_lines(category):
+        return [i.split(" ", 1)[1].replace(" ", "_") for i in category]
+    for i in prefilter_cat:
+        hlt_prefilter_lines_in_plot += read_lines(getattr(prefilter_categories, i))
+
+    cutResultIdentifiers["prefilter"] = {"prefilter": hlt_prefilter_lines_in_plot}
+
+    path.add_module("SoftwareTriggerHLTDQM",
+                    cutResultIdentifiers=cutResultIdentifiers,
+                    createHLTUnitHistograms=False,
+                    createTotalResultHistograms=False,
+                    createErrorFlagHistograms=False,
+                    createExpRunEventHistograms=False,
+                    histogramDirectoryName="softwaretrigger_before_prefilter",
+                    pathLocation="before prefilter")
+
+    # Get total_result for prefilter
     hlt_prefilter_module = path.add_module("TriggerSkim", triggerLines=["software_trigger_cut&prefilter&total_result"])
+
+    # Filter events rejected by prefilter, only save event metadata
     hlt_event_abort(hlt_prefilter_module, "<1", ROOT.Belle2.EventMetaData.c_HLTPrefilterDiscard)
+
+    # Save module statistics
     path.add_module('StatisticsSummary').set_name('Sum_HLTPrefilter')

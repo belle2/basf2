613d04182f6323db81c395c52d309b8d70856d6c
Bjoern Spruck <bspruck@uni-mainz.de>
2026-04-29 08:57:08 +0200

    Edit DelayDQMModule.cc

--- dqm/modules/src/DelayDQMModule.cc
+++ dqm/modules/src/DelayDQMModule.cc
@@ -34,7 +34,7 @@ DelayDQMModule::DelayDQMModule() : HistoModule()
   addParam("useMeta", m_useMeta, "Use time from EvtMetadata or FTSW", false);
 }
 
-static void DelayDQMModule::BinLogX(TH1* h)
+void DelayDQMModule::BinLogX(TH1* h)
 {
 // function copied from root-talk
 

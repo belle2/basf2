5da102bfc3689f27a2935fae169b274aeb0f39e1
Christopher Hearty <hearty@physics.ubc.ca>
2026-06-03 04:04:15 +0900

    enable use of bhabhaGamma payload

--- ecl/calibration/src/eclMergingCrystalTimingAlgorithm.cc
+++ ecl/calibration/src/eclMergingCrystalTimingAlgorithm.cc
@@ -243,7 +243,7 @@ CalibrationAlgorithm::EResult eclMergingCrystalTimingAlgorithm::calibrate()
   for (int ic = 0; ic < m_numCrystals; ic++) {
     /* Define a good bhabha calibration value.  This is the
        uncertainty on the calibration constant (e.g. fit mean),
-       not the gaussian width of the timing distribution for
+       not the guassian width of the timing distribution for
        that crystal (i.e. resolution).
 
        Uncertainty stored as ticks so converted to ns for
@@ -340,11 +340,39 @@ CalibrationAlgorithm::EResult eclMergingCrystalTimingAlgorithm::calibrate()
                                         meanGoodCrystalCalibPerCrate_cosmic[crate_id_from_crystal - 1] ;
   }
 
+  //------------------------------------------------------------------------
+  //..For each crate, get the average Bhabha calibration using only crystals
+  //  with good Bhabha calibrations and cellID [673, 8160], the region with
+  //  consistently high statistics.
+  const int firstHiStatCellID = 673;
+  const int lastHiStatCellID = 8160;
+  int nGoodBhabhaPerCrate[m_numCrates] = {};
+  double averageBhabhaCalibPerCrate[m_numCrates] = {};
+
+  //..Find the good crystals and sum
+  for (int cellID = firstHiStatCellID; cellID <= lastHiStatCellID; cellID++) {
+    int crysID = cellID - 1;
+    int crateID = crystalMapper->getCrateID(cellID);
+    if (bhabhaCalibGoodQuality[crysID]) {
+      nGoodBhabhaPerCrate[crateID - 1]++;
+      averageBhabhaCalibPerCrate[crateID - 1] += bhabhaCalib[crysID];
+    }
+  }
+
+  //..Crate average
+  for (int crateID = 1; crateID <= m_numCrates; crateID++) {
+    if (nGoodBhabhaPerCrate[crateID - 1] != 0) {
+      averageBhabhaCalibPerCrate[crateID - 1] /= nGoodBhabhaPerCrate[crateID - 1];
+    }
+    B2INFO("crateID " << crateID << " nGood " << nGoodBhabhaPerCrate[crateID - 1] << " average Bhabha calib " <<
+           averageBhabhaCalibPerCrate[crateID - 1]);
+  }
+
 
   //------------------------------------------------------------------------
   /** Calculate the new ECLCrystalTimeOffset from existing payloads.  Use
       the bhabha calibrations as the default but then resort to the
-      radiative bhabha calibrations if the bhabha calibrations are not
+      radiative bhabha calibrations if the bhabha calibraitons are not
       good enough (most likely to be when the electrons are outside
       the CDC acceptance).  If the radiative bhabha calibrations are not
       good or unavailable then use the cosmic calibration values, shifted
@@ -397,7 +425,7 @@ CalibrationAlgorithm::EResult eclMergingCrystalTimingAlgorithm::calibrate()
       /* If there is a radiative bhabha calibration for this crystal then
          use it otherwise use the cosmic calibration */
       if (bhabhaGammaCalib[ic] != 0 || bhabhaGammaCalibGoodQuality[ic]) {
-        newCalib[ic] = bhabhaGammaCalib[ic] ;
+        newCalib[ic] = bhabhaGammaCalib[ic] + averageBhabhaCalibPerCrate[crate_id_from_crystal - 1];
         newCalibUnc[ic] = fabs(bhabhaGammaCalibUnc[ic]);
         whichCalibUsed = "bhabhaGamma";
       } else {
@@ -532,7 +560,7 @@ CalibrationAlgorithm::EResult eclMergingCrystalTimingAlgorithm::calibrate()
 
 
   /* Re-save the new bhabha calibrations to a payload.  The bhabha calibrations have NOT
-     been changed; however, the best way of controlling which payloads get get uploaded to
+     been changed; however, the best way of controling which payloads get get uploaded to
      the GT as part of the prommp calibrations is to save a second copy in this calibration
      directory since we don't want to save all the payloads from the previous directory.
      See the "<calibration>.save_payloads = <True/False>" code in the airflow steering file.*/

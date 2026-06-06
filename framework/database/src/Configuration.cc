7b05f5cddfa5580f0ba0ddf8553745c9004ff3b7
Giacomo De Pietro <giacomo.pietro@kit.edu>
2026-06-06 11:49:00 +0200

    Revert "TUPPR: update default globaltag to prerelease-11-00-00b"
    This reverts commit 87d05c1294e3e912b154888b4b914ebe1329abc3.

--- framework/database/src/Configuration.cc
+++ framework/database/src/Configuration.cc
@@ -21,7 +21,7 @@
 #include <regex>
 
 // Current default globaltag when generating events.
-#define CURRENT_DEFAULT_TAG "prerelease-11-00-00b"
+#define CURRENT_DEFAULT_TAG "main_2026-06-06"
 
 namespace py = boost::python;
 

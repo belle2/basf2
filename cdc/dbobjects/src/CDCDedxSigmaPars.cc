89a780d6415e292699349e08cb73961943c982d7
Christian Wessel <christian.wessel@desy.de>
2026-06-25 19:34:27 +0200

    Fix: remove unnecessary parameter.

--- cdc/dbobjects/src/CDCDedxSigmaPars.cc
+++ cdc/dbobjects/src/CDCDedxSigmaPars.cc
@@ -25,7 +25,7 @@ namespace Belle2 {
       } else if (par[0] == 2) { // return nhit or sin(theta) parameterization
         f = par[1] * x * x * x * x + par[2] * x * x * x + par[3] * x * x + par[4] * x + par[5];
       } else if (par[0] == 3) { // return cos(theta) parameterization
-        f = par[1] * std::exp(-0.5 * square(((x - par[2]) / par[3]), 2)) +
+        f = par[1] * std::exp(-0.5 * square(((x - par[2]) / par[3]))) +
             par[4] * (pow5(x) * x) + par[5] * pow5(x) + par[6] * pow4(x) +
             par[7] * x * x * x + par[8] * x * x + par[9] * x + par[10];
       }

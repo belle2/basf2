8cb6c60dea586ced7d1d7baf2200e529441b6098
Giulio Dujany <giulio.dujany@iphc.cnrs.fr>
2026-07-16 16:26:49 +0200

    Edit caf_ecl_WaveformTemplates.py

--- calibration/scripts/prompt/calibrations/caf_ecl_WaveformTemplates.py
+++ calibration/scripts/prompt/calibrations/caf_ecl_WaveformTemplates.py
@@ -31,8 +31,7 @@ settings = CalibrationSettings(
     expert_config={
         "C2_MinEnergyThreshold": 2.0,
         "nFilesCollector": 50,
-        "nParallelAlgos": 1,
-        "EfsiDimsSlsiOdts": 100
+        "nParallelAlgos": 10
     },
     produced_payloads=["ECLDigitWaveformParameters"])
 

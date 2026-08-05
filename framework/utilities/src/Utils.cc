8ccc2cbbc0ee9f2c3a4d38f38fd9551b53334d10
D.Giesegh <d.giesegh@campus.lmu.de>
2026-08-05 10:54:19 +0200

    Added additional check of errno to getStatmSize

--- framework/utilities/src/Utils.cc
+++ framework/utilities/src/Utils.cc
@@ -63,10 +63,19 @@ namespace {
       B2WARNING("getStatmSize failed to open /proc/pid/statm");
       return std::make_pair(0UL, 0UL);
     }
+    errno = 0;
     rewind(stream);
+    if (errno != 0) {
+      B2WARNING("getStatmSize failed to rewind open stream to /proc/pid/statm with error: " << std::strerror(errno));
+      return std::make_pair(0UL, 0UL);
+    }
     int nReadValues = fscanf(stream, "%lu %lu", &vmSizePages, &rssPages);
     if (nReadValues != 2) {
-      B2WARNING("getStatmSize failed to read virtual and resident memory from /proc/pid/statm");
+      if (errno != 0) {
+        B2WARNING("getStatmSize failed to read virtual and resident memory from /proc/pid/statm with error: " << std::strerror(errno));
+      } else {
+        B2WARNING("getStatmSize failed to read virtual and resident memory from /proc/pid/statm");
+      }
       return std::make_pair(0UL, 0UL);
     }
     return std::make_pair(vmSizePages * pageSizeKb, rssPages * pageSizeKb);
